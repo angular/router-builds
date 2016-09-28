d2ad139b05e0aca95ce772964e0c750910a2f0d6
Rado Kirov <rkirov@gmail.com>
2016-09-28 00:24:09 +0000

    1564042 fix(ngc): allow ReflectorHost passed as argument to CodeGenerator#create (#11951)

--- src/private_export.d.ts
+++ src/private_export.d.ts
@@ -1,3 +1,10 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 import { ROUTES } from './router_config_loader';
 import { ROUTER_PROVIDERS } from './router_module';
 import { flatten } from './utils/collection';
