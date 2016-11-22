9b8ca9be1206df81e4774884804a2ba5ed0a5ef3
Chuck Jazdzewski <chuckj@google.com>
2016-11-22 23:34:00 +0000

    4553948 chore(release): cut the 2.3.0-beta.1 release

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v3.3.0-beta.0-86ffa88
+ * @license Angular v3.3.0-beta.1-4553948
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
