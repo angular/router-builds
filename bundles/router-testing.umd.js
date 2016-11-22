749a87e48562eead301080fcde9b83795c9240e4
GitHub <noreply@github.com>
2016-11-22 21:52:10 +0000

    86ffa88 fix(build): update versions of umd bundles (#13038)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v0.0.0-ROUTERPLACEHOLDER
+ * @license Angular v3.3.0-beta.0-86ffa88
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
