efd213cda4b8b2f8a5a72759802da81b5ce9ae91
vsavkin <avix1000@gmail.com>
2016-11-28 22:49:13 +0000

    94b8612 Fix(http): invalidStateError response body

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v3.3.0-beta.0-f5c8e09
+ * @license Angular v3.3.0-beta.0-94b8612
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
