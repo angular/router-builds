e5982c0e140ec3e98f21f9412c660c22abca0939
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-11-30 23:32:10 +0000

    d420080 docs(changelog): add changelog for 2.2.4

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v3.3.0-beta.0-2975d89
+ * @license Angular v3.3.0-beta.0-d420080
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5407,7 +5407,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('3.3.0-beta.0-2975d89');
+  var /** @type {?} */ VERSION = new _angular_core.Version('3.3.0-beta.0-d420080');
 
   exports.VERSION = VERSION;
   exports.RouterLink = RouterLink;
