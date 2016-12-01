c675d3748abac038d9b9f0817b40c3c9218c8a5a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-01 21:38:47 +0000

    25a6da2 refactor(compiler-cli): refactor compiler host parameters (#13147)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v3.3.0-rc.0-5908b66
+ * @license Angular v3.3.0-rc.0-25a6da2
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5407,7 +5407,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('3.3.0-rc.0-5908b66');
+  var /** @type {?} */ VERSION = new _angular_core.Version('3.3.0-rc.0-25a6da2');
 
   exports.VERSION = VERSION;
   exports.RouterLink = RouterLink;
