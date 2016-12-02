88ec1ba1f78d469e02019916c4786e9db25a90b1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-02 18:21:13 +0000

    75d1617 fix(compiler): include the summaries of reexported modules / directives / pipes (#13196)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v3.3.0-rc.0-6c0b65b
+ * @license Angular v3.3.0-rc.0-75d1617
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5405,7 +5405,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('3.3.0-rc.0-6c0b65b');
+  var /** @type {?} */ VERSION = new _angular_core.Version('3.3.0-rc.0-75d1617');
 
   exports.VERSION = VERSION;
   exports.RouterLink = RouterLink;
