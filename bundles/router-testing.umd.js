d5eb3e7a640ac786f2eacda38d4dbf948c604b50
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-05 18:37:50 +0000

    d2e5198 docs(contributing): fix styleguide link and typos (#13198)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v3.3.0-rc.0-6cf7a1b
+ * @license Angular v3.3.0-rc.0-d2e5198
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
