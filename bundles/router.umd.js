80baee4cb8b0986ae437d00ffc7c635186436036
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-05 18:40:11 +0000

    dea5916 docs(developer): add anchor to source code formatting (#13199)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v3.3.0-rc.0-a132287
+ * @license Angular v3.3.0-rc.0-dea5916
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5409,7 +5409,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('3.3.0-rc.0-a132287');
+  var /** @type {?} */ VERSION = new _angular_core.Version('3.3.0-rc.0-dea5916');
 
   exports.VERSION = VERSION;
   exports.RouterLink = RouterLink;
