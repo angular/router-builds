0c258f65d08e1b18828ed8e89e0ad461d9ce5790
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-07 18:19:58 +0000

    cf52284 docs(changelog): fix typo AplicationRef => ApplicationRef (#13284)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v3.3.0-rc.0-4a09c81
+ * @license Angular v3.3.0-rc.0-cf52284
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5432,7 +5432,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('3.3.0-rc.0-4a09c81');
+  var /** @type {?} */ VERSION = new _angular_core.Version('3.3.0-rc.0-cf52284');
 
   var /** @type {?} */ __router_private__ = {
       ROUTER_PROVIDERS: ROUTER_PROVIDERS,
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('3.3.0-rc.0-4a09c81');
+export var /** @type {?} */ VERSION = new Version('3.3.0-rc.0-cf52284');
 //# sourceMappingURL=version.js.map
