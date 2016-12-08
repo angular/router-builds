24f150efbf8f968e72a4ede2ea609c6d2c65dd17
Victor Berchet <victor@suumit.com>
2016-12-08 23:55:31 +0000

    bf93389 doc: update triage owners for language service and router (#13325)

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('3.3.0-4398056');
+export var /** @type {?} */ VERSION = new Version('3.3.0-bf93389');
 //# sourceMappingURL=version.js.map
