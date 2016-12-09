91430db9f02ab125c9b07deb64e96e464ec95949
GitHub <noreply@github.com>
2016-12-09 19:35:47 +0000

    4022173 fix(compiler): fix PR 13322 (#13331)

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('3.3.0-c8baf51');
+export var /** @type {?} */ VERSION = new Version('3.3.0-4022173');
 //# sourceMappingURL=version.js.map
