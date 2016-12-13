d1a5022edfe627b9d03f36987d4ad102f3c0cbb5
Victor Berchet <victor@suumit.com>
2016-12-13 20:47:50 +0000

    d036165 refactor: remove intl from facades (#13404)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v3.3.0-d17e690
+ * @license Angular v3.3.0-d036165
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5421,7 +5421,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('3.3.0-d17e690');
+  var /** @type {?} */ VERSION = new _angular_core.Version('3.3.0-d036165');
 
   var /** @type {?} */ __router_private__ = {
       ROUTER_PROVIDERS: ROUTER_PROVIDERS,
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('3.3.0-d17e690');
+export var /** @type {?} */ VERSION = new Version('3.3.0-d036165');
 //# sourceMappingURL=version.js.map
