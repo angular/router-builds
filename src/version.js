b8f052135f2ce47b24a8094be8e4279c110cab01
Victor Berchet <victor@suumit.com>
2016-12-14 01:57:36 +0000

    a0a0504 refactor: format & lint

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('3.3.0-d62d893');
+export var /** @type {?} */ VERSION = new Version('3.3.0-a0a0504');
 //# sourceMappingURL=version.js.map
