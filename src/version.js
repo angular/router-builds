aaed4166ffe45d146aa36e6ead7b3e07f1ba7655
Igor Minar <iminar@google.com>
2016-12-15 17:29:46 +0000

    33910dd refactor(compiler): store metadata of top level symbols also in summaries (#13289)

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('3.3.1-dbb364e');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-33910dd');
 //# sourceMappingURL=version.js.map
