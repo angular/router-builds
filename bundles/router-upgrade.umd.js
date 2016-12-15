633422efc86e3dd5937e64d995aa8de099c625c7
Victor Berchet <victor@suumit.com>
2016-12-15 02:46:42 +0000

    540b119 fix(form): fix merge errors

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-6cefccb
+ * @license Angular v3.3.1-540b119
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-6cefccb');
+export var /** @type {?} */ VERSION = new Version('3.3.1-540b119');
 //# sourceMappingURL=version.js.map
