9c7ac1d8237ab813cd967f470d697240dc86b7df
Chuck Jazdzewski <chuckj@google.com>
2016-12-15 21:21:39 +0000

    732f446 docs(common): fix ngIf example (#13496)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-f0e0925
+ * @license Angular v4.0.0-beta.0-732f446
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-f0e0925');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-732f446');
 //# sourceMappingURL=version.js.map
