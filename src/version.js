be282bfa12d45d87a092a583404033c6412072a7
Victor Berchet <victor@suumit.com>
2016-12-15 06:13:28 +0000

    dbb364e docs(changelog): minor updates to 2.3.1 changelog

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-01ca2db');
+export var /** @type {?} */ VERSION = new Version('3.3.1-dbb364e');
 //# sourceMappingURL=version.js.map
