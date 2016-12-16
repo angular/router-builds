1594e8ec9ece530151e3991fe358cc1086437ebd
Chuck Jazdzewski <chuckj@google.com>
2016-12-16 23:35:16 +0000

    4568d5d refactor(core): fix typo (#13515)

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-c6e8939');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-4568d5d');
 //# sourceMappingURL=version.js.map
