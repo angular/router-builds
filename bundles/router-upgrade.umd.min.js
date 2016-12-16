a2ae30b7063e17508ce15c6cce0d8c110779a7bc
Chuck Jazdzewski <chuckj@google.com>
2016-12-16 01:20:34 +0000

    55dfa1b test(forms): refactor integration tests to improve speed (#13500)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-0fe3cd9
+ * @license Angular v4.0.0-beta.0-55dfa1b
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-0fe3cd9');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-55dfa1b');
 //# sourceMappingURL=version.js.map
