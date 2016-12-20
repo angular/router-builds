b189260ec86d4bb598bf322eb185d60476871f7c
Chuck Jazdzewski <chuckj@google.com>
2016-12-20 02:19:15 +0000

    6efdf84 docs(changelog): add changelog for 2.4.0

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-171a9bd');
+export var /** @type {?} */ VERSION = new Version('3.4.0-6efdf84');
 //# sourceMappingURL=version.js.map
