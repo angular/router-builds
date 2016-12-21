66955b91d4aa7b7a045605cec91e8a64212bee70
Chuck Jazdzewski <chuckj@google.com>
2016-12-21 00:31:33 +0000

    9da4c25 feat(upgrade): support the `$doCheck()` lifecycle hook in `UpgradeComponent` (#13015)

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-383adc9');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-9da4c25');
 //# sourceMappingURL=version.js.map
