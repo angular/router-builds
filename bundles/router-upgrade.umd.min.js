a575a3ba4164d71487033195f0eb0e985dea49ec
Hans <hans@hansl.ca>
2016-12-22 22:04:59 +0000

    6976903 fix(common): add link to trackBy docs (#13634)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-0448e80
+ * @license Angular v4.0.0-beta.1-6976903
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-0448e80');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-6976903');
 //# sourceMappingURL=version.js.map
