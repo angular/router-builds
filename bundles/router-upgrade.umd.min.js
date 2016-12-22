221963ef621a02f730351f6aac73b8817c6412d7
Chuck Jazdzewski <chuckj@google.com>
2016-12-22 01:10:41 +0000

    f114e40 docs(changelog): add changelog for 4.0.0-beta.1

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-842f52e
+ * @license Angular v4.0.0-beta.1-f114e40
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.0-842f52e');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-f114e40');
 //# sourceMappingURL=version.js.map
