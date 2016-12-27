00f14bd862e3117c80e9c0fcae62d8e0103fbba7
Hans <hans@hansl.ca>
2016-12-27 23:11:04 +0000

    eed8344 chore(tslint): update tslint to 4.x (#13603)

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-e5c4e58');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-eed8344');
 //# sourceMappingURL=version.js.map
