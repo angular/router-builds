0155d74f7517ffd8b218b224568e98ad7a621e8e
Hans <hans@hansl.ca>
2016-12-28 01:11:52 +0000

    b2ae7b6 docs(Core): fix API docs for ContentChild and ViewChildren (#13656)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-7690d02
+ * @license Angular v4.0.0-beta.1-b2ae7b6
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-7690d02');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-b2ae7b6');
 //# sourceMappingURL=version.js.map
