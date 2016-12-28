daa46d2fb678999636941b53bea2a5dbc732b04d
Hans <hans@hansl.ca>
2016-12-28 00:42:10 +0000

    7c21064 fix(compiler): query `<template>` elements before their children. (#13677)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-07e0fce
+ * @license Angular v4.0.0-beta.1-7c21064
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-07e0fce');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-7c21064');
 //# sourceMappingURL=version.js.map
