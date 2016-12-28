51ea5ddca3c35bce11e3814545e1720ae90018ed
Hans <hans@hansl.ca>
2016-12-28 01:31:50 +0000

    6b02b80 fix(compiler): improve error message for undefined providers (#13546)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-2c0c86e
+ * @license Angular v4.0.0-beta.1-6b02b80
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-2c0c86e');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-6b02b80');
 //# sourceMappingURL=version.js.map
