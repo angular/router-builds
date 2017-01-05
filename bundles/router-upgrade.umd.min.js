82e7e653f96326044622c0ef10e156bf94651016
Igor Minar <iminar@google.com>
2017-01-05 19:33:15 +0000

    1bd04e9 refactor: remove unused imports

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-f822f95
+ * @license Angular v4.0.0-beta.1-1bd04e9
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-f822f95');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-1bd04e9');
 //# sourceMappingURL=version.js.map
