a6962314fe4346d1c5f04ac8bf362e30fa1d5470
Igor Minar <iminar@google.com>
2017-01-05 19:49:22 +0000

    8063b0d fix(language-service): support TypeScript 2.1 (#13655)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-21030e9
+ * @license Angular v4.0.0-beta.1-8063b0d
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-21030e9');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.1-8063b0d');
 //# sourceMappingURL=version.js.map
