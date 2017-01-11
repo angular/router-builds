2fb41db2be993ed37f25b905e481cb71bd6706cc
Matias Niemelä <matias@yearofmoo.com>
2017-01-11 02:13:43 +0000

    95cbca2 chore(owners): configure pullapprove.com

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.2-ed32340
+ * @license Angular v4.0.0-beta.2-95cbca2
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5446,7 +5446,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.2-ed32340');
+  var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.2-95cbca2');
 
   var /** @type {?} */ __router_private__ = {
       ROUTER_PROVIDERS: ROUTER_PROVIDERS,
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.2-ed32340');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.2-95cbca2');
 //# sourceMappingURL=version.js.map
