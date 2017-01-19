fe7aa0e8bca81513b560a9f4bca4c4d87f75a168
Miško Hevery <misko@hevery.com>
2017-01-19 00:08:13 +0000

    2d7b3a8 refactor(core): remove an unused import in application_ref (#13901)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.3-523fd84
+ * @license Angular v4.0.0-beta.3-2d7b3a8
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.3-523fd84');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.3-2d7b3a8');
 //# sourceMappingURL=version.js.map
