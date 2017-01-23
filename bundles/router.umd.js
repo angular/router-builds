025d60f5052bcdf4c2954b26e973a4f573f55f8c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-23 19:09:04 +0000

    dff6ee3 ci: validate the message of each new commit as part of the CI linting

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.4-0589f93
+ * @license Angular v4.0.0-beta.4-dff6ee3
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5563,7 +5563,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.4-0589f93');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.4-dff6ee3');
 
     var /** @type {?} */ __router_private__ = {
         ROUTER_PROVIDERS: ROUTER_PROVIDERS,
