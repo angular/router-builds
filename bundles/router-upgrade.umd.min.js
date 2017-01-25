ac24080e94a22165b0127104a16015fc9db40f4a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-25 20:09:21 +0000

    4931a61 docs(core): add docs for `AnimationStyles` and `AnimationKeyframe` (#14107)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.4-a733444
+ * @license Angular v4.0.0-beta.4-4931a61
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-a733444');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-4931a61');
 //# sourceMappingURL=version.js.map
