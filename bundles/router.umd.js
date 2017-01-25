f86db47ad93aa68ff470665de1b2bfebc543ba26
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-25 21:34:59 +0000

    d2859cd style(compiler): run format

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.4-4931a61
+ * @license Angular v4.0.0-beta.4-d2859cd
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5563,7 +5563,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.4-4931a61');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.4-d2859cd');
 
     var /** @type {?} */ __router_private__ = {
         ROUTER_PROVIDERS: ROUTER_PROVIDERS,
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-4931a61');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.4-d2859cd');
 //# sourceMappingURL=version.js.map
