bb7632db4ba06823576dffa5705628fee4304aec
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-26 00:11:56 +0000

    c4e7c08 docs(changelog): add changelog for 4.0.0-beta.5

---
+++ src/version.js
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { Version } from '@angular/core';
+/**
+ * @stable
+ */
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-c4e7c08');
+//# sourceMappingURL=version.js.map
