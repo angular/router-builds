8aa272b6d74ef303a0831346fc51390fa5dcd862
GitHub <noreply@github.com>
2017-01-26 01:58:30 +0000

    05b2b49 fix(compiler): allow empty translations for attributes (#14085)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-3ef73c2
+ * @license Angular v4.0.0-beta.5-05b2b49
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-3ef73c2
+ * @license Angular v4.0.0-beta.5-05b2b49
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5563,7 +5563,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.5-3ef73c2');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.5-05b2b49');
 
     var /** @type {?} */ __router_private__ = {
         ROUTER_PROVIDERS: ROUTER_PROVIDERS,
