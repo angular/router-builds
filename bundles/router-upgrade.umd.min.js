40add67c928710ecd7e936a420ffb1122fc6b3b2
Igor Minar <iminar@google.com>
2017-01-27 08:37:55 +0000

    e130bc1 feat(aio): add initial angular-cli scaffold (#14118)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-b141a22
+ * @license Angular v4.0.0-beta.5-e130bc1
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-b141a22');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-e130bc1');
 //# sourceMappingURL=version.js.map
