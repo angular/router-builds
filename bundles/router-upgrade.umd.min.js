0d36fa96e088379f008091aacb60311242a3bcd2
Igor Minar <iminar@google.com>
2017-01-28 02:41:12 +0000

    24ea3f0 test(compiler): don't print warnings from closure compiler (#14159)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-3368f29
+ * @license Angular v4.0.0-beta.5-24ea3f0
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-3368f29');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.5-24ea3f0');
 //# sourceMappingURL=version.js.map
