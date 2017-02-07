3700a0c5cced1331e74b102f8da6b7a5c96816e9
Igor Minar <iminar@google.com>
2017-02-07 21:16:31 +0000

    2e14130 feat(aio): refactor DocViewer w/ services component builder (#14294)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.6-a378aab
+ * @license Angular v4.0.0-beta.6-2e14130
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.6-a378aab');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.6-2e14130');
 //# sourceMappingURL=version.js.map
