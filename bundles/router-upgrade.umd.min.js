2588a1ba07be13894146838a011e8fa61a69fd0a
Igor Minar <iminar@google.com>
2017-02-09 20:20:18 +0000

    600402d build(aio): big move of docs related files (#14361)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-5e7a2fa
+ * @license Angular v4.0.0-beta.7-600402d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- src/version.js
+++ src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-5e7a2fa');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-600402d');
 //# sourceMappingURL=version.js.map
