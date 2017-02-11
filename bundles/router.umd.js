00ceac281e1ef40456f8e1ec4b4e554ff0bc4028
Misko Hevery <misko@hevery.com>
2017-02-11 05:16:36 +0000

    d3f174a ci: fix validate-commit-messages

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-8dd16bb
+ * @license Angular v4.0.0-beta.7-d3f174a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5626,7 +5626,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-8dd16bb');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-d3f174a');
 
     var /** @type {?} */ __router_private__ = {
         ROUTER_PROVIDERS: ROUTER_PROVIDERS,
