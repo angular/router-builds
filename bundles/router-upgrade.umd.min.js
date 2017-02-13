39e54e26cabc52b6b37df8f01cb82bfd69372b12
Miško Hevery <misko@hevery.com>
2017-02-13 23:08:36 +0000

    221b7a1 build: use `ts-api-guardian` v0.2.1 (#14408)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-d3f174a
+ * @license Angular v4.0.0-beta.7-221b7a1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- src/version.js
+++ src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-d3f174a');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-221b7a1');
 //# sourceMappingURL=version.js.map
