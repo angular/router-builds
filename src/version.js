0cd008e321b006472f6094bb12383b4723d76ae2
GitHub <noreply@github.com>
2017-02-15 16:57:58 +0000

    3a2e5f5 build: update tbosch/vicb in pullapprove

---
+++ src/version.js
@@ -0,0 +1,18 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+/**
+ * @module
+ * @description
+ * Entry point for all public APIs of the common package.
+ */
+import { Version } from '@angular/core';
+/**
+ * @stable
+ */
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-3a2e5f5');
+//# sourceMappingURL=version.js.map
