483c37a2e11a065d6eab2b1f1a6e4b7475185daa
Igor Minar <iminar@google.com>
2017-02-18 07:16:09 +0000

    c4a6263 feat(aio): replace `take(1)` with more semantic `first()` (#14571)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-ab26b65
+ * @license Angular v4.0.0-beta.7-c4a6263
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- src/version.js
+++ src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-ab26b65');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-c4a6263');
 //# sourceMappingURL=version.js.map
