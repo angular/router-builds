44a5eb4bf6be7ba28f605714a1cfa8af51f149b0
Igor Minar <iminar@google.com>
2017-02-18 07:24:57 +0000

    45eac23 feat(aio): fill in NavMap from current doc inventory (#14516)

--- src/version.js
+++ src/version.js
@@ -14,5 +14,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-c4a6263');
+export var /** @type {?} */ VERSION = new Version('4.0.0-beta.7-45eac23');
 //# sourceMappingURL=version.js.map
