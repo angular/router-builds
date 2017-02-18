b0510a8de9d43cf661be8fc44d868d829b6c5902
Igor Minar <igor@angularjs.org>
2017-02-18 23:37:11 +0000

    1bdf706 release: cut v4.0.0-beta.8

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export { RouterLink, RouterLinkWithHref, RouterLinkActive, RouterOutlet, RouteReuseStrategy, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RoutesRecognized, ROUTES, ROUTER_CONFIGURATION, ROUTER_INITIALIZER, RouterModule, provideRoutes, RouterOutletMap, NoPreloading, PreloadAllModules, PreloadingStrategy, RouterPreloader, ActivatedRoute, ActivatedRouteSnapshot, RouterState, RouterStateSnapshot, PRIMARY_OUTLET, UrlHandlingStrategy, DefaultUrlSerializer, UrlSegment, UrlSegmentGroup, UrlSerializer, UrlTree, VERSION, __router_private__ } from './public_api';
+export { ROUTER_FORROOT_GUARD as ɵa, ROUTER_PROVIDERS as ɵb, initialRouterNavigation as ɵh, provideForRootGuard as ɵe, provideLocationStrategy as ɵd, provideRouterInitializer as ɵi, rootRoute as ɵg, routerNgProbeToken as ɵc, setupRouter as ɵf } from './src/router_module';
+export { Tree as ɵj, TreeNode as ɵk } from './src/utils/tree';
+//# sourceMappingURL=index.js.map
