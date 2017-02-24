2f48aac63bf84a8ab162311d8ea6f28fd86956db
Igor Minar <iminar@google.com>
2017-02-24 07:16:13 +0000

    2da3844 build(language-service): support deep import bundling of language services (#14689)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-41da599
+ * @license Angular v4.0.0-beta.8-2da3844
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -5538,7 +5538,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-41da599');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-2da3844');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
