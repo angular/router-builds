6d27f43559667722a1cf03f35075e693fe7b7290
Igor Minar <iminar@google.com>
2017-02-24 20:32:45 +0000

    436a179 fix(animations): properly cache renderer and namespace triggers (#14703)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -5538,7 +5538,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-5094aef');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-436a179');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
