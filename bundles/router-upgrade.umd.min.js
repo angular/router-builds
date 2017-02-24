dfad6b95ffde3621e772d5747dff529228a565eb
Igor Minar <iminar@google.com>
2017-02-24 05:36:39 +0000

    01907ba build(platform-server): update parse5 to v3 (#14538)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-39f56fa
+ * @license Angular v4.0.0-beta.8-01907ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -5525,7 +5525,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-39f56fa');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-01907ba');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
