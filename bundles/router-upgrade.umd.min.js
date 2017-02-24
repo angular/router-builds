0859250b074574956ef440abeb82209d706e39fa
GitHub <noreply@github.com>
2017-02-24 21:26:31 +0000

    3c9a46c fix(core): fix directive instantiation (#14715)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-436a179
+ * @license Angular v4.0.0-beta.8-3c9a46c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -5538,7 +5538,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-436a179');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-3c9a46c');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
