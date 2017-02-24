7bdae05ed9d373048ceaca3591977b7ce40218eb
Igor Minar <iminar@google.com>
2017-02-24 17:58:39 +0000

    93ddd38 refactor(animations): make modules and bundles into plural Animations (#14695)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -5538,7 +5538,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-32c2fd5');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-93ddd38');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
