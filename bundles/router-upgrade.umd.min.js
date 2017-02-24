4db5f8ab7ca07f90555dda6508af028d70fa2ef2
Igor Minar <iminar@google.com>
2017-02-24 05:02:26 +0000

    39f56fa feat(compiler-cli): add an `outFile` option to ng-xi18n

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-e99d721
+ * @license Angular v4.0.0-beta.8-39f56fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -5525,7 +5525,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-e99d721');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-39f56fa');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
