3b226d945422048c8e6c3b9917f8e6761108374a
Igor Minar <igor@angularjs.org>
2017-02-25 02:29:31 +0000

    c7d1b36 docs: update release schedule due to bad 4.0.0-rc.0 release

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-2602b03
+ * @license Angular v4.0.0-rc.1-c7d1b36
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -5538,7 +5538,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-2602b03');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-c7d1b36');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
