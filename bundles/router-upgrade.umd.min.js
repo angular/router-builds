c23024a9617c1a072d8a3aae769101695e22c189
GitHub <noreply@github.com>
2017-02-25 02:44:26 +0000

    f32f4de docs: more changelog fixes

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-26ed262
+ * @license Angular v4.0.0-rc.1-f32f4de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -5538,7 +5538,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-26ed262');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-f32f4de');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
