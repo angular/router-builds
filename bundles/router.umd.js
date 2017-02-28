db92ad9b009e51569fd909ddb8d17106fe985baf
Igor Minar <iminar@google.com>
2017-02-28 07:29:12 +0000

    126fda2 perf: delete pre-view-engine core, compiler, platform-browser, etc code (#14788)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -5538,7 +5538,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-e58cb7b');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-126fda2');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
