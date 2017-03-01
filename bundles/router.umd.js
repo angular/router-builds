3f2a02f22c615a4a6834947c3e606efd38fd064d
Igor Minar <iminar@google.com>
2017-03-01 16:26:11 +0000

    d1182af refactor: change remaining `<template>` to `<ng-template>` (#14706)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -5538,7 +5538,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-5753de5');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-d1182af');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
