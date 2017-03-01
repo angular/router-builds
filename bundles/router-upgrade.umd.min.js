0540b4be5bee86dab76c7e348d9454028e791290
Igor Minar <iminar@google.com>
2017-03-01 16:22:50 +0000

    fc9e6b2 docs: add details about `OpaqueToken` deprecation in the changelog (#14790)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-968995a
+ * @license Angular v4.0.0-rc.1-fc9e6b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -5538,7 +5538,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-968995a');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-fc9e6b2');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
