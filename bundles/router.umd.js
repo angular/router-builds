8af874605b500f597d3dfe627814c99e3c856f40
Igor Minar <iminar@google.com>
2017-03-01 20:37:50 +0000

    79fc1e3 fix(compiler): apply element bindings before host bindings (#14823)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -5538,7 +5538,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-9402df9');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-79fc1e3');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
