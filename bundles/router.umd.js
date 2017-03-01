35bd845a5f09feb5a944c43e110d2cabc7c5006c
Igor Minar <iminar@google.com>
2017-03-01 15:12:29 +0000

    968995a fix(upgrade): fix upgrade component Closure optimization. (#14801)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -5538,7 +5538,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-5ba55b0');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-968995a');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
