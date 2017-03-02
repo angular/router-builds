01f2eb14d8179929a0afd533130d8648321f0ffe
Igor Minar <igor@angularjs.org>
2017-03-02 07:20:41 +0000

    180b705 release: cut the 4.0.0-rc.2 release

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-9560ad8
+ * @license Angular v4.0.0-rc.2-180b705
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -5538,7 +5538,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-9560ad8');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-180b705');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
