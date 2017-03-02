14d0d2aedf8dee03b58f3fc03598b81341a1c67e
Igor Minar <iminar@google.com>
2017-03-02 08:18:14 +0000

    e6e8123 build: update to yarn 0.21.3 (#14805)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.2-49aa508
+ * @license Angular v4.0.0-rc.2-e6e8123
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -5538,7 +5538,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-49aa508');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-e6e8123');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
