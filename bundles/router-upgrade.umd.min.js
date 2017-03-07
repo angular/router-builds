969296b1894cb163f31d24756d827e7e43c89c66
Igor Minar <iminar@google.com>
2017-03-07 07:06:40 +0000

    ebd4463 fix(aio): reimplement the top menu dropping into the side bar

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.2-b7e76cc
+ * @license Angular v4.0.0-rc.2-ebd4463
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -5538,7 +5538,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-b7e76cc');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-ebd4463');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
