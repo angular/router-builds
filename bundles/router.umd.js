91463d4afe8307ea6c4435a61c23e3d4d623b6d2
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 17:50:19 +0000

    4e1cf5b build(aio): replace all occurrences of env vars on a line in `aio-builds-setup`

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-0e2dd76
+ * @license Angular v4.0.0-rc.3-4e1cf5b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5755,7 +5755,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-0e2dd76');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-4e1cf5b');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
--- typings/upgrade.d.ts
+++ typings/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-0e2dd76
+ * @license Angular v4.0.0-rc.3-4e1cf5b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
