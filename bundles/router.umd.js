530d9d6aa18e7b738fe550815d1f6e5429a79122
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 23:06:02 +0000

    e6c81d2 Revert "refactor(platform-browser): move platform-browser/animations to animations/browser (#15043)"

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-75147ff
+ * @license Angular v4.0.0-rc.3-e6c81d2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5755,7 +5755,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-75147ff');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-e6c81d2');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
--- typings/upgrade.d.ts
+++ typings/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-75147ff
+ * @license Angular v4.0.0-rc.3-e6c81d2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
