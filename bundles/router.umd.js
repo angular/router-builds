4863c07b066e50f6c22cddb450e14831bd5d2a5b
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 18:44:58 +0000

    6559425 fix(core): ErrorHandler should not rethrow an error by default (#15077)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-df914ef
+ * @license Angular v4.0.0-rc.3-6559425
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5755,7 +5755,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-df914ef');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-6559425');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
--- typings/testing.d.ts
+++ typings/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-df914ef
+ * @license Angular v4.0.0-rc.3-6559425
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
