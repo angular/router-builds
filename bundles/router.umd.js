202246573ca8d6c3ad7b1e6c81cd28a3b8b156f6
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 17:49:04 +0000

    0e2dd76 build: remove obsolete `ci-lite/` directory and files (#14994)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-ff71eff
+ * @license Angular v4.0.0-rc.3-0e2dd76
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5755,7 +5755,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-ff71eff');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-0e2dd76');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
--- typings/upgrade.d.ts
+++ typings/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-ff71eff
+ * @license Angular v4.0.0-rc.3-0e2dd76
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
