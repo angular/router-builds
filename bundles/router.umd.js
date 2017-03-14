4f3d220475996b2d9aa195d8cd70558e72a7ebea
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 00:20:25 +0000

    ab40fcb docs(aio): fix relative links

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-6497633
+ * @license Angular v4.0.0-rc.3-ab40fcb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5755,7 +5755,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-6497633');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-ab40fcb');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
--- typings/testing.d.ts
+++ typings/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-6497633
+ * @license Angular v4.0.0-rc.3-ab40fcb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
