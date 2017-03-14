ab67245b21f0c0a46de16781bafe0434b876fe18
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-14 12:47:26 +0000

    d4205bb fix(aio): ignore private exports

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-1c1085b
+ * @license Angular v4.0.0-rc.3-d4205bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5755,7 +5755,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-1c1085b');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-d4205bb');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
--- typings/testing.d.ts
+++ typings/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-1c1085b
+ * @license Angular v4.0.0-rc.3-d4205bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
