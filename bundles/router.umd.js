b66da66ed99ee20467fff0218779b6b567cbdf43
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 22:57:49 +0000

    778f7d6 fix(forms): remove equalsTo validator (#15050)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-2c5a671
+ * @license Angular v4.0.0-rc.3-778f7d6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5757,7 +5757,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-2c5a671');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-778f7d6');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
--- typings/upgrade.d.ts
+++ typings/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-2c5a671
+ * @license Angular v4.0.0-rc.3-778f7d6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
