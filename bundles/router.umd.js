57c181c91e68fdbd49f03afdcceab28a70ceb295
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 22:58:21 +0000

    80649ea fix(platform-server): correctly implement get href in parse5 adapter (#15022)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-778f7d6
+ * @license Angular v4.0.0-rc.3-80649ea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5757,7 +5757,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-778f7d6');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-80649ea');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
--- typings/testing.d.ts
+++ typings/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-778f7d6
+ * @license Angular v4.0.0-rc.3-80649ea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
