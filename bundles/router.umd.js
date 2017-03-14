67eb5e52582e91eaa5b91688bddf640a00987784
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-14 15:23:10 +0000

    423bfb0 ci(aio): whitelist 'aio-contributors' for deploying PR previews

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-2f250c1
+ * @license Angular v4.0.0-rc.3-423bfb0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5755,7 +5755,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-2f250c1');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-423bfb0');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
--- typings/upgrade.d.ts
+++ typings/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-2f250c1
+ * @license Angular v4.0.0-rc.3-423bfb0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
