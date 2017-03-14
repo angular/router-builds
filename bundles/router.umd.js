bbc220e1967824a4fcbfd99731f35ca522889c0d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-14 14:09:55 +0000

    2f250c1 refactor(aio): move window:resize handler into component

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-d4205bb
+ * @license Angular v4.0.0-rc.3-2f250c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5755,7 +5755,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-d4205bb');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-2f250c1');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
--- typings/testing.d.ts
+++ typings/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-d4205bb
+ * @license Angular v4.0.0-rc.3-2f250c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
