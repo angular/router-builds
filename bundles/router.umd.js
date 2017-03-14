2e4463c65ffc75f123020ac94a11b8d9fd4f7944
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 01:53:51 +0000

    1c1085b feat(aio): add google analytics (#15081)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-914797a
+ * @license Angular v4.0.0-rc.3-1c1085b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5755,7 +5755,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-914797a');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-1c1085b');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
