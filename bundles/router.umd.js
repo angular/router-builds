340f55b15c4212a35f61f9fe374a7eb2066bdf82
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 22:45:37 +0000

    f1b33ab fix(http): Make ResponseOptionsArgs an interface (#14607) (#14623)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-029d0f2
+ * @license Angular v4.0.0-rc.3-f1b33ab
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5817,7 +5817,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-029d0f2');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-f1b33ab');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
--- typings/upgrade.d.ts
+++ typings/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-029d0f2
+ * @license Angular v4.0.0-rc.3-f1b33ab
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
