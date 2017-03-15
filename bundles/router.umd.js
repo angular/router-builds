23afc35c8b530604e013a4311b7af5f91cd522c4
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 00:26:55 +0000

    ff60c04 test(platform-server): add initial e2e tests for platform-server (#15061)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-13686bb
+ * @license Angular v4.0.0-rc.3-ff60c04
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5795,7 +5795,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-13686bb');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-ff60c04');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
--- typings/testing.d.ts
+++ typings/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-13686bb
+ * @license Angular v4.0.0-rc.3-ff60c04
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
