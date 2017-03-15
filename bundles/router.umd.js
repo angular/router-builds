a05d9d773ce127522e982f9914543f64a307d941
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 00:28:02 +0000

    3b1956b fix(compiler): warning prints "WARNING" instead of "ERROR" (#15125)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-ff60c04
+ * @license Angular v4.0.0-rc.3-3b1956b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5795,7 +5795,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-ff60c04');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-3b1956b');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
