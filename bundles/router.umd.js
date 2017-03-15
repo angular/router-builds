8e0d607328d640a256f11cc794a6443fdc7d814f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-15 22:42:51 +0000

    a9b1880 feat(aio): top bar typography, sidenav size

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-ec548ad
+ * @license Angular v4.0.0-rc.3-a9b1880
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5795,7 +5795,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-ec548ad');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-a9b1880');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
--- typings/upgrade.d.ts
+++ typings/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-ec548ad
+ * @license Angular v4.0.0-rc.3-a9b1880
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
