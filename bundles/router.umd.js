c7e55cb8a57fd78978f7c0682f069d87ad91069f
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 20:56:56 +0000

    80112a9 fix: fix path locally to empty.js (#15073)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-795638e
+ * @license Angular v4.0.0-rc.3-80112a9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5811,7 +5811,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-795638e');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-80112a9');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
--- typings/testing.d.ts
+++ typings/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-795638e
+ * @license Angular v4.0.0-rc.3-80112a9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
