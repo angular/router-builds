cbdc7ab5872a51507b808a61fa470436e8f0b355
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 17:31:42 +0000

    26d4ce2 fix(forms): ensure observable validators are properly canceled (#15132)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-41f61b0
+ * @license Angular v4.0.0-rc.3-26d4ce2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5817,7 +5817,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-41f61b0');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-26d4ce2');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
