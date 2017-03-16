e183de82a2efdbf1a307b24128f894c38e5c9196
Igor Minar <iminar@google.com>
2017-03-16 06:50:52 +0000

    480a407 build(aio): implement prerendering

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-923d0c5
+ * @license Angular v4.0.0-rc.3-480a407
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5817,7 +5817,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-923d0c5');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-480a407');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
--- typings/upgrade.d.ts
+++ typings/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-923d0c5
+ * @license Angular v4.0.0-rc.3-480a407
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
