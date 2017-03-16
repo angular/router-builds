8d61af991cb4cd9b89c09a1b4212466fda58db42
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 00:28:24 +0000

    6e98757 fix(core): use presence of .subscribe to detect observables rather then Symbol.observable (#15171)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-3131581
+ * @license Angular v4.0.0-rc.3-6e98757
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5817,7 +5817,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-3131581');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-6e98757');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
