4c25925b2a05d8de5fa761a9f3f83ec38283144d
Chuck Jazdzewski <chuckj@google.com>
2017-03-17 17:49:04 +0000

    2d78c8c docs: add changelog for 4.0.0-rc.5

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.4-7fb4528
+ * @license Angular v4.0.0-rc.5-2d78c8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -5828,7 +5828,7 @@
     /**
      * \@stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-rc.4-7fb4528');
+    var VERSION = new _angular_core.Version('4.0.0-rc.5-2d78c8c');
 
     exports.RouterLink = RouterLink;
     exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.4-7fb4528
+ * @license Angular v4.0.0-rc.5-2d78c8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
