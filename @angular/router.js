a1babdbe942a3d4ef6456fcde5ae3f214abc8c7d
Miško Hevery <misko@hevery.com>
2017-03-19 18:22:11 +0000

    f634c62 test: add systemjs+umd integration test (#14196)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-80075af
+ * @license Angular v4.0.0-rc.5-f634c62
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5735,7 +5735,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-80075af');
+const VERSION = new Version('4.0.0-rc.5-f634c62');
 
 /**
  * @license
