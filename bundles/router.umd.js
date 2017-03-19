7bd9ad519618c34faf0509e3194e5c89b5606b9b
Miško Hevery <misko@hevery.com>
2017-03-19 17:36:31 +0000

    80075af fix(animations): only process element nodes through the animation engine (#15268)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-bcc29ff
+ * @license Angular v4.0.0-rc.5-80075af
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -14,7 +14,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.5-bcc29ff
+ * @license Angular v4.0.0-rc.5-80075af
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
@@ -5958,7 +5958,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.0.0-rc.5-bcc29ff');
+var VERSION = new _angular_core.Version('4.0.0-rc.5-80075af');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-bcc29ff
+ * @license Angular v4.0.0-rc.5-80075af
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
