9de5ca3cec177824035100ef14da0bac52c4f592
Igor Minar <iminar@google.com>
2017-03-21 15:34:09 +0000

    81b6a5f build(aio): upgrade @angular/cli to 1.0.0-rc.4

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-ea979cf
+ * @license Angular v4.0.0-rc.5-81b6a5f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -14,7 +14,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.5-ea979cf
+ * @license Angular v4.0.0-rc.5-81b6a5f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
@@ -6091,7 +6091,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.0.0-rc.5-ea979cf');
+var VERSION = new _angular_core.Version('4.0.0-rc.5-81b6a5f');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-ea979cf
+ * @license Angular v4.0.0-rc.5-81b6a5f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
