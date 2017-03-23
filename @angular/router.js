1d302681ab96b69e0ddd6f7711c75c8d39b39625
Igor Minar <iminar@google.com>
2017-03-23 00:35:42 +0000

    9319b5f docs(common): update ngFor docs to new `as` syntax (#15166)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-edb2571
+ * @license Angular v4.0.0-rc.5-9319b5f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5837,7 +5837,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-edb2571');
+const VERSION = new Version('4.0.0-rc.5-9319b5f');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-edb2571
+ * @license Angular v4.0.0-rc.5-9319b5f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
