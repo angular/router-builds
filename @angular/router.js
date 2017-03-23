339ca3f1d7ca0d2ed3192e67b16a871269960fdb
Victor Berchet <victor@suumit.com>
2017-03-23 20:56:21 +0000

    0dda01e fix(compiler): correctly handle when `toString` is exported (#15430)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-92084f2
+ * @license Angular v4.0.0-rc.6-0dda01e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5837,7 +5837,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.6-92084f2');
+const VERSION = new Version('4.0.0-rc.6-0dda01e');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-92084f2
+ * @license Angular v4.0.0-rc.6-0dda01e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
