f27eecae7532aee85acfde4becb081bee6cac5b2
GitHub <noreply@github.com>
2017-03-23 07:09:40 +0000

    73808dd build(aio): upgrade to @angular@4.0.0-rc.6 (#15412)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-ee03418
+ * @license Angular v4.0.0-rc.6-73808dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5837,7 +5837,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.6-ee03418');
+const VERSION = new Version('4.0.0-rc.6-73808dd');
 
 /**
  * @license
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-ee03418
+ * @license Angular v4.0.0-rc.6-73808dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -14,7 +14,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.6-ee03418
+ * @license Angular v4.0.0-rc.6-73808dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
@@ -6091,7 +6091,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.0.0-rc.6-ee03418');
+var VERSION = new _angular_core.Version('4.0.0-rc.6-73808dd');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
