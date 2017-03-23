58646ab5bc986349e474b366a743aef13ec245a5
Igor Minar <iminar@google.com>
2017-03-23 04:45:37 +0000

    de87c47 docs: spelling errors

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-08d8675
+ * @license Angular v4.0.0-rc.5-de87c47
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -14,7 +14,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.5-08d8675
+ * @license Angular v4.0.0-rc.5-de87c47
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
@@ -6091,7 +6091,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.0.0-rc.5-08d8675');
+var VERSION = new _angular_core.Version('4.0.0-rc.5-de87c47');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-08d8675
+ * @license Angular v4.0.0-rc.5-de87c47
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
