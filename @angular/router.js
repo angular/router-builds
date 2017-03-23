14988817e7e64eea3e7b3c11e7ef84ecc5c80224
Igor Minar <iminar@google.com>
2017-03-23 00:31:38 +0000

    26f6bd4 ci(travis): update excluded branch to g3 (#15391)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-ea49a95
+ * @license Angular v4.0.0-rc.5-26f6bd4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5837,7 +5837,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-ea49a95');
+const VERSION = new Version('4.0.0-rc.5-26f6bd4');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-ea49a95
+ * @license Angular v4.0.0-rc.5-26f6bd4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
