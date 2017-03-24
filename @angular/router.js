787136a1fd66c28945bc49f6d0e5996f89fbf24e
GitHub <noreply@github.com>
2017-03-24 00:37:38 +0000

    db0dca3 docs: add a note about exclusion of symbols prefixed with ɵ from our public api surface (#15440)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-7a715b2
+ * @license Angular v4.0.0-db0dca3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5837,7 +5837,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-7a715b2');
+const VERSION = new Version('4.0.0-db0dca3');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-7a715b2
+ * @license Angular v4.0.0-db0dca3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
