55c78178f620afbd5b501e807bdf42edfaeb07cb
Victor Berchet <victor@suumit.com>
2017-03-24 00:23:42 +0000

    7a715b2 docs: add changelog for 4.0.0

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-b800a0c
+ * @license Angular v4.0.0-7a715b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -14,7 +14,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.6-b800a0c
+ * @license Angular v4.0.0-7a715b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
@@ -6091,7 +6091,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.0.0-rc.6-b800a0c');
+var VERSION = new _angular_core.Version('4.0.0-7a715b2');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-b800a0c
+ * @license Angular v4.0.0-7a715b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
