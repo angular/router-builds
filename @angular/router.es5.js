a27aa79f6de8894b19d8a3da42dfdb5b766a29f5
Victor Berchet <victor@suumit.com>
2017-03-24 22:04:35 +0000

    c17b912 refactor: use object spread operator rather than merge (#15426)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-8785b2b
+ * @license Angular v4.0.0-c17b912
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6103,7 +6103,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.0-8785b2b');
+var VERSION = new Version('4.0.0-c17b912');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
