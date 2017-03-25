e647fd7de46a46807d276f64d1986bd3a88af6bb
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-25 21:50:45 +0000

    1616cae build(aio): add renderExamples processor

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-c9e51d9
+ * @license Angular v4.0.0-1616cae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -14,7 +14,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-c9e51d9
+ * @license Angular v4.0.0-1616cae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
@@ -6092,7 +6092,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.0.0-c9e51d9');
+var VERSION = new _angular_core.Version('4.0.0-1616cae');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-c9e51d9
+ * @license Angular v4.0.0-1616cae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
