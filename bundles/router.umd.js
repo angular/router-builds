62329c622f2c13e8b2b7367dfdb624b1c7def312
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 08:38:08 +0000

    a6fd22c docs(aio): fix typo

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-1616cae
+ * @license Angular v4.0.0-a6fd22c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -14,7 +14,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-1616cae
+ * @license Angular v4.0.0-a6fd22c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
@@ -6092,7 +6092,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.0.0-1616cae');
+var VERSION = new _angular_core.Version('4.0.0-a6fd22c');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
