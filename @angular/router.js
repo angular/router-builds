0fd806fffca509e6fd3a606b5f898692af5ac998
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 11:42:31 +0000

    837ed78 feat(aio): add code-example and code-tabs

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-52ea193
+ * @license Angular v4.0.0-837ed78
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5838,7 +5838,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-52ea193');
+const VERSION = new Version('4.0.0-837ed78');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-52ea193
+ * @license Angular v4.0.0-837ed78
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
