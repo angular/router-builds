3865347e89317a437e541b4c9084a8db01add0b7
Victor Berchet <victor@suumit.com>
2017-03-28 17:06:26 +0000

    b7fa5de fix(core): improve error msg for invalid KeyValueDiffer.diff arg (#15489)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-d05aa70
+ * @license Angular v4.0.0-b7fa5de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6103,7 +6103,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.0-d05aa70');
+var VERSION = new Version('4.0.0-b7fa5de');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-d05aa70
+ * @license Angular v4.0.0-b7fa5de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
