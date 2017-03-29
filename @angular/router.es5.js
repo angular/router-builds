939d3e5f61d18034284b49c5c94688f8296cad49
Victor Berchet <victor@suumit.com>
2017-03-29 22:28:27 +0000

    19cb503 docs(http): remove deprecated stuff and outdated plunkrs (#15598)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-9f2acf5
+ * @license Angular v4.0.0-19cb503
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6074,7 +6074,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.0-9f2acf5');
+var VERSION = new Version('4.0.0-19cb503');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
