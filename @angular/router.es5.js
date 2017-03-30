fd0d101bead44eaf5379506cf5d648dc5a2eec62
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-03-30 19:52:15 +0000

    9e883f5 feat(aio): improve ProgressiveWepApp-ability (#15628)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.0-43a9619
+ * @license Angular v4.1.0-beta.0-9e883f5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6074,7 +6074,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.0-43a9619');
+var VERSION = new Version('4.1.0-beta.0-9e883f5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-43a9619
+ * @license Angular v4.1.0-beta.0-9e883f5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
