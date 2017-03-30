ae3e6f979ae082f799bbf35058f2f09bfcc64948
Victor Berchet <victor@suumit.com>
2017-03-30 00:22:23 +0000

    67719f2 docs: add changelog for 4.1.0-beta.0

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-1dda05a
+ * @license Angular v4.1.0-beta.0-67719f2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6074,7 +6074,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.0-1dda05a');
+var VERSION = new Version('4.1.0-beta.0-67719f2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-1dda05a
+ * @license Angular v4.1.0-beta.0-67719f2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
