5f4d51748d68f862c84fa410abd64dfff17b9b4f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-03-31 18:53:19 +0000

    5c38012 fix(aio): contributor.html typos

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-46f8a6d
+ * @license Angular v4.1.0-beta.0-5c38012
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5804,7 +5804,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-46f8a6d');
+const VERSION = new Version('4.1.0-beta.0-5c38012');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-46f8a6d
+ * @license Angular v4.1.0-beta.0-5c38012
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
