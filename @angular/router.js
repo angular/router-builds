62113e0c735868b0f7704e1f16a402743c52538a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-31 08:55:55 +0000

    4c21114 feat(aio): remove google feedback

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-65bb237
+ * @license Angular v4.1.0-beta.0-4c21114
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5804,7 +5804,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-65bb237');
+const VERSION = new Version('4.1.0-beta.0-4c21114');
 
 /**
  * @license
