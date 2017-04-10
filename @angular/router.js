bef015059fb788310f83f54367333e5d0bebba03
Hans <hans@hansl.ca>
2017-04-10 22:18:50 +0000

    258d539 fix(language-service): detect when there isn't a tsconfig.json

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-09d9f5f
+ * @license Angular v4.1.0-beta.0-258d539
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5804,7 +5804,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-09d9f5f');
+const VERSION = new Version('4.1.0-beta.0-258d539');
 
 /**
  * @license
