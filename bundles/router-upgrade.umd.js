b1bb6644ece0d953da96074b8fda6c436c17c9c6
Hans <hans@hansl.ca>
2017-04-11 21:42:06 +0000

    0799f18 fix(aio): v2 - when browser lacks service worker, disable sw feature

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-91b2e39
+ * @license Angular v4.1.0-beta.0-0799f18
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -9,7 +9,7 @@
 }(this, (function (exports,_angular_core,_angular_router,_angular_upgrade_static) { 'use strict';
 
 /**
- * @license Angular v4.1.0-beta.0-91b2e39
+ * @license Angular v4.1.0-beta.0-0799f18
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-91b2e39
+ * @license Angular v4.1.0-beta.0-0799f18
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
