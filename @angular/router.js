1b64d705df50c9ec67c5443bba0f7a4d4c22ead6
Tobias Bosch <tbosch1009@gmail.com>
2017-04-13 19:11:37 +0000

    70b1d6d fix(tsc-wrapped): collect new expressions with no arguments (#15908)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-9394835
+ * @license Angular v4.1.0-beta.1-70b1d6d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5760,7 +5760,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-9394835');
+const VERSION = new Version('4.1.0-beta.1-70b1d6d');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-9394835
+ * @license Angular v4.1.0-beta.1-70b1d6d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
