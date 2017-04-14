1d8a221aa274c7257463cd0373fc024f9b611363
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 21:10:55 +0000

    c36ec9b fix(http): Update types for TypeScript nullability support

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-86396a4
+ * @license Angular v4.1.0-beta.1-c36ec9b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6030,7 +6030,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-86396a4');
+var VERSION = new Version('4.1.0-beta.1-c36ec9b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
