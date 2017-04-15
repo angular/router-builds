57c4365c68f55a12b525c52aa02e32743551b2ce
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:39:24 +0000

    8479e9e feat(aio): high res screen spacing

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-092f0df
+ * @license Angular v4.1.0-beta.1-8479e9e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -14,7 +14,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-092f0df
+ * @license Angular v4.1.0-beta.1-8479e9e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
@@ -6020,7 +6020,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.1-092f0df');
+var VERSION = new _angular_core.Version('4.1.0-beta.1-8479e9e');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-092f0df
+ * @license Angular v4.1.0-beta.1-8479e9e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
