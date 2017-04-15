2f090b9f09459a4f126045308b7a2b42c0d1238a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:52:08 +0000

    16673fa build(aio): remove unused config code

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-624b5a5
+ * @license Angular v4.1.0-beta.1-16673fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5759,7 +5759,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-624b5a5');
+const VERSION = new Version('4.1.0-beta.1-16673fa');
 
 /**
  * @license
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-624b5a5
+ * @license Angular v4.1.0-beta.1-16673fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -14,7 +14,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-624b5a5
+ * @license Angular v4.1.0-beta.1-16673fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
@@ -6020,7 +6020,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.1-624b5a5');
+var VERSION = new _angular_core.Version('4.1.0-beta.1-16673fa');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
