0296566ffc3ad6cf9fa9fb4f36bae33b9331bbe6
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 17:32:01 +0000

    5b141fb fix(compiler): support `<ng-container>` whatever the namespace

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-2688842
+ * @license Angular v4.1.0-beta.1-5b141fb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -14,7 +14,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-2688842
+ * @license Angular v4.1.0-beta.1-5b141fb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
@@ -6019,7 +6019,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.1-2688842');
+var VERSION = new _angular_core.Version('4.1.0-beta.1-5b141fb');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
