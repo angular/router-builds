fbfaae0082ee832328916765922f28c28f934c06
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 21:53:05 +0000

    90814e4 feat(upgrade): fixes for allow setting the angularjs lib at runtime

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-38a7e0d
+ * @license Angular v4.1.0-beta.1-90814e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -14,7 +14,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-38a7e0d
+ * @license Angular v4.1.0-beta.1-90814e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
@@ -6018,7 +6018,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.1-38a7e0d');
+var VERSION = new _angular_core.Version('4.1.0-beta.1-90814e4');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
