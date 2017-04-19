e4b67ba6a8fc2a267c380ab6205c8227e59c7361
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 11:14:53 +0000

    c9983e6 docs(aio): Fix reference to the right example

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-51098c4
+ * @license Angular v4.1.0-beta.1-c9983e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6030,7 +6030,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-51098c4');
+var VERSION = new Version('4.1.0-beta.1-c9983e6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-51098c4
+ * @license Angular v4.1.0-beta.1-c9983e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
