48c8d753bace11bb292c6bf816027a702c7b1452
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 09:48:58 +0000

    9d40ab9 docs(aio): rename ToH to match the guide name

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-691e86c
+ * @license Angular v4.1.0-beta.1-9d40ab9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5758,7 +5758,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-691e86c');
+const VERSION = new Version('4.1.0-beta.1-9d40ab9');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-691e86c
+ * @license Angular v4.1.0-beta.1-9d40ab9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
