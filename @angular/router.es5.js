0d5840cd3c565e648add22e3753460ac7305ca0a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 09:45:46 +0000

    691e86c docs(aio): fix minor typos in deployment guide

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-93d8f8c
+ * @license Angular v4.1.0-beta.1-691e86c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6030,7 +6030,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-93d8f8c');
+var VERSION = new Version('4.1.0-beta.1-691e86c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-93d8f8c
+ * @license Angular v4.1.0-beta.1-691e86c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
