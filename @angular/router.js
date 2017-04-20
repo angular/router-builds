a9b1715cf343efae96d2bf05697d3a112611c50b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-20 09:55:40 +0000

    ab03852 fix(aio): accessibility items in search and home

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-f902581
+ * @license Angular v4.1.0-beta.1-ab03852
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5758,7 +5758,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-f902581');
+const VERSION = new Version('4.1.0-beta.1-ab03852');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-f902581
+ * @license Angular v4.1.0-beta.1-ab03852
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
