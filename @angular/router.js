43c92d5775035e237470c60ea384d6636467a1d4
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-20 15:24:46 +0000

    a54fe63 docs(aio): move file to the right folder

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-800b1b0
+ * @license Angular v4.1.0-beta.1-a54fe63
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5758,7 +5758,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-800b1b0');
+const VERSION = new Version('4.1.0-beta.1-a54fe63');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-800b1b0
+ * @license Angular v4.1.0-beta.1-a54fe63
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
