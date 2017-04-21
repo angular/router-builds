d84d809bf186aae6f137d570097d4140680901e8
Miško Hevery <misko@hevery.com>
2017-04-21 03:26:37 +0000

    cbe95f3 feat(aio): add code-of-conduct to footer #16105 (#16216)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-134f542
+ * @license Angular v4.1.0-beta.1-cbe95f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5758,7 +5758,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-134f542');
+const VERSION = new Version('4.1.0-beta.1-cbe95f3');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-134f542
+ * @license Angular v4.1.0-beta.1-cbe95f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
