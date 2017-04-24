f8eb17db9d1382eb42dadc29ad3fce7150e921e7
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-24 14:02:19 +0000

    d7719aa perf(aio): improve unit test rebuild time

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-rc.0-54e587a
+ * @license Angular v4.1.0-rc.0-d7719aa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6030,7 +6030,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-rc.0-54e587a');
+var VERSION = new Version('4.1.0-rc.0-d7719aa');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-54e587a
+ * @license Angular v4.1.0-rc.0-d7719aa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
