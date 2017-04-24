9f56ee83d8dc6c91cdb10965d915aa024d29e9c3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-24 12:32:13 +0000

    c3fa880 fix(aio): use SVG icons for page load sensitive UI

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-3cad5da
+ * @license Angular v4.1.0-rc.0-c3fa880
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5758,7 +5758,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-3cad5da');
+const VERSION = new Version('4.1.0-rc.0-c3fa880');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-3cad5da
+ * @license Angular v4.1.0-rc.0-c3fa880
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
