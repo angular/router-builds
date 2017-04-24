11a98695cda627a80435e3d283f45fcf0a55651d
Miško Hevery <misko@hevery.com>
2017-04-24 17:10:37 +0000

    2991221 docs(aio): remove duplicate `as` (#16265)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-d7719aa
+ * @license Angular v4.1.0-rc.0-2991221
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -9,7 +9,7 @@
 }(this, (function (exports,_angular_common,_angular_common_testing,_angular_core,_angular_router) { 'use strict';
 
 /**
- * @license Angular v4.1.0-rc.0-d7719aa
+ * @license Angular v4.1.0-rc.0-2991221
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-d7719aa
+ * @license Angular v4.1.0-rc.0-2991221
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
