3950c1126292ce4d817eade6a8a7acaad96796f4
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-25 06:23:41 +0000

    ff0f53c build(aio): include missing packages in the docs

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-64ef69f
+ * @license Angular v4.1.0-rc.0-ff0f53c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -9,7 +9,7 @@
 }(this, (function (exports,_angular_common,_angular_common_testing,_angular_core,_angular_router) { 'use strict';
 
 /**
- * @license Angular v4.1.0-rc.0-64ef69f
+ * @license Angular v4.1.0-rc.0-ff0f53c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-64ef69f
+ * @license Angular v4.1.0-rc.0-ff0f53c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
