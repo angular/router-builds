04fd116a2c5b6a86fc7eca3f75713e96de7774a7
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-25 07:02:59 +0000

    11b2f62 feat(aio): don't animate sidenav on launch.

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-e7c37d7
+ * @license Angular v4.1.0-rc.0-11b2f62
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5758,7 +5758,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-e7c37d7');
+const VERSION = new Version('4.1.0-rc.0-11b2f62');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-e7c37d7
+ * @license Angular v4.1.0-rc.0-11b2f62
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
