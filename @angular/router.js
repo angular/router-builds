6dbbf446d7f3ada0ca71db7f3c5a3db85918fc5b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-25 11:49:49 +0000

    9b7f2ce test(aio): add missing test for AppComponent 'starting' feature

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-11b2f62
+ * @license Angular v4.1.0-rc.0-9b7f2ce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5758,7 +5758,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-11b2f62');
+const VERSION = new Version('4.1.0-rc.0-9b7f2ce');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-11b2f62
+ * @license Angular v4.1.0-rc.0-9b7f2ce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
