4ad1ca2e1c1e681cc8a3a492e9059ebf84ad8401
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-26 09:09:15 +0000

    35a2dfc fix(aio): resource nav

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-d8e2829
+ * @license Angular v4.1.0-rc.0-35a2dfc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5758,7 +5758,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-d8e2829');
+const VERSION = new Version('4.1.0-rc.0-35a2dfc');
 
 /**
  * @license
