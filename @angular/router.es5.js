469f93b7bf552d4bf0524d2855320a8dd2fe2263
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-26 13:52:45 +0000

    bb52e22 feat(aio): api pages styling

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-rc.0-35a2dfc
+ * @license Angular v4.1.0-rc.0-bb52e22
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6030,7 +6030,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-rc.0-35a2dfc');
+var VERSION = new Version('4.1.0-rc.0-bb52e22');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-35a2dfc
+ * @license Angular v4.1.0-rc.0-bb52e22
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
