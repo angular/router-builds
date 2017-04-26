655373a3ddaccf8b631cc661240058acca008db5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-26 08:42:12 +0000

    d8e2829 feat(aio): boilerplate:add cleans (removes) before adding

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-ac5e6ba
+ * @license Angular v4.1.0-rc.0-d8e2829
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -14,7 +14,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-rc.0-ac5e6ba
+ * @license Angular v4.1.0-rc.0-d8e2829
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
@@ -6019,7 +6019,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-rc.0-ac5e6ba');
+var VERSION = new _angular_core.Version('4.1.0-rc.0-d8e2829');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-ac5e6ba
+ * @license Angular v4.1.0-rc.0-d8e2829
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
