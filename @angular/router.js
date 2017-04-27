a1c47ffd284c5d2c7b63a1b81b5031de2231c6c8
Miško Hevery <misko@hevery.com>
2017-04-27 22:50:04 +0000

    3f46645 feat(aio): add Table of Contents (toc) component. (#16078)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-2a7f636
+ * @license Angular v4.1.0-3f46645
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5758,7 +5758,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-2a7f636');
+const VERSION = new Version('4.1.0-3f46645');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-2a7f636
+ * @license Angular v4.1.0-3f46645
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
