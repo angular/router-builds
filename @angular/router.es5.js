7819ba6a607275a54a9d83879d198145ef96c345
Miško Hevery <misko@hevery.com>
2017-04-28 23:28:19 +0000

    a4de214 fix(core): don’t set `ng-version` for dynamically created components (#16394)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-a2b2afb
+ * @license Angular v4.1.0-a4de214
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6030,7 +6030,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-a2b2afb');
+var VERSION = new Version('4.1.0-a4de214');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-a2b2afb
+ * @license Angular v4.1.0-a4de214
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
