83318b1c3528ffe9dd3b0b57ef36602f5b01cad8
Matias Niemelä <matias@yearofmoo.com>
2017-04-28 20:09:55 +0000

    a619991 feat(animations): introduce a wave of new animation features

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-ed4eaf3
+ * @license Angular v4.1.0-a619991
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6026,7 +6026,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-ed4eaf3');
+var VERSION = new Version('4.1.0-a619991');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
