fccec7a01a16919e742ca4766b6edd0424be3b17
Miško Hevery <misko@hevery.com>
2017-04-28 22:52:36 +0000

    b70b960 docs(aio): apply guidelines to ts-to-js - adapt for aio site (#16054)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-9384061
+ * @license Angular v4.1.0-b70b960
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -14,7 +14,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-9384061
+ * @license Angular v4.1.0-b70b960
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
@@ -6019,7 +6019,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-9384061');
+var VERSION = new _angular_core.Version('4.1.0-b70b960');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-9384061
+ * @license Angular v4.1.0-b70b960
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
