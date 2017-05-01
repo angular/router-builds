d95dcb710701c97ef80d025b729c3f6acb98b68c
Matias Niemelä <matias@yearofmoo.com>
2017-05-01 23:06:40 +0000

    65a8f7f fix(aio): search result styling

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-f32bfcd
+ * @license Angular v4.1.0-65a8f7f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5758,7 +5758,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-f32bfcd');
+const VERSION = new Version('4.1.0-65a8f7f');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-f32bfcd
+ * @license Angular v4.1.0-65a8f7f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
