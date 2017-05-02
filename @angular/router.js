19d4baeeb1141a2484fca7bf3fe94fa57ea18401
Matias Niemelä <matias@yearofmoo.com>
2017-05-02 23:09:37 +0000

    c946a92 refactor(compiler): simplify AOT tests

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-21c96a5
+ * @license Angular v4.1.0-c946a92
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5758,7 +5758,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-21c96a5');
+const VERSION = new Version('4.1.0-c946a92');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-21c96a5
+ * @license Angular v4.1.0-c946a92
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
