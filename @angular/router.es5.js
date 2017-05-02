b630a488637aa91a313c13bc791ec056e1f7f519
Matias Niemelä <matias@yearofmoo.com>
2017-05-02 22:30:22 +0000

    79ed0e7 feat(aio): display API icons in search results

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-04dc248
+ * @license Angular v4.1.0-79ed0e7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6030,7 +6030,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-04dc248');
+var VERSION = new Version('4.1.0-79ed0e7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-04dc248
+ * @license Angular v4.1.0-79ed0e7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
