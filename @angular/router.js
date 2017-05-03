044a0decaa582e51a3c995406b944edb837da9d7
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-03 08:19:25 +0000

    ea9d8a6 docs(aio): move pipes guide under components

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-9650ff0
+ * @license Angular v4.1.0-ea9d8a6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5758,7 +5758,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-9650ff0');
+const VERSION = new Version('4.1.0-ea9d8a6');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-9650ff0
+ * @license Angular v4.1.0-ea9d8a6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
