df4ebfb43b10c9c3a850475cc49d27dd7f155133
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 20:48:20 +0000

    8760bf7 build(aio): fix paths to template macros in overview-dump template

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-c8dc116
+ * @license Angular v4.1.0-8760bf7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5758,7 +5758,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-c8dc116');
+const VERSION = new Version('4.1.0-8760bf7');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-c8dc116
+ * @license Angular v4.1.0-8760bf7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
