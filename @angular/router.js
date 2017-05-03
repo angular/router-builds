9b2a66be85817756ec12a842473bca38ddf14598
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 21:08:21 +0000

    55b8de9 build(aio): lint examples

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-eb56ab3
+ * @license Angular v4.1.0-55b8de9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5758,7 +5758,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-eb56ab3');
+const VERSION = new Version('4.1.0-55b8de9');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-eb56ab3
+ * @license Angular v4.1.0-55b8de9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
