6ab5f67dce1aea04721106ba76e95286d4bfea39
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 21:00:08 +0000

    464701a docs(aio): revert removal of tutorial examples

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-5b96fb9
+ * @license Angular v4.1.0-464701a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -14,7 +14,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-5b96fb9
+ * @license Angular v4.1.0-464701a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
@@ -6019,7 +6019,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-5b96fb9');
+var VERSION = new _angular_core.Version('4.1.0-464701a');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
