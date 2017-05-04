0f146e9f7b6ed16c8d7d9daa0a5ba4fb67dd5a7a
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 02:51:04 +0000

    6748aea build(aio): update to dgeni-packages 0.19.0

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-b3e63c0
+ * @license Angular v4.1.0-6748aea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6030,7 +6030,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-b3e63c0');
+var VERSION = new Version('4.1.0-6748aea');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-b3e63c0
+ * @license Angular v4.1.0-6748aea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
