f3c31245fde1932d5ca9c1bf732516abbd45199d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-04 11:41:48 +0000

    aec65de refactor(aio): simplify DocViewer

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-919ff12
+ * @license Angular v4.1.0-aec65de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5758,7 +5758,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-919ff12');
+const VERSION = new Version('4.1.0-aec65de');
 
 /**
  * @license
