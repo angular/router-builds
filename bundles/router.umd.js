c1b17bde85d1414ca0dc3e9d8ab025f8aee19b02
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-05 10:28:19 +0000

    d27588b feat(aio): api label styles

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-2714644
+ * @license Angular v4.2.0-beta.0-d27588b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -14,7 +14,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.2.0-beta.0-2714644
+ * @license Angular v4.2.0-beta.0-d27588b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
@@ -6019,7 +6019,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-beta.0-2714644');
+var VERSION = new _angular_core.Version('4.2.0-beta.0-d27588b');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-2714644
+ * @license Angular v4.2.0-beta.0-d27588b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
