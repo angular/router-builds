45ac057eda680f9c35e554264681d888ea6195c3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-06 13:51:37 +0000

    03513e9 fix(aio): constrain header-link styles

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.2.0-beta.0-b9ed97c
+ * @license Angular v4.2.0-beta.0-03513e9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6030,7 +6030,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.0-b9ed97c');
+var VERSION = new Version('4.2.0-beta.0-03513e9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
