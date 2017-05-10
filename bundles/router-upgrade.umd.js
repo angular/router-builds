dbbfd718ca239f7b78fc4000d0805c84de64da0d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 14:08:19 +0000

    063db64 fix(aio): style sidenav-content scrollbar similar to other scrollbars

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-b44f5c6
+ * @license Angular v4.2.0-beta.0-063db64
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -9,7 +9,7 @@
 }(this, (function (exports,_angular_core,_angular_router,_angular_upgrade_static) { 'use strict';
 
 /**
- * @license Angular v4.2.0-beta.0-b44f5c6
+ * @license Angular v4.2.0-beta.0-063db64
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
