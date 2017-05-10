60d9553928d6ee209d958689755b97a3a17365ac
Jason Aden <jasonaden@google.com>
2017-05-10 23:53:54 +0000

    62a8618 docs: add changelog for 4.1.2

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.1-e46a65f
+ * @license Angular v4.1.2-62a8618
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5765,7 +5765,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.1-e46a65f');
+const VERSION = new Version('4.1.2-62a8618');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.1-e46a65f
+ * @license Angular v4.1.2-62a8618
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
