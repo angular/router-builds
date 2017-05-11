3d5c67c1610037138d8c8d3841b27a79e76d6a59
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-11 07:32:51 +0000

    67dc970 docs(common): remove h1s from API docs

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.2.0-beta.1-d28a3f7
+ * @license Angular v4.2.0-beta.1-67dc970
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6037,7 +6037,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-d28a3f7');
+var VERSION = new Version('4.2.0-beta.1-67dc970');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
