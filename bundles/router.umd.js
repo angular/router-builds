aea8be8560d34141d83f37fe788053c5c30c8c8f
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-11 20:41:06 +0000

    abb36e3 fix(aio): switch to ngu-sw-manifest gen to unblock critical path (#16709)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-84c30be
+ * @license Angular v4.2.0-beta.1-abb36e3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -14,7 +14,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.2.0-beta.1-84c30be
+ * @license Angular v4.2.0-beta.1-abb36e3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
@@ -6026,7 +6026,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-beta.1-84c30be');
+var VERSION = new _angular_core.Version('4.2.0-beta.1-abb36e3');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-84c30be
+ * @license Angular v4.2.0-beta.1-abb36e3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
