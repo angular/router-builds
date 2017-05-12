383a9e9b549d1a05510cfefeb6e7bdb58ed5a156
Igor Minar <iminar@google.com>
2017-05-12 07:05:44 +0000

    37f1fcb docs: remove angular.io from changelog (#16723)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-4a599ee
+ * @license Angular v4.2.0-beta.1-37f1fcb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5765,7 +5765,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-4a599ee');
+const VERSION = new Version('4.2.0-beta.1-37f1fcb');
 
 /**
  * @license
