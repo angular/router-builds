790a4db31bd9f666da6ac5653bc5b9055ec378c0
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-12 21:07:44 +0000

    38c524d feat(core): introduce fixture.whenRenderingDone for testing (#16732)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-0626464
+ * @license Angular v4.2.0-beta.1-38c524d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5768,7 +5768,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-0626464');
+const VERSION = new Version('4.2.0-beta.1-38c524d');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-0626464
+ * @license Angular v4.2.0-beta.1-38c524d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
