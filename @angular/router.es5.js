9d9801f29b5a9792c6b6b5a0dde9f30deb2e9cc7
Igor Minar <iminar@google.com>
2017-05-12 05:59:26 +0000

    4a599ee ci: fix syntax error in `deploy.sh` (#16739)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.2.0-beta.1-decb4cc
+ * @license Angular v4.2.0-beta.1-4a599ee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6037,7 +6037,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-decb4cc');
+var VERSION = new Version('4.2.0-beta.1-4a599ee');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
