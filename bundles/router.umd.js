a9d61d4d41e7084b4a691194b1190da227e25f6c
Igor Minar <iminar@google.com>
2017-05-12 19:37:11 +0000

    0626464 build: use subshells when changing directories

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-21d213d
+ * @license Angular v4.2.0-beta.1-0626464
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -14,7 +14,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.2.0-beta.1-21d213d
+ * @license Angular v4.2.0-beta.1-0626464
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
@@ -6029,7 +6029,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-beta.1-21d213d');
+var VERSION = new _angular_core.Version('4.2.0-beta.1-0626464');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-21d213d
+ * @license Angular v4.2.0-beta.1-0626464
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
