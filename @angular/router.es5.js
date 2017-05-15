a4e08514ce87568151c3dd60a99016af74aae4fc
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-15 10:14:24 +0000

    a9d9aa1 build(aio): automatically link code blocks to API docs

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.2.0-beta.1-ecc356e
+ * @license Angular v4.2.0-beta.1-a9d9aa1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6040,7 +6040,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-ecc356e');
+var VERSION = new Version('4.2.0-beta.1-a9d9aa1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-ecc356e
+ * @license Angular v4.2.0-beta.1-a9d9aa1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
