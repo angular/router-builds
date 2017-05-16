2922dfc88c63d6d6bdafdbbecab033239275ce8d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-16 10:02:30 +0000

    221f85a fix(aio): no white band below toolbar when mobile

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-a68ad6d
+ * @license Angular v4.2.0-beta.1-221f85a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5768,7 +5768,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-a68ad6d');
+const VERSION = new Version('4.2.0-beta.1-221f85a');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-a68ad6d
+ * @license Angular v4.2.0-beta.1-221f85a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
