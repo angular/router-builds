f8effc8f1cfcf96ccfa53b64c1f461043e9ebaf2
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-16 17:39:14 +0000

    af99cf2 build(aio): compute `{@link ...} titles more effectively (#16813)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-221f85a
+ * @license Angular v4.2.0-beta.1-af99cf2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5768,7 +5768,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-221f85a');
+const VERSION = new Version('4.2.0-beta.1-af99cf2');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-221f85a
+ * @license Angular v4.2.0-beta.1-af99cf2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
