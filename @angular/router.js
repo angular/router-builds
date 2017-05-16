e4fac19e097d5c1c99253fae271d18a9887c0868
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-16 09:07:00 +0000

    2ee7662 test(aio): remove compileComponents from all tests

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-4419585
+ * @license Angular v4.2.0-beta.1-2ee7662
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5768,7 +5768,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-4419585');
+const VERSION = new Version('4.2.0-beta.1-2ee7662');
 
 /**
  * @license
