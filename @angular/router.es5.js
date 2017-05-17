d39eb1c02b78b89f941594a720df0b0ee975a059
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-17 00:58:20 +0000

    54a6e4f refactor(animations): make animation testing work with fixture.whenRenderingDone

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-d761059
+ * @license Angular v4.2.0-beta.1-54a6e4f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6024,7 +6024,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-d761059');
+var VERSION = new Version('4.2.0-beta.1-54a6e4f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
