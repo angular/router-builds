1da2b09def0c669e7d0692c419f8c0c8571ef351
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-17 16:00:40 +0000

    6416e79 build: do not create the bundles when updating the public API

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-238c523
+ * @license Angular v4.2.0-beta.1-6416e79
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-238c523');
+const VERSION = new Version('4.2.0-beta.1-6416e79');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-238c523
+ * @license Angular v4.2.0-beta.1-6416e79
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
