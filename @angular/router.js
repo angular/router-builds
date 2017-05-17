7081922cb2800c3b6a781191e92a447443eac978
Igor Minar <iminar@google.com>
2017-05-17 04:24:18 +0000

    593fe5e build(aio): enable HTTP/2 on the preview server (#16826)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-54a6e4f
+ * @license Angular v4.2.0-beta.1-593fe5e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-54a6e4f');
+const VERSION = new Version('4.2.0-beta.1-593fe5e');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-54a6e4f
+ * @license Angular v4.2.0-beta.1-593fe5e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
