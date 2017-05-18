f815aa71e2d1639a8f091362273c2eb88f47766e
Peter Bacon Darwin <pete@bacondarwin.com>
2017-05-18 14:09:39 +0000

    0e13a59 fix(aio): add small delay before autoscrolling

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-3361a7b
+ * @license Angular v4.2.0-beta.1-0e13a59
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */(function (global, factory) {
@@ -19,7 +19,7 @@ var __extends = (undefined && undefined.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-3361a7b
+ * @license Angular v4.2.0-beta.1-0e13a59
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ /**
@@ -6052,7 +6052,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-beta.1-3361a7b');
+var VERSION = new _angular_core.Version('4.2.0-beta.1-0e13a59');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-3361a7b
+ * @license Angular v4.2.0-beta.1-0e13a59
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
