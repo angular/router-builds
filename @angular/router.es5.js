5c3e515a24a155360f596d09354be8a726d1f416
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-19 13:30:32 +0000

    eed67dd feat(aio): update aio-shell class when sidenav opens or closes

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-be9e8b9
+ * @license Angular v4.2.0-beta.1-eed67dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ import { APP_BASE_HREF, HashLocationStrategy, LOCATION_INITIALIZED, Location, LocationStrategy, PathLocationStrategy, PlatformLocation } from '@angular/common';
@@ -6024,7 +6024,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-be9e8b9');
+var VERSION = new Version('4.2.0-beta.1-eed67dd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
