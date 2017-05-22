4a061081a23ba4a7127782d8da8c996add6d197c
GitHub <noreply@github.com>
2017-05-22 17:36:19 +0000

    11c10b2 fix(tsc-wrapped): resolve short-hand literal values to locals (#16873)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-rc.0-98849de
+ * @license Angular v4.2.0-rc.0-11c10b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-98849de
+ * @license Angular v4.2.0-rc.0-11c10b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
