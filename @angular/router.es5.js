8b3c41c1afb0d8ca4303e3f27ccc4c562fad7891
GitHub <noreply@github.com>
2017-05-22 18:37:25 +0000

    6e41add feat(tsc-wrapped): support template literals in metadata collection (#16880)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-rc.0-11c10b2
+ * @license Angular v4.2.0-rc.0-6e41add
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-11c10b2
+ * @license Angular v4.2.0-rc.0-6e41add
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
