7c2363d88e6cdf65368e2dc10f9cd324b94be9b3
Chuck Jazdzewski <chuckj@google.com>
2017-05-22 18:40:52 +0000

    08dfe91 feat(compiler): add location note to extracted xliff2 files (#16791)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-rc.0-85d4c4b
+ * @license Angular v4.2.0-rc.0-08dfe91
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-85d4c4b
+ * @license Angular v4.2.0-rc.0-08dfe91
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
