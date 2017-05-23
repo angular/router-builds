910c8465d6b60b952a37cfd4e8a76c1f3a6f441c
Chuck Jazdzewski <chuckj@google.com>
2017-05-23 00:14:54 +0000

    e7d9fd8 fix(animations): repair flicker issues with WA polyfill (#16937)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-08dfe91
+ * @license Angular v4.2.0-rc.0-e7d9fd8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_router,_angular_upgrade_static) { 'use strict';
 
 /**
- * @license Angular v4.2.0-rc.0-08dfe91
+ * @license Angular v4.2.0-rc.0-e7d9fd8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
