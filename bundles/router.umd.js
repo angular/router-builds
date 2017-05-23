b72f3e285ce18b69b6e65e127c22613cb4843728
Chuck Jazdzewski <chuckj@google.com>
2017-05-23 00:55:38 +0000

    f5b2ce0 docs: update the release schedule (#16943)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-78e3be1
+ * @license Angular v4.2.0-rc.0-f5b2ce0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ var __extends = (undefined && undefined.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-rc.0-78e3be1
+ * @license Angular v4.2.0-rc.0-f5b2ce0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-78e3be1
+ * @license Angular v4.2.0-rc.0-f5b2ce0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
