e0fae72e3bca6b2d3d8e7047c73b62c5ca3df8ee
Chuck Jazdzewski <chuckj@google.com>
2017-05-23 18:11:00 +0000

    1651a8f fix(tsc-wrapped): ignore `|null` and `|undefined` when collecting types (#16222)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-rc.0-a80ac0a
+ * @license Angular v4.2.0-rc.0-1651a8f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-a80ac0a
+ * @license Angular v4.2.0-rc.0-1651a8f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
