17d9e7afd24deaf8888afcb63c20228c0ec45e43
Chuck Jazdzewski <chuckj@google.com>
2017-05-23 21:03:15 +0000

    eba59aa refactor(compiler): add ability to produce stub .ngfactory / .ngsummary files (#16963)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.0-fa809ec
+ * @license Angular v4.2.0-rc.0-eba59aa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
