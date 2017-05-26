1c644663686040e472d29568a5e69a82233cce06
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-26 13:43:17 +0000

    6949510 feat(aio): provide noop service worker file

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.0-9e7e178
+ * @license Angular v4.2.0-rc.0-6949510
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
