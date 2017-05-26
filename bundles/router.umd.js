7509a0fd1cacbc15c116f8b11b79113127268db0
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 16:32:13 +0000

    7ffb75f refactor(compiler): change `bundle` to `flatModuleIndex` in the code (#17028)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-c0981b8
+ * @license Angular v4.2.0-rc.0-7ffb75f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.0-c0981b8
+ * @license Angular v4.2.0-rc.0-7ffb75f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-c0981b8
+ * @license Angular v4.2.0-rc.0-7ffb75f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
