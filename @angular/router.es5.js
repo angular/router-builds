8f7e1d695030390c48254bfb2021a5059469a63d
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 23:19:07 +0000

    0018acf fix(aio): remove es2015 syntax from the onerror webmaster tools debugger (#17056)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.1-51d7a65
+ * @license Angular v4.2.0-rc.1-0018acf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
