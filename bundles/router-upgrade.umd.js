07b65975f722633f8a05aa26fc1172bd06813fa2
Victor Berchet <victor@suumit.com>
2017-05-31 22:54:05 +0000

    665e707 fix(animations): always change to desired animation state even if no transition fires (#17025)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-c20f60b
+ * @license Angular v4.2.0-rc.1-665e707
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_router,_angular_upgrade_static) { 'use strict';
 
 /**
- * @license Angular v4.2.0-rc.1-c20f60b
+ * @license Angular v4.2.0-rc.1-665e707
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-c20f60b
+ * @license Angular v4.2.0-rc.1-665e707
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
