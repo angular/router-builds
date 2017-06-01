58b827f793ff8a6e0c97cf453dfefb75dc935a06
Victor Berchet <victor@suumit.com>
2017-06-01 16:59:33 +0000

    1338995 fix(aio): fix home learn more button alignment (#17154)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.1-47e4fca
+ * @license Angular v4.2.0-rc.1-1338995
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
