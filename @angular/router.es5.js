a1bf062ca320a8d7b046d580fceaa4b4445bf4ea
Victor Berchet <victor@suumit.com>
2017-06-01 22:19:57 +0000

    82ec02d docs: add changelog for 4.2.0-rc.2

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.1-7822187
+ * @license Angular v4.2.0-rc.2-82ec02d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-7822187
+ * @license Angular v4.2.0-rc.2-82ec02d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
