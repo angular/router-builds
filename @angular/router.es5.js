24b10126f7ff33cc6ceed15e73183de38c74d6b3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-02 17:36:16 +0000

    d5ce086 build(aio): fix up API doc-gen templates

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-82ec02d
+ * @license Angular v4.2.0-rc.2-d5ce086
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-82ec02d
+ * @license Angular v4.2.0-rc.2-d5ce086
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
