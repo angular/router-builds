3996d65483959547f767be32b7c44b0402043988
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-05 18:38:31 +0000

    47c2a2e ci: switch CircleCI to 2.0

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-7c78282
+ * @license Angular v4.2.0-rc.2-47c2a2e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_router,_angular_upgrade_static) { 'use strict';
 
 /**
- * @license Angular v4.2.0-rc.2-7c78282
+ * @license Angular v4.2.0-rc.2-47c2a2e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
