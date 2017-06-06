170471783a115f009549196b9853d3e7cb939507
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-06 21:28:30 +0000

    269bbe0 fix(upgrade): call setInterval outside the Angular zone

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-bb2fc6b
+ * @license Angular v4.2.0-rc.2-269bbe0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-bb2fc6b
+ * @license Angular v4.2.0-rc.2-269bbe0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
