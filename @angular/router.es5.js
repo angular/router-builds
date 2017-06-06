f4c9778979ce2d074807c125475e91e9d1d26508
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-06 21:39:03 +0000

    b00b80a feat(compiler-cli): introduce synchronous codegen API

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-269bbe0
+ * @license Angular v4.2.0-rc.2-b00b80a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-269bbe0
+ * @license Angular v4.2.0-rc.2-b00b80a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
