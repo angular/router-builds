2508653a782caf9a7e7554731d7cf3f561093a31
GitHub <noreply@github.com>
2017-06-06 06:27:44 +0000

    42176a7 build(aio): upgrade ngo and purify (#17210)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-76b4b80
+ * @license Angular v4.2.0-rc.2-42176a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
