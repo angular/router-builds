e8de255203b44fd6474b3c2a64eeab1fe5db5d9e
Igor Minar <iminar@google.com>
2017-06-08 19:47:18 +0000

    75a311f fix(aio): style and fix hover color for features cta button (#17342)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-4d2ee51
+ * @license Angular v4.2.0-rc.2-75a311f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.2-4d2ee51
+ * @license Angular v4.2.0-rc.2-75a311f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-4d2ee51
+ * @license Angular v4.2.0-rc.2-75a311f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
