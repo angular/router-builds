b1499ab7b63a3d10b8ae89ce62c72dee324ddd23
Igor Minar <iminar@google.com>
2017-06-08 19:12:33 +0000

    67ef0f0 fix(aio): api list column overflow fix (#17300)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-e9886d7
+ * @license Angular v4.2.0-rc.2-67ef0f0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_common,_angular_common_testing,_angular_core,_angular_router) { 'use strict';
 
 /**
- * @license Angular v4.2.0-rc.2-e9886d7
+ * @license Angular v4.2.0-rc.2-67ef0f0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
