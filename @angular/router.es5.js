6d4737db7383e039ec96bf1d7f8f333af79ab4a7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 16:54:56 +0000

    022835b ci: update Chromium to 464641 (Chrome v59)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-33ba3e3
+ * @license Angular v4.2.0-rc.2-022835b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
