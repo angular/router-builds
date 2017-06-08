da8d6f411c27070c19896fba65ac4f84e1801b70
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 17:43:55 +0000

    e9886d7 fix(animations): evaluate substitutions on option param values

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-022835b
+ * @license Angular v4.2.0-rc.2-e9886d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-022835b
+ * @license Angular v4.2.0-rc.2-e9886d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
