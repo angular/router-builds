e830b2767751f7a8b55e4e7acf3183bffb495c5f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-09 07:39:19 +0000

    f7422a9 fix(aio): animate hamburger in/out

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-cf0a9e0
+ * @license Angular v4.2.0-f7422a9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-cf0a9e0
+ * @license Angular v4.2.0-f7422a9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
