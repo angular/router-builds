1c9a1b77f23ad27e4c1f49b32a64ee2573025b61
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-09 13:56:16 +0000

    a0b30e5 docs(aio): remove example specific `.gitignore` files

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-e894f5c
+ * @license Angular v4.2.0-a0b30e5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-e894f5c
+ * @license Angular v4.2.0-a0b30e5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
