cc3af64d2dadc6d74187a44751e4dd9a0fa2981f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-11 18:06:22 +0000

    d961911 docs(aio): fix broken example in upgrade guide

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.1-e543272
+ * @license Angular v4.2.1-d961911
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
