6843915e958abf7137d37e34668f19168bb3137d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-11 14:26:09 +0000

    e543272 fix(aio): remove gap between sidenav menus

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.1-df7774c
+ * @license Angular v4.2.1-e543272
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-df7774c
+ * @license Angular v4.2.1-e543272
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
