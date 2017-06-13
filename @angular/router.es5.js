6c17c4c6490ad6df8972bfeff47a88d5a5772024
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 07:19:48 +0000

    fc774a1 docs: clarify when non-null-assertion-operator is needed in template-syntax

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-d647db2
+ * @license Angular v4.2.2-fc774a1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
