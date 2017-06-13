aed4789a0f6d8af0ebb4af69e6d5c60ff59f99dc
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 13:15:51 +0000

    052331f build(aio): add staging environment

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-541c9a9
+ * @license Angular v4.2.2-052331f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-541c9a9
+ * @license Angular v4.2.2-052331f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
