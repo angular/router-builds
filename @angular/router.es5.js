c4fc943482d14e396be18233609b0ff3f8bfa944
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 10:51:44 +0000

    7caa0a8 fix(aio): show search results when search box gets focus

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-fc774a1
+ * @license Angular v4.2.2-7caa0a8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-fc774a1
+ * @license Angular v4.2.2-7caa0a8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
