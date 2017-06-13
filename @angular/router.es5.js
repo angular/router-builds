b6460ab2b2c2d6bbfb7e6112a412c22e5a9b927f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 22:57:53 +0000

    11dfb68 ci: update github templates (#17466)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-4164369
+ * @license Angular v4.2.2-11dfb68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
