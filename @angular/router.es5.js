323d543bb6e45eaf5c27f17ee751489d519fc7b3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-15 07:04:58 +0000

    06a0cf2 docs(aio): http guide shows how to import toPromise operator from rxjs

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-a8afa65
+ * @license Angular v4.2.2-06a0cf2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-a8afa65
+ * @license Angular v4.2.2-06a0cf2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
