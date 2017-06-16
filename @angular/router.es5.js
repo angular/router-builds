41238d3f73fdf6e346f5d7392231af531c0f69dc
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-16 07:24:09 +0000

    dbc6a4c build(aio): do not fail if `check-env` for the main angular project fails

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-b192dd5
+ * @license Angular v4.2.2-dbc6a4c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-b192dd5
+ * @license Angular v4.2.2-dbc6a4c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
