b290deb0ab54fd8d3fdec6497b999397edc5f084
Hans Larsen <hans@hansl.ca>
2017-06-16 19:34:49 +0000

    71a8ef5 fix(aio): improve no-javascript screen

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-623b9c6
+ * @license Angular v4.2.3-71a8ef5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_common,_angular_common_testing,_angular_core,_angular_router) { 'use strict';
 
 /**
- * @license Angular v4.2.3-623b9c6
+ * @license Angular v4.2.3-71a8ef5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-623b9c6
+ * @license Angular v4.2.3-71a8ef5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
