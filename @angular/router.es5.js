23aeac17345c86d2f7da7b6de7e7258a6c9d1d40
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-16 07:21:54 +0000

    2cc931e fix(aio): add missing redirect rule for `/styleguide`

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-ce00fa3
+ * @license Angular v4.2.2-2cc931e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-ce00fa3
+ * @license Angular v4.2.2-2cc931e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
