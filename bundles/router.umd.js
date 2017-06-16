16b3eda86afa2b307e8947ec848363ee9fa6c703
Hans Larsen <hans@hansl.ca>
2017-06-16 21:14:20 +0000

    ab81c1c build(aio): update @angular/service-worker to 1.0.0-beta.16

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-71a8ef5
+ * @license Angular v4.2.3-ab81c1c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.3-71a8ef5
+ * @license Angular v4.2.3-ab81c1c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-71a8ef5
+ * @license Angular v4.2.3-ab81c1c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
