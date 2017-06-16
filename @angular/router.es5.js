c46e40394e11930ee987170b6c097bcf2ecbb581
Hans <hans@hansl.ca>
2017-06-16 18:42:15 +0000

    9315ab8 test(compiler): fix typo "mamespace"

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-1ddbddb
+ * @license Angular v4.2.2-9315ab8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-1ddbddb
+ * @license Angular v4.2.2-9315ab8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
