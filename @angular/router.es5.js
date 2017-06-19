3ec22d32eb25dcc5896a848e36dc0987be8e4b20
Hans <hans@hansl.ca>
2017-06-19 22:19:18 +0000

    319ce18 docs(RELEASE_SCHEDULE): fix version numbers for August/September releases

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-aa92f3a
+ * @license Angular v4.2.2-319ce18
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-aa92f3a
+ * @license Angular v4.2.2-319ce18
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
