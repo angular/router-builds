320d1cbea4cc35d274e1f40715ebea32e24ec980
Hans <hans@hansl.ca>
2017-06-19 22:45:20 +0000

    1c6a252 build(aio): upgrade jasmine to v2.6.4

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-d3c92a3
+ * @license Angular v4.2.2-1c6a252
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6075,7 +6075,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-d3c92a3');
+var VERSION = new Version('4.2.2-1c6a252');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-d3c92a3
+ * @license Angular v4.2.2-1c6a252
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5806,7 +5806,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-d3c92a3');
+const VERSION = new Version('4.2.2-1c6a252');
 
 /**
  * @license
