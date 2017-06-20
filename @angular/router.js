f18cf5bbc798b7fd4151a9efcf80823f4cb95a20
Hans Larsen <hans@hansl.ca>
2017-06-20 16:39:21 +0000

    4ab7353 fix(forms): roll back breaking change with min/max directives

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-2e8efda
+ * @license Angular v4.2.3-4ab7353
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5806,7 +5806,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.3-2e8efda');
+const VERSION = new Version('4.2.3-4ab7353');
 
 /**
  * @license
