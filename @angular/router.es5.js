a58e35a705b15199831a0876d0bd73f8a9954185
Hans <hans@hansl.ca>
2017-06-20 21:55:01 +0000

    46ddf50 fix(tsc-wrapped): skip collecting metadata for default functions

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-8c89cc4
+ * @license Angular v4.2.2-46ddf50
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6076,7 +6076,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-8c89cc4');
+var VERSION = new Version('4.2.2-46ddf50');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
