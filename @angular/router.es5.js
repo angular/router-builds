1e1101a29d04a6693d304637842a512998ccb2b5
Hans <hans@hansl.ca>
2017-06-21 18:57:17 +0000

    0034bb2 fix(aio): restore component-styles/exclude hidden children

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-d168528
+ * @license Angular v4.2.2-0034bb2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6076,7 +6076,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-d168528');
+var VERSION = new Version('4.2.2-0034bb2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-d168528
+ * @license Angular v4.2.2-0034bb2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5807,7 +5807,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-d168528');
+const VERSION = new Version('4.2.2-0034bb2');
 
 /**
  * @license
