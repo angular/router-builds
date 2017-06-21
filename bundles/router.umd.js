a5dcda826cc59b86213bbcc56cf6669fe1055fe6
Hans Larsen <hans@hansl.ca>
2017-06-21 23:53:33 +0000

    c112232 fix(compiler): avoid emitting self importing factories

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-3634832
+ * @license Angular v4.2.3-c112232
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.3-3634832
+ * @license Angular v4.2.3-c112232
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6091,7 +6091,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.3-3634832');
+var VERSION = new _angular_core.Version('4.2.3-c112232');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-3634832
+ * @license Angular v4.2.3-c112232
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
