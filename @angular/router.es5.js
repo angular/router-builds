ff4d21811c3d4f2b2112b5fc1313b1ad85a56396
Hans Larsen <hans@hansl.ca>
2017-06-22 00:29:30 +0000

    02a38d3 docs: add changelog for 4.2.4

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-37cdc4f
+ * @license Angular v4.2.2-02a38d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6076,7 +6076,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-37cdc4f');
+var VERSION = new Version('4.2.2-02a38d3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-37cdc4f
+ * @license Angular v4.2.2-02a38d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
