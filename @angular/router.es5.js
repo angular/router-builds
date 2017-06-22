9ea6b429b9bf9a3d68a9e760b61ff6e4e651523e
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:14:52 +0000

    92bcfef docs(aio): remove min, max & move built-in validators paragraph

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.0-68b64a2
+ * @license Angular v4.3.0-beta.0-92bcfef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6076,7 +6076,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-68b64a2');
+var VERSION = new Version('4.3.0-beta.0-92bcfef');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-68b64a2
+ * @license Angular v4.3.0-beta.0-92bcfef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-68b64a2
+ * @license Angular v4.3.0-beta.0-92bcfef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6091,7 +6091,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-68b64a2');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-92bcfef');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
