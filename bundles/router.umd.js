18b439e7fea7be87fab3d842029e76002b5ce190
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 16:47:35 +0000

    7ba720a docs(aio): resources.json - replace “Angular 2” in titles/descriptions

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-eacc36b
+ * @license Angular v4.3.0-beta.0-7ba720a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-eacc36b
+ * @license Angular v4.3.0-beta.0-7ba720a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6091,7 +6091,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-eacc36b');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-7ba720a');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-eacc36b
+ * @license Angular v4.3.0-beta.0-7ba720a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
