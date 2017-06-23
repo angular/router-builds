7a090e750a2dac1d234aa3cd8f0cf74610a6fe83
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 19:21:45 +0000

    14d2de1 fix(aio): fix links on `/about` in Firefox

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-849200b
+ * @license Angular v4.3.0-beta.0-14d2de1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5807,7 +5807,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-849200b');
+const VERSION = new Version('4.3.0-beta.0-14d2de1');
 
 /**
  * @license
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-849200b
+ * @license Angular v4.3.0-beta.0-14d2de1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-849200b
+ * @license Angular v4.3.0-beta.0-14d2de1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6091,7 +6091,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-849200b');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-14d2de1');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
