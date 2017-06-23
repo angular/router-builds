678855b3ff44809a33fbc78c78bf01daf21bcdc3
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 17:49:34 +0000

    9344c86 docs: fix “under to hood” typo in changelog.md

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.4-353de51
+ * @license Angular v4.2.4-9344c86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5807,7 +5807,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.4-353de51');
+const VERSION = new Version('4.2.4-9344c86');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.4-353de51
+ * @license Angular v4.2.4-9344c86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
