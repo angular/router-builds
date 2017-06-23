474a295e42ebc80e34e0333a8f04562b5ceb1b87
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 23:41:14 +0000

    43c187b refactor(forms): remove usage of deprecated Renderer

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.0-3097083
+ * @license Angular v4.3.0-beta.0-43c187b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6083,7 +6083,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-3097083');
+var VERSION = new Version('4.3.0-beta.0-43c187b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
