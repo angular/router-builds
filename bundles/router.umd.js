6bcbb95bc8451cec0c29cf252690e1a220ff6884
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 22:11:49 +0000

    b754e60 feat(compiler): add support `::ng-deep`

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-e80851d
+ * @license Angular v4.3.0-beta.0-b754e60
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-e80851d
+ * @license Angular v4.3.0-beta.0-b754e60
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6091,7 +6091,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-e80851d');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-b754e60');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-e80851d
+ * @license Angular v4.3.0-beta.0-b754e60
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
