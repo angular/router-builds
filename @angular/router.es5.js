62ab496bd93168e34bcc02622fb05783263b67c8
Matias Niemelä <matias@yearofmoo.com>
2017-06-26 18:45:20 +0000

    f365a0f docs(aio): animations typos fixed

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.0-233044e
+ * @license Angular v4.3.0-beta.0-f365a0f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6083,7 +6083,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-233044e');
+var VERSION = new Version('4.3.0-beta.0-f365a0f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
