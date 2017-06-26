1c19467b57adca454221ae6c48e61d5468ca0229
Matias Niemelä <matias@yearofmoo.com>
2017-06-26 20:42:42 +0000

    448d9f9 test(compiler): add a test for components not part of any `NgModule`

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-97bb374
+ * @license Angular v4.3.0-beta.0-448d9f9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_common,_angular_common_testing,_angular_core,_angular_router) { 'use strict';
 
 /**
- * @license Angular v4.3.0-beta.0-97bb374
+ * @license Angular v4.3.0-beta.0-448d9f9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-97bb374
+ * @license Angular v4.3.0-beta.0-448d9f9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
