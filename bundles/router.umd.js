dd7454740d45b873e18b6d5fa6195e78fd226123
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 17:00:43 +0000

    8d01db4 docs: Updated router guide content and examples for paramMap

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-4268c82
+ * @license Angular v4.3.0-beta.0-8d01db4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-4268c82
+ * @license Angular v4.3.0-beta.0-8d01db4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6098,7 +6098,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-4268c82');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-8d01db4');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-4268c82
+ * @license Angular v4.3.0-beta.0-8d01db4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
