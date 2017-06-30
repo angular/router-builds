16a22d8111f7e745a3aa68d4fdd7965bc80048b9
Matias Niemelä <matias@yearofmoo.com>
2017-06-30 00:25:11 +0000

    f31b0d6 fix(core): add needed closure compiler warning suppression

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.4-7728d7e
+ * @license Angular v4.2.4-f31b0d6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.4-7728d7e
+ * @license Angular v4.2.4-f31b0d6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6091,7 +6091,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.4-7728d7e');
+var VERSION = new _angular_core.Version('4.2.4-f31b0d6');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
