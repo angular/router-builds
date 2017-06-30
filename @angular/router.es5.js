bcad878e6b50027fa6b04e2ea6163507c49b22ea
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-30 11:15:25 +0000

    4459e0c fix(aio): restore visible sidenav keyboard focus; shift TOC up

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-40921bb
+ * @license Angular v4.3.0-beta.1-4459e0c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6083,7 +6083,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-40921bb');
+var VERSION = new Version('4.3.0-beta.1-4459e0c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-40921bb
+ * @license Angular v4.3.0-beta.1-4459e0c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
