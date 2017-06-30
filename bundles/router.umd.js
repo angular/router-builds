6296a696b55c22b2787bc1213d8ff9714b3d8e10
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-30 11:24:24 +0000

    b052ef5 fix(aio): do not include hidden content in window title

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-4459e0c
+ * @license Angular v4.3.0-beta.1-b052ef5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.1-4459e0c
+ * @license Angular v4.3.0-beta.1-b052ef5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6098,7 +6098,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.1-4459e0c');
+var VERSION = new _angular_core.Version('4.3.0-beta.1-b052ef5');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
