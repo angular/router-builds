3b5bbceaaa4a15683b12407863f9cd87afa545da
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-01 14:12:16 +0000

    d5dc53e ci(aio): avoid printing too large PWA report (and restore `print-logs.sh`)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-8e00161
+ * @license Angular v4.3.0-beta.1-d5dc53e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.1-8e00161
+ * @license Angular v4.3.0-beta.1-d5dc53e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6098,7 +6098,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.1-8e00161');
+var VERSION = new _angular_core.Version('4.3.0-beta.1-d5dc53e');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-8e00161
+ * @license Angular v4.3.0-beta.1-d5dc53e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
