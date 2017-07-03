664acb86c6ac4901d274a49289fc266cc2fb9249
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-03 07:43:29 +0000

    f54a901 docs(aio): fix visual-studio-2015 typo

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-8a1a989
+ * @license Angular v4.3.0-beta.1-f54a901
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5934,7 +5934,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-8a1a989');
+const VERSION = new Version('4.3.0-beta.1-f54a901');
 
 /**
  * @license
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-8a1a989
+ * @license Angular v4.3.0-beta.1-f54a901
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.1-8a1a989
+ * @license Angular v4.3.0-beta.1-f54a901
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6222,7 +6222,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.1-8a1a989');
+var VERSION = new _angular_core.Version('4.3.0-beta.1-f54a901');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
