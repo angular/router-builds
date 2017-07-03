1ee641e6e5d88ebbcaa7843767ff8cb9c7efb962
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-03 07:43:42 +0000

    fa34ed8 ci(aio): use valid database path for Firebase payload size upload

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-f54a901
+ * @license Angular v4.3.0-beta.1-fa34ed8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6207,7 +6207,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-f54a901');
+var VERSION = new Version('4.3.0-beta.1-fa34ed8');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-f54a901
+ * @license Angular v4.3.0-beta.1-fa34ed8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
