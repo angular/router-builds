05e14f1f6aabb3106b5f8f13c8bf0d07e6727a33
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-05 22:06:05 +0000

    92d7ecf docs: shefali edits to Authors Style Guide (#17853)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-c6b75b0
+ * @license Angular v4.3.0-beta.1-92d7ecf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5934,7 +5934,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-c6b75b0');
+const VERSION = new Version('4.3.0-beta.1-92d7ecf');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-c6b75b0
+ * @license Angular v4.3.0-beta.1-92d7ecf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
