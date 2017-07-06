93fb88937d6db8acd8cc101e2efe4cfacb771b4a
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-06 19:31:01 +0000

    70981c6 feat(aio): serve-and-sync command (#17850)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-b116901
+ * @license Angular v4.3.0-beta.1-70981c6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5934,7 +5934,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-b116901');
+const VERSION = new Version('4.3.0-beta.1-70981c6');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-b116901
+ * @license Angular v4.3.0-beta.1-70981c6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
