4fe94cd742726a0ca01d5d88e0f95a5b1db5ada3
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-06 18:32:20 +0000

    6fc5174 build: disable sauce connect logging (#17947)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-105e920
+ * @license Angular v4.3.0-beta.1-6fc5174
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6207,7 +6207,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-105e920');
+var VERSION = new Version('4.3.0-beta.1-6fc5174');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-105e920
+ * @license Angular v4.3.0-beta.1-6fc5174
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
