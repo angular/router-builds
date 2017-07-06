40df5ae1161f760e748243b7ab9f23f4b7134a63
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-06 18:32:44 +0000

    1bb2476 docs(aio): add instructions for example e2e tests to README (#17819)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-6fc5174
+ * @license Angular v4.3.0-beta.1-1bb2476
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6207,7 +6207,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-6fc5174');
+var VERSION = new Version('4.3.0-beta.1-1bb2476');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-6fc5174
+ * @license Angular v4.3.0-beta.1-1bb2476
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
