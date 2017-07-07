0cafddffb0a2006b35df30ecd19989c6e81c4704
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 18:36:37 +0000

    e1174f3 fix(aio): home & marketing styles cleanup (#17926)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-cb16e9c
+ * @license Angular v4.3.0-beta.1-e1174f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6221,7 +6221,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-cb16e9c');
+var VERSION = new Version('4.3.0-beta.1-e1174f3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-cb16e9c
+ * @license Angular v4.3.0-beta.1-e1174f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-cb16e9c');
+const VERSION = new Version('4.3.0-beta.1-e1174f3');
 
 /**
  * @license
