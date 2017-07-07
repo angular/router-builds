87cea4edb12ea39d63116250b117cfec1cf7b3e6
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 16:18:51 +0000

    cb16e9c fix(tsc-wrapped): emit exports metadata in flat modules (#17893)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-6bae73c
+ * @license Angular v4.3.0-beta.1-cb16e9c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6221,7 +6221,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-6bae73c');
+var VERSION = new Version('4.3.0-beta.1-cb16e9c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-6bae73c
+ * @license Angular v4.3.0-beta.1-cb16e9c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-6bae73c');
+const VERSION = new Version('4.3.0-beta.1-cb16e9c');
 
 /**
  * @license
