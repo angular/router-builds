eb7fa7478e47618d988e1e74375849dd7dad2666
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 23:52:30 +0000

    abbac4b feat(aio): use new ngo (#17977)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-671a175
+ * @license Angular v4.3.0-beta.1-abbac4b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6221,7 +6221,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-671a175');
+var VERSION = new Version('4.3.0-beta.1-abbac4b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
