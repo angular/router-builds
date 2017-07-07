e2fc2c52024e9743a90b3c072e0306d67b7946b6
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 23:35:41 +0000

    0ede642 docs(aio): Update `NgStyle` and `NgClass` to use quoted keys

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-8e28382
+ * @license Angular v4.3.0-beta.1-0ede642
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-8e28382');
+const VERSION = new Version('4.3.0-beta.1-0ede642');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-8e28382
+ * @license Angular v4.3.0-beta.1-0ede642
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
