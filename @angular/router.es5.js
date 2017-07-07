199d58404be0681f9bf65937e91c64bb8ce89046
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 18:38:43 +0000

    504500d fix(aio): activate ServiceWorker updates asap (#17699)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-e1174f3
+ * @license Angular v4.3.0-beta.1-504500d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6221,7 +6221,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-e1174f3');
+var VERSION = new Version('4.3.0-beta.1-504500d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
