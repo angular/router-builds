123b7db2e454f6732716593c9fd7202842134c69
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 19:20:00 +0000

    72747e5 ci: disable sauce-connect logging for realz (#17995)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-504500d
+ * @license Angular v4.3.0-beta.1-72747e5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.1-504500d
+ * @license Angular v4.3.0-beta.1-72747e5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6236,7 +6236,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.1-504500d');
+var VERSION = new _angular_core.Version('4.3.0-beta.1-72747e5');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-504500d
+ * @license Angular v4.3.0-beta.1-72747e5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
