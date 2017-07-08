e7aca7eeb7aca4346d7d69727d7ed90a7e1df90c
Jason Aden <jasonaden@google.com>
2017-07-08 05:35:21 +0000

    cc611c9 docs: add changelog for 4.3.0-rc.0

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-05a33d5
+ * @license Angular v4.3.0-rc.0-cc611c9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6221,7 +6221,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-05a33d5');
+var VERSION = new Version('4.3.0-rc.0-cc611c9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
