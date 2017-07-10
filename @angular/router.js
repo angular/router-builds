72b2fb4705f9dd2e1ec690f0e01f3c7de6be580f
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-10 12:01:13 +0000

    274c229 fix(aio): fix text blurring when hovering over item in resources

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-ab13f1f
+ * @license Angular v4.2.6-274c229
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5821,7 +5821,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.6-ab13f1f');
+const VERSION = new Version('4.2.6-274c229');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-ab13f1f
+ * @license Angular v4.2.6-274c229
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
