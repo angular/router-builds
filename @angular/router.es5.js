c7e238bcd16176c575dacce240b5d85a0ea2848b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-11 14:30:02 +0000

    e28f097 ci: add aio/content to aio group

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-e8bbf86
+ * @license Angular v4.3.0-rc.0-e28f097
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6221,7 +6221,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-e8bbf86');
+var VERSION = new Version('4.3.0-rc.0-e28f097');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
