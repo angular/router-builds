ae5acdf77e0866245d392b34cf4b1a6184b50cdd
Igor Minar <iminar@google.com>
2017-07-11 20:13:31 +0000

    bbf2133 docs: update ECMAScript info (#17801)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-ec14679
+ * @license Angular v4.3.0-rc.0-bbf2133
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-ec14679');
+const VERSION = new Version('4.3.0-rc.0-bbf2133');
 
 /**
  * @license
