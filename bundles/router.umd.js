4ca22c81a2e2f40ec793a1b40e9c8a95d5ef8ec5
Igor Minar <iminar@google.com>
2017-07-13 19:49:56 +0000

    bc3b2ac docs(aio): Updated resources to UI Components (#18067)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-4f1e4ff
+ * @license Angular v4.3.0-rc.0-bc3b2ac
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-4f1e4ff
+ * @license Angular v4.3.0-rc.0-bc3b2ac
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6236,7 +6236,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-rc.0-4f1e4ff');
+var VERSION = new _angular_core.Version('4.3.0-rc.0-bc3b2ac');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-4f1e4ff
+ * @license Angular v4.3.0-rc.0-bc3b2ac
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
