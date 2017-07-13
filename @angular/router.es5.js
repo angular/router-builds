a855aa3150b3af0218d5a6425db8602c7fca3246
Igor Minar <iminar@google.com>
2017-07-13 22:42:38 +0000

    1bf7ba8 ci(aio): exclude changes in aio/content folder

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-db96c96
+ * @license Angular v4.3.0-rc.0-1bf7ba8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6221,7 +6221,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-db96c96');
+var VERSION = new Version('4.3.0-rc.0-1bf7ba8');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-db96c96
+ * @license Angular v4.3.0-rc.0-1bf7ba8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
