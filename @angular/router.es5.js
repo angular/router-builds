27a91a20ad1edfa8aff4e58ca4f7715481be3c39
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 20:10:43 +0000

    e0fa727 fix(aio): document XSRF protection in HttpClient

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-3ecc5e5
+ * @license Angular v4.3.0-rc.0-e0fa727
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6221,7 +6221,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-3ecc5e5');
+var VERSION = new Version('4.3.0-rc.0-e0fa727');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
