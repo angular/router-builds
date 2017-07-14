4c129603f616690a5fe20e20581d18de7146803a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 18:22:23 +0000

    f7686d4 Revert "fix(animations): make sure @.disabled works in non-animation components"

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-01a2688
+ * @license Angular v4.3.0-rc.0-f7686d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6221,7 +6221,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-01a2688');
+var VERSION = new Version('4.3.0-rc.0-f7686d4');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-01a2688
+ * @license Angular v4.3.0-rc.0-f7686d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
