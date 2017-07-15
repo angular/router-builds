953e6dc108f299d29c0346f927b3159bd756585e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-15 14:55:13 +0000

    95698d9 fix(aio): remove title from callout

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-c649da9
+ * @license Angular v4.3.0-95698d9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6221,7 +6221,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-c649da9');
+var VERSION = new Version('4.3.0-95698d9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
