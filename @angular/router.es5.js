682d76e24869b611d80caf2c304b2473a44e11e2
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-15 14:53:37 +0000

    34834a9 fix(aio): remove unused `news.html` file

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-6e2ddcc
+ * @license Angular v4.3.0-34834a9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6221,7 +6221,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-6e2ddcc');
+var VERSION = new Version('4.3.0-34834a9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-6e2ddcc
+ * @license Angular v4.3.0-34834a9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
