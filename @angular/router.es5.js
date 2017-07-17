de3e3cae11061c95c7e2ce909215d1a0c07ad91f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 21:45:29 +0000

    9f99f4f docs: fix HttpClient sample

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-c6ad212
+ * @license Angular v4.3.0-9f99f4f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6221,7 +6221,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-c6ad212');
+var VERSION = new Version('4.3.0-9f99f4f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
