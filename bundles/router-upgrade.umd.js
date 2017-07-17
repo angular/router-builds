8ccf8a6af27b1a99383c2aa73f1fa99beaeb10f4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 22:10:36 +0000

    3f331b5 docs(aio): update jelbourn photo

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-375d598
+ * @license Angular v4.3.0-3f331b5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_router,_angular_upgrade_static) { 'use strict';
 
 /**
- * @license Angular v4.3.0-375d598
+ * @license Angular v4.3.0-3f331b5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-375d598
+ * @license Angular v4.3.0-3f331b5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
