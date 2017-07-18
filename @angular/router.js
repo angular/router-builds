e815c779a879b6d3fce700457de1c8a0d2eb7c51
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-18 11:13:24 +0000

    c453b7b build(aio): fail doc-gen if referenced images are missing

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-3f331b5
+ * @license Angular v4.3.0-c453b7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-3f331b5');
+const VERSION = new Version('4.3.0-c453b7b');
 
 /**
  * @license
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-3f331b5
+ * @license Angular v4.3.0-c453b7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-3f331b5
+ * @license Angular v4.3.0-c453b7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6236,7 +6236,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-3f331b5');
+var VERSION = new _angular_core.Version('4.3.0-c453b7b');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
