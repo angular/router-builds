5e9a81e771f7c45c877882def44c4eb5be5a365e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-18 17:17:32 +0000

    7d0f2cd fix(aio): remove title attribute from `CodeExampleComponent`

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-36faba1
+ * @license Angular v4.3.0-7d0f2cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-36faba1');
+const VERSION = new Version('4.3.0-7d0f2cd');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-36faba1
+ * @license Angular v4.3.0-7d0f2cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
