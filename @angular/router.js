14783bb6518fc44388fa35cbd3006e44f95306a1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 21:31:06 +0000

    8076482 fix(platform-server): provide XhrFactory for HttpClient

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-a5205c6
+ * @license Angular v4.3.0-8076482
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5974,7 +5974,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-a5205c6');
+const VERSION = new Version('4.3.0-8076482');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-a5205c6
+ * @license Angular v4.3.0-8076482
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
