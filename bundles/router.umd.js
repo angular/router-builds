da9304500cbdec8cebbef931c92043a55827ad44
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-28 22:58:04 +0000

    4620753 ci: short-circuit npm install for aio builds that use yarn only

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-416ed69
+ * @license Angular v5.0.0-beta.1-4620753
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.1-416ed69
+ * @license Angular v5.0.0-beta.1-4620753
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6575,7 +6575,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.1-416ed69');
+var VERSION = new _angular_core.Version('5.0.0-beta.1-4620753');
 
 exports.Route = Route;
 exports.RouterLink = RouterLink;
