4065c03baad013955ad300faf7a77356e638a5dc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 19:47:27 +0000

    381471d fix(compiler): fix for element needing implicit parent placed in top-level ng-container

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-ebef5e6
+ * @license Angular v5.0.0-beta.1-381471d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6285,7 +6285,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.1-ebef5e6');
+const VERSION = new Version('5.0.0-beta.1-381471d');
 
 /**
  * @fileoverview added by tsickle
