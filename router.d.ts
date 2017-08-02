18f2d1a6aaefb6f63f9d4322485b9707b1a11ec3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-08-02 20:58:16 +0000

    1e1af7f docs: changelog for 5.0.0-beta.2 release

---
+++ router.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public_api';
+export { RouterEvent as ɵa } from './src/events';
+export { ROUTER_FORROOT_GUARD as ɵb, RouterInitializer as ɵh, getAppInitializer as ɵi, getBootstrapListener as ɵj, provideForRootGuard as ɵe, provideLocationStrategy as ɵd, provideRouterInitializer as ɵk, rootRoute as ɵg, routerNgProbeToken as ɵc, setupRouter as ɵf } from './src/router_module';
+export { Tree as ɵl, TreeNode as ɵm } from './src/utils/tree';
