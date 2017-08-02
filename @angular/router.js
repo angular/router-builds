e6e89dc2d89cec84644d9c8eb4b9e75061734d95
Victor Berchet <victor@suumit.com>
2017-08-02 23:54:18 +0000

    99017bf fix(aio): correctly redirect `cookbook/a1-a2-quick-reference.html` (#18418)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-4d117fa
+ * @license Angular v5.0.0-beta.2-99017bf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6423,7 +6423,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.2-4d117fa');
+const VERSION = new Version('5.0.0-beta.2-99017bf');
 
 /**
  * @fileoverview added by tsickle
