b1e0759a143c1183d07a63c2d2a07d2014958812
Victor Berchet <victor@suumit.com>
2017-08-02 23:23:39 +0000

    5cc9913 docs(aio): replace old blog link in footer (#18448)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.2-9adf40a
+ * @license Angular v5.0.0-beta.2-5cc9913
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6738,7 +6738,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.2-9adf40a');
+var VERSION = new Version('5.0.0-beta.2-5cc9913');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-9adf40a
+ * @license Angular v5.0.0-beta.2-5cc9913
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6423,7 +6423,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.2-9adf40a');
+const VERSION = new Version('5.0.0-beta.2-5cc9913');
 
 /**
  * @fileoverview added by tsickle
