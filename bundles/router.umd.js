02bcd5113206503e54871790b61548ccb2c1f15a
Victor Berchet <victor@suumit.com>
2017-08-02 23:23:32 +0000

    b65fe3e ci: remove chromium fold reference (#18445)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-dbc5c58
+ * @license Angular v4.3.3-b65fe3e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.3-dbc5c58
+ * @license Angular v4.3.3-b65fe3e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6268,7 +6268,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.3-dbc5c58');
+var VERSION = new _angular_core.Version('4.3.3-b65fe3e');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-dbc5c58
+ * @license Angular v4.3.3-b65fe3e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
