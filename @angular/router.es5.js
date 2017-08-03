1c4ef203ea6320471d2f17b91e63309bb2723b35
Victor Berchet <victor@suumit.com>
2017-08-03 18:24:46 +0000

    e7e7622 fix(compiler): ignore @import in multi-line css (#18452)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.3-4176832
+ * @license Angular v4.3.3-e7e7622
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6253,7 +6253,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.3-4176832');
+var VERSION = new Version('4.3.3-e7e7622');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-4176832
+ * @license Angular v4.3.3-e7e7622
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
