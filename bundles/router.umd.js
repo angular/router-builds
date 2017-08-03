0e44d29b5f42297cf2d5da53209283b38f3e2e91
Victor Berchet <victor@suumit.com>
2017-08-03 00:35:20 +0000

    7c47b62 fix(compiler): cleanly compile with TypeScript 2.4 (#18456)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-6a88659
+ * @license Angular v5.0.0-beta.2-7c47b62
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.2-6a88659
+ * @license Angular v5.0.0-beta.2-7c47b62
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6753,7 +6753,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.2-6a88659');
+var VERSION = new _angular_core.Version('5.0.0-beta.2-7c47b62');
 
 exports.Route = Route;
 exports.RouterLink = RouterLink;
