f553a7b32ce2b4f0e13269cf368143ff372f4275
Victor Berchet <victor@suumit.com>
2017-08-03 00:43:22 +0000

    5f501c7 refactor(forms): compile forms cleanly with TypeScript 2.4 (#18462)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-ea07856
+ * @license Angular v5.0.0-beta.2-5f501c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6423,7 +6423,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.2-ea07856');
+const VERSION = new Version('5.0.0-beta.2-5f501c7');
 
 /**
  * @fileoverview added by tsickle
