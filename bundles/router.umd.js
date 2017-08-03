16411f070c6f4824c3ddfe2c09d4e06fefe89636
GitHub <noreply@github.com>
2017-08-03 22:00:26 +0000

    ca695e0 fix(compiler-cli): disable buggy expression lowering (#18513)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-9b015a9
+ * @license Angular v5.0.0-beta.2-ca695e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.2-9b015a9
+ * @license Angular v5.0.0-beta.2-ca695e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6753,7 +6753,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.2-9b015a9');
+var VERSION = new _angular_core.Version('5.0.0-beta.2-ca695e0');
 
 exports.Route = Route;
 exports.RouterLink = RouterLink;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-9b015a9
+ * @license Angular v5.0.0-beta.2-ca695e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
