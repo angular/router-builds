97299cf049f5ec7dfec9b12ac6e3b80cfe21fa52
Victor Berchet <victor@suumit.com>
2017-08-07 22:01:08 +0000

    685cc26 fix(common): don't recreate view when context shape doesn't change (#18277)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-5b7432b
+ * @license Angular v5.0.0-beta.2-685cc26
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6423,7 +6423,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.2-5b7432b');
+const VERSION = new Version('5.0.0-beta.2-685cc26');
 
 /**
  * @fileoverview added by tsickle
