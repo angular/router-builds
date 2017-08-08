a7f7720af5bbbf1101e2f9377d12514f1799c294
GitHub <noreply@github.com>
2017-08-08 19:09:22 +0000

    f0a5501 fix(core): fix platform-browser-dynamic (#18576)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.2-f69561b
+ * @license Angular v5.0.0-beta.2-f0a5501
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6738,7 +6738,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.2-f69561b');
+var VERSION = new Version('5.0.0-beta.2-f0a5501');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
