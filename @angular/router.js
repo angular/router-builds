37c4c2736c234883029bd604b8ba2c4e98a68627
Hans <hans@hansl.ca>
2017-08-10 23:28:45 +0000

    06faac8 fix(aio): skip PWA test when redeploying non-public commit

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-4d523fd
+ * @license Angular v5.0.0-beta.3-06faac8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6423,7 +6423,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.3-4d523fd');
+const VERSION = new Version('5.0.0-beta.3-06faac8');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-4d523fd
+ * @license Angular v5.0.0-beta.3-06faac8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
