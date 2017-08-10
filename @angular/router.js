c46c78d3a1fa4d589d93b73b53a7875d38c6affa
Victor Berchet <victor@suumit.com>
2017-08-10 03:49:08 +0000

    77fa3c3 test(aio): fix tests for example-boilerplate (#18619)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-f4cb453
+ * @license Angular v4.3.3-77fa3c3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5977,7 +5977,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.3-f4cb453');
+const VERSION = new Version('4.3.3-77fa3c3');
 
 /**
  * @license
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-f4cb453
+ * @license Angular v4.3.3-77fa3c3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.3-f4cb453
+ * @license Angular v4.3.3-77fa3c3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6268,7 +6268,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.3-f4cb453');
+var VERSION = new _angular_core.Version('4.3.3-77fa3c3');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
