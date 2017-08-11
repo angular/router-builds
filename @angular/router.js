b14749974756650e6e7af6ca8cbd35e844d9d883
Hans <hans@hansl.ca>
2017-08-11 03:49:50 +0000

    679608d refactor(compiler-cli): use the transformer based compiler by default

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-06faac8
+ * @license Angular v5.0.0-beta.3-679608d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6423,7 +6423,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.3-06faac8');
+const VERSION = new Version('5.0.0-beta.3-679608d');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-06faac8
+ * @license Angular v5.0.0-beta.3-679608d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
