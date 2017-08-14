0f21331ae5d5c881ad6f8f79cd9788a62567b21b
Hans <hans@hansl.ca>
2017-08-14 19:39:51 +0000

    972538b fix(core): forbid destroyed views to be inserted or moved in VC

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.4-d7be4f1
+ * @license Angular v4.3.4-972538b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6253,7 +6253,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.4-d7be4f1');
+var VERSION = new Version('4.3.4-972538b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.4-d7be4f1
+ * @license Angular v4.3.4-972538b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
