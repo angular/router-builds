596abc53a53e69578b02da42226cc83a12aab9fc
Hans Larsen <hans@hansl.ca>
2017-08-15 23:40:01 +0000

    cce2ab2 docs: add changelog for 4.3.4

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.3-38addac
+ * @license Angular v5.0.0-beta.3-cce2ab2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6738,7 +6738,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.3-38addac');
+var VERSION = new Version('5.0.0-beta.3-cce2ab2');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
