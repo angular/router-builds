da3a130c01590934f5b58702601a4c533def98a2
Hans <hans@hansl.ca>
2017-08-15 23:58:38 +0000

    1cfa79c feat(forms): add updateOn support to ngModelOptions

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-77747e1
+ * @license Angular v5.0.0-beta.3-1cfa79c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6423,7 +6423,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.3-77747e1');
+const VERSION = new Version('5.0.0-beta.3-1cfa79c');
 
 /**
  * @fileoverview added by tsickle
