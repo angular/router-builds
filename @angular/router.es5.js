2b9b94b37840303d213c1c643c7aadd812e1aff5
Hans Larsen <hans@hansl.ca>
2017-08-16 18:14:56 +0000

    7e94405 docs: add changelog for 4.3.5

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.4-a1624f2
+ * @license Angular v4.3.5-7e94405
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6253,7 +6253,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.4-a1624f2');
+var VERSION = new Version('4.3.5-7e94405');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
