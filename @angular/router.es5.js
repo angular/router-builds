761ab95815632ec5ddcc5ed6287faca1d2e21145
Miško Hevery <misko@hevery.com>
2017-08-18 23:53:02 +0000

    f2a2a6b refactor(core): remove deprecated `Testability#findBindings` (#18782)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.4-a56468c
+ * @license Angular v5.0.0-beta.4-f2a2a6b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6738,7 +6738,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.4-a56468c');
+var VERSION = new Version('5.0.0-beta.4-f2a2a6b');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-a56468c
+ * @license Angular v5.0.0-beta.4-f2a2a6b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.4-a56468c
+ * @license Angular v5.0.0-beta.4-f2a2a6b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6753,7 +6753,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.4-a56468c');
+var VERSION = new _angular_core.Version('5.0.0-beta.4-f2a2a6b');
 
 exports.Route = Route;
 exports.RouterLink = RouterLink;
