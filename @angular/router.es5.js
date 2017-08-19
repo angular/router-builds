e913cd0b02180eaefaa8651b263dd75dfe8cacd1
Miško Hevery <misko@hevery.com>
2017-08-19 14:29:50 +0000

    7062811 fix(animations): restore auto-style support for removed DOM nodes (#18787)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.4-e228f2c
+ * @license Angular v5.0.0-beta.4-7062811
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6738,7 +6738,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.4-e228f2c');
+var VERSION = new Version('5.0.0-beta.4-7062811');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-e228f2c
+ * @license Angular v5.0.0-beta.4-7062811
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
