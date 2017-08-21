f8a4b8cd03c15fc7f1726d49bf9ea116fa77c0e3
Miško Hevery <misko@hevery.com>
2017-08-21 18:26:30 +0000

    5a1b9a3 style(animations): format integration spec (#18805)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-7062811
+ * @license Angular v5.0.0-beta.4-5a1b9a3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.4-7062811
+ * @license Angular v5.0.0-beta.4-5a1b9a3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6753,7 +6753,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.4-7062811');
+var VERSION = new _angular_core.Version('5.0.0-beta.4-5a1b9a3');
 
 exports.Route = Route;
 exports.RouterLink = RouterLink;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-7062811
+ * @license Angular v5.0.0-beta.4-5a1b9a3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
