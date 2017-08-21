e18387cab45d831126ab5bf39eb2b02cf48e0e1e
Miško Hevery <misko@hevery.com>
2017-08-21 23:26:49 +0000

    7ce9e06 fix(aio): do not redirect API pages on archive and next deployments (#18791)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-55d151a
+ * @license Angular v5.0.0-beta.4-7ce9e06
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.4-55d151a
+ * @license Angular v5.0.0-beta.4-7ce9e06
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6753,7 +6753,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.4-55d151a');
+var VERSION = new _angular_core.Version('5.0.0-beta.4-7ce9e06');
 
 exports.Route = Route;
 exports.RouterLink = RouterLink;
