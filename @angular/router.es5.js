23cdd5aec83330d70b2cf7226985ed03cf058f44
Miško Hevery <misko@hevery.com>
2017-08-22 01:53:54 +0000

    1f43713 fix(aio): do not redirect API pages on archive and next deployments (#18791)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.5-14d34c9
+ * @license Angular v4.3.5-1f43713
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6253,7 +6253,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.5-14d34c9');
+var VERSION = new Version('4.3.5-1f43713');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
