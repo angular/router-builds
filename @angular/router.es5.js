e60e29d331917628461f302592546d2a0301c97a
Miško Hevery <misko@hevery.com>
2017-08-22 03:15:37 +0000

    409688f feat(animations): report errors when invalid CSS properties are detected (#18718)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.4-ec56760
+ * @license Angular v5.0.0-beta.4-409688f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6738,7 +6738,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.4-ec56760');
+var VERSION = new Version('5.0.0-beta.4-409688f');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
