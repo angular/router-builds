6199be767700ad7b45e1b92b154f869239bd9030
Miško Hevery <misko@hevery.com>
2017-08-23 18:46:21 +0000

    641be64 docs(aio): add info about `--local` option in the readme (#18824)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.5-1f43713
+ * @license Angular v4.3.5-641be64
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6253,7 +6253,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.5-1f43713');
+var VERSION = new Version('4.3.5-641be64');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.5-1f43713
+ * @license Angular v4.3.5-641be64
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
