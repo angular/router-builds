0cbb069ea49222198224ed95cd7547abafa6f1dc
Misko Hevery <misko@hevery.com>
2017-08-24 17:05:42 +0000

    1c2f3d4 build: fix build by pinning angular-cli to specific version (#18865)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-3db84c8
+ * @license Angular v5.0.0-beta.4-1c2f3d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.4-3db84c8
+ * @license Angular v5.0.0-beta.4-1c2f3d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6753,7 +6753,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.4-3db84c8');
+var VERSION = new _angular_core.Version('5.0.0-beta.4-1c2f3d4');
 
 exports.Route = Route;
 exports.RouterLink = RouterLink;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-3db84c8
+ * @license Angular v5.0.0-beta.4-1c2f3d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
