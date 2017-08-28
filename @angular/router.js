083c6c051aadbe159edbd7e4142ec84f169ef4dd
Miško Hevery <misko@hevery.com>
2017-08-28 21:46:53 +0000

    655b495 build: reenable npm install for air builds

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-92dbaf3
+ * @license Angular v5.0.0-beta.4-655b495
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6423,7 +6423,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.4-92dbaf3');
+const VERSION = new Version('5.0.0-beta.4-655b495');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-92dbaf3
+ * @license Angular v5.0.0-beta.4-655b495
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
