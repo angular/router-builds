c016a3bea58eaf6c6e8c4e540ff4327e4ce003e4
Jason Aden <jasonaden@google.com>
2017-08-30 00:47:42 +0000

    a203a95 fix(common): fix improper packaging for @angular/common/http (#18613)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-8821723
+ * @license Angular v4.3.6-a203a95
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.6-8821723
+ * @license Angular v4.3.6-a203a95
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6268,7 +6268,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.6-8821723');
+var VERSION = new _angular_core.Version('4.3.6-a203a95');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-8821723
+ * @license Angular v4.3.6-a203a95
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
