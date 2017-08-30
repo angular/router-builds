ba040fabfad5777291fd5bb73c1ecfe6d86e0b8c
GitHub <noreply@github.com>
2017-08-30 00:32:16 +0000

    fdd5010 fix(common): fix a duplicate case in the locale switch (#18941)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-24faaf4
+ * @license Angular v5.0.0-beta.5-fdd5010
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6423,7 +6423,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.5-24faaf4');
+const VERSION = new Version('5.0.0-beta.5-fdd5010');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-24faaf4
+ * @license Angular v5.0.0-beta.5-fdd5010
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
