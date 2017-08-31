c14f3a5807cfd13f998ed26628d3574945e25103
Jason Aden <jasonaden@google.com>
2017-08-31 17:17:35 +0000

    cf7d47d feat(compiler-cli): add watch mode to `ngc` (#18818)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-c7e1bda
+ * @license Angular v5.0.0-beta.5-cf7d47d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6423,7 +6423,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.5-c7e1bda');
+const VERSION = new Version('5.0.0-beta.5-cf7d47d');
 
 /**
  * @fileoverview added by tsickle
