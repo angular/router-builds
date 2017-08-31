8a73294a894d5d50637c6b7acceca33a2e984298
Jason Aden <jasonaden@google.com>
2017-08-31 01:22:50 +0000

    2f2d5f3 feat(platform-server): provide a DOM implementation on the server

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-30d53a8
+ * @license Angular v5.0.0-beta.5-2f2d5f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6423,7 +6423,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.5-30d53a8');
+const VERSION = new Version('5.0.0-beta.5-2f2d5f3');
 
 /**
  * @fileoverview added by tsickle
