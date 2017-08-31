aef7c19e85a4f893150ca7b1b00252fb2b609b4f
Jason Aden <jasonaden@google.com>
2017-08-31 22:59:03 +0000

    ee04217 refactor(compiler-cli): expose ngtools api separately (#18978)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-a1293b2
+ * @license Angular v5.0.0-beta.5-ee04217
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6423,7 +6423,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.5-a1293b2');
+const VERSION = new Version('5.0.0-beta.5-ee04217');
 
 /**
  * @fileoverview added by tsickle
