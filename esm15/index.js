878d637148af5296a2bcea912fd1481c61a9a9f3
Miško Hevery <misko@hevery.com>
2017-09-01 21:51:38 +0000

    185a941 test: support debugging unit tests in node (#18987)

--- esm15/index.js
+++ esm15/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-6af2396
+ * @license Angular v5.0.0-beta.5-185a941
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6422,7 +6422,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.5-6af2396');
+const VERSION = new Version('5.0.0-beta.5-185a941');
 
 /**
  * @fileoverview added by tsickle
--- esm5/index.js
+++ esm5/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-6af2396
+ * @license Angular v5.0.0-beta.5-185a941
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6768,7 +6768,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.5-6af2396');
+var VERSION = new Version('5.0.0-beta.5-185a941');
 
 /**
  * @fileoverview added by tsickle
