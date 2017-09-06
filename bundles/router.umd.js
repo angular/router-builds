4a1e419ec9400f19dfa28072fc45473382abaab4
Miško Hevery <misko@hevery.com>
2017-09-06 17:48:54 +0000

    3c480e4 build(aio): support rendering of constructor overloads in API docs (#19042)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-29762f1
+ * @license Angular v5.0.0-beta.6-3c480e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.6-29762f1
+ * @license Angular v5.0.0-beta.6-3c480e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6766,7 +6766,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.6-29762f1');
+var VERSION = new _angular_core.Version('5.0.0-beta.6-3c480e4');
 
 exports.Route = Route;
 exports.RouterLink = RouterLink;
--- esm5/index.js
+++ esm5/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-29762f1
+ * @license Angular v5.0.0-beta.6-3c480e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6744,7 +6744,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.6-29762f1');
+var VERSION = new Version('5.0.0-beta.6-3c480e4');
 
 /**
  * @fileoverview added by tsickle
