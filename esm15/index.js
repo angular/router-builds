8da9e41ed9b11b248ceae18bf8aa2c7bcc397a68
Miško Hevery <misko@hevery.com>
2017-09-06 16:36:06 +0000

    29762f1 test(aio): fix docs example e2e test (#19070)

--- esm15/index.js
+++ esm15/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-d2707f1
+ * @license Angular v5.0.0-beta.6-29762f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6416,7 +6416,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.6-d2707f1');
+const VERSION = new Version('5.0.0-beta.6-29762f1');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-d2707f1
+ * @license Angular v5.0.0-beta.6-29762f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
