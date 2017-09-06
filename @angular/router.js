b0c2aa2c620d764e298d024cdb9124475547f36e
Miško Hevery <misko@hevery.com>
2017-09-06 04:53:09 +0000

    dd2d1be fix(aio): align footer background image repeat (#19035)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.0-RC.0-aeb98db
+ * @license Angular v4.4.0-RC.0-dd2d1be
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5977,7 +5977,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.0-RC.0-aeb98db');
+const VERSION = new Version('4.4.0-RC.0-dd2d1be');
 
 /**
  * @license
