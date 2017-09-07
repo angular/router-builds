b61f3c95f613c807e19002c4a18e504908a99282
Matias Niemelä <matias@yearofmoo.com>
2017-09-07 23:27:26 +0000

    5ef6e63 fix(compiler): lower variables with a closure by exporting the variable.

--- esm15/index.js
+++ esm15/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-b6833d1
+ * @license Angular v5.0.0-beta.6-5ef6e63
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6416,7 +6416,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.6-b6833d1');
+const VERSION = new Version('5.0.0-beta.6-5ef6e63');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-b6833d1
+ * @license Angular v5.0.0-beta.6-5ef6e63
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
