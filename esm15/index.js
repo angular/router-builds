f815d50e20b255048289c446a89aff8a3161b850
Matias Niemelä <matias@yearofmoo.com>
2017-09-14 23:06:49 +0000

    b6431c6 refactor: core, http & platform-webworker to remove public private class separation (#19143)

--- esm15/index.js
+++ esm15/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-4695c69
+ * @license Angular v5.0.0-beta.7-b6431c6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6102,7 +6102,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-4695c69');
+const VERSION = new Version('5.0.0-beta.7-b6431c6');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-4695c69
+ * @license Angular v5.0.0-beta.7-b6431c6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
