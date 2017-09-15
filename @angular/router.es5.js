8b8cb1608b8e0164e5626ae3d9fb9cff4d917c74
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 23:20:59 +0000

    ff87294 docs(changelog): fix the changelog link for comparing 4.4.1 to 4.3.6

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.1-0c72f7d
+ * @license Angular v4.4.1-ff87294
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6253,7 +6253,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.1-0c72f7d');
+var VERSION = new Version('4.4.1-ff87294');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
