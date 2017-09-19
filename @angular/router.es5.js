17a21c4b56a46df05a7eb1b401647825b2687c95
Igor Minar <igor@angularjs.org>
2017-09-19 23:00:06 +0000

    e2e8ba6 release: cut the 4.4.3 release

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.2-0371538
+ * @license Angular v4.4.3-e2e8ba6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6253,7 +6253,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.2-0371538');
+var VERSION = new Version('4.4.3-e2e8ba6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.2-0371538
+ * @license Angular v4.4.3-e2e8ba6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
