4c6e88be932e79068ef33f47d8d6cb98caf8fd5e
Igor Minar <igor@angularjs.org>
2017-09-19 04:36:42 +0000

    49d122e release: cut the 4.4.2 release

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.1-ff87294
+ * @license Angular v4.4.2-49d122e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6253,7 +6253,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.1-ff87294');
+var VERSION = new Version('4.4.2-49d122e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.1-ff87294
+ * @license Angular v4.4.2-49d122e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
