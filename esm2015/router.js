d5c5615467005752b84e0afe1c32d100b1d7ef05
Igor Minar <igor@angularjs.org>
2017-09-20 23:45:41 +0000

    9e84719 test: fix cli-integration tests that got broken by yarn update (#19297)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-2c4107c
+ * @license Angular v5.0.0-beta.7-9e84719
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6089,7 +6089,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-2c4107c');
+const VERSION = new Version('5.0.0-beta.7-9e84719');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-2c4107c
+ * @license Angular v5.0.0-beta.7-9e84719
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
