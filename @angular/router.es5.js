f6a08be212b5f105c57c4ef1aa8dcbb93de436fd
Igor Minar <igor@angularjs.org>
2017-09-21 17:16:21 +0000

    e7eb0b8 docs: update README and author guide about doc build errors (#19276)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.3-ae52851
+ * @license Angular v4.4.3-e7eb0b8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6253,7 +6253,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.3-ae52851');
+var VERSION = new Version('4.4.3-e7eb0b8');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
