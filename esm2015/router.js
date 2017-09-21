7dd965e11baa6577f3b8ef014b6709704f6cff40
Igor Minar <igor@angularjs.org>
2017-09-21 18:44:28 +0000

    4c73b52 docs(aio): tidy up tooling documentation (#18151)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-4586fcc
+ * @license Angular v5.0.0-beta.7-4c73b52
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6089,7 +6089,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-4586fcc');
+const VERSION = new Version('5.0.0-beta.7-4c73b52');
 
 /**
  * @fileoverview added by tsickle
