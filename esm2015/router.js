4493fafeb1a0f66f67c2192e0e198b775f4d4ab9
Victor Berchet <victor@suumit.com>
2017-09-25 22:00:39 +0000

    ffceae0 refactor(aio): remove unnecessary `--` from yarn commands

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-bb1665c
+ * @license Angular v5.0.0-beta.7-ffceae0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6088,7 +6088,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-bb1665c');
+const VERSION = new Version('5.0.0-beta.7-ffceae0');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-bb1665c
+ * @license Angular v5.0.0-beta.7-ffceae0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
