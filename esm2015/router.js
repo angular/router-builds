07d8838a5ede8513bd8eea67e932392fe7990f7c
Victor Berchet <victor@suumit.com>
2017-09-25 20:00:50 +0000

    9ad4b3b build: update to latest @bazel/typescript (#19277)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-68078fd
+ * @license Angular v5.0.0-beta.7-9ad4b3b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6088,7 +6088,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-68078fd');
+const VERSION = new Version('5.0.0-beta.7-9ad4b3b');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-68078fd
+ * @license Angular v5.0.0-beta.7-9ad4b3b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
