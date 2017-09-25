5873b151f9206599f8d2865010e7f6669ce1d56b
Victor Berchet <victor@suumit.com>
2017-09-25 19:21:44 +0000

    4ae546b build(aio): ensure decorators with shared interface types are found (#19361)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-adb0b76
+ * @license Angular v5.0.0-beta.7-4ae546b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6088,7 +6088,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-adb0b76');
+const VERSION = new Version('5.0.0-beta.7-4ae546b');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-adb0b76
+ * @license Angular v5.0.0-beta.7-4ae546b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
