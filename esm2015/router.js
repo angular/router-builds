bb351300f5f50ee4f2712d54726f0b719b6a2a7e
Victor Berchet <victor@suumit.com>
2017-09-25 20:01:44 +0000

    e224e3d fix(compiler-cli): update ngtools2 EmitFlags (#19375)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-9ad4b3b
+ * @license Angular v5.0.0-beta.7-e224e3d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6088,7 +6088,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-9ad4b3b');
+const VERSION = new Version('5.0.0-beta.7-e224e3d');
 
 /**
  * @fileoverview added by tsickle
