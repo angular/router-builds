f70e3b6352bfd57ba08b4ac4bd0c42d24d5b42d7
Victor Berchet <victor@suumit.com>
2017-09-26 19:23:24 +0000

    86ffacf feat(animations): support negative query option values

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-e889c68
+ * @license Angular v5.0.0-beta.7-86ffacf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7251,7 +7251,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-e889c68');
+var VERSION = new Version('5.0.0-beta.7-86ffacf');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-e889c68
+ * @license Angular v5.0.0-beta.7-86ffacf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
