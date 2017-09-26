9a338c9a794716c507b5b5d5ceedf28958dfb019
Victor Berchet <victor@suumit.com>
2017-09-26 22:23:04 +0000

    8a0e458 fix(http): introduce named type for HttpParams options (#19360)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-b21a1d1
+ * @license Angular v5.0.0-beta.7-8a0e458
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.7-b21a1d1
+ * @license Angular v5.0.0-beta.7-8a0e458
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7273,7 +7273,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-b21a1d1');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-8a0e458');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-b21a1d1
+ * @license Angular v5.0.0-beta.7-8a0e458
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
