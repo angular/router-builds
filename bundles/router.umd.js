fa879e172eacd1530a4c475d2690b9b8a1e4d6b7
Victor Berchet <victor@suumit.com>
2017-09-26 16:51:15 +0000

    27c6638 fix(compiler-cli): set source file ranges in node emitter (#19348)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-3f100eb
+ * @license Angular v5.0.0-beta.7-27c6638
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.7-3f100eb
+ * @license Angular v5.0.0-beta.7-27c6638
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7273,7 +7273,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-3f100eb');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-27c6638');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-3f100eb
+ * @license Angular v5.0.0-beta.7-27c6638
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
