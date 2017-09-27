1816a66a4116a5ae2d264b7a44dee915627987de
Victor Berchet <victor@suumit.com>
2017-09-27 21:09:16 +0000

    55a7443 docs(aio): edit summary and next step headers (#16962)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.3-bc81fbd
+ * @license Angular v4.4.3-55a7443
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6253,7 +6253,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.3-bc81fbd');
+var VERSION = new Version('4.4.3-55a7443');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
