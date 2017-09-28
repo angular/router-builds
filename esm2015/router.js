c8b797345d09b3d18b4d5629bdaade535164d162
Victor Berchet <victor@suumit.com>
2017-09-28 18:17:04 +0000

    b3db3f8 fix(compiler): do not consider a reference with members as a reference (#19454)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-ff5b050
+ * @license Angular v5.0.0-beta.7-b3db3f8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6085,7 +6085,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-ff5b050');
+const VERSION = new Version('5.0.0-beta.7-b3db3f8');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-ff5b050
+ * @license Angular v5.0.0-beta.7-b3db3f8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
