9be40815166d4da5ff732d694ca010c02370cfcc
Victor Berchet <victor@suumit.com>
2017-09-28 18:13:05 +0000

    ff5b050 fix(compiler): correctly map error message locations (#19424)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-2b84b86
+ * @license Angular v5.0.0-beta.7-ff5b050
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-2b84b86');
+var VERSION = new Version('5.0.0-beta.7-ff5b050');
 
 /**
  * @fileoverview added by tsickle
