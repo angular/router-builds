92a6dcbe4b854e953a2703f76f836f7158fc24f4
Victor Berchet <victor@suumit.com>
2017-09-28 23:45:05 +0000

    d442b68 feat(service-worker): introduce the @angular/service-worker package (#19274)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-7c1d3e0
+ * @license Angular v5.0.0-beta.7-d442b68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7251,7 +7251,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-7c1d3e0');
+var VERSION = new Version('5.0.0-beta.7-d442b68');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-7c1d3e0
+ * @license Angular v5.0.0-beta.7-d442b68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
