228c9db6faa6ba170105fedaaadb684311716778
Victor Berchet <victor@suumit.com>
2017-09-28 19:43:35 +0000

    bed8ac7 docs: add changelog for 4.4.4

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-a9d32a3
+ * @license Angular v5.0.0-beta.7-bed8ac7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6085,7 +6085,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-a9d32a3');
+const VERSION = new Version('5.0.0-beta.7-bed8ac7');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-a9d32a3
+ * @license Angular v5.0.0-beta.7-bed8ac7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
