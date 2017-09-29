c4b5b4425c62a119c8716008e18cfe44b24e3011
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-09-29 16:48:21 +0000

    0038712 fix(compiler): add typings for `COMPILED`.

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-d442b68
+ * @license Angular v5.0.0-rc.0-0038712
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6088,7 +6088,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-d442b68');
+const VERSION = new Version('5.0.0-rc.0-0038712');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-d442b68
+ * @license Angular v5.0.0-rc.0-0038712
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
