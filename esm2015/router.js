d4e8d659ba4fb8e3915379f109f546121063f4e1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 20:03:28 +0000

    0f43299 docs(aio): document the special treatment of undefined

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-9ae3742
+ * @license Angular v5.0.0-rc.0-0f43299
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6088,7 +6088,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.0-9ae3742');
+const VERSION = new Version('5.0.0-rc.0-0f43299');
 
 /**
  * @fileoverview added by tsickle
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-9ae3742
+ * @license Angular v5.0.0-rc.0-0f43299
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7251,7 +7251,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.0-9ae3742');
+var VERSION = new Version('5.0.0-rc.0-0f43299');
 
 /**
  * @fileoverview added by tsickle
