4f17e53d705578b94b9554d801a084e0f769151a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 20:06:15 +0000

    d4d9009 docs(aio): fix broken link in toh-pt5

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-0f43299
+ * @license Angular v5.0.0-rc.0-d4d9009
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6088,7 +6088,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.0-0f43299');
+const VERSION = new Version('5.0.0-rc.0-d4d9009');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-0f43299
+ * @license Angular v5.0.0-rc.0-d4d9009
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
