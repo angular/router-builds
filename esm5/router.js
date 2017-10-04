27ca3e57048196a21a9724c799083ccce1b24b51
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 22:21:19 +0000

    696af79 fix(compiler): properly work on windows

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-d4d9009
+ * @license Angular v5.0.0-rc.0-696af79
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7251,7 +7251,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.0-d4d9009');
+var VERSION = new Version('5.0.0-rc.0-696af79');
 
 /**
  * @fileoverview added by tsickle
