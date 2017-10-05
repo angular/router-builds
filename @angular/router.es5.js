d06a7ac4799673d625e1a38db23f34ad65c0ee9d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-05 20:46:37 +0000

    8f66880 Revert "fix(platform-browser): support customEqualityTesters when overriding Jasmine toEqual"

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.4-f98eb35
+ * @license Angular v4.4.4-8f66880
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6252,7 +6252,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.4-f98eb35');
+var VERSION = new Version('4.4.4-8f66880');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
