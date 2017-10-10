1778451d4fb47de7d3332371ed4769428c89247b
Chuck Jazdzewski <chuckj@google.com>
2017-10-10 23:19:33 +0000

    f89ac92 test(aio): temporarily disable failing examples e2e tests (#19600)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.4-ccc25ee
+ * @license Angular v4.4.4-f89ac92
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6252,7 +6252,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.4-ccc25ee');
+var VERSION = new Version('4.4.4-f89ac92');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-ccc25ee
+ * @license Angular v4.4.4-f89ac92
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
