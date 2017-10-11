ced27c9aa4e041ade7e849b4a76ad53621771aa5
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 20:41:46 +0000

    3acf9c7 docs(aio): update banner ad (#19669)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-566606e
+ * @license Angular v5.0.0-rc.1-3acf9c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.1-566606e
+ * @license Angular v5.0.0-rc.1-3acf9c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7273,7 +7273,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.1-566606e');
+var VERSION = new _angular_core.Version('5.0.0-rc.1-3acf9c7');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-566606e
+ * @license Angular v5.0.0-rc.1-3acf9c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7251,7 +7251,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.1-566606e');
+var VERSION = new Version('5.0.0-rc.1-3acf9c7');
 
 /**
  * @fileoverview added by tsickle
