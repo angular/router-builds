85eb9fc999492a82aa86ad591db1199d0157053a
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 17:55:44 +0000

    94a925a Revert "perf(compiler): skip type check and emit in bazel in some cases. (#19646)"

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-6b7cead
+ * @license Angular v5.0.0-rc.1-94a925a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7251,7 +7251,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.1-6b7cead');
+var VERSION = new Version('5.0.0-rc.1-94a925a');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-6b7cead
+ * @license Angular v5.0.0-rc.1-94a925a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
