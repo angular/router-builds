89036cd61709f85bdd0b05ad1b931d31971d7579
Tobias Bosch <tbosch1009@gmail.com>
2017-10-12 23:30:38 +0000

    653a211 Revert "Revert "Revert "perf(compiler): skip type check and emit in bazel in some cases. (#19646)"""

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-522ec9a
+ * @license Angular v5.0.0-rc.2-653a211
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7251,7 +7251,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.2-522ec9a');
+var VERSION = new Version('5.0.0-rc.2-653a211');
 
 /**
  * @fileoverview added by tsickle
