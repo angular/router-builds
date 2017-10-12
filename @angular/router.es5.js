36dcef43821b8cbf9909a384c62b9ccb70d3c143
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 17:12:21 +0000

    f27f6e4 docs(aio): improve the Angular Connect homepage banner (#19684)

--- @angular/router.es5.js
+++ @angular/router.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.4-a15abbb
+ * @license Angular v4.4.4-f27f6e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6252,7 +6252,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.4-a15abbb');
+var VERSION = new Version('4.4.4-f27f6e4');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-a15abbb
+ * @license Angular v4.4.4-f27f6e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
