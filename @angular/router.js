f248a424fa67b49e22165e82c5bdae9a9e606b2d
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 17:00:05 +0000

    18f1b01 build(aio): consider `devDependencies` when overwriting dependencies of local Angular packages (#19687)

--- @angular/router.js
+++ @angular/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-591dcc2
+ * @license Angular v4.4.5-18f1b01
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5976,7 +5976,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.5-591dcc2');
+const VERSION = new Version('4.4.5-18f1b01');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-591dcc2
+ * @license Angular v4.4.5-18f1b01
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
