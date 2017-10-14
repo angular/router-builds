7f292ad73b73f07c4b45851c2d0fde7ed105c8ff
Tobias Bosch <tbosch1009@gmail.com>
2017-10-14 00:51:23 +0000

    405ccc7 fix(bazel): fix the output directory for extractor to be genfiles/ instead of bin/ (#19716)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-836c889
+ * @license Angular v5.0.0-rc.2-405ccc7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7251,7 +7251,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.2-836c889');
+var VERSION = new Version('5.0.0-rc.2-405ccc7');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-836c889
+ * @license Angular v5.0.0-rc.2-405ccc7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
