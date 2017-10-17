db261e1f85a97a5e9be1a9c7f478c37ebf56fc17
Tobias Bosch <tbosch1009@gmail.com>
2017-10-17 18:04:32 +0000

    15a8429 fix(service-worker): add missing annotation for SwPush (#19721)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-d035175
+ * @license Angular v5.0.0-rc.2-15a8429
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7251,7 +7251,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.2-d035175');
+var VERSION = new Version('5.0.0-rc.2-15a8429');
 
 /**
  * @fileoverview added by tsickle
