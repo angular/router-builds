717c2d8664b5f5bd5e26ecefd34788aeef483d8f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:03:33 +0000

    14380ff build: add warning about changing ngtools_api2

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-441e01c
+ * @license Angular v5.0.0-rc.3-14380ff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6089,7 +6089,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.3-441e01c');
+const VERSION = new Version('5.0.0-rc.3-14380ff');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-441e01c
+ * @license Angular v5.0.0-rc.3-14380ff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
