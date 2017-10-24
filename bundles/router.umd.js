899b190b270de527c298e057891d1e71eb668a77
Matias Niemelä <matias@yearofmoo.com>
2017-10-24 01:14:23 +0000

    bde5701 docs: add changelog for 5.0.0-rc.4

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-6bed189
+ * @license Angular v5.0.0-rc.4-bde5701
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.3-6bed189
+ * @license Angular v5.0.0-rc.4-bde5701
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7272,7 +7272,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.3-6bed189');
+var VERSION = new _angular_core.Version('5.0.0-rc.4-bde5701');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
