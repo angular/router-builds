9124f4336b583d7eeda1c628d5c45057290c50d8
Matias Niemelä <matias@yearofmoo.com>
2017-10-24 00:01:13 +0000

    6bed189 build(service-worker): set skipTemplateCodegen for build (#19875)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-230b98d
+ * @license Angular v5.0.0-rc.3-6bed189
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6089,7 +6089,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.3-230b98d');
+const VERSION = new Version('5.0.0-rc.3-6bed189');
 
 /**
  * @fileoverview added by tsickle
