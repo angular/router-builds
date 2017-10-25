2e6b1f89d7e782139679a914645e61c4b6eaceb1
Matias Niemelä <matias@yearofmoo.com>
2017-10-25 22:09:20 +0000

    47bc6f1 docs: add changelog for 5.0.0-rc.6

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-680bcf7
+ * @license Angular v5.0.0-rc.6-47bc6f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6089,7 +6089,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.5-680bcf7');
+const VERSION = new Version('5.0.0-rc.6-47bc6f1');
 
 /**
  * @fileoverview added by tsickle
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-680bcf7
+ * @license Angular v5.0.0-rc.6-47bc6f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7250,7 +7250,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.5-680bcf7');
+var VERSION = new Version('5.0.0-rc.6-47bc6f1');
 
 /**
  * @fileoverview added by tsickle
