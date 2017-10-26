9bc4ead3069a8cc36a5b98ea78223f429f3704e9
Matias Niemelä <matias@yearofmoo.com>
2017-10-26 20:37:52 +0000

    17142a7 fix(service-worker): don't block initialization on registration (#19936)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.6-703fcda
+ * @license Angular v5.0.0-rc.6-17142a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7275,7 +7275,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.6-703fcda');
+var VERSION = new Version('5.0.0-rc.6-17142a7');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.6-703fcda
+ * @license Angular v5.0.0-rc.6-17142a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
