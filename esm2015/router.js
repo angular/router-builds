3b5fbfcda83772e00945d8c365924607f17435e2
Matias Niemelä <matias@yearofmoo.com>
2017-10-27 02:14:40 +0000

    31797d3 fix(compiler): make watch mode work on windows (#19953)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.6-eca822b
+ * @license Angular v5.0.0-rc.6-31797d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6108,7 +6108,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.6-eca822b');
+const VERSION = new Version('5.0.0-rc.6-31797d3');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.6-eca822b
+ * @license Angular v5.0.0-rc.6-31797d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
