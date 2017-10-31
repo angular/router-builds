05bc05683aa55ab415372e430be7e1b097c93adc
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 04:07:50 +0000

    3bdbb18 build: update rollup lint rule from bad merge (#20047)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-13f8648
+ * @license Angular v5.0.0-rc.8-3bdbb18
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7275,7 +7275,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.8-13f8648');
+var VERSION = new Version('5.0.0-rc.8-3bdbb18');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-13f8648
+ * @license Angular v5.0.0-rc.8-3bdbb18
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
