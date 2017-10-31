4ef28fa622bcafb5cc9b8f76a8fb5c0d3efd2db5
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 01:42:29 +0000

    e3a16ed fix(compiler): reexport less symbols in `.ngfactory.ts` files (#19884)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-fd37f3f
+ * @license Angular v5.0.0-rc.8-e3a16ed
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6089,7 +6089,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.8-fd37f3f');
+const VERSION = new Version('5.0.0-rc.8-e3a16ed');
 
 /**
  * @fileoverview added by tsickle
