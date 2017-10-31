737d32aa9350ce0efab068cbf4cd333c2eec9b36
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 03:31:39 +0000

    812786f fix: add missing globals from each rollup configuration (#20028)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-de24d54
+ * @license Angular v5.0.0-rc.8-812786f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6089,7 +6089,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.8-de24d54');
+const VERSION = new Version('5.0.0-rc.8-812786f');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-de24d54
+ * @license Angular v5.0.0-rc.8-812786f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
