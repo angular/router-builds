cd0ec78b58875a271bb2b815ae0e05a3b91ca937
Victor Berchet <victor@suumit.com>
2017-11-02 22:28:17 +0000

    8639995 fix(compiler-cli): don't report emit diagnostics when --noEmitOnError is off (#20063)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-d550b68
+ * @license Angular v5.0.0-8639995
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-d550b68
+ * @license Angular v5.0.0-8639995
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7274,7 +7274,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-d550b68');
+var VERSION = new _angular_core.Version('5.0.0-8639995');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-d550b68
+ * @license Angular v5.0.0-8639995
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7252,7 +7252,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-d550b68');
+var VERSION = new Version('5.0.0-8639995');
 
 /**
  * @fileoverview added by tsickle
