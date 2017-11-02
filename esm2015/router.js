06defbb5769dd5ccae7175460ae1a30c83081a81
Victor Berchet <victor@suumit.com>
2017-11-02 22:20:14 +0000

    d550b68 docs: fix changelog for v5 (#20097)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-e846953
+ * @license Angular v5.0.0-d550b68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6091,7 +6091,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-e846953');
+const VERSION = new Version('5.0.0-d550b68');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-e846953
+ * @license Angular v5.0.0-d550b68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
