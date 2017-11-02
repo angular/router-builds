9cc76c1037c0074d3c47b4f4f22744c740c132a0
Igor Minar <igor@angular.io>
2017-11-02 00:26:38 +0000

    3db7112 docs(aio): fix linting for universal (#20086)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-2ea76cc
+ * @license Angular v5.1.0-beta.0-3db7112
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.0-2ea76cc
+ * @license Angular v5.1.0-beta.0-3db7112
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7299,7 +7299,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-2ea76cc');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-3db7112');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-2ea76cc
+ * @license Angular v5.1.0-beta.0-3db7112
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
