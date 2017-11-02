f5d40cc09636550dbf11cb25a137939c3c2eabe7
Victor Berchet <victor@suumit.com>
2017-11-02 23:28:45 +0000

    25843fe build(aio): avoid building before running the local PWA tests

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-37a740b
+ * @license Angular v5.0.0-25843fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6091,7 +6091,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-37a740b');
+const VERSION = new Version('5.0.0-25843fe');
 
 /**
  * @fileoverview added by tsickle
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-37a740b
+ * @license Angular v5.0.0-25843fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7252,7 +7252,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-37a740b');
+var VERSION = new Version('5.0.0-25843fe');
 
 /**
  * @fileoverview added by tsickle
