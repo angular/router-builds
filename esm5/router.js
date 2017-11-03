4b01417daef2891ce725ee7db7562ff3b5a3e9e3
Victor Berchet <victor@suumit.com>
2017-11-03 19:35:19 +0000

    567cc26 docs: Component Styles guide for CLI (#19791)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-169cedd
+ * @license Angular v5.1.0-beta.0-567cc26
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7277,7 +7277,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-169cedd');
+var VERSION = new Version('5.1.0-beta.0-567cc26');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-169cedd
+ * @license Angular v5.1.0-beta.0-567cc26
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
