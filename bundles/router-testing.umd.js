3f58f3560922ccf7ea1c8354f98204f5f5ba356a
Victor Berchet <victor@suumit.com>
2017-11-03 22:40:50 +0000

    dbec3ca docs(aio): update pipes for CLI (#19553)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-f7c9b94
+ * @license Angular v5.1.0-beta.0-dbec3ca
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_common,_angular_common_testing,_angular_core,_angular_router) { 'use strict';
 
 /**
- * @license Angular v5.1.0-beta.0-f7c9b94
+ * @license Angular v5.1.0-beta.0-dbec3ca
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-f7c9b94
+ * @license Angular v5.1.0-beta.0-dbec3ca
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
