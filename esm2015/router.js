93627699e599a6fec66c9a4efe7c8495119b9c91
Victor Berchet <victor@suumit.com>
2017-11-03 23:14:42 +0000

    3997d97 revert: feat(elements): implement `@angular/elements` #19469 (#20152)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-200d92d
+ * @license Angular v5.1.0-beta.0-3997d97
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6110,7 +6110,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-200d92d');
+const VERSION = new Version('5.1.0-beta.0-3997d97');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-200d92d
+ * @license Angular v5.1.0-beta.0-3997d97
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
