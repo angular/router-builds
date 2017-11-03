2ef8c5534006b56fd493a2be34074ac93194f57e
Victor Berchet <victor@suumit.com>
2017-11-03 19:53:11 +0000

    a99eb16 test(aio): make e2e tests less flakey

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-faa6212
+ * @license Angular v5.1.0-beta.0-a99eb16
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6110,7 +6110,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-faa6212');
+const VERSION = new Version('5.1.0-beta.0-a99eb16');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-faa6212
+ * @license Angular v5.1.0-beta.0-a99eb16
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
