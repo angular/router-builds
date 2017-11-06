5c4688815024be8bc28105f3ae5a8ef0e5be3f7c
Victor Berchet <victor@suumit.com>
2017-11-06 18:23:59 +0000

    8b50ed0 docs(aio): fixed case-typo for code (#20196)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-3997d97
+ * @license Angular v5.1.0-beta.0-8b50ed0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.0-3997d97
+ * @license Angular v5.1.0-beta.0-8b50ed0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7299,7 +7299,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-3997d97');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-8b50ed0');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
