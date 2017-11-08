ef42a03bb4d3450818831f74e0e1fef05bce4876
Victor Berchet <victor@suumit.com>
2017-11-08 20:10:47 +0000

    78ba39b docs: add changelog for 5.1.0-beta.0

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-119034c
+ * @license Angular v5.1.0-beta.0-78ba39b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.0-119034c
+ * @license Angular v5.1.0-beta.0-78ba39b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7299,7 +7299,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-119034c');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-78ba39b');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-119034c
+ * @license Angular v5.1.0-beta.0-78ba39b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6110,7 +6110,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-119034c');
+const VERSION = new Version('5.1.0-beta.0-78ba39b');
 
 /**
  * @fileoverview added by tsickle
