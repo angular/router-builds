d0f4fb002c39710a7e46f27dabf2dfe861bfbe50
Jason Aden <jasonaden@google.com>
2017-11-10 19:33:52 +0000

    2aefac8 docs(aio): empty line between HTML tag and content (#20341) PR Close #20341

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-424a323
+ * @license Angular v5.0.1-2aefac8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7252,7 +7252,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.1-424a323');
+var VERSION = new Version('5.0.1-2aefac8');
 
 /**
  * @fileoverview added by tsickle
