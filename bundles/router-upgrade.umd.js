cff14df6524c144cab64991e2f54326e72a59cfc
Jason Aden <jasonaden@google.com>
2017-11-15 20:19:09 +0000

    2d6126e style(core): fix comment format for linter

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-bc4b4b5
+ * @license Angular v5.0.1-2d6126e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_router,_angular_upgrade_static) { 'use strict';
 
 /**
- * @license Angular v5.0.1-bc4b4b5
+ * @license Angular v5.0.1-2d6126e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-bc4b4b5
+ * @license Angular v5.0.1-2d6126e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
