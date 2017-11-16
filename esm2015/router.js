d0f96fca74d2ca303e2446841d43730602c2c5c3
Miško Hevery <misko@hevery.com>
2017-11-16 01:01:37 +0000

    f8658cd Revert "fix(animations): always fire inner trigger callbacks even if blocked by parent animations (#19753)"

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-043e408
+ * @license Angular v5.1.0-beta.0-f8658cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6110,7 +6110,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-043e408');
+const VERSION = new Version('5.1.0-beta.0-f8658cd');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-043e408
+ * @license Angular v5.1.0-beta.0-f8658cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
