8843422a02fddf6d1359da685ef45559fb3d9a4e
Miško Hevery <misko@hevery.com>
2017-11-16 22:38:59 +0000

    4064cbe docs: add changelog for 5.1.0-beta.1

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-dcfffbf
+ * @license Angular v5.1.0-beta.1-4064cbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6194,7 +6194,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-dcfffbf');
+const VERSION = new Version('5.1.0-beta.1-4064cbe');
 
 /**
  * @fileoverview added by tsickle
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-dcfffbf
+ * @license Angular v5.1.0-beta.1-4064cbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7361,7 +7361,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-dcfffbf');
+var VERSION = new Version('5.1.0-beta.1-4064cbe');
 
 /**
  * @fileoverview added by tsickle
