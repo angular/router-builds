60778c5c2da55f93b369565e683b12994c8951c4
Miško Hevery <misko@hevery.com>
2017-11-17 04:35:19 +0000

    1861e41 fix(animations): validate against trigger() names that use @ symbols (#20326)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-4064cbe
+ * @license Angular v5.1.0-beta.1-1861e41
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.1-4064cbe
+ * @license Angular v5.1.0-beta.1-1861e41
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7383,7 +7383,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.1-4064cbe');
+var VERSION = new _angular_core.Version('5.1.0-beta.1-1861e41');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-4064cbe
+ * @license Angular v5.1.0-beta.1-1861e41
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
