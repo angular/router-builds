2b863b846aab4367e678cecbdd6d07d15833ba73
Miško Hevery <misko@hevery.com>
2017-11-17 22:10:18 +0000

    365712e docs(aio): fix typo in Attribute Directives documentation (#20143)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-1861e41
+ * @license Angular v5.1.0-beta.1-365712e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6194,7 +6194,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.1-1861e41');
+const VERSION = new Version('5.1.0-beta.1-365712e');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-1861e41
+ * @license Angular v5.1.0-beta.1-365712e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
