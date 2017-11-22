3953f4e4fea309f9d64c7516818c424ecfcca9fa
Miško Hevery <misko@hevery.com>
2017-11-22 17:11:19 +0000

    efce396 docs(aio): add a French onsite training ressource (#19889) PR Close #19889

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.2-b53ead4
+ * @license Angular v5.0.2-efce396
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6091,7 +6091,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.2-b53ead4');
+const VERSION = new Version('5.0.2-efce396');
 
 /**
  * @fileoverview added by tsickle
