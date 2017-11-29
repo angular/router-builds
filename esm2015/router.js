ce700d9905c544f673d966807b0a0e3fca35b78a
Miško Hevery <misko@hevery.com>
2017-11-29 22:47:56 +0000

    647ca64 docs(aio): Updating with Ignite UI for Angular (#20663)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-c7b211c
+ * @license Angular v5.0.3-647ca64
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6091,7 +6091,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.3-c7b211c');
+const VERSION = new Version('5.0.3-647ca64');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-c7b211c
+ * @license Angular v5.0.3-647ca64
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
