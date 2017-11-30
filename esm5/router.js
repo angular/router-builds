d3c153b1ba4fc7a2ae27d13a483cff98cc8e7774
Miško Hevery <misko@hevery.com>
2017-11-30 03:56:17 +0000

    b9e4d62 docs(aio): remove services plurality (#20696)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-ba850b3
+ * @license Angular v5.1.0-beta.2-b9e4d62
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7361,7 +7361,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.2-ba850b3');
+var VERSION = new Version('5.1.0-beta.2-b9e4d62');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-ba850b3
+ * @license Angular v5.1.0-beta.2-b9e4d62
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
