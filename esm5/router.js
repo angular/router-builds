ffa2384fe3d167ee840ccbd08858e33dbeac765a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-01 22:46:12 +0000

    f582620 fix(service-worker): use relative path for ngsw.json

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-f841fbe
+ * @license Angular v5.1.0-rc.0-f582620
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7361,7 +7361,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-rc.0-f841fbe');
+var VERSION = new Version('5.1.0-rc.0-f582620');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-f841fbe
+ * @license Angular v5.1.0-rc.0-f582620
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
