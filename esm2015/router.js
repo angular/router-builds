ce99374e137d64435b8b50c5e8f0f8eb99b1f473
Igor Minar <igor@angularjs.org>
2017-12-05 00:42:42 +0000

    e37f7c0 build: set `preserveWhitespaces` to false by default on Bazel (#20783)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-6c78cfb
+ * @license Angular v5.0.5-e37f7c0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6091,7 +6091,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.5-6c78cfb');
+const VERSION = new Version('5.0.5-e37f7c0');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-6c78cfb
+ * @license Angular v5.0.5-e37f7c0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
