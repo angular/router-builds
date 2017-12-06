cd0ecf617449ff169213fe5b19c39c0466a7c995
Igor Minar <igor@angularjs.org>
2017-12-06 15:15:58 +0000

    c91c0d4 fix(service-worker): esm2015 points to wrong path (#20800)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-9efb5da
+ * @license Angular v5.0.5-c91c0d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6091,7 +6091,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.5-9efb5da');
+const VERSION = new Version('5.0.5-c91c0d4');
 
 /**
  * @fileoverview added by tsickle
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-9efb5da
+ * @license Angular v5.0.5-c91c0d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7252,7 +7252,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.5-9efb5da');
+var VERSION = new Version('5.0.5-c91c0d4');
 
 /**
  * @fileoverview added by tsickle
