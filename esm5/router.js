ee31438f03eca3a0a8dba4758ba6e0a9d809588e
Jason Aden <jasonaden@google.com>
2017-12-08 18:32:00 +0000

    ddada6e build(aio): upgrade to latest `@angular/cli` (#18428)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-225baf4
+ * @license Angular v5.1.0-ddada6e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7376,7 +7376,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-225baf4');
+var VERSION = new Version('5.1.0-ddada6e');
 
 /**
  * @fileoverview added by tsickle
