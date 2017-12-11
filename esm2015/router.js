a6f503479589f919bd7e7cee46e0656687da6eea
Jason Aden <jasonaden@google.com>
2017-12-11 19:56:33 +0000

    9fbf850 build: remove bazel option --noshow_results (#20943)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-d09d497
+ * @license Angular v5.1.0-9fbf850
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6203,7 +6203,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-d09d497');
+const VERSION = new Version('5.1.0-9fbf850');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-d09d497
+ * @license Angular v5.1.0-9fbf850
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
