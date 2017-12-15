bb8b239af53966081b59ce439f7694d3b9b6f1dd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-15 16:19:00 +0000

    d91ff17 fix(compiler): generate the correct imports for summary type-check

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-05ff6c0
+ * @license Angular v5.2.0-beta.0-d91ff17
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7369,7 +7369,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-beta.0-05ff6c0');
+var VERSION = new Version('5.2.0-beta.0-d91ff17');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-05ff6c0
+ * @license Angular v5.2.0-beta.0-d91ff17
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
