234deeb1881b68379bf9289fe30c2edff474224c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-15 16:13:56 +0000

    05ff6c0 fix(compiler): make tsx file aot compatible

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-2e7e935
+ * @license Angular v5.2.0-beta.0-05ff6c0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-beta.0-2e7e935
+ * @license Angular v5.2.0-beta.0-05ff6c0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7391,7 +7391,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-beta.0-2e7e935');
+var VERSION = new _angular_core.Version('5.2.0-beta.0-05ff6c0');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-2e7e935
+ * @license Angular v5.2.0-beta.0-05ff6c0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6200,7 +6200,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-beta.0-2e7e935');
+const VERSION = new Version('5.2.0-beta.0-05ff6c0');
 
 /**
  * @fileoverview added by tsickle
