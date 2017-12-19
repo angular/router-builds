57f08070cbdbb36ef32750f85d0c6466a715ae71
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-19 19:05:40 +0000

    8ceffd8 fix(aio): improve transitions between pages

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-74e3115
+ * @license Angular v5.2.0-beta.0-8ceffd8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7369,7 +7369,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-beta.0-74e3115');
+var VERSION = new Version('5.2.0-beta.0-8ceffd8');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-74e3115
+ * @license Angular v5.2.0-beta.0-8ceffd8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
