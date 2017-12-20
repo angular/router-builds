64b1c7d5de7a8b9afdd0ee034b75bf6482a5a2e3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-20 22:13:36 +0000

    abca7c0 docs: add changelog for 5.2.0-beta.1

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-135ead6
+ * @license Angular v5.2.0-beta.1-abca7c0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-beta.0-135ead6');
+const VERSION = new Version('5.2.0-beta.1-abca7c0');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-135ead6
+ * @license Angular v5.2.0-beta.1-abca7c0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
