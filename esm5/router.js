5a9abca16334c86b6846bc046417db21e12a89ea
Igor Minar <igor@angularjs.org>
2017-12-22 04:32:54 +0000

    0aa6341 build: make umd.min.js source map paths relative (#21147)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-41abcc3
+ * @license Angular v5.1.2-0aa6341
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7376,7 +7376,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-41abcc3');
+var VERSION = new Version('5.1.2-0aa6341');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-41abcc3
+ * @license Angular v5.1.2-0aa6341
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
