ce3763c1fd36894a4c684ef9d59e20d9b4ad57c4
Igor Minar <igor@angularjs.org>
2017-12-22 17:43:53 +0000

    3e34fa8 fix(animations): avoid infinite loop with multiple blocked sub triggers (#21119)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-8c99175
+ * @license Angular v5.1.2-3e34fa8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7381,7 +7381,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-8c99175');
+var VERSION = new Version('5.1.2-3e34fa8');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-8c99175
+ * @license Angular v5.1.2-3e34fa8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
