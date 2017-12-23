2da082ab5461c6f2b0ce1755df9d8a90da183d1a
Igor Minar <igor@angular.io>
2017-12-23 05:58:56 +0000

    12702b2 docs(changelog): fix typo in 5.1.1 (#21007)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-5de91fe
+ * @license Angular v5.1.2-12702b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6208,7 +6208,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.2-5de91fe');
+const VERSION = new Version('5.1.2-12702b2');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-5de91fe
+ * @license Angular v5.1.2-12702b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
