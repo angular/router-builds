cf1169114331d9ef5a8dd2f653b766420e940a5d
Kara Erickson <karakara@google.com>
2018-01-04 19:40:43 +0000

    ccea372 refactor(aio): use one argument for `DocViewer` error reporting (#21293)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-cdc66f6
+ * @license Angular v5.2.0-rc.0-ccea372
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-cdc66f6');
+const VERSION = new Version('5.2.0-rc.0-ccea372');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-cdc66f6
+ * @license Angular v5.2.0-rc.0-ccea372
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
