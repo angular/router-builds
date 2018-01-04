d4e3b1fc587d8b4484fa480097a8f9c98b5bd65a
Kara Erickson <karakara@google.com>
2018-01-04 23:00:51 +0000

    d79a0ba docs(aio): add missing imports to interceptor example (#21259)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-b7307c9
+ * @license Angular v5.1.3-d79a0ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7381,7 +7381,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.3-b7307c9');
+var VERSION = new Version('5.1.3-d79a0ba');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-b7307c9
+ * @license Angular v5.1.3-d79a0ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
