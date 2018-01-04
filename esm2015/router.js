372422c7966572051eb38c042a85352497cb9553
Kara Erickson <karakara@google.com>
2018-01-04 20:25:00 +0000

    ef78538 fix(aio): don't use Object.keys on NamedNodeMap to prevent SEO errors (#21305)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-ccea372
+ * @license Angular v5.2.0-rc.0-ef78538
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-ccea372');
+const VERSION = new Version('5.2.0-rc.0-ef78538');
 
 /**
  * @fileoverview added by tsickle
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-ccea372
+ * @license Angular v5.2.0-rc.0-ef78538
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-rc.0-ccea372');
+var VERSION = new Version('5.2.0-rc.0-ef78538');
 
 /**
  * @fileoverview added by tsickle
