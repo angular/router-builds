a6d6448803eb95d8de83c2c2262472919ddef465
Kara Erickson <karakara@google.com>
2018-01-04 22:36:18 +0000

    81823e8 build(aio): fix example protractor config for Travis (#21309)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-ef78538
+ * @license Angular v5.2.0-rc.0-81823e8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-rc.0-ef78538');
+var VERSION = new Version('5.2.0-rc.0-81823e8');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-ef78538
+ * @license Angular v5.2.0-rc.0-81823e8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */ 
