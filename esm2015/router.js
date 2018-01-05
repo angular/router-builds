0e1ef7dbbd26bf7c6f36b6d490e65c89fe98b900
Kara Erickson <karakara@google.com>
2018-01-05 21:43:58 +0000

    f9436f7 docs(aio): delete duplicated word (#21129)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-e726d81
+ * @license Angular v5.2.0-rc.0-f9436f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-e726d81');
+const VERSION = new Version('5.2.0-rc.0-f9436f7');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-e726d81
+ * @license Angular v5.2.0-rc.0-f9436f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
