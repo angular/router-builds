c6a1d49b949921e456ec35e65beac44f1cfefb95
Kara Erickson <karakara@google.com>
2018-01-08 18:38:42 +0000

    b15d50a build(core): improve payload size failure message (#21346)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-db55e86
+ * @license Angular v5.2.0-rc.0-b15d50a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-db55e86');
+const VERSION = new Version('5.2.0-rc.0-b15d50a');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-db55e86
+ * @license Angular v5.2.0-rc.0-b15d50a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
