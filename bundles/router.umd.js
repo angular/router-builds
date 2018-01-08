996b1b14ae4bd495385862a30783325e28a522d2
Kara Erickson <karakara@google.com>
2018-01-08 18:55:11 +0000

    1bd03a4 docs(aio): document the template compiler options (#21333)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-8b197b2
+ * @license Angular v5.1.3-1bd03a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.3-8b197b2
+ * @license Angular v5.1.3-1bd03a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7403,7 +7403,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.3-8b197b2');
+var VERSION = new _angular_core.Version('5.1.3-1bd03a4');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-8b197b2
+ * @license Angular v5.1.3-1bd03a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6208,7 +6208,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.3-8b197b2');
+const VERSION = new Version('5.1.3-1bd03a4');
 
 /**
  * @fileoverview added by tsickle
