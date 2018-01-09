93a033d700169631602598db7f25272aff511537
Kara Erickson <karakara@google.com>
2018-01-09 19:21:16 +0000

    80a5b91 build(aio): upgrade to zone.js@0.8.19 (#21349)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-9728dce
+ * @license Angular v5.2.0-rc.0-80a5b91
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-9728dce');
+const VERSION = new Version('5.2.0-rc.0-80a5b91');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-9728dce
+ * @license Angular v5.2.0-rc.0-80a5b91
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
