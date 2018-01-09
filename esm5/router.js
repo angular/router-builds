d0c840ca684b3caf04f3777a60acb7d69b7a1b4f
Kara Erickson <karakara@google.com>
2018-01-09 19:14:31 +0000

    9728dce fix(bazel): Give correct module names for ES6 output (#21320)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-463e287
+ * @license Angular v5.2.0-rc.0-9728dce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-rc.0-463e287');
+var VERSION = new Version('5.2.0-rc.0-9728dce');
 
 /**
  * @fileoverview added by tsickle
