ae5de20b0e30cb84bd9ff8ebb8c7464b94e27fae
Kara Erickson <karakara@google.com>
2018-01-09 22:37:33 +0000

    3db91ff feat(core): add ngAfterViewInit and ngAfterViewChecked support to render3 (#21266)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-229b76c
+ * @license Angular v5.2.0-rc.0-3db91ff
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-229b76c');
+const VERSION = new Version('5.2.0-rc.0-3db91ff');
 
 /**
  * @fileoverview added by tsickle
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-229b76c
+ * @license Angular v5.2.0-rc.0-3db91ff
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-rc.0-229b76c');
+var VERSION = new Version('5.2.0-rc.0-3db91ff');
 
 /**
  * @fileoverview added by tsickle
