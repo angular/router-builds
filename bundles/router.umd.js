01fac620972922d8c6f4b79f91427af9aa0eb9bb
Alex Eagle <alexeagle@google.com>
2018-01-10 21:30:53 +0000

    7493b8a build: upgrade yarn to 1.3.2 (#21406)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-c4f02e2
+ * @license Angular v5.2.0-7493b8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-c4f02e2
+ * @license Angular v5.2.0-7493b8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7426,7 +7426,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-c4f02e2');
+var VERSION = new _angular_core.Version('5.2.0-7493b8a');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-c4f02e2
+ * @license Angular v5.2.0-7493b8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
