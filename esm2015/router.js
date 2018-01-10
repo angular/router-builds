ca0a83cabe182720583c9f3b8674644632f84e7f
Alex Eagle <alexeagle@google.com>
2018-01-10 20:21:47 +0000

    167cbed build: add bazel rulse for benchmarks (#21436)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-70e8802
+ * @license Angular v5.2.0-167cbed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-70e8802');
+const VERSION = new Version('5.2.0-167cbed');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-70e8802
+ * @license Angular v5.2.0-167cbed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
