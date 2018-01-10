4678737c862875712e767d7b60e0b3a38fd0f791
Alex Eagle <alexeagle@google.com>
2018-01-10 21:45:09 +0000

    e3e2fc0 fix(compiler): cache external reference resolution (#21359)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-7493b8a
+ * @license Angular v5.2.0-e3e2fc0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-7493b8a');
+const VERSION = new Version('5.2.0-e3e2fc0');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-7493b8a
+ * @license Angular v5.2.0-e3e2fc0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
