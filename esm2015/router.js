b29d8769f7c88ec6c46ec5216c015c77e22b582b
Kara Erickson <karakara@google.com>
2018-01-10 01:38:29 +0000

    04dd971 docs: add changelog for 5.2.0

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-3db91ff
+ * @license Angular v5.2.0-04dd971
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-3db91ff');
+const VERSION = new Version('5.2.0-04dd971');
 
 /**
  * @fileoverview added by tsickle
