ab42b96f9b956b0e7b578236ba5eb1dd794064a4
Alex Eagle <alexeagle@google.com>
2018-01-10 21:42:09 +0000

    7bdd9ae build: upgrade yarn to 1.3.2 (#21406)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-5ede67c
+ * @license Angular v5.2.0-7bdd9ae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-5ede67c');
+const VERSION = new Version('5.2.0-7bdd9ae');
 
 /**
  * @fileoverview added by tsickle
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-5ede67c
+ * @license Angular v5.2.0-7bdd9ae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-5ede67c');
+var VERSION = new Version('5.2.0-7bdd9ae');
 
 /**
  * @fileoverview added by tsickle
