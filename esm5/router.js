e72621b0af64c192a35f77f96717718db029690d
Alex Eagle <alexeagle@google.com>
2018-01-10 23:53:46 +0000

    6a97b5b docs(ivy): add Ivy as a recognized label (#21428)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-aad1126
+ * @license Angular v5.2.0-6a97b5b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-aad1126');
+var VERSION = new Version('5.2.0-6a97b5b');
 
 /**
  * @fileoverview added by tsickle
