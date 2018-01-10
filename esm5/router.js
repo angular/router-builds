7b6e6fa44db43983c3db8e8f782f47b245e4e0ea
Kara Erickson <karakara@google.com>
2018-01-10 19:33:36 +0000

    908129e docs(core): provide note for unit test changes with the `ng-star-inserted` className (#21450)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-04dd971
+ * @license Angular v5.2.0-908129e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-04dd971');
+var VERSION = new Version('5.2.0-908129e');
 
 /**
  * @fileoverview added by tsickle
