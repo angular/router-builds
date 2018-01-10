4e0926b05d50810b8ecd90858b3d2191fbad2b41
Alex Eagle <alexeagle@google.com>
2018-01-10 20:38:36 +0000

    72246d4 ci: update forms approval list to include guides (#21449)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-d2aa8ac
+ * @license Angular v5.2.0-72246d4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-d2aa8ac');
+const VERSION = new Version('5.2.0-72246d4');
 
 /**
  * @fileoverview added by tsickle
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-d2aa8ac
+ * @license Angular v5.2.0-72246d4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-d2aa8ac');
+var VERSION = new Version('5.2.0-72246d4');
 
 /**
  * @fileoverview added by tsickle
