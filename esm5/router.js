b97930c038347819d0a25aa09ffaf56b5006cc23
Alex Eagle <alexeagle@google.com>
2018-01-10 22:48:40 +0000

    137b9c7 fix(aio): preserve static Observable methods (#21351)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-2ad92ed
+ * @license Angular v5.2.0-137b9c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-2ad92ed');
+var VERSION = new Version('5.2.0-137b9c7');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-2ad92ed
+ * @license Angular v5.2.0-137b9c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
