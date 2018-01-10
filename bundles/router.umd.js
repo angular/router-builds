f60e700586e0adb7cadfeee1e9cc7586c4c7785e
Alex Eagle <alexeagle@google.com>
2018-01-10 20:19:36 +0000

    ef956a2 build: add bazel rulse for benchmarks (#21436)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-fc3e7e0
+ * @license Angular v5.2.0-ef956a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-fc3e7e0
+ * @license Angular v5.2.0-ef956a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7426,7 +7426,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-fc3e7e0');
+var VERSION = new _angular_core.Version('5.2.0-ef956a2');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-fc3e7e0
+ * @license Angular v5.2.0-ef956a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
