8d7e11f97c508da05cecb5fec74a3060ec055ce7
Alex Eagle <alexeagle@google.com>
2018-01-10 21:15:53 +0000

    31b671a fix(aio): support multibyte character in heading (#21414)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-3804d20
+ * @license Angular v5.2.0-31b671a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-3804d20');
+const VERSION = new Version('5.2.0-31b671a');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-3804d20
+ * @license Angular v5.2.0-31b671a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
