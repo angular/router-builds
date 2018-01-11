e74eb13d1aa1c8ce633eee469011755684984bdb
Alex Eagle <alexeagle@google.com>
2018-01-11 19:13:59 +0000

    9b84a32 fix(benchpress): should still support selenium_webdriver < 3.6.0 (#21477)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-00300f6
+ * @license Angular v5.2.0-9b84a32
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-00300f6');
+var VERSION = new Version('5.2.0-9b84a32');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-00300f6
+ * @license Angular v5.2.0-9b84a32
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
