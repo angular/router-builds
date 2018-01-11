a634a5974bf7a331e53ea21d112ce9fc7e141cc8
Alex Eagle <alexeagle@google.com>
2018-01-11 15:23:15 +0000

    00300f6 docs(forms): clarify note in reactive forms doc (#21134)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-5eaaac3
+ * @license Angular v5.2.0-00300f6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-5eaaac3');
+var VERSION = new Version('5.2.0-00300f6');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-5eaaac3
+ * @license Angular v5.2.0-00300f6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
