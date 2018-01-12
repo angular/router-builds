affe24e4ab5e75dfa12232daa0c96088d67cbe13
Alex Eagle <alexeagle@google.com>
2018-01-12 22:56:40 +0000

    2d44a2a docs(compiler): document the $any type cast function (#20968)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-c5c6d84
+ * @license Angular v5.2.0-2d44a2a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_common,_angular_common_testing,_angular_core,_angular_router) { 'use strict';
 
 /**
- * @license Angular v5.2.0-c5c6d84
+ * @license Angular v5.2.0-2d44a2a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-c5c6d84
+ * @license Angular v5.2.0-2d44a2a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
