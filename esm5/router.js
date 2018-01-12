b6ab9817fa8c905060a3e89c847452c3c5feec61
Alex Eagle <alexeagle@google.com>
2018-01-12 22:05:06 +0000

    cf4bea5 ci(aio): run e2e tests in `production` mode (#21470)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-8e9cd57
+ * @license Angular v5.2.0-cf4bea5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-8e9cd57');
+var VERSION = new Version('5.2.0-cf4bea5');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-8e9cd57
+ * @license Angular v5.2.0-cf4bea5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
