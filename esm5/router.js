22b8b171401fb89d97a3e4af2aaf65dc0e276c3b
Alex Eagle <alexeagle@google.com>
2018-01-12 19:06:41 +0000

    9b5a485 perf(ivy): add missing dom element in render3_function tree benchmark (#21476)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-50ca01e
+ * @license Angular v5.2.0-9b5a485
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-50ca01e');
+var VERSION = new Version('5.2.0-9b5a485');
 
 /**
  * @fileoverview added by tsickle
