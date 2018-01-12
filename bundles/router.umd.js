151a17ffd6bb4b2a2cb5abb51fdc69a800e8a310
Alex Eagle <alexeagle@google.com>
2018-01-12 21:55:10 +0000

    05208b8 fix(common): set correct timezone for ISO8601 dates in Safari (#21506)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-bb62458
+ * @license Angular v5.2.0-05208b8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-bb62458
+ * @license Angular v5.2.0-05208b8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7426,7 +7426,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-bb62458');
+var VERSION = new _angular_core.Version('5.2.0-05208b8');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-bb62458
+ * @license Angular v5.2.0-05208b8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-bb62458');
+var VERSION = new Version('5.2.0-05208b8');
 
 /**
  * @fileoverview added by tsickle
