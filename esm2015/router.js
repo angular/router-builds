92ed1a560516b5954e454a675c802c12e2323006
Alex Eagle <alexeagle@google.com>
2018-01-12 20:24:34 +0000

    cffa0fe feat(bazel): allow ng_module rules to control whether type checking is enabled (#21460)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-9b5a485
+ * @license Angular v5.2.0-cffa0fe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-9b5a485');
+const VERSION = new Version('5.2.0-cffa0fe');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-9b5a485
+ * @license Angular v5.2.0-cffa0fe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
