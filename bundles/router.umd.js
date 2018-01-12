a8a340c8c8f44dd56269f28caa5b7f19947ab019
Alex Eagle <alexeagle@google.com>
2018-01-12 22:57:59 +0000

    71dd92b style(aio): enforce strict TypeScript checks (#21342)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-977978e
+ * @license Angular v5.2.0-71dd92b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-977978e
+ * @license Angular v5.2.0-71dd92b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7426,7 +7426,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-977978e');
+var VERSION = new _angular_core.Version('5.2.0-71dd92b');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-977978e
+ * @license Angular v5.2.0-71dd92b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
