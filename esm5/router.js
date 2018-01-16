41bb06fe3d94aaaefa1ef45fda0ee9164b0e3496
Alex Eagle <alexeagle@google.com>
2018-01-16 15:31:36 +0000

    a771ee5 build(aio): move file cleaning to later in the doc gen (#21540)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-a4cbe35
+ * @license Angular v5.2.0-a771ee5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-a4cbe35');
+var VERSION = new Version('5.2.0-a771ee5');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-a4cbe35
+ * @license Angular v5.2.0-a771ee5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
