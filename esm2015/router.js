eaf7ff2c19f5515e90b55b2bed759b10c3963b4f
Alex Eagle <alexeagle@google.com>
2018-01-17 15:35:54 +0000

    54e7576 fix(core): fix chained http call (#20924)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-d3333f0
+ * @license Angular v5.2.0-54e7576
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-d3333f0');
+const VERSION = new Version('5.2.0-54e7576');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-d3333f0
+ * @license Angular v5.2.0-54e7576
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
