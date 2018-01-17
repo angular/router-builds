a6388b6cf4b6f75e066127163666cc76985a68dc
Alex Eagle <alexeagle@google.com>
2018-01-17 16:00:29 +0000

    4556532 feat(core): add binding name to content changed error (#20352)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-54e7576
+ * @license Angular v5.2.0-4556532
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-54e7576');
+var VERSION = new Version('5.2.0-4556532');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-54e7576
+ * @license Angular v5.2.0-4556532
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
