b44a4b81a0326c386cb7342b7c25b241fe17a9a1
Alex Eagle <alexeagle@google.com>
2018-01-17 17:28:42 +0000

    420f5c4 build(bazel): remove spurious file (#21455)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-b773a4a
+ * @license Angular v5.2.0-420f5c4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-b773a4a
+ * @license Angular v5.2.0-420f5c4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7426,7 +7426,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-b773a4a');
+var VERSION = new _angular_core.Version('5.2.0-420f5c4');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-b773a4a
+ * @license Angular v5.2.0-420f5c4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-b773a4a');
+var VERSION = new Version('5.2.0-420f5c4');
 
 /**
  * @fileoverview added by tsickle
