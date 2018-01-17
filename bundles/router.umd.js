6f3fe775c6c23665d26903f762332a07b19b9a6b
GitHub <noreply@github.com>
2018-01-17 23:42:08 +0000

    0eabd07 fix: avoid triggering a cli bug (#21611)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.0-8503cc1
+ * @license Angular v5.9.9-6-beta.0-0eabd07
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.0-8503cc1
+ * @license Angular v5.9.9-6-beta.0-0eabd07
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7426,7 +7426,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.0-8503cc1');
+var VERSION = new _angular_core.Version('5.9.9-6-beta.0-0eabd07');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.0-8503cc1
+ * @license Angular v5.9.9-6-beta.0-0eabd07
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.0-8503cc1');
+var VERSION = new Version('5.9.9-6-beta.0-0eabd07');
 
 /**
  * @fileoverview added by tsickle
