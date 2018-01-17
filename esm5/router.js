af82d9f038b1803a98de403969dcfcf0471037da
Alex Eagle <alexeagle@google.com>
2018-01-17 11:57:18 +0000

    fb4d84d refactor(core): Refactor IterableDiffers#create method (#21383)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-3bcc0e6
+ * @license Angular v5.2.0-fb4d84d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-3bcc0e6');
+var VERSION = new Version('5.2.0-fb4d84d');
 
 /**
  * @fileoverview added by tsickle
