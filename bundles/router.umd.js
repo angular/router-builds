40ba752055ecaa638e55d2886c6f659431502bf2
Chuck Jazdzewski <chuckj@google.com>
2018-01-18 00:06:25 +0000

    47b7898 Revert "fix(core): fix chained http call (#20924)"

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-0eabd07
+ * @license Angular v5.9.9-6-beta.0-47b7898
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.9.9-6-beta.0-0eabd07
+ * @license Angular v5.9.9-6-beta.0-47b7898
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7426,7 +7426,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.9.9-6-beta.0-0eabd07');
+var VERSION = new _angular_core.Version('5.9.9-6-beta.0-47b7898');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-0eabd07
+ * @license Angular v5.9.9-6-beta.0-47b7898
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
