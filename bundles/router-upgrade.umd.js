887d085e21b851c6431d2d15e1650a2050bddac9
Chuck Jazdzewski <chuckj@google.com>
2018-01-18 04:11:11 +0000

    2cf9d6d fix(aio): add a required comma in firebase.json

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-af2a843
+ * @license Angular v5.9.9-6-beta.0-2cf9d6d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_router,_angular_upgrade_static) { 'use strict';
 
 /**
- * @license Angular v5.9.9-6-beta.0-af2a843
+ * @license Angular v5.9.9-6-beta.0-2cf9d6d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-af2a843
+ * @license Angular v5.9.9-6-beta.0-2cf9d6d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
