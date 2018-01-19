ffb413c280d808f0eb0cae3d5b90b1e3784af17a
Miško Hevery <misko@hevery.com>
2018-01-19 21:30:25 +0000

    c828e56 build: Remove angular_src nested workspace (#21096)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-af4eb00
+ * @license Angular v5.2.1-c828e56
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.1-af4eb00');
+const VERSION = new Version('5.2.1-c828e56');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-af4eb00
+ * @license Angular v5.2.1-c828e56
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
