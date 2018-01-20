5f8e835a4e8d9cc38858fec3ca8880d61adc4b9e
Miško Hevery <misko@hevery.com>
2018-01-20 17:52:00 +0000

    86d9612 feat(ivy): update compiler to specification (#21657)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-8c51c27
+ * @license Angular v5.9.9-6-beta.0-86d9612
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.9.9-6-beta.0-8c51c27');
+var VERSION = new Version('5.9.9-6-beta.0-86d9612');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-8c51c27
+ * @license Angular v5.9.9-6-beta.0-86d9612
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
