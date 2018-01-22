20fe3950091c3947a635dd0114b92016ab207b18
Miško Hevery <misko@hevery.com>
2018-01-22 21:17:36 +0000

    a64af40 build(aio): generate sitemap from the generated pages (#21689)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-86d9612
+ * @license Angular v5.9.9-6-beta.0-a64af40
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.9.9-6-beta.0-86d9612');
+var VERSION = new Version('5.9.9-6-beta.0-a64af40');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-86d9612
+ * @license Angular v5.9.9-6-beta.0-a64af40
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
