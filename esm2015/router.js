c95274c73bfbe7497d2657199a0f06ac684d3911
Miško Hevery <misko@hevery.com>
2018-01-23 21:55:33 +0000

    52970c0 fix(compiler-cli): do not fold errors past calls in the collector (#21708)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-21f7669
+ * @license Angular v5.2.1-52970c0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.1-21f7669');
+const VERSION = new Version('5.2.1-52970c0');
 
 /**
  * @fileoverview added by tsickle
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-21f7669
+ * @license Angular v5.2.1-52970c0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.1-21f7669');
+var VERSION = new Version('5.2.1-52970c0');
 
 /**
  * @fileoverview added by tsickle
