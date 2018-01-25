24477555ef0fda3e3f4d9dc6f56b2b8ca1b5ed1c
Miško Hevery <misko@hevery.com>
2018-01-25 00:17:31 +0000

    e4b11b2 Revert "fix(aio): update redirects to fix unwanted 404s (#21712)"

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-5f681f9
+ * @license Angular v5.9.9-6-beta.0-e4b11b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.9.9-6-beta.0-5f681f9
+ * @license Angular v5.9.9-6-beta.0-e4b11b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7426,7 +7426,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.9.9-6-beta.0-5f681f9');
+var VERSION = new _angular_core.Version('5.9.9-6-beta.0-e4b11b2');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-5f681f9
+ * @license Angular v5.9.9-6-beta.0-e4b11b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.9.9-6-beta.0-5f681f9');
+var VERSION = new Version('5.9.9-6-beta.0-e4b11b2');
 
 /**
  * @fileoverview added by tsickle
