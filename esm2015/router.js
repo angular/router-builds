b3d969b39607e0e98a118a126c1eb560b50fffdb
Misko Hevery <misko@hevery.com>
2018-01-25 05:10:23 +0000

    da9e57b build: Update to latest rules_typescript. (#21675)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-5423882
+ * @license Angular v5.2.1-da9e57b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.1-5423882');
+const VERSION = new Version('5.2.1-da9e57b');
 
 /**
  * @fileoverview added by tsickle
