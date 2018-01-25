2430782efe7a60743b409887650f461d964ffc6e
Misko Hevery <misko@hevery.com>
2018-01-25 06:30:54 +0000

    d08785d docs: add changelog for 6.0.0-beta.1

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-e4b11b2
+ * @license Angular v6.0.0-beta.1-d08785d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.9.9-6-beta.0-e4b11b2
+ * @license Angular v6.0.0-beta.1-d08785d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7426,7 +7426,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.9.9-6-beta.0-e4b11b2');
+var VERSION = new _angular_core.Version('6.0.0-beta.1-d08785d');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-e4b11b2
+ * @license Angular v6.0.0-beta.1-d08785d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
