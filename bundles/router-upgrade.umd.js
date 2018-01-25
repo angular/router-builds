32eb5e7f458d5517157dddc66dc8a60565231e0e
Miško Hevery <misko@hevery.com>
2018-01-25 21:59:27 +0000

    27ecd07 docs(aio): fix missing stylesheet in component-styles example (#21772)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-3aaf43f
+ * @license Angular v5.2.2-27ecd07
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_router,_angular_upgrade_static) { 'use strict';
 
 /**
- * @license Angular v5.2.2-3aaf43f
+ * @license Angular v5.2.2-27ecd07
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
