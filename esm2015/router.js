fad0f3a14e69c751f13e2dd8ac5bf2545b8369e7
Miško Hevery <misko@hevery.com>
2018-01-26 18:49:13 +0000

    67806a7 fix(aio): close SideNav on non-sidenav doc on wide screen (#21538)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-87e06d7
+ * @license Angular v5.2.2-67806a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.2-87e06d7');
+const VERSION = new Version('5.2.2-67806a7');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-87e06d7
+ * @license Angular v5.2.2-67806a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
