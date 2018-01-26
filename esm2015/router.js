c13ce59c36db3e4b6f3ea09eb549f29aebf1b3f9
Misko Hevery <misko@hevery.com>
2018-01-26 07:04:42 +0000

    bbb8f38 feat(ivy): implement template variables (#21760)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-08aa54e
+ * @license Angular v6.0.0-beta.1-bbb8f38
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.1-08aa54e');
+const VERSION = new Version('6.0.0-beta.1-bbb8f38');
 
 /**
  * @fileoverview added by tsickle
