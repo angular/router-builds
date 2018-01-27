1c360ad7ea9a54035a601ccf884203d20ae0f513
Misko Hevery <misko@hevery.com>
2018-01-27 19:20:16 +0000

    6a7689d build: update to latest bazel rules (#21821)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-696ba01
+ * @license Angular v5.2.2-6a7689d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.2-696ba01');
+const VERSION = new Version('5.2.2-6a7689d');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-696ba01
+ * @license Angular v5.2.2-6a7689d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
