38a84f7d6aed07cd57a825e2e454e8c66dceee7d
Jason Aden <jasonaden@google.com>
2018-01-27 00:01:17 +0000

    2af19c9 fix(core): fix retrieving the binding name when an expression changes (#21814)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-dcca799
+ * @license Angular v6.0.0-beta.1-2af19c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6260,7 +6260,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.1-dcca799');
+const VERSION = new Version('6.0.0-beta.1-2af19c9');
 
 /**
  * @fileoverview added by tsickle
