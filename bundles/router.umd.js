884e9ecd45fd363599c37af180ebd337a80a5b68
Jason Aden <jasonaden@google.com>
2018-01-27 00:30:00 +0000

    c3fb820 fix(aio): don't set noindex metatag in the static index.html (#21816)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-2af19c9
+ * @license Angular v6.0.0-beta.1-c3fb820
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.1-2af19c9
+ * @license Angular v6.0.0-beta.1-c3fb820
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7451,7 +7451,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.1-2af19c9');
+var VERSION = new _angular_core.Version('6.0.0-beta.1-c3fb820');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
