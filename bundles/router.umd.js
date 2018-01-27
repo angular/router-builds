b64e184fb26476742d6729f9c38c2596d025d5e1
Misko Hevery <misko@hevery.com>
2018-01-27 19:10:06 +0000

    ab69f12 refactor(ivy): code review changes (#21638)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-ede9cb7
+ * @license Angular v6.0.0-beta.1-ab69f12
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.1-ede9cb7
+ * @license Angular v6.0.0-beta.1-ab69f12
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7426,7 +7426,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.1-ede9cb7');
+var VERSION = new _angular_core.Version('6.0.0-beta.1-ab69f12');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-ede9cb7
+ * @license Angular v6.0.0-beta.1-ab69f12
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
