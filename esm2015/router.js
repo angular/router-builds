bc5d1bdf3284db8f1cdaad8c5eaa0d06d8a8d098
Jason Aden <jasonaden@google.com>
2018-01-29 20:02:06 +0000

    72265f7 fix(aio): missing plural s in preserveWhiteSpaces example (#21854)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-6e8bc31
+ * @license Angular v6.0.0-beta.1-72265f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6260,7 +6260,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.1-6e8bc31');
+const VERSION = new Version('6.0.0-beta.1-72265f7');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-6e8bc31
+ * @license Angular v6.0.0-beta.1-72265f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
