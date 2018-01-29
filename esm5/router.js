9577a3f47b97fb831f6afe2469f7a4ce2a7b9c05
Jason Aden <jasonaden@google.com>
2018-01-29 20:09:49 +0000

    dd48df1 perf(ivy): use buildOptimizer in hello_world__render3__rollup integration test (#21744)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-18174e5
+ * @license Angular v6.0.0-beta.1-dd48df1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7429,7 +7429,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-18174e5');
+var VERSION = new Version('6.0.0-beta.1-dd48df1');
 
 /**
  * @fileoverview added by tsickle
