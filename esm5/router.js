d5e7375f8057d348b37a6c65853eb33b725e3e6b
Jason Aden <jasonaden@google.com>
2018-01-30 19:57:29 +0000

    5d75df8 ci: unblock master by ignoring date pipe tests while we fix it (#21906)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-ed2b717
+ * @license Angular v5.2.2-5d75df8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.2-ed2b717');
+var VERSION = new Version('5.2.2-5d75df8');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-ed2b717
+ * @license Angular v5.2.2-5d75df8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
