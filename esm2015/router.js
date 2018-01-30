37f6513e4b1261c270677cbced6dd243c17ef84a
Jason Aden <jasonaden@google.com>
2018-01-30 00:35:10 +0000

    8dff9d8 refactor(ivy): save check methods separately (#21795)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-b4cd279
+ * @license Angular v6.0.0-beta.1-8dff9d8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6260,7 +6260,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.1-b4cd279');
+const VERSION = new Version('6.0.0-beta.1-8dff9d8');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-b4cd279
+ * @license Angular v6.0.0-beta.1-8dff9d8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
