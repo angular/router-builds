848fe79b1a93f2f505d30e2e15dd48668f332f1c
Jason Aden <jasonaden@google.com>
2018-01-30 00:33:46 +0000

    11b1267 docs: document debugging a Node test in VSCode/Bazel (#21868)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-dd48df1
+ * @license Angular v6.0.0-beta.1-11b1267
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7429,7 +7429,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-dd48df1');
+var VERSION = new Version('6.0.0-beta.1-11b1267');
 
 /**
  * @fileoverview added by tsickle
