722bfc2667cbe7c2346b52c8716089e8b990879c
Jason Aden <jasonaden@google.com>
2018-01-30 20:09:39 +0000

    b10540a feat(service-worker): add helper script which will uninstall SW (#21863)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-b62739a
+ * @license Angular v6.0.0-beta.1-b10540a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6260,7 +6260,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.1-b62739a');
+const VERSION = new Version('6.0.0-beta.1-b10540a');
 
 /**
  * @fileoverview added by tsickle
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-b62739a
+ * @license Angular v6.0.0-beta.1-b10540a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7429,7 +7429,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-b62739a');
+var VERSION = new Version('6.0.0-beta.1-b10540a');
 
 /**
  * @fileoverview added by tsickle
