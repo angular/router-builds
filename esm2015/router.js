2e2c2980ebda8e53f34bd432f8bffe761ab829ea
Jason Aden <jasonaden@google.com>
2018-01-31 18:40:42 +0000

    06d6c76 fix(ivy): compiler should emit new refresh pattern (#21862)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-407b5cf
+ * @license Angular v6.0.0-beta.1-06d6c76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6260,7 +6260,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.1-407b5cf');
+const VERSION = new Version('6.0.0-beta.1-06d6c76');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-407b5cf
+ * @license Angular v6.0.0-beta.1-06d6c76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
