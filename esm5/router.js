04c11ac0b0edcce6ea1b15f11e4c959831a3be84
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-01 16:54:20 +0000

    f9fa157 docs(aio): add missing closing `<code-examle>` tag (#21771)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-1aa2947
+ * @license Angular v6.0.0-beta.2-f9fa157
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7429,7 +7429,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.2-1aa2947');
+var VERSION = new Version('6.0.0-beta.2-f9fa157');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-1aa2947
+ * @license Angular v6.0.0-beta.2-f9fa157
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
