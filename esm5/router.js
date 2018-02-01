90071e2d493688514ba05ec26354756d1b11ebe4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-01 16:53:20 +0000

    1aa2947 feat(ivy): add support for attributes on ng-content nodes (#21935)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-61abba4
+ * @license Angular v6.0.0-beta.2-1aa2947
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7429,7 +7429,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.2-61abba4');
+var VERSION = new Version('6.0.0-beta.2-1aa2947');
 
 /**
  * @fileoverview added by tsickle
