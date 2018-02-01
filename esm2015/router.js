647c1bd9624840ad7fd5e87e34bf1f2dfd29dcce
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-01 17:06:38 +0000

    65cf1ad fix(ivy): remove unnecessary parameter of NgOnChangesFeature (#21879)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-8b14488
+ * @license Angular v6.0.0-beta.2-65cf1ad
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6260,7 +6260,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.2-8b14488');
+const VERSION = new Version('6.0.0-beta.2-65cf1ad');
 
 /**
  * @fileoverview added by tsickle
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-8b14488
+ * @license Angular v6.0.0-beta.2-65cf1ad
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7429,7 +7429,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.2-8b14488');
+var VERSION = new Version('6.0.0-beta.2-65cf1ad');
 
 /**
  * @fileoverview added by tsickle
