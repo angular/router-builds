2112cb75ff86766665b47d8ecd78f65edf98c127
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:14:04 +0000

    1242839 build(aio): move zip and live-example generation to `yarn predocs` task (#21970)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-3cc1d76
+ * @license Angular v6.0.0-beta.2-1242839
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7429,7 +7429,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.2-3cc1d76');
+var VERSION = new Version('6.0.0-beta.2-1242839');
 
 /**
  * @fileoverview added by tsickle
