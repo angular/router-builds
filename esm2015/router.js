06968ecae4fc86d6227776cbf76e60ac36a64602
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:15:18 +0000

    75eecdc docs: add missing underline (#21892) PR Close #21892

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-965eecc
+ * @license Angular v5.2.3-75eecdc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6232,7 +6232,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.3-965eecc');
+const VERSION = new Version('5.2.3-75eecdc');
 
 /**
  * @fileoverview added by tsickle
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-965eecc
+ * @license Angular v5.2.3-75eecdc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7404,7 +7404,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.3-965eecc');
+var VERSION = new Version('5.2.3-75eecdc');
 
 /**
  * @fileoverview added by tsickle
