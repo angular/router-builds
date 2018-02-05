7df3edebe50007f8f42b7b6a7dc1f73aa821661c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 21:53:22 +0000

    0c9ec37 ci: mark PRs with rejection as not green (#21922) PR Close #21922

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-9a0700f
+ * @license Angular v6.0.0-beta.2-0c9ec37
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6260,7 +6260,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.2-9a0700f');
+const VERSION = new Version('6.0.0-beta.2-0c9ec37');
 
 /**
  * @fileoverview added by tsickle
