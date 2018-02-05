7e230fb42fea664159191ae879617ea34758691c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 22:20:28 +0000

    3aa7e02 docs(aio): fix swap value (#20905)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-9d3326c
+ * @license Angular v6.0.0-beta.2-3aa7e02
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6260,7 +6260,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.2-9d3326c');
+const VERSION = new Version('6.0.0-beta.2-3aa7e02');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-9d3326c
+ * @license Angular v6.0.0-beta.2-3aa7e02
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
