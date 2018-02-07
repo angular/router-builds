afb0c4ceb4781fe275690552ac4d0bd848509742
Miško Hevery <misko@hevery.com>
2018-02-07 21:10:50 +0000

    9fdb804 test(forms): update test name with correct wording (#21833)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-9d02db3
+ * @license Angular v5.2.4-9fdb804
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6233,7 +6233,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.4-9d02db3');
+const VERSION = new Version('5.2.4-9fdb804');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-9d02db3
+ * @license Angular v5.2.4-9fdb804
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
