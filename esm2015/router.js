3c7f35fdeafd70dd81f4a70a44496f33dbb16ad9
Miško Hevery <misko@hevery.com>
2018-02-07 21:07:54 +0000

    7e95802 fix(aio): ignore `.header-link` when selecting the heading text (#21695)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-9744a1c
+ * @license Angular v6.0.0-beta.3-7e95802
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6261,7 +6261,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-9744a1c');
+const VERSION = new Version('6.0.0-beta.3-7e95802');
 
 /**
  * @fileoverview added by tsickle
