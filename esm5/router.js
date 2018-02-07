ccfe875c41e4960f59fd02d3971a393cc0860699
Miško Hevery <misko@hevery.com>
2018-02-07 21:26:10 +0000

    07769e5 test(common): disable deprecated date pipe tests on chrome mobile (#21933)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-9fdb804
+ * @license Angular v5.2.4-07769e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7405,7 +7405,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-9fdb804');
+var VERSION = new Version('5.2.4-07769e5');
 
 /**
  * @fileoverview added by tsickle
