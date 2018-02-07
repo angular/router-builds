51f33f69ae8724d1236ca5df5248183ebe43e002
Miško Hevery <misko@hevery.com>
2018-02-07 20:52:34 +0000

    0bcfae7 fix(forms): prevent event emission on enable/disable when emitEvent is false (#12366) (#21018)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-140e7c0
+ * @license Angular v6.0.0-beta.3-0bcfae7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6261,7 +6261,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-140e7c0');
+const VERSION = new Version('6.0.0-beta.3-0bcfae7');
 
 /**
  * @fileoverview added by tsickle
