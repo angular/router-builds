a9c64932aa78fe6f0d5dcc25cf5000cf1fd7abd4
Miško Hevery <misko@hevery.com>
2018-02-07 20:39:52 +0000

    941e88f feat(forms): multiple validators for array method (#20766)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-7e928db
+ * @license Angular v6.0.0-beta.3-941e88f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6261,7 +6261,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-7e928db');
+const VERSION = new Version('6.0.0-beta.3-941e88f');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-7e928db
+ * @license Angular v6.0.0-beta.3-941e88f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
