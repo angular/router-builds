c8bd0f3c3a0a6d650ed83f02b3a815653aa97aa8
Miško Hevery <misko@hevery.com>
2018-02-07 21:52:41 +0000

    efc67ee fix(ivy): make pipe invocation locality neutral (#22030)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-7a406a3
+ * @license Angular v6.0.0-beta.3-efc67ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6261,7 +6261,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-7a406a3');
+const VERSION = new Version('6.0.0-beta.3-efc67ee');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-7a406a3
+ * @license Angular v6.0.0-beta.3-efc67ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
