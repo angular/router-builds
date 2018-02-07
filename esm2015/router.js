c7a458253168a9bd98771de57d29ca4d219de21b
Miško Hevery <misko@hevery.com>
2018-02-07 22:06:22 +0000

    b58c352 test(ivy): add canonical spec for object literals (#22045)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-efc67ee
+ * @license Angular v6.0.0-beta.3-b58c352
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6261,7 +6261,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-efc67ee');
+const VERSION = new Version('6.0.0-beta.3-b58c352');
 
 /**
  * @fileoverview added by tsickle
