e40a5e5570b268da17adbb71eb146cf00b0f0e50
Miško Hevery <misko@hevery.com>
2018-02-07 20:49:00 +0000

    140e7c0 fix(forms): make Validators.email support optional controls (#20869)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-941e88f
+ * @license Angular v6.0.0-beta.3-140e7c0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6261,7 +6261,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-941e88f');
+const VERSION = new Version('6.0.0-beta.3-140e7c0');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-941e88f
+ * @license Angular v6.0.0-beta.3-140e7c0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
