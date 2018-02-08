c0dca4af0d34cee67ca37d31e7a689416e082af6
Miško Hevery <misko@hevery.com>
2018-02-08 22:59:41 +0000

    0b1f5d2 Revert "docs(common): add `HttpParamsOptions` to the public API (#20332)"

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-dcf64a0
+ * @license Angular v6.0.0-beta.3-0b1f5d2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6261,7 +6261,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-dcf64a0');
+const VERSION = new Version('6.0.0-beta.3-0b1f5d2');
 
 /**
  * @fileoverview added by tsickle
