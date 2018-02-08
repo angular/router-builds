0932a154d768ae63cec7748a3c0a4c959e2ade15
Miško Hevery <misko@pangular.mtv.corp.google.com>
2018-02-08 00:33:26 +0000

    16e5b86 test(ivy): also track the size of the compressed hello world bundle (#22056)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-cd25939
+ * @license Angular v6.0.0-beta.3-16e5b86
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.3-cd25939
+ * @license Angular v6.0.0-beta.3-16e5b86
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7452,7 +7452,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.3-cd25939');
+var VERSION = new _angular_core.Version('6.0.0-beta.3-16e5b86');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-cd25939
+ * @license Angular v6.0.0-beta.3-16e5b86
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
