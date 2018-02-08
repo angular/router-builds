b9d025137fb02738a1a443b3026b6853898e2835
Miško Hevery <misko@pangular.mtv.corp.google.com>
2018-02-08 00:30:20 +0000

    cd25939 build(aio): update examples to CLI to 1.6.5 (#21222)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-b58c352
+ * @license Angular v6.0.0-beta.3-cd25939
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7430,7 +7430,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-b58c352');
+var VERSION = new Version('6.0.0-beta.3-cd25939');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-b58c352
+ * @license Angular v6.0.0-beta.3-cd25939
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
