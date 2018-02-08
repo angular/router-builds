1f0ff1e30246d1646288d3cc3a12c88d016fd1a7
Miško Hevery <misko@pangular.mtv.corp.google.com>
2018-02-08 01:25:43 +0000

    3f5a3d6 refactor(ivy): add internal `isProceduralRenderer()` (#22055)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-67cf712
+ * @license Angular v6.0.0-beta.3-3f5a3d6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7430,7 +7430,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-67cf712');
+var VERSION = new Version('6.0.0-beta.3-3f5a3d6');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-67cf712
+ * @license Angular v6.0.0-beta.3-3f5a3d6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
