e358f19fc13e291b12b7a3a2a9bb1c9136d72ea7
Miško Hevery <misko@hevery.com>
2018-02-09 21:59:46 +0000

    ac815f7 docs: fix typo in http.md (#22058)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-854f0ff
+ * @license Angular v5.2.4-ac815f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.4-854f0ff
+ * @license Angular v5.2.4-ac815f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7427,7 +7427,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.4-854f0ff');
+var VERSION = new _angular_core.Version('5.2.4-ac815f7');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-854f0ff
+ * @license Angular v5.2.4-ac815f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
