8ca8ed8497fa9e73cda9ca72940384fe46aae999
Miško Hevery <misko@hevery.com>
2018-02-10 01:36:28 +0000

    9b280ee build: merge-pr new checks that all requested changes have been addressed (#21817)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-ac815f7
+ * @license Angular v5.2.4-9b280ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_core,_angular_router,_angular_upgrade_static) { 'use strict';
 
 /**
- * @license Angular v5.2.4-ac815f7
+ * @license Angular v5.2.4-9b280ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-ac815f7
+ * @license Angular v5.2.4-9b280ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
