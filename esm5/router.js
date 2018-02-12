9de1cd9bc257d5c14cada5b39b5e2e577ab5f785
Miško Hevery <misko@hevery.com>
2018-02-12 18:34:29 +0000

    a616dd6 fix(aio): remove broken span closing tag (#22146)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-d57fd0b
+ * @license Angular v5.2.4-a616dd6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7405,7 +7405,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-d57fd0b');
+var VERSION = new Version('5.2.4-a616dd6');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-d57fd0b
+ * @license Angular v5.2.4-a616dd6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
