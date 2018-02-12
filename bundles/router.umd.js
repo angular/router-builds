f5ad56402ba5b81626e86839e21434fc3234f44c
Miško Hevery <misko@hevery.com>
2018-02-12 23:05:48 +0000

    1a897e4 docs(aio): add angular-playground to resources (#22042)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-603e50d
+ * @license Angular v5.2.4-1a897e4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.4-603e50d
+ * @license Angular v5.2.4-1a897e4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7459,7 +7459,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.4-603e50d');
+var VERSION = new _angular_core.Version('5.2.4-1a897e4');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-603e50d
+ * @license Angular v5.2.4-1a897e4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6249,7 +6249,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.4-603e50d');
+const VERSION = new Version('5.2.4-1a897e4');
 
 /**
  * @fileoverview added by tsickle
