ce03c723199d5d03d617afea5e07ad4cea77957b
Miško Hevery <misko@hevery.com>
2018-02-13 00:20:41 +0000

    eb48750 docs(aio): fix typo in "preserveWhitespaces" example (#22182)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-b333919
+ * @license Angular v6.0.0-beta.3-eb48750
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7462,7 +7462,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-b333919');
+var VERSION = new Version('6.0.0-beta.3-eb48750');
 
 /**
  * @fileoverview added by tsickle
