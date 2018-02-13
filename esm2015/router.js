b0506a0d207c239b5dd65ea6a294e79f7ae36fb8
Miško Hevery <misko@hevery.com>
2018-02-13 18:56:10 +0000

    f938774 ci: remove conditional clause for bazel install (#22170)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-e97afae
+ * @license Angular v5.2.4-f938774
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6249,7 +6249,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.4-e97afae');
+const VERSION = new Version('5.2.4-f938774');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-e97afae
+ * @license Angular v5.2.4-f938774
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
