644d159ebe4ddf5e7f7a0cffcc95df15cd672775
Miško Hevery <misko@hevery.com>
2018-02-13 19:07:06 +0000

    b75cf3f ci: update ngbot config file (#22173)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-4f19491
+ * @license Angular v6.0.0-beta.3-b75cf3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7462,7 +7462,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-4f19491');
+var VERSION = new Version('6.0.0-beta.3-b75cf3f');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-4f19491
+ * @license Angular v6.0.0-beta.3-b75cf3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
