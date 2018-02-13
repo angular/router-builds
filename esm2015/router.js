fb0ed1719465bcb20266426f8498235f678b55fb
Miško Hevery <misko@hevery.com>
2018-02-13 21:31:46 +0000

    72f8abd fix(compiler): make unary plus operator consistent to JavaScript (#22154)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-20a900b
+ * @license Angular v6.0.0-beta.3-72f8abd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6277,7 +6277,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-20a900b');
+const VERSION = new Version('6.0.0-beta.3-72f8abd');
 
 /**
  * @fileoverview added by tsickle
