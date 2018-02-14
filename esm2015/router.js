d29596d97a3d094e520765373655d1ad7c9cc84e
Miško Hevery <misko@hevery.com>
2018-02-14 05:22:18 +0000

    c4f841f fix(core): add stacktrace in log when error during cleanup component in TestBed (#22162)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-1b8ea10
+ * @license Angular v5.2.4-c4f841f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6249,7 +6249,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.4-1b8ea10');
+const VERSION = new Version('5.2.4-c4f841f');
 
 /**
  * @fileoverview added by tsickle
