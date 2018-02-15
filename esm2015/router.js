4a0ddad2ee288c8b8f354d06fdb1f02e6df666a6
Matias Niemelä <matias@yearofmoo.com>
2018-02-15 23:13:57 +0000

    f628797 Revert "refactor(ivy): assertion (#22189)"

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-ba9cd5b
+ * @license Angular v6.0.0-beta.4-f628797
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6277,7 +6277,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-ba9cd5b');
+const VERSION = new Version('6.0.0-beta.4-f628797');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-ba9cd5b
+ * @license Angular v6.0.0-beta.4-f628797
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
