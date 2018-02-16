08805c517ca2405f95c04bda5af9c60d55abde61
Victor Berchet <victor@suumit.com>
2018-02-16 23:45:24 +0000

    884de18 docs: replace plnkr with StackBlitz (#20365)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-dfa2fb9
+ * @license Angular v6.0.0-beta.4-884de18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.4-dfa2fb9
+ * @license Angular v6.0.0-beta.4-884de18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7484,7 +7484,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.4-dfa2fb9');
+var VERSION = new _angular_core.Version('6.0.0-beta.4-884de18');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-dfa2fb9
+ * @license Angular v6.0.0-beta.4-884de18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7462,7 +7462,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-dfa2fb9');
+var VERSION = new Version('6.0.0-beta.4-884de18');
 
 /**
  * @fileoverview added by tsickle
