b5cf2075d16ce56a07426199fe95b2629bfd5d80
Victor Berchet <victor@suumit.com>
2018-02-16 23:35:06 +0000

    2639b4b fix(common): correct mapping of Observable methods (#20518)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-f1a0632
+ * @license Angular v6.0.0-beta.4-2639b4b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6277,7 +6277,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-f1a0632');
+const VERSION = new Version('6.0.0-beta.4-2639b4b');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-f1a0632
+ * @license Angular v6.0.0-beta.4-2639b4b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
