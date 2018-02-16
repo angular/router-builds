d30f67aa1ea4c802e788df4b9a0c20e6106456d5
Victor Berchet <victor@suumit.com>
2018-02-16 00:39:19 +0000

    f755db7 fix(core): require factory to be provided for shakeable InjectionToken (#22207)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-f628797
+ * @license Angular v6.0.0-beta.4-f755db7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6277,7 +6277,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-f628797');
+const VERSION = new Version('6.0.0-beta.4-f755db7');
 
 /**
  * @fileoverview added by tsickle
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-f628797
+ * @license Angular v6.0.0-beta.4-f755db7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7462,7 +7462,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-f628797');
+var VERSION = new Version('6.0.0-beta.4-f755db7');
 
 /**
  * @fileoverview added by tsickle
