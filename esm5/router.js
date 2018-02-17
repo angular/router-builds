0ecea02d1571898dba5b1cf35af39d26e3fafa0b
Victor Berchet <victor@suumit.com>
2018-02-17 02:27:14 +0000

    f693be3 feat(ivy): add pureFunction0 instruction (#22214)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-e1bf067
+ * @license Angular v6.0.0-beta.4-f693be3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7462,7 +7462,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-e1bf067');
+var VERSION = new Version('6.0.0-beta.4-f693be3');
 
 /**
  * @fileoverview added by tsickle
