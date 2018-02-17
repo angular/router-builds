a908f4301307b8deae94b043599a24554c9553db
Victor Berchet <victor@suumit.com>
2018-02-17 02:25:46 +0000

    e1bf067 fix(animations): report correct totalTime value even during noOp animations (#22225)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-884de18
+ * @license Angular v6.0.0-beta.4-e1bf067
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6277,7 +6277,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-884de18');
+const VERSION = new Version('6.0.0-beta.4-e1bf067');
 
 /**
  * @fileoverview added by tsickle
