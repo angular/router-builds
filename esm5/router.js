eddf3d5530801f3bd5c6366890be09a11eb64a13
Victor Berchet <victor@suumit.com>
2018-02-18 21:28:26 +0000

    f4845fa build: use authenticated mode for the merge script (#22269)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-f693be3
+ * @license Angular v6.0.0-beta.4-f4845fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7462,7 +7462,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-f693be3');
+var VERSION = new Version('6.0.0-beta.4-f4845fa');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-f693be3
+ * @license Angular v6.0.0-beta.4-f4845fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
