d727372bf4bcdf966461adbbeddb7304447bacb7
Victor Berchet <victor@suumit.com>
2018-02-18 21:55:42 +0000

    ac2b04a test(ivy): Add small_app spec for sprint #3 (#22018)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-4ec40c6
+ * @license Angular v6.0.0-beta.4-ac2b04a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6277,7 +6277,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-4ec40c6');
+const VERSION = new Version('6.0.0-beta.4-ac2b04a');
 
 /**
  * @fileoverview added by tsickle
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-4ec40c6
+ * @license Angular v6.0.0-beta.4-ac2b04a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7462,7 +7462,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-4ec40c6');
+var VERSION = new Version('6.0.0-beta.4-ac2b04a');
 
 /**
  * @fileoverview added by tsickle
