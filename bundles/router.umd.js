467bca3024e72b2aac03657c9721900f52df46e7
Victor Berchet <victor@suumit.com>
2018-02-19 04:11:01 +0000

    cdfedc1 fix(common): fix merge error in ng_if.ts

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-c726d1d
+ * @license Angular v5.2.5-cdfedc1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.5-c726d1d
+ * @license Angular v5.2.5-cdfedc1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7459,7 +7459,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.5-c726d1d');
+var VERSION = new _angular_core.Version('5.2.5-cdfedc1');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-c726d1d
+ * @license Angular v5.2.5-cdfedc1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6249,7 +6249,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.5-c726d1d');
+const VERSION = new Version('5.2.5-cdfedc1');
 
 /**
  * @fileoverview added by tsickle
