6ee7b3b7b046b1d31d59ee603ef770c2f90f6b8a
Victor Berchet <victor@suumit.com>
2018-02-20 18:32:04 +0000

    e2bdef4 test(language-service): fix minor typos (#21372) PR Close #21372

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-8115edc
+ * @license Angular v6.0.0-beta.4-e2bdef4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6277,7 +6277,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-8115edc');
+const VERSION = new Version('6.0.0-beta.4-e2bdef4');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-8115edc
+ * @license Angular v6.0.0-beta.4-e2bdef4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
