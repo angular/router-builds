b3fe3994ec33e1b4395aff12c8e961d924e24348
Victor Berchet <victor@suumit.com>
2018-02-20 22:35:34 +0000

    99909bb feat(ivy): generate pipe references and definitions (#22034)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-ee60bb5
+ * @license Angular v6.0.0-beta.4-99909bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7462,7 +7462,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-ee60bb5');
+var VERSION = new Version('6.0.0-beta.4-99909bb');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-ee60bb5
+ * @license Angular v6.0.0-beta.4-99909bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
