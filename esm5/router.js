8e4bee5dbf8948ab88c4b1a404a9ab507811f77a
Victor Berchet <victor@suumit.com>
2018-02-21 00:46:53 +0000

    1c1cbba docs: add ngStyle to cheat sheet (#22070)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-3b692a5
+ * @license Angular v6.0.0-beta.4-1c1cbba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7462,7 +7462,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-3b692a5');
+var VERSION = new Version('6.0.0-beta.4-1c1cbba');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-3b692a5
+ * @license Angular v6.0.0-beta.4-1c1cbba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
