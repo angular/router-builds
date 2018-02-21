aa91ba791956530426a3fd95a812302bd7d124ad
Victor Berchet <victor@suumit.com>
2018-02-21 00:14:00 +0000

    b5ca275 docs(aio): Fix name of component (#22332) PR Close #22332

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-519f022
+ * @license Angular v6.0.0-beta.4-b5ca275
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7462,7 +7462,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-519f022');
+var VERSION = new Version('6.0.0-beta.4-b5ca275');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-519f022
+ * @license Angular v6.0.0-beta.4-b5ca275
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
