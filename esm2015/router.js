ed7babaa08289ca783a383842e56dd9b729d6629
Victor Berchet <victor@suumit.com>
2018-02-21 00:19:18 +0000

    69a0578 docs(aio): fix the css of the heroes component's buttons (#22333)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-b5ca275
+ * @license Angular v6.0.0-beta.4-69a0578
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6277,7 +6277,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-b5ca275');
+const VERSION = new Version('6.0.0-beta.4-69a0578');
 
 /**
  * @fileoverview added by tsickle
