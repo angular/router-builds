3282127c57b31d776a01915d54cfe0feea3a4be3
Victor Berchet <victor@suumit.com>
2018-02-22 19:44:28 +0000

    e86b64b feat(forms): allow markAsPending to emit events (#20212)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-90e9c59
+ * @license Angular v6.0.0-beta.5-e86b64b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.5-90e9c59
+ * @license Angular v6.0.0-beta.5-e86b64b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7484,7 +7484,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-90e9c59');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-e86b64b');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
