680189636ada2df763dbb790d874e5183471ed80
Victor Berchet <victor@suumit.com>
2018-02-22 01:44:23 +0000

    6091a95 docs: add changelog for 6.0.0-beta.5

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-5c89d6b
+ * @license Angular v6.0.0-beta.5-6091a95
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.4-5c89d6b
+ * @license Angular v6.0.0-beta.5-6091a95
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7484,7 +7484,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.4-5c89d6b');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-6091a95');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-5c89d6b
+ * @license Angular v6.0.0-beta.5-6091a95
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
