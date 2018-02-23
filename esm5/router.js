a211563489805ea375cffd674523bd95a3ba8c53
Victor Berchet <victor@suumit.com>
2018-02-23 00:35:02 +0000

    f791862 build: add release helper scripts (#22378)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-b2f366b
+ * @license Angular v6.0.0-beta.5-f791862
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7470,7 +7470,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-b2f366b');
+var VERSION = new Version('6.0.0-beta.5-f791862');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-b2f366b
+ * @license Angular v6.0.0-beta.5-f791862
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
