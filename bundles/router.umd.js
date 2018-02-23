24dada820c75c2ef8378959dabcc903d55622f9d
Victor Berchet <victor@suumit.com>
2018-02-23 18:13:39 +0000

    aad4316 refactor(ivy): rename componentRefresh to directiveRefresh (#22395)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-a81d599
+ * @license Angular v6.0.0-beta.5-aad4316
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.5-a81d599
+ * @license Angular v6.0.0-beta.5-aad4316
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-a81d599');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-aad4316');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-a81d599
+ * @license Angular v6.0.0-beta.5-aad4316
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
