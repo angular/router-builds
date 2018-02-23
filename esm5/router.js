fd0a1b4146f818c4a6e4b0824da5d5b7a84ce612
Victor Berchet <victor@suumit.com>
2018-02-23 19:54:10 +0000

    b43b164 feat(bazel): add an ng_package rule (#22221)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-1dcbc12
+ * @license Angular v6.0.0-beta.5-b43b164
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7470,7 +7470,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-1dcbc12');
+var VERSION = new Version('6.0.0-beta.5-b43b164');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-1dcbc12
+ * @license Angular v6.0.0-beta.5-b43b164
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
