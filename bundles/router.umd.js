069e48fcc2819df1820f746b6dd669fbf70c6b19
Alex Eagle <alexeagle@google.com>
2018-02-26 22:05:58 +0000

    25a4304 build(aio): group API members by type in overview (#22438)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-c593d69
+ * @license Angular v6.0.0-beta.5-25a4304
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.5-c593d69
+ * @license Angular v6.0.0-beta.5-25a4304
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-c593d69');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-25a4304');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-c593d69
+ * @license Angular v6.0.0-beta.5-25a4304
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6285,7 +6285,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.5-c593d69');
+const VERSION = new Version('6.0.0-beta.5-25a4304');
 
 /**
  * @fileoverview added by tsickle
