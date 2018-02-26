cf540d1bc0dd0519b1502d72172ddcb6b9aacd9c
Alex Eagle <alexeagle@google.com>
2018-02-26 23:20:35 +0000

    d3827a0 feat(platform-server): bump Domino to v2.0 (#22411)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-a7e1f23
+ * @license Angular v6.0.0-beta.5-d3827a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6285,7 +6285,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.5-a7e1f23');
+const VERSION = new Version('6.0.0-beta.5-d3827a0');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-a7e1f23
+ * @license Angular v6.0.0-beta.5-d3827a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
