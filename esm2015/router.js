4183da051b55f8f7ed4af26945d5ca9886ef1df6
Alex Eagle <alexeagle@google.com>
2018-02-26 21:57:33 +0000

    1f59981 build(aio): fix rendering of callable and constructable interface members (#22428)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-13ab91e
+ * @license Angular v6.0.0-beta.5-1f59981
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6285,7 +6285,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.5-13ab91e');
+const VERSION = new Version('6.0.0-beta.5-1f59981');
 
 /**
  * @fileoverview added by tsickle
