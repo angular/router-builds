cf859095b9d4a0ca386b7a2ca7b6d3bdc5a1f781
Alex Eagle <alexeagle@google.com>
2018-02-27 02:09:11 +0000

    40ba009 fix(platform-server): generate correct stylings for camel case names (#22263)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-d3827a0
+ * @license Angular v6.0.0-beta.5-40ba009
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6285,7 +6285,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.5-d3827a0');
+const VERSION = new Version('6.0.0-beta.5-40ba009');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-d3827a0
+ * @license Angular v6.0.0-beta.5-40ba009
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
