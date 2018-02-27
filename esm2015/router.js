442461e521bb5db19625ce7690c17ac9aeaf28ab
Alex Eagle <alexeagle@google.com>
2018-02-27 02:21:41 +0000

    e1fbe20 style(aio): updated padding-right for the .alert class in _heading-anchors.scss (#22431)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-d033106
+ * @license Angular v5.2.6-e1fbe20
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6257,7 +6257,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.6-d033106');
+const VERSION = new Version('5.2.6-e1fbe20');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-d033106
+ * @license Angular v5.2.6-e1fbe20
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
