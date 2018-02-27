fb6711bea78fed5aef606d9edd25f10bc22d9828
Alex Eagle <alexeagle@google.com>
2018-02-27 19:47:37 +0000

    f2fa7a2 docs(aio): add Observable and Rx docs (#21423)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-e1fbe20
+ * @license Angular v5.2.6-f2fa7a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.6-e1fbe20
+ * @license Angular v5.2.6-f2fa7a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7467,7 +7467,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.6-e1fbe20');
+var VERSION = new _angular_core.Version('5.2.6-f2fa7a2');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-e1fbe20
+ * @license Angular v5.2.6-f2fa7a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
