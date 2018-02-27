6c1f34a22ce75c34d934baff203a3924f7d87c10
Alex Eagle <alexeagle@google.com>
2018-02-27 02:12:58 +0000

    d033106 docs: update i18n guide for projects that don't use the cli (#21767)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-de02a7a
+ * @license Angular v5.2.6-d033106
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6257,7 +6257,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.6-de02a7a');
+const VERSION = new Version('5.2.6-d033106');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-de02a7a
+ * @license Angular v5.2.6-d033106
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
