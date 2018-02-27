18af61f37aff12888a2a34c7b19ab7ed92ff26fc
Alex Eagle <alexeagle@google.com>
2018-02-27 02:12:17 +0000

    ef99126 test(aio): move forgotten e2e test to the correct folder (#22445)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-40ba009
+ * @license Angular v6.0.0-beta.5-ef99126
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6285,7 +6285,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.5-40ba009');
+const VERSION = new Version('6.0.0-beta.5-ef99126');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-40ba009
+ * @license Angular v6.0.0-beta.5-ef99126
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
