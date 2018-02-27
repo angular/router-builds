5038f6593fd174007b2629156302dd105b603b18
Alex Eagle <alexeagle@google.com>
2018-02-27 23:43:39 +0000

    142117f fix(aio): do not show Properties/Methods sections if only internal members (#22471)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-79656e7
+ * @license Angular v6.0.0-beta.5-142117f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.5-79656e7
+ * @license Angular v6.0.0-beta.5-142117f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-79656e7');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-142117f');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-79656e7
+ * @license Angular v6.0.0-beta.5-142117f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
