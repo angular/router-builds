b6f535939a966106e6fea35eef0fc0fe7e8de149
Alex Eagle <alexeagle@google.com>
2018-02-28 22:11:28 +0000

    9eaf1bb feat(ivy): support injecting ChangeDetectorRef (#22469)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-aabe16c
+ * @license Angular v6.0.0-beta.5-9eaf1bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7470,7 +7470,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-aabe16c');
+var VERSION = new Version('6.0.0-beta.5-9eaf1bb');
 
 /**
  * @fileoverview added by tsickle
