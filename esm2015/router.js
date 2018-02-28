b2abd02ca86a1dc860bafe49d5abf32d1d566230
Alex Eagle <alexeagle@google.com>
2018-02-28 23:20:14 +0000

    288851c release: add ng update package group metadata to angular (#22482)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-9eaf1bb
+ * @license Angular v6.0.0-beta.5-288851c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6285,7 +6285,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.5-9eaf1bb');
+const VERSION = new Version('6.0.0-beta.5-288851c');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-9eaf1bb
+ * @license Angular v6.0.0-beta.5-288851c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
