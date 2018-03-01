83a702bf12d7330b5e0661483962ceaef80d9ef8
Alex Eagle <alexeagle@google.com>
2018-03-01 16:47:16 +0000

    8bb2f5c docs(compiler): ivy separate compilation design document (#22480)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-94707fe
+ * @license Angular v6.0.0-beta.6-8bb2f5c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6285,7 +6285,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.6-94707fe');
+const VERSION = new Version('6.0.0-beta.6-8bb2f5c');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-94707fe
+ * @license Angular v6.0.0-beta.6-8bb2f5c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
