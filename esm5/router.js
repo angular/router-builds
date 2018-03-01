db83d9f8dc6ae9806bd83543e437eefb032be06d
Alex Eagle <alexeagle@google.com>
2018-03-01 17:51:25 +0000

    ec445b5 ci: speed up lint job on CircleCI (#22526)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-ab790f3
+ * @license Angular v6.0.0-beta.6-ec445b5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7470,7 +7470,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.6-ab790f3');
+var VERSION = new Version('6.0.0-beta.6-ec445b5');
 
 /**
  * @fileoverview added by tsickle
