c14b8bdaeffef6bf98f881ae6208b787b0761fe5
Alex Eagle <alexeagle@google.com>
2018-03-01 16:35:15 +0000

    94707fe build(aio): initialise `exampleMap` correctly (#22502)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-b8fe121
+ * @license Angular v6.0.0-beta.6-94707fe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6285,7 +6285,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.6-b8fe121');
+const VERSION = new Version('6.0.0-beta.6-94707fe');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-b8fe121
+ * @license Angular v6.0.0-beta.6-94707fe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
