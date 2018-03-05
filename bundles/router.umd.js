8021923153b19450e1842e4e7c5860a3a1782df1
Alex Eagle <alexeagle@google.com>
2018-03-05 19:13:47 +0000

    58932c7 build(aio): indicate whether properties are read-only in API pages (#22584)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-5bb9f64
+ * @license Angular v6.0.0-beta.6-58932c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.6-5bb9f64
+ * @license Angular v6.0.0-beta.6-58932c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.6-5bb9f64');
+var VERSION = new _angular_core.Version('6.0.0-beta.6-58932c7');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-5bb9f64
+ * @license Angular v6.0.0-beta.6-58932c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6285,7 +6285,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.6-5bb9f64');
+const VERSION = new Version('6.0.0-beta.6-58932c7');
 
 /**
  * @fileoverview added by tsickle
