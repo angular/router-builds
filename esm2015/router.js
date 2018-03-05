6ff852ae5752179e7233e2cb6863f85a60a8a9c8
Alex Eagle <alexeagle@google.com>
2018-03-05 18:33:18 +0000

    51ca643 test(ivy): add injectAttribute spec (#22510)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-4f60968
+ * @license Angular v6.0.0-beta.6-51ca643
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6285,7 +6285,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.6-4f60968');
+const VERSION = new Version('6.0.0-beta.6-51ca643');
 
 /**
  * @fileoverview added by tsickle
