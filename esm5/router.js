272e2e4d5c7dee012009c04c5097468a8eecf2d7
Alex Eagle <alexeagle@google.com>
2018-03-06 05:43:50 +0000

    2a1e3d1 docs: add info about the WebStorm plugin to BAZEL.md (#22572) PR Close #22572

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-2c2b62f
+ * @license Angular v6.0.0-beta.6-2a1e3d1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7470,7 +7470,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.6-2c2b62f');
+var VERSION = new Version('6.0.0-beta.6-2a1e3d1');
 
 /**
  * @fileoverview added by tsickle
