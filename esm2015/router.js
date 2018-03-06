f2967ff79f9a56a5cb46a37d4c7e0d9249a3fe3b
Alex Eagle <alexeagle@google.com>
2018-03-06 23:26:10 +0000

    505ae75 docs(aio): update deprecated Http reference to HttpClientModule, remove Http reference because another context is used (#21984)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-b3d1761
+ * @license Angular v6.0.0-beta.6-505ae75
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6318,7 +6318,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.6-b3d1761');
+const VERSION = new Version('6.0.0-beta.6-505ae75');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-b3d1761
+ * @license Angular v6.0.0-beta.6-505ae75
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
