460232ecfe59ae86b5385963e96abbd8413c633d
Alex Eagle <alexeagle@google.com>
2018-03-06 23:26:17 +0000

    363dfa5 test(ivy): Back patch example (#22235)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-505ae75
+ * @license Angular v6.0.0-beta.6-363dfa5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6318,7 +6318,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.6-505ae75');
+const VERSION = new Version('6.0.0-beta.6-363dfa5');
 
 /**
  * @fileoverview added by tsickle
