530703296e52cf914c3209cdcf684c022a79ad38
Alex Eagle <alexeagle@google.com>
2018-03-07 22:56:51 +0000

    7816791 build(common): export locale data as commonjs instead of es2015 (#20624)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-4f744cc
+ * @license Angular v6.0.0-beta.6-7816791
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7503,7 +7503,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.6-4f744cc');
+var VERSION = new Version('6.0.0-beta.6-7816791');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-4f744cc
+ * @license Angular v6.0.0-beta.6-7816791
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
