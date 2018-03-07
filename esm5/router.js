b27f68581eb176218e878db29f8a1f15aca1e4dd
Alex Eagle <alexeagle@google.com>
2018-03-07 19:18:43 +0000

    4f744cc docs: update RELEASE_SCHEDULE.md by pushing out v6 rc by one week

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-363dfa5
+ * @license Angular v6.0.0-beta.6-4f744cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7503,7 +7503,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.6-363dfa5');
+var VERSION = new Version('6.0.0-beta.6-4f744cc');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-363dfa5
+ * @license Angular v6.0.0-beta.6-4f744cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
