3ce6ab4f6f854f5da120342aec7cb6e6ed5a638b
Alex Eagle <alexeagle@google.com>
2018-03-07 22:59:32 +0000

    ad8fb84 test: include router in public_api_guard tests (#22628)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-7816791
+ * @license Angular v6.0.0-beta.6-ad8fb84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.6-7816791
+ * @license Angular v6.0.0-beta.6-ad8fb84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7525,7 +7525,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.6-7816791');
+var VERSION = new _angular_core.Version('6.0.0-beta.6-ad8fb84');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-7816791
+ * @license Angular v6.0.0-beta.6-ad8fb84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
