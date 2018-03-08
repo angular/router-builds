5752516c7708e8160f675f9346e3039c590ffdc9
Kara Erickson <karakara@google.com>
2018-03-08 05:39:32 +0000

    4c089c1 feat(ivy): support ChangeDetectorRef.detectChanges (#22614)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-8407fcc
+ * @license Angular v6.0.0-beta.7-4c089c1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6318,7 +6318,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-8407fcc');
+const VERSION = new Version('6.0.0-beta.7-4c089c1');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-8407fcc
+ * @license Angular v6.0.0-beta.7-4c089c1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
