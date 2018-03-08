2041add18fee23c0852ba5976582eddb88d0f519
Kara Erickson <karakara@google.com>
2018-03-08 19:19:31 +0000

    c499c8f build: update angular-bot g3sync paths (#22637)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-4c089c1
+ * @license Angular v6.0.0-beta.7-c499c8f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7503,7 +7503,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-4c089c1');
+var VERSION = new Version('6.0.0-beta.7-c499c8f');
 
 /**
  * @fileoverview added by tsickle
