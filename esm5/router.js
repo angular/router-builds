b72a4fd5e31e4fc5c9a999db02296ebc36256a99
Kara Erickson <karakara@google.com>
2018-03-08 02:43:29 +0000

    3112320 docs: add HeroService to code tabs and fix headers (#22373)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-4aeb04d
+ * @license Angular v5.2.8-3112320
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7478,7 +7478,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.7-4aeb04d');
+var VERSION = new Version('5.2.8-3112320');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-4aeb04d
+ * @license Angular v5.2.8-3112320
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
