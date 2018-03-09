33a9e97b7a25b8774298b471462747571e8b7942
Kara Erickson <karakara@google.com>
2018-03-09 21:28:48 +0000

    e8326e6 fix: overloading a function doesn't generate all of the signatures (#22569)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-b5be18f
+ * @license Angular v6.0.0-beta.7-e8326e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6318,7 +6318,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-b5be18f');
+const VERSION = new Version('6.0.0-beta.7-e8326e6');
 
 /**
  * @fileoverview added by tsickle
