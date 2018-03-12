68052c8868e97eaf4c805d0439b803ba5061a7f3
Kara Erickson <karakara@google.com>
2018-03-12 20:39:34 +0000

    f95730b fix(ivy): elements properties should not be stringified (#22683)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-cd58c0a
+ * @license Angular v6.0.0-beta.7-f95730b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7508,7 +7508,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-cd58c0a');
+var VERSION = new Version('6.0.0-beta.7-f95730b');
 
 /**
  * @fileoverview added by tsickle
