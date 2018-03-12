e149d805a2dfdf6c9def65009bb0041ccb470ec4
Kara Erickson <karakara@google.com>
2018-03-12 16:45:32 +0000

    0bf6fa5 fix(router): correct over-encoding of URL fragment (#22687)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-aa7dba2
+ * @license Angular v6.0.0-beta.7-0bf6fa5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7508,7 +7508,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-aa7dba2');
+var VERSION = new Version('6.0.0-beta.7-0bf6fa5');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-aa7dba2
+ * @license Angular v6.0.0-beta.7-0bf6fa5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
