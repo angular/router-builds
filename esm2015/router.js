32f3da41b167f0ef5b8d1efcb009a61d1a340482
Kara Erickson <karakara@google.com>
2018-03-12 05:41:15 +0000

    40315be fix(compiler-cli): enableResourceInlining handles both styles and styleUrls (#22688)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-fa451bc
+ * @license Angular v6.0.0-beta.7-40315be
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6318,7 +6318,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-fa451bc');
+const VERSION = new Version('6.0.0-beta.7-40315be');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-fa451bc
+ * @license Angular v6.0.0-beta.7-40315be
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
