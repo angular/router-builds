caf3c392df273a8ec91b4a804b04b40a278fb68b
Kara Erickson <karakara@google.com>
2018-03-12 22:34:55 +0000

    f5d75d8 build: fix postinstall for windows (#22720)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-6e00410
+ * @license Angular v6.0.0-beta.7-f5d75d8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7508,7 +7508,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-6e00410');
+var VERSION = new Version('6.0.0-beta.7-f5d75d8');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-6e00410
+ * @license Angular v6.0.0-beta.7-f5d75d8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
