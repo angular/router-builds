add5b0adbb5ad17dba9361be7b7cfb7c20dca28a
Kara Erickson <karakara@google.com>
2018-03-14 06:57:56 +0000

    b1365d1 refactor(ivy): remove directiveRefresh instruction (#22745)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-4ac606b
+ * @license Angular v6.0.0-beta.7-b1365d1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7508,7 +7508,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-4ac606b');
+var VERSION = new Version('6.0.0-beta.7-b1365d1');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-4ac606b
+ * @license Angular v6.0.0-beta.7-b1365d1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
