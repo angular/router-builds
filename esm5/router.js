e74280e48f57b35c00d9fb6cc88350653e13a572
Kara Erickson <karakara@google.com>
2018-03-14 03:27:18 +0000

    51027d7 fix(ivy): Update rollup rule to prevent inlining symbols in debug. (#22747)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-48636f3
+ * @license Angular v6.0.0-beta.7-51027d7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7508,7 +7508,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-48636f3');
+var VERSION = new Version('6.0.0-beta.7-51027d7');
 
 /**
  * @fileoverview added by tsickle
