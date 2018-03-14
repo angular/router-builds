d3dbd4512cc9c135eaa74f7b21e629b141636216
Kara Erickson <karakara@google.com>
2018-03-14 18:18:46 +0000

    049757b fix(aio): constrain error logging to improve reporting (#22713)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-1f97343
+ * @license Angular v6.0.0-beta.7-049757b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6323,7 +6323,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-1f97343');
+const VERSION = new Version('6.0.0-beta.7-049757b');
 
 /**
  * @fileoverview added by tsickle
