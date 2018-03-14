1d9c7dd3dadff45731cc4f94a27638614cf3692d
Kara Erickson <karakara@google.com>
2018-03-14 21:37:17 +0000

    cd2ebd2 fix(platform-server): add styles to elements correctly (#22527)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-112431d
+ * @license Angular v6.0.0-beta.7-cd2ebd2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6323,7 +6323,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-112431d');
+const VERSION = new Version('6.0.0-beta.7-cd2ebd2');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-112431d
+ * @license Angular v6.0.0-beta.7-cd2ebd2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
