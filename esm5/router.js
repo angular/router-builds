e90715a5c6003157c7e19d89ec0dc77fa21dae4c
Kara Erickson <karakara@google.com>
2018-03-14 19:31:37 +0000

    c09bd67 fix(ivy): fix views manipulation logic (#22656)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-9df13ad
+ * @license Angular v6.0.0-beta.7-c09bd67
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7508,7 +7508,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-9df13ad');
+var VERSION = new Version('6.0.0-beta.7-c09bd67');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-9df13ad
+ * @license Angular v6.0.0-beta.7-c09bd67
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
