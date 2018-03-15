b8d6106a636bbeda581ec0a0b56226abe70f082d
Miško Hevery <misko@hevery.com>
2018-03-15 19:18:46 +0000

    88b3198 feat(bazel): change ng_package rule to APF v6 (#22782)

--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-6e5e819
+ * @license Angular v6.0.0-beta.7-88b3198
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7508,7 +7508,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-6e5e819');
+var VERSION = new Version('6.0.0-beta.7-88b3198');
 
 /**
  * @fileoverview added by tsickle
