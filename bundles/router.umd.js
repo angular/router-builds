b2fbdebb998369e2b413641b2ce3d4d6cdd47041
Miško Hevery <misko@hevery.com>
2018-03-15 19:03:56 +0000

    db0afa9 fix(compiler-cli): emit correct css string escape sequences (#22776)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-5298b2b
+ * @license Angular v5.2.9-db0afa9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.9-5298b2b
+ * @license Angular v5.2.9-db0afa9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7505,7 +7505,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.9-5298b2b');
+var VERSION = new _angular_core.Version('5.2.9-db0afa9');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-5298b2b
+ * @license Angular v5.2.9-db0afa9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
