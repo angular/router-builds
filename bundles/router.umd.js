d2fd2befd944518d570f332082077aeaa2ad75e0
Miško Hevery <misko@hevery.com>
2018-03-16 04:43:04 +0000

    269c3a1 build(compiler-cli): fix tsconfig.json circularity issue (#22722)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-b149424
+ * @license Angular v6.0.0-beta.7-269c3a1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.7-b149424
+ * @license Angular v6.0.0-beta.7-269c3a1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7536,7 +7536,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-b149424');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-269c3a1');
 
 exports.RouterLink = RouterLink;
 exports.RouterLinkWithHref = RouterLinkWithHref;
