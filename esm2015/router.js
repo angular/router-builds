6235e0d1298e759f47bd4cc881b856feb0ad9931
Miško Hevery <misko@hevery.com>
2018-03-16 18:51:32 +0000

    cedc04c docs(ivy): document project status (#22751)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-269c3a1
+ * @license Angular v6.0.0-beta.7-cedc04c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6329,7 +6329,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-269c3a1');
+const VERSION = new Version('6.0.0-beta.7-cedc04c');
 
 /**
  * @fileoverview added by tsickle
--- esm5/router.js
+++ esm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-269c3a1
+ * @license Angular v6.0.0-beta.7-cedc04c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7514,7 +7514,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-269c3a1');
+var VERSION = new Version('6.0.0-beta.7-cedc04c');
 
 /**
  * @fileoverview added by tsickle
