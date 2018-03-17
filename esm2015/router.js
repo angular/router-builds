e9bc7023d225f5abcf28c9900574a3ad34e11414
Miško Hevery <misko@hevery.com>
2018-03-17 05:40:49 +0000

    5d82d8d fix(elements): fix elements test bootstrap (#22839)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-6ef9f22
+ * @license Angular v6.0.0-beta.7-5d82d8d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6329,7 +6329,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-6ef9f22');
+const VERSION = new Version('6.0.0-beta.7-5d82d8d');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-6ef9f22
+ * @license Angular v6.0.0-beta.7-5d82d8d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
