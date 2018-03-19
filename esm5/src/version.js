543f4a9002a16993ec1c35f8094217734d2a9cac
Miško Hevery <misko@hevery.com>
2018-03-19 23:26:56 +0000

    5c387a7 fix(compiler): do not emit line/char in ngsummary files. (#22840)

--- esm5/src/version.js
+++ esm5/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * \@stable
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-beta.7-fc50c77');
+export var /** @type {?} */ VERSION = new Version('6.0.0-beta.7-5c387a7');
 //# sourceMappingURL=version.js.map
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-fc50c77
+ * @license Angular v6.0.0-beta.7-5c387a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7514,7 +7514,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-fc50c77');
+var VERSION = new Version('6.0.0-beta.7-5c387a7');
 
 /**
  * @fileoverview added by tsickle
