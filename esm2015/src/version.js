b89429bc23d8edde72baea0099a48c04c168fae9
Igor Minar <igor@angular.io>
2018-03-21 21:14:17 +0000

    f461f43 docs: fix a typo in aot compiler guide (#22876)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * \@stable
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-64efcf1');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-f461f43');
 //# sourceMappingURL=version.js.map
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-64efcf1
+ * @license Angular v6.0.0-rc.0-f461f43
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7506,7 +7506,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-64efcf1');
+var VERSION = new Version('6.0.0-rc.0-f461f43');
 
 /**
  * @fileoverview added by tsickle
