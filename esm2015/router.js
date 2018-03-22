a390b52f232018d470438a9c88698376e43fb38e
Matias Niemelä <matsko@matskocloud.c.googlers.com>
2018-03-22 22:43:55 +0000

    49f6d1d build: rm --noimplicit_deps from bazel query (#22912)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-641cc49
+ * @license Angular v5.2.9-49f6d1d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6301,7 +6301,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.9-641cc49');
+const VERSION = new Version('5.2.9-49f6d1d');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-641cc49
+ * @license Angular v5.2.9-49f6d1d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
