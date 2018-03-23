39349b19ac9883e0111083f184b93587eafcb126
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 17:38:32 +0000

    bcaa07b refactor(compiler): move handling of translations to the ConstantPool (#22942)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * \@stable
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-16f021c');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-bcaa07b');
 //# sourceMappingURL=version.js.map
--- esm5/src/version.js
+++ esm5/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * \@stable
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.0-16f021c');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.0-bcaa07b');
 //# sourceMappingURL=version.js.map
