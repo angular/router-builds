6458dee7e3085a0f2b5ed4704500c7830cf2ad6f
Matias Niemelä <matias@yearofmoo.com>
2018-03-27 19:34:47 +0000

    776bb82 build: update browserstack key (#23026)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-d7f4aa6
+ * @license Angular v5.2.9-776bb82
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6301,7 +6301,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.9-d7f4aa6');
+const VERSION = new Version('5.2.9-776bb82');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-d7f4aa6
+ * @license Angular v5.2.9-776bb82
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
