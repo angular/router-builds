cdb81848456aa8dceab272800e15651ba66365bf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-29 16:54:57 +0000

    de0b13d docs: update for hero service name change (#22920)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * \@stable
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-28058b7');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-de0b13d');
 //# sourceMappingURL=version.js.map
