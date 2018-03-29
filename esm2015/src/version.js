b8aafe9431c94d656aa78674987665055b6fde08
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-29 16:27:51 +0000

    28058b7 fix(compiler): fix support for html-like text in translatable attributes (#23053)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * \@stable
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-0049743');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.0-28058b7');
 //# sourceMappingURL=version.js.map
