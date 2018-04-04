8c598d3a5e034b693384d1ca159f543d08dd6569
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 15:46:30 +0000

    dd61595 fix(forms): improve error message for invalid value accessors (#22731)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-ae76eec
+ * @license Angular v5.2.9-dd61595
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6301,7 +6301,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.9-ae76eec');
+const VERSION = new Version('5.2.9-dd61595');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-ae76eec
+ * @license Angular v5.2.9-dd61595
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
