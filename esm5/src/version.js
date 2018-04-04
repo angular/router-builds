e39bc21d69466baf063b5c836980a532d6c8d36b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 17:22:48 +0000

    f99cb5c fix(compiler-cli): flat module index metadata should be transformed (#23129)

--- esm5/src/version.js
+++ esm5/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  * \@stable
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.1-5c8340a');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.1-f99cb5c');
 //# sourceMappingURL=version.js.map
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-5c8340a
+ * @license Angular v6.0.0-rc.1-f99cb5c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
