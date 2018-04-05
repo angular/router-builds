03d3f1339173b3cc712cf3e651a16f95a56ff133
Igor Minar <igor@angular.io>
2018-04-05 19:24:38 +0000

    f3d2623 docs: add changelog for 6.0.0-rc.2

---
+++ esm2015/src/version.js
@@ -0,0 +1,17 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { Version } from '@angular/core';
+/**
+ * \@stable
+ */
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.2-f3d2623');
+//# sourceMappingURL=version.js.map
---
+++ testing.d.ts
@@ -0,0 +1,6 @@
+/**
+ * @license Angular v6.0.0-rc.2-f3d2623
+ * (c) 2010-2018 Google, Inc. https://angular.io/
+ * License: MIT
+ */ 
+ export * from './testing/testing'
