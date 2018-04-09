a45a0aeb5317c64af172ba02279ab808ad8f5b34
Igor Minar <igor@angular.io>
2018-04-09 23:37:54 +0000

    bb3f0e5 feat(ivy): support projection of ViewContainerRef (#23272)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-37c1634
+ * @license Angular v6.0.0-rc.3-bb3f0e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7554,7 +7554,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.3-37c1634');
+var VERSION = new Version('6.0.0-rc.3-bb3f0e5');
 
 /**
  * @fileoverview added by tsickle
