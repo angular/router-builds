d60d07a352dd4a19335cfc1b10f5973987063795
Igor Minar <igor@angular.io>
2018-04-09 23:36:39 +0000

    37c1634 build: Add sourcemap output from ng_rollup_bundle (#23276)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-5814355
+ * @license Angular v6.0.0-rc.3-37c1634
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6375,7 +6375,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-rc.3-5814355');
+const VERSION = new Version('6.0.0-rc.3-37c1634');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-5814355
+ * @license Angular v6.0.0-rc.3-37c1634
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
