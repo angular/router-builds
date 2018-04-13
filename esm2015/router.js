913c6a4d0a10030a4b9cdf1c60e15a303142618c
Igor Minar <igor@angular.io>
2018-04-13 06:57:48 +0000

    47229fa docs(aio): update text InMemoryWebApiModule to HttpClientInMemoryWebApiModule (#23285)

--- esm2015/router.js
+++ esm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-dabc076
+ * @license Angular v5.2.9-47229fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6301,7 +6301,7 @@ function provideRouterInitializer() {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.9-dabc076');
+const VERSION = new Version('5.2.9-47229fa');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-dabc076
+ * @license Angular v5.2.9-47229fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
