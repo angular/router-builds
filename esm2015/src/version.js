476d784ace7f091784506bb2708f83eb38dca4ca
Igor Minar <igor@angular.io>
2018-04-13 07:48:31 +0000

    2bb7838 fix(ivy): support ViewContainerRef on nodes projected into an embedded view (#23333)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.4-6199ea5');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.4-2bb7838');
 //# sourceMappingURL=version.js.map
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-6199ea5
+ * @license Angular v6.0.0-rc.4-2bb7838
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7554,7 +7554,7 @@ function provideRouterInitializer() {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-6199ea5');
+var VERSION = new Version('6.0.0-rc.4-2bb7838');
 
 /**
  * @fileoverview added by tsickle
