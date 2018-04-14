f31a796f09d5415172ad0a15b53e4c5f72dad0c4
Igor Minar <igor@angular.io>
2018-04-14 11:12:47 +0000

    815ae29 build: add '@angular/elements' the framework package group

--- esm5/src/version.js
+++ esm5/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.5-8f690c9');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.5-815ae29');
 //# sourceMappingURL=version.js.map
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-8f690c9
+ * @license Angular v6.0.0-rc.5-815ae29
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6375,7 +6375,7 @@ function provideRouterInitializer() {
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5-8f690c9');
+const VERSION = new Version('6.0.0-rc.5-815ae29');
 
 /**
  * @fileoverview added by tsickle
