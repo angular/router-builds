89be65f26fa6b38925f0d6156d85d4b704bb519b
Igor Minar <igor@angular.io>
2018-04-16 16:22:31 +0000

    9c29127 docs: document how to enable service worker using ng add command (#23348)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-4361560
+ * @license Angular v6.0.0-rc.5-9c29127
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6375,7 +6375,7 @@ function provideRouterInitializer() {
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5-4361560');
+const VERSION = new Version('6.0.0-rc.5-9c29127');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-4361560
+ * @license Angular v6.0.0-rc.5-9c29127
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
