85e6fbab1acff3812f24715bd4872556416e4f75
Victor Berchet <victor@suumit.com>
2018-04-20 23:34:31 +0000

    4662878 refactor(compiler): refactor template binding parsing (#23460)

--- esm5/src/version.js
+++ esm5/src/version.js
@@ -13,5 +13,5 @@ import { Version } from '@angular/core';
 /**
  *
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-rc.5-ca776c5');
+export var /** @type {?} */ VERSION = new Version('6.0.0-rc.5-4662878');
 //# sourceMappingURL=version.js.map
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-ca776c5
+ * @license Angular v6.0.0-rc.5-4662878
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
