23626c5024ffb8f54c9752abfe44c109c67392ae
Victor Berchet <victor@suumit.com>
2018-04-24 00:33:58 +0000

    1c9200e ci: require green integration tests to publish snapshot (#23517)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+73.sha-ace6440
+ * @license Angular v6.0.0-rc.5+74.sha-1c9200e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+73.sha-ace6440
+ * @license Angular v6.0.0-rc.5+74.sha-1c9200e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6379,7 +6379,7 @@ function provideRouterInitializer() {
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+73.sha-ace6440');
+const VERSION = new Version('6.0.0-rc.5+74.sha-1c9200e');
 
 /**
  * @fileoverview added by tsickle
