5684c2d53ec2c8147da3fc66ffa2bf3cdc2c7f02
Victor Berchet <victor@suumit.com>
2018-04-24 22:05:26 +0000

    43a49d3 docs: fix typo (#23514)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+80.sha-811a7f2
+ * @license Angular v6.0.0-rc.5+81.sha-43a49d3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+80.sha-811a7f2
+ * @license Angular v6.0.0-rc.5+81.sha-43a49d3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5536,7 +5536,7 @@ function provideRouterInitializer() {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5+80.sha-811a7f2');
+var VERSION = new Version('6.0.0-rc.5+81.sha-43a49d3');
 
 /**
  * @license
