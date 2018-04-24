25c532e7810cca1fe63d33dacdd203199532ab56
Victor Berchet <victor@suumit.com>
2018-04-24 22:01:54 +0000

    811a7f2 docs(benchpress): fix typo in README (#23471) (#23488)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+79.sha-9ed5fb6
+ * @license Angular v6.0.0-rc.5+80.sha-811a7f2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6379,7 +6379,7 @@ function provideRouterInitializer() {
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+79.sha-9ed5fb6');
+const VERSION = new Version('6.0.0-rc.5+80.sha-811a7f2');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+79.sha-9ed5fb6
+ * @license Angular v6.0.0-rc.5+80.sha-811a7f2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
