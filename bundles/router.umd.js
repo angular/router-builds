abaa2427d38034d006f66de8ddc4512bc04b1c0f
Victor Berchet <victor@suumit.com>
2018-04-24 22:18:07 +0000

    e1c4930 fix(compiler): avoid a crash in ngc-wrapped. (#23468)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+76.sha-b1d03fe
+ * @license Angular v6.0.0-rc.5+78.sha-e1c4930
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5617,7 +5617,7 @@ function provideRouterInitializer() {
 /**
  *
  */
-var VERSION = new core.Version('6.0.0-rc.5+76.sha-b1d03fe');
+var VERSION = new core.Version('6.0.0-rc.5+78.sha-e1c4930');
 
 /**
  * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+76.sha-b1d03fe
+ * @license Angular v6.0.0-rc.5+78.sha-e1c4930
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6379,7 +6379,7 @@ function provideRouterInitializer() {
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+76.sha-b1d03fe');
+const VERSION = new Version('6.0.0-rc.5+78.sha-e1c4930');
 
 /**
  * @fileoverview added by tsickle
