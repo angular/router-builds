777c8ba4e58c2bae28c4885e30a98750c99a64ab
Igor Minar <igor@angular.io>
2018-05-03 23:02:41 +0000

    d8d4f65 build: update the scripts/release/post-check script for 6.0.x

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+125.sha-2c4850d
+ * @license Angular v6.0.0-rc.5+126.sha-d8d4f65
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+125.sha-2c4850d
+ * @license Angular v6.0.0-rc.5+126.sha-d8d4f65
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5617,7 +5617,7 @@ function provideRouterInitializer() {
 /**
  *
  */
-var VERSION = new core.Version('6.0.0-rc.5+125.sha-2c4850d');
+var VERSION = new core.Version('6.0.0-rc.5+126.sha-d8d4f65');
 
 /**
  * @license
