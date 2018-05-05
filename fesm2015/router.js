0072da0a67e410ab842123645956e76f20db97d3
Igor Minar <igor@angular.io>
2018-05-05 15:39:26 +0000

    e351896 style(animations): fix short param names (#23668)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+136.sha-7e9649b
+ * @license Angular v6.0.0-rc.5+138.sha-e351896
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6379,7 +6379,7 @@ function provideRouterInitializer() {
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+136.sha-7e9649b');
+const VERSION = new Version('6.0.0-rc.5+138.sha-e351896');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+136.sha-7e9649b
+ * @license Angular v6.0.0-rc.5+138.sha-e351896
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
