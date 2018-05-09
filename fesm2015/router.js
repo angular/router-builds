01224029a9b9f86f4b0b638911795bee750415f1
Igor Minar <igor@angular.io>
2018-05-09 19:18:50 +0000

    971e78d ci: Remove Chuck from pullapprove (#23798)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+144.sha-b0eca85
+ * @license Angular v6.0.0-rc.5+145.sha-971e78d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6379,7 +6379,7 @@ function provideRouterInitializer() {
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+144.sha-b0eca85');
+const VERSION = new Version('6.0.0-rc.5+145.sha-971e78d');
 
 /**
  * @fileoverview added by tsickle
