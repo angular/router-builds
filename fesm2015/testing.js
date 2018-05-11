9bdaf3377c42de89f6bb77564d4df429d4ab4401
Matias Niemelä <matias@yearofmoo.com>
2018-05-11 16:45:07 +0000

    14d4625 docs: update version to 6 in language-service (#20795)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+51.sha-fd880a8
+ * @license Angular v6.0.0+52.sha-14d4625
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+51.sha-fd880a8
+ * @license Angular v6.0.0+52.sha-14d4625
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5533,7 +5533,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.0.0+51.sha-fd880a8');
+var VERSION = new Version('6.0.0+52.sha-14d4625');
 
 /**
  * @license
