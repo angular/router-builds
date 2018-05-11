aeb953539775c5643e27908a40414ab0a56ee999
Igor Minar <igor@angular.io>
2018-05-11 06:02:17 +0000

    5e307d5 release: cut the v6.0.1 release

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+165.sha-089fe83
+ * @license Angular v6.0.0-rc.5+166.sha-5e307d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+165.sha-089fe83
+ * @license Angular v6.0.0-rc.5+166.sha-5e307d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5614,7 +5614,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new core.Version('6.0.0-rc.5+165.sha-089fe83');
+var VERSION = new core.Version('6.0.0-rc.5+166.sha-5e307d5');
 
 /**
  * @license
