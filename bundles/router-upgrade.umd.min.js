90aa8bf1a9fd92304fe86c25ab7342186d50789e
Matias Niemelä <matias@yearofmoo.com>
2018-05-11 22:35:45 +0000

    d4b8b24 fix(elements): prevent closure renaming of platform properties (#23843)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+174.sha-cfde36d
+ * @license Angular v6.0.0-rc.5+175.sha-d4b8b24
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+174.sha-cfde36d
+ * @license Angular v6.0.0-rc.5+175.sha-d4b8b24
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6376,7 +6376,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.0-rc.5+174.sha-cfde36d');
+const VERSION = new Version('6.0.0-rc.5+175.sha-d4b8b24');
 
 /**
  * @fileoverview added by tsickle
