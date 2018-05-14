f75f9e0d7e0559d50e07cbca2fb2d16a013eff8f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-14 18:01:11 +0000

    a2e8b3a build(aio): ensure usageNotes are copied into decorator API docs (#23901)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+177.sha-02acb5e
+ * @license Angular v6.0.0-rc.5+176.sha-a2e8b3a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+177.sha-02acb5e
+ * @license Angular v6.0.0-rc.5+176.sha-a2e8b3a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5533,7 +5533,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.0.0-rc.5+177.sha-02acb5e');
+var VERSION = new Version('6.0.0-rc.5+176.sha-a2e8b3a');
 
 /**
  * @license
