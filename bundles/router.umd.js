8f683d36d0a70fbacb8a4f6bac0832a1be8684b5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-14 20:06:53 +0000

    66b2d78 build: only match version tags for BUILD_SCM_VERSION (#23903)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+178.sha-67b8d57
+ * @license Angular v6.0.0-rc.5+179.sha-66b2d78
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5614,7 +5614,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new core.Version('6.0.0-rc.5+178.sha-67b8d57');
+var VERSION = new core.Version('6.0.0-rc.5+179.sha-66b2d78');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+178.sha-67b8d57
+ * @license Angular v6.0.0-rc.5+179.sha-66b2d78
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
