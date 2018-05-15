c990fe1e6c5e1752c02fc8137e98ddd2800431f8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-15 20:09:49 +0000

    a8f6542 release: cut the v6.0.2 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.1+12.sha-c6b618d
+ * @license Angular v6.0.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6376,7 +6376,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.1+12.sha-c6b618d');
+const VERSION = new Version('6.0.2');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.1+12.sha-c6b618d
+ * @license Angular v6.0.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
