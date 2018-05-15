94dc063c3fe820b5ab002977ab73eabd41a1b739
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-15 22:59:47 +0000

    95fca24 feat(aio): add brand and concept icons, img style class more flexible (#23589)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2
+ * @license Angular v6.0.2+1.sha-95fca24
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2
+ * @license Angular v6.0.2+1.sha-95fca24
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5614,7 +5614,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new core.Version('6.0.2');
+var VERSION = new core.Version('6.0.2+1.sha-95fca24');
 
 /**
  * @license
