ff1f75f32dc0260aaa23b218271799c80b97e3f1
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:01:35 +0000

    547efb5 docs(aio): fix path to observables guide (#23858) PR Close #23858

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+190.sha-091b11a
+ * @license Angular v6.0.0-rc.5+191.sha-547efb5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5616,7 +5616,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new core.Version('6.0.0-rc.5+190.sha-091b11a');
+var VERSION = new core.Version('6.0.0-rc.5+191.sha-547efb5');
 
 /**
  * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+190.sha-091b11a
+ * @license Angular v6.0.0-rc.5+191.sha-547efb5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
