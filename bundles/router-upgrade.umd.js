e237a7b6e584987cae637cac13a5e9979bf5c8c9
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:12:50 +0000

    20d7637 docs(aio): Expose server and CLI configuration for universal in guide (#23842)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+194.sha-8ee25e6
+ * @license Angular v6.0.0-rc.5+195.sha-20d7637
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+194.sha-8ee25e6
+ * @license Angular v6.0.0-rc.5+195.sha-20d7637
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6376,7 +6376,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.0-rc.5+194.sha-8ee25e6');
+const VERSION = new Version('6.0.0-rc.5+195.sha-20d7637');
 
 /**
  * @fileoverview added by tsickle
