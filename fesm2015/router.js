988b28b9b50f0a97896da260ff39fa3d06027777
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:03:47 +0000

    f3361ab test: don't run unit tests on Firefox (#23942)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+5.sha-41c2030
+ * @license Angular v6.0.2+7.sha-f3361ab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6376,7 +6376,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.2+5.sha-41c2030');
+const VERSION = new Version('6.0.2+7.sha-f3361ab');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+5.sha-41c2030
+ * @license Angular v6.0.2+7.sha-f3361ab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
