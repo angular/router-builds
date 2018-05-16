e4a82f782c73c7b69fcfd1fa7e773d0a804e457a
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:14:26 +0000

    43ee10f docs(aio): changed 'onVoted' output property to 'voted' to be in line with the styleguide (#23832)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+8.sha-5d318ff
+ * @license Angular v6.0.2+10.sha-43ee10f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5614,7 +5614,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new core.Version('6.0.2+8.sha-5d318ff');
+var VERSION = new core.Version('6.0.2+10.sha-43ee10f');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+8.sha-5d318ff
+ * @license Angular v6.0.2+10.sha-43ee10f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
