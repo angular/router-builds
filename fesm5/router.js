ebe202638d4d5c7dca5e8500d428514f95dfa4e9
Matias Niemelä <matias@yearofmoo.com>
2018-05-21 20:46:27 +0000

    95344d6 fix(aio): avoid unnecessary re-calculations in live-examples (#23960)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+17.sha-eec231d
+ * @license Angular v6.0.2+16.sha-95344d6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5533,7 +5533,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.0.2+17.sha-eec231d');
+var VERSION = new Version('6.0.2+16.sha-95344d6');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+17.sha-eec231d
+ * @license Angular v6.0.2+16.sha-95344d6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
