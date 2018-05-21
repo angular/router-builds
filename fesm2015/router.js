b63190b378414c0a203160ba772ec834e2d8e870
Matias Niemelä <matias@yearofmoo.com>
2018-05-21 20:43:50 +0000

    de267e9 docs(aio): add ant design of angular in resources (#23953)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+203.sha-f8c6947
+ * @license Angular v6.0.0-rc.5+204.sha-de267e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6376,7 +6376,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.0-rc.5+203.sha-f8c6947');
+const VERSION = new Version('6.0.0-rc.5+204.sha-de267e9');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+203.sha-f8c6947
+ * @license Angular v6.0.0-rc.5+204.sha-de267e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
