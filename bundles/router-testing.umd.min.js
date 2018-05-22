832d3a1f71a31aed35d30a14c029ac53ce1d7a86
Matias Niemelä <matias@yearofmoo.com>
2018-05-22 21:00:07 +0000

    7fee1fd docs(aio): fix typo (#23925)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+21.sha-0ee5b7e
+ * @license Angular v6.0.2+22.sha-7fee1fd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+21.sha-0ee5b7e
+ * @license Angular v6.0.2+22.sha-7fee1fd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6376,7 +6376,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.2+21.sha-0ee5b7e');
+const VERSION = new Version('6.0.2+22.sha-7fee1fd');
 
 /**
  * @fileoverview added by tsickle
