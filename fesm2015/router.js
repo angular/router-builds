b882452897f55bad2e5c2ce5f9fa9a3fced23d3e
Matias Niemelä <matias@yearofmoo.com>
2018-05-22 18:00:26 +0000

    23f2a70 docs: fix typo (#23998)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+20.sha-afff84c
+ * @license Angular v6.0.2+19.sha-23f2a70
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6376,7 +6376,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.2+20.sha-afff84c');
+const VERSION = new Version('6.0.2+19.sha-23f2a70');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+20.sha-afff84c
+ * @license Angular v6.0.2+19.sha-23f2a70
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
