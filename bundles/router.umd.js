9b7f947da0553506ea8b4585d75623007b582b5f
Matias Niemelä <matias@yearofmoo.com>
2018-05-22 19:32:05 +0000

    0bdd30e fix(service-worker): check platformBrowser before accessing navigator.serviceWorker (#21231)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+211.sha-373fa78
+ * @license Angular v6.0.0-rc.5+212.sha-0bdd30e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5616,7 +5616,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new core.Version('6.0.0-rc.5+211.sha-373fa78');
+var VERSION = new core.Version('6.0.0-rc.5+212.sha-0bdd30e');
 
 /**
  * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+211.sha-373fa78
+ * @license Angular v6.0.0-rc.5+212.sha-0bdd30e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
