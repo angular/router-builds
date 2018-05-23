c8218dff0208acd5974ac9826906a8f12e94d59a
Matias Niemelä <matias@yearofmoo.com>
2018-05-23 00:03:23 +0000

    1542893 release: cut the v6.0.3 release

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+23.sha-469b1e4
+ * @license Angular v6.0.3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+23.sha-469b1e4
+ * @license Angular v6.0.3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5614,7 +5614,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new core.Version('6.0.2+23.sha-469b1e4');
+var VERSION = new core.Version('6.0.3');
 
 /**
  * @license
