1927e837f7c06967a778acecefb0bfa11eda919d
Matias Niemelä <matias@yearofmoo.com>
2018-05-25 18:17:27 +0000

    01b5acd fix(compiler): generate core-compliant hostBindings property (#24087)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+219.sha-609e6b9
+ * @license Angular v6.0.0-rc.5+221.sha-01b5acd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5535,7 +5535,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.0.0-rc.5+219.sha-609e6b9');
+var VERSION = new Version('6.0.0-rc.5+221.sha-01b5acd');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+219.sha-609e6b9
+ * @license Angular v6.0.0-rc.5+221.sha-01b5acd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
