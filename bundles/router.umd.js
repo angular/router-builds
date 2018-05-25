8dd7b4b20a297f4f4366b665ffcf3e0e39a5596d
Matias Niemelä <matias@yearofmoo.com>
2018-05-25 18:24:02 +0000

    a638f50 docs(aio): add material community components (#24042)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+3.sha-a29c756
+ * @license Angular v6.0.3+4.sha-a638f50
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5614,7 +5614,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new core.Version('6.0.3+3.sha-a29c756');
+var VERSION = new core.Version('6.0.3+4.sha-a638f50');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+3.sha-a29c756
+ * @license Angular v6.0.3+4.sha-a638f50
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
