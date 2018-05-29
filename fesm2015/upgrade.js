d9cec5aee99f5eda72da8409769eb5129b1f0ef2
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 23:11:47 +0000

    96655f7 style(compiler): fix typo and formatting (#23729)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+19.sha-253f509
+ * @license Angular v6.0.3+20.sha-96655f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+19.sha-253f509
+ * @license Angular v6.0.3+20.sha-96655f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5533,7 +5533,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.0.3+19.sha-253f509');
+var VERSION = new Version('6.0.3+20.sha-96655f7');
 
 /**
  * @license
