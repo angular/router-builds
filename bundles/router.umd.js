d81c230e4bc673aee087530811b78f7f0f858207
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 21:14:51 +0000

    e43d3fa build: pick up rules_typescript karma fix (#24184)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+228.sha-729c797
+ * @license Angular v6.0.0-rc.5+231.sha-e43d3fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5616,7 +5616,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new core.Version('6.0.0-rc.5+228.sha-729c797');
+var VERSION = new core.Version('6.0.0-rc.5+231.sha-e43d3fa');
 
 /**
  * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+228.sha-729c797
+ * @license Angular v6.0.0-rc.5+231.sha-e43d3fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
