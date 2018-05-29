3d66344362e4dbfb499f4127c42b901ba04ea2ef
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 23:03:46 +0000

    253f509 refactor(core): use Partial<T> for MetadataOverride (#24103)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+18.sha-5a02ae2
+ * @license Angular v6.0.3+19.sha-253f509
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5614,7 +5614,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new core.Version('6.0.3+18.sha-5a02ae2');
+var VERSION = new core.Version('6.0.3+19.sha-253f509');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+18.sha-5a02ae2
+ * @license Angular v6.0.3+19.sha-253f509
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
