074efd56f2436768d353003d651f1840e20220e8
Victor Berchet <victor@suumit.com>
2018-05-30 17:47:42 +0000

    29dfc8f docs(bazel): improve error message for ng_package with no metadata (#22964)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+22.sha-b5533e0
+ * @license Angular v6.0.3+23.sha-29dfc8f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+22.sha-b5533e0
+ * @license Angular v6.0.3+23.sha-29dfc8f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6376,7 +6376,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.3+22.sha-b5533e0');
+const VERSION = new Version('6.0.3+23.sha-29dfc8f');
 
 /**
  * @fileoverview added by tsickle
