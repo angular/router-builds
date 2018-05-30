a5f85cd19d324c1ab1b25b4c67179d126f7be386
Victor Berchet <victor@suumit.com>
2018-05-30 17:34:01 +0000

    f6f44ed docs: update ivy perf notes (#24035)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+247.sha-e53179e
+ * @license Angular v6.0.0-rc.5+250.sha-f6f44ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6376,7 +6376,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.0-rc.5+247.sha-e53179e');
+const VERSION = new Version('6.0.0-rc.5+250.sha-f6f44ed');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+247.sha-e53179e
+ * @license Angular v6.0.0-rc.5+250.sha-f6f44ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
