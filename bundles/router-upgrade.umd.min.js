b9ed9ec926fec953a22da9935ff3807a1bbdac8f
Victor Berchet <victor@suumit.com>
2018-05-30 17:36:16 +0000

    90bf5d8 feat(ivy): separate attributes for directive matching purposes (#23991)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+248.sha-b87d650
+ * @license Angular v6.0.0-rc.5+249.sha-90bf5d8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+248.sha-b87d650
+ * @license Angular v6.0.0-rc.5+249.sha-90bf5d8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5616,7 +5616,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new core.Version('6.0.0-rc.5+248.sha-b87d650');
+var VERSION = new core.Version('6.0.0-rc.5+249.sha-90bf5d8');
 
 /**
  * @license
