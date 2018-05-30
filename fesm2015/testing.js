73c477277ce0544ecbcc5830388f4822d9c952f6
Victor Berchet <victor@suumit.com>
2018-05-30 17:53:30 +0000

    d6595eb feat(platform-server): use EventManagerPlugin on the server (#24132)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+253.sha-3ed7fc6
+ * @license Angular v6.0.0-rc.5+255.sha-d6595eb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+253.sha-3ed7fc6
+ * @license Angular v6.0.0-rc.5+255.sha-d6595eb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5535,7 +5535,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.0.0-rc.5+253.sha-3ed7fc6');
+var VERSION = new Version('6.0.0-rc.5+255.sha-d6595eb');
 
 /**
  * @license
