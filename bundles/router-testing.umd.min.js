82f948ab392012bb458a0f3675ae70c5bffb6f98
Victor Berchet <victor@suumit.com>
2018-05-31 17:35:22 +0000

    6a663a4 fix(platform-server): don't reflect innerHTML property to attibute (#24213)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+286.sha-ec57133
+ * @license Angular v6.0.0-rc.5+287.sha-6a663a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+286.sha-ec57133
+ * @license Angular v6.0.0-rc.5+287.sha-6a663a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5535,7 +5535,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.0.0-rc.5+286.sha-ec57133');
+var VERSION = new Version('6.0.0-rc.5+287.sha-6a663a4');
 
 /**
  * @license
