90fc57b488972d46a23462c160c55e8629d3dfce
Victor Berchet <victor@suumit.com>
2018-05-31 00:25:24 +0000

    e2e7b49 docs: fix typo (#24210)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+36.sha-c9eb491
+ * @license Angular v6.0.3+42.sha-e2e7b49
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6376,7 +6376,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.3+36.sha-c9eb491');
+const VERSION = new Version('6.0.3+42.sha-e2e7b49');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+36.sha-c9eb491
+ * @license Angular v6.0.3+42.sha-e2e7b49
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
