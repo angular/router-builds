50bc164f9e9df81b93601cfa02964b07a4085edd
Victor Berchet <victor@suumit.com>
2018-05-31 20:44:05 +0000

    7e3f8f7 refactor(ivy): replace LView.child with TView.childIndex lookup (#24211)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+281.sha-b86d4de
+ * @license Angular v6.0.0-rc.5+288.sha-7e3f8f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6376,7 +6376,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.0-rc.5+281.sha-b86d4de');
+const VERSION = new Version('6.0.0-rc.5+288.sha-7e3f8f7');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+281.sha-b86d4de
+ * @license Angular v6.0.0-rc.5+288.sha-7e3f8f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
