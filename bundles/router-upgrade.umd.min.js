a4b647936e2ffeeaf953bb6399033ec448fc82bc
Victor Berchet <victor@suumit.com>
2018-05-31 00:54:14 +0000

    2f377db style(compiler-cli): fix typo error (#23897) PR Close #23897

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+42.sha-e2e7b49
+ * @license Angular v6.0.3+43.sha-2f377db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+42.sha-e2e7b49
+ * @license Angular v6.0.3+43.sha-2f377db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5533,7 +5533,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.0.3+42.sha-e2e7b49');
+var VERSION = new Version('6.0.3+43.sha-2f377db');
 
 /**
  * @license
