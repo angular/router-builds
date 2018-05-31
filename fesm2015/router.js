9c852363ffe2951eb7825ef058ca1b23b0e493eb
Victor Berchet <victor@suumit.com>
2018-05-31 00:52:26 +0000

    4ab70fb style(compiler-cli): fix typo error (#23897) PR Close #23897

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+282.sha-5d6074e
+ * @license Angular v6.0.0-rc.5+283.sha-4ab70fb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6376,7 +6376,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.0-rc.5+282.sha-5d6074e');
+const VERSION = new Version('6.0.0-rc.5+283.sha-4ab70fb');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+282.sha-5d6074e
+ * @license Angular v6.0.0-rc.5+283.sha-4ab70fb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
