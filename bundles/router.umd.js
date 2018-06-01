f5e4230b89b21e793355ff8029831545073d172e
Victor Berchet <victor@suumit.com>
2018-06-01 15:57:56 +0000

    60aa943 fix(platform-server): avoid dependency cycle when using http interceptor (#24229)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+293.sha-68a799e
+ * @license Angular v6.0.0-rc.5+294.sha-60aa943
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5616,7 +5616,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new core.Version('6.0.0-rc.5+293.sha-68a799e');
+var VERSION = new core.Version('6.0.0-rc.5+294.sha-60aa943');
 
 /**
  * @license
