fa26e6e3d496f696a43baa07ed5e17de5a292550
Victor Berchet <victor@suumit.com>
2018-06-03 02:56:23 +0000

    cb65724 refactor(ivy): combine lifecycleStage with LViewFlags (#24263)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+299.sha-0561b66
+ * @license Angular v6.0.0-rc.5+302.sha-cb65724
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+299.sha-0561b66
+ * @license Angular v6.0.0-rc.5+302.sha-cb65724
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6376,7 +6376,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.0-rc.5+299.sha-0561b66');
+const VERSION = new Version('6.0.0-rc.5+302.sha-cb65724');
 
 /**
  * @fileoverview added by tsickle
