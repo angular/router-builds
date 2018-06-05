47c6a26a109f4e6d8a2991abc4db76bfb0fed141
Victor Berchet <victor@suumit.com>
2018-06-05 19:35:33 +0000

    9c40375 refactor(ivy): misc minor fixes in the JIT compiler (#24308)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+310.sha-83a0686
+ * @license Angular v6.0.0-rc.5+311.sha-9c40375
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5211,7 +5211,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.0.0-rc.5+310.sha-83a0686');
+var VERSION = new core.Version('6.0.0-rc.5+311.sha-9c40375');
 
 /**
  * @license
