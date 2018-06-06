595e9002e3bc17b38facd98b861745bdf0384644
Victor Berchet <victor@suumit.com>
2018-06-06 22:24:07 +0000

    7de2ba0 Revert "feat(ivy): add namespace instructions for SVG and others (#23899)"

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+335.sha-acf270d
+ * @license Angular v6.1.0-beta.0+11.sha-7de2ba0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+335.sha-acf270d
+ * @license Angular v6.1.0-beta.0+11.sha-7de2ba0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6389,7 +6389,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.0-rc.5+335.sha-acf270d');
+const VERSION = new Version('6.1.0-beta.0+11.sha-7de2ba0');
 
 /**
  * @fileoverview added by tsickle
