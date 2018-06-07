022feb031649850a80d1d7fb10638884fe9ca500
Miško Hevery <misko@hevery.com>
2018-06-07 23:12:47 +0000

    a6e797b build(bazel): fix ng_package rollup root dir for fesm2015 output (#24298)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+14.sha-f781f74
+ * @license Angular v6.1.0-beta.0+16.sha-a6e797b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5223,7 +5223,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.0+14.sha-f781f74');
+var VERSION = new core.Version('6.1.0-beta.0+16.sha-a6e797b');
 
 /**
  * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+14.sha-f781f74
+ * @license Angular v6.1.0-beta.0+16.sha-a6e797b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
