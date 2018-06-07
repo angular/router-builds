0357d87a1bb80aad5abfe46fd8b52355eb2abc75
Miško Hevery <misko@hevery.com>
2018-06-07 23:20:07 +0000

    d4c66d5 docs(ivy): update status of impl progress (#24323)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+16.sha-a6e797b
+ * @license Angular v6.1.0-beta.0+18.sha-d4c66d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+16.sha-a6e797b
+ * @license Angular v6.1.0-beta.0+18.sha-d4c66d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5223,7 +5223,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.0+16.sha-a6e797b');
+var VERSION = new core.Version('6.1.0-beta.0+18.sha-d4c66d5');
 
 /**
  * @license
