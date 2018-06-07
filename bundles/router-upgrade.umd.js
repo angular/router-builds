7f488bafb02ae623e1e894deccb18e42cfc3061d
Miško Hevery <misko@hevery.com>
2018-06-07 23:11:14 +0000

    7031972 docs(aio): Added resource link to Amexio Canvas Web Based IDE (#24336)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+1.sha-cfa5b65
+ * @license Angular v6.0.4+2.sha-7031972
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+1.sha-cfa5b65
+ * @license Angular v6.0.4+2.sha-7031972
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5128,7 +5128,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.4+1.sha-cfa5b65');
+var VERSION = new Version('6.0.4+2.sha-7031972');
 
 /**
  * @license
