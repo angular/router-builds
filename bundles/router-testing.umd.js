dadb45cf5e654c208fbd4af15ad5493bfa58c71c
Miško Hevery <misko@hevery.com>
2018-06-08 22:44:44 +0000

    1135563 fix(ivy): compute transitive scopes from NgModuleDef only (#24334)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+18.sha-d4c66d5
+ * @license Angular v6.1.0-beta.0+23.sha-1135563
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+18.sha-d4c66d5
+ * @license Angular v6.1.0-beta.0+23.sha-1135563
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6389,7 +6389,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.0+18.sha-d4c66d5');
+const VERSION = new Version('6.1.0-beta.0+23.sha-1135563');
 
 /**
  * @fileoverview added by tsickle
