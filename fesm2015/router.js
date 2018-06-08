5ec790f76cd268ad3731a625f8ee89ebd5d45584
Miško Hevery <misko@hevery.com>
2018-06-08 23:06:20 +0000

    1b253e1 fix(ivy): special case [style] and [class] bindings for future use (#23232)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+25.sha-8c1ac28
+ * @license Angular v6.1.0-beta.0+26.sha-1b253e1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6389,7 +6389,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.0+25.sha-8c1ac28');
+const VERSION = new Version('6.1.0-beta.0+26.sha-1b253e1');
 
 /**
  * @fileoverview added by tsickle
