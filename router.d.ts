bd9fa00e5597d19663e125bdcfb7965601b1052c
Miško Hevery <misko@hevery.com>
2018-06-08 23:11:11 +0000

    49c5234 feat(router): implement scrolling restoration service (#20030)

--- router.d.ts
+++ router.d.ts
@@ -2,5 +2,6 @@
  * Generated bundle index. Do not edit.
  */
 export * from './index';
-export { ROUTER_FORROOT_GUARD as ɵangular_packages_router_router_a, RouterInitializer as ɵangular_packages_router_router_g, getAppInitializer as ɵangular_packages_router_router_h, getBootstrapListener as ɵangular_packages_router_router_i, provideForRootGuard as ɵangular_packages_router_router_d, provideLocationStrategy as ɵangular_packages_router_router_c, provideRouterInitializer as ɵangular_packages_router_router_j, rootRoute as ɵangular_packages_router_router_f, routerNgProbeToken as ɵangular_packages_router_router_b, setupRouter as ɵangular_packages_router_router_e } from './src/router_module';
-export { Tree as ɵangular_packages_router_router_k, TreeNode as ɵangular_packages_router_router_l } from './src/utils/tree';
+export { ROUTER_FORROOT_GUARD as ɵangular_packages_router_router_a, RouterInitializer as ɵangular_packages_router_router_h, createRouterScroller as ɵangular_packages_router_router_c, getAppInitializer as ɵangular_packages_router_router_i, getBootstrapListener as ɵangular_packages_router_router_j, provideForRootGuard as ɵangular_packages_router_router_e, provideLocationStrategy as ɵangular_packages_router_router_d, provideRouterInitializer as ɵangular_packages_router_router_k, rootRoute as ɵangular_packages_router_router_g, routerNgProbeToken as ɵangular_packages_router_router_b, setupRouter as ɵangular_packages_router_router_f } from './src/router_module';
+export { RouterScroller as ɵangular_packages_router_router_n } from './src/router_scroller';
+export { Tree as ɵangular_packages_router_router_l, TreeNode as ɵangular_packages_router_router_m } from './src/utils/tree';
