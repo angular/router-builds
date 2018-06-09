053a2fba69c45ebffa3aa36cfd4663774110b547
Miško Hevery <misko@hevery.com>
2018-06-09 00:15:03 +0000

    8aa70c2 docs: adds information about the VSCode clang-format extension (#24351)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+27.sha-49c5234
+ * @license Angular v6.1.0-beta.0+28.sha-8aa70c2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4899,7 +4899,7 @@ var RouterModule_1;
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.0+27.sha-49c5234');
+const VERSION = new Version('6.1.0-beta.0+28.sha-8aa70c2');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+27.sha-49c5234
+ * @license Angular v6.1.0-beta.0+28.sha-8aa70c2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
