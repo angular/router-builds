eed7c8f192dd99da271ddda1ab10e4597186a01c
Miško Hevery <misko@hevery.com>
2018-06-11 18:42:20 +0000

    3ed2d75 fix(service-worker): fix `SwPush.unsubscribe()` (#24162)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+34.sha-8dd99ac
+ * @license Angular v6.1.0-beta.0+38.sha-3ed2d75
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4899,7 +4899,7 @@ var RouterModule_1;
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.0+34.sha-8dd99ac');
+const VERSION = new Version('6.1.0-beta.0+38.sha-3ed2d75');
 
 /**
  * @license
