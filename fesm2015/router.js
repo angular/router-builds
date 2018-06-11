1c0e9d52e343700b8a01160a569a5c141abe0af6
Miško Hevery <misko@hevery.com>
2018-06-11 16:51:12 +0000

    5e8bf2f build(docs-infra): ensure `dist/` directory is cleaned before running `tsc --watch` (#24372)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+30.sha-29eb24b
+ * @license Angular v6.1.0-beta.0+32.sha-5e8bf2f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6539,7 +6539,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.0+30.sha-29eb24b');
+const VERSION = new Version('6.1.0-beta.0+32.sha-5e8bf2f');
 
 /**
  * @fileoverview added by tsickle
