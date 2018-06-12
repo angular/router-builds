a7ea75bab9ddd46cff0ea2fed50414fbcbbd2090
Miško Hevery <misko@hevery.com>
2018-06-12 19:05:47 +0000

    ea3669e fix(bazel): Allow ng_module to depend on targets w no deps (#24446)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+13.sha-ea2987c
+ * @license Angular v6.0.4+14.sha-ea3669e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+13.sha-ea2987c
+ * @license Angular v6.0.4+14.sha-ea3669e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5209,7 +5209,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.0.4+13.sha-ea2987c');
+var VERSION = new core.Version('6.0.4+14.sha-ea3669e');
 
 /**
  * @license
