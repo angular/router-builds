5681f3abed29d32a5a797f3899a5df4c06e57a0d
Miško Hevery <misko@hevery.com>
2018-06-12 19:18:45 +0000

    c2b5ebf build: update buildifier to latest (#24296)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+40.sha-1316024
+ * @license Angular v6.1.0-beta.0+42.sha-c2b5ebf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5261,7 +5261,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.0+40.sha-1316024');
+var VERSION = new Version('6.1.0-beta.0+42.sha-c2b5ebf');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,8 +1,3 @@
-/**
- * @license Angular v6.1.0-beta.0+40.sha-1316024
- * (c) 2010-2018 Google, Inc. https://angular.io/
- * License: MIT
- */
 
   export * from './testing/testing';
   
