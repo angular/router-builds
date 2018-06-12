f2e9db924b0b757abc444301404c5210558618cd
Miško Hevery <misko@hevery.com>
2018-06-12 19:12:43 +0000

    282d351 fix(bazel): Allow ng_module to depend on targets w no deps (#24446)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+38.sha-3ed2d75
+ * @license Angular v6.1.0-beta.0+39.sha-282d351
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4899,7 +4899,7 @@ var RouterModule_1;
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.0+38.sha-3ed2d75');
+const VERSION = new Version('6.1.0-beta.0+39.sha-282d351');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+38.sha-3ed2d75
+ * @license Angular v6.1.0-beta.0+39.sha-282d351
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
