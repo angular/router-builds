aacf61428c10dda96abd79970268478f0f418857
Miško Hevery <misko@hevery.com>
2018-06-12 19:17:41 +0000

    1316024 docs: change `aio` scope to `docs-infra` (#24410)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+39.sha-282d351
+ * @license Angular v6.1.0-beta.0+40.sha-1316024
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5400,7 +5400,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.0+39.sha-282d351');
+var VERSION = new core.Version('6.1.0-beta.0+40.sha-1316024');
 
 /**
  * @license
