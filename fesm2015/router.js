cc032fb79e8fa564f85597740cd34be7769768ca
Miško Hevery <misko@hevery.com>
2018-06-13 21:13:47 +0000

    503a524 docs: change capitalization for css hex color values (#23511)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+55.sha-a577c9e
+ * @license Angular v6.1.0-beta.0+56.sha-503a524
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6575,7 +6575,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.0+55.sha-a577c9e');
+const VERSION = new Version('6.1.0-beta.0+56.sha-503a524');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+55.sha-a577c9e
+ * @license Angular v6.1.0-beta.0+56.sha-503a524
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
