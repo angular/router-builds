bb07666435f05596ca4401122240e0e1f2695a77
Miško Hevery <misko@hevery.com>
2018-06-14 21:45:45 +0000

    7c8159b test(bazel): fix flakey bazel integration e2e test (#24522)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+24.sha-5aa12c7
+ * @license Angular v6.1.0-beta.1+25.sha-7c8159b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5379,7 +5379,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.1+24.sha-5aa12c7');
+var VERSION = new core.Version('6.1.0-beta.1+25.sha-7c8159b');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+24.sha-5aa12c7
+ * @license Angular v6.1.0-beta.1+25.sha-7c8159b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
