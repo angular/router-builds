e15f2ea15d5a3d208207da9d8ea0515f20ddbf63
Miško Hevery <misko@hevery.com>
2018-06-14 00:27:58 +0000

    39af314 build(aio): add github links to API doc members (#24000)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+1.sha-03f93b3
+ * @license Angular v6.1.0-beta.1+22.sha-39af314
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5347,7 +5347,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.1+1.sha-03f93b3');
+var VERSION = new Version('6.1.0-beta.1+22.sha-39af314');
 
 /**
  * @license
