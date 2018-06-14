bbe6ba0606adeeab21a6a7f6c21702a7bf45aa65
Misko Hevery <misko@hevery.com>
2018-06-14 04:00:28 +0000

    d8f7b29 fix(compiler): support `.` in import statements. (#20634)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+22.sha-39af314
+ * @license Angular v6.1.0-beta.1+23.sha-d8f7b29
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5379,7 +5379,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.1+22.sha-39af314');
+var VERSION = new core.Version('6.1.0-beta.1+23.sha-d8f7b29');
 
 /**
  * @license
