059e1ce6b67a76ce5036ed843c03d7fa0820a850
Miško Hevery <misko@hevery.com>
2018-06-14 21:47:14 +0000

    7c8159b test(bazel): fix flakey bazel integration e2e test (#24522)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+24.sha-5aa12c7
+ * @license Angular v6.1.0-beta.1+25.sha-7c8159b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5347,7 +5347,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.1+24.sha-5aa12c7');
+var VERSION = new Version('6.1.0-beta.1+25.sha-7c8159b');
 
 /**
  * @license
