50859d94f7e4f65dfc99620163da5c96f7d32d8a
Miško Hevery <misko@hevery.com>
2018-06-15 00:23:09 +0000

    f3625e4 test(common): rename keyvalue e2e test (#24489)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+31.sha-ccbda9d
+ * @license Angular v6.1.0-beta.1+32.sha-f3625e4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4933,7 +4933,7 @@ var RouterModule_1;
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.1+31.sha-ccbda9d');
+const VERSION = new Version('6.1.0-beta.1+32.sha-f3625e4');
 
 /**
  * @license
