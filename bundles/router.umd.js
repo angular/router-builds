a02da8ae8ab791288cb0ac1c8daee19ee8811d0d
Miško Hevery <misko@hevery.com>
2018-06-15 16:24:19 +0000

    ccbda9d fix(core): Injector correctly honors the @Self flag (#24520)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+28.sha-a45fad3
+ * @license Angular v6.1.0-beta.1+31.sha-ccbda9d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5379,7 +5379,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.1+28.sha-a45fad3');
+var VERSION = new core.Version('6.1.0-beta.1+31.sha-ccbda9d');
 
 /**
  * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+28.sha-a45fad3
+ * @license Angular v6.1.0-beta.1+31.sha-ccbda9d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
