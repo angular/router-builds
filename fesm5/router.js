c1ac92b35da617985c4abc53b00d041722d76a94
Miško Hevery <misko@hevery.com>
2018-06-18 22:31:58 +0000

    794584e docs: Remove outdated Webpack guide and example (#24478)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+33.sha-45862d0
+ * @license Angular v6.1.0-beta.1+34.sha-794584e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5298,7 +5298,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.1+33.sha-45862d0');
+var VERSION = new Version('6.1.0-beta.1+34.sha-794584e');
 
 /**
  * @license
