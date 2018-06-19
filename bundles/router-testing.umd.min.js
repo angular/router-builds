0db810317d1dd9ff81479b40a35da5ecbe97c52e
Miško Hevery <misko@hevery.com>
2018-06-19 18:29:22 +0000

    5b76f04 docs: More edits (#24255)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+34.sha-794584e
+ * @license Angular v6.1.0-beta.1+41.sha-5b76f04
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+34.sha-794584e
+ * @license Angular v6.1.0-beta.1+41.sha-5b76f04
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4933,7 +4933,7 @@ var RouterModule_1;
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.1+34.sha-794584e');
+const VERSION = new Version('6.1.0-beta.1+41.sha-5b76f04');
 
 /**
  * @license
