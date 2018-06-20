343e9eec804ff971e2e866d1569baae33ed1e625
Miško Hevery <misko@hevery.com>
2018-06-20 22:11:22 +0000

    e8354ed test(animations): properly reference body node for SSR environments (#23300)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+32.sha-f3625e4
+ * @license Angular v6.1.0-beta.1+42.sha-e8354ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5379,7 +5379,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.1+32.sha-f3625e4');
+var VERSION = new core.Version('6.1.0-beta.1+42.sha-e8354ed');
 
 /**
  * @license
