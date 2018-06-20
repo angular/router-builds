74b632ee0134736b1b5ff9e39af120d8e49feb8c
Miško Hevery <misko@hevery.com>
2018-06-20 18:34:35 +0000

    e8354ed test(animations): properly reference body node for SSR environments (#23300)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+41.sha-5b76f04
+ * @license Angular v6.1.0-beta.1+42.sha-e8354ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6575,7 +6575,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.1+41.sha-5b76f04');
+const VERSION = new Version('6.1.0-beta.1+42.sha-e8354ed');
 
 /**
  * @fileoverview added by tsickle
