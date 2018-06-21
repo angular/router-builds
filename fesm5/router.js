a29d62a3466d9540edeab7085cd9d614aaabd0ef
Miško Hevery <misko@hevery.com>
2018-06-21 00:22:09 +0000

    def354d release: cut the v6.1.0-beta.2 release

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+43.sha-9782736
+ * @license Angular v6.1.0-beta.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5298,7 +5298,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.1+43.sha-9782736');
+var VERSION = new Version('6.1.0-beta.2');
 
 /**
  * @license
