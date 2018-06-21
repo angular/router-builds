77bd5fc4d9396d36e975ca01a7e36aab31a18ab6
Pete Bacon Darwin <pete@bacondarwin.com>
2018-06-21 14:10:22 +0000

    d9c4acf feat(ivy): WIP

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+37.sha-801103d
+ * @license Angular v6.1.0-beta.1+38.sha-d9c4acf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4933,7 +4933,7 @@ var RouterModule_1;
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.1+37.sha-801103d');
+const VERSION = new Version('6.1.0-beta.1+38.sha-d9c4acf');
 
 /**
  * @license
