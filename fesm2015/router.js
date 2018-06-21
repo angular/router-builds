20c6a032bde7f4caaea03ec38fec85010eaa0440
Miško Hevery <misko@hevery.com>
2018-06-21 00:32:00 +0000

    3e1a3b2 fix(ivy): support queries for views inserted in lifecycle hooks (#24587)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+1.sha-a269658
+ * @license Angular v6.1.0-beta.2+5.sha-3e1a3b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4933,7 +4933,7 @@ var RouterModule_1;
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.2+1.sha-a269658');
+const VERSION = new Version('6.1.0-beta.2+5.sha-3e1a3b2');
 
 /**
  * @license
