2ac648cf684ef62c2c7661f6d1a23c12ce36e413
Miško Hevery <misko@hevery.com>
2018-06-21 00:29:17 +0000

    1ce5a49 docs(aio): add elana olson to contributor.json file (#24579)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+1.sha-1f532aa
+ * @license Angular v6.0.6+2.sha-1ce5a49
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+1.sha-1f532aa
+ * @license Angular v6.0.6+2.sha-1ce5a49
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5246,7 +5246,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.0.6+1.sha-1f532aa');
+var VERSION = new core.Version('6.0.6+2.sha-1ce5a49');
 
 /**
  * @license
