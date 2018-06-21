0309503302a79a293a5c243e3d0c63eb402852a8
Miško Hevery <misko@hevery.com>
2018-06-21 20:47:57 +0000

    10da6a4 refactor(ivy): first pass at extracting ReflectionHost for abstract reflection (#24541)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+7.sha-84272e2
+ * @license Angular v6.1.0-beta.2+8.sha-10da6a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6575,7 +6575,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.2+7.sha-84272e2');
+const VERSION = new Version('6.1.0-beta.2+8.sha-10da6a4');
 
 /**
  * @fileoverview added by tsickle
