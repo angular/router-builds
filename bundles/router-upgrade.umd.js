23b86df89f53516ff09a808e85f7ed2dc2b5e45a
Miško Hevery <misko@hevery.com>
2018-06-25 16:03:32 +0000

    39c7769 docs(core): typo in static injector tests (#24548)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+16.sha-8c51ce6
+ * @license Angular v6.1.0-beta.2+17.sha-39c7769
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+16.sha-8c51ce6
+ * @license Angular v6.1.0-beta.2+17.sha-39c7769
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5379,7 +5379,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.2+16.sha-8c51ce6');
+var VERSION = new core.Version('6.1.0-beta.2+17.sha-39c7769');
 
 /**
  * @license
