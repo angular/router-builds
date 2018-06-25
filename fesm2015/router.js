f380f85921da88277ff55807c452ab8ce4ac2dbe
Miško Hevery <misko@hevery.com>
2018-06-25 16:01:29 +0000

    71b0c3d docs(elements): mention `comp: elements` as a valid label (#24443)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+12.sha-fe8fe9b
+ * @license Angular v6.1.0-beta.2+15.sha-71b0c3d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6575,7 +6575,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.2+12.sha-fe8fe9b');
+const VERSION = new Version('6.1.0-beta.2+15.sha-71b0c3d');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+12.sha-fe8fe9b
+ * @license Angular v6.1.0-beta.2+15.sha-71b0c3d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
