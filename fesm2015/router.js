2e635f83bee8964dfedcfe71e67c084a2f9a06ee
Miško Hevery <misko@hevery.com>
2018-06-25 16:10:43 +0000

    fba3f10 docs: add guide-angular.wishtack.io to education resources (#24585)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+17.sha-39c7769
+ * @license Angular v6.1.0-beta.2+19.sha-fba3f10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6575,7 +6575,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.2+17.sha-39c7769');
+const VERSION = new Version('6.1.0-beta.2+19.sha-fba3f10');
 
 /**
  * @fileoverview added by tsickle
