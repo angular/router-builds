66ea21915d06ba23f1b3261a56fea1aad9f0850f
Miško Hevery <misko@hevery.com>
2018-06-25 16:04:51 +0000

    39c7769 docs(core): typo in static injector tests (#24548)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+18.sha-c95437f
+ * @license Angular v6.1.0-beta.2+17.sha-39c7769
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4933,7 +4933,7 @@ var RouterModule_1;
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.2+18.sha-c95437f');
+const VERSION = new Version('6.1.0-beta.2+17.sha-39c7769');
 
 /**
  * @license
