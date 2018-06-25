5af0b232116ce2e1d0c9cb248b726cb8b8837302
Jason Aden <jasonaden@google.com>
2018-06-25 17:14:25 +0000

    65744e4 docs: test api doc for pipes (#24141)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+10.sha-56bc869
+ * @license Angular v6.0.6+11.sha-65744e4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+10.sha-56bc869
+ * @license Angular v6.0.6+11.sha-65744e4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5165,7 +5165,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.6+10.sha-56bc869');
+var VERSION = new Version('6.0.6+11.sha-65744e4');
 
 /**
  * @license
