dbe6d85206760e1a5098dfc3e27b8d42ca7761ef
Miško Hevery <misko@hevery.com>
2018-06-25 16:11:08 +0000

    3649958 build(docs-infra): move overload short description above syntax (#24526)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+5.sha-8919577
+ * @license Angular v6.0.6+6.sha-3649958
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5246,7 +5246,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.0.6+5.sha-8919577');
+var VERSION = new core.Version('6.0.6+6.sha-3649958');
 
 /**
  * @license
