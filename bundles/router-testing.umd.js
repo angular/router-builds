563819fd7bcf904e15d8c8ee500229b385bb1398
Miško Hevery <misko@hevery.com>
2018-06-25 16:16:01 +0000

    f229449 refactor(ivy): insert embedded views immediately (#24629)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+21.sha-6e20e0a
+ * @license Angular v6.1.0-beta.2+22.sha-f229449
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+21.sha-6e20e0a
+ * @license Angular v6.1.0-beta.2+22.sha-f229449
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4933,7 +4933,7 @@ var RouterModule_1;
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.2+21.sha-6e20e0a');
+const VERSION = new Version('6.1.0-beta.2+22.sha-f229449');
 
 /**
  * @license
