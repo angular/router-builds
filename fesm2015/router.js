a44e8605832e32e5e5c9cbfb89a1d437284adafb
Miško Hevery <misko@hevery.com>
2018-06-25 16:33:36 +0000

    f229449 refactor(ivy): insert embedded views immediately (#24629)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+10.sha-7b2b1af
+ * @license Angular v6.1.0-beta.2+22.sha-f229449
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6575,7 +6575,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.2+10.sha-7b2b1af');
+const VERSION = new Version('6.1.0-beta.2+22.sha-f229449');
 
 /**
  * @fileoverview added by tsickle
