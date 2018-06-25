01cbe43ed260b737e0dca5ca9d83ae2779bf1cac
George Kalpakas <kalpakas.g@gmail.com>
2018-06-25 20:20:10 +0000

    a5799e6 build(ivy): fix bazel `srcs` for ngcc

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+45.sha-db21dbf
+ * @license Angular v6.1.0-beta.1+46.sha-a5799e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4933,7 +4933,7 @@ var RouterModule_1;
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.1+45.sha-db21dbf');
+const VERSION = new Version('6.1.0-beta.1+46.sha-a5799e6');
 
 /**
  * @license
