dfcf6846bdceb8fcaeefc551420543faf71a4672
Jason Aden <jasonaden@google.com>
2018-06-26 18:37:42 +0000

    5c0e681 docs(aio): fix adding to template driven forms (#23743)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+33.sha-cf0968f
+ * @license Angular v6.1.0-beta.2+38.sha-5c0e681
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5298,7 +5298,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+33.sha-cf0968f');
+var VERSION = new Version('6.1.0-beta.2+38.sha-5c0e681');
 
 /**
  * @license
