025bb7956359db44d12f72d62a89706e6705af9c
Jason Aden <jasonaden@google.com>
2018-06-26 18:34:32 +0000

    cf0968f build(docs-infra): support hiding constructors of injectable classes (#24529)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+32.sha-855e8ad
+ * @license Angular v6.1.0-beta.2+33.sha-cf0968f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+32.sha-855e8ad
+ * @license Angular v6.1.0-beta.2+33.sha-cf0968f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4933,7 +4933,7 @@ var RouterModule_1;
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.2+32.sha-855e8ad');
+const VERSION = new Version('6.1.0-beta.2+33.sha-cf0968f');
 
 /**
  * @license
