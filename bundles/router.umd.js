281a48f69046053943ee0b019ecec750ad611ec5
Jason Aden <jasonaden@google.com>
2018-06-26 18:32:49 +0000

    3513ae4 docs(aio): fix adding to template driven forms (#23743)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+17.sha-426f971
+ * @license Angular v6.0.6+22.sha-3513ae4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5246,7 +5246,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.0.6+17.sha-426f971');
+var VERSION = new core.Version('6.0.6+22.sha-3513ae4');
 
 /**
  * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+17.sha-426f971
+ * @license Angular v6.0.6+22.sha-3513ae4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
