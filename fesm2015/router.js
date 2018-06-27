48c21928fb0a57e3055dd6321f146798a8677f87
Jason Aden <jasonaden@google.com>
2018-06-27 21:53:20 +0000

    fe8fcc8 refactor(ivy): remove dynamicParent from LNode (#24678)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+38.sha-5c0e681
+ * @license Angular v6.1.0-beta.2+39.sha-fe8fcc8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4933,7 +4933,7 @@ var RouterModule_1;
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.2+38.sha-5c0e681');
+const VERSION = new Version('6.1.0-beta.2+39.sha-fe8fcc8');
 
 /**
  * @license
