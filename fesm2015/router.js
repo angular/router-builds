184bc450dc177bc211ab77fda8dc478600ac5081
Jason Aden <jasonaden@google.com>
2018-06-27 23:10:56 +0000

    39c8bae fix(common): use correct ICU plural for locale mk (#24659) PR Close #24659

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+32.sha-855e8ad
+ * @license Angular v6.1.0-beta.2+54.sha-39c8bae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4933,7 +4933,7 @@ var RouterModule_1;
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.2+32.sha-855e8ad');
+const VERSION = new Version('6.1.0-beta.2+54.sha-39c8bae');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+32.sha-855e8ad
+ * @license Angular v6.1.0-beta.2+54.sha-39c8bae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
