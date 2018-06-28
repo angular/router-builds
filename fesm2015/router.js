89f5230b9665f16985b2201642a3d965bb6b24ae
Miško Hevery <misko@hevery.com>
2018-06-28 22:42:23 +0000

    01e7ff6 test(ivy): todo app only includes reflect-metadata in JIT mode (#24677)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+9.sha-676ec41
+ * @license Angular v6.1.0-beta.3+8.sha-01e7ff6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4933,7 +4933,7 @@ var RouterModule_1;
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.3+9.sha-676ec41');
+const VERSION = new Version('6.1.0-beta.3+8.sha-01e7ff6');
 
 /**
  * @license
