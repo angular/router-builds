f0f6579a7a72d2bbc78bd8cdd49dc7caf2ac1139
Miško Hevery <misko@hevery.com>
2018-06-28 22:42:18 +0000

    676ec41 docs: consistent spacing in tutorial html files (#23105) (#24497)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+56.sha-c57b491
+ * @license Angular v6.1.0-beta.3+9.sha-676ec41
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5437,7 +5437,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.2+56.sha-c57b491');
+var VERSION = new core.Version('6.1.0-beta.3+9.sha-676ec41');
 
 /**
  * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+56.sha-c57b491
+ * @license Angular v6.1.0-beta.3+9.sha-676ec41
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4933,7 +4933,7 @@ var RouterModule_1;
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.2+56.sha-c57b491');
+const VERSION = new Version('6.1.0-beta.3+9.sha-676ec41');
 
 /**
  * @license
