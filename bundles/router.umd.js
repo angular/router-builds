a21f78ebc873d166ffa10f57aa9b632da1ce6067
Miško Hevery <misko@hevery.com>
2018-06-28 22:49:58 +0000

    99bdd25 fix(ivy): support projecting containers into containers (#24695)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+13.sha-3db9d57
+ * @license Angular v6.1.0-beta.3+14.sha-99bdd25
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5437,7 +5437,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.3+13.sha-3db9d57');
+var VERSION = new core.Version('6.1.0-beta.3+14.sha-99bdd25');
 
 /**
  * @license
