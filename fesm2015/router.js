6feeb279c6c456b57bf7c0bbdabe32521430885e
Miško Hevery <misko@hevery.com>
2018-06-28 22:42:09 +0000

    0ede987 feat(ivy): Support resource resolution in JIT mode. (#24637)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+8.sha-01e7ff6
+ * @license Angular v6.1.0-beta.3+11.sha-0ede987
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6575,7 +6575,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.3+8.sha-01e7ff6');
+const VERSION = new Version('6.1.0-beta.3+11.sha-0ede987');
 
 /**
  * @fileoverview added by tsickle
