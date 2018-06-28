bb1f61e0b9057f68401ecb38af3cbf46427a75a8
Jason Aden <jasonaden@google.com>
2018-06-28 02:02:47 +0000

    c57b491 release: cut the v6.1.0-beta.3 release

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+55.sha-1dc7d0d
+ * @license Angular v6.1.0-beta.2+56.sha-c57b491
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+55.sha-1dc7d0d
+ * @license Angular v6.1.0-beta.2+56.sha-c57b491
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5437,7 +5437,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.2+55.sha-1dc7d0d');
+var VERSION = new core.Version('6.1.0-beta.2+56.sha-c57b491');
 
 /**
  * @license
