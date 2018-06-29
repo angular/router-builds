920626f96ba8cb6c92e792cd53143839879ebc0f
Misko Hevery <misko@hevery.com>
2018-06-29 14:35:42 +0000

    d243baf refactor(ivy): remove pChild from LNode (#24705)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+19.sha-ff84c5c
+ * @license Angular v6.1.0-beta.3+20.sha-d243baf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5379,7 +5379,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.3+19.sha-ff84c5c');
+var VERSION = new core.Version('6.1.0-beta.3+20.sha-d243baf');
 
 /**
  * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+19.sha-ff84c5c
+ * @license Angular v6.1.0-beta.3+20.sha-d243baf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6575,7 +6575,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.3+19.sha-ff84c5c');
+const VERSION = new Version('6.1.0-beta.3+20.sha-d243baf');
 
 /**
  * @fileoverview added by tsickle
