72b3540625b1e4f2c6737748c4c3a7030b1cdb2b
Misko Hevery <misko@hevery.com>
2018-06-29 17:43:54 +0000

    d243baf refactor(ivy): remove pChild from LNode (#24705)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+16.sha-13d60ea
+ * @license Angular v6.1.0-beta.3+20.sha-d243baf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4933,7 +4933,7 @@ var RouterModule_1;
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.3+16.sha-13d60ea');
+const VERSION = new Version('6.1.0-beta.3+20.sha-d243baf');
 
 /**
  * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+16.sha-13d60ea
+ * @license Angular v6.1.0-beta.3+20.sha-d243baf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5347,7 +5347,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.3+16.sha-13d60ea');
+var VERSION = new Version('6.1.0-beta.3+20.sha-d243baf');
 
 /**
  * @license
