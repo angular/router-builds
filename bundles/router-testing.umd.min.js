1477521a04032e2ea1c6c89a9e1423de0aa8274a
Misko Hevery <misko@hevery.com>
2018-06-29 14:23:31 +0000

    9803cb0 feat(ivy): Add InheritanceDefinitionFeature to support directive inheritance (#24570)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+16.sha-13d60ea
+ * @license Angular v6.1.0-beta.3+17.sha-9803cb0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+16.sha-13d60ea
+ * @license Angular v6.1.0-beta.3+17.sha-9803cb0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6575,7 +6575,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.3+16.sha-13d60ea');
+const VERSION = new Version('6.1.0-beta.3+17.sha-9803cb0');
 
 /**
  * @fileoverview added by tsickle
