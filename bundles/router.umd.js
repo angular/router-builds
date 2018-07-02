6b289a618565b9a3ada44e9f382173e683335fe1
Miško Hevery <misko@hevery.com>
2018-07-02 22:44:31 +0000

    a294e0d fix(ivy): correct position for re-projected containers (#24721)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+20.sha-d243baf
+ * @license Angular v6.1.0-beta.3+25.sha-a294e0d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5437,7 +5437,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.3+20.sha-d243baf');
+var VERSION = new core.Version('6.1.0-beta.3+25.sha-a294e0d');
 
 /**
  * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+20.sha-d243baf
+ * @license Angular v6.1.0-beta.3+25.sha-a294e0d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4933,7 +4933,7 @@ var RouterModule_1;
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.3+20.sha-d243baf');
+const VERSION = new Version('6.1.0-beta.3+25.sha-a294e0d');
 
 /**
  * @license
