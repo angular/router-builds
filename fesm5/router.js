0016c8e9649f9262e5eb6bb41503d28900771835
Miško Hevery <misko@hevery.com>
2018-07-02 22:06:42 +0000

    d1805d0 docs: fix docregion in attribute directives for highlight directive (#23972)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+20.sha-d243baf
+ * @license Angular v6.1.0-beta.3+21.sha-d1805d0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5347,7 +5347,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.3+20.sha-d243baf');
+var VERSION = new Version('6.1.0-beta.3+21.sha-d1805d0');
 
 /**
  * @license
