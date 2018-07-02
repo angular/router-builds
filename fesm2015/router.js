6ba6e3318738b0e74f0eb39c54a1ef6eae01cc2d
Miško Hevery <misko@hevery.com>
2018-07-02 22:11:13 +0000

    1ae3f87 docs: update HTTP error test example again (#24701)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+22.sha-4e7a44c
+ * @license Angular v6.1.0-beta.3+23.sha-1ae3f87
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4933,7 +4933,7 @@ var RouterModule_1;
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.3+22.sha-4e7a44c');
+const VERSION = new Version('6.1.0-beta.3+23.sha-1ae3f87');
 
 /**
  * @license
