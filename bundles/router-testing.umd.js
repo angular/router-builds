45260693252adf447792dac96b553e96e45cf125
Miško Hevery <misko@hevery.com>
2018-07-03 22:58:59 +0000

    563e8e3 docs: fix documention for attributes directive (#24367)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+13.sha-dd931c7
+ * @license Angular v6.0.7+14.sha-563e8e3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+13.sha-dd931c7
+ * @license Angular v6.0.7+14.sha-563e8e3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5246,7 +5246,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.0.7+13.sha-dd931c7');
+var VERSION = new core.Version('6.0.7+14.sha-563e8e3');
 
 /**
  * @license
