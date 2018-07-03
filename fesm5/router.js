21d583e648b992b24031d2b3503c38f9207cd5a6
Miško Hevery <misko@hevery.com>
2018-07-03 16:26:53 +0000

    0922228 fix(core): use addCustomEqualityTester instead of overriding toEqual (#22983)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+27.sha-c94a2c9
+ * @license Angular v6.1.0-beta.3+28.sha-0922228
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5347,7 +5347,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.3+27.sha-c94a2c9');
+var VERSION = new Version('6.1.0-beta.3+28.sha-0922228');
 
 /**
  * @license
