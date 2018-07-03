c22c1871e92a164dab6b159855b990f1dcec9c40
Miško Hevery <misko@hevery.com>
2018-07-03 16:07:35 +0000

    c94a2c9 build(bazel): update to latest rules_typescript (#24737)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+26.sha-948e223
+ * @license Angular v6.1.0-beta.3+27.sha-c94a2c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5437,7 +5437,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.3+26.sha-948e223');
+var VERSION = new core.Version('6.1.0-beta.3+27.sha-c94a2c9');
 
 /**
  * @license
