fc08e495d2806b95237014ed07fa999758741823
Miško Hevery <misko@hevery.com>
2018-07-06 18:20:18 +0000

    b719905 docs: clarify faqs about services (#24079)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+38.sha-56a8533
+ * @license Angular v6.1.0-beta.3+39.sha-b719905
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4935,7 +4935,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.3+38.sha-56a8533');
+const VERSION = new Version('6.1.0-beta.3+39.sha-b719905');
 
 /**
  * @license
