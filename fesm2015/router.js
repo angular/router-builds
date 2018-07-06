bb8a405e106e69ce98d46943f7724582047dc959
Miško Hevery <misko@hevery.com>
2018-07-06 18:17:55 +0000

    56a8533 docs: add app.module to changed documents (#23876)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+37.sha-b72dbc8
+ * @license Angular v6.1.0-beta.3+38.sha-56a8533
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4935,7 +4935,7 @@ function provideRouterInitializer() {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.3+37.sha-b72dbc8');
+const VERSION = new Version('6.1.0-beta.3+38.sha-56a8533');
 
 /**
  * @license
