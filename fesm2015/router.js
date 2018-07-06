50e2a9e532ff2168a79b5a56d6e2842118b757e9
Miško Hevery <misko@hevery.com>
2018-07-06 21:48:02 +0000

    3980640 feat(ivy): properly apply style="", [style], [style.foo] and [attr.style] bindings (#24602)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+60.sha-52d43a9
+ * @license Angular v6.1.0-beta.3+61.sha-3980640
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4925,7 +4925,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.3+60.sha-52d43a9');
+const VERSION = new Version('6.1.0-beta.3+61.sha-3980640');
 
 /**
  * @license
