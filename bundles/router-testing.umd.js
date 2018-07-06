f8ff870ccb46b522e673b9258f14b42f581b2cee
Miško Hevery <misko@hevery.com>
2018-07-06 18:16:18 +0000

    b51ce62 docs(aio): added a link to Angular Zero online course (Traditional Chinese) (#24228)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+17.sha-69c8226
+ * @license Angular v6.0.7+19.sha-b51ce62
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+17.sha-69c8226
+ * @license Angular v6.0.7+19.sha-b51ce62
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6414,7 +6414,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.7+17.sha-69c8226');
+const VERSION = new Version('6.0.7+19.sha-b51ce62');
 
 /**
  * @fileoverview added by tsickle
