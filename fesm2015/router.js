863edab3bfa8dc2e15835cd67e461258eed0ab14
Miško Hevery <misko@hevery.com>
2018-07-06 18:24:45 +0000

    d5a9396 docs(changelog): correct inaccuracies (#24713) PR Close #24713

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+43.sha-3e6a722
+ * @license Angular v6.1.0-beta.3+44.sha-d5a9396
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6904,7 +6904,7 @@ function provideRouterInitializer() {
  * found in the LICENSE file at https://angular.io/license
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+43.sha-3e6a722');
+const VERSION = new Version('6.1.0-beta.3+44.sha-d5a9396');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+43.sha-3e6a722
+ * @license Angular v6.1.0-beta.3+44.sha-d5a9396
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
