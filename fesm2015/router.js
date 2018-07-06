5a967a347765a86d56c7428be76eb4747ddae54a
Miško Hevery <misko@hevery.com>
2018-07-06 18:26:22 +0000

    fddd2af test: integration test for TS 2.9.x (#24749)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+44.sha-d5a9396
+ * @license Angular v6.1.0-beta.3+45.sha-fddd2af
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6904,7 +6904,7 @@ function provideRouterInitializer() {
  * found in the LICENSE file at https://angular.io/license
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+44.sha-d5a9396');
+const VERSION = new Version('6.1.0-beta.3+45.sha-fddd2af');
 
 /**
  * @fileoverview added by tsickle
