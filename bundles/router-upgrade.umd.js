586d2135be08e59043486bc2e66583a118344349
Miško Hevery <misko@hevery.com>
2018-07-06 21:46:44 +0000

    3980640 feat(ivy): properly apply style="", [style], [style.foo] and [attr.style] bindings (#24602)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+60.sha-52d43a9
+ * @license Angular v6.1.0-beta.3+61.sha-3980640
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+60.sha-52d43a9
+ * @license Angular v6.1.0-beta.3+61.sha-3980640
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5357,7 +5357,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0-beta.3+60.sha-52d43a9');
+    var VERSION = new core.Version('6.1.0-beta.3+61.sha-3980640');
 
     /**
      * @license
