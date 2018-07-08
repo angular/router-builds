ea04eefbcb8c0a34b9080a4d21b9065ea44c931e
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-08 11:32:20 +0000

    2222bbc feat(ivy): write ngcc transformed files out to disk

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+80.sha-6c604bd
+ * @license Angular v6.1.0-beta.3+81.sha-2222bbc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5339,7 +5339,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-beta.3+80.sha-6c604bd');
+var VERSION = new Version('6.1.0-beta.3+81.sha-2222bbc');
 
 /**
  * @license
