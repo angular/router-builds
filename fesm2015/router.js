248fb1250b1d9cbafd037d327dbbb2afc21716be
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-09 10:32:56 +0000

    15ca58a feat(ivy): implement ngcc `Esm5ReflectionHost`

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+82.sha-ac61c99
+ * @license Angular v6.1.0-beta.3+83.sha-15ca58a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4925,7 +4925,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.3+82.sha-ac61c99');
+const VERSION = new Version('6.1.0-beta.3+83.sha-15ca58a');
 
 /**
  * @license
