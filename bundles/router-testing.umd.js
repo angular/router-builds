d60d10bd10d165522a40c9029410f4c4ff0c4aca
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-09 10:31:29 +0000

    15ca58a feat(ivy): implement ngcc `Esm5ReflectionHost`

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+82.sha-ac61c99
+ * @license Angular v6.1.0-beta.3+83.sha-15ca58a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+82.sha-ac61c99
+ * @license Angular v6.1.0-beta.3+83.sha-15ca58a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6711,7 +6711,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+82.sha-ac61c99');
+const VERSION = new Version('6.1.0-beta.3+83.sha-15ca58a');
 
 /**
  * @fileoverview added by tsickle
