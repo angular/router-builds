929d0928caa484e9c6bb13d1050d04bae8cd6b89
Miško Hevery <misko@hevery.com>
2018-07-10 19:27:45 +0000

    1089261 fix(core): mark NgModule as not the root if APP_ROOT is set to false (#24814)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+66.sha-ddb792d
+ * @license Angular v6.1.0-beta.3+67.sha-1089261
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5421,7 +5421,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0-beta.3+66.sha-ddb792d');
+    var VERSION = new core.Version('6.1.0-beta.3+67.sha-1089261');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+66.sha-ddb792d
+ * @license Angular v6.1.0-beta.3+67.sha-1089261
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4925,7 +4925,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.3+66.sha-ddb792d');
+const VERSION = new Version('6.1.0-beta.3+67.sha-1089261');
 
 /**
  * @license
