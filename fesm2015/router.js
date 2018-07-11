2873b250d23d0337f11dc00ddab267d6acc4bee9
Miško Hevery <misko@hevery.com>
2018-07-11 03:05:33 +0000

    05e3e4d docs(forms): update API reference for form validators (#24734)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+83.sha-ffb9dc6
+ * @license Angular v6.1.0-beta.3+87.sha-05e3e4d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6739,7 +6739,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+83.sha-ffb9dc6');
+const VERSION = new Version('6.1.0-beta.3+87.sha-05e3e4d');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+83.sha-ffb9dc6
+ * @license Angular v6.1.0-beta.3+87.sha-05e3e4d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5311,7 +5311,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-beta.3+83.sha-ffb9dc6');
+var VERSION = new Version('6.1.0-beta.3+87.sha-05e3e4d');
 
 /**
  * @license
