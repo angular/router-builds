9114a2650e4bd651f2dcb68080fd9082795f7414
Miško Hevery <misko@hevery.com>
2018-07-11 02:51:47 +0000

    50fbed8 docs: correct project definition (#24807)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+77.sha-7d27ecc
+ * @license Angular v6.1.0-beta.3+78.sha-50fbed8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5357,7 +5357,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0-beta.3+77.sha-7d27ecc');
+    var VERSION = new core.Version('6.1.0-beta.3+78.sha-50fbed8');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+77.sha-7d27ecc
+ * @license Angular v6.1.0-beta.3+78.sha-50fbed8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6711,7 +6711,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+77.sha-7d27ecc');
+const VERSION = new Version('6.1.0-beta.3+78.sha-50fbed8');
 
 /**
  * @fileoverview added by tsickle
