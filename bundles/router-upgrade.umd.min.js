d01967f1d165e668855b4a08e7744ce36d84fb65
Miško Hevery <misko@hevery.com>
2018-07-11 02:54:43 +0000

    a527c69 fix(common): do not round factional seconds (#24831)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+79.sha-8057664
+ * @license Angular v6.1.0-beta.3+80.sha-a527c69
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+79.sha-8057664
+ * @license Angular v6.1.0-beta.3+80.sha-a527c69
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5290,7 +5290,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-beta.3+79.sha-8057664');
+var VERSION = new Version('6.1.0-beta.3+80.sha-a527c69');
 
 /**
  * @license
