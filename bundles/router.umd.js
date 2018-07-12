302806cbde0975c20f01dae8aa72115a8c0e063f
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 21:45:09 +0000

    ee50ee4 build(bazel): try removing gazelle (#24787)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+95.sha-0399c69
+ * @license Angular v6.1.0-beta.3+98.sha-ee50ee4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5462,7 +5462,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0-beta.3+95.sha-0399c69');
+    var VERSION = new core.Version('6.1.0-beta.3+98.sha-ee50ee4');
 
     /**
      * @license
