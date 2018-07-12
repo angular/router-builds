56ac316f9edbc38f4568a225d84544ff63cf670f
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 19:13:20 +0000

    0d6b74d docs: fix typo in component architecture guide (#24832)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+91.sha-52d11f6
+ * @license Angular v6.1.0-beta.3+92.sha-0d6b74d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5442,7 +5442,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0-beta.3+91.sha-52d11f6');
+    var VERSION = new core.Version('6.1.0-beta.3+92.sha-0d6b74d');
 
     /**
      * @license
