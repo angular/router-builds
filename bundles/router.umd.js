4f24a15c50ac04cb01a4feab6edc192aa50a03d0
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 21:42:59 +0000

    0399c69 refactor(ivy): remove content query creation from directive factories (#24811)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+93.sha-4d8b8ad
+ * @license Angular v6.1.0-beta.3+95.sha-0399c69
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3468,6 +3468,17 @@
              * - `'always'`, enables unconditional inheritance of parent params.
              */
             this.paramsInheritanceStrategy = 'emptyOnly';
+            /**
+             * Defines when the router updates the browser URL. The default behavior is to update after
+             * successful navigation. However, some applications may prefer a mode where the URL gets
+             * updated at the beginning of navigation. The most common use case would be updating the
+             * URL early so if navigation fails, you can show an error message with the URL that failed.
+             * Available options are:
+             *
+             * - `'deferred'`, the default, updates the browser URL after navigation has finished.
+             * - `'eager'`, updates browser URL at the beginning of navigation.
+             */
+            this.urlUpdateStrategy = 'deferred';
             var onLoadStart = function (r) { return _this.triggerEvent(new RouteConfigLoadStart(r)); };
             var onLoadEnd = function (r) { return _this.triggerEvent(new RouteConfigLoadEnd(r)); };
             this.ngModule = injector.get(core.NgModuleRef);
@@ -3763,6 +3774,9 @@
             var urlTransition = !this.navigated || url.toString() !== this.currentUrlTree.toString();
             if ((this.onSameUrlNavigation === 'reload' ? true : urlTransition) &&
                 this.urlHandlingStrategy.shouldProcessUrl(rawUrl)) {
+                if (this.urlUpdateStrategy === 'eager' && !extras.skipLocationChange) {
+                    this.setBrowserUrl(rawUrl, !!extras.replaceUrl, id);
+                }
                 this.events
                     .next(new NavigationStart(id, this.serializeUrl(url), source, state));
                 Promise.resolve()
@@ -3903,14 +3917,8 @@
                 _this.currentUrlTree = appliedUrl;
                 _this.rawUrlTree = _this.urlHandlingStrategy.merge(_this.currentUrlTree, rawUrl);
                 _this.routerState = state;
-                if (!skipLocationChange) {
-                    var path = _this.urlSerializer.serialize(_this.rawUrlTree);
-                    if (_this.location.isCurrentPathEqualTo(path) || replaceUrl) {
-                        _this.location.replaceState(path, '', { navigationId: id });
-                    }
-                    else {
-                        _this.location.go(path, '', { navigationId: id });
-                    }
+                if (_this.urlUpdateStrategy === 'deferred' && !skipLocationChange) {
+                    _this.setBrowserUrl(_this.rawUrlTree, replaceUrl, id);
                 }
                 new ActivateRoutes(_this.routeReuseStrategy, state, storedState, function (evt) { return _this.triggerEvent(evt); })
                     .activate(_this.rootContexts);
@@ -3950,6 +3958,15 @@
                     }
                 }
             });
+        };
+        Router.prototype.setBrowserUrl = function (url, replaceUrl, id) {
+            var path = this.urlSerializer.serialize(url);
+            if (this.location.isCurrentPathEqualTo(path) || replaceUrl) {
+                this.location.replaceState(path, '', { navigationId: id });
+            }
+            else {
+                this.location.go(path, '', { navigationId: id });
+            }
         };
         Router.prototype.resetStateAndUrl = function (storedState, storedUrl, rawUrl) {
             this.routerState = storedState;
@@ -5317,6 +5334,9 @@
         if (opts.paramsInheritanceStrategy) {
             router.paramsInheritanceStrategy = opts.paramsInheritanceStrategy;
         }
+        if (opts.urlUpdateStrategy) {
+            router.urlUpdateStrategy = opts.urlUpdateStrategy;
+        }
         return router;
     }
     function rootRoute(router) {
@@ -5442,7 +5462,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0-beta.3+93.sha-4d8b8ad');
+    var VERSION = new core.Version('6.1.0-beta.3+95.sha-0399c69');
 
     /**
      * @license
