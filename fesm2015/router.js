bd18542b86cd83fb83c07267fa628894567023a7
Matias Niemelä <matias@yearofmoo.com>
2018-07-13 19:40:44 +0000

    97277bc build: update to Bazel 0.15 (#24841)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+112.sha-1821b75
+ * @license Angular v6.1.0-beta.3+113.sha-97277bc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6765,7 +6765,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+112.sha-1821b75');
+const VERSION = new Version('6.1.0-beta.3+113.sha-97277bc');
 
 /**
  * @fileoverview added by tsickle
