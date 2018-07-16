cc9ec1d95c9c119d113b92bad14098e1f0d82f64
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-16 10:08:59 +0000

    31b4043 fixup! refactor(ivy): allow `ImportManager` to have configurable prefix

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+129.sha-acdb672
+ * @license Angular v6.1.0-beta.3+130.sha-31b4043
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5462,7 +5462,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0-beta.3+129.sha-acdb672');
+    var VERSION = new core.Version('6.1.0-beta.3+130.sha-31b4043');
 
     /**
      * @license
