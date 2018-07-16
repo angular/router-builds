75e549ad3488677f7bf53d3d6684fb50f34846b6
Matias Niemelä <matias@yearofmoo.com>
2018-07-16 20:37:01 +0000

    e97d961 fix(compiler): i18n_extractor now outputs the correct source file name (#24885) for non-inline templates

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+10.sha-d42a662
+ * @license Angular v6.0.9+11.sha-e97d961
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5243,7 +5243,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.0.9+10.sha-d42a662');
+    var VERSION = new core.Version('6.0.9+11.sha-e97d961');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+10.sha-d42a662
+ * @license Angular v6.0.9+11.sha-e97d961
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
