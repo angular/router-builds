c1044e3dd2983a3f135ee00231ff79f929bce41f
Matias Niemelä <matias@yearofmoo.com>
2018-07-17 21:22:10 +0000

    0b28732 docs: typos in directives docs (#24665)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+119.sha-06a3398
+ * @license Angular v6.1.0-beta.3+120.sha-0b28732
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4966,7 +4966,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.3+119.sha-06a3398');
+const VERSION = new Version('6.1.0-beta.3+120.sha-0b28732');
 
 /**
  * @license
