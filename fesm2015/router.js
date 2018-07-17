5a1b30d751e4b61dfdbdd6c42548a1bd761559ad
Matias Niemelä <matias@yearofmoo.com>
2018-07-17 21:17:28 +0000

    06a3398 build: rename angular_devkit dependency to angular_cli (#24842)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+118.sha-ba3eb8b
+ * @license Angular v6.1.0-beta.3+119.sha-06a3398
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6765,7 +6765,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+118.sha-ba3eb8b');
+const VERSION = new Version('6.1.0-beta.3+119.sha-06a3398');
 
 /**
  * @fileoverview added by tsickle
