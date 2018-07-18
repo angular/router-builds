faab68291ad138529ba130325d96023ad27a51ef
Matias Niemelä <matias@yearofmoo.com>
2018-07-18 22:54:47 +0000

    d76531d fix(animations): @internal must use JSDoc tags. (#24928)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+121.sha-23dc9a9
+ * @license Angular v6.1.0-beta.3+122.sha-d76531d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+121.sha-23dc9a9
+ * @license Angular v6.1.0-beta.3+122.sha-d76531d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4966,7 +4966,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.3+121.sha-23dc9a9');
+const VERSION = new Version('6.1.0-beta.3+122.sha-d76531d');
 
 /**
  * @license
