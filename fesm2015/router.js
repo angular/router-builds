f6d31825ec688bd71747b0e679416f1ebe804785
Matias Niemelä <matias@yearofmoo.com>
2018-07-18 18:36:59 +0000

    23dc9a9 docs: fix typo in user input guide (#22630)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+120.sha-0b28732
+ * @license Angular v6.1.0-beta.3+121.sha-23dc9a9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6765,7 +6765,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+120.sha-0b28732');
+const VERSION = new Version('6.1.0-beta.3+121.sha-23dc9a9');
 
 /**
  * @fileoverview added by tsickle
