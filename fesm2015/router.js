fdf25b1f4decf30069d7fea1a4341d7cf8e5f1d7
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-18 14:22:47 +0000

    082c994 fixup! feat(ivy): implement esm2015 and esm5 reflection hosts

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+130.sha-ee37256
+ * @license Angular v6.1.0-beta.3+142.sha-082c994
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6765,7 +6765,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+130.sha-ee37256');
+const VERSION = new Version('6.1.0-beta.3+142.sha-082c994');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+130.sha-ee37256
+ * @license Angular v6.1.0-beta.3+142.sha-082c994
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5331,7 +5331,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-beta.3+130.sha-ee37256');
+var VERSION = new Version('6.1.0-beta.3+142.sha-082c994');
 
 /**
  * @license
