cfcbabcec181d1d76a27a0b4c07473baf08f773a
Matias Niemelä <matias@yearofmoo.com>
2018-07-19 22:07:04 +0000

    d76531d fix(animations): @internal must use JSDoc tags. (#24928)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+117.sha-c8ad965
+ * @license Angular v6.1.0-beta.3+122.sha-d76531d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5462,7 +5462,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0-beta.3+117.sha-c8ad965');
+    var VERSION = new core.Version('6.1.0-beta.3+122.sha-d76531d');
 
     /**
      * @license
