e173024990f67f9ead8380756206a00372db617c
Victor Berchet <victor@suumit.com>
2018-07-20 18:41:21 +0000

    740ab7c build(docs-infra): add support for examples of type `elements` (#24840)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+16.sha-422de27
+ * @license Angular v6.0.9+22.sha-740ab7c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6269,7 +6269,7 @@ function provideRouterInitializer() {
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
  */
-const /** @type {?} */ VERSION = new Version('6.0.9+16.sha-422de27');
+const /** @type {?} */ VERSION = new Version('6.0.9+22.sha-740ab7c');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+16.sha-422de27
+ * @license Angular v6.0.9+22.sha-740ab7c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5176,7 +5176,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.0.9+16.sha-422de27');
+var VERSION = new Version('6.0.9+22.sha-740ab7c');
 
 /**
  * @license
