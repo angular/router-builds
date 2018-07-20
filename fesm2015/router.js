71580b92ba90c67b2fef238f054c52c6e942bf14
Victor Berchet <victor@suumit.com>
2018-07-20 19:30:18 +0000

    23766b8 build: Fix windows tests (#24927)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+13.sha-2d38fa1
+ * @license Angular v6.1.0-rc.3+15.sha-23766b8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6765,7 +6765,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3+13.sha-2d38fa1');
+const VERSION = new Version('6.1.0-rc.3+15.sha-23766b8');
 
 /**
  * @fileoverview added by tsickle
