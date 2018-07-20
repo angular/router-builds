2a88cd6de39e62002e0fe940006272ce7b0dbee7
Victor Berchet <victor@suumit.com>
2018-07-20 19:59:15 +0000

    bb58138 docs: fix bad link (#24825)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+16.sha-b8f740b
+ * @license Angular v6.1.0-rc.3+17.sha-bb58138
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5462,7 +5462,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0-rc.3+16.sha-b8f740b');
+    var VERSION = new core.Version('6.1.0-rc.3+17.sha-bb58138');
 
     /**
      * @license
