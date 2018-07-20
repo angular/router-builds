14e2983537a2eecac2ca022e2b7f8c1f1cd2ae76
Victor Berchet <victor@suumit.com>
2018-07-20 19:40:22 +0000

    b8f740b docs: remove closing parenthesis from provides guide (#24935)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+15.sha-23766b8
+ * @license Angular v6.1.0-rc.3+16.sha-b8f740b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6765,7 +6765,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3+15.sha-23766b8');
+const VERSION = new Version('6.1.0-rc.3+16.sha-b8f740b');
 
 /**
  * @fileoverview added by tsickle
