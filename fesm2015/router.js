c96ab451f84e1870acdcf70f3817d63b6f13d1f7
Victor Berchet <victor@suumit.com>
2018-07-20 19:25:24 +0000

    c438b5e build(bazel): turn on preserve-symlinks (#24881)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+8.sha-1c533c9
+ * @license Angular v6.1.0-rc.3+11.sha-c438b5e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6765,7 +6765,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3+8.sha-1c533c9');
+const VERSION = new Version('6.1.0-rc.3+11.sha-c438b5e');
 
 /**
  * @fileoverview added by tsickle
