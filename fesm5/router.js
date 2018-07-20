2306381a4225a1564777d41446abab6917a6fde4
Victor Berchet <victor@suumit.com>
2018-07-20 19:27:29 +0000

    2d38fa1 test(platform-webworker): avoid flakes due to existing `PlatformRef` (#24916)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+11.sha-c438b5e
+ * @license Angular v6.1.0-rc.3+13.sha-2d38fa1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5331,7 +5331,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-rc.3+11.sha-c438b5e');
+var VERSION = new Version('6.1.0-rc.3+13.sha-2d38fa1');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+11.sha-c438b5e
+ * @license Angular v6.1.0-rc.3+13.sha-2d38fa1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
