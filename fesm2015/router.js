469d138bfea8bfbabfede5c775dd0abe6924de88
Victor Berchet <victor@suumit.com>
2018-07-20 20:27:29 +0000

    7960d18 docs: technical review incorporated (#24744)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+41.sha-8620373
+ * @license Angular v6.1.0-rc.3+43.sha-7960d18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6765,7 +6765,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3+41.sha-8620373');
+const VERSION = new Version('6.1.0-rc.3+43.sha-7960d18');
 
 /**
  * @fileoverview added by tsickle
