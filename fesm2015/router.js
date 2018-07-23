096f3388b6eaaa94a5bb0d8090a2d96ff3b1bd7f
Victor Berchet <victor@suumit.com>
2018-07-23 20:43:08 +0000

    6eca367 docs: tests for number/percent/currency pipe (#25028)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+39.sha-060867d
+ * @license Angular v6.0.9+40.sha-6eca367
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6269,7 +6269,7 @@ function provideRouterInitializer() {
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
  */
-const /** @type {?} */ VERSION = new Version('6.0.9+39.sha-060867d');
+const /** @type {?} */ VERSION = new Version('6.0.9+40.sha-6eca367');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+39.sha-060867d
+ * @license Angular v6.0.9+40.sha-6eca367
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
