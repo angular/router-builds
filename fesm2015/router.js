2fb707b2525f4c5a5266156f196f15974c2de235
Victor Berchet <victor@suumit.com>
2018-07-23 17:27:43 +0000

    bd576bb docs: fix multicasting example in observable guide (#24911)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+47.sha-7729bb2
+ * @license Angular v6.1.0-rc.3+49.sha-bd576bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4966,7 +4966,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-rc.3+47.sha-7729bb2');
+const VERSION = new Version('6.1.0-rc.3+49.sha-bd576bb');
 
 /**
  * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+47.sha-7729bb2
+ * @license Angular v6.1.0-rc.3+49.sha-bd576bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
