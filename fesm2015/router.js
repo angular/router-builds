8d54b3e27d3213e200c7838868ed561d22c8756c
Victor Berchet <victor@suumit.com>
2018-07-23 17:20:12 +0000

    7f3a5ec docs: square odds example in rxjs guide (#24947)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+31.sha-68df0d2
+ * @license Angular v6.0.9+35.sha-7f3a5ec
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6269,7 +6269,7 @@ function provideRouterInitializer() {
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
  */
-const /** @type {?} */ VERSION = new Version('6.0.9+31.sha-68df0d2');
+const /** @type {?} */ VERSION = new Version('6.0.9+35.sha-7f3a5ec');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+31.sha-68df0d2
+ * @license Angular v6.0.9+35.sha-7f3a5ec
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
