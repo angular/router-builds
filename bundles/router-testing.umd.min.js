c733cb2593b1f6d77e3ae2fd947f42e5560d481f
Victor Berchet <victor@suumit.com>
2018-07-23 17:24:28 +0000

    ac7658e docs: fix multicasting example in observable guide (#24911)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+36.sha-ac52e1a
+ * @license Angular v6.0.9+34.sha-ac7658e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+36.sha-ac52e1a
+ * @license Angular v6.0.9+34.sha-ac7658e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5243,7 +5243,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.0.9+36.sha-ac52e1a');
+    var VERSION = new core.Version('6.0.9+34.sha-ac7658e');
 
     /**
      * @license
