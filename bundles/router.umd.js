67a47269dbc890ac544060b8272d5fd77aeaf3f6
Victor Berchet <victor@suumit.com>
2018-07-23 17:19:44 +0000

    4263245 docs: update rxjs link to version 6 (#24269)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+44.sha-6b859da
+ * @license Angular v6.1.0-rc.3+46.sha-4263245
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5462,7 +5462,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0-rc.3+44.sha-6b859da');
+    var VERSION = new core.Version('6.1.0-rc.3+46.sha-4263245');
 
     /**
      * @license
