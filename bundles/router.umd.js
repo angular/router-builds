b6415ec728b0a3769557a4da4ebf2dc187295157
Victor Berchet <victor@suumit.com>
2018-07-23 17:51:52 +0000

    6b859da fix(core): stop reusing provider definitions across NgModuleRef instances (#25022)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+122.sha-d76531d
+ * @license Angular v6.1.0-rc.3+44.sha-6b859da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5462,7 +5462,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0-beta.3+122.sha-d76531d');
+    var VERSION = new core.Version('6.1.0-rc.3+44.sha-6b859da');
 
     /**
      * @license
