732e8538af08460720d4cfa37b83479e7b4afb73
Victor Berchet <victor@suumit.com>
2018-07-23 17:22:00 +0000

    7729bb2 docs: fix instructions for switching directories (#24439)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+46.sha-4263245
+ * @license Angular v6.1.0-rc.3+47.sha-7729bb2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5256,7 +5256,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.0-rc.3+46.sha-4263245');
+    var VERSION = new i0.Version('6.1.0-rc.3+47.sha-7729bb2');
 
     /**
      * @license
