5fae4ae8137ed806afaaf0072e6f976d8fd6f77a
Victor Berchet <victor@suumit.com>
2018-07-23 16:55:45 +0000

    68df0d2 docs: update rxjs link to version 6 (#24269)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+30.sha-d3656bc
+ * @license Angular v6.0.9+31.sha-68df0d2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6269,7 +6269,7 @@ function provideRouterInitializer() {
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
  */
-const /** @type {?} */ VERSION = new Version('6.0.9+30.sha-d3656bc');
+const /** @type {?} */ VERSION = new Version('6.0.9+31.sha-68df0d2');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+30.sha-d3656bc
+ * @license Angular v6.0.9+31.sha-68df0d2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
