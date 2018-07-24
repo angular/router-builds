46ac003dcd08dc56926fb87b11502e3510bbb217
Victor Berchet <victor@suumit.com>
2018-07-24 23:31:52 +0000

    c7d147e Revert "build: update to newer circleCI bazel remote cache proxy (#25054)" (#25076)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+32.sha-6a74ef0
+ * @license Angular v6.0.9+44.sha-c7d147e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5176,7 +5176,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.0.9+32.sha-6a74ef0');
+var VERSION = new Version('6.0.9+44.sha-c7d147e');
 
 /**
  * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+32.sha-6a74ef0
+ * @license Angular v6.0.9+44.sha-c7d147e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
