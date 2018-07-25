0d7e10adfbbf566e60b40f894921a2dbf3453e86
Victor Berchet <victor@suumit.com>
2018-07-25 18:19:56 +0000

    0bcf20c docs(animations): typo fix in the comments (#22652)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+71.sha-cf81823
+ * @license Angular v6.1.0-rc.3+72.sha-0bcf20c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5398,7 +5398,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0-rc.3+71.sha-cf81823');
+    var VERSION = new core.Version('6.1.0-rc.3+72.sha-0bcf20c');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+71.sha-cf81823
+ * @license Angular v6.1.0-rc.3+72.sha-0bcf20c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6765,7 +6765,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3+71.sha-cf81823');
+const VERSION = new Version('6.1.0-rc.3+72.sha-0bcf20c');
 
 /**
  * @fileoverview added by tsickle
