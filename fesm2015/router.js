72c079bbf7a90ef84cf10869dfe0183c1c6b168f
Victor Berchet <victor@suumit.com>
2018-07-25 18:39:09 +0000

    1e28495 fix(ivy): update compiler with latest runtime for view queries (#25061)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+72.sha-0bcf20c
+ * @license Angular v6.1.0-rc.3+73.sha-1e28495
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4775,7 +4775,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-rc.3+72.sha-0bcf20c');
+const VERSION = new Version('6.1.0-rc.3+73.sha-1e28495');
 
 /**
  * @license
