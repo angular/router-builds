d9da6abebff83d1888b90cc22b5c10e2aa85e3ac
Victor Berchet <victor@suumit.com>
2018-07-25 04:38:59 +0000

    65e18dc docs: refactor style guide example 03-06 (#24996)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+66.sha-d0c066a
+ * @license Angular v6.1.0-rc.3+65.sha-65e18dc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6765,7 +6765,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3+66.sha-d0c066a');
+const VERSION = new Version('6.1.0-rc.3+65.sha-65e18dc');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+66.sha-d0c066a
+ * @license Angular v6.1.0-rc.3+65.sha-65e18dc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
