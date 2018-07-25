e04249282bdc907bb7ff0aa3d7afa6a5526733fa
Victor Berchet <victor@suumit.com>
2018-07-25 04:38:40 +0000

    d0c066a docs: replaced old angular/http example (#25065) PR Close #25065

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+64.sha-1ceddb6
+ * @license Angular v6.1.0-rc.3+66.sha-d0c066a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6765,7 +6765,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3+64.sha-1ceddb6');
+const VERSION = new Version('6.1.0-rc.3+66.sha-d0c066a');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+64.sha-1ceddb6
+ * @license Angular v6.1.0-rc.3+66.sha-d0c066a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
