8d5c5a570c5f944d15b906b1b3d917e1f2649b48
Victor Berchet <victor@suumit.com>
2018-07-25 22:29:47 +0000

    48d7205 release: cut the v6.1.0 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+75.sha-e1c6fd5
+ * @license Angular v6.1.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3+75.sha-e1c6fd5');
+const VERSION = new Version('6.1.0');
 
 /**
  * @fileoverview added by tsickle
