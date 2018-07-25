0d302fe83751bfb49796845d1804c7bdb991e41d
Victor Berchet <victor@suumit.com>
2018-07-25 04:39:57 +0000

    d0c066a docs: replaced old angular/http example (#25065) PR Close #25065

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+64.sha-1ceddb6
+ * @license Angular v6.1.0-rc.3+66.sha-d0c066a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5462,7 +5462,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0-rc.3+64.sha-1ceddb6');
+    var VERSION = new core.Version('6.1.0-rc.3+66.sha-d0c066a');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+64.sha-1ceddb6
+ * @license Angular v6.1.0-rc.3+66.sha-d0c066a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4966,7 +4966,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-rc.3+64.sha-1ceddb6');
+const VERSION = new Version('6.1.0-rc.3+66.sha-d0c066a');
 
 /**
  * @license
