08565d81b2a5c7cbaca2c1b79125987e3e14160e
Igor Minar <igor@angular.io>
2018-07-27 19:29:21 +0000

    3426784 test: fix typings for `DoneFn` (#25163)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+17.sha-e8d4211
+ * @license Angular v6.1.0+18.sha-3426784
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+17.sha-e8d4211');
+const VERSION = new Version('6.1.0+18.sha-3426784');
 
 /**
  * @fileoverview added by tsickle
