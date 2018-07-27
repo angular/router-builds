b4e87748dd20d7266f2da984936c7958026ba977
Igor Minar <igor@angular.io>
2018-07-27 17:54:30 +0000

    a3cf61b docs: refactor feature modules example (#25069)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+14.sha-a1b185b
+ * @license Angular v6.1.0+15.sha-a3cf61b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+14.sha-a1b185b');
+const VERSION = new Version('6.1.0+15.sha-a3cf61b');
 
 /**
  * @fileoverview added by tsickle
