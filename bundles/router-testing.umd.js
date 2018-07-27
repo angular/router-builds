0091e7024b69ea8c1fff5e86398fd6f40148e78b
Igor Minar <igor@angular.io>
2018-07-27 19:30:41 +0000

    3426784 test: fix typings for `DoneFn` (#25163)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+17.sha-e8d4211
+ * @license Angular v6.1.0+18.sha-3426784
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+17.sha-e8d4211
+ * @license Angular v6.1.0+18.sha-3426784
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0+17.sha-e8d4211');
+    var VERSION = new core.Version('6.1.0+18.sha-3426784');
 
     /**
      * @license
