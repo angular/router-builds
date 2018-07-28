b9db84a912e5bc4126b91f0517baf5b899a9d9b6
Igor Minar <igor@angular.io>
2018-07-28 01:05:16 +0000

    6a797d5 refactor(ivy): `element` and `ElementStart` retuns `void` (#25173)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+41.sha-f82b6b2
+ * @license Angular v6.1.0+43.sha-6a797d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+41.sha-f82b6b2');
+const VERSION = new Version('6.1.0+43.sha-6a797d5');
 
 /**
  * @fileoverview added by tsickle
