b361af5abceeece024d56ed123dcdd4d76f76e5b
Igor Minar <igor@angular.io>
2018-07-28 01:06:58 +0000

    f82b6b2 build(bazel): fix typos in comments (#25172)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+40.sha-a87d44c
+ * @license Angular v6.1.0+41.sha-f82b6b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5276,7 +5276,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.0+40.sha-a87d44c');
+    var VERSION = new i0.Version('6.1.0+41.sha-f82b6b2');
 
     /**
      * @license
