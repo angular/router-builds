175f9e3feeda30388663ad248a2d0a671dc5a8d1
Igor Minar <igor@angular.io>
2018-07-30 23:44:35 +0000

    a972c03 docs: fix typo in dependency injection guide (#24972)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+12.sha-f5e1802
+ * @license Angular v6.1.0+13.sha-a972c03
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+12.sha-f5e1802');
+const VERSION = new Version('6.1.0+13.sha-a972c03');
 
 /**
  * @fileoverview added by tsickle
