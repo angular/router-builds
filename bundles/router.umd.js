782edb630686ccd759ce0267c5df48e6484ac4fd
Igor Minar <igor@angular.io>
2018-07-30 23:54:43 +0000

    22ebd53 docs: update bootstrapping and entry component guide to use httpclient (#25178)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+13.sha-a972c03
+ * @license Angular v6.1.0+14.sha-22ebd53
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5276,7 +5276,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.0+13.sha-a972c03');
+    var VERSION = new i0.Version('6.1.0+14.sha-22ebd53');
 
     /**
      * @license
