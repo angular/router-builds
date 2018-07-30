55f8e6f187b35bdd80cd96ab3df9f2d15b525bfd
Igor Minar <igor@angular.io>
2018-07-30 23:41:24 +0000

    4d5167e docs: update bootstrapping and entry component guide to use httpclient (#25178)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+47.sha-2ef777b
+ * @license Angular v6.1.0+49.sha-4d5167e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4979,7 +4979,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0+47.sha-2ef777b');
+const VERSION = new Version('6.1.0+49.sha-4d5167e');
 
 /**
  * @license
