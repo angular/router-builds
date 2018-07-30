aa415a18f21982b7d53a4c3800019cc8a14ffa0e
Igor Minar <igor@angular.io>
2018-07-30 22:27:51 +0000

    f5e1802 docs: refactor pipe example to use the HttpClient (#22741)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+11.sha-317c708
+ * @license Angular v6.1.0+12.sha-f5e1802
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0+11.sha-317c708');
+    var VERSION = new core.Version('6.1.0+12.sha-f5e1802');
 
     /**
      * @license
