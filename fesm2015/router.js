c62dddab31fd38dcfc0c6beb716c86fb11da3c27
Igor Minar <igor@angular.io>
2018-07-30 23:36:54 +0000

    2ef777b fix(ivy): convert context code into a tree-shakable instruction (#24943)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+44.sha-9a6d26e
+ * @license Angular v6.1.0+47.sha-2ef777b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4979,7 +4979,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0+44.sha-9a6d26e');
+const VERSION = new Version('6.1.0+47.sha-2ef777b');
 
 /**
  * @license
