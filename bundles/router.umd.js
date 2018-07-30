0059dba9e349e8072f5352008781363ff4e816a5
Igor Minar <igor@angular.io>
2018-07-30 23:37:46 +0000

    2ef777b fix(ivy): convert context code into a tree-shakable instruction (#24943)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+44.sha-9a6d26e
+ * @license Angular v6.1.0+47.sha-2ef777b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5276,7 +5276,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.0+44.sha-9a6d26e');
+    var VERSION = new i0.Version('6.1.0+47.sha-2ef777b');
 
     /**
      * @license
