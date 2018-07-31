9ecb51faed203a8e1ec01ae126950bf0f4def6cb
Igor Minar <igor@angular.io>
2018-07-31 00:42:24 +0000

    e99d860 feat(compiler): add "original" placeholder value on extracted XMB (#25079)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+51.sha-24789e9
+ * @license Angular v6.1.0+52.sha-e99d860
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+51.sha-24789e9');
+const VERSION = new Version('6.1.0+52.sha-e99d860');
 
 /**
  * @fileoverview added by tsickle
