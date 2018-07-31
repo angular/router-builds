ec2fbab5f748083aabefb3da22c9069d84cc148a
Igor Minar <igor@angular.io>
2018-07-31 20:57:54 +0000

    fa59748 build: make `postinstall` script compatible with Windows (#25232)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+21.sha-fa38828
+ * @license Angular v6.1.0+20.sha-fa59748
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+21.sha-fa38828');
+const VERSION = new Version('6.1.0+20.sha-fa59748');
 
 /**
  * @fileoverview added by tsickle
