3983388dad9d56434ae7a42aac1c9019dd9ed014
Igor Minar <igor@angular.io>
2018-07-31 05:34:50 +0000

    e3834b7 feat(ivy): support change detection on the root view (#25085)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+56.sha-3f20a2f
+ * @license Angular v6.1.0+55.sha-e3834b7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4979,7 +4979,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0+56.sha-3f20a2f');
+const VERSION = new Version('6.1.0+55.sha-e3834b7');
 
 /**
  * @license
