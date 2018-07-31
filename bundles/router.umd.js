34681e22fb18e0d85510b7756c68f3f3e9e7f110
Igor Minar <igor@angular.io>
2018-07-31 00:09:41 +0000

    efc6684 docs: fix typo in dependency injection guide (#24972)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+49.sha-4d5167e
+ * @license Angular v6.1.0+48.sha-efc6684
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5276,7 +5276,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.0+49.sha-4d5167e');
+    var VERSION = new i0.Version('6.1.0+48.sha-efc6684');
 
     /**
      * @license
