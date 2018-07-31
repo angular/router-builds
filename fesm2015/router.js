84505e1873d1ade82186830f8378ce213ce38412
Igor Minar <igor@angular.io>
2018-07-31 00:37:12 +0000

    24789e9 docs(aio): add StrongBrew to the trainer list (#24891)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+50.sha-f94f964
+ * @license Angular v6.1.0+51.sha-24789e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+50.sha-f94f964');
+const VERSION = new Version('6.1.0+51.sha-24789e9');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+50.sha-f94f964
+ * @license Angular v6.1.0+51.sha-24789e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
