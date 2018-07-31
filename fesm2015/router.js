846ac7bb8443745f21e7ae790b393894f5946ac8
Igor Minar <igor@angular.io>
2018-07-31 22:45:48 +0000

    d4bf2da refactor(core): remove withBody from public testing API (#25171)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+20.sha-fa59748
+ * @license Angular v6.1.0+22.sha-d4bf2da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+20.sha-fa59748');
+const VERSION = new Version('6.1.0+22.sha-d4bf2da');
 
 /**
  * @fileoverview added by tsickle
