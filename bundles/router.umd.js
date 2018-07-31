21e4fc40e08631080ad9b42113c7ac0ec840c230
Igor Minar <igor@angular.io>
2018-07-31 05:33:29 +0000

    74964bd docs: fix link to "Override component providers" (#24967)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+16.sha-785fb5c
+ * @license Angular v6.1.0+17.sha-74964bd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5276,7 +5276,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.0+16.sha-785fb5c');
+    var VERSION = new i0.Version('6.1.0+17.sha-74964bd');
 
     /**
      * @license
