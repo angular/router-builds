49a357ea8edf3544eb002828f3dff5af2749cab9
Igor Minar <igor@angular.io>
2018-08-01 20:28:38 +0000

    1dd2eaa docs: fix typos and missing word in tutorial (#20764)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+73.sha-166ddaa
+ * @license Angular v6.1.0+80.sha-1dd2eaa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+73.sha-166ddaa');
+const VERSION = new Version('6.1.0+80.sha-1dd2eaa');
 
 /**
  * @fileoverview added by tsickle
