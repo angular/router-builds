a5e4c57d988aa8da94b0b5755646e8fe732c67bd
Igor Minar <igor@angular.io>
2018-08-01 20:25:07 +0000

    a9ecf4b docs: refactor lazy loading modules example (#25071)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+75.sha-01e6dab
+ * @license Angular v6.1.0+74.sha-a9ecf4b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5276,7 +5276,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.0+75.sha-01e6dab');
+    var VERSION = new i0.Version('6.1.0+74.sha-a9ecf4b');
 
     /**
      * @license
