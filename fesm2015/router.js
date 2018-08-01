3a55e217ef857f5662ac329f984393192c7d19f3
Igor Minar <igor@angular.io>
2018-08-01 20:08:38 +0000

    3ab31a4 docs: update to account for CLI changes (#25223)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+23.sha-43dcf77
+ * @license Angular v6.1.0+24.sha-3ab31a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+23.sha-43dcf77');
+const VERSION = new Version('6.1.0+24.sha-3ab31a4');
 
 /**
  * @fileoverview added by tsickle
