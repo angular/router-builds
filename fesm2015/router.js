0e1b82442b481d2b5d1957060e219960c4afcfe6
Igor Minar <igor@angular.io>
2018-08-01 20:18:13 +0000

    3d3a1a4 docs(aio): add Kevin Yang to GDE resources (#24791)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+31.sha-0452712
+ * @license Angular v6.1.0+33.sha-3d3a1a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+31.sha-0452712');
+const VERSION = new Version('6.1.0+33.sha-3d3a1a4');
 
 /**
  * @fileoverview added by tsickle
