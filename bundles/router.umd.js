670724a56dbf87480c752431102fd7f8ac855e6a
Igor Minar <igor@angular.io>
2018-08-01 20:09:43 +0000

    a83b9f7 docs(changelog): remove reverted feature entry (#25206)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+22.sha-d4bf2da
+ * @license Angular v6.1.0+26.sha-a83b9f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5276,7 +5276,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.0+22.sha-d4bf2da');
+    var VERSION = new i0.Version('6.1.0+26.sha-a83b9f7');
 
     /**
      * @license
