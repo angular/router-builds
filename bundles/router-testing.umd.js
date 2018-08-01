7859c3448740de48420f33dce13f46e66280399e
Igor Minar <igor@angular.io>
2018-08-01 20:10:14 +0000

    3ab31a4 docs: update to account for CLI changes (#25223)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+23.sha-43dcf77
+ * @license Angular v6.1.0+24.sha-3ab31a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+23.sha-43dcf77
+ * @license Angular v6.1.0+24.sha-3ab31a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0+23.sha-43dcf77');
+    var VERSION = new core.Version('6.1.0+24.sha-3ab31a4');
 
     /**
      * @license
