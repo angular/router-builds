3577d87af63d45f6474f0215e2799a7245ee002d
Igor Minar <igor@angular.io>
2018-08-01 20:27:31 +0000

    7a4fb44 docs(aio): add Kevin Yang to GDE resources (#24791)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+74.sha-a9ecf4b
+ * @license Angular v6.1.0+77.sha-7a4fb44
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+74.sha-a9ecf4b
+ * @license Angular v6.1.0+77.sha-7a4fb44
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+74.sha-a9ecf4b');
+const VERSION = new Version('6.1.0+77.sha-7a4fb44');
 
 /**
  * @fileoverview added by tsickle
