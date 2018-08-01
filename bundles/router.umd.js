c2f002786b2336183c47100fe0db065c1a7c1f2b
Igor Minar <igor@angular.io>
2018-08-01 20:09:33 +0000

    43dcf77 build(bazel): fix typo in protractor test target definition (#25235)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+26.sha-a83b9f7
+ * @license Angular v6.1.0+23.sha-43dcf77
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0+26.sha-a83b9f7');
+    var VERSION = new core.Version('6.1.0+23.sha-43dcf77');
 
     /**
      * @license
