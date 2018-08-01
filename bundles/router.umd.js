1b44255db4b9d5a73bb6560c08059d4145e61671
Igor Minar <igor@angular.io>
2018-08-01 20:24:26 +0000

    01e6dab fix(compiler-cli): correct realPath to realpath. (#25023)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+72.sha-1e53278
+ * @license Angular v6.1.0+75.sha-01e6dab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5276,7 +5276,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.0+72.sha-1e53278');
+    var VERSION = new i0.Version('6.1.0+75.sha-01e6dab');
 
     /**
      * @license
