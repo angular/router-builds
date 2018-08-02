e6cc5708ac06d4fd91a0614993886e9ac42bccd3
Igor Minar <igor@angular.io>
2018-08-02 00:41:33 +0000

    d46a961 docs(ivy): API doc tweaks (#25258)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+88.sha-20b4530
+ * @license Angular v6.1.0+89.sha-d46a961
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+88.sha-20b4530');
+const VERSION = new Version('6.1.0+89.sha-d46a961');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+88.sha-20b4530
+ * @license Angular v6.1.0+89.sha-d46a961
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
