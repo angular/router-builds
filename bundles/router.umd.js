b116be5dc100231d2534d4a7914ae89a9218e586
Igor Minar <igor@angular.io>
2018-08-02 00:28:54 +0000

    06a1974 docs: Update the link to the Jasmine docs (#25175)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+86.sha-3d7f555
+ * @license Angular v6.1.0+87.sha-06a1974
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0+86.sha-3d7f555');
+    var VERSION = new core.Version('6.1.0+87.sha-06a1974');
 
     /**
      * @license
