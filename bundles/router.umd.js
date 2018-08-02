98550c1e8dc785821b5cae52c431a1a813d41122
Igor Minar <igor@angular.io>
2018-08-02 16:29:10 +0000

    0a6434b test(common): TokenExtractor should extend HttpXsrfTokenExtractor in xsrf spec (#24649)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+48.sha-9ddf269
+ * @license Angular v6.1.0+51.sha-0a6434b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0+48.sha-9ddf269');
+    var VERSION = new core.Version('6.1.0+51.sha-0a6434b');
 
     /**
      * @license
