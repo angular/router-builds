b4b1f79ccc798c42ad7d6657ff23a3d80ab5ce39
Igor Minar <igor@angular.io>
2018-08-02 01:16:30 +0000

    a224df4 feat(docs-infra): support sending Google Analytics events (#25042)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+89.sha-d46a961
+ * @license Angular v6.1.0+90.sha-a224df4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0+89.sha-d46a961');
+    var VERSION = new core.Version('6.1.0+90.sha-a224df4');
 
     /**
      * @license
