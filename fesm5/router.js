3b699f9869e494a9c88299f6719496feb71a6991
Igor Minar <igor@angular.io>
2018-08-02 00:51:29 +0000

    1ef2eae feat(docs-infra): support sending Google Analytics events (#25042)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+44.sha-d5d034a
+ * @license Angular v6.1.0+45.sha-1ef2eae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5345,7 +5345,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0+44.sha-d5d034a');
+var VERSION = new Version('6.1.0+45.sha-1ef2eae');
 
 /**
  * @license
