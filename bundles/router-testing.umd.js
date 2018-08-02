69c94df21c33daaf999cdbb74b10b02450c91a98
Igor Minar <igor@angular.io>
2018-08-02 16:59:04 +0000

    e26c25a Revert "docs: refactor http module import for style guide app.module (#25001)" (#25263)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+51.sha-0a6434b
+ * @license Angular v6.1.0+52.sha-e26c25a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+51.sha-0a6434b
+ * @license Angular v6.1.0+52.sha-e26c25a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5276,7 +5276,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.0+51.sha-0a6434b');
+    var VERSION = new i0.Version('6.1.0+52.sha-e26c25a');
 
     /**
      * @license
