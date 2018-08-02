487d7142ef2c72ead7ed6375f46b942bd8c95572
Igor Minar <igor@angular.io>
2018-08-02 17:48:35 +0000

    183757d fix(core): fix tests for uninitialized @Output error (#19116)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+98.sha-74bce18
+ * @license Angular v6.1.0+100.sha-183757d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5276,7 +5276,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.0+98.sha-74bce18');
+    var VERSION = new i0.Version('6.1.0+100.sha-183757d');
 
     /**
      * @license
