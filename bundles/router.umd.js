1ef2cda77a897af82bc7bc49a4d63c8446e52d15
Kara Erickson <karakara@google.com>
2018-08-03 18:16:41 +0000

    0822dc7 feat(ivy): generate .ngfactory stubs if requested (#25176)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+5.sha-b38931b
+ * @license Angular v7.0.0-beta.0+9.sha-0822dc7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-beta.0+5.sha-b38931b');
+    var VERSION = new core.Version('7.0.0-beta.0+9.sha-0822dc7');
 
     /**
      * @license
