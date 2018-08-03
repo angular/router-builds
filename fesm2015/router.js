a4de9e3961419821254b4d16192cd5af1c5e16ba
Igor Minar <igor@angular.io>
2018-08-03 00:33:33 +0000

    183757d fix(core): fix tests for uninitialized @Output error (#19116)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+8.sha-3169edd
+ * @license Angular v6.1.0+100.sha-183757d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+8.sha-3169edd');
+const VERSION = new Version('6.1.0+100.sha-183757d');
 
 /**
  * @fileoverview added by tsickle
