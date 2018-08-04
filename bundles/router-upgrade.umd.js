3d5a04e7558e9ab27344a6ca0140ffabdeb175a9
Kara Erickson <karakara@google.com>
2018-08-04 22:24:54 +0000

    26a15cc build: skip ivy builds when not publishing (#25299)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+17.sha-b0d86c1
+ * @license Angular v7.0.0-beta.0+18.sha-26a15cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+17.sha-b0d86c1
+ * @license Angular v7.0.0-beta.0+18.sha-26a15cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.0+17.sha-b0d86c1');
+const VERSION = new Version('7.0.0-beta.0+18.sha-26a15cc');
 
 /**
  * @fileoverview added by tsickle
