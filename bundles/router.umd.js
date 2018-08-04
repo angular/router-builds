9ba371061d2ce505b29f7a8dc9e5b3820c9b03db
Kara Erickson <karakara@google.com>
2018-08-04 22:27:13 +0000

    26a15cc build: skip ivy builds when not publishing (#25299)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+17.sha-b0d86c1
+ * @license Angular v7.0.0-beta.0+18.sha-26a15cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5276,7 +5276,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('7.0.0-beta.0+17.sha-b0d86c1');
+    var VERSION = new i0.Version('7.0.0-beta.0+18.sha-26a15cc');
 
     /**
      * @license
