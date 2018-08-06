f8ce31b68042d236ba51b1a7cb7152ae05d1100c
Kara Erickson <karakara@google.com>
2018-08-06 19:43:55 +0000

    4eb8ac6 build: update Bazel to 0.16 (#25316)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+23.sha-ba1e25f
+ * @license Angular v7.0.0-beta.0+24.sha-4eb8ac6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4979,7 +4979,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.0+23.sha-ba1e25f');
+const VERSION = new Version('7.0.0-beta.0+24.sha-4eb8ac6');
 
 /**
  * @license
