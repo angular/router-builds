15840f187f257f4ba4d6c99568d6fc5868cd4cbb
Kara Erickson <karakara@google.com>
2018-08-06 22:25:10 +0000

    0c028a0 docs: remove code in universal hero detail component (#25215)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+12.sha-40904ce
+ * @license Angular v6.1.1+14.sha-0c028a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5345,7 +5345,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.1+12.sha-40904ce');
+var VERSION = new Version('6.1.1+14.sha-0c028a0');
 
 /**
  * @license
