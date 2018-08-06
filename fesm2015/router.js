b18b473021f7a041140f2e070f80d14b366b7e60
Kara Erickson <karakara@google.com>
2018-08-06 17:36:21 +0000

    478eca3 docs(aio): add Made with Angular (#21297)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+5.sha-2e16039
+ * @license Angular v6.1.1+6.sha-478eca3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4979,7 +4979,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.1+5.sha-2e16039');
+const VERSION = new Version('6.1.1+6.sha-478eca3');
 
 /**
  * @license
