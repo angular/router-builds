1a94d6aaebfc7a69b22934bf551879df056c5487
Kara Erickson <karakara@google.com>
2018-08-06 22:30:45 +0000

    66547d8 docs(core): clarify supported ViewChild selectors (#22784)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+14.sha-0c028a0
+ * @license Angular v6.1.1+17.sha-66547d8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.1+14.sha-0c028a0');
+    var VERSION = new core.Version('6.1.1+17.sha-66547d8');
 
     /**
      * @license
