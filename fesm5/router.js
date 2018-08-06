91cd2249d31646360edcd734bf080356535de042
Kara Erickson <karakara@google.com>
2018-08-06 20:08:39 +0000

    c67f1bb docs: several fixes for NPM package guide (#20186)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+24.sha-4eb8ac6
+ * @license Angular v7.0.0-beta.0+26.sha-c67f1bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5394,7 +5394,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.0+24.sha-4eb8ac6');
+var VERSION = new Version('7.0.0-beta.0+26.sha-c67f1bb');
 
 /**
  * @license
