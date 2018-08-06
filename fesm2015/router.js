87fe5daafc145b33d3fecd29cc0afcf929db7b5d
Kara Erickson <karakara@google.com>
2018-08-06 22:32:21 +0000

    8cbb836 docs: clarify heroes example (#21216)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+16.sha-6e7d5f0
+ * @license Angular v6.1.1+19.sha-8cbb836
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.1+16.sha-6e7d5f0');
+const VERSION = new Version('6.1.1+19.sha-8cbb836');
 
 /**
  * @fileoverview added by tsickle
