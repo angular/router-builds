a78a379042c4aeb031e3cc60a1c29c90af971356
Kara Erickson <karakara@google.com>
2018-08-06 22:54:04 +0000

    b8887dd docs: fix table in comparing observables guide (#22485)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+32.sha-7d006c5
+ * @license Angular v7.0.0-beta.0+34.sha-b8887dd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4979,7 +4979,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.0+32.sha-7d006c5');
+const VERSION = new Version('7.0.0-beta.0+34.sha-b8887dd');
 
 /**
  * @license
