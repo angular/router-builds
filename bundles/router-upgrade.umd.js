5145ac9baaacc07a9ca25eb81ec98f90e1869d1b
Kara Erickson <karakara@google.com>
2018-08-06 19:32:08 +0000

    a57f8a1 build: update Bazel to 0.16 (#25316)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+9.sha-e4acd83
+ * @license Angular v6.1.1+8.sha-a57f8a1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+9.sha-e4acd83
+ * @license Angular v6.1.1+8.sha-a57f8a1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.1+9.sha-e4acd83');
+const VERSION = new Version('6.1.1+8.sha-a57f8a1');
 
 /**
  * @fileoverview added by tsickle
