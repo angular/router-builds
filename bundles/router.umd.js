d880f7cac3b89e613173d8a8f22292f22c875af3
Kara Erickson <karakara@google.com>
2018-08-06 19:32:53 +0000

    e4acd83 docs(http): fixed example unit test for error catching (#25306)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+7.sha-ae9b4e6
+ * @license Angular v6.1.1+9.sha-e4acd83
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.1+7.sha-ae9b4e6');
+    var VERSION = new core.Version('6.1.1+9.sha-e4acd83');
 
     /**
      * @license
