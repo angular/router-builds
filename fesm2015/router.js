2c22758b6b9ddc0e673263a67443edfd26ceaf7b
Kara Erickson <karakara@google.com>
2018-08-06 22:29:50 +0000

    6e7d5f0 docs(core): fix tree-shakable spelling (#24057)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+17.sha-66547d8
+ * @license Angular v6.1.1+16.sha-6e7d5f0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.1+17.sha-66547d8');
+const VERSION = new Version('6.1.1+16.sha-6e7d5f0');
 
 /**
  * @fileoverview added by tsickle
