fe5c637c4d6c91cdc1d315a8f32d2484012a8d40
Kara Erickson <karakara@google.com>
2018-08-06 22:23:29 +0000

    88f01f5 docs: fix typo (#25331) PR Close #25331

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+10.sha-c66794c
+ * @license Angular v6.1.1+11.sha-88f01f5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.1+10.sha-c66794c');
+const VERSION = new Version('6.1.1+11.sha-88f01f5');
 
 /**
  * @fileoverview added by tsickle
