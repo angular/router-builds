a7e481c8144f51e153c4adc778885f637d817e4c
Kara Erickson <karakara@google.com>
2018-08-06 22:31:56 +0000

    6e7d5f0 docs(core): fix tree-shakable spelling (#24057)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+17.sha-66547d8
+ * @license Angular v6.1.1+16.sha-6e7d5f0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5276,7 +5276,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.1+17.sha-66547d8');
+    var VERSION = new i0.Version('6.1.1+16.sha-6e7d5f0');
 
     /**
      * @license
