328515b61c88c42fba244c8cb669355dae57a877
Kara Erickson <karakara@google.com>
2018-08-06 19:32:38 +0000

    ba1e25f fix(router): take base uri into account in `setUpLocationSync()` (#20244)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+22.sha-97b5cb2
+ * @license Angular v7.0.0-beta.0+23.sha-ba1e25f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.0+22.sha-97b5cb2');
+const VERSION = new Version('7.0.0-beta.0+23.sha-ba1e25f');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+22.sha-97b5cb2
+ * @license Angular v7.0.0-beta.0+23.sha-ba1e25f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
