aba1f44ad66c4c81fcaabc5ca448ff358b776441
Kara Erickson <karakara@google.com>
2018-08-06 20:13:33 +0000

    637ae13 docs(http): fixed example unit test for error catching (#25306)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+26.sha-c67f1bb
+ * @license Angular v7.0.0-beta.0+25.sha-637ae13
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.0+26.sha-c67f1bb');
+const VERSION = new Version('7.0.0-beta.0+25.sha-637ae13');
 
 /**
  * @fileoverview added by tsickle
