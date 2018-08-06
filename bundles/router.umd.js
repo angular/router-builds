1cdea4912764a4bb7bdc335f4b404d78eceaa652
Kara Erickson <karakara@google.com>
2018-08-06 22:55:23 +0000

    6861bc5 docs: clarify heroes example (#21216)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+34.sha-b8887dd
+ * @license Angular v7.0.0-beta.0+35.sha-6861bc5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5418,7 +5418,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-beta.0+34.sha-b8887dd');
+    var VERSION = new core.Version('7.0.0-beta.0+35.sha-6861bc5');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+34.sha-b8887dd
+ * @license Angular v7.0.0-beta.0+35.sha-6861bc5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
