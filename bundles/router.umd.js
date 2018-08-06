50a38f03c72735e62e8ddf2b4882f21a1c4a8a2b
Kara Erickson <karakara@google.com>
2018-08-06 22:26:45 +0000

    a54c049 docs: replace npm with yarn in lockfile readme (#25309)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+11.sha-88f01f5
+ * @license Angular v6.1.1+13.sha-a54c049
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5276,7 +5276,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.1+11.sha-88f01f5');
+    var VERSION = new i0.Version('6.1.1+13.sha-a54c049');
 
     /**
      * @license
