a04262d2675d2327da05cbeee20d0480de119732
Kara Erickson <karakara@google.com>
2018-08-06 22:34:23 +0000

    8cbb836 docs: clarify heroes example (#21216)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+16.sha-6e7d5f0
+ * @license Angular v6.1.1+19.sha-8cbb836
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+16.sha-6e7d5f0
+ * @license Angular v6.1.1+19.sha-8cbb836
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5203,7 +5203,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.1+16.sha-6e7d5f0');
+var VERSION = new Version('6.1.1+19.sha-8cbb836');
 
 /**
  * @license
