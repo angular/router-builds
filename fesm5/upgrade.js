cf366f847da5b08733d9d615083bbf4791eb5a0d
Kara Erickson <karakara@google.com>
2018-08-07 21:45:14 +0000

    16c03c0 fix(core): In Testability.whenStable update callback, pass more complete (#25010) data about tasks.

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,9 +1,10 @@
 /**
- * @license Angular v6.1.0+100.sha-183757d
+ * @license Angular v7.0.0-beta.0+38.sha-16c03c0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
+import { Location } from '@angular/common';
 import { APP_BOOTSTRAP_LISTENER } from '@angular/core';
 import { Router } from '@angular/router';
 import { UpgradeModule } from '@angular/upgrade/static';
@@ -65,12 +66,45 @@ function setUpLocationSync(ngUpgrade) {
         throw new Error("\n        RouterUpgradeInitializer can be used only after UpgradeModule.bootstrap has been called.\n        Remove RouterUpgradeInitializer and call setUpLocationSync after UpgradeModule.bootstrap.\n      ");
     }
     var router = ngUpgrade.injector.get(Router);
-    var url = document.createElement('a');
+    var location = ngUpgrade.injector.get(Location);
     ngUpgrade.$injector.get('$rootScope')
         .$on('$locationChangeStart', function (_, next, __) {
-        url.href = next;
-        router.navigateByUrl(url.pathname + url.search + url.hash);
+        var url = resolveUrl(next);
+        var path = location.normalize(url.pathname);
+        router.navigateByUrl(path + url.search + url.hash);
     });
+}
+/**
+ * Normalize and parse a URL.
+ *
+ * - Normalizing means that a relative URL will be resolved into an absolute URL in the context of
+ *   the application document.
+ * - Parsing means that the anchor's `protocol`, `hostname`, `port`, `pathname` and related
+ *   properties are all populated to reflect the normalized URL.
+ *
+ * While this approach has wide compatibility, it doesn't work as expected on IE. On IE, normalizing
+ * happens similar to other browsers, but the parsed components will not be set. (E.g. if you assign
+ * `a.href = 'foo'`, then `a.protocol`, `a.host`, etc. will not be correctly updated.)
+ * We work around that by performing the parsing in a 2nd step by taking a previously normalized URL
+ * and assigning it again. This correctly populates all properties.
+ *
+ * See
+ * https://github.com/angular/angular.js/blob/2c7400e7d07b0f6cec1817dab40b9250ce8ebce6/src/ng/urlUtils.js#L26-L33
+ * for more info.
+ */
+var anchor;
+function resolveUrl(url) {
+    if (!anchor) {
+        anchor = document.createElement('a');
+    }
+    anchor.setAttribute('href', url);
+    anchor.setAttribute('href', anchor.href);
+    return {
+        // IE does not start `pathname` with `/` like other browsers.
+        pathname: "/" + anchor.pathname.replace(/^\//, ''),
+        search: anchor.search,
+        hash: anchor.hash
+    };
 }
 
 /**
