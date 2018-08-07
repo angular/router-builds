26a70293af043655603e53ec2566f8a459170ab4
Kara Erickson <karakara@google.com>
2018-08-07 19:16:33 +0000

    2fe05ab docs: update resources to include UI-jar (#21200)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+41.sha-2505c07
+ * @license Angular v7.0.0-beta.0+42.sha-2fe05ab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5276,7 +5276,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('7.0.0-beta.0+41.sha-2505c07');
+    var VERSION = new i0.Version('7.0.0-beta.0+42.sha-2fe05ab');
 
     /**
      * @license
