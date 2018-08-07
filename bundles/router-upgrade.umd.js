eabb1c52701bd3576e4547716893c632d084d4bb
Kara Erickson <karakara@google.com>
2018-08-07 21:36:58 +0000

    732026c feat(core): add DoBootstrap interface. (#24558)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+45.sha-fefc860
+ * @license Angular v7.0.0-beta.0+50.sha-732026c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+45.sha-fefc860
+ * @license Angular v7.0.0-beta.0+50.sha-732026c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.0+45.sha-fefc860');
+const VERSION = new Version('7.0.0-beta.0+50.sha-732026c');
 
 /**
  * @fileoverview added by tsickle
