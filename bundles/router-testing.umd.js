a3c5c8cdd6e0465df5b87c593c6581d9981c6974
Kara Erickson <karakara@google.com>
2018-08-07 19:52:12 +0000

    02e201a fix: add mappings for ngfactory & ngsummary files to their module names in aot summary resolver (#25335)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+43.sha-7bf5a43
+ * @license Angular v7.0.0-beta.0+44.sha-02e201a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+43.sha-7bf5a43
+ * @license Angular v7.0.0-beta.0+44.sha-02e201a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4979,7 +4979,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.0+43.sha-7bf5a43');
+const VERSION = new Version('7.0.0-beta.0+44.sha-02e201a');
 
 /**
  * @license
