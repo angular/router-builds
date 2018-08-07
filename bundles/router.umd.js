cce15961356611b74f93ee3bf7e686ed6425f44f
Kara Erickson <karakara@google.com>
2018-08-07 20:03:20 +0000

    fefc860 fix(ivy): fix bug with banana-in-a-box expressions in nested templates (#25321)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+44.sha-02e201a
+ * @license Angular v7.0.0-beta.0+45.sha-fefc860
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5418,7 +5418,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-beta.0+44.sha-02e201a');
+    var VERSION = new core.Version('7.0.0-beta.0+45.sha-fefc860');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+44.sha-02e201a
+ * @license Angular v7.0.0-beta.0+45.sha-fefc860
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.0+44.sha-02e201a');
+const VERSION = new Version('7.0.0-beta.0+45.sha-fefc860');
 
 /**
  * @fileoverview added by tsickle
