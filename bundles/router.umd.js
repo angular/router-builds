ae02186cd390738411c5fe49372d0171a3900f54
Kara Erickson <karakara@google.com>
2018-08-07 20:03:31 +0000

    2f23533 docs(aio): Angular course in Portuguese #21836

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+26.sha-054fbbe
+ * @license Angular v6.1.1+27.sha-2f23533
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5418,7 +5418,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.1+26.sha-054fbbe');
+    var VERSION = new core.Version('6.1.1+27.sha-2f23533');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+26.sha-054fbbe
+ * @license Angular v6.1.1+27.sha-2f23533
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5345,7 +5345,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.1+26.sha-054fbbe');
+var VERSION = new Version('6.1.1+27.sha-2f23533');
 
 /**
  * @license
