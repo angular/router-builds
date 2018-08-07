a9e8571b2ee3c3cd9e933018a0e0b1cad8c4299c
Kara Erickson <karakara@google.com>
2018-08-07 18:51:35 +0000

    155d938 docs: refining code of tutorial 7 routing (#22151)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+23.sha-b75a985
+ * @license Angular v6.1.1+25.sha-155d938
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5276,7 +5276,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.1+23.sha-b75a985');
+    var VERSION = new i0.Version('6.1.1+25.sha-155d938');
 
     /**
      * @license
