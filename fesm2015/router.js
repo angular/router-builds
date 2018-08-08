6a7f606a256e3fb7cf6db6162b999349cc811a79
Kara Erickson <karakara@google.com>
2018-08-08 21:47:08 +0000

    f39551c docs: Clarify breaking change in minor release (#25393)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+4.sha-e76211a
+ * @license Angular v6.1.2+1.sha-f39551c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.2+4.sha-e76211a');
+const VERSION = new Version('6.1.2+1.sha-f39551c');
 
 /**
  * @fileoverview added by tsickle
