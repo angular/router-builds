88a55fe67295111269e4ff0bd278aa2c7a9f92e2
Kara Erickson <karakara@google.com>
2018-08-08 05:16:11 +0000

    01e6255 build(bazel): update to rules_nodejs 0.11.2 and latest rules_typescript (#25365)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+27.sha-2f23533
+ * @license Angular v6.1.1+28.sha-01e6255
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.1+27.sha-2f23533');
+const VERSION = new Version('6.1.1+28.sha-01e6255');
 
 /**
  * @fileoverview added by tsickle
