c260ac2710190b8d44667242ebba4facccef2b64
Pete Bacon Darwin <pete@bacondarwin.com>
2018-08-08 21:53:55 +0000

    26adee9 feat(ivy): compile all package formats in ngcc

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+64.sha-2ee0c9e
+ * @license Angular v6.1.0+66.sha-26adee9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+64.sha-2ee0c9e');
+const VERSION = new Version('6.1.0+66.sha-26adee9');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+64.sha-2ee0c9e
+ * @license Angular v6.1.0+66.sha-26adee9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
