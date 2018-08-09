865bbdef5d67e53103928e573aac4e7d3d3820d1
Ben Lesh <ben@benlesh.com>
2018-08-09 21:28:19 +0000

    2d75992 feat(ivy): add support for ng-container in the compiler (#25383)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+18.sha-7058072
+ * @license Angular v7.0.0-beta.1+19.sha-2d75992
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.1+18.sha-7058072');
+const VERSION = new Version('7.0.0-beta.1+19.sha-2d75992');
 
 /**
  * @fileoverview added by tsickle
