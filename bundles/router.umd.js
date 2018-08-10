6c6da979c63e6921965ac2bfbac5c679f1aee09d
Ben Lesh <ben@benlesh.com>
2018-08-10 17:22:28 +0000

    d0f089a docs(aio): add async validation chapter (#25189)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+8.sha-fda30cb
+ * @license Angular v6.1.2+10.sha-d0f089a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.2+8.sha-fda30cb');
+    var VERSION = new core.Version('6.1.2+10.sha-d0f089a');
 
     /**
      * @license
