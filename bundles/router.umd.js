55cc964e5d577bd7a3696973d40b18451372089e
Ben Lesh <ben@benlesh.com>
2018-08-14 20:18:38 +0000

    0c4209f refactor(ivy): harmonize container and element / elementContainer signatures (#25458)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+34.sha-ecb5dc0
+ * @license Angular v7.0.0-beta.1+37.sha-0c4209f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-beta.1+34.sha-ecb5dc0');
+    var VERSION = new core.Version('7.0.0-beta.1+37.sha-0c4209f');
 
     /**
      * @license
