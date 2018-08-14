bae962d34b81010ab4e87e9fe2a3a82683a5359f
Ben Lesh <ben@benlesh.com>
2018-08-14 05:59:25 +0000

    c869b14 build: drop unused re-export of json_marshal. (#25449)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+29.sha-e0314b5
+ * @license Angular v7.0.0-beta.1+30.sha-c869b14
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+29.sha-e0314b5
+ * @license Angular v7.0.0-beta.1+30.sha-c869b14
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.1+29.sha-e0314b5');
+const VERSION = new Version('7.0.0-beta.1+30.sha-c869b14');
 
 /**
  * @fileoverview added by tsickle
