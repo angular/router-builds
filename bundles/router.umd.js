4bf7f14577ce215f84d76b0390d4e3eea8b2995a
Ben Lesh <ben@benlesh.com>
2018-08-14 05:38:12 +0000

    151e4b9 docs: add link to Yarn in README (#24856)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+9.sha-cb05f9b
+ * @license Angular v6.1.2+11.sha-151e4b9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5418,7 +5418,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.2+9.sha-cb05f9b');
+    var VERSION = new core.Version('6.1.2+11.sha-151e4b9');
 
     /**
      * @license
