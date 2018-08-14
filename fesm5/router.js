0d74577cbb674f2bbe3effb5d4ef6fee360f7242
Ben Lesh <ben@benlesh.com>
2018-08-14 20:14:42 +0000

    ecb5dc0 docs: add Accelebrate to resources (#23204)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+38.sha-9117fa1
+ * @license Angular v7.0.0-beta.1+34.sha-ecb5dc0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5345,7 +5345,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.1+38.sha-9117fa1');
+var VERSION = new Version('7.0.0-beta.1+34.sha-ecb5dc0');
 
 /**
  * @license
