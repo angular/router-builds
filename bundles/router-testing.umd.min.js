a74c71fea9dfbae1d7ab7792409f5db1dd97626b
Ben Lesh <ben@benlesh.com>
2018-08-14 21:30:37 +0000

    cdee9ad build(docs-infra): remove stability labels from API docs (#25453)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+15.sha-9d6e869
+ * @license Angular v6.1.2+19.sha-cdee9ad
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+15.sha-9d6e869
+ * @license Angular v6.1.2+19.sha-cdee9ad
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.2+15.sha-9d6e869');
+const VERSION = new Version('6.1.2+19.sha-cdee9ad');
 
 /**
  * @fileoverview added by tsickle
