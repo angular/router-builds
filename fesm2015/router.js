d26ea81d0afc992451932c40e50653740ed5d152
Ben Lesh <ben@benlesh.com>
2018-08-14 21:17:04 +0000

    9d6e869 docs: add api doc for programmatic animation classes (#24668)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+14.sha-e906bf4
+ * @license Angular v6.1.2+15.sha-9d6e869
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.2+14.sha-e906bf4');
+const VERSION = new Version('6.1.2+15.sha-9d6e869');
 
 /**
  * @fileoverview added by tsickle
