560eb1cd27c8ab9417b8088b59ed7a6f057134c1
Ben Lesh <ben@benlesh.com>
2018-08-15 01:02:41 +0000

    be2cf4d docs(core): Correct spelling error in directives docs (#25377)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+47.sha-a0a29fd
+ * @license Angular v7.0.0-beta.1+50.sha-be2cf4d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.1+47.sha-a0a29fd');
+const VERSION = new Version('7.0.0-beta.1+50.sha-be2cf4d');
 
 /**
  * @fileoverview added by tsickle
