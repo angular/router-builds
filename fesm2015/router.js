08ad2d418d989ae109f83038fa880f07335d82d5
Ben Lesh <ben@benlesh.com>
2018-08-15 01:10:17 +0000

    1319ff4 fix(service-worker): `Cache-Control: no-cache` on assets breaks service worker (#25408)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+21.sha-2ce9348
+ * @license Angular v6.1.2+23.sha-1319ff4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.2+21.sha-2ce9348');
+const VERSION = new Version('6.1.2+23.sha-1319ff4');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+21.sha-2ce9348
+ * @license Angular v6.1.2+23.sha-1319ff4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
