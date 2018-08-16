72d3580c232e9644fc9c587dd1e980c3626ad247
Jason Aden <jasonaden@google.com>
2018-08-16 21:50:56 +0000

    de03abb docs: reactive forms guide copy edits (#25417)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+12.sha-9ee6702
+ * @license Angular v7.0.0-beta.2+16.sha-de03abb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.2+12.sha-9ee6702');
+const VERSION = new Version('7.0.0-beta.2+16.sha-de03abb');
 
 /**
  * @fileoverview added by tsickle
