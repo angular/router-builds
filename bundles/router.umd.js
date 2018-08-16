704f4c82a8a31fee06f44f6492ee73de0185559d
Jason Aden <jasonaden@google.com>
2018-08-16 22:18:41 +0000

    620d140 docs: add HttpClientModule import code to services tutorial (#24854)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+1.sha-36fb4f4
+ * @license Angular v6.1.3+2.sha-620d140
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.3+1.sha-36fb4f4');
+    var VERSION = new core.Version('6.1.3+2.sha-620d140');
 
     /**
      * @license
