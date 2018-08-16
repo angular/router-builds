d11d34d0102842954a4141f236245bcdbef64b82
Jason Aden <jasonaden@google.com>
2018-08-16 21:52:13 +0000

    de03abb docs: reactive forms guide copy edits (#25417)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+12.sha-9ee6702
+ * @license Angular v7.0.0-beta.2+16.sha-de03abb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+12.sha-9ee6702
+ * @license Angular v7.0.0-beta.2+16.sha-de03abb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5394,7 +5394,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.2+12.sha-9ee6702');
+var VERSION = new Version('7.0.0-beta.2+16.sha-de03abb');
 
 /**
  * @license
