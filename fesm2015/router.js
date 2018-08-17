99759f826dc8c4dc5c8ea80fa640b6d3977f0b4c
Jason Aden <jasonaden@google.com>
2018-08-17 00:17:46 +0000

    116946f style: tslint enforces no debugger statements left behind (#25532)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+17.sha-cc55d60
+ * @license Angular v7.0.0-beta.2+19.sha-116946f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.2+17.sha-cc55d60');
+const VERSION = new Version('7.0.0-beta.2+19.sha-116946f');
 
 /**
  * @fileoverview added by tsickle
