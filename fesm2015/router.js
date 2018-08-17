f66a1a272f07cc1d57ef58e4c08d30793f31f088
Jason Aden <jasonaden@google.com>
2018-08-17 18:44:23 +0000

    e9026a5 docs: fix script path reference (#25552)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+21.sha-31f0f5b
+ * @license Angular v7.0.0-beta.2+23.sha-e9026a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.2+21.sha-31f0f5b');
+const VERSION = new Version('7.0.0-beta.2+23.sha-e9026a5');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+21.sha-31f0f5b
+ * @license Angular v7.0.0-beta.2+23.sha-e9026a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
