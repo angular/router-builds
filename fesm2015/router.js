3a88e633fdad53c3e8d4db140bfbf35326a51c01
Jason Aden <jasonaden@google.com>
2018-08-17 21:47:55 +0000

    a5cc9db ci: ensure aio_preview job has needed node_modules (#25536)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+2.sha-620d140
+ * @license Angular v6.1.3+14.sha-a5cc9db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.3+2.sha-620d140');
+const VERSION = new Version('6.1.3+14.sha-a5cc9db');
 
 /**
  * @fileoverview added by tsickle
