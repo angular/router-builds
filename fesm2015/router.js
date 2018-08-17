cd667961fc477e176057e0ad254e52bf982b52a9
Jason Aden <jasonaden@google.com>
2018-08-17 18:00:13 +0000

    f053a3f test(bazel): Remove --no-sandbox for protractor tests (#25362)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+20.sha-07d8d39
+ * @license Angular v7.0.0-beta.2+22.sha-f053a3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.2+20.sha-07d8d39');
+const VERSION = new Version('7.0.0-beta.2+22.sha-f053a3f');
 
 /**
  * @fileoverview added by tsickle
