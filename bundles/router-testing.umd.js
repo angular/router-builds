7df884cc6fb0d62b3d78cea05c1813a4607ddf09
Jason Aden <jasonaden@google.com>
2018-08-17 18:17:38 +0000

    31f0f5b feat(ivy): add support for local refs on ng-template (#25482)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+22.sha-f053a3f
+ * @license Angular v7.0.0-beta.2+21.sha-31f0f5b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+22.sha-f053a3f
+ * @license Angular v7.0.0-beta.2+21.sha-31f0f5b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5312,7 +5312,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('7.0.0-beta.2+22.sha-f053a3f');
+    var VERSION = new i0.Version('7.0.0-beta.2+21.sha-31f0f5b');
 
     /**
      * @license
