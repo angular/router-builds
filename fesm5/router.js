358024e6c75c78faf78b386bfad38461df02a607
Jason Aden <jasonaden@google.com>
2018-08-20 19:22:47 +0000

    21a1440 refactor(ivy): generate vars in component defs (#25562)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+26.sha-f2aa9c6
+ * @license Angular v7.0.0-beta.2+28.sha-21a1440
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5346,7 +5346,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.2+26.sha-f2aa9c6');
+var VERSION = new Version('7.0.0-beta.2+28.sha-21a1440');
 
 /**
  * @license
