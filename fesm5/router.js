817fb3e7f041449b2d3329a68b019ee24b2df0af
Jason Aden <jasonaden@google.com>
2018-08-20 19:13:03 +0000

    77d9975 build(aio): update dgeni-packages to 0.26.3 to fix reference types issue (#25528)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+15.sha-5939c42
+ * @license Angular v6.1.3+18.sha-77d9975
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5394,7 +5394,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.3+15.sha-5939c42');
+var VERSION = new Version('6.1.3+18.sha-77d9975');
 
 /**
  * @license
