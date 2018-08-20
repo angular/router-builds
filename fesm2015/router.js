854e730569b5322da46b14ad5625a79001d88b11
Jason Aden <jasonaden@google.com>
2018-08-20 19:33:15 +0000

    78750a7 docs: fix typo in service worker getting started guide (#25512)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+16.sha-292b435
+ * @license Angular v6.1.3+19.sha-78750a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6783,7 +6783,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.3+16.sha-292b435');
+const VERSION = new Version('6.1.3+19.sha-78750a7');
 
 /**
  * @fileoverview added by tsickle
