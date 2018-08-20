67c67e4648f652bc6b9fa0c181c45d66208105d6
Jason Aden <jasonaden@google.com>
2018-08-20 19:29:15 +0000

    292b435 docs: fix typo in reactive forms guide (#25543)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+18.sha-77d9975
+ * @license Angular v6.1.3+16.sha-292b435
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.3+18.sha-77d9975');
+    var VERSION = new core.Version('6.1.3+16.sha-292b435');
 
     /**
      * @license
