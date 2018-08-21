9dec93ff64ed769f1a51acd1907c1b410d6d0eae
Jason Aden <jasonaden@google.com>
2018-08-21 21:38:54 +0000

    f33dbf4 docs(changelog): add v6.1.3 release to main CHANGELOG.md (#25603)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+38.sha-6176974
+ * @license Angular v7.0.0-beta.2+39.sha-f33dbf4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5315,7 +5315,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('7.0.0-beta.2+38.sha-6176974');
+    var VERSION = new i0.Version('7.0.0-beta.2+39.sha-f33dbf4');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+38.sha-6176974
+ * @license Angular v7.0.0-beta.2+39.sha-f33dbf4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
