b96c5c4ede33260ceef824348263bfc09a444c05
Matias Niemelä <matias@yearofmoo.com>
2018-08-22 21:16:42 +0000

    ace4e4f build: remove `NGBUILDS_IO_KEY` now that it is not used any more (#25601)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+22.sha-1fa9790
+ * @license Angular v6.1.3+23.sha-ace4e4f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4982,7 +4982,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.3+22.sha-1fa9790');
+const VERSION = new Version('6.1.3+23.sha-ace4e4f');
 
 /**
  * @license
