ee088d06607ded75c9e5120452586d69385d5a09
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 02:52:13 +0000

    668bfce fix(docs-infra): fix closure warning issue for improper internal flag (#25628)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+28.sha-eb1fe19
+ * @license Angular v7.0.0-beta.3+29.sha-668bfce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5486,7 +5486,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-beta.3+28.sha-eb1fe19');
+    var VERSION = new core.Version('7.0.0-beta.3+29.sha-668bfce');
 
     /**
      * @license
