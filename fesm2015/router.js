e32a42fc234f446284db73a41c853579b1ed2a8b
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 01:57:25 +0000

    22d58fc build(bazel): remove workaround no longer needed for module names for ngfactory & ngsummary files (#25604)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+24.sha-61218f5
+ * @license Angular v7.0.0-beta.3+27.sha-22d58fc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4983,7 +4983,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.3+24.sha-61218f5');
+const VERSION = new Version('7.0.0-beta.3+27.sha-22d58fc');
 
 /**
  * @license
