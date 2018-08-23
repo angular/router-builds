17bfd1ba70df0951ffb1f29b09e88658dc915a56
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 03:06:00 +0000

    cc0d0a9 fix(docs-infra): fix closure warning issue for improper internal flag (#25628)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.4+1.sha-82f26fe
+ * @license Angular v6.1.4+2.sha-cc0d0a9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.4+1.sha-82f26fe');
+const VERSION = new Version('6.1.4+2.sha-cc0d0a9');
 
 /**
  * @fileoverview added by tsickle
