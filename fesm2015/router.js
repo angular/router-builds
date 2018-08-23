80a3425cfd071d51094b3e0a7d88ef6a3c8ec602
Jason Aden <jasonaden@google.com>
2018-08-23 00:39:57 +0000

    8de57c9 release: cut the v6.1.4 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+23.sha-ace4e4f
+ * @license Angular v6.1.4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.3+23.sha-ace4e4f');
+const VERSION = new Version('6.1.4');
 
 /**
  * @fileoverview added by tsickle
