be565d199f6b4249b070856ee4350ab614fdd57e
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 02:14:37 +0000

    eb1fe19 fix(ivy): ngtsc directive compilation should use shared ConstantPool (#25620)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+26.sha-27e2039
+ * @license Angular v7.0.0-beta.3+28.sha-eb1fe19
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6789,7 +6789,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.3+26.sha-27e2039');
+const VERSION = new Version('7.0.0-beta.3+28.sha-eb1fe19');
 
 /**
  * @fileoverview added by tsickle
