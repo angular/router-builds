563197f91c9a982af29e7d322cf98948d5f274ec
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 20:24:27 +0000

    3d41739 fix(aio): show aio-themed 404 page for unknown resources (#23188)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+29.sha-668bfce
+ * @license Angular v7.0.0-beta.3+30.sha-3d41739
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5422,7 +5422,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-beta.3+29.sha-668bfce');
+    var VERSION = new core.Version('7.0.0-beta.3+30.sha-3d41739');
 
     /**
      * @license
