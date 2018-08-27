34118701ba317f9ddd762021791c72a7fe3c5b5e
Matias Niemelä <matias@yearofmoo.com>
2018-08-27 23:26:40 +0000

    a081d20 build(docs-infra): remove "annotations" section from API pages (#25677)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+59.sha-6ed7993
+ * @license Angular v7.0.0-beta.3+60.sha-a081d20
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6789,7 +6789,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.3+59.sha-6ed7993');
+const VERSION = new Version('7.0.0-beta.3+60.sha-a081d20');
 
 /**
  * @fileoverview added by tsickle
