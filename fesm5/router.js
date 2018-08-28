f9d69f6acac675147528b07d78fb8d5dc68b506d
Matias Niemelä <matias@yearofmoo.com>
2018-08-28 19:43:00 +0000

    0386c44 fix(ivy): inject attributes for directives on ng-template / ng-container (#25697)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+69.sha-0fe708f
+ * @license Angular v7.0.0-beta.3+70.sha-0386c44
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5398,7 +5398,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.3+69.sha-0fe708f');
+var VERSION = new Version('7.0.0-beta.3+70.sha-0386c44');
 
 /**
  * @license
