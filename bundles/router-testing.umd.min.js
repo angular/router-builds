8f6b5723b48f0b5443a4b87054665e878190fa0c
Miško Hevery <misko@hevery.com>
2018-08-28 19:18:41 +0000

    0fe708f ci: remove `vicb` from `pullapprove.yml` (#25702)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+68.sha-b069514
+ * @license Angular v7.0.0-beta.3+69.sha-0fe708f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+68.sha-b069514
+ * @license Angular v7.0.0-beta.3+69.sha-0fe708f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6789,7 +6789,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.3+68.sha-b069514');
+const VERSION = new Version('7.0.0-beta.3+69.sha-0fe708f');
 
 /**
  * @fileoverview added by tsickle
