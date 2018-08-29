6370c87586b82f35704d9d4b3fe5a49f016c23d1
Matias Niemelä <matias@yearofmoo.com>
2018-08-29 22:19:26 +0000

    f394ba0 fix(bazel): Cache fileNameToModuleName lookups (#25731)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+75.sha-268cf79
+ * @license Angular v7.0.0-beta.3+76.sha-f394ba0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5398,7 +5398,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.3+75.sha-268cf79');
+var VERSION = new Version('7.0.0-beta.3+76.sha-f394ba0');
 
 /**
  * @license
