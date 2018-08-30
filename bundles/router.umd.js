60ce8b864a6c4d30972ce694a57fbac59547cec6
Matias Niemelä <matias@yearofmoo.com>
2018-08-30 00:37:33 +0000

    4a8d56a release: cut the v6.1.6 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.5+2.sha-0a3dd87
+ * @license Angular v6.1.6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5485,7 +5485,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.5+2.sha-0a3dd87');
+    var VERSION = new core.Version('6.1.6');
 
     /**
      * @license
