aade3f9a7e56b2caf47e1df3bfd80694e50b70be
Matias Niemelä <matias@yearofmoo.com>
2018-08-30 19:02:50 +0000

    2e09115 docs: edit and organize di guide (#21915)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6
+ * @license Angular v6.1.6+1.sha-2e09115
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5485,7 +5485,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.6');
+    var VERSION = new core.Version('6.1.6+1.sha-2e09115');
 
     /**
      * @license
