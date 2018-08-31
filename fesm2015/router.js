46b6055aa3c2ab0cf4e826dbc39aa2051f6e61ea
Misko Hevery <misko@hevery.com>
2018-08-31 22:37:50 +0000

    a4405d7 docs(forms): update API reference for reactive and template-driven forms modules (#25687)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+8.sha-88f7ddb
+ * @license Angular v6.1.6+9.sha-a4405d7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4982,7 +4982,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.6+8.sha-88f7ddb');
+const VERSION = new Version('6.1.6+9.sha-a4405d7');
 
 /**
  * @license
