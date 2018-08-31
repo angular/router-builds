5c7d47d0d7ce28f56a34a9903149c8b8c638f8e6
Misko Hevery <misko@hevery.com>
2018-08-31 21:33:31 +0000

    abd29f5 docs(forms): update API reference for reactive and template-driven forms modules (#25687)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+31.sha-31349fd
+ * @license Angular v7.0.0-beta.4+35.sha-abd29f5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6789,7 +6789,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.4+31.sha-31349fd');
+const VERSION = new Version('7.0.0-beta.4+35.sha-abd29f5');
 
 /**
  * @fileoverview added by tsickle
