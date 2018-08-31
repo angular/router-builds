ea073df1ed7c376b872a0e9c97dbb8f9063ccb30
Misko Hevery <misko@hevery.com>
2018-08-31 18:11:50 +0000

    a469c2c feat(ivy): produce contextual diagnostics in ngtsc mode (#25647)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+21.sha-b424b31
+ * @license Angular v7.0.0-beta.4+23.sha-a469c2c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5315,7 +5315,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('7.0.0-beta.4+21.sha-b424b31');
+    var VERSION = new i0.Version('7.0.0-beta.4+23.sha-a469c2c');
 
     /**
      * @license
