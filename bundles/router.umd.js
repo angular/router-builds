e999b93b36110817831dc9e3bf720e38328d1fba
Misko Hevery <misko@hevery.com>
2018-09-04 20:18:32 +0000

    e84da19 ci: ensure build-packages-dist works on OS/X bash (#25591)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+41.sha-51c0d9c
+ * @license Angular v7.0.0-beta.4+36.sha-e84da19
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5486,7 +5486,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-beta.4+41.sha-51c0d9c');
+    var VERSION = new core.Version('7.0.0-beta.4+36.sha-e84da19');
 
     /**
      * @license
