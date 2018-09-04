c142a139a00522a0dede0d9a40b1e9029972d503
Misko Hevery <misko@hevery.com>
2018-09-04 20:57:15 +0000

    4fe369e docs: add pwa keyword to service worker page (#25725) PR Close #25725

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+11.sha-d8930bb
+ * @license Angular v6.1.6+12.sha-4fe369e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4982,7 +4982,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.6+11.sha-d8930bb');
+const VERSION = new Version('6.1.6+12.sha-4fe369e');
 
 /**
  * @license
