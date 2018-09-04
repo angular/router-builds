0a83e0e2c93ac5f3228dd2d774239191a2bfd5ee
Misko Hevery <misko@hevery.com>
2018-09-04 20:48:42 +0000

    d8930bb docs: correct misspellings and add missing punctuation in tutorial (#25676)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+9.sha-a4405d7
+ * @license Angular v6.1.6+11.sha-d8930bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5279,7 +5279,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.6+9.sha-a4405d7');
+    var VERSION = new i0.Version('6.1.6+11.sha-d8930bb');
 
     /**
      * @license
