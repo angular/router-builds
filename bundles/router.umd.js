639f65f7441e8420f0235d5e5252ccbd9d1eda3b
Misko Hevery <misko@hevery.com>
2018-09-05 20:06:45 +0000

    d96e962 test(ivy): mock the filesystem in ngcc main() integration tests (#25557)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+54.sha-2a672a9
+ * @license Angular v7.0.0-beta.4+56.sha-d96e962
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5493,7 +5493,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-beta.4+54.sha-2a672a9');
+    var VERSION = new core.Version('7.0.0-beta.4+56.sha-d96e962');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+54.sha-2a672a9
+ * @license Angular v7.0.0-beta.4+56.sha-d96e962
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4990,7 +4990,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.4+54.sha-2a672a9');
+const VERSION = new Version('7.0.0-beta.4+56.sha-d96e962');
 
 /**
  * @license
