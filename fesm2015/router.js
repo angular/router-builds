af951b84fc19a2d9f78d51c8a002fab9dbc9c2af
Misko Hevery <misko@hevery.com>
2018-09-05 19:20:59 +0000

    a417b2b fix(ivy): detect frozen flyweight objects in definitions and unfreeze (#25755)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+44.sha-25c1f33
+ * @license Angular v7.0.0-beta.4+46.sha-a417b2b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4983,7 +4983,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.4+44.sha-25c1f33');
+const VERSION = new Version('7.0.0-beta.4+46.sha-a417b2b');
 
 /**
  * @license
