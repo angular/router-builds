bbd3c34de6b4ec8e3264ae22c81a237dcf23499f
Misko Hevery <misko@hevery.com>
2018-09-05 20:25:58 +0000

    3a0b735 build: upgrade Chromium and ChromeDriver to latest versions (#25602) (#25669)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+23.sha-f0bcfd0
+ * @license Angular v6.1.6+27.sha-3a0b735
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.6+23.sha-f0bcfd0');
+const VERSION = new Version('6.1.6+27.sha-3a0b735');
 
 /**
  * @fileoverview added by tsickle
