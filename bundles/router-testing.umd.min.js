e9efa3e2df4e54b64ce7c56e8f525a4186aad27b
Misko Hevery <misko@hevery.com>
2018-09-05 18:49:35 +0000

    25c1f33 refactor(docs-infra): bump polyfills payload limit (#25806)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+42.sha-c1ae3c1
+ * @license Angular v7.0.0-beta.4+44.sha-25c1f33
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+42.sha-c1ae3c1
+ * @license Angular v7.0.0-beta.4+44.sha-25c1f33
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5315,7 +5315,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('7.0.0-beta.4+42.sha-c1ae3c1');
+    var VERSION = new i0.Version('7.0.0-beta.4+44.sha-25c1f33');
 
     /**
      * @license
