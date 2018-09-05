59c849a3fe18e55af57e21bd4ebda67e9d6f2002
Misko Hevery <misko@hevery.com>
2018-09-05 20:52:48 +0000

    2c1f550 build: update ngcontainer to node 10.9.0 (#25812)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+24.sha-e72f741
+ * @license Angular v6.1.6+25.sha-2c1f550
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5492,7 +5492,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.6+24.sha-e72f741');
+    var VERSION = new core.Version('6.1.6+25.sha-2c1f550');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+24.sha-e72f741
+ * @license Angular v6.1.6+25.sha-2c1f550
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5404,7 +5404,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.6+24.sha-e72f741');
+var VERSION = new Version('6.1.6+25.sha-2c1f550');
 
 /**
  * @license
