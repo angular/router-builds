285d21ed54bd6769f14e98298a501b07ab8e13b2
Misko Hevery <misko@hevery.com>
2018-09-05 19:28:32 +0000

    27f0817 docs(changelog): fix version 6.1.5 typo (#25760) PR Close #25760

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+18.sha-6f7df8a
+ * @license Angular v6.1.6+16.sha-27f0817
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.6+18.sha-6f7df8a');
+const VERSION = new Version('6.1.6+16.sha-27f0817');
 
 /**
  * @fileoverview added by tsickle
