b032dcec4183a8beba2d3763b9422da98bfbefb4
Misko Hevery <misko@hevery.com>
2018-09-05 19:21:44 +0000

    46de203 refactor(docs-infra): simplify custom-element polyfill setup (#25806)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+10.sha-ad7be50
+ * @license Angular v6.1.6+14.sha-46de203
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4982,7 +4982,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.6+10.sha-ad7be50');
+const VERSION = new Version('6.1.6+14.sha-46de203');
 
 /**
  * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+10.sha-ad7be50
+ * @license Angular v6.1.6+14.sha-46de203
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
