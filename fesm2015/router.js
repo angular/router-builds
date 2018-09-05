a0164c2829260a4960a6f96ab3186d9c327940b0
Misko Hevery <misko@hevery.com>
2018-09-05 20:16:47 +0000

    dcad054 build: update ngcontainer to node 10.9.0 (#25812)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+57.sha-397b047
+ * @license Angular v7.0.0-beta.4+59.sha-dcad054
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4990,7 +4990,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.4+57.sha-397b047');
+const VERSION = new Version('7.0.0-beta.4+59.sha-dcad054');
 
 /**
  * @license
