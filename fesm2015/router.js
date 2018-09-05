d7ff57d16782a2027ae26ac026e08fcdabec1e3d
Misko Hevery <misko@hevery.com>
2018-09-05 20:11:17 +0000

    1bc6f64 docs: update event page (#25799) docs: change reactiveconf event location PR Close #25799

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+56.sha-d96e962
+ * @license Angular v7.0.0-beta.4+58.sha-1bc6f64
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6797,7 +6797,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.4+56.sha-d96e962');
+const VERSION = new Version('7.0.0-beta.4+58.sha-1bc6f64');
 
 /**
  * @fileoverview added by tsickle
