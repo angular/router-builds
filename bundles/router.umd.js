a39149f1da5bcc3da0e6fbaa6eaabe8a28fa0c42
Misko Hevery <misko@hevery.com>
2018-09-05 20:35:16 +0000

    e72f741 docs: update event page (#25799) docs: change reactiveconf event location PR Close #25799

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+22.sha-82e0676
+ * @license Angular v6.1.6+24.sha-e72f741
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5428,7 +5428,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.6+22.sha-82e0676');
+    var VERSION = new core.Version('6.1.6+24.sha-e72f741');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+22.sha-82e0676
+ * @license Angular v6.1.6+24.sha-e72f741
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.6+22.sha-82e0676');
+const VERSION = new Version('6.1.6+24.sha-e72f741');
 
 /**
  * @fileoverview added by tsickle
