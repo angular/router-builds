fb57886358359348bfe4b7eea4af2c858e2e570b
Misko Hevery <misko@hevery.com>
2018-09-05 20:12:04 +0000

    397b047 docs: fix showcase address truly-ui (#25757) PR Close #25757

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+58.sha-1bc6f64
+ * @license Angular v7.0.0-beta.4+57.sha-397b047
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+58.sha-1bc6f64
+ * @license Angular v7.0.0-beta.4+57.sha-397b047
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6797,7 +6797,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.4+58.sha-1bc6f64');
+const VERSION = new Version('7.0.0-beta.4+57.sha-397b047');
 
 /**
  * @fileoverview added by tsickle
