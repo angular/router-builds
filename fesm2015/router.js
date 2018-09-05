1ea0461715515b269bb62a500f8f78ed07d25ee8
Misko Hevery <misko@hevery.com>
2018-09-05 19:25:43 +0000

    13b8399 test(docs-infra): fix double-slash in URL of `aio_monitoring` test (#25641)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+52.sha-51c26b8
+ * @license Angular v7.0.0-beta.4+50.sha-13b8399
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6797,7 +6797,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.4+52.sha-51c26b8');
+const VERSION = new Version('7.0.0-beta.4+50.sha-13b8399');
 
 /**
  * @fileoverview added by tsickle
