af1626d54b8b7c97fd1565e444ca45f400a2c633
Misko Hevery <misko@hevery.com>
2018-09-05 19:26:52 +0000

    6f7df8a feat(docs-infra): disable "status" selector in API list when displaying only packages (#25718)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+17.sha-92298e5
+ * @license Angular v6.1.6+18.sha-6f7df8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5397,7 +5397,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.6+17.sha-92298e5');
+var VERSION = new Version('6.1.6+18.sha-6f7df8a');
 
 /**
  * @license
