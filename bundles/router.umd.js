9157a7c0d3058102ec15fc0a0d92e01d5f0c7d9f
Igor Minar <igor@angular.io>
2018-09-06 23:03:19 +0000

    d6cd041 docs(service-worker): update http-server command (#25845) PR Close #25845

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+1.sha-34ec924
+ * @license Angular v7.0.0-beta.5+4.sha-d6cd041
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5493,7 +5493,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-beta.5+1.sha-34ec924');
+    var VERSION = new core.Version('7.0.0-beta.5+4.sha-d6cd041');
 
     /**
      * @license
