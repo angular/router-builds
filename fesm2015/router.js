9f295e252db0d03c2cd67e1d32b733f7323e0668
Igor Minar <igor@angular.io>
2018-09-06 22:40:29 +0000

    34ec924 build: update to Node 10 (#25822)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5
+ * @license Angular v7.0.0-beta.5+1.sha-34ec924
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6797,7 +6797,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5');
+const VERSION = new Version('7.0.0-beta.5+1.sha-34ec924');
 
 /**
  * @fileoverview added by tsickle
