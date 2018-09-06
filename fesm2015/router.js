17f2bd19537afa529288bb3e4516051a05e478d1
Miško Hevery <misko@hevery.com>
2018-09-06 02:30:50 +0000

    d1063c6 build(docs-infra): restore correct dependency versions in `package.json` (#25827)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+25.sha-2c1f550
+ * @license Angular v6.1.6+28.sha-d1063c6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.6+25.sha-2c1f550');
+const VERSION = new Version('6.1.6+28.sha-d1063c6');
 
 /**
  * @fileoverview added by tsickle
