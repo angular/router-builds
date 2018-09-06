fda330f57ad3f51a1db95c41c3d64a6bc2793c7f
Igor Minar <igor@angular.io>
2018-09-06 23:41:35 +0000

    e244b51 build(bazel): update bazel integration to build angular from source (#25774)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+7.sha-f67229e
+ * @license Angular v6.1.7+3.sha-e244b51
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.7+7.sha-f67229e');
+const VERSION = new Version('6.1.7+3.sha-e244b51');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+7.sha-f67229e
+ * @license Angular v6.1.7+3.sha-e244b51
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
