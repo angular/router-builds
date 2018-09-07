ac9634552461ffa67b8108ec9dc2cc3ba69165ce
Igor Minar <igor@angular.io>
2018-09-07 22:29:00 +0000

    2a21ca0 feat(bazel): add additional parameters to `ts_api_guardian_test` def (#25694)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+6.sha-62be8c2
+ * @license Angular v7.0.0-beta.5+8.sha-2a21ca0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6797,7 +6797,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+6.sha-62be8c2');
+const VERSION = new Version('7.0.0-beta.5+8.sha-2a21ca0');
 
 /**
  * @fileoverview added by tsickle
