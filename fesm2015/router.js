b29609f1d6b27551add5718c20ae329e1c349d59
Igor Minar <igor@angular.io>
2018-09-10 19:07:16 +0000

    8f9aeaa docs: move compiler options to last section of the page (#22353)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+10.sha-9f3da65
+ * @license Angular v6.1.7+9.sha-8f9aeaa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.7+10.sha-9f3da65');
+const VERSION = new Version('6.1.7+9.sha-8f9aeaa');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+10.sha-9f3da65
+ * @license Angular v6.1.7+9.sha-8f9aeaa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
