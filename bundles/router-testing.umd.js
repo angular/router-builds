e13cd9d935b0cf7977deb3b7437f9038e2a99a55
Igor Minar <igor@angular.io>
2018-09-10 18:48:52 +0000

    9f3da65 docs(aio): add ng-sq-ui to resources (#25874)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+3.sha-e244b51
+ * @license Angular v6.1.7+10.sha-9f3da65
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+3.sha-e244b51
+ * @license Angular v6.1.7+10.sha-9f3da65
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.7+3.sha-e244b51');
+const VERSION = new Version('6.1.7+10.sha-9f3da65');
 
 /**
  * @fileoverview added by tsickle
