b2fe94bed99a3e03606e74066cdb47ffa5c22b27
Igor Minar <igor@angular.io>
2018-09-11 14:50:35 +0000

    a9099e8 fix(ivy): ensure Ivy *Ref classes derive from view engine equivalents (#25775)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+16.sha-7ba0cb7
+ * @license Angular v7.0.0-beta.5+20.sha-a9099e8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4990,7 +4990,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.5+16.sha-7ba0cb7');
+const VERSION = new Version('7.0.0-beta.5+20.sha-a9099e8');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+16.sha-7ba0cb7
+ * @license Angular v7.0.0-beta.5+20.sha-a9099e8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
