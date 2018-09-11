5c633b6d50e768ebf16e8dc11c7f1141095bd888
Igor Minar <igor@angular.io>
2018-09-11 16:12:04 +0000

    f3b552f build(bazel): remove outdated "cfg = "data"" references (#25434)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+23.sha-c5b594e
+ * @license Angular v7.0.0-beta.5+24.sha-f3b552f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6797,7 +6797,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+23.sha-c5b594e');
+const VERSION = new Version('7.0.0-beta.5+24.sha-f3b552f');
 
 /**
  * @fileoverview added by tsickle
