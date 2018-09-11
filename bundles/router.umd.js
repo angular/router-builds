fcf514b581ee494ba3f54a4f2eb8f14fe09a0602
Igor Minar <igor@angular.io>
2018-09-11 15:12:10 +0000

    c5b594e docs(bazel): add info on options for nodejs_test rule (#25877)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+22.sha-86a3be8
+ * @license Angular v7.0.0-beta.5+23.sha-c5b594e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5493,7 +5493,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-beta.5+22.sha-86a3be8');
+    var VERSION = new core.Version('7.0.0-beta.5+23.sha-c5b594e');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+22.sha-86a3be8
+ * @license Angular v7.0.0-beta.5+23.sha-c5b594e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
