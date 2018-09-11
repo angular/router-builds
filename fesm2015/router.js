9cd24e3461029cb7201c124032e22d83e24b2269
Igor Minar <igor@angular.io>
2018-09-11 14:52:14 +0000

    d5bd86a fix(ivy): don't accidently read the inherited definition (#25736)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+20.sha-a9099e8
+ * @license Angular v7.0.0-beta.5+21.sha-d5bd86a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6797,7 +6797,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+20.sha-a9099e8');
+const VERSION = new Version('7.0.0-beta.5+21.sha-d5bd86a');
 
 /**
  * @fileoverview added by tsickle
