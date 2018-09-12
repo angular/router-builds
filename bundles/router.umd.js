4897eb62b1fac046ebaf36149fafa07e5d71949a
Igor Minar <igor@angular.io>
2018-09-12 00:23:43 +0000

    8dc2b11 fix(router): mount correct component if router outlet was not instantiated and if using a route reuse strategy (#25313) (#25314)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+28.sha-f2ba55f
+ * @license Angular v7.0.0-beta.5+29.sha-8dc2b11
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4138,6 +4138,7 @@
                     else {
                         var config = parentLoadedConfig(future.snapshot);
                         var cmpFactoryResolver = config ? config.module.componentFactoryResolver : null;
+                        context.attachRef = null;
                         context.route = future;
                         context.resolver = cmpFactoryResolver;
                         if (context.outlet) {
@@ -5496,7 +5497,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-beta.5+28.sha-f2ba55f');
+    var VERSION = new core.Version('7.0.0-beta.5+29.sha-8dc2b11');
 
     /**
      * @license
