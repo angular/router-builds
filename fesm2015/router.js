f0eaa202c3f8d6819bb8851e6adbf37a4d47be04
Igor Minar <igor@angular.io>
2018-09-12 00:28:10 +0000

    21009b0 fix(ivy): use proper sanitizer names (#25817)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+27.sha-00d3666
+ * @license Angular v7.0.0-beta.5+26.sha-21009b0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6850,7 +6850,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+27.sha-00d3666');
+const VERSION = new Version('7.0.0-beta.5+26.sha-21009b0');
 
 /**
  * @fileoverview added by tsickle
