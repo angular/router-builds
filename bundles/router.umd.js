eb4b10a07959c7e18f5a76617f74e73601d58107
Ben Lesh <ben@benlesh.com>
2018-09-13 23:14:13 +0000

    a09c392 docs: delete old comments from example (#25931)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+30.sha-10a656f
+ * @license Angular v7.0.0-beta.5+31.sha-a09c392
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5326,7 +5326,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('7.0.0-beta.5+30.sha-10a656f');
+    var VERSION = new i0.Version('7.0.0-beta.5+31.sha-a09c392');
 
     /**
      * @license
