bcc1a74272db5bc7fb978a3ceb8bd64b66e5f364
Ben Lesh <ben@benlesh.com>
2018-09-13 23:13:22 +0000

    a09c392 docs: delete old comments from example (#25931)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+30.sha-10a656f
+ * @license Angular v7.0.0-beta.5+31.sha-a09c392
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5409,7 +5409,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.5+30.sha-10a656f');
+var VERSION = new Version('7.0.0-beta.5+31.sha-a09c392');
 
 /**
  * @license
