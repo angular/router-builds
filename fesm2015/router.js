006ad99fe05882ce2ba290e2e36004cfaab4cd2d
Ben Lesh <ben@benlesh.com>
2018-09-15 00:46:33 +0000

    8f81dba docs: fix typo in bootstrapping guide (#25939)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+33.sha-aedebaf
+ * @license Angular v7.0.0-beta.5+34.sha-8f81dba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6801,7 +6801,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+33.sha-aedebaf');
+const VERSION = new Version('7.0.0-beta.5+34.sha-8f81dba');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+33.sha-aedebaf
+ * @license Angular v7.0.0-beta.5+34.sha-8f81dba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
