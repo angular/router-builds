65bea41c5dd0a006bf6df6d8e3a763a0bf879e79
Ben Lesh <ben@benlesh.com>
2018-09-15 01:21:34 +0000

    d9bd860 docs: fix typo in bootstrapping guide (#25939)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+15.sha-076374b
+ * @license Angular v6.1.7+16.sha-d9bd860
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4993,7 +4993,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.7+15.sha-076374b');
+const VERSION = new Version('6.1.7+16.sha-d9bd860');
 
 /**
  * @license
