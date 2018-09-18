9033f40ce6dae38d3cdd9e7d6c8d40cb5b8c82e1
Ben Lesh <ben@benlesh.com>
2018-09-18 21:43:32 +0000

    c693624 build: update .nvmrc file to correct node version (#25992)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+17.sha-274dc1e
+ * @license Angular v6.1.7+19.sha-c693624
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+17.sha-274dc1e
+ * @license Angular v6.1.7+19.sha-c693624
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5290,7 +5290,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.7+17.sha-274dc1e');
+    var VERSION = new i0.Version('6.1.7+19.sha-c693624');
 
     /**
      * @license
