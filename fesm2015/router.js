20970a6b3d306aaac32642d90769001fbe9cad41
Ben Lesh <ben@benlesh.com>
2018-09-18 21:27:24 +0000

    cbbad1b refactor(ivy): pre-factor: set explicit type parameters for ModuleWithProviders (#25970)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+53.sha-e952c65
+ * @license Angular v7.0.0-beta.5+51.sha-cbbad1b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6801,7 +6801,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+53.sha-e952c65');
+const VERSION = new Version('7.0.0-beta.5+51.sha-cbbad1b');
 
 /**
  * @fileoverview added by tsickle
