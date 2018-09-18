a2e355f64953c0245f1e8c6f51456fe599aa967f
Ben Lesh <ben@benlesh.com>
2018-09-18 21:12:04 +0000

    56283ed build: update .nvmrc file to correct node version (#25992)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+50.sha-96ee898
+ * @license Angular v7.0.0-beta.5+46.sha-56283ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6801,7 +6801,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+50.sha-96ee898');
+const VERSION = new Version('7.0.0-beta.5+46.sha-56283ed');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+50.sha-96ee898
+ * @license Angular v7.0.0-beta.5+46.sha-56283ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
