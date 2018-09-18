c08caf54a8bf2c3ac8176e983d1e5de45943cce6
Alex Eagle <alexeagle@google.com>
2018-09-18 23:39:15 +0000

    48d7f4e fix(bazel): move bazel managed runtime deps for downstream usage (#25690)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+21.sha-110c81f
+ * @license Angular v6.1.7+36.sha-48d7f4e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6798,7 +6798,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.7+21.sha-110c81f');
+const VERSION = new Version('6.1.7+36.sha-48d7f4e');
 
 /**
  * @fileoverview added by tsickle
