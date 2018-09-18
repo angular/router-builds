812d16724bb7e9a21a1f1f5b2a583c4d0a8b78b0
Ben Lesh <ben@benlesh.com>
2018-09-18 21:13:33 +0000

    5241ea0 test(bazel): Run Angular test on RBE (#25370)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+48.sha-a53a559
+ * @license Angular v7.0.0-beta.5+52.sha-5241ea0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6801,7 +6801,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+48.sha-a53a559');
+const VERSION = new Version('7.0.0-beta.5+52.sha-5241ea0');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+48.sha-a53a559
+ * @license Angular v7.0.0-beta.5+52.sha-5241ea0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
