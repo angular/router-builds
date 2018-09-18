80a6e77abbd6b27bd82d650d9d7f33a644100d1e
Ben Lesh <ben@benlesh.com>
2018-09-18 21:02:17 +0000

    9b1bb37 fix(ivy): ngcc should compile entry-points in the correct order (#25862)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+37.sha-21e566d
+ * @license Angular v7.0.0-beta.5+44.sha-9b1bb37
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6801,7 +6801,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+37.sha-21e566d');
+const VERSION = new Version('7.0.0-beta.5+44.sha-9b1bb37');
 
 /**
  * @fileoverview added by tsickle
