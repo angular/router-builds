4cc960ba28446b679ce6dd08098727bc63f4f25c
Ben Lesh <ben@benlesh.com>
2018-09-18 21:15:42 +0000

    e952c65 docs: correct changelog for 6.1.7

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+52.sha-5241ea0
+ * @license Angular v7.0.0-beta.5+53.sha-e952c65
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6801,7 +6801,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+52.sha-5241ea0');
+const VERSION = new Version('7.0.0-beta.5+53.sha-e952c65');
 
 /**
  * @fileoverview added by tsickle
