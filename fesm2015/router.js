7f984a7c0e488d97104dcd355a7c12e2da576802
Ben Lesh <ben@benlesh.com>
2018-09-19 22:56:48 +0000

    ca1e56d release: cut the v7.0.0-beta.6 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+59.sha-d0e710d
+ * @license Angular v7.0.0-beta.6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6801,7 +6801,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+59.sha-d0e710d');
+const VERSION = new Version('7.0.0-beta.6');
 
 /**
  * @fileoverview added by tsickle
