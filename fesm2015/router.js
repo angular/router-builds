9c7a3ad08a72234c0d025ded33f1d6bed8bdf881
Ben Lesh <ben@benlesh.com>
2018-09-19 18:21:18 +0000

    f91b045 docs(animations): updated animation docs (#24206)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+54.sha-e8bab13
+ * @license Angular v7.0.0-beta.5+55.sha-f91b045
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6854,7 +6854,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+54.sha-e8bab13');
+const VERSION = new Version('7.0.0-beta.5+55.sha-f91b045');
 
 /**
  * @fileoverview added by tsickle
