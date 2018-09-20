99e2578d841f32ea619cb2f754eac582e4b1e8f6
Kara Erickson <karakara@google.com>
2018-09-20 02:54:52 +0000

    0ec925b docs: copy-edit (#25732)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+8.sha-f0d7054
+ * @license Angular v6.1.8+10.sha-0ec925b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5290,7 +5290,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.8+8.sha-f0d7054');
+    var VERSION = new i0.Version('6.1.8+10.sha-0ec925b');
 
     /**
      * @license
