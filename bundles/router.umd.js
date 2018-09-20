a5131e661bc19e6b0e55975ac352c2119a0cbc51
Kara Erickson <karakara@google.com>
2018-09-20 00:45:09 +0000

    c7a6adc docs(forms): update form apis based on review feedback (#25724)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8
+ * @license Angular v6.1.8+3.sha-c7a6adc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5290,7 +5290,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.8');
+    var VERSION = new i0.Version('6.1.8+3.sha-c7a6adc');
 
     /**
      * @license
