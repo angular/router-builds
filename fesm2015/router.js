359496b0f1edd9d53df3f2c081b43b4e36c3cf82
Kara Erickson <karakara@google.com>
2018-09-20 00:00:43 +0000

    d7e5bbf feat(compiler-cli): add support to extend `angularCompilerOptions` (#22717)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6
+ * @license Angular v7.0.0-beta.6+6.sha-d7e5bbf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6801,7 +6801,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.6');
+const VERSION = new Version('7.0.0-beta.6+6.sha-d7e5bbf');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6
+ * @license Angular v7.0.0-beta.6+6.sha-d7e5bbf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5409,7 +5409,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.6');
+var VERSION = new Version('7.0.0-beta.6+6.sha-d7e5bbf');
 
 /**
  * @license
