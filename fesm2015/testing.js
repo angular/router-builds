70b480bda392d895b86f1dcc849217166e9f9284
Kara Erickson <karakara@google.com>
2018-09-20 18:25:20 +0000

    f1cb460 ci: only run `aio_preview` job on PR builds (#26030)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+10.sha-0ec925b
+ * @license Angular v6.1.8+11.sha-f1cb460
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+10.sha-0ec925b
+ * @license Angular v6.1.8+11.sha-f1cb460
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5217,7 +5217,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.8+10.sha-0ec925b');
+var VERSION = new Version('6.1.8+11.sha-f1cb460');
 
 /**
  * @license
