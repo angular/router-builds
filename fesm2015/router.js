ab84a8c8f4136281b68d4c8467ba365042568f5b
Kara Erickson <karakara@google.com>
2018-09-21 18:06:42 +0000

    4809483 docs: add Sajee to contributors (#26028)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+30.sha-9931bd7
+ * @license Angular v7.0.0-beta.6+29.sha-4809483
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6801,7 +6801,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.6+30.sha-9931bd7');
+const VERSION = new Version('7.0.0-beta.6+29.sha-4809483');
 
 /**
  * @fileoverview added by tsickle
