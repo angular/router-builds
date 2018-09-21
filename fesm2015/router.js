7c788ed97b6f2557a3c4514f9287d82862aa5273
Kara Erickson <karakara@google.com>
2018-09-21 18:55:08 +0000

    61b4c26 ci: correct github robot size check configuration (#26057)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+13.sha-a135f48
+ * @license Angular v6.1.8+12.sha-61b4c26
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6785,7 +6785,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.8+13.sha-a135f48');
+const VERSION = new Version('6.1.8+12.sha-61b4c26');
 
 /**
  * @fileoverview added by tsickle
