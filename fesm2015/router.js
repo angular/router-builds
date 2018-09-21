11e868a00b7e96891076540597f961d752fb81a1
Kara Erickson <karakara@google.com>
2018-09-21 18:56:25 +0000

    05d1b84 fix(docs-infra): ensure that only search is removed from URL on click (#26056)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+12.sha-61b4c26
+ * @license Angular v6.1.8+18.sha-05d1b84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6798,7 +6798,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.8+12.sha-61b4c26');
+const VERSION = new Version('6.1.8+18.sha-05d1b84');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+12.sha-61b4c26
+ * @license Angular v6.1.8+18.sha-05d1b84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
