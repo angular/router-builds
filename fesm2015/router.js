763c0be36c364d07127345d67f6e8fe021d22bf6
Kara Erickson <karakara@google.com>
2018-09-21 19:02:06 +0000

    ee9f0b5 build(docs-infra): do not include license comment in first API doc (#26050)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+18.sha-05d1b84
+ * @license Angular v6.1.8+14.sha-ee9f0b5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6798,7 +6798,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.8+18.sha-05d1b84');
+const VERSION = new Version('6.1.8+14.sha-ee9f0b5');
 
 /**
  * @fileoverview added by tsickle
