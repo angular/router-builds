d97b81bfe6ab28d6782eeffe8574cdf75c4df913
Kara Erickson <karakara@google.com>
2018-09-21 01:40:40 +0000

    d5f47d6 refactor(ivy): special injection tokens should not be cached (#26048)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+25.sha-64aa670
+ * @license Angular v7.0.0-beta.6+26.sha-d5f47d6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6930,7 +6930,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.6+25.sha-64aa670');
+const VERSION = new Version('7.0.0-beta.6+26.sha-d5f47d6');
 
 /**
  * @fileoverview added by tsickle
