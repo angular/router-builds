aa8b1f64e860bed80a8ec83b43ddd86c4d8ef376
Kara Erickson <karakara@google.com>
2018-09-22 00:43:18 +0000

    7f1cace build(docs-infra): sort NgModule exports by id (#26051)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+35.sha-82a14dc
+ * @license Angular v7.0.0-beta.6+37.sha-7f1cace
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6854,7 +6854,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.6+35.sha-82a14dc');
+const VERSION = new Version('7.0.0-beta.6+37.sha-7f1cace');
 
 /**
  * @fileoverview added by tsickle
