d40a5b551da8f577545a0be4f78eb4e75ae512d5
Kara Erickson <karakara@google.com>
2018-09-22 01:36:55 +0000

    37f3b92 build(docs-infra): sort NgModule exports by id (#26051)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+14.sha-ee9f0b5
+ * @license Angular v6.1.8+20.sha-37f3b92
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6798,7 +6798,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.8+14.sha-ee9f0b5');
+const VERSION = new Version('6.1.8+20.sha-37f3b92');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+14.sha-ee9f0b5
+ * @license Angular v6.1.8+20.sha-37f3b92
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
