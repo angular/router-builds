38a404b7d4b0b4f27903d96041c5593d5ded6527
Kara Erickson <karakara@google.com>
2018-09-24 21:29:28 +0000

    cf095d9 docs: fix a typo (#26074) PR Close #26074

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+56.sha-23ec88e
+ * @license Angular v7.0.0-beta.6+57.sha-cf095d9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6867,7 +6867,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.6+56.sha-23ec88e');
+const VERSION = new Version('7.0.0-beta.6+57.sha-cf095d9');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+56.sha-23ec88e
+ * @license Angular v7.0.0-beta.6+57.sha-cf095d9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5266,7 +5266,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.6+56.sha-23ec88e');
+var VERSION = new Version('7.0.0-beta.6+57.sha-cf095d9');
 
 /**
  * @license
