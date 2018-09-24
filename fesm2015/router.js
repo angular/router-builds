c836d2035a42d364078c412659d9f0e1b4e37087
Kara Erickson <karakara@google.com>
2018-09-24 18:15:22 +0000

    a169743 fix(service-worker): do not blow up when caches are unwritable (#26042)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+35.sha-77d2cbd
+ * @license Angular v6.1.8+38.sha-a169743
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6811,7 +6811,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.8+35.sha-77d2cbd');
+const VERSION = new Version('6.1.8+38.sha-a169743');
 
 /**
  * @fileoverview added by tsickle
