bcd9d63a9b1820938a356985bd45b777713a20a5
Kara Erickson <karakara@google.com>
2018-09-24 17:38:23 +0000

    b907e5a docs: correct path reference in upgrade guide (#26072)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+34.sha-a730fc7
+ * @license Angular v6.1.8+36.sha-b907e5a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6811,7 +6811,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.8+34.sha-a730fc7');
+const VERSION = new Version('6.1.8+36.sha-b907e5a');
 
 /**
  * @fileoverview added by tsickle
