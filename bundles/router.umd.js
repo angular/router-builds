f6b67aa6ad653077a6a50b4a7fd622b2b5065aff
Kara Erickson <karakara@google.com>
2018-09-24 16:55:46 +0000

    1e02cd9 docs: Fixes typo in FormArray (#26031) PR Close #26031

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+52.sha-47eb212
+ * @license Angular v7.0.0-beta.6+54.sha-1e02cd9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5510,7 +5510,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-beta.6+52.sha-47eb212');
+    var VERSION = new core.Version('7.0.0-beta.6+54.sha-1e02cd9');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+52.sha-47eb212
+ * @license Angular v7.0.0-beta.6+54.sha-1e02cd9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6814,7 +6814,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.6+52.sha-47eb212');
+const VERSION = new Version('7.0.0-beta.6+54.sha-1e02cd9');
 
 /**
  * @fileoverview added by tsickle
