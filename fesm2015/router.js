c9f026960e9c88e1b120e07c95a172aa3f308ddb
Kara Erickson <karakara@google.com>
2018-09-25 20:32:47 +0000

    6a62ed2 fix(ivy): objects like ElementRef should not use a special injection fn (#26064)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+59.sha-482e12c
+ * @license Angular v7.0.0-beta.6+60.sha-6a62ed2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6814,7 +6814,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.6+59.sha-482e12c');
+const VERSION = new Version('7.0.0-beta.6+60.sha-6a62ed2');
 
 /**
  * @fileoverview added by tsickle
