8aae4f106aa199fc25b3489e46917465f507bc71
Kara Erickson <karakara@google.com>
2018-09-26 20:54:06 +0000

    4597582 docs: release notes for the v6.1.9 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+88.sha-f29b218
+ * @license Angular v7.0.0-beta.6+89.sha-4597582
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6867,7 +6867,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.6+88.sha-f29b218');
+const VERSION = new Version('7.0.0-beta.6+89.sha-4597582');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+88.sha-f29b218
+ * @license Angular v7.0.0-beta.6+89.sha-4597582
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
