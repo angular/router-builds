51774aa6af90c45d1fd12c49900e47feff0a4fb5
Kara Erickson <karakara@google.com>
2018-09-26 23:07:22 +0000

    e1990a5 docs: firefox web components info (#26118)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7
+ * @license Angular v7.0.0-beta.7+1.sha-e1990a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5007,7 +5007,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.7');
+const VERSION = new Version('7.0.0-beta.7+1.sha-e1990a5');
 
 /**
  * @license
