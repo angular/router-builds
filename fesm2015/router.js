8418e66e24e8adf98415b0afeeb3aa316da2f757
Kara Erickson <karakara@google.com>
2018-09-26 23:03:51 +0000

    a01acec fix(docs-infra): use correct parameters for paginated requests to GitHub (#25671)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+89.sha-4597582
+ * @license Angular v7.0.0-beta.7+18.sha-a01acec
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6814,7 +6814,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.6+89.sha-4597582');
+const VERSION = new Version('7.0.0-beta.7+18.sha-a01acec');
 
 /**
  * @fileoverview added by tsickle
