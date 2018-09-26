ac3237abca686dde4718656ac76d2932fa5c5942
Kara Erickson <karakara@google.com>
2018-09-26 03:20:39 +0000

    559c647 docs: fix issues related to tutorial. (#24445)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.8+59.sha-c8817f3
+ * @license Angular v6.1.8+41.sha-559c647
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6811,7 +6811,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.8+59.sha-c8817f3');
+const VERSION = new Version('6.1.8+41.sha-559c647');
 
 /**
  * @fileoverview added by tsickle
