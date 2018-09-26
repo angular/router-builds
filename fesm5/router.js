0425b2bf58bbc6232a49a7e1cb3fcb275355d294
Kara Erickson <karakara@google.com>
2018-09-26 23:07:03 +0000

    4cff5b2 release: cut the v7.0.0-beta.7 release

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7+18.sha-a01acec
+ * @license Angular v7.0.0-beta.7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5422,7 +5422,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.7+18.sha-a01acec');
+var VERSION = new Version('7.0.0-beta.7');
 
 /**
  * @license
