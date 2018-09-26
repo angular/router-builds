3b42c4be0d77bbcfc7289fdcb5e79d60c03ff4c4
Kara Erickson <karakara@google.com>
2018-09-26 17:59:24 +0000

    bc88f31 docs: update routing integration section based on feedback (#20023)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+80.sha-15a2b8f
+ * @license Angular v7.0.0-beta.6+86.sha-bc88f31
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5422,7 +5422,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.6+80.sha-15a2b8f');
+var VERSION = new Version('7.0.0-beta.6+86.sha-bc88f31');
 
 /**
  * @license
