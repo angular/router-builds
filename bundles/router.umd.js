a0c6fe6920195f75069b14bc93be36638d937e08
Kara Erickson <karakara@google.com>
2018-09-26 07:43:51 +0000

    15a2b8f fix(ivy): wrapper fns arent necessary anymore (#26108)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+79.sha-d191085
+ * @license Angular v7.0.0-beta.6+80.sha-15a2b8f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5510,7 +5510,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-beta.6+79.sha-d191085');
+    var VERSION = new core.Version('7.0.0-beta.6+80.sha-15a2b8f');
 
     /**
      * @license
