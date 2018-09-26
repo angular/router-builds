feb9032684304ff98a7793d8fef2487f2c4f4450
Kara Erickson <karakara@google.com>
2018-09-26 19:46:41 +0000

    15a2b8f fix(ivy): wrapper fns arent necessary anymore (#26108)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+58.sha-0c34471
+ * @license Angular v7.0.0-beta.6+80.sha-15a2b8f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5007,7 +5007,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.6+58.sha-0c34471');
+const VERSION = new Version('7.0.0-beta.6+80.sha-15a2b8f');
 
 /**
  * @license
