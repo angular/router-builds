63fd4e667c45f2511bc2b5e4ff19b57aaadf7d78
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 19:55:33 +0000

    63b795a refactor(ivy): make sure that test bed symbols are imported from ivy_switch (#26121)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7+29.sha-5f6900e
+ * @license Angular v7.0.0-beta.7+30.sha-63b795a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6867,7 +6867,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.7+29.sha-5f6900e');
+const VERSION = new Version('7.0.0-beta.7+30.sha-63b795a');
 
 /**
  * @fileoverview added by tsickle
