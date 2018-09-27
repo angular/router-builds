49c5d19b9f0e855b0ad3f25841eba77bdc4425c9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 23:14:57 +0000

    f455518 docs: integrate cli doc from wiki into main doc (#25776)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7+47.sha-7cf5807
+ * @license Angular v7.0.0-beta.7+48.sha-f455518
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6951,7 +6951,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.7+47.sha-7cf5807');
+const VERSION = new Version('7.0.0-beta.7+48.sha-f455518');
 
 /**
  * @fileoverview added by tsickle
