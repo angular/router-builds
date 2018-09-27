d5035bd6a60a6c5309d3376f7e4e77f42ae6a387
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 23:15:55 +0000

    7cf5807 fix(ivy): ensure [style] and [class] bindings are placed in the same instruction (#26126)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7+46.sha-9523991
+ * @license Angular v7.0.0-beta.7+47.sha-7cf5807
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7004,7 +7004,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.7+46.sha-9523991');
+const VERSION = new Version('7.0.0-beta.7+47.sha-7cf5807');
 
 /**
  * @fileoverview added by tsickle
