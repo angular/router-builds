ec285e32175875f593f4545dfb928d8801356583
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 20:00:06 +0000

    4d544bc style: update gulp task to format untracked and diff files separately (#24969)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7+31.sha-aac08e0
+ * @license Angular v7.0.0-beta.7+35.sha-4d544bc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5007,7 +5007,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.7+31.sha-aac08e0');
+const VERSION = new Version('7.0.0-beta.7+35.sha-4d544bc');
 
 /**
  * @license
