70b6ccff8d63a8de0c73abe53e6e073d028d6f76
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-27 20:24:51 +0000

    04a023c build: pass `stripExportPattern` as an array of `RegExp` (#26012)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+18.sha-c12e553
+ * @license Angular v6.1.9+19.sha-04a023c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5421,7 +5421,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.9+18.sha-c12e553');
+var VERSION = new Version('6.1.9+19.sha-04a023c');
 
 /**
  * @license
