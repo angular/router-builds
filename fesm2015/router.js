4b97fc9d5f51d1a45c36b216b57efe7f3159bae7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-28 17:19:32 +0000

    86e6a20 test: remove typescript 2.9 and 3.0 typings tests (#26151)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7+52.sha-794c359
+ * @license Angular v7.0.0-beta.7+50.sha-86e6a20
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5129,7 +5129,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.7+52.sha-794c359');
+const VERSION = new Version('7.0.0-beta.7+50.sha-86e6a20');
 
 /**
  * @license
