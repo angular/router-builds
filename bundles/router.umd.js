646127db4d970337b5336e04435d6612d5fc0815
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-28 17:57:08 +0000

    a48bf0b docs: fix a typo in the Universal guide (#25853)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+20.sha-22dc8ad
+ * @license Angular v6.1.9+21.sha-a48bf0b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5303,7 +5303,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.9+20.sha-22dc8ad');
+    var VERSION = new i0.Version('6.1.9+21.sha-a48bf0b');
 
     /**
      * @license
