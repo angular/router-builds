909f1a6ad148670aa070e142a60a288c4aba855f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-28 17:52:44 +0000

    22dc8ad build: use separate tags for ivy builds in `publish-build-artifacts.sh` (#26159)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+19.sha-04a023c
+ * @license Angular v6.1.9+20.sha-22dc8ad
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6811,7 +6811,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.9+19.sha-04a023c');
+const VERSION = new Version('6.1.9+20.sha-22dc8ad');
 
 /**
  * @fileoverview added by tsickle
