c96a0a8b74c8affe0fe6b5acf6ca21dbd23ebbb8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-01 18:08:28 +0000

    c1bf82a fix(docs-infra): remove unnecessary margin on short descriptions (#25768)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+21.sha-a48bf0b
+ * @license Angular v6.1.9+36.sha-c1bf82a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5303,7 +5303,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.9+21.sha-a48bf0b');
+    var VERSION = new i0.Version('6.1.9+36.sha-c1bf82a');
 
     /**
      * @license
