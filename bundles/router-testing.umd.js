62ab37fa2a9e125a0f2498020af4dc886bf630b5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-01 17:22:57 +0000

    68fadd9 refactor(ivy): replace LNode.nodeInjector with TNode.injectorIndex (#26177)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+23.sha-a2878b0
+ * @license Angular v7.0.0-rc.0+1.sha-68fadd9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+23.sha-a2878b0
+ * @license Angular v7.0.0-rc.0+1.sha-68fadd9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6931,7 +6931,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.0+23.sha-a2878b0');
+const VERSION = new Version('7.0.0-rc.0+1.sha-68fadd9');
 
 /**
  * @fileoverview added by tsickle
