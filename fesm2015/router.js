667cfbd402deeb5f70895e59bb4f03efea867efa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-01 17:22:30 +0000

    a2878b0 fix(docs-infra): remove unnecessary margin on short descriptions (#25768)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+5.sha-ab379ab
+ * @license Angular v7.0.0-rc.0+23.sha-a2878b0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6984,7 +6984,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.0+5.sha-ab379ab');
+const VERSION = new Version('7.0.0-rc.0+23.sha-a2878b0');
 
 /**
  * @fileoverview added by tsickle
