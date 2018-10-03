d679247594818400096fff7aa9fae69397490926
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-03 22:04:13 +0000

    d815e41 build(docs-infra): upgrade @angular/cli to 6.2.3 (#26145) (#25997)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+37.sha-66c2d08
+ * @license Angular v6.1.9+56.sha-d815e41
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5509,7 +5509,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.9+37.sha-66c2d08');
+    var VERSION = new core.Version('6.1.9+56.sha-d815e41');
 
     /**
      * @license
