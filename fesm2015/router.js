9e2e2e01c2e57cf19a2e115cdc52fcdc32c00596
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-03 23:10:36 +0000

    7306799 refactor(ivy): flatten LInjector into LViewData (#26220)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+25.sha-cb59d87
+ * @license Angular v7.0.0-rc.0+26.sha-7306799
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6931,7 +6931,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.0+25.sha-cb59d87');
+const VERSION = new Version('7.0.0-rc.0+26.sha-7306799');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+25.sha-cb59d87
+ * @license Angular v7.0.0-rc.0+26.sha-7306799
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
