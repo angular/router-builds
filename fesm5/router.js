8c4e3dde12ebabc31ff8a04ddfd5c1dfa71bd3fa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-03 20:10:06 +0000

    cb59d87 feat(ivy): compiler support to generate QUERY_READ_FROM_NODE calls (#26171)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+24.sha-d216a46
+ * @license Angular v7.0.0-rc.0+25.sha-cb59d87
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5383,7 +5383,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-rc.0+24.sha-d216a46');
+var VERSION = new Version('7.0.0-rc.0+25.sha-cb59d87');
 
 /**
  * @license
