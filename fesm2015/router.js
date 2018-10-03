5c54cc2523ac29a7ea671a881180c397fcf61066
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-03 22:02:54 +0000

    d815e41 build(docs-infra): upgrade @angular/cli to 6.2.3 (#26145) (#25997)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+37.sha-66c2d08
+ * @license Angular v6.1.9+56.sha-d815e41
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6811,7 +6811,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.9+37.sha-66c2d08');
+const VERSION = new Version('6.1.9+56.sha-d815e41');
 
 /**
  * @fileoverview added by tsickle
