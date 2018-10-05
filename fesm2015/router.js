a2603723ee6268eef1c0377a46015a150638df49
Jason Aden <jasonaden@google.com>
2018-10-05 23:24:39 +0000

    8c6c2fc docs: add fakeAsync test new feature document (#23117)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+70.sha-3b4d9dc
+ * @license Angular v6.1.9+69.sha-8c6c2fc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5006,7 +5006,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.9+70.sha-3b4d9dc');
+const VERSION = new Version('6.1.9+69.sha-8c6c2fc');
 
 /**
  * @license
