0f222b0de2c2cef93c80ac022b6292ef78771ccf
Jason Aden <jasonaden@google.com>
2018-10-05 21:49:11 +0000

    b4421bb docs: fix wording (#26207)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+62.sha-ecb28bf
+ * @license Angular v6.1.9+63.sha-b4421bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6811,7 +6811,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.9+62.sha-ecb28bf');
+const VERSION = new Version('6.1.9+63.sha-b4421bb');
 
 /**
  * @fileoverview added by tsickle
