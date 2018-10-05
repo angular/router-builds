a64fe94988962fa76064c804ba36bc73b4e1a7aa
Jason Aden <jasonaden@google.com>
2018-10-05 21:49:56 +0000

    ecb28bf docs: fix spelling errors (#26213)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+61.sha-5c5164b
+ * @license Angular v6.1.9+62.sha-ecb28bf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5303,7 +5303,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.9+61.sha-5c5164b');
+    var VERSION = new i0.Version('6.1.9+62.sha-ecb28bf');
 
     /**
      * @license
