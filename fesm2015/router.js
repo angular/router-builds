6ffcc16f1c05b718f750019fa1a91ffc3370d982
Jason Aden <jasonaden@google.com>
2018-10-05 23:21:14 +0000

    3886bfa docs: Rename 'QuickStart' into 'Getting Started' (#25762)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+64.sha-2fe401d
+ * @license Angular v6.1.9+68.sha-3886bfa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6811,7 +6811,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.9+64.sha-2fe401d');
+const VERSION = new Version('6.1.9+68.sha-3886bfa');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+64.sha-2fe401d
+ * @license Angular v6.1.9+68.sha-3886bfa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5421,7 +5421,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.9+64.sha-2fe401d');
+var VERSION = new Version('6.1.9+68.sha-3886bfa');
 
 /**
  * @license
