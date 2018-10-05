e54297ee6d14c275c8caa54d5469a567d2af4946
Jason Aden <jasonaden@google.com>
2018-10-05 23:16:02 +0000

    f543d71 docs: add package doc files (#26047)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+57.sha-ab6f055
+ * @license Angular v7.0.0-rc.0+59.sha-f543d71
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6920,7 +6920,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.0+57.sha-ab6f055');
+const VERSION = new Version('7.0.0-rc.0+59.sha-f543d71');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+57.sha-ab6f055
+ * @license Angular v7.0.0-rc.0+59.sha-f543d71
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
