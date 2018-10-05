3aaa9a11842c3a2911971f454b4b380e0a04914f
Jason Aden <jasonaden@google.com>
2018-10-05 21:44:08 +0000

    4006c9b docs: fix spelling errors (#26213)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+48.sha-8f25321
+ * @license Angular v7.0.0-rc.0+55.sha-4006c9b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6920,7 +6920,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.0+48.sha-8f25321');
+const VERSION = new Version('7.0.0-rc.0+55.sha-4006c9b');
 
 /**
  * @fileoverview added by tsickle
