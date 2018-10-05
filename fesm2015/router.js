d6ae1770c676f4c9cee7abaef1cd354a62f2074e
Jason Aden <jasonaden@google.com>
2018-10-05 23:17:09 +0000

    ab6f055 ci(docs-infra): show custom 404 page on preview server (for consistency) (#26199)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+58.sha-3a5cb1c
+ * @license Angular v7.0.0-rc.0+57.sha-ab6f055
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6973,7 +6973,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.0+58.sha-3a5cb1c');
+const VERSION = new Version('7.0.0-rc.0+57.sha-ab6f055');
 
 /**
  * @fileoverview added by tsickle
