bc6fd2cd5aecd7d510da5003d346e01fae609d23
Jason Aden <jasonaden@google.com>
2018-10-05 23:17:11 +0000

    55d54c7 fix(platform-browser): fix #22155, destroy hammer manager when off (#22156)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+62.sha-ccceff5
+ * @license Angular v7.0.0-rc.0+63.sha-55d54c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6920,7 +6920,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.0+62.sha-ccceff5');
+const VERSION = new Version('7.0.0-rc.0+63.sha-55d54c7');
 
 /**
  * @fileoverview added by tsickle
