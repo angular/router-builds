71b13b3200ab68c167facd1abba3383990d13914
Jason Aden <jasonaden@google.com>
2018-10-08 21:14:04 +0000

    ed04e99 docs: update docs to reflect the changes in RxJS 6 (#26238)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+72.sha-623adbb
+ * @license Angular v6.1.9+73.sha-ed04e99
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5303,7 +5303,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.9+72.sha-623adbb');
+    var VERSION = new i0.Version('6.1.9+73.sha-ed04e99');
 
     /**
      * @license
