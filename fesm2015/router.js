22385064d70aca9bccd3e99085ea3f83a3259f98
Jason Aden <jasonaden@google.com>
2018-10-08 22:06:11 +0000

    b22c376 Revert "fix(upgrade): properly destroy upgraded component elements and descendants (#26209)"

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+78.sha-8a6f372
+ * @license Angular v6.1.9+79.sha-b22c376
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6811,7 +6811,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.9+78.sha-8a6f372');
+const VERSION = new Version('6.1.9+79.sha-b22c376');
 
 /**
  * @fileoverview added by tsickle
