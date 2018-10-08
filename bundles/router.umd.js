9c39f65b6fd8958802256c721ef47f7c803a5f5f
Jason Aden <jasonaden@google.com>
2018-10-08 21:15:37 +0000

    8a6f372 build(docs-infra): upgrade Angular to 7.0.0-rc.0 and TypeScript to 3.1.1 (#26306)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+73.sha-ed04e99
+ * @license Angular v6.1.9+78.sha-8a6f372
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5509,7 +5509,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.9+73.sha-ed04e99');
+    var VERSION = new core.Version('6.1.9+78.sha-8a6f372');
 
     /**
      * @license
