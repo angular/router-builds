2d22d860cd027fc3c0b3733bc32f991d60f80200
Jason Aden <jasonaden@google.com>
2018-10-10 19:21:24 +0000

    e26cb21 docs: release notes for the v release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+81.sha-456f23f
+ * @license Angular v7.0.0-rc.0+102.sha-e26cb21
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5103,7 +5103,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-rc.0+81.sha-456f23f');
+const VERSION = new Version('7.0.0-rc.0+102.sha-e26cb21');
 
 /**
  * @license
