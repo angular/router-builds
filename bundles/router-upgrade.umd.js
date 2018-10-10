0c838e1fa7ea7872001b870501382b89f9d5cf7e
Jason Aden <jasonaden@google.com>
2018-10-10 21:45:49 +0000

    912f3d1 fix(upgrade): properly destroy upgraded component elements and descendants (#26209)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+102.sha-e26cb21
+ * @license Angular v7.0.0-rc.0+103.sha-912f3d1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+102.sha-e26cb21
+ * @license Angular v7.0.0-rc.0+103.sha-912f3d1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6920,7 +6920,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.0+102.sha-e26cb21');
+const VERSION = new Version('7.0.0-rc.0+103.sha-912f3d1');
 
 /**
  * @fileoverview added by tsickle
