23b3dedbb0ae248660a8c7c5c2f6cc3a21c4a849
Miško Hevery <misko@hevery.com>
2018-10-11 21:51:09 +0000

    f1e14a3 docs: add angularmix to events page (#26374)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+2.sha-65555fe
+ * @license Angular v6.1.10+4.sha-f1e14a3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5421,7 +5421,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.10+2.sha-65555fe');
+var VERSION = new Version('6.1.10+4.sha-f1e14a3');
 
 /**
  * @license
