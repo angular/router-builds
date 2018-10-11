57b7bc171b4a465954bc263066d65688e22a9796
Miško Hevery <misko@hevery.com>
2018-10-11 23:17:03 +0000

    0a3f817 refactor(platform-browser): remove type assertion on console.profileEnd (#26303)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+103.sha-912f3d1
+ * @license Angular v7.0.0-rc.1+22.sha-0a3f817
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5103,7 +5103,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-rc.0+103.sha-912f3d1');
+const VERSION = new Version('7.0.0-rc.1+22.sha-0a3f817');
 
 /**
  * @license
