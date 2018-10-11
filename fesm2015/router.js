1e70f38196c0ef214369c46572a4862ad9a40bfa
Miško Hevery <misko@hevery.com>
2018-10-11 21:49:15 +0000

    1e69d60 fix(ivy): ensure that `ɵNgModuleDefWithMeta` is exported (#26082)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+6.sha-7d0e175
+ * @license Angular v7.0.0-rc.1+16.sha-1e69d60
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5103,7 +5103,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-rc.1+6.sha-7d0e175');
+const VERSION = new Version('7.0.0-rc.1+16.sha-1e69d60');
 
 /**
  * @license
