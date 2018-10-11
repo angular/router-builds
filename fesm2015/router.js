5a496d87ef2379427f6b373341267cfaab0bb17f
Miško Hevery <misko@hevery.com>
2018-10-11 21:49:39 +0000

    4b494f2 fix(ivy): fix generated code for style bindings with units (#26397)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+16.sha-1e69d60
+ * @license Angular v7.0.0-rc.1+19.sha-4b494f2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5103,7 +5103,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-rc.1+16.sha-1e69d60');
+const VERSION = new Version('7.0.0-rc.1+19.sha-4b494f2');
 
 /**
  * @license
