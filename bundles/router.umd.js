c7f9635876edce6b6cad1812251f1277dbc6e702
Miško Hevery <misko@hevery.com>
2018-10-11 21:51:55 +0000

    f1e14a3 docs: add angularmix to events page (#26374)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+2.sha-65555fe
+ * @license Angular v6.1.10+4.sha-f1e14a3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5303,7 +5303,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.10+2.sha-65555fe');
+    var VERSION = new i0.Version('6.1.10+4.sha-f1e14a3');
 
     /**
      * @license
