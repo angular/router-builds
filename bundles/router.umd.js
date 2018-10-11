e25635d66affb38c43e7365a50e4a0d0db63f7ab
Miško Hevery <misko@hevery.com>
2018-10-11 21:06:08 +0000

    7d0e175 ci(docs-infra): allow `aio_local` builds to fail on Travis (#26366)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+5.sha-053bf27
+ * @license Angular v7.0.0-rc.1+6.sha-7d0e175
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5596,7 +5596,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-rc.1+5.sha-053bf27');
+    var VERSION = new core.Version('7.0.0-rc.1+6.sha-7d0e175');
 
     /**
      * @license
