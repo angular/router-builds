a76ce9e6cd901e0c90f49d83c2f6189c46747b7d
Miško Hevery <misko@hevery.com>
2018-10-11 21:52:36 +0000

    6a64ac4 fix(ivy): Renderer2 should not use a special injection fn (#26369)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+22.sha-0a3f817
+ * @license Angular v7.0.0-rc.1+21.sha-6a64ac4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5596,7 +5596,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-rc.1+22.sha-0a3f817');
+    var VERSION = new core.Version('7.0.0-rc.1+21.sha-6a64ac4');
 
     /**
      * @license
