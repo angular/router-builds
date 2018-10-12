3073c48e38a48db3dd876854fef95b61322bfa2e
Miško Hevery <misko@hevery.com>
2018-10-12 05:31:04 +0000

    071934e fix(upgrade): properly destroy upgraded component elements and descendants (#26209)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+22.sha-0a3f817
+ * @license Angular v7.0.0-rc.1+27.sha-071934e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __values, __extends, __decorate, __param, __metadata, __assign, __spread } from 'tslib';
+import { __values, __decorate, __param, __metadata, __extends, __assign, __spread } from 'tslib';
 import { Component, ɵisObservable, ɵisPromise, NgModuleRef, InjectionToken, NgModuleFactory, NgZone, isDevMode, ɵConsole, Attribute, Directive, ElementRef, HostBinding, HostListener, Input, Renderer2, ChangeDetectorRef, ContentChildren, QueryList, ComponentFactoryResolver, EventEmitter, Output, ViewContainerRef, Compiler, Injectable, Injector, NgModuleFactoryLoader, ANALYZE_FOR_ENTRY_COMPONENTS, APP_BOOTSTRAP_LISTENER, APP_INITIALIZER, ApplicationRef, Inject, NgModule, NgProbeToken, Optional, SkipSelf, SystemJsNgModuleLoader, Version } from '@angular/core';
 import { from, of, BehaviorSubject, EmptyError, Observable, EMPTY, Subject } from 'rxjs';
 import { concatAll, every, last, map, mergeAll, catchError, first, mergeMap, switchMap, concatMap, reduce, filter, finalize, tap } from 'rxjs/operators';
@@ -5508,7 +5508,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-rc.1+22.sha-0a3f817');
+var VERSION = new Version('7.0.0-rc.1+27.sha-071934e');
 
 /**
  * @license
