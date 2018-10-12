a406e2c0fb726ef7392fbc3129dea39cfe18f510
Miško Hevery <misko@hevery.com>
2018-10-12 21:40:11 +0000

    a19b690 docs: move cli commands above api in nav (#26405)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+38.sha-9e32dc7
+ * @license Angular v7.0.0-rc.1+37.sha-a19b690
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5508,7 +5508,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-rc.1+38.sha-9e32dc7');
+var VERSION = new Version('7.0.0-rc.1+37.sha-a19b690');
 
 /**
  * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+38.sha-9e32dc7
+ * @license Angular v7.0.0-rc.1+37.sha-a19b690
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
