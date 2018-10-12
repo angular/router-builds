5ab3f6ffc706964a0f7f7b0f64f38c6294954874
Miško Hevery <misko@hevery.com>
2018-10-12 16:37:45 +0000

    db49bea build(docs-infra): upgrade @angular/material to 7.0.0-rc.1 (#26394)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+5.sha-4f09f7d
+ * @license Angular v6.1.10+8.sha-db49bea
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5421,7 +5421,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.10+5.sha-4f09f7d');
+var VERSION = new Version('6.1.10+8.sha-db49bea');
 
 /**
  * @license
