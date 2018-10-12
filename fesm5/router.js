a6bbd133f01a9cc188ab453a18d63eee25c3b96b
Miško Hevery <misko@hevery.com>
2018-10-12 17:17:39 +0000

    ff15043 fix(core): allow null value for renderer setElement(…) (#17065)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+27.sha-071934e
+ * @license Angular v7.0.0-rc.1+34.sha-ff15043
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5508,7 +5508,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-rc.1+27.sha-071934e');
+var VERSION = new Version('7.0.0-rc.1+34.sha-ff15043');
 
 /**
  * @license
