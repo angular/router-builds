c5bcd9bf98abbd584107fc689719adeb217b6abe
Miško Hevery <misko@hevery.com>
2018-10-12 16:33:56 +0000

    7777a99 fix(docs-infra): highlight current CLI command in navigation menu (#26388)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+31.sha-9ebb4c0
+ * @license Angular v7.0.0-rc.1+32.sha-7777a99
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5508,7 +5508,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-rc.1+31.sha-9ebb4c0');
+var VERSION = new Version('7.0.0-rc.1+32.sha-7777a99');
 
 /**
  * @license
