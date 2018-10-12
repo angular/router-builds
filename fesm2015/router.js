011b3ad60b3cb37bf97bf96a3d3fee123a6e0848
Miško Hevery <misko@hevery.com>
2018-10-12 04:33:32 +0000

    735dfd3 refactor(ivy): replace LNode.dynamicLContainerNode with flat LContainers (#26407)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+27.sha-071934e
+ * @license Angular v7.0.0-rc.1+26.sha-735dfd3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6920,7 +6920,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+27.sha-071934e');
+const VERSION = new Version('7.0.0-rc.1+26.sha-735dfd3');
 
 /**
  * @fileoverview added by tsickle
