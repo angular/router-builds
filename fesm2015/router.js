631ca5513eb6c470853dddc4b32d30f79236692c
Miško Hevery <misko@hevery.com>
2018-10-12 04:49:49 +0000

    4f09f7d fix(upgrade): properly destroy upgraded component elements and descendants (#26209)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+3.sha-50de03a
+ * @license Angular v6.1.10+5.sha-4f09f7d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5006,7 +5006,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.10+3.sha-50de03a');
+const VERSION = new Version('6.1.10+5.sha-4f09f7d');
 
 /**
  * @license
