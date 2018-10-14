f7e9e7a1bb8f743e12adfd1382d85b747acbb023
Miško Hevery <misko@hevery.com>
2018-10-14 05:06:56 +0000

    1f06b6c build: upgrade `@types/jasminewd2` to 2.0.4 (#26432)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+10.sha-6790709
+ * @license Angular v6.1.10+11.sha-1f06b6c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5006,7 +5006,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.10+10.sha-6790709');
+const VERSION = new Version('6.1.10+11.sha-1f06b6c');
 
 /**
  * @license
