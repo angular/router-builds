57fa5d32ca6e90d6d9f8a974b956bf7aee8d115d
Miško Hevery <misko@hevery.com>
2018-10-15 19:05:16 +0000

    a251374 docs: update process for cli tool and restructure doc (#25752)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+13.sha-2b00c17
+ * @license Angular v6.1.10+14.sha-a251374
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6811,7 +6811,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.10+13.sha-2b00c17');
+const VERSION = new Version('6.1.10+14.sha-a251374');
 
 /**
  * @fileoverview added by tsickle
