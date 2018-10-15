ebb5b0cbd8730a57e0fab5e5cbe95d211e25ac9a
Miško Hevery <misko@hevery.com>
2018-10-15 18:56:29 +0000

    e76a570 refactor(ivy): remove LNode (#26426)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+41.sha-931e603
+ * @license Angular v7.0.0-rc.1+43.sha-e76a570
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5596,7 +5596,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-rc.1+41.sha-931e603');
+    var VERSION = new core.Version('7.0.0-rc.1+43.sha-e76a570');
 
     /**
      * @license
