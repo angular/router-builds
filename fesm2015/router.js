c9b03dfcf5fa51d17937b6f9e3b27aeecc53002e
Miško Hevery <misko@hevery.com>
2018-10-15 22:49:30 +0000

    327c614 test(docs-infra): improve logging output in `test-pwa-score[-localhost]` (#26459)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+44.sha-5183bbf
+ * @license Angular v7.0.0-rc.1+47.sha-327c614
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6920,7 +6920,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+44.sha-5183bbf');
+const VERSION = new Version('7.0.0-rc.1+47.sha-327c614');
 
 /**
  * @fileoverview added by tsickle
