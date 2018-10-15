06ddefbe1a041da6b09f64d362f7034d69945424
Miško Hevery <misko@hevery.com>
2018-10-15 18:58:45 +0000

    9afc9a7 docs: minor wording correction. "use" to "user". (#26452) PR Close #26452

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+43.sha-e76a570
+ * @license Angular v7.0.0-rc.1+42.sha-9afc9a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+43.sha-e76a570
+ * @license Angular v7.0.0-rc.1+42.sha-9afc9a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6973,7 +6973,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+43.sha-e76a570');
+const VERSION = new Version('7.0.0-rc.1+42.sha-9afc9a7');
 
 /**
  * @fileoverview added by tsickle
