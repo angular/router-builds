47e4c2a178686515f2397fe0706b847ca0a46ba6
Miško Hevery <misko@hevery.com>
2018-10-15 17:58:49 +0000

    81724f5 ci(docs-infra): allow `aio_local` builds to fail on Travis

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+11.sha-1f06b6c
+ * @license Angular v6.1.10+12.sha-81724f5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6811,7 +6811,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.10+11.sha-1f06b6c');
+const VERSION = new Version('6.1.10+12.sha-81724f5');
 
 /**
  * @fileoverview added by tsickle
