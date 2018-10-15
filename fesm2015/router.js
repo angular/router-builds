b14b9f467a5743926fcba300f3864e2c16797a94
Miško Hevery <misko@hevery.com>
2018-10-15 17:39:34 +0000

    45732e5 fix(ivy): fix reference to pipeBind instruction with 2 args (#26451)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+39.sha-b2db32b
+ * @license Angular v7.0.0-rc.1+40.sha-45732e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6920,7 +6920,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+39.sha-b2db32b');
+const VERSION = new Version('7.0.0-rc.1+40.sha-45732e5');
 
 /**
  * @fileoverview added by tsickle
