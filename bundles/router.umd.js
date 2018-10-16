b8dc6478a95e89be7fcc58aa4814bb874bfd1c4f
Miško Hevery <misko@hevery.com>
2018-10-16 22:23:21 +0000

    81c9720 docs: fix links to setup and cli docs (#26463)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+56.sha-ea2cfbb
+ * @license Angular v7.0.0-rc.1+57.sha-81c9720
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5426,7 +5426,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('7.0.0-rc.1+56.sha-ea2cfbb');
+    var VERSION = new i0.Version('7.0.0-rc.1+57.sha-81c9720');
 
     /**
      * @license
