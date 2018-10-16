f8feb44bb5ad2b760c763d560a185af258c321b8
Miško Hevery <misko@hevery.com>
2018-10-16 00:16:10 +0000

    164f79a style: typos in TS 3.1 integration test (#26433)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+47.sha-327c614
+ * @license Angular v7.0.0-rc.1+48.sha-164f79a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+47.sha-327c614
+ * @license Angular v7.0.0-rc.1+48.sha-164f79a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6920,7 +6920,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+47.sha-327c614');
+const VERSION = new Version('7.0.0-rc.1+48.sha-164f79a');
 
 /**
  * @fileoverview added by tsickle
