fbef7974600a437d1373437700f64fe57f178512
Miško Hevery <misko@hevery.com>
2018-10-16 00:18:32 +0000

    dd8a851 docs: Indicate that PRs should have an associated issue (#25436) PR Close #25436

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+48.sha-164f79a
+ * @license Angular v7.0.0-rc.1+49.sha-dd8a851
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5352,7 +5352,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-rc.1+48.sha-164f79a');
+var VERSION = new Version('7.0.0-rc.1+49.sha-dd8a851');
 
 /**
  * @license
