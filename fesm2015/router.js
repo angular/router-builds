345dfa9c1a2b8ac8b3abc27a3c806670a2971bff
Miško Hevery <misko@hevery.com>
2018-10-16 22:15:19 +0000

    ea2cfbb docs: remove setup for local dev and anatomy of setup docs from nav (#26380)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+55.sha-2326b9c
+ * @license Angular v7.0.0-rc.1+56.sha-ea2cfbb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5104,7 +5104,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-rc.1+55.sha-2326b9c');
+const VERSION = new Version('7.0.0-rc.1+56.sha-ea2cfbb');
 
 /**
  * @license
