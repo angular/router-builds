c3f4d940031d2758cee51622a5312711d4a6c10c
Miško Hevery <misko@hevery.com>
2018-10-16 22:14:51 +0000

    41de0e0 docs: overview for cli reference section (#26043)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+53.sha-f6a2dbf
+ * @license Angular v7.0.0-rc.1+54.sha-41de0e0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5426,7 +5426,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('7.0.0-rc.1+53.sha-f6a2dbf');
+    var VERSION = new i0.Version('7.0.0-rc.1+54.sha-41de0e0');
 
     /**
      * @license
