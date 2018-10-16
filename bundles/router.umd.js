3be1fda43e75b34d3a4f58ab3d00c5f0c6b512c5
Miško Hevery <misko@hevery.com>
2018-10-16 22:25:04 +0000

    0f7d2ca build: add more labels to review-pr script (#26493)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+57.sha-81c9720
+ * @license Angular v7.0.0-rc.1+58.sha-0f7d2ca
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5426,7 +5426,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('7.0.0-rc.1+57.sha-81c9720');
+    var VERSION = new i0.Version('7.0.0-rc.1+58.sha-0f7d2ca');
 
     /**
      * @license
