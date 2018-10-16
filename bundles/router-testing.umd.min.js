5d03fe17524ec35659fed79f9302031915e8b38c
Miško Hevery <misko@hevery.com>
2018-10-16 00:23:04 +0000

    eeebe62 docs: Indicate that PRs should have an associated issue (#25436) PR Close #25436

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+17.sha-05f279d
+ * @license Angular v6.1.10+18.sha-eeebe62
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+17.sha-05f279d
+ * @license Angular v6.1.10+18.sha-eeebe62
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5303,7 +5303,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.10+17.sha-05f279d');
+    var VERSION = new i0.Version('6.1.10+18.sha-eeebe62');
 
     /**
      * @license
