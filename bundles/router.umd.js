22e3d6ed4575feedca55eb9945bd2aafb2d7ebd6
Misko Hevery <misko@hevery.com>
2018-10-17 04:11:27 +0000

    34aff0b docs(forms): change headings (#25900)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+54.sha-7ac4b76
+ * @license Angular v7.0.0-rc.1+58.sha-34aff0b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5426,7 +5426,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('7.0.0-rc.1+54.sha-7ac4b76');
+    var VERSION = new i0.Version('7.0.0-rc.1+58.sha-34aff0b');
 
     /**
      * @license
