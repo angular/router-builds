8a761ca33b793d29ddb8b58122508744b42867c2
Misko Hevery <misko@hevery.com>
2018-10-17 19:20:41 +0000

    3fa876c fix(docs-infra): fix top menu logo position (#26473)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+68.sha-516515d
+ * @license Angular v7.0.0-rc.1+69.sha-3fa876c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5597,7 +5597,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-rc.1+68.sha-516515d');
+    var VERSION = new core.Version('7.0.0-rc.1+69.sha-3fa876c');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+68.sha-516515d
+ * @license Angular v7.0.0-rc.1+69.sha-3fa876c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6921,7 +6921,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+68.sha-516515d');
+const VERSION = new Version('7.0.0-rc.1+69.sha-3fa876c');
 
 /**
  * @fileoverview added by tsickle
