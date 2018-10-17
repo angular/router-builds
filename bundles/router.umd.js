58d9d27c7cc566ff98e507b7d57508d0a5f2bb20
Misko Hevery <misko@hevery.com>
2018-10-17 19:26:05 +0000

    d769b44 docs: change links to cli wiki to link to new aio docs (#26489)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+61.sha-017a087
+ * @license Angular v7.0.0-rc.1+64.sha-d769b44
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5426,7 +5426,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('7.0.0-rc.1+61.sha-017a087');
+    var VERSION = new i0.Version('7.0.0-rc.1+64.sha-d769b44');
 
     /**
      * @license
