c7a329b7a43e2294d174d5a7b3c8e38ddddd78b1
Misko Hevery <misko@hevery.com>
2018-10-17 19:25:56 +0000

    beebf7f docs: change links to cli wiki to link to new aio docs (#26489)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+74.sha-0ef1f7e
+ * @license Angular v7.0.0-rc.1+75.sha-beebf7f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6921,7 +6921,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+74.sha-0ef1f7e');
+const VERSION = new Version('7.0.0-rc.1+75.sha-beebf7f');
 
 /**
  * @fileoverview added by tsickle
