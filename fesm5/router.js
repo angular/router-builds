9eac451805d388b4e15a180d62153b8e12177804
Misko Hevery <misko@hevery.com>
2018-10-17 04:01:19 +0000

    371ffef test(ivy): enable unit tests for @angular/animations (#26470)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+58.sha-0f7d2ca
+ * @license Angular v7.0.0-rc.1+61.sha-371ffef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5509,7 +5509,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-rc.1+58.sha-0f7d2ca');
+var VERSION = new Version('7.0.0-rc.1+61.sha-371ffef');
 
 /**
  * @license
