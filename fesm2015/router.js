36b4764e0ebd1a3b658e4199e1519ce2665bdb10
Misko Hevery <misko@hevery.com>
2018-10-17 04:09:31 +0000

    34aff0b docs(forms): change headings (#25900)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+54.sha-7ac4b76
+ * @license Angular v7.0.0-rc.1+58.sha-34aff0b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6921,7 +6921,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+54.sha-7ac4b76');
+const VERSION = new Version('7.0.0-rc.1+58.sha-34aff0b');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+54.sha-7ac4b76
+ * @license Angular v7.0.0-rc.1+58.sha-34aff0b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
