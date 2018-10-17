e3328e18de9dfd27d7e7076824c10f4046074900
Misko Hevery <misko@hevery.com>
2018-10-17 19:19:43 +0000

    4b52257 fix(docs-infra): fix top menu logo position (#26473)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+58.sha-34aff0b
+ * @license Angular v7.0.0-rc.1+60.sha-4b52257
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6921,7 +6921,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+58.sha-34aff0b');
+const VERSION = new Version('7.0.0-rc.1+60.sha-4b52257');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+58.sha-34aff0b
+ * @license Angular v7.0.0-rc.1+60.sha-4b52257
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
