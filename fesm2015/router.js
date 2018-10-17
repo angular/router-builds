2751a750b76d5b1029a5b7c902dae532e1e64c0b
Miško Hevery <misko@hevery.com>
2018-10-17 20:42:21 +0000

    f8195e5 fix(ivy): stub TestBed.compileComponents implementation (#26506)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+76.sha-a43b80a
+ * @license Angular v7.0.0-rc.1+85.sha-f8195e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5104,7 +5104,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-rc.1+76.sha-a43b80a');
+const VERSION = new Version('7.0.0-rc.1+85.sha-f8195e5');
 
 /**
  * @license
