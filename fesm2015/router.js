ca72269fdf3ad98afce687a033eb8fd0c69d007a
Misko Hevery <misko@hevery.com>
2018-10-17 20:41:14 +0000

    8a3fd58 feat(ivy): i18n compiler support for i18nStart and i18nEnd instructions (#26442)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+64.sha-fa8e633
+ * @license Angular v7.0.0-rc.1+71.sha-8a3fd58
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5104,7 +5104,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-rc.1+64.sha-fa8e633');
+const VERSION = new Version('7.0.0-rc.1+71.sha-8a3fd58');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+64.sha-fa8e633
+ * @license Angular v7.0.0-rc.1+71.sha-8a3fd58
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
