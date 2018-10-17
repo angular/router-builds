f549ea8f992341825a4ca0f59531a0353a8925e2
Misko Hevery <misko@hevery.com>
2018-10-17 19:21:31 +0000

    ecd473b fix(docs-infra): don't hide contributor links on devices that cannot hover (#26410)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+69.sha-3fa876c
+ * @license Angular v7.0.0-rc.1+72.sha-ecd473b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+69.sha-3fa876c
+ * @license Angular v7.0.0-rc.1+72.sha-ecd473b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6921,7 +6921,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+69.sha-3fa876c');
+const VERSION = new Version('7.0.0-rc.1+72.sha-ecd473b');
 
 /**
  * @fileoverview added by tsickle
