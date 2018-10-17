d620831e998fe197e3c18d73d25be899219282cf
Misko Hevery <misko@hevery.com>
2018-10-17 19:23:32 +0000

    0ef1f7e docs: change links to cli wiki to link to new aio docs files m to z (#26492)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+72.sha-ecd473b
+ * @license Angular v7.0.0-rc.1+74.sha-0ef1f7e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5104,7 +5104,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-rc.1+72.sha-ecd473b');
+const VERSION = new Version('7.0.0-rc.1+74.sha-0ef1f7e');
 
 /**
  * @license
