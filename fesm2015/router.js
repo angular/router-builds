265ae2b003f89539ffa2eb03acceca742c2dc7be
Misko Hevery <misko@hevery.com>
2018-10-17 04:00:33 +0000

    fdfedce feat(router): add prioritizedGuardValue operator optimization and allowing UrlTree return from guard (#26478)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+61.sha-371ffef
+ * @license Angular v7.0.0-rc.1+60.sha-fdfedce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6921,7 +6921,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+61.sha-371ffef');
+const VERSION = new Version('7.0.0-rc.1+60.sha-fdfedce');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+61.sha-371ffef
+ * @license Angular v7.0.0-rc.1+60.sha-fdfedce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
