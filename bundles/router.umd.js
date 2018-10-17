7645461d4344c5a88b7624476ae41a4c4fe0d6aa
Misko Hevery <misko@hevery.com>
2018-10-17 19:23:20 +0000

    3808434 build(docs-infra): do not add extra space after links in tables (#26505)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+60.sha-4b52257
+ * @license Angular v7.0.0-rc.1+59.sha-3808434
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5426,7 +5426,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('7.0.0-rc.1+60.sha-4b52257');
+    var VERSION = new i0.Version('7.0.0-rc.1+59.sha-3808434');
 
     /**
      * @license
