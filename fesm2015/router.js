babf0963a4b3861fbc5b2d384297c4bee0d3e02a
Misko Hevery <misko@hevery.com>
2018-10-17 19:25:34 +0000

    b29e709 fix(docs-infra): rename "title" by "header" to avoid unwanted tooltips (#26396)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+64.sha-d769b44
+ * @license Angular v7.0.0-rc.1+62.sha-b29e709
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6921,7 +6921,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+64.sha-d769b44');
+const VERSION = new Version('7.0.0-rc.1+62.sha-b29e709');
 
 /**
  * @fileoverview added by tsickle
