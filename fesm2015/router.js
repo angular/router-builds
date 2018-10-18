d17b4a9dd203bc8cc108a89926a7dceb5e6e6b09
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:44:24 +0000

    624433c docs: removing errant double-quote (#26483) PR Close #26483

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+93.sha-09cc458
+ * @license Angular v7.0.0-rc.1+94.sha-624433c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5104,7 +5104,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-rc.1+93.sha-09cc458');
+const VERSION = new Version('7.0.0-rc.1+94.sha-624433c');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+93.sha-09cc458
+ * @license Angular v7.0.0-rc.1+94.sha-624433c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
