9f19c44cf22b07bf55dc9a02db84c5140dad8a21
Miško Hevery <misko@hevery.com>
2018-10-18 20:52:50 +0000

    6c48455 docs: add release announcement URL

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+7.sha-f85a969
+ * @license Angular v7.0.0+22.sha-6c48455
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+7.sha-f85a969
+ * @license Angular v7.0.0+22.sha-6c48455
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5509,7 +5509,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0+7.sha-f85a969');
+var VERSION = new Version('7.0.0+22.sha-6c48455');
 
 /**
  * @license
