e16de3dc7b92c56b2377cc90c5d64993da36ec95
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:46:52 +0000

    1a666de ci: add forms-overview guide to forms approval list (#26549)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+94.sha-624433c
+ * @license Angular v7.0.0-rc.1+98.sha-1a666de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5353,7 +5353,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-rc.1+94.sha-624433c');
+var VERSION = new Version('7.0.0-rc.1+98.sha-1a666de');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+94.sha-624433c
+ * @license Angular v7.0.0-rc.1+98.sha-1a666de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
