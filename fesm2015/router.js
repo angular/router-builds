82dd4ab52f5f86a7635fc5629c75e211c9bf351f
Miško Hevery <misko@hevery.com>
2018-10-18 20:54:21 +0000

    331989c docs: add release announcement URL

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+100.sha-bbf96db
+ * @license Angular v7.0.0-rc.1+101.sha-331989c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6921,7 +6921,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+100.sha-bbf96db');
+const VERSION = new Version('7.0.0-rc.1+101.sha-331989c');
 
 /**
  * @fileoverview added by tsickle
