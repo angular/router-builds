775593a9291641ccfaaeed972ec2fb48aefd84f5
Miško Hevery <misko@hevery.com>
2018-10-18 18:45:36 +0000

    bf656d6 release: cut the v7.0.0 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+78.sha-3b183ce
+ * @license Angular v7.0.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6974,7 +6974,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+78.sha-3b183ce');
+const VERSION = new Version('7.0.0');
 
 /**
  * @fileoverview added by tsickle
