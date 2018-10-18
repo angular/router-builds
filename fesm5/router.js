9e1ab5eb123308862c143a8579b907a232433b58
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-18 19:45:32 +0000

    a403c4f docs: type fix (#26386)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0
+ * @license Angular v7.0.0+1.sha-a403c4f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5353,7 +5353,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0');
+var VERSION = new Version('7.0.0+1.sha-a403c4f');
 
 /**
  * @license
