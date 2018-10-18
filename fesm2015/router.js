f24298980774fdca499928a7bf7c2fedff2dd77f
Miško Hevery <misko@hevery.com>
2018-10-18 17:28:46 +0000

    1035c6a build(docs-infra): update git ref for cli command docs (#26545)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+73.sha-a4ebf3f
+ * @license Angular v7.0.0-rc.1+75.sha-1035c6a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6921,7 +6921,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+73.sha-a4ebf3f');
+const VERSION = new Version('7.0.0-rc.1+75.sha-1035c6a');
 
 /**
  * @fileoverview added by tsickle
