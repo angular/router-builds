99545ac7763fe34f23a75b7c5c6dc94b38471ce8
Miško Hevery <misko@hevery.com>
2018-10-18 17:29:05 +0000

    d9d2260 build(docs-infra): allow `""` as empty region in `{@example}` tags (#26514)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+88.sha-d557f1d
+ * @license Angular v7.0.0-rc.1+91.sha-d9d2260
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6921,7 +6921,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+88.sha-d557f1d');
+const VERSION = new Version('7.0.0-rc.1+91.sha-d9d2260');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+88.sha-d557f1d
+ * @license Angular v7.0.0-rc.1+91.sha-d9d2260
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
