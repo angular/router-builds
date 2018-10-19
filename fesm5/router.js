45952c0fa32d104749f5a118a9db10aa317ced88
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 20:58:30 +0000

    31022cb feat(ivy): generate .ngsummary.js shims (#26495)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+115.sha-1700bd6
+ * @license Angular v7.0.0-rc.1+118.sha-31022cb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5353,7 +5353,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-rc.1+115.sha-1700bd6');
+var VERSION = new Version('7.0.0-rc.1+118.sha-31022cb');
 
 /**
  * @license
