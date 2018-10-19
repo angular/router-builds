08cd83be094bda36949eed7367f311b108f2abef
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:04:39 +0000

    d4cee51 refactor(ivy): obviate the Bazel component of the ivy_switch (#26550)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+85.sha-f8195e5
+ * @license Angular v7.0.0-rc.1+102.sha-d4cee51
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6974,7 +6974,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+85.sha-f8195e5');
+const VERSION = new Version('7.0.0-rc.1+102.sha-d4cee51');
 
 /**
  * @fileoverview added by tsickle
