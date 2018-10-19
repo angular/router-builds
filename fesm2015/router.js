2b892f80899024cea052f0c7c807bc069bc2a9a6
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:25:05 +0000

    1700bd6 docs: fix release info navigation (#26558)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+114.sha-b89bc37
+ * @license Angular v7.0.0-rc.1+115.sha-1700bd6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5104,7 +5104,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-rc.1+114.sha-b89bc37');
+const VERSION = new Version('7.0.0-rc.1+115.sha-1700bd6');
 
 /**
  * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+114.sha-b89bc37
+ * @license Angular v7.0.0-rc.1+115.sha-1700bd6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
