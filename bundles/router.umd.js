83132dce7b027e56db249f88166a03155a0f3dbe
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 20:56:34 +0000

    31022cb feat(ivy): generate .ngsummary.js shims (#26495)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+115.sha-1700bd6
+ * @license Angular v7.0.0-rc.1+118.sha-31022cb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5597,7 +5597,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('7.0.0-rc.1+115.sha-1700bd6');
+    var VERSION = new core.Version('7.0.0-rc.1+118.sha-31022cb');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+115.sha-1700bd6
+ * @license Angular v7.0.0-rc.1+118.sha-31022cb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6921,7 +6921,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+115.sha-1700bd6');
+const VERSION = new Version('7.0.0-rc.1+118.sha-31022cb');
 
 /**
  * @fileoverview added by tsickle
