82c9716ed555ca8c0bcf9915955e0ed05896c5dc
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:14:24 +0000

    5d90aed docs(service-worker): updated browser support for Service Worker (#26408)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+22.sha-6c48455
+ * @license Angular v7.0.0+26.sha-5d90aed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5104,7 +5104,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0+22.sha-6c48455');
+const VERSION = new Version('7.0.0+26.sha-5d90aed');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+22.sha-6c48455
+ * @license Angular v7.0.0+26.sha-5d90aed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
