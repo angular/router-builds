151c999f5c097a815bf19583869fe26a04aae612
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:15:23 +0000

    aa55d88 docs: forms overview copy edit (#26450)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+106.sha-b9bd95b
+ * @license Angular v7.0.0-rc.1+108.sha-aa55d88
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6974,7 +6974,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+106.sha-b9bd95b');
+const VERSION = new Version('7.0.0-rc.1+108.sha-aa55d88');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+106.sha-b9bd95b
+ * @license Angular v7.0.0-rc.1+108.sha-aa55d88
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
