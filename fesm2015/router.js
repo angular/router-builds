38a1e4ac821f2e5be85f367ec7d35d3700d8f257
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 20:36:58 +0000

    5b4cf38 refactor(bazel): remove workaround for TS bug (#26516)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+102.sha-d4cee51
+ * @license Angular v7.0.0-rc.1+111.sha-5b4cf38
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5104,7 +5104,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-rc.1+102.sha-d4cee51');
+const VERSION = new Version('7.0.0-rc.1+111.sha-5b4cf38');
 
 /**
  * @license
