08ae43c9762744989630667f3b8250ea255df4c5
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:13:38 +0000

    658a18c build(docs-infra): break long CLI options onto two lines (#26272)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+26.sha-5d90aed
+ * @license Angular v7.0.0+25.sha-658a18c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6921,7 +6921,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0+26.sha-5d90aed');
+const VERSION = new Version('7.0.0+25.sha-658a18c');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+26.sha-5d90aed
+ * @license Angular v7.0.0+25.sha-658a18c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5509,7 +5509,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0+26.sha-5d90aed');
+var VERSION = new Version('7.0.0+25.sha-658a18c');
 
 /**
  * @license
