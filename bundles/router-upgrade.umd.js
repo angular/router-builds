ea871379fad0652b750a9b797fd27bc5ca29114c
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:14:44 +0000

    078a928 docs: forms overview copy edit (#26450)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+25.sha-658a18c
+ * @license Angular v7.0.0+27.sha-078a928
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+25.sha-658a18c
+ * @license Angular v7.0.0+27.sha-078a928
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6921,7 +6921,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0+25.sha-658a18c');
+const VERSION = new Version('7.0.0+27.sha-078a928');
 
 /**
  * @fileoverview added by tsickle
