ff4f35669e9cc348bc09c9ee84f5e2328e6763ec
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:19:46 +0000

    716d887 docs: add Katerina Skroumpelou to GDE resources (#26547)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+110.sha-4c0ad52
+ * @license Angular v7.0.0-rc.1+112.sha-716d887
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5509,7 +5509,7 @@ function provideRouterInitializer() {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-rc.1+110.sha-4c0ad52');
+var VERSION = new Version('7.0.0-rc.1+112.sha-716d887');
 
 /**
  * @license
