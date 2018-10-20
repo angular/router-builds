e631bb3f18e1603db6ea21a3cea0b1aa6a1a68b3
Igor Minar <igor@angular.io>
2018-10-20 06:19:15 +0000

    c8adef2 build: remove manual tags from platform jasmine tests (#26606)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+52.sha-778e1c2
+ * @license Angular v7.0.0+53.sha-c8adef2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5431,7 +5431,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.0.0+52.sha-778e1c2');
+    var VERSION = new i0.Version('7.0.0+53.sha-c8adef2');
 
     /**
      * @license
