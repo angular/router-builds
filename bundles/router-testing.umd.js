b1d278857da8f37300e13e6491ae63446bc6ad92
Igor Minar <igor@angular.io>
2018-10-21 04:26:35 +0000

    100c7ef build: fix ivy package-dist generation (#26629)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+137.sha-4c2ce4e
+ * @license Angular v7.0.0-rc.1+156.sha-100c7ef.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
