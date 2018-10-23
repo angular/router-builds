0e849880729b50e56f0e7a06b958288269b3d9c8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-23 21:57:55 +0000

    b52c97c ci: disable ivy CI jobs on the patch branch (#26702)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+67.sha-929739c.with-local-changes
+ * @license Angular v7.0.0+68.sha-b52c97c.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+67.sha-929739c.with-local-changes
+ * @license Angular v7.0.0+68.sha-b52c97c.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5514,7 +5514,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.0+67.sha-929739c.with-local-changes');
+var VERSION = new Version('7.0.0+68.sha-b52c97c.with-local-changes');
 
 /**
  * @license
