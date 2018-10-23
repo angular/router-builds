e483762fa543e57565c97b339e5bad9360cb2547
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-23 21:03:40 +0000

    2f9e957 ci: re-encrypt .circleci/github_token (#26698)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+156.sha-100c7ef.with-local-changes
+ * @license Angular v7.0.0-rc.1+176.sha-2f9e957.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5514,7 +5514,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.0-rc.1+156.sha-100c7ef.with-local-changes');
+var VERSION = new Version('7.0.0-rc.1+176.sha-2f9e957.with-local-changes');
 
 /**
  * @license
