ace11663785dc8242c2a4c6698e73901322057bf
Igor Minar <igor@angular.io>
2018-10-23 21:37:34 +0000

    ee0b857 build: rename the ivy compile mode 'local' to 'aot' (#26686)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+156.sha-100c7ef.with-local-changes
+ * @license Angular v7.0.0-rc.1+178.sha-ee0b857.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
