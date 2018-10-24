e88efb0e7ba6f513eefd5e050a65644d25f24776
Matias Niemelä <matias@yearofmoo.com>
2018-10-24 23:03:13 +0000

    f6c2db8 fix(ivy): ensure styling pipes are allocated before used in bindings (#26593)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0
+ * @license Angular v7.1.0-beta.0+1.sha-f6c2db8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0-beta.0');
+    var VERSION = new core.Version('7.1.0-beta.0+1.sha-f6c2db8');
 
     /**
      * @license
