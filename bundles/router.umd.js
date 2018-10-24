fb9c7565f14954354136e3be0acbef67025aece6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-24 22:42:56 +0000

    67789f1 release: cut the v7.1.0-beta.0 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+194.sha-6e16a17
+ * @license Angular v7.1.0-beta.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5431,7 +5431,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.0.0-rc.1+194.sha-6e16a17');
+    var VERSION = new i0.Version('7.1.0-beta.0');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+194.sha-6e16a17
+ * @license Angular v7.1.0-beta.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5358,7 +5358,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.0-rc.1+194.sha-6e16a17');
+var VERSION = new Version('7.1.0-beta.0');
 
 /**
  * @license
