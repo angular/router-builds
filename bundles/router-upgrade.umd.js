713892a6e8c173848f9b1f55bf5010c75e365a79
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-24 22:59:13 +0000

    4e91ead style: format (#26708)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+178.sha-ee0b857.with-local-changes
+ * @license Angular v7.0.0-rc.1+192.sha-4e91ead.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
