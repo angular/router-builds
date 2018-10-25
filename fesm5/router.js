3b7a643dc34a84ebafa0d5c46af67892db5cacad
Matias Niemelä <matias@yearofmoo.com>
2018-10-25 00:13:00 +0000

    0cc9842 test(upgrade): make e2e tests for upgrade docs examples less flaky (#26726)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+1.sha-f6c2db8
+ * @license Angular v7.1.0-beta.0+5.sha-0cc9842
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5514,7 +5514,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+1.sha-f6c2db8');
+var VERSION = new Version('7.1.0-beta.0+5.sha-0cc9842');
 
 /**
  * @license
