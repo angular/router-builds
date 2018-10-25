ccaf590d6d5afeb97314af90ada9514f5ddd5bdf
Matias Niemelä <matias@yearofmoo.com>
2018-10-25 00:12:38 +0000

    0238744 test(upgrade): make e2e tests for upgrade docs examples less flaky (#26726)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+87.sha-062af7a.with-local-changes
+ * @license Angular v7.0.1+4.sha-0238744.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
