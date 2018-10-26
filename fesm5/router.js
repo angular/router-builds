e0563aaa399480e998cc5cd063b081549b1e871c
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 17:07:20 +0000

    aefa06f docs(core): update security guide sanitize example (#26777)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+8.sha-b0476f3
+ * @license Angular v7.1.0-beta.0+18.sha-aefa06f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5514,7 +5514,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+8.sha-b0476f3');
+var VERSION = new Version('7.1.0-beta.0+18.sha-aefa06f');
 
 /**
  * @license
