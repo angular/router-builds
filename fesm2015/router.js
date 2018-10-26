067afa54316f73e31891fe9266178a3b57b2999e
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 17:43:13 +0000

    9cd8327 ci: move CircleCI env variables definition to shell script (#26596)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+20.sha-7c730fe
+ * @license Angular v7.1.0-beta.0+23.sha-9cd8327
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6925,7 +6925,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.0+20.sha-7c730fe');
+const VERSION = new Version('7.1.0-beta.0+23.sha-9cd8327');
 
 /**
  * @fileoverview added by tsickle
