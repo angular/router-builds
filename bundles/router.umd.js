8286af4821d6d61d93db8cadee70781d7e25ef7d
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 21:54:33 +0000

    d50d400 build: fix formatting issue in host_binding_spec file

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+35.sha-19fcfc3
+ * @license Angular v7.1.0-beta.0+36.sha-d50d400
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0-beta.0+35.sha-19fcfc3');
+    var VERSION = new core.Version('7.1.0-beta.0+36.sha-d50d400');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+35.sha-19fcfc3
+ * @license Angular v7.1.0-beta.0+36.sha-d50d400
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
