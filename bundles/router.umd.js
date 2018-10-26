dd98187205b6dbc78da51c703087f036178e1aff
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 21:47:14 +0000

    19fcfc3 fix(compiler): generate inputs with aliases properly (#26774)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+34.sha-c048358
+ * @license Angular v7.1.0-beta.0+35.sha-19fcfc3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5431,7 +5431,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-beta.0+34.sha-c048358');
+    var VERSION = new i0.Version('7.1.0-beta.0+35.sha-19fcfc3');
 
     /**
      * @license
