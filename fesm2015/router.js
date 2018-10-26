045190ba574da1ecec2c1088a501e093c2fba459
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 23:00:52 +0000

    95993e1 fix(ivy): ensure pipes indices are referenced in styling bindings (#26755)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+42.sha-d2e6d69
+ * @license Angular v7.1.0-beta.0+44.sha-95993e1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6925,7 +6925,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.0+42.sha-d2e6d69');
+const VERSION = new Version('7.1.0-beta.0+44.sha-95993e1');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+42.sha-d2e6d69
+ * @license Angular v7.1.0-beta.0+44.sha-95993e1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5514,7 +5514,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+42.sha-d2e6d69');
+var VERSION = new Version('7.1.0-beta.0+44.sha-95993e1');
 
 /**
  * @license
