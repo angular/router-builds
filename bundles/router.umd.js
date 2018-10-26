8bb21dcb9d098ed369cbfa636702efa8aab253d9
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 19:10:34 +0000

    f385913 ci: configure chrome options for protractor (#26735)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+23.sha-9cd8327
+ * @license Angular v7.1.0-beta.0+30.sha-f385913
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5431,7 +5431,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-beta.0+23.sha-9cd8327');
+    var VERSION = new i0.Version('7.1.0-beta.0+30.sha-f385913');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+23.sha-9cd8327
+ * @license Angular v7.1.0-beta.0+30.sha-f385913
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6978,7 +6978,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.0+23.sha-9cd8327');
+const VERSION = new Version('7.1.0-beta.0+30.sha-f385913');
 
 /**
  * @fileoverview added by tsickle
