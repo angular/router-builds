21d40c3a5ce23a352abd713de95d009f5f10bbb8
Matias Niemelä <matias@yearofmoo.com>
2018-10-29 17:32:33 +0000

    c0bf222 ci: exclude ngcc from g3sync check (#26740)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+54.sha-d72d50d
+ * @license Angular v7.1.0-beta.0+55.sha-c0bf222
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0-beta.0+54.sha-d72d50d');
+    var VERSION = new core.Version('7.1.0-beta.0+55.sha-c0bf222');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+54.sha-d72d50d
+ * @license Angular v7.1.0-beta.0+55.sha-c0bf222
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5514,7 +5514,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+54.sha-d72d50d');
+var VERSION = new Version('7.1.0-beta.0+55.sha-c0bf222');
 
 /**
  * @license
