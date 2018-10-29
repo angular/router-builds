7e115bfeebe5da17ec2add8a6a01c15a2da4d9d7
Matias Niemelä <matias@yearofmoo.com>
2018-10-29 23:05:47 +0000

    96770e5 fix(ivy): optional dependencies should not throw with module injector (#26763)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+192.sha-4e91ead.with-local-changes
+ * @license Angular v7.1.0-beta.0+58.sha-96770e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+192.sha-4e91ead.with-local-changes
+ * @license Angular v7.1.0-beta.0+58.sha-96770e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5514,7 +5514,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.0-rc.1+192.sha-4e91ead.with-local-changes');
+var VERSION = new Version('7.1.0-beta.0+58.sha-96770e5');
 
 /**
  * @license
