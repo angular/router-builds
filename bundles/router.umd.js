cba54efc541baaf277132573fa5e6d014d574d86
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 18:22:51 +0000

    91d2368 docs: update example dependencies to Angular/CLI/Universal V7 releases (#26820)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+59.sha-578e4c7
+ * @license Angular v7.1.0-beta.0+61.sha-91d2368
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5431,7 +5431,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-beta.0+59.sha-578e4c7');
+    var VERSION = new i0.Version('7.1.0-beta.0+61.sha-91d2368');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+59.sha-578e4c7
+ * @license Angular v7.1.0-beta.0+61.sha-91d2368
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
