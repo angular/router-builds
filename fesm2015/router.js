7590ff606bd76263afed61fed47ecd5c4eaa7942
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 18:29:52 +0000

    95743e3 fix(core): support computed base class in metadata inheritance (#24014)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+64.sha-9ad54d7
+ * @license Angular v7.1.0-beta.0+65.sha-95743e3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6925,7 +6925,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.0+64.sha-9ad54d7');
+const VERSION = new Version('7.1.0-beta.0+65.sha-95743e3');
 
 /**
  * @fileoverview added by tsickle
