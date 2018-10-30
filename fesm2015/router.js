3a1ad1c3cefbf2617fbe8d4c700d764271ccb9d6
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 23:00:26 +0000

    332394d build(bazel): generalize fix for AMD name for flat module out file (#26837)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+73.sha-62b4ff52
+ * @license Angular v7.1.0-beta.0+74.sha-332394d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6978,7 +6978,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.0+73.sha-62b4ff52');
+const VERSION = new Version('7.1.0-beta.0+74.sha-332394d');
 
 /**
  * @fileoverview added by tsickle
