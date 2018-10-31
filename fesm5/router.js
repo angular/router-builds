012eba19287b75c985decf75faba225b33f2b56d
GitHub <noreply@github.com>
2018-10-31 22:31:17 +0000

    f4e7920 fixup! docs: add `Function Inlining` rules to `PERF_NOTES.md`

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+80.sha-774f6fb
+ * @license Angular v7.1.0-beta.0+82.sha-f4e7920
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5514,7 +5514,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+80.sha-774f6fb');
+var VERSION = new Version('7.1.0-beta.0+82.sha-f4e7920');
 
 /**
  * @license
