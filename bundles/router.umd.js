00c8ceb38e3b665bc03d68ee2c67c5b7c3a49fe9
Matias Niemelä <matias@yearofmoo.com>
2018-10-31 23:27:54 +0000

    ec29fd3 build: remove problematic `yarn` prefix for running bazel

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+77.sha-d042c4a
+ * @license Angular v7.1.0-beta.0+78.sha-ec29fd3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0-beta.0+77.sha-d042c4a');
+    var VERSION = new core.Version('7.1.0-beta.0+78.sha-ec29fd3');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+77.sha-d042c4a
+ * @license Angular v7.1.0-beta.0+78.sha-ec29fd3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5514,7 +5514,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+77.sha-d042c4a');
+var VERSION = new Version('7.1.0-beta.0+78.sha-ec29fd3');
 
 /**
  * @license
