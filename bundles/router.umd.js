228a6a41a5838c6229e8cddc9ea0c64314adf19b
Matias Niemelä <matias@yearofmoo.com>
2018-10-31 19:46:46 +0000

    d042c4a fix(core): Remove static dependency from @angular/core to @angular/compiler (#26734) PR Close #26734

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+74.sha-332394d
+ * @license Angular v7.1.0-beta.0+77.sha-d042c4a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5431,7 +5431,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-beta.0+74.sha-332394d');
+    var VERSION = new i0.Version('7.1.0-beta.0+77.sha-d042c4a');
 
     /**
      * @license
