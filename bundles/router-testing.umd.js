4280b78c0e215ef7c155dd7a7759936e2a61430d
Kara Erickson <karakara@google.com>
2018-11-01 21:15:28 +0000

    2810482 build: update `canonical-path` dependency (#26719)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2
+ * @license Angular v7.0.2+1.sha-2810482
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2
+ * @license Angular v7.0.2+1.sha-2810482
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6925,7 +6925,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.0.2');
+const VERSION = new Version('7.0.2+1.sha-2810482');
 
 /**
  * @fileoverview added by tsickle
