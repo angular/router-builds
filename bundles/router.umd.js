4d1b924ccfc136d88b5c492a441299a87b8cdbcc
Kara Erickson <karakara@google.com>
2018-11-01 20:54:06 +0000

    aadbc8a fix(ivy): sync exported r3 symbol with compiler generated symbol (#26877)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+14.sha-2e7b5c5
+ * @license Angular v7.1.0-beta.1+15.sha-aadbc8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5717,7 +5717,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0-beta.1+14.sha-2e7b5c5');
+    var VERSION = new core.Version('7.1.0-beta.1+15.sha-aadbc8a');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+14.sha-2e7b5c5
+ * @license Angular v7.1.0-beta.1+15.sha-aadbc8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5629,7 +5629,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+14.sha-2e7b5c5');
+var VERSION = new Version('7.1.0-beta.1+15.sha-aadbc8a');
 
 /**
  * @license
