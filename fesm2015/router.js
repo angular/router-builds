f2c434124e2414ab0b615f73de1a6847a1803098
Kara Erickson <karakara@google.com>
2018-11-01 21:05:47 +0000

    a2929df fix(ivy): dynamically created components should run init hooks (#26864)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+15.sha-aadbc8a
+ * @license Angular v7.1.0-beta.1+17.sha-a2929df
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7071,7 +7071,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.1+15.sha-aadbc8a');
+const VERSION = new Version('7.1.0-beta.1+17.sha-a2929df');
 
 /**
  * @fileoverview added by tsickle
