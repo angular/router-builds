1376c9958a3647a17250e59c354fc67fac185e63
Kara Erickson <karakara@google.com>
2018-11-01 22:14:23 +0000

    ea0a996 fix(ivy): init hooks should be called before host bindings (#26802)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+36.sha-18b6d58
+ * @license Angular v7.1.0-beta.1+39.sha-ea0a996
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5717,7 +5717,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0-beta.1+36.sha-18b6d58');
+    var VERSION = new core.Version('7.1.0-beta.1+39.sha-ea0a996');
 
     /**
      * @license
