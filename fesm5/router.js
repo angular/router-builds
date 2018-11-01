31c7309a219306f1ed322bdfcdf82a5452b5a0e3
Kara Erickson <karakara@google.com>
2018-11-01 22:59:28 +0000

    e9e804f feat(forms): add updateOn option to FormBuilder (#24599)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+50.sha-aed95fd
+ * @license Angular v7.1.0-beta.1+51.sha-e9e804f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5638,7 +5638,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+50.sha-aed95fd');
+var VERSION = new Version('7.1.0-beta.1+51.sha-e9e804f');
 
 /**
  * @license
