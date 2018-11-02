645b7c6714546aa647c1ac52529c5e960fea0d4e
Kara Erickson <karakara@google.com>
2018-11-02 18:09:34 +0000

    affcbbd feat(docs-infra): add getting started widgets (#26059)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+61.sha-c016066
+ * @license Angular v7.1.0-beta.1+53.sha-affcbbd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5717,7 +5717,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0-beta.1+61.sha-c016066');
+    var VERSION = new core.Version('7.1.0-beta.1+53.sha-affcbbd');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+61.sha-c016066
+ * @license Angular v7.1.0-beta.1+53.sha-affcbbd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7071,7 +7071,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.1+61.sha-c016066');
+const VERSION = new Version('7.1.0-beta.1+53.sha-affcbbd');
 
 /**
  * @fileoverview added by tsickle
