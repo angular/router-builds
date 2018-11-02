3baa969df2ade6b2a5a4f709733612dc66f9ca69
Kara Erickson <karakara@google.com>
2018-11-02 18:01:53 +0000

    fe1c0d7 feat(docs-infra): add getting started widgets (#26059)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2+9.sha-dc05385
+ * @license Angular v7.0.2+10.sha-fe1c0d7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2+9.sha-dc05385
+ * @license Angular v7.0.2+10.sha-fe1c0d7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.0.2+9.sha-dc05385');
+    var VERSION = new core.Version('7.0.2+10.sha-fe1c0d7');
 
     /**
      * @license
