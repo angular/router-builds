650475ba35f492a0605e2c1d8d705bb2b017875c
Kara Erickson <karakara@google.com>
2018-11-02 18:22:34 +0000

    952ca59 build(docs-infra): upgrade cli command docs sources to b50950b97 (#26915)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+53.sha-affcbbd
+ * @license Angular v7.1.0-beta.1+54.sha-952ca59
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7071,7 +7071,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.1+53.sha-affcbbd');
+const VERSION = new Version('7.1.0-beta.1+54.sha-952ca59');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+53.sha-affcbbd
+ * @license Angular v7.1.0-beta.1+54.sha-952ca59
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
