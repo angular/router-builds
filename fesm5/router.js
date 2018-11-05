788ba28b17b12f45ac7625e881c3e7935997d854
Kara Erickson <karakara@google.com>
2018-11-05 21:18:11 +0000

    6744b19 refactor(compiler): typo (#25496)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+72.sha-f9a4abb
+ * @license Angular v7.1.0-beta.1+73.sha-6744b19
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5629,7 +5629,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+72.sha-f9a4abb');
+var VERSION = new Version('7.1.0-beta.1+73.sha-6744b19');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+72.sha-f9a4abb
+ * @license Angular v7.1.0-beta.1+73.sha-6744b19
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
