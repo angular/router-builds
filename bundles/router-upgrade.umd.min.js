9fc41e17fa2ddbe553f968958dac679acfd37064
Kara Erickson <karakara@google.com>
2018-11-05 18:20:32 +0000

    d0037b2 fix(ivy): correct ngtsc path handling in Windows (#26703)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+69.sha-d568d7a
+ * @license Angular v7.1.0-beta.1+70.sha-d0037b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+69.sha-d568d7a
+ * @license Angular v7.1.0-beta.1+70.sha-d0037b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7071,7 +7071,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.1+69.sha-d568d7a');
+const VERSION = new Version('7.1.0-beta.1+70.sha-d0037b2');
 
 /**
  * @fileoverview added by tsickle
