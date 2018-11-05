d2985a8e6dae9a6e572cb7a797ccd5bdf5cbf219
Kara Erickson <karakara@google.com>
2018-11-05 19:43:48 +0000

    4e4bca6 Revert "fix(ivy): correct ngtsc path handling in Windows (#26703)"

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+70.sha-d0037b2
+ * @license Angular v7.1.0-beta.1+71.sha-4e4bca6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+70.sha-d0037b2
+ * @license Angular v7.1.0-beta.1+71.sha-4e4bca6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7289,7 +7289,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.1+70.sha-d0037b2');
+const VERSION = new Version('7.1.0-beta.1+71.sha-4e4bca6');
 
 /**
  * @fileoverview added by tsickle
