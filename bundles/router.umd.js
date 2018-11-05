ce839eeb914c1d0f128e526368d55bc19693645d
Kara Erickson <karakara@google.com>
2018-11-05 18:19:49 +0000

    d568d7a ci: make `integration_test` job logs less verbose (#26929)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+68.sha-86d41a1
+ * @license Angular v7.1.0-beta.1+69.sha-d568d7a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5711,7 +5711,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-beta.1+68.sha-86d41a1');
+    var VERSION = new i0.Version('7.1.0-beta.1+69.sha-d568d7a');
 
     /**
      * @license
