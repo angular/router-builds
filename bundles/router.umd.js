d890b325b59169d471b1783021e27aef0312b049
Kara Erickson <karakara@google.com>
2018-11-05 20:02:24 +0000

    f9a4abb fix: missing semi-colon and empty line (#26945)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+71.sha-4e4bca6
+ * @license Angular v7.1.0-beta.1+72.sha-f9a4abb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5711,7 +5711,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-beta.1+71.sha-4e4bca6');
+    var VERSION = new i0.Version('7.1.0-beta.1+72.sha-f9a4abb');
 
     /**
      * @license
