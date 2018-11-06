289484a5e3a4dd3157d7ca722ba19d6d1aa3cf01
Kara Erickson <karakara@google.com>
2018-11-06 19:47:09 +0000

    dc2464e fix(ivy): jit should handle undefined type in constructor deps (#26956)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+77.sha-7dbc103
+ * @license Angular v7.1.0-beta.1+78.sha-dc2464e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5638,7 +5638,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+77.sha-7dbc103');
+var VERSION = new Version('7.1.0-beta.1+78.sha-dc2464e');
 
 /**
  * @license
