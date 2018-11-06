1d50af52c9e552eb39532b005a94070c435c10c5
Kara Erickson <karakara@google.com>
2018-11-06 19:45:55 +0000

    dc2464e fix(ivy): jit should handle undefined type in constructor deps (#26956)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+77.sha-7dbc103
+ * @license Angular v7.1.0-beta.1+78.sha-dc2464e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+77.sha-7dbc103
+ * @license Angular v7.1.0-beta.1+78.sha-dc2464e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7071,7 +7071,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.1+77.sha-7dbc103');
+const VERSION = new Version('7.1.0-beta.1+78.sha-dc2464e');
 
 /**
  * @fileoverview added by tsickle
