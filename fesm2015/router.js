5db69469d74c4f56c3e896342d375199c87c380a
Kara Erickson <karakara@google.com>
2018-11-07 19:32:53 +0000

    9e26216 fix(ivy): compiler should generate bindings to host attrs properly (#26973)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+84.sha-a4398aa
+ * @license Angular v7.1.0-beta.1+85.sha-9e26216
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7071,7 +7071,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.1+84.sha-a4398aa');
+const VERSION = new Version('7.1.0-beta.1+85.sha-9e26216');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+84.sha-a4398aa
+ * @license Angular v7.1.0-beta.1+85.sha-9e26216
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5629,7 +5629,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+84.sha-a4398aa');
+var VERSION = new Version('7.1.0-beta.1+85.sha-9e26216');
 
 /**
  * @license
