7c59601a094312fdbca0b4867ef471b765f22374
Kara Erickson <karakara@google.com>
2018-11-07 20:30:28 +0000

    9263671 release: cut the v7.0.3 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2+25.sha-14ff340
+ * @license Angular v7.0.3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.0.2+25.sha-14ff340');
+    var VERSION = new core.Version('7.0.3');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2+25.sha-14ff340
+ * @license Angular v7.0.3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6925,7 +6925,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.0.2+25.sha-14ff340');
+const VERSION = new Version('7.0.3');
 
 /**
  * @fileoverview added by tsickle
