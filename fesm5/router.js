58445dfd096d1b9cc729e260f7ee92df3825619a
Kara Erickson <karakara@google.com>
2018-11-07 21:02:00 +0000

    ff2ee64 release: cut the v7.1.0-beta.2 release

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+86.sha-b57ce92
+ * @license Angular v7.1.0-beta.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5638,7 +5638,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+86.sha-b57ce92');
+var VERSION = new Version('7.1.0-beta.2');
 
 /**
  * @license
