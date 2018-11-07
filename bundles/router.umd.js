234dc0b97ea106a10937aa3b14197a5a32a9d5ea
Kara Erickson <karakara@google.com>
2018-11-07 19:02:26 +0000

    a4398aa docs(forms): update description for FormBuilder.group (#26970)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+83.sha-53e4e0c
+ * @license Angular v7.1.0-beta.1+84.sha-a4398aa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5717,7 +5717,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0-beta.1+83.sha-53e4e0c');
+    var VERSION = new core.Version('7.1.0-beta.1+84.sha-a4398aa');
 
     /**
      * @license
