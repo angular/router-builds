c7bdcfd6aafb294770b1110737458d32594a2557
Andrew Kushnir <akushnir@google.com>
2018-11-08 19:29:00 +0000

    791d192 docs: correct https://angularconsole.com/ link (#26999) PR Close #26999

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+2.sha-15e671e
+ * @license Angular v7.1.0-beta.2+3.sha-791d192
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7071,7 +7071,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.2+2.sha-15e671e');
+const VERSION = new Version('7.1.0-beta.2+3.sha-791d192');
 
 /**
  * @fileoverview added by tsickle
