e9a8e9c4a598d965bc12d40f62551e48c6307757
Andrew Kushnir <akushnir@google.com>
2018-11-08 19:53:38 +0000

    0d972d9 docs: update hero search component to use input event (#26440)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+4.sha-19546c2
+ * @license Angular v7.1.0-beta.2+5.sha-0d972d9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5717,7 +5717,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0-beta.2+4.sha-19546c2');
+    var VERSION = new core.Version('7.1.0-beta.2+5.sha-0d972d9');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+4.sha-19546c2
+ * @license Angular v7.1.0-beta.2+5.sha-0d972d9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7071,7 +7071,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.2+4.sha-19546c2');
+const VERSION = new Version('7.1.0-beta.2+5.sha-0d972d9');
 
 /**
  * @fileoverview added by tsickle
