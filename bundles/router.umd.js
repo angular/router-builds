9516cd3aba9bc79c7ff4b70213b079b00f521927
Andrew Kushnir <akushnir@google.com>
2018-11-08 01:16:09 +0000

    15e671e build(bazel): upgrade benchmarks to protractor_web_test_suite for CI without local chrome (#26908)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+1.sha-3567e81
+ * @license Angular v7.1.0-beta.2+2.sha-15e671e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5711,7 +5711,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-beta.2+1.sha-3567e81');
+    var VERSION = new i0.Version('7.1.0-beta.2+2.sha-15e671e');
 
     /**
      * @license
