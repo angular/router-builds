a3f3313f643374b1d4d19271a633e6c1b8a0e958
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:48:30 +0000

    9729e8f docs: removed unused pull-left classes from hero form component (#26178)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+19.sha-78b6f88
+ * @license Angular v7.1.0-beta.2+20.sha-9729e8f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5713,7 +5713,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-beta.2+19.sha-78b6f88');
+    var VERSION = new i0.Version('7.1.0-beta.2+20.sha-9729e8f');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+19.sha-78b6f88
+ * @license Angular v7.1.0-beta.2+20.sha-9729e8f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
