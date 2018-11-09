de67f7a8f16c438a0dd3e6441a8a40a0cb1cc967
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:18:44 +0000

    edb50a2 docs: override code formatting for CLI commands (#26619)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+14.sha-ac1988d
+ * @license Angular v7.1.0-beta.2+13.sha-edb50a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5711,7 +5711,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-beta.2+14.sha-ac1988d');
+    var VERSION = new i0.Version('7.1.0-beta.2+13.sha-edb50a2');
 
     /**
      * @license
