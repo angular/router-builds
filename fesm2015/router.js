fea8138f948b018d7a101becc4b7c4af266b0ab8
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:22:25 +0000

    78b6f88 fix(ivy): TestBed should use an NgZone (#27015)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+16.sha-83c9bff
+ * @license Angular v7.1.0-beta.2+19.sha-78b6f88
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7073,7 +7073,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.2+16.sha-83c9bff');
+const VERSION = new Version('7.1.0-beta.2+19.sha-78b6f88');
 
 /**
  * @fileoverview added by tsickle
