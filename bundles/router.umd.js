e30d0bd31c36da0a090315bfa38ded0372b152b0
Andrew Kushnir <akushnir@google.com>
2018-11-12 21:32:43 +0000

    b278ea1 docs: fix typo in toh-pt0.md (#27044) PR Close #27044

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+24.sha-1810cdf
+ * @license Angular v7.1.0-beta.2+25.sha-b278ea1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5713,7 +5713,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-beta.2+24.sha-1810cdf');
+    var VERSION = new i0.Version('7.1.0-beta.2+25.sha-b278ea1');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+24.sha-1810cdf
+ * @license Angular v7.1.0-beta.2+25.sha-b278ea1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
