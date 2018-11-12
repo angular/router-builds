56251597e65c2f463a66e914c572dbcbdbfcf034
Andrew Kushnir <akushnir@google.com>
2018-11-12 22:50:21 +0000

    8b9249a fix(ivy): host bindings should support content children and content hooks (#27065)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+25.sha-b278ea1
+ * @license Angular v7.1.0-beta.2+27.sha-8b9249a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5631,7 +5631,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+25.sha-b278ea1');
+var VERSION = new Version('7.1.0-beta.2+27.sha-8b9249a');
 
 /**
  * @license
