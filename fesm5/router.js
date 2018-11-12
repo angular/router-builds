eda663cd88c2d0e749b2d370e0a4d913004d3a62
Andrew Kushnir <akushnir@google.com>
2018-11-12 21:23:27 +0000

    f80c600 docs: Fix wrong quoting in SSR guide (#27057) PR Close #27057

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+22.sha-99c5db1
+ * @license Angular v7.1.0-beta.2+26.sha-f80c600
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5631,7 +5631,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+22.sha-99c5db1');
+var VERSION = new Version('7.1.0-beta.2+26.sha-f80c600');
 
 /**
  * @license
