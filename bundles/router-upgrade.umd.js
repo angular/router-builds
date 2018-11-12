28750b1e2c5f378c86efdea08436abb48c6a5581
Andrew Kushnir <akushnir@google.com>
2018-11-12 22:51:34 +0000

    8b9249a fix(ivy): host bindings should support content children and content hooks (#27065)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+25.sha-b278ea1
+ * @license Angular v7.1.0-beta.2+27.sha-8b9249a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+25.sha-b278ea1
+ * @license Angular v7.1.0-beta.2+27.sha-8b9249a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7291,7 +7291,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.2+25.sha-b278ea1');
+const VERSION = new Version('7.1.0-beta.2+27.sha-8b9249a');
 
 /**
  * @fileoverview added by tsickle
