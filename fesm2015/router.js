58e8ee75776cba3bd36b6b428486b9b2ee6a38c0
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:24:27 +0000

    1b97971 docs: fixed typo on Github issue template (#27070) PR Close #27070

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+31.sha-efa443b
+ * @license Angular v7.1.0-beta.2+35.sha-1b97971
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7291,7 +7291,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.2+31.sha-efa443b');
+const VERSION = new Version('7.1.0-beta.2+35.sha-1b97971');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+31.sha-efa443b
+ * @license Angular v7.1.0-beta.2+35.sha-1b97971
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
