512e98e8d556f99d6cdc0c2646b388d0c4af8a3b
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:24:16 +0000

    efa443b docs: fix typo (#26878) PR Close #26878

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+33.sha-6b1780d
+ * @license Angular v7.1.0-beta.2+31.sha-efa443b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5640,7 +5640,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+33.sha-6b1780d');
+var VERSION = new Version('7.1.0-beta.2+31.sha-efa443b');
 
 /**
  * @license
