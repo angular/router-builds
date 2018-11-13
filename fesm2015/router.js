dfe284e72d30817c5c4078dde947a4fb220b15c7
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:15:48 +0000

    f5a0ec0 fix(ivy): ngcc should not fail on invalid package.json (#26539)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+28.sha-095b6e8
+ * @license Angular v7.1.0-beta.2+30.sha-f5a0ec0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7291,7 +7291,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.2+28.sha-095b6e8');
+const VERSION = new Version('7.1.0-beta.2+30.sha-f5a0ec0');
 
 /**
  * @fileoverview added by tsickle
