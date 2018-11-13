d6b6b9cabab3f9bb1b0e628e253d73f5ef6253c3
Andrew Kushnir <akushnir@google.com>
2018-11-13 20:44:42 +0000

    bc652a2 fix(ivy): jit compilation should support content queries with type predicates (#27068)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+39.sha-e6e5904
+ * @license Angular v7.1.0-beta.2+40.sha-bc652a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5640,7 +5640,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+39.sha-e6e5904');
+var VERSION = new Version('7.1.0-beta.2+40.sha-bc652a2');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+39.sha-e6e5904
+ * @license Angular v7.1.0-beta.2+40.sha-bc652a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
