831cc7f65c9b4640fa3abdc6e8cc3faad612ac4f
Andrew Kushnir <akushnir@google.com>
2018-11-13 22:22:58 +0000

    c31e78f fix(compiler-cli): add missing tslib dependency (#27063)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+40.sha-bc652a2
+ * @license Angular v7.1.0-beta.2+41.sha-c31e78f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7073,7 +7073,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.2+40.sha-bc652a2');
+const VERSION = new Version('7.1.0-beta.2+41.sha-c31e78f');
 
 /**
  * @fileoverview added by tsickle
