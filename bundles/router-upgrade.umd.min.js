87f4655af8b1347f8ff5c25f7e1001d681455a01
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:25:21 +0000

    9741f5b docs: new doc for core directives (#26923)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+34.sha-0ada23a
+ * @license Angular v7.1.0-beta.2+36.sha-9741f5b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+34.sha-0ada23a
+ * @license Angular v7.1.0-beta.2+36.sha-9741f5b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5631,7 +5631,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+34.sha-0ada23a');
+var VERSION = new Version('7.1.0-beta.2+36.sha-9741f5b');
 
 /**
  * @license
