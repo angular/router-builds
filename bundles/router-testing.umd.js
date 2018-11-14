f35d4b6d231b02684b5e914a3c90c892ff48be09
Andrew Kushnir <akushnir@google.com>
2018-11-14 20:58:53 +0000

    bf6ac6c feat(router): add pathParamsChange mode for runGuardsAndResolvers (#26861)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+51.sha-ce52424
+ * @license Angular v7.1.0-beta.2+48.sha-bf6ac6c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+51.sha-ce52424
+ * @license Angular v7.1.0-beta.2+48.sha-bf6ac6c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-beta.2+51.sha-ce52424');
+    var VERSION = new i0.Version('7.1.0-beta.2+48.sha-bf6ac6c');
 
     /**
      * @license
