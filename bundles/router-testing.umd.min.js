60e89b59beb3aab137251a5c4452ef4608f497c0
Andrew Kushnir <akushnir@google.com>
2018-11-14 22:59:21 +0000

    4756324 docs: release notes for the v7.0.4 release

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+48.sha-bf6ac6c
+ * @license Angular v7.1.0-beta.2+52.sha-4756324
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+48.sha-bf6ac6c
+ * @license Angular v7.1.0-beta.2+52.sha-4756324
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5642,7 +5642,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+48.sha-bf6ac6c');
+var VERSION = new Version('7.1.0-beta.2+52.sha-4756324');
 
 /**
  * @license
