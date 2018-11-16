e00055e2617be0bf032bfca93b4d5322868af23b
Misko Hevery <misko@hevery.com>
2018-11-16 20:52:33 +0000

    a4934a7 docs: fix code examples style (#26573)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+10.sha-91bffa9
+ * @license Angular v7.1.0-rc.0+11.sha-a4934a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7294,7 +7294,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-rc.0+10.sha-91bffa9');
+const VERSION = new Version('7.1.0-rc.0+11.sha-a4934a7');
 
 /**
  * @fileoverview added by tsickle
