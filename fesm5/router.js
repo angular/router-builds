34ed600b38599a69b8793e7c179f3a04fc03ba49
Misko Hevery <misko@hevery.com>
2018-11-16 05:45:28 +0000

    65943b4 docs: add link to workspace config page (#26927)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+2.sha-3ec7c50
+ * @license Angular v7.1.0-rc.0+5.sha-65943b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5633,7 +5633,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-rc.0+2.sha-3ec7c50');
+var VERSION = new Version('7.1.0-rc.0+5.sha-65943b4');
 
 /**
  * @license
