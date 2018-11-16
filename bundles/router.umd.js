6917c35464049a72119c5e176af6c4df9d729da4
Misko Hevery <misko@hevery.com>
2018-11-16 05:44:28 +0000

    6574e61 test(ivy): skip useJit tests with Ivy (#27067)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+3.sha-ee12e72
+ * @license Angular v7.1.0-rc.0+4.sha-6574e61
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5721,7 +5721,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0-rc.0+3.sha-ee12e72');
+    var VERSION = new core.Version('7.1.0-rc.0+4.sha-6574e61');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+3.sha-ee12e72
+ * @license Angular v7.1.0-rc.0+4.sha-6574e61
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
