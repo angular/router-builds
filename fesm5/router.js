a6137e710e5ad22eee9365858ce25a6864ee11af
Misko Hevery <misko@hevery.com>
2018-11-16 20:46:41 +0000

    60800da fix(ivy): export elementContainerStart/End instructions (#27053)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+8.sha-b07bd30
+ * @license Angular v7.1.0-rc.0+9.sha-60800da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5633,7 +5633,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-rc.0+8.sha-b07bd30');
+var VERSION = new Version('7.1.0-rc.0+9.sha-60800da');
 
 /**
  * @license
