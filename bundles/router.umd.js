8ff9829675d9baa7a6999650c42166d471f70d19
Misko Hevery <misko@hevery.com>
2018-11-17 20:42:11 +0000

    81e975a build: update to latest Bazel rules_typescript (#27138)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+15.sha-20ea5b5
+ * @license Angular v7.1.0-rc.0+16.sha-81e975a.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5721,7 +5721,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0-rc.0+15.sha-20ea5b5');
+    var VERSION = new core.Version('7.1.0-rc.0+16.sha-81e975a.with-local-changes');
 
     /**
      * @license
