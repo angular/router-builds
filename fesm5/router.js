88835a5f30baa620803c4727cede99e6a9098a24
Misko Hevery <misko@hevery.com>
2018-11-17 02:17:53 +0000

    4222b63 Revert "refactor(ivy): ensure directive host bindings use the styling algorithm (#27134)"

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+12.sha-92e80af
+ * @license Angular v7.1.0-rc.0+14.sha-4222b63
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5633,7 +5633,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-rc.0+12.sha-92e80af');
+var VERSION = new Version('7.1.0-rc.0+14.sha-4222b63');
 
 /**
  * @license
