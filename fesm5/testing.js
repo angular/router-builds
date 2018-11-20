dbca00910d6552b99fa14464ab0e4d07acf3e8a1
Miško Hevery <misko@hevery.com>
2018-11-20 06:04:38 +0000

    20729b3 fix(ivy): deduplicate imported modules in r3_injector (#27102) PR Close #27102

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+16.sha-81e975a.with-local-changes
+ * @license Angular v7.1.0-rc.0+19.sha-20729b3.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
