9835ff710d5301a91cf6ca4bbd7433bd284ab946
Misko Hevery <misko@hevery.com>
2018-11-20 19:16:22 +0000

    2a7210e build: release scripts should use bazel from yarn (#26899)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+20.sha-a43998c.with-local-changes
+ * @license Angular v7.1.0-rc.0+25.sha-2a7210e.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
