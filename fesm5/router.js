c4562dab73ee17d235e6a18937af3d1723b27091
Misko Hevery <misko@hevery.com>
2018-11-20 20:06:50 +0000

    ca40565 fix(ivy): hack implementation of host styles (#27180)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+26.sha-34306c3.with-local-changes
+ * @license Angular v7.1.0-rc.0+28.sha-ca40565.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5633,7 +5633,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-rc.0+26.sha-34306c3.with-local-changes');
+var VERSION = new Version('7.1.0-rc.0+28.sha-ca40565.with-local-changes');
 
 /**
  * @license
