6c77e7ba29aa6a401d4a8e02244d00dcad21aca5
Misko Hevery <misko@hevery.com>
2018-11-20 19:18:29 +0000

    391767f build: fix size artifacts not measured by github robot (#27042)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+21.sha-859da3a.with-local-changes
+ * @license Angular v7.1.0-rc.0+24.sha-391767f.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
