c41ba56b8bc61c3c9e4d4147022817f3a0879d50
Misko Hevery <misko@hevery.com>
2018-11-20 20:08:01 +0000

    ca40565 fix(ivy): hack implementation of host styles (#27180)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+26.sha-34306c3.with-local-changes
+ * @license Angular v7.1.0-rc.0+28.sha-ca40565.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-rc.0+26.sha-34306c3.with-local-changes');
+    var VERSION = new i0.Version('7.1.0-rc.0+28.sha-ca40565.with-local-changes');
 
     /**
      * @license
