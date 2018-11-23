e31a56f93216ead39d3a9041c271f5bd12e204ff
Filipe Silva <filipematossilva@gmail.com>
2018-11-23 11:02:45 +0000

    e201a67 ci: test ts-api-guardian on windows

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+17.sha-d3ddd31.with-local-changes
+ * @license Angular v7.1.0-rc.0+17.sha-e201a67.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
