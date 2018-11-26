47bb65e4b74988d4f4708416a8393b9a01c0cb0a
Jason Aden <jasonaden@google.com>
2018-11-26 20:44:27 +0000

    f45aedc build(docs-infra): upgrade `npm-run-all` to latest version for security (#27274)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+1.sha-dc300c5
+ * @license Angular v7.1.0+11.sha-f45aedc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+1.sha-dc300c5
+ * @license Angular v7.1.0+11.sha-f45aedc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5721,7 +5721,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0+1.sha-dc300c5');
+    var VERSION = new core.Version('7.1.0+11.sha-f45aedc');
 
     /**
      * @license
