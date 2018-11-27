04a4dcd30f09a548f36a6714d3da2d60c5504882
Jason Aden <jasonaden@google.com>
2018-11-27 18:45:52 +0000

    d767e0b fix(ivy): consider providers for view/content queries (#27156)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+11.sha-f45aedc
+ * @license Angular v7.1.0+13.sha-d767e0b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5721,7 +5721,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0+11.sha-f45aedc');
+    var VERSION = new core.Version('7.1.0+13.sha-d767e0b');
 
     /**
      * @license
