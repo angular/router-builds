18ed3aaafe68a4f2a20f0df3c551cbeaa4ac6184
Jason Aden <jasonaden@google.com>
2018-11-28 18:59:09 +0000

    8282e15 release: cut the v7.1.1 release

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+5.sha-bdf5f3e
+ * @license Angular v7.1.1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+5.sha-bdf5f3e
+ * @license Angular v7.1.1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5633,7 +5633,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+5.sha-bdf5f3e');
+var VERSION = new Version('7.1.1');
 
 /**
  * @license
