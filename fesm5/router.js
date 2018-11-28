25d777a0ec155f4101a612574258fdd8d744b121
Jason Aden <jasonaden@google.com>
2018-11-28 18:13:42 +0000

    39e426c fix(ivy): TestBed.get(Compiler) throws "Error: Runtime compiler is not loaded" (#27223)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+30.sha-4390e10.with-local-changes
+ * @license Angular v7.1.0+34.sha-39e426c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5633,7 +5633,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-rc.0+30.sha-4390e10.with-local-changes');
+var VERSION = new Version('7.1.0+34.sha-39e426c');
 
 /**
  * @license
