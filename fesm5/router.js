6ba92e4e948a0623b9634cbd9288ac790357b254
Igor Minar <igor@angular.io>
2018-11-30 06:22:12 +0000

    2389a68 test(platform-server): import ivyEnabled from @angular/private/testing (#27302)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+68.sha-c331fc6
+ * @license Angular v7.1.0+67.sha-2389a68
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5649,7 +5649,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+68.sha-c331fc6');
+var VERSION = new Version('7.1.0+67.sha-2389a68');
 
 /**
  * @license
