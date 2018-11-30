813c54c15b45d737a43678ece6c392f3934a7fe0
Igor Minar <igor@angular.io>
2018-11-30 06:18:28 +0000

    eb99753 test(ivy): enable @angular/service-worker targets to run on CI (#27310)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+60.sha-1402175
+ * @license Angular v7.1.0+63.sha-eb99753
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+60.sha-1402175
+ * @license Angular v7.1.0+63.sha-eb99753
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5721,7 +5721,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0+60.sha-1402175');
+    var VERSION = new core.Version('7.1.0+63.sha-eb99753');
 
     /**
      * @license
