4a7edc61a6c4ab64399ddc81a836be11ff6616ae
Igor Minar <igor@angular.io>
2018-11-30 22:47:12 +0000

    64a3461 fix(ivy): support string tokens in dependency injection (#27383)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+84.sha-c96dea2
+ * @license Angular v7.1.0+85.sha-64a3461
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7135,7 +7135,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+84.sha-c96dea2');
+const VERSION = new Version('7.1.0+85.sha-64a3461');
 
 /**
  * @fileoverview added by tsickle
