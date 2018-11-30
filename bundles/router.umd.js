7971c77be6b9376a80d3cda42cc2c98d10b10b15
Igor Minar <igor@angular.io>
2018-11-30 22:07:46 +0000

    eb17502 fix(bazel): ng_package should correctly map to source maps in secondary entry-points (#27313)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+81.sha-c8a8dcf
+ * @license Angular v7.1.0+82.sha-eb17502
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5781,7 +5781,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0+81.sha-c8a8dcf');
+    var VERSION = new i0.Version('7.1.0+82.sha-eb17502');
 
     /**
      * @license
