881026f1467671dc21d5c3673de1d576418a8e79
Igor Minar <igor@angular.io>
2018-11-30 06:17:15 +0000

    65775a3 refactor(ivy): add fixmeIvy blocks to ivy failing tests (#27311)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+57.sha-23bc8ed
+ * @license Angular v7.1.0+59.sha-65775a3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+57.sha-23bc8ed
+ * @license Angular v7.1.0+59.sha-65775a3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5649,7 +5649,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+57.sha-23bc8ed');
+var VERSION = new Version('7.1.0+59.sha-65775a3');
 
 /**
  * @license
