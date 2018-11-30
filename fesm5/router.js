bf53671b5abab1a561fb6572c12e8417aa6d70ea
Igor Minar <igor@angular.io>
2018-11-30 18:22:42 +0000

    aedc343 feat(ivy): updated translation const names (that include message ids) (#27185)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+70.sha-9129f9a
+ * @license Angular v7.1.0+71.sha-aedc343
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5633,7 +5633,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+70.sha-9129f9a');
+var VERSION = new Version('7.1.0+71.sha-aedc343');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+70.sha-9129f9a
+ * @license Angular v7.1.0+71.sha-aedc343
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
