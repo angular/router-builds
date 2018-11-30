b5b66131b21d81b455891dbd302e1e49b2cb8c8a
Igor Minar <igor@angular.io>
2018-11-30 06:20:13 +0000

    4effb89 test(ivy): update //packages/compiler tests for Ivy (#27301)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+63.sha-eb99753
+ * @license Angular v7.1.0+62.sha-4effb89
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5722,7 +5722,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0+63.sha-eb99753');
+    var VERSION = new i0.Version('7.1.0+62.sha-4effb89');
 
     /**
      * @license
