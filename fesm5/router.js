3db59d87d2637a0d5c25296ab094853275a29a63
Igor Minar <igor@angular.io>
2018-12-01 00:31:42 +0000

    f5f323d build: only stamp version info when releasing (#27362)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+101.sha-35e02ad
+ * @license Angular v7.1.0+102.sha-f5f323d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5696,7 +5696,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+101.sha-35e02ad');
+var VERSION = new Version('7.1.0+102.sha-f5f323d');
 
 /**
  * @license
