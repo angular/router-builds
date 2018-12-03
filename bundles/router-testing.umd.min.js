8dc2d45ea836caf8bfc06c5d8665f4c1e47f4f2a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 23:08:05 +0000

    7ec05b4 fix(ivy): run initialializers in TestBed on init (#27355)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+110.sha-2a39425
+ * @license Angular v7.1.0+112.sha-7ec05b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+110.sha-2a39425
+ * @license Angular v7.1.0+112.sha-7ec05b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5782,7 +5782,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0+110.sha-2a39425');
+    var VERSION = new i0.Version('7.1.0+112.sha-7ec05b4');
 
     /**
      * @license
