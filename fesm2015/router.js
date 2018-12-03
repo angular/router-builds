872bf910194db7af8ef5b3f321587972d9cff366
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 19:29:58 +0000

    2a39425 fix(ivy): temporary hack to set host styles and static classes (#27385)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+109.sha-7d89cff
+ * @license Angular v7.1.0+110.sha-2a39425
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7135,7 +7135,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+109.sha-7d89cff');
+const VERSION = new Version('7.1.0+110.sha-2a39425');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+109.sha-7d89cff
+ * @license Angular v7.1.0+110.sha-2a39425
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
