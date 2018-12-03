9d7b9ccdd1bf1dcd1463f05cb8e3bba656efc8a1
Igor Minar <igor@angular.io>
2018-12-03 16:54:20 +0000

    a082f64 fix(ivy): fix style prop instructions to account for zero-based values (#27270)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+105.sha-d847051
+ * @license Angular v7.1.0+106.sha-a082f64
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5782,7 +5782,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0+105.sha-d847051');
+    var VERSION = new i0.Version('7.1.0+106.sha-a082f64');
 
     /**
      * @license
