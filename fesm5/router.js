37ab472655d5e91dd0a2f1e8ed64fcd2a80cb374
Igor Minar <igor@angular.io>
2018-12-04 22:38:48 +0000

    d3c08e7 fix(compiler-cli): flatModuleIndex files not generated on windows with multiple input files (#27200)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+117.sha-7ad6b03
+ * @license Angular v7.1.0+121.sha-d3c08e7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5696,7 +5696,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+117.sha-7ad6b03');
+var VERSION = new Version('7.1.0+121.sha-d3c08e7');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,3 +1,8 @@
+/**
+ * @license Angular v7.1.0+121.sha-d3c08e7
+ * (c) 2010-2018 Google, Inc. https://angular.io/
+ * License: MIT
+ */
 
   export * from './testing/testing';
   
