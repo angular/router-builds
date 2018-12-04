e1a94f9133dfdfb4b9dec1a0263e9cfbbf7b5a0d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-04 00:04:17 +0000

    6552471 fix(ivy): account for `useValue: undefined` providers in module injector (#27035)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+109.sha-7d89cff
+ * @license Angular v7.1.0+115.sha-6552471
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5784,7 +5784,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0+109.sha-7d89cff');
+    var VERSION = new core.Version('7.1.0+115.sha-6552471');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+109.sha-7d89cff
+ * @license Angular v7.1.0+115.sha-6552471
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
