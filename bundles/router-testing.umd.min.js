d53ecee1e0769f85332c2c9d313ada30478d3a49
Igor Minar <igor@angular.io>
2018-12-04 22:40:45 +0000

    1597886 fix(ivy): resolve resources using TS module resolution semantics (#27357)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+128.sha-8e644d9
+ * @license Angular v7.1.0+127.sha-1597886
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+128.sha-8e644d9
+ * @license Angular v7.1.0+127.sha-1597886
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5782,7 +5782,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0+128.sha-8e644d9');
+    var VERSION = new i0.Version('7.1.0+127.sha-1597886');
 
     /**
      * @license
