d9788167ba8ecf431e33ccbbf1411deb1b0ad893
Igor Minar <igor@angular.io>
2018-12-05 21:10:21 +0000

    a655d28 ci: test ts-api-guardian on windows (#27205)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+28.sha-2bc85fb
+ * @license Angular v7.1.1+31.sha-a655d28
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+28.sha-2bc85fb
+ * @license Angular v7.1.1+31.sha-a655d28
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5646,7 +5646,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.1+28.sha-2bc85fb');
+var VERSION = new Version('7.1.1+31.sha-a655d28');
 
 /**
  * @license
