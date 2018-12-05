d6a2f930c4ceaf82d345ac21dc96c54448f9147e
Igor Minar <igor@angular.io>
2018-12-05 04:45:12 +0000

    c61a8b7 fix(bazel): do not throw if ts compile action does not create esm5 outputs (#27401)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+129.sha-f0b0d64
+ * @license Angular v7.1.0+131.sha-c61a8b7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5782,7 +5782,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0+129.sha-f0b0d64');
+    var VERSION = new i0.Version('7.1.0+131.sha-c61a8b7');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+129.sha-f0b0d64
+ * @license Angular v7.1.0+131.sha-c61a8b7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
