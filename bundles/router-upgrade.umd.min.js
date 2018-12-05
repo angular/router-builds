c403a3d16bcb5a6a854bfa88dd533519ce77cebe
Igor Minar <igor@angular.io>
2018-12-05 19:50:45 +0000

    821fecb test(ivy): update root causes for @angular/core TestBed failures (#27479)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+162.sha-2643350
+ * @license Angular v7.1.0+163.sha-821fecb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+162.sha-2643350
+ * @license Angular v7.1.0+163.sha-821fecb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5725,7 +5725,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+162.sha-2643350');
+var VERSION = new Version('7.1.0+163.sha-821fecb');
 
 /**
  * @license
