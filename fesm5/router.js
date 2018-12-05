2ae9471c4974181b65665751494f50357dc8873e
Igor Minar <igor@angular.io>
2018-12-05 07:44:09 +0000

    4b9948c fix(core): More precise return type for `InjectableDecorator` (#27360)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+149.sha-faca8b1
+ * @license Angular v7.1.0+150.sha-4b9948c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5709,7 +5709,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+149.sha-faca8b1');
+var VERSION = new Version('7.1.0+150.sha-4b9948c');
 
 /**
  * @license
