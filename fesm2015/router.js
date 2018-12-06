7dfb956663df39ebce8834cd40574af0a344df45
Igor Minar <igor@angular.io>
2018-12-06 05:15:59 +0000

    d7c72fc build: remove obsolete gulp build task (#27386)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+44.sha-0508143
+ * @license Angular v7.1.1+53.sha-d7c72fc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7091,7 +7091,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.1+44.sha-0508143');
+const VERSION = new Version('7.1.1+53.sha-d7c72fc');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+44.sha-0508143
+ * @license Angular v7.1.1+53.sha-d7c72fc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5646,7 +5646,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.1+44.sha-0508143');
+var VERSION = new Version('7.1.1+53.sha-d7c72fc');
 
 /**
  * @license
