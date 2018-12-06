da4bd5cb1ff2d800ced794e23327cedf8e9ac187
Igor Minar <igor@angular.io>
2018-12-06 17:53:12 +0000

    160cace test(ivy): fix query compliance spec (#27501)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+197.sha-ca1c430
+ * @license Angular v7.1.0+198.sha-160cace
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5712,7 +5712,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+197.sha-ca1c430');
+var VERSION = new Version('7.1.0+198.sha-160cace');
 
 /**
  * @license
