8b476f59a2a9e3dc3bb21b7d5eb327023bfc7a24
Igor Minar <igor@angular.io>
2018-12-06 22:10:16 +0000

    c71d7b5 fix(ivy): setting up animation properties correctly (FW-643) (#27496)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+205.sha-4da739a
+ * @license Angular v7.1.0+206.sha-c71d7b5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5699,7 +5699,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+205.sha-4da739a');
+var VERSION = new Version('7.1.0+206.sha-c71d7b5');
 
 /**
  * @license
