d8868fd72289818b1b6a4d0aa04c332a0e5e2b19
Igor Minar <igor@angular.io>
2018-12-06 05:19:44 +0000

    6740d0d build: remove obsolete gulp build task (#27386)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+185.sha-a315ded
+ * @license Angular v7.1.0+194.sha-6740d0d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5800,7 +5800,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0+185.sha-a315ded');
+    var VERSION = new core.Version('7.1.0+194.sha-6740d0d');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+185.sha-a315ded
+ * @license Angular v7.1.0+194.sha-6740d0d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7153,7 +7153,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+185.sha-a315ded');
+const VERSION = new Version('7.1.0+194.sha-6740d0d');
 
 /**
  * @fileoverview added by tsickle
