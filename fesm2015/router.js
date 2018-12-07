08214dbeedfa045bd6e2f992c6f7421e82a7e3d9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 20:19:59 +0000

    84ad0cc build: update source-map-support to get rid of Node's DeprecationWarning (#27538)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+15.sha-255e672
+ * @license Angular v7.2.0-beta.1+14.sha-84ad0cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7138,7 +7138,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.2.0-beta.1+15.sha-255e672');
+const VERSION = new Version('7.2.0-beta.1+14.sha-84ad0cc');
 
 /**
  * @fileoverview added by tsickle
