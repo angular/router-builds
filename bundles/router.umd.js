a537dd2368d3385ba7da3d75753e4bd292e70d53
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 19:21:02 +0000

    f034114 fix(bazel): tsickle dependency not working with typescript 3.1.x (#27402)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+10.sha-055dc2d
+ * @license Angular v7.2.0-beta.1+13.sha-f034114
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5785,7 +5785,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-beta.1+10.sha-055dc2d');
+    var VERSION = new i0.Version('7.2.0-beta.1+13.sha-f034114');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+10.sha-055dc2d
+ * @license Angular v7.2.0-beta.1+13.sha-f034114
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
