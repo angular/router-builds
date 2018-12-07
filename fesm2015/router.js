144706364d0ba0753892169bb9052bf38ab6c91e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 18:02:20 +0000

    15ca1bb style: run latest buildifier binary (#27489)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+7.sha-f6a67c9
+ * @license Angular v7.2.0-beta.1+9.sha-15ca1bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7138,7 +7138,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.2.0-beta.1+7.sha-f6a67c9');
+const VERSION = new Version('7.2.0-beta.1+9.sha-15ca1bb');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+7.sha-f6a67c9
+ * @license Angular v7.2.0-beta.1+9.sha-15ca1bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5699,7 +5699,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.1+7.sha-f6a67c9');
+var VERSION = new Version('7.2.0-beta.1+9.sha-15ca1bb');
 
 /**
  * @license
