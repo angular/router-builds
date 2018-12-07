33db14e4bc9f1632313e5cb20ee825a3ccbf7e08
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 22:49:53 +0000

    8fc763d build: update to Bazel 0.20 (#27394)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.2+6.sha-7a5136b
+ * @license Angular v7.1.2+7.sha-8fc763d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7076,7 +7076,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.2+6.sha-7a5136b');
+const VERSION = new Version('7.1.2+7.sha-8fc763d');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.2+6.sha-7a5136b
+ * @license Angular v7.1.2+7.sha-8fc763d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
