ae3e41aeeb4c87d74f09ecda479a3e2d0bf0ff7f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 18:03:53 +0000

    2a35471 test(upgrade): add diagnosis to failing ivy tests (#27542)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+9.sha-15ca1bb
+ * @license Angular v7.2.0-beta.1+11.sha-2a35471
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7368,7 +7368,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.2.0-beta.1+9.sha-15ca1bb');
+const VERSION = new Version('7.2.0-beta.1+11.sha-2a35471');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+9.sha-15ca1bb
+ * @license Angular v7.2.0-beta.1+11.sha-2a35471
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
