affe1482b462d268ae96bc2609af829cdedadc72
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 22:21:16 +0000

    098a787 docs: release notes for the v7.1.3 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+32.sha-053b43d
+ * @license Angular v7.2.0-beta.1+33.sha-098a787
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7368,7 +7368,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.2.0-beta.1+32.sha-053b43d');
+const VERSION = new Version('7.2.0-beta.1+33.sha-098a787');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+32.sha-053b43d
+ * @license Angular v7.2.0-beta.1+33.sha-098a787
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
