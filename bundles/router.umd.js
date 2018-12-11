50ce44e99d87dd57a9d6a6c50ffd7759415dfeb7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 00:58:40 +0000

    50687e1 build: fix type-check errors introduced during rules_ts 0.21 (#27586)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+22.sha-05cdfb9
+ * @license Angular v7.2.0-beta.1+24.sha-50687e1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5787,7 +5787,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-beta.1+22.sha-05cdfb9');
+    var VERSION = new core.Version('7.2.0-beta.1+24.sha-50687e1');
 
     /**
      * @license
