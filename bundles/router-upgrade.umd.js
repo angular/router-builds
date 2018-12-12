22ce1b258a52a980dd95ff7544804dfb879f3c3c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-12 19:24:25 +0000

    fc6dc78 fix(docs-infra): fix top menu item clickable area (#27620)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+3.sha-cabf1c7
+ * @license Angular v7.2.0-beta.2+5.sha-fc6dc78
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+3.sha-cabf1c7
+ * @license Angular v7.2.0-beta.2+5.sha-fc6dc78
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5787,7 +5787,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-beta.2+3.sha-cabf1c7');
+    var VERSION = new core.Version('7.2.0-beta.2+5.sha-fc6dc78');
 
     /**
      * @license
