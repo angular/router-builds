b95a76d12165bad52dbbefae6f35adcd9902bc81
Miško Hevery <misko@hevery.com>
2018-12-12 21:31:44 +0000

    7fabe44 fix(ivy): add support for optional nullable injection tokens (#27552)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+9.sha-37c05bd
+ * @license Angular v7.2.0-beta.2+10.sha-7fabe44
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5787,7 +5787,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-beta.2+9.sha-37c05bd');
+    var VERSION = new core.Version('7.2.0-beta.2+10.sha-7fabe44');
 
     /**
      * @license
