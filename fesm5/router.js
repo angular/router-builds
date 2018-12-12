c54a0d6bd50e71dc45abfdd365248307e1fa9adc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-12 19:25:41 +0000

    fc6dc78 fix(docs-infra): fix top menu item clickable area (#27620)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+3.sha-cabf1c7
+ * @license Angular v7.2.0-beta.2+5.sha-fc6dc78
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5712,7 +5712,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+3.sha-cabf1c7');
+var VERSION = new Version('7.2.0-beta.2+5.sha-fc6dc78');
 
 /**
  * @license
