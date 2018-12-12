53bbbd0826e201df7285fac3f1fa9aeb4efe1fcd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-12 19:26:47 +0000

    042463f build(bazel): fix postinstall-patches.js script run during yarn postinstall when running yarn_install (#27610)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+5.sha-fc6dc78
+ * @license Angular v7.2.0-beta.2+4.sha-042463f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5785,7 +5785,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-beta.2+5.sha-fc6dc78');
+    var VERSION = new i0.Version('7.2.0-beta.2+4.sha-042463f');
 
     /**
      * @license
