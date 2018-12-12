d888eb9d9fee9c3743cde4837577fbbe2c693aaa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-12 00:57:24 +0000

    93820b4 build: bump @bazel/ibazel to 0.9.0 (#27601)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2
+ * @license Angular v7.2.0-beta.2+2.sha-93820b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7138,7 +7138,7 @@ function provideRouterInitializer() {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.2.0-beta.2');
+const VERSION = new Version('7.2.0-beta.2+2.sha-93820b4');
 
 /**
  * @fileoverview added by tsickle
