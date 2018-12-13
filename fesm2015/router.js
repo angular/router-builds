5aee217c785a84fa8b4b314e38171fe421127aef
Miško Hevery <misko@hevery.com>
2018-12-13 19:46:50 +0000

    1cc08b4 fix(bazel): fix major/minor semver check between @angular/bazel npm packager version and angular bazel repo version (#27635)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+12.sha-f00af3e
+ * @license Angular v7.2.0-beta.2+14.sha-1cc08b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7430,7 +7430,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+12.sha-f00af3e');
+const VERSION = new Version('7.2.0-beta.2+14.sha-1cc08b4');
 
 /**
  * @fileoverview added by tsickle
