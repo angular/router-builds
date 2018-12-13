f876b869c4bca00b1f0e26a365da467969911c49
Miško Hevery <misko@hevery.com>
2018-12-13 19:45:00 +0000

    f00af3e ci: exclude unnedeed files and directories from angular-robot g3sync check (#27653)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+10.sha-7fabe44
+ * @license Angular v7.2.0-beta.2+12.sha-f00af3e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5699,7 +5699,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+10.sha-7fabe44');
+var VERSION = new Version('7.2.0-beta.2+12.sha-f00af3e');
 
 /**
  * @license
