faa7509c844d32dec1d91ba9fe76252b8138fb79
Miško Hevery <misko@hevery.com>
2018-12-13 19:40:01 +0000

    a808c7d build: ts-api-guardian tsconfig warning with bazel (#27583)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+16.sha-89ace1a
+ * @license Angular v7.1.3+17.sha-a808c7d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+16.sha-89ace1a
+ * @license Angular v7.1.3+17.sha-a808c7d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7136,7 +7136,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.1.3+16.sha-89ace1a');
+const VERSION = new Version('7.1.3+17.sha-a808c7d');
 
 /**
  * @fileoverview added by tsickle
