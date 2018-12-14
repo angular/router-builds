4c147948e84b9ec07ed2bed44eff72875bf60083
Miško Hevery <misko@hevery.com>
2018-12-14 19:03:30 +0000

    d32939d build: restrict visibility of npm_package targets (#27611)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+28.sha-3680aef
+ * @license Angular v7.2.0-beta.2+34.sha-d32939d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7206,7 +7206,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+28.sha-3680aef');
+const VERSION = new Version('7.2.0-beta.2+34.sha-d32939d');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+28.sha-3680aef
+ * @license Angular v7.2.0-beta.2+34.sha-d32939d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
