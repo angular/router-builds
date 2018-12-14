803a0b0a0614d38a87702f197c1ec7d52615fb43
Miško Hevery <misko@hevery.com>
2018-12-14 23:51:43 +0000

    c220328 docs: adding ngAtlanta 2019 (#27522)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+26.sha-fa3af8b
+ * @license Angular v7.1.3+25.sha-c220328
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5721,7 +5721,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.3+26.sha-fa3af8b');
+    var VERSION = new core.Version('7.1.3+25.sha-c220328');
 
     /**
      * @license
