96e05f180206dc9ab205b90337d77fec55a078ca
Miško Hevery <misko@hevery.com>
2018-12-14 19:04:44 +0000

    d32939d build: restrict visibility of npm_package targets (#27611)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+28.sha-3680aef
+ * @license Angular v7.2.0-beta.2+34.sha-d32939d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-beta.2+28.sha-3680aef');
+    var VERSION = new i0.Version('7.2.0-beta.2+34.sha-d32939d');
 
     /**
      * @license
