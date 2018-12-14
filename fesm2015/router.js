ce69cc5c3646033b476c258f6eb185461c16cb6d
Miško Hevery <misko@hevery.com>
2018-12-14 19:03:41 +0000

    d528e30 test(ivy): update fixmes in packages/core/test/linker/jit_summaries_integration_spec.ts (#27639)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+34.sha-d32939d
+ * @license Angular v7.2.0-beta.2+33.sha-d528e30
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7206,7 +7206,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+34.sha-d32939d');
+const VERSION = new Version('7.2.0-beta.2+33.sha-d528e30');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+34.sha-d32939d
+ * @license Angular v7.2.0-beta.2+33.sha-d528e30
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
