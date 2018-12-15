f3e83f36f37ea041a9250e55d879d53c96befe3d
Miško Hevery <misko@hevery.com>
2018-12-15 00:05:17 +0000

    b39efdd fix(common): KeyValuePipe should return empty array for empty objects (#27258)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+42.sha-0b78f54
+ * @license Angular v7.2.0-beta.2+44.sha-b39efdd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5785,7 +5785,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-beta.2+42.sha-0b78f54');
+    var VERSION = new i0.Version('7.2.0-beta.2+44.sha-b39efdd');
 
     /**
      * @license
