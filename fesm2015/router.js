2f7a51afe55dc048778d1325dd62f3004ecf66d8
Miško Hevery <misko@hevery.com>
2018-12-15 00:07:29 +0000

    7dab458 docs(common): fix documentation for getLocaleCurrencyName (#27087) PR Close #27087

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+43.sha-f9545d1
+ * @license Angular v7.2.0-beta.2+51.sha-7dab458
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7200,7 +7200,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+43.sha-f9545d1');
+const VERSION = new Version('7.2.0-beta.2+51.sha-7dab458');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+43.sha-f9545d1
+ * @license Angular v7.2.0-beta.2+51.sha-7dab458
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
