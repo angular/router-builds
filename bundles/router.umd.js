09f0c212f376bf568e1c086d0b9efaa26662592c
Miško Hevery <misko@hevery.com>
2018-12-15 00:08:27 +0000

    f1c9d6a fix(animations): do not truncate decimals for delay (#24455)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+48.sha-b00aeef
+ * @license Angular v7.2.0-beta.2+47.sha-f1c9d6a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5785,7 +5785,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-beta.2+48.sha-b00aeef');
+    var VERSION = new i0.Version('7.2.0-beta.2+47.sha-f1c9d6a');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+48.sha-b00aeef
+ * @license Angular v7.2.0-beta.2+47.sha-f1c9d6a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7430,7 +7430,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+48.sha-b00aeef');
+const VERSION = new Version('7.2.0-beta.2+47.sha-f1c9d6a');
 
 /**
  * @fileoverview added by tsickle
