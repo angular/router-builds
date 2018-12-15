9eceb2242d141e085dfd3362c9437dcb77d2b5e3
Miško Hevery <misko@hevery.com>
2018-12-15 00:06:46 +0000

    f4a797d fix(ivy): prevent ngcc from inadvertently dropping Ivy definitions (#27159)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+49.sha-4df82bd
+ * @license Angular v7.2.0-beta.2+50.sha-f4a797d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+49.sha-4df82bd
+ * @license Angular v7.2.0-beta.2+50.sha-f4a797d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5787,7 +5787,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-beta.2+49.sha-4df82bd');
+    var VERSION = new core.Version('7.2.0-beta.2+50.sha-f4a797d');
 
     /**
      * @license
