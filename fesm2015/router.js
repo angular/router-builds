7b4e1241f159e3fe827f7499d11ae6b2acb2e0b0
Miško Hevery <misko@hevery.com>
2018-12-15 00:05:45 +0000

    df123e0 fix(animations): mark actual descendant node as disabled (#26180)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+44.sha-b39efdd
+ * @license Angular v7.2.0-beta.2+46.sha-df123e0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7430,7 +7430,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+44.sha-b39efdd');
+const VERSION = new Version('7.2.0-beta.2+46.sha-df123e0');
 
 /**
  * @fileoverview added by tsickle
