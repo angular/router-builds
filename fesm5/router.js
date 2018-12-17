0bf3b548fb032e057b023845b211ca3e84ca4c34
Miško Hevery <misko@hevery.com>
2018-12-17 18:45:44 +0000

    52544ff fix(ivy): ngcc generates setClassMetadata calls for ES5 bundles (#27438)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+52.sha-f8096d4
+ * @license Angular v7.2.0-beta.2+58.sha-52544ff
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5699,7 +5699,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+52.sha-f8096d4');
+var VERSION = new Version('7.2.0-beta.2+58.sha-52544ff');
 
 /**
  * @license
