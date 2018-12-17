825cef16e4bb24241507463495e7cd09432b42e4
Miško Hevery <misko@hevery.com>
2018-12-17 23:18:15 +0000

    c986d3d fix(upgrade): upgrade Directive facade should not return different instance from constructor (#27660)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+64.sha-e94975d
+ * @license Angular v7.2.0-beta.2+66.sha-c986d3d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5787,7 +5787,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-beta.2+64.sha-e94975d');
+    var VERSION = new core.Version('7.2.0-beta.2+66.sha-c986d3d');
 
     /**
      * @license
