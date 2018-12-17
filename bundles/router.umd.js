0acef5bdccd0fd7ef28066eda1890687a4ee3cf8
Miško Hevery <misko@hevery.com>
2018-12-17 18:53:48 +0000

    d132bae test(ivy): add more test for content projection with ngIf / ngFor (#27705)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+61.sha-44ec66d
+ * @license Angular v7.2.0-beta.2+63.sha-d132bae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5785,7 +5785,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-beta.2+61.sha-44ec66d');
+    var VERSION = new i0.Version('7.2.0-beta.2+63.sha-d132bae');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+61.sha-44ec66d
+ * @license Angular v7.2.0-beta.2+63.sha-d132bae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
