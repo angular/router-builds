d136d6e183bb9e69f1a2dec33c80e6ee164a4b46
Miško Hevery <misko@hevery.com>
2018-12-17 18:17:44 +0000

    94f17e9 docs: remove misplaced code in Universal guide (#26865)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+53.sha-ea10a3a
+ * @license Angular v7.2.0-beta.2+62.sha-94f17e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5785,7 +5785,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-beta.2+53.sha-ea10a3a');
+    var VERSION = new i0.Version('7.2.0-beta.2+62.sha-94f17e9');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+53.sha-ea10a3a
+ * @license Angular v7.2.0-beta.2+62.sha-94f17e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7430,7 +7430,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+53.sha-ea10a3a');
+const VERSION = new Version('7.2.0-beta.2+62.sha-94f17e9');
 
 /**
  * @fileoverview added by tsickle
