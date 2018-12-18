d62100a4b42615ee5eb02812dcbae806a8220a54
Miško Hevery <misko@hevery.com>
2018-12-18 01:05:13 +0000

    c986d3d fix(upgrade): upgrade Directive facade should not return different instance from constructor (#27660)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+58.sha-52544ff
+ * @license Angular v7.2.0-beta.2+66.sha-c986d3d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5699,7 +5699,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+58.sha-52544ff');
+var VERSION = new Version('7.2.0-beta.2+66.sha-c986d3d');
 
 /**
  * @license
