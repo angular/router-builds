7efb4f691c560df9c22e5a7ae3f419d53daf39c8
Miško Hevery <misko@hevery.com>
2018-12-18 21:44:32 +0000

    b04bc5d test(ivy): re-enable passing upgrade tests (#27736)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+82.sha-1c93afe
+ * @license Angular v7.2.0-beta.2+83.sha-b04bc5d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5787,7 +5787,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-beta.2+82.sha-1c93afe');
+    var VERSION = new core.Version('7.2.0-beta.2+83.sha-b04bc5d');
 
     /**
      * @license
