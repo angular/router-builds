e0e03010d309a77b6ae9145a8dd194238682dbd1
Miško Hevery <misko@hevery.com>
2018-12-18 04:37:34 +0000

    4bf8d64 fix(ivy): restore `fixmeIvy` imports for Router-related tests (#27720)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+70.sha-8042140
+ * @license Angular v7.2.0-beta.2+74.sha-4bf8d64
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5699,7 +5699,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+70.sha-8042140');
+var VERSION = new Version('7.2.0-beta.2+74.sha-4bf8d64');
 
 /**
  * @license
