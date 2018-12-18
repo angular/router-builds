5537129891cbc9604d36edce6fc58d930b65be82
Miško Hevery <misko@hevery.com>
2018-12-18 18:01:31 +0000

    d766ad0 build: update gulp-clang-format dependency (#27712)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+75.sha-e98c57a
+ * @license Angular v7.2.0-beta.2+77.sha-d766ad0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5787,7 +5787,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-beta.2+75.sha-e98c57a');
+    var VERSION = new core.Version('7.2.0-beta.2+77.sha-d766ad0');
 
     /**
      * @license
