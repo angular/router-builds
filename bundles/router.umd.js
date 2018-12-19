d951235411285a642638a7b620b4f9a0c9e8035f
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 23:49:46 +0000

    b08f3ac test(bazel): Make sure CLI project created with Bazel works with original workflow (#27741)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+5.sha-3ab25ab
+ * @license Angular v7.2.0-rc.0+6.sha-b08f3ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-rc.0+5.sha-3ab25ab');
+    var VERSION = new core.Version('7.2.0-rc.0+6.sha-b08f3ac');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+5.sha-3ab25ab
+ * @license Angular v7.2.0-rc.0+6.sha-b08f3ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
