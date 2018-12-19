93246861ee27bac01691474ab0cb841b3fc62a2b
Miško Hevery <misko@hevery.com>
2018-12-19 00:13:19 +0000

    24fa0b7 docs: release notes for the v7.1.4 release

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+91.sha-12c3176
+ * @license Angular v7.2.0-beta.2+92.sha-24fa0b7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+91.sha-12c3176');
+var VERSION = new Version('7.2.0-beta.2+92.sha-24fa0b7');
 
 /**
  * @license
