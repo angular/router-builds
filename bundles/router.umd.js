5c203145796669634b2a748d530e44b563d647ac
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 20:43:57 +0000

    8f8572f fix(ivy): @Host should behave as in View Engine (#27646)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+2.sha-e8f7241
+ * @license Angular v7.2.0-rc.0+3.sha-8f8572f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-rc.0+2.sha-e8f7241');
+    var VERSION = new core.Version('7.2.0-rc.0+3.sha-8f8572f');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+2.sha-e8f7241
+ * @license Angular v7.2.0-rc.0+3.sha-8f8572f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7262,7 +7262,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+2.sha-e8f7241');
+const VERSION = new Version('7.2.0-rc.0+3.sha-8f8572f');
 
 /**
  * @fileoverview added by tsickle
