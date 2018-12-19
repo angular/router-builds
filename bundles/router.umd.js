6a3074c5160647c8133eb931e312d51a75c900f7
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 20:44:20 +0000

    0604527 Revert "build: update gulp-clang-format dependency (#27712)" (#27759)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+3.sha-8f8572f
+ * @license Angular v7.2.0-rc.0+4.sha-0604527
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-rc.0+3.sha-8f8572f');
+    var VERSION = new core.Version('7.2.0-rc.0+4.sha-0604527');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+3.sha-8f8572f
+ * @license Angular v7.2.0-rc.0+4.sha-0604527
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
