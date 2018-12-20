e6248cb7c230994b363a6ad6c77a74d8e8287d45
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 17:27:36 +0000

    a833b98 fix(ivy): change detection strategy not being passed to compiler (#27753)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+12.sha-a0585c9
+ * @license Angular v7.2.0-rc.0+11.sha-a833b98
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-rc.0+12.sha-a0585c9');
+    var VERSION = new core.Version('7.2.0-rc.0+11.sha-a833b98');
 
     /**
      * @license
