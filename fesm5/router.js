b7a7128e5512d8701c58f75c40614961c14d66db
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 17:28:16 +0000

    a0585c9 fix(ivy): correct content projection with nested templates (#27755)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+10.sha-4b70a4e
+ * @license Angular v7.2.0-rc.0+12.sha-a0585c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+10.sha-4b70a4e');
+var VERSION = new Version('7.2.0-rc.0+12.sha-a0585c9');
 
 /**
  * @license
