a782833e8fbd2f9aee0b0e9046eb0b04c5137e6a
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 17:43:12 +0000

    880e8a5 refactor: fix broken formatting

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+11.sha-a833b98
+ * @license Angular v7.2.0-rc.0+13.sha-880e8a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+11.sha-a833b98');
+var VERSION = new Version('7.2.0-rc.0+13.sha-880e8a5');
 
 /**
  * @license
