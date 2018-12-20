cd6a70e508f25c735ce780540295e6bc513bf4ad
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 21:55:02 +0000

    50a91ba docs(upgrade): add gotchas/tips/example for multiple downgraded modules (#27217)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+14.sha-07ada7f
+ * @license Angular v7.2.0-rc.0+19.sha-50a91ba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5794,7 +5794,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-rc.0+14.sha-07ada7f');
+    var VERSION = new i0.Version('7.2.0-rc.0+19.sha-50a91ba');
 
     /**
      * @license
