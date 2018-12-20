2f8e665d6c9882133fd04b39f8a46a67cd1e42a3
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 17:41:56 +0000

    880e8a5 refactor: fix broken formatting

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+11.sha-a833b98
+ * @license Angular v7.2.0-rc.0+13.sha-880e8a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+11.sha-a833b98
+ * @license Angular v7.2.0-rc.0+13.sha-880e8a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7262,7 +7262,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+11.sha-a833b98');
+const VERSION = new Version('7.2.0-rc.0+13.sha-880e8a5');
 
 /**
  * @fileoverview added by tsickle
