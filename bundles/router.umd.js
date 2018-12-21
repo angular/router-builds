358de78d4586dfea1d4443aac1f7d8cb8099815a
Matias Niemelä <matias@yearofmoo.com>
2018-12-21 22:07:14 +0000

    4b67b0a fix(ivy): adding TestBed.overrideProvider support (#27693)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+22.sha-509aa61
+ * @license Angular v7.2.0-rc.0+25.sha-4b67b0a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5794,7 +5794,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-rc.0+22.sha-509aa61');
+    var VERSION = new i0.Version('7.2.0-rc.0+25.sha-4b67b0a');
 
     /**
      * @license
