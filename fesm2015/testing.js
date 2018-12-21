90d0423de7b9083bd5a70498044df87d8d057246
Matias Niemelä <matias@yearofmoo.com>
2018-12-21 23:40:13 +0000

    13eb57a fix(ivy): merge static style rendering across elements, directives and components (#27661)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+26.sha-0b3ae3d
+ * @license Angular v7.2.0-rc.0+27.sha-13eb57a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+26.sha-0b3ae3d
+ * @license Angular v7.2.0-rc.0+27.sha-13eb57a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5708,7 +5708,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+26.sha-0b3ae3d');
+var VERSION = new Version('7.2.0-rc.0+27.sha-13eb57a');
 
 /**
  * @license
