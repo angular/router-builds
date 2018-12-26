c293c84a85f1725997c4e5720f4f55f8a33ab7a0
Ben Lesh <ben@benlesh.com>
2018-12-26 19:46:15 +0000

    3f64e87 test: check payload size for cli-hello-world-ivy (#27797)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+30.sha-c34eee4
+ * @license Angular v7.2.0-rc.0+32.sha-3f64e87
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-rc.0+30.sha-c34eee4');
+    var VERSION = new core.Version('7.2.0-rc.0+32.sha-3f64e87');
 
     /**
      * @license
