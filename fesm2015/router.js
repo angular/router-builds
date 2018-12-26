23f2f65cc0e022635ef06b6d3d530d9530977fb0
Ben Lesh <ben@benlesh.com>
2018-12-26 19:55:49 +0000

    707c682 docs(elements): add "Firefox" for support of custom elements (#27789)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+32.sha-3f64e87
+ * @license Angular v7.2.0-rc.0+33.sha-707c682
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+32.sha-3f64e87');
+const VERSION = new Version('7.2.0-rc.0+33.sha-707c682');
 
 /**
  * @fileoverview added by tsickle
