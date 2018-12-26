89471e24d1dd9ac9738c29d88a7f8bb9e0822772
Ben Lesh <ben@benlesh.com>
2018-12-26 20:00:42 +0000

    d1de9ff build: update to latest rules_nodejs (#27764)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+33.sha-707c682
+ * @license Angular v7.2.0-rc.0+34.sha-d1de9ff
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-rc.0+33.sha-707c682');
+    var VERSION = new i0.Version('7.2.0-rc.0+34.sha-d1de9ff');
 
     /**
      * @license
