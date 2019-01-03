7355c44c3e7f2d52a310f8db53ff378bf1adcfcb
Ben Lesh <ben@benlesh.com>
2019-01-03 18:20:34 +0000

    7df9040 docs: add missing comma to environment config in build guide (#27476)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+44.sha-1dc95c4
+ * @license Angular v7.2.0-rc.0+42.sha-7df9040
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+44.sha-1dc95c4
+ * @license Angular v7.2.0-rc.0+42.sha-7df9040
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+44.sha-1dc95c4');
+const VERSION = new Version('7.2.0-rc.0+42.sha-7df9040');
 
 /**
  * @fileoverview added by tsickle
