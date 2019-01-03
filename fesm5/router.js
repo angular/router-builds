51cc92a23d2129fd7c0062d003890e5e69bc467a
Ben Lesh <ben@benlesh.com>
2019-01-03 18:22:42 +0000

    880b4aa fix(docs-infra): upgrade to latest dgeni-packages to fix linking problem (#27864)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+47.sha-5fee444
+ * @license Angular v7.2.0-rc.0+48.sha-880b4aa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+47.sha-5fee444');
+var VERSION = new Version('7.2.0-rc.0+48.sha-880b4aa');
 
 /**
  * @license
