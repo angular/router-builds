0b3550002f374a52e6cd82bf5313c7770d2f392a
Ben Lesh <ben@benlesh.com>
2019-01-03 18:21:26 +0000

    5fee444 test(ivy): update test failure root cause (#27855)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+45.sha-a979001
+ * @license Angular v7.2.0-rc.0+47.sha-5fee444
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5702,7 +5702,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+45.sha-a979001');
+var VERSION = new Version('7.2.0-rc.0+47.sha-5fee444');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+45.sha-a979001
+ * @license Angular v7.2.0-rc.0+47.sha-5fee444
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
