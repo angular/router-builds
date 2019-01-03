1a70971031a067634cea47bf7afdfd90b6127172
Ben Lesh <ben@benlesh.com>
2019-01-03 18:18:10 +0000

    a0a3648 docs: add more info on what is being cached (#27343)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+40.sha-755c809
+ * @license Angular v7.2.0-rc.0+43.sha-a0a3648
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7262,7 +7262,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+40.sha-755c809');
+const VERSION = new Version('7.2.0-rc.0+43.sha-a0a3648');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+40.sha-755c809
+ * @license Angular v7.2.0-rc.0+43.sha-a0a3648
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
