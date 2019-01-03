5418b06cb33afd7a4347810077b886283e440ffc
Ben Lesh <ben@benlesh.com>
2019-01-03 18:21:30 +0000

    880b4aa fix(docs-infra): upgrade to latest dgeni-packages to fix linking problem (#27864)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+47.sha-5fee444
+ * @license Angular v7.2.0-rc.0+48.sha-880b4aa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-rc.0+47.sha-5fee444');
+    var VERSION = new core.Version('7.2.0-rc.0+48.sha-880b4aa');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+47.sha-5fee444
+ * @license Angular v7.2.0-rc.0+48.sha-880b4aa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7262,7 +7262,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+47.sha-5fee444');
+const VERSION = new Version('7.2.0-rc.0+48.sha-880b4aa');
 
 /**
  * @fileoverview added by tsickle
