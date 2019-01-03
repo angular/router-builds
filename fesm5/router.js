c8397e316e0dd6eb6f1e54ee33385d6046790763
Ben Lesh <ben@benlesh.com>
2019-01-03 18:19:37 +0000

    5638c1d docs: fix npm description in architecture modules guide (#27818)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+40.sha-755c809
+ * @license Angular v7.2.0-rc.0+41.sha-5638c1d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+40.sha-755c809');
+var VERSION = new Version('7.2.0-rc.0+41.sha-5638c1d');
 
 /**
  * @license
