332dec25a76ccfca81625458a23a8b55a482ff5a
Ben Lesh <ben@benlesh.com>
2019-01-03 18:20:22 +0000

    1dc95c4 docs: fix typo in TOH http section (#26127)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+41.sha-5638c1d
+ * @license Angular v7.2.0-rc.0+44.sha-1dc95c4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+41.sha-5638c1d');
+const VERSION = new Version('7.2.0-rc.0+44.sha-1dc95c4');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+41.sha-5638c1d
+ * @license Angular v7.2.0-rc.0+44.sha-1dc95c4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+41.sha-5638c1d');
+var VERSION = new Version('7.2.0-rc.0+44.sha-1dc95c4');
 
 /**
  * @license
