ce56e4e0bd45218dfcf829c81d13fe5a85db71a8
Ben Lesh <ben@benlesh.com>
2019-01-03 18:20:25 +0000

    a979001 fix(ivy): resolve `forwardRefs` correctly in TestBed (JIT) (#27737)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+42.sha-7df9040
+ * @license Angular v7.2.0-rc.0+45.sha-a979001
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7262,7 +7262,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+42.sha-7df9040');
+const VERSION = new Version('7.2.0-rc.0+45.sha-a979001');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+42.sha-7df9040
+ * @license Angular v7.2.0-rc.0+45.sha-a979001
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5702,7 +5702,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+42.sha-7df9040');
+var VERSION = new Version('7.2.0-rc.0+45.sha-a979001');
 
 /**
  * @license
