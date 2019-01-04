df330bc14e38e003ae7644f2e23980d4ef6bfa2c
Kara Erickson <karakara@google.com>
2019-01-04 21:03:27 +0000

    13d23f3 fix(ivy): ngtsc program emit ignoring custom transformers (#27837)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+59.sha-48555f9
+ * @license Angular v7.2.0-rc.0+60.sha-13d23f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+59.sha-48555f9
+ * @license Angular v7.2.0-rc.0+60.sha-13d23f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-rc.0+59.sha-48555f9');
+    var VERSION = new i0.Version('7.2.0-rc.0+60.sha-13d23f3');
 
     /**
      * @license
