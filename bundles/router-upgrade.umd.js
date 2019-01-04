cda54df3df9e28a3847b9c94d936980b114e3cae
Kara Erickson <karakara@google.com>
2019-01-04 20:52:32 +0000

    3f2ebbd fix(ivy): support bindings in ICUs in nested contexts (#27914)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+57.sha-7eb2c41
+ * @license Angular v7.2.0-rc.0+56.sha-3f2ebbd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+57.sha-7eb2c41
+ * @license Angular v7.2.0-rc.0+56.sha-3f2ebbd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-rc.0+57.sha-7eb2c41');
+    var VERSION = new i0.Version('7.2.0-rc.0+56.sha-3f2ebbd');
 
     /**
      * @license
