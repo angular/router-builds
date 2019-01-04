10855bc3ec87ff2cb6de128dbecb444493c48927
Kara Erickson <karakara@google.com>
2019-01-04 23:25:37 +0000

    929334b fix(ivy): should not throw when getting VCRef.parentInjector on the root view (#27909)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+63.sha-fb7816f
+ * @license Angular v7.2.0-rc.0+64.sha-929334b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-rc.0+63.sha-fb7816f');
+    var VERSION = new i0.Version('7.2.0-rc.0+64.sha-929334b');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+63.sha-fb7816f
+ * @license Angular v7.2.0-rc.0+64.sha-929334b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+63.sha-fb7816f');
+const VERSION = new Version('7.2.0-rc.0+64.sha-929334b');
 
 /**
  * @fileoverview added by tsickle
