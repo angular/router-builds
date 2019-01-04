2bf2ef4d93c7ba9b7b43c2af3927b6c03d3e6073
Kara Erickson <karakara@google.com>
2019-01-04 20:29:09 +0000

    8a05199 test: remove bower and polymer benchmarks (#27931)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+52.sha-51a0bd2
+ * @license Angular v7.2.0-rc.0+53.sha-8a05199
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-rc.0+52.sha-51a0bd2');
+    var VERSION = new core.Version('7.2.0-rc.0+53.sha-8a05199');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+52.sha-51a0bd2
+ * @license Angular v7.2.0-rc.0+53.sha-8a05199
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
