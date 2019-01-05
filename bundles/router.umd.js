76c400fffb6b800251670f26c4c7946ba23ee048
Kara Erickson <karakara@google.com>
2019-01-05 03:04:47 +0000

    411de25 fix(ivy): disable test which rely on DebugElement in WebWorker (#27895)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+65.sha-b61dafa
+ * @license Angular v7.2.0-rc.0+66.sha-411de25
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-rc.0+65.sha-b61dafa');
+    var VERSION = new core.Version('7.2.0-rc.0+66.sha-411de25');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+65.sha-b61dafa
+ * @license Angular v7.2.0-rc.0+66.sha-411de25
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
