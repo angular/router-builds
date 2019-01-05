dac6885b6b952da067f844537b75b7fb53c021eb
Filipe Silva <filipematossilva@gmail.com>
2019-01-05 22:42:27 +0000

    784453c ci: use image based cache

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+53.sha-9576e48
+ * @license Angular v7.2.0-rc.0+53.sha-784453c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7262,7 +7262,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+53.sha-9576e48');
+const VERSION = new Version('7.2.0-rc.0+53.sha-784453c');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+53.sha-9576e48
+ * @license Angular v7.2.0-rc.0+53.sha-784453c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
