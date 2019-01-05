629b1806d00f143089ed37e75d2d9d50d4215ea3
Kara Erickson <karakara@google.com>
2019-01-05 03:06:00 +0000

    411de25 fix(ivy): disable test which rely on DebugElement in WebWorker (#27895)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+65.sha-b61dafa
+ * @license Angular v7.2.0-rc.0+66.sha-411de25
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+65.sha-b61dafa');
+var VERSION = new Version('7.2.0-rc.0+66.sha-411de25');
 
 /**
  * @license
