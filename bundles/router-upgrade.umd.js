459ec3eb166bbf53a63b55d2f4a9c8ff557c4ec8
Kara Erickson <karakara@google.com>
2019-01-05 03:04:08 +0000

    6cc864a build: enforce minimum Bazel version of 0.21.0 in WORKSPACE so cache works with `yarn bazel` (#27935)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+27.sha-0a54825
+ * @license Angular v7.2.0-rc.0+29.sha-6cc864a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+27.sha-0a54825
+ * @license Angular v7.2.0-rc.0+29.sha-6cc864a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5702,7 +5702,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+27.sha-0a54825');
+var VERSION = new Version('7.2.0-rc.0+29.sha-6cc864a');
 
 /**
  * @license
