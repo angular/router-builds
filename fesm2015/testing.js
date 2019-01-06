9b25fce08275396dff9acfe3d1eb1f6ed483b662
Kara Erickson <karakara@google.com>
2019-01-06 02:10:00 +0000

    7242551 ci: fix typos in codeowners file (#27945)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+30.sha-1ad88e0
+ * @license Angular v7.2.0-rc.0+31.sha-7242551
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+30.sha-1ad88e0
+ * @license Angular v7.2.0-rc.0+31.sha-7242551
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5702,7 +5702,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+30.sha-1ad88e0');
+var VERSION = new Version('7.2.0-rc.0+31.sha-7242551');
 
 /**
  * @license
