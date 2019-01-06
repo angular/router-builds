262397d0ff54b3f86d7b1ec41f76e02a366215da
Filipe Silva <filipematossilva@gmail.com>
2019-01-06 14:45:54 +0000

    b68bc8b ci: add linux buildkite tests

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+54.sha-a78a590
+ * @license Angular v7.2.0-rc.0+54.sha-b68bc8b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+54.sha-a78a590
+ * @license Angular v7.2.0-rc.0+54.sha-b68bc8b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5702,7 +5702,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+54.sha-a78a590');
+var VERSION = new Version('7.2.0-rc.0+54.sha-b68bc8b');
 
 /**
  * @license
