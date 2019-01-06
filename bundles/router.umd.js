afa456946594795d9037b5e3d963f36ceb7018ef
Kara Erickson <karakara@google.com>
2019-01-06 02:06:31 +0000

    224d787 ci: replace our pullapprove setup with GitHub CODEOWNERS (#27690)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+68.sha-7adcbb3
+ * @license Angular v7.2.0-rc.0+69.sha-224d787
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-rc.0+68.sha-7adcbb3');
+    var VERSION = new core.Version('7.2.0-rc.0+69.sha-224d787');
 
     /**
      * @license
