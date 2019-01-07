3207e7a6f08073e4fd85a2b1a15ca310b6757587
Kara Erickson <karakara@google.com>
2019-01-07 17:08:36 +0000

    e94e7ee docs: add Alyle UI to resources (#27954) PR Close #27954

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0
+ * @license Angular v7.2.0+2.sha-e94e7ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0');
+var VERSION = new Version('7.2.0+2.sha-e94e7ee');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0
+ * @license Angular v7.2.0+2.sha-e94e7ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
