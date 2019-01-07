5fc016816919e8529972f441d3294268554cda13
Kara Erickson <karakara@google.com>
2019-01-07 17:13:33 +0000

    4588053 test(ivy): update root causes for @angular/core tests (#27961)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+6.sha-e08feb7
+ * @license Angular v7.2.0+7.sha-4588053
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5702,7 +5702,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+6.sha-e08feb7');
+var VERSION = new Version('7.2.0+7.sha-4588053');
 
 /**
  * @license
