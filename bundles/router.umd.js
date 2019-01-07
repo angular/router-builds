7d7f9886984e1b8bdd0614c2f9b53ac941f9e6ba
Kara Erickson <karakara@google.com>
2019-01-07 17:09:05 +0000

    654055b ci: add brandonroberts to @angular/framework-global-approvers-for-docs-only-changes (#27949)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+2.sha-e94e7ee
+ * @license Angular v7.2.0+1.sha-654055b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+2.sha-e94e7ee');
+    var VERSION = new i0.Version('7.2.0+1.sha-654055b');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+2.sha-e94e7ee
+ * @license Angular v7.2.0+1.sha-654055b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+2.sha-e94e7ee');
+const VERSION = new Version('7.2.0+1.sha-654055b');
 
 /**
  * @fileoverview added by tsickle
