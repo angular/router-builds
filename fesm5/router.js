1982cd34ffc9ee449e043ef5035a9ba1be9dd9e4
Kara Erickson <karakara@google.com>
2019-01-07 17:09:37 +0000

    e08feb7 refactor(ivy): unify native node handling around render parent and element / ICU containers (#27925)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+1.sha-654055b
+ * @license Angular v7.2.0+6.sha-e08feb7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5702,7 +5702,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+1.sha-654055b');
+var VERSION = new Version('7.2.0+6.sha-e08feb7');
 
 /**
  * @license
