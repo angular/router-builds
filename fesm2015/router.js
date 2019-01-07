674a2365ff8ee9c3a7f70a40fd98fca6080c55f0
Kara Erickson <karakara@google.com>
2019-01-07 23:10:06 +0000

    e775313 fix(ivy): ngOnDestroy hooks should be called on providers (#27955)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+7.sha-4588053
+ * @license Angular v7.2.0+9.sha-e775313
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7262,7 +7262,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+7.sha-4588053');
+const VERSION = new Version('7.2.0+9.sha-e775313');
 
 /**
  * @fileoverview added by tsickle
