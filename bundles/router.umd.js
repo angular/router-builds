a22426612b964469535120c4ce86af6660ae2f56
Kara Erickson <karakara@google.com>
2019-01-08 19:46:38 +0000

    1edb8af docs: group fixes and features for 7.2.0 in changelog desc (#27974)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+52.sha-1cca9cc
+ * @license Angular v7.2.0-rc.0+54.sha-1edb8af
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-rc.0+52.sha-1cca9cc');
+    var VERSION = new core.Version('7.2.0-rc.0+54.sha-1edb8af');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+52.sha-1cca9cc
+ * @license Angular v7.2.0-rc.0+54.sha-1edb8af
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
