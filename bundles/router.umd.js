7e6ae11a99e586c456dc0d8e52e215aa0d9bb1da
Kara Erickson <karakara@google.com>
2019-01-08 20:45:29 +0000

    a084024 ci: update payload size for cli-hello-world (#27994)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+33.sha-917c09c
+ * @license Angular v7.2.0+43.sha-a084024
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+33.sha-917c09c');
+    var VERSION = new core.Version('7.2.0+43.sha-a084024');
 
     /**
      * @license
