cc339e1af9e10bdf745883278e59d92e5e3d7f82
Kara Erickson <karakara@google.com>
2019-01-08 22:21:45 +0000

    1a7f92c test(ivy): re-enable passing animation tests (#27997)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+43.sha-a084024
+ * @license Angular v7.2.0+44.sha-1a7f92c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5702,7 +5702,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+43.sha-a084024');
+var VERSION = new Version('7.2.0+44.sha-1a7f92c');
 
 /**
  * @license
