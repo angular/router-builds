49bf4dea0a0fe2f05a232b065fa65e4df4e234e8
Andrew Kushnir <akushnir@google.com>
2019-01-09 21:03:52 +0000

    582395b docs: fix typo PageNotFoundCompponent (#27799) closes: #27795

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+74.sha-9b2b9b3
+ * @license Angular v7.2.0+72.sha-582395b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+74.sha-9b2b9b3');
+    var VERSION = new i0.Version('7.2.0+72.sha-582395b');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+74.sha-9b2b9b3
+ * @license Angular v7.2.0+72.sha-582395b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+74.sha-9b2b9b3');
+var VERSION = new Version('7.2.0+72.sha-582395b');
 
 /**
  * @license
