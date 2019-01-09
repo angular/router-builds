e08e214de90963c1b352b61cd29c31f485cc8d3d
Andrew Kushnir <akushnir@google.com>
2019-01-09 19:30:16 +0000

    d6cfe2e build: use cross platform workspace_status_command (#27431)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+56.sha-c3aa24c
+ * @license Angular v7.2.0+67.sha-d6cfe2e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+56.sha-c3aa24c');
+    var VERSION = new i0.Version('7.2.0+67.sha-d6cfe2e');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+56.sha-c3aa24c
+ * @license Angular v7.2.0+67.sha-d6cfe2e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+56.sha-c3aa24c');
+const VERSION = new Version('7.2.0+67.sha-d6cfe2e');
 
 /**
  * @fileoverview added by tsickle
