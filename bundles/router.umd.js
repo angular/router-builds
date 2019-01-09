1f1269d84eca96e6222f40ba60968d255dcbaaeb
Andrew Kushnir <akushnir@google.com>
2019-01-09 23:56:20 +0000

    d0c3e25 docs: remove deprecated `fromPromise` from RxJS guide (#28015)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+78.sha-e1e4887
+ * @license Angular v7.2.0+79.sha-d0c3e25
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+78.sha-e1e4887');
+    var VERSION = new i0.Version('7.2.0+79.sha-d0c3e25');
 
     /**
      * @license
