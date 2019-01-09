73b640c9042b20444e813051fd4ee4d7887a873d
Andrew Kushnir <akushnir@google.com>
2019-01-09 21:07:35 +0000

    eb3345b docs: fix typo PageNotFoundCompponent (#27799) closes: #27795

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+72.sha-a06469c
+ * @license Angular v7.2.0-rc.0+71.sha-eb3345b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7262,7 +7262,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+72.sha-a06469c');
+const VERSION = new Version('7.2.0-rc.0+71.sha-eb3345b');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+72.sha-a06469c
+ * @license Angular v7.2.0-rc.0+71.sha-eb3345b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
