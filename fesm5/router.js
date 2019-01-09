1cbf27c1ece8b47c306fda1ade5714db4c552b80
Kara Erickson <karakara@google.com>
2019-01-09 01:45:46 +0000

    7227b4a docs: use static zip function as an Observable creator. (#26790)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+60.sha-5a7dd73
+ * @license Angular v7.2.0-rc.0+61.sha-7227b4a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5702,7 +5702,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+60.sha-5a7dd73');
+var VERSION = new Version('7.2.0-rc.0+61.sha-7227b4a');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+60.sha-5a7dd73
+ * @license Angular v7.2.0-rc.0+61.sha-7227b4a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
