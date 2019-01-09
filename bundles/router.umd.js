7064eb8e786976a9a8925d14e8288a7de3e76af0
Andrew Kushnir <akushnir@google.com>
2019-01-09 21:04:09 +0000

    91a8a4f docs(forms): remove duplicated link (#27884) PR Close #27884

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+72.sha-582395b
+ * @license Angular v7.2.0+73.sha-91a8a4f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+72.sha-582395b');
+    var VERSION = new i0.Version('7.2.0+73.sha-91a8a4f');
 
     /**
      * @license
