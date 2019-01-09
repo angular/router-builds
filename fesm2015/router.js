62c25e09b83b70e25a97dae40b96053fb3de155c
Kara Erickson <karakara@google.com>
2019-01-09 01:06:45 +0000

    1c39ad3 feat(ivy): reference external classes by their exported name (#27743)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+47.sha-b621a69
+ * @license Angular v7.2.0+54.sha-1c39ad3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7262,7 +7262,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+47.sha-b621a69');
+const VERSION = new Version('7.2.0+54.sha-1c39ad3');
 
 /**
  * @fileoverview added by tsickle
