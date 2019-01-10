29bcd9c487ebffd3fb0bdc4de7f21a411edb187b
Andrew Kushnir <akushnir@google.com>
2019-01-10 01:27:20 +0000

    4694c93 refactor(ivy): avoid `transitiveScopesFor` function duplication in TestBed (#27860)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+83.sha-94c0b7a
+ * @license Angular v7.2.0+84.sha-4694c93
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5702,7 +5702,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+83.sha-94c0b7a');
+var VERSION = new Version('7.2.0+84.sha-4694c93');
 
 /**
  * @license
