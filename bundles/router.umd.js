c5e862c3392ad40014b82bd68b39c60d2cf0aafe
Andrew Kushnir <akushnir@google.com>
2019-01-10 01:59:52 +0000

    5a0deb8 fix(bazel): Add @bazel/bazel to dev deps (#28032)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+84.sha-4694c93
+ * @license Angular v7.2.0+85.sha-5a0deb8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+84.sha-4694c93');
+    var VERSION = new core.Version('7.2.0+85.sha-5a0deb8');
 
     /**
      * @license
