1db54c5f085deb74044b142c1a72ee65a36a0102
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:33:20 +0000

    7374dfd docs: document why not using `compare-url` orb (#28010)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+89.sha-c5ab3e8
+ * @license Angular v7.2.0+90.sha-7374dfd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+89.sha-c5ab3e8');
+    var VERSION = new i0.Version('7.2.0+90.sha-7374dfd');
 
     /**
      * @license
