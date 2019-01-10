419d80abf0c41abf3b6e010cc2ad69697cf90903
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:22:29 +0000

    6003145 fix(ivy): properly rewrite imports in generated factory shims (#27998)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+85.sha-5a0deb8
+ * @license Angular v7.2.0+87.sha-6003145
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+85.sha-5a0deb8');
+    var VERSION = new core.Version('7.2.0+87.sha-6003145');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+85.sha-5a0deb8
+ * @license Angular v7.2.0+87.sha-6003145
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5702,7 +5702,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+85.sha-5a0deb8');
+var VERSION = new Version('7.2.0+87.sha-6003145');
 
 /**
  * @license
