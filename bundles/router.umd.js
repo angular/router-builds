35ccd7eb092d6eb7deb18652bc1f490b5d803440
Andrew Kushnir <akushnir@google.com>
2019-01-10 21:51:28 +0000

    feebe03 fix(ivy): pass ngContentSelectors through to `defineComponent()` calls (#27867)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+98.sha-a6ba789
+ * @license Angular v7.2.0+100.sha-feebe03
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+98.sha-a6ba789');
+    var VERSION = new core.Version('7.2.0+100.sha-feebe03');
 
     /**
      * @license
