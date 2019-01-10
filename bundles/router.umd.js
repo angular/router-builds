7b90892d3c3c59ba46a400cf7c8a4b888c6842fb
Andrew Kushnir <akushnir@google.com>
2019-01-10 21:57:13 +0000

    b05baa5 fix(bazel): Add /bazel-out to .gitignore (#27874)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+100.sha-feebe03
+ * @license Angular v7.2.0+102.sha-b05baa5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+100.sha-feebe03');
+    var VERSION = new core.Version('7.2.0+102.sha-b05baa5');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+100.sha-feebe03
+ * @license Angular v7.2.0+102.sha-b05baa5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
