51b85125fc0a44c628dce70a75294942406fcf57
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:33:58 +0000

    ee60c76 test(ivy): turn on passing i18n test in CI (#28035)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+90.sha-7374dfd
+ * @license Angular v7.2.0+94.sha-ee60c76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+90.sha-7374dfd
+ * @license Angular v7.2.0+94.sha-ee60c76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+90.sha-7374dfd');
+const VERSION = new Version('7.2.0+94.sha-ee60c76');
 
 /**
  * @fileoverview added by tsickle
