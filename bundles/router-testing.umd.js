31eb7021b49abe8c2c9bdaefba91c0d88ae8523e
Andrew Kushnir <akushnir@google.com>
2019-01-10 01:59:36 +0000

    69ca7cd fix(bazel): Add @bazel/bazel to dev deps (#28032)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+74.sha-e5f5ad3
+ * @license Angular v7.2.0-rc.0+75.sha-69ca7cd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+74.sha-e5f5ad3
+ * @license Angular v7.2.0-rc.0+75.sha-69ca7cd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5702,7 +5702,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+74.sha-e5f5ad3');
+var VERSION = new Version('7.2.0-rc.0+75.sha-69ca7cd');
 
 /**
  * @license
