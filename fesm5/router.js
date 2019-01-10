aacdefac66a962dd3df09ef03940a2a5bd357ece
Andrew Kushnir <akushnir@google.com>
2019-01-10 00:18:17 +0000

    8934b73 test(ivy): re-enable passing upgrade tests (#28030)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+80.sha-11325ba
+ * @license Angular v7.2.0+81.sha-8934b73
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+80.sha-11325ba');
+var VERSION = new Version('7.2.0+81.sha-8934b73');
 
 /**
  * @license
