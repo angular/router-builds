bedb3d08dd73d7a077c543014f2d836c0f0b8e16
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:34:44 +0000

    3bafc00 test(ivy): update root cause for @angular/core (#28046)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+94.sha-ee60c76
+ * @license Angular v7.2.0+97.sha-3bafc00
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+94.sha-ee60c76
+ * @license Angular v7.2.0+97.sha-3bafc00
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+94.sha-ee60c76');
+const VERSION = new Version('7.2.0+97.sha-3bafc00');
 
 /**
  * @fileoverview added by tsickle
