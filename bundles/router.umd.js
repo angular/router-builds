ae3af3836c2c56eac4d61b1da7100434acee0056
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:25:22 +0000

    9bda446 fix(bazel): protractor rule does not run spec files with underscore (#28022)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+76.sha-acb69e8
+ * @license Angular v7.2.0-rc.0+77.sha-9bda446
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-rc.0+76.sha-acb69e8');
+    var VERSION = new core.Version('7.2.0-rc.0+77.sha-9bda446');
 
     /**
      * @license
