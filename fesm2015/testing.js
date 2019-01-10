3a9978f96bd981977b550e3744db63c2998846a8
Andrew Kushnir <akushnir@google.com>
2019-01-10 21:55:48 +0000

    d4b476f fix(bazel): Add /bazel-out to .gitignore (#27874)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+78.sha-a64a7a4
+ * @license Angular v7.2.0-rc.0+79.sha-d4b476f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+78.sha-a64a7a4
+ * @license Angular v7.2.0-rc.0+79.sha-d4b476f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5702,7 +5702,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+78.sha-a64a7a4');
+var VERSION = new Version('7.2.0-rc.0+79.sha-d4b476f');
 
 /**
  * @license
