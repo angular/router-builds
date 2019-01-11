820f0e3511e992036c6646de437c3bec9e04120c
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:28:12 +0000

    7bafe18 docs(router): fixed a typo in CanLoad title text (#27894)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+81.sha-9447b85
+ * @license Angular v7.2.0-rc.0+85.sha-7bafe18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-rc.0+81.sha-9447b85');
+    var VERSION = new core.Version('7.2.0-rc.0+85.sha-7bafe18');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+81.sha-9447b85
+ * @license Angular v7.2.0-rc.0+85.sha-7bafe18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
