e82ec210eef0167ce632a3de917f9260e9717024
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:37:45 +0000

    b0f3c20 test(ivy): resolve root cause for core animation tests (#28062)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+109.sha-bccf8da
+ * @license Angular v7.2.0+111.sha-b0f3c20
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+109.sha-bccf8da');
+    var VERSION = new i0.Version('7.2.0+111.sha-b0f3c20');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+109.sha-bccf8da
+ * @license Angular v7.2.0+111.sha-b0f3c20
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+109.sha-bccf8da');
+const VERSION = new Version('7.2.0+111.sha-b0f3c20');
 
 /**
  * @fileoverview added by tsickle
