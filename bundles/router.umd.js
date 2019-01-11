a6add926c0d9dda2bdf26f0cb4dbbb7b52955dbb
Andrew Kushnir <akushnir@google.com>
2019-01-11 19:28:45 +0000

    d505468 docs: re-write interpolation section and add example (#25170)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+112.sha-f854eb7
+ * @license Angular v7.2.0+113.sha-d505468
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+112.sha-f854eb7');
+    var VERSION = new core.Version('7.2.0+113.sha-d505468');
 
     /**
      * @license
