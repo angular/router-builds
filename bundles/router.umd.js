9fe2ce8887ba89eacce996223d383cd13b307cff
Andrew Kushnir <akushnir@google.com>
2019-01-11 18:40:20 +0000

    f854eb7 test(ivy): mark tests where checkNoChanges results are different in the view engine and ivy (#28073)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+111.sha-b0f3c20
+ * @license Angular v7.2.0+112.sha-f854eb7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+111.sha-b0f3c20');
+    var VERSION = new i0.Version('7.2.0+112.sha-f854eb7');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+111.sha-b0f3c20
+ * @license Angular v7.2.0+112.sha-f854eb7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+111.sha-b0f3c20');
+var VERSION = new Version('7.2.0+112.sha-f854eb7');
 
 /**
  * @license
