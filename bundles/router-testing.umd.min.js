91ad07ee75af474079e0b2efa4e2f0db5bf1a9ae
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:26:12 +0000

    9447b85 refactor(core): improve type for EventEmitter (#27228)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+82.sha-c18c46a
+ * @license Angular v7.2.0-rc.0+81.sha-9447b85
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+82.sha-c18c46a
+ * @license Angular v7.2.0-rc.0+81.sha-9447b85
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7262,7 +7262,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+82.sha-c18c46a');
+const VERSION = new Version('7.2.0-rc.0+81.sha-9447b85');
 
 /**
  * @fileoverview added by tsickle
