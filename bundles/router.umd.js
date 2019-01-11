2b9b7e6c85be95389debb5f8da14ef5f4110f8f4
Andrew Kushnir <akushnir@google.com>
2019-01-11 22:45:22 +0000

    b73d678 build: support external angular in ng_rollup_bundle (#28049)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+132.sha-0c6fa1d
+ * @license Angular v7.2.0+133.sha-b73d678
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+132.sha-0c6fa1d');
+    var VERSION = new core.Version('7.2.0+133.sha-b73d678');
 
     /**
      * @license
