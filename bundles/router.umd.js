eaad5ce283105926db4c51ace86f61145fcedab9
Andrew Kushnir <akushnir@google.com>
2019-01-11 19:37:59 +0000

    e31afb7 fix(ivy): ngcc - identify all ESM5 decorated classes (#27848)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+113.sha-d505468
+ * @license Angular v7.2.0+114.sha-e31afb7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+113.sha-d505468');
+    var VERSION = new i0.Version('7.2.0+114.sha-e31afb7');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+113.sha-d505468
+ * @license Angular v7.2.0+114.sha-e31afb7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+113.sha-d505468');
+const VERSION = new Version('7.2.0+114.sha-e31afb7');
 
 /**
  * @fileoverview added by tsickle
