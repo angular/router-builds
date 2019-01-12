3029eec622ebc8b0354fb24df5638fe959ab21e1
Andrew Kushnir <akushnir@google.com>
2019-01-12 00:16:41 +0000

    01d475d fix(bazel): Add ibazel to deps of Bazel project (#28090)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+95.sha-3c08b86
+ * @license Angular v7.2.0-rc.0+96.sha-01d475d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+95.sha-3c08b86
+ * @license Angular v7.2.0-rc.0+96.sha-01d475d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5702,7 +5702,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+95.sha-3c08b86');
+var VERSION = new Version('7.2.0-rc.0+96.sha-01d475d');
 
 /**
  * @license
