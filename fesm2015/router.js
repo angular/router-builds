42f9d6bf37dd4a09b6abb3cec97da6a1bf313a55
Andrew Kushnir <akushnir@google.com>
2019-01-14 18:22:51 +0000

    48a03fc test(docs-infra): increase timeout for all redirection tests (#28103)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+146.sha-6fff74e
+ * @license Angular v7.2.0+147.sha-48a03fc
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+146.sha-6fff74e');
+const VERSION = new Version('7.2.0+147.sha-48a03fc');
 
 /**
  * @fileoverview added by tsickle
