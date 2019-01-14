e3ff4cbfc0880927d1f9a0748c550a0489320230
Andrew Kushnir <akushnir@google.com>
2019-01-14 18:33:11 +0000

    c5b67b9 docs: update link in universal guide (#26628)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+148.sha-78bc21c
+ * @license Angular v7.2.0+149.sha-c5b67b9
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+148.sha-78bc21c');
+    var VERSION = new core.Version('7.2.0+149.sha-c5b67b9');
 
     /**
      * @license
