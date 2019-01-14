01d9955e7aa649725e8ae409b4975c7200059897
Andrew Kushnir <akushnir@google.com>
2019-01-14 19:31:57 +0000

    1ec01d6 docs: change in(what should I import? answer) NgModule FAQs (#27677)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+156.sha-0897eba
+ * @license Angular v7.2.0+158.sha-1ec01d6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+156.sha-0897eba');
+    var VERSION = new i0.Version('7.2.0+158.sha-1ec01d6');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+156.sha-0897eba
+ * @license Angular v7.2.0+158.sha-1ec01d6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
