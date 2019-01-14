079494580ad4b0d3073bd26813bfac557113f246
Andrew Kushnir <akushnir@google.com>
2019-01-14 22:47:13 +0000

    f1fb62d fix(core): proper @internal and @nocollapse combined usage fix (#28138)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+169.sha-d12db4e
+ * @license Angular v7.2.0+170.sha-f1fb62d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+169.sha-d12db4e
+ * @license Angular v7.2.0+170.sha-f1fb62d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+169.sha-d12db4e');
+    var VERSION = new core.Version('7.2.0+170.sha-f1fb62d');
 
     /**
      * @license
