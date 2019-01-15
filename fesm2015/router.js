8ce93b53bf76884904dc4caee4cff046b65a73a8
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:16:32 +0000

    6072ca8 fix(ivy): deps are actually supported (#28076)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+185.sha-9460218
+ * @license Angular v7.2.0+186.sha-6072ca8
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7262,7 +7262,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+185.sha-9460218');
+const VERSION = new Version('7.2.0+186.sha-6072ca8');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+185.sha-9460218
+ * @license Angular v7.2.0+186.sha-6072ca8
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5702,7 +5702,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+185.sha-9460218');
+var VERSION = new Version('7.2.0+186.sha-6072ca8');
 
 /**
  * @license
