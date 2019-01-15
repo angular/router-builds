e1121613e3fe725e26c32bd5719225fff0dc0554
Andrew Kushnir <akushnir@google.com>
2019-01-15 01:21:50 +0000

    cfb4145 docs: update testing doc example style for HostListener (#26372)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+170.sha-f1fb62d
+ * @license Angular v7.2.0+171.sha-cfb4145
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+170.sha-f1fb62d');
+    var VERSION = new core.Version('7.2.0+171.sha-cfb4145');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+170.sha-f1fb62d
+ * @license Angular v7.2.0+171.sha-cfb4145
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
