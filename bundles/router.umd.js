270f8d163f6be4053a4539c6a2ad53cf4efe49be
Andrew Kushnir <akushnir@google.com>
2019-01-15 18:10:32 +0000

    2e5752e docs: update the triage and labels doc with the latest processes (#28127)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+180.sha-da8ee29
+ * @license Angular v7.2.0+181.sha-2e5752e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+180.sha-da8ee29');
+    var VERSION = new i0.Version('7.2.0+181.sha-2e5752e');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+180.sha-da8ee29
+ * @license Angular v7.2.0+181.sha-2e5752e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+180.sha-da8ee29');
+const VERSION = new Version('7.2.0+181.sha-2e5752e');
 
 /**
  * @fileoverview added by tsickle
