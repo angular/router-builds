f6c20997139381cae6cb2faeb4391ed5e291ab97
Andrew Kushnir <akushnir@google.com>
2019-01-15 18:11:27 +0000

    a241200 docs(core): fix typo (#28042) PR Close #28042

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+182.sha-d49d1e7
+ * @license Angular v7.2.0+183.sha-a241200
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+182.sha-d49d1e7
+ * @license Angular v7.2.0+183.sha-a241200
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+182.sha-d49d1e7');
+    var VERSION = new core.Version('7.2.0+183.sha-a241200');
 
     /**
      * @license
