79cc9022599a1d346b75218dd72e73b57d0b731f
Andrew Kushnir <akushnir@google.com>
2019-01-15 18:10:40 +0000

    d49d1e7 fix(service-worker): navigation urls backwards compatibility (#27244)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+181.sha-2e5752e
+ * @license Angular v7.2.0+182.sha-d49d1e7
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7262,7 +7262,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+181.sha-2e5752e');
+const VERSION = new Version('7.2.0+182.sha-d49d1e7');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+181.sha-2e5752e
+ * @license Angular v7.2.0+182.sha-d49d1e7
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5702,7 +5702,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+181.sha-2e5752e');
+var VERSION = new Version('7.2.0+182.sha-d49d1e7');
 
 /**
  * @license
