2eabdb3a40a8ac28de82d1817360c21f0199a3d1
Andrew Kushnir <akushnir@google.com>
2019-01-15 01:33:24 +0000

    fa53150 test(ivy): NodeInjector should know how to get itself (INJECTOR) (#28009)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+173.sha-b5c2ef2
+ * @license Angular v7.2.0+174.sha-fa53150
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+173.sha-b5c2ef2
+ * @license Angular v7.2.0+174.sha-fa53150
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7262,7 +7262,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+173.sha-b5c2ef2');
+const VERSION = new Version('7.2.0+174.sha-fa53150');
 
 /**
  * @fileoverview added by tsickle
