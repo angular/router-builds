2913b2b3ea413d03bbfca5569d5f4c1d11f4d5d9
Andrew Kushnir <akushnir@google.com>
2019-01-15 01:35:37 +0000

    06e5bf1 fix(bazel): Bazel schematics should add router package (#28141)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+176.sha-60fecc1
+ * @license Angular v7.2.0+177.sha-06e5bf1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7262,7 +7262,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+176.sha-60fecc1');
+const VERSION = new Version('7.2.0+177.sha-06e5bf1');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+176.sha-60fecc1
+ * @license Angular v7.2.0+177.sha-06e5bf1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
