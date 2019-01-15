b438870aceaf08855299f0faf478c16b6c2aed8b
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:36:15 +0000

    bc02e31 fix(ivy): normalize summary and factory shim files paths (#28006)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+186.sha-6072ca8
+ * @license Angular v7.2.0+187.sha-bc02e31
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+186.sha-6072ca8');
+    var VERSION = new core.Version('7.2.0+187.sha-bc02e31');
 
     /**
      * @license
