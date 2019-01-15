fe77f6d4f5a33fbf4a27e6df3e9fa0852fed148c
Andrew Kushnir <akushnir@google.com>
2019-01-15 18:11:56 +0000

    d49d1e7 fix(service-worker): navigation urls backwards compatibility (#27244)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+181.sha-2e5752e
+ * @license Angular v7.2.0+182.sha-d49d1e7
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+181.sha-2e5752e
+ * @license Angular v7.2.0+182.sha-d49d1e7
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+181.sha-2e5752e');
+    var VERSION = new i0.Version('7.2.0+182.sha-d49d1e7');
 
     /**
      * @license
