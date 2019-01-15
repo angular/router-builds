9d8b39da39be2430768f493aada9cffc718064b8
Andrew Kushnir <akushnir@google.com>
2019-01-15 01:35:52 +0000

    60fecc1 fixup! refactor(ivy): simplify context discover based on feedback (#28101)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+174.sha-fa53150
+ * @license Angular v7.2.0+176.sha-60fecc1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+174.sha-fa53150');
+    var VERSION = new i0.Version('7.2.0+176.sha-60fecc1');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+174.sha-fa53150
+ * @license Angular v7.2.0+176.sha-60fecc1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+174.sha-fa53150');
+const VERSION = new Version('7.2.0+176.sha-60fecc1');
 
 /**
  * @fileoverview added by tsickle
