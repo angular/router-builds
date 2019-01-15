a682076c67088a3528904f5cdaaa411ad2490b1e
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:37:39 +0000

    bc02e31 fix(ivy): normalize summary and factory shim files paths (#28006)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+186.sha-6072ca8
+ * @license Angular v7.2.0+187.sha-bc02e31
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+186.sha-6072ca8');
+const VERSION = new Version('7.2.0+187.sha-bc02e31');
 
 /**
  * @fileoverview added by tsickle
