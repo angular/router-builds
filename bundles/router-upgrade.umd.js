d2814dcab378f24bbd5a02623a0faa7647bfb848
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:11:52 +0000

    918d906 docs(router): add clarification for Router config (#28159)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+124.sha-9faad4b
+ * @license Angular v7.2.0-rc.0+126.sha-918d906
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+124.sha-9faad4b
+ * @license Angular v7.2.0-rc.0+126.sha-918d906
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7268,7 +7268,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+124.sha-9faad4b');
+const VERSION = new Version('7.2.0-rc.0+126.sha-918d906');
 
 /**
  * @fileoverview added by tsickle
