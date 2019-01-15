fdad83a2548245e2e5fa3d4194b06e194e7d902a
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:47:26 +0000

    3a31a27 refactor(language-service): Cleanup ts_plugin (#28145)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+188.sha-ce3a746
+ * @license Angular v7.2.0+189.sha-3a31a27
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+188.sha-ce3a746
+ * @license Angular v7.2.0+189.sha-3a31a27
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+188.sha-ce3a746');
+var VERSION = new Version('7.2.0+189.sha-3a31a27');
 
 /**
  * @license
