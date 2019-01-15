9cc0f52e6c6f6881c5c32a1570e4123e1d8579cb
Andrew Kushnir <akushnir@google.com>
2019-01-15 18:03:33 +0000

    e62eeed fix(ivy): ensure component/directive `class` selectors are properly understood (#27849)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+177.sha-06e5bf1
+ * @license Angular v7.2.0+178.sha-e62eeed
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+177.sha-06e5bf1');
+const VERSION = new Version('7.2.0+178.sha-e62eeed');
 
 /**
  * @fileoverview added by tsickle
