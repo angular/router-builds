96c9052488f89c8eb2f1a20975073ac076bf8f75
Andrew Kushnir <akushnir@google.com>
2019-01-15 18:08:40 +0000

    da8ee29 fix(ivy): throw meaningful error for uninitialized output (#28085)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+179.sha-6930451
+ * @license Angular v7.2.0+180.sha-da8ee29
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+179.sha-6930451');
+    var VERSION = new core.Version('7.2.0+180.sha-da8ee29');
 
     /**
      * @license
