01b2a56438da5b4c9fda107e2932811bf8c4e5b6
Andrew Kushnir <akushnir@google.com>
2019-01-16 21:51:38 +0000

    0516839 release: cut the v7.2.1 release

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+98.sha-03293c4
+ * @license Angular v7.2.1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+98.sha-03293c4
+ * @license Angular v7.2.1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7268,7 +7268,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+98.sha-03293c4');
+const VERSION = new Version('7.2.1');
 
 /**
  * @fileoverview added by tsickle
