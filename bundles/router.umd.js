c53cc59e3c04b3d464e28e1673099020a526a281
Andrew Kushnir <akushnir@google.com>
2019-01-16 18:13:43 +0000

    9a81f0d fix(ivy): update i18n/i18nStart and i18nAttributes instruction order (#28163)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+193.sha-3b7a571
+ * @license Angular v7.2.0+196.sha-9a81f0d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+193.sha-3b7a571');
+    var VERSION = new i0.Version('7.2.0+196.sha-9a81f0d');
 
     /**
      * @license
