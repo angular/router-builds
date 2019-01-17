1e48243f4884798452913522424b60e736ad07ff
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 18:19:05 +0000

    5a582a8 fix(ivy): ensure element removal triggers host removal animations (#28162)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+9.sha-896cf35
+ * @license Angular v8.0.0-beta.0+12.sha-5a582a8
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.0+9.sha-896cf35');
+    var VERSION = new core.Version('8.0.0-beta.0+12.sha-5a582a8');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+9.sha-896cf35
+ * @license Angular v8.0.0-beta.0+12.sha-5a582a8
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7262,7 +7262,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.0+9.sha-896cf35');
+const VERSION = new Version('8.0.0-beta.0+12.sha-5a582a8');
 
 /**
  * @fileoverview added by tsickle
