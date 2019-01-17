2adaba9ce9fbf632b5774b47ba8d9c638fc05637
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 02:02:36 +0000

    50cf2ac fix(docs-infra): improve a11y (#28086)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+1.sha-a570fdf
+ * @license Angular v8.0.0-beta.0+6.sha-50cf2ac
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+1.sha-a570fdf');
+var VERSION = new Version('8.0.0-beta.0+6.sha-50cf2ac');
 
 /**
  * @license
