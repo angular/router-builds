ad367c927eb86042d64c3a71cb4cb9be3604bfe3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 18:18:45 +0000

    1f7d3b9 fix(ivy): TestBed should use annotation for the last match rather than the first (#28195)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+7.sha-8a08ff1
+ * @license Angular v8.0.0-beta.0+8.sha-1f7d3b9
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+7.sha-8a08ff1
+ * @license Angular v8.0.0-beta.0+8.sha-1f7d3b9
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.0+7.sha-8a08ff1');
+    var VERSION = new i0.Version('8.0.0-beta.0+8.sha-1f7d3b9');
 
     /**
      * @license
