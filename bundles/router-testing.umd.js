7f9554776e4b0071506baf1ad5821467e00982fb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 02:01:21 +0000

    50cf2ac fix(docs-infra): improve a11y (#28086)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+1.sha-a570fdf
+ * @license Angular v8.0.0-beta.0+6.sha-50cf2ac
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+1.sha-a570fdf
+ * @license Angular v8.0.0-beta.0+6.sha-50cf2ac
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7262,7 +7262,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.0+1.sha-a570fdf');
+const VERSION = new Version('8.0.0-beta.0+6.sha-50cf2ac');
 
 /**
  * @fileoverview added by tsickle
