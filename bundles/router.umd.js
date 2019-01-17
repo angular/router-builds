ebe5e301cb021fcbde8f73daaca0ad6e92929ac4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 18:18:42 +0000

    896cf35 fix(ivy): ensure renderer begin/end methods are only called during change detection (#28192)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+8.sha-1f7d3b9
+ * @license Angular v8.0.0-beta.0+9.sha-896cf35
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.0+8.sha-1f7d3b9');
+    var VERSION = new core.Version('8.0.0-beta.0+9.sha-896cf35');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+8.sha-1f7d3b9
+ * @license Angular v8.0.0-beta.0+9.sha-896cf35
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
