f06ededdc5779f2ad2afb615c4a6e532419ae69e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 18:19:16 +0000

    e172e97 fix(ivy): ensure animation @trigger ordering is correctly delivered to the renderer (#28165)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+12.sha-5a582a8
+ * @license Angular v8.0.0-beta.0+11.sha-e172e97
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.0+12.sha-5a582a8');
+    var VERSION = new core.Version('8.0.0-beta.0+11.sha-e172e97');
 
     /**
      * @license
