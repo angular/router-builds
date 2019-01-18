656a12aa4b27a48f2ea2bb7da4131af5c32f6db5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 17:24:24 +0000

    c1c8746 build(bazel): Use local rollup & build-optimizer for ng_rollup_bundle (#28215)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+14.sha-a0cdefb
+ * @license Angular v8.0.0-beta.0+16.sha-c1c8746
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.0+14.sha-a0cdefb');
+    var VERSION = new core.Version('8.0.0-beta.0+16.sha-c1c8746');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+14.sha-a0cdefb
+ * @license Angular v8.0.0-beta.0+16.sha-c1c8746
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
