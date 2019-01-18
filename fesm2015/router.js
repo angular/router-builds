e08b55f55cf19bab7227ab6cb90d3c4665814ef2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 18:36:43 +0000

    38343a2 build: set a default module_name for ts_library rules (#28051)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+21.sha-45bf911
+ * @license Angular v8.0.0-beta.0+22.sha-38343a2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.0+21.sha-45bf911');
+const VERSION = new Version('8.0.0-beta.0+22.sha-38343a2');
 
 /**
  * @fileoverview added by tsickle
