316f8ebdb93d82c837169f1f06fc8d57c191f17b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 18:28:09 +0000

    ab2bf83 fix(ivy): destroy injector when module is destroyed (#27793)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+19.sha-2b9cc85
+ * @license Angular v8.0.0-beta.0+20.sha-ab2bf83
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.0+19.sha-2b9cc85');
+    var VERSION = new i0.Version('8.0.0-beta.0+20.sha-ab2bf83');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+19.sha-2b9cc85
+ * @license Angular v8.0.0-beta.0+20.sha-ab2bf83
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.0+19.sha-2b9cc85');
+const VERSION = new Version('8.0.0-beta.0+20.sha-ab2bf83');
 
 /**
  * @fileoverview added by tsickle
