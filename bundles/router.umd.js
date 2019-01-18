a3b9a8beb6c9f86d1214bfa1b618519a1e3278e9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 19:58:23 +0000

    45bf911 feat(forms): add `markAllAsTouched()` to `AbstractControl` (#26812)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+3.sha-808898d
+ * @license Angular v8.0.0-beta.0+21.sha-45bf911
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.0+3.sha-808898d');
+    var VERSION = new core.Version('8.0.0-beta.0+21.sha-45bf911');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+3.sha-808898d
+ * @license Angular v8.0.0-beta.0+21.sha-45bf911
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5702,7 +5702,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+3.sha-808898d');
+var VERSION = new Version('8.0.0-beta.0+21.sha-45bf911');
 
 /**
  * @license
