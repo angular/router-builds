6ff39f8765d9acea0bf79ca3d7c7bd4a27cd3462
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 19:18:35 +0000

    73dcd72 refactor(ivy): expose resolving URLs in the `ResourceLoader` (#28199)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+22.sha-38343a2
+ * @license Angular v8.0.0-beta.0+24.sha-73dcd72
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.0+22.sha-38343a2');
+    var VERSION = new core.Version('8.0.0-beta.0+24.sha-73dcd72');
 
     /**
      * @license
