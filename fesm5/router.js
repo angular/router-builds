b91049bcf4fffbbb5e0932ae4e89d36ce662f1e7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 17:25:39 +0000

    c1c8746 build(bazel): Use local rollup & build-optimizer for ng_rollup_bundle (#28215)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+14.sha-a0cdefb
+ * @license Angular v8.0.0-beta.0+16.sha-c1c8746
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+14.sha-a0cdefb');
+var VERSION = new Version('8.0.0-beta.0+16.sha-c1c8746');
 
 /**
  * @license
