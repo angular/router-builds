963e14cd9c6788b54bfbe39bdb563243b07812d5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 18:04:54 +0000

    4c00059 feat(compiler-cli): resolve generated Sass/Less files to .css inputs (#28166)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+7.sha-93d78c9
+ * @license Angular v7.2.1+8.sha-4c00059
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+7.sha-93d78c9
+ * @license Angular v7.2.1+8.sha-4c00059
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5796,7 +5796,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.1+7.sha-93d78c9');
+    var VERSION = new core.Version('7.2.1+8.sha-4c00059');
 
     /**
      * @license
