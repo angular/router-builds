8b28cbd374ebf10e67e0f060cb82a79a2b3f4c5b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 19:59:38 +0000

    d336bff fix(ivy): add missging getDirectives export (#28259)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+41.sha-f99082f
+ * @license Angular v8.0.0-beta.0+42.sha-d336bff
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5702,7 +5702,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+41.sha-f99082f');
+var VERSION = new Version('8.0.0-beta.0+42.sha-d336bff');
 
 /**
  * @license
