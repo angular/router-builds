4c4fef9a429d0d6a559c05cb3a3769151e061212
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 18:36:56 +0000

    7439b46 docs: fix explanation of Injectable decorator in architecture guide (#27480)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+34.sha-9a965c9
+ * @license Angular v8.0.0-beta.0+35.sha-7439b46
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.0+34.sha-9a965c9');
+    var VERSION = new i0.Version('8.0.0-beta.0+35.sha-7439b46');
 
     /**
      * @license
