21562f5b4e3f5873675c9899ff61157955d191f6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 19:55:36 +0000

    f99082f build: hide vscode settings e.g. debug launch config (#28299)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+39.sha-197676a
+ * @license Angular v8.0.0-beta.0+41.sha-f99082f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+39.sha-197676a');
+var VERSION = new Version('8.0.0-beta.0+41.sha-f99082f');
 
 /**
  * @license
