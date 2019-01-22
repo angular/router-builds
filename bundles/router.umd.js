3d39f491eae2d74a23315d84086a53676ad8fa49
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 20:38:47 +0000

    18a9afc docs(core): deprecate ViewEncapsulation.Native (#26361)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+59.sha-9882434
+ * @license Angular v8.0.0-beta.0+62.sha-18a9afc
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.0+59.sha-9882434');
+    var VERSION = new i0.Version('8.0.0-beta.0+62.sha-18a9afc');
 
     /**
      * @license
