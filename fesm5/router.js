43e20a33f16d1d4692dd0f36dbaaa4bd6bb2b979
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 18:38:17 +0000

    6324ad4 docs: correct minor typo (#27382)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+35.sha-7439b46
+ * @license Angular v8.0.0-beta.0+36.sha-6324ad4
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+35.sha-7439b46');
+var VERSION = new Version('8.0.0-beta.0+36.sha-6324ad4');
 
 /**
  * @license
