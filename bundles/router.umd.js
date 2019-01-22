0b38ebdad91e10f0a252e41d28547cea6c79820b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 18:14:36 +0000

    058aafc fix(ivy): fix styling context resolution for host bindings on containers (#28221)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+29.sha-6bd20e8
+ * @license Angular v8.0.0-beta.0+31.sha-058aafc
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.0+29.sha-6bd20e8');
+    var VERSION = new core.Version('8.0.0-beta.0+31.sha-058aafc');
 
     /**
      * @license
