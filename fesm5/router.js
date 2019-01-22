afc56fb6ffa30875f42a6b56403e356320f505b6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 20:43:18 +0000

    ea1b5c1 fix(ivy): not applying camelCased style properties (#28276)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+62.sha-18a9afc
+ * @license Angular v8.0.0-beta.0+63.sha-ea1b5c1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5702,7 +5702,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+62.sha-18a9afc');
+var VERSION = new Version('8.0.0-beta.0+63.sha-ea1b5c1');
 
 /**
  * @license
