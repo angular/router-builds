dc0a11331777c99c3755524ffac13fe7af8afedd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 18:11:31 +0000

    6bd20e8 docs: add missing type when handling errors in hero service. (#28256)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+28.sha-366a6bf
+ * @license Angular v8.0.0-beta.0+29.sha-6bd20e8
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.0+28.sha-366a6bf');
+    var VERSION = new core.Version('8.0.0-beta.0+29.sha-6bd20e8');
 
     /**
      * @license
