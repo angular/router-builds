3077ca91bea8d3994a9bcde041359401abd16aad
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 19:35:40 +0000

    1df8be5 docs: update link to post on redirects in router guide (#27256)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+36.sha-6324ad4
+ * @license Angular v8.0.0-beta.0+37.sha-1df8be5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.0+36.sha-6324ad4');
+    var VERSION = new core.Version('8.0.0-beta.0+37.sha-1df8be5');
 
     /**
      * @license
