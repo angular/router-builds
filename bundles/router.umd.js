f93103d716f35b230099466539e4f07e42c5ae4d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 18:07:48 +0000

    366a6bf docs: make the styles section less ambiguous (#26092) PR Close #26092

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+27.sha-5b08a88
+ * @license Angular v8.0.0-beta.0+28.sha-366a6bf
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.0+27.sha-5b08a88');
+    var VERSION = new core.Version('8.0.0-beta.0+28.sha-366a6bf');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+27.sha-5b08a88
+ * @license Angular v8.0.0-beta.0+28.sha-366a6bf
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
