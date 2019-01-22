f38531712513a69c6610b09f8905aed3d8fc0872
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 17:44:22 +0000

    9dabeea ci: add a rebase check to the merge-pr script (#28250)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+24.sha-73dcd72
+ * @license Angular v8.0.0-beta.0+25.sha-9dabeea
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7492,7 +7492,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.0+24.sha-73dcd72');
+const VERSION = new Version('8.0.0-beta.0+25.sha-9dabeea');
 
 /**
  * @fileoverview added by tsickle
