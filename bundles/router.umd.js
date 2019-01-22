93d1d920ee178d571c698a6aaaaf16f5ce26ce3f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 20:36:43 +0000

    37f8263 docs: correct array to map (#27379)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+51.sha-e0fbe86
+ * @license Angular v8.0.0-beta.0+54.sha-37f8263
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5790,7 +5790,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.0+51.sha-e0fbe86');
+    var VERSION = new core.Version('8.0.0-beta.0+54.sha-37f8263');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+51.sha-e0fbe86
+ * @license Angular v8.0.0-beta.0+54.sha-37f8263
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
