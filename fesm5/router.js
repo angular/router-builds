5de84ceae2cb4654bf2fdf9b5120a82c524cfe97
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 20:29:35 +0000

    e0fbe86 docs: add api doc to template_ref (#27380)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+50.sha-94e305f
+ * @license Angular v8.0.0-beta.0+51.sha-e0fbe86
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+50.sha-94e305f');
+var VERSION = new Version('8.0.0-beta.0+51.sha-e0fbe86');
 
 /**
  * @license
