3245d508ba906472d7e378a3ea90b19cd99906ad
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 20:28:18 +0000

    e0fbe86 docs: add api doc to template_ref (#27380)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+50.sha-94e305f
+ * @license Angular v8.0.0-beta.0+51.sha-e0fbe86
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+50.sha-94e305f
+ * @license Angular v8.0.0-beta.0+51.sha-e0fbe86
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7262,7 +7262,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.0+50.sha-94e305f');
+const VERSION = new Version('8.0.0-beta.0+51.sha-e0fbe86');
 
 /**
  * @fileoverview added by tsickle
