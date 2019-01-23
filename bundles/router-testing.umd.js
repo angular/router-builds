028eb63ae2aa167eb74802eda63dbb1d6bbcd2b8
Jason Aden <jasonaden@google.com>
2019-01-23 23:24:16 +0000

    cbd6264 fix(ivy): link correct ngModule's injector to the bootstrapped component (#28183)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+17.sha-317cc92
+ * @license Angular v8.0.0-beta.1+18.sha-cbd6264
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+17.sha-317cc92
+ * @license Angular v8.0.0-beta.1+18.sha-cbd6264
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7502,7 +7502,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.1+17.sha-317cc92');
+const VERSION = new Version('8.0.0-beta.1+18.sha-cbd6264');
 
 /**
  * @fileoverview added by tsickle
