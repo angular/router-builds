6d7343207059e4afd7676a83f786331d4bcd32f5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 19:26:12 +0000

    1fd6735 build: update and unify gitignore (#28316)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+6.sha-583061d
+ * @license Angular v8.0.0-beta.1+1.sha-1fd6735
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5800,7 +5800,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.1+6.sha-583061d');
+    var VERSION = new core.Version('8.0.0-beta.1+1.sha-1fd6735');
 
     /**
      * @license
