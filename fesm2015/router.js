ab46df8725724c973ffe86ae64b88ad94287e9c3
Jason Aden <jasonaden@google.com>
2019-01-23 23:23:48 +0000

    317cc92 test(ivy): turn on passing tests in platform-browser-dynamic (#28307)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+15.sha-32c61f4
+ * @license Angular v8.0.0-beta.1+17.sha-317cc92
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7502,7 +7502,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.1+15.sha-32c61f4');
+const VERSION = new Version('8.0.0-beta.1+17.sha-317cc92');
 
 /**
  * @fileoverview added by tsickle
