3986937ba35d6fa037ff2ec72eb24a7592290d96
Jason Aden <jasonaden@google.com>
2019-01-25 08:21:05 +0000

    e2c98fb fix(docs-infra): change the key used to find out the cli section (#28293)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+22.sha-465abab
+ * @license Angular v8.0.0-beta.1+28.sha-e2c98fb
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5725,7 +5725,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.1+22.sha-465abab');
+var VERSION = new Version('8.0.0-beta.1+28.sha-e2c98fb');
 
 /**
  * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+22.sha-465abab
+ * @license Angular v8.0.0-beta.1+28.sha-e2c98fb
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
