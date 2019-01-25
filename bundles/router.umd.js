c18a13b17831f1dca953e11912e3a7810357d748
Jason Aden <jasonaden@google.com>
2019-01-25 21:27:57 +0000

    3d5a919 refactor(ivy): clean up TNode not depending on LView (#28354)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+41.sha-2da82db
+ * @license Angular v8.0.0-beta.1+43.sha-3d5a919
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5798,7 +5798,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.1+41.sha-2da82db');
+    var VERSION = new i0.Version('8.0.0-beta.1+43.sha-3d5a919');
 
     /**
      * @license
