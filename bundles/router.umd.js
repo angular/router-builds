7c9444d34178e9d9f8ebe2a31f0698899c1c9c63
Jason Aden <jasonaden@google.com>
2019-01-25 21:25:56 +0000

    7496630 test(docs-infra): increase timeout for redirection tests more (#28290)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+29.sha-b2811e5
+ * @license Angular v8.0.0-beta.1+39.sha-7496630
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5798,7 +5798,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.1+29.sha-b2811e5');
+    var VERSION = new i0.Version('8.0.0-beta.1+39.sha-7496630');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+29.sha-b2811e5
+ * @license Angular v8.0.0-beta.1+39.sha-7496630
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5725,7 +5725,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.1+29.sha-b2811e5');
+var VERSION = new Version('8.0.0-beta.1+39.sha-7496630');
 
 /**
  * @license
