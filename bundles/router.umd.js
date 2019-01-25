7a82b60545e233e045aced9f3daf360281f5581c
Jason Aden <jasonaden@google.com>
2019-01-25 21:28:12 +0000

    fdc2b0b fix(ivy): queries should register matches from top to bottom (#28319)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+43.sha-3d5a919
+ * @license Angular v8.0.0-beta.1+35.sha-fdc2b0b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5798,7 +5798,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.1+43.sha-3d5a919');
+    var VERSION = new i0.Version('8.0.0-beta.1+35.sha-fdc2b0b');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+43.sha-3d5a919
+ * @license Angular v8.0.0-beta.1+35.sha-fdc2b0b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7502,7 +7502,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.1+43.sha-3d5a919');
+const VERSION = new Version('8.0.0-beta.1+35.sha-fdc2b0b');
 
 /**
  * @fileoverview added by tsickle
