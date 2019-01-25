b8cfb6ad40e783cd9791b9fb07ca25f6312bd7d9
Jason Aden <jasonaden@google.com>
2019-01-25 21:14:19 +0000

    b2811e5 refactor(router): initalize browserUrlTree to empty tree (#28376)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+28.sha-e2c98fb
+ * @license Angular v8.0.0-beta.1+29.sha-b2811e5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+28.sha-e2c98fb
+ * @license Angular v8.0.0-beta.1+29.sha-b2811e5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4036,7 +4036,7 @@ var Router = /** @class */ (function () {
         this.resetConfig(config);
         this.currentUrlTree = createEmptyUrlTree();
         this.rawUrlTree = this.currentUrlTree;
-        this.browserUrlTree = this.parseUrl(this.location.path());
+        this.browserUrlTree = this.currentUrlTree;
         this.configLoader = new RouterConfigLoader(loader, compiler, onLoadStart, onLoadEnd);
         this.routerState = createEmptyState(this.currentUrlTree, this.rootComponentType);
         this.transitions = new BehaviorSubject({
@@ -5725,7 +5725,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.1+28.sha-e2c98fb');
+var VERSION = new Version('8.0.0-beta.1+29.sha-b2811e5');
 
 /**
  * @license
