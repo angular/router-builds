70eeb77d0680e61903a10b0a216a17522080b71e
Jason Aden <jasonaden@google.com>
2019-01-30 01:25:41 +0000

    ad49962 docs: fix typo for Browserslist in build guide (#28328)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+95.sha-41e68f7
+ * @license Angular v8.0.0-beta.1+100.sha-ad49962
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5799,7 +5799,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.1+95.sha-41e68f7');
+    var VERSION = new i0.Version('8.0.0-beta.1+100.sha-ad49962');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+95.sha-41e68f7
+ * @license Angular v8.0.0-beta.1+100.sha-ad49962
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7503,7 +7503,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.1+95.sha-41e68f7');
+const VERSION = new Version('8.0.0-beta.1+100.sha-ad49962');
 
 /**
  * @fileoverview added by tsickle
