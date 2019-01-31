f40b75a407c0cb538c5dd09ba51179d8ea8cf7ad
Matias Niemelä <matias@yearofmoo.com>
2019-01-31 02:01:58 +0000

    35e45dc fix(ivy): prevent errors from views being destroyed twice (#28413)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+109.sha-a227c52
+ * @license Angular v8.0.0-beta.2+3.sha-35e45dc
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5800,7 +5800,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.1+109.sha-a227c52');
+    var VERSION = new core.Version('8.0.0-beta.2+3.sha-35e45dc');
 
     /**
      * @license
