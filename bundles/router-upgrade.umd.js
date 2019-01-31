ba15ec1d612d371dbaf0baf960bfaaabd8056930
Matias Niemelä <matias@yearofmoo.com>
2019-01-31 17:49:22 +0000

    c0b3835 ci: fix symbol test to handle duplicate symbols (#28459)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+3.sha-35e45dc
+ * @license Angular v8.0.0-beta.2+4.sha-c0b3835
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+3.sha-35e45dc
+ * @license Angular v8.0.0-beta.2+4.sha-c0b3835
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5799,7 +5799,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.2+3.sha-35e45dc');
+    var VERSION = new i0.Version('8.0.0-beta.2+4.sha-c0b3835');
 
     /**
      * @license
