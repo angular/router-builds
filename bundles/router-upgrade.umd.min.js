09b40188540907cbb1ec1efab59689abbd5e3f96
Matias Niemelä <matias@yearofmoo.com>
2019-02-01 19:12:24 +0000

    ec6e730 perf: pngcrush all pngs (#28479)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+8.sha-1b6d8a7
+ * @license Angular v8.0.0-beta.2+10.sha-ec6e730
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+8.sha-1b6d8a7
+ * @license Angular v8.0.0-beta.2+10.sha-ec6e730
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7272,7 +7272,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.2+8.sha-1b6d8a7');
+const VERSION = new Version('8.0.0-beta.2+10.sha-ec6e730');
 
 /**
  * @fileoverview added by tsickle
