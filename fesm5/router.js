94c98769ba05978a721a38c384f9049e567ef91f
Matias Niemelä <matias@yearofmoo.com>
2019-02-01 19:13:38 +0000

    ec6e730 perf: pngcrush all pngs (#28479)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+8.sha-1b6d8a7
+ * @license Angular v8.0.0-beta.2+10.sha-ec6e730
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5726,7 +5726,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.2+8.sha-1b6d8a7');
+var VERSION = new Version('8.0.0-beta.2+10.sha-ec6e730');
 
 /**
  * @license
