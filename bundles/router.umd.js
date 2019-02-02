1cd17a1d9c9ca4e96222736806e52e88ae2a613a
Matias Niemelä <matias@yearofmoo.com>
2019-02-02 01:39:39 +0000

    72c3695 ci: pin ChromeDriver to a version compatible with docker image's Chrome (#28494)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+12.sha-8930f60
+ * @license Angular v8.0.0-beta.2+17.sha-72c3695
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5799,7 +5799,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.2+12.sha-8930f60');
+    var VERSION = new i0.Version('8.0.0-beta.2+17.sha-72c3695');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+12.sha-8930f60
+ * @license Angular v8.0.0-beta.2+17.sha-72c3695
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7503,7 +7503,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.2+12.sha-8930f60');
+const VERSION = new Version('8.0.0-beta.2+17.sha-72c3695');
 
 /**
  * @fileoverview added by tsickle
