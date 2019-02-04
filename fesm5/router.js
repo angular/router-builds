0b4e7ee342709ae1992a6628d849e69a64c91509
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 20:17:28 +0000

    a98d660 docs(bazel): Explain explicit version requirement (#28482)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+22.sha-52d3795
+ * @license Angular v8.0.0-beta.2+25.sha-a98d660
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5712,7 +5712,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.2+22.sha-52d3795');
+var VERSION = new Version('8.0.0-beta.2+25.sha-a98d660');
 
 /**
  * @license
