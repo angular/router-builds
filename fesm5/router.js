916dcbf1b8a00739f1ae7df76d56ac7700067851
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 22:57:10 +0000

    7219639 fix(compiler-cli): base synthetic filepaths on input filepath (#28453)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+35.sha-f2621db
+ * @license Angular v8.0.0-beta.2+36.sha-7219639
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5712,7 +5712,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.2+35.sha-f2621db');
+var VERSION = new Version('8.0.0-beta.2+36.sha-7219639');
 
 /**
  * @license
