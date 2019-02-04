e00147a7a2b523c0ffaa416d9cd2bd4ee550663b
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 22:08:55 +0000

    463a789 build: `gulp format` only changed lines by default (#28411)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+26.sha-fc88a79
+ * @license Angular v8.0.0-beta.2+27.sha-463a789
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5726,7 +5726,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.2+26.sha-fc88a79');
+var VERSION = new Version('8.0.0-beta.2+27.sha-463a789');
 
 /**
  * @license
