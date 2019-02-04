202de2e91f1d1d167e5392599331cfbf7613a59c
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 22:10:26 +0000

    f2621db fix(core): remove createInjector() from public API (#28509)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+27.sha-463a789
+ * @license Angular v8.0.0-beta.2+35.sha-f2621db
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7272,7 +7272,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.2+27.sha-463a789');
+const VERSION = new Version('8.0.0-beta.2+35.sha-f2621db');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+27.sha-463a789
+ * @license Angular v8.0.0-beta.2+35.sha-f2621db
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5712,7 +5712,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.2+27.sha-463a789');
+var VERSION = new Version('8.0.0-beta.2+35.sha-f2621db');
 
 /**
  * @license
