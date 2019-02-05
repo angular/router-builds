c45a0600c45ce3fed0f9397667604c207bf26339
Matias Niemelä <matias@yearofmoo.com>
2019-02-05 18:23:38 +0000

    07fb4b5 build: fix bazel repositories not cached on circleci (#28515)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+36.sha-7219639
+ * @license Angular v8.0.0-beta.2+38.sha-07fb4b5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+36.sha-7219639
+ * @license Angular v8.0.0-beta.2+38.sha-07fb4b5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5726,7 +5726,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.2+36.sha-7219639');
+var VERSION = new Version('8.0.0-beta.2+38.sha-07fb4b5');
 
 /**
  * @license
