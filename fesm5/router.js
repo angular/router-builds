f39c284950d1cf21ed4f862b2856cc6d043cdaea
Matias Niemelä <matias@yearofmoo.com>
2019-02-05 19:55:54 +0000

    744b020 build: fix failing compiler-cli tests on windows (#28352)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+39.sha-9ef8d2b
+ * @license Angular v8.0.0-beta.2+48.sha-744b020
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5726,7 +5726,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.2+39.sha-9ef8d2b');
+var VERSION = new Version('8.0.0-beta.2+48.sha-744b020');
 
 /**
  * @license
