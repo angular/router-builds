107076d4d867827c9273d1e5cea7ad5a641c4448
Matias Niemelä <matias@yearofmoo.com>
2019-02-05 19:54:36 +0000

    744b020 build: fix failing compiler-cli tests on windows (#28352)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+39.sha-9ef8d2b
+ * @license Angular v8.0.0-beta.2+48.sha-744b020
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5800,7 +5800,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.2+39.sha-9ef8d2b');
+    var VERSION = new core.Version('8.0.0-beta.2+48.sha-744b020');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+39.sha-9ef8d2b
+ * @license Angular v8.0.0-beta.2+48.sha-744b020
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
