0c520069efd37092ca48a6a88d366ccad4365136
Matias Niemelä <matias@yearofmoo.com>
2019-02-05 18:22:18 +0000

    07fb4b5 build: fix bazel repositories not cached on circleci (#28515)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+36.sha-7219639
+ * @license Angular v8.0.0-beta.2+38.sha-07fb4b5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5800,7 +5800,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.2+36.sha-7219639');
+    var VERSION = new core.Version('8.0.0-beta.2+38.sha-07fb4b5');
 
     /**
      * @license
