002a41becd32f890f88ed6758067681042ee56e4
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 04:51:23 +0000

    3f73dfa fix(ivy): sanitize external i18n ids before generating const names (#28522)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+2.sha-5a2c3ff
+ * @license Angular v8.0.0-beta.3+5.sha-3f73dfa
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5800,7 +5800,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.3+2.sha-5a2c3ff');
+    var VERSION = new core.Version('8.0.0-beta.3+5.sha-3f73dfa');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+2.sha-5a2c3ff
+ * @license Angular v8.0.0-beta.3+5.sha-3f73dfa
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5712,7 +5712,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+2.sha-5a2c3ff');
+var VERSION = new Version('8.0.0-beta.3+5.sha-3f73dfa');
 
 /**
  * @license
