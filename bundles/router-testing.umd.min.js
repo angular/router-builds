e7404ab071bf48f174bc6c74a7f02001f305c57e
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 20:46:50 +0000

    47f5b5f docs(service-worker): note about isStable/setInterval (#28102)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+21.sha-a0e3a8e
+ * @license Angular v7.2.4+23.sha-47f5b5f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+21.sha-a0e3a8e
+ * @license Angular v7.2.4+23.sha-47f5b5f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5712,7 +5712,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.4+21.sha-a0e3a8e');
+var VERSION = new Version('7.2.4+23.sha-47f5b5f');
 
 /**
  * @license
