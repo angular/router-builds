700009241156cd39389702369570dc2def38405d
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 19:35:52 +0000

    6709db9 build: switch to typescript's es2015 typings (#28570)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+24.sha-43081a0
+ * @license Angular v8.0.0-beta.3+25.sha-6709db9
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5799,7 +5799,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.3+24.sha-43081a0');
+    var VERSION = new i0.Version('8.0.0-beta.3+25.sha-6709db9');
 
     /**
      * @license
