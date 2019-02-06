27d6d594951ad780ca083ad06faab516e1f5b308
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 04:48:16 +0000

    5a2c3ff fix(ivy): proper component resolution in case of inheritance (#28439)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+1.sha-ed0cf7e
+ * @license Angular v8.0.0-beta.3+2.sha-5a2c3ff
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5800,7 +5800,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.3+1.sha-ed0cf7e');
+    var VERSION = new core.Version('8.0.0-beta.3+2.sha-5a2c3ff');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+1.sha-ed0cf7e
+ * @license Angular v8.0.0-beta.3+2.sha-5a2c3ff
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5712,7 +5712,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+1.sha-ed0cf7e');
+var VERSION = new Version('8.0.0-beta.3+2.sha-5a2c3ff');
 
 /**
  * @license
