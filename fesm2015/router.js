b09b80845bb0eebdf1272bf519f3c14784d4e7e6
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 04:55:40 +0000

    d42f32c feat: optionally save complete performance log in chrome benchpress tests (#27551)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+6.sha-baf103c
+ * @license Angular v8.0.0-beta.3+10.sha-d42f32c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7272,7 +7272,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+6.sha-baf103c');
+const VERSION = new Version('8.0.0-beta.3+10.sha-d42f32c');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+6.sha-baf103c
+ * @license Angular v8.0.0-beta.3+10.sha-d42f32c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
