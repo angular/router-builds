2e7fa7719b90445146af203dfaac42133593eaf5
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 19:52:54 +0000

    0e6f799 test(ivy): update e2e tests to use Renderer2 instead of Renderer (#28558)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+25.sha-6709db9
+ * @license Angular v8.0.0-beta.3+26.sha-0e6f799
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5800,7 +5800,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.3+25.sha-6709db9');
+    var VERSION = new core.Version('8.0.0-beta.3+26.sha-0e6f799');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+25.sha-6709db9
+ * @license Angular v8.0.0-beta.3+26.sha-0e6f799
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7272,7 +7272,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+25.sha-6709db9');
+const VERSION = new Version('8.0.0-beta.3+26.sha-0e6f799');
 
 /**
  * @fileoverview added by tsickle
