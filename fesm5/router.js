a7fca57ebae6b1c7f439701967e700837967e0ac
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 07:37:34 +0000

    ae8b7b1 test(ivy): root cause analysis for mat-selection-list (#28525)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+16.sha-62a13e7
+ * @license Angular v8.0.0-beta.3+17.sha-ae8b7b1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5712,7 +5712,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+16.sha-62a13e7');
+var VERSION = new Version('8.0.0-beta.3+17.sha-ae8b7b1');
 
 /**
  * @license
