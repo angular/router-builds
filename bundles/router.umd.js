5f248707d72bfff0acce3fd8898d2ea6ad424425
Misko Hevery <misko@hevery.com>
2019-02-07 05:40:45 +0000

    8d90142 build: disable sandbox when running protractor through bazel (#28557)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+34.sha-3de06dd
+ * @license Angular v8.0.0-beta.3+29.sha-8d90142
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5799,7 +5799,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.3+34.sha-3de06dd');
+    var VERSION = new i0.Version('8.0.0-beta.3+29.sha-8d90142');
 
     /**
      * @license
