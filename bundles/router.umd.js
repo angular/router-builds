89643695e782c69a430bdbe84cadb2d48ad791f8
Misko Hevery <misko@hevery.com>
2019-02-07 05:39:23 +0000

    3de06dd build(docs-infra): use pinned dependencies when possible in `ng-packages-installer` (#28510)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+28.sha-851cf16
+ * @license Angular v8.0.0-beta.3+34.sha-3de06dd
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5800,7 +5800,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.3+28.sha-851cf16');
+    var VERSION = new core.Version('8.0.0-beta.3+34.sha-3de06dd');
 
     /**
      * @license
