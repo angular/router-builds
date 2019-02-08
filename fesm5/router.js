1588a7d5f126ac8be2432584dd1bb7f0e8573699
Igor Minar <igor@angular.io>
2019-02-08 18:50:29 +0000

    44de68c ci: do not install firebase-tools without cache (#28615)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+58.sha-6e4ef91
+ * @license Angular v8.0.0-beta.3+62.sha-44de68c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5731,7 +5731,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+58.sha-6e4ef91');
+var VERSION = new Version('8.0.0-beta.3+62.sha-44de68c');
 
 /**
  * @license
