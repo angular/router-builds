fd827278e284b908ba6a1d03786e0ef90f3053e0
Miško Hevery <misko@hevery.com>
2019-02-08 01:03:54 +0000

    c3fadad docs: update HttpHeader documentation (#26233)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+47.sha-b65fe62
+ * @license Angular v8.0.0-beta.3+48.sha-c3fadad
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7508,7 +7508,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+47.sha-b65fe62');
+const VERSION = new Version('8.0.0-beta.3+48.sha-c3fadad');
 
 /**
  * @fileoverview added by tsickle
