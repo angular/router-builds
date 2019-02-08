dd1918072cb6f6be06a51e5e53c99345116045d8
Miško Hevery <misko@hevery.com>
2019-02-08 17:48:00 +0000

    1ff3699 test(upgrade): change flaky test to not be affected by other tests (#28617)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+59.sha-0e4705a
+ * @license Angular v8.0.0-beta.3+60.sha-1ff3699
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5805,7 +5805,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.3+59.sha-0e4705a');
+    var VERSION = new core.Version('8.0.0-beta.3+60.sha-1ff3699');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+59.sha-0e4705a
+ * @license Angular v8.0.0-beta.3+60.sha-1ff3699
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
