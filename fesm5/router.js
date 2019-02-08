b1935e3c029be0c04d9d5689386a4d32e8d6881c
Miško Hevery <misko@hevery.com>
2019-02-08 02:11:23 +0000

    94f042b refactor(ivy): remove unneeded detach property (#28595) PR Close #28595

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+50.sha-4b7264f
+ * @license Angular v8.0.0-beta.3+53.sha-94f042b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5717,7 +5717,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+50.sha-4b7264f');
+var VERSION = new Version('8.0.0-beta.3+53.sha-94f042b');
 
 /**
  * @license
