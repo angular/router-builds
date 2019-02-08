4ca3795548b9a8dbbbd1ba8a58f71346f024fd67
Miško Hevery <misko@hevery.com>
2019-02-08 22:16:45 +0000

    fb9a4a6 fix(ivy): support web worker nodes in unknown property checks (#28610)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+68.sha-7115e7c
+ * @license Angular v8.0.0-beta.3+64.sha-fb9a4a6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+68.sha-7115e7c
+ * @license Angular v8.0.0-beta.3+64.sha-fb9a4a6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7508,7 +7508,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+68.sha-7115e7c');
+const VERSION = new Version('8.0.0-beta.3+64.sha-fb9a4a6');
 
 /**
  * @fileoverview added by tsickle
