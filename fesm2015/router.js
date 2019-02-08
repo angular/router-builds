cd98cc695f471a2962d881176bcc9be4c155d09e
Miško Hevery <misko@hevery.com>
2019-02-08 17:35:00 +0000

    ceada77 ci: add all required statuses to the requiredStatuses list for angular-robot (#28613)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+56.sha-d91ea2c
+ * @license Angular v8.0.0-beta.3+57.sha-ceada77
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7277,7 +7277,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+56.sha-d91ea2c');
+const VERSION = new Version('8.0.0-beta.3+57.sha-ceada77');
 
 /**
  * @fileoverview added by tsickle
