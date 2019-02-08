4dbda27c4c365a1c79d266bd989c1f3be99cb3d0
Miško Hevery <misko@hevery.com>
2019-02-08 17:33:48 +0000

    d91ea2c docs(core): fixed typo (#28618)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+55.sha-faf0b25
+ * @license Angular v8.0.0-beta.3+56.sha-d91ea2c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+55.sha-faf0b25
+ * @license Angular v8.0.0-beta.3+56.sha-d91ea2c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5731,7 +5731,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+55.sha-faf0b25');
+var VERSION = new Version('8.0.0-beta.3+56.sha-d91ea2c');
 
 /**
  * @license
