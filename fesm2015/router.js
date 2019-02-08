664df5f6d189ea9cc7e8d426e9b797548cf8526a
Miško Hevery <misko@hevery.com>
2019-02-08 17:32:32 +0000

    d91ea2c docs(core): fixed typo (#28618)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+55.sha-faf0b25
+ * @license Angular v8.0.0-beta.3+56.sha-d91ea2c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7277,7 +7277,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+55.sha-faf0b25');
+const VERSION = new Version('8.0.0-beta.3+56.sha-d91ea2c');
 
 /**
  * @fileoverview added by tsickle
