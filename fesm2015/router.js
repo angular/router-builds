15853b153f2840a5b74846e2fd90e1399c02afb2
Miško Hevery <misko@hevery.com>
2019-02-08 17:47:45 +0000

    0e4705a test(ivy): run view_injector_integration tests on node (#28593)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+57.sha-ceada77
+ * @license Angular v8.0.0-beta.3+59.sha-0e4705a
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7508,7 +7508,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+57.sha-ceada77');
+const VERSION = new Version('8.0.0-beta.3+59.sha-0e4705a');
 
 /**
  * @fileoverview added by tsickle
