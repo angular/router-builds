51cad546cb4e996735df0479a329932ab7cd8ba5
Miško Hevery <misko@hevery.com>
2019-02-08 01:11:16 +0000

    6f9dd1b refactor(common): Remove deprecated FileType (#28583)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+48.sha-c3fadad
+ * @license Angular v8.0.0-beta.3+49.sha-6f9dd1b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5805,7 +5805,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.3+48.sha-c3fadad');
+    var VERSION = new core.Version('8.0.0-beta.3+49.sha-6f9dd1b');
 
     /**
      * @license
