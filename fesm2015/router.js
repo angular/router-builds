a54763d453dbd17c81385e34d65d53bc5ca86362
Miško Hevery <misko@hevery.com>
2019-02-12 18:56:06 +0000

    0cb02d9 fix(ivy): mark query test as modified in Ivy (#28660)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+92.sha-1e64f37
+ * @license Angular v8.0.0-beta.3+107.sha-0cb02d9
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+92.sha-1e64f37');
+const VERSION = new Version('8.0.0-beta.3+107.sha-0cb02d9');
 
 /**
  * @fileoverview added by tsickle
