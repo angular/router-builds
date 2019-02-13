52ce498c6cba8640e2709c53d5c6a37fa8ee928e
Miško Hevery <misko@hevery.com>
2019-02-13 20:46:27 +0000

    77eee42 fix(core): improve global variable detection (#28679)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+141.sha-5cafd44
+ * @license Angular v8.0.0-beta.3+143.sha-77eee42
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7529,7 +7529,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+141.sha-5cafd44');
+const VERSION = new Version('8.0.0-beta.3+143.sha-77eee42');
 
 /**
  * @fileoverview added by tsickle
