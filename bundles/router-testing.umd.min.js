1f515d00ba3dbaacc858489a3c0e8318fce9977d
Miško Hevery <misko@hevery.com>
2019-02-13 18:27:20 +0000

    49fb8c3 fix(bazel): Install angular repo before yarn_install (#28670)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+133.sha-2ea030c
+ * @license Angular v8.0.0-beta.3+132.sha-49fb8c3
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+133.sha-2ea030c
+ * @license Angular v8.0.0-beta.3+132.sha-49fb8c3
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5742,7 +5742,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+133.sha-2ea030c');
+var VERSION = new Version('8.0.0-beta.3+132.sha-49fb8c3');
 
 /**
  * @license
