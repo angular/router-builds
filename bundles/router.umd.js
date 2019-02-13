d41363e6e90c58d9c6beac8fd2a6133aed06c4f6
Miško Hevery <misko@hevery.com>
2019-02-13 04:53:49 +0000

    6050cd0 ci: downgrade to sauce-connect v4.5.1 (#28659)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+107.sha-0cb02d9
+ * @license Angular v8.0.0-beta.3+108.sha-6050cd0
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5815,7 +5815,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.3+107.sha-0cb02d9');
+    var VERSION = new i0.Version('8.0.0-beta.3+108.sha-6050cd0');
 
     /**
      * @license
