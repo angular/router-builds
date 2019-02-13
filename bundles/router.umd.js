3f4a436ff559e7e83855c667b9194fdc358415a4
Miško Hevery <misko@hevery.com>
2019-02-13 18:20:28 +0000

    91b7152 feat(compiler-cli): no longer re-export external symbols by default (#28633)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+130.sha-06ec95f
+ * @license Angular v8.0.0-beta.3+129.sha-91b7152
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.3+130.sha-06ec95f');
+    var VERSION = new core.Version('8.0.0-beta.3+129.sha-91b7152');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+130.sha-06ec95f
+ * @license Angular v8.0.0-beta.3+129.sha-91b7152
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
