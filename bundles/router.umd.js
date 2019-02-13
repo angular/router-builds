a6e6bd7af95b77cd2e08302d44e109210ac51e87
Miško Hevery <misko@hevery.com>
2019-02-13 20:23:57 +0000

    644e7a2 docs: add di-related api doc (#27731)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+134.sha-f8b9e61
+ * @license Angular v8.0.0-beta.3+135.sha-644e7a2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.3+134.sha-f8b9e61');
+    var VERSION = new core.Version('8.0.0-beta.3+135.sha-644e7a2');
 
     /**
      * @license
