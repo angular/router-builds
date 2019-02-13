5d01cd94444f3c0b03ba258d3311c55bdea4cdd6
Miško Hevery <misko@hevery.com>
2019-02-13 20:37:17 +0000

    cb0a8b5 docs(router): change slideInDownAnimation into slideInAnimation (#28640)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+136.sha-39d0311
+ * @license Angular v8.0.0-beta.3+140.sha-cb0a8b5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+136.sha-39d0311
+ * @license Angular v8.0.0-beta.3+140.sha-cb0a8b5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.3+136.sha-39d0311');
+    var VERSION = new i0.Version('8.0.0-beta.3+140.sha-cb0a8b5');
 
     /**
      * @license
