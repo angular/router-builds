518d7f491086ab5ccec68284634fc5ceeee7d5a6
Miško Hevery <misko@hevery.com>
2019-02-13 20:29:39 +0000

    39d0311 refactor(ivy): combine contentQueries and contentQueriesRefresh functions (#28503)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+135.sha-644e7a2
+ * @license Angular v8.0.0-beta.3+136.sha-39d0311
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4903,7 +4903,14 @@
                 this.linkWithHref && isActiveCheckFn(this.linkWithHref) ||
                 this.links.some(isActiveCheckFn) || this.linksWithHrefs.some(isActiveCheckFn);
         };
-        RouterLinkActive.ngDirectiveDef = i0.ɵdefineDirective({ type: RouterLinkActive, selectors: [["", "routerLinkActive", ""]], factory: function RouterLinkActive_Factory(t) { return new (t || RouterLinkActive)(i0.ɵdirectiveInject(Router), i0.ɵdirectiveInject(i0.ElementRef), i0.ɵdirectiveInject(i0.Renderer2), i0.ɵdirectiveInject(RouterLink, 8), i0.ɵdirectiveInject(RouterLinkWithHref, 8)); }, contentQueries: function RouterLinkActive_ContentQueries(dirIndex) { i0.ɵcontentQuery(dirIndex, RouterLink, true); i0.ɵcontentQuery(dirIndex, RouterLinkWithHref, true); }, contentQueriesRefresh: function RouterLinkActive_ContentQueriesRefresh(dirIndex) { var instance = i0.ɵload(dirIndex); var _t; (i0.ɵqueryRefresh((_t = i0.ɵloadContentQuery())) && (instance.links = _t)); (i0.ɵqueryRefresh((_t = i0.ɵloadContentQuery())) && (instance.linksWithHrefs = _t)); }, inputs: { routerLinkActiveOptions: "routerLinkActiveOptions", routerLinkActive: "routerLinkActive" }, exportAs: ["routerLinkActive"], features: [i0.ɵNgOnChangesFeature()] });
+        RouterLinkActive.ngDirectiveDef = i0.ɵdefineDirective({ type: RouterLinkActive, selectors: [["", "routerLinkActive", ""]], factory: function RouterLinkActive_Factory(t) { return new (t || RouterLinkActive)(i0.ɵdirectiveInject(Router), i0.ɵdirectiveInject(i0.ElementRef), i0.ɵdirectiveInject(i0.Renderer2), i0.ɵdirectiveInject(RouterLink, 8), i0.ɵdirectiveInject(RouterLinkWithHref, 8)); }, contentQueries: function RouterLinkActive_ContentQueries(rf, ctx, dirIndex) { if (rf & 1) {
+                i0.ɵcontentQuery(dirIndex, RouterLink, true);
+                i0.ɵcontentQuery(dirIndex, RouterLinkWithHref, true);
+            } if (rf & 2) {
+                var _t;
+                (i0.ɵqueryRefresh((_t = i0.ɵloadContentQuery())) && (ctx.links = _t));
+                (i0.ɵqueryRefresh((_t = i0.ɵloadContentQuery())) && (ctx.linksWithHrefs = _t));
+            } }, inputs: { routerLinkActiveOptions: "routerLinkActiveOptions", routerLinkActive: "routerLinkActive" }, exportAs: ["routerLinkActive"], features: [i0.ɵNgOnChangesFeature()] });
         return RouterLinkActive;
     }());
     /*@__PURE__*/ i0.ɵsetClassMetadata(RouterLinkActive, [{
@@ -5815,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.3+135.sha-644e7a2');
+    var VERSION = new i0.Version('8.0.0-beta.3+136.sha-39d0311');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v8.0.0-beta.3+135.sha-644e7a2
+ * @license Angular v8.0.0-beta.3+136.sha-39d0311
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
 
 import { LocationStrategy, Location, LOCATION_INITIALIZED, PlatformLocation, APP_BASE_HREF, ViewportScroller, HashLocationStrategy, PathLocationStrategy } from '@angular/common';
-import { ɵdefineComponent, ɵelement, ɵsetClassMetadata, Component, ɵisObservable, ɵisPromise, NgModuleRef, InjectionToken, NgModuleFactory, isDevMode, NgZone, ɵConsole, ɵdefineDirective, ɵdirectiveInject, ɵinjectAttribute, Renderer2, ElementRef, ɵlistener, Directive, Attribute, Input, HostListener, ɵallocHostVars, ɵelementAttribute, ɵbind, ɵelementProperty, ɵsanitizeUrl, ɵNgOnChangesFeature, HostBinding, ɵcontentQuery, ɵload, ɵqueryRefresh, ɵloadContentQuery, Optional, ContentChildren, ViewContainerRef, ComponentFactoryResolver, ChangeDetectorRef, Output, EventEmitter, defineInjectable, inject, NgModuleFactoryLoader, Compiler, Injector, Injectable, ApplicationRef, SystemJsNgModuleLoader, ɵdefineNgModule, defineInjector, NgModule, Inject, ɵsetComponentScope, ANALYZE_FOR_ENTRY_COMPONENTS, SkipSelf, NgProbeToken, APP_INITIALIZER, APP_BOOTSTRAP_LISTENER, Version } from '@angular/core';
+import { ɵdefineComponent, ɵelement, ɵsetClassMetadata, Component, ɵisObservable, ɵisPromise, NgModuleRef, InjectionToken, NgModuleFactory, isDevMode, NgZone, ɵConsole, ɵdefineDirective, ɵdirectiveInject, ɵinjectAttribute, Renderer2, ElementRef, ɵlistener, Directive, Attribute, Input, HostListener, ɵallocHostVars, ɵelementAttribute, ɵbind, ɵelementProperty, ɵsanitizeUrl, ɵNgOnChangesFeature, HostBinding, ɵcontentQuery, ɵqueryRefresh, ɵloadContentQuery, Optional, ContentChildren, ViewContainerRef, ComponentFactoryResolver, ChangeDetectorRef, Output, EventEmitter, defineInjectable, inject, NgModuleFactoryLoader, Compiler, Injector, Injectable, ApplicationRef, SystemJsNgModuleLoader, ɵdefineNgModule, defineInjector, NgModule, Inject, ɵsetComponentScope, ANALYZE_FOR_ENTRY_COMPONENTS, SkipSelf, NgProbeToken, APP_INITIALIZER, APP_BOOTSTRAP_LISTENER, Version } from '@angular/core';
 import { __extends, __assign, __values, __spread } from 'tslib';
 import { from, of, BehaviorSubject, EmptyError, Observable, combineLatest, defer, EMPTY, Subject } from 'rxjs';
 import { map, concatAll, last, catchError, first, mergeMap, every, switchMap, take, startWith, scan, filter, concatMap, reduce, tap, finalize, mergeAll } from 'rxjs/operators';
@@ -4830,7 +4830,14 @@ var RouterLinkActive = /** @class */ (function () {
             this.linkWithHref && isActiveCheckFn(this.linkWithHref) ||
             this.links.some(isActiveCheckFn) || this.linksWithHrefs.some(isActiveCheckFn);
     };
-    RouterLinkActive.ngDirectiveDef = ɵdefineDirective({ type: RouterLinkActive, selectors: [["", "routerLinkActive", ""]], factory: function RouterLinkActive_Factory(t) { return new (t || RouterLinkActive)(ɵdirectiveInject(Router), ɵdirectiveInject(ElementRef), ɵdirectiveInject(Renderer2), ɵdirectiveInject(RouterLink, 8), ɵdirectiveInject(RouterLinkWithHref, 8)); }, contentQueries: function RouterLinkActive_ContentQueries(dirIndex) { ɵcontentQuery(dirIndex, RouterLink, true); ɵcontentQuery(dirIndex, RouterLinkWithHref, true); }, contentQueriesRefresh: function RouterLinkActive_ContentQueriesRefresh(dirIndex) { var instance = ɵload(dirIndex); var _t; (ɵqueryRefresh((_t = ɵloadContentQuery())) && (instance.links = _t)); (ɵqueryRefresh((_t = ɵloadContentQuery())) && (instance.linksWithHrefs = _t)); }, inputs: { routerLinkActiveOptions: "routerLinkActiveOptions", routerLinkActive: "routerLinkActive" }, exportAs: ["routerLinkActive"], features: [ɵNgOnChangesFeature()] });
+    RouterLinkActive.ngDirectiveDef = ɵdefineDirective({ type: RouterLinkActive, selectors: [["", "routerLinkActive", ""]], factory: function RouterLinkActive_Factory(t) { return new (t || RouterLinkActive)(ɵdirectiveInject(Router), ɵdirectiveInject(ElementRef), ɵdirectiveInject(Renderer2), ɵdirectiveInject(RouterLink, 8), ɵdirectiveInject(RouterLinkWithHref, 8)); }, contentQueries: function RouterLinkActive_ContentQueries(rf, ctx, dirIndex) { if (rf & 1) {
+            ɵcontentQuery(dirIndex, RouterLink, true);
+            ɵcontentQuery(dirIndex, RouterLinkWithHref, true);
+        } if (rf & 2) {
+            var _t;
+            (ɵqueryRefresh((_t = ɵloadContentQuery())) && (ctx.links = _t));
+            (ɵqueryRefresh((_t = ɵloadContentQuery())) && (ctx.linksWithHrefs = _t));
+        } }, inputs: { routerLinkActiveOptions: "routerLinkActiveOptions", routerLinkActive: "routerLinkActive" }, exportAs: ["routerLinkActive"], features: [ɵNgOnChangesFeature()] });
     return RouterLinkActive;
 }());
 /*@__PURE__*/ ɵsetClassMetadata(RouterLinkActive, [{
@@ -5742,7 +5749,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+135.sha-644e7a2');
+var VERSION = new Version('8.0.0-beta.3+136.sha-39d0311');
 
 /**
  * @license
