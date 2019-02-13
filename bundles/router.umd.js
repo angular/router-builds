89e891cf747355e5153811caa610887909e95598
Misko Hevery <misko@hevery.com>
2019-02-13 05:16:20 +0000

    d68a98f test(ivy): add template source mapping tests (#28055)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+108.sha-6050cd0
+ * @license Angular v8.0.0-beta.3+125.sha-d68a98f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.3+108.sha-6050cd0');
+    var VERSION = new core.Version('8.0.0-beta.3+125.sha-d68a98f');
 
     /**
      * @license
