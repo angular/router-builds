59d0190ee6d9e9dd5467d23da0bb57db71c7fd7d
Miško Hevery <misko@hevery.com>
2019-02-13 20:53:58 +0000

    2f27a80 test(ivy): ivy change detection doesn't descend into CD-detached view trees (#28680)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+143.sha-77eee42
+ * @license Angular v8.0.0-beta.3+144.sha-2f27a80
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.3+143.sha-77eee42');
+    var VERSION = new i0.Version('8.0.0-beta.3+144.sha-2f27a80');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+143.sha-77eee42
+ * @license Angular v8.0.0-beta.3+144.sha-2f27a80
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7529,7 +7529,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+143.sha-77eee42');
+const VERSION = new Version('8.0.0-beta.3+144.sha-2f27a80');
 
 /**
  * @fileoverview added by tsickle
