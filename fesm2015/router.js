a2476dfb4581b2f5e354604c497b25d6ad8b9ba0
Miško Hevery <misko@hevery.com>
2019-02-13 19:42:28 +0000

    06ec95f fix(ivy): allow directive inheritance in strict mode (#28634)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+124.sha-08de52b
+ * @license Angular v8.0.0-beta.3+130.sha-06ec95f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+124.sha-08de52b');
+const VERSION = new Version('8.0.0-beta.3+130.sha-06ec95f');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+124.sha-08de52b
+ * @license Angular v8.0.0-beta.3+130.sha-06ec95f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
