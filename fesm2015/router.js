2ddf9a7a68eb018a058107252e84b143a696b712
Miško Hevery <misko@hevery.com>
2019-02-13 20:41:44 +0000

    5cafd44 fix(docs-infra): fix filtering in run-example-e2e.js (#28663)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+140.sha-cb0a8b5
+ * @license Angular v8.0.0-beta.3+141.sha-5cafd44
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+140.sha-cb0a8b5');
+const VERSION = new Version('8.0.0-beta.3+141.sha-5cafd44');
 
 /**
  * @fileoverview added by tsickle
