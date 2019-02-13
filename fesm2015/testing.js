82bbf4689912ec07d75e202aa6f137060059667b
Miško Hevery <misko@hevery.com>
2019-02-13 20:25:21 +0000

    644e7a2 docs: add di-related api doc (#27731)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+134.sha-f8b9e61
+ * @license Angular v8.0.0-beta.3+135.sha-644e7a2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+134.sha-f8b9e61
+ * @license Angular v8.0.0-beta.3+135.sha-644e7a2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5742,7 +5742,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+134.sha-f8b9e61');
+var VERSION = new Version('8.0.0-beta.3+135.sha-644e7a2');
 
 /**
  * @license
