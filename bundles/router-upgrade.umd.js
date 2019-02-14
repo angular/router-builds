a12662076e6aa96bfbfdbb93562fae48541a8cdc
Misko Hevery <misko@hevery.com>
2019-02-14 05:44:52 +0000

    65c2dea refactor(bazel): Fix warning about overridden tsconfig options (#28674)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+167.sha-1df3aef
+ * @license Angular v8.0.0-beta.3+173.sha-65c2dea
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+167.sha-1df3aef
+ * @license Angular v8.0.0-beta.3+173.sha-65c2dea
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.3+167.sha-1df3aef');
+    var VERSION = new i0.Version('8.0.0-beta.3+173.sha-65c2dea');
 
     /**
      * @license
