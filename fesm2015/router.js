4f48cc4dec8017dcf215bf7f22706fc1a533aef0
Miško Hevery <misko@hevery.com>
2019-02-14 19:57:33 +0000

    80a5934 fix(ivy): support schemas at runtime (#28637)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+177.sha-63e5d27
+ * @license Angular v8.0.0-beta.3+179.sha-80a5934
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+177.sha-63e5d27');
+const VERSION = new Version('8.0.0-beta.3+179.sha-80a5934');
 
 /**
  * @fileoverview added by tsickle
