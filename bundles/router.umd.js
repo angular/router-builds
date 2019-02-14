2990224779a720aaac2aa172f2e639d75c52c9db
Misko Hevery <misko@hevery.com>
2019-02-14 03:47:52 +0000

    36df905 docs(animations): fixed some grammar (#28708) PR Close #28708

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+160.sha-2c6a6f1
+ * @license Angular v8.0.0-beta.3+161.sha-36df905
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.3+160.sha-2c6a6f1');
+    var VERSION = new core.Version('8.0.0-beta.3+161.sha-36df905');
 
     /**
      * @license
