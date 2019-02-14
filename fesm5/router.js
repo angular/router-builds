e4b35041390f6d3742dc3a1b3345d6bea0698c6f
Miško Hevery <misko@hevery.com>
2019-02-14 03:27:05 +0000

    28bdeee build(bazel): update to rules_nodejs 0.18.6 (#28699)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+130.sha-06ec95f
+ * @license Angular v8.0.0-beta.3+147.sha-28bdeee
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5723,7 +5723,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+130.sha-06ec95f');
+var VERSION = new Version('8.0.0-beta.3+147.sha-28bdeee');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+130.sha-06ec95f
+ * @license Angular v8.0.0-beta.3+147.sha-28bdeee
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
