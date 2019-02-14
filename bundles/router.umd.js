0589e820ebf5b71def96bb9f616644a592dfc244
Miško Hevery <misko@hevery.com>
2019-02-14 19:48:44 +0000

    627cecd fix(ivy): ensure host bindings and host styling works on a root component (#28664)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+174.sha-b41da03
+ * @license Angular v8.0.0-beta.3+175.sha-627cecd
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.3+174.sha-b41da03');
+    var VERSION = new i0.Version('8.0.0-beta.3+175.sha-627cecd');
 
     /**
      * @license
