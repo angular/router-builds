34a4dd939f369861b91e5c38e28d62a5f97ece66
Misko Hevery <misko@hevery.com>
2019-02-14 03:52:11 +0000

    8cec4b3 build: make api-extractor work in google3 (#28588)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+168.sha-b0afc4c
+ * @license Angular v8.0.0-beta.3+166.sha-8cec4b3
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.3+168.sha-b0afc4c');
+    var VERSION = new i0.Version('8.0.0-beta.3+166.sha-8cec4b3');
 
     /**
      * @license
