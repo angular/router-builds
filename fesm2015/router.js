57a3fb92d335380f83cd5269dc768d7d6d515ff7
Misko Hevery <misko@hevery.com>
2019-02-14 03:53:03 +0000

    3842dd6 fix(ivy): OnChanges should support updating one Input among many (#28693)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+172.sha-841a1d3
+ * @license Angular v8.0.0-beta.3+162.sha-3842dd6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7529,7 +7529,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+172.sha-841a1d3');
+const VERSION = new Version('8.0.0-beta.3+162.sha-3842dd6');
 
 /**
  * @fileoverview added by tsickle
