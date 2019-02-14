983cff230fb925e5d21701c8ee2c36fcd8b47ff1
Misko Hevery <misko@hevery.com>
2019-02-14 03:51:41 +0000

    3842dd6 fix(ivy): OnChanges should support updating one Input among many (#28693)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+172.sha-841a1d3
+ * @license Angular v8.0.0-beta.3+162.sha-3842dd6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.3+172.sha-841a1d3');
+    var VERSION = new core.Version('8.0.0-beta.3+162.sha-3842dd6');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+172.sha-841a1d3
+ * @license Angular v8.0.0-beta.3+162.sha-3842dd6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
