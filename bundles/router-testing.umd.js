263baa630cc912113a8cc8cec614c92f93577694
Misko Hevery <misko@hevery.com>
2019-02-14 03:47:34 +0000

    5e701a2 build(bazel): rollback to chromium 69 (latest version that still works on OSX) (#28698)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+74.sha-b23582d
+ * @license Angular v7.2.4+78.sha-5e701a2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+74.sha-b23582d
+ * @license Angular v7.2.4+78.sha-5e701a2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7277,7 +7277,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.4+74.sha-b23582d');
+const VERSION = new Version('7.2.4+78.sha-5e701a2');
 
 /**
  * @fileoverview added by tsickle
