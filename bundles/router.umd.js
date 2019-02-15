ba7bfad1838382a9eaefe4fb1a23fa68545228b9
Miško Hevery <misko@hevery.com>
2019-02-15 22:52:41 +0000

    6fcf286 release: cut the v8.0.0-beta.4 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+182.sha-fcc8c56
+ * @license Angular v8.0.0-beta.4
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.3+182.sha-fcc8c56');
+    var VERSION = new core.Version('8.0.0-beta.4');
 
     /**
      * @license
