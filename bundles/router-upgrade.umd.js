897a5047ffc62a94c01b2d6311b97b43ab892213
Misko Hevery <misko@hevery.com>
2019-02-17 05:29:28 +0000

    1e6ed52 fix(docs-infra): all examples should get Ivy boilerplate (#28757)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+6.sha-fdca001
+ * @license Angular v8.0.0-beta.4+5.sha-1e6ed52
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+6.sha-fdca001
+ * @license Angular v8.0.0-beta.4+5.sha-1e6ed52
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.4+6.sha-fdca001');
+    var VERSION = new core.Version('8.0.0-beta.4+5.sha-1e6ed52');
 
     /**
      * @license
