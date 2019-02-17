6e9edafdf3ab7faadfb27fd805c48af29cc0fb68
Misko Hevery <misko@hevery.com>
2019-02-17 05:28:50 +0000

    fdca001 feat: upgrade domino to v2.1.2 (#28767)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+4.sha-04a1923
+ * @license Angular v8.0.0-beta.4+6.sha-fdca001
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.4+4.sha-04a1923');
+    var VERSION = new core.Version('8.0.0-beta.4+6.sha-fdca001');
 
     /**
      * @license
