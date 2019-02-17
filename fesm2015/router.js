3a6384f18decd324a7f5e85f48b6a98de8160a95
Misko Hevery <misko@hevery.com>
2019-02-17 05:30:45 +0000

    1e6ed52 fix(docs-infra): all examples should get Ivy boilerplate (#28757)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+6.sha-fdca001
+ * @license Angular v8.0.0-beta.4+5.sha-1e6ed52
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7529,7 +7529,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.4+6.sha-fdca001');
+const VERSION = new Version('8.0.0-beta.4+5.sha-1e6ed52');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+6.sha-fdca001
+ * @license Angular v8.0.0-beta.4+5.sha-1e6ed52
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5749,7 +5749,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+6.sha-fdca001');
+var VERSION = new Version('8.0.0-beta.4+5.sha-1e6ed52');
 
 /**
  * @license
