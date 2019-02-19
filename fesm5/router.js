666a07eecb7e26ede98ac04f34226e2ba8922897
Igor Minar <igor@angular.io>
2019-02-19 20:59:45 +0000

    caefc5d ci: add /aio/content/images/bios/** to @angular/fw-docs-marketing group (#28835)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+12.sha-a17fd43
+ * @license Angular v8.0.0-beta.4+14.sha-caefc5d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5749,7 +5749,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+12.sha-a17fd43');
+var VERSION = new Version('8.0.0-beta.4+14.sha-caefc5d');
 
 /**
  * @license
