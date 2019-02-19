baa20541ac14936575efbd6ab6f630a746c7161f
Igor Minar <igor@angular.io>
2019-02-19 20:50:38 +0000

    f487121 ci: add /aio/content/images/bios/** to @angular/fw-docs-marketing group (#28835)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+5.sha-2a23eca
+ * @license Angular v7.2.5+6.sha-f487121
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+5.sha-2a23eca
+ * @license Angular v7.2.5+6.sha-f487121
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5717,7 +5717,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.5+5.sha-2a23eca');
+var VERSION = new Version('7.2.5+6.sha-f487121');
 
 /**
  * @license
