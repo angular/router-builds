8ea3da8841877fe95c0ccbd7deb8907c8a0ae2d8
Igor Minar <igor@angular.io>
2019-02-19 19:50:29 +0000

    0cc5804 docs(aio): add Keilla Fernandes to GDE resources (#28437)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+10.sha-55b144b
+ * @license Angular v8.0.0-beta.4+11.sha-0cc5804
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.4+10.sha-55b144b');
+    var VERSION = new i0.Version('8.0.0-beta.4+11.sha-0cc5804');
 
     /**
      * @license
