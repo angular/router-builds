301d3c7cad4e1720f74078e938c92c21ee4f04fe
Igor Minar <igor@angular.io>
2019-02-19 21:08:22 +0000

    80c7aff build(docs-infra): improve caching configuration for Firebase hosting (#28785)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+19.sha-6788d86
+ * @license Angular v8.0.0-beta.4+20.sha-80c7aff
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+19.sha-6788d86
+ * @license Angular v8.0.0-beta.4+20.sha-80c7aff
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.4+19.sha-6788d86');
+    var VERSION = new i0.Version('8.0.0-beta.4+20.sha-80c7aff');
 
     /**
      * @license
