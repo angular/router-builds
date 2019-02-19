336edc164ec714251e7bfb09503cde6d794c49ba
Igor Minar <igor@angular.io>
2019-02-19 21:06:47 +0000

    af8d58c docs: capitalize karma (#28775)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+14.sha-caefc5d
+ * @license Angular v8.0.0-beta.4+17.sha-af8d58c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.4+14.sha-caefc5d');
+    var VERSION = new i0.Version('8.0.0-beta.4+17.sha-af8d58c');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+14.sha-caefc5d
+ * @license Angular v8.0.0-beta.4+17.sha-af8d58c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7529,7 +7529,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.4+14.sha-caefc5d');
+const VERSION = new Version('8.0.0-beta.4+17.sha-af8d58c');
 
 /**
  * @fileoverview added by tsickle
