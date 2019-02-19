b4f3d387b8e04f1dfcf1855345fe003718a1bbbc
Igor Minar <igor@angular.io>
2019-02-19 19:25:51 +0000

    980bb52 docs: warn that directives don't support namespaces (#25855)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+7.sha-3c7ce82
+ * @license Angular v8.0.0-beta.4+9.sha-980bb52
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.4+7.sha-3c7ce82');
+    var VERSION = new core.Version('8.0.0-beta.4+9.sha-980bb52');
 
     /**
      * @license
