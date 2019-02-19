b40e3c6df34973ee3a9aa96e86af77979b9029de
Igor Minar <igor@angular.io>
2019-02-19 21:28:34 +0000

    d8ae899 docs(common): fix a typo in a debounce requests example header (#28829)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+28.sha-19afb79
+ * @license Angular v8.0.0-beta.4+29.sha-d8ae899
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+28.sha-19afb79
+ * @license Angular v8.0.0-beta.4+29.sha-d8ae899
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.4+28.sha-19afb79');
+const VERSION = new Version('8.0.0-beta.4+29.sha-d8ae899');
 
 /**
  * @fileoverview added by tsickle
