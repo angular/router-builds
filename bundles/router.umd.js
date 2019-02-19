385c45bbd6672d650e0a0487440bb0f525cb57c8
Igor Minar <igor@angular.io>
2019-02-19 21:22:36 +0000

    745c9c5 build(compiler-cli): upgrade chokidar to latest version (#28797)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+23.sha-692ddfc
+ * @license Angular v8.0.0-beta.4+26.sha-745c9c5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.4+23.sha-692ddfc');
+    var VERSION = new core.Version('8.0.0-beta.4+26.sha-745c9c5');
 
     /**
      * @license
