95e8a4688e2444d3d3b276166f609fa0dd3a7366
Igor Minar <igor@angular.io>
2019-02-19 20:46:14 +0000

    a17fd43 docs: display the beginning label in interpolation guide example (#28602)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+11.sha-0cc5804
+ * @license Angular v8.0.0-beta.4+12.sha-a17fd43
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.4+11.sha-0cc5804');
+    var VERSION = new i0.Version('8.0.0-beta.4+12.sha-a17fd43');
 
     /**
      * @license
