3457dcce9f641a8eec95b4b0fa18d423d5a5905f
Igor Minar <igor@angular.io>
2019-02-19 21:25:57 +0000

    19afb79 feat(core): allow users to define timing of ViewChild/ContentChild queries (#28810)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+27.sha-5e68e35
+ * @license Angular v8.0.0-beta.4+28.sha-19afb79
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.4+27.sha-5e68e35');
+const VERSION = new Version('8.0.0-beta.4+28.sha-19afb79');
 
 /**
  * @fileoverview added by tsickle
