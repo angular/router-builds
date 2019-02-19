0d7aa75d2b1dea92b62433d3ee9ba820ef9b2092
Igor Minar <igor@angular.io>
2019-02-19 21:09:29 +0000

    00a8b07 ci: combine "define_env_vars" and "download_yarn" anchor (#28788)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+20.sha-80c7aff
+ * @license Angular v8.0.0-beta.4+21.sha-00a8b07
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.4+20.sha-80c7aff');
+    var VERSION = new core.Version('8.0.0-beta.4+21.sha-00a8b07');
 
     /**
      * @license
