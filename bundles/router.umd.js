53ce58a66311b78c8861171ceaa27cb9189c49bb
Igor Minar <igor@angular.io>
2019-02-20 00:49:25 +0000

    02ee8c7 build: fix inflight PR conflict in integration/bazel (#28720)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+36.sha-295a143
+ * @license Angular v8.0.0-beta.4+41.sha-02ee8c7
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.4+36.sha-295a143');
+    var VERSION = new core.Version('8.0.0-beta.4+41.sha-02ee8c7');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+36.sha-295a143
+ * @license Angular v8.0.0-beta.4+41.sha-02ee8c7
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
