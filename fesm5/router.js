d22594c09fb8a7f24d81494b7b4edf9d1025c3b8
Igor Minar <igor@angular.io>
2019-02-20 00:37:28 +0000

    295a143 docs: add Sharon DiOrio to contributors (#28671)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+35.sha-ad6475f
+ * @license Angular v8.0.0-beta.4+36.sha-295a143
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5723,7 +5723,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+35.sha-ad6475f');
+var VERSION = new Version('8.0.0-beta.4+36.sha-295a143');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+35.sha-ad6475f
+ * @license Angular v8.0.0-beta.4+36.sha-295a143
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
