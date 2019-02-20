1e7d016d7f8527902e77588422538282e48e3576
Igor Minar <igor@angular.io>
2019-02-20 21:40:58 +0000

    81c3104 docs: release notes for the v7.2.6 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+53.sha-68da465
+ * @license Angular v8.0.0-beta.4+54.sha-81c3104
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7529,7 +7529,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.4+53.sha-68da465');
+const VERSION = new Version('8.0.0-beta.4+54.sha-81c3104');
 
 /**
  * @fileoverview added by tsickle
