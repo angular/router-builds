a95276db2398a7a6901eec2ec5a359687a026436
Igor Minar <igor@angular.io>
2019-02-20 19:18:27 +0000

    b9eb662 test(bazel): Upgrade CLI dependencies for schematics test (#28872)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+51.sha-04ae125
+ * @license Angular v8.0.0-beta.4+52.sha-b9eb662
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+51.sha-04ae125
+ * @license Angular v8.0.0-beta.4+52.sha-b9eb662
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.4+51.sha-04ae125');
+    var VERSION = new i0.Version('8.0.0-beta.4+52.sha-b9eb662');
 
     /**
      * @license
