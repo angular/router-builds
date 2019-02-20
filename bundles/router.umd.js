f7980263b3d4b3934351f6d288662eab086a5498
Igor Minar <igor@angular.io>
2019-02-20 00:56:16 +0000

    2d80419 fix(bazel): Schematics should upgrade rxjs to 6.4.0 (#28841)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+42.sha-fd4e1d6
+ * @license Angular v8.0.0-beta.4+43.sha-2d80419
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.4+42.sha-fd4e1d6');
+    var VERSION = new i0.Version('8.0.0-beta.4+43.sha-2d80419');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+42.sha-fd4e1d6
+ * @license Angular v8.0.0-beta.4+43.sha-2d80419
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
