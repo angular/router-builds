7de48bc60d4dd59aac4e967fffe925de17527942
Igor Minar <igor@angular.io>
2019-02-20 20:00:54 +0000

    68da465 test(ivy): update material blocklist to turn on passing tests (#28873)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+52.sha-b9eb662
+ * @license Angular v8.0.0-beta.4+53.sha-68da465
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.4+52.sha-b9eb662');
+    var VERSION = new i0.Version('8.0.0-beta.4+53.sha-68da465');
 
     /**
      * @license
