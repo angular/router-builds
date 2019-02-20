a9dae19c98946bf10fd968a16051e63068285849
Igor Minar <igor@angular.io>
2019-02-20 00:50:34 +0000

    19afb79 feat(core): allow users to define timing of ViewChild/ContentChild queries (#28810)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+8.sha-6b511a3
+ * @license Angular v8.0.0-beta.4+28.sha-19afb79
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+8.sha-6b511a3
+ * @license Angular v8.0.0-beta.4+28.sha-19afb79
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.4+8.sha-6b511a3');
+    var VERSION = new core.Version('8.0.0-beta.4+28.sha-19afb79');
 
     /**
      * @license
