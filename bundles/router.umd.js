e6e83e79ca41dd36b6a19b44b3bc8f726b13def3
Igor Minar <igor@angular.io>
2019-02-20 00:38:41 +0000

    295a143 docs: add Sharon DiOrio to contributors (#28671)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+35.sha-ad6475f
+ * @license Angular v8.0.0-beta.4+36.sha-295a143
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.4+35.sha-ad6475f');
+    var VERSION = new i0.Version('8.0.0-beta.4+36.sha-295a143');
 
     /**
      * @license
