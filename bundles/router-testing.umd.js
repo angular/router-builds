4bf318313c6bd31ad5dd9be703ee78fd662259ec
Igor Minar <igor@angular.io>
2019-02-21 08:33:19 +0000

    be121bb fix(ivy): restore @fileoverview annotations for Closure (#28723)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+9.sha-58436fd
+ * @license Angular v8.0.0-beta.5+10.sha-be121bb
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+9.sha-58436fd
+ * @license Angular v8.0.0-beta.5+10.sha-be121bb
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.5+9.sha-58436fd');
+    var VERSION = new core.Version('8.0.0-beta.5+10.sha-be121bb');
 
     /**
      * @license
