7954d148f39e0abe9f683f346a2dffb983339146
Igor Minar <igor@angular.io>
2019-02-21 08:40:17 +0000

    95d9aa2 fix(ivy): allow HTML comments to be present inside <ng-content> (#28849)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+14.sha-9ae14db
+ * @license Angular v8.0.0-beta.5+13.sha-95d9aa2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.5+14.sha-9ae14db');
+    var VERSION = new i0.Version('8.0.0-beta.5+13.sha-95d9aa2');
 
     /**
      * @license
