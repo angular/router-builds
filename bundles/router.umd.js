826712f19336a2ba784ad50eb1b32ed9c4f03e14
Ben Lesh <ben@benlesh.com>
2019-02-21 02:47:54 +0000

    58436fd fix(ivy): unable to import shim factory files on case-insensitive platforms (#28831)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+7.sha-d0b6622
+ * @license Angular v8.0.0-beta.5+9.sha-58436fd
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.5+7.sha-d0b6622');
+    var VERSION = new i0.Version('8.0.0-beta.5+9.sha-58436fd');
 
     /**
      * @license
