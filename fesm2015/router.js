c1240bea09e39bac81d3c652cfc809e9768e34eb
Igor Minar <igor@angular.io>
2019-02-21 17:55:47 +0000

    e1aaa7e fix(ivy): component destroy hook called twice when configured as provider (#28470)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+15.sha-ebffde7
+ * @license Angular v8.0.0-beta.5+17.sha-e1aaa7e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7529,7 +7529,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.5+15.sha-ebffde7');
+const VERSION = new Version('8.0.0-beta.5+17.sha-e1aaa7e');
 
 /**
  * @fileoverview added by tsickle
