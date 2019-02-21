44550f2c67cf71219df4ff73684bb5e0391e008f
Ben Lesh <ben@benlesh.com>
2019-02-21 21:01:12 +0000

    3f2b51b ci: make yarn globally available and independent of `$BASH_ENV` (#28889)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+19.sha-d8704a3
+ * @license Angular v8.0.0-beta.5+20.sha-3f2b51b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.5+19.sha-d8704a3');
+const VERSION = new Version('8.0.0-beta.5+20.sha-3f2b51b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
