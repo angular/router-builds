ccfffd9542c92d8f9139ecb9d4b34fc09919620c
Igor Minar <igor@angular.io>
2019-02-21 08:39:59 +0000

    9ae14db fix(bazel): rxjs_umd_modules should always be present (#28881)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+12.sha-df627e6
+ * @license Angular v8.0.0-beta.5+14.sha-9ae14db
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+12.sha-df627e6
+ * @license Angular v8.0.0-beta.5+14.sha-9ae14db
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.5+12.sha-df627e6');
+    var VERSION = new i0.Version('8.0.0-beta.5+14.sha-9ae14db');
 
     /**
      * @license
