295272f84e733ea064424a2083fa81ae6f894c9a
Igor Minar <igor@angular.io>
2019-02-21 16:09:40 +0000

    ebffde7 build: update to rules_typescript 0.25.1 (#28625)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+16.sha-0ea216b
+ * @license Angular v8.0.0-beta.5+15.sha-ebffde7
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5723,7 +5723,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+16.sha-0ea216b');
+var VERSION = new Version('8.0.0-beta.5+15.sha-ebffde7');
 
 /**
  * @license
