df0be2240e53b8972608676c54f056e19f96f6fa
Igor Minar <igor@angular.io>
2019-02-22 02:08:46 +0000

    f4d6525 build: use uname instead of relying on sed --version (#28900)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+20.sha-3f2b51b.with-local-changes
+ * @license Angular v8.0.0-beta.5+22.sha-f4d6525.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.5+20.sha-3f2b51b.with-local-changes');
+const VERSION = new Version('8.0.0-beta.5+22.sha-f4d6525.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+20.sha-3f2b51b.with-local-changes
+ * @license Angular v8.0.0-beta.5+22.sha-f4d6525.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
