3fa5db8db8e9eff789e9610affde6871e128a60c
Ben Lesh <ben@benlesh.com>
2019-02-22 18:09:53 +0000

    8b9f03d ci: run saucelabs legacy tests every hour (#28903)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+20.sha-40f1f94.with-local-changes
+ * @license Angular v7.2.6+21.sha-8b9f03d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+20.sha-40f1f94.with-local-changes
+ * @license Angular v7.2.6+21.sha-8b9f03d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7277,7 +7277,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.6+20.sha-40f1f94.with-local-changes');
+const VERSION = new Version('7.2.6+21.sha-8b9f03d.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
