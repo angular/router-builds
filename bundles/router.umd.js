d997f6fd1b358d8e6f2ddee2ac9f266d94b3e769
Ben Lesh <ben@benlesh.com>
2019-02-22 21:51:22 +0000

    f0989b7 ci: share data between jobs using workspaces (#28928)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+49.sha-c1392ce.with-local-changes
+ * @license Angular v8.0.0-beta.5+58.sha-f0989b7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.5+49.sha-c1392ce.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.5+58.sha-f0989b7.with-local-changes');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+49.sha-c1392ce.with-local-changes
+ * @license Angular v8.0.0-beta.5+58.sha-f0989b7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7529,7 +7529,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.5+49.sha-c1392ce.with-local-changes');
+const VERSION = new Version('8.0.0-beta.5+58.sha-f0989b7.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
