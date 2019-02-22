3a7b0eb0927855c069deb379e62efaa8d1189228
Ben Lesh <ben@benlesh.com>
2019-02-22 21:27:55 +0000

    f0f81f4 build: added comments after review (#27721)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+47.sha-fafabc0.with-local-changes
+ * @license Angular v8.0.0-beta.5+53.sha-f0f81f4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.5+47.sha-fafabc0.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.5+53.sha-f0f81f4.with-local-changes');
 
     /**
      * @license
