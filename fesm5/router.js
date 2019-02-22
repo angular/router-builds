67a03b4dabee69108feb1104d3ffea8b5ca6bd21
Ben Lesh <ben@benlesh.com>
2019-02-22 21:39:32 +0000

    c1392ce feat(ivy): produce and consume ES2015 re-exports for NgModule re-exports (#28852)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+57.sha-c64b13e.with-local-changes
+ * @license Angular v8.0.0-beta.5+49.sha-c1392ce.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5749,7 +5749,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+57.sha-c64b13e.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+49.sha-c1392ce.with-local-changes');
 
 /**
  * @license
