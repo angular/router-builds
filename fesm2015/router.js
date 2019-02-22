53533d4d1c22b13c0d505c8719d2c6288c0fc497
Ben Lesh <ben@benlesh.com>
2019-02-22 18:17:19 +0000

    7c20bf8 build(docs-infra): upgrade cli command docs sources to c91579a2e (#28922)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+39.sha-82820b0.with-local-changes
+ * @license Angular v8.0.0-beta.5+40.sha-7c20bf8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.5+39.sha-82820b0.with-local-changes');
+const VERSION = new Version('8.0.0-beta.5+40.sha-7c20bf8.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+39.sha-82820b0.with-local-changes
+ * @license Angular v8.0.0-beta.5+40.sha-7c20bf8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
