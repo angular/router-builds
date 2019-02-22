658c01fdd77a0a092aaf2c8d9b4ba06fd43e8165
Ben Lesh <ben@benlesh.com>
2019-02-22 20:36:42 +0000

    fafabc0 build(bazel): use http_server rule from rules_nodejs (#28905)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+45.sha-43181ea.with-local-changes
+ * @license Angular v8.0.0-beta.5+47.sha-fafabc0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7529,7 +7529,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.5+45.sha-43181ea.with-local-changes');
+const VERSION = new Version('8.0.0-beta.5+47.sha-fafabc0.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
