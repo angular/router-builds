19b2142eb5fbd14cf2cc30ab8abaaee1ed2b1ce1
Igor Minar <igor@angular.io>
2019-02-22 11:19:19 +0000

    c7fe3a9 build: enable bundle_dts for a upgrade package (#28642)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+32.sha-599e2e2.with-local-changes
+ * @license Angular v8.0.0-beta.5+35.sha-c7fe3a9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7529,7 +7529,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.5+32.sha-599e2e2.with-local-changes');
+const VERSION = new Version('8.0.0-beta.5+35.sha-c7fe3a9.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+32.sha-599e2e2.with-local-changes
+ * @license Angular v8.0.0-beta.5+35.sha-c7fe3a9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
