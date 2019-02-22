268d63e06d78259f9d44a672753b4f5b71d78953
Ben Lesh <ben@benlesh.com>
2019-02-22 19:13:46 +0000

    7f3e3a8 docs(bazel): Use CLI 8 for Bazel schematics (#28908)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+41.sha-230a941.with-local-changes
+ * @license Angular v8.0.0-beta.5+43.sha-7f3e3a8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
