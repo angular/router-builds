0263525a02b761f6f543148952f29deda46a2b6c
Ben Lesh <ben@benlesh.com>
2019-02-22 22:59:38 +0000

    395fb18 docs(router): reword relativeLinkResolution docs to not mention version numbers (#26991)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+58.sha-f0989b7.with-local-changes
+ * @license Angular v8.0.0-beta.5+59.sha-395fb18.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.5+58.sha-f0989b7.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.5+59.sha-395fb18.with-local-changes');
 
     /**
      * @license
