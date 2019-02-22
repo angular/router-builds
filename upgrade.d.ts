eb754e0e3430eac1104853d871d83cefc43470c2
Igor Minar <igor@angular.io>
2019-02-22 07:48:36 +0000

    599e2e2 feat(docs-infra): add meta[name]=Description to angular.io's index.html (#28916)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+28.sha-94223a0.with-local-changes
+ * @license Angular v8.0.0-beta.5+32.sha-599e2e2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
