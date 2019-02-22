f011887e23e66c30848eea3602d3ff2bb577a434
Igor Minar <igor@angular.io>
2019-02-22 06:40:00 +0000

    94223a0 ci: update circleci post checkout to v2 syntax (#28472)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+27.sha-65d839d.with-local-changes
+ * @license Angular v8.0.0-beta.5+28.sha-94223a0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.5+27.sha-65d839d.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.5+28.sha-94223a0.with-local-changes');
 
     /**
      * @license
