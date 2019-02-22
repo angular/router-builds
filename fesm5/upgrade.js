f3c6120cb11719332aba628e33bb52bd9f2fd199
Igor Minar <igor@angular.io>
2019-02-22 06:33:54 +0000

    9b77434 ci: update circleci post checkout to v2 syntax (#28472)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+13.sha-ce219cc.with-local-changes
+ * @license Angular v7.2.6+14.sha-9b77434.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
