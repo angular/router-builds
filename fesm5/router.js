d190f8f05befd9ddb2e5b29d6831cdecba02108b
Ben Lesh <ben@benlesh.com>
2019-02-22 23:39:53 +0000

    f75acbd fix(compiler): use correct variable in invalid function (#28656)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+59.sha-395fb18.with-local-changes
+ * @license Angular v8.0.0-beta.5+60.sha-f75acbd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5723,7 +5723,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+59.sha-395fb18.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+60.sha-f75acbd.with-local-changes');
 
 /**
  * @license
