ac28be806e8e7b6b2647c36aee1238c6e8be2fc2
Ben Lesh <ben@benlesh.com>
2019-02-22 17:10:24 +0000

    2270467 docs: fix ngSwitch example (#28899)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+18.sha-8efda5b.with-local-changes
+ * @license Angular v7.2.6+19.sha-2270467.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5805,7 +5805,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.6+18.sha-8efda5b.with-local-changes');
+    var VERSION = new core.Version('7.2.6+19.sha-2270467.with-local-changes');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+18.sha-8efda5b.with-local-changes
+ * @license Angular v7.2.6+19.sha-2270467.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
