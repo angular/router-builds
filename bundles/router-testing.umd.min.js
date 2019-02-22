bb10adb11a3850ebdaaacc7505f5c0e2d90f6401
Ben Lesh <ben@benlesh.com>
2019-02-22 21:37:51 +0000

    c1392ce feat(ivy): produce and consume ES2015 re-exports for NgModule re-exports (#28852)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+57.sha-c64b13e.with-local-changes
+ * @license Angular v8.0.0-beta.5+49.sha-c1392ce.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+57.sha-c64b13e.with-local-changes
+ * @license Angular v8.0.0-beta.5+49.sha-c1392ce.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.5+57.sha-c64b13e.with-local-changes');
+const VERSION = new Version('8.0.0-beta.5+49.sha-c1392ce.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
