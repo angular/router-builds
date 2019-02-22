55317ff96c77ffba4f844580bf30018556bfb55c
Igor Minar <igor@angular.io>
2019-02-22 02:16:50 +0000

    1145bdb fix(bazel): Pin browsers for schematics (#28913)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+22.sha-f4d6525.with-local-changes
+ * @license Angular v8.0.0-beta.5+23.sha-1145bdb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
