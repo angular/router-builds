7803a9d0b709e7a5fae99d016bdef92745a7e814
Igor Minar <igor@angular.io>
2019-02-22 05:13:51 +0000

    ce219cc docs(common): update docs for HttpClient methods (#26143)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+12.sha-53bbb01.with-local-changes
+ * @license Angular v7.2.6+13.sha-ce219cc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+12.sha-53bbb01.with-local-changes
+ * @license Angular v7.2.6+13.sha-ce219cc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5717,7 +5717,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.6+12.sha-53bbb01.with-local-changes');
+var VERSION = new Version('7.2.6+13.sha-ce219cc.with-local-changes');
 
 /**
  * @license
