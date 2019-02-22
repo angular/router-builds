9ea49445a2c649a2f86638b173fa8307b062367f
Ben Lesh <ben@benlesh.com>
2019-02-22 17:14:38 +0000

    cbfc1f2 docs: fix ngSwitch example (#28899)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+35.sha-c7fe3a9.with-local-changes
+ * @license Angular v8.0.0-beta.5+36.sha-cbfc1f2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.5+35.sha-c7fe3a9.with-local-changes');
+const VERSION = new Version('8.0.0-beta.5+36.sha-cbfc1f2.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
