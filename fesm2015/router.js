3af4cc0fb912e2182cd9c2d27eaef7ac6e94c544
Ben Lesh <ben@benlesh.com>
2019-02-22 18:17:05 +0000

    82820b0 ci: run saucelabs legacy tests every hour (#28903)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+38.sha-1778bd3.with-local-changes
+ * @license Angular v8.0.0-beta.5+39.sha-82820b0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7529,7 +7529,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.5+38.sha-1778bd3.with-local-changes');
+const VERSION = new Version('8.0.0-beta.5+39.sha-82820b0.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
