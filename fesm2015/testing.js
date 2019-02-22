cbf277a987be36f4099fe92910ee2f50e17db912
Ben Lesh <ben@benlesh.com>
2019-02-22 20:35:28 +0000

    fafabc0 build(bazel): use http_server rule from rules_nodejs (#28905)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+45.sha-43181ea.with-local-changes
+ * @license Angular v8.0.0-beta.5+47.sha-fafabc0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
