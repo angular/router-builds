61fc7905e9a525b4d1e529e7703d5585c25e7495
Igor Minar <igor@angular.io>
2019-02-22 02:18:55 +0000

    9defc00 build(bazel): cleanup the jasmine bootstrap code (#28906)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+23.sha-1145bdb.with-local-changes
+ * @license Angular v8.0.0-beta.5+24.sha-9defc00.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.5+23.sha-1145bdb.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.5+24.sha-9defc00.with-local-changes');
 
     /**
      * @license
