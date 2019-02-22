8963f316bcb8fd8a4bd9979ccadf010011ca2a61
Igor Minar <igor@angular.io>
2019-02-22 02:21:07 +0000

    32ae84d fixup! fix(ivy): incorrectly remapping certain properties that refer to inputs (#28765)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+24.sha-9defc00.with-local-changes
+ * @license Angular v8.0.0-beta.5+26.sha-32ae84d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+24.sha-9defc00.with-local-changes
+ * @license Angular v8.0.0-beta.5+26.sha-32ae84d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.5+24.sha-9defc00.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.5+26.sha-32ae84d.with-local-changes');
 
     /**
      * @license
