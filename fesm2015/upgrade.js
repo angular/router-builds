2bc6a57a2d4c0da7ab1ce0dc6e9881429be23fd0
Ben Lesh <ben@benlesh.com>
2019-02-22 20:21:54 +0000

    9dac04f fix(ivy): ngOnChanges hooks should be inherited from grand-superclasses (#28888)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+43.sha-7f3e3a8.with-local-changes
+ * @license Angular v8.0.0-beta.5+44.sha-9dac04f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
