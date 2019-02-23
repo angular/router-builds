71ad53cc6f040b6d28743ac066509d851db96060
Ben Lesh <ben@benlesh.com>
2019-02-23 00:44:39 +0000

    10ba91b build: fix saucelabs_test cronjob in CI (#28939)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+60.sha-f75acbd.with-local-changes
+ * @license Angular v8.0.0-beta.5+61.sha-10ba91b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.5+60.sha-f75acbd.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.5+61.sha-10ba91b.with-local-changes');
 
     /**
      * @license
