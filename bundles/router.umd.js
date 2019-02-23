76e0148f2582dd03715c7dff687926d3349153fa
Igor Minar <igor@angular.io>
2019-02-23 01:52:12 +0000

    350802b build: update to latest bazel rules (#28931)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+61.sha-10ba91b.with-local-changes
+ * @license Angular v8.0.0-beta.5+62.sha-350802b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.5+61.sha-10ba91b.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.5+62.sha-350802b.with-local-changes');
 
     /**
      * @license
