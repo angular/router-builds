c20a94e0bc07cb9a3adcbdc665c21885fe1601b7
Ben Lesh <ben@benlesh.com>
2019-02-24 21:01:34 +0000

    f79cd59 docs(core): export the `...Decorator` interfaces to ensure they are documented (#28836)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+62.sha-350802b.with-local-changes
+ * @license Angular v8.0.0-beta.5+63.sha-f79cd59.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
