1285586df7617811b8c7f05d7fc0aeffedcef85c
Ben Lesh <ben@benlesh.com>
2019-02-25 18:50:49 +0000

    d374787 ci: add bazel_repository_cache to CircleCI workspace (#28935) (#28965)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+30.sha-1e97d51.with-local-changes
+ * @license Angular v7.2.6+34.sha-d374787.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+30.sha-1e97d51.with-local-changes
+ * @license Angular v7.2.6+34.sha-d374787.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5717,7 +5717,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.6+30.sha-1e97d51.with-local-changes');
+var VERSION = new Version('7.2.6+34.sha-d374787.with-local-changes');
 
 /**
  * @license
