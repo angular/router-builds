0e390013b6880da7c68c0beded9073041f845845
Ben Lesh <ben@benlesh.com>
2019-02-25 18:37:45 +0000

    7e1e00c docs: add ngVikings 2019 to events page (#28501)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+28.sha-481f4b7.with-local-changes
+ * @license Angular v7.2.6+29.sha-7e1e00c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+28.sha-481f4b7.with-local-changes
+ * @license Angular v7.2.6+29.sha-7e1e00c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5805,7 +5805,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.6+28.sha-481f4b7.with-local-changes');
+    var VERSION = new core.Version('7.2.6+29.sha-7e1e00c.with-local-changes');
 
     /**
      * @license
