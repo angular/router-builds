df17d9da1ba4a6389100feea6e14a1290d8e0666
Ben Lesh <ben@benlesh.com>
2019-02-25 21:22:42 +0000

    642c015 docs: modify the descendant spelling and remove multiple times parent's word (#27886)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+37.sha-38b5ed0.with-local-changes
+ * @license Angular v7.2.6+36.sha-642c015.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5717,7 +5717,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.6+37.sha-38b5ed0.with-local-changes');
+var VERSION = new Version('7.2.6+36.sha-642c015.with-local-changes');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+37.sha-38b5ed0.with-local-changes
+ * @license Angular v7.2.6+36.sha-642c015.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
