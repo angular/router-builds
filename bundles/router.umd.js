6445a9c596b7e88352ee108e689b07b1d9d34fe4
Ben Lesh <ben@benlesh.com>
2019-02-25 18:53:54 +0000

    65d3dda docs: add Human Coders trainings in resources.json (#28840) PR Close #28840

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+72.sha-ef1b9e6.with-local-changes
+ * @license Angular v8.0.0-beta.5+71.sha-65d3dda.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.5+72.sha-ef1b9e6.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.5+71.sha-65d3dda.with-local-changes');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+72.sha-ef1b9e6.with-local-changes
+ * @license Angular v8.0.0-beta.5+71.sha-65d3dda.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
