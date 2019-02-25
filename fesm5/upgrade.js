8c3408a7fcbd4d7deeb251ce686f5b62f08790ec
Ben Lesh <ben@benlesh.com>
2019-02-25 21:22:59 +0000

    7a11242 docs: add Nebular to UI Components section in resources (#28838) PR Close #28838

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+73.sha-65de0d6.with-local-changes
+ * @license Angular v8.0.0-beta.5+74.sha-7a11242.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
