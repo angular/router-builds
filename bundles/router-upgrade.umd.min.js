7e3be0554574ce15b3f675d4c09baeaf8ed24a96
Ben Lesh <ben@benlesh.com>
2019-02-25 21:13:24 +0000

    0977d95 docs(core): export the `...Decorator` interfaces to ensure they are documented (#28836) (#28961)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+34.sha-d374787.with-local-changes
+ * @license Angular v7.2.6+35.sha-0977d95.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
