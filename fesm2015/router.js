c1f04cfef8957bb75363dc81c506db53b1e67edd
Ben Lesh <ben@benlesh.com>
2019-02-25 18:52:39 +0000

    65d3dda docs: add Human Coders trainings in resources.json (#28840) PR Close #28840

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+70.sha-0b8aad2.with-local-changes
+ * @license Angular v8.0.0-beta.5+71.sha-65d3dda.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.5+70.sha-0b8aad2.with-local-changes');
+const VERSION = new Version('8.0.0-beta.5+71.sha-65d3dda.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
