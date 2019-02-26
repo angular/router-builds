2d3e68f67813fff78eedd3527c55b3c1e43f7d1b
Ben Lesh <ben@benlesh.com>
2019-02-26 21:48:55 +0000

    fdcf877 fix(core): traverse and sanitize content of unsafe elements (#28804)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+39.sha-0e73657.with-local-changes
+ * @license Angular v7.2.6+41.sha-fdcf877.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+39.sha-0e73657.with-local-changes
+ * @license Angular v7.2.6+41.sha-fdcf877.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5717,7 +5717,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.6+39.sha-0e73657.with-local-changes');
+var VERSION = new Version('7.2.6+41.sha-fdcf877.with-local-changes');
 
 /**
  * @license
