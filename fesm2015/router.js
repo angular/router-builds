5c420575e0b53bdfb8dfe3a92a9cc5529d315658
Ben Lesh <ben@benlesh.com>
2019-02-26 17:09:44 +0000

    846c431 docs: re-phrase note in architecture guide for clarity (#28543)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+78.sha-edb6c2d.with-local-changes
+ * @license Angular v8.0.0-beta.5+79.sha-846c431.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7529,7 +7529,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.5+78.sha-edb6c2d.with-local-changes');
+const VERSION = new Version('8.0.0-beta.5+79.sha-846c431.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
