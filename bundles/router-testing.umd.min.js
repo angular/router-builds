985209efc48f5d6e6ae1f01b5203afef7e99185e
Ben Lesh <ben@benlesh.com>
2019-02-26 17:08:16 +0000

    846c431 docs: re-phrase note in architecture guide for clarity (#28543)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+78.sha-edb6c2d.with-local-changes
+ * @license Angular v8.0.0-beta.5+79.sha-846c431.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
