7d954a52dfe9e9194bf19349b7e947c34c337d82
Miško Hevery <misko@hevery.com>
2019-02-26 02:24:53 +0000

    edb6c2d feat(bazel): add ts_config extending support for ng_module (#21883)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+77.sha-3cb497c.with-local-changes
+ * @license Angular v8.0.0-beta.5+78.sha-edb6c2d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.5+77.sha-3cb497c.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.5+78.sha-edb6c2d.with-local-changes');
 
     /**
      * @license
