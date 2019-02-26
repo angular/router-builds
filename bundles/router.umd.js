7b76b0ee7990b36152bb6f6faf322e73ae45595b
Miško Hevery <misko@hevery.com>
2019-02-26 02:23:40 +0000

    3cb497c refactor(ivy): simplify differentiation of LView, `RNode`, `LView`, `LContainer`, `StylingContext` (#28947)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+75.sha-1930e8a.with-local-changes
+ * @license Angular v8.0.0-beta.5+77.sha-3cb497c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.5+75.sha-1930e8a.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.5+77.sha-3cb497c.with-local-changes');
 
     /**
      * @license
