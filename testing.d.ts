c7b9be6db57aa21d4a8187db05271a02de7c818a
Ben Lesh <ben@benlesh.com>
2019-02-26 19:20:33 +0000

    25a2fef fix(ivy): ViewRef.rootNodes not including projected nodes (#28951)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+81.sha-dbd9ecf.with-local-changes
+ * @license Angular v8.0.0-beta.5+82.sha-25a2fef.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
