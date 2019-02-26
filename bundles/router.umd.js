f780b83c615a821e90ce813306159635951fc304
Ben Lesh <ben@benlesh.com>
2019-02-26 19:23:05 +0000

    25a2fef fix(ivy): ViewRef.rootNodes not including projected nodes (#28951)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+81.sha-dbd9ecf.with-local-changes
+ * @license Angular v8.0.0-beta.5+82.sha-25a2fef.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.5+81.sha-dbd9ecf.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.5+82.sha-25a2fef.with-local-changes');
 
     /**
      * @license
