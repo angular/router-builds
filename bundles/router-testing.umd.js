1a54cea2ad68490bcba53ef5f01082266168547a
Ben Lesh <ben@benlesh.com>
2019-02-26 17:10:32 +0000

    f01247f docs: add ivy opt-in docs (#28569)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+79.sha-846c431.with-local-changes
+ * @license Angular v8.0.0-beta.5+80.sha-f01247f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
