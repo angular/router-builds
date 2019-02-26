26018e156643db7fcec9b00f7ed4d6fb6774f02b
Ben Lesh <ben@benlesh.com>
2019-02-26 21:54:27 +0000

    262ba67 fix(core): traverse and sanitize content of unsafe elements (#28804)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+84.sha-dc9f0af.with-local-changes
+ * @license Angular v8.0.0-beta.5+85.sha-262ba67.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
