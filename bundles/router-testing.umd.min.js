e2c49243f749a47d4ec23a8ab6343afca8ed1deb
Ben Lesh <ben@benlesh.com>
2019-02-26 21:52:04 +0000

    dc9f0af fix(ivy): static host classes and styles should work on root component (#28926)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+82.sha-25a2fef.with-local-changes
+ * @license Angular v8.0.0-beta.5+84.sha-dc9f0af.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+82.sha-25a2fef.with-local-changes
+ * @license Angular v8.0.0-beta.5+84.sha-dc9f0af.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.5+82.sha-25a2fef.with-local-changes');
+const VERSION = new Version('8.0.0-beta.5+84.sha-dc9f0af.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
