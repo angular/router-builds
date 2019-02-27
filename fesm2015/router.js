aa5634a0ccf18ee3ee358542a7a0c2c2420bbd61
Ben Lesh <ben@benlesh.com>
2019-02-27 01:22:26 +0000

    41de05e test(ivy): update select root causes (#28989)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+3.sha-dad5a25.with-local-changes
+ * @license Angular v8.0.0-beta.6+4.sha-41de05e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7529,7 +7529,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.6+3.sha-dad5a25.with-local-changes');
+const VERSION = new Version('8.0.0-beta.6+4.sha-41de05e.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
