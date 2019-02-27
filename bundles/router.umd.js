b440a6d2f7a20c67a7e041cb7a6fb6840250d0df
Ben Lesh <ben@benlesh.com>
2019-02-27 19:03:15 +0000

    034de06 fix(ivy): avoid duplicate i18n consts to be present in generated output (#28967)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+10.sha-2dd44d7.with-local-changes
+ * @license Angular v8.0.0-beta.6+11.sha-034de06.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.6+10.sha-2dd44d7.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.6+11.sha-034de06.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+10.sha-2dd44d7.with-local-changes
+ * @license Angular v8.0.0-beta.6+11.sha-034de06.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
