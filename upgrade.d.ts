14e728920b72b64c2cb8a40bab95ed3596e0e182
Ben Lesh <ben@benlesh.com>
2019-02-27 17:08:23 +0000

    7b944c4 style: format (#28995)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+4.sha-41de05e.with-local-changes
+ * @license Angular v8.0.0-beta.6+6.sha-7b944c4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
