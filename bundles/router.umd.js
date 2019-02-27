e05609e3310c20cd31964d3507a8b0fd8a3a6821
Ben Lesh <ben@benlesh.com>
2019-02-27 01:07:13 +0000

    f380263 release: cut the v8.0.0-beta.6 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+86.sha-5a4c402.with-local-changes
+ * @license Angular v8.0.0-beta.6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.5+86.sha-5a4c402.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.6.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+86.sha-5a4c402.with-local-changes
+ * @license Angular v8.0.0-beta.6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
