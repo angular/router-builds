3d0fca277af90b4ca12b835fa8d522bafda2786d
Ben Lesh <ben@benlesh.com>
2019-02-27 20:19:06 +0000

    86981b3 fix(animations): ensure `position` and `display` styles are handled outside of keyframes/web-animations (#28911)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+2.sha-feec963.with-local-changes
+ * @license Angular v7.2.7+3.sha-86981b3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
