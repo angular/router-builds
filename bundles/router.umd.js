3d61a6a8b91d0c2c3989fcf43f7bc4738555c111
Ben Lesh <ben@benlesh.com>
2019-02-27 18:50:50 +0000

    b8d8749 test(ivy): record root cause of CdkVirtualScrollViewport failures (#28999)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+9.sha-f16fca4.with-local-changes
+ * @license Angular v8.0.0-beta.6+8.sha-b8d8749.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.6+9.sha-f16fca4.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.6+8.sha-b8d8749.with-local-changes');
 
     /**
      * @license
