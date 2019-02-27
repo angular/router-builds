4d5ed1b61bd7eccf6c5de036a657becc76a84567
Andrew Kushnir <akushnir@google.com>
2019-02-27 22:32:27 +0000

    efa10e3 fix(ivy): resolve forwardRef when analyzing NgModule (#28942)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+18.sha-91a161a.with-local-changes
+ * @license Angular v8.0.0-beta.6+17.sha-efa10e3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
