5de6ca7dbcba7cc186ed4a176c41064d65252c72
Ben Lesh <ben@benlesh.com>
2019-02-27 20:24:02 +0000

    827e89c feat(ivy): support inline <style> and <link> tags in components (#28997)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+15.sha-a6ae759.with-local-changes
+ * @license Angular v8.0.0-beta.6+14.sha-827e89c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
