ce1fa72dc41917a68d78b1dd1ecc952bb8aac417
Ben Lesh <ben@benlesh.com>
2019-02-27 18:59:13 +0000

    2dd44d7 ci(core): fix `legacy-unit-tests-saucelabs` job (#29009)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+8.sha-b8d8749.with-local-changes
+ * @license Angular v8.0.0-beta.6+10.sha-2dd44d7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
