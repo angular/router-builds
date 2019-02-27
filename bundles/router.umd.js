b55eafb41a9d2e311206f4d1e4edab27a01697e4
Ben Lesh <ben@benlesh.com>
2019-02-27 17:16:50 +0000

    d127d05 fix(ivy): correctly resolve shorthand property declarations (#28936)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+6.sha-7b944c4.with-local-changes
+ * @license Angular v8.0.0-beta.6+7.sha-d127d05.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.6+6.sha-7b944c4.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.6+7.sha-d127d05.with-local-changes');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+6.sha-7b944c4.with-local-changes
+ * @license Angular v8.0.0-beta.6+7.sha-d127d05.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
