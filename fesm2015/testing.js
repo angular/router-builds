0a30568fc4c779d04300dae7235ffca7cea194c9
Ben Lesh <ben@benlesh.com>
2019-02-27 18:51:23 +0000

    f16fca4 build(docs-infra): upgrade cli command docs sources to 3b1fe6437 (#29003)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+7.sha-d127d05.with-local-changes
+ * @license Angular v8.0.0-beta.6+9.sha-f16fca4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
