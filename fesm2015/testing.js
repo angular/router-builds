fe901c0e0b18972d9dc4a9592dc243bd4a740a2e
Igor Minar <igor@angular.io>
2019-02-28 20:16:29 +0000

    03d2e5c refactor: Consistently use index access on index signature types. (#28937)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+19.sha-772b24c.with-local-changes
+ * @license Angular v8.0.0-beta.6+25.sha-03d2e5c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
