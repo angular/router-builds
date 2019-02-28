f8b5262b00b5faafb268df6742fe189e74e92819
Andrew Kushnir <akushnir@google.com>
2019-02-28 22:37:27 +0000

    0bc26fc ci: remove dev-infra as owner on material-ci folder (#28990)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+37.sha-c532646.with-local-changes
+ * @license Angular v8.0.0-beta.6+38.sha-0bc26fc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
