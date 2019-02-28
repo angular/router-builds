52fb7aa2d6cf5877352c29940e92f016ad1dd563
Igor Minar <igor@angular.io>
2019-02-28 11:05:14 +0000

    a54a752 build: add VSCode extension recommendations (#28784)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+3.sha-86981b3.with-local-changes
+ * @license Angular v7.2.7+5.sha-a54a752.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7277,7 +7277,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.7+3.sha-86981b3.with-local-changes');
+const VERSION = new Version('7.2.7+5.sha-a54a752.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+3.sha-86981b3.with-local-changes
+ * @license Angular v7.2.7+5.sha-a54a752.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5717,7 +5717,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.7+3.sha-86981b3.with-local-changes');
+var VERSION = new Version('7.2.7+5.sha-a54a752.with-local-changes');
 
 /**
  * @license
