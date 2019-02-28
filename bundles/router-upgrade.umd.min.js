844b22e404ac10ef0cb25e784877adb9153e5f9b
Igor Minar <igor@angular.io>
2019-02-28 11:15:20 +0000

    9226b42 style: enforce buildifier lint rules (#29019)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+6.sha-b06847f.with-local-changes
+ * @license Angular v7.2.7+7.sha-9226b42.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
