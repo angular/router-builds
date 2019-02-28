889d07491476591be8652a9beff62ed49c63d226
Andrew Kushnir <akushnir@google.com>
2019-02-28 19:02:10 +0000

    d207c48 ci: speed up `publish_artifacts` CircleCI job for PRs (#29028)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+26.sha-5fdf24e.with-local-changes
+ * @license Angular v8.0.0-beta.6+27.sha-d207c48.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.6+26.sha-5fdf24e.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.6+27.sha-d207c48.with-local-changes');
 
     /**
      * @license
