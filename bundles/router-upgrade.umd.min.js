428f3f4737fa2a109118df828c0040badc7a1378
Igor Minar <igor@angular.io>
2019-02-28 11:09:31 +0000

    2b974d4 build: add VSCode extension recommendations (#28784)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+19.sha-772b24c.with-local-changes
+ * @license Angular v8.0.0-beta.6+24.sha-2b974d4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
