f07fef50cfe2eb55467b609ca03b7c351193fc73
Andrew Kushnir <akushnir@google.com>
2019-02-28 19:00:51 +0000

    d207c48 ci: speed up `publish_artifacts` CircleCI job for PRs (#29028)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+26.sha-5fdf24e.with-local-changes
+ * @license Angular v8.0.0-beta.6+27.sha-d207c48.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
