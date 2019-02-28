b8cdde4acab653006d3a14a8f9c5e2588957af0a
Igor Minar <igor@angular.io>
2019-02-28 11:13:23 +0000

    03d2e5c refactor: Consistently use index access on index signature types. (#28937)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+22.sha-cb20b3b.with-local-changes
+ * @license Angular v8.0.0-beta.6+25.sha-03d2e5c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.6+22.sha-cb20b3b.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.6+25.sha-03d2e5c.with-local-changes');
 
     /**
      * @license
