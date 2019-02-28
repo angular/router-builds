4372cadf5ddc050379587a6289937a569cf14160
Andrew Kushnir <akushnir@google.com>
2019-02-28 00:31:06 +0000

    772b24c fix(ivy): avoid missing imports for types that can be represented as values (#28941)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+12.sha-34bdebc.with-local-changes
+ * @license Angular v8.0.0-beta.6+19.sha-772b24c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.6+12.sha-34bdebc.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.6+19.sha-772b24c.with-local-changes');
 
     /**
      * @license
