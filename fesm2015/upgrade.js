0fe97ea82cf132fabe933546a7ae20b1be1fe846
Andrew Kushnir <akushnir@google.com>
2019-03-01 23:37:43 +0000

    2f49a23 test(animations): fix unit-based delays within the animation DSL (#28993)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+15.sha-c6eaaf5.with-local-changes
+ * @license Angular v7.2.7+16.sha-2f49a23.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
