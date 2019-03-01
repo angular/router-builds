46c4dde7a7644b07e761804404df7b0eacda71f3
Andrew Kushnir <akushnir@google.com>
2019-03-01 18:39:14 +0000

    b5629d9 fix(bazel): api extractor don't generate tsdoc metadata (#29023)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+39.sha-ac76e5d.with-local-changes
+ * @license Angular v8.0.0-beta.6+40.sha-b5629d9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
