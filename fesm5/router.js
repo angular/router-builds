c4169d98996f877b3e324559e8c69181d017da63
Andrew Kushnir <akushnir@google.com>
2019-03-01 18:37:55 +0000

    b5629d9 fix(bazel): api extractor don't generate tsdoc metadata (#29023)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+39.sha-ac76e5d.with-local-changes
+ * @license Angular v8.0.0-beta.6+40.sha-b5629d9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5723,7 +5723,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+39.sha-ac76e5d.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+40.sha-b5629d9.with-local-changes');
 
 /**
  * @license
