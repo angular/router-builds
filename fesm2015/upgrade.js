5e41728c82f486d608e6d1d07fb5bd0679278420
Andrew Kushnir <akushnir@google.com>
2019-03-02 03:20:35 +0000

    dcafdde fix(ivy): change for-of to forEach for pipes represented with Map (#29068)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+50.sha-c0757d1.with-local-changes
+ * @license Angular v8.0.0-beta.6+51.sha-dcafdde.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
