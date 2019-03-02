1eb564d13cc6c8e634d80e8c6044e8d427e03557
Andrew Kushnir <akushnir@google.com>
2019-03-02 01:11:29 +0000

    c0757d1 fix(ivy): attached flag not being reset when view is destroyed (#29064)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+49.sha-a06824a.with-local-changes
+ * @license Angular v8.0.0-beta.6+50.sha-c0757d1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.6+49.sha-a06824a.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.6+50.sha-c0757d1.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+49.sha-a06824a.with-local-changes
+ * @license Angular v8.0.0-beta.6+50.sha-c0757d1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
