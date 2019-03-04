9c372f1a7de6f260957425ccba640136527b795c
Andrew Kushnir <akushnir@google.com>
2019-03-04 20:08:09 +0000

    95989a1 docs: fix and add decorator api doc (#28986)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+64.sha-48214e2.with-local-changes
+ * @license Angular v8.0.0-beta.6+63.sha-95989a1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7529,7 +7529,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.6+64.sha-48214e2.with-local-changes');
+const VERSION = new Version('8.0.0-beta.6+63.sha-95989a1.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
