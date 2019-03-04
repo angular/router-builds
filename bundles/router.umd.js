842f6333a84ca592733221d04317539600ad1dbe
Andrew Kushnir <akushnir@google.com>
2019-03-04 18:40:52 +0000

    d2f015f docs: fix minor typo (#29100)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+60.sha-c875851.with-local-changes
+ * @license Angular v8.0.0-beta.6+59.sha-d2f015f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.6+60.sha-c875851.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.6+59.sha-d2f015f.with-local-changes');
 
     /**
      * @license
