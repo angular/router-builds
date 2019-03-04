259366a31e9adde51348cefc91f3a877fe4b1f3c
Andrew Kushnir <akushnir@google.com>
2019-03-04 21:52:17 +0000

    78adcfe fix(ivy): ensure static styling is properly inherited into child components (#29015)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+63.sha-95989a1.with-local-changes
+ * @license Angular v8.0.0-beta.6+65.sha-78adcfe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
