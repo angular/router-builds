6a65e4da5cf2d451b34aed193bf0814a3db51f42
Andrew Kushnir <akushnir@google.com>
2019-03-04 22:59:10 +0000

    04cf4ef test(ivy): remove ngComponentOutlet example with a lazy-loaded NgModule (#29094)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+66.sha-aa57bdb.with-local-changes
+ * @license Angular v8.0.0-beta.6+67.sha-04cf4ef.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
