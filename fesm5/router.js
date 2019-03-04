59399c306baed017419e1f8adecc54c232a92fe4
Andrew Kushnir <akushnir@google.com>
2019-03-04 18:25:44 +0000

    a352b73 test(ivy): update root causes for drag drop (#29080)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+53.sha-fa82d2d.with-local-changes
+ * @license Angular v8.0.0-beta.6+54.sha-a352b73.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5749,7 +5749,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+53.sha-fa82d2d.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+54.sha-a352b73.with-local-changes');
 
 /**
  * @license
