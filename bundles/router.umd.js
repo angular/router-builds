cacd5ed243090b7104c37a354faee89ed4d4addf
Andrew Kushnir <akushnir@google.com>
2019-03-04 21:04:55 +0000

    95989a1 docs: fix and add decorator api doc (#28986)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+33.sha-ea09430.with-local-changes
+ * @license Angular v8.0.0-beta.6+63.sha-95989a1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.6+33.sha-ea09430.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.6+63.sha-95989a1.with-local-changes');
 
     /**
      * @license
