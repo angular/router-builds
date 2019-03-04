1d7c03b254dccb83b6b993b0128c1499f1e914ba
Andrew Kushnir <akushnir@google.com>
2019-03-04 18:42:11 +0000

    d2f015f docs: fix minor typo (#29100)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+60.sha-c875851.with-local-changes
+ * @license Angular v8.0.0-beta.6+59.sha-d2f015f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
