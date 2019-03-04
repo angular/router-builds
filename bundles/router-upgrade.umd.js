92f0b8392505e5d1f92427f98fee0d4891c9077d
Andrew Kushnir <akushnir@google.com>
2019-03-04 18:51:11 +0000

    cf916a0 docs(core): tidy up the description of `resolveComponentResources()` (#28055) (#28736)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+20.sha-dfb331c.with-local-changes
+ * @license Angular v7.2.7+23.sha-cf916a0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
