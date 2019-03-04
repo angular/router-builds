29418b5f31379dae93c93a45670bfc2ddcadcea9
Andrew Kushnir <akushnir@google.com>
2019-03-04 18:23:12 +0000

    83ba587 test(ivy): update sidenav root causes (#29073)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+51.sha-dcafdde.with-local-changes
+ * @license Angular v8.0.0-beta.6+52.sha-83ba587.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+51.sha-dcafdde.with-local-changes
+ * @license Angular v8.0.0-beta.6+52.sha-83ba587.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7529,7 +7529,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.6+51.sha-dcafdde.with-local-changes');
+const VERSION = new Version('8.0.0-beta.6+52.sha-83ba587.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
