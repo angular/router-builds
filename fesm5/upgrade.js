114b70715f6a0b5a405a065c7212e5b8a75467e6
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:07:19 +0000

    2a25ac2 fix(router): removed obsolete TODO comment (#29085)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+27.sha-5669333.with-local-changes
+ * @license Angular v7.2.7+28.sha-2a25ac2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
