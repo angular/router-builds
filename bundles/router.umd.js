aad181517bcc601155fae432bd2fa1adc12241a2
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:00:56 +0000

    79e2ca0 fix(platform-server): update minimum domino version to latest released (#28893)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+69.sha-5fded9f.with-local-changes
+ * @license Angular v8.0.0-beta.6+71.sha-79e2ca0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.6+69.sha-5fded9f.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.6+71.sha-79e2ca0.with-local-changes');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+69.sha-5fded9f.with-local-changes
+ * @license Angular v8.0.0-beta.6+71.sha-79e2ca0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
