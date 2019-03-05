d73b8d429ec67ae014bc68971dcd0a844b2cb11a
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:09:53 +0000

    24a1e14 refactor(language-service): clean up imports language-service and misspellings in public API (#29097)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+29.sha-2256920.with-local-changes
+ * @license Angular v7.2.7+30.sha-24a1e14.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
