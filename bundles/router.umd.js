df8e36981532d9abdaf02ee6b8caac221440a613
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:09:47 +0000

    7c57293 refactor(language-service): clean up imports language-service and misspellings in public API (#29097)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+74.sha-3063547.with-local-changes
+ * @license Angular v8.0.0-beta.6+75.sha-7c57293.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.6+74.sha-3063547.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.6+75.sha-7c57293.with-local-changes');
 
     /**
      * @license
