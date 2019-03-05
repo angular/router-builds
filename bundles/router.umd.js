a84ebfbdf21b092944628a4cadccb5feceadf833
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:13:14 +0000

    4486dab docs: order upcoming events in chronological order (#29115)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+76.sha-01577b0.with-local-changes
+ * @license Angular v8.0.0-beta.6+77.sha-4486dab.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.6+76.sha-01577b0.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.6+77.sha-4486dab.with-local-changes');
 
     /**
      * @license
