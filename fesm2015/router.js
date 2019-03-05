c20eb3c7781918a9fcf61365a415712a3eccdf9e
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:02:28 +0000

    79e2ca0 fix(platform-server): update minimum domino version to latest released (#28893)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+69.sha-5fded9f.with-local-changes
+ * @license Angular v8.0.0-beta.6+71.sha-79e2ca0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7529,7 +7529,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.6+69.sha-5fded9f.with-local-changes');
+const VERSION = new Version('8.0.0-beta.6+71.sha-79e2ca0.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
