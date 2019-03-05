0fb1b32c27d26cc7677d45f21da48f8e343a2d10
Andrew Kushnir <akushnir@google.com>
2019-03-05 01:07:38 +0000

    866d500 fix(ivy): copy top-level comments into generated factory shims (#29065)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+63.sha-95989a1.with-local-changes
+ * @license Angular v8.0.0-beta.6+68.sha-866d500.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
