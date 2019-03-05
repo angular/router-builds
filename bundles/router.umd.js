9d17bffe5daa83fe799339c37971108bf93d587d
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:06:13 +0000

    72ecc45 fix(router): removed obsolete TODO comment (#29085)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+72.sha-7d17496.with-local-changes
+ * @license Angular v8.0.0-beta.6+73.sha-72ecc45.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.6+72.sha-7d17496.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.6+73.sha-72ecc45.with-local-changes');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+72.sha-7d17496.with-local-changes
+ * @license Angular v8.0.0-beta.6+73.sha-72ecc45.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
