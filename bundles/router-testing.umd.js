aad650a7044b180909e76df1bc9c6d50e3c87eba
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:09:44 +0000

    3063547 docs(router): align examples in interfaces docs (#29093)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+73.sha-72ecc45.with-local-changes
+ * @license Angular v8.0.0-beta.6+74.sha-3063547.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+73.sha-72ecc45.with-local-changes
+ * @license Angular v8.0.0-beta.6+74.sha-3063547.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5749,7 +5749,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+73.sha-72ecc45.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+74.sha-3063547.with-local-changes');
 
 /**
  * @license
