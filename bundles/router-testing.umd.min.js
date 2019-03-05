df8d41bf003e0f39f3b929021a7bdc9b952848b6
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:10:49 +0000

    01577b0 ci: bazel saucelabs test job does not setup circleci bazelrc (#29106)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+75.sha-7c57293.with-local-changes
+ * @license Angular v8.0.0-beta.6+76.sha-01577b0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
