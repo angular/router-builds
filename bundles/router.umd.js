7aa5d7fd87782d4bdfe5e86ef4a27f3f7b6412fb
Andrew Kushnir <akushnir@google.com>
2019-03-06 19:24:38 +0000

    0bd4261 test(ivy): diagnose root causes of failures in MatSnackBar (#29134)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+4.sha-7102ea8.with-local-changes
+ * @license Angular v8.0.0-beta.7+9.sha-0bd4261.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.7+4.sha-7102ea8.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.7+9.sha-0bd4261.with-local-changes');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+4.sha-7102ea8.with-local-changes
+ * @license Angular v8.0.0-beta.7+9.sha-0bd4261.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
