468507c5adbeffda3b4c925ad892d3a60511c03d
Andrew Kushnir <akushnir@google.com>
2019-03-06 21:03:39 +0000

    22ddbf4 fix(ivy): content projection should not corrupt TNode data structures (#29130)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+84.sha-25166d4.with-local-changes
+ * @license Angular v8.0.0-beta.7+7.sha-22ddbf4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
