6cf677c9fbea6d092b2eddd08ee48a93ec3de0f4
Andrew Kushnir <akushnir@google.com>
2019-03-06 19:21:54 +0000

    f96efd1 test(ivy): enable passing MatChipList tests (#29130)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+5.sha-dc6192c.with-local-changes
+ * @license Angular v8.0.0-beta.7+8.sha-f96efd1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.7+5.sha-dc6192c.with-local-changes');
+const VERSION = new Version('8.0.0-beta.7+8.sha-f96efd1.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
