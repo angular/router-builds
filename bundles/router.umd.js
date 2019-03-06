3cf061e8da61c22ac1b38fe5d94b39e5506ec2ee
Kara Erickson <karakara@google.com>
2019-03-06 23:49:15 +0000

    79fb9d4 build(docs-infra): use pinned dependencies when possible in `ng-packages-installer` (#28510)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.10+20.sha-36cbfb1
+ * @license Angular v6.1.10+26.sha-79fb9d4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5303,7 +5303,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.10+20.sha-36cbfb1');
+    var VERSION = new i0.Version('6.1.10+26.sha-79fb9d4');
 
     /**
      * @license
