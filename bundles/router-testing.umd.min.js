3b119453fc4281c5cabe90ed01104a05b2b95c9c
Andrew Kushnir <akushnir@google.com>
2019-03-06 19:23:35 +0000

    f96efd1 test(ivy): enable passing MatChipList tests (#29130)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+5.sha-dc6192c.with-local-changes
+ * @license Angular v8.0.0-beta.7+8.sha-f96efd1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
