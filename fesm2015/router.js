62e26751cfa6aa3b777634796c50522e64ca3d83
Andrew Kushnir <akushnir@google.com>
2019-03-06 19:22:14 +0000

    6b98b53 docs: update ivy opt-in flag (#29010)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+2.sha-c5d9035.with-local-changes
+ * @license Angular v8.0.0-beta.7+3.sha-6b98b53.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7471,7 +7471,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.7+2.sha-c5d9035.with-local-changes');
+const VERSION = new Version('8.0.0-beta.7+3.sha-6b98b53.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
