4ae53da6e0d0a19be810cc4967d67da40037bb63
George Kalpakas <kalpakas.g@gmail.com>
2019-03-06 00:43:44 +0000

    d8b63d0 build(compiler-cli): upgrade chokidar to latest version

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.11-a065dc2
+ * @license Angular v5.2.11-d8b63d0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_common,_angular_common_testing,_angular_core,_angular_router) { 'use strict';
 
 /**
- * @license Angular v5.2.11-a065dc2
+ * @license Angular v5.2.11-d8b63d0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.11-a065dc2
+ * @license Angular v5.2.11-d8b63d0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */ 
