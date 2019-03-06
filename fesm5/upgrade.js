6d872249f13f540638b4e6f49fe43696c2b7a99b
Andrew Kushnir <akushnir@google.com>
2019-03-06 19:21:41 +0000

    dc6192c fix(ivy): properly detect "inputs" and "outputs" field names that should be wrapped in quotes (#29126)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+3.sha-6b98b53.with-local-changes
+ * @license Angular v8.0.0-beta.7+5.sha-dc6192c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
