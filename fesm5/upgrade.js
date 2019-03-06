7c3ed5bc85afbebb246e00636805a964fbc2ffa8
Andrew Kushnir <akushnir@google.com>
2019-03-06 19:11:22 +0000

    9c1ced1 release: cut the v8.0.0-beta.7 release

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+91.sha-54286b8.with-local-changes
+ * @license Angular v8.0.0-beta.7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
