1ed17a9dbbf46ed1d5fd6a881051a073b48ef912
Andrew Kushnir <akushnir@google.com>
2019-03-06 01:06:04 +0000

    84f3dfb test(service-worker): verify that `config/schema.json` is published to npm (#27859)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+86.sha-881807d.with-local-changes
+ * @license Angular v8.0.0-beta.6+88.sha-84f3dfb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.6+86.sha-881807d.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.6+88.sha-84f3dfb.with-local-changes');
 
     /**
      * @license
