cfc615bc765e822344f993b317b492454a1cbd68
Andrew Kushnir <akushnir@google.com>
2019-03-06 18:41:09 +0000

    54286b8 docs: release notes for the v7.2.8 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+90.sha-84406e4.with-local-changes
+ * @license Angular v8.0.0-beta.6+91.sha-54286b8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.6+90.sha-84406e4.with-local-changes');
+const VERSION = new Version('8.0.0-beta.6+91.sha-54286b8.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
