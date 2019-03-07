51830cae52683ff9bbc910e6f17f727355af0bec
Kara Erickson <karakara@google.com>
2019-03-07 17:59:34 +0000

    479ae51 test(ivy): update native table root causes (#29161)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+17.sha-eccbc78.with-local-changes
+ * @license Angular v8.0.0-beta.7+18.sha-479ae51.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7471,7 +7471,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.7+17.sha-eccbc78.with-local-changes');
+const VERSION = new Version('8.0.0-beta.7+18.sha-479ae51.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
