18556a59b3fa1e653a0c0adcb07095ae75d5dadf
Kara Erickson <karakara@google.com>
2019-03-07 15:50:06 +0000

    c866c11 docs: add tomas trajan to GDE resources (#29116)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+14.sha-15e8495.with-local-changes
+ * @license Angular v8.0.0-beta.7+15.sha-c866c11.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7471,7 +7471,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.7+14.sha-15e8495.with-local-changes');
+const VERSION = new Version('8.0.0-beta.7+15.sha-c866c11.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+14.sha-15e8495.with-local-changes
+ * @license Angular v8.0.0-beta.7+15.sha-c866c11.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
