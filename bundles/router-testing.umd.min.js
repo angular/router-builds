549dfa2038e414f323927451e8641a675227fc7b
Kara Erickson <karakara@google.com>
2019-03-07 19:43:40 +0000

    5ad2097 fix(ivy): teach template type checker about template attributes (#29041)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+22.sha-9a19592.with-local-changes
+ * @license Angular v8.0.0-beta.7+29.sha-5ad2097.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
