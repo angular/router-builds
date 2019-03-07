3b818091826ef8fcc5c31f905f658dcbd309037b
Kara Erickson <karakara@google.com>
2019-03-07 19:44:56 +0000

    5ad2097 fix(ivy): teach template type checker about template attributes (#29041)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+22.sha-9a19592.with-local-changes
+ * @license Angular v8.0.0-beta.7+29.sha-5ad2097.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5691,7 +5691,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.7+22.sha-9a19592.with-local-changes');
+var VERSION = new Version('8.0.0-beta.7+29.sha-5ad2097.with-local-changes');
 
 /**
  * @license
