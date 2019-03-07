410201194ed0bd4402b0384da248967ec21d5245
Kara Erickson <karakara@google.com>
2019-03-07 19:06:59 +0000

    9a19592 build: remove now redundant `bundle_dts = True` attribute (#29128)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+18.sha-479ae51.with-local-changes
+ * @license Angular v8.0.0-beta.7+22.sha-9a19592.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+18.sha-479ae51.with-local-changes
+ * @license Angular v8.0.0-beta.7+22.sha-9a19592.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.7+18.sha-479ae51.with-local-changes');
+const VERSION = new Version('8.0.0-beta.7+22.sha-9a19592.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
