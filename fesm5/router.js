c45d124b77accaf34f81dac6ebebedb663abffd3
Kara Erickson <karakara@google.com>
2019-03-08 03:11:19 +0000

    014841d test(ivy): fix failing view ref test (#29178)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+39.sha-63d1806.with-local-changes
+ * @license Angular v8.0.0-beta.7+40.sha-014841d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.7+39.sha-63d1806.with-local-changes');
+var VERSION = new Version('8.0.0-beta.7+40.sha-014841d.with-local-changes');
 
 /**
  * @license
