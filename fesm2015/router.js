fd57e6104d7f3afd8326b3a1671f1520a8db5b05
Kara Erickson <karakara@google.com>
2019-03-08 20:52:48 +0000

    b012ab2 test: add interm fix for test that rely on an index.d.ts file (#28884)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+46.sha-a746b5b.with-local-changes
+ * @license Angular v8.0.0-beta.7+49.sha-b012ab2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7285,7 +7285,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.7+46.sha-a746b5b.with-local-changes');
+const VERSION = new Version('8.0.0-beta.7+49.sha-b012ab2.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+46.sha-a746b5b.with-local-changes
+ * @license Angular v8.0.0-beta.7+49.sha-b012ab2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
