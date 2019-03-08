516a328e0db4db99bb02232b439f60344ce27243
Kara Erickson <karakara@google.com>
2019-03-08 20:31:20 +0000

    a746b5b fix(ivy): inherited host listeners called twice (#29170)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+45.sha-b6f6b11.with-local-changes
+ * @license Angular v8.0.0-beta.7+46.sha-a746b5b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
