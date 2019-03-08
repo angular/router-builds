e7338683e5f402049d7a9f9b9dc3472b979043e7
Kara Erickson <karakara@google.com>
2019-03-08 20:55:14 +0000

    fc30530 docs(forms): adding `clear` method to FormArray documentation (#29180)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+49.sha-b012ab2.with-local-changes
+ * @license Angular v8.0.0-beta.7+50.sha-fc30530.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.7+49.sha-b012ab2.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.7+50.sha-fc30530.with-local-changes');
 
     /**
      * @license
