47e021b2cdff7dc5582b606f0583a0d750c9ddef
Kara Erickson <karakara@google.com>
2019-03-09 00:26:11 +0000

    49dccf4 fix(ivy): process separate declarations and exports for summaries (#29193)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+51.sha-c37ec8b.with-local-changes
+ * @license Angular v8.0.0-beta.7+53.sha-49dccf4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.7+51.sha-c37ec8b.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.7+53.sha-49dccf4.with-local-changes');
 
     /**
      * @license
