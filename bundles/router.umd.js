f1d6adc16fbb2c03af5046077f3ae22657a332fe
Kara Erickson <karakara@google.com>
2019-03-11 15:49:15 +0000

    ca20f57 fix(ivy): always convert `rootDirs` to `AbsoluteFsPath` in `getRootDirs` (#29151)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+54.sha-a5b8420.with-local-changes
+ * @license Angular v8.0.0-beta.7+55.sha-ca20f57.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.7+54.sha-a5b8420.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.7+55.sha-ca20f57.with-local-changes');
 
     /**
      * @license
