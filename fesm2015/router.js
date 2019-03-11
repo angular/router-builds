8bd99e5d77c67ae40eeb05b4b246e6a2c641c657
Kara Erickson <karakara@google.com>
2019-03-11 22:54:55 +0000

    ed4675e docs: update Universal guide (#28956)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+61.sha-146256a.with-local-changes
+ * @license Angular v8.0.0-beta.7+63.sha-ed4675e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7285,7 +7285,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.7+61.sha-146256a.with-local-changes');
+const VERSION = new Version('8.0.0-beta.7+63.sha-ed4675e.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
