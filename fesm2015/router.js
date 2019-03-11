d7439b891a706a0126e2498b9a1341940b81e273
Kara Erickson <karakara@google.com>
2019-03-11 18:31:37 +0000

    99aa967 test(ivy): remove chip list tests from blocklist (#29228)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+58.sha-6b6fdff.with-local-changes
+ * @license Angular v8.0.0-beta.7+59.sha-99aa967.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7473,7 +7473,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.7+58.sha-6b6fdff.with-local-changes');
+const VERSION = new Version('8.0.0-beta.7+59.sha-99aa967.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
