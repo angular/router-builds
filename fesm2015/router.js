81a754b5cce620ec0cc263d98439e18c75bface7
Kara Erickson <karakara@google.com>
2019-03-11 14:32:50 +0000

    a5b8420 fix(ivy): render alias exports for private declarations if possible (#28735)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+53.sha-49dccf4.with-local-changes
+ * @license Angular v8.0.0-beta.7+54.sha-a5b8420.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7285,7 +7285,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.7+53.sha-49dccf4.with-local-changes');
+const VERSION = new Version('8.0.0-beta.7+54.sha-a5b8420.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
