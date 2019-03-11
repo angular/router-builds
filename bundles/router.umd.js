71642b94fee5c96bf64e8111de597a0dbf4ddf19
Kara Erickson <karakara@google.com>
2019-03-11 18:30:16 +0000

    99aa967 test(ivy): remove chip list tests from blocklist (#29228)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+58.sha-6b6fdff.with-local-changes
+ * @license Angular v8.0.0-beta.7+59.sha-99aa967.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.7+58.sha-6b6fdff.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.7+59.sha-99aa967.with-local-changes');
 
     /**
      * @license
