114f06ed71915ea99eafb1319ffbd8712ca297ba
Kara Erickson <karakara@google.com>
2019-03-12 18:07:29 +0000

    b4ee746 docs: fix typo in directive decorator description (#29207)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.8+11.sha-09e55ea.with-local-changes
+ * @license Angular v7.2.8+13.sha-b4ee746.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
