15700d36a01c56e34b60295f92885636cc0080c1
Kara Erickson <karakara@google.com>
2019-03-12 01:16:15 +0000

    1d88c2b fix(ivy): handle aliased Angular decorators (#29195)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+53.sha-49dccf4.with-local-changes
+ * @license Angular v8.0.0-beta.7+60.sha-1d88c2b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
