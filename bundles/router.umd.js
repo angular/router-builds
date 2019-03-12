f3467ed77e396a08b46018ca1d2877a24835ca2e
Kara Erickson <karakara@google.com>
2019-03-12 21:26:45 +0000

    fe76494 fix(ivy): use default selector for Components if selector is empty (#29239)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+87.sha-9d4b7d7.with-local-changes
+ * @license Angular v8.0.0-beta.7+88.sha-fe76494.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.7+87.sha-9d4b7d7.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.7+88.sha-fe76494.with-local-changes');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+87.sha-9d4b7d7.with-local-changes
+ * @license Angular v8.0.0-beta.7+88.sha-fe76494.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
