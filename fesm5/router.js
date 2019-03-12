fdad05576847556d225bed7d34269216352099d5
Kara Erickson <karakara@google.com>
2019-03-12 19:37:27 +0000

    47d1216 ci(docs-infra): run cli docs examples concurrently (#29261)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.8+17.sha-efecf36.with-local-changes
+ * @license Angular v7.2.8+19.sha-47d1216.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5726,7 +5726,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.8+17.sha-efecf36.with-local-changes');
+var VERSION = new Version('7.2.8+19.sha-47d1216.with-local-changes');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.8+17.sha-efecf36.with-local-changes
+ * @license Angular v7.2.8+19.sha-47d1216.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
