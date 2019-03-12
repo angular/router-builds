9b44773c9aee392eb845b5afd3a0641ba8ff2eb1
Kara Erickson <karakara@google.com>
2019-03-12 19:18:17 +0000

    efecf36 docs: fix failing specs in testing example (#29256)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.8+16.sha-b68d297.with-local-changes
+ * @license Angular v7.2.8+17.sha-efecf36.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7279,7 +7279,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.8+16.sha-b68d297.with-local-changes');
+const VERSION = new Version('7.2.8+17.sha-efecf36.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.8+16.sha-b68d297.with-local-changes
+ * @license Angular v7.2.8+17.sha-efecf36.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
