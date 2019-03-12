be8b1a8806c75d9aaca08a4d820edb91f0768388
Kara Erickson <karakara@google.com>
2019-03-12 17:53:42 +0000

    76119b8 docs: update enableIvy link (#29238) PR Close #29238

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+65.sha-defc30c.with-local-changes
+ * @license Angular v8.0.0-beta.7+66.sha-76119b8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+65.sha-defc30c.with-local-changes
+ * @license Angular v8.0.0-beta.7+66.sha-76119b8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.7+65.sha-defc30c.with-local-changes');
+var VERSION = new Version('8.0.0-beta.7+66.sha-76119b8.with-local-changes');
 
 /**
  * @license
