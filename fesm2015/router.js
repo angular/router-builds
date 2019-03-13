8a489ae01f39f62b65a5f2da6d2a31ec42030b21
Kara Erickson <karakara@google.com>
2019-03-13 17:59:15 +0000

    eb00a37 build: user bazelrc should be able to overwrite existing flags (#29279)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+11.sha-7b20cec.with-local-changes
+ * @license Angular v8.0.0-beta.8+10.sha-eb00a37.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8067,7 +8067,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.8+11.sha-7b20cec.with-local-changes');
+const VERSION = new Version('8.0.0-beta.8+10.sha-eb00a37.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
