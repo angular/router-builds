13a47485ef0db35d02db4c1267d1d5248b1fd5bc
Kara Erickson <karakara@google.com>
2019-03-13 02:08:23 +0000

    955e4e7 test(ivy): update blocklist to turn on table tests (#29271)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+6.sha-73da279.with-local-changes
+ * @license Angular v8.0.0-beta.8+7.sha-955e4e7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5700,7 +5700,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+6.sha-73da279.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+7.sha-955e4e7.with-local-changes');
 
 /**
  * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+6.sha-73da279.with-local-changes
+ * @license Angular v8.0.0-beta.8+7.sha-955e4e7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
