ab54a0a784a27ac1946e269768776fdee8ab39bd
Kara Erickson <karakara@google.com>
2019-03-13 01:17:52 +0000

    ccb70e1 fix(ivy): reuse default imports in type-to-value references (#29266)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+1.sha-940fbf7.with-local-changes
+ * @license Angular v8.0.0-beta.8+2.sha-ccb70e1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+1.sha-940fbf7.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+2.sha-ccb70e1.with-local-changes');
 
 /**
  * @license
