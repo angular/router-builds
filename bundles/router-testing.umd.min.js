fa65de60a5d173856378076d82244fa51f52cbb6
Kara Erickson <karakara@google.com>
2019-03-13 00:38:20 +0000

    940fbf7 fix(ivy): unable to inject class and style attributes (#29192)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8.with-local-changes
+ * @license Angular v8.0.0-beta.8+1.sha-940fbf7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
