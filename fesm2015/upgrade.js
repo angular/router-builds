05ee97ccb532b2f8f18cc072034d5d3e51492a3c
Kara Erickson <karakara@google.com>
2019-03-13 00:35:01 +0000

    ec01594 release: cut the v8.0.0-beta.8 release

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+89.sha-ac0553e.with-local-changes
+ * @license Angular v8.0.0-beta.8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
