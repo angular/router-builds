32fc1941576d1d58c6e4a9dc85453db8e28a4d9e
Kara Erickson <karakara@google.com>
2019-03-13 01:42:23 +0000

    73da279 fix(ivy): properly compile NgModules with forward referenced types (#29198)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+5.sha-1625d86.with-local-changes
+ * @license Angular v8.0.0-beta.8+6.sha-73da279.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.8+5.sha-1625d86.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.8+6.sha-73da279.with-local-changes');
 
     /**
      * @license
