cabecea325fdb68e16a31dcc54371827c13b0a67
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 21:14:40 +0000

    1fb670e docs: unify `comp: upgrade/{dynamic,static}` labels as `comp: upgrade` (#29276)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+34.sha-fe448e8.with-local-changes
+ * @license Angular v8.0.0-beta.8+36.sha-1fb670e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
