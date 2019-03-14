755e61ac649112ad771fb0ce648d19c2de947898
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 15:50:48 +0000

    a3e1054 test(ivy): update blocklist to turn on passing tests (#29300)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+19.sha-2064508.with-local-changes
+ * @license Angular v8.0.0-beta.8+20.sha-a3e1054.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.8+19.sha-2064508.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.8+20.sha-a3e1054.with-local-changes');
 
     /**
      * @license
