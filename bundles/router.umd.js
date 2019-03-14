be7a41b6b78a39a9925ae91ceaa3d686c3e05793
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 22:48:51 +0000

    ee49bbe build(docs-infra): update AIO dependencies within their current ranges (#29316)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+18.sha-da78b02.with-local-changes
+ * @license Angular v7.2.9+19.sha-ee49bbe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5814,7 +5814,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.9+18.sha-da78b02.with-local-changes');
+    var VERSION = new core.Version('7.2.9+19.sha-ee49bbe.with-local-changes');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+18.sha-da78b02.with-local-changes
+ * @license Angular v7.2.9+19.sha-ee49bbe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
