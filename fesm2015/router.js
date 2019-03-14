e56bcc34811a38c08b0bda840bf7e672a9b85a97
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 22:44:26 +0000

    8ed13a3 docs: clarify commands to integrate routing (#26530)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+39.sha-1877e6c.with-local-changes
+ * @license Angular v8.0.0-beta.8+40.sha-8ed13a3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8255,7 +8255,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.8+39.sha-1877e6c.with-local-changes');
+const VERSION = new Version('8.0.0-beta.8+40.sha-8ed13a3.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
