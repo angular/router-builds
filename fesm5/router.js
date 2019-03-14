37a89cf13b329117f7686ea3c62cdb4183459995
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 21:04:25 +0000

    fe448e8 test(ivy): update root causes for stepper (#29314)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+33.sha-7c297e0.with-local-changes
+ * @license Angular v8.0.0-beta.8+34.sha-fe448e8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+33.sha-7c297e0.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+34.sha-fe448e8.with-local-changes');
 
 /**
  * @license
