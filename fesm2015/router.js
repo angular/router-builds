d611c18b20ffccb84f1f84c52ba8cd1424728584
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 20:23:05 +0000

    6ab8c0b build(bazel): update bazel managed yarn to 1.13.0 (same as CI version) (#29311)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+29.sha-7b70760.with-local-changes
+ * @license Angular v8.0.0-beta.8+28.sha-6ab8c0b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8255,7 +8255,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.8+29.sha-7b70760.with-local-changes');
+const VERSION = new Version('8.0.0-beta.8+28.sha-6ab8c0b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
