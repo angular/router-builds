368b2c8310dddb5714028fec939e4383b8db6258
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 07:25:18 +0000

    018477e fix(ivy): do not inject attributes with namespace (#29257)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+14.sha-019e65a.with-local-changes
+ * @license Angular v8.0.0-beta.8+17.sha-018477e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
