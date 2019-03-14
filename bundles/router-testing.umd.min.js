d0a0aa83455c3c724f4b07b7bdfb5eb436632c0c
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 21:16:53 +0000

    090b6d9 docs: remove configuration instructions at the top of the i18n guide (#29313)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+13.sha-a5cbfa2.with-local-changes
+ * @license Angular v7.2.9+14.sha-090b6d9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
