e06b9f9c4b56c8003fbbf4b3058650d7054e3634
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 22:40:43 +0000

    da78b02 docs: clarify commands to integrate routing (#26530)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+17.sha-d787925.with-local-changes
+ * @license Angular v7.2.9+18.sha-da78b02.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
