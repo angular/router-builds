ad72ebff999e8b755ec59a1ecba0ac3d0ef79cb3
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 22:37:41 +0000

    1877e6c fix(ivy): ensure template styles/classes are applied before directives are instantiated (#29269)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+38.sha-1a9ab27.with-local-changes
+ * @license Angular v8.0.0-beta.8+39.sha-1877e6c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.8+38.sha-1a9ab27.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.8+39.sha-1877e6c.with-local-changes');
 
     /**
      * @license
