f86ff472298cbebb15f2a22a19a5a6319c2d89d3
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 21:15:49 +0000

    f50928f docs: remove configuration instructions at the top of the i18n guide (#29313)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+36.sha-1fb670e.with-local-changes
+ * @license Angular v8.0.0-beta.8+35.sha-f50928f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
