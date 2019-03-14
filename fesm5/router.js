8a1a6dec09d2df61c1d4fc9acb772fce489f75ac
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 20:22:30 +0000

    7b70760 refactor: move DOCUMENT imports from platform-browser to common (#29237)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+26.sha-a5c747f.with-local-changes
+ * @license Angular v8.0.0-beta.8+29.sha-7b70760.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5700,7 +5700,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+26.sha-a5c747f.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+29.sha-7b70760.with-local-changes');
 
 /**
  * @license
