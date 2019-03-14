a0bb0530be87ae22cddc9deac3b81c179a2b55b8
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 07:23:08 +0000

    1f0eadf test(ivy): enable passing Material tests (#29219)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+13.sha-0ffa2f2.with-local-changes
+ * @license Angular v8.0.0-beta.8+16.sha-1f0eadf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+13.sha-0ffa2f2.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+16.sha-1f0eadf.with-local-changes');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+13.sha-0ffa2f2.with-local-changes
+ * @license Angular v8.0.0-beta.8+16.sha-1f0eadf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
