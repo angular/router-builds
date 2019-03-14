85e80a96e02ecf66394db0d2983de13832dce36f
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 20:21:01 +0000

    7b70760 refactor: move DOCUMENT imports from platform-browser to common (#29237)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+26.sha-a5c747f.with-local-changes
+ * @license Angular v8.0.0-beta.8+29.sha-7b70760.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+26.sha-a5c747f.with-local-changes
+ * @license Angular v8.0.0-beta.8+29.sha-7b70760.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.8+26.sha-a5c747f.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.8+29.sha-7b70760.with-local-changes');
 
     /**
      * @license
