f32e7b79635e0f453d1167003a679716e2e375df
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 21:34:27 +0000

    d787925 docs: update developer guide for use with yarn (#29044)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+16.sha-4e6efb3.with-local-changes
+ * @license Angular v7.2.9+17.sha-d787925.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
