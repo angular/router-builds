001594735e93b7d65148a76d12797141bcb601d3
Matias Niemelä <matias@yearofmoo.com>
2019-03-15 17:00:38 +0000

    67f4852 docs: fix a typo in creating libraries guide (#29154)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+20.sha-08f6a64.with-local-changes
+ * @license Angular v7.2.9+21.sha-67f4852.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
