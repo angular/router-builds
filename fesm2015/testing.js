e8633fcd0dfcf2e58dcb9ac0f63614e7908c51d7
Matias Niemelä <matias@yearofmoo.com>
2019-03-15 04:33:12 +0000

    8ef690c fix(bazel): Support new e2e project layout (#29318)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+44.sha-1db8bf3.with-local-changes
+ * @license Angular v8.0.0-beta.8+46.sha-8ef690c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
