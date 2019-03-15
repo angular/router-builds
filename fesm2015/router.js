4511b9d4cc4d5c69d57247ae4a1e80d8376c1c8a
Matias Niemelä <matias@yearofmoo.com>
2019-03-15 04:32:48 +0000

    1db8bf3 fix(bazel): Remove @angular/upgrade from dev dependencies (#29319)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+43.sha-f7738ad.with-local-changes
+ * @license Angular v8.0.0-beta.8+44.sha-1db8bf3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8255,7 +8255,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.8+43.sha-f7738ad.with-local-changes');
+const VERSION = new Version('8.0.0-beta.8+44.sha-1db8bf3.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
