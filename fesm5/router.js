6e7616d2104c7a62f34a48c6543792009fa0f7ee
Matias Niemelä <matias@yearofmoo.com>
2019-03-15 04:34:29 +0000

    8ef690c fix(bazel): Support new e2e project layout (#29318)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+44.sha-1db8bf3.with-local-changes
+ * @license Angular v8.0.0-beta.8+46.sha-8ef690c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5700,7 +5700,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+44.sha-1db8bf3.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+46.sha-8ef690c.with-local-changes');
 
 /**
  * @license
