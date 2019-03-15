998908bd767cb1e8417cf6f7f080a3d6bc1828da
Matias Niemelä <matias@yearofmoo.com>
2019-03-15 04:31:26 +0000

    1db8bf3 fix(bazel): Remove @angular/upgrade from dev dependencies (#29319)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+43.sha-f7738ad.with-local-changes
+ * @license Angular v8.0.0-beta.8+44.sha-1db8bf3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.8+43.sha-f7738ad.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.8+44.sha-1db8bf3.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+43.sha-f7738ad.with-local-changes
+ * @license Angular v8.0.0-beta.8+44.sha-1db8bf3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
