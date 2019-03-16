8d44a2c2031e82e8f313f0814244dbd150627b58
Matias Niemelä <matias@yearofmoo.com>
2019-03-16 00:44:41 +0000

    604f37b test: revert to earlier @bazel/jasmine without sharding support (#29347)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+50.sha-bf2db12.with-local-changes
+ * @license Angular v8.0.0-beta.8+52.sha-604f37b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
