e361c32032c249ca9ae6d2d8eb2635a85dfd297f
Matias Niemelä <matias@yearofmoo.com>
2019-03-16 00:43:21 +0000

    604f37b test: revert to earlier @bazel/jasmine without sharding support (#29347)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+50.sha-bf2db12.with-local-changes
+ * @license Angular v8.0.0-beta.8+52.sha-604f37b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+50.sha-bf2db12.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+52.sha-604f37b.with-local-changes');
 
 /**
  * @license
