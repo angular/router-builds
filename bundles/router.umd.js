55fabbbdeff402c7cd3f203a44f811870f204772
Jason Aden <jasonaden@google.com>
2019-03-18 14:38:49 +0000

    8e70ca3 refactor(ivy): Add docs to i18n interfaces (#29344)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+55.sha-9d090cb.with-local-changes
+ * @license Angular v8.0.0-beta.8+56.sha-8e70ca3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.8+55.sha-9d090cb.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.8+56.sha-8e70ca3.with-local-changes');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+55.sha-9d090cb.with-local-changes
+ * @license Angular v8.0.0-beta.8+56.sha-8e70ca3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5700,7 +5700,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+55.sha-9d090cb.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+56.sha-8e70ca3.with-local-changes');
 
 /**
  * @license
