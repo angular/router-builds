b1fe238a29ad2db7c55bb2c7f553da15596d8c80
Matias Niemelä <matias@yearofmoo.com>
2019-03-18 19:26:42 +0000

    105cfaf fix(compiler-cli): incorrect metadata bundle for multiple unnamed re-exports (#29360)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+58.sha-2ab194c.with-local-changes
+ * @license Angular v8.0.0-beta.8+59.sha-105cfaf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+58.sha-2ab194c.with-local-changes
+ * @license Angular v8.0.0-beta.8+59.sha-105cfaf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+58.sha-2ab194c.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+59.sha-105cfaf.with-local-changes');
 
 /**
  * @license
