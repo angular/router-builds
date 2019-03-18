e70d10786480ccbb58db1bf74131dfa967bf54b0
Matias Niemelä <matias@yearofmoo.com>
2019-03-18 21:55:37 +0000

    f03d724 build: re-add bazel symlink prefix in patch branch (#29312)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+21.sha-67f4852.with-local-changes
+ * @license Angular v7.2.9+23.sha-f03d724.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+21.sha-67f4852.with-local-changes
+ * @license Angular v7.2.9+23.sha-f03d724.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7279,7 +7279,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.9+21.sha-67f4852.with-local-changes');
+const VERSION = new Version('7.2.9+23.sha-f03d724.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
