dc4880b09708f8c43444189469a35a2aa93b9489
Jason Aden <jasonaden@google.com>
2019-03-18 16:29:29 +0000

    7b5d326 build(docs-infra): upgrade cli command docs sources to cafa558cf (#29350)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+56.sha-8e70ca3.with-local-changes
+ * @license Angular v8.0.0-beta.8+57.sha-7b5d326.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.8+56.sha-8e70ca3.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.8+57.sha-7b5d326.with-local-changes');
 
     /**
      * @license
