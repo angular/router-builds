92f1dcb535882780cee76b9560e1da7143746ae5
Jason Aden <jasonaden@google.com>
2019-03-18 14:36:48 +0000

    9d090cb fix(bazel): correct regexp test for self-references in metadata (#29346)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+53.sha-e8df000.with-local-changes
+ * @license Angular v8.0.0-beta.8+55.sha-9d090cb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.8+53.sha-e8df000.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.8+55.sha-9d090cb.with-local-changes');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+53.sha-e8df000.with-local-changes
+ * @license Angular v8.0.0-beta.8+55.sha-9d090cb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
