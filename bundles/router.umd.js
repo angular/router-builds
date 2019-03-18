28f4e8b3e75ff40914fc8ae7fd6517c973432466
Matias Niemelä <matias@yearofmoo.com>
2019-03-18 22:21:18 +0000

    ce4da3f fix(ivy): run annotations handlers' `resolve()` in `ngcc` (#28963)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+63.sha-4742385.with-local-changes
+ * @license Angular v8.0.0-beta.8+68.sha-ce4da3f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.8+63.sha-4742385.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.8+68.sha-ce4da3f.with-local-changes');
 
     /**
      * @license
