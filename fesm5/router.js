c93bb99c15d38d2279786a1666d98b938beef6e1
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 18:30:31 +0000

    a3ec058 revert: fix(core): parse incorrect ML open tag as text (#29328)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+78.sha-4605df8.with-local-changes
+ * @license Angular v8.0.0-beta.8+79.sha-a3ec058.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+78.sha-4605df8.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+79.sha-a3ec058.with-local-changes');
 
 /**
  * @license
