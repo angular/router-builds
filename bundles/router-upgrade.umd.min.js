60da4cd465754a7e470b1e084e33cb30d1e21a0e
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 19:42:21 +0000

    a3ec058 revert: fix(core): parse incorrect ML open tag as text (#29328)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+56.sha-8e70ca3.with-local-changes
+ * @license Angular v8.0.0-beta.8+79.sha-a3ec058.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
