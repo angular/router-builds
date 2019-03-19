7089e80adaf713e1191f409216d731a1e4faf3cf
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 17:29:28 +0000

    e769f9c docs(forms): fix small typo in forms documentation (#29370)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+74.sha-4a665ca.with-local-changes
+ * @license Angular v8.0.0-beta.8+75.sha-e769f9c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+74.sha-4a665ca.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+75.sha-e769f9c.with-local-changes');
 
 /**
  * @license
