b68deee6739d34e326e2d322d3a326ad2f6d64c8
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 17:34:25 +0000

    c0ad9e1 docs: edit $any() section and add example in Template Syntax (#28157)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+75.sha-e769f9c.with-local-changes
+ * @license Angular v8.0.0-beta.8+77.sha-c0ad9e1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.8+75.sha-e769f9c.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.8+77.sha-c0ad9e1.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+75.sha-e769f9c.with-local-changes
+ * @license Angular v8.0.0-beta.8+77.sha-c0ad9e1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
