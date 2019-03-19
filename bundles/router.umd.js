7a7a88f1a3e93229be48e1ac9f4e75f21258d81d
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 05:29:14 +0000

    86aba1e build: add moduleName to ngFactory sourcefiles (#29385)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+68.sha-ce4da3f.with-local-changes
+ * @license Angular v8.0.0-beta.8+70.sha-86aba1e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.8+68.sha-ce4da3f.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.8+70.sha-86aba1e.with-local-changes');
 
     /**
      * @license
