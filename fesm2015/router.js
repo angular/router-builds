23257350d15ba6c590f5f27185c5e868bbcf5dc6
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 17:23:55 +0000

    8e57965 docs: fix typo in section introduction (#29394) PR Close #29394

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+25.sha-761bb0a.with-local-changes
+ * @license Angular v7.2.9+26.sha-8e57965.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7279,7 +7279,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.9+25.sha-761bb0a.with-local-changes');
+const VERSION = new Version('7.2.9+26.sha-8e57965.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+25.sha-761bb0a.with-local-changes
+ * @license Angular v7.2.9+26.sha-8e57965.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
