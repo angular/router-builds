3db526bebf255903965de88efc959193f7602523
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 17:46:21 +0000

    4605df8 fix(core): parse incorrect ML open tag as text (#29328)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+77.sha-c0ad9e1.with-local-changes
+ * @license Angular v8.0.0-beta.8+78.sha-4605df8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.8+77.sha-c0ad9e1.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.8+78.sha-4605df8.with-local-changes');
 
     /**
      * @license
