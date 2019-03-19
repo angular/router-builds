8637cdd79d14da55464258041482f5e5411b9341
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 17:30:25 +0000

    280ea13 docs(core): fix typo in NgModule imports example description (#29272)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+26.sha-8e57965.with-local-changes
+ * @license Angular v7.2.9+28.sha-280ea13.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
