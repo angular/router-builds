5d92a24f5f41a36182b7c29649720f367a1e68ff
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 05:27:52 +0000

    86aba1e build: add moduleName to ngFactory sourcefiles (#29385)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+68.sha-ce4da3f.with-local-changes
+ * @license Angular v8.0.0-beta.8+70.sha-86aba1e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
