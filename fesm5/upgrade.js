3f96de1563717e29ed2e65e5a965462cf326e313
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 17:26:49 +0000

    4a665ca docs: fix typo in section introduction (#29394) PR Close #29394

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+73.sha-e9fab63.with-local-changes
+ * @license Angular v8.0.0-beta.8+74.sha-4a665ca.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
