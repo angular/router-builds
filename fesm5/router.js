91220094a772613cb5ba3b405ea51383ba5f179c
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 07:04:16 +0000

    e9fab63 ci: add `.vscode/**` to the `@angular/fw-dev-infra` group (#29302)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+71.sha-0244a24.with-local-changes
+ * @license Angular v8.0.0-beta.8+73.sha-e9fab63.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5700,7 +5700,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+71.sha-0244a24.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+73.sha-e9fab63.with-local-changes');
 
 /**
  * @license
