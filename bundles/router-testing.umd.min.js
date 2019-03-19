b729733df1a5e3e30a69addb8f171725eb7b7929
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 17:35:29 +0000

    fd1d161 docs: edit $any() section and add example in Template Syntax (#28157)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+28.sha-280ea13.with-local-changes
+ * @license Angular v7.2.9+29.sha-fd1d161.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
