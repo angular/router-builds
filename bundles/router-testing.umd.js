0d530d5033980ca5d4a92b06549b9cc75e287a04
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 18:03:23 +0000

    d5c815f docs: add alexeagle to @angular/fw-global-approvers (#29287)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+29.sha-fd1d161.with-local-changes
+ * @license Angular v7.2.10+2.sha-d5c815f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
