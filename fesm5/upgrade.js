c1883b22cb2445354675269aed42babf8e2bec5b
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 03:55:59 +0000

    2d7435d build(bazel): fix router test failure (#29375)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+2.sha-dafbbf8.with-local-changes
+ * @license Angular v8.0.0-beta.9+9.sha-2d7435d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
