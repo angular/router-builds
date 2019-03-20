414ede7d9f3e4620ca5d1960f87e232707daeb59
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 04:32:46 +0000

    dafbbf8 fix(core): parse incorrect ML open tag as text (#29328)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+83.sha-067657c.with-local-changes
+ * @license Angular v8.0.0-beta.9+2.sha-dafbbf8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
