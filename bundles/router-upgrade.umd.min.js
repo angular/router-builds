fc424527bfe90115f75f6d80fa860f83b455444e
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 00:26:24 +0000

    067657c fix(ivy): prevent templateOverrides from causing infinite loop (#29402)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+79.sha-a3ec058.with-local-changes
+ * @license Angular v8.0.0-beta.8+83.sha-067657c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
