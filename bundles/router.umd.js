ec49440e1cda7361b4f59650718f4531e9957864
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 17:51:35 +0000

    1a4d4a0 docs: fix typo (culd -> could) (#29412)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+13.sha-3121409.with-local-changes
+ * @license Angular v8.0.0-beta.9+14.sha-1a4d4a0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.9+13.sha-3121409.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.9+14.sha-1a4d4a0.with-local-changes');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+13.sha-3121409.with-local-changes
+ * @license Angular v8.0.0-beta.9+14.sha-1a4d4a0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
