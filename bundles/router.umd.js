2a9007260a58ed051e11f178466ef539c69e1094
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 17:31:41 +0000

    fd122b0 ci: cache Material `node_modules` based on the lockfile checksum (#29417)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+11.sha-9a364a8.with-local-changes
+ * @license Angular v8.0.0-beta.9+12.sha-fd122b0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.9+11.sha-9a364a8.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.9+12.sha-fd122b0.with-local-changes');
 
     /**
      * @license
