db8c8586fae143599ec7752a582d48f88760994b
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 23:21:16 +0000

    c75e16a docs: add Luis Aviles to GDE resources (#29405)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.10+6.sha-1affcc6.with-local-changes
+ * @license Angular v7.2.10+7.sha-c75e16a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7279,7 +7279,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.10+6.sha-1affcc6.with-local-changes');
+const VERSION = new Version('7.2.10+7.sha-c75e16a.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.10+6.sha-1affcc6.with-local-changes
+ * @license Angular v7.2.10+7.sha-c75e16a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
