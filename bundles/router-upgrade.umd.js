a18f461cd8b13acb361ac198b449a1aaef21d5c2
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 19:27:19 +0000

    10734ac fix(ivy): Class selector directives execute properly on container elements (#29383)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+40.sha-64e5628.with-local-changes
+ * @license Angular v8.0.0-beta.9+42.sha-10734ac.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
