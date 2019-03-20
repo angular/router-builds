29d8b18924f5c29035f0ab4a8f0bed1bd7fbedff
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 00:28:59 +0000

    aae6f7b docs: add usage analytics gathering guide to aio (#29382)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+87.sha-efcd6af.with-local-changes
+ * @license Angular v8.0.0-beta.8+89.sha-aae6f7b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8067,7 +8067,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.8+87.sha-efcd6af.with-local-changes');
+const VERSION = new Version('8.0.0-beta.8+89.sha-aae6f7b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
