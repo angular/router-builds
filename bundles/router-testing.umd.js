da2c1c4bb23d5e7733c2e7e243082c5a32b05dbf
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 17:50:34 +0000

    3121409 Revert "build: add a nice Angular-themed color for the vscode status bar (#29407)" (#29419)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+12.sha-fd122b0.with-local-changes
+ * @license Angular v8.0.0-beta.9+13.sha-3121409.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
