70d455887b3742834839f5c0d72a14bdaa60ed93
Miško Hevery <misko@hevery.com>
2019-03-21 23:17:26 +0000

    ed8d60d docs: add app shell guide (#28591)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+71.sha-02debeb.with-local-changes
+ * @license Angular v8.0.0-beta.9+70.sha-ed8d60d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
