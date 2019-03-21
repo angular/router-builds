766fbe8f63b0aa9ca00d81fdb6cf3b33720ce284
Kara Erickson <karakara@google.com>
2019-03-21 17:20:38 +0000

    693b350 build: add @npm//jasmine-core dep back to jasmine_node_test in defaults.bzl (#29444)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+56.sha-dc10355.with-local-changes
+ * @license Angular v8.0.0-beta.9+59.sha-693b350.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.9+56.sha-dc10355.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.9+59.sha-693b350.with-local-changes');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+56.sha-dc10355.with-local-changes
+ * @license Angular v8.0.0-beta.9+59.sha-693b350.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
