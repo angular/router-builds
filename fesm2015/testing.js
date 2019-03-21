5406bbba052dc5020c58233d1df250b7ed9fb06a
Matias Niemelä <matias@yearofmoo.com>
2019-03-21 19:47:51 +0000

    c18fa7b build(docs-infra): upgrade cli command docs sources to 18d979cdc (#29437)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+59.sha-693b350.with-local-changes
+ * @license Angular v8.0.0-beta.9+60.sha-c18fa7b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
