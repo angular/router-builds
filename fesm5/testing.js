f119f76a5a61231a7dd11208ad89b38437dfd4b7
Matias Niemelä <matias@yearofmoo.com>
2019-03-21 07:04:04 +0000

    41737bb docs: fix html tags in changelog (#29411)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+47.sha-afe3e72.with-local-changes
+ * @license Angular v8.0.0-beta.9+48.sha-41737bb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
