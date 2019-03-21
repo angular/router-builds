16c388f72eef63ef0123d921765759685be34e80
Miško Hevery <misko@hevery.com>
2019-03-21 23:03:30 +0000

    a677d8b docs(core): indicate OnPush applies to children (#28320)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.10+7.sha-c75e16a.with-local-changes
+ * @license Angular v7.2.10+10.sha-a677d8b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.10+7.sha-c75e16a.with-local-changes
+ * @license Angular v7.2.10+10.sha-a677d8b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7279,7 +7279,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.10+7.sha-c75e16a.with-local-changes');
+const VERSION = new Version('7.2.10+10.sha-a677d8b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
