cd3ed20515a5165d564e016ed7656dd77d2114d6
Miško Hevery <misko@hevery.com>
2019-03-21 23:16:17 +0000

    a00c2a9 docs: add app shell guide (#28591)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.10+12.sha-9e1ce31.with-local-changes
+ * @license Angular v7.2.10+11.sha-a00c2a9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.10+12.sha-9e1ce31.with-local-changes
+ * @license Angular v7.2.10+11.sha-a00c2a9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7279,7 +7279,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.10+12.sha-9e1ce31.with-local-changes');
+const VERSION = new Version('7.2.10+11.sha-a00c2a9.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
