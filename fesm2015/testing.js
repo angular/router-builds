8f49b5c51012e6964174b05aeed10aa7090ccc30
Matias Niemelä <matias@yearofmoo.com>
2019-03-21 16:33:22 +0000

    dc10355 build(compiler-cli): enable full TypeScript strictness (#29436)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+55.sha-6cd3743.with-local-changes
+ * @license Angular v8.0.0-beta.9+56.sha-dc10355.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
