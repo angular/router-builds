9e7f63a25955b01229ad69fa44c0366f3dab8c0e
Miško Hevery <misko@hevery.com>
2019-03-21 22:35:33 +0000

    6f3052b ci: disable sauce-connect ssl bumping (#29447)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+60.sha-c18fa7b.with-local-changes
+ * @license Angular v8.0.0-beta.9+61.sha-6f3052b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8067,7 +8067,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.9+60.sha-c18fa7b.with-local-changes');
+const VERSION = new Version('8.0.0-beta.9+61.sha-6f3052b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
