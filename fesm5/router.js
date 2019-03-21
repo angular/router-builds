97c49cfec3651f70810a64b58fb6b11ea40f63ab
Miško Hevery <misko@hevery.com>
2019-03-21 23:26:01 +0000

    ed8d60d docs: add app shell guide (#28591)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+72.sha-cfa6e8e.with-local-changes
+ * @license Angular v8.0.0-beta.9+70.sha-ed8d60d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+72.sha-cfa6e8e.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+70.sha-ed8d60d.with-local-changes');
 
 /**
  * @license
