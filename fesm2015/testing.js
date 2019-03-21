9550809645f4778da1b7ebbb39f3f6e9a62e1cfc
Miško Hevery <misko@hevery.com>
2019-03-21 23:18:06 +0000

    cfa6e8e docs: add Siddharth Ajmera to GDE resources (#28456)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+70.sha-ed8d60d.with-local-changes
+ * @license Angular v8.0.0-beta.9+72.sha-cfa6e8e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
