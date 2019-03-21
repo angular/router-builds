3d40b69ec73461ad00401168e6097178f9a45152
Miško Hevery <misko@hevery.com>
2019-03-21 23:13:12 +0000

    02debeb docs: add NG-DE 2019 to events page (#29079)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+69.sha-9ea0d64.with-local-changes
+ * @license Angular v8.0.0-beta.9+71.sha-02debeb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.9+69.sha-9ea0d64.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.9+71.sha-02debeb.with-local-changes');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+69.sha-9ea0d64.with-local-changes
+ * @license Angular v8.0.0-beta.9+71.sha-02debeb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
