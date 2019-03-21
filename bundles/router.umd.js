f6c036aa5a0dea760dc7629ce332c1713f39b3e8
Matias Niemelä <matias@yearofmoo.com>
2019-03-21 16:25:10 +0000

    6cd3743 refactor(service-worker): use `Adapter#parseUrl()` for all URL parsing (#27080)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+48.sha-41737bb.with-local-changes
+ * @license Angular v8.0.0-beta.9+55.sha-6cd3743.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.9+48.sha-41737bb.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.9+55.sha-6cd3743.with-local-changes');
 
     /**
      * @license
