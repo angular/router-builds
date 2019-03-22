09dc369f7280c6a689d51473a5c112dfa9a0f51d
Miško Hevery <misko@hevery.com>
2019-03-22 23:59:40 +0000

    bef5043 fix(ivy): TestBed overriding custom ErrorHandler (#29482)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+82.sha-b5295ad.with-local-changes
+ * @license Angular v8.0.0-beta.9+83.sha-bef5043.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
