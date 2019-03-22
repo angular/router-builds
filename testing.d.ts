f8e9abb3c37292440c5065ffea55be96ed81ffa6
Miško Hevery <misko@hevery.com>
2019-03-22 21:43:50 +0000

    769d960 fix(bazel): workaround problem reading summary files from node_modules (#29459)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+66.sha-2790352.with-local-changes
+ * @license Angular v8.0.0-beta.9+81.sha-769d960.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
