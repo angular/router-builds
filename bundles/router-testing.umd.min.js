ab06ce968193693741f4a6cac3d3fec0937d30c0
Miško Hevery <misko@hevery.com>
2019-03-22 20:34:52 +0000

    8b1d28f docs(core): close tags in example (#29474)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.10+15.sha-463e967.with-local-changes
+ * @license Angular v7.2.10+16.sha-8b1d28f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
