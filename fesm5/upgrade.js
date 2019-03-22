975ed03048875814d667ba665f03eb40023550aa
Miško Hevery <misko@hevery.com>
2019-03-22 20:34:30 +0000

    a06f034 docs(core): close tags in example (#29474)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+77.sha-66b72bf.with-local-changes
+ * @license Angular v8.0.0-beta.9+78.sha-a06f034.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
