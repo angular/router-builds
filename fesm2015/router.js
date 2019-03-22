75713a2ad4fca2951dda026f016532515b3b588c
Miško Hevery <misko@hevery.com>
2019-03-22 20:28:33 +0000

    66b72bf fix(ivy): ViewContainerRef.destroy should properly clean the DOM (#29414)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+73.sha-f3e0cc8.with-local-changes
+ * @license Angular v8.0.0-beta.9+77.sha-66b72bf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8067,7 +8067,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.9+73.sha-f3e0cc8.with-local-changes');
+const VERSION = new Version('8.0.0-beta.9+77.sha-66b72bf.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
