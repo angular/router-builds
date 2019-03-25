5b629a823076a1953bdb46443b9e30e2f325c3c3
Misko Hevery <misko@hevery.com>
2019-03-25 16:40:21 +0000

    b3102b9 docs: changing "struture" to "structure" (#29497) PR Close #29497

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+84.sha-cfe6581.with-local-changes
+ * @license Angular v8.0.0-beta.9+85.sha-b3102b9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
