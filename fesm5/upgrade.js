48fee935f27dfdfe1192574bc080865c790d8b04
Misko Hevery <misko@hevery.com>
2019-03-25 16:42:19 +0000

    3612ddb refactor: remove extra line break in gulpfile (#29489)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+85.sha-b3102b9.with-local-changes
+ * @license Angular v8.0.0-beta.9+87.sha-3612ddb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
