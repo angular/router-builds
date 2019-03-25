7c50f2316ec537dd2ebb79eb181e6d4c6a58a9ae
Misko Hevery <misko@hevery.com>
2019-03-25 16:52:46 +0000

    c34c223 fix(ivy): avoid type coercion in binding-related instruction asserts (#29470)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+88.sha-4122528.with-local-changes
+ * @license Angular v8.0.0-beta.9+90.sha-c34c223.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5700,7 +5700,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+88.sha-4122528.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+90.sha-c34c223.with-local-changes');
 
 /**
  * @license
