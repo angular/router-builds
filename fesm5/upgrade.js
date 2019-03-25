e785b4447df01f02466945730a6e51a8947cfa10
Misko Hevery <misko@hevery.com>
2019-03-25 16:45:08 +0000

    4122528 ci: add codefresh badge (#29475)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+87.sha-3612ddb.with-local-changes
+ * @license Angular v8.0.0-beta.9+88.sha-4122528.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
