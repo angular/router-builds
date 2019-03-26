b5d9a1d4d22c5d6e27258aad0ee60679fa0defdb
Miško Hevery <misko@hevery.com>
2019-03-26 23:29:22 +0000

    a28b3e3 release: cut the v8.0.0-beta.10 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10.with-local-changes
+ * @license Angular v8.0.0-beta.10+1.sha-a28b3e3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.10.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.10+1.sha-a28b3e3.with-local-changes');
 
     /**
      * @license
