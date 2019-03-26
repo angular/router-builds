59d25ed7514d6b6ca62e0c07125bf0a9e62071ce
Miško Hevery <misko@hevery.com>
2019-03-26 20:01:18 +0000

    9724247 fix(ivy): generate empty QueryList for root component content queries (#29514)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+94.sha-7951c4f.with-local-changes
+ * @license Angular v8.0.0-beta.9+96.sha-9724247.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+94.sha-7951c4f.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+96.sha-9724247.with-local-changes');
 
 /**
  * @license
