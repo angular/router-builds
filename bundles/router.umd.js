6f28855859830c36ba7444f51313d47d1ce637c0
Miško Hevery <misko@hevery.com>
2019-03-26 20:06:17 +0000

    c412374 fix(ivy): DebugNode.query should query nodes in the logical tree (#29480)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+96.sha-9724247.with-local-changes
+ * @license Angular v8.0.0-beta.9+97.sha-c412374.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.9+96.sha-9724247.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.9+97.sha-c412374.with-local-changes');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+96.sha-9724247.with-local-changes
+ * @license Angular v8.0.0-beta.9+97.sha-c412374.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8067,7 +8067,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.9+96.sha-9724247.with-local-changes');
+const VERSION = new Version('8.0.0-beta.9+97.sha-c412374.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
