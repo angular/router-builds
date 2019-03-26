db5c9f2d5ca9358d9eb58778e6f84f8986183e7d
Miško Hevery <misko@hevery.com>
2019-03-26 20:02:43 +0000

    9724247 fix(ivy): generate empty QueryList for root component content queries (#29514)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+94.sha-7951c4f.with-local-changes
+ * @license Angular v8.0.0-beta.9+96.sha-9724247.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.9+94.sha-7951c4f.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.9+96.sha-9724247.with-local-changes');
 
     /**
      * @license
