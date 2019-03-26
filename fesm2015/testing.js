2f4ac70164ed9047f2783eb8208549010ad7988a
Miško Hevery <misko@hevery.com>
2019-03-26 23:28:05 +0000

    a28b3e3 release: cut the v8.0.0-beta.10 release

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10.with-local-changes
+ * @license Angular v8.0.0-beta.10+1.sha-a28b3e3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
