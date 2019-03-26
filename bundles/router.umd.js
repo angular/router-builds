1149654d10646ee181ca10c84fcdc6814fc1b035
Kara Erickson <karakara@google.com>
2019-03-26 18:03:22 +0000

    7951c4f test(platform-browser): fix shadow dom test not working in firefox 65 (#29518)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+91.sha-6feef36.with-local-changes
+ * @license Angular v8.0.0-beta.9+94.sha-7951c4f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.9+91.sha-6feef36.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.9+94.sha-7951c4f.with-local-changes');
 
     /**
      * @license
