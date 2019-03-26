21511399b6c4e9c8687bd5caf2dbfa4d0a65125a
Miško Hevery <misko@hevery.com>
2019-03-26 20:07:40 +0000

    c412374 fix(ivy): DebugNode.query should query nodes in the logical tree (#29480)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+96.sha-9724247.with-local-changes
+ * @license Angular v8.0.0-beta.9+97.sha-c412374.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.9+96.sha-9724247.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.9+97.sha-c412374.with-local-changes');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+96.sha-9724247.with-local-changes
+ * @license Angular v8.0.0-beta.9+97.sha-c412374.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
