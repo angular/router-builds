8be255ffa0d1b44d69e63c5655828c40efbf7a9b
Miško Hevery <misko@hevery.com>
2019-03-27 17:16:39 +0000

    309ffe7 fix(ivy): TestBed rewrite to avoid unnecessary recompilations (#29483)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+8.sha-fea2a0f.with-local-changes
+ * @license Angular v8.0.0-beta.10+9.sha-309ffe7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.10+8.sha-fea2a0f.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.10+9.sha-309ffe7.with-local-changes');
 
     /**
      * @license
