6df4856c68f6ae83b7e650c98f56af3ac1a59076
Miško Hevery <misko@hevery.com>
2019-03-27 19:58:19 +0000

    401b8ee fix(bazel): Update schematics to support routing (#29548)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+23.sha-1727fe2.with-local-changes
+ * @license Angular v8.0.0-beta.10+19.sha-401b8ee.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+23.sha-1727fe2.with-local-changes
+ * @license Angular v8.0.0-beta.10+19.sha-401b8ee.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+23.sha-1727fe2.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+19.sha-401b8ee.with-local-changes');
 
 /**
  * @license
