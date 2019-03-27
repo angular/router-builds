29b2fcf708af60f7cbe76ba91fe4a98558320445
Miško Hevery <misko@hevery.com>
2019-03-27 21:00:51 +0000

    80161d5 build: remove unused dependency from `common` example (#29532)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+24.sha-c981006.with-local-changes
+ * @license Angular v8.0.0-beta.10+27.sha-80161d5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+24.sha-c981006.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+27.sha-80161d5.with-local-changes');
 
 /**
  * @license
