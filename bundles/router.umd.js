a57bd7ecc36675e4e5df2681dff1f6796d60b5f7
Miško Hevery <misko@hevery.com>
2019-03-27 17:41:06 +0000

    f8c7c3c docs: add schematic details and links to config page (#27272)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+12.sha-ac3ce0d.with-local-changes
+ * @license Angular v8.0.0-beta.10+17.sha-f8c7c3c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.10+12.sha-ac3ce0d.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.10+17.sha-f8c7c3c.with-local-changes');
 
     /**
      * @license
