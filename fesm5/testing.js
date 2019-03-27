a811970c0b894367a3553a6cc0f276c06d47de00
Miško Hevery <misko@hevery.com>
2019-03-27 22:29:22 +0000

    9419165 build: add version name pre release check [patch] (#29552)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+12.sha-1f749fe.with-local-changes
+ * @license Angular v7.2.11+13.sha-9419165.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
