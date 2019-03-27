8c6da4600050350a6ab768d12cf6e463d432fa88
Miško Hevery <misko@hevery.com>
2019-03-27 21:06:13 +0000

    aaa8a3a build: update bazel to 0.24 (#29530)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+27.sha-80161d5.with-local-changes
+ * @license Angular v8.0.0-beta.10+28.sha-aaa8a3a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
