c2bb3c9617269b021891c850474948e9380fb642
Miško Hevery <misko@hevery.com>
2019-03-27 20:00:58 +0000

    3d60f6d ci: lock chrome and firefox versions for saucelabs (#29529)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+10.sha-8aefbfe.with-local-changes
+ * @license Angular v7.2.11+11.sha-3d60f6d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+10.sha-8aefbfe.with-local-changes
+ * @license Angular v7.2.11+11.sha-3d60f6d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5726,7 +5726,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.11+10.sha-8aefbfe.with-local-changes');
+var VERSION = new Version('7.2.11+11.sha-3d60f6d.with-local-changes');
 
 /**
  * @license
