a15d5c4424183487a2335bf0a3c8a882e49d189d
Miško Hevery <misko@hevery.com>
2019-03-27 20:01:01 +0000

    9745f55 feat: remove @angular/http dependency from @angular/platform-server (#29408)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+21.sha-96b800c.with-local-changes
+ * @license Angular v8.0.0-beta.10+25.sha-9745f55.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
