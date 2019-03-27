60bdabecc54bd8d25c7f7264b1d454a529f62943
Miško Hevery <misko@hevery.com>
2019-03-27 17:02:57 +0000

    a2f8f55 refactor(ivy): rename flushHooksUpTo to select (#29527)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+1.sha-a28b3e3.with-local-changes
+ * @license Angular v8.0.0-beta.10+2.sha-a2f8f55.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.10+1.sha-a28b3e3.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.10+2.sha-a2f8f55.with-local-changes');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+1.sha-a28b3e3.with-local-changes
+ * @license Angular v8.0.0-beta.10+2.sha-a2f8f55.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+1.sha-a28b3e3.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+2.sha-a2f8f55.with-local-changes');
 
 /**
  * @license
