a27ce06e880b46acd33e8b3b13f393aa3edd7151
Miško Hevery <misko@hevery.com>
2019-03-27 17:21:50 +0000

    6c33058 docs: add library structure (#29376)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+4.sha-06056b4.with-local-changes
+ * @license Angular v8.0.0-beta.10+11.sha-6c33058.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+4.sha-06056b4.with-local-changes
+ * @license Angular v8.0.0-beta.10+11.sha-6c33058.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5700,7 +5700,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+4.sha-06056b4.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+11.sha-6c33058.with-local-changes');
 
 /**
  * @license
