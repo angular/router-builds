a9fb0e5d2227f08834be2bd758d2c2e709f0c062
Miško Hevery <misko@hevery.com>
2019-03-27 17:27:05 +0000

    b17d1a9 docs: update description text (#28507)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+3.sha-3c11646.with-local-changes
+ * @license Angular v8.0.0-beta.10+16.sha-b17d1a9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.10+3.sha-3c11646.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.10+16.sha-b17d1a9.with-local-changes');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+3.sha-3c11646.with-local-changes
+ * @license Angular v8.0.0-beta.10+16.sha-b17d1a9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5700,7 +5700,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+3.sha-3c11646.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+16.sha-b17d1a9.with-local-changes');
 
 /**
  * @license
