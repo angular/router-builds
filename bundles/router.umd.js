c439df7b076158b57192b6ac698f2801b85de620
Miško Hevery <misko@hevery.com>
2019-03-27 17:26:57 +0000

    ac3ce0d docs: replaced hero with heroes component property (#29487) PR Close #29487

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+16.sha-b17d1a9.with-local-changes
+ * @license Angular v8.0.0-beta.10+12.sha-ac3ce0d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.10+16.sha-b17d1a9.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.10+12.sha-ac3ce0d.with-local-changes');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+16.sha-b17d1a9.with-local-changes
+ * @license Angular v8.0.0-beta.10+12.sha-ac3ce0d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
