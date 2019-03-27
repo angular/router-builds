d7cb0faf881a2bfc3546983fc56d244661f4ba57
Miško Hevery <misko@hevery.com>
2019-03-27 20:00:32 +0000

    96b800c feat(ivy): select instruction now generated in front of all relevant instructions (#29546)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+23.sha-1727fe2.with-local-changes
+ * @license Angular v8.0.0-beta.10+21.sha-96b800c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8255,7 +8255,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.10+23.sha-1727fe2.with-local-changes');
+const VERSION = new Version('8.0.0-beta.10+21.sha-96b800c.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
