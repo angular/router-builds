16d32792fe69a4f2a89ec4aeed89769fb912e5bd
Miško Hevery <misko@hevery.com>
2019-03-27 21:07:32 +0000

    aaa8a3a build: update bazel to 0.24 (#29530)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+27.sha-80161d5.with-local-changes
+ * @license Angular v8.0.0-beta.10+28.sha-aaa8a3a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.10+27.sha-80161d5.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.10+28.sha-aaa8a3a.with-local-changes');
 
     /**
      * @license
