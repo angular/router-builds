44f844e2802178adef30ab0ae4500e36145fc13c
Miško Hevery <misko@hevery.com>
2019-03-27 20:02:53 +0000

    c981006 ci: lock chrome and firefox versions for saucelabs (#29529)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+25.sha-9745f55.with-local-changes
+ * @license Angular v8.0.0-beta.10+24.sha-c981006.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.10+25.sha-9745f55.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.10+24.sha-c981006.with-local-changes');
 
     /**
      * @license
