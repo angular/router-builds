495cec820869772cfbcb4269d9e296aab9d0d31c
Miško Hevery <misko@hevery.com>
2019-03-27 17:20:33 +0000

    3c11646 ci: update windows CI bazelrc (#29542)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+11.sha-6c33058.with-local-changes
+ * @license Angular v8.0.0-beta.10+3.sha-3c11646.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
