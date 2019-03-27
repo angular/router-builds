55b26f72fef3cf7cc482f968654a810c0a1b28c2
Miško Hevery <misko@hevery.com>
2019-03-27 23:41:16 +0000

    931d356 Revert "feat(ivy): add property instruction (#29513)"

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+31.sha-e4c1c88.with-local-changes
+ * @license Angular v8.0.0-beta.10+32.sha-931d356.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+31.sha-e4c1c88.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+32.sha-931d356.with-local-changes');
 
 /**
  * @license
