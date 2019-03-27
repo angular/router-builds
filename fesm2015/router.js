b8478935f41b66c6104d883a9cb99041508bb97c
Miško Hevery <misko@hevery.com>
2019-03-27 21:07:49 +0000

    1e5a818 fix(ivy): ngtsc is unable to detect flat module entry-point on windows (#29453)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+28.sha-aaa8a3a.with-local-changes
+ * @license Angular v8.0.0-beta.10+30.sha-1e5a818.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8067,7 +8067,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.10+28.sha-aaa8a3a.with-local-changes');
+const VERSION = new Version('8.0.0-beta.10+30.sha-1e5a818.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
