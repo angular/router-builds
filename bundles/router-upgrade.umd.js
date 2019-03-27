382a94419693c32cf089980a0a01cfd141a3db02
Miško Hevery <misko@hevery.com>
2019-03-27 17:18:41 +0000

    50fb629 docs: add Bonnie to GDE page (#29432)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+9.sha-309ffe7.with-local-changes
+ * @license Angular v8.0.0-beta.10+10.sha-50fb629.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+9.sha-309ffe7.with-local-changes
+ * @license Angular v8.0.0-beta.10+10.sha-50fb629.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.10+9.sha-309ffe7.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.10+10.sha-50fb629.with-local-changes');
 
     /**
      * @license
