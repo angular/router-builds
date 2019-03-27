1382b0ec3c988d17b26fdf62c1fd7eb068ec537e
Miško Hevery <misko@hevery.com>
2019-03-27 19:58:52 +0000

    96b800c feat(ivy): select instruction now generated in front of all relevant instructions (#29546)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+19.sha-401b8ee.with-local-changes
+ * @license Angular v8.0.0-beta.10+21.sha-96b800c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.10+19.sha-401b8ee.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.10+21.sha-96b800c.with-local-changes');
 
     /**
      * @license
