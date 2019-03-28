cefa02d1d9782b091679d7f9e4ee3f9519ffc42f
Jason Aden <jasonaden@google.com>
2019-03-28 22:51:59 +0000

    6c76dfc docs: copy-edit ivy opt-in page (#29507)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+41.sha-78ba503.with-local-changes
+ * @license Angular v8.0.0-beta.10+42.sha-6c76dfc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8255,7 +8255,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.10+41.sha-78ba503.with-local-changes');
+const VERSION = new Version('8.0.0-beta.10+42.sha-6c76dfc.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+41.sha-78ba503.with-local-changes
+ * @license Angular v8.0.0-beta.10+42.sha-6c76dfc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
