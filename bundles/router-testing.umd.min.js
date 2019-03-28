e01df6be2e4e62bc7ae1894363cd1f281552512d
Kara Erickson <karakara@google.com>
2019-03-28 21:16:46 +0000

    7b27009 fix(ivy): fix proliferation of provider overrides for modules (#29571)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+34.sha-71ec998.with-local-changes
+ * @license Angular v8.0.0-beta.10+36.sha-7b27009.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+34.sha-71ec998.with-local-changes
+ * @license Angular v8.0.0-beta.10+36.sha-7b27009.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8255,7 +8255,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.10+34.sha-71ec998.with-local-changes');
+const VERSION = new Version('8.0.0-beta.10+36.sha-7b27009.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
