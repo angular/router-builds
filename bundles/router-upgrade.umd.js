b4262c21a9ca784898f7f6317bb3014216cedfa7
Miško Hevery <misko@hevery.com>
2019-03-28 17:33:38 +0000

    71ec998 fix(ivy): allow TestBed to recompile AOT-compiled components in case of template overrides (#29555)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+35.sha-d46a7c8.with-local-changes
+ * @license Angular v8.0.0-beta.10+34.sha-71ec998.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
