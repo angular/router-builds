341c9bbd28ad8c288efd3360b249762ce2096542
Jason Aden <jasonaden@google.com>
2019-03-28 22:51:33 +0000

    3ad67e5 docs: add asset config details (#28214)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+15.sha-baec8ab.with-local-changes
+ * @license Angular v7.2.11+16.sha-3ad67e5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5814,7 +5814,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.11+15.sha-baec8ab.with-local-changes');
+    var VERSION = new core.Version('7.2.11+16.sha-3ad67e5.with-local-changes');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+15.sha-baec8ab.with-local-changes
+ * @license Angular v7.2.11+16.sha-3ad67e5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
