6a0ea593adc2db2e5168064054de47c6c38ccc31
Jason Aden <jasonaden@google.com>
2019-03-28 22:51:25 +0000

    57f7996 docs: add asset config details (#28214)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+42.sha-6c76dfc.with-local-changes
+ * @license Angular v8.0.0-beta.10+44.sha-57f7996.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8067,7 +8067,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.10+42.sha-6c76dfc.with-local-changes');
+const VERSION = new Version('8.0.0-beta.10+44.sha-57f7996.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
