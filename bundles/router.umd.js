f3b2afc3da88852819c3fecc6e82ff153a914b7e
Kara Erickson <karakara@google.com>
2019-03-28 21:56:52 +0000

    7b27009 fix(ivy): fix proliferation of provider overrides for modules (#29571)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+34.sha-71ec998.with-local-changes
+ * @license Angular v8.0.0-beta.10+36.sha-7b27009.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.10+34.sha-71ec998.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.10+36.sha-7b27009.with-local-changes');
 
     /**
      * @license
