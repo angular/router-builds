3e18e07e9efa25801559b71954647f1ee1b8c3a3
Jason Aden <jasonaden@google.com>
2019-03-28 22:37:19 +0000

    632669f refactor: update tsickle comment to be more precise (#29513)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+36.sha-7b27009.with-local-changes
+ * @license Angular v8.0.0-beta.10+38.sha-632669f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.10+36.sha-7b27009.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.10+38.sha-632669f.with-local-changes');
 
     /**
      * @license
