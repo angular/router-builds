eb27f962e6ff98b645945d0363dc712412794339
Jason Aden <jasonaden@google.com>
2019-03-28 22:52:53 +0000

    57f7996 docs: add asset config details (#28214)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+42.sha-6c76dfc.with-local-changes
+ * @license Angular v8.0.0-beta.10+44.sha-57f7996.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.10+42.sha-6c76dfc.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.10+44.sha-57f7996.with-local-changes');
 
     /**
      * @license
