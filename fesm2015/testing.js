3466c9db6d958afd291c6ddb2fffc7029a99c8af
Jason Aden <jasonaden@google.com>
2019-03-28 22:39:04 +0000

    632669f refactor: update tsickle comment to be more precise (#29513)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+36.sha-7b27009.with-local-changes
+ * @license Angular v8.0.0-beta.10+38.sha-632669f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+36.sha-7b27009.with-local-changes
+ * @license Angular v8.0.0-beta.10+38.sha-632669f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5700,7 +5700,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+36.sha-7b27009.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+38.sha-632669f.with-local-changes');
 
 /**
  * @license
