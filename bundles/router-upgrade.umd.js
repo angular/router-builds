95d14c411ff00b61d93ed87c2b669859b8c36d61
Jason Aden <jasonaden@google.com>
2019-03-29 18:07:47 +0000

    c67f6a7 docs: fix typo in renderer description (#29581)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+55.sha-12c9bd2.with-local-changes
+ * @license Angular v8.0.0-beta.10+57.sha-c67f6a7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
