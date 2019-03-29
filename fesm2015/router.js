6797e953114c73c8830f0232ad238be9d2b2fdba
Jason Aden <jasonaden@google.com>
2019-03-29 18:09:25 +0000

    60b5bf7 docs: fix typo in renderer description (#29581)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+24.sha-7671c73.with-local-changes
+ * @license Angular v7.2.11+25.sha-60b5bf7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7279,7 +7279,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.11+24.sha-7671c73.with-local-changes');
+const VERSION = new Version('7.2.11+25.sha-60b5bf7.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
