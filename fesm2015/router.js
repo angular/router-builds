8e932fe016b9fd8ff3485eab93dafcfdcdb842f9
Jason Aden <jasonaden@google.com>
2019-03-29 17:50:41 +0000

    a81fd5f build(docs-infra): upgrade cli command docs sources to 35df0b528 (#29583)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+44.sha-57f7996.with-local-changes
+ * @license Angular v8.0.0-beta.10+45.sha-a81fd5f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8255,7 +8255,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.10+44.sha-57f7996.with-local-changes');
+const VERSION = new Version('8.0.0-beta.10+45.sha-a81fd5f.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
