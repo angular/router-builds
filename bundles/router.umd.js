cd41691e1ccc69ddd24ca38725a6f6b9969332c7
Jason Aden <jasonaden@google.com>
2019-03-29 17:49:37 +0000

    71daa11 docs: add note about ngModel usage in structural directives guide (#29522)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+45.sha-a81fd5f.with-local-changes
+ * @license Angular v8.0.0-beta.10+46.sha-71daa11.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.10+45.sha-a81fd5f.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.10+46.sha-71daa11.with-local-changes');
 
     /**
      * @license
