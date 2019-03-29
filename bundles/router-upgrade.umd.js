f7aee36f70a05314ca954a52597b2c0a856c96e7
Jason Aden <jasonaden@google.com>
2019-03-29 17:49:23 +0000

    a81fd5f build(docs-infra): upgrade cli command docs sources to 35df0b528 (#29583)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+44.sha-57f7996.with-local-changes
+ * @license Angular v8.0.0-beta.10+45.sha-a81fd5f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+44.sha-57f7996.with-local-changes
+ * @license Angular v8.0.0-beta.10+45.sha-a81fd5f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+44.sha-57f7996.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+45.sha-a81fd5f.with-local-changes');
 
 /**
  * @license
