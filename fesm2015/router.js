d0361886d452faff2bd60c797069c8286a5b2361
Jason Aden <jasonaden@google.com>
2019-03-29 18:00:09 +0000

    303eae9 fix(ivy): querying should be possible on any debug element (#29534)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+51.sha-22b89ea.with-local-changes
+ * @license Angular v8.0.0-beta.10+53.sha-303eae9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8255,7 +8255,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.10+51.sha-22b89ea.with-local-changes');
+const VERSION = new Version('8.0.0-beta.10+53.sha-303eae9.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
