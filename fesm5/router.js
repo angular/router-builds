85de709b1508d749d0b59c270f855073cc059444
Jason Aden <jasonaden@google.com>
2019-03-29 17:55:29 +0000

    22b89ea docs: move text that mention what is expected to fail up higher into the section (#27329)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+47.sha-eb0e29b.with-local-changes
+ * @license Angular v8.0.0-beta.10+51.sha-22b89ea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+47.sha-eb0e29b.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+51.sha-22b89ea.with-local-changes');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+47.sha-eb0e29b.with-local-changes
+ * @license Angular v8.0.0-beta.10+51.sha-22b89ea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
