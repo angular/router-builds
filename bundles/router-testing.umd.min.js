701d1fa96d75fb3e4cd0f751604cb0d7bcf2ae79
Jason Aden <jasonaden@google.com>
2019-03-29 18:19:11 +0000

    2b6e107 build(docs-infra): add check to ensure all contributor pictures exist (#29553)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+57.sha-c67f6a7.with-local-changes
+ * @license Angular v8.0.0-beta.10+64.sha-2b6e107.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
