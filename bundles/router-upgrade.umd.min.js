01da6e39e101b1495cc66202aac3a2fe9af73833
Jason Aden <jasonaden@google.com>
2019-03-29 18:02:09 +0000

    7671c73 fix(common): escape query selector used when anchor scrolling (#29577)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+22.sha-cfa6ab1.with-local-changes
+ * @license Angular v7.2.11+24.sha-7671c73.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
