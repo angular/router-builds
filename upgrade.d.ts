44dab81bcc3310df506e200ee2498046f96817eb
Jason Aden <jasonaden@google.com>
2019-03-29 17:48:36 +0000

    d9b1136 docs: add note about ngModel usage in structural directives guide (#29522)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+16.sha-3ad67e5.with-local-changes
+ * @license Angular v7.2.11+18.sha-d9b1136.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
