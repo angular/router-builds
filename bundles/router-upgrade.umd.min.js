3a1c928635f00b66626d57263f80ffaa1db4a486
Jason Aden <jasonaden@google.com>
2019-04-01 22:14:51 +0000

    23f0a04 build: Remove --watchfs from bazelrc file (#29635)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+45.sha-e884c0c.with-local-changes
+ * @license Angular v7.2.11+46.sha-23f0a04.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
