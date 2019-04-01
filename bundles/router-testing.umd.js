68f89006fa14a32a5931dbbd91b15dd8dc72e6e1
Jason Aden <jasonaden@google.com>
2019-04-01 18:49:08 +0000

    9cd9053 docs: add missing window key shortcut to language service guide (#29613)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+77.sha-ff2a55f.with-local-changes
+ * @license Angular v8.0.0-beta.10+78.sha-9cd9053.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+77.sha-ff2a55f.with-local-changes
+ * @license Angular v8.0.0-beta.10+78.sha-9cd9053.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.10+77.sha-ff2a55f.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.10+78.sha-9cd9053.with-local-changes');
 
     /**
      * @license
