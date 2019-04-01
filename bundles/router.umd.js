b6dee9d6b0833ee9c47bb9cd0f3edfed85245cdc
Jason Aden <jasonaden@google.com>
2019-04-01 23:34:07 +0000

    deca6a6 test(docs-infra): avoid click-related CI flake in e2e test (#29641)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+92.sha-8bfaaf1.with-local-changes
+ * @license Angular v8.0.0-beta.10+94.sha-deca6a6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.10+92.sha-8bfaaf1.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.10+94.sha-deca6a6.with-local-changes');
 
     /**
      * @license
