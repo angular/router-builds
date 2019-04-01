f1b77c64cded08c00bb97d6fffb228064adfd6af
Jason Aden <jasonaden@google.com>
2019-04-01 18:13:18 +0000

    6759aa6 docs: added complete path to the spec file (#29621) PR Close #29621

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+65.sha-ab4be7b.with-local-changes
+ * @license Angular v8.0.0-beta.10+66.sha-6759aa6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.10+65.sha-ab4be7b.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.10+66.sha-6759aa6.with-local-changes');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+65.sha-ab4be7b.with-local-changes
+ * @license Angular v8.0.0-beta.10+66.sha-6759aa6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+65.sha-ab4be7b.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+66.sha-6759aa6.with-local-changes');
 
 /**
  * @license
