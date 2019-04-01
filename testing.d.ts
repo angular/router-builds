fd0d495d2e05d6129f6976e01d35bbcbaf80aeab
Jason Aden <jasonaden@google.com>
2019-04-01 18:25:07 +0000

    a52123f docs: add missing picture of Jason Bedard (#29596)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+25.sha-60b5bf7.with-local-changes
+ * @license Angular v7.2.11+37.sha-a52123f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
