164705835436669c86238ebb6766882de967b35b
Jason Aden <jasonaden@google.com>
2019-04-01 19:24:26 +0000

    ab72e06 docs: add custom-element ref to library overview (#28476)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+43.sha-5218f5a.with-local-changes
+ * @license Angular v7.2.11+44.sha-ab72e06.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5814,7 +5814,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.11+43.sha-5218f5a.with-local-changes');
+    var VERSION = new core.Version('7.2.11+44.sha-ab72e06.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+43.sha-5218f5a.with-local-changes
+ * @license Angular v7.2.11+44.sha-ab72e06.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
