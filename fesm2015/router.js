6d9cc31643712ad8c62f3872e6b8ad9f6c3fae69
Jason Aden <jasonaden@google.com>
2019-04-01 22:31:45 +0000

    39ecc7b docs: improve formatDate description (#29632)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+46.sha-23f0a04.with-local-changes
+ * @license Angular v7.2.11+47.sha-39ecc7b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7279,7 +7279,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.11+46.sha-23f0a04.with-local-changes');
+const VERSION = new Version('7.2.11+47.sha-39ecc7b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+46.sha-23f0a04.with-local-changes
+ * @license Angular v7.2.11+47.sha-39ecc7b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
