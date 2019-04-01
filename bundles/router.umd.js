58023276717a8d0d92494cbe05a88538f33f2fd2
Jason Aden <jasonaden@google.com>
2019-04-01 18:21:16 +0000

    b2c03b8 build(docs-infra): remove unused `PhantomJS` dependency (#29611)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+67.sha-fd4c939.with-local-changes
+ * @license Angular v8.0.0-beta.10+68.sha-b2c03b8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.10+67.sha-fd4c939.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.10+68.sha-b2c03b8.with-local-changes');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+67.sha-fd4c939.with-local-changes
+ * @license Angular v8.0.0-beta.10+68.sha-b2c03b8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5700,7 +5700,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+67.sha-fd4c939.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+68.sha-b2c03b8.with-local-changes');
 
 /**
  * @license
