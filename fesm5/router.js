9f6233d22e696f3ff8310bb4a532be09f45ee66f
Jason Aden <jasonaden@google.com>
2019-04-01 22:49:25 +0000

    8bfaaf1 docs: add Web Worker guide (#29633)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+91.sha-1f469cd.with-local-changes
+ * @license Angular v8.0.0-beta.10+92.sha-8bfaaf1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5700,7 +5700,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+91.sha-1f469cd.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+92.sha-8bfaaf1.with-local-changes');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+91.sha-1f469cd.with-local-changes
+ * @license Angular v8.0.0-beta.10+92.sha-8bfaaf1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
