f84791db8d6c11c02f13e91523b3025c27ec4743
Jason Aden <jasonaden@google.com>
2019-04-01 18:22:37 +0000

    2ff3d2d test(upgrade): work around SauceLabs issue with loading AngularJS files (#29603)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+69.sha-33016b8.with-local-changes
+ * @license Angular v8.0.0-beta.10+70.sha-2ff3d2d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
