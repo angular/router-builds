29f8dbd2c758d2ac19af23c0309d394d94a6f494
Jason Aden <jasonaden@google.com>
2019-04-01 22:32:15 +0000

    7041e61 perf(ivy): basic incremental compilation for ngtsc (#29380)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+87.sha-3e56976.with-local-changes
+ * @license Angular v8.0.0-beta.10+90.sha-7041e61.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
