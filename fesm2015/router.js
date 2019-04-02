e74187fb2c97de4c8f34a258a3d0d841d57ee3d4
Jason Aden <jasonaden@google.com>
2019-04-02 18:04:33 +0000

    91f2b1e refactor(platform-browser): rename _singleTagWhitelist (#29592)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+57.sha-6bf8e37.with-local-changes
+ * @license Angular v7.2.11+56.sha-91f2b1e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7279,7 +7279,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.11+57.sha-6bf8e37.with-local-changes');
+const VERSION = new Version('7.2.11+56.sha-91f2b1e.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
