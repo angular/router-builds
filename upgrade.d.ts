ea8291260080aecdce418643929a1ad2475ccb64
Jason Aden <jasonaden@google.com>
2019-04-02 18:12:50 +0000

    98f8b0f fix(ivy): ngcc - properly handle aliases class expressions (#29119)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+104.sha-e5201f9.with-local-changes
+ * @license Angular v8.0.0-beta.10+108.sha-98f8b0f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
