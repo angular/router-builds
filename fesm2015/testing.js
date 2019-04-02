5e58eed5fbcf77321c973a7d23c4214422a7d0bc
Jason Aden <jasonaden@google.com>
2019-04-02 23:29:39 +0000

    afd4a4e feat(core): Add "AbstractType<T>" interface (#29295)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+120.sha-60afe88.with-local-changes
+ * @license Angular v8.0.0-beta.10+121.sha-afd4a4e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
