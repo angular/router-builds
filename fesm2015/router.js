5b1b46273e19f6bbf7591770e2151f9a9dcd1584
Jason Aden <jasonaden@google.com>
2019-04-02 23:05:39 +0000

    7c8f4e3 feat(core): template-var-assignment update schematic (#29608)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+109.sha-03d914a.with-local-changes
+ * @license Angular v8.0.0-beta.10+113.sha-7c8f4e3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8067,7 +8067,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.10+109.sha-03d914a.with-local-changes');
+const VERSION = new Version('8.0.0-beta.10+113.sha-7c8f4e3.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+109.sha-03d914a.with-local-changes
+ * @license Angular v8.0.0-beta.10+113.sha-7c8f4e3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
