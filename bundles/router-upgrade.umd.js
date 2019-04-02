3d3df1f4cb84130e40cc0a863f4dc6071d29ec0c
Jason Aden <jasonaden@google.com>
2019-04-02 18:03:43 +0000

    9f54d76 refactor(upgrade): use Bazel packages to avoid symlinks in the source (#29466)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+106.sha-cbd5d28.with-local-changes
+ * @license Angular v8.0.0-beta.10+105.sha-9f54d76.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
