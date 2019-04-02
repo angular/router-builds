40c1a76e130e3b8ecf66d8b6367b9d87b49b1ec5
Jason Aden <jasonaden@google.com>
2019-04-02 17:58:51 +0000

    1293da1 refactor(core): update docs (#29600)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+101.sha-d11b0c0.with-local-changes
+ * @license Angular v8.0.0-beta.10+100.sha-1293da1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
