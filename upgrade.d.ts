3c78e5e1e3ebd0619d5354c762a5edbe8855d408
Jason Aden <jasonaden@google.com>
2019-04-02 23:16:44 +0000

    6b39c9c fix(compiler-cli): ngcc - cope with processing entry-points multiple times (#29657)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+116.sha-c579949.with-local-changes
+ * @license Angular v8.0.0-beta.10+117.sha-6b39c9c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
