aca4b273c911a96e9cda23be166006db472b8dc6
Jason Aden <jasonaden@google.com>
2019-04-02 17:53:23 +0000

    7c1f73a build(bazel): update to nodejs rules 0.27.9 (#29647)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+95.sha-9e4c0bd.with-local-changes
+ * @license Angular v8.0.0-beta.10+99.sha-7c1f73a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.10+95.sha-9e4c0bd.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.10+99.sha-7c1f73a.with-local-changes');
 
     /**
      * @license
