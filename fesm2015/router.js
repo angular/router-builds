3ab65e565707c8f18ff7ebddc893edd4edbcbd4c
Jason Aden <jasonaden@google.com>
2019-04-02 17:56:36 +0000

    1c07061 refactor: change error message (#29594)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+99.sha-7c1f73a.with-local-changes
+ * @license Angular v8.0.0-beta.10+102.sha-1c07061.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8067,7 +8067,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.10+99.sha-7c1f73a.with-local-changes');
+const VERSION = new Version('8.0.0-beta.10+102.sha-1c07061.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+99.sha-7c1f73a.with-local-changes
+ * @license Angular v8.0.0-beta.10+102.sha-1c07061.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
