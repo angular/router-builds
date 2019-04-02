28a7bd7f2d6b319a33b2e3c97e871fbae871e858
Jason Aden <jasonaden@google.com>
2019-04-02 18:14:30 +0000

    03d914a build: hide @angular/http for Angular v8 (#29550)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+108.sha-98f8b0f.with-local-changes
+ * @license Angular v8.0.0-beta.10+109.sha-03d914a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
