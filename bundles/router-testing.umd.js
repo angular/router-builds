0d19d4225b5648441a3bb31cee40be18f5c26d6e
Jason Aden <jasonaden@google.com>
2019-04-02 23:08:07 +0000

    69786c2 docs: add Deborah Kurata to Trusted Collaborators (#29605)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+58.sha-50bcaa6.with-local-changes
+ * @license Angular v7.2.11+59.sha-69786c2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
