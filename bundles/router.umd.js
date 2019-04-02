481c4f9ded38946e970e67ade09b8d160b16a14f
Jason Aden <jasonaden@google.com>
2019-04-02 23:27:58 +0000

    afd4a4e feat(core): Add "AbstractType<T>" interface (#29295)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+120.sha-60afe88.with-local-changes
+ * @license Angular v8.0.0-beta.10+121.sha-afd4a4e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.10+120.sha-60afe88.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.10+121.sha-afd4a4e.with-local-changes');
 
     /**
      * @license
