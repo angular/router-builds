5f634137906546b8cb5462dee5baf2a1c09c8f6d
Jason Aden <jasonaden@google.com>
2019-04-02 18:04:15 +0000

    e5201f9 refactor(platform-browser): rename _singleTagWhitelist (#29592)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+105.sha-9f54d76.with-local-changes
+ * @license Angular v8.0.0-beta.10+104.sha-e5201f9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
