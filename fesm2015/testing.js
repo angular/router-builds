d640815407608a5bce5a18992272c19c33141367
Jason Aden <jasonaden@google.com>
2019-04-02 18:01:56 +0000

    c7075fe refactor(compiler): rename INTERPOLATION_BLACKLIST_REGEXPS (#29593)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+53.sha-f6864cc.with-local-changes
+ * @license Angular v7.2.11+55.sha-c7075fe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
