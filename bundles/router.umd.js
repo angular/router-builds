144410756b74b6f5672e4cc5af4c6e71467d502f
Jason Aden <jasonaden@google.com>
2019-04-02 18:14:16 +0000

    98f8b0f fix(ivy): ngcc - properly handle aliases class expressions (#29119)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+104.sha-e5201f9.with-local-changes
+ * @license Angular v8.0.0-beta.10+108.sha-98f8b0f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.10+104.sha-e5201f9.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.10+108.sha-98f8b0f.with-local-changes');
 
     /**
      * @license
