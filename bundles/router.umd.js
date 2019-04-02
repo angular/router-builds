83d7ed00987d924eabcc3f1030a5684138444ca9
Jason Aden <jasonaden@google.com>
2019-04-02 17:50:01 +0000

    9e4c0bd docs: fix typos in getting-started (#29665)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+94.sha-deca6a6.with-local-changes
+ * @license Angular v8.0.0-beta.10+95.sha-9e4c0bd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.10+94.sha-deca6a6.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.10+95.sha-9e4c0bd.with-local-changes');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+94.sha-deca6a6.with-local-changes
+ * @license Angular v8.0.0-beta.10+95.sha-9e4c0bd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
