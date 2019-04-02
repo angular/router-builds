deb7575498aa5af40a6767d0393833aafc411d8c
Jason Aden <jasonaden@google.com>
2019-04-02 17:57:54 +0000

    1c07061 refactor: change error message (#29594)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+99.sha-7c1f73a.with-local-changes
+ * @license Angular v8.0.0-beta.10+102.sha-1c07061.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5773,7 +5773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.10+99.sha-7c1f73a.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.10+102.sha-1c07061.with-local-changes');
 
     /**
      * @license
