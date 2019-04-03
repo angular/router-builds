020744966b0e2e1e341cc4f1c1312ea75b084a1d
Jason Aden <jasonaden@google.com>
2019-04-03 22:49:04 +0000

    9810c6c fix(bazel): support running ng-add on minimal applications (#29681)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+2.sha-3246399.with-local-changes
+ * @license Angular v8.0.0-beta.11+4.sha-9810c6c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+2.sha-3246399.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+4.sha-9810c6c.with-local-changes');
 
 /**
  * @license
