5a6699061a617221d2c3cc1df257d2c4b9c04b89
Jason Aden <jasonaden@google.com>
2019-04-03 22:13:01 +0000

    dc5577d docs: release notes for the v7.2.12 release

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+122.sha-ddb935f.with-local-changes
+ * @license Angular v8.0.0-beta.10+124.sha-dc5577d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+122.sha-ddb935f.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+124.sha-dc5577d.with-local-changes');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+122.sha-ddb935f.with-local-changes
+ * @license Angular v8.0.0-beta.10+124.sha-dc5577d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
