0be7790f6b82b2809a71377db084ed919d55c353
Jason Aden <jasonaden@google.com>
2019-04-03 22:59:30 +0000

    c7ff728 docs: add angular.de to the workshops page (#28856)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+6.sha-b9fead7.with-local-changes
+ * @license Angular v8.0.0-beta.11+8.sha-c7ff728.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.11+6.sha-b9fead7.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.11+8.sha-c7ff728.with-local-changes');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+6.sha-b9fead7.with-local-changes
+ * @license Angular v8.0.0-beta.11+8.sha-c7ff728.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
