e0a97187e69ded15243c9a91bd87e57b7e1baaf5
Jason Aden <jasonaden@google.com>
2019-04-03 22:44:33 +0000

    3246399 fix(ivy): ngcc - show logging via CLI by default (#29686)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11.with-local-changes
+ * @license Angular v8.0.0-beta.11+2.sha-3246399.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
