1f4555b86a199589b7b6ecd256a535039604b890
Jason Aden <jasonaden@google.com>
2019-04-03 23:01:15 +0000

    0930820 docs: add angular.de to the workshops page (#28856)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+3.sha-96ff497.with-local-changes
+ * @license Angular v7.2.12+4.sha-0930820.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+3.sha-96ff497.with-local-changes
+ * @license Angular v7.2.12+4.sha-0930820.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7279,7 +7279,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.12+3.sha-96ff497.with-local-changes');
+const VERSION = new Version('7.2.12+4.sha-0930820.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
