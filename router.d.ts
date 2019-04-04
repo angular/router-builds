3c800fd081a99e1cdddc84eb530ca08394443d59
Igor Minar <igor@angular.io>
2019-04-04 21:43:46 +0000

    2bd9214 fix(bazel): remove karma-jasmine from ts_web_test_suite (#29695)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+13.sha-c3aea73.with-local-changes
+ * @license Angular v8.0.0-beta.11+17.sha-2bd9214.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
