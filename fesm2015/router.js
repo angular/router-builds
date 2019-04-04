b1bfb3a560951028c68b5854f404c0d0bb437f78
Igor Minar <igor@angular.io>
2019-04-04 21:41:40 +0000

    e483078 docs: add universal terms to glossary (#28492)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+10.sha-29481a1.with-local-changes
+ * @license Angular v7.2.12+11.sha-e483078.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7279,7 +7279,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.12+10.sha-29481a1.with-local-changes');
+const VERSION = new Version('7.2.12+11.sha-e483078.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+10.sha-29481a1.with-local-changes
+ * @license Angular v7.2.12+11.sha-e483078.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
