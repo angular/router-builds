865e2d05f9e5115d5736fbc214bd9ce0ef0d7ce8
Igor Minar <igor@angular.io>
2019-04-04 23:46:56 +0000

    1423cc0 docs: correct route path description (#29669)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+11.sha-e483078.with-local-changes
+ * @license Angular v7.2.12+12.sha-1423cc0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
