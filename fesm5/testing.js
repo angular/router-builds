0c6daa9fabb743be6a819509bbef877b9e747b2d
Jason Aden <jasonaden@google.com>
2019-04-04 18:05:21 +0000

    9e33dc3 fix(bazel): use //:tsconfig.json as the default for ng_module (#29670) (#29711)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+4.sha-0930820.with-local-changes
+ * @license Angular v7.2.12+5.sha-9e33dc3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
