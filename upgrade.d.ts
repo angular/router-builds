18d0e3d97db4cc7136c272770a74b80fd912fa9c
Jason Aden <jasonaden@google.com>
2019-04-04 18:14:22 +0000

    c3aea73 build: add render3 entry-point to legacy test systemjs config (#29675)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+12.sha-eaa49bd.with-local-changes
+ * @license Angular v8.0.0-beta.11+13.sha-c3aea73.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
