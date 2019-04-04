7fd269f9e35f2597d4074367da4752c7d7316f1a
Igor Minar <igor@angular.io>
2019-04-04 23:47:37 +0000

    609024f fix(core): Deprecate TestBed.get(...):any (#29290)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+23.sha-9c056b9.with-local-changes
+ * @license Angular v8.0.0-beta.11+24.sha-609024f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5774,7 +5774,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.11+23.sha-9c056b9.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.11+24.sha-609024f.with-local-changes');
 
     /**
      * @license
