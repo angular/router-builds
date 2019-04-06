c72a1c62809fe16d258585a8f90613f8636b9da3
Igor Minar <igor@angular.io>
2019-04-06 00:18:14 +0000

    ec56354 fix(ivy): ensure parent/sub-class components evaluate styling correctly (#29602)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+28.sha-5c13fee.with-local-changes
+ * @license Angular v8.0.0-beta.11+29.sha-ec56354.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
