84f6c5ff9ec4cb50b0cd2e1b493bd9c3ebbaee35
Igor Minar <igor@angular.io>
2019-04-08 17:10:27 +0000

    895b045 refactor(docs-infra): rename properties (blacklisted --> ignored) (#29754)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+14.sha-7d6d4c4.with-local-changes
+ * @license Angular v7.2.12+16.sha-895b045.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
