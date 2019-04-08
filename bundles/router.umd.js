a5d7cfccace95435644c4f262af991510ad5a128
Igor Minar <igor@angular.io>
2019-04-08 17:08:43 +0000

    902a53a feat(bazel): update the build to use the new architect api (#29720)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+34.sha-6d35c50.with-local-changes
+ * @license Angular v8.0.0-beta.11+36.sha-902a53a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.11+34.sha-6d35c50.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.11+36.sha-902a53a.with-local-changes');
 
     /**
      * @license
