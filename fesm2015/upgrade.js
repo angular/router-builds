a67c7ca18b552b8d6b22c6f353af7720de97705b
Igor Minar <igor@angular.io>
2019-04-08 17:09:59 +0000

    902a53a feat(bazel): update the build to use the new architect api (#29720)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+34.sha-6d35c50.with-local-changes
+ * @license Angular v8.0.0-beta.11+36.sha-902a53a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
