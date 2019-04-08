ccba308827ef6356cd45b875b03ae1d5a8a32ecb
Igor Minar <igor@angular.io>
2019-04-08 19:22:52 +0000

    cb9ee34 ci: simplify RBE execution (#29731)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+44.sha-98cf3e8.with-local-changes
+ * @license Angular v8.0.0-beta.11+43.sha-cb9ee34.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
