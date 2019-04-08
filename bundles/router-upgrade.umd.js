84ff3e701042013b239eb07f411c6d4d0520eab8
Igor Minar <igor@angular.io>
2019-04-08 17:05:59 +0000

    6d35c50 test(core): add test for template variable object property assignment (#29708)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+32.sha-8bbf481.with-local-changes
+ * @license Angular v8.0.0-beta.11+34.sha-6d35c50.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
