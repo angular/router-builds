c068f6add071eed8149386d602d33c8db453f6fe
Igor Minar <igor@angular.io>
2019-04-08 19:22:39 +0000

    98cf3e8 fix(ivy): creation mode not run on view detached from change detection (#29741)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+45.sha-84be7c5.with-local-changes
+ * @license Angular v8.0.0-beta.11+44.sha-98cf3e8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+45.sha-84be7c5.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+44.sha-98cf3e8.with-local-changes');
 
 /**
  * @license
