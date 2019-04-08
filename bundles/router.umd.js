f841d01cf0eba4565dfb4ff47c42fc0344020d2e
Igor Minar <igor@angular.io>
2019-04-08 19:23:17 +0000

    d144a3b fix(ivy): not throwing error for unknown properties on container nodes (#29691)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+43.sha-cb9ee34.with-local-changes
+ * @license Angular v8.0.0-beta.11+42.sha-d144a3b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.11+43.sha-cb9ee34.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.11+42.sha-d144a3b.with-local-changes');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+43.sha-cb9ee34.with-local-changes
+ * @license Angular v8.0.0-beta.11+42.sha-d144a3b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+43.sha-cb9ee34.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+42.sha-d144a3b.with-local-changes');
 
 /**
  * @license
