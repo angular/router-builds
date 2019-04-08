970b35b5d34d0a7067f67c14ba20332ba0fa420b
Igor Minar <igor@angular.io>
2019-04-08 17:10:06 +0000

    cf40105 build: ignore packages/bazel/node_modules (#29738)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+40.sha-c65ac7f.with-local-changes
+ * @license Angular v8.0.0-beta.11+38.sha-cf40105.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
