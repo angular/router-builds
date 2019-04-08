367023ba4c3b55b16398a144df5182a597f7ba3a
Igor Minar <igor@angular.io>
2019-04-08 19:20:54 +0000

    66b87ce fix(ivy): add dev mode counter information to styling (#29579)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+38.sha-cf40105.with-local-changes
+ * @license Angular v8.0.0-beta.11+41.sha-66b87ce.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
