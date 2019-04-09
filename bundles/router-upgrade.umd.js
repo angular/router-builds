d33647732cfa4d0df21278d251e714e1cfe8db1c
Igor Minar <igor@angular.io>
2019-04-09 00:39:29 +0000

    8c0c689 docs(router): use pipe in activatedRoute example (#29752)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+19.sha-77ee22e.with-local-changes
+ * @license Angular v7.2.12+18.sha-8c0c689.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
