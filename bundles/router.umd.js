d2ae4f67198af7a353bdac07f9c77345df2f13f8
Igor Minar <igor@angular.io>
2019-04-09 00:35:41 +0000

    632847d docs: typo fix, change "insure" to "ensure" (#29737) PR Close #29737

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+46.sha-e682b44.with-local-changes
+ * @license Angular v8.0.0-beta.11+47.sha-632847d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.11+46.sha-e682b44.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.11+47.sha-632847d.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+46.sha-e682b44.with-local-changes
+ * @license Angular v8.0.0-beta.11+47.sha-632847d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
