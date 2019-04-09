d1572a00a42425c301709d0eb603f3555541dc69
Igor Minar <igor@angular.io>
2019-04-09 19:40:18 +0000

    411c505 docs: update authors style guide to highlight related guidelines and position these guidelines (#28481)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+48.sha-82c77ce.with-local-changes
+ * @license Angular v8.0.0-beta.11+51.sha-411c505.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
