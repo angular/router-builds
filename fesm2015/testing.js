a0315b7e3496d90acb83553ee355648098ca93da
Igor Minar <igor@angular.io>
2019-04-09 19:45:09 +0000

    b960902 docs: fix incorrect header on custom event docs (#29784)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+20.sha-47c4868.with-local-changes
+ * @license Angular v7.2.12+23.sha-b960902.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
