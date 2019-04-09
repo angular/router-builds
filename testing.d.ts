63040c7054966e4ca36df9a34352534f761c8f12
Igor Minar <igor@angular.io>
2019-04-09 19:45:38 +0000

    2e1fc36 docs: update release schedule and examples of release numbers (#29714)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+23.sha-b960902.with-local-changes
+ * @license Angular v7.2.12+21.sha-2e1fc36.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
