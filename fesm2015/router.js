18812dfa6e8af0f4f13adc59ba61cb99bc9a85f4
Igor Minar <igor@angular.io>
2019-04-09 19:42:33 +0000

    4f0110e docs(core): DI module-level info fix (#29756)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+51.sha-411c505.with-local-changes
+ * @license Angular v8.0.0-beta.11+53.sha-4f0110e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8067,7 +8067,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.11+51.sha-411c505.with-local-changes');
+const VERSION = new Version('8.0.0-beta.11+53.sha-4f0110e.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
