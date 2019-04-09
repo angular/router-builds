1734abeaad0078b5bab1affbca31b7f333c63b68
Igor Minar <igor@angular.io>
2019-04-09 00:45:34 +0000

    82c77ce docs(core): update API doc examples to use static injector (#29729)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+50.sha-1102b02.with-local-changes
+ * @license Angular v8.0.0-beta.11+48.sha-82c77ce.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5774,7 +5774,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.11+50.sha-1102b02.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.11+48.sha-82c77ce.with-local-changes');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+50.sha-1102b02.with-local-changes
+ * @license Angular v8.0.0-beta.11+48.sha-82c77ce.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
