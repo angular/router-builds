a0ec1228240e7df2914d7d52e55593ea788c0d44
Igor Minar <igor@angular.io>
2019-04-09 00:40:15 +0000

    1102b02 refactor(core): static-query schematic should handle function callbacks (#29663)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+47.sha-632847d.with-local-changes
+ * @license Angular v8.0.0-beta.11+50.sha-1102b02.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.11+47.sha-632847d.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.11+50.sha-1102b02.with-local-changes');
 
     /**
      * @license
