9721a692c30f4683210598e878fbc7b78a287fa2
Igor Minar <igor@angular.io>
2019-04-09 19:43:09 +0000

    47c4868 docs: update authors style guide to highlight related guidelines and position these guidelines (#28481)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+18.sha-8c0c689.with-local-changes
+ * @license Angular v7.2.12+20.sha-47c4868.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+18.sha-8c0c689.with-local-changes
+ * @license Angular v7.2.12+20.sha-47c4868.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5814,7 +5814,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.12+18.sha-8c0c689.with-local-changes');
+    var VERSION = new core.Version('7.2.12+20.sha-47c4868.with-local-changes');
 
     /**
      * @license
