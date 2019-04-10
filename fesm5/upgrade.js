bab8594b643b06957163397ff1bbc11469543e14
Kara Erickson <karakara@google.com>
2019-04-10 02:48:59 +0000

    db62ccf build(bazel): pin versions of nodejs rules in @angular/bazel peer deps and @bazel/schematics (#29800)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+54.sha-b0eefc8.with-local-changes
+ * @license Angular v8.0.0-beta.11+55.sha-db62ccf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
