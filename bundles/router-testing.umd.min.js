ec93ee20ba90ed668d599f3ad9c0b2a4127cae77
Igor Minar <igor@angular.io>
2019-04-10 19:42:17 +0000

    0997512 docs: fix a grammatical error in the building and serving documentation (#29768)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+59.sha-39bfd1a.with-local-changes
+ * @license Angular v8.0.0-beta.11+60.sha-0997512.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
