482d9d8b289ebd1e39bccf59ee7301fabdf94953
Igor Minar <igor@angular.io>
2019-04-10 19:43:20 +0000

    3e46985 docs: showing hamburger menu for getting started pages on desktop (#29791)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+62.sha-8c37cdc.with-local-changes
+ * @license Angular v8.0.0-beta.11+63.sha-3e46985.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8067,7 +8067,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.11+62.sha-8c37cdc.with-local-changes');
+const VERSION = new Version('8.0.0-beta.11+63.sha-3e46985.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+62.sha-8c37cdc.with-local-changes
+ * @license Angular v8.0.0-beta.11+63.sha-3e46985.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
