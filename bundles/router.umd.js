a54ab74ba4c0349542bb810c2c22790f4806716e
Igor Minar <igor@angular.io>
2019-04-10 19:42:46 +0000

    17c4ed9 docs: update path mappings and exports (#29810)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+60.sha-0997512.with-local-changes
+ * @license Angular v8.0.0-beta.11+65.sha-17c4ed9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.11+60.sha-0997512.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.11+65.sha-17c4ed9.with-local-changes');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+60.sha-0997512.with-local-changes
+ * @license Angular v8.0.0-beta.11+65.sha-17c4ed9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
