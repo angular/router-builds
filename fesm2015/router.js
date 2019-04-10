5ab48d8e79ecf305dbded3ad1d61d80c17eb7bba
Igor Minar <igor@angular.io>
2019-04-10 19:42:11 +0000

    42b7f85 docs: redirect to schematics-for-libraries (#29780)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+24.sha-7089c20.with-local-changes
+ * @license Angular v7.2.12+26.sha-42b7f85.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7279,7 +7279,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.12+24.sha-7089c20.with-local-changes');
+const VERSION = new Version('7.2.12+26.sha-42b7f85.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+24.sha-7089c20.with-local-changes
+ * @license Angular v7.2.12+26.sha-42b7f85.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
