0e0b5e18aa9dbdf26418e8ce6da9347972a4ca40
Igor Minar <igor@angular.io>
2019-04-11 15:44:17 +0000

    91c7b45 feat(ivy): support i18n without closure (#28689)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+82.sha-387fbb8.with-local-changes
+ * @license Angular v8.0.0-beta.11+83.sha-91c7b45.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8256,7 +8256,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.11+82.sha-387fbb8.with-local-changes');
+const VERSION = new Version('8.0.0-beta.11+83.sha-91c7b45.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
