7a327969747290a42e52935e45b75f1493bbece2
Igor Minar <igor@angular.io>
2019-04-11 18:20:33 +0000

    54058ba fix(core): resolve ts compile issues due to lenient tsconfig (#29843)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+83.sha-91c7b45.with-local-changes
+ * @license Angular v8.0.0-beta.11+84.sha-54058ba.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
