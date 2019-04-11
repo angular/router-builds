2700f7d8dedfe282f4323babd10760f3b4916559
Igor Minar <igor@angular.io>
2019-04-11 15:41:16 +0000

    387fbb8 fix(common): add `@Injectable()` to common pipes (#29834)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+81.sha-5b32f55.with-local-changes
+ * @license Angular v8.0.0-beta.11+82.sha-387fbb8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
