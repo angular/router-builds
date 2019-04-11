a5d8afc7119e4a3e461edab6b79f4b13f0249451
Igor Minar <igor@angular.io>
2019-04-11 15:34:40 +0000

    ddadb8e refactor(ivy): Update @publicApi to @codeGenApi on ivy instructions (#29820)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+76.sha-def73a6.with-local-changes
+ * @license Angular v8.0.0-beta.11+77.sha-ddadb8e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
