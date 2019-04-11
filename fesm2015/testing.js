a70a7efc2727b3bd04c30a2feb03fd39b24cbfe3
Igor Minar <igor@angular.io>
2019-04-11 14:57:59 +0000

    4191344 perf(ivy): coalesce handlers for events with the same name on the same element (#29786)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+71.sha-faf8fdd.with-local-changes
+ * @license Angular v8.0.0-beta.11+70.sha-4191344.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
