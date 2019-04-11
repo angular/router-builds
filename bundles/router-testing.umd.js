12af7138993e0b175eb9a1a8a57686b80fb375f4
Igor Minar <igor@angular.io>
2019-04-11 15:37:49 +0000

    cc2e4b6 fix(bazel): docs formatting (#29817)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+78.sha-6227d0b.with-local-changes
+ * @license Angular v8.0.0-beta.11+79.sha-cc2e4b6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
