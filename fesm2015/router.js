035299e0319f6240513e5263563852959618fcef
Igor Minar <igor@angular.io>
2019-04-11 15:02:47 +0000

    3ea8d65 build(docs-infra): upgrade cli command docs sources to 664990cad (#29827)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+70.sha-4191344.with-local-changes
+ * @license Angular v8.0.0-beta.11+72.sha-3ea8d65.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8256,7 +8256,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.11+70.sha-4191344.with-local-changes');
+const VERSION = new Version('8.0.0-beta.11+72.sha-3ea8d65.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
