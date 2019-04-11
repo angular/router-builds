53d6bc6312aa2820c4d120ead6d365c04c4be58e
Igor Minar <igor@angular.io>
2019-04-11 15:01:09 +0000

    3ea8d65 build(docs-infra): upgrade cli command docs sources to 664990cad (#29827)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+70.sha-4191344.with-local-changes
+ * @license Angular v8.0.0-beta.11+72.sha-3ea8d65.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.11+70.sha-4191344.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.11+72.sha-3ea8d65.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+70.sha-4191344.with-local-changes
+ * @license Angular v8.0.0-beta.11+72.sha-3ea8d65.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
