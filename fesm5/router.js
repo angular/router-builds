0125a66c5694d4fc86cfc6a22bc1f5342c7383d9
Igor Minar <igor@angular.io>
2019-04-11 14:54:27 +0000

    faf8fdd refactor(core): Fix "explicit-query-timing" failure span. (#29792)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+69.sha-a80637e.with-local-changes
+ * @license Angular v8.0.0-beta.11+71.sha-faf8fdd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+69.sha-a80637e.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+71.sha-faf8fdd.with-local-changes');
 
 /**
  * @license
