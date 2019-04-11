60b2da927ccc6e4e660ff3ffa1cf6fe4ab1fa4ec
Igor Minar <igor@angular.io>
2019-04-11 15:42:55 +0000

    91c7b45 feat(ivy): support i18n without closure (#28689)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+82.sha-387fbb8.with-local-changes
+ * @license Angular v8.0.0-beta.11+83.sha-91c7b45.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
