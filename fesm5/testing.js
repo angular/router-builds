3216314db3e703e52a69a8f2d162df038768694e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-12 18:40:26 +0000

    ac3dc3c test(docs-infra): ignore lazy loading examples in Ivy (#28685)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+85.sha-b057806.with-local-changes
+ * @license Angular v8.0.0-beta.11+87.sha-ac3dc3c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
