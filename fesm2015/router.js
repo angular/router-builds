1f4472da7d29fde72a2ffd46761c0dad092eefe9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-13 16:49:43 +0000

    ea70d41 test: ensure correct TS version before running postinstall (#29885)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+92.sha-911e0b8.with-local-changes
+ * @license Angular v8.0.0-beta.12+1.sha-ea70d41.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8067,7 +8067,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.11+92.sha-911e0b8.with-local-changes');
+const VERSION = new Version('8.0.0-beta.12+1.sha-ea70d41.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
