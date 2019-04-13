3bc62806f55c84f84e1706a7690d4885d1d9c18a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-13 00:11:52 +0000

    0aa0f11 fix(ivy): log errors instead of re-throwing them (#29853)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+87.sha-ac3dc3c.with-local-changes
+ * @license Angular v8.0.0-beta.11+90.sha-0aa0f11.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.11+87.sha-ac3dc3c.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.11+90.sha-0aa0f11.with-local-changes');
 
     /**
      * @license
