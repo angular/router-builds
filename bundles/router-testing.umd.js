faa83f90ffe2109d303a2d482f84b03114d3a915
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-15 16:34:36 +0000

    8031e58 fix(docs-infra): add missing ng-conf image (#29902)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+31.sha-d801941.with-local-changes
+ * @license Angular v7.2.13+3.sha-8031e58.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
