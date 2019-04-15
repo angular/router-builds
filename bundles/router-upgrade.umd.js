fbb63e15c362dd8ff82492e6c7e2480b5b19cd59
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-15 16:29:10 +0000

    d1d0f4a fix(docs-infra): add missing ng-conf image (#29902)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+1.sha-ea70d41.with-local-changes
+ * @license Angular v8.0.0-beta.12+4.sha-d1d0f4a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+1.sha-ea70d41.with-local-changes
+ * @license Angular v8.0.0-beta.12+4.sha-d1d0f4a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.12+1.sha-ea70d41.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.12+4.sha-d1d0f4a.with-local-changes');
 
     /**
      * @license
