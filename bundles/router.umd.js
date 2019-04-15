739bdff005d5ce43c4b964c4685e4c76fee34434
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-15 18:31:57 +0000

    3a836c3 build(docs-infra): convert `search-worker.js` to TypeScript (#29764)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+8.sha-b2962db.with-local-changes
+ * @license Angular v8.0.0-beta.12+15.sha-3a836c3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.12+8.sha-b2962db.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.12+15.sha-3a836c3.with-local-changes');
 
     /**
      * @license
