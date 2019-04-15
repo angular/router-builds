a6d2ce2d4e74a44c049af0787e667c0734d01c5e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-15 18:36:49 +0000

    e5cbc9b build(docs-infra): convert `search-worker.js` to TypeScript (#29908)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.13+3.sha-8031e58.with-local-changes
+ * @license Angular v7.2.13+5.sha-e5cbc9b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7279,7 +7279,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.13+3.sha-8031e58.with-local-changes');
+const VERSION = new Version('7.2.13+5.sha-e5cbc9b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.13+3.sha-8031e58.with-local-changes
+ * @license Angular v7.2.13+5.sha-e5cbc9b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
