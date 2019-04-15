970618dfd8549aec62ca5c302c80cb6239805155
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-15 18:07:34 +0000

    b2962db test: add additional test for codeGenApi (#29844)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+6.sha-6a8cca7.with-local-changes
+ * @license Angular v8.0.0-beta.12+8.sha-b2962db.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+6.sha-6a8cca7.with-local-changes
+ * @license Angular v8.0.0-beta.12+8.sha-b2962db.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.12+6.sha-6a8cca7.with-local-changes');
+var VERSION = new Version('8.0.0-beta.12+8.sha-b2962db.with-local-changes');
 
 /**
  * @license
