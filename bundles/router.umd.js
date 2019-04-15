935125d8ec3b4d1b0b2c5022e673434d41a52e79
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-15 23:17:44 +0000

    8027b3e ci(docs-infra): temporarily lower the min required PWA score for localhost tests (#29911)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+15.sha-3a836c3.with-local-changes
+ * @license Angular v8.0.0-beta.12+16.sha-8027b3e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.12+15.sha-3a836c3.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.12+16.sha-8027b3e.with-local-changes');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+15.sha-3a836c3.with-local-changes
+ * @license Angular v8.0.0-beta.12+16.sha-8027b3e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
