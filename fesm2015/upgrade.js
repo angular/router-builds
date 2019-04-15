5b75b2b6efb386cdc3bf0108cbfd901dc7a27bda
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-15 18:33:08 +0000

    3a836c3 build(docs-infra): convert `search-worker.js` to TypeScript (#29764)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+8.sha-b2962db.with-local-changes
+ * @license Angular v8.0.0-beta.12+15.sha-3a836c3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
