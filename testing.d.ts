aa8a67d6614809d940e6e794b02883968b050983
Brandon <robertsbt@gmail.com>
2019-04-15 18:42:49 +0000

    afa4c50 docs: simplify components description

---
+++ testing.d.ts
@@ -0,0 +1,7 @@
+/**
+ * @license Angular v8.0.0-beta.12+16.sha-afa4c50.with-local-changes
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+export * from './testing/testing';
