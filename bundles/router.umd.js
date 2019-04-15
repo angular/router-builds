7436226c952cbda4befa76e38bb0dd4e818f40e8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-15 17:53:50 +0000

    6a8cca7 feat(ivy): use i18n locale data to determine the plural form of ICU expressions (#29249)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+4.sha-d1d0f4a.with-local-changes
+ * @license Angular v8.0.0-beta.12+6.sha-6a8cca7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.12+4.sha-d1d0f4a.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.12+6.sha-6a8cca7.with-local-changes');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+4.sha-d1d0f4a.with-local-changes
+ * @license Angular v8.0.0-beta.12+6.sha-6a8cca7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.12+4.sha-d1d0f4a.with-local-changes');
+var VERSION = new Version('8.0.0-beta.12+6.sha-6a8cca7.with-local-changes');
 
 /**
  * @license
