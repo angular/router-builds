35124fdff88a2ce3b4a44814d7263aff020b7039
Brandon <robertsbt@gmail.com>
2019-04-15 18:41:56 +0000

    58355b5 docs: explicitly state purpose of components

---
+++ testing.d.ts
@@ -0,0 +1,7 @@
+/**
+ * @license Angular v8.0.0-beta.12+16.sha-58355b5.with-local-changes
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+export * from './testing/testing';
