6a9e0a04724889047c379a8df76dca4bf6a0423a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 17:48:46 +0000

    9af4dcc docs: use correct values from `code-example` in i18n (#29920)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.13+5.sha-e5cbc9b.with-local-changes
+ * @license Angular v7.2.13+8.sha-9af4dcc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7279,7 +7279,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.13+5.sha-e5cbc9b.with-local-changes');
+const VERSION = new Version('7.2.13+8.sha-9af4dcc.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.13+5.sha-e5cbc9b.with-local-changes
+ * @license Angular v7.2.13+8.sha-9af4dcc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
