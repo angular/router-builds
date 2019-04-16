4edb555e8d69778a75adbff96e65f3e41c5bc64f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 16:11:48 +0000

    9147092 Revert "feat(ivy): use i18n locale data to determine the plural form of ICU expressions (#29249)" (#29918)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+85.sha-b057806.with-local-changes
+ * @license Angular v8.0.0-beta.12+17.sha-9147092.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+85.sha-b057806.with-local-changes
+ * @license Angular v8.0.0-beta.12+17.sha-9147092.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.11+85.sha-b057806.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.12+17.sha-9147092.with-local-changes');
 
     /**
      * @license
