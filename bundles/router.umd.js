f735f306cc95df5d1cc0a91f44ca413918bb903d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 17:04:27 +0000

    2deac0a perf(ivy): cache multiple reads to an element's stylingContext (#29818)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+16.sha-8027b3e.with-local-changes
+ * @license Angular v8.0.0-beta.12+18.sha-2deac0a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5774,7 +5774,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.12+16.sha-8027b3e.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.12+18.sha-2deac0a.with-local-changes');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+16.sha-8027b3e.with-local-changes
+ * @license Angular v8.0.0-beta.12+18.sha-2deac0a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
