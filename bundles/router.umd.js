2fdc2c07fc3670704e72b4875bd38fd72dd3c41f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 18:00:25 +0000

    2bfb6a0 fix(router): support non-NgFactory promise in loadChildren typings (#29832)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+23.sha-bd3164f.with-local-changes
+ * @license Angular v8.0.0-beta.12+24.sha-2bfb6a0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.12+23.sha-bd3164f.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.12+24.sha-2bfb6a0.with-local-changes');
 
     /**
      * @license
