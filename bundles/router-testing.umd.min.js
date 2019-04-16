8ee366d3c2e5a0d683273dd5f016ac2fe805939d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 23:04:14 +0000

    ab60362 release: cut the v8.0.0-beta.13 release

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+25.sha-86a3f90.with-local-changes
+ * @license Angular v8.0.0-beta.13.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
