a84049fb5942c0fa6ef346c02d198a3f179c4a5e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 17:05:23 +0000

    5d824c4 fix(ivy): DebugNode.properties not preserving type of values (#29914)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+18.sha-2deac0a.with-local-changes
+ * @license Angular v8.0.0-beta.12+20.sha-5d824c4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
