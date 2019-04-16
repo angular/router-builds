1d0bd7050673fc9363f6ef8a345982ee23cff31e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 17:45:08 +0000

    bd3164f docs: use correct values from `code-example` in i18n (#29920)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+21.sha-5f1b637.with-local-changes
+ * @license Angular v8.0.0-beta.12+23.sha-bd3164f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+21.sha-5f1b637.with-local-changes
+ * @license Angular v8.0.0-beta.12+23.sha-bd3164f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.12+21.sha-5f1b637.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.12+23.sha-bd3164f.with-local-changes');
 
     /**
      * @license
