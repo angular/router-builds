4dcdd5d8bd4f81f41d49c5c7b4ffad717d9eccdc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 20:49:22 +0000

    1794a8e fix(ivy): coalesced listeners should preventDefault if any returns false (#29934)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+24.sha-2bfb6a0.with-local-changes
+ * @license Angular v8.0.0-beta.12+26.sha-1794a8e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5732,7 +5732,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.12+24.sha-2bfb6a0.with-local-changes');
+var VERSION = new Version('8.0.0-beta.12+26.sha-1794a8e.with-local-changes');
 
 /**
  * @license
