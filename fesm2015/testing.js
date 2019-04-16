824b7b0a1d20e3915019973b33cb956d0c6b3e85
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 17:06:15 +0000

    ca755a6 build: Fix label name (#29777)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+20.sha-5d824c4.with-local-changes
+ * @license Angular v8.0.0-beta.12+19.sha-ca755a6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
