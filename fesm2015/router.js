dd07efffbb77e51f48fcfd9266b097d8501eb94a
Ben Lesh <ben@benlesh.com>
2019-04-17 19:34:12 +0000

    6e190b1 ci(docs-infra): increase wait for SW on localhost to avoid CI flakes (#29953)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.13+7.sha-16ba4ae.with-local-changes
+ * @license Angular v7.2.13+10.sha-6e190b1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7279,7 +7279,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.13+7.sha-16ba4ae.with-local-changes');
+const VERSION = new Version('7.2.13+10.sha-6e190b1.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
