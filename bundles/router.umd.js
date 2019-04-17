3edd048b364ab22a1031c3a6f3588c910b3354af
Ben Lesh <ben@benlesh.com>
2019-04-17 19:33:14 +0000

    60e9d2d ci(docs-infra): increase wait for SW on localhost to avoid CI flakes (#29953)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+3.sha-a48d288.with-local-changes
+ * @license Angular v8.0.0-beta.13+5.sha-60e9d2d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5774,7 +5774,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.13+3.sha-a48d288.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.13+5.sha-60e9d2d.with-local-changes');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+3.sha-a48d288.with-local-changes
+ * @license Angular v8.0.0-beta.13+5.sha-60e9d2d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
