39d5fca6617c24c1cb8e88a5198d3bd9849d905a
Ben Lesh <ben@benlesh.com>
2019-04-17 19:30:30 +0000

    a48d288 build(language-service): no need to bootstrap init_node_spec.js (#29937)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+2.sha-d9c39dc.with-local-changes
+ * @license Angular v8.0.0-beta.13+3.sha-a48d288.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.13+2.sha-d9c39dc.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.13+3.sha-a48d288.with-local-changes');
 
     /**
      * @license
