dd2d5eb07a34008422cfa3d2fc85369c994f5925
Brandon Roberts <robertsbt@gmail.com>
2019-04-17 20:03:08 +0000

    1b7749d docs: explicitly state purpose of components

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+16.sha-58355b5.with-local-changes
+ * @license Angular v8.0.0-beta.12+18.sha-1b7749d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
