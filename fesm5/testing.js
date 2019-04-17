721399a0556d2491e880735680ad3cb543ade889
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-17 02:30:20 +0000

    ca2462c fix(ivy): support providing components and dirs in tests (#29945)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13.with-local-changes
+ * @license Angular v8.0.0-beta.13+1.sha-ca2462c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
