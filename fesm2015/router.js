6fed373574de9852fb3d3e6792267fad27672617
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-17 03:29:04 +0000

    d9c39dc fix(ivy): directiveInject should fall back to inject (#29948)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+26.sha-1794a8e.with-local-changes
+ * @license Angular v8.0.0-beta.13+2.sha-d9c39dc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8067,7 +8067,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.12+26.sha-1794a8e.with-local-changes');
+const VERSION = new Version('8.0.0-beta.13+2.sha-d9c39dc.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
