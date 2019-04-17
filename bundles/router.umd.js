c8a2ea8cd5e0e5165e2c0575b9df7d6dd43061d7
Ben Lesh <ben@benlesh.com>
2019-04-17 19:35:15 +0000

    1a56cd5 fix(language-service): Use proper types instead of any (#29942)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+5.sha-60e9d2d.with-local-changes
+ * @license Angular v8.0.0-beta.13+6.sha-1a56cd5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5820,7 +5820,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.13+5.sha-60e9d2d.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.13+6.sha-1a56cd5.with-local-changes');
 
     /**
      * @license
