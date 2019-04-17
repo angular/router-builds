3570d1a938a0dbcd450c30e50778de6969cec0bd
Ben Lesh <ben@benlesh.com>
2019-04-17 19:38:26 +0000

    696e520 fix(ivy): make metadata calls side-effect free in Closure (#29947)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+7.sha-5fee9da.with-local-changes
+ * @license Angular v8.0.0-beta.13+8.sha-696e520.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8256,7 +8256,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.13+7.sha-5fee9da.with-local-changes');
+const VERSION = new Version('8.0.0-beta.13+8.sha-696e520.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
