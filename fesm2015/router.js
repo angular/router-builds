b6056c6ea72aef7bf8353846f6b60a5e55746ad9
Ben Lesh <ben@benlesh.com>
2019-04-18 21:09:18 +0000

    0629ebd build(bazel): Bump CLI and @angular-devkit/* to v8 beta 15 (#29966)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+22.sha-45c2429.with-local-changes
+ * @license Angular v8.0.0-beta.13+23.sha-0629ebd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8270,7 +8270,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.13+22.sha-45c2429.with-local-changes');
+const VERSION = new Version('8.0.0-beta.13+23.sha-0629ebd.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
