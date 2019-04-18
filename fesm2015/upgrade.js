0c267efbdb88708d41b34f57788e4216bf331d27
Ben Lesh <ben@benlesh.com>
2019-04-18 00:42:54 +0000

    69eb46a docs: update extra options available for RouterModule.forRoot() method (#29846)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.13+10.sha-6e190b1.with-local-changes
+ * @license Angular v7.2.13+11.sha-69eb46a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
