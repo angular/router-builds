e0b9dd100abf0aff9f9c3c2503aa67038b7d0b59
Ben Lesh <ben@benlesh.com>
2019-04-19 21:49:17 +0000

    a181e8e refactor: improve merge script formatting (#29991)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+54.sha-6ae0084.with-local-changes
+ * @license Angular v8.0.0-beta.13+55.sha-a181e8e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.13+54.sha-6ae0084.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.13+55.sha-a181e8e.with-local-changes');
 
     /**
      * @license
