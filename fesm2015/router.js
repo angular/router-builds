fbcd5cd77b829c22eb5291ebfbb67576e7bf05fe
Ben Lesh <ben@benlesh.com>
2019-04-19 18:16:31 +0000

    d7f7826 build: publish ts-api-guardian rule in the npm package (#29977)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+35.sha-9b93bd6.with-local-changes
+ * @license Angular v8.0.0-beta.13+36.sha-d7f7826.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8270,7 +8270,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.13+35.sha-9b93bd6.with-local-changes');
+const VERSION = new Version('8.0.0-beta.13+36.sha-d7f7826.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
