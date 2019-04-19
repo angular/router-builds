183ba9e4f687230d165b220209f32989ded31b62
Ben Lesh <ben@benlesh.com>
2019-04-19 17:16:18 +0000

    0c4430b build(docs-infra): make tsconfig path detection in `switch-to-ivy` more robust (#29989)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.13+12.sha-4994527.with-local-changes
+ * @license Angular v7.2.13+17.sha-0c4430b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
