0db72c8cd4d4b033284a90252f4410e4d94d1c75
Ben Lesh <ben@benlesh.com>
2019-04-19 23:27:34 +0000

    f348dea test: put host binding acceptances tests in their own file (#29946)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+55.sha-a181e8e.with-local-changes
+ * @license Angular v8.0.0-beta.13+57.sha-f348dea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
