c1cf8a2fb70aa4dc50aee8053863814fa1017ca3
Ben Lesh <ben@benlesh.com>
2019-04-19 01:41:18 +0000

    c1d5fbd refactor(core): static-query template strategy should not parse stylesheets (#29876)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+23.sha-0629ebd.with-local-changes
+ * @license Angular v8.0.0-beta.13+28.sha-c1d5fbd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+23.sha-0629ebd.with-local-changes
+ * @license Angular v8.0.0-beta.13+28.sha-c1d5fbd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.13+23.sha-0629ebd.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.13+28.sha-c1d5fbd.with-local-changes');
 
     /**
      * @license
