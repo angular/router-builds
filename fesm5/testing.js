fb07a042b4b572774bd85161300f989404edfbde
Ben Lesh <ben@benlesh.com>
2019-04-19 18:27:16 +0000

    0df719a feat(ivy): register NgModules with ids when compiled with AOT (#29980)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+36.sha-d7f7826.with-local-changes
+ * @license Angular v8.0.0-beta.13+38.sha-0df719a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
