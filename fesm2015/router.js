bf71ff492907d4ab2926bde0201ecd6ea93da5a9
Ben Lesh <ben@benlesh.com>
2019-04-19 21:30:37 +0000

    4e13700 fix(ivy): set ng-reflect properties for unbound directive inputs (#29973)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+51.sha-d9ce8a4.with-local-changes
+ * @license Angular v8.0.0-beta.13+52.sha-4e13700.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8081,7 +8081,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.13+51.sha-d9ce8a4.with-local-changes');
+const VERSION = new Version('8.0.0-beta.13+52.sha-4e13700.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
