538d33a100a6ee64edd65005834bb4d2b0d774c5
Ben Lesh <ben@benlesh.com>
2019-04-19 18:15:11 +0000

    d7f7826 build: publish ts-api-guardian rule in the npm package (#29977)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+35.sha-9b93bd6.with-local-changes
+ * @license Angular v8.0.0-beta.13+36.sha-d7f7826.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
