67cdccaa834b895677be52728be5295436dbb6ee
Ben Lesh <ben@benlesh.com>
2019-04-19 23:29:09 +0000

    f348dea test: put host binding acceptances tests in their own file (#29946)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+55.sha-a181e8e.with-local-changes
+ * @license Angular v8.0.0-beta.13+57.sha-f348dea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.13+55.sha-a181e8e.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.13+57.sha-f348dea.with-local-changes');
 
     /**
      * @license
