4cf40b49a324a0a59b405c369c695e14025eb6d9
Ben Lesh <ben@benlesh.com>
2019-04-19 01:35:39 +0000

    dcfde77 docs(animations): fixed some closing brackets on query animation page (#29854) (#29855)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.13+11.sha-69eb46a.with-local-changes
+ * @license Angular v7.2.13+13.sha-dcfde77.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7293,7 +7293,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.13+11.sha-69eb46a.with-local-changes');
+const VERSION = new Version('7.2.13+13.sha-dcfde77.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.13+11.sha-69eb46a.with-local-changes
+ * @license Angular v7.2.13+13.sha-dcfde77.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
