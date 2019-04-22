f3e8f456a674018cdb6063a4a394f06a1a986de7
Ben Lesh <ben@benlesh.com>
2019-04-22 16:03:37 +0000

    8cba4e1 fix(ivy): ngcc - do not copy declaration files into bundle clone (#30020)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+61.sha-3f6bf6d.with-local-changes
+ * @license Angular v8.0.0-beta.13+63.sha-8cba4e1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
