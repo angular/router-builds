4dcd3a33fd9c8d0777909181976c3ad71fe196b7
Ben Lesh <ben@benlesh.com>
2019-04-22 18:41:36 +0000

    ceea0b4 docs: fix typo in routing section of Tour of Heroes (#29961)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+69.sha-a9242c4.with-local-changes
+ * @license Angular v8.0.0-beta.13+72.sha-ceea0b4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
