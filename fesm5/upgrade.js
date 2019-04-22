102ae1be04ae04e800334f6570a19ffded6434fa
Ben Lesh <ben@benlesh.com>
2019-04-22 16:09:33 +0000

    96a8289 fix(bazel): restore ng build --prod (#30005)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+64.sha-71498a4.with-local-changes
+ * @license Angular v8.0.0-beta.13+65.sha-96a8289.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
