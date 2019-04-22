5ae687d84a29d750cb81cd2f8efa2275dc1bcb9a
Ben Lesh <ben@benlesh.com>
2019-04-22 23:50:15 +0000

    9a81e52 docs: fixed typo on HttpParamsOptions (#29930) PR Close #29930

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.13+21.sha-7f15c01.with-local-changes
+ * @license Angular v7.2.13+23.sha-9a81e52.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
