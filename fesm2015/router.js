39281f286b0f99a2f79611ad21c49cfc8660b3f5
Ben Lesh <ben@benlesh.com>
2019-04-22 16:02:37 +0000

    5a07aa4 docs: add ng-japan 2019 event (#30021) PR Close #30021

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+63.sha-8cba4e1.with-local-changes
+ * @license Angular v8.0.0-beta.13+62.sha-5a07aa4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8081,7 +8081,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.13+63.sha-8cba4e1.with-local-changes');
+const VERSION = new Version('8.0.0-beta.13+62.sha-5a07aa4.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
