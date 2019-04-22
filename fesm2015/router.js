385d673cd241ad953d93c2b49edee55c6aee5715
Ben Lesh <ben@benlesh.com>
2019-04-22 23:53:59 +0000

    0cab437 docs: fixed typo on HttpParamsOptions (#29930) PR Close #29930

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+74.sha-57c8f78.with-local-changes
+ * @license Angular v8.0.0-beta.13+76.sha-0cab437.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8270,7 +8270,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.13+74.sha-57c8f78.with-local-changes');
+const VERSION = new Version('8.0.0-beta.13+76.sha-0cab437.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
