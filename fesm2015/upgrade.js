556d23db4582f56a348a037ad339321e4aaa7046
Ben Lesh <ben@benlesh.com>
2019-04-23 00:49:03 +0000

    4382cea docs: fix grammatical errors in the guides and API documentation (#29928)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.13+23.sha-9a81e52.with-local-changes
+ * @license Angular v7.2.13+24.sha-4382cea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
