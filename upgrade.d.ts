153d65f1ec5f94cdc5a1401b62ae163d206983d1
Ben Lesh <ben@benlesh.com>
2019-04-23 02:12:28 +0000

    9873356 docs: fix grammatical errors in the guides and API documentation (#29928)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+70.sha-63523f7.with-local-changes
+ * @license Angular v8.0.0-beta.13+80.sha-9873356.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
