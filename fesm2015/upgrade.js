af623ff6e3f9067b0f86d2a0fdc1bfca5b942018
Ben Lesh <ben@benlesh.com>
2019-04-23 00:46:57 +0000

    9873356 docs: fix grammatical errors in the guides and API documentation (#29928)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+79.sha-0f9230d.with-local-changes
+ * @license Angular v8.0.0-beta.13+80.sha-9873356.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
