c15498170320ece9bfb633511194c86e661f702b
Ben Lesh <ben@benlesh.com>
2019-04-23 00:42:21 +0000

    19dfadb fix(ivy): include context name for template functions for `ng-content` (#30025)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+77.sha-0bcb232.with-local-changes
+ * @license Angular v8.0.0-beta.13+78.sha-19dfadb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
