2b3f8a5a2af40144f0cb5cb923d35b57caf33941
Ben Lesh <ben@benlesh.com>
2019-04-23 22:35:55 +0000

    8ca208f docs: remove outdated docs change log (#26102)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+4.sha-089bfdf.with-local-changes
+ * @license Angular v8.0.0-beta.14+5.sha-8ca208f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
