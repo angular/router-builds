6ea0fec724e673ec8706e10fee80d63e996894cc
Ben Lesh <ben@benlesh.com>
2019-04-23 20:52:26 +0000

    230e329 release: cut the v8.0.0-beta.14 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+90.sha-c4dd2d1.with-local-changes
+ * @license Angular v8.0.0-beta.14.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8082,7 +8082,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.13+90.sha-c4dd2d1.with-local-changes');
+const VERSION = new Version('8.0.0-beta.14.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
