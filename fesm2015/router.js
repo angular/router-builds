1c7e0de41436b65387e881533e3f44aedc97ba63
Ben Lesh <ben@benlesh.com>
2019-04-23 22:37:00 +0000

    8ca208f docs: remove outdated docs change log (#26102)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+4.sha-089bfdf.with-local-changes
+ * @license Angular v8.0.0-beta.14+5.sha-8ca208f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8270,7 +8270,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.14+4.sha-089bfdf.with-local-changes');
+const VERSION = new Version('8.0.0-beta.14+5.sha-8ca208f.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
