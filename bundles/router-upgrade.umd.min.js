3af157d957d915d2b6a37d3401b6c70c4504f6a4
Ben Lesh <ben@benlesh.com>
2019-04-23 15:47:47 +0000

    00ce9aa fix(ivy): explicitly compile ngModuleDef for RootScopeModule in R3TestBed (#30037)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+82.sha-8e73f9b.with-local-changes
+ * @license Angular v8.0.0-beta.13+83.sha-00ce9aa.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
