5618662b7020f76c8f2d2860d17e098ab353e757
Ben Lesh <ben@benlesh.com>
2019-04-23 15:48:53 +0000

    00ce9aa fix(ivy): explicitly compile ngModuleDef for RootScopeModule in R3TestBed (#30037)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+82.sha-8e73f9b.with-local-changes
+ * @license Angular v8.0.0-beta.13+83.sha-00ce9aa.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8270,7 +8270,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.13+82.sha-8e73f9b.with-local-changes');
+const VERSION = new Version('8.0.0-beta.13+83.sha-00ce9aa.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
