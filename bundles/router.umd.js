7c2186dc70af0a60d354479f70c06b94be33aad8
Ben Lesh <ben@benlesh.com>
2019-04-24 23:21:10 +0000

    071ee64 refactor(common): rename LocationUpgradeService and remove angular/upgrade dependency (#30055)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+19.sha-3938563.with-local-changes
+ * @license Angular v8.0.0-beta.14+31.sha-071ee64.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5835,7 +5835,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.14+19.sha-3938563.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.14+31.sha-071ee64.with-local-changes');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+19.sha-3938563.with-local-changes
+ * @license Angular v8.0.0-beta.14+31.sha-071ee64.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
