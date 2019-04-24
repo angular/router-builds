9573e521641e771a97f2392ed5be795686909771
Andrew Kushnir <akushnir@google.com>
2019-04-24 18:01:32 +0000

    e8d3246 build(docs-infra): upgrade RxJS in docs examples to 6.5.1 (#30048)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+6.sha-c7f1b0a.with-local-changes
+ * @license Angular v8.0.0-beta.14+8.sha-e8d3246.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
