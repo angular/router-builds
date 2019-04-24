47dff4663c10f4681192bfc40916e0578ef6ea4f
Andrew Kushnir <akushnir@google.com>
2019-04-24 17:53:57 +0000

    c7f1b0a fix(ivy): queries not being inherited from undecorated classes (#30015)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+5.sha-8ca208f.with-local-changes
+ * @license Angular v8.0.0-beta.14+6.sha-c7f1b0a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8270,7 +8270,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.14+5.sha-8ca208f.with-local-changes');
+const VERSION = new Version('8.0.0-beta.14+6.sha-c7f1b0a.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
