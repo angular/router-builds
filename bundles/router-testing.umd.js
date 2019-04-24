aab92012a09ea03d0d2bc641b0db0f7f8b12592b
Andrew Kushnir <akushnir@google.com>
2019-04-24 18:49:55 +0000

    2e21997 perf(ivy): remove megamorphic read from renderStringify (#30082)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+15.sha-24c61cb.with-local-changes
+ * @license Angular v8.0.0-beta.14+16.sha-2e21997.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
