6d8e914ff3375973d9a9d01636224292b4b2322f
Andrew Kushnir <akushnir@google.com>
2019-04-24 18:18:38 +0000

    d0ca7c7 docs: add Andrew Kushnir to Angular team (#30045)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+13.sha-6c88eb7.with-local-changes
+ * @license Angular v8.0.0-beta.14+14.sha-d0ca7c7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5835,7 +5835,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.14+13.sha-6c88eb7.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.14+14.sha-d0ca7c7.with-local-changes');
 
     /**
      * @license
