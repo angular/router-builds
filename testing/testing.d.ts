458710383eae9c19f8913eaa568d197deec8e184
Andrew Kushnir <akushnir@google.com>
2019-04-24 18:51:00 +0000

    ae93ba1 fix(ivy): don't throw error when evaluating function with more than one statement (#30061)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+18.sha-00ea398.with-local-changes
+ * @license Angular v8.0.0-beta.14+17.sha-ae93ba1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
