7d53b088cf8d2f9fad85258eb46247eaaec0ef67
Andrew Kushnir <akushnir@google.com>
2019-04-24 18:50:24 +0000

    00ea398 docs(docs-infra): fix typo in the aio/tools/examples/README (#30041)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+16.sha-2e21997.with-local-changes
+ * @license Angular v8.0.0-beta.14+18.sha-00ea398.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
