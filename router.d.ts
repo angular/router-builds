5c6328d9badd259cb14da028d0dda927ef273452
Andrew Kushnir <akushnir@google.com>
2019-04-24 20:44:40 +0000

    3938563 fix(ivy): don't reuse a ts.Program more than once in ngtsc (#30090)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+3.sha-d92fb25.with-local-changes
+ * @license Angular v8.0.0-beta.14+19.sha-3938563.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
