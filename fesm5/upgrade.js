049065f5bc3ef273cbba96a7c6921370c0d2fe10
Andrew Kushnir <akushnir@google.com>
2019-04-24 18:19:45 +0000

    d0ca7c7 docs: add Andrew Kushnir to Angular team (#30045)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+13.sha-6c88eb7.with-local-changes
+ * @license Angular v8.0.0-beta.14+14.sha-d0ca7c7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
