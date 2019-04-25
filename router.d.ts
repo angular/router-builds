16311bbb6ee93e022ecb626df333d0f6864df3d6
Andrew Kushnir <akushnir@google.com>
2019-04-25 00:20:13 +0000

    abcb2cf refactor(upgrade): rename `module` constant to avoid webpack bug (#30058)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+37.sha-a9379e0.with-local-changes
+ * @license Angular v8.0.0-beta.14+38.sha-abcb2cf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
