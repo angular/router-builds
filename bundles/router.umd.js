90128dcc194927ba0c58b912dae965f31013d8e8
Andrew Kushnir <akushnir@google.com>
2019-04-25 19:53:23 +0000

    304a12f feat(compiler): support skipping leading trivia in template source-maps (#30095)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+62.sha-909557d.with-local-changes
+ * @license Angular v8.0.0-beta.14+70.sha-304a12f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5835,7 +5835,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.14+62.sha-909557d.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.14+70.sha-304a12f.with-local-changes');
 
     /**
      * @license
