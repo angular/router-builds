31137731395ac269d4cee4bee9454866ba39d7d9
Andrew Kushnir <akushnir@google.com>
2019-04-25 00:27:43 +0000

    fe5e226 build(docs-infra): upgrade RxJS in docs examples to 6.5.1 (#30048) (#30096)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.14+4.sha-7368dc1.with-local-changes
+ * @license Angular v7.2.14+6.sha-fe5e226.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
