009d0eb59a61d4d8235145153f44af1bcb7d5347
Andrew Kushnir <akushnir@google.com>
2019-04-25 19:25:18 +0000

    2236ea4 test(upgrade): rename `angular.module()` to `angular.module_()` (#30126)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+52.sha-2dc4e88.with-local-changes
+ * @license Angular v8.0.0-beta.14+54.sha-2236ea4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
