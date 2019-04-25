7d273943b2a06d1653b14cfcdd7971c3fbb70bd9
Andrew Kushnir <akushnir@google.com>
2019-04-25 00:30:40 +0000

    8275442 docs(docs-infra): add jennifer to fw-docs-intro codeowners, add codeowners for schematics docs (#28992)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.14+6.sha-fe5e226.with-local-changes
+ * @license Angular v7.2.14+7.sha-8275442.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7293,7 +7293,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.14+6.sha-fe5e226.with-local-changes');
+const VERSION = new Version('7.2.14+7.sha-8275442.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
