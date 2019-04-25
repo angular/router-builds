d1acf43b48dc63c0ad5141eac8a1a4bf36d54047
Andrew Kushnir <akushnir@google.com>
2019-04-25 19:32:40 +0000

    07f0385 test(upgrade): rename `angular.module()` to `angular.module_()` (#30107)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.14+10.sha-d025159.with-local-changes
+ * @license Angular v7.2.14+13.sha-07f0385.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7293,7 +7293,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.14+10.sha-d025159.with-local-changes');
+const VERSION = new Version('7.2.14+13.sha-07f0385.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
