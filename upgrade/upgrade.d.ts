5c978905028e157f9c1aa5d69cea15f80c9462f0
Andrew Kushnir <akushnir@google.com>
2019-04-25 00:14:48 +0000

    6da1959 docs: copy edits to Ivy opt-in (#29955)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+32.sha-75a23ab.with-local-changes
+ * @license Angular v8.0.0-beta.14+36.sha-6da1959.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
