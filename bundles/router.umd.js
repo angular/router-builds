8fdf416ab008240b1f3ed22e6eebe59e300b2308
Andrew Kushnir <akushnir@google.com>
2019-04-25 01:23:55 +0000

    a400429 docs: temporary disable aio_monitoring job due to a version skew (#30102)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+43.sha-e720675.with-local-changes
+ * @license Angular v8.0.0-beta.14+44.sha-a400429.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5835,7 +5835,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.14+43.sha-e720675.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.14+44.sha-a400429.with-local-changes');
 
     /**
      * @license
