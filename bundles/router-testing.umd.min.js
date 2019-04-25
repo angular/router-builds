22ea6b823db86d35139b2075bf62f32a3e066738
Andrew Kushnir <akushnir@google.com>
2019-04-25 21:58:22 +0000

    61365a9 fix(bazel): transitive npm deps in ng_module (#30065)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+72.sha-3a9d247.with-local-changes
+ * @license Angular v8.0.0-beta.14+73.sha-61365a9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
