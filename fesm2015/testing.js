6fc1da6b6177d5807d68948f8e3cfa412f7864af
Andrew Kushnir <akushnir@google.com>
2019-04-25 18:17:46 +0000

    d025159 ci(docs-infra): loosen conditions to reduce flakiness on CI (#29757)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.14+7.sha-8275442.with-local-changes
+ * @license Angular v7.2.14+10.sha-d025159.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.14+7.sha-8275442.with-local-changes
+ * @license Angular v7.2.14+10.sha-d025159.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5740,7 +5740,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.14+7.sha-8275442.with-local-changes');
+var VERSION = new Version('7.2.14+10.sha-d025159.with-local-changes');
 
 /**
  * @license
