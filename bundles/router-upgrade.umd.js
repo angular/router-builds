680b9bcdb58f9a6e82efedf5cd0ea68c0f7898dd
Andrew Kushnir <akushnir@google.com>
2019-04-25 18:17:54 +0000

    2dc4e88 ci(docs-infra): loosen conditions to reduce flakiness on CI (#29757)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+44.sha-a400429.with-local-changes
+ * @license Angular v8.0.0-beta.14+52.sha-2dc4e88.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
