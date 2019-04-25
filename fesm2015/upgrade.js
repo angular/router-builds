6513a64197f8e7a26d957b099acb6221569f6575
Andrew Kushnir <akushnir@google.com>
2019-04-25 20:30:10 +0000

    6200732 feat(service-worker): support bypassing SW with specific header/query param (#30010)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+69.sha-acaf1aa.with-local-changes
+ * @license Angular v8.0.0-beta.14+71.sha-6200732.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
