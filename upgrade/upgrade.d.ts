d184d7383b0ee0c313aa8e8e8d63d01b2d43fbb2
Andrew Kushnir <akushnir@google.com>
2019-04-25 00:17:21 +0000

    a9379e0 docs(bazel): add aio/guide/bazel (#30042)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+36.sha-6da1959.with-local-changes
+ * @license Angular v8.0.0-beta.14+37.sha-a9379e0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
