ee2b75a2317ccd5c19c83ae165971551b7963039
Andrew Kushnir <akushnir@google.com>
2019-04-25 21:56:53 +0000

    3a9d247 feat: remove deprecated DOCUMENT token from platform-browser (#28117)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+71.sha-6200732.with-local-changes
+ * @license Angular v8.0.0-beta.14+72.sha-3a9d247.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
