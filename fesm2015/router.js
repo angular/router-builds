373f7b1af66aee37634a10dc2349c53c795e4333
Andrew Kushnir <akushnir@google.com>
2019-04-26 23:39:19 +0000

    c99d379 refactor(ivy): move di tests for inject to acceptance (#29299)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+13.sha-b945bc3.with-local-changes
+ * @license Angular v8.0.0-rc.0+18.sha-c99d379.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8270,7 +8270,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+13.sha-b945bc3.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+18.sha-c99d379.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
