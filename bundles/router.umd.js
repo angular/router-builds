7afacdfe6e2de0273ed8400fe4e530b0c94f91b3
Andrew Kushnir <akushnir@google.com>
2019-04-26 18:34:06 +0000

    8dd9192 refactor(ivy): undeprecate inject (#30132)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+7.sha-6711f22.with-local-changes
+ * @license Angular v8.0.0-rc.0+6.sha-8dd9192.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5835,7 +5835,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+7.sha-6711f22.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+6.sha-8dd9192.with-local-changes');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+7.sha-6711f22.with-local-changes
+ * @license Angular v8.0.0-rc.0+6.sha-8dd9192.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
