42511b237fd50c818ba954f48320a825e9b7c70b
Andrew Kushnir <akushnir@google.com>
2019-04-26 19:52:37 +0000

    b945bc3 test(ivy): pin deps on hello world size tests (#30152)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+12.sha-4ae0ee8.with-local-changes
+ * @license Angular v8.0.0-rc.0+13.sha-b945bc3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5835,7 +5835,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+12.sha-4ae0ee8.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+13.sha-b945bc3.with-local-changes');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+12.sha-4ae0ee8.with-local-changes
+ * @license Angular v8.0.0-rc.0+13.sha-b945bc3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
