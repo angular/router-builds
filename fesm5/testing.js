63031c0f2bbb319e092255bd29aaf5fd0b742a24
Andrew Kushnir <akushnir@google.com>
2019-04-26 19:48:10 +0000

    4ae0ee8 fix(bazel): update peerDep ranges (#30155)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+11.sha-6c86ae7.with-local-changes
+ * @license Angular v8.0.0-rc.0+12.sha-4ae0ee8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
