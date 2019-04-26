47e9dcb70efded321814a41ea1fbc69977c1d36e
Andrew Kushnir <akushnir@google.com>
2019-04-26 18:33:26 +0000

    6711f22 fix(bazel): Exclude common/upgrade* in metadata.tsconfig.json (#30133)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+5.sha-870e0da.with-local-changes
+ * @license Angular v8.0.0-rc.0+7.sha-6711f22.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
