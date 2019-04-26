a009cde7a8d9ba8ec96e4bf27c6880417ad21d38
Andrew Kushnir <akushnir@google.com>
2019-04-26 18:38:26 +0000

    ba2a359 fix(ivy): output should not be subscribe twice when 2 listeners (#30144)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+10.sha-a50bfe5.with-local-changes
+ * @license Angular v8.0.0-rc.0+11.sha-ba2a359.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
