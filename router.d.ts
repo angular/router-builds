f1f52dcd44674d83427569c8ccef3bd3c5a36cbd
Andrew Kushnir <akushnir@google.com>
2019-04-26 18:37:43 +0000

    a50bfe5 fix(ivy): property bindings use correct indices (#30129)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+8.sha-8d6d2c6.with-local-changes
+ * @license Angular v8.0.0-rc.0+10.sha-a50bfe5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
