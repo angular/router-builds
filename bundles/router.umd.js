02c71254c396adce199560a7c0d1c0ca44ce79f9
Andrew Kushnir <akushnir@google.com>
2019-04-26 21:06:25 +0000

    0776dae release: cut the v8.0.0-rc.1 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+13.sha-615e1a5.with-local-changes
+ * @license Angular v8.0.0-rc.1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5835,7 +5835,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+13.sha-615e1a5.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.1.with-local-changes');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+13.sha-615e1a5.with-local-changes
+ * @license Angular v8.0.0-rc.1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5747,7 +5747,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+13.sha-615e1a5.with-local-changes');
+var VERSION = new Version('8.0.0-rc.1.with-local-changes');
 
 /**
  * @license
