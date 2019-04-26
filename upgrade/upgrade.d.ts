b5fae30788a029b6a5fc82ae73f15ea90f03d556
Andrew Kushnir <akushnir@google.com>
2019-04-26 19:48:55 +0000

    6067583 fix(bazel): update peerDep ranges (#30155)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+11.sha-ba2a359.with-local-changes
+ * @license Angular v8.0.0-rc.0+12.sha-6067583.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
