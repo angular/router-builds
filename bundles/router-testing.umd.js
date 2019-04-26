ed3e2e39dfd5046ccc0184322a12dee0d58ae1e8
Andrew Kushnir <akushnir@google.com>
2019-04-26 18:32:54 +0000

    870e0da fix(ivy): remove debug utilities from ivy production builds (#30130)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+4.sha-f45684f.with-local-changes
+ * @license Angular v8.0.0-rc.0+5.sha-870e0da.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
