ea7699c324b656b100aea1b4ed6e465503a95680
Alex Eagle <alexeagle@angular.io>
2019-04-27 00:09:33 +0000

    e5c3695 Revert "refactor: use new Http library in playground (#29355)"

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+18.sha-c99d379.with-local-changes
+ * @license Angular v8.0.0-rc.0+38.sha-e5c3695.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
