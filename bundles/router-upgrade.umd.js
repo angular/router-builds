09bf55dfce7f4b480323e2c7789873938dbd5f0e
Andrew Kushnir <akushnir@google.com>
2019-04-29 20:42:13 +0000

    f015dbe docs: add architect terms (#26963)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+57.sha-029a939.with-local-changes
+ * @license Angular v8.0.0-rc.0+58.sha-f015dbe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
