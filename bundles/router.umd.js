e78a095286a63c68bcaa34bdd48acd66400e09c6
Andrew Kushnir <akushnir@google.com>
2019-04-29 20:51:50 +0000

    05eabb1 test(language-service): Add external template (#30128)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+60.sha-6701250.with-local-changes
+ * @license Angular v8.0.0-rc.0+62.sha-05eabb1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+60.sha-6701250.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+62.sha-05eabb1.with-local-changes');
 
     /**
      * @license
