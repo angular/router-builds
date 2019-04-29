b40519190302a7aee65810024cdbc4618e787c90
Andrew Kushnir <akushnir@google.com>
2019-04-29 19:36:20 +0000

    9798229 release: cut the v8.0.0-rc.2 release

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.1+28.sha-4b2fcfd.with-local-changes
+ * @license Angular v8.0.0-rc.2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
