676dbb1b09486f127ef208cd6eaed108a42cddf0
Andrew Kushnir <akushnir@google.com>
2019-04-29 20:52:52 +0000

    6eeca70 refactor(core): static-query migrations fails if options cannot be transformed (#30178)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2+6.sha-9f68c35.with-local-changes
+ * @license Angular v8.0.0-rc.2+7.sha-6eeca70.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
