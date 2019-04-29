3e58ce7b140783b265af1f675df7230441748b45
Andrew Kushnir <akushnir@google.com>
2019-04-29 19:47:13 +0000

    2945f47 build: add size-tracking bazel test (#30070)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+42.sha-a44b510.with-local-changes
+ * @license Angular v8.0.0-rc.0+43.sha-2945f47.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8270,7 +8270,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+42.sha-a44b510.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+43.sha-2945f47.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
