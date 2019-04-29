def779fbe3402b48122838bbbbda535ee0ddffd7
Andrew Kushnir <akushnir@google.com>
2019-04-29 22:41:15 +0000

    68ff2cc fix(ivy): host bindings and listeners not being inherited from undecorated classes (#30158)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+11.sha-6c86ae7.with-local-changes
+ * @license Angular v8.0.0-rc.0+66.sha-68ff2cc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
