45548822769e85b134c8309a680b20656ecdbee9
Andrew Kushnir <akushnir@google.com>
2019-04-29 20:51:58 +0000

    1353bf0 fix(bazel): Bump ibazel to 0.10.1 for windows fixes (#30196)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+62.sha-05eabb1.with-local-changes
+ * @license Angular v8.0.0-rc.0+64.sha-1353bf0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5835,7 +5835,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+62.sha-05eabb1.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+64.sha-1353bf0.with-local-changes');
 
     /**
      * @license
