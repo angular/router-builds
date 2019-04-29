5262d341b4469ad25b0f24cf21885ab88fee524c
Andrew Kushnir <akushnir@google.com>
2019-04-29 19:52:33 +0000

    029a939 refactor(ivy): ngcc - remove the last remnants of `path` and `canonical-path` (#29643)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+44.sha-1195dab.with-local-changes
+ * @license Angular v8.0.0-rc.0+57.sha-029a939.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5835,7 +5835,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+44.sha-1195dab.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+57.sha-029a939.with-local-changes');
 
     /**
      * @license
