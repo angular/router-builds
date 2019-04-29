1a7aaf6451e1f8783c573782a435eb033030643a
Andrew Kushnir <akushnir@google.com>
2019-04-29 19:49:36 +0000

    1195dab build(docs-infra): update the aio app to framework and cli 8.0.0-rc.1 (#30183)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+43.sha-2945f47.with-local-changes
+ * @license Angular v8.0.0-rc.0+44.sha-1195dab.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
