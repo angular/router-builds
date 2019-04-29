2cec9453b8d2ef0a5d1aa300ff4025e319bfd741
Andrew Kushnir <akushnir@google.com>
2019-04-29 16:57:28 +0000

    b4d291a fix(ivy): injectable pipe schematic generating incorrect import statements (#30170)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.1+26.sha-b0ecafd.with-local-changes
+ * @license Angular v8.0.0-rc.1+27.sha-b4d291a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
