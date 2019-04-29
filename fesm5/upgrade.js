267170baf9f807d323128e1b1f6bf189d249eee7
Andrew Kushnir <akushnir@google.com>
2019-04-29 19:41:42 +0000

    a44b510 docs: release notes for the v8.0.0-rc.2 release

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+41.sha-572b549.with-local-changes
+ * @license Angular v8.0.0-rc.0+42.sha-a44b510.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
