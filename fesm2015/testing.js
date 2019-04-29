26a39fb4eeab1c79401f3d3ed6f239342a896f5c
Andrew Kushnir <akushnir@google.com>
2019-04-29 19:50:18 +0000

    d9fd301 build(docs-infra): update the aio app to framework and cli 8.0.0-rc.1 (#30183)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2+1.sha-71c5d80.with-local-changes
+ * @license Angular v8.0.0-rc.2+2.sha-d9fd301.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
