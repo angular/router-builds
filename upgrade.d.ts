9b4e32dd79e4f6a5b343cb46741fbd19a126b8bc
Andrew Kushnir <akushnir@google.com>
2019-04-29 16:58:21 +0000

    4b2fcfd fix: disable injectable-pipe migration (#30180)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.1+27.sha-b4d291a.with-local-changes
+ * @license Angular v8.0.0-rc.1+28.sha-4b2fcfd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
