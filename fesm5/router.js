f52e42a82d8aed93b306e49502eccdafa33279ad
Andrew Kushnir <akushnir@google.com>
2019-04-29 20:56:06 +0000

    68ff2cc fix(ivy): host bindings and listeners not being inherited from undecorated classes (#30158)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+63.sha-6dc884f.with-local-changes
+ * @license Angular v8.0.0-rc.0+66.sha-68ff2cc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+63.sha-6dc884f.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+66.sha-68ff2cc.with-local-changes');
 
 /**
  * @license
