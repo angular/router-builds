08ea29b8f4aa9eed711de227fbd0e8619ec24b24
Andrew Kushnir <akushnir@google.com>
2019-04-29 20:44:52 +0000

    28a1caa docs: add in-page nav, change title to match left nav better (#30093)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+58.sha-f015dbe.with-local-changes
+ * @license Angular v8.0.0-rc.0+59.sha-28a1caa.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5788,7 +5788,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+58.sha-f015dbe.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+59.sha-28a1caa.with-local-changes');
 
     /**
      * @license
