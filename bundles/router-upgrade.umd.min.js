83aae79d7b93c62d7d7ba7653217e893e7aeb346
Kara Erickson <karakara@google.com>
2019-04-30 23:33:59 +0000

    ad94e02 perf(ivy): store views directly on LContainer (#30179)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+69.sha-00ffc03.with-local-changes
+ * @license Angular v8.0.0-rc.0+70.sha-ad94e02.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
