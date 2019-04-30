4ffeaa789d2429db407fedcffe770536379dca55
Kara Erickson <karakara@google.com>
2019-04-30 23:32:52 +0000

    37c598d fix(ivy): inherited listeners from grand super classes invoked multiple times (#30169)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+70.sha-ad94e02.with-local-changes
+ * @license Angular v8.0.0-rc.0+71.sha-37c598d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+70.sha-ad94e02.with-local-changes
+ * @license Angular v8.0.0-rc.0+71.sha-37c598d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5835,7 +5835,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+70.sha-ad94e02.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+71.sha-37c598d.with-local-changes');
 
     /**
      * @license
