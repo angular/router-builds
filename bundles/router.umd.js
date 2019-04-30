1e5d537e52280bc808bd77e7fdc465ac850115f0
Kara Erickson <karakara@google.com>
2019-04-30 03:18:16 +0000

    00ffc03 fix(ivy): support forward refs in provider deps (#30201)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+68.sha-e53cf81.with-local-changes
+ * @license Angular v8.0.0-rc.0+69.sha-00ffc03.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5835,7 +5835,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+68.sha-e53cf81.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+69.sha-00ffc03.with-local-changes');
 
     /**
      * @license
