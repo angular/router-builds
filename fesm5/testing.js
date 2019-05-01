5f7dce8eda0d804b15a3fd930aa280aff2e75ecc
Kara Erickson <karakara@google.com>
2019-05-01 20:57:00 +0000

    7ec8806 docs: explicitly state purpose of components (#29879)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+74.sha-7d6f488.with-local-changes
+ * @license Angular v8.0.0-rc.0+75.sha-7ec8806.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
