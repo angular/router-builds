0defca3c6ba6d5037bc62a06b9a08226e323ad71
Kara Erickson <karakara@google.com>
2019-05-01 23:59:19 +0000

    b70d20b fix(docs-infra): update app code to work with Ivy (#28530)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+77.sha-5b80ab3.with-local-changes
+ * @license Angular v8.0.0-rc.0+79.sha-b70d20b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
