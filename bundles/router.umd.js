792c67600a3432fabd64623d76c37a5707da5ce4
Kara Erickson <karakara@google.com>
2019-05-01 20:57:12 +0000

    1dc134b docs: explicitly state purpose of components (#29879)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2+10.sha-6a61d37.with-local-changes
+ * @license Angular v8.0.0-rc.2+11.sha-1dc134b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5835,7 +5835,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.2+10.sha-6a61d37.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.2+11.sha-1dc134b.with-local-changes');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2+10.sha-6a61d37.with-local-changes
+ * @license Angular v8.0.0-rc.2+11.sha-1dc134b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
