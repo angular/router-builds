0d8d67eb822bf17ade1f6a047face60bba696306
Kara Erickson <karakara@google.com>
2019-05-01 04:50:16 +0000

    7d6f488 fix(ivy): properly tree-shake away StaticInjector (#30219)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+73.sha-b1506a3.with-local-changes
+ * @license Angular v8.0.0-rc.0+74.sha-7d6f488.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
