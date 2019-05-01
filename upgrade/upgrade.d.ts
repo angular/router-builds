daec730cf131f35369b5f371ed448aa936e40f25
Kara Erickson <karakara@google.com>
2019-05-01 22:58:40 +0000

    5b80ab3 fix(ivy): use `CompilerHost.resolveModuleNames()` if available (#30017)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+75.sha-7ec8806.with-local-changes
+ * @license Angular v8.0.0-rc.0+77.sha-5b80ab3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
