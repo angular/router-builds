314e31ccd9d5a4c1e2327706e45dc66bf64b117f
Kara Erickson <karakara@google.com>
2019-05-02 17:52:59 +0000

    3fe3a84 fix(core): fix interpolate identifier in AOT (#30243)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2+13.sha-28e4187.with-local-changes
+ * @license Angular v8.0.0-rc.2+14.sha-3fe3a84.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
