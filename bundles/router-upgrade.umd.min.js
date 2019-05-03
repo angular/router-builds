e1b3255b282ce9920dd068a399e06a07bc687135
Kara Erickson <karakara@google.com>
2019-05-03 15:32:26 +0000

    b1506a3 fix(ivy): support injection flags for provider deps without new (#30216)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+66.sha-68ff2cc.with-local-changes
+ * @license Angular v8.0.0-rc.0+73.sha-b1506a3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+66.sha-68ff2cc.with-local-changes
+ * @license Angular v8.0.0-rc.0+73.sha-b1506a3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8082,7 +8082,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+66.sha-68ff2cc.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+73.sha-b1506a3.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
