114e02b39f05add88f17ee0f3f6f2a014c291b8f
Kara Erickson <karakara@google.com>
2019-05-06 17:37:02 +0000

    817c2b4 docs: add explanation on what lazy loading is (#29667)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2+18.sha-fed07c7.with-local-changes
+ * @license Angular v8.0.0-rc.2+19.sha-817c2b4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
