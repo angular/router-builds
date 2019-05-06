ea8893958491399da1591924c6ec1234c74a7104
Kara Erickson <karakara@google.com>
2019-05-06 21:32:36 +0000

    548b003 fix(compiler-cli): log ngcc skipping messages as debug instead of info (#30232)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2+20.sha-48dc41d.with-local-changes
+ * @license Angular v8.0.0-rc.2+21.sha-548b003.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2+20.sha-48dc41d.with-local-changes
+ * @license Angular v8.0.0-rc.2+21.sha-548b003.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5835,7 +5835,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.2+20.sha-48dc41d.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.2+21.sha-548b003.with-local-changes');
 
     /**
      * @license
