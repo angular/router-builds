e29675af73b3264209992e14946e7ac44a99ff98
Kara Erickson <karakara@google.com>
2019-05-06 16:52:44 +0000

    60a8888 fix(compiler-cli): log ngcc skipping messages as debug instead of info (#30232)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+85.sha-39f2c9f.with-local-changes
+ * @license Angular v8.0.0-rc.0+84.sha-60a8888.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
