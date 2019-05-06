a598e9043ff37f22f03f9cfa6a488ac184824a7a
Kara Erickson <karakara@google.com>
2019-05-06 16:39:51 +0000

    d7283c6 docs: fix example header in singleton services guide to match styleguide (#30097)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2+14.sha-3fe3a84.with-local-changes
+ * @license Angular v8.0.0-rc.2+15.sha-d7283c6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5747,7 +5747,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.2+14.sha-3fe3a84.with-local-changes');
+var VERSION = new Version('8.0.0-rc.2+15.sha-d7283c6.with-local-changes');
 
 /**
  * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2+14.sha-3fe3a84.with-local-changes
+ * @license Angular v8.0.0-rc.2+15.sha-d7283c6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
