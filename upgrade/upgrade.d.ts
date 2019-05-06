6efa169532fca56d407d6144b2fdd399654742d0
Kara Erickson <karakara@google.com>
2019-05-06 16:49:22 +0000

    390cac6 docs: fix `targetEntryPointPath` description (#30237)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2+16.sha-8eb0b8b.with-local-changes
+ * @license Angular v8.0.0-rc.2+17.sha-390cac6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
