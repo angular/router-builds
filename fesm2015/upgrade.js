46535bf55f70d8e2ec5c65f959da8e5490cca788
Kara Erickson <karakara@google.com>
2019-05-06 19:12:14 +0000

    48dc41d docs: fix naming miss and improve anchor reference in router guide (#30225)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2+19.sha-817c2b4.with-local-changes
+ * @license Angular v8.0.0-rc.2+20.sha-48dc41d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
