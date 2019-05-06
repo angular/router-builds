4bd6213b713cb8e6f7be95029cb99889655ebeb5
Kara Erickson <karakara@google.com>
2019-05-06 23:29:54 +0000

    3327bd8 fix(router): fix a problem with router not responding to back button (#30160)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+89.sha-ea9a381.with-local-changes
+ * @license Angular v8.0.0-rc.0+90.sha-3327bd8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3969,6 +3969,7 @@
                              * way the next navigation will be coming from the current URL in the browser.
                              */
                             _this.rawUrlTree = t.rawUrl;
+                            _this.browserUrlTree = t.urlAfterRedirects;
                             t.resolve(null);
                             return rxjs.EMPTY;
                         }
@@ -5788,7 +5789,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+89.sha-ea9a381.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+90.sha-3327bd8.with-local-changes');
 
     /**
      * @license
