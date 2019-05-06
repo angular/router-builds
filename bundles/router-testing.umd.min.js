3c8001a20dcd2f54f3a2a2dd93a33cc6a7c85687
Kara Erickson <karakara@google.com>
2019-05-06 21:28:20 +0000

    39f2c9f docs(forms): fix ControlValueAccessor registerOnChange code example (#30276)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+80.sha-30d1f29.with-local-changes
+ * @license Angular v8.0.0-rc.0+85.sha-39f2c9f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
