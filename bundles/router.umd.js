2e7d9c733b436a42df7f6a8b3568d42b35412648
Kara Erickson <karakara@google.com>
2019-05-07 18:28:01 +0000

    be8fbac refactor(ivy): break apart stylingMap into styleMap and classMap instructions (#30293)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+96.sha-98a38ec.with-local-changes
+ * @license Angular v8.0.0-rc.0+97.sha-be8fbac.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5836,7 +5836,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+96.sha-98a38ec.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+97.sha-be8fbac.with-local-changes');
 
     /**
      * @license
