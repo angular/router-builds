7c4f23cca58140c7b38bf149822753e870129ab3
Kara Erickson <karakara@google.com>
2019-05-07 17:42:08 +0000

    1660b34 test: fix several bazel compiler-cli tests in windows (#30189)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+91.sha-b40f6f3.with-local-changes
+ * @license Angular v8.0.0-rc.0+92.sha-1660b34.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5789,7 +5789,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+91.sha-b40f6f3.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+92.sha-1660b34.with-local-changes');
 
     /**
      * @license
