aa1a6c434b87d6d4ee4c2181cbe17c7abce9f376
Kara Erickson <karakara@google.com>
2019-05-07 17:47:49 +0000

    f6d7271 refactor: remove tslint no-use-before-declare rule (#30288)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2+25.sha-c1f3faf.with-local-changes
+ * @license Angular v8.0.0-rc.2+26.sha-f6d7271.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2+25.sha-c1f3faf.with-local-changes
+ * @license Angular v8.0.0-rc.2+26.sha-f6d7271.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5836,7 +5836,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.2+25.sha-c1f3faf.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.2+26.sha-f6d7271.with-local-changes');
 
     /**
      * @license
