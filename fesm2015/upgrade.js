7a4dd088c68088821ca5a9183cdefe1b4c42762b
Kara Erickson <karakara@google.com>
2019-05-07 22:10:42 +0000

    bd37622 docs: release notes for the v8.0.0-rc.3 release

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+99.sha-48de6e8.with-local-changes
+ * @license Angular v8.0.0-rc.0+100.sha-bd37622.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
