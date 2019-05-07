37191fc619d9dfaeef98706785d8073ab75241ed
Kara Erickson <karakara@google.com>
2019-05-07 17:57:44 +0000

    82fd192 docs: update date pipe example for minutes and seconds (#30281) (#30295)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2+26.sha-f6d7271.with-local-changes
+ * @license Angular v8.0.0-rc.2+27.sha-82fd192.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
