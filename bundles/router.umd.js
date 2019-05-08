491363a0980a4a609b5fa90682638c191767cc92
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 16:56:59 +0000

    6857533 fix(docs-infra): remove scroll position from sessionStorage when a ServiceWorker update has been activated (#29958)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.3+8.sha-6b07711.with-local-changes
+ * @license Angular v8.0.0-rc.3+9.sha-6857533.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5835,7 +5835,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.3+8.sha-6b07711.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.3+9.sha-6857533.with-local-changes');
 
     /**
      * @license
