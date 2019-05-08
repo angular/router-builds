88ca24e7bbcab6a8efb0445e95b2bac753aacdce
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 16:39:31 +0000

    de65112 feat(ivy): add more DOM manipulation counters to ngDevMode (#30315)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+105.sha-4a96ddf.with-local-changes
+ * @license Angular v8.0.0-rc.0+106.sha-de65112.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
