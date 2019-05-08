b059df5f3644de15191b666646a04da60168ac7a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 00:44:58 +0000

    08841e3 Revert "fix(router): fix a problem with router not responding to back button (#30160)" (#30320)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.3+2.sha-d1fcc2b.with-local-changes
+ * @license Angular v8.0.0-rc.3+3.sha-08841e3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3980,7 +3980,6 @@
                              * way the next navigation will be coming from the current URL in the browser.
                              */
                             _this.rawUrlTree = t.rawUrl;
-                            _this.browserUrlTree = t.urlAfterRedirects;
                             t.resolve(null);
                             return rxjs.EMPTY;
                         }
@@ -5836,7 +5835,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.3+2.sha-d1fcc2b.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.3+3.sha-08841e3.with-local-changes');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.3+2.sha-d1fcc2b.with-local-changes
+ * @license Angular v8.0.0-rc.3+3.sha-08841e3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
