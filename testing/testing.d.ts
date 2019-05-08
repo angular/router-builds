1792c1035717fe7a5dfc8d5d508b632b53a92821
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 00:10:18 +0000

    d1fcc2b fix(bazel): Directly spawn native Bazel binary (#30306)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.3+1.sha-fac0044.with-local-changes
+ * @license Angular v8.0.0-rc.3+2.sha-d1fcc2b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
