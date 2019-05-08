69f9cda7d1d98b88d43633059ac2fcb5222a4fea
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 16:54:36 +0000

    bcc72b0 test(ivy): move host binding tests to acceptance (#30308)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.3+7.sha-12fb639.with-local-changes
+ * @license Angular v8.0.0-rc.3+5.sha-bcc72b0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.3+7.sha-12fb639.with-local-changes
+ * @license Angular v8.0.0-rc.3+5.sha-bcc72b0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8082,7 +8082,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.3+7.sha-12fb639.with-local-changes');
+const VERSION = new Version('8.0.0-rc.3+5.sha-bcc72b0.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
