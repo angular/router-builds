e87e6b03ec26bc1c241c52b8e897cb0a453a365a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 16:43:16 +0000

    7c8a62d refactor(ivy): remove elementIndex param from all element-level styling instructions (#30313)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+106.sha-de65112.with-local-changes
+ * @license Angular v8.0.0-rc.0+107.sha-7c8a62d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5747,7 +5747,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+106.sha-de65112.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+107.sha-7c8a62d.with-local-changes');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+106.sha-de65112.with-local-changes
+ * @license Angular v8.0.0-rc.0+107.sha-7c8a62d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
