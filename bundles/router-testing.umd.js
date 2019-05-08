4c5200e482e6a9a9b422444c535dabcd6d77e398
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 19:18:35 +0000

    509352f fix(core): static-query migration should gracefully exit if AOT compiler throws (#30269)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+113.sha-29786e8.with-local-changes
+ * @license Angular v8.0.0-rc.0+116.sha-509352f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
