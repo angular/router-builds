5989bf30f4c3854be6867a3ca13a71cc56e6f901
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 17:34:56 +0000

    29786e8 fix(ivy): unable to bind SafeStyle as camel case property (#30328)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+112.sha-bf6bedd.with-local-changes
+ * @license Angular v8.0.0-rc.0+113.sha-29786e8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+112.sha-bf6bedd.with-local-changes
+ * @license Angular v8.0.0-rc.0+113.sha-29786e8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5747,7 +5747,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+112.sha-bf6bedd.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+113.sha-29786e8.with-local-changes');
 
 /**
  * @license
