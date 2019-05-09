f09b8e0ef6e4eb1660905a34f8991e1667520ce7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 18:56:50 +0000

    dd299f9 fix(core): static-query migration should handle queries on accessors (#30327)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.3+17.sha-49ec3f3.with-local-changes
+ * @license Angular v8.0.0-rc.3+18.sha-dd299f9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
