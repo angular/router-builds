977bca27e218cf5c7a75305bae0c56d82438b5f7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 18:58:30 +0000

    806f811 docs: remove reference to e2e applications (#30324)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.3+18.sha-dd299f9.with-local-changes
+ * @license Angular v8.0.0-rc.3+19.sha-806f811.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5826,7 +5826,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.3+18.sha-dd299f9.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.3+19.sha-806f811.with-local-changes');
 
     /**
      * @license
