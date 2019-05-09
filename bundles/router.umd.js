bfd7a8a818e8fb5ef83f934caf0d8258b3079a9d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 19:01:38 +0000

    bf031fc test(ivy): update devkit in hello world tests to rc.2 (#30162)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+125.sha-cb6ad97.with-local-changes
+ * @license Angular v8.0.0-rc.0+129.sha-bf031fc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5779,7 +5779,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+125.sha-cb6ad97.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+129.sha-bf031fc.with-local-changes');
 
     /**
      * @license
