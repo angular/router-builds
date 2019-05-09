51a1509447cd8d8215beb01852dced4d9f791ffb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 21:57:58 +0000

    8d3365e fix(core): static-query usage migration strategy should detect ambiguous query usage (#30215)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+134.sha-8cec8f5.with-local-changes
+ * @license Angular v8.0.0-rc.0+135.sha-8d3365e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5779,7 +5779,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+134.sha-8cec8f5.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+135.sha-8d3365e.with-local-changes');
 
     /**
      * @license
