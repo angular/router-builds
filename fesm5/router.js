d365cd5ed38748915dd030c4ecfc09dc84f582fe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 18:56:38 +0000

    cb6ad97 test(ivy): add onlyInIvy perf counter expectations (#30339)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+127.sha-f26f036.with-local-changes
+ * @license Angular v8.0.0-rc.0+125.sha-cb6ad97.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5738,7 +5738,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+127.sha-f26f036.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+125.sha-cb6ad97.with-local-changes');
 
 /**
  * @license
