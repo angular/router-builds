84b50e1acdfb49c9028063f4af386e9a9f053a77
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 21:56:46 +0000

    8d3365e fix(core): static-query usage migration strategy should detect ambiguous query usage (#30215)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+134.sha-8cec8f5.with-local-changes
+ * @license Angular v8.0.0-rc.0+135.sha-8d3365e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
