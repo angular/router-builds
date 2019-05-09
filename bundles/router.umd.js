20cc87b52c5efdc9d683384b0296c50f90f8b3c3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 19:10:39 +0000

    3aff79c fix(bazel): pass correct arguments to http_server in Windows (#30346)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+132.sha-de996c6.with-local-changes
+ * @license Angular v8.0.0-rc.0+123.sha-3aff79c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5826,7 +5826,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+132.sha-de996c6.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+123.sha-3aff79c.with-local-changes');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+132.sha-de996c6.with-local-changes
+ * @license Angular v8.0.0-rc.0+123.sha-3aff79c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
