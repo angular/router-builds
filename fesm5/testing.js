3d0d102f78dc9044c794ef5745ecf87e731efb66
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 18:55:51 +0000

    f26f036 docs: remove reference to e2e applications (#30324)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+126.sha-0ffdb48.with-local-changes
+ * @license Angular v8.0.0-rc.0+127.sha-f26f036.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
