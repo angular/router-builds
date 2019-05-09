4aa37fa5b6dda8da378c99f5fe822222abc3f9d2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 19:01:46 +0000

    de996c6 docs(forms): properly convert number to string for formControlName input (#29473)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+130.sha-d2c83ea.with-local-changes
+ * @license Angular v8.0.0-rc.0+132.sha-de996c6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5826,7 +5826,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+130.sha-d2c83ea.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+132.sha-de996c6.with-local-changes');
 
     /**
      * @license
