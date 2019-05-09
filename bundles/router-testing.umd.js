ffd6c7696fe6c96cb5f3f45631778556c378e17b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 19:04:23 +0000

    0cefa9e docs: fix filename in example headers for provider guides (#29889)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.3+20.sha-994d48a.with-local-changes
+ * @license Angular v8.0.0-rc.3+22.sha-0cefa9e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
