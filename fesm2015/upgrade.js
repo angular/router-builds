6f15ccc71665cb0d8d51194f121f8dad6615f612
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-10 19:18:55 +0000

    b7edef0 docs: remove mention of chrome 41 from differential loading (#30390)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.3+30.sha-39fa937.with-local-changes
+ * @license Angular v8.0.0-rc.3+31.sha-b7edef0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
