bc2451f833cd5c98af6bd07eff9412eebb0b87cf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-10 16:41:33 +0000

    2977829 test(ivy): move content projection tests to acceptance (#30357)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.3+28.sha-dbe845e.with-local-changes
+ * @license Angular v8.0.0-rc.3+29.sha-2977829.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5826,7 +5826,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.3+28.sha-dbe845e.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.3+29.sha-2977829.with-local-changes');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.3+28.sha-dbe845e.with-local-changes
+ * @license Angular v8.0.0-rc.3+29.sha-2977829.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
