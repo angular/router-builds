c92d133e826e39249ab0338b48e12e7c0190dd7b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-10 21:54:38 +0000

    cb6dea4 test(ivy): move pipe tests into acceptance (#30389)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+147.sha-5ab809d.with-local-changes
+ * @license Angular v8.0.0-rc.0+148.sha-cb6dea4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8261,7 +8261,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+147.sha-5ab809d.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+148.sha-cb6dea4.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+147.sha-5ab809d.with-local-changes
+ * @license Angular v8.0.0-rc.0+148.sha-cb6dea4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
