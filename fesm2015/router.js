c3c2bb16c2d2dbf7028203de8b0bc7a8773d4d09
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-13 18:14:00 +0000

    42fc5c9 docs: change reference to jQuery programmers (#30386)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.3+35.sha-74afdc3.with-local-changes
+ * @license Angular v8.0.0-rc.3+37.sha-42fc5c9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8073,7 +8073,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.3+35.sha-74afdc3.with-local-changes');
+const VERSION = new Version('8.0.0-rc.3+37.sha-42fc5c9.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
