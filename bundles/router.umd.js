9be4de1539b731cd89282a4cccecfb80fd551b7b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-13 18:19:45 +0000

    7dad328 release: ts_api_guardian (#30120)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+154.sha-7569a2e.with-local-changes
+ * @license Angular v8.0.0-rc.0+153.sha-7dad328.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5779,7 +5779,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+154.sha-7569a2e.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+153.sha-7dad328.with-local-changes');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+154.sha-7569a2e.with-local-changes
+ * @license Angular v8.0.0-rc.0+153.sha-7dad328.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
