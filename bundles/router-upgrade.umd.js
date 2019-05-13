4040252ac4f30045fcb73deb3271404adb2291f5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-13 18:32:32 +0000

    f74373f fix(ivy): align NgModule registration timing with ViewEngine (#30244)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+156.sha-2f35dbf.with-local-changes
+ * @license Angular v8.0.0-rc.0+157.sha-f74373f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
