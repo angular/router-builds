fa8c62ec137aec4efc1c09b6fb49c8b12c594cc3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-13 18:47:32 +0000

    18c0ba5 test: fix ngcc integration tests in windows (#30297)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+157.sha-f74373f.with-local-changes
+ * @license Angular v8.0.0-rc.0+160.sha-18c0ba5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5779,7 +5779,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+157.sha-f74373f.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+160.sha-18c0ba5.with-local-changes');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+157.sha-f74373f.with-local-changes
+ * @license Angular v8.0.0-rc.0+160.sha-18c0ba5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5706,7 +5706,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+157.sha-f74373f.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+160.sha-18c0ba5.with-local-changes');
 
 /**
  * @license
