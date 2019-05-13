7e0027a76fa8d19678ab4045df5a82c490c7ac8e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-13 18:27:38 +0000

    2f35dbf test: fix ngtsc tests in windows (#30146)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+153.sha-7dad328.with-local-changes
+ * @license Angular v8.0.0-rc.0+156.sha-2f35dbf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8261,7 +8261,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+153.sha-7dad328.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+156.sha-2f35dbf.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
