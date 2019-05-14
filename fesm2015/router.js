8ddd08042bf835ec403bd56e0543d3eccb281a2e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-14 22:08:52 +0000

    dbb150a Revert "fix(core): CSS sanitizer now allows parens in file names (#30322)" (#30463)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+176.sha-9e5377a.with-local-changes
+ * @license Angular v8.0.0-rc.0+177.sha-dbb150a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8262,7 +8262,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+176.sha-9e5377a.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+177.sha-dbb150a.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
