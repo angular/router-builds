3b49cab7453232d75296101f03b255d0e258ca96
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-14 16:56:40 +0000

    d09d8e0 test(ivy): move render3 directive tests to acceptance (#30432)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+161.sha-197584d.with-local-changes
+ * @license Angular v8.0.0-rc.0+163.sha-d09d8e0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5780,7 +5780,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+161.sha-197584d.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+163.sha-d09d8e0.with-local-changes');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+161.sha-197584d.with-local-changes
+ * @license Angular v8.0.0-rc.0+163.sha-d09d8e0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
