5a294a0ffe0baa088adebd11dca40636b3b3e9c0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-14 19:00:07 +0000

    3136d9f fix(bazel): Use existing npm/yarn lock files (#30438)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.3+43.sha-381d7c4.with-local-changes
+ * @license Angular v8.0.0-rc.3+45.sha-3136d9f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5739,7 +5739,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.3+43.sha-381d7c4.with-local-changes');
+var VERSION = new Version('8.0.0-rc.3+45.sha-3136d9f.with-local-changes');
 
 /**
  * @license
