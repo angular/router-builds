522236f1dba73f6f7181c1bcd35df866e328a30e
Jason Aden <jasonaden@google.com>
2019-05-15 21:49:34 +0000

    3a8f74e docs: fix example "testing" app compilation (#30427)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+188.sha-8555016.with-local-changes
+ * @license Angular v8.0.0-rc.0+189.sha-3a8f74e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+188.sha-8555016.with-local-changes
+ * @license Angular v8.0.0-rc.0+189.sha-3a8f74e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+188.sha-8555016.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+189.sha-3a8f74e.with-local-changes');
 
 /**
  * @license
