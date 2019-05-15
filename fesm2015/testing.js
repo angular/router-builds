778698c4321e88246b2d3cd07c48af1557a45944
Jason Aden <jasonaden@google.com>
2019-05-15 21:49:17 +0000

    8555016 docs: fix the payload in event-binding sample code (#30429)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+186.sha-1a0e500.with-local-changes
+ * @license Angular v8.0.0-rc.0+188.sha-8555016.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
