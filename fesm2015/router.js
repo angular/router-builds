c24a301d322a11cc70c16d4069afa70686952669
Jason Aden <jasonaden@google.com>
2019-05-15 21:47:49 +0000

    1a0e500 fix(ivy): support sub-class services that only inherit `@Injectable` (#30388)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+185.sha-35c1750.with-local-changes
+ * @license Angular v8.0.0-rc.0+186.sha-1a0e500.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8274,7 +8274,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+185.sha-35c1750.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+186.sha-1a0e500.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
