4395787017065d5bb13fd840bf118facac1665ee
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-15 20:07:38 +0000

    b6aa99d release: cut the v8.0.0-rc.4 release

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.3+57.sha-e10b213.with-local-changes
+ * @license Angular v8.0.0-rc.4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
