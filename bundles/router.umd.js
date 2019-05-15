45207396c39d63a47ce98a9585f231239ac60c5b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-15 20:10:23 +0000

    d20b0f4 docs: release notes for the v8.0.0-rc.4 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+182.sha-79d4b16.with-local-changes
+ * @license Angular v8.0.0-rc.0+183.sha-d20b0f4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5835,7 +5835,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+182.sha-79d4b16.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+183.sha-d20b0f4.with-local-changes');
 
     /**
      * @license
