1e3ce469f4d70efbf618324341c00074729d40fb
Jason Aden <jasonaden@google.com>
2019-05-15 21:51:13 +0000

    ede4246 docs: fix the payload in event-binding sample code (#30429)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.4+1.sha-32daa93.with-local-changes
+ * @license Angular v8.0.0-rc.4+3.sha-ede4246.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.4+1.sha-32daa93.with-local-changes');
+var VERSION = new Version('8.0.0-rc.4+3.sha-ede4246.with-local-changes');
 
 /**
  * @license
