a6122bee9749f0f05ae7143e5411162cf7db381c
Jason Aden <jasonaden@google.com>
2019-05-15 21:46:35 +0000

    1a0e500 fix(ivy): support sub-class services that only inherit `@Injectable` (#30388)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+185.sha-35c1750.with-local-changes
+ * @license Angular v8.0.0-rc.0+186.sha-1a0e500.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
