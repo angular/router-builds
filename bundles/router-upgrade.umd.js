686450644fad403a123950740041c101880c5d3f
Jason Aden <jasonaden@google.com>
2019-05-15 21:57:09 +0000

    effc580 docs: correct types in transform method to match specification. (#29812)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.4+3.sha-ede4246.with-local-changes
+ * @license Angular v8.0.0-rc.4+5.sha-effc580.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.4+3.sha-ede4246.with-local-changes
+ * @license Angular v8.0.0-rc.4+5.sha-effc580.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.4+3.sha-ede4246.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.4+5.sha-effc580.with-local-changes');
 
     /**
      * @license
