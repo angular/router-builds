65e957ec020e1c44c603fbc09cab72e4294d7ea3
Jason Aden <jasonaden@google.com>
2019-05-16 17:06:42 +0000

    0778093 test: fix compiler tests in windows ci (#30482)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+193.sha-c62c5e2.with-local-changes
+ * @license Angular v8.0.0-rc.0+196.sha-0778093.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
