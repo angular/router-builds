3eb57814b9af21cf3f6286e799ae9f375504b032
Jason Aden <jasonaden@google.com>
2019-05-16 19:05:40 +0000

    1b613c3 fix(ivy): evaluate external declaration usages as dynamic (#30247)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+199.sha-e9ead2b.with-local-changes
+ * @license Angular v8.0.0-rc.0+201.sha-1b613c3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
