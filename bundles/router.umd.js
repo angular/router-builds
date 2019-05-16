10494a1c8caac096384e6d1b180c121bfd129472
Jason Aden <jasonaden@google.com>
2019-05-16 17:16:38 +0000

    e9ead2b feat(ivy): more accurate type narrowing for `ngIf` directive (#30248)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+196.sha-0778093.with-local-changes
+ * @license Angular v8.0.0-rc.0+199.sha-e9ead2b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+196.sha-0778093.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+199.sha-e9ead2b.with-local-changes');
 
     /**
      * @license
