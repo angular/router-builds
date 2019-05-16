8398dd77b64c8bf60ac2895916bcff1399fe1d32
Jason Aden <jasonaden@google.com>
2019-05-16 20:54:06 +0000

    c9b588b feat(ivy): ngtsc - support namespaced `forwardRef` calls (#25445)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+199.sha-e9ead2b.with-local-changes
+ * @license Angular v8.0.0-rc.0+216.sha-c9b588b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+199.sha-e9ead2b.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+216.sha-c9b588b.with-local-changes');
 
 /**
  * @license
