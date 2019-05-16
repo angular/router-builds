c572856fd3e0b9b07ad3640c647f498ba8f86b1e
Jason Aden <jasonaden@google.com>
2019-05-16 17:12:27 +0000

    3de26a8 fix(bazel): Disable sandbox on Mac OS (#30460)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.4+6.sha-581336a.with-local-changes
+ * @license Angular v8.0.0-rc.4+8.sha-3de26a8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
