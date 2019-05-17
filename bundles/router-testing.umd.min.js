1d023f5f472abcf622effc69979275ace144b285
Jason Aden <jasonaden@google.com>
2019-05-17 21:39:07 +0000

    661a57d docs: update docs example dependencies to version 8 (#30385)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+249.sha-8e2e9dc.with-local-changes
+ * @license Angular v8.0.0-rc.0+254.sha-661a57d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+249.sha-8e2e9dc.with-local-changes
+ * @license Angular v8.0.0-rc.0+254.sha-661a57d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+249.sha-8e2e9dc.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+254.sha-661a57d.with-local-changes');
 
     /**
      * @license
