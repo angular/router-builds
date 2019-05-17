cf5f41f065956c0f2c7990887034c91fb49b0c99
Jason Aden <jasonaden@google.com>
2019-05-17 20:56:06 +0000

    9d5abfb docs: add kamil mysliwiec to GDE resources (#30508)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+247.sha-fd7dd4d.with-local-changes
+ * @license Angular v8.0.0-rc.0+248.sha-9d5abfb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+247.sha-fd7dd4d.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+248.sha-9d5abfb.with-local-changes');
 
     /**
      * @license
