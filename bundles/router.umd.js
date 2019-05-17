9fd1a0c65c44adffffe6da032d28bcb749488e0c
Jason Aden <jasonaden@google.com>
2019-05-17 19:25:09 +0000

    eda09e6 fix(ivy): ngtsc - do not wrap arguments unnecessarily (#30349)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+238.sha-98ded94.with-local-changes
+ * @license Angular v8.0.0-rc.0+244.sha-eda09e6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+238.sha-98ded94.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+244.sha-eda09e6.with-local-changes');
 
     /**
      * @license
