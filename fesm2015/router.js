e42b595cb18333bc935057f01928975e3c2eec80
Jason Aden <jasonaden@google.com>
2019-05-17 21:46:20 +0000

    c1135ee build: update version to 8.0.0-rc.4 in `package.json` (#30493)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+254.sha-661a57d.with-local-changes
+ * @license Angular v8.0.0-rc.0+257.sha-c1135ee.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+254.sha-661a57d.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+257.sha-c1135ee.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
