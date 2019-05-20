4df9b6cdeef931d03cdd3c7f7888e3167a2e53b0
Jason Aden <jasonaden@google.com>
2019-05-20 17:33:52 +0000

    6454f76 refactor(ivy): remove ngPrivateData megamorphic prop access (#30548)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+257.sha-c1135ee.with-local-changes
+ * @license Angular v8.0.0-rc.0+259.sha-6454f76.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
