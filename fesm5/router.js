012718a7833230ec84576b00e1d0938c2f8704bd
Jason Aden <jasonaden@google.com>
2019-05-21 20:56:25 +0000

    66f269c refactor(ivy): rename query-related functions for clarity (#30587)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+281.sha-6debe9d.with-local-changes
+ * @license Angular v8.0.0-rc.0+287.sha-66f269c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+281.sha-6debe9d.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+287.sha-66f269c.with-local-changes');
 
 /**
  * @license
