b7d0bcbc4a9bc981e11d0f0d98e481bec70b90d0
Jason Aden <jasonaden@google.com>
2019-05-21 20:50:20 +0000

    a50c1bb docs: fix spelling of 'its' (#30455)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+275.sha-f3c69e7.with-local-changes
+ * @license Angular v8.0.0-rc.0+276.sha-a50c1bb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
