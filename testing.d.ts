85510182e5819a60de346cc664863a19f18d1323
Jason Aden <jasonaden@google.com>
2019-05-21 20:54:23 +0000

    6debe9d docs(docs-infra): add common/upgrade to authors package for API docs (#30567)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+282.sha-f6bf892.with-local-changes
+ * @license Angular v8.0.0-rc.0+281.sha-6debe9d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
