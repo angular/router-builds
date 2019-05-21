320a69e9b0b411ee6d7eee2986bf63219d47e3a7
Jason Aden <jasonaden@google.com>
2019-05-21 19:56:19 +0000

    ebfbc04 ci(docs-infra): fix `test_docs_examples_ivy` job (#30593)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+270.sha-9e946c9.with-local-changes
+ * @license Angular v8.0.0-rc.0+272.sha-ebfbc04.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
