4b995d3c10f99048b625e992a6937e4b526916a8
Jason Aden <jasonaden@google.com>
2019-05-21 00:01:57 +0000

    e688e02 fix(bazel): ng test should run specific ts_web_test_suite (#30526)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+262.sha-d7eaae6.with-local-changes
+ * @license Angular v8.0.0-rc.0+263.sha-e688e02.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
