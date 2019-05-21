de597cbae16d3eea26840fd52f56915a8c936982
Jason Aden <jasonaden@google.com>
2019-05-21 16:38:05 +0000

    9e946c9 docs(changelog): remove docs-infra changes (#25833)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+271.sha-e1af6e3.with-local-changes
+ * @license Angular v8.0.0-rc.0+270.sha-9e946c9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
