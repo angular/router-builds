615479c28fcc8323970bf505f819b6b97d64e7d4
Jason Aden <jasonaden@google.com>
2019-05-21 16:37:14 +0000

    e1af6e3 docs: use dynamic import syntax in examples using lazy loading (#30563)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+269.sha-0d97143.with-local-changes
+ * @license Angular v8.0.0-rc.0+271.sha-e1af6e3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
