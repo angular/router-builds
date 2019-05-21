a8132fa195d86d8559e48a61fdacd94e4e9ad124
Jason Aden <jasonaden@google.com>
2019-05-21 21:13:45 +0000

    0f5da82 docs: deprecations summary fix api heading level, remove inject, add index by version (#30400)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+283.sha-7c0667d.with-local-changes
+ * @license Angular v8.0.0-rc.0+288.sha-0f5da82.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+283.sha-7c0667d.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+288.sha-0f5da82.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+283.sha-7c0667d.with-local-changes
+ * @license Angular v8.0.0-rc.0+288.sha-0f5da82.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
