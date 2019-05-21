72fbe0d1c03aa84ac239915fa41d6607b639b1ee
Jason Aden <jasonaden@google.com>
2019-05-21 20:50:54 +0000

    d1345c7 docs(docs-infra): add common/upgrade to authors package for API docs (#30567)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.4+16.sha-736d3ef.with-local-changes
+ * @license Angular v8.0.0-rc.4+36.sha-d1345c7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
