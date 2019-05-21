16114aec8835c4822a619232c61b862678cfc77d
Jason Aden <jasonaden@google.com>
2019-05-21 20:43:52 +0000

    c9f5f3d docs: add section to upgrade guide on lazy loading AngularJS (#30541)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+273.sha-a96976e.with-local-changes
+ * @license Angular v8.0.0-rc.0+274.sha-c9f5f3d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+273.sha-a96976e.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+274.sha-c9f5f3d.with-local-changes');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+273.sha-a96976e.with-local-changes
+ * @license Angular v8.0.0-rc.0+274.sha-c9f5f3d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
