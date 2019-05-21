c16bbd8930c668e348debb1de822dcb684b93b1e
Jason Aden <jasonaden@google.com>
2019-05-21 20:05:38 +0000

    a96976e fix(core): remove deprecated `TestBed.deprecatedOverrideProvider` API (#30576)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+272.sha-ebfbc04.with-local-changes
+ * @license Angular v8.0.0-rc.0+273.sha-a96976e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+272.sha-ebfbc04.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+273.sha-a96976e.with-local-changes');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+272.sha-ebfbc04.with-local-changes
+ * @license Angular v8.0.0-rc.0+273.sha-a96976e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+272.sha-ebfbc04.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+273.sha-a96976e.with-local-changes');
 
 /**
  * @license
