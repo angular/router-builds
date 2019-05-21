efede7b2d9b9bf2818cc1c058fe39e6aaa07df78
Jason Aden <jasonaden@google.com>
2019-05-21 16:39:25 +0000

    9e946c9 docs(changelog): remove docs-infra changes (#25833)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+271.sha-e1af6e3.with-local-changes
+ * @license Angular v8.0.0-rc.0+270.sha-9e946c9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+271.sha-e1af6e3.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+270.sha-9e946c9.with-local-changes');
 
     /**
      * @license
