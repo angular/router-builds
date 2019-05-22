7a05dc4b6029bc0ae08d46bffd2b13fbd8c4c166
Jason Aden <jasonaden@google.com>
2019-05-22 00:27:22 +0000

    87a2366 build: update base SHA for rebase on merge-pr script

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+288.sha-0f5da82.with-local-changes
+ * @license Angular v8.0.0-rc.0+289.sha-87a2366.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
