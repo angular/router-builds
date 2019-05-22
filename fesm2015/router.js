bee5997050162702121d0b1e48f6211c7fb02ee4
Jason Aden <jasonaden@google.com>
2019-05-22 23:57:30 +0000

    bf38df4 fix(bazel): allow ts_library interop with list-typed inputs (#30600)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.4+47.sha-79a0253.with-local-changes
+ * @license Angular v8.0.0-rc.4+48.sha-bf38df4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.4+47.sha-79a0253.with-local-changes');
+const VERSION = new Version('8.0.0-rc.4+48.sha-bf38df4.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
