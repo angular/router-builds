6da7ecd896e493c691b9ebc3a16b31053a552911
Jason Aden <jasonaden@google.com>
2019-05-22 23:54:08 +0000

    3125376 fix(bazel): allow ts_library interop with list-typed inputs (#30600)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+293.sha-3c54818.with-local-changes
+ * @license Angular v8.0.0-rc.0+295.sha-3125376.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
