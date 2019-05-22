bd4a0c03ade4626cdc92d1017bc31a77f9488398
Jason Aden <jasonaden@google.com>
2019-05-22 23:58:12 +0000

    86c4690 fix(ivy): R3TestBed doesn't allow template overrides with an empty string (#30602)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+296.sha-70fd430.with-local-changes
+ * @license Angular v8.0.0-rc.0+304.sha-86c4690.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
