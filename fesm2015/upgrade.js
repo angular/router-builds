f1709d0c83d65e03f82d479bc0652b3afe771b92
Jason Aden <jasonaden@google.com>
2019-05-22 23:59:57 +0000

    e20b92b feat(ivy): ngcc - turn on CommonJS support (#30200)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+304.sha-86c4690.with-local-changes
+ * @license Angular v8.0.0-rc.0+302.sha-e20b92b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
