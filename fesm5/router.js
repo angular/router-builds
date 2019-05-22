d6b2f74347513d0dd0aa2fcb53381285537c92d9
Jason Aden <jasonaden@google.com>
2019-05-22 23:47:36 +0000

    79a0253 docs: add new cli builder api guide (#29964)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.4+46.sha-62c936a.with-local-changes
+ * @license Angular v8.0.0-rc.4+47.sha-79a0253.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.4+46.sha-62c936a.with-local-changes');
+var VERSION = new Version('8.0.0-rc.4+47.sha-79a0253.with-local-changes');
 
 /**
  * @license
