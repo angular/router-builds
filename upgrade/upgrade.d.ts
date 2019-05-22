4008afbfe89ece31d0de23cea3ffb402a8564e2a
Jason Aden <jasonaden@google.com>
2019-05-22 23:42:11 +0000

    3c54818 docs: update node and typescript versions mentioned in docs (#30601)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+292.sha-ae91f45.with-local-changes
+ * @license Angular v8.0.0-rc.0+293.sha-3c54818.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
