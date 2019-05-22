bb4c9dffc7d3de2d6d73a007ee7da3c15d6d503b
Jason Aden <jasonaden@google.com>
2019-05-22 00:10:28 +0000

    66f269c refactor(ivy): rename query-related functions for clarity (#30587)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+259.sha-6454f76.with-local-changes
+ * @license Angular v8.0.0-rc.0+287.sha-66f269c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+259.sha-6454f76.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+287.sha-66f269c.with-local-changes');
 
     /**
      * @license
