cdf9042b85a6ab09c30fb590e9d6e76ffd30b3c4
Jason Aden <jasonaden@google.com>
2019-05-22 19:49:51 +0000

    ae91f45 docs: update file structure and workspace config for v8 (#30595)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+290.sha-f1ea78b.with-local-changes
+ * @license Angular v8.0.0-rc.0+292.sha-ae91f45.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
