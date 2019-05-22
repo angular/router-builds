e35682fe39863426c68dec5d76974dd0ccd60144
Jason Aden <jasonaden@google.com>
2019-05-22 23:43:40 +0000

    3c54818 docs: update node and typescript versions mentioned in docs (#30601)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+292.sha-ae91f45.with-local-changes
+ * @license Angular v8.0.0-rc.0+293.sha-3c54818.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+292.sha-ae91f45.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+293.sha-3c54818.with-local-changes');
 
     /**
      * @license
