b437beb18c1e14e15dc502f0a3b7258d4be4c9dc
Jason Aden <jasonaden@google.com>
2019-05-23 00:04:04 +0000

    d4e6263 docs: update simple stackblitz deployment instructions (#30465)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+303.sha-fa6cbb3.with-local-changes
+ * @license Angular v8.0.0-rc.0+305.sha-d4e6263.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5718,7 +5718,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+303.sha-fa6cbb3.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+305.sha-d4e6263.with-local-changes');
 
 /**
  * @license
