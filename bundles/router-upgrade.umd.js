d254a87728ec1f82660116da87bde3c06d946ed1
Jason Aden <jasonaden@google.com>
2019-05-23 20:57:44 +0000

    3f2f937 docs: add v8 deprecations (#30598)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.4+51.sha-256fe24.with-local-changes
+ * @license Angular v8.0.0-rc.4+52.sha-3f2f937.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
