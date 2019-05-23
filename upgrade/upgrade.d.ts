6902c68be31c3f15e13cc9eec3c6bbc18aae1120
Jason Aden <jasonaden@google.com>
2019-05-23 22:28:45 +0000

    c8af830 fix(core): require 'static' flag on queries in typings (#30641)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.4+52.sha-3f2f937.with-local-changes
+ * @license Angular v8.0.0-rc.4+57.sha-c8af830.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
