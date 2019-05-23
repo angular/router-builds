7a233c20e08b7a116c04a3bdbbe4d6b026aa0737
Jason Aden <jasonaden@google.com>
2019-05-23 18:12:51 +0000

    faac51f test(core): update query-specific tests in core (#30626)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+320.sha-21e8ac1.with-local-changes
+ * @license Angular v8.0.0-rc.0+324.sha-faac51f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+320.sha-21e8ac1.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+324.sha-faac51f.with-local-changes');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+320.sha-21e8ac1.with-local-changes
+ * @license Angular v8.0.0-rc.0+324.sha-faac51f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
