e387616129da8a516a71b96d771de8f205cfc336
Jason Aden <jasonaden@google.com>
2019-05-23 22:31:54 +0000

    361f181 feat(platform-webworker): deprecate platform-webworker (#30642)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.4+57.sha-c8af830.with-local-changes
+ * @license Angular v8.0.0-rc.4+59.sha-361f181.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.4+57.sha-c8af830.with-local-changes');
+var VERSION = new Version('8.0.0-rc.4+59.sha-361f181.with-local-changes');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.4+57.sha-c8af830.with-local-changes
+ * @license Angular v8.0.0-rc.4+59.sha-361f181.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
