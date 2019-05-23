533a6c75c2da366f21fcedd84d00625e25465386
Jason Aden <jasonaden@google.com>
2019-05-23 23:46:04 +0000

    2508872 docs(core): update deprecations guide (#30644)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+329.sha-ccc76f7.with-local-changes
+ * @license Angular v8.0.0-rc.0+331.sha-2508872.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
