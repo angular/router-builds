c962d32295fe31a10543ac5e235f56ca627b35c9
Jason Aden <jasonaden@google.com>
2019-05-23 00:01:11 +0000

    fa6cbb3 fix(ivy): ng-container with ViewContainerRef creates two comments (#30611)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+302.sha-e20b92b.with-local-changes
+ * @license Angular v8.0.0-rc.0+303.sha-fa6cbb3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+302.sha-e20b92b.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+303.sha-fa6cbb3.with-local-changes');
 
     /**
      * @license
