704728995cbe8676f885cc1ead5fa75188e852d0
Jason Aden <jasonaden@google.com>
2019-05-23 20:54:36 +0000

    b5c8939 docs: add v8 deprecations (#30598)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+324.sha-faac51f.with-local-changes
+ * @license Angular v8.0.0-rc.0+327.sha-b5c8939.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+324.sha-faac51f.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+327.sha-b5c8939.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
