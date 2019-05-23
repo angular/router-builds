fb9d6c5b6cab3eb37ba48f4c68ebba53274c6b13
Jason Aden <jasonaden@google.com>
2019-05-23 18:12:25 +0000

    21e8ac1 refactor(ivy): createNodeAtIndex no longer depends on L* (#30453)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+326.sha-132c61d.with-local-changes
+ * @license Angular v8.0.0-rc.0+320.sha-21e8ac1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8274,7 +8274,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+326.sha-132c61d.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+320.sha-21e8ac1.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
