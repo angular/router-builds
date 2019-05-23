61624017d32b2574d95f398a4df1864f754c1947
Jason Aden <jasonaden@google.com>
2019-05-23 18:07:54 +0000

    2cdbe9b refactor(ivy): ensure new attribute instructons are available in JIT (#30503)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+287.sha-66f269c.with-local-changes
+ * @license Angular v8.0.0-rc.0+311.sha-2cdbe9b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
