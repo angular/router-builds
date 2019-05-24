c3ce464e29c35433c35fbb16dce839bed38d7660
Jason Aden <jasonaden@google.com>
2019-05-24 05:34:51 +0000

    911a816 docs(core): add more info to static query guide (#30646)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+331.sha-2508872.with-local-changes
+ * @license Angular v8.0.0-rc.0+333.sha-911a816.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
