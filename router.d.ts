302cf158178dbcf4c2c7c4fb33a891b1b4f3857a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-05-24 10:46:21 +0000

    54ee5cd docs(core): static-query migration should use permalink for migration guide

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+336.sha-05479f8.with-local-changes
+ * @license Angular v8.0.0-rc.0+336.sha-54ee5cd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
