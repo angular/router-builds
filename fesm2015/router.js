ce8679c8780eb094d2117ec7710c75bd70eaf4c8
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 18:12:55 +0000

    be0bc79 docs: drop coding conventions from style guide (#30334)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+337.sha-0d23ced.with-local-changes
+ * @license Angular v8.0.0-rc.0+338.sha-be0bc79.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8274,7 +8274,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+337.sha-0d23ced.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+338.sha-be0bc79.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
