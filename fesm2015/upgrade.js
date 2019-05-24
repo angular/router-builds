c6e223d46d108cd76b9e19ceed632eb56d14e094
Jason Aden <jasonaden@google.com>
2019-05-24 05:44:50 +0000

    8b9a3e4 docs(core): add more info to static query guide (#30646)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.4+61.sha-063cc30.with-local-changes
+ * @license Angular v8.0.0-rc.4+63.sha-8b9a3e4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
