2e94b48a46686e38c96fdd37533f88630de05619
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 18:53:08 +0000

    dc6406e refactor(ivy): evaluate map-based styling bindings with a new algorithm (#30543)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+340.sha-68cd0ca.with-local-changes
+ * @license Angular v8.0.0-rc.0+343.sha-dc6406e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+340.sha-68cd0ca.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+343.sha-dc6406e.with-local-changes');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+340.sha-68cd0ca.with-local-changes
+ * @license Angular v8.0.0-rc.0+343.sha-dc6406e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
