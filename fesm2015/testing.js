13f4d386a3f41ef12bc9a1d80d7866f23939a14c
Igor Minar <igor@angular.io>
2019-05-24 15:33:12 +0000

    6bf5ec2 docs(core): static-query migration should use permalink for migration guide (#30649)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+334.sha-bfedfa7.with-local-changes
+ * @license Angular v8.0.0-rc.0+336.sha-6bf5ec2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
