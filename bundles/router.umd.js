8bee8bded13375d9d7080363fbebf73fa6a91ab9
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 18:54:23 +0000

    dc6406e refactor(ivy): evaluate map-based styling bindings with a new algorithm (#30543)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+340.sha-68cd0ca.with-local-changes
+ * @license Angular v8.0.0-rc.0+343.sha-dc6406e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+340.sha-68cd0ca.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+343.sha-dc6406e.with-local-changes');
 
     /**
      * @license
