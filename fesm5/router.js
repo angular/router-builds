42d1aabdd8b609d9d290a7e38e923202b54c7e4f
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 18:03:41 +0000

    0d23ced docs: add Michael Hladky to GDE resources (#30596)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+336.sha-6bf5ec2.with-local-changes
+ * @license Angular v8.0.0-rc.0+337.sha-0d23ced.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5718,7 +5718,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+336.sha-6bf5ec2.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+337.sha-0d23ced.with-local-changes');
 
 /**
  * @license
