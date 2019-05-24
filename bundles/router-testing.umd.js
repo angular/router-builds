a7e0e545359a8dec4b6cde1e950d358cdd4b0500
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 22:39:32 +0000

    573f57a build(docs-infra): upgrade cli command docs sources to 30036352e (#30608)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+346.sha-41cf066.with-local-changes
+ * @license Angular v8.0.0-rc.0+347.sha-573f57a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+346.sha-41cf066.with-local-changes
+ * @license Angular v8.0.0-rc.0+347.sha-573f57a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+346.sha-41cf066.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+347.sha-573f57a.with-local-changes');
 
     /**
      * @license
