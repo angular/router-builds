6b3b095695b6f933ba5727d0c19c661b61246bd8
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 18:21:53 +0000

    deb77bd feat(ivy): TestBed support for reusing non-exported components (#30578)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+341.sha-d5f96a8.with-local-changes
+ * @license Angular v8.0.0-rc.0+342.sha-deb77bd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
