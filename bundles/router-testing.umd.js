3408ec6cd9c34fd674d7ed1ac524bb2d333476b3
Igor Minar <igor@angular.io>
2019-05-28 17:02:15 +0000

    4761d40 docs: fix typo in a declaration alert (#30545)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.5+9.sha-002fbdd.with-local-changes
+ * @license Angular v8.0.0-rc.5+8.sha-4761d40.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.5+9.sha-002fbdd.with-local-changes
+ * @license Angular v8.0.0-rc.5+8.sha-4761d40.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.5+9.sha-002fbdd.with-local-changes');
+var VERSION = new Version('8.0.0-rc.5+8.sha-4761d40.with-local-changes');
 
 /**
  * @license
