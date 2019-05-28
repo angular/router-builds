889262cb0debe098f09c15cf9296aad0da404068
Rado Kirov <rkirov@gmail.com>
2019-05-28 20:54:50 +0000

    8a68ec0 docs: improve glossary for entry-point.

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+353.sha-1551920.with-local-changes
+ * @license Angular v8.0.0-rc.0+353.sha-8a68ec0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+353.sha-1551920.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+353.sha-8a68ec0.with-local-changes');
 
 /**
  * @license
