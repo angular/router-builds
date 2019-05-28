df583ebcd8b4ed7034effff4848f73f1d5932834
Igor Minar <igor@angular.io>
2019-05-28 16:58:33 +0000

    75cdda5 docs: fix typo in a declaration alert (#30545)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+350.sha-875eedb.with-local-changes
+ * @license Angular v8.0.0-rc.0+349.sha-75cdda5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
