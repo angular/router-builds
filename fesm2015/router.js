3806b6ac04aacdada25e3b0fd36204d2bfee4f9f
Igor Minar <igor@angular.io>
2019-05-28 17:02:41 +0000

    1b5096d docs: add missing preposition (#30691)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+349.sha-75cdda5.with-local-changes
+ * @license Angular v8.0.0-rc.0+351.sha-1b5096d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+349.sha-75cdda5.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+351.sha-1b5096d.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
