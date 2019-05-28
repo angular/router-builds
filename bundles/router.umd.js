bf5fceb97c2dcef98b138da174e8c289a13d65bf
Miško Hevery <misko@hevery.com>
2019-05-28 23:42:33 +0000

    51cb592 release: cut the v8.0.0 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+352.sha-a36caca.with-local-changes
+ * @license Angular v8.0.0-rc.0+353.sha-51cb592.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+352.sha-a36caca.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+353.sha-51cb592.with-local-changes');
 
     /**
      * @license
