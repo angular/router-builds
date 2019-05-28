1931ef033f2a3408cec86a50e5737ca66ba2475c
Rado Kirov <rkirov@gmail.com>
2019-05-28 18:15:28 +0000

    1551920 docs: improve glossary for entry-point.

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+353.sha-639eef1.with-local-changes
+ * @license Angular v8.0.0-rc.0+353.sha-1551920.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
