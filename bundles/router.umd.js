03900e8ae4d670937029a7844bdf52e4615f8383
Igor Minar <igor@angular.io>
2019-05-28 16:56:44 +0000

    875eedb docs: add default AOT to migrating projects to Ivy (#30697)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+347.sha-573f57a.with-local-changes
+ * @license Angular v8.0.0-rc.0+350.sha-875eedb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+347.sha-573f57a.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+350.sha-875eedb.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+347.sha-573f57a.with-local-changes
+ * @license Angular v8.0.0-rc.0+350.sha-875eedb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
