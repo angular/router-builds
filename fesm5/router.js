6dffd30998a3ad3ff797c4c50487be0e8dcd5bf1
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:37:28 +0000

    31aba96 docs: improve glossary for entry-point. (#29433)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+13.sha-facce2c.with-local-changes
+ * @license Angular v8.0.0+14.sha-31aba96.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0+13.sha-facce2c.with-local-changes');
+var VERSION = new Version('8.0.0+14.sha-31aba96.with-local-changes');
 
 /**
  * @license
