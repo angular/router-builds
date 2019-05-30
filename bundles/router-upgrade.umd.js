73cfdb914045befdc300bce84b905f3dabf2c8c0
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 05:26:22 +0000

    82682bb refactor(ivy): enable sanitization support for the new styling algorithm (#30667)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+356.sha-d72479b.with-local-changes
+ * @license Angular v8.0.0-rc.0+357.sha-82682bb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
