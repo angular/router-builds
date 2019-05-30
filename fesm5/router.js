15ff7af0dd1e59f1a959c1dd0afa704549be19d9
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:20:23 +0000

    0b23adc docs(common): fix typo in ngSwitch directive description (#30483)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+359.sha-7bf7244.with-local-changes
+ * @license Angular v8.0.0-rc.0+368.sha-0b23adc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+359.sha-7bf7244.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+368.sha-0b23adc.with-local-changes');
 
 /**
  * @license
