3ac8e386d2c5a2321bce5268c49e27752f8ccba0
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:29:45 +0000

    71be1da docs: fix deprecation information (#30717)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+373.sha-e3f3b10.with-local-changes
+ * @license Angular v8.0.0-rc.0+371.sha-71be1da.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
