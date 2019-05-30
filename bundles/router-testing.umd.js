39b30d62a29849e77cb8815f9499b66f62d9ae2f
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:28:39 +0000

    4bff2d0 docs: fix deprecation information (#30717)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+10.sha-cfc608a.with-local-changes
+ * @license Angular v8.0.0+11.sha-4bff2d0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+10.sha-cfc608a.with-local-changes
+ * @license Angular v8.0.0+11.sha-4bff2d0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0+10.sha-cfc608a.with-local-changes');
+    var VERSION = new core.Version('8.0.0+11.sha-4bff2d0.with-local-changes');
 
     /**
      * @license
