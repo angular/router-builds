e4d32d010d93fbc519f318d74a7ac63757a8f188
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:43:21 +0000

    06bdecf docs: remove mention of project settings from analytics (#30701)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+12.sha-edaf058.with-local-changes
+ * @license Angular v8.0.0+16.sha-06bdecf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0+12.sha-edaf058.with-local-changes');
+    var VERSION = new core.Version('8.0.0+16.sha-06bdecf.with-local-changes');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+12.sha-edaf058.with-local-changes
+ * @license Angular v8.0.0+16.sha-06bdecf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
