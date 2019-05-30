586280afab02cd6947e26035c5de17c78e78ad4a
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:21:39 +0000

    0b23adc docs(common): fix typo in ngSwitch directive description (#30483)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+359.sha-7bf7244.with-local-changes
+ * @license Angular v8.0.0-rc.0+368.sha-0b23adc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
