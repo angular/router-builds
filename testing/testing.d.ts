bf9d740535c31b382b6c10fddc49b492674c52f5
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:12:56 +0000

    7bf7244 docs(changelog): cleanup 8.0.0 release notes (#30740)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+357.sha-82682bb.with-local-changes
+ * @license Angular v8.0.0-rc.0+359.sha-7bf7244.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
