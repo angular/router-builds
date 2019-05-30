604c3443f2fec372b201876e21f289487172b927
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:33:47 +0000

    99ddec1 docs(ivy): fix symbol in example AST (#30745)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+11.sha-4bff2d0.with-local-changes
+ * @license Angular v8.0.0+15.sha-99ddec1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
