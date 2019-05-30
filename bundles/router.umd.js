57b02b6ff06811554d81a62b5a45aafe618f47d6
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 19:29:46 +0000

    5e0f982 feat(ivy): use i18n locale data to determine the plural form of ICU expressions (#29249)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+377.sha-f5b0c8a.with-local-changes
+ * @license Angular v8.0.0-rc.0+378.sha-5e0f982.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+377.sha-f5b0c8a.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+378.sha-5e0f982.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+377.sha-f5b0c8a.with-local-changes
+ * @license Angular v8.0.0-rc.0+378.sha-5e0f982.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
