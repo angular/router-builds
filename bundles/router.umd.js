941ac25b58e712964fc9f7aaed504f9ebd598a74
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:26:32 +0000

    62332b8 docs: fix typo in upgrade guide example (#30428)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+372.sha-fd23663.with-local-changes
+ * @license Angular v8.0.0-rc.0+374.sha-62332b8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+372.sha-fd23663.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+374.sha-62332b8.with-local-changes');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+372.sha-fd23663.with-local-changes
+ * @license Angular v8.0.0-rc.0+374.sha-62332b8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8274,7 +8274,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+372.sha-fd23663.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+374.sha-62332b8.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
