afd45fb20773bb95896641269f6a315e86db8669
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:23:09 +0000

    1a98d51 docs: change wording for more clarity in TypeScript Configuration guide (#30748)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+362.sha-24ca8ba.with-local-changes
+ * @license Angular v8.0.0-rc.0+358.sha-1a98d51.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+362.sha-24ca8ba.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+358.sha-1a98d51.with-local-changes');
 
 /**
  * @license
