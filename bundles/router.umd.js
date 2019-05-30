5e97c2ba4a888c82a8067e968db7a0dd740545d4
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 19:30:08 +0000

    7a0f8ac fix(ivy): generate explicit type annotation for NgModuleFactory calls in ngfactories (#30708)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+378.sha-5e0f982.with-local-changes
+ * @license Angular v8.0.0-rc.0+379.sha-7a0f8ac.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+378.sha-5e0f982.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+379.sha-7a0f8ac.with-local-changes');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+378.sha-5e0f982.with-local-changes
+ * @license Angular v8.0.0-rc.0+379.sha-7a0f8ac.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
