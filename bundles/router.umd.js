f13c08007a796aa27de917a866ee039cd26e4310
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 21:28:34 +0000

    19bbf4b test(ivy): fix sanitization state leaking issue

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+385.sha-53c6b78.with-local-changes
+ * @license Angular v8.0.0-rc.0+386.sha-19bbf4b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+385.sha-53c6b78.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+386.sha-19bbf4b.with-local-changes');
 
     /**
      * @license
