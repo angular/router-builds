07f7f5b21ecd924e1e86ab67525529b4c6741ec9
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:11:21 +0000

    9a9ae60 docs: code sample closing in ivy.md (#30711) PR Close #30711

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+3.sha-f8fa2f2.with-local-changes
+ * @license Angular v8.0.0+5.sha-9a9ae60.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
