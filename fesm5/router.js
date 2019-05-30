347dd306b2d3c3a1338745f3bf8a74a8718648fe
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:23:28 +0000

    cfc608a docs: merge duplicated platform-webworker content in Deprecation Guide (#30651)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+6.sha-1c67b90.with-local-changes
+ * @license Angular v8.0.0+10.sha-cfc608a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0+6.sha-1c67b90.with-local-changes');
+var VERSION = new Version('8.0.0+10.sha-cfc608a.with-local-changes');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+6.sha-1c67b90.with-local-changes
+ * @license Angular v8.0.0+10.sha-cfc608a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
