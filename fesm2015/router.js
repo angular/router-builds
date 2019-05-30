c8bfe33bf47fe8cc72f1a655c68a40963fd60553
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 20:17:27 +0000

    41f372f ci: fix payload limits value

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+379.sha-7a0f8ac.with-local-changes
+ * @license Angular v8.0.0-rc.0+383.sha-41f372f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8274,7 +8274,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+379.sha-7a0f8ac.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+383.sha-41f372f.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
