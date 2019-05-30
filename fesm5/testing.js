59188cb32d4e9eabefd0761ab5da5de857117ca0
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:26:19 +0000

    fd23663 docs: fix closing tags in deprecation summary (#30735)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+358.sha-1a98d51.with-local-changes
+ * @license Angular v8.0.0-rc.0+372.sha-fd23663.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
