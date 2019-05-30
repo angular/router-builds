10e1dd273ba8ec7105a39b9cea80b9818947822e
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:20:29 +0000

    7757826 docs: code sample closing in ivy.md (#30711) PR Close #30711

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+368.sha-0b23adc.with-local-changes
+ * @license Angular v8.0.0-rc.0+360.sha-7757826.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-rc.0+368.sha-0b23adc.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+360.sha-7757826.with-local-changes');
 
     /**
      * @license
