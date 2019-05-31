9563117072802a20e4bbd3c3288844e75ddb25ee
Miško Hevery <misko@hevery.com>
2019-05-31 17:08:47 +0000

    1f79c82 build(docs-infra): upgrade cli command docs sources to 2bbd7f288 (#30776)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+6.sha-e0c3662.with-local-changes
+ * @license Angular v8.1.0-beta.0+7.sha-1f79c82.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
