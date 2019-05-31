38f8d1c301368da748cb0c90fbe1a9d0cb0ee846
Miško Hevery <misko@hevery.com>
2019-05-31 22:48:36 +0000

    f88f941 docs: increase color contrast on elements in tutorial

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+22.sha-0e17958.with-local-changes
+ * @license Angular v8.0.0+23.sha-f88f941.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0+22.sha-0e17958.with-local-changes');
+var VERSION = new Version('8.0.0+23.sha-f88f941.with-local-changes');
 
 /**
  * @license
