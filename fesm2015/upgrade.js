566c5ebfdd8b45dc48b32d557c62d973fea0f718
Miško Hevery <misko@hevery.com>
2019-05-31 22:38:39 +0000

    aca339e fix(ivy): unable to project into multiple slots with default selector (#30561)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+376.sha-d2b0ac7.with-local-changes
+ * @license Angular v8.1.0-beta.0+10.sha-aca339e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+376.sha-d2b0ac7.with-local-changes
+ * @license Angular v8.1.0-beta.0+10.sha-aca339e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+376.sha-d2b0ac7.with-local-changes');
+var VERSION = new Version('8.1.0-beta.0+10.sha-aca339e.with-local-changes');
 
 /**
  * @license
