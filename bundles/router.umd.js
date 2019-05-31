bcdf350739acfbfc81fca0337d4f9354372e0626
Misko Hevery <misko@hevery.com>
2019-05-31 03:44:57 +0000

    09c57ec docs: add final release date and update future schedule (#30712)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0.with-local-changes
+ * @license Angular v8.1.0-beta.0+1.sha-09c57ec.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.1.0-beta.0.with-local-changes');
+    var VERSION = new core.Version('8.1.0-beta.0+1.sha-09c57ec.with-local-changes');
 
     /**
      * @license
