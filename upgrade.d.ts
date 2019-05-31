7d33576d42f896b5bbefefbad71a958a79445763
Miško Hevery <misko@hevery.com>
2019-05-31 17:13:48 +0000

    f4cd374 fix(docs-infra): update tooltip of events link (#30643)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+7.sha-1f79c82.with-local-changes
+ * @license Angular v8.1.0-beta.0+9.sha-f4cd374.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
