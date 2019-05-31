9ca09aa059338b74f47cd418f67d1efeb3aa5aee
Miško Hevery <misko@hevery.com>
2019-05-31 17:04:32 +0000

    cdf586d build(docs-infra): upgrade cli command docs sources to e8bdfe6b9 (#30777)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+17.sha-0f352b6.with-local-changes
+ * @license Angular v8.0.0+19.sha-cdf586d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0+17.sha-0f352b6.with-local-changes');
+var VERSION = new Version('8.0.0+19.sha-cdf586d.with-local-changes');
 
 /**
  * @license
