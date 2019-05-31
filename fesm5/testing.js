2b2aa72fe2bb7a3f4bcba1ee6341cb33d710cab5
Miško Hevery <misko@hevery.com>
2019-05-31 22:51:12 +0000

    81b7559 docs(docs-infra): compile module first for Ivy when loading element modules (#30704)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+23.sha-f88f941.with-local-changes
+ * @license Angular v8.0.0+25.sha-81b7559.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
