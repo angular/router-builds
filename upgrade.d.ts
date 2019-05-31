d84217fea81f46cbe0265afd5a7ba505feea995d
Miško Hevery <misko@hevery.com>
2019-05-31 22:50:17 +0000

    32886cf docs(docs-infra): compile module first for Ivy when loading element modules (#30704)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+13.sha-8ce288a.with-local-changes
+ * @license Angular v8.1.0-beta.0+16.sha-32886cf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
