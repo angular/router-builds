147f58909295c368634e9fd66c3b41628f3600a8
Miško Hevery <misko@hevery.com>
2019-05-31 22:45:14 +0000

    8ce288a fix(docs-infra): add v7 in versions dropdown (#30775)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+12.sha-f936590.with-local-changes
+ * @license Angular v8.1.0-beta.0+13.sha-8ce288a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.1.0-beta.0+12.sha-f936590.with-local-changes');
+    var VERSION = new i0.Version('8.1.0-beta.0+13.sha-8ce288a.with-local-changes');
 
     /**
      * @license
