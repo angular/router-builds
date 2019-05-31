1eaa5b622b326117c10a70316f632d00256c8b03
Miško Hevery <misko@hevery.com>
2019-05-31 17:20:44 +0000

    aca339e fix(ivy): unable to project into multiple slots with default selector (#30561)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+8.sha-6d861f2.with-local-changes
+ * @license Angular v8.1.0-beta.0+10.sha-aca339e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
