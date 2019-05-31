2c93b2a0445de1965be486a6fda47014fa310533
Miško Hevery <misko@hevery.com>
2019-05-31 17:23:45 +0000

    e82b45c docs(docs-infra): remove deprecated `Buffer` usage from stackblitz builder (#30369)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+20.sha-8422ef2.with-local-changes
+ * @license Angular v8.0.0+21.sha-e82b45c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0+20.sha-8422ef2.with-local-changes');
+const VERSION = new Version('8.0.0+21.sha-e82b45c.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+20.sha-8422ef2.with-local-changes
+ * @license Angular v8.0.0+21.sha-e82b45c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
