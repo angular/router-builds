1547ecfef031d73a0d1cfca98747b1d4f3054a15
Miško Hevery <misko@hevery.com>
2019-05-31 17:17:12 +0000

    6d861f2 fix(ivy): module with providers are processed too early (#30688)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+9.sha-f4cd374.with-local-changes
+ * @license Angular v8.1.0-beta.0+8.sha-6d861f2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
