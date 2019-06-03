d3561f8b2934bef1e27abe246aed1f64fa3add14
Misko Hevery <misko@hevery.com>
2019-06-03 16:31:47 +0000

    dd8cf19 docs: fix a small typo in injectable decorator description (#30785)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+19.sha-21328f2.with-local-changes
+ * @license Angular v8.1.0-beta.0+18.sha-dd8cf19.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.1.0-beta.0+19.sha-21328f2.with-local-changes');
+    var VERSION = new i0.Version('8.1.0-beta.0+18.sha-dd8cf19.with-local-changes');
 
     /**
      * @license
