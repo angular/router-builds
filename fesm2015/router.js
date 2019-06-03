f723268b19b0f218b1319e982ba5a6815d871de1
Misko Hevery <misko@hevery.com>
2019-06-03 16:28:30 +0000

    8bedf50 docs: update example references from "my-app" to "app-root" (#30789)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+16.sha-32886cf.with-local-changes
+ * @license Angular v8.1.0-beta.0+17.sha-8bedf50.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8274,7 +8274,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-beta.0+16.sha-32886cf.with-local-changes');
+const VERSION = new Version('8.1.0-beta.0+17.sha-8bedf50.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
