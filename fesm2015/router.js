c211d4ef61dc583790373ca6f636f2b57d15f7aa
Misko Hevery <misko@hevery.com>
2019-06-03 16:39:33 +0000

    a981dd2 docs(core): fix typo in style bindings comment (#30661)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+21.sha-b4e6802.with-local-changes
+ * @license Angular v8.1.0-beta.0+22.sha-a981dd2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-beta.0+21.sha-b4e6802.with-local-changes');
+const VERSION = new Version('8.1.0-beta.0+22.sha-a981dd2.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
