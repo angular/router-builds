a43e20f238116f85116455bd2adbacf71dfb3127
Misko Hevery <misko@hevery.com>
2019-06-03 16:34:35 +0000

    393529d docs: update example references from "my-app" to "app-root" (#30789)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+27.sha-9e4b2f1.with-local-changes
+ * @license Angular v8.0.0+26.sha-393529d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
