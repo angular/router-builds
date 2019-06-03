5983cecc0b7b83088f8bb7f4fe9dc814207beec6
Misko Hevery <misko@hevery.com>
2019-06-03 19:31:20 +0000

    fcdd784 refactor(core): cleanup code with side-effects which was preventing tree-shaking (#30580)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+12.sha-f936590.with-local-changes
+ * @license Angular v8.1.0-beta.0+23.sha-fcdd784.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
