7da6e37b16e439a0ad6887534746d731d0d74421
Misko Hevery <misko@hevery.com>
2019-06-03 19:14:23 +0000

    cd6fc8b docs: document UrlTree in guard return type in cheatsheet (#30822)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+23.sha-fcdd784.with-local-changes
+ * @license Angular v8.1.0-beta.0+24.sha-cd6fc8b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
