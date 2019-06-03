afea332e202ec7ab2e6b2aa55d21411e6decadd8
Misko Hevery <misko@hevery.com>
2019-06-03 16:36:28 +0000

    680d385 fix(ivy): correctly project bare ICU expressions (#30696)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+18.sha-dd8cf19.with-local-changes
+ * @license Angular v8.1.0-beta.0+20.sha-680d385.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5718,7 +5718,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-beta.0+18.sha-dd8cf19.with-local-changes');
+var VERSION = new Version('8.1.0-beta.0+20.sha-680d385.with-local-changes');
 
 /**
  * @license
