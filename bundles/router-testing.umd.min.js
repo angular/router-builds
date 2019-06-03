63fcb5f18872f2ea43b42d5bd1ad533b6b918c91
Misko Hevery <misko@hevery.com>
2019-06-03 16:27:10 +0000

    8bedf50 docs: update example references from "my-app" to "app-root" (#30789)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+16.sha-32886cf.with-local-changes
+ * @license Angular v8.1.0-beta.0+17.sha-8bedf50.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+16.sha-32886cf.with-local-changes
+ * @license Angular v8.1.0-beta.0+17.sha-8bedf50.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.1.0-beta.0+16.sha-32886cf.with-local-changes');
+    var VERSION = new core.Version('8.1.0-beta.0+17.sha-8bedf50.with-local-changes');
 
     /**
      * @license
