0033cfd99e30f4f8af9a4b6d01bff1d00fe0a2b6
Misko Hevery <misko@hevery.com>
2019-06-03 19:29:13 +0000

    07cd65b docs: fix `quickstart` link in readme (#30807)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+24.sha-cd6fc8b.with-local-changes
+ * @license Angular v8.1.0-beta.0+25.sha-07cd65b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
