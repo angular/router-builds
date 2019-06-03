c68cc17b01055dafd78b171c72071f04a2280743
Misko Hevery <misko@hevery.com>
2019-06-03 16:32:09 +0000

    9e4b2f1 docs: fix a small typo in injectable decorator description (#30785)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+25.sha-81b7559.with-local-changes
+ * @license Angular v8.0.0+27.sha-9e4b2f1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0+25.sha-81b7559.with-local-changes');
+const VERSION = new Version('8.0.0+27.sha-9e4b2f1.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
