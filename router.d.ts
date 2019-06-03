d1eaa29e4d295505fe6d42573a0d527d0f697b97
Misko Hevery <misko@hevery.com>
2019-06-03 16:35:11 +0000

    680d385 fix(ivy): correctly project bare ICU expressions (#30696)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+18.sha-dd8cf19.with-local-changes
+ * @license Angular v8.1.0-beta.0+20.sha-680d385.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
