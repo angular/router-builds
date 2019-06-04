9d9b3908f8ec70e19a9ea576251386dce20b560f
Misko Hevery <misko@hevery.com>
2019-06-04 19:31:57 +0000

    b74df20 docs(service-worker): mention that HTTPS is required, unless on `localhost` (#30847)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+33.sha-4ecff42.with-local-changes
+ * @license Angular v8.1.0-beta.0+34.sha-b74df20.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.1.0-beta.0+33.sha-4ecff42.with-local-changes');
+    var VERSION = new i0.Version('8.1.0-beta.0+34.sha-b74df20.with-local-changes');
 
     /**
      * @license
