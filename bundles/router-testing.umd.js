58d0da68ac9fcafe39e6487f205ebc16d4042f6d
Misko Hevery <misko@hevery.com>
2019-06-04 21:13:21 +0000

    ea2adb1 docs: correct typos and formatting in the Getting Started guide (#30758)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+34.sha-ccc1c27.with-local-changes
+ * @license Angular v8.0.0+36.sha-ea2adb1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
