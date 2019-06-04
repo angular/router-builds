e41548067962e2c07518bccf350905517804a9f6
Misko Hevery <misko@hevery.com>
2019-06-04 19:23:14 +0000

    7c41abe docs: rename component for constructor example (#30845) PR Close #30845

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+33.sha-97268b9.with-local-changes
+ * @license Angular v8.0.0+32.sha-7c41abe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+33.sha-97268b9.with-local-changes
+ * @license Angular v8.0.0+32.sha-7c41abe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0+33.sha-97268b9.with-local-changes');
+var VERSION = new Version('8.0.0+32.sha-7c41abe.with-local-changes');
 
 /**
  * @license
