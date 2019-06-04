35f2b17845b6584514bafbfad3c3da6731c69e13
Misko Hevery <misko@hevery.com>
2019-06-04 19:17:26 +0000

    3ed7463 docs: rename component for constructor example (#30845) PR Close #30845

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+28.sha-812c231.with-local-changes
+ * @license Angular v8.1.0-beta.0+27.sha-3ed7463.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.1.0-beta.0+28.sha-812c231.with-local-changes');
+    var VERSION = new i0.Version('8.1.0-beta.0+27.sha-3ed7463.with-local-changes');
 
     /**
      * @license
