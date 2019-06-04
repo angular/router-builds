e198fcf59b8bf72932a80be8847c1a6c331e3b3b
Misko Hevery <misko@hevery.com>
2019-06-04 19:30:31 +0000

    b74df20 docs(service-worker): mention that HTTPS is required, unless on `localhost` (#30847)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+33.sha-4ecff42.with-local-changes
+ * @license Angular v8.1.0-beta.0+34.sha-b74df20.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
