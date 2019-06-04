011cbd0fcdb83684e322c4e2ee81a0b629854db3
Misko Hevery <misko@hevery.com>
2019-06-04 19:37:50 +0000

    ccc1c27 refactor: Move away from index signature based "updateCache". (#30518)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+35.sha-19caace.with-local-changes
+ * @license Angular v8.0.0+34.sha-ccc1c27.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+35.sha-19caace.with-local-changes
+ * @license Angular v8.0.0+34.sha-ccc1c27.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0+35.sha-19caace.with-local-changes');
+const VERSION = new Version('8.0.0+34.sha-ccc1c27.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
