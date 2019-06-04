67da9fbd2515461eb6be4195e04e9a307a760557
Misko Hevery <misko@hevery.com>
2019-06-04 19:15:53 +0000

    97268b9 docs: update example dependencies to 8.x release (#30755)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+29.sha-06ffed5.with-local-changes
+ * @license Angular v8.0.0+33.sha-97268b9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
