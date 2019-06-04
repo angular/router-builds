33a460a11c83d806de1b500eb688377f64df9bd9
Misko Hevery <misko@hevery.com>
2019-06-04 19:24:30 +0000

    456f2e7 fix(ivy): type checking - handle $implicit ng-template variables (#30675)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+32.sha-55a14e4.with-local-changes
+ * @license Angular v8.1.0-beta.0+30.sha-456f2e7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
