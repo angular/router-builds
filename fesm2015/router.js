17fbb8f64488a4143a8ca7e620395d55d4beea80
Miško Hevery <misko@hevery.com>
2019-06-05 17:19:29 +0000

    d1df0a9 refactor(ivy): remove ɵɵelementProperty instruction (#30645)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+35.sha-0d4f8c7.with-local-changes
+ * @license Angular v8.1.0-beta.0+39.sha-d1df0a9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-beta.0+35.sha-0d4f8c7.with-local-changes');
+const VERSION = new Version('8.1.0-beta.0+39.sha-d1df0a9.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
