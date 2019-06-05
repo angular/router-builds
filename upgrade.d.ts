bda6695c5d5c556b9412e5050b973be9af50daaa
Jeremy Elbourn <jelbourn@google.com>
2019-06-05 17:34:07 +0000

    95ad7f1 fix(bazel): exclude components schematics from build

---
+++ upgrade.d.ts
@@ -0,0 +1,7 @@
+/**
+ * @license Angular v8.1.0-beta.0+24.sha-95ad7f1.with-local-changes
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+export * from './upgrade/upgrade';
