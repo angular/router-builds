b7bb854837ea4ecff31b95d83ab04eaebbe4bfe1
Miško Hevery <misko@hevery.com>
2019-06-05 16:40:52 +0000

    00cc905 feat(ivy): support `ng-content` in runtime i18n translations (#30782)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+39.sha-d1df0a9.with-local-changes
+ * @license Angular v8.1.0-beta.0+38.sha-00cc905.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
