a90e9ac9971659725db608afc9cef160fcb06989
Miško Hevery <misko@hevery.com>
2019-06-05 16:27:22 +0000

    337b6fe build: remove unreferenced tsconfig-build.json files (#30858)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+34.sha-b74df20.with-local-changes
+ * @license Angular v8.1.0-beta.0+37.sha-337b6fe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
