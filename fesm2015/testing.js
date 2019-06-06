c84f6899a966798cec6795cc1557ce75b708a61e
Misko Hevery <misko@hevery.com>
2019-06-06 04:52:23 +0000

    05a43ca fix(bazel): exclude components schematics from build (#30825)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+5.sha-30efb6b.with-local-changes
+ * @license Angular v8.1.0-next.1+4.sha-05a43ca.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
