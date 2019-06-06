128ee8f171728ca9e70690c52ca5b3983a020048
Misko Hevery <misko@hevery.com>
2019-06-06 04:50:38 +0000

    17bfedd fix(bazel): Load global stylesheet in dev and prod (#30879)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+3.sha-b51d8dd.with-local-changes
+ * @license Angular v8.1.0-next.1+1.sha-17bfedd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
