74995305df1889cf9740fde6136719d4af561c7e
Misko Hevery <misko@hevery.com>
2019-06-06 04:51:29 +0000

    65f2010 fix(ivy): ngcc - use spaces in overwritten `package.json` content for readability (#30831)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+38.sha-5a7bcd1.with-local-changes
+ * @license Angular v8.0.0+39.sha-65f2010.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
