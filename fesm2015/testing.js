c52d7e498cd0c396d55711d89961c97690c224fe
Misko Hevery <misko@hevery.com>
2019-06-06 04:49:44 +0000

    b51d8dd fix(ivy): error for empty bindings on ng-template (#30829)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+2.sha-ea2d453.with-local-changes
+ * @license Angular v8.1.0-next.1+3.sha-b51d8dd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
