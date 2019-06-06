3a21be166b323857fb6e4cc376ed236bc8afaeaa
Miško Hevery <misko@hevery.com>
2019-06-06 16:11:26 +0000

    f440bd1 build(docs-infra): fix CLI command github links (#30889)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+6.sha-3859bcc.with-local-changes
+ * @license Angular v8.1.0-next.1+7.sha-f440bd1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.1.0-next.1+6.sha-3859bcc.with-local-changes');
+    var VERSION = new core.Version('8.1.0-next.1+7.sha-f440bd1.with-local-changes');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+6.sha-3859bcc.with-local-changes
+ * @license Angular v8.1.0-next.1+7.sha-f440bd1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
