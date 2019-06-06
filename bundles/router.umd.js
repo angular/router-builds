3729402c0d336a10812b356c6d6f22d2854b79b0
Misko Hevery <misko@hevery.com>
2019-06-06 04:50:58 +0000

    30efb6b fix(ivy): don't project removed placeholders with runtime i18n (#30783)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+1.sha-17bfedd.with-local-changes
+ * @license Angular v8.1.0-next.1+5.sha-30efb6b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.1.0-next.1+1.sha-17bfedd.with-local-changes');
+    var VERSION = new core.Version('8.1.0-next.1+5.sha-30efb6b.with-local-changes');
 
     /**
      * @license
