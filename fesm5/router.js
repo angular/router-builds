7394c219ea3030969484c252c6d66cbe7babea0b
Misko Hevery <misko@hevery.com>
2019-06-06 04:47:45 +0000

    ea2d453 fix(ivy): ngcc - use spaces in overwritten `package.json` content for readability (#30831)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1.with-local-changes
+ * @license Angular v8.1.0-next.1+2.sha-ea2d453.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.1.with-local-changes');
+var VERSION = new Version('8.1.0-next.1+2.sha-ea2d453.with-local-changes');
 
 /**
  * @license
