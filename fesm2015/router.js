50fc93c06f8cdcf41685ee690050bbdd2aca0df1
Misko Hevery <misko@hevery.com>
2019-06-07 16:20:00 +0000

    08c38a1 fix(service-worker): avoid uncaught rejection warning when registration fails (#30876)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+48.sha-9ace748.with-local-changes
+ * @license Angular v8.0.0+47.sha-08c38a1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0+48.sha-9ace748.with-local-changes');
+const VERSION = new Version('8.0.0+47.sha-08c38a1.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
