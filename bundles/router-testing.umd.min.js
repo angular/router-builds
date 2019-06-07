d07dbc380117f12896aa4ce52dd25b99107d24f5
Misko Hevery <misko@hevery.com>
2019-06-07 16:14:43 +0000

    8b6fbf8 build(docs-infra): remove unneeded `allowEmptyCodegenFiles` option in Ivy mode (#30911)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+9.sha-04587a3.with-local-changes
+ * @license Angular v8.1.0-next.1+12.sha-8b6fbf8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
