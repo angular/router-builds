671c494c855147da42e93b7600d231742c2e508f
Misko Hevery <misko@hevery.com>
2019-06-07 16:28:35 +0000

    5c18f23 fix(common): expose the `HttpUploadProgressEvent` interface as public API (#30852)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+14.sha-297222f.with-local-changes
+ * @license Angular v8.1.0-next.1+17.sha-5c18f23.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
