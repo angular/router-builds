1d65ae83b04ade8141c0759a0da3b2cd350f040a
Misko Hevery <misko@hevery.com>
2019-06-07 16:21:38 +0000

    297222f build(docs-infra): fail hard if the CLI source is not what we expect (#30901)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+18.sha-c038675.with-local-changes
+ * @license Angular v8.1.0-next.1+14.sha-297222f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
