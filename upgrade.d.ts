6ca07c43c42819eaad49d3143dea91c13d917d27
Misko Hevery <misko@hevery.com>
2019-06-07 17:05:21 +0000

    c038675 refactor(ivy): inherently call ɵɵselect(0) (#30830)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+10.sha-11a4454.with-local-changes
+ * @license Angular v8.1.0-next.1+18.sha-c038675.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
