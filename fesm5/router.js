8d670aa5cf7e6f5042b3b7ee547b42555cd7d39f
Misko Hevery <misko@hevery.com>
2019-06-07 16:18:00 +0000

    9ace748 docs: add dev.to link to the help section (#30873)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+45.sha-8aef446.with-local-changes
+ * @license Angular v8.0.0+48.sha-9ace748.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0+45.sha-8aef446.with-local-changes');
+var VERSION = new Version('8.0.0+48.sha-9ace748.with-local-changes');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+45.sha-8aef446.with-local-changes
+ * @license Angular v8.0.0+48.sha-9ace748.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
