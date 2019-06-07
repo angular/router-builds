c73ba577cdaf88a9e3e43fc41bc898ff432c1ff7
Misko Hevery <misko@hevery.com>
2019-06-07 16:23:32 +0000

    4e8614b fix(common): expose the `HttpUploadProgressEvent` interface as public API (#30852)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+47.sha-08c38a1.with-local-changes
+ * @license Angular v8.0.0+49.sha-4e8614b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+47.sha-08c38a1.with-local-changes
+ * @license Angular v8.0.0+49.sha-4e8614b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0+47.sha-08c38a1.with-local-changes');
+    var VERSION = new core.Version('8.0.0+49.sha-4e8614b.with-local-changes');
 
     /**
      * @license
