5c3321d6616ca5e609c0af48cbff786f66a7dae1
JiaLiPassion <JiaLi.Passion@gmail.com>
2019-06-09 14:40:56 +0000

    99331c0 test: remove rsync

---
+++ testing.d.ts
@@ -0,0 +1,7 @@
+/**
+ * @license Angular v8.1.0-beta.0+32.sha-99331c0.with-local-changes
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+export * from './testing/testing';
