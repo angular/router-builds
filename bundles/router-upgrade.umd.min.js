2d3a30a0dfd106b19e104908d983ba77d9cc99be
JiaLiPassion <JiaLi.Passion@gmail.com>
2019-06-10 19:15:39 +0000

    4f1008b test: modify for windows

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+32.sha-99331c0.with-local-changes
+ * @license Angular v8.1.0-beta.0+33.sha-4f1008b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
