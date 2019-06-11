ed5d8381947d417faa513b8e4f9ad8f1ac24fa33
Igor Minar <igor@angular.io>
2019-06-11 00:20:58 +0000

    6a48485 build: update rules_nodejs and clean up bazel warnings (#30370) (#30763)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+49.sha-4e8614b.with-local-changes
+ * @license Angular v8.0.0+51.sha-6a48485.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+49.sha-4e8614b.with-local-changes
+ * @license Angular v8.0.0+51.sha-6a48485.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0+49.sha-4e8614b.with-local-changes');
+var VERSION = new Version('8.0.0+51.sha-6a48485.with-local-changes');
 
 /**
  * @license
