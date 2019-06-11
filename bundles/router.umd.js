956e7776da3d2b2f811c5d2f12c932829aa43c9f
Igor Minar <igor@angular.io>
2019-06-11 00:36:23 +0000

    dcf9f05 docs: use `Node.js` consistently (#30934) PR Close #30934

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+51.sha-6a48485.with-local-changes
+ * @license Angular v8.0.0+55.sha-dcf9f05.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0+51.sha-6a48485.with-local-changes');
+    var VERSION = new core.Version('8.0.0+55.sha-dcf9f05.with-local-changes');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+51.sha-6a48485.with-local-changes
+ * @license Angular v8.0.0+55.sha-dcf9f05.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0+51.sha-6a48485.with-local-changes');
+var VERSION = new Version('8.0.0+55.sha-dcf9f05.with-local-changes');
 
 /**
  * @license
