006fbeba4b54cb2e985bc2a6259c0ab21ed835a2
Andrew Kushnir <akushnir@google.com>
2019-06-11 23:02:00 +0000

    49307f0 fix(bazel): do not modify tsconfig.json (#30984)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+63.sha-762fc28.with-local-changes
+ * @license Angular v8.0.0+64.sha-49307f0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
