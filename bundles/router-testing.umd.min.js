36b3ec0858c2e954fd0ea0d42f1902db4be60f86
Andrew Kushnir <akushnir@google.com>
2019-06-11 22:35:31 +0000

    052ef65 test(compiler-cli): fix failing ivy template compliance test (#30983)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+47.sha-7912db3.with-local-changes
+ * @license Angular v8.1.0-next.1+49.sha-052ef65.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
