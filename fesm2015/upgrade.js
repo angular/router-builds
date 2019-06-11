530288ef8689a9ba1ee2c6dcde8ea2692f7329b3
Andrew Kushnir <akushnir@google.com>
2019-06-11 21:51:20 +0000

    7912db3 fix(ivy): call factory functions with correct type for derived classes (#30855)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+43.sha-b086676.with-local-changes
+ * @license Angular v8.1.0-next.1+47.sha-7912db3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
