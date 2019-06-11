4b404594b8300321ddb7620c4d95ae930ffc8d06
Igor Minar <igor@angular.io>
2019-06-11 00:36:43 +0000

    fe02462 build(docs-infra): update @angular/* to v8.1.0-next.1 and @angular/material to v8.0.0 (#30935)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+55.sha-dcf9f05.with-local-changes
+ * @license Angular v8.0.0+57.sha-fe02462.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
