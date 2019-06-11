47d91b7d6599486a4d31ef0f6b60c3da80fd9f9c
Andrew Kushnir <akushnir@google.com>
2019-06-11 21:42:11 +0000

    bb635c0 docs: use const in dynamic-component-loader example (#30888)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+39.sha-87bf179.with-local-changes
+ * @license Angular v8.1.0-next.1+41.sha-bb635c0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
