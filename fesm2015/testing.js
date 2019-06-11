150d42544d279c05e34868510440ad8370212e11
Andrew Kushnir <akushnir@google.com>
2019-06-11 16:59:24 +0000

    8c4bd61 test(core): add missing static flag to view_insertion test view queries (#30967)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+17.sha-5c18f23.with-local-changes
+ * @license Angular v8.1.0-next.1+38.sha-8c4bd61.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
