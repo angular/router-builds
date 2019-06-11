36835f66236b9f53a64f47706c328d61d4c36133
Andrew Kushnir <akushnir@google.com>
2019-06-11 21:44:21 +0000

    699283c feat(docs-infra): layout ui polish (#30883)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+40.sha-c15e675.with-local-changes
+ * @license Angular v8.1.0-next.1+42.sha-699283c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
