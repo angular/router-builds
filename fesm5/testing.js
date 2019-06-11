af5c84790136d35c88de332959ac3f772ea581d3
Igor Minar <igor@angular.io>
2019-06-11 00:27:47 +0000

    41e56c9 fixup! refactor(docs-infra): make archive redirection tests DRY

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.15+1.sha-b62a9d0.with-local-changes
+ * @license Angular v7.2.15+5.sha-41e56c9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
