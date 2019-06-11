ed58b2a4bbbcd166e31bc88361f2d9c473dcc67e
Andrew Kushnir <akushnir@google.com>
2019-06-11 18:15:34 +0000

    501bacd build: update golden for //packages/core/test/bundling/injection:symbol_test (#30965)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+57.sha-fe02462.with-local-changes
+ * @license Angular v8.0.0+59.sha-501bacd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0+57.sha-fe02462.with-local-changes');
+const VERSION = new Version('8.0.0+59.sha-501bacd.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
