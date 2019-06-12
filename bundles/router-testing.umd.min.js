92b66767aad9413a69340aa0129c1478313916b3
Igor Minar <igor@angular.io>
2019-06-12 16:45:05 +0000

    aa9eb55 build(docs-infra): upgrade cli command docs sources to 032be4e17 (#30998)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+49.sha-052ef65.with-local-changes
+ * @license Angular v8.1.0-next.1+50.sha-aa9eb55.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
