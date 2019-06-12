d1c9812917fec0fdc2e8b70cb31c2e8282838136
Andrew Kushnir <akushnir@google.com>
2019-06-12 19:05:37 +0000

    4ca401c build(docs-infra): upgrade cli command docs sources to 7da10691d (#30999)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+65.sha-3dcd5eb.with-local-changes
+ * @license Angular v8.0.0+66.sha-4ca401c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0+65.sha-3dcd5eb.with-local-changes');
+    var VERSION = new core.Version('8.0.0+66.sha-4ca401c.with-local-changes');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+65.sha-3dcd5eb.with-local-changes
+ * @license Angular v8.0.0+66.sha-4ca401c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
