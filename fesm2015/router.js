6b05aad4f604de5b9e7fc564fe46a4f4a1869861
Andrew Kushnir <akushnir@google.com>
2019-06-12 19:13:12 +0000

    82e0b4a fix(docs-infra): enable only vertical scrolling for contributors bio (#30991)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+51.sha-574990e.with-local-changes
+ * @license Angular v8.1.0-next.1+52.sha-82e0b4a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8274,7 +8274,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.1+51.sha-574990e.with-local-changes');
+const VERSION = new Version('8.1.0-next.1+52.sha-82e0b4a.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
