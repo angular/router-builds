e12e5b4835486ec06fb70b737def86f8098229a9
Andrew Kushnir <akushnir@google.com>
2019-06-14 19:22:17 +0000

    e76690b docs: edit codeowners for new Template Syntax examples (#31060)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+19.sha-f41242f.with-local-changes
+ * @license Angular v8.1.0-next.2+21.sha-e76690b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
