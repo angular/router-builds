ee4c73fb7aa9960b2462b84bfe59b6dd3fc71cac
Andrew Kushnir <akushnir@google.com>
2019-06-14 16:40:56 +0000

    48d11d5 ci: temporarily disable payload size tracking (#31057)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+52.sha-82e0b4a.with-local-changes
+ * @license Angular v8.1.0-next.2+4.sha-48d11d5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
