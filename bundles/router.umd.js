46f8d6ab1e778b7e9859918e460c4a0845926104
Andrew Kushnir <akushnir@google.com>
2019-06-14 19:22:47 +0000

    f41242f docs: rewrite inputs/outputs section of Template Syntax (#27685)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+18.sha-e2fd628.with-local-changes
+ * @license Angular v8.1.0-next.2+19.sha-f41242f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.1.0-next.2+18.sha-e2fd628.with-local-changes');
+    var VERSION = new i0.Version('8.1.0-next.2+19.sha-f41242f.with-local-changes');
 
     /**
      * @license
