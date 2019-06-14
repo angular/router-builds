32d6d0f685caacfb1c2f1cea70fd53bf06279055
Andrew Kushnir <akushnir@google.com>
2019-06-14 20:37:37 +0000

    6fbfb5a feat(ivy): ngcc - recognize static properties on the outer symbol in ES5 (#30795)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+23.sha-16aa6ce.with-local-changes
+ * @license Angular v8.1.0-next.2+24.sha-6fbfb5a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.2+23.sha-16aa6ce.with-local-changes');
+var VERSION = new Version('8.1.0-next.2+24.sha-6fbfb5a.with-local-changes');
 
 /**
  * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+23.sha-16aa6ce.with-local-changes
+ * @license Angular v8.1.0-next.2+24.sha-6fbfb5a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
