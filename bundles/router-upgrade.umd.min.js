4be13645551c9bc1459a6d39b365af527219f943
Andrew Kushnir <akushnir@google.com>
2019-06-14 23:24:37 +0000

    ce7131f ci(docs-infra): disable payload size tracking for `test_aio_local` and `test_aio_local_ivy` (#31064)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+18.sha-2abb54c.with-local-changes
+ * @license Angular v8.0.1+21.sha-ce7131f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
