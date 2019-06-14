49a670b70a03673adbec5c63df610cc1933c7195
Andrew Kushnir <akushnir@google.com>
2019-06-14 18:07:38 +0000

    cc0bd11 build(docs-infra): update `browserlist` configuration file (#31045)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+6.sha-6bc9c78.with-local-changes
+ * @license Angular v8.1.0-next.2+9.sha-cc0bd11.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
