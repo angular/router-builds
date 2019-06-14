e4bd386d194b717cdd18d144cd535ad4b04b84f2
Andrew Kushnir <akushnir@google.com>
2019-06-14 19:22:10 +0000

    e2fd628 docs: edit template ref vars copy and example (#27371)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+17.sha-2c2135d.with-local-changes
+ * @license Angular v8.1.0-next.2+18.sha-e2fd628.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.1.0-next.2+17.sha-2c2135d.with-local-changes');
+    var VERSION = new i0.Version('8.1.0-next.2+18.sha-e2fd628.with-local-changes');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+17.sha-2c2135d.with-local-changes
+ * @license Angular v8.1.0-next.2+18.sha-e2fd628.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8274,7 +8274,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.2+17.sha-2c2135d.with-local-changes');
+const VERSION = new Version('8.1.0-next.2+18.sha-e2fd628.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
