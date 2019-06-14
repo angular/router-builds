1a147dd01e651cd8edd9778a5eef5b81047fb2ad
Andrew Kushnir <akushnir@google.com>
2019-06-14 18:10:14 +0000

    0c3bb6a fix(ivy): ngcc - capture entry-points in top-level path-mapped folders (#31027)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+8.sha-5d24a75.with-local-changes
+ * @license Angular v8.1.0-next.2+10.sha-0c3bb6a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+8.sha-5d24a75.with-local-changes
+ * @license Angular v8.1.0-next.2+10.sha-0c3bb6a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.2+8.sha-5d24a75.with-local-changes');
+var VERSION = new Version('8.1.0-next.2+10.sha-0c3bb6a.with-local-changes');
 
 /**
  * @license
