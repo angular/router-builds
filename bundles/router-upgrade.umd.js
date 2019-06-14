765af33c66989eb526418df55ee1466144ea55d2
Andrew Kushnir <akushnir@google.com>
2019-06-14 18:08:00 +0000

    5d24a75 docs: minor fix in `get-commit-range.js` docs (#31049)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+7.sha-799d1fd.with-local-changes
+ * @license Angular v8.1.0-next.2+8.sha-5d24a75.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
