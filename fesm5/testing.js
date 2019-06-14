2236a34ac110ee21bf621c38a2fd4b26daa3525c
Andrew Kushnir <akushnir@google.com>
2019-06-14 16:55:35 +0000

    3bbc89b ci: temporarily disable payload size tracking (#31057)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+68.sha-cbb3794.with-local-changes
+ * @license Angular v8.0.1+4.sha-3bbc89b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
