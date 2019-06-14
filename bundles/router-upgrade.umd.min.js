c089c6154a2d0d18c2a073c4972308f011a5cca1
Andrew Kushnir <akushnir@google.com>
2019-06-14 19:14:35 +0000

    7e3a60a docs: add example and edit two-way-binding section of Template Syntax (#26278)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+15.sha-85d38ae.with-local-changes
+ * @license Angular v8.1.0-next.2+16.sha-7e3a60a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
