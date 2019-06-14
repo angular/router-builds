acd8a64f5a5223447f569c0bb998f07c8a455ad7
Andrew Kushnir <akushnir@google.com>
2019-06-14 19:26:58 +0000

    6c4d912 docs: edit and add example for Template Expression Operators section of Template Syntax (#28087)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+21.sha-e76690b.with-local-changes
+ * @license Angular v8.1.0-next.2+20.sha-6c4d912.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
