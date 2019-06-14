ba0902994c8ab8b6f2d9cfba30c67f90ea09962e
Andrew Kushnir <akushnir@google.com>
2019-06-14 18:06:10 +0000

    6bc9c78 fix(core): temporarily remove @deprecated jsdoc tag for a TextBedStatic.get overload (#30714)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+5.sha-bb4e230.with-local-changes
+ * @license Angular v8.1.0-next.2+6.sha-6bc9c78.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
