d005dfcf6965d56773f31896d4f6060a39832d38
Andrew Kushnir <akushnir@google.com>
2019-06-14 21:57:28 +0000

    c596795 ci(docs-infra): check and track payload sizes for `test_aio_local` and `test_aio_local_ivy` (#31047)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+24.sha-6fbfb5a.with-local-changes
+ * @license Angular v8.1.0-next.2+26.sha-c596795.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
