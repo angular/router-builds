3e3060e61b87450d02002b38fabcf5dcfb1295c2
Andrew Kushnir <akushnir@google.com>
2019-06-14 19:41:07 +0000

    16aa6ce refactor(ivy): update ɵɵtextBinding to not take index (#30792)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+20.sha-6c4d912.with-local-changes
+ * @license Angular v8.1.0-next.2+23.sha-16aa6ce.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.1.0-next.2+20.sha-6c4d912.with-local-changes');
+    var VERSION = new core.Version('8.1.0-next.2+23.sha-16aa6ce.with-local-changes');
 
     /**
      * @license
