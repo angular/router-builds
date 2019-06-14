b70f6f9c2dd1c3d8329c590d91544c85d360028f
Andrew Kushnir <akushnir@google.com>
2019-06-14 18:12:53 +0000

    8f5c396 fix(ivy): don't throw when attempting to destroy a destroyed ComponentRef (#31022)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+10.sha-0c3bb6a.with-local-changes
+ * @license Angular v8.1.0-next.2+11.sha-8f5c396.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
