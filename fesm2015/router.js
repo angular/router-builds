47c35d7c10c1aa78c83e5b419c174703ceaa5b21
Andrew Kushnir <akushnir@google.com>
2019-06-14 19:44:20 +0000

    2abb54c docs: rewrite attribute binding section and add example (#26004)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+10.sha-7f21449.with-local-changes
+ * @license Angular v8.0.1+18.sha-2abb54c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.1+10.sha-7f21449.with-local-changes');
+const VERSION = new Version('8.0.1+18.sha-2abb54c.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
