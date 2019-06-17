d0d5e4b1bcde76133cd6cbe53833e5347f4890de
Andrew Kushnir <akushnir@google.com>
2019-06-17 21:17:12 +0000

    4adf95ed6 ci: publish snapshots job is unable to decode github token (#31099)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+26.sha-c596795.with-local-changes
+ * @license Angular v8.1.0-next.2+29.sha-4adf95e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
