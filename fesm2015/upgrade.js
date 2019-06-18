eb80105aa47b0e8371819fc16682a1b1986bc332
Andrew Kushnir <akushnir@google.com>
2019-06-18 18:11:05 +0000

    5909c442b docs: update group() link to point to the animations group API (#30618)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+40.sha-f5fa9dc.with-local-changes
+ * @license Angular v8.1.0-next.2+41.sha-5909c44.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
