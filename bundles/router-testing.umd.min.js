09ac775985bc09d97cad99e1301dce44bf18c063
Andrew Kushnir <akushnir@google.com>
2019-06-18 17:14:23 +0000

    eb970b462 docs: rewrite Observable examples with pipable operators (#31074)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+26.sha-6a01fa5.with-local-changes
+ * @license Angular v8.0.1+28.sha-eb970b4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+26.sha-6a01fa5.with-local-changes
+ * @license Angular v8.0.1+28.sha-eb970b4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.1+26.sha-6a01fa5.with-local-changes');
+    var VERSION = new core.Version('8.0.1+28.sha-eb970b4.with-local-changes');
 
     /**
      * @license
