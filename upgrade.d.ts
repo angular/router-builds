1e995efe9ece23809cac2edd0ab87cb9dd48d065
Andrew Kushnir <akushnir@google.com>
2019-06-18 00:00:39 +0000

    6a01fa532 docs: add mock heroes to the code review tabs for TOH pt. 2 (#31080)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+24.sha-53c6425.with-local-changes
+ * @license Angular v8.0.1+26.sha-6a01fa5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
