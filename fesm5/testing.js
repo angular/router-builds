d40b273cbd3da702a56cb2943e6b7b4d9c0dc6ec
Andrew Kushnir <akushnir@google.com>
2019-06-18 17:27:22 +0000

    16ac611a8 build: ts-api-guardian npm package contains invalid references (#31096)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+39.sha-87665fe.with-local-changes
+ * @license Angular v8.1.0-next.2+38.sha-16ac611.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
