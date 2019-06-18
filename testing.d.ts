21a2e1a5d81d578adc8fa9ba3a77d86a07b33254
Andrew Kushnir <akushnir@google.com>
2019-06-18 18:22:59 +0000

    8492499fe docs: add missing word in build and deploy guide to clarify sentence (#31093)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+31.sha-4817401.with-local-changes
+ * @license Angular v8.0.1+32.sha-8492499.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
