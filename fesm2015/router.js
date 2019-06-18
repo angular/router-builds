c9d59ea6a7db33e65d5cf7a0fdb91bb459208bc4
Andrew Kushnir <akushnir@google.com>
2019-06-18 17:11:49 +0000

    7e49beb8c docs: rewrite Observable examples with pipable operators (#31074)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+31.sha-98515e1.with-local-changes
+ * @license Angular v8.1.0-next.2+33.sha-7e49beb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.2+31.sha-98515e1.with-local-changes');
+const VERSION = new Version('8.1.0-next.2+33.sha-7e49beb.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+31.sha-98515e1.with-local-changes
+ * @license Angular v8.1.0-next.2+33.sha-7e49beb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
