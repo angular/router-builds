541f96887957c08f9a39f50974c21c1be9828200
Andrew Kushnir <akushnir@google.com>
2019-06-18 21:37:16 +0000

    bf9de8ca5 test: update side-effects test to be more descriptive (#31005)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+34.sha-df37c47.with-local-changes
+ * @license Angular v8.0.1+35.sha-bf9de8c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+34.sha-df37c47.with-local-changes
+ * @license Angular v8.0.1+35.sha-bf9de8c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.1+34.sha-df37c47.with-local-changes');
+var VERSION = new Version('8.0.1+35.sha-bf9de8c.with-local-changes');
 
 /**
  * @license
