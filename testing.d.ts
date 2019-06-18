0c061b1c6c5b1c59c91cb7e599ff9eb1c76fc4b5
Andrew Kushnir <akushnir@google.com>
2019-06-18 17:28:02 +0000

    1460e46ba build: ts-api-guardian npm package contains invalid references (#31096)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+29.sha-a7ff7d8.with-local-changes
+ * @license Angular v8.0.1+30.sha-1460e46.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
