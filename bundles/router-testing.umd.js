59cfa35af7d39c83aa9e34bd10d957c78ff93a44
Andrew Kushnir <akushnir@google.com>
2019-06-18 17:15:52 +0000

    18d301258 docs: change interceptor documentation (#30969) PR Close #30969

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+28.sha-eb970b4.with-local-changes
+ * @license Angular v8.0.1+27.sha-18d3012.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+28.sha-eb970b4.with-local-changes
+ * @license Angular v8.0.1+27.sha-18d3012.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.1+28.sha-eb970b4.with-local-changes');
+const VERSION = new Version('8.0.1+27.sha-18d3012.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
