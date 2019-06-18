75fa2f3e79e81581bc7fce5282b2daa12ed318cb
Andrew Kushnir <akushnir@google.com>
2019-06-18 17:19:09 +0000

    a7ff7d8d2 docs: refresh TypeScript configuration guide with updated info and files (#31097)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+27.sha-18d3012.with-local-changes
+ * @license Angular v8.0.1+29.sha-a7ff7d8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.1+27.sha-18d3012.with-local-changes');
+var VERSION = new Version('8.0.1+29.sha-a7ff7d8.with-local-changes');
 
 /**
  * @license
