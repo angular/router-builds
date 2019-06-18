c2abc0009f50be94a89edfac6cef8aed80f50e29
Andrew Kushnir <akushnir@google.com>
2019-06-18 17:17:39 +0000

    65544ac74 fix(ivy): reprojected ICU expression nodes when creating embedded views (#30979)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+32.sha-036294d.with-local-changes
+ * @license Angular v8.1.0-next.2+35.sha-65544ac.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8274,7 +8274,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.2+32.sha-036294d.with-local-changes');
+const VERSION = new Version('8.1.0-next.2+35.sha-65544ac.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
