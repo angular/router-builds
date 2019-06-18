c27ae4bb7913e7390e88b98eb1d9b428e6bd6eb4
Andrew Kushnir <akushnir@google.com>
2019-06-18 20:51:22 +0000

    beaab27a4 feat(ivy): index identifiers discovered in templates (#30963)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+14.sha-4ad323a.with-local-changes
+ * @license Angular v8.1.0-next.2+37.sha-beaab27.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
