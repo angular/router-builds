2df265f65d9cf8020213d73b61d45d94de7f8e4c
Kara Erickson <karakara@google.com>
2019-06-19 22:52:46 +0000

    ab86a58f2 docs: remove inappropriate link (#31091)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+3.sha-4ec5081.with-local-changes
+ * @license Angular v8.1.0-next.3+4.sha-ab86a58.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8274,7 +8274,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.3+3.sha-4ec5081.with-local-changes');
+const VERSION = new Version('8.1.0-next.3+4.sha-ab86a58.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
