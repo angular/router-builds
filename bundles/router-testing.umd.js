c20c48c8d9797e7ea7b703d3eca317154d01ae6e
Kara Erickson <karakara@google.com>
2019-06-19 23:17:51 +0000

    4c45aa39e ci: re-enable payload size tracking (#31138)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+12.sha-c5d1e1a.with-local-changes
+ * @license Angular v8.1.0-next.3+13.sha-4c45aa3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+12.sha-c5d1e1a.with-local-changes
+ * @license Angular v8.1.0-next.3+13.sha-4c45aa3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.3+12.sha-c5d1e1a.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+13.sha-4c45aa3.with-local-changes');
 
 /**
  * @license
