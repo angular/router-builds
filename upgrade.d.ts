9f36a98d58b5a6382b7bbdd4475d63dfa9590352
Kara Erickson <karakara@google.com>
2019-06-19 22:53:59 +0000

    b68f2d6fa docs: remove inappropriate link (#31091)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+2.sha-d8def6d.with-local-changes
+ * @license Angular v8.0.2+3.sha-b68f2d6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
