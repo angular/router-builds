35dad9d7c1f16f19b3cda648270f8f412735796f
Kara Erickson <karakara@google.com>
2019-06-19 23:20:31 +0000

    9c222350f ci: re-enable payload size tracking (#31138)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+7.sha-c36edff.with-local-changes
+ * @license Angular v8.0.2+8.sha-9c22235.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
