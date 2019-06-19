4d00adf161be1b3552d97a3b8df97826b0359b53
Kara Erickson <karakara@google.com>
2019-06-19 23:02:12 +0000

    4b16d9895 perf(ivy): chain multiple property instructions (#31078)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+4.sha-ab86a58.with-local-changes
+ * @license Angular v8.1.0-next.3+5.sha-4b16d98.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.3+4.sha-ab86a58.with-local-changes');
+const VERSION = new Version('8.1.0-next.3+5.sha-4b16d98.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
