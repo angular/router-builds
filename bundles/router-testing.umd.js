f7602b29596d11bdd4be4375665c8e5610d0d187
Kara Erickson <karakara@google.com>
2019-06-19 22:39:29 +0000

    4ec50811d refactor(language-service): Remove NgLSHost -> NgLS dependency (#31122)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+2.sha-c34abf2.with-local-changes
+ * @license Angular v8.1.0-next.3+3.sha-4ec5081.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
