82e540c652b40e7e9d931061204520df402940e2
Kara Erickson <karakara@google.com>
2019-06-19 23:12:37 +0000

    029f1be20 perf(ivy): avoid unnecessary function call in i18n instruction (#31106)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+7.sha-91008bd.with-local-changes
+ * @license Angular v8.1.0-next.3+8.sha-029f1be.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
