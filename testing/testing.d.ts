4e6470f2108b76642116fd071e50680bc03285f8
Andrew Kushnir <akushnir@google.com>
2019-06-19 16:12:43 +0000

    2fe8f2b1e build: update buildifier to version that supports windows (#31129)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+36.sha-f1c08c6.with-local-changes
+ * @license Angular v8.0.1+37.sha-2fe8f2b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
