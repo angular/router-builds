0c727099908440a02c00ad63a53a2dee3e5bda18
Andrew Kushnir <akushnir@google.com>
2019-06-19 16:10:11 +0000

    f1c08c691 docs: add core and cli version alignment note (#30976) docs: add core and cli version alignment note PR Close #30976

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+35.sha-bf9de8c.with-local-changes
+ * @license Angular v8.0.1+36.sha-f1c08c6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.1+35.sha-bf9de8c.with-local-changes');
+    var VERSION = new core.Version('8.0.1+36.sha-f1c08c6.with-local-changes');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+35.sha-bf9de8c.with-local-changes
+ * @license Angular v8.0.1+36.sha-f1c08c6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
