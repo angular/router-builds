990bd9dbfae6f20639130942453f9f3d430b9265
Kara Erickson <karakara@google.com>
2019-06-19 23:14:33 +0000

    c5d1e1a3d docs: fix bad tag (#31115)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+8.sha-029f1be.with-local-changes
+ * @license Angular v8.1.0-next.3+12.sha-c5d1e1a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.3+8.sha-029f1be.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+12.sha-c5d1e1a.with-local-changes');
 
 /**
  * @license
