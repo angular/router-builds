660c4cf134e44719b5304c051fa8968d9a99182d
Kara Erickson <karakara@google.com>
2019-06-19 23:19:23 +0000

    c36edffe2 docs: fix bad tag (#31115)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+3.sha-b68f2d6.with-local-changes
+ * @license Angular v8.0.2+7.sha-c36edff.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+3.sha-b68f2d6.with-local-changes
+ * @license Angular v8.0.2+7.sha-c36edff.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.2+3.sha-b68f2d6.with-local-changes');
+const VERSION = new Version('8.0.2+7.sha-c36edff.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
