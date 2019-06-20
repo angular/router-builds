ff38e56b264389d4167501a1a630db587fff82c5
Kara Erickson <karakara@google.com>
2019-06-20 18:46:31 +0000

    b356fb01d build(docs-infra): upgrade @angular/cli to 8.1.0-beta.2 (#31137)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+15.sha-70ad91e.with-local-changes
+ * @license Angular v8.1.0-next.3+17.sha-b356fb0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
