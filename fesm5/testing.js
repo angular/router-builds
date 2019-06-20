24650d86f7e1ad4f80cf1620d5d091ff36f02094
Kara Erickson <karakara@google.com>
2019-06-20 23:25:30 +0000

    34eaafdf4 build: correct zone.js bazel test visibilty rule (#31175)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+20.sha-bf7d046.with-local-changes
+ * @license Angular v8.1.0-next.3+21.sha-34eaafd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
