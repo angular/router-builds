f5af97ea90a218f0ff8bb4010be86652a54fb789
Kara Erickson <karakara@google.com>
2019-06-20 19:26:32 +0000

    f2219081e refactor(compiler): allow parsing templates with custom leadingTriviaChars (#31136)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+8.sha-029f1be.with-local-changes
+ * @license Angular v8.1.0-next.3+16.sha-f221908.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
