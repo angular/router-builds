c68a2ad91184254a58e9e8a3f8fcfcee99df897f
Kara Erickson <karakara@google.com>
2019-06-20 18:51:17 +0000

    7b3bcc23a build: user bazelrc should be able to overwrite all flags (#31164)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+17.sha-b356fb0.with-local-changes
+ * @license Angular v8.1.0-next.3+18.sha-7b3bcc2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.1.0-next.3+17.sha-b356fb0.with-local-changes');
+    var VERSION = new i0.Version('8.1.0-next.3+18.sha-7b3bcc2.with-local-changes');
 
     /**
      * @license
