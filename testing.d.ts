dbdf081e44e49d177b4f2e81a75bc48e9298f959
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2019-06-20 18:48:19 +0000

    c0d7cf4f2 fixup! fix(compiler): fix Elements not making a new ParseSourceSpan

---
+++ testing.d.ts
@@ -0,0 +1,7 @@
+/**
+ * @license Angular v8.1.0-next.3+16.sha-c0d7cf4.with-local-changes
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+export * from './testing/testing';
