25a02f28e2c7cad90801fd653f72c302658b2779
Kara Erickson <karakara@google.com>
2019-06-20 01:15:44 +0000

    75ac72484 fix(ivy): quote dots in directive inputs and outputs (#31146)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+13.sha-4c45aa3.with-local-changes
+ * @license Angular v8.1.0-next.3+14.sha-75ac724.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.3+13.sha-4c45aa3.with-local-changes');
+const VERSION = new Version('8.1.0-next.3+14.sha-75ac724.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
