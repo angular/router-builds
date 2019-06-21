7bf2dc4e0b67a0606e2cc295ce51716b6453dfcc
Kara Erickson <karakara@google.com>
2019-06-21 17:54:00 +0000

    83f0304cf docs: clarify wording in Template Syntax (#31119)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+33.sha-4bd6fca.with-local-changes
+ * @license Angular v8.1.0-next.3+35.sha-83f0304.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
