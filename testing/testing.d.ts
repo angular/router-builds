3b1eee92b2ec453eef04e57e911c603f5cb7ca21
Kara Erickson <karakara@google.com>
2019-06-21 22:53:39 +0000

    7fe95b188 ci: send failure notifications from `saucelabs_tests` to `dev-infra-ci-failures` (#31202)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+18.sha-ff59345.with-local-changes
+ * @license Angular v8.0.2+22.sha-7fe95b1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
