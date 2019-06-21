881b6c31bffa60f402631c0b59fbef7304d0404a
Kara Erickson <karakara@google.com>
2019-06-21 21:43:29 +0000

    65974154e feat(docs-infra): change typography font sizes to be rem based (#31118)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+37.sha-7035f22.with-local-changes
+ * @license Angular v8.1.0-next.3+38.sha-6597415.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
