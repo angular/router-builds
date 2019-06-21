8eca438a6956d8cf2df431723ea22666519cc1a8
Kara Erickson <karakara@google.com>
2019-06-21 22:53:23 +0000

    ff5934597 build: Set up Build Event Service Configuration for RBE executed bazel (#31197)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+17.sha-09b60ed.with-local-changes
+ * @license Angular v8.0.2+18.sha-ff59345.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.2+17.sha-09b60ed.with-local-changes');
+var VERSION = new Version('8.0.2+18.sha-ff59345.with-local-changes');
 
 /**
  * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+17.sha-09b60ed.with-local-changes
+ * @license Angular v8.0.2+18.sha-ff59345.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
