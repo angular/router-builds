97fc66ab08bf756060c859392d09ea04c8dcf501
Kara Erickson <karakara@google.com>
2019-06-21 00:03:28 +0000

    fad03c3c1 refactor: early compatibility with TypeScript 3.5 (#31174)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+21.sha-34eaafd.with-local-changes
+ * @license Angular v8.1.0-next.3+22.sha-fad03c3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.3+21.sha-34eaafd.with-local-changes');
+const VERSION = new Version('8.1.0-next.3+22.sha-fad03c3.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
