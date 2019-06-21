e4c837c42db9835cb1cee47f4b73a06f60a041d5
Kara Erickson <karakara@google.com>
2019-06-21 00:24:12 +0000

    3fb78aaac feat(upgrade): provide unit test helpers for wiring up injectors (#16848)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+22.sha-fad03c3.with-local-changes
+ * @license Angular v8.1.0-next.3+24.sha-3fb78aa.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
