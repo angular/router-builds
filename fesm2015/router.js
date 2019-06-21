77eb95b7ded8a5ea09d6b0b3b85faaedf0ab9f45
Kara Erickson <karakara@google.com>
2019-06-21 17:47:55 +0000

    ecd7f6ecd docs: fix javascript module link (#31181)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+28.sha-1f1cf1a.with-local-changes
+ * @license Angular v8.1.0-next.3+30.sha-ecd7f6e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8274,7 +8274,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.3+28.sha-1f1cf1a.with-local-changes');
+const VERSION = new Version('8.1.0-next.3+30.sha-ecd7f6e.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
