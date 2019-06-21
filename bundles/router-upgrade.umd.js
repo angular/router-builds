4a0c10098c18a910850c75643cf4e80931f98df8
Kara Erickson <karakara@google.com>
2019-06-21 17:46:36 +0000

    ecd7f6ecd docs: fix javascript module link (#31181)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+28.sha-1f1cf1a.with-local-changes
+ * @license Angular v8.1.0-next.3+30.sha-ecd7f6e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
