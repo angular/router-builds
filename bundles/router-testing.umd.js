29b1eb91ec5adb1d2fea602b2b08c6ff1f725163
Kara Erickson <karakara@google.com>
2019-06-21 17:56:24 +0000

    8ea91887c docs: mark interfaces as public (#30955)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+15.sha-de04741.with-local-changes
+ * @license Angular v8.0.2+14.sha-8ea9188.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
