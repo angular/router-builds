d9c24b3c9bd02a40fb021ead35b2facbb45fc13c
Kara Erickson <karakara@google.com>
2019-06-21 17:46:32 +0000

    15d138fdd docs: fix typo in browser-support.md (ES1015 --> ES2015) (#31157)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+13.sha-ff184b5.with-local-changes
+ * @license Angular v8.0.2+12.sha-15d138f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.2+13.sha-ff184b5.with-local-changes');
+var VERSION = new Version('8.0.2+12.sha-15d138f.with-local-changes');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+13.sha-ff184b5.with-local-changes
+ * @license Angular v8.0.2+12.sha-15d138f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
