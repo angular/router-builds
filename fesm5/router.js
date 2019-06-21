6e4d756aa762bd8f36ea20430cdbf7122f0e2ae9
Kara Erickson <karakara@google.com>
2019-06-21 17:45:52 +0000

    ff184b56e docs: fix javascript module link (#31181)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+11.sha-1699b54.with-local-changes
+ * @license Angular v8.0.2+13.sha-ff184b5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.2+11.sha-1699b54.with-local-changes');
+var VERSION = new Version('8.0.2+13.sha-ff184b5.with-local-changes');
 
 /**
  * @license
