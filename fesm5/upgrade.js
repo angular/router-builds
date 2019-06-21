62194a69dc458d6f342a7a6dcaece59198a6335e
Kara Erickson <karakara@google.com>
2019-06-21 02:08:48 +0000

    dc613b336 refactor(ivy): removing unused properties from i18n interfaces (#31171)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+25.sha-7ff628f.with-local-changes
+ * @license Angular v8.1.0-next.3+26.sha-dc613b3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
