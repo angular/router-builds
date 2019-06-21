7530d504dcf3d85d7b00f5e01cccaae934406ad1
Kara Erickson <karakara@google.com>
2019-06-21 00:37:09 +0000

    7ff628f8d refactor(ivy): make `bind` an internal-only function (#31131)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+24.sha-3fb78aa.with-local-changes
+ * @license Angular v8.1.0-next.3+25.sha-7ff628f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.1.0-next.3+24.sha-3fb78aa.with-local-changes');
+    var VERSION = new core.Version('8.1.0-next.3+25.sha-7ff628f.with-local-changes');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+24.sha-3fb78aa.with-local-changes
+ * @license Angular v8.1.0-next.3+25.sha-7ff628f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
