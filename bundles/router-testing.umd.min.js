900f71ec112114e565ac80592355e1fcd70d443a
Kara Erickson <karakara@google.com>
2019-06-21 22:52:17 +0000

    131e2440f build: Set up Build Event Service Configuration for RBE executed bazel (#31197)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+38.sha-6597415.with-local-changes
+ * @license Angular v8.1.0-next.3+39.sha-131e244.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+38.sha-6597415.with-local-changes
+ * @license Angular v8.1.0-next.3+39.sha-131e244.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.1.0-next.3+38.sha-6597415.with-local-changes');
+    var VERSION = new i0.Version('8.1.0-next.3+39.sha-131e244.with-local-changes');
 
     /**
      * @license
