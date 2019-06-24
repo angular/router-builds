6bbc2b36f70cb0a256ff8f0feccc1184cf552d0f
Kara Erickson <karakara@google.com>
2019-06-24 18:51:43 +0000

    fcb03abc7 fix(ivy): injecting incorrect Renderer2 into child components (#31063)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+40.sha-a4fc98c.with-local-changes
+ * @license Angular v8.1.0-next.3+44.sha-fcb03ab.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
