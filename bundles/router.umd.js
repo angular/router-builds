31be3c9cfb33b5be40ae2acf0a610fbec98ec298
Kara Erickson <karakara@google.com>
2019-06-25 00:10:31 +0000

    dabf1325e ci: upgrade clang-format to 1.0.27 to match master (#31247)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+26.sha-987b185.with-local-changes
+ * @license Angular v8.0.2+27.sha-dabf132.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.2+26.sha-987b185.with-local-changes');
+    var VERSION = new core.Version('8.0.2+27.sha-dabf132.with-local-changes');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+26.sha-987b185.with-local-changes
+ * @license Angular v8.0.2+27.sha-dabf132.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
