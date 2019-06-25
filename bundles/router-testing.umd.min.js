1173ccf9534bafdb566ab75eecb66eff4e4294c6
Kara Erickson <karakara@google.com>
2019-06-25 17:55:47 +0000

    b11a2057c docs(core): change from CSS Event to DOM event (#31229) PR Close #31229

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+52.sha-f7e9659.with-local-changes
+ * @license Angular v8.1.0-next.3+61.sha-b11a205.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
