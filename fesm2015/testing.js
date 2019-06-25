49043fb1a8b4e76927d665bd98aef9742805a6d7
Kara Erickson <karakara@google.com>
2019-06-25 22:04:22 +0000

    ae1ac4598 docs(core): update code sample for "outputs" attribute (#31199)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+80.sha-280e856.with-local-changes
+ * @license Angular v8.1.0-next.3+81.sha-ae1ac45.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
