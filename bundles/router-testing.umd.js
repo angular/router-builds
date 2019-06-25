10dc90fd27011cd019a738d85aad330bf0af9e92
Kara Erickson <karakara@google.com>
2019-06-25 22:02:18 +0000

    280e8563f Revert "build(bazel): update to bazel 0.27.0 and fix compat in @angular/bazel package (#31019)" (#31267)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+72.sha-98685e6.with-local-changes
+ * @license Angular v8.1.0-next.3+80.sha-280e856.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
