f370aeb07168e491310b72ca4b7e4072ee3ad3da
Kara Erickson <karakara@google.com>
2019-06-25 23:45:18 +0000

    7186f9c01 refactor(ivy): implement a virtual file-system layer in ngtsc + ngcc (#30921)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+82.sha-1e7e065.with-local-changes
+ * @license Angular v8.1.0-next.3+83.sha-7186f9c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.3+82.sha-1e7e065.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+83.sha-7186f9c.with-local-changes');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+82.sha-1e7e065.with-local-changes
+ * @license Angular v8.1.0-next.3+83.sha-7186f9c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
