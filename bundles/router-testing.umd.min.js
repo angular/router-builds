75cf7a759e34787c54c60a0c6b826258b43d0bb1
Kara Erickson <karakara@google.com>
2019-06-25 19:30:28 +0000

    e9ae88598 ci: add owners for zone.js to CODEOWNERS (#31203)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+30.sha-00242e8.with-local-changes
+ * @license Angular v8.0.2+37.sha-e9ae885.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+30.sha-00242e8.with-local-changes
+ * @license Angular v8.0.2+37.sha-e9ae885.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.2+30.sha-00242e8.with-local-changes');
+const VERSION = new Version('8.0.2+37.sha-e9ae885.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
