896754b7055b701207359003f25e5e613e09f60b
Kara Erickson <karakara@google.com>
2019-06-25 18:21:13 +0000

    a1d436e6a docs: fix formatting (#31120)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+65.sha-00ecfc7.with-local-changes
+ * @license Angular v8.1.0-next.3+68.sha-a1d436e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
