d594ca4fb1b70fa008bd3547f91348af48f4e05a
Kara Erickson <karakara@google.com>
2019-06-25 17:54:30 +0000

    a1d436e6a docs: fix formatting (#31120)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+61.sha-b11a205.with-local-changes
+ * @license Angular v8.1.0-next.3+68.sha-a1d436e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.3+61.sha-b11a205.with-local-changes');
+const VERSION = new Version('8.1.0-next.3+68.sha-a1d436e.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
