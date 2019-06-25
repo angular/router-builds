81706415504327fad810224da8ca130c3dbe6315
Kara Erickson <karakara@google.com>
2019-06-25 17:59:37 +0000

    0a46b2acf fix(docs-infra): separate vendor-specific CSS selectors (#31252)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+34.sha-1d193df.with-local-changes
+ * @license Angular v8.0.2+33.sha-0a46b2a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.2+34.sha-1d193df.with-local-changes');
+    var VERSION = new core.Version('8.0.2+33.sha-0a46b2a.with-local-changes');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+34.sha-1d193df.with-local-changes
+ * @license Angular v8.0.2+33.sha-0a46b2a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
