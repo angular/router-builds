05781579a412662a3adb23ea374e65cd2988c7a5
Kara Erickson <karakara@google.com>
2019-06-25 22:11:32 +0000

    54f68c342 docs(core): update code sample for "outputs" attribute (#31199)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+41.sha-79471f7.with-local-changes
+ * @license Angular v8.0.2+40.sha-54f68c3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
