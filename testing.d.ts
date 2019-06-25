f30229f48d714018fe5bd2b7b7e2226c1131ab8c
Kara Erickson <karakara@google.com>
2019-06-25 02:12:46 +0000

    6bad2ca58 fix(bazel): exclude all angular schematics folders from metadata build (#31237)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+27.sha-dabf132.with-local-changes
+ * @license Angular v8.0.2+28.sha-6bad2ca.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
