645431cb9b036dc605613efbe24f6afc77dec0ee
Kara Erickson <karakara@google.com>
2019-06-25 22:05:41 +0000

    79471f761 docs: correct indention for code example in deployment guide (#31255)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+37.sha-e9ae885.with-local-changes
+ * @license Angular v8.0.2+41.sha-79471f7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.2+37.sha-e9ae885.with-local-changes');
+var VERSION = new Version('8.0.2+41.sha-79471f7.with-local-changes');
 
 /**
  * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+37.sha-e9ae885.with-local-changes
+ * @license Angular v8.0.2+41.sha-79471f7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
