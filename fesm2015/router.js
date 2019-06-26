2db00fbe38638f77be313c0ce5a79415c777fa5f
Kara Erickson <karakara@google.com>
2019-06-26 20:52:09 +0000

    9c87d223e ci: target default pool for linux RBE executions (#31297)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+106.sha-2be061a.with-local-changes
+ * @license Angular v8.1.0-next.3+107.sha-9c87d22.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.3+106.sha-2be061a.with-local-changes');
+const VERSION = new Version('8.1.0-next.3+107.sha-9c87d22.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
