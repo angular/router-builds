1d9d1d2afbf490c125ac212e097428c60259550c
Kara Erickson <karakara@google.com>
2019-06-26 20:53:44 +0000

    385154467 style: fix code style in several files (#31295)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+107.sha-9c87d22.with-local-changes
+ * @license Angular v8.1.0-next.3+105.sha-3851544.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
