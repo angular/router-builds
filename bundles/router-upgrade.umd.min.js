6238a3e0e494e735ae9b76a66f85dae17e389fb2
Kara Erickson <karakara@google.com>
2019-06-26 15:26:26 +0000

    f690a4e0a fix(ivy): ngcc - do not analyze files outside the current package (#30591)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+98.sha-3788ebb.with-local-changes
+ * @license Angular v8.1.0-next.3+96.sha-f690a4e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
