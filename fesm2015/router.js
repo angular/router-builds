215ae560dd42fab1c6ab615e1ffa85c49dea6d54
Kara Erickson <karakara@google.com>
2019-06-26 15:35:14 +0000

    a8f3b317f build(docs-infra): upgrade cli command docs sources to 01a7186bb (#31281)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+97.sha-32c760f.with-local-changes
+ * @license Angular v8.1.0-next.3+99.sha-a8f3b31.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.3+97.sha-32c760f.with-local-changes');
+const VERSION = new Version('8.1.0-next.3+99.sha-a8f3b31.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
