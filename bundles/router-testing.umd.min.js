60d3fa1ab4ce5fb73c2d43933c5fdff07de51f9b
Kara Erickson <karakara@google.com>
2019-06-26 21:20:00 +0000

    da4f7fbe1 docs: release notes for the v8.0.3 release

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+105.sha-3851544.with-local-changes
+ * @license Angular v8.1.0-next.3+108.sha-da4f7fb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+105.sha-3851544.with-local-changes
+ * @license Angular v8.1.0-next.3+108.sha-da4f7fb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.1.0-next.3+105.sha-3851544.with-local-changes');
+    var VERSION = new i0.Version('8.1.0-next.3+108.sha-da4f7fb.with-local-changes');
 
     /**
      * @license
