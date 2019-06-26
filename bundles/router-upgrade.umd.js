5f89219bf23b2fe6e93d65c35d4bf73f6eff9449
Kara Erickson <karakara@google.com>
2019-06-26 16:44:42 +0000

    e01d697ee build: filter out changelog entries for zone.js from framework changelog (#31277)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+99.sha-a8f3b31.with-local-changes
+ * @license Angular v8.1.0-next.3+101.sha-e01d697.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
