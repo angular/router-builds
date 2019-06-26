4d09d1f0545ac960e10fa1f934d714c61110bb26
Kara Erickson <karakara@google.com>
2019-06-26 21:44:57 +0000

    641a4ea76 release: cut the v8.1.0-rc.0 release

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+108.sha-da4f7fb.with-local-changes
+ * @license Angular v8.1.0-rc.0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.3+108.sha-da4f7fb.with-local-changes');
+var VERSION = new Version('8.1.0-rc.0.with-local-changes');
 
 /**
  * @license
