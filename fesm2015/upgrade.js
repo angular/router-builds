0ec681213ce952e3bf6f9213a3604acf885d9230
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-27 23:13:26 +0000

    c8875f2db docs: added svg example (#30559)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+11.sha-579f129.with-local-changes
+ * @license Angular v8.1.0-rc.0+15.sha-c8875f2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
