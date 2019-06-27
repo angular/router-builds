082c9c56064905bdb5fccc9adea7f3a65d7462fa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-27 23:12:16 +0000

    4f38419e3 fix(ivy): handle ICU expressions in `executeActionOnNode` (#31313)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+12.sha-d171006.with-local-changes
+ * @license Angular v8.1.0-rc.0+14.sha-4f38419.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-rc.0+12.sha-d171006.with-local-changes');
+var VERSION = new Version('8.1.0-rc.0+14.sha-4f38419.with-local-changes');
 
 /**
  * @license
