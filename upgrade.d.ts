0c3913517bd37936d7ca5de5cd565b211c6d023a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-27 17:10:46 +0000

    f2360aab9 fix(ivy): incorrect namespace for root node created through ViewContainerRef (#31232)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+3.sha-6c0cca0.with-local-changes
+ * @license Angular v8.1.0-rc.0+2.sha-f2360aa.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
