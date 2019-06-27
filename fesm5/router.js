96f4043b51c49344b59555e50c7367f8b0875e97
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-27 17:11:41 +0000

    6c0cca093 docs: couple of small UI fixes throughout some documents (#31155)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+1.sha-d7b4172.with-local-changes
+ * @license Angular v8.1.0-rc.0+3.sha-6c0cca0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5718,7 +5718,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-rc.0+1.sha-d7b4172.with-local-changes');
+var VERSION = new Version('8.1.0-rc.0+3.sha-6c0cca0.with-local-changes');
 
 /**
  * @license
