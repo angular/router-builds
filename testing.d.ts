e61c2a2ff87ce03b3a35a0a91e479d50aaac0f3e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-27 19:54:28 +0000

    d17100608 fix(ivy): ngtsc - NgtscCompilerHost should cope with directories that look like files (#31289)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+11.sha-19e8570.with-local-changes
+ * @license Angular v8.1.0-rc.0+12.sha-d171006.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
