6375f38d8b6c74280dc559cb43c77c3da31cece0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 16:50:11 +0000

    81332150a perf(ivy): chain host binding instructions (#31296)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+20.sha-2b5d52f.with-local-changes
+ * @license Angular v8.1.0-rc.0+19.sha-8133215.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5813,7 +5813,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.1.0-rc.0+20.sha-2b5d52f.with-local-changes');
+    var VERSION = new i0.Version('8.1.0-rc.0+19.sha-8133215.with-local-changes');
 
     /**
      * @license
