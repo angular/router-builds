176e1f8e2cb47c44f5b7eb7ba5b78dde90e3c018
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 19:44:38 +0000

    4bbf60ed0 docs: change order of Angular Elements article (#30521)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+17.sha-299a43c.with-local-changes
+ * @license Angular v8.1.0-rc.0+18.sha-4bbf60e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-rc.0+17.sha-299a43c.with-local-changes');
+const VERSION = new Version('8.1.0-rc.0+18.sha-4bbf60e.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
