801390079552574bf4f6f73dac2ba3d0c1c1dc57
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 23:06:08 +0000

    7ca611cd1 fix(ivy): properly handle re-projection with an empty set of nodes to re-project (#31306)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+22.sha-0de5d79.with-local-changes
+ * @license Angular v8.1.0-rc.0+25.sha-7ca611c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
