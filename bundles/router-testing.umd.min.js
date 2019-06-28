7a269270345209b1e6cbc1d7aa5edab7c47f839c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 19:04:41 +0000

    0de5d79bf Revert "fix(router): adjust UrlTree redirect to replace URL if in eager update (#31168)" (#31344)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+19.sha-8133215.with-local-changes
+ * @license Angular v8.1.0-rc.0+22.sha-0de5d79.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+19.sha-8133215.with-local-changes
+ * @license Angular v8.1.0-rc.0+22.sha-0de5d79.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4086,23 +4086,9 @@
                         }
                         var navCancel = new NavigationCancel(t.id, _this.serializeUrl(t.extractedUrl), e.message);
                         eventsSubject.next(navCancel);
-                        // When redirecting, we need to delay resolving the navigation promise and push
-                        // it to the redirect navigation
-                        if (!redirecting) {
-                            t.resolve(false);
-                        }
-                        else {
-                            // setTimeout is required so this navigation finishes with the return EMPTY
-                            // below. If it isn't allowed to finish processing, there can be multiple
-                            // navigations to the same URL.
-                            setTimeout(function () {
-                                var mergedTree = _this.urlHandlingStrategy.merge(e.url, _this.rawUrlTree);
-                                var extras = {
-                                    skipLocationChange: t.extras.skipLocationChange,
-                                    replaceUrl: _this.urlUpdateStrategy === 'eager'
-                                };
-                                return _this.scheduleNavigation(mergedTree, 'imperative', null, extras, { resolve: t.resolve, reject: t.reject, promise: t.promise });
-                            }, 0);
+                        t.resolve(false);
+                        if (redirecting) {
+                            _this.navigateByUrl(e.url);
                         }
                         /* All other errors should reset to the router's internal URL reference to the
                          * pre-error state. */
@@ -4395,7 +4381,7 @@
                 t.resolve(true);
             }, function (e) { _this.console.warn("Unhandled Navigation Error: "); });
         };
-        Router.prototype.scheduleNavigation = function (rawUrl, source, restoredState, extras, priorPromise) {
+        Router.prototype.scheduleNavigation = function (rawUrl, source, restoredState, extras) {
             var lastNavigation = this.getTransition();
             // If the user triggers a navigation imperatively (e.g., by using navigateByUrl),
             // and that navigation results in 'replaceState' that leads to the same URL,
@@ -4418,20 +4404,12 @@
                 lastNavigation.rawUrl.toString() === rawUrl.toString()) {
                 return Promise.resolve(true); // return value is not used
             }
-            var resolve;
-            var reject;
-            var promise;
-            if (priorPromise) {
-                resolve = priorPromise.resolve;
-                reject = priorPromise.reject;
-                promise = priorPromise.promise;
-            }
-            else {
-                promise = new Promise(function (res, rej) {
-                    resolve = res;
-                    reject = rej;
-                });
-            }
+            var resolve = null;
+            var reject = null;
+            var promise = new Promise(function (res, rej) {
+                resolve = res;
+                reject = rej;
+            });
             var id = ++this.navigationId;
             this.setTransition({
                 id: id,
@@ -5813,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.1.0-rc.0+19.sha-8133215.with-local-changes');
+    var VERSION = new i0.Version('8.1.0-rc.0+22.sha-0de5d79.with-local-changes');
 
     /**
      * @license
