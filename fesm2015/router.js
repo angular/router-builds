baa661aa2f1c3c1ac87052bba8ca7f11cf5cfdb1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 16:47:17 +0000

    f57e77eeb build(docs-infra): upgrade cli command docs sources to 9edecc522 (#31331)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+18.sha-c6b29f4.with-local-changes
+ * @license Angular v8.1.0-rc.0+21.sha-f57e77e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8114,7 +8114,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-rc.0+18.sha-c6b29f4.with-local-changes');
+const VERSION = new Version('8.1.0-rc.0+21.sha-f57e77e.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+18.sha-c6b29f4.with-local-changes
+ * @license Angular v8.1.0-rc.0+21.sha-f57e77e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
