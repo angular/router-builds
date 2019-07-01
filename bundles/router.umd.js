84899d20c03a79891f0279911db0eed6f0a93404
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-01 17:29:49 +0000

    0d6fd134d docs(core): update link to external i18n doc (#31353)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+26.sha-bbb27b5.with-local-changes
+ * @license Angular v8.1.0-rc.0+27.sha-0d6fd13.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.1.0-rc.0+26.sha-bbb27b5.with-local-changes');
+    var VERSION = new i0.Version('8.1.0-rc.0+27.sha-0d6fd13.with-local-changes');
 
     /**
      * @license
