514312f1630cf9c839520ce8384f931b0828a624
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-01 17:28:51 +0000

    b7e3d8087 feat(ivy): ngcc - handle top-level helper calls in CommonJS (#31335)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+18.sha-4bbf60e.with-local-changes
+ * @license Angular v8.1.0-rc.0+19.sha-b7e3d80.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
