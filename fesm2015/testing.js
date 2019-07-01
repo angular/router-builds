cd204b95d912afaa5da18b09a1994ba17965e72c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-01 17:28:31 +0000

    0d6fd134d docs(core): update link to external i18n doc (#31353)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+26.sha-bbb27b5.with-local-changes
+ * @license Angular v8.1.0-rc.0+27.sha-0d6fd13.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
