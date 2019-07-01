4254dc4560ffd634142393583461bfe42d70007f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-01 17:29:53 +0000

    dd36f3ac9 feat(ivy): ngcc - handle top-level helper calls in CommonJS (#31335)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+27.sha-0d6fd13.with-local-changes
+ * @license Angular v8.1.0-rc.0+28.sha-dd36f3a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-rc.0+27.sha-0d6fd13.with-local-changes');
+const VERSION = new Version('8.1.0-rc.0+28.sha-dd36f3a.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+27.sha-0d6fd13.with-local-changes
+ * @license Angular v8.1.0-rc.0+28.sha-dd36f3a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
