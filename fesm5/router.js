5e2dba7067e4600ca6b045234ca6c70d3ad978a6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-01 17:34:56 +0000

    80ccd6c19 fix(core): handle `undefined` meta in `injectArgs` (#31333)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+19.sha-b7e3d80.with-local-changes
+ * @license Angular v8.1.0-rc.0+20.sha-80ccd6c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-rc.0+19.sha-b7e3d80.with-local-changes');
+var VERSION = new Version('8.1.0-rc.0+20.sha-80ccd6c.with-local-changes');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+19.sha-b7e3d80.with-local-changes
+ * @license Angular v8.1.0-rc.0+20.sha-80ccd6c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
