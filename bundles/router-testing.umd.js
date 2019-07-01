ad274b01ca7564822ad9c1be4d20f22bb94eaec7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-01 21:43:38 +0000

    a4a423a08 build: fix build failures with worker mode cache and @types/events (#31325)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+29.sha-f83dfd6.with-local-changes
+ * @license Angular v8.1.0-rc.0+37.sha-a4a423a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
