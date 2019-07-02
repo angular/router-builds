2ef412a9710bd14538457b1b4acc5c0d43e23739
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-02 19:04:31 +0000

    376c5fceb docs: add accessibility guide (#30851)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+32.sha-d0c32e0.with-local-changes
+ * @license Angular v8.1.0-rc.0+34.sha-376c5fc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
