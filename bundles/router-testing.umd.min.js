d1202f1f7023d213eaadff4654b4ef20efb244f0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-02 18:51:34 +0000

    0fe6110b9 build(docs-infra): update dgeni-packages dependency (#31368)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+31.sha-a57ea26.with-local-changes
+ * @license Angular v8.1.0-rc.0+29.sha-0fe6110.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
