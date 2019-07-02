9681e1dadb1d092bc3a368803618e1c264e2e0a8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-02 18:28:49 +0000

    dca713c08 test(ivy): verify equality of ngBaseDef metadata between compiler and core (#31210)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+40.sha-02491a6.with-local-changes
+ * @license Angular v8.1.0-rc.0+39.sha-dca713c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
