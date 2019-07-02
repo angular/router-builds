4f62d3168901913cb77f8487a167c0cde3bd503c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-02 18:48:44 +0000

    1db3ac457 build(docs-infra): update dgeni-packages dependency (#31368)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+39.sha-dca713c.with-local-changes
+ * @license Angular v8.1.0-rc.0+41.sha-1db3ac4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
