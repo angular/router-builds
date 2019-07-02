bb4fabc853b30ecc5cc7e3f7a59526501b367d3f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-02 18:51:20 +0000

    a57ea2640 test(zone.js): fix typos (#31358)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+28.sha-ffe7050.with-local-changes
+ * @license Angular v8.1.0-rc.0+31.sha-a57ea26.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-rc.0+28.sha-ffe7050.with-local-changes');
+var VERSION = new Version('8.1.0-rc.0+31.sha-a57ea26.with-local-changes');
 
 /**
  * @license
