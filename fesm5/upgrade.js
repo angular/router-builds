38cf15776d5deef32b2581cd61605e2e725aaca4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-02 18:57:07 +0000

    5039faff8 docs: add doc for complex config values (#31010)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+43.sha-fde016b.with-local-changes
+ * @license Angular v8.1.0-rc.0+47.sha-5039faf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
