c5b572412d7c1c086bb548b5f4c0596f6952739e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-02 18:54:00 +0000

    d0c32e03b docs(core): in template syntax guide, make SVG example more clear (#31356)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+29.sha-0fe6110.with-local-changes
+ * @license Angular v8.1.0-rc.0+32.sha-d0c32e0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
