5be5d920214d571ac59e15e56b13b8ad3192df0d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-02 19:43:38 +0000

    95a9d6759 test(zone.js): fix typos (#31358)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+29.sha-f83dfd6.with-local-changes
+ * @license Angular v8.1.0-rc.0+44.sha-95a9d67.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
