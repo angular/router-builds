e455f92cd4301fb68ca81f33e129f261f52eaeff
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-02 19:11:54 +0000

    9e34670b2 docs: add accessibility guide (#30851)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+47.sha-5039faf.with-local-changes
+ * @license Angular v8.1.0-rc.0+48.sha-9e34670.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
