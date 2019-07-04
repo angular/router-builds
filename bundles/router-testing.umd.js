3d698a751b578e5dae953ec67dda2a2d66bfb592
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-04 00:11:51 +0000

    1d3e22766 fix(bazel): revert location of xi18n outputs to bazel-genfiles (#31410)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+2.sha-c095597.with-local-changes
+ * @license Angular v8.2.0-next.0+3.sha-1d3e227.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
