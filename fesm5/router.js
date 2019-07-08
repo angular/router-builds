b4157f91aafd70a9efc80440cf21669dec1d8f1e
Jason Aden <jasonaden@google.com>
2019-07-08 17:04:47 +0000

    7b0a28786 docs: fix formatting in workspace config and add helpful info for universal (#31399)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+1.sha-dcf9c13.with-local-changes
+ * @license Angular v8.1.0+2.sha-7b0a287.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0+1.sha-dcf9c13.with-local-changes');
+var VERSION = new Version('8.1.0+2.sha-7b0a287.with-local-changes');
 
 /**
  * @license
