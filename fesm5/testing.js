fecca91cee70ccc1c33eed484670032cbe164938
Jason Aden <jasonaden@google.com>
2019-07-08 17:46:26 +0000

    9a2d1fab8 docs: add correct default cli ngModule (#31166)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+8.sha-8052de0.with-local-changes
+ * @license Angular v8.2.0-next.0+9.sha-9a2d1fa.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
