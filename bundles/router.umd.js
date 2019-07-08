5ee0ef853c14e2f80c8b48affcdf9abef14b695d
Jason Aden <jasonaden@google.com>
2019-07-08 17:19:10 +0000

    40aaa42f4 docs: api doc cleanup (#31377)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+5.sha-4b05ebc.with-local-changes
+ * @license Angular v8.2.0-next.0+7.sha-40aaa42.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.0-next.0+5.sha-4b05ebc.with-local-changes');
+    var VERSION = new i0.Version('8.2.0-next.0+7.sha-40aaa42.with-local-changes');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+5.sha-4b05ebc.with-local-changes
+ * @license Angular v8.2.0-next.0+7.sha-40aaa42.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
