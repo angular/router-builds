09a28a8cc871e4a14d44b50b64f0cfeac37b6ba0
Jason Aden <jasonaden@google.com>
2019-07-08 17:00:47 +0000

    e30f494a3 refactor(ivy): remove interpolation instructions (#31395)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+3.sha-1d3e227.with-local-changes
+ * @license Angular v8.2.0-next.0+4.sha-e30f494.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
