28a46781a14a1346d4ca83b1c1204dc89c31573d
Jason Aden <jasonaden@google.com>
2019-07-08 17:02:13 +0000

    e30f494a3 refactor(ivy): remove interpolation instructions (#31395)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+3.sha-1d3e227.with-local-changes
+ * @license Angular v8.2.0-next.0+4.sha-e30f494.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5718,7 +5718,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.0+3.sha-1d3e227.with-local-changes');
+var VERSION = new Version('8.2.0-next.0+4.sha-e30f494.with-local-changes');
 
 /**
  * @license
