dd9c8ab5b892957a58559dd76a2f7b4024ed1810
Jason Aden <jasonaden@google.com>
2019-07-09 16:59:29 +0000

    36d3062a4 ci(docs-infra): increase waiting time to reduce flakiness on CI (#31408)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+12.sha-e6f1b04.with-local-changes
+ * @license Angular v8.2.0-next.0+14.sha-36d3062.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
