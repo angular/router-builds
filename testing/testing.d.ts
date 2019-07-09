e2abbc89e79cc8086d22c79da088314ffaa202b9
Jason Aden <jasonaden@google.com>
2019-07-09 17:03:08 +0000

    24fd1a18a fix(ivy): handle namespaced imports correctly (#31367)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+7.sha-bdfbb92.with-local-changes
+ * @license Angular v8.1.0+9.sha-24fd1a1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
