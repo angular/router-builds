e62e0dd1057aa95f01671e76c50b8666ecf741fb
Jason Aden <jasonaden@google.com>
2019-07-09 21:14:37 +0000

    7724f7446 refactor(bazel): remove unused lockfile (#31458)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+15.sha-98a68ad.with-local-changes
+ * @license Angular v8.2.0-next.0+20.sha-7724f74.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.0-next.0+15.sha-98a68ad.with-local-changes');
+    var VERSION = new i0.Version('8.2.0-next.0+20.sha-7724f74.with-local-changes');
 
     /**
      * @license
