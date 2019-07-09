06333350b4ad881936f54f6a6a058619cfb04da6
Jason Aden <jasonaden@google.com>
2019-07-09 21:13:17 +0000

    7724f7446 refactor(bazel): remove unused lockfile (#31458)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+15.sha-98a68ad.with-local-changes
+ * @license Angular v8.2.0-next.0+20.sha-7724f74.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
