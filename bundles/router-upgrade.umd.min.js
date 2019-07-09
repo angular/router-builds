4df9c41d4dcc23c567e685252d3019f5c11a1a9f
Jason Aden <jasonaden@google.com>
2019-07-09 22:46:53 +0000

    989ebcbb6 fix(ivy): inconsistent value returned by DebugNode.context (#31442)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+17.sha-7f2330a.with-local-changes
+ * @license Angular v8.2.0-next.0+23.sha-989ebcb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+17.sha-7f2330a.with-local-changes
+ * @license Angular v8.2.0-next.0+23.sha-989ebcb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.0+17.sha-7f2330a.with-local-changes');
+var VERSION = new Version('8.2.0-next.0+23.sha-989ebcb.with-local-changes');
 
 /**
  * @license
