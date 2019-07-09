0ccef73be7f7c11b2878d44c752d6238060a4db0
Jason Aden <jasonaden@google.com>
2019-07-09 21:49:15 +0000

    2b44be984 fix(ivy): i18n should not alloc expando slots when there is no new var (#31451)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+20.sha-7724f74.with-local-changes
+ * @license Angular v8.2.0-next.0+24.sha-2b44be9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
