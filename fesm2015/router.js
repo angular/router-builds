ee6ca38c0b5a2af56a09505fd143f31f6a981073
Jason Aden <jasonaden@google.com>
2019-07-10 17:46:35 +0000

    c69e552a8 docs: release notes for the v8.1.1 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+24.sha-2b44be9.with-local-changes
+ * @license Angular v8.2.0-next.0+26.sha-c69e552.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.0+24.sha-2b44be9.with-local-changes');
+const VERSION = new Version('8.2.0-next.0+26.sha-c69e552.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+24.sha-2b44be9.with-local-changes
+ * @license Angular v8.2.0-next.0+26.sha-c69e552.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
