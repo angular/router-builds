d5543ea00ae2d3c213369c536f39b923428eb9f5
Matias Niemelä <matias@yearofmoo.com>
2019-07-10 22:48:36 +0000

    76e3b57a1 test(ivy): verify no translations are generated for bound attributes (#31481)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+2.sha-6da1446.with-local-changes
+ * @license Angular v8.2.0-next.1+1.sha-76e3b57.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
