e4af664fb5c8babb95577aafb2e9e440bbf0d323
Matias Niemelä <matias@yearofmoo.com>
2019-07-10 23:25:44 +0000

    53212db3e refactor(ivy): remove duplicate forward ref logic (#31423)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+3.sha-dee16a4.with-local-changes
+ * @license Angular v8.2.0-next.1+4.sha-53212db.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
