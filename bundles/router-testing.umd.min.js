8b2d648293961bbb6d08ae4558ff37365e50f131
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 16:10:14 +0000

    103a5b42e fix(ivy): ngcc - resolve `main` property paths correctly (#31509)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+8.sha-1cba5d4.with-local-changes
+ * @license Angular v8.2.0-next.1+7.sha-103a5b4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
