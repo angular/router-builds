323be53bdcbaf7c562c8c5243300675ef9c5b282
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 16:09:23 +0000

    1cba5d42d fix(ivy): handle rooted resource paths correctly (#31511)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+6.sha-fe17938.with-local-changes
+ * @license Angular v8.2.0-next.1+8.sha-1cba5d4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
