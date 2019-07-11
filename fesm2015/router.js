47ce2bc6924a020756aab08f93cdbbcdd3b66bd0
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 17:22:39 +0000

    a55c6df07 feat(docs-infra): add prominent archive mode banner styling (#31245)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+18.sha-e85fa5d.with-local-changes
+ * @license Angular v8.2.0-next.1+21.sha-a55c6df.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.1+18.sha-e85fa5d.with-local-changes');
+const VERSION = new Version('8.2.0-next.1+21.sha-a55c6df.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
