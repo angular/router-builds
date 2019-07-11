38def95fabf9da271cbd99a0d764fdb27e02d9b9
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 21:40:14 +0000

    58260fc4a build: remove redundant `@types/source-map` dependency (#31468)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+34.sha-3ade93f.with-local-changes
+ * @license Angular v8.1.0+36.sha-58260fc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0+34.sha-3ade93f.with-local-changes');
+const VERSION = new Version('8.1.0+36.sha-58260fc.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+34.sha-3ade93f.with-local-changes
+ * @license Angular v8.1.0+36.sha-58260fc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
