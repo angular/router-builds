714aa8d71b3bb2e1c44c6f0ddc91086e0d8b8dcf
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 21:39:12 +0000

    4bb283cbb build: remove redundant `@types/source-map` dependency (#31468)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+27.sha-565a58e.with-local-changes
+ * @license Angular v8.2.0-next.1+34.sha-4bb283c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.0-next.1+27.sha-565a58e.with-local-changes');
+    var VERSION = new core.Version('8.2.0-next.1+34.sha-4bb283c.with-local-changes');
 
     /**
      * @license
