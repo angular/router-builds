a11f2ccf91cac795be25eec5b20b7685c090e523
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 22:46:17 +0000

    d545bbeee fix(ivy): handle ModuleWithProviders in R3TestBed correctly (#31415)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+34.sha-4bb283c.with-local-changes
+ * @license Angular v8.2.0-next.1+35.sha-d545bbe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+34.sha-4bb283c.with-local-changes
+ * @license Angular v8.2.0-next.1+35.sha-d545bbe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.1+34.sha-4bb283c.with-local-changes');
+const VERSION = new Version('8.2.0-next.1+35.sha-d545bbe.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
