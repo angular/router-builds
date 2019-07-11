752d164f76055eb9fbd162886fd652679e5ab62e
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 21:38:07 +0000

    3ade93f6f ci(docs-infra): run a11y audits on certain pages on CI (#31414)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+29.sha-4aed480.with-local-changes
+ * @license Angular v8.1.0+34.sha-3ade93f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
