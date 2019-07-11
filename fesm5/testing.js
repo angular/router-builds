7d5cdc04a7841d45fe0c76af8d066dd6a177e100
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 21:34:33 +0000

    565a58e26 refactor(ivy): limit usage of global state (#31490)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+25.sha-6f50aad.with-local-changes
+ * @license Angular v8.2.0-next.1+27.sha-565a58e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
