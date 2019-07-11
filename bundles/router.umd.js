2ca0dae1587464517b2040ad3867b05ed2693439
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 19:28:06 +0000

    7c62a8f9c docs: remove heroes reference in i18n example (#31515)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+21.sha-a55c6df.with-local-changes
+ * @license Angular v8.2.0-next.1+22.sha-7c62a8f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.0-next.1+21.sha-a55c6df.with-local-changes');
+    var VERSION = new i0.Version('8.2.0-next.1+22.sha-7c62a8f.with-local-changes');
 
     /**
      * @license
