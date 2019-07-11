d66613db93be56e0cfdce538e97585561f92f973
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 16:11:34 +0000

    103a5b42e fix(ivy): ngcc - resolve `main` property paths correctly (#31509)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+8.sha-1cba5d4.with-local-changes
+ * @license Angular v8.2.0-next.1+7.sha-103a5b4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.0-next.1+8.sha-1cba5d4.with-local-changes');
+    var VERSION = new i0.Version('8.2.0-next.1+7.sha-103a5b4.with-local-changes');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+8.sha-1cba5d4.with-local-changes
+ * @license Angular v8.2.0-next.1+7.sha-103a5b4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8274,7 +8274,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.1+8.sha-1cba5d4.with-local-changes');
+const VERSION = new Version('8.2.0-next.1+7.sha-103a5b4.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
