ca48621ecf453ebce785f88f5cd0b28d9aa87aeb
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 16:03:52 +0000

    b591035e4 docs: add angular-in-depth to community curations (#31212)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+14.sha-a395cb1.with-local-changes
+ * @license Angular v8.1.0+16.sha-b591035.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0+14.sha-a395cb1.with-local-changes');
+const VERSION = new Version('8.1.0+16.sha-b591035.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+14.sha-a395cb1.with-local-changes
+ * @license Angular v8.1.0+16.sha-b591035.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
