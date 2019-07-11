dbd573c3bde96b313e8ecdbe218e719ff12256be
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 05:55:33 +0000

    a395cb1a5 docs: add Renderer to deprecations doc (#31419)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+12.sha-1863254.with-local-changes
+ * @license Angular v8.1.0+14.sha-a395cb1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
