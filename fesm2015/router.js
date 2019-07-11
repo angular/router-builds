5e29f36078287d2bd17126c36cd23eb1c13bb4c0
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 21:33:13 +0000

    565a58e26 refactor(ivy): limit usage of global state (#31490)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+25.sha-6f50aad.with-local-changes
+ * @license Angular v8.2.0-next.1+27.sha-565a58e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.1+25.sha-6f50aad.with-local-changes');
+const VERSION = new Version('8.2.0-next.1+27.sha-565a58e.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
