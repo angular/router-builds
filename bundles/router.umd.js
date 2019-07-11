d6efe6cb95075d2c7b69695ac86837cd7da31199
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 05:56:40 +0000

    68940f05d docs: add Renderer to deprecations doc (#31419)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+4.sha-53212db.with-local-changes
+ * @license Angular v8.2.0-next.1+5.sha-68940f0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.0-next.1+4.sha-53212db.with-local-changes');
+    var VERSION = new core.Version('8.2.0-next.1+5.sha-68940f0.with-local-changes');
 
     /**
      * @license
