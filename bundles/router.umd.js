3463879207830945c9be40528991ca7e32806ba0
Matias Niemelä <matias@yearofmoo.com>
2019-07-12 15:58:38 +0000

    63e458dd3 fix(ivy): handle ICUs with placeholders in case other nested ICUs are present (#31516)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+35.sha-d545bbe.with-local-changes
+ * @license Angular v8.2.0-next.1+36.sha-63e458d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5791,7 +5791,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.0-next.1+35.sha-d545bbe.with-local-changes');
+    var VERSION = new i0.Version('8.2.0-next.1+36.sha-63e458d.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+35.sha-d545bbe.with-local-changes
+ * @license Angular v8.2.0-next.1+36.sha-63e458d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
