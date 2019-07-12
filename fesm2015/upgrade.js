cf5a7d2263c819f02ac65898535310a5a333da75
Matias Niemelä <matias@yearofmoo.com>
2019-07-12 22:18:47 +0000

    098659554 fix(docs-infra): fix API list search color and styles (#31272)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+44.sha-2ed8127.with-local-changes
+ * @license Angular v8.1.0+42.sha-0986595.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
