ea2b9a8fb3c3080eb06aa593f690a1d8d73e89be
Matias Niemelä <matias@yearofmoo.com>
2019-07-12 22:16:20 +0000

    604d9063c feat(ivy): index template elements for selectors, attributes, directives (#31240)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+48.sha-c529be9.with-local-changes
+ * @license Angular v8.2.0-next.1+45.sha-604d906.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
