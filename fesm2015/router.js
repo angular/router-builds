6b6adb8f5880850208fb3ee9d16a836559002313
Matias Niemelä <matias@yearofmoo.com>
2019-07-12 22:18:11 +0000

    b65e11e3c build(docs-infra): display CLI positional option enum values (#31529)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+45.sha-604d906.with-local-changes
+ * @license Angular v8.2.0-next.1+47.sha-b65e11e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8274,7 +8274,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.1+45.sha-604d906.with-local-changes');
+const VERSION = new Version('8.2.0-next.1+47.sha-b65e11e.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
