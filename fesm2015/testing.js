f85160fd31024c0ecd851c58e62e38f1e2ac074b
Matias Niemelä <matias@yearofmoo.com>
2019-07-12 22:18:06 +0000

    2ed812745 docs: add microsyntax details (#31517)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+43.sha-6737a8d.with-local-changes
+ * @license Angular v8.1.0+44.sha-2ed8127.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
