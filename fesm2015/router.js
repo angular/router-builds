68a9409feaa1f71dc408bb571114d02dd5b77605
Matias Niemelä <matias@yearofmoo.com>
2019-07-15 21:13:09 +0000

    78a809808 docs: include svg files in stackblitz and download examples (#31559)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+48.sha-781cbf8.with-local-changes
+ * @license Angular v8.1.0+49.sha-78a8098.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0+48.sha-781cbf8.with-local-changes');
+const VERSION = new Version('8.1.0+49.sha-78a8098.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
