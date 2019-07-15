3db8996368ffe78a891d4c3144b31ba01b1adc3d
Matias Niemelä <matias@yearofmoo.com>
2019-07-15 16:54:49 +0000

    d6c80871f docs: remove and update resource list (#31469)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+47.sha-b65e11e.with-local-changes
+ * @license Angular v8.2.0-next.1+50.sha-d6c8087.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
