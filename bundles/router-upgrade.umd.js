6eeaa8b0b3f3428142e1b3cdd9c4e27f0a7a41ef
Matias Niemelä <matias@yearofmoo.com>
2019-07-15 20:48:13 +0000

    31ea254a0 fix(ivy): align NgModule assertions with ViewEngine (#31543)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+23.sha-989ebcb.with-local-changes
+ * @license Angular v8.2.0-next.1+52.sha-31ea254.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+23.sha-989ebcb.with-local-changes
+ * @license Angular v8.2.0-next.1+52.sha-31ea254.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.0-next.0+23.sha-989ebcb.with-local-changes');
+    var VERSION = new core.Version('8.2.0-next.1+52.sha-31ea254.with-local-changes');
 
     /**
      * @license
