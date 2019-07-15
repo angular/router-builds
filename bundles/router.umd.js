5c59bfa8b856fd96dc0439889e79f035a459cab8
Matias Niemelä <matias@yearofmoo.com>
2019-07-15 22:58:00 +0000

    3166cffd2 fix(compiler-cli): Return original sourceFile instead of redirected sourceFile from getSourceFile (#26036)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+54.sha-40705f3.with-local-changes
+ * @license Angular v8.2.0-next.1+55.sha-3166cff.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.0-next.1+54.sha-40705f3.with-local-changes');
+    var VERSION = new core.Version('8.2.0-next.1+55.sha-3166cff.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+54.sha-40705f3.with-local-changes
+ * @license Angular v8.2.0-next.1+55.sha-3166cff.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
