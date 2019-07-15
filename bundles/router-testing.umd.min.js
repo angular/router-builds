1966268746fe45dd6986ba228c7c9742de27f978
Matias Niemelä <matias@yearofmoo.com>
2019-07-15 21:28:38 +0000

    40705f336 docs: include svg files in stackblitz and download examples (#31559)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+53.sha-19bc111.with-local-changes
+ * @license Angular v8.2.0-next.1+54.sha-40705f3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
