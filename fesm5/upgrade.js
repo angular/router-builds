1d33a1b71812a2c4593bbf9de287eb0d41deb78f
Matias Niemelä <matias@yearofmoo.com>
2019-07-15 17:00:12 +0000

    7c7774ff2 docs: remove and update resource list (#31469)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+42.sha-0986595.with-local-changes
+ * @license Angular v8.1.0+46.sha-7c7774f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
