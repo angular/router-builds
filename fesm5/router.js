4a71d2f7bb3cab774cf9be735149087523f17a22
Matias Niemelä <matias@yearofmoo.com>
2019-07-15 22:59:23 +0000

    3166cffd2 fix(compiler-cli): Return original sourceFile instead of redirected sourceFile from getSourceFile (#26036)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+54.sha-40705f3.with-local-changes
+ * @license Angular v8.2.0-next.1+55.sha-3166cff.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5718,7 +5718,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.1+54.sha-40705f3.with-local-changes');
+var VERSION = new Version('8.2.0-next.1+55.sha-3166cff.with-local-changes');
 
 /**
  * @license
