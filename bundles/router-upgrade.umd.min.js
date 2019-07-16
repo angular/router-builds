1c1e21d64e32f3859469d9f35f38806d45027d48
Matias Niemelä <matias@yearofmoo.com>
2019-07-16 17:24:13 +0000

    177cf26e4 build(docs-infra): never show linenums for triple-backticked code blocks (#31493)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+50.sha-13dbb98.with-local-changes
+ * @license Angular v8.1.0+52.sha-177cf26.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+50.sha-13dbb98.with-local-changes
+ * @license Angular v8.1.0+52.sha-177cf26.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0+50.sha-13dbb98.with-local-changes');
+var VERSION = new Version('8.1.0+52.sha-177cf26.with-local-changes');
 
 /**
  * @license
