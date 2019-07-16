421a63999aef3906aa855ad4809944b61dd71fa6
Matias Niemelä <matias@yearofmoo.com>
2019-07-16 17:28:18 +0000

    f166b6d8f docs: add note about current working directory for cli projects (#31507)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+60.sha-09576e9.with-local-changes
+ * @license Angular v8.2.0-next.1+61.sha-f166b6d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5750,7 +5750,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.1+60.sha-09576e9.with-local-changes');
+var VERSION = new Version('8.2.0-next.1+61.sha-f166b6d.with-local-changes');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+60.sha-09576e9.with-local-changes
+ * @license Angular v8.2.0-next.1+61.sha-f166b6d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
