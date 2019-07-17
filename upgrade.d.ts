4b5b3b91121520aa270b3564bf1b4b6ded2de111
Matias Niemelä <matias@yearofmoo.com>
2019-07-17 17:50:02 +0000

    e1cfa419d docs(core): fix typo in event-binding code example (#31611)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+57.sha-00db95e.with-local-changes
+ * @license Angular v8.1.0+58.sha-e1cfa41.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
