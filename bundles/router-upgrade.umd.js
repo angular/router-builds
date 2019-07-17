8ae9664e2dae4b6ef76eba45721255a9dc705e27
Matias Niemelä <matias@yearofmoo.com>
2019-07-17 17:49:21 +0000

    00db95e77 docs: fix typo in CLI overview (#31608)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+54.sha-cf460d8.with-local-changes
+ * @license Angular v8.1.0+57.sha-00db95e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
