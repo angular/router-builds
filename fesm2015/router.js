a4e23b826a21a56aa3cd2b016fe1b343cf267bbe
Matias Niemelä <matias@yearofmoo.com>
2019-07-17 21:49:15 +0000

    e8ae3c5f2 release: cut the v8.2.0-next.2 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+68.sha-fc61284.with-local-changes
+ * @license Angular v8.2.0-next.2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8086,7 +8086,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.1+68.sha-fc61284.with-local-changes');
+const VERSION = new Version('8.2.0-next.2.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+68.sha-fc61284.with-local-changes
+ * @license Angular v8.2.0-next.2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
