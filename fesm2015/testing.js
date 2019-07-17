f55f7012631256173b37632d24c264bb08a4139a
Matias Niemelä <matias@yearofmoo.com>
2019-07-17 17:46:51 +0000

    32c07ceca docs(core): fix typo in event-binding code example (#31611)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+67.sha-12fd069.with-local-changes
+ * @license Angular v8.2.0-next.1+70.sha-32c07ce.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
