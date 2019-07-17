c681121caa1d25478e8149f24169b76611280ec8
Matias Niemelä <matias@yearofmoo.com>
2019-07-17 04:20:35 +0000

    12fd06916 fix(ivy): don't match directives against attribute bindings (#31541)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+66.sha-9e83822.with-local-changes
+ * @license Angular v8.2.0-next.1+67.sha-12fd069.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
