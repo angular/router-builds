9f90cad6373c06616d9107e5fbe82d7ed4eee2e3
Matias Niemelä <matias@yearofmoo.com>
2019-07-17 17:46:18 +0000

    fc61284db docs: remove prerequisite section in NgModules docs (#31169)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+70.sha-32c07ce.with-local-changes
+ * @license Angular v8.2.0-next.1+68.sha-fc61284.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+70.sha-32c07ce.with-local-changes
+ * @license Angular v8.2.0-next.1+68.sha-fc61284.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.0-next.1+70.sha-32c07ce.with-local-changes');
+    var VERSION = new core.Version('8.2.0-next.1+68.sha-fc61284.with-local-changes');
 
     /**
      * @license
