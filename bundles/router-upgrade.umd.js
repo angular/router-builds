cf99867a4dbe43af8dbc06866fd954bf66fd687c
Miško Hevery <misko@hevery.com>
2019-07-18 21:45:15 +0000

    376ad9c3c refactor(ivy): remove deep imports into the compiler (#31376)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+22.sha-60f58bf.with-local-changes
+ * @license Angular v8.2.0-next.2+24.sha-376ad9c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
