fd9f61d0bcc03d58852e2c8e10bdb978afd5092e
Miško Hevery <misko@hevery.com>
2019-07-18 21:36:15 +0000

    78e7fdd98 fix(core): allow Z variations of CSS transforms in sanitizer (#29264) PR Close #29264

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+7.sha-1e9eeaf.with-local-changes
+ * @license Angular v8.2.0-next.2+8.sha-78e7fdd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5739,7 +5739,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.0-next.2+7.sha-1e9eeaf.with-local-changes');
+    var VERSION = new i0.Version('8.2.0-next.2+8.sha-78e7fdd.with-local-changes');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+7.sha-1e9eeaf.with-local-changes
+ * @license Angular v8.2.0-next.2+8.sha-78e7fdd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
