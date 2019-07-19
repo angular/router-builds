15341062d49b8c0bdf46277e0be550a55a3ba007
Miško Hevery <misko@hevery.com>
2019-07-19 21:20:29 +0000

    f10d6c66c ci: add info about `test_saucelabs_bazel` being limited to master builds (#31651)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+30.sha-4495a46.with-local-changes
+ * @license Angular v8.2.0-next.2+31.sha-f10d6c6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
