48d50415ff4269dda5f263e78160308204e8ae05
Miško Hevery <misko@hevery.com>
2019-07-19 00:09:39 +0000

    ae3bcd858  docs: rephrased explanation to fork before save (#31384)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+8.sha-434b796.with-local-changes
+ * @license Angular v8.1.2+12.sha-ae3bcd8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+8.sha-434b796.with-local-changes
+ * @license Angular v8.1.2+12.sha-ae3bcd8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8026,7 +8026,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.2+8.sha-434b796.with-local-changes');
+const VERSION = new Version('8.1.2+12.sha-ae3bcd8.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
