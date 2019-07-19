c10f33cb0114b02985427a2d1a80139e034f124d
Miško Hevery <misko@hevery.com>
2019-07-19 17:22:42 +0000

    157456b39 ci: add info about `test_saucelabs_bazel` being limited to master builds (#31651)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+11.sha-b844f5f.with-local-changes
+ * @license Angular v8.1.2+13.sha-157456b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+11.sha-b844f5f.with-local-changes
+ * @license Angular v8.1.2+13.sha-157456b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5698,7 +5698,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.2+11.sha-b844f5f.with-local-changes');
+var VERSION = new Version('8.1.2+13.sha-157456b.with-local-changes');
 
 /**
  * @license
