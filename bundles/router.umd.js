aaee29b2228347ab78743051a482f221f59ee8b9
Miško Hevery <misko@hevery.com>
2019-07-19 21:21:51 +0000

    f10d6c66c ci: add info about `test_saucelabs_bazel` being limited to master builds (#31651)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+30.sha-4495a46.with-local-changes
+ * @license Angular v8.2.0-next.2+31.sha-f10d6c6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5742,7 +5742,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.0-next.2+30.sha-4495a46.with-local-changes');
+    var VERSION = new i0.Version('8.2.0-next.2+31.sha-f10d6c6.with-local-changes');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+30.sha-4495a46.with-local-changes
+ * @license Angular v8.2.0-next.2+31.sha-f10d6c6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8217,7 +8217,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.2+30.sha-4495a46.with-local-changes');
+const VERSION = new Version('8.2.0-next.2+31.sha-f10d6c6.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
