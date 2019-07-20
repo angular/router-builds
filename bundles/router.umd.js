beefdaeaa7edcb1502ca14bfa418dc9d059a0ebd
Kara Erickson <karakara@google.com>
2019-07-20 04:54:03 +0000

    cb848b941 refactor(ivy): reduce code duplication and code size (#31489)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+33.sha-9c954eb.with-local-changes
+ * @license Angular v8.2.0-next.2+35.sha-cb848b9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5742,7 +5742,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.0-next.2+33.sha-9c954eb.with-local-changes');
+    var VERSION = new i0.Version('8.2.0-next.2+35.sha-cb848b9.with-local-changes');
 
     /**
      * @license
