9279c203a6b6f3858ee0c1d70caeb30965b53d55
Kara Erickson <karakara@google.com>
2019-07-20 04:52:38 +0000

    cb848b941 refactor(ivy): reduce code duplication and code size (#31489)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+33.sha-9c954eb.with-local-changes
+ * @license Angular v8.2.0-next.2+35.sha-cb848b9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
