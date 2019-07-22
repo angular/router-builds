1558b920426cb6c895af266b74ae6c8d05d366a6
Miško Hevery <misko@hevery.com>
2019-07-22 17:17:24 +0000

    8b26447c4 perf(ivy): avoid using array splice (#31656)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+42.sha-c845a7b.with-local-changes
+ * @license Angular v8.2.0-next.2+41.sha-8b26447.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
