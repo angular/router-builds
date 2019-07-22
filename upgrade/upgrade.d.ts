90a0a4b060a95978464bdf244ed7915db8d4bec5
Kara Erickson <karakara@google.com>
2019-07-22 19:34:19 +0000

    5a8eb924b fix(bazel): increase memory limit of ngc under bazel from 2 to 4 GB (#31784)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+41.sha-8b26447.with-local-changes
+ * @license Angular v8.2.0-next.2+43.sha-5a8eb92.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
