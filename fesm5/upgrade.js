7aaa38e47319ce3148dd0c26b8cf1b7e854fe8b5
Miško Hevery <misko@hevery.com>
2019-07-22 17:06:55 +0000

    8f084d721 docs: clarify toh (#28571)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+35.sha-cb848b9.with-local-changes
+ * @license Angular v8.2.0-next.2+36.sha-8f084d7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
