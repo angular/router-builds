8f9ac12f987e0c53326b0c2f9583fc7ebd2336b6
Miško Hevery <misko@hevery.com>
2019-07-22 17:11:07 +0000

    ebc71f7ca fix(docs-infra): fix mobile toc styles (#31533)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+36.sha-8f084d7.with-local-changes
+ * @license Angular v8.2.0-next.2+38.sha-ebc71f7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5742,7 +5742,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.0-next.2+36.sha-8f084d7.with-local-changes');
+    var VERSION = new i0.Version('8.2.0-next.2+38.sha-ebc71f7.with-local-changes');
 
     /**
      * @license
