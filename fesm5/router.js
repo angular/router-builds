be1e0c7f907a8d61f8f7283c22fd9a8d2e0a89cc
Miško Hevery <misko@hevery.com>
2019-07-22 17:08:20 +0000

    8f084d721 docs: clarify toh (#28571)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+35.sha-cb848b9.with-local-changes
+ * @license Angular v8.2.0-next.2+36.sha-8f084d7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5669,7 +5669,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.2+35.sha-cb848b9.with-local-changes');
+var VERSION = new Version('8.2.0-next.2+36.sha-8f084d7.with-local-changes');
 
 /**
  * @license
