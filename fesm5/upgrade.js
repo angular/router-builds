69fb79f26feca982027a1f4e9f9193084c6999f4
Kara Erickson <karakara@google.com>
2019-07-23 23:08:12 +0000

    f50dede8f refactor(ivy): remove all old styling code prior to refactor (#31193)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+46.sha-0e68c7e.with-local-changes
+ * @license Angular v8.2.0-next.2+47.sha-f50dede.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
