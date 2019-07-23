812b5caf7e45d5ecba96b6ee1abeebbf456877c9
Kara Erickson <karakara@google.com>
2019-07-23 23:11:25 +0000

    24ca582bc perf(compiler): avoid copying from prototype while cloning an object (#31638)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+47.sha-f50dede.with-local-changes
+ * @license Angular v8.2.0-next.2+51.sha-24ca582.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5742,7 +5742,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.0-next.2+47.sha-f50dede.with-local-changes');
+    var VERSION = new i0.Version('8.2.0-next.2+51.sha-24ca582.with-local-changes');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+47.sha-f50dede.with-local-changes
+ * @license Angular v8.2.0-next.2+51.sha-24ca582.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8217,7 +8217,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.2+47.sha-f50dede.with-local-changes');
+const VERSION = new Version('8.2.0-next.2+51.sha-24ca582.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
