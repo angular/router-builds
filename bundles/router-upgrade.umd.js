86653e83b957ba2f218e6c2c630b2eaf52a06ce0
Kara Erickson <karakara@google.com>
2019-07-23 23:13:54 +0000

    f69e4e6f7 refactor(core): move google3 migration rules into single directory (#30956)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+52.sha-221782a.with-local-changes
+ * @license Angular v8.2.0-next.2+55.sha-f69e4e6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
