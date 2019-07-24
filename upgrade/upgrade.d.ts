ec35417bca405251987fc493af43aa6c018e5d01
Miško Hevery <misko@hevery.com>
2019-07-24 21:54:56 +0000

    29e1c53a3 feat(upgrade): support $element in upgraded component template/templateUrl functions (#31637)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+72.sha-2bb9a65.with-local-changes
+ * @license Angular v8.2.0-next.2+74.sha-29e1c53.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
