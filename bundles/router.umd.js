41e609a556640ccea5ffe59dd60fe0d7c1fb9231
Miško Hevery <misko@hevery.com>
2019-07-24 21:52:13 +0000

    2bb9a6535 fix(zone.js): don't wrap uncaught promise error. (#31443)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+69.sha-b6aeace.with-local-changes
+ * @license Angular v8.2.0-next.2+72.sha-2bb9a65.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5789,7 +5789,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.0-next.2+69.sha-b6aeace.with-local-changes');
+    var VERSION = new core.Version('8.2.0-next.2+72.sha-2bb9a65.with-local-changes');
 
     /**
      * @license
