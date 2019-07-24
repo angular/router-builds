11459f110dd425484adf72723e708146e9eda33d
Misko Hevery <misko@hevery.com>
2019-07-24 04:37:34 +0000

    82055b2fb feat(bazel): compile targets used for indexing by Kythe with Ivy (#31786)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+68.sha-5ae9b76.with-local-changes
+ * @license Angular v8.2.0-next.2+67.sha-82055b2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
