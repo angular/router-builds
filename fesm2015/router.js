63315ed9b8fbe785753687eabca4b301f50c680c
Misko Hevery <misko@hevery.com>
2019-07-24 04:38:09 +0000

    77143b5a8 docs: add ngxs to library section (#31792) PR Close #31792

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+15.sha-a9f3547.with-local-changes
+ * @license Angular v8.1.2+24.sha-77143b5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8026,7 +8026,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.2+15.sha-a9f3547.with-local-changes');
+const VERSION = new Version('8.1.2+24.sha-77143b5.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+15.sha-a9f3547.with-local-changes
+ * @license Angular v8.1.2+24.sha-77143b5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
