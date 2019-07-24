b12ffe404f301cafca09e7598913214d0f3feab0
Miško Hevery <misko@hevery.com>
2019-07-24 21:56:25 +0000

    29e1c53a3 feat(upgrade): support $element in upgraded component template/templateUrl functions (#31637)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+72.sha-2bb9a65.with-local-changes
+ * @license Angular v8.2.0-next.2+74.sha-29e1c53.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8217,7 +8217,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.2+72.sha-2bb9a65.with-local-changes');
+const VERSION = new Version('8.2.0-next.2+74.sha-29e1c53.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
