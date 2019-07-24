801cd4e149fda4a08aab6b05e1031a640dd9496c
Miško Hevery <misko@hevery.com>
2019-07-24 22:18:57 +0000

    9e10d0307 fix(zone.js): handle MSPointer event correctly (#31722)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+30.sha-53286d1.with-local-changes
+ * @license Angular v8.1.2+31.sha-9e10d03.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
