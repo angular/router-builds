5b5cceecd327b4658f80d055b4b2d776f537c980
Miško Hevery <misko@hevery.com>
2019-07-24 22:02:13 +0000

    716af1059 fix(zone.js): move property patch to legacy (#31660)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+73.sha-87ce4e9.with-local-changes
+ * @license Angular v8.2.0-next.2+77.sha-716af10.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
