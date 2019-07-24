3829a13bac312680d71443eb238de3d3cd18d0c1
Miško Hevery <misko@hevery.com>
2019-07-24 22:18:23 +0000

    35a025fbc fix(zone.js): patch shadydom (#31717)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+81.sha-a86850e.with-local-changes
+ * @license Angular v8.2.0-next.2+82.sha-35a025f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8029,7 +8029,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.2+81.sha-a86850e.with-local-changes');
+const VERSION = new Version('8.2.0-next.2+82.sha-35a025f.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
