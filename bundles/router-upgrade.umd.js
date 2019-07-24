4bf0900866266f060c490bd257c329f3572c5e5b
Miško Hevery <misko@hevery.com>
2019-07-24 22:15:50 +0000

    53286d1ef fix(zone.js): patch shadydom (#31717)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+28.sha-c73b75f.with-local-changes
+ * @license Angular v8.1.2+30.sha-53286d1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
