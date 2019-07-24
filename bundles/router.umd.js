4b39bbdf3ce46dc10d18dd67e09482516184a0e9
Miško Hevery <misko@hevery.com>
2019-07-24 23:58:21 +0000

    fdf2f699c fix(zone.js): don't fire unhandledrejection if Zone handled error (#31718)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+31.sha-9e10d03.with-local-changes
+ * @license Angular v8.1.2+33.sha-fdf2f69.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5786,7 +5786,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.1.2+31.sha-9e10d03.with-local-changes');
+    var VERSION = new core.Version('8.1.2+33.sha-fdf2f69.with-local-changes');
 
     /**
      * @license
