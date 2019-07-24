44beb82c6199e42a63b87321dbe892c6584867c6
Miško Hevery <misko@hevery.com>
2019-07-24 22:07:32 +0000

    a86850e3f refactor(docs-infra): remove obsolete `format` attribute from code examples (#31674)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+83.sha-2c402d5.with-local-changes
+ * @license Angular v8.2.0-next.2+81.sha-a86850e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5742,7 +5742,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.0-next.2+83.sha-2c402d5.with-local-changes');
+    var VERSION = new i0.Version('8.2.0-next.2+81.sha-a86850e.with-local-changes');
 
     /**
      * @license
