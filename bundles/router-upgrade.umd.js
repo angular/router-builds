f7de7cebdd223aeb3b6f9979730c9ad32a874879
Miško Hevery <misko@hevery.com>
2019-07-24 22:04:58 +0000

    c73b75f27 fix(zone.js): should remove on symbol property after removeAllListeners (#31644)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+25.sha-0440dc9.with-local-changes
+ * @license Angular v8.1.2+28.sha-c73b75f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+25.sha-0440dc9.with-local-changes
+ * @license Angular v8.1.2+28.sha-c73b75f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5786,7 +5786,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.1.2+25.sha-0440dc9.with-local-changes');
+    var VERSION = new core.Version('8.1.2+28.sha-c73b75f.with-local-changes');
 
     /**
      * @license
