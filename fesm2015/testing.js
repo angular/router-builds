1f53a37ce2872a3124c006cfb5014b2e7bc16a8d
Misko Hevery <misko@hevery.com>
2019-07-24 18:00:55 +0000

    b696413a7 fix(ivy): support attribute selectors in dynamic component creation (#31812)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+55.sha-f69e4e6.with-local-changes
+ * @license Angular v8.2.0-next.2+70.sha-b696413.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
