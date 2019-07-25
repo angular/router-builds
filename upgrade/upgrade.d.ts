de014ec97758566570f8c9d236d72f9924d90d77
Miško Hevery <misko@hevery.com>
2019-07-25 21:55:55 +0000

    6b67cd562 feat(ivy): index template references, variables, bound attributes/events (#31535)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+85.sha-c7542a1.with-local-changes
+ * @license Angular v8.2.0-next.2+89.sha-6b67cd5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
