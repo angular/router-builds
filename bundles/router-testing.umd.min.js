c45564428a8d66eea5dbb82c1b4a961e562f06fb
Misko Hevery <misko@hevery.com>
2019-07-25 23:24:36 +0000

    55fab0a5c build: lock material unit tests job to specific commit (#31569) (#31847)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+33.sha-fdf2f69.with-local-changes
+ * @license Angular v8.1.2+35.sha-55fab0a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+33.sha-fdf2f69.with-local-changes
+ * @license Angular v8.1.2+35.sha-55fab0a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5698,7 +5698,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.2+33.sha-fdf2f69.with-local-changes');
+var VERSION = new Version('8.1.2+35.sha-55fab0a.with-local-changes');
 
 /**
  * @license
