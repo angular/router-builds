8104502ff1bc1e8f604ca0080e2e49829732fbdc
Miško Hevery <misko@hevery.com>
2019-07-25 20:28:51 +0000

    85d051f8d perf(ivy): introduce new benchmark for view create, destroy, traverse (#31797)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+78.sha-3d7303e.with-local-changes
+ * @license Angular v8.2.0-next.2+86.sha-85d051f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
