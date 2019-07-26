066eb01d73c57b787a0d67fd7e2545292bb26aed
Andrew Kushnir <akushnir@google.com>
2019-07-26 22:22:42 +0000

    dcbc28f72 build: update to nodejs rules 0.34.0 and bazel 0.28.1 (#31824)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+5.sha-9bdffb1.with-local-changes
+ * @license Angular v8.2.0-rc.0+6.sha-dcbc28f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
