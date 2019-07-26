97331166b3f1b33eac5f4278eea233a51611b543
Andrew Kushnir <akushnir@google.com>
2019-07-26 21:29:02 +0000

    5296c04f6 fix(ivy): set LOCALE_ID when using the injector (#31566)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+101.sha-4da8052.with-local-changes
+ * @license Angular v8.2.0-next.2+103.sha-5296c04.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8217,7 +8217,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.2+101.sha-4da8052.with-local-changes');
+const VERSION = new Version('8.2.0-next.2+103.sha-5296c04.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+101.sha-4da8052.with-local-changes
+ * @license Angular v8.2.0-next.2+103.sha-5296c04.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
