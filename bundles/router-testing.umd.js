6bef362d3ec029df08ad6c85932256139960011b
Miško Hevery <misko@hevery.com>
2019-07-26 17:46:15 +0000

    6033446d2 docs: release notes for the v8.2.0-rc.0 release

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+95.sha-174770e.with-local-changes
+ * @license Angular v8.2.0-next.2+96.sha-6033446.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
