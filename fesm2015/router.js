d1c994d4695dac0f4287f0969ba0fbca5c352d91
Miško Hevery <misko@hevery.com>
2019-07-26 00:40:46 +0000

    3a2b195a5 feat(ivy): translate type-check diagnostics to their original source (#30181)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+89.sha-6b67cd5.with-local-changes
+ * @license Angular v8.2.0-next.2+93.sha-3a2b195.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8029,7 +8029,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.2+89.sha-6b67cd5.with-local-changes');
+const VERSION = new Version('8.2.0-next.2+93.sha-3a2b195.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
