b45b563e152c84cebba4e6ade1e0d926d3a8ce6c
Andrew Kushnir <akushnir@google.com>
2019-07-26 19:43:05 +0000

    397d0ba9a test(ivy): fix broken testcase in Windows (#31860)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+98.sha-859ebdd.with-local-changes
+ * @license Angular v8.2.0-next.2+99.sha-397d0ba.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5701,7 +5701,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.2+98.sha-859ebdd.with-local-changes');
+var VERSION = new Version('8.2.0-next.2+99.sha-397d0ba.with-local-changes');
 
 /**
  * @license
