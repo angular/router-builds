2b6c1d9b848413a5c75cd5c21303136e7ef96121
Andrew Kushnir <akushnir@google.com>
2019-07-26 22:23:17 +0000

    5f0d5e9cc build: update to nodejs rules 0.34.0 and bazel 0.28.1 (#31824)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+103.sha-5296c04.with-local-changes
+ * @license Angular v8.2.0-next.2+104.sha-5f0d5e9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
