6288e8c7b45775eefa2741c41586fa23f6428ce4
Andrew Kushnir <akushnir@google.com>
2019-07-26 19:34:41 +0000

    2647f708b build(docs-infra): upgrade cli command docs sources to 6b6b8f828 (#31856)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+1.sha-fa15814.with-local-changes
+ * @license Angular v8.2.0-rc.0+2.sha-2647f70.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
