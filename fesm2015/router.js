2835f490946bbb45d7d4f4474feecd5ba06acb3e
Miško Hevery <misko@hevery.com>
2019-07-26 17:47:48 +0000

    6033446d2 docs: release notes for the v8.2.0-rc.0 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+95.sha-174770e.with-local-changes
+ * @license Angular v8.2.0-next.2+96.sha-6033446.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8217,7 +8217,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.2+95.sha-174770e.with-local-changes');
+const VERSION = new Version('8.2.0-next.2+96.sha-6033446.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
