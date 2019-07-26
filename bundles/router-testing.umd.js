6437c14ace259e38d8a8881960d84be3ba24dc9d
Andrew Kushnir <akushnir@google.com>
2019-07-26 21:21:51 +0000

    155f40c17 docs: fix header mistake (#31811)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+3.sha-8c446b0.with-local-changes
+ * @license Angular v8.2.0-rc.0+4.sha-155f40c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
