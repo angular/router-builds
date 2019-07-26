482d6a32523bd8046bc8cb16f05d5c53e2cab866
Andrew Kushnir <akushnir@google.com>
2019-07-26 21:21:52 +0000

    4da805243 docs(core): clarify @ContentChild(ren) behavior (#31846)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+99.sha-397d0ba.with-local-changes
+ * @license Angular v8.2.0-next.2+101.sha-4da8052.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
