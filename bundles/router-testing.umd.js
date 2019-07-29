171da834e6b73a6f44ee8a4545fcd4cef004837b
Andrew Kushnir <akushnir@google.com>
2019-07-29 23:31:09 +0000

    06c49b4a6 build: secure yarn lock files (#31640)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+7.sha-46b160e.with-local-changes
+ * @license Angular v8.2.0-rc.0+9.sha-06c49b4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
