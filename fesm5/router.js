1bb0730a5e2e4d803cee7696a07a7e01387f7e8c
Andrew Kushnir <akushnir@google.com>
2019-07-29 17:28:01 +0000

    14dba72ae fix(core): DebugElement.listeners not cleared on destroy (#31820)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+104.sha-5f0d5e9.with-local-changes
+ * @license Angular v8.2.0-next.2+105.sha-14dba72.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5669,7 +5669,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.2+104.sha-5f0d5e9.with-local-changes');
+var VERSION = new Version('8.2.0-next.2+105.sha-14dba72.with-local-changes');
 
 /**
  * @license
