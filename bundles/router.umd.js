6d01896c2725c2830f53a4629a0849fce1c14274
Andrew Kushnir <akushnir@google.com>
2019-07-29 17:24:41 +0000

    46b160e92 fix(core): DebugElement.listeners not cleared on destroy (#31820)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+6.sha-dcbc28f.with-local-changes
+ * @license Angular v8.2.0-rc.0+7.sha-46b160e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5789,7 +5789,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.0-rc.0+6.sha-dcbc28f.with-local-changes');
+    var VERSION = new core.Version('8.2.0-rc.0+7.sha-46b160e.with-local-changes');
 
     /**
      * @license
