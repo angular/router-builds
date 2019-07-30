09cfc74c40decfaf5b31a685f3979c0e97eafbe3
Andrew Kushnir <akushnir@google.com>
2019-07-30 20:21:59 +0000

    4f42eb4e7 test(zone.js): add test codes to ensure not include sourcemap (#31892)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+125.sha-185b3dd.with-local-changes
+ * @license Angular v8.2.0-next.2+116.sha-4f42eb4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8029,7 +8029,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.2+125.sha-185b3dd.with-local-changes');
+const VERSION = new Version('8.2.0-next.2+116.sha-4f42eb4.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
