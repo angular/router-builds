5008493c94cbfbae9293081345a749d0f689114e
Andrew Kushnir <akushnir@google.com>
2019-07-30 22:27:14 +0000

    03e8a31bf Revert "fix(zone.js): don't wrap uncaught promise error. (#31443)" (#31918)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+12.sha-800e90e.with-local-changes
+ * @license Angular v8.2.0-rc.0+13.sha-03e8a31.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5789,7 +5789,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.0-rc.0+12.sha-800e90e.with-local-changes');
+    var VERSION = new core.Version('8.2.0-rc.0+13.sha-03e8a31.with-local-changes');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+12.sha-800e90e.with-local-changes
+ * @license Angular v8.2.0-rc.0+13.sha-03e8a31.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
