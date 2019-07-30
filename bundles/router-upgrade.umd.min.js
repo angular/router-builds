b782ec581a1eea99c07feebb7e98f0504202edd2
Andrew Kushnir <akushnir@google.com>
2019-07-30 20:16:26 +0000

    14890e911 docs: change Angular Material Design to Material Design in animations guide (#31889)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+8.sha-d269b11.with-local-changes
+ * @license Angular v8.2.0-rc.0+10.sha-14890e9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
