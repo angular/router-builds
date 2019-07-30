7f87181c2a26938224135fbd6a0d376ec91ffeda
Andrew Kushnir <akushnir@google.com>
2019-07-30 20:19:23 +0000

    5c9a8961d fix(zone.js): don't rely on global node typings outside of node/ directory (#31783)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+113.sha-3479fdd.with-local-changes
+ * @license Angular v8.2.0-next.2+114.sha-5c9a896.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
