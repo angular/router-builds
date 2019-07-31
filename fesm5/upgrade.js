0d0ca6ff6fd6200cdaed357773bd91140a08c633
Andrew Kushnir <akushnir@google.com>
2019-07-31 19:01:20 +0000

    8e7a0d4ff docs: add ReactiveConf 2019 to events (#31895) PR Close #31895

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0.with-local-changes
+ * @license Angular v9.0.0-next.0+2.sha-8e7a0d4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
