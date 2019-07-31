8f43a009ba07c21c6f290103e4acc3530ecdc7eb
Andrew Kushnir <akushnir@google.com>
2019-07-31 18:27:48 +0000

    eac993dfc docs: release notes for the v8.2.0 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+126.sha-975917b.with-local-changes
+ * @license Angular v8.2.0-next.2+127.sha-eac993d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5742,7 +5742,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.2.0-next.2+126.sha-975917b.with-local-changes');
+    var VERSION = new i0.Version('8.2.0-next.2+127.sha-eac993d.with-local-changes');
 
     /**
      * @license
