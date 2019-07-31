b9f3cbff3eddc74680b5168b68c38bc797ecbaa9
Andrew Kushnir <akushnir@google.com>
2019-07-31 19:07:28 +0000

    ecffbda66 test: fix outDir in TS integration tests (#29284)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+2.sha-8e7a0d4.with-local-changes
+ * @license Angular v9.0.0-next.0+5.sha-ecffbda.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5789,7 +5789,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.0+2.sha-8e7a0d4.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.0+5.sha-ecffbda.with-local-changes');
 
     /**
      * @license
