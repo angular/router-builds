bc145e0bdd3483935a5fcef09fd82f70be13d23c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-31 23:41:40 +0000

    b8f269414 fix(ivy): speed up ngtsc if project has no templates to check (#31922)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0+2.sha-8ed83ca.with-local-changes
+ * @license Angular v8.2.0+3.sha-b8f2694.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
