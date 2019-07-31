5045e044522392444cdf97d98b1b2529c00642c0
Andrew Kushnir <akushnir@google.com>
2019-07-31 18:44:26 +0000

    76503e65c release: cut the v9.0.0-next.0 release

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+127.sha-eac993d.with-local-changes
+ * @license Angular v9.0.0-next.0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
