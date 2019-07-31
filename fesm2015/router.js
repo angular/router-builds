e78ccd358f8ce1fa2b60d2452a69d7aab7b8cb86
Andrew Kushnir <akushnir@google.com>
2019-07-31 18:42:49 +0000

    76503e65c release: cut the v9.0.0-next.0 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+127.sha-eac993d.with-local-changes
+ * @license Angular v9.0.0-next.0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8029,7 +8029,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.2+127.sha-eac993d.with-local-changes');
+const VERSION = new Version('9.0.0-next.0.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
