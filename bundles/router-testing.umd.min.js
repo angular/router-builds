eb777ec91500ade2a7070d6b70a9a909b6791328
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-01 17:31:34 +0000

    c1ae6124c fix(upgrade): compile downgraded components synchronously (if possible) (#31840)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+11.sha-d0d875a.with-local-changes
+ * @license Angular v9.0.0-next.0+8.sha-c1ae612.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
