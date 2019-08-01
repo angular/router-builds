f1c883501fa87022bae553f29c85d5b8d490b593
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-01 17:31:52 +0000

    9e9179e91 docs: update Zenika trainings URL in resources.json (#31910) PR Close #31910

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+8.sha-c1ae612.with-local-changes
+ * @license Angular v9.0.0-next.0+9.sha-9e9179e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5789,7 +5789,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.0+8.sha-c1ae612.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.0+9.sha-9e9179e.with-local-changes');
 
     /**
      * @license
