d55af9d893833df3d43092d7c0dbca8dee3e070c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-01 17:31:54 +0000

    e8b8f6d09 fix(language-service): getSourceFile() should only be called on TS files (#31920)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+6.sha-82b9728.with-local-changes
+ * @license Angular v9.0.0-next.0+10.sha-e8b8f6d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5742,7 +5742,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.0+6.sha-82b9728.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.0+10.sha-e8b8f6d.with-local-changes');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+6.sha-82b9728.with-local-changes
+ * @license Angular v9.0.0-next.0+10.sha-e8b8f6d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8217,7 +8217,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.0+6.sha-82b9728.with-local-changes');
+const VERSION = new Version('9.0.0-next.0+10.sha-e8b8f6d.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
