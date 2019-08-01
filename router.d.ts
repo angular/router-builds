4d08931b7bd37bc2c5a3ab547648d51913d120f3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-01 17:33:07 +0000

    a610d1226 docs(core): add missing closing brace in directive input example (#31901) PR Close #31901

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+9.sha-9e9179e.with-local-changes
+ * @license Angular v9.0.0-next.0+12.sha-a610d12.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
