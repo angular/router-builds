2875bc50a5c8b26da189cb6812153f7407a980d1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-01 18:33:40 +0000

    f2d47c96c fix(ivy): ngcc emits static fields before extra statements (#31933)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+13.sha-184d270.with-local-changes
+ * @license Angular v9.0.0-next.0+14.sha-f2d47c9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
