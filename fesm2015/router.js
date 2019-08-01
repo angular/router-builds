fb7fb44140238d6b3988e8e40f983d8f8fac5618
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-01 17:33:20 +0000

    c1ae6124c fix(upgrade): compile downgraded components synchronously (if possible) (#31840)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+11.sha-d0d875a.with-local-changes
+ * @license Angular v9.0.0-next.0+8.sha-c1ae612.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8217,7 +8217,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.0+11.sha-d0d875a.with-local-changes');
+const VERSION = new Version('9.0.0-next.0+8.sha-c1ae612.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
