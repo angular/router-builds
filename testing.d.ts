7c12529c6777d9c790c1f68559b48357287f6f0b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-01 17:30:18 +0000

    e8b8f6d09 fix(language-service): getSourceFile() should only be called on TS files (#31920)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+6.sha-82b9728.with-local-changes
+ * @license Angular v9.0.0-next.0+10.sha-e8b8f6d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
