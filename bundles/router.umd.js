7642f5f7d685da67eeb54d5deb96ab58ab3aaf7c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-01 17:34:53 +0000

    184d27072 fix(ivy): DebugElement.triggerEventHandler not picking up events registered via Renderer2 (#31845)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+12.sha-a610d12.with-local-changes
+ * @license Angular v9.0.0-next.0+13.sha-184d270.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5742,7 +5742,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.0+12.sha-a610d12.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.0+13.sha-184d270.with-local-changes');
 
     /**
      * @license
