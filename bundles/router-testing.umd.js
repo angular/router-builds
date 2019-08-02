cd643fe96a2a48c822dc4777ee77186190756e7b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-02 01:02:16 +0000

    3122f3415 perf(ivy): remove global state acces from postProcessDirective (#31946)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+14.sha-f2d47c9.with-local-changes
+ * @license Angular v9.0.0-next.0+18.sha-3122f34.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
