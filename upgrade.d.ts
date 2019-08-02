5c81a63fd746c37e4226d8ab2fbe24390f455982
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-02 01:06:17 +0000

    44b5bf486 docs: add links to web.dev/angular guides (#31804)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0+5.sha-04ebd59.with-local-changes
+ * @license Angular v8.2.0+7.sha-44b5bf4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
