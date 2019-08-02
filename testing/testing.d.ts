fe14129b1610d8c788d72a9c1c76050a54ee0890
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-02 17:00:44 +0000

    bb1fdb6c0 docs: add `anyComponentStyle` budget description (#31955)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0+7.sha-44b5bf4.with-local-changes
+ * @license Angular v8.2.0+8.sha-bb1fdb6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
