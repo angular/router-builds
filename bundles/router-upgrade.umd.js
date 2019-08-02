aaf31d3f1ce90c2a46196095735fa63b6a4c3e77
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-02 02:31:24 +0000

    a2183ddb7 fix(ivy): directive matching not working in some cases when preceded by styling attributes (#31942)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+13.sha-184d270.with-local-changes
+ * @license Angular v9.0.0-next.0+19.sha-a2183dd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+13.sha-184d270.with-local-changes
+ * @license Angular v9.0.0-next.0+19.sha-a2183dd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5789,7 +5789,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.0+13.sha-184d270.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.0+19.sha-a2183dd.with-local-changes');
 
     /**
      * @license
