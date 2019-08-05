4377fc680d06980d47f390b72c8d9c8de58f17c3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-05 20:18:25 +0000

    f542649b2 docs: fix typo in Workspace Config guide code snippet (sourceMaps --> sourceMap) (#31957)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+30.sha-382d3ed.with-local-changes
+ * @license Angular v9.0.0-next.0+39.sha-f542649.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5742,7 +5742,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.0+30.sha-382d3ed.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.0+39.sha-f542649.with-local-changes');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+30.sha-382d3ed.with-local-changes
+ * @license Angular v9.0.0-next.0+39.sha-f542649.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8217,7 +8217,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.0+30.sha-382d3ed.with-local-changes');
+const VERSION = new Version('9.0.0-next.0+39.sha-f542649.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
