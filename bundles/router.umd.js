b99036092a000648d394f4d42cfe7a39e389a81c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-05 20:16:49 +0000

    f542649b2 docs: fix typo in Workspace Config guide code snippet (sourceMaps --> sourceMap) (#31957)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+30.sha-382d3ed.with-local-changes
+ * @license Angular v9.0.0-next.0+39.sha-f542649.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5789,7 +5789,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.0+30.sha-382d3ed.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.0+39.sha-f542649.with-local-changes');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+30.sha-382d3ed.with-local-changes
+ * @license Angular v9.0.0-next.0+39.sha-f542649.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
