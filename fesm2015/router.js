0652380c4bcd1ad39c14b42b54a48691d7fad680
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-05 20:13:44 +0000

    7db269ba6 fix(ivy): ngcc - correctly detect formats processed in previous runs (#32003)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+25.sha-96cbcd6.with-local-changes
+ * @license Angular v9.0.0-next.0+34.sha-7db269b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8217,7 +8217,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.0+25.sha-96cbcd6.with-local-changes');
+const VERSION = new Version('9.0.0-next.0+34.sha-7db269b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+25.sha-96cbcd6.with-local-changes
+ * @license Angular v9.0.0-next.0+34.sha-7db269b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5669,7 +5669,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.0+25.sha-96cbcd6.with-local-changes');
+var VERSION = new Version('9.0.0-next.0+34.sha-7db269b.with-local-changes');
 
 /**
  * @license
