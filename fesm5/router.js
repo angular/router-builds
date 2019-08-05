1db786ec94a42397331662830ad89fbdf0feb270
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-05 20:12:29 +0000

    382d3ed1d fix(docs-infra): ui polish (#31013)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+34.sha-7db269b.with-local-changes
+ * @license Angular v9.0.0-next.0+30.sha-382d3ed.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5701,7 +5701,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.0+34.sha-7db269b.with-local-changes');
+var VERSION = new Version('9.0.0-next.0+30.sha-382d3ed.with-local-changes');
 
 /**
  * @license
