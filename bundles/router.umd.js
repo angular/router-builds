21d4f75ae335db185df29fe33c3caaf337faddea
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-05 20:20:31 +0000

    fa699f65d docs: updated doc for timezone param in DatePipe (#31827) PR Close #31827

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+38.sha-a574e46.with-local-changes
+ * @license Angular v9.0.0-next.0+41.sha-fa699f6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5789,7 +5789,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.0+38.sha-a574e46.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.0+41.sha-fa699f6.with-local-changes');
 
     /**
      * @license
