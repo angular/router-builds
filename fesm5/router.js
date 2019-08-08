ed540eb26b0b544c75f56556a6f4f4db8c434a84
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 19:04:45 +0000

    22d3cabc1 docs: add missing Oxford comma in styleguide (#32040)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+59.sha-a06043b.with-local-changes
+ * @license Angular v9.0.0-next.0+60.sha-22d3cab.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5669,7 +5669,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.0+59.sha-a06043b.with-local-changes');
+var VERSION = new Version('9.0.0-next.0+60.sha-22d3cab.with-local-changes');
 
 /**
  * @license
