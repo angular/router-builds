316396264594813f03666cb2647df421847d2fcd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 18:35:51 +0000

    8af2cc1ef docs: use proper case in styleguide sentence (#32039)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+52.sha-29d3b68.with-local-changes
+ * @license Angular v9.0.0-next.0+54.sha-8af2cc1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
