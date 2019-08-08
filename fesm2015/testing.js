09d66160e0df29e633e4b3394233929ea96b4500
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 19:22:39 +0000

    a8e2ee134 fix(language-service): Make Definition and QuickInfo compatible with TS LS (#31972)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+62.sha-e906a4f.with-local-changes
+ * @license Angular v9.0.0-next.0+63.sha-a8e2ee1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
