c21997d6519ba1299ba06617020309d35e92aeac
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 19:06:58 +0000

    b5b33d12d docs: remove extraneous word from sentence in styleguide (#32038)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+60.sha-22d3cab.with-local-changes
+ * @license Angular v9.0.0-next.0+61.sha-b5b33d1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
