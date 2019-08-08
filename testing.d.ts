6f4d267c5835605a2ee54973ff01c0e5deb9d46a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 19:04:23 +0000

    19095835a docs: add missing Oxford comma in styleguide (#32040)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0+26.sha-b560207.with-local-changes
+ * @license Angular v8.2.0+27.sha-1909583.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
