be55105c6ba5d4216abf9dc1985c12604c6d2c7f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 18:36:26 +0000

    afd76a80d docs: use proper case in styleguide sentence (#32039)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0+18.sha-6ba31b9.with-local-changes
+ * @license Angular v8.2.0+21.sha-afd76a8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
