96a87cf0f0d3d97f6fe933fc5c4d5b68aa2043f8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 18:36:36 +0000

    f7eebd022 build(docs-infra): upgrade cli command docs sources to bb4be27da (#32036)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+53.sha-e5a89e0.with-local-changes
+ * @license Angular v9.0.0-next.0+55.sha-f7eebd0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
