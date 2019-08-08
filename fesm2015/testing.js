0137492aef2bffc0ab12c277a73e6a8a9d451ff6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 20:10:45 +0000

    cfaefa996 fix(ivy): set LOCALE_ID when using the injector (#31983)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0+33.sha-a48376b.with-local-changes
+ * @license Angular v8.2.0+34.sha-cfaefa9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
