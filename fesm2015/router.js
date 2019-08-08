2bfe2721ed84854854d9d958ae6426663c1caf71
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 19:06:36 +0000

    038d82394 docs: remove extraneous word from sentence in styleguide (#32038)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0+27.sha-1909583.with-local-changes
+ * @license Angular v8.2.0+28.sha-038d823.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8029,7 +8029,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0+27.sha-1909583.with-local-changes');
+const VERSION = new Version('8.2.0+28.sha-038d823.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
