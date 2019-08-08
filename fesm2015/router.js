3efc2f7a4fb90ae2707f033683c661abe24aebea
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 18:37:06 +0000

    e5a89e047 docs: update node:8-browsers to 10 in testing (#32049)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+54.sha-8af2cc1.with-local-changes
+ * @license Angular v9.0.0-next.0+53.sha-e5a89e0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8217,7 +8217,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.0+54.sha-8af2cc1.with-local-changes');
+const VERSION = new Version('9.0.0-next.0+53.sha-e5a89e0.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
