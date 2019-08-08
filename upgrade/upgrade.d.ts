b76e18b02b6b7e8fa376df14ce0413d06f0b508e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 19:33:08 +0000

    17e289c39 docs: correct description of output decorator and add links to guide (#31780)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+69.sha-f8c27d4.with-local-changes
+ * @license Angular v9.0.0-next.0+71.sha-17e289c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
