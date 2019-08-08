4855856ea784f48a1f0fe4db9c4c9894d1aee92b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 18:38:54 +0000

    bb4627786 docs: update node:8-browsers to 10 in testing (#32049)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0+21.sha-afd76a8.with-local-changes
+ * @license Angular v8.2.0+20.sha-bb46277.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
