489f983962c1157c85a6ed5462ccdfd2c8b2f532
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 18:57:36 +0000

    b560207bf build(docs-infra): ensure the locally built packages exist and are up-to-date in `yarn setup-local` (#31985)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0+22.sha-5bf9125.with-local-changes
+ * @license Angular v8.2.0+26.sha-b560207.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
