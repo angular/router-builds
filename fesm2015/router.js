d64ade99b47518bf5b933e6a6227151dd1cbc7b1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 18:44:05 +0000

    5bf912599 docs: remove duplicate sentence in toh-pt5 (#32021)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0+20.sha-bb46277.with-local-changes
+ * @license Angular v8.2.0+22.sha-5bf9125.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8029,7 +8029,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0+20.sha-bb46277.with-local-changes');
+const VERSION = new Version('8.2.0+22.sha-5bf9125.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
