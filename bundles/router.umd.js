7298950e95e4202e4ff9d0772e8b2168de2829d5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 18:39:59 +0000

    939529ce5 docs: remove duplicate sentence in toh-pt5 (#32021)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+56.sha-46304a4.with-local-changes
+ * @license Angular v9.0.0-next.0+57.sha-939529c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5742,7 +5742,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.0+56.sha-46304a4.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.0+57.sha-939529c.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+56.sha-46304a4.with-local-changes
+ * @license Angular v9.0.0-next.0+57.sha-939529c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
