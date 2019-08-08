77119362affb45782e1c2784b2dfebefa188a0d9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 19:01:17 +0000

    a06043b70 perf(ivy): remove global state access from setUpAttributes (#32041)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+57.sha-939529c.with-local-changes
+ * @license Angular v9.0.0-next.0+59.sha-a06043b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
