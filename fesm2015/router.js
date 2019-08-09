c9f9b7fa0425181d8216d53f706231d761829846
Kara Erickson <karakara@google.com>
2019-08-09 19:45:31 +0000

    0ddf0c489 fix(compiler): do not remove whitespace wrapping i18n expansions (#31962)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+9.sha-eb5412d.with-local-changes
+ * @license Angular v9.0.0-next.1+11.sha-0ddf0c4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8029,7 +8029,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.1+9.sha-eb5412d.with-local-changes');
+const VERSION = new Version('9.0.0-next.1+11.sha-0ddf0c4.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
