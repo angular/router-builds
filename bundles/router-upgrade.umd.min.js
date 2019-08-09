77ed2c5530b29caf7791fdf48bdeb25cf76fde18
Kara Erickson <karakara@google.com>
2019-08-09 18:13:34 +0000

    eb5412d76 fix(ivy): reuse compilation scope for incremental template changes. (#31932)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+10.sha-fd6ed17.with-local-changes
+ * @license Angular v9.0.0-next.1+9.sha-eb5412d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+10.sha-fd6ed17.with-local-changes
+ * @license Angular v9.0.0-next.1+9.sha-eb5412d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5701,7 +5701,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.1+10.sha-fd6ed17.with-local-changes');
+var VERSION = new Version('9.0.0-next.1+9.sha-eb5412d.with-local-changes');
 
 /**
  * @license
