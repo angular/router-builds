f9d76cd946f350feda9bf15af3cf9d3267f04ea4
Kara Erickson <karakara@google.com>
2019-08-09 20:25:25 +0000

    0ddf0c489 fix(compiler): do not remove whitespace wrapping i18n expansions (#31962)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+9.sha-eb5412d.with-local-changes
+ * @license Angular v9.0.0-next.1+11.sha-0ddf0c4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5789,7 +5789,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.1+9.sha-eb5412d.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.1+11.sha-0ddf0c4.with-local-changes');
 
     /**
      * @license
