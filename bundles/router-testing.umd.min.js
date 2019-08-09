da98b14c51fdeb56e49dca931d2cf2347fe4b7e1
Kara Erickson <karakara@google.com>
2019-08-09 18:08:27 +0000

    695f322dc docs: clarify pipe naming (#31806)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+7.sha-9896d43.with-local-changes
+ * @license Angular v9.0.0-next.1+5.sha-695f322.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+7.sha-9896d43.with-local-changes
+ * @license Angular v9.0.0-next.1+5.sha-695f322.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5742,7 +5742,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.1+7.sha-9896d43.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.1+5.sha-695f322.with-local-changes');
 
     /**
      * @license
