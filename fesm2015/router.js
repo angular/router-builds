9b51fe6a40abde2e3f3e5a16f14dbdbaa1725753
Kara Erickson <karakara@google.com>
2019-08-09 22:33:23 +0000

    c67f490d2 build: ensure fixup commits match an earlier, unmerged commit (#32023)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.1+6.sha-eccb60c.with-local-changes
+ * @license Angular v8.2.1+10.sha-c67f490.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9896,7 +9896,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.1+6.sha-eccb60c.with-local-changes');
+const VERSION = new Version('8.2.1+10.sha-c67f490.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.1+6.sha-eccb60c.with-local-changes
+ * @license Angular v8.2.1+10.sha-c67f490.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
