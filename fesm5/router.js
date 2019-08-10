8dbcee97b6b9ef6338fe73d8b598467c2e9d8721
Kara Erickson <karakara@google.com>
2019-08-10 01:43:26 +0000

    37de490e2 Revert "feat(compiler): allow selector-less directives as base classes (#31379)" (#32089)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+19.sha-7530801.with-local-changes
+ * @license Angular v9.0.0-next.1+20.sha-37de490.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5709,7 +5709,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.1+19.sha-7530801.with-local-changes');
+var VERSION = new Version('9.0.0-next.1+20.sha-37de490.with-local-changes');
 
 /**
  * @license
