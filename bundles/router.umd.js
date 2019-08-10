5681f0056d49b7ea9a18b63fe85cdead88b090b1
Kara Erickson <karakara@google.com>
2019-08-10 00:48:50 +0000

    753080133 Revert "docs(forms): use a number as input value for formControlName (#30606)" (#32088)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+17.sha-3df54be.with-local-changes
+ * @license Angular v9.0.0-next.1+19.sha-7530801.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5895,7 +5895,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.1+17.sha-3df54be.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.1+19.sha-7530801.with-local-changes');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+17.sha-3df54be.with-local-changes
+ * @license Angular v9.0.0-next.1+19.sha-7530801.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
