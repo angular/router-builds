f072b860f33c4582140a49ad6dba3588306f62c6
Kara Erickson <karakara@google.com>
2019-08-12 20:51:32 +0000

    914900a56 refactor(ivy): remove load instruction (#32067)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+23.sha-4ea3e7e.with-local-changes
+ * @license Angular v9.0.0-next.1+24.sha-914900a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10084,7 +10084,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.1+23.sha-4ea3e7e.with-local-changes');
+const VERSION = new Version('9.0.0-next.1+24.sha-914900a.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
