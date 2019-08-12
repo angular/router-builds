7a59a23b0e156b1416d6e24e446f399a394c7223
Kara Erickson <karakara@google.com>
2019-08-12 17:56:46 +0000

    6eb9c2fab perf(ivy): don't read global state when interpolated values don't change (#32093)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+21.sha-7b94145.with-local-changes
+ * @license Angular v9.0.0-next.1+22.sha-6eb9c2f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
