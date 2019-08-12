3109a8da25b3ae6088deaa05778c2310bb911080
Kara Erickson <karakara@google.com>
2019-08-12 21:42:57 +0000

    9406104c0 release: cut the v9.0.0-next.2 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+25.sha-c0194e0.with-local-changes
+ * @license Angular v9.0.0-next.2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5895,7 +5895,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.1+25.sha-c0194e0.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.2.with-local-changes');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+25.sha-c0194e0.with-local-changes
+ * @license Angular v9.0.0-next.2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
