ed2cf3d1ebe163e7eaed587b6fc9eb0028eb691c
Kara Erickson <karakara@google.com>
2019-08-12 17:57:21 +0000

    4ea3e7e00 refactor(ivy): combine query load instructions (#32100)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+22.sha-6eb9c2f.with-local-changes
+ * @license Angular v9.0.0-next.1+23.sha-4ea3e7e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5709,7 +5709,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.1+22.sha-6eb9c2f.with-local-changes');
+var VERSION = new Version('9.0.0-next.1+23.sha-4ea3e7e.with-local-changes');
 
 /**
  * @license
