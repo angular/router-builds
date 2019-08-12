b450e0355fd369ce2005a34972fd03b6157b7ad4
Kara Erickson <karakara@google.com>
2019-08-12 14:30:38 +0000

    7b9414565 style: fix inline comment typo (#32090)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+20.sha-37de490.with-local-changes
+ * @license Angular v9.0.0-next.1+21.sha-7b94145.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
