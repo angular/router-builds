6a7ce176541646300104f5ac2554beebc730b942
Kara Erickson <karakara@google.com>
2019-08-13 17:21:36 +0000

    2f812f31d docs: update events page and fix ordering (#32106)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+4.sha-dec7e52.with-local-changes
+ * @license Angular v8.2.2+3.sha-2f812f3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
