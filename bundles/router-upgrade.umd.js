92627887b9f79f4425d22c0913552cde012295df
Kara Erickson <karakara@google.com>
2019-08-13 17:16:30 +0000

    dec7e5286 ci: use local strategy for AngularTemplateCompile and TypescriptCompile on CI (#32112)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+2.sha-18bac15.with-local-changes
+ * @license Angular v8.2.2+4.sha-dec7e52.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+2.sha-18bac15.with-local-changes
+ * @license Angular v8.2.2+4.sha-dec7e52.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9896,7 +9896,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.2+2.sha-18bac15.with-local-changes');
+const VERSION = new Version('8.2.2+4.sha-dec7e52.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
