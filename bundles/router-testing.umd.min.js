9525d87ba239693cc7b0539900faa318626692a0
Kara Erickson <karakara@google.com>
2019-08-13 18:56:14 +0000

    dc76c14e3 docs: edit location doc (#32042)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+3.sha-2f812f3.with-local-changes
+ * @license Angular v8.2.2+7.sha-dc76c14.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+3.sha-2f812f3.with-local-changes
+ * @license Angular v8.2.2+7.sha-dc76c14.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9896,7 +9896,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.2+3.sha-2f812f3.with-local-changes');
+const VERSION = new Version('8.2.2+7.sha-dc76c14.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
