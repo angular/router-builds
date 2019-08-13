103952edc193f280224b3e2a77dbbad84b485bbd
Kara Erickson <karakara@google.com>
2019-08-13 22:42:58 +0000

    b9dfe6602 perf(ivy): split view processing into render (create) and refresh (update) pass (#32020)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+17.sha-4d96cf5.with-local-changes
+ * @license Angular v9.0.0-next.2+18.sha-b9dfe66.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5863,7 +5863,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.2+17.sha-4d96cf5.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.2+18.sha-b9dfe66.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+17.sha-4d96cf5.with-local-changes
+ * @license Angular v9.0.0-next.2+18.sha-b9dfe66.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
