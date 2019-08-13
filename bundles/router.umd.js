abb33811e29d6cc4bfec9693858b2b926bca95d9
Kara Erickson <karakara@google.com>
2019-08-13 19:00:10 +0000

    e4d5102b1 build: ensure schematics are built with typescript strict flag (#31967)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+9.sha-a5f39ae.with-local-changes
+ * @license Angular v9.0.0-next.2+12.sha-e4d5102.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5863,7 +5863,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.2+9.sha-a5f39ae.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.2+12.sha-e4d5102.with-local-changes');
 
     /**
      * @license
