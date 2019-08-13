b37030105f75c476b494b57ac73e431f2c5e8cec
Kara Erickson <karakara@google.com>
2019-08-13 19:29:12 +0000

    3d1b82be6 docs: correct description of animation example (#32009)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+8.sha-7b3bd21.with-local-changes
+ * @license Angular v8.2.2+5.sha-3d1b82b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+8.sha-7b3bd21.with-local-changes
+ * @license Angular v8.2.2+5.sha-3d1b82b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9896,7 +9896,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.2+8.sha-7b3bd21.with-local-changes');
+const VERSION = new Version('8.2.2+5.sha-3d1b82b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
