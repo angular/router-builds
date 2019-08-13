12e2a9ef4c39940ae2b75601a4c21ded7bea61d1
Kara Erickson <karakara@google.com>
2019-08-13 22:38:52 +0000

    628b0c115 feat(forms): formControlName also accepts a number (#30606)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+12.sha-e4d5102.with-local-changes
+ * @license Angular v9.0.0-next.2+13.sha-628b0c1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9896,7 +9896,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.2+12.sha-e4d5102.with-local-changes');
+const VERSION = new Version('9.0.0-next.2+13.sha-628b0c1.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+12.sha-e4d5102.with-local-changes
+ * @license Angular v9.0.0-next.2+13.sha-628b0c1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5709,7 +5709,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.2+12.sha-e4d5102.with-local-changes');
+var VERSION = new Version('9.0.0-next.2+13.sha-628b0c1.with-local-changes');
 
 /**
  * @license
