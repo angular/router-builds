6a00fa3bbff274fbc364d741873fd7df00a2eba7
Kara Erickson <karakara@google.com>
2019-08-13 18:37:15 +0000

    a95f860a9 docs: correct description of animation example (#32009)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+6.sha-6215636.with-local-changes
+ * @license Angular v9.0.0-next.2+7.sha-a95f860.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
