560bdee2f5f798bc5a8c5abfc8da8be933204fe8
Kara Erickson <karakara@google.com>
2019-08-13 17:16:23 +0000

    3cd8a2b90 docs: update events page and fix ordering (#32106)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+4.sha-ebcf79d.with-local-changes
+ * @license Angular v9.0.0-next.2+5.sha-3cd8a2b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5895,7 +5895,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.2+4.sha-ebcf79d.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.2+5.sha-3cd8a2b.with-local-changes');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+4.sha-ebcf79d.with-local-changes
+ * @license Angular v9.0.0-next.2+5.sha-3cd8a2b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9896,7 +9896,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.2+4.sha-ebcf79d.with-local-changes');
+const VERSION = new Version('9.0.0-next.2+5.sha-3cd8a2b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
