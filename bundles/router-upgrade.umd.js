b8ff63f3c24106246ca8c3c05b00abecb47d324d
Kara Erickson <karakara@google.com>
2019-08-13 17:18:38 +0000

    6215636bc ci: use local strategy for AngularTemplateCompile and TypescriptCompile on CI (#32112)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+5.sha-3cd8a2b.with-local-changes
+ * @license Angular v9.0.0-next.2+6.sha-6215636.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
