7d21495c910bbd2c176460ba4403d5b79c92b3f1
Kara Erickson <karakara@google.com>
2019-08-13 18:39:03 +0000

    a95f860a9 docs: correct description of animation example (#32009)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+6.sha-6215636.with-local-changes
+ * @license Angular v9.0.0-next.2+7.sha-a95f860.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10084,7 +10084,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.2+6.sha-6215636.with-local-changes');
+const VERSION = new Version('9.0.0-next.2+7.sha-a95f860.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
