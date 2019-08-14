ce045b5c67b65d4b2807533c8d2bc32426a7bede
Andrew Kushnir <akushnir@google.com>
2019-08-14 19:18:44 +0000

    843881120 docs: doc browser support for service workers (#32046)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+20.sha-cda205d.with-local-changes
+ * @license Angular v9.0.0-next.2+24.sha-8438811.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9896,7 +9896,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.2+20.sha-cda205d.with-local-changes');
+const VERSION = new Version('9.0.0-next.2+24.sha-8438811.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+20.sha-cda205d.with-local-changes
+ * @license Angular v9.0.0-next.2+24.sha-8438811.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
