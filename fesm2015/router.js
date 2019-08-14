fd996286ba5d9217fa46dc47f630bd692a829a75
Andrew Kushnir <akushnir@google.com>
2019-08-14 19:24:34 +0000

    8af85d812 docs: add info about reviewing PRs from code owners (#32108)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+28.sha-4d43680.with-local-changes
+ * @license Angular v9.0.0-next.2+25.sha-8af85d8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9896,7 +9896,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.2+28.sha-4d43680.with-local-changes');
+const VERSION = new Version('9.0.0-next.2+25.sha-8af85d8.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
