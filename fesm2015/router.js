2bebaa1f60ba98fdf6af1d8810591484c3b62385
Andrew Kushnir <akushnir@google.com>
2019-08-14 19:21:32 +0000

    984d23f68 fix(docs-infra): fix broken toc ul styles (#32124)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+23.sha-9e6c677.with-local-changes
+ * @license Angular v9.0.0-next.2+26.sha-984d23f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9896,7 +9896,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.2+23.sha-9e6c677.with-local-changes');
+const VERSION = new Version('9.0.0-next.2+26.sha-984d23f.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
