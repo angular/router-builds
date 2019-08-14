a0e072cf5973c7a088bbc3f72af350f30f5453f8
Kara Erickson <karakara@google.com>
2019-08-14 00:15:30 +0000

    cda205deb release: cut the zone.js-0.10.2 release (#32128)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+18.sha-b9dfe66.with-local-changes
+ * @license Angular v9.0.0-next.2+20.sha-cda205d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5895,7 +5895,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.2+18.sha-b9dfe66.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.2+20.sha-cda205d.with-local-changes');
 
     /**
      * @license
