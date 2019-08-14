d471f2435f9c7ba81763cc64bf4404d074a49a9e
Andrew Kushnir <akushnir@google.com>
2019-08-14 19:27:38 +0000

    84a3daf60 docs: doc browser support for service workers (#32046)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+14.sha-0757702.with-local-changes
+ * @license Angular v8.2.2+10.sha-84a3daf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
