5708d1a64d62e07e562c1733129f57fc072c60c1
Andrew Kushnir <akushnir@google.com>
2019-08-14 19:23:06 +0000

    984d23f68 fix(docs-infra): fix broken toc ul styles (#32124)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+23.sha-9e6c677.with-local-changes
+ * @license Angular v9.0.0-next.2+26.sha-984d23f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
