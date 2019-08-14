97c59fc44660c082ee05571bfe4ed85088fdeb61
Andrew Kushnir <akushnir@google.com>
2019-08-14 19:20:46 +0000

    9e6c67713 test(language-service): Remove test for external template (#32017)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+24.sha-8438811.with-local-changes
+ * @license Angular v9.0.0-next.2+23.sha-9e6c677.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
