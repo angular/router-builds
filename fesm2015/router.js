9efe06fb0e7871a2151b58af12be80b9e280ed26
Andrew Kushnir <akushnir@google.com>
2019-08-14 19:24:20 +0000

    7a75f7805 build(core): add missing tsconfig-build.json dependency (#31943)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+26.sha-984d23f.with-local-changes
+ * @license Angular v9.0.0-next.2+21.sha-7a75f78.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10084,7 +10084,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.2+26.sha-984d23f.with-local-changes');
+const VERSION = new Version('9.0.0-next.2+21.sha-7a75f78.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
