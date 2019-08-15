cd8145a7a2b229c4d1918d5401cc8bc3aab93009
Andrew Kushnir <akushnir@google.com>
2019-08-15 20:06:01 +0000

    373d9660d docs: fix typo of Typescript to TypeScript (#32153)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+41.sha-1c65161.with-local-changes
+ * @license Angular v9.0.0-next.2+42.sha-373d966.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
