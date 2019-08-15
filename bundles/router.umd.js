8b4177599223af38b40bfd28fce4bf08f87a99cf
Andrew Kushnir <akushnir@google.com>
2019-08-15 17:24:49 +0000

    40b28742a refactor(language-service): Differentiate Inline and External template (#32127)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+37.sha-253a112.with-local-changes
+ * @license Angular v9.0.0-next.2+38.sha-40b2874.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5863,7 +5863,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.2+37.sha-253a112.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.2+38.sha-40b2874.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+37.sha-253a112.with-local-changes
+ * @license Angular v9.0.0-next.2+38.sha-40b2874.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
