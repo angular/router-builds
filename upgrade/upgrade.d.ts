b3ed0807bb325515a926c35b9b472642bf1809c3
Andrew Kushnir <akushnir@google.com>
2019-08-15 20:03:07 +0000

    1c6516199 docs: clarify hierarchical injectors (#28700)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+39.sha-2e4d17f.with-local-changes
+ * @license Angular v9.0.0-next.2+41.sha-1c65161.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
