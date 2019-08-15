7d77faec89a84777e597a498c66c14c355520cdd
Andrew Kushnir <akushnir@google.com>
2019-08-15 22:06:05 +0000

    964d72610 fix(ivy): ngcc should only index .d.ts exports within the package (#32129)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+44.sha-69ce1c2.with-local-changes
+ * @license Angular v9.0.0-next.2+46.sha-964d726.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
