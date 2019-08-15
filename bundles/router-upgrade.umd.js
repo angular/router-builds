b6988bb8aa660f7da8fd621ef70b9cc21159d15c
Andrew Kushnir <akushnir@google.com>
2019-08-15 17:17:03 +0000

    253a1125b test(ivy): add style binding node-based micro benchmark (#32104)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+32.sha-f6a1de6.with-local-changes
+ * @license Angular v9.0.0-next.2+37.sha-253a112.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
