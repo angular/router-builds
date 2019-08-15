825435fe6abb3ee54ca60964d4d010aad3cb94f9
Andrew Kushnir <akushnir@google.com>
2019-08-15 20:03:46 +0000

    6f2e8afae docs: fix typo of Typescript to TypeScript (#32153)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+17.sha-adc869f.with-local-changes
+ * @license Angular v8.2.2+19.sha-6f2e8af.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
