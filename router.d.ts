fb66e69597b9132d1c1fddfe01384b2a962195e6
Andrew Kushnir <akushnir@google.com>
2019-08-16 17:18:51 +0000

    ee486233e build(zone.js): update zone.js to 0.10.2 (#31975)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+47.sha-4c3b791.with-local-changes
+ * @license Angular v9.0.0-next.2+48.sha-ee48623.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
