cff0a7941f26aa11c5017ada6d3e0d0339b6a0fd
Andrew Kushnir <akushnir@google.com>
2019-08-19 17:28:24 +0000

    adb4f7c7c build(docs-infra): upgrade cli command docs sources to 6aae7f563 (#32174)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+52.sha-5f76de1.with-local-changes
+ * @license Angular v9.0.0-next.2+53.sha-adb4f7c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
