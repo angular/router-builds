9f3b66d4447ca655233f6121a96bb6399f3d0729
Andrew Kushnir <akushnir@google.com>
2019-08-19 17:32:35 +0000

    e85ec2303 docs(service-worker): mention that dataGroups only cache non-mutating requests (#32142)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+53.sha-adb4f7c.with-local-changes
+ * @license Angular v9.0.0-next.2+55.sha-e85ec23.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5863,7 +5863,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.2+53.sha-adb4f7c.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.2+55.sha-e85ec23.with-local-changes');
 
     /**
      * @license
