bfe8cfbb3d5e92cb105f5ec2fb1e7dc6752503ff
Andrew Kushnir <akushnir@google.com>
2019-08-19 17:30:56 +0000

    e85ec2303 docs(service-worker): mention that dataGroups only cache non-mutating requests (#32142)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+53.sha-adb4f7c.with-local-changes
+ * @license Angular v9.0.0-next.2+55.sha-e85ec23.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
