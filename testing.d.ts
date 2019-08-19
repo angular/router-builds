a97a427fd837da9bef6b7640c63031b04182011a
Andrew Kushnir <akushnir@google.com>
2019-08-19 23:12:16 +0000

    a7b94783b fix(bazel): pin `@microsoft/api-extractor` (#32187)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+22.sha-789dd6a.with-local-changes
+ * @license Angular v8.2.2+23.sha-a7b9478.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
