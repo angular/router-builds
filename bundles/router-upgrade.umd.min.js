642e5e4e255b6055aadcce470eaa369257b5e746
Andrew Kushnir <akushnir@google.com>
2019-08-19 23:03:26 +0000

    639b73202 refactor(core): remove disabled injectable-pipe migration (#32184)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+63.sha-0b1bf14.with-local-changes
+ * @license Angular v9.0.0-next.2+65.sha-639b732.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
