5ee4171f0dcd0671a3fb470e1fe5ce7d25f0ff5f
Andrew Kushnir <akushnir@google.com>
2019-08-19 17:30:39 +0000

    e5b18e810 build(docs-infra): upgrade cli command docs sources to e0055d293 (#32175)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+20.sha-b8f86ad.with-local-changes
+ * @license Angular v8.2.2+21.sha-e5b18e8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
