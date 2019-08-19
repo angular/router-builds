3c85c998600101cff08ec3060541ef733f36372d
Andrew Kushnir <akushnir@google.com>
2019-08-19 23:06:35 +0000

    5da5ca5c2 fix(bazel): pin `@microsoft/api-extractor` (#32187)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+65.sha-639b732.with-local-changes
+ * @license Angular v9.0.0-next.2+64.sha-5da5ca5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5895,7 +5895,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.2+65.sha-639b732.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.2+64.sha-5da5ca5.with-local-changes');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+65.sha-639b732.with-local-changes
+ * @license Angular v9.0.0-next.2+64.sha-5da5ca5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5709,7 +5709,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.2+65.sha-639b732.with-local-changes');
+var VERSION = new Version('9.0.0-next.2+64.sha-5da5ca5.with-local-changes');
 
 /**
  * @license
