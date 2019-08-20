e96f58adaec1c153aa0f66810f86c91e74f6f2b2
Andrew Kushnir <akushnir@google.com>
2019-08-20 23:59:44 +0000

    ec4381dd4 feat: make the Ivy compiler the default for ngc (#32219)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+71.sha-388578f.with-local-changes
+ * @license Angular v9.0.0-next.2+73.sha-ec4381d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
