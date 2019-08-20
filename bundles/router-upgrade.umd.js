811848a73e326f356548cfe5e4091f0cd40d0109
Andrew Kushnir <akushnir@google.com>
2019-08-20 17:18:16 +0000

    f8b995dbf fix(ngcc): ignore format properties that exist but are undefined (#32205)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+67.sha-bb3c684.with-local-changes
+ * @license Angular v9.0.0-next.2+66.sha-f8b995d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
