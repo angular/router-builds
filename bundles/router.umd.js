72f5bfd54f99789fd1d43d50ef60979ebe3c7791
Andrew Kushnir <akushnir@google.com>
2019-08-20 17:34:49 +0000

    cfed0c0cf fix(ivy): Support selector-less directive as base classes (#32125)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+66.sha-f8b995d.with-local-changes
+ * @license Angular v9.0.0-next.2+68.sha-cfed0c0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5895,7 +5895,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.2+66.sha-f8b995d.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.2+68.sha-cfed0c0.with-local-changes');
 
     /**
      * @license
