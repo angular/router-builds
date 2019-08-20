c2db9e9dd93e5b8c13a9b9c463aca6c43e2cacbe
Andrew Kushnir <akushnir@google.com>
2019-08-20 22:02:09 +0000

    287247ef0 ci: update material commit we use to run integration tests (#32224)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+25.sha-af170d2.with-local-changes
+ * @license Angular v8.2.2+26.sha-287247e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5709,7 +5709,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.2+25.sha-af170d2.with-local-changes');
+var VERSION = new Version('8.2.2+26.sha-287247e.with-local-changes');
 
 /**
  * @license
