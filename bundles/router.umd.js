fcd9700041b4a6405ce8869526f54957465413da
Andrew Kushnir <akushnir@google.com>
2019-08-21 17:32:05 +0000

    8e1a72546 build(docs-infra): upgrade cli command docs sources to d2489aeda (#32236)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+78.sha-424ab48.with-local-changes
+ * @license Angular v9.0.0-next.2+79.sha-8e1a725.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5863,7 +5863,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.2+78.sha-424ab48.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.2+79.sha-8e1a725.with-local-changes');
 
     /**
      * @license
