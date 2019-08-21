0df17424c1902b54698d01ebb7820cb796b4f3bb
Andrew Kushnir <akushnir@google.com>
2019-08-21 21:15:04 +0000

    e8c09e3b5 docs: release notes for the v8.2.3 release

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+88.sha-53bfa7c.with-local-changes
+ * @license Angular v9.0.0-next.2+89.sha-e8c09e3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
