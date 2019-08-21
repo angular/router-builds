3db7b75f85c7615a25775ae5a91217a6a3273114
Andrew Kushnir <akushnir@google.com>
2019-08-21 21:16:29 +0000

    e8c09e3b5 docs: release notes for the v8.2.3 release

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+88.sha-53bfa7c.with-local-changes
+ * @license Angular v9.0.0-next.2+89.sha-e8c09e3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5677,7 +5677,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.2+88.sha-53bfa7c.with-local-changes');
+var VERSION = new Version('9.0.0-next.2+89.sha-e8c09e3.with-local-changes');
 
 /**
  * @license
