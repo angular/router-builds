9bfe4462c5121c2e14c3c8e01ccc627e7581dd64
Andrew Kushnir <akushnir@google.com>
2019-08-21 19:10:56 +0000

    10629600c perf(ivy): split hooks processing into init and check phases (#32131)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+81.sha-daac386.with-local-changes
+ * @license Angular v9.0.0-next.2+86.sha-1062960.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
