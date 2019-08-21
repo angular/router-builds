935ffc809fbb3fec6d5a3e3352ed966a6b8db3e5
Andrew Kushnir <akushnir@google.com>
2019-08-21 17:25:01 +0000

    62f414063 build: add blocklist for material unit tests (#32239)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+76.sha-6477057.with-local-changes
+ * @license Angular v9.0.0-next.2+77.sha-62f4140.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5863,7 +5863,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.2+76.sha-6477057.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.2+77.sha-62f4140.with-local-changes');
 
     /**
      * @license
