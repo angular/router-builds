1b5d701b25bea837e7a97adbb684ca29d77ab9f0
atscott <atscott@google.com>
2019-08-21 22:48:24 +0000

    f661f460b docs(upgrade): position old setup guide as legacy until it can be removed (#32193)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+1.sha-44f233c.with-local-changes
+ * @license Angular v8.2.3+2.sha-f661f46.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+1.sha-44f233c.with-local-changes
+ * @license Angular v8.2.3+2.sha-f661f46.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9896,7 +9896,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.3+1.sha-44f233c.with-local-changes');
+const VERSION = new Version('8.2.3+2.sha-f661f46.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
