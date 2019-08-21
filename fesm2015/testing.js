26b719544c9a7012162ac82f8be3fe0df72ff2cd
Andrew Kushnir <akushnir@google.com>
2019-08-21 00:06:19 +0000

    26dc82682 docs: change to global approvers (#31940)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+26.sha-287247e.with-local-changes
+ * @license Angular v8.2.2+27.sha-26dc826.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
