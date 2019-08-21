e6c7dd146b8acbed2c19b3d4f47ba8a38217c674
Andrew Kushnir <akushnir@google.com>
2019-08-21 16:18:32 +0000

    64770571b perf: don't create holey arrays (#32155)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+74.sha-10ea3a9.with-local-changes
+ * @license Angular v9.0.0-next.2+76.sha-6477057.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
