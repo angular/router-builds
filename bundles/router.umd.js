ab6c4a88bc6979ba5ba11e02853ad06e2d7f2db8
atscott <atscott@google.com>
2019-08-21 22:43:03 +0000

    b85ac0313 docs(upgrade): position old setup guide as legacy until it can be removed (#32193)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+2.sha-7fed0fa.with-local-changes
+ * @license Angular v9.0.0-next.3+3.sha-b85ac03.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5895,7 +5895,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.3+2.sha-7fed0fa.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.3+3.sha-b85ac03.with-local-changes');
 
     /**
      * @license
