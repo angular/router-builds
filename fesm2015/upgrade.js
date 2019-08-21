04b1c97214e8b6d0ca1f27e409afa90fea6d181b
atscott <atscott@google.com>
2019-08-21 22:55:51 +0000

    a84b5ef81 build: use bazel workers for ngc and tsc in local development (#32246)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+3.sha-b85ac03.with-local-changes
+ * @license Angular v9.0.0-next.3+5.sha-a84b5ef.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
