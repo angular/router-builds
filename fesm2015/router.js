fcc0b7ae861f770d6eb4b7fef86452eeabf37e1b
atscott <atscott@google.com>
2019-08-21 22:40:38 +0000

    5c94833b8 test(language-service): Add test for CRLF line endings (#32245)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+1.sha-02a567e.with-local-changes
+ * @license Angular v9.0.0-next.3+4.sha-5c94833.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9896,7 +9896,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.3+1.sha-02a567e.with-local-changes');
+const VERSION = new Version('9.0.0-next.3+4.sha-5c94833.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
