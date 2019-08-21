42f857a8c0a772f860e67e7e540a251a77dbb233
Andrew Kushnir <akushnir@google.com>
2019-08-21 17:24:11 +0000

    424ab4867 fix(compiler): return enableIvy true when using `readConfiguration` (#32234)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+77.sha-62f4140.with-local-changes
+ * @license Angular v9.0.0-next.2+78.sha-424ab48.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
