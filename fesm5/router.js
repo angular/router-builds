616204ed90eb01ba5d2c3b6b7e1d159e68cb512c
Andrew Kushnir <akushnir@google.com>
2019-08-21 16:35:43 +0000

    e7c4e94c9 docs: update collaborators page (#32229)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+27.sha-26dc826.with-local-changes
+ * @license Angular v8.2.2+28.sha-e7c4e94.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5709,7 +5709,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.2+27.sha-26dc826.with-local-changes');
+var VERSION = new Version('8.2.2+28.sha-e7c4e94.with-local-changes');
 
 /**
  * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+27.sha-26dc826.with-local-changes
+ * @license Angular v8.2.2+28.sha-e7c4e94.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
