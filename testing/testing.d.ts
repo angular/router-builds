291fa3b2d6b622b54e0ac063ab9f0fb40cef4d5f
atscott <atscott@google.com>
2019-08-22 17:25:29 +0000

    904a2018e feat(core): add undecorated classes with decorated fields schematic (#32130)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+7.sha-21edc6a.with-local-changes
+ * @license Angular v9.0.0-next.3+9.sha-904a201.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
