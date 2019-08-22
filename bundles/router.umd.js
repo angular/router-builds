2e4e56329df44155d974b688979d6614c65af26b
atscott <atscott@google.com>
2019-08-22 17:36:11 +0000

    4f7c971ee fix(ivy): ngtsc throws if "flatModuleOutFile" is set to null (#32235)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+9.sha-904a201.with-local-changes
+ * @license Angular v9.0.0-next.3+11.sha-4f7c971.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5863,7 +5863,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.3+9.sha-904a201.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.3+11.sha-4f7c971.with-local-changes');
 
     /**
      * @license
