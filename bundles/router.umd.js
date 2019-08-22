93f8b793ddc0b26ec2212ca752c58cf536d0e8e0
atscott <atscott@google.com>
2019-08-22 22:17:29 +0000

    d443a2874 build(ivy): enable prodserver on the largetable benchmark (#32072)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+19.sha-21bb74d.with-local-changes
+ * @license Angular v9.0.0-next.3+20.sha-d443a28.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5863,7 +5863,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.3+19.sha-21bb74d.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.3+20.sha-d443a28.with-local-changes');
 
     /**
      * @license
