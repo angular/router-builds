dd146a4d553db2e22a816edd6b39b18860e4ced7
atscott <atscott@google.com>
2019-08-22 17:35:58 +0000

    0677cf0cb feat(ivy): use the schema registry to check DOM bindings (#32171)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+11.sha-4f7c971.with-local-changes
+ * @license Angular v9.0.0-next.3+10.sha-0677cf0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
