574689dc30a5d09019fa633f4033bfd117b54f35
atscott <atscott@google.com>
2019-08-22 17:38:51 +0000

    6b245a39e fix(ivy): reset binding index before executing a template in `refreshView` call (#32201)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+10.sha-0677cf0.with-local-changes
+ * @license Angular v9.0.0-next.3+12.sha-6b245a3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5863,7 +5863,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.3+10.sha-0677cf0.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.3+12.sha-6b245a3.with-local-changes');
 
     /**
      * @license
