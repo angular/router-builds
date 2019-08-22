4ee35419470e67bdb683693c5c54a78e1d714ab0
atscott <atscott@google.com>
2019-08-22 19:54:01 +0000

    6d1115465 refactor(language-service): Remove redudant 'TemplateInfo' type (#32250)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+12.sha-6b245a3.with-local-changes
+ * @license Angular v9.0.0-next.3+13.sha-6d11154.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
