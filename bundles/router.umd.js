198767d20089bd2b0859a0d764f3b85d7f675086
atscott <atscott@google.com>
2019-08-22 20:53:30 +0000

    21bb74d16 docs: improve structure (#32227)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+13.sha-6d11154.with-local-changes
+ * @license Angular v9.0.0-next.3+19.sha-21bb74d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5863,7 +5863,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.3+13.sha-6d11154.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.3+19.sha-21bb74d.with-local-changes');
 
     /**
      * @license
