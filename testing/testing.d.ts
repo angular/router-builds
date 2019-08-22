86af4dd69ac6d468753e43b4d4ac04e62d029497
atscott <atscott@google.com>
2019-08-22 13:59:41 +0000

    b6fa9299e build: add .vimrc to .gitignore (#32253)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+6.sha-a367b90.with-local-changes
+ * @license Angular v9.0.0-next.3+8.sha-b6fa929.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
