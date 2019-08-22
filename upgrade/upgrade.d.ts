2ba1c7b60e7cc2bbb204243c20d614a7a36a7c99
atscott <atscott@google.com>
2019-08-22 14:01:57 +0000

    5e4babeaa docs: update http guide (#32045)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+2.sha-f661f46.with-local-changes
+ * @license Angular v8.2.3+3.sha-5e4babe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
