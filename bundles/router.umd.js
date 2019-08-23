80e386a2447388a40fa096a32ef2a7fa66dcb3a9
atscott <atscott@google.com>
2019-08-23 19:50:13 +0000

    a58a5fd7b build(docs-infra): upgrade cli command docs sources to 70e761c12 (#32285)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+24.sha-9bb5f05.with-local-changes
+ * @license Angular v9.0.0-next.3+25.sha-a58a5fd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5863,7 +5863,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.3+24.sha-9bb5f05.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.3+25.sha-a58a5fd.with-local-changes');
 
     /**
      * @license
