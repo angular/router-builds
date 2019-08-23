4213fd49ccf3f41db092ecc49ca05b98d67d4b7f
Andrew Scott <atscott01@gmail.com>
2019-08-23 16:29:39 +0000

    43bac2130 docs: fix http example to use correct property (#32288)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+10.sha-4df4150.with-local-changes
+ * @license Angular v8.2.3+11.sha-43bac21.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
