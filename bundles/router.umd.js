72f86ae882adf4ed14867333b73134c84698e231
Andrew Scott <atscott01@gmail.com>
2019-08-23 16:22:44 +0000

    9bb5f05cb docs: fix http example to use correct property (#32288)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+23.sha-3693d7a.with-local-changes
+ * @license Angular v9.0.0-next.3+24.sha-9bb5f05.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5895,7 +5895,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.3+23.sha-3693d7a.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.3+24.sha-9bb5f05.with-local-changes');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+23.sha-3693d7a.with-local-changes
+ * @license Angular v9.0.0-next.3+24.sha-9bb5f05.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9896,7 +9896,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.3+23.sha-3693d7a.with-local-changes');
+const VERSION = new Version('9.0.0-next.3+24.sha-9bb5f05.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
