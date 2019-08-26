e19801ebddd2d232ef0dd4ff30ee8d6e8860ae12
Andrew Scott <atscott01@gmail.com>
2019-08-26 16:55:16 +0000

    a80a8636b docs: fix animations reference links to api pages (#32267)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+15.sha-0fb78d6.with-local-changes
+ * @license Angular v8.2.3+14.sha-a80a863.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
