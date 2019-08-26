1aff8d03adf498445b42dbea5f463ef25e80a429
atscott <atscott@google.com>
2019-08-26 19:07:21 +0000

    6b0b7d01b docs: fix typo in compiler options guide (#32312)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+16.sha-b5cb120.with-local-changes
+ * @license Angular v8.2.3+18.sha-6b0b7d0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
