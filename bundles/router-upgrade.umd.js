129ef6cd3d0b675e7ed9f0e87f60c23e98b52b61
atscott <atscott@google.com>
2019-08-26 16:50:46 +0000

    b9fbd9bb0 docs: fix wrong HttpEventType of HttpResponse class (#32296)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+25.sha-a58a5fd.with-local-changes
+ * @license Angular v9.0.0-next.3+26.sha-b9fbd9b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
