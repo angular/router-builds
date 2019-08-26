7bf616f61ea88e7d61598efadb30ea81cf74c807
atscott <atscott@google.com>
2019-08-26 16:50:56 +0000

    0878d6775 docs: fix wrong HttpEventType of HttpResponse class (#32296)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+12.sha-5e0890b.with-local-changes
+ * @license Angular v8.2.3+13.sha-0878d67.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
