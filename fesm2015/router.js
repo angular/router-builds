e6a27966b628b9636ef25979999f1e3291ef5e8f
atscott <atscott@google.com>
2019-08-27 21:19:55 +0000

    14feb5613 fix(ivy): debug node names should match user declaration (#32328)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+52.sha-f209aac.with-local-changes
+ * @license Angular v9.0.0-next.3+53.sha-14feb56.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10084,7 +10084,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.3+52.sha-f209aac.with-local-changes');
+const VERSION = new Version('9.0.0-next.3+53.sha-14feb56.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
