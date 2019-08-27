0ae1c896a3d145956618ef5975b6cb5d773814ed
atscott <atscott@google.com>
2019-08-27 16:28:42 +0000

    f57c17de2 build: remove the deprecated `aio` commit message scope (#32341)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+49.sha-c7c7f9f.with-local-changes
+ * @license Angular v9.0.0-next.3+47.sha-f57c17d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5895,7 +5895,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.3+49.sha-c7c7f9f.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.3+47.sha-f57c17d.with-local-changes');
 
     /**
      * @license
