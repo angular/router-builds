d9c4ea4c98e7686b1a07d8e94cac0cc8efba5db7
atscott <atscott@google.com>
2019-08-27 16:25:41 +0000

    c7c7f9fbe style: align i18n angular config (#32334)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+46.sha-0874bf4.with-local-changes
+ * @license Angular v9.0.0-next.3+49.sha-c7c7f9f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
