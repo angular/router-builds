3e1e670a4b9763153be473d28299d8fc6778c678
atscott <atscott@google.com>
2019-08-27 23:37:22 +0000

    aa8dd7417 docs: clean up formats in template syntax guide (#32197)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+27.sha-ed6dcce.with-local-changes
+ * @license Angular v8.2.3+28.sha-aa8dd74.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+27.sha-ed6dcce.with-local-changes
+ * @license Angular v8.2.3+28.sha-aa8dd74.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5709,7 +5709,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.3+27.sha-ed6dcce.with-local-changes');
+var VERSION = new Version('8.2.3+28.sha-aa8dd74.with-local-changes');
 
 /**
  * @license
