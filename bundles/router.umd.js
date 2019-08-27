757700ab3d3ca5f972d657d77975a6f79c906def
atscott <atscott@google.com>
2019-08-27 16:32:50 +0000

    55eaa5fb6 docs: Use ngOnInit to fetch data from services in the getting started guide (#32273)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+50.sha-70cf8ed.with-local-changes
+ * @license Angular v9.0.0-next.3+51.sha-55eaa5f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5863,7 +5863,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.3+50.sha-70cf8ed.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.3+51.sha-55eaa5f.with-local-changes');
 
     /**
      * @license
