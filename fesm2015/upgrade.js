830982743f04b6b267129f15a66973ff5caa0356
atscott <atscott@google.com>
2019-08-27 16:32:06 +0000

    70cf8ed05 docs: Fix typo in singleton-services.md page (#32330)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+47.sha-f57c17d.with-local-changes
+ * @license Angular v9.0.0-next.3+50.sha-70cf8ed.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
