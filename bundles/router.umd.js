c25e5a4c86559c940b1081f443aaf8dec0ad94ff
atscott <atscott@google.com>
2019-08-27 16:32:23 +0000

    f209aacbf docs(language-service): update integration test information (#32269)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+51.sha-55eaa5f.with-local-changes
+ * @license Angular v9.0.0-next.3+52.sha-f209aac.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5895,7 +5895,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.3+51.sha-55eaa5f.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.3+52.sha-f209aac.with-local-changes');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+51.sha-55eaa5f.with-local-changes
+ * @license Angular v9.0.0-next.3+52.sha-f209aac.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
