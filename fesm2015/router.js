8021054ddbcce4069a427301066c802a874161d7
atscott <atscott@google.com>
2019-08-27 23:15:41 +0000

    d170e4578 docs: change lite-server baseDir path to output folder (#32164)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+25.sha-7033e7e.with-local-changes
+ * @license Angular v8.2.3+26.sha-d170e45.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9896,7 +9896,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.3+25.sha-7033e7e.with-local-changes');
+const VERSION = new Version('8.2.3+26.sha-d170e45.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+25.sha-7033e7e.with-local-changes
+ * @license Angular v8.2.3+26.sha-d170e45.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
