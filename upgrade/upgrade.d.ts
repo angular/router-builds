ebfba902cadc6d45dc17a3e7ce174f4ad67e46e5
atscott <atscott@google.com>
2019-08-27 23:31:14 +0000

    ed6dcce13 docs: update marketing resources.json with ExtAngular (#32126)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+26.sha-d170e45.with-local-changes
+ * @license Angular v8.2.3+27.sha-ed6dcce.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
