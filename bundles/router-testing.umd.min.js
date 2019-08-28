903ea2a68dfd9f4fb9f87ae8c1de2538a8c81bd3
Andrew Scott <atscott01@gmail.com>
2019-08-28 19:27:00 +0000

    261593aab docs: update release notes for 8.2.4 to indicate a docs only release

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+35.sha-b7f95be.with-local-changes
+ * @license Angular v8.2.4+1.sha-261593a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+35.sha-b7f95be.with-local-changes
+ * @license Angular v8.2.4+1.sha-261593a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9899,7 +9899,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.3+35.sha-b7f95be.with-local-changes');
+const VERSION = new Version('8.2.4+1.sha-261593a.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
