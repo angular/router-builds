0f213a4500930944bb4b83f71d460d950607a4c5
atscott <atscott@google.com>
2019-08-28 16:24:08 +0000

    125ef0a0e build(docs-infra): upgrade cli command docs sources to 0d56c1f5f (#32367)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+57.sha-ef20475.with-local-changes
+ * @license Angular v9.0.0-next.3+61.sha-125ef0a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -288,7 +288,10 @@
         return NavigationEnd;
     }(RouterEvent));
     /**
-     * An event triggered when a navigation is canceled.
+     * An event triggered when a navigation is canceled, directly or indirectly.
+     *
+     * This can happen when a [route guard](guide/router#milestone-5-route-guards)
+     * returns `false` or initiates a redirect by returning a `UrlTree`.
      *
      * @publicApi
      */
@@ -5868,7 +5871,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.3+57.sha-ef20475.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.3+61.sha-125ef0a.with-local-changes');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+57.sha-ef20475.with-local-changes
+ * @license Angular v9.0.0-next.3+61.sha-125ef0a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
