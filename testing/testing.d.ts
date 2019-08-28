498be89988704d21998a31493d8ba17868e69471
atscott <atscott@google.com>
2019-08-28 16:25:41 +0000

    2f6062f63 docs: fix callout header (#32325)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+61.sha-125ef0a.with-local-changes
+ * @license Angular v9.0.0-next.3+63.sha-2f6062f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
