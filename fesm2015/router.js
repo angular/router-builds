6abc84658c59485cffc214ab03fcbf77c3f4c9c6
Miško Hevery <misko@hevery.com>
2019-08-29 00:31:24 +0000

    b094936d7 refactor(core): remove more misc unused utilities from DomAdapters (#32291)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4.with-local-changes
+ * @license Angular v9.0.0-next.4+7.sha-b094936.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10092,7 +10092,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.4.with-local-changes');
+const VERSION = new Version('9.0.0-next.4+7.sha-b094936.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
