dd234bbd4fff97dce37ee11671ca62c4279e9203
Miško Hevery <misko@hevery.com>
2019-08-29 00:46:55 +0000

    97fc45f32 fix(language-service): Invalidate Reflector caches when program changes (#32357)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+19.sha-d4703d9.with-local-changes
+ * @license Angular v9.0.0-next.4+17.sha-97fc45f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5871,7 +5871,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.4+19.sha-d4703d9.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.4+17.sha-97fc45f.with-local-changes');
 
     /**
      * @license
