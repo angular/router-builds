e470a302e4b77adf65e4657d51309f22f5e84bf4
Miško Hevery <misko@hevery.com>
2019-08-29 19:49:23 +0000

    63dff9c88 ci: temporarily disable chrome mobile tests on android (#32403)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+21.sha-18ce58c.with-local-changes
+ * @license Angular v9.0.0-next.4+30.sha-63dff9c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5712,7 +5712,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.4+21.sha-18ce58c.with-local-changes');
+var VERSION = new Version('9.0.0-next.4+30.sha-63dff9c.with-local-changes');
 
 /**
  * @license
