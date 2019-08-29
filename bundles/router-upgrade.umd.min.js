f238dc426ba7403ac4f44e3d5c7bbbb1c69fa576
Miško Hevery <misko@hevery.com>
2019-08-29 19:58:19 +0000

    266c267b6 test: export TableModule in benchmarks (#32404)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+33.sha-b4d3468.with-local-changes
+ * @license Angular v9.0.0-next.4+32.sha-266c267.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
