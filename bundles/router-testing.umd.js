d021223787d0e6885eb035465c8f68cfc68bf1c8
Miško Hevery <misko@hevery.com>
2019-08-29 19:57:16 +0000

    9d2348b9a docs: fix typo in compiler options guide (#32348)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+6.sha-4c5cfec.with-local-changes
+ * @license Angular v8.2.4+5.sha-9d2348b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
