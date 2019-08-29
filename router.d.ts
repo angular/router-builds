82c4aa5335bb18f541d44a443799687b194b30d6
Miško Hevery <misko@hevery.com>
2019-08-29 19:52:24 +0000

    d5101dff3 fix(ivy): ngcc - improve the "ngcc version changed" error message (#32396)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+30.sha-63dff9c.with-local-changes
+ * @license Angular v9.0.0-next.4+31.sha-d5101df.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
