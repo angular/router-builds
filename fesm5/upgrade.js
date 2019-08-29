dea0660a47f8460daad2ff8b2748b657dad1aa79
Miško Hevery <misko@hevery.com>
2019-08-29 00:30:53 +0000

    46caf88b2 feat(language-service): add definitions for templateUrl (#32238)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+7.sha-b094936.with-local-changes
+ * @license Angular v9.0.0-next.4+1.sha-46caf88.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
