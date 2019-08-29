652f239e4f50c1fc7c434fafe10ce725a5a719a9
Miško Hevery <misko@hevery.com>
2019-08-29 19:53:51 +0000

    d5101dff3 fix(ivy): ngcc - improve the "ngcc version changed" error message (#32396)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+30.sha-63dff9c.with-local-changes
+ * @license Angular v9.0.0-next.4+31.sha-d5101df.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10092,7 +10092,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.4+30.sha-63dff9c.with-local-changes');
+const VERSION = new Version('9.0.0-next.4+31.sha-d5101df.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
