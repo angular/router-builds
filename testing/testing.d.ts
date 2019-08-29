5468a4138f082cb701bdc5aec9070aaa2682af62
Miško Hevery <misko@hevery.com>
2019-08-29 19:49:17 +0000

    45c909a23 ci: temporarily disable chrome mobile tests on android (#32403)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+2.sha-5b76b93.with-local-changes
+ * @license Angular v8.2.4+3.sha-45c909a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
