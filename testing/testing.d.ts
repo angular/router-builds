c499900837da76ba1852814bee602725938c57ab
Miško Hevery <misko@hevery.com>
2019-08-29 00:38:15 +0000

    5b76b939a build: bump yarn requirement to 1.17.3 (#32344)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+1.sha-261593a.with-local-changes
+ * @license Angular v8.2.4+2.sha-5b76b93.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
