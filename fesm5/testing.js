de94afd7d1bb79977ca353792706482215a3671a
Miško Hevery <misko@hevery.com>
2019-08-29 00:32:31 +0000

    581b837e8 perf(ivy): remove repeated memory read / write in addComponentLogic (#32339)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+9.sha-d0f3539.with-local-changes
+ * @license Angular v9.0.0-next.4+12.sha-581b837.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
