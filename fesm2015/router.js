c2a3d3482c7f10f0ca2c19401cda4f6986e05e7f
Miško Hevery <misko@hevery.com>
2019-08-29 00:33:27 +0000

    d0f3539e6 test(core): cleanup bazel target names for schematic tests (#32318)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+1.sha-46caf88.with-local-changes
+ * @license Angular v9.0.0-next.4+9.sha-d0f3539.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10092,7 +10092,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.4+1.sha-46caf88.with-local-changes');
+const VERSION = new Version('9.0.0-next.4+9.sha-d0f3539.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
