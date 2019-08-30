6f78bf1ad7afcaf584582045f9a57d0957b6eedc
Misko Hevery <misko@hevery.com>
2019-08-30 20:08:18 +0000

    c8b065524 refactor(ivy): cleanup di tests from render3 (#32165)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+55.sha-d498314.with-local-changes
+ * @license Angular v9.0.0-next.4+60.sha-c8b0655.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
