851ac76c569aaa2ce091a87a7d5e5f44b2076c05
Matias Niemelä <matias@yearofmoo.com>
2019-08-30 18:12:48 +0000

    3a45bef0e docs: add clarification regarding development in older browsers in Deployment guide (#32210)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+5.sha-9d2348b.with-local-changes
+ * @license Angular v8.2.4+8.sha-3a45bef.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
