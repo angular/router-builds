94bb33911e11574fab08ce90746456eb96766c80
Matias Niemelä <matias@yearofmoo.com>
2019-08-30 18:16:05 +0000

    efd13d31f build(docs-infra): upgrade cli command docs sources to 6734efe52 (#32414)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+8.sha-3a45bef.with-local-changes
+ * @license Angular v8.2.4+9.sha-efd13d3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9899,7 +9899,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.4+8.sha-3a45bef.with-local-changes');
+const VERSION = new Version('8.2.4+9.sha-efd13d3.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+8.sha-3a45bef.with-local-changes
+ * @license Angular v8.2.4+9.sha-efd13d3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
