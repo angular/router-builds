010d05a7ab33f06650a20c3edcd4f0821478a4ae
Misko Hevery <misko@hevery.com>
2019-08-30 20:02:10 +0000

    8dc3f3647 perf(ivy): properly initialise global state in the element_text_create benchmark (#32397)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+52.sha-43619fc.with-local-changes
+ * @license Angular v9.0.0-next.4+54.sha-8dc3f36.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
