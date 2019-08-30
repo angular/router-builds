3671511468dafcdfe0514b8a946cb0a2b3e07786
Matias Niemelä <matias@yearofmoo.com>
2019-08-30 18:15:45 +0000

    9bbe67f28 docs: add clarification regarding development in older browsers in Deployment guide (#32210)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+49.sha-8c388e3.with-local-changes
+ * @license Angular v9.0.0-next.4+48.sha-9bbe67f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10091,7 +10091,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.4+49.sha-8c388e3.with-local-changes');
+const VERSION = new Version('9.0.0-next.4+48.sha-9bbe67f.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
