5f20c5d778825d5fde4d92a1bcc22e0da8ec42cf
Misko Hevery <misko@hevery.com>
2019-08-30 20:11:33 +0000

    260217a80 fix(ivy): Prevent errors when querying for elements outside Angular context (#32361)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+59.sha-60f9639.with-local-changes
+ * @license Angular v9.0.0-next.4+61.sha-260217a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
