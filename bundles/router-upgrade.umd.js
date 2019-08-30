f53a345a027c169b7a2db1ba89304242fcbae12c
Matias Niemelä <matias@yearofmoo.com>
2019-08-30 18:22:32 +0000

    350ea47de feat(bazel): update to the latest `@microsoft/api-extractor` (#32185)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+46.sha-ba5e07e.with-local-changes
+ * @license Angular v9.0.0-next.4+50.sha-350ea47.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
