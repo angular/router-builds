c1b4ba504cb2fc199f68ba1a9f254f3d9eaa24ea
Misko Hevery <misko@hevery.com>
2019-08-30 20:12:47 +0000

    60f963986 ci: update codeowners for docs guides/examples/images (#32360)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+56.sha-c56c241.with-local-changes
+ * @license Angular v9.0.0-next.4+59.sha-60f9639.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5871,7 +5871,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.4+56.sha-c56c241.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.4+59.sha-60f9639.with-local-changes');
 
     /**
      * @license
