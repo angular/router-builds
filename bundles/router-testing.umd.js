24e2c4a58141baf9d6f46bae498e30db721e36eb
Matias Niemelä <matias@yearofmoo.com>
2019-08-30 18:14:50 +0000

    ba5e07efc perf(ivy): add a micro benchmark for map-based style and class bindings (#32401)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+48.sha-9bbe67f.with-local-changes
+ * @license Angular v9.0.0-next.4+46.sha-ba5e07e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+48.sha-9bbe67f.with-local-changes
+ * @license Angular v9.0.0-next.4+46.sha-ba5e07e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9898,7 +9898,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.4+48.sha-9bbe67f.with-local-changes');
+const VERSION = new Version('9.0.0-next.4+46.sha-ba5e07e.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
