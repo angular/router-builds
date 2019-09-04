b5587dfdf679e5c8602592286229b5655af50101
Miško Hevery <misko@hevery.com>
2019-09-04 18:56:00 +0000

    e213080c0 docs: add app-shell production reference (#32471)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+79.sha-04d4fea.with-local-changes
+ * @license Angular v9.0.0-next.4+80.sha-e213080.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5711,7 +5711,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.4+79.sha-04d4fea.with-local-changes');
+var VERSION = new Version('9.0.0-next.4+80.sha-e213080.with-local-changes');
 
 /**
  * @license
