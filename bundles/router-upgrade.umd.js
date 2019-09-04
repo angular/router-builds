dfc279791c176f42e575b2131a1a315dd8040206
Miško Hevery <misko@hevery.com>
2019-09-04 19:00:21 +0000

    cfa09b84d docs: fix typo in structural directive example (#32459) Fixes #32458

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+80.sha-e213080.with-local-changes
+ * @license Angular v9.0.0-next.4+81.sha-cfa09b8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
