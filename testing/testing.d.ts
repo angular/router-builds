33a45b70180fb9429c9d98df550128fdd501626b
Miško Hevery <misko@hevery.com>
2019-09-04 21:04:29 +0000

    8be20f3a3 build: fix incorrect formating which caused a broken build

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+19.sha-5112669.with-local-changes
+ * @license Angular v8.2.4+20.sha-8be20f3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
