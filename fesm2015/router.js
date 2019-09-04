ade191122841948b64391401c9de6e8f0b9ed972
Miško Hevery <misko@hevery.com>
2019-09-04 19:01:22 +0000

    a383a5a16 refactor(ivy): simplify property binding metadata storage (#32457)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+81.sha-cfa09b8.with-local-changes
+ * @license Angular v9.0.0-next.4+82.sha-a383a5a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9898,7 +9898,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.4+81.sha-cfa09b8.with-local-changes');
+const VERSION = new Version('9.0.0-next.4+82.sha-a383a5a.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+81.sha-cfa09b8.with-local-changes
+ * @license Angular v9.0.0-next.4+82.sha-a383a5a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
