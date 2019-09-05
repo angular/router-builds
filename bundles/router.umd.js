00b9acef97ed40f6aa1e15698def91045c0ea484
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 23:09:27 +0000

    a9ff48e67 fix(core): improve the "missing `$localize`" error message (#32491)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+16.sha-da42a76.with-local-changes
+ * @license Angular v9.0.0-next.5+17.sha-a9ff48e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5871,7 +5871,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.5+16.sha-da42a76.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.5+17.sha-a9ff48e.with-local-changes');
 
     /**
      * @license
