26e99505996cd99faf91d417fef3768416d36aca
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 17:55:02 +0000

    e505fa61f docs: add Alexey Zuev to GDE resources (#32440)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+9.sha-cc06153.with-local-changes
+ * @license Angular v9.0.0-next.5+10.sha-e505fa6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5711,7 +5711,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.5+9.sha-cc06153.with-local-changes');
+var VERSION = new Version('9.0.0-next.5+10.sha-e505fa6.with-local-changes');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+9.sha-cc06153.with-local-changes
+ * @license Angular v9.0.0-next.5+10.sha-e505fa6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
