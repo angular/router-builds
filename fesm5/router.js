d367078a092bd13932389db842d858e03824f021
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 22:29:57 +0000

    497d6b132 ci: re-run flaky docs examples e2e tests in `test_docs_examples[_ivy]` jobs (#32497)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+14.sha-fed6b25.with-local-changes
+ * @license Angular v9.0.0-next.5+20.sha-497d6b1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5711,7 +5711,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.5+14.sha-fed6b25.with-local-changes');
+var VERSION = new Version('9.0.0-next.5+20.sha-497d6b1.with-local-changes');
 
 /**
  * @license
