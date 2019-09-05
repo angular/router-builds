81dc744575091825714b4ab6427deb88698262e4
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 22:32:21 +0000

    098feec4a fix(ivy): maintain coalesced listeners order (#32484)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+19.sha-62d92f8.with-local-changes
+ * @license Angular v9.0.0-next.5+18.sha-098feec.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5871,7 +5871,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.5+19.sha-62d92f8.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.5+18.sha-098feec.with-local-changes');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+19.sha-62d92f8.with-local-changes
+ * @license Angular v9.0.0-next.5+18.sha-098feec.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10091,7 +10091,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.5+19.sha-62d92f8.with-local-changes');
+const VERSION = new Version('9.0.0-next.5+18.sha-098feec.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
