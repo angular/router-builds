f36fe2637d754943f8adf4d8f398b4aa0858982c
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 22:30:00 +0000

    5ab7cb418 refactor(ivy): remove superfluous bind() function (#32489)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+13.sha-7bbc352.with-local-changes
+ * @license Angular v9.0.0-next.5+15.sha-5ab7cb4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
