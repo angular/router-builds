6604217ab41f0cb194c5210b775acb94cb8393dd
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 17:53:51 +0000

    896a8a441 docs: added note in animation keyframe offset (#32350)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.5+5.sha-be30f25.with-local-changes
+ * @license Angular v8.2.5+3.sha-896a8a4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
