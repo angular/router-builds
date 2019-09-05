bd478b2fe34e3e742dcd9228b4bea213ebe1d68d
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 23:07:58 +0000

    a9ff48e67 fix(core): improve the "missing `$localize`" error message (#32491)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+16.sha-da42a76.with-local-changes
+ * @license Angular v9.0.0-next.5+17.sha-a9ff48e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
