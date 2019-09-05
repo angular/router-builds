9ff0b2152b860f87344a3627aacc88c50af3be70
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 18:47:32 +0000

    7bbc3523b ci: set up angular-automatic-lock-bot for the repo (#32500)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+12.sha-f00d033.with-local-changes
+ * @license Angular v9.0.0-next.5+13.sha-7bbc352.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
