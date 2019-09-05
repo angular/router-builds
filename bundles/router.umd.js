13e2026047aa3636244afc99b46b94dccf7edce9
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 17:55:27 +0000

    36d613dd6 docs: update default path for xi18n (#32480)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+6.sha-094538c.with-local-changes
+ * @license Angular v9.0.0-next.5+11.sha-36d613d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5871,7 +5871,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.5+6.sha-094538c.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.5+11.sha-36d613d.with-local-changes');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+6.sha-094538c.with-local-changes
+ * @license Angular v9.0.0-next.5+11.sha-36d613d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10091,7 +10091,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.5+6.sha-094538c.with-local-changes');
+const VERSION = new Version('9.0.0-next.5+11.sha-36d613d.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
