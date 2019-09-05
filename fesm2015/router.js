fd915a05423ca77811e574f5b87747625bbcdf3e
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 17:53:45 +0000

    be30f25da docs: update default path for xi18n (#32480)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+20.sha-8be20f3.with-local-changes
+ * @license Angular v8.2.5+5.sha-be30f25.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9899,7 +9899,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.4+20.sha-8be20f3.with-local-changes');
+const VERSION = new Version('8.2.5+5.sha-be30f25.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+20.sha-8be20f3.with-local-changes
+ * @license Angular v8.2.5+5.sha-be30f25.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
