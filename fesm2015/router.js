26884e19962ad36a24b402d427a64786f0156e7e
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 22:30:23 +0000

    62d92f809 fix(ivy): unable to bind to properties that start with class or style (#32421)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+20.sha-497d6b1.with-local-changes
+ * @license Angular v9.0.0-next.5+19.sha-62d92f8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9898,7 +9898,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.5+20.sha-497d6b1.with-local-changes');
+const VERSION = new Version('9.0.0-next.5+19.sha-62d92f8.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
