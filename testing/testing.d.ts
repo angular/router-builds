3bb9e0f7c1641ca5fed2b16b29a4a077f16d3019
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 17:54:48 +0000

    cc06153e8 docs: added note in animation keyframe offset (#32350)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+11.sha-36d613d.with-local-changes
+ * @license Angular v9.0.0-next.5+9.sha-cc06153.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
