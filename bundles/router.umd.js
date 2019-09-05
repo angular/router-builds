71468b51f9be0bf5ce5c3d0e70bccce37343cfc0
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 23:21:46 +0000

    4c3674f66 docs: expand abbreviation in providers guide (#32400)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+17.sha-a9ff48e.with-local-changes
+ * @license Angular v9.0.0-next.5+21.sha-4c3674f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5898,7 +5898,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.5+17.sha-a9ff48e.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.5+21.sha-4c3674f.with-local-changes');
 
     /**
      * @license
