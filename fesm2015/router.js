1b27f85e3108af828c60d668faf01ccffc6b11a7
Matias Niemelä <matias@yearofmoo.com>
2019-09-06 22:48:04 +0000

    8838a1b54 docs: edit copy and example in hierarchical injectors guide (#32501)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.5+8.sha-e742edc.with-local-changes
+ * @license Angular v8.2.5+9.sha-8838a1b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9899,7 +9899,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.5+8.sha-e742edc.with-local-changes');
+const VERSION = new Version('8.2.5+9.sha-8838a1b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.5+8.sha-e742edc.with-local-changes
+ * @license Angular v8.2.5+9.sha-8838a1b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
