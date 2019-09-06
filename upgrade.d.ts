62431da7d3113fb6ea55e9f1e951eaecad66fff7
Matias Niemelä <matias@yearofmoo.com>
2019-09-06 19:18:40 +0000

    e742edca7 docs: add akita to the list of data libraries (#32505)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.5+7.sha-8304343.with-local-changes
+ * @license Angular v8.2.5+8.sha-e742edc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
