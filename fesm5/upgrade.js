11da2cd5b56049f8cef057d71ddbf68bf5a24322
Matias Niemelä <matias@yearofmoo.com>
2019-09-06 19:18:11 +0000

    8ae7f1425 docs: add akita to the list of data libraries (#32505)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+22.sha-bc061b7.with-local-changes
+ * @license Angular v9.0.0-next.5+23.sha-8ae7f14.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
