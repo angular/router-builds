20a3838719e673a28c5d3d9770cc3536d0a8e477
Matias Niemelä <matias@yearofmoo.com>
2019-09-09 23:32:33 +0000

    ea6a2e9f2 fix(ivy): template compiler should render correct $localize placeholder names (#32509)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+48.sha-9166baf.with-local-changes
+ * @license Angular v9.0.0-next.5+49.sha-ea6a2e9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9898,7 +9898,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.5+48.sha-9166baf.with-local-changes');
+const VERSION = new Version('9.0.0-next.5+49.sha-ea6a2e9.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
