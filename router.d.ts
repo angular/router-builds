1253ab2eb79896e08aec75cf9792d039fbae924b
Matias Niemelä <matias@yearofmoo.com>
2019-09-09 20:15:00 +0000

    48e4b0eb7 docs: add NG Rome MMXIX to Events resources (#31500)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.5+10.sha-aaea387.with-local-changes
+ * @license Angular v8.2.5+12.sha-48e4b0e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
