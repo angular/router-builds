6ebbdb5ab01387e397d889b1676a5322aa139537
Matias Niemelä <matias@yearofmoo.com>
2019-09-09 20:16:03 +0000

    66df74572 docs: add NG Rome MMXIX to Events resources (#31500)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+42.sha-18798a1.with-local-changes
+ * @license Angular v9.0.0-next.5+43.sha-66df745.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
