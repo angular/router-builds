bb138c0addb872c0fd9739667c5c00282bc4b16c
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 12:19:03 +0000

    bbd4a33f6 build(docs-infra): upgrade cli command docs sources to 53ae26cb6 (#32576)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+57.sha-5b2408f.with-local-changes
+ * @license Angular v9.0.0-next.5+56.sha-bbd4a33.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4312,7 +4312,7 @@
         /**
          * Applies an array of commands to the current URL tree and creates a new URL tree.
          *
-         * When given an activated route, applies the given commands starting from the route.
+         * When given an activate route, applies the given commands starting from the route.
          * Otherwise, applies the given command starting from the root.
          *
          * @param commands An array of commands to apply.
@@ -5871,7 +5871,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.5+57.sha-5b2408f.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.5+56.sha-bbd4a33.with-local-changes');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+57.sha-5b2408f.with-local-changes
+ * @license Angular v9.0.0-next.5+56.sha-bbd4a33.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
