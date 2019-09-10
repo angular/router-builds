ce1275b1908bafbac228630df1815324a6c070d8
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 10:50:59 +0000

    ded57245e fix(ivy): match class and attribute value without case-sensitivity (#32548)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+51.sha-664e001.with-local-changes
+ * @license Angular v9.0.0-next.5+54.sha-ded5724.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
