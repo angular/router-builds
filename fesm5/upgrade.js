61859bf0ae326c16430890ff4f5d112315be5177
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 10:50:33 +0000

    664e0015d perf(ivy): replace select instruction with advance (#32516)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+49.sha-ea6a2e9.with-local-changes
+ * @license Angular v9.0.0-next.5+51.sha-664e001.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
