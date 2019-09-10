f3d5850611e4cce088ed3bd063f55f6d27495c15
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 17:27:07 +0000

    e38351fce docs: fixed typo for toh-pt5.md (#32362)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+58.sha-f00bb85.with-local-changes
+ * @license Angular v9.0.0-next.5+59.sha-e38351f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
