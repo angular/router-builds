891d3f0d7777401dfd13e5b2f97adc1b5287d54d
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 10:54:14 +0000

    6052b12fb fix(language-service): Use module resolution cache (#32479)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+52.sha-a1beba4.with-local-changes
+ * @license Angular v9.0.0-next.5+55.sha-6052b12.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
