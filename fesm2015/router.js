70bdbc055188ac74d587292db01230bb33f329bc
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 12:17:40 +0000

    5b2408f0a docs: fix typos in the routing and testing documentation (#32329)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+55.sha-6052b12.with-local-changes
+ * @license Angular v9.0.0-next.5+57.sha-5b2408f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7328,7 +7328,7 @@ class Router {
     /**
      * Applies an array of commands to the current URL tree and creates a new URL tree.
      *
-     * When given an activate route, applies the given commands starting from the route.
+     * When given an activated route, applies the given commands starting from the route.
      * Otherwise, applies the given command starting from the root.
      *
      * \@usageNotes
@@ -10091,7 +10091,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.5+55.sha-6052b12.with-local-changes');
+const VERSION = new Version('9.0.0-next.5+57.sha-5b2408f.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+55.sha-6052b12.with-local-changes
+ * @license Angular v9.0.0-next.5+57.sha-5b2408f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4125,7 +4125,7 @@ var Router = /** @class */ (function () {
     /**
      * Applies an array of commands to the current URL tree and creates a new URL tree.
      *
-     * When given an activate route, applies the given commands starting from the route.
+     * When given an activated route, applies the given commands starting from the route.
      * Otherwise, applies the given command starting from the root.
      *
      * @param commands An array of commands to apply.
@@ -5684,7 +5684,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.5+55.sha-6052b12.with-local-changes');
+var VERSION = new Version('9.0.0-next.5+57.sha-5b2408f.with-local-changes');
 
 /**
  * @license
