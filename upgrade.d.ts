f7ec13f35ffa402911905a9ec334e4a78e4e5c78
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 10:50:18 +0000

    2230dfaea docs: describe your change... (#32552)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+54.sha-ded5724.with-local-changes
+ * @license Angular v9.0.0-next.5+50.sha-2230dfa.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
