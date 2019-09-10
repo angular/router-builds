7dfc539fb9325f0a28952926f215ec36daf33df5
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 10:51:54 +0000

    2230dfaea docs: describe your change... (#32552)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+54.sha-ded5724.with-local-changes
+ * @license Angular v9.0.0-next.5+50.sha-2230dfa.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5871,7 +5871,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.5+54.sha-ded5724.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.5+50.sha-2230dfa.with-local-changes');
 
     /**
      * @license
