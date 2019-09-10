a14124b827d2ba910cdc822d75208504b5cca8f5
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 21:47:31 +0000

    2565f6795 docs: add mdb angular boilerplate to docs resources (#30448)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.5+17.sha-38f4dcd.with-local-changes
+ * @license Angular v8.2.5+18.sha-2565f67.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
