bceeab3aa6ec33428a89560d8823520816af6716
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 17:31:29 +0000

    25d90dabb docs: port accessibility app from stackblitz to aio (#32377)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+60.sha-544eb89.with-local-changes
+ * @license Angular v9.0.0-next.5+61.sha-25d90da.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
