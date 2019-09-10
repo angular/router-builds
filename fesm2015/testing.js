6a5957f6a9bf5c9e6fbe36590b7fe28e615283d3
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 22:14:20 +0000

    a813ae07e test: make expanding_rows test work in g3 (#32568)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+72.sha-6ccb4e0.with-local-changes
+ * @license Angular v9.0.0-next.5+73.sha-a813ae0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
