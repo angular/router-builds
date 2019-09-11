595a59d819bdca94288fe10c39a020205a8d79d9
Matias Niemelä <matias@yearofmoo.com>
2019-09-11 23:46:07 +0000

    a32df388f docs: fix doc links to current version (#32566)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+5.sha-4f03323.with-local-changes
+ * @license Angular v9.0.0-next.6+8.sha-a32df38.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
