9fab8501c577e6cfaab3c262ad46104a976b3078
Matias Niemelä <matias@yearofmoo.com>
2019-09-11 23:34:36 +0000

    f5982fd74 feat(core): add dynamic queries schematic (#32231)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+4.sha-53dbff6.with-local-changes
+ * @license Angular v9.0.0-next.6+6.sha-f5982fd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5711,7 +5711,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.6+4.sha-53dbff6.with-local-changes');
+var VERSION = new Version('9.0.0-next.6+6.sha-f5982fd.with-local-changes');
 
 /**
  * @license
