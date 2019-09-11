729744d70aee5e765472151a74422364bc05b0f1
Matias Niemelä <matias@yearofmoo.com>
2019-09-11 23:49:05 +0000

    8a911773b docs: add header to code examples (#32563)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+10.sha-b6f439d.with-local-changes
+ * @license Angular v9.0.0-next.6+7.sha-8a91177.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
