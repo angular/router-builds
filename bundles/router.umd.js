aefc294f110f8f7debde16b00b52c0e2ae9dd584
Matias Niemelä <matias@yearofmoo.com>
2019-09-11 23:47:35 +0000

    8a911773b docs: add header to code examples (#32563)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+10.sha-b6f439d.with-local-changes
+ * @license Angular v9.0.0-next.6+7.sha-8a91177.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5898,7 +5898,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.6+10.sha-b6f439d.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.6+7.sha-8a91177.with-local-changes');
 
     /**
      * @license
