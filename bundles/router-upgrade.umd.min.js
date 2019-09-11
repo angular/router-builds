3079685acac1ec8ac091ed567ab9d4ceb1b6e2bc
Matias Niemelä <matias@yearofmoo.com>
2019-09-11 23:47:26 +0000

    b6f439d91 refactor(language-service): remove unnecessary size check (#32587)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+9.sha-d30cd33.with-local-changes
+ * @license Angular v9.0.0-next.6+10.sha-b6f439d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
