e2828a51e56f7282a48208f62831d1205f109401
Matias Niemelä <matias@yearofmoo.com>
2019-09-11 22:45:33 +0000

    53dbff66d revert: refactor(ivy): remove styling state storage and introduce direct style writing (#32259)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+3.sha-bb9e612.with-local-changes
+ * @license Angular v9.0.0-next.6+4.sha-53dbff6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
