c275e8aed471be38b6ba598e6d1b2187a022136b
Matias Niemelä <matias@yearofmoo.com>
2019-09-11 21:03:08 +0000

    bb9e61202 docs: remove duplicate entry in changelog for 8.2.6

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+2.sha-c1ac21d.with-local-changes
+ * @license Angular v9.0.0-next.6+3.sha-bb9e612.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9898,7 +9898,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.6+2.sha-c1ac21d.with-local-changes');
+const VERSION = new Version('9.0.0-next.6+3.sha-bb9e612.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
