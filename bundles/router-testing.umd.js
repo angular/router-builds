29be45e16a03e068b2385ffcd7010756a399e621
Kara Erickson <karakara@google.com>
2019-09-12 17:45:37 +0000

    5cf597249 docs: modify lazy-load module instructions for new cli flag (#32588)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+20.sha-7a569a7.with-local-changes
+ * @license Angular v9.0.0-next.6+19.sha-5cf5972.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
