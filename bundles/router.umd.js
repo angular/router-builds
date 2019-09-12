d07203ebee4206afa22f6cbaeee91c1c63d0df6c
Matias Niemelä <matias@yearofmoo.com>
2019-09-12 16:56:52 +0000

    0a0489d3d ci: run lock closed issues more frequently during backlog clearing, update to latest version from dev-infra (#32637)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+17.sha-fcdd068.with-local-changes
+ * @license Angular v9.0.0-next.6+18.sha-0a0489d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5871,7 +5871,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.6+17.sha-fcdd068.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.6+18.sha-0a0489d.with-local-changes');
 
     /**
      * @license
