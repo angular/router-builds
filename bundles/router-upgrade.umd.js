2f729bbc7bf5f4dbfa4c162c87d0a61dea2cb33e
Kara Erickson <karakara@google.com>
2019-09-12 18:14:31 +0000

    222af3846 docs: move renderer2 deprecation guide into own file (#32626)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+28.sha-73cb581.with-local-changes
+ * @license Angular v9.0.0-next.6+29.sha-222af38.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+28.sha-73cb581.with-local-changes
+ * @license Angular v9.0.0-next.6+29.sha-222af38.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9898,7 +9898,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.6+28.sha-73cb581.with-local-changes');
+const VERSION = new Version('9.0.0-next.6+29.sha-222af38.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
