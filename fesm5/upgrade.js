418e431864b975367621341a11a677a3d321231a
Matias Niemelä <matias@yearofmoo.com>
2019-09-12 00:44:11 +0000

    31f06ee3f build: enforce proper compile mode for size-tracking test (#32613)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.6+6.sha-08a7c6f.with-local-changes
+ * @license Angular v8.2.6+5.sha-31f06ee.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
