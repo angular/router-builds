5851f82a207b6da2e509d1935a2dbc4e0386af64
Matias Niemelä <matias@yearofmoo.com>
2019-09-12 00:50:10 +0000

    5f095a501 fix(core): initialize global ngDevMode without toplevel side effects (#32079)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+14.sha-a85eccd.with-local-changes
+ * @license Angular v9.0.0-next.6+15.sha-5f095a5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
