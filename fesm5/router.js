4a0bc080aa3849b763ab7a3e4f2e0278f5ac5f58
Kara Erickson <karakara@google.com>
2019-09-12 22:12:51 +0000

    e82f56b96 feat(language-service): enable logging on TypeScriptHost (#32645)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+39.sha-3c7da76.with-local-changes
+ * @license Angular v9.0.0-next.6+40.sha-e82f56b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5711,7 +5711,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.6+39.sha-3c7da76.with-local-changes');
+var VERSION = new Version('9.0.0-next.6+40.sha-e82f56b.with-local-changes');
 
 /**
  * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+39.sha-3c7da76.with-local-changes
+ * @license Angular v9.0.0-next.6+40.sha-e82f56b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
