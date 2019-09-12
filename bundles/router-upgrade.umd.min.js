9391366d3ae37665bacd6ada580a831d5a4da7e2
Kara Erickson <karakara@google.com>
2019-09-12 19:38:06 +0000

    bd679581e docs: add undecorated classes migration faq (#32478)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+35.sha-ad178c5.with-local-changes
+ * @license Angular v9.0.0-next.6+36.sha-bd67958.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
