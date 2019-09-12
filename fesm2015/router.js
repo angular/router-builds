cc1e703f4dc143c7a39c2da4bf0928072fc359c0
Kara Erickson <karakara@google.com>
2019-09-12 18:52:28 +0000

    e013aee63 refactor(ivy): migrate export spec from render3 (#32624)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+31.sha-373e133.with-local-changes
+ * @license Angular v9.0.0-next.6+32.sha-e013aee.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10091,7 +10091,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.6+31.sha-373e133.with-local-changes');
+const VERSION = new Version('9.0.0-next.6+32.sha-e013aee.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
