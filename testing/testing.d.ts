da3ebed849cc6138a300557ce3a24160522d23f8
Kara Erickson <karakara@google.com>
2019-09-12 23:46:36 +0000

    2bf5606bb feat(ivy): i18n - reorganize entry-points for better reuse (#32488)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+40.sha-e82f56b.with-local-changes
+ * @license Angular v9.0.0-next.6+41.sha-2bf5606.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
