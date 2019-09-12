c58ea230d78fddabb6e8751256ec58037c2bd514
Matias Niemelä <matias@yearofmoo.com>
2019-09-12 00:44:26 +0000

    97dae900f build: enforce proper compile mode for size-tracking test (#32613)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+11.sha-f0a9695.with-local-changes
+ * @license Angular v9.0.0-next.6+12.sha-97dae90.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9898,7 +9898,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.6+11.sha-f0a9695.with-local-changes');
+const VERSION = new Version('9.0.0-next.6+12.sha-97dae90.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
