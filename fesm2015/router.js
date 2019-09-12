dbd980f914a35f568a4af393589abf4ef375ed0c
Kara Erickson <karakara@google.com>
2019-09-12 19:31:13 +0000

    ad178c55f perf(ivy): initialise TNode inputs / outputs on the first creation pass (#32608)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+34.sha-adeee0f.with-local-changes
+ * @license Angular v9.0.0-next.6+35.sha-ad178c5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9898,7 +9898,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.6+34.sha-adeee0f.with-local-changes');
+const VERSION = new Version('9.0.0-next.6+35.sha-ad178c5.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
