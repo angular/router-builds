224327552bab632d95393b74e12ef5cedc6d37ad
Kara Erickson <karakara@google.com>
2019-09-12 17:53:39 +0000

    1771d6ff2 fix(language-service): Lazily instantiate MetadataResolver (#32631)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+27.sha-527ce3b.with-local-changes
+ * @license Angular v9.0.0-next.6+21.sha-1771d6f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+27.sha-527ce3b.with-local-changes
+ * @license Angular v9.0.0-next.6+21.sha-1771d6f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5871,7 +5871,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.6+27.sha-527ce3b.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.6+21.sha-1771d6f.with-local-changes');
 
     /**
      * @license
