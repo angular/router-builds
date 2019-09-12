826a3c2d76d3c4d7b126dc30a09c739acd5931b9
Kara Erickson <karakara@google.com>
2019-09-12 20:39:20 +0000

    3c7da767d fix(ngcc): resolve imports in `.d.ts` files for UMD/CommonJS bundles (#32619)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+37.sha-5a830c4.with-local-changes
+ * @license Angular v9.0.0-next.6+39.sha-3c7da76.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
