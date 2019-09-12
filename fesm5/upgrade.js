0152f6984d98b97b099edce455f1a6405768d1ac
Kara Erickson <karakara@google.com>
2019-09-12 18:34:47 +0000

    373e1337d fix(ngcc): consistently use outer declaration for classes (#32539)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+29.sha-222af38.with-local-changes
+ * @license Angular v9.0.0-next.6+31.sha-373e133.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
