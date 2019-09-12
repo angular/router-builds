6a8d9b7fb47dfd5a22f5d5ca8297279d6268a7fe
Kara Erickson <karakara@google.com>
2019-09-12 19:38:13 +0000

    581b99143 docs: add undecorated classes migration faq (#32478)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.6+9.sha-4014aab.with-local-changes
+ * @license Angular v8.2.6+10.sha-581b991.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
