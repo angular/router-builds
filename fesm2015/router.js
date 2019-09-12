db5ca2ac9cb4fdcf967f8ab1f4b576aadb33146d
Matias Niemelä <matias@yearofmoo.com>
2019-09-12 00:51:37 +0000

    5f095a501 fix(core): initialize global ngDevMode without toplevel side effects (#32079)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+14.sha-a85eccd.with-local-changes
+ * @license Angular v9.0.0-next.6+15.sha-5f095a5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10091,7 +10091,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.6+14.sha-a85eccd.with-local-changes');
+const VERSION = new Version('9.0.0-next.6+15.sha-5f095a5.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
