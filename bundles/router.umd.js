d7a7ee98e9c7fadb2bc716f06e34ba8ba236d065
Kara Erickson <karakara@google.com>
2019-09-13 16:17:15 +0000

    e1065eec5 refactor(language-service): let -> const (#32668)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+45.sha-082b0d3.with-local-changes
+ * @license Angular v9.0.0-next.6+46.sha-e1065ee.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5898,7 +5898,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.6+45.sha-082b0d3.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.6+46.sha-e1065ee.with-local-changes');
 
     /**
      * @license
