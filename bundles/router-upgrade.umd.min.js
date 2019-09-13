4eff8d99d794e8eba561f81938c8d40a1a579828
Kara Erickson <karakara@google.com>
2019-09-13 16:16:53 +0000

    6cd61aeb1 ci: add @josephperrott to global approvers in `CODEOWNERS` (#32667)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.6+10.sha-581b991.with-local-changes
+ * @license Angular v8.2.6+11.sha-6cd61ae.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.6+10.sha-581b991.with-local-changes
+ * @license Angular v8.2.6+11.sha-6cd61ae.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9899,7 +9899,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.6+10.sha-581b991.with-local-changes');
+const VERSION = new Version('8.2.6+11.sha-6cd61ae.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
