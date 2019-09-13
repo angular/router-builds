6dcdcde846dd00cf52428c64f61b240c092e2b4a
Kara Erickson <karakara@google.com>
2019-09-13 20:45:25 +0000

    217db9b21 feat(bazel): support ts_library targets as entry-points for ng_package (#32610)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+50.sha-be13e8b.with-local-changes
+ * @license Angular v9.0.0-next.6+51.sha-217db9b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5711,7 +5711,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.6+50.sha-be13e8b.with-local-changes');
+var VERSION = new Version('9.0.0-next.6+51.sha-217db9b.with-local-changes');
 
 /**
  * @license
