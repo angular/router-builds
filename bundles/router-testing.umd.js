6dc726d4e57bda17a8fbef2bb41e5f39fa1b413d
Kara Erickson <karakara@google.com>
2019-09-16 16:27:46 +0000

    f6d66671b fix(compiler-cli): fix typo in diagnostic template info. (#32684)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+53.sha-3a90a7c.with-local-changes
+ * @license Angular v9.0.0-next.6+54.sha-f6d6667.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
