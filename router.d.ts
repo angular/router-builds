f4ed1892c0f23aade5112a17f56a1f765af2ac11
Kara Erickson <karakara@google.com>
2019-09-16 18:08:19 +0000

    4c061271d docs: add autoprefixer backward compatability instruction (#32109)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+56.sha-52a6da0.with-local-changes
+ * @license Angular v9.0.0-next.6+57.sha-4c06127.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
