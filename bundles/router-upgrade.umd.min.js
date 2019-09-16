731157c9f2dc46e44fdc371a76fc7d947bd9e51e
Kara Erickson <karakara@google.com>
2019-09-16 18:14:51 +0000

    e6ed4a21e perf(ivy): avoid repeat LView reads in property instructions (#32681)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+57.sha-4c06127.with-local-changes
+ * @license Angular v9.0.0-next.6+58.sha-e6ed4a2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
