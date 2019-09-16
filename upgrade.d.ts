05e54dbb22c45038c4edea961418f734f734677c
Andrew Kushnir <akushnir@google.com>
2019-09-16 23:31:31 +0000

    894c4b539 ci: use automatic selection strategies (#32678)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+62.sha-67d08b1.with-local-changes
+ * @license Angular v9.0.0-next.6+65.sha-894c4b5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
