3bee76a475a2aa376b82454a625fb41bcd8fb811
Andrew Kushnir <akushnir@google.com>
2019-09-16 21:39:35 +0000

    67d08b15e ci: fix name of lock-closed workflow (#32698)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+60.sha-a2e890e.with-local-changes
+ * @license Angular v9.0.0-next.6+62.sha-67d08b1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
