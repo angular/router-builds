98fc27cfdc5c2c1a74f68c9fdb26198338ab3869
Andrew Kushnir <akushnir@google.com>
2019-09-17 21:01:16 +0000

    4c168ed9b feat(language-service): provide diagnostics for invalid styleUrls (#32674)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+72.sha-a0d04c6.with-local-changes
+ * @license Angular v9.0.0-next.6+74.sha-4c168ed.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
