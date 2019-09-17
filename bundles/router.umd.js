0d877463fbf4e12872a62d77d082024cf66612a2
Andrew Kushnir <akushnir@google.com>
2019-09-17 22:35:28 +0000

    252966bcc ci: build bazel saucelabs tests remotely (#32719)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+74.sha-4c168ed.with-local-changes
+ * @license Angular v9.0.0-next.6+76.sha-252966b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5898,7 +5898,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.6+74.sha-4c168ed.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.6+76.sha-252966b.with-local-changes');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+74.sha-4c168ed.with-local-changes
+ * @license Angular v9.0.0-next.6+76.sha-252966b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
