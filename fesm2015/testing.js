d2fd89c485611683a3901caf32f4013cab4e4216
Andrew Kushnir <akushnir@google.com>
2019-09-17 21:08:52 +0000

    e367aa2ca docs: Typo fixes in hierarchical DI guide (#32536)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.6+15.sha-0ee2b75.with-local-changes
+ * @license Angular v8.2.6+16.sha-e367aa2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
