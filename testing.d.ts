ff8f3d7bd805b068a9d9ec367149f9813ee67344
Andrew Kushnir <akushnir@google.com>
2019-09-17 23:24:53 +0000

    ba1ef6b1a fix(bazel): ng_package(data) should support non-text files (#32721)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.6+17.sha-acebf64.with-local-changes
+ * @license Angular v8.2.6+18.sha-ba1ef6b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
