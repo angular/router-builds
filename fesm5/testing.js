7073fb278041a7640f60ff16708f7ad52813ef49
Andrew Kushnir <akushnir@google.com>
2019-09-18 20:23:52 +0000

    2846505db feat(language-service): expose determining the NgModule of a Directive (#32710)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.7.with-local-changes
+ * @license Angular v9.0.0-next.7+1.sha-2846505.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
