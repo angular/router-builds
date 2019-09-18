c68027f1f44002aba99c2f06a0a07b7a828db2ec
Andrew Kushnir <akushnir@google.com>
2019-09-18 20:30:25 +0000

    9d8dc793d test(language-service): Create proper test project (#32653)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.7+1.sha-2846505.with-local-changes
+ * @license Angular v9.0.0-next.7+2.sha-9d8dc79.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5871,7 +5871,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.7+1.sha-2846505.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.7+2.sha-9d8dc79.with-local-changes');
 
     /**
      * @license
