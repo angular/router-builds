7430ea5a1083ef176dc34ed217493aeca2d74914
Andrew Kushnir <akushnir@google.com>
2019-09-18 22:51:50 +0000

    f3859ff2b test(language-service): remove MockData from MockTypescriptHost (#32752)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.7+10.sha-0450f39.with-local-changes
+ * @license Angular v9.0.0-next.7+11.sha-f3859ff.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
