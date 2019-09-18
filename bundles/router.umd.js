da99e81de4411efa14773f7fc63207076e777227
Andrew Kushnir <akushnir@google.com>
2019-09-18 22:30:19 +0000

    0450f3962 refactor(ivy): move all styling util code into `utils/styling_utils.ts` (#32731)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.7+5.sha-708ae4c.with-local-changes
+ * @license Angular v9.0.0-next.7+10.sha-0450f39.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5898,7 +5898,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.7+5.sha-708ae4c.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.7+10.sha-0450f39.with-local-changes');
 
     /**
      * @license
