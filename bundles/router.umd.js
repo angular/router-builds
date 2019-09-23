8c718d26cd4ba9b9ee831228453b9f3dc0f900d6
Andrew Kushnir <akushnir@google.com>
2019-09-23 21:53:13 +0000

    5651fa3a9 fix(ivy): ensure `window.ng.getDebugNode` returns debug info for component elements (#32780)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.7+14.sha-353368c.with-local-changes
+ * @license Angular v9.0.0-next.7+15.sha-5651fa3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5898,7 +5898,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.7+14.sha-353368c.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.7+15.sha-5651fa3.with-local-changes');
 
     /**
      * @license
