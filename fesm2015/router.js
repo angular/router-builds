c643b228190f65febff87926d0ffdb18981a4401
Andrew Kushnir <akushnir@google.com>
2019-09-23 21:47:28 +0000

    353368ccc docs: correct default for router extra option (#32702)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.7+13.sha-7a00c67.with-local-changes
+ * @license Angular v9.0.0-next.7+14.sha-353368c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9688,11 +9688,24 @@ if (false) {
      */
     ExtraOptions.prototype.useHash;
     /**
-     * One of `enabled` (the default) or `disabled`.
-     * By default, the initial navigation starts before the root component is created.
-     * The bootstrap is blocked until the initial navigation is complete.
+     * One of `enabled` or `disabled`.
+     * When set to `enabled`, the initial navigation starts before the root component is created.
+     * The bootstrap is blocked until the initial navigation is complete. This value is required for
+     * [server-side rendering](guide/universal) to work.
      * When set to `disabled`, the initial navigation is not performed.
      * The location listener is set up before the root component gets created.
+     * Use if there is a reason to have more control over when the router
+     * starts its initial navigation due to some complex initialization logic.
+     *
+     * Legacy values are deprecated since v4 and should not be used for new applications:
+     *
+     * * `legacy_enabled` - Default for compatibility.
+     * The initial navigation starts after the root component has been created,
+     * but the bootstrap is not blocked until the initial navigation is complete.
+     * * `legacy_disabled` - The initial navigation is not performed.
+     * The location listener is set up after the root component gets created.
+     * * `true` - same as `legacy_enabled`.
+     * * `false` - same as `legacy_disabled`.
      * @type {?|undefined}
      */
     ExtraOptions.prototype.initialNavigation;
@@ -10091,7 +10104,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.7+13.sha-7a00c67.with-local-changes');
+const VERSION = new Version('9.0.0-next.7+14.sha-353368c.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
