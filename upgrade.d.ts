303c15f9c8bc02baa2df1b530b44ea5b80a7126f
Andrew Kushnir <akushnir@google.com>
2019-09-23 21:56:11 +0000

    52552b052 perf(ivy): remove extra SafeStyle detection code (#32775)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.7+15.sha-5651fa3.with-local-changes
+ * @license Angular v9.0.0-next.7+16.sha-52552b0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
