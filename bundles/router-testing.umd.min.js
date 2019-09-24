18dc92c44b7496564ddaebd7350e3f67a70036a2
Andrew Kushnir <akushnir@google.com>
2019-09-24 21:22:46 +0000

    c093390c2 docs: fix typo in upgrade guide (#32807)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.7+20.sha-32b6c22.with-local-changes
+ * @license Angular v9.0.0-next.7+21.sha-c093390.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
