7e20e2c36fd9197ab2de789e80d3a8374c1cf816
Andrew Kushnir <akushnir@google.com>
2019-09-24 19:20:52 +0000

    86fd5719b fix(ivy): ensure multiple map-based bindings do not skip intermediate values (#32774)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.7+18.sha-32f4544.with-local-changes
+ * @license Angular v9.0.0-next.7+19.sha-86fd571.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5711,7 +5711,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.7+18.sha-32f4544.with-local-changes');
+var VERSION = new Version('9.0.0-next.7+19.sha-86fd571.with-local-changes');
 
 /**
  * @license
