0674aec57fb08b86956601e343d1126a5f643288
Andrew Kushnir <akushnir@google.com>
2019-09-24 18:01:02 +0000

    32f4544f3 feat(ivy): improve debugging experience for styles/classes (#32753)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.7+16.sha-52552b0.with-local-changes
+ * @license Angular v9.0.0-next.7+18.sha-32f4544.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
