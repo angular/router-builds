09b222ad744f17129331fe76fdee0cc35d846746
Andrew Kushnir <akushnir@google.com>
2019-09-25 18:54:50 +0000

    0ea4875b1 fix(ngcc): make the build-marker error more clear (#32712)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+4.sha-c134646.with-local-changes
+ * @license Angular v9.0.0-next.8+5.sha-0ea4875.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
