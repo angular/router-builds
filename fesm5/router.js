c6921fb0baa64d0594da16390eb90d0c5e4a77af
Andrew Kushnir <akushnir@google.com>
2019-09-25 18:53:23 +0000

    0ea4875b1 fix(ngcc): make the build-marker error more clear (#32712)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+4.sha-c134646.with-local-changes
+ * @license Angular v9.0.0-next.8+5.sha-0ea4875.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5711,7 +5711,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.8+4.sha-c134646.with-local-changes');
+var VERSION = new Version('9.0.0-next.8+5.sha-0ea4875.with-local-changes');
 
 /**
  * @license
