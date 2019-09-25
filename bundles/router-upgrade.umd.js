f3e26fb5a4530e39dacf41075dc973756ffc1e8b
Andrew Kushnir <akushnir@google.com>
2019-09-25 19:27:04 +0000

    7dbde6f57 docs: remove extra parenthesis from inline animations snippet (#32735)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.7+5.sha-104c786.with-local-changes
+ * @license Angular v8.2.8+1.sha-7dbde6f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
