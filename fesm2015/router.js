971dbb947548e7721a331053bbf2b0f8f7c50397
atscott <atscott@google.com>
2019-09-27 17:27:28 +0000

    f6914159e docs: fix sentence phrasing (#32846)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+13.sha-278d634.with-local-changes
+ * @license Angular v9.0.0-next.8+12.sha-f691415.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9911,7 +9911,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.8+13.sha-278d634.with-local-changes');
+const VERSION = new Version('9.0.0-next.8+12.sha-f691415.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
