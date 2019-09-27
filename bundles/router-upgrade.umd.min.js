5e35b0f89029dfc698ebb825391dc0da4ff5e887
atscott <atscott@google.com>
2019-09-27 00:30:29 +0000

    85990c321 fix(docs-infra): correctly run on IE11 (#32871)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+8.sha-fbad4ff.with-local-changes
+ * @license Angular v9.0.0-next.8+9.sha-85990c3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+8.sha-fbad4ff.with-local-changes
+ * @license Angular v9.0.0-next.8+9.sha-85990c3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5871,7 +5871,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.8+8.sha-fbad4ff.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.8+9.sha-85990c3.with-local-changes');
 
     /**
      * @license
