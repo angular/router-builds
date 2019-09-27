1fb7217cd4c479afbd53a5b94ce52fc4cf4a1005
atscott <atscott@google.com>
2019-09-27 00:27:28 +0000

    e81fc14da fix(docs-infra): correctly run on IE11 (#32871)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+2.sha-d5c210a.with-local-changes
+ * @license Angular v8.2.8+3.sha-e81fc14.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+2.sha-d5c210a.with-local-changes
+ * @license Angular v8.2.8+3.sha-e81fc14.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9912,7 +9912,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.8+2.sha-d5c210a.with-local-changes');
+const VERSION = new Version('8.2.8+3.sha-e81fc14.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
