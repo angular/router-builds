deb1f467330c54cffad18782558abb2770ca194b
atscott <atscott@google.com>
2019-09-27 17:52:51 +0000

    466c754cc docs: fix typo in deployment guide (#32777)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+6.sha-45fee06.with-local-changes
+ * @license Angular v8.2.8+7.sha-466c754.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9912,7 +9912,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.8+6.sha-45fee06.with-local-changes');
+const VERSION = new Version('8.2.8+7.sha-466c754.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+6.sha-45fee06.with-local-changes
+ * @license Angular v8.2.8+7.sha-466c754.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
