acf48c452edebd591958d2efa134ecb79d30ebd9
atscott <atscott@google.com>
2019-09-27 20:35:42 +0000

    e41cbfb58 feat(ivy): support ng-add in localize package (#32791)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+17.sha-84feda1.with-local-changes
+ * @license Angular v9.0.0-next.8+18.sha-e41cbfb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
