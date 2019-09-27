b4f04927e385845039997892ebfeff9987b1321c
atscott <atscott@google.com>
2019-09-27 00:28:58 +0000

    85990c321 fix(docs-infra): correctly run on IE11 (#32871)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+8.sha-fbad4ff.with-local-changes
+ * @license Angular v9.0.0-next.8+9.sha-85990c3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5711,7 +5711,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.8+8.sha-fbad4ff.with-local-changes');
+var VERSION = new Version('9.0.0-next.8+9.sha-85990c3.with-local-changes');
 
 /**
  * @license
