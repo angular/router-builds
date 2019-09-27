8db04e30d3fe6c7766a7102dbdf8602db6df23c1
atscott <atscott@google.com>
2019-09-27 23:21:56 +0000

    877f9ad7b docs: add missing space between "Component" and "will" in guide (#32885)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+19.sha-a23ee2b.with-local-changes
+ * @license Angular v9.0.0-next.8+20.sha-877f9ad.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5871,7 +5871,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.8+19.sha-a23ee2b.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.8+20.sha-877f9ad.with-local-changes');
 
     /**
      * @license
