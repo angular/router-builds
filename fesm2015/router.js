761fdda417df4826a01fb67e8ac42513bb8f71a2
atscott <atscott@google.com>
2019-09-30 21:38:00 +0000

    45c893d0e docs(service-worker): add info about recovering from degraded Driver state (#32682)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+28.sha-53b32f1.with-local-changes
+ * @license Angular v9.0.0-next.8+27.sha-45c893d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9908,7 +9908,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.8+28.sha-53b32f1.with-local-changes');
+const VERSION = new Version('9.0.0-next.8+27.sha-45c893d.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
