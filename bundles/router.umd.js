c09b296dd4465a13d15c0799834c670d3176e1e4
atscott <atscott@google.com>
2019-09-30 17:49:53 +0000

    f6526a087 docs: add filename headers to code examples (#32701)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+9.sha-bdea243.with-local-changes
+ * @license Angular v8.2.8+12.sha-f6526a0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5898,7 +5898,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.8+9.sha-bdea243.with-local-changes');
+    var VERSION = new core.Version('8.2.8+12.sha-f6526a0.with-local-changes');
 
     /**
      * @license
