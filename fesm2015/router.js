f78ec857be0223f251ea6c4ebfae057a6ebf62c6
atscott <atscott@google.com>
2019-09-30 21:33:52 +0000

    747f0cff9 fix(ngcc): handle presence of both `ctorParameters` and `__decorate` (#32901)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+24.sha-002a97d.with-local-changes
+ * @license Angular v9.0.0-next.8+25.sha-747f0cf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10101,7 +10101,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.8+24.sha-002a97d.with-local-changes');
+const VERSION = new Version('9.0.0-next.8+25.sha-747f0cf.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
