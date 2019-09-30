f88227a1d591562a8a1541bfbc299ea4e95d2fa0
atscott <atscott@google.com>
2019-09-30 17:54:30 +0000

    898523380 docs: add prompts section to schematic guide (#32728)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+22.sha-d507165.with-local-changes
+ * @license Angular v9.0.0-next.8+21.sha-8985233.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
