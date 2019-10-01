788ab5a5fa2202bf69389833e17bfda41895855b
atscott <atscott01@google.com>
2019-10-01 22:05:39 +0000

    32b6548be docs: fix capitalization in elements docs (#32917) (#32936)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+34.sha-98feee7.with-local-changes
+ * @license Angular v9.0.0-next.8+35.sha-32b6548.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
