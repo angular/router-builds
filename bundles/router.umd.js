b253c5a6f2bc6c8d2f20b2677ea4f2b1c0955022
atscott <atscott@google.com>
2019-10-01 18:20:39 +0000

    ffc34b367 perf(ivy): avoid repeat global state accesses in i18n instructions (#32916)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+29.sha-475e36a.with-local-changes
+ * @license Angular v9.0.0-next.8+31.sha-ffc34b3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5871,7 +5871,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.8+29.sha-475e36a.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.8+31.sha-ffc34b3.with-local-changes');
 
     /**
      * @license
