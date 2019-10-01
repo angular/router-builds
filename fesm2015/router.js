d359e5475e0756b8114aa2f760eb3049ceb3a9e3
atscott <atscott01@google.com>
2019-10-01 22:03:52 +0000

    42f741952 docs: emphasize headers for code examples (#32938)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+16.sha-06e7272.with-local-changes
+ * @license Angular v8.2.8+18.sha-42f7419.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9909,7 +9909,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.8+16.sha-06e7272.with-local-changes');
+const VERSION = new Version('8.2.8+18.sha-42f7419.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
