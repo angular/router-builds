114467fa32ddbd6bb4a29705c342e93e470066fc
atscott <atscott01@google.com>
2019-10-01 22:26:36 +0000

    66658c447 feat: update rxjs peerDependencies minimum requirment to 6.5.3 (#32812)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+40.sha-01e4d44.with-local-changes
+ * @license Angular v9.0.0-next.8+39.sha-66658c4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+40.sha-01e4d44.with-local-changes
+ * @license Angular v9.0.0-next.8+39.sha-66658c4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5871,7 +5871,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.8+40.sha-01e4d44.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.8+39.sha-66658c4.with-local-changes');
 
     /**
      * @license
