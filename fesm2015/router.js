ff62a506d3a52ad75cc379f1f7db8f9af8653335
atscott <atscott@google.com>
2019-10-01 18:19:11 +0000

    ffc34b367 perf(ivy): avoid repeat global state accesses in i18n instructions (#32916)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+29.sha-475e36a.with-local-changes
+ * @license Angular v9.0.0-next.8+31.sha-ffc34b3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9908,7 +9908,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.8+29.sha-475e36a.with-local-changes');
+const VERSION = new Version('9.0.0-next.8+31.sha-ffc34b3.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+29.sha-475e36a.with-local-changes
+ * @license Angular v9.0.0-next.8+31.sha-ffc34b3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5711,7 +5711,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.8+29.sha-475e36a.with-local-changes');
+var VERSION = new Version('9.0.0-next.8+31.sha-ffc34b3.with-local-changes');
 
 /**
  * @license
