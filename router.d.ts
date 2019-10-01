65a0db8eaee03febe5a8fb83c9e146a5525e7022
atscott <atscott@google.com>
2019-10-01 18:23:56 +0000

    06e72721c docs: clarify http response type (#32817)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+14.sha-0818e2c.with-local-changes
+ * @license Angular v8.2.8+16.sha-06e7272.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
