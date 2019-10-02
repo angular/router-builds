610f360f4a2e312d409da438b554495b8428c63b
atscott <atscott01@google.com>
2019-10-02 00:44:20 +0000

    206fb8233 docs: add dynamic queries flag migration documentation (#32582)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+41.sha-63256b5.with-local-changes
+ * @license Angular v9.0.0-next.8+42.sha-206fb82.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5898,7 +5898,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.8+41.sha-63256b5.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.8+42.sha-206fb82.with-local-changes');
 
     /**
      * @license
