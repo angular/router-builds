6ef1dfac7e25ed764fe96d37e74d47b950fcd15b
atscott <atscott01@google.com>
2019-10-02 00:37:23 +0000

    63256b511 fix(ivy): Only restore registered modules if user compiles modules with TestBed (#32944)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+39.sha-66658c4.with-local-changes
+ * @license Angular v9.0.0-next.8+41.sha-63256b5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5711,7 +5711,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.8+39.sha-66658c4.with-local-changes');
+var VERSION = new Version('9.0.0-next.8+41.sha-63256b5.with-local-changes');
 
 /**
  * @license
