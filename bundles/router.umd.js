270e043ecfd7f8fadb67e3c2913fb58dd70a95a0
atscott <atscott01@gmail.com>
2019-10-02 20:51:57 +0000

    0d186dda3 feat(language-service): module definitions on directive hover (#32763)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+8.sha-e7729b6.with-local-changes
+ * @license Angular v9.0.0-next.9+7.sha-0d186dd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5873,7 +5873,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.9+8.sha-e7729b6.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.9+7.sha-0d186dd.with-local-changes');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+8.sha-e7729b6.with-local-changes
+ * @license Angular v9.0.0-next.9+7.sha-0d186dd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
