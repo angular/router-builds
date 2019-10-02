cea0d0c08b170a2def2e0af32be9d92e0b22d33f
atscott <atscott01@google.com>
2019-10-02 00:39:14 +0000

    b45d2e0fb docs: add dynamic queries flag migration documentation (#32582)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+18.sha-42f7419.with-local-changes
+ * @license Angular v8.2.8+19.sha-b45d2e0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
