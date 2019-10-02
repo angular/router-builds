0b7c9549de18e417fe5f7fa0b6e065e82c0a3392
atscott <atscott01@google.com>
2019-10-02 17:06:51 +0000

    448749caf docs: added '>' to ng-template closing tag. (#32939)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+51.sha-948b01c.with-local-changes
+ * @license Angular v9.0.0-next.8+53.sha-448749c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
