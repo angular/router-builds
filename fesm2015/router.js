bc1d0e6bd1b89b1ada60671327fef7d580ac51f2
atscott <atscott01@google.com>
2019-10-02 18:02:03 +0000

    879ad6960 Revert "feat(core): make static query flag optional (#32686)" (#32965)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+54.sha-39e8ceb.with-local-changes
+ * @license Angular v9.0.0-next.8+55.sha-879ad69.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9908,7 +9908,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.8+54.sha-39e8ceb.with-local-changes');
+const VERSION = new Version('9.0.0-next.8+55.sha-879ad69.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
