a10e4129e6ce419dca531fa3c79bb8a284a34eb8
atscott <atscott01@google.com>
2019-10-02 16:55:17 +0000

    5d5c94d83 feat(service-worker): remove deprecated `versionedFiles` option (#32862)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+50.sha-0f3a48e.with-local-changes
+ * @license Angular v9.0.0-next.8+44.sha-5d5c94d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
