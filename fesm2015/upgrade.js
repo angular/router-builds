af30bfb97e7b1d91a3b0766d54cc74a0e24849f4
atscott <atscott01@google.com>
2019-10-02 18:03:32 +0000

    879ad6960 Revert "feat(core): make static query flag optional (#32686)" (#32965)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+54.sha-39e8ceb.with-local-changes
+ * @license Angular v9.0.0-next.8+55.sha-879ad69.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
