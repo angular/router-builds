e88dbe9a35b9a9219142fabc8af046a17fc0de45
atscott <atscott01@gmail.com>
2019-10-02 18:52:59 +0000

    be83ae752 docs: release notes for the v8.2.9 release

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+58.sha-70429bf.with-local-changes
+ * @license Angular v9.0.0-next.8+59.sha-be83ae7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
