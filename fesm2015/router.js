d9822d9e2440ecda014e48331bdeb140752d78e6
atscott <atscott01@gmail.com>
2019-10-02 21:07:02 +0000

    9ba898d58 docs: remove Renderer2 section (#32972)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+24.sha-d96167f.with-local-changes
+ * @license Angular v8.2.9+9.sha-9ba898d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9909,7 +9909,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.8+24.sha-d96167f.with-local-changes');
+const VERSION = new Version('8.2.9+9.sha-9ba898d.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+24.sha-d96167f.with-local-changes
+ * @license Angular v8.2.9+9.sha-9ba898d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
