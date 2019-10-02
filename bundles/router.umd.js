78a72d471619df2faeba6b50bd044656055c64ce
atscott <atscott01@google.com>
2019-10-02 17:22:40 +0000

    39e8ceb2c fix(upgrade): fix AngularJsUrlCodec to support Safari (#32959)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+53.sha-448749c.with-local-changes
+ * @license Angular v9.0.0-next.8+54.sha-39e8ceb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5871,7 +5871,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.8+53.sha-448749c.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.8+54.sha-39e8ceb.with-local-changes');
 
     /**
      * @license
