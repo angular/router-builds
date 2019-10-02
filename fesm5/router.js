1224f77c9580d2534c5bbe8475169ccdfc5c6daf
atscott <atscott01@google.com>
2019-10-02 16:58:23 +0000

    25219baeb feat(core): make static query flag optional (#32686)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+44.sha-5d5c94d.with-local-changes
+ * @license Angular v9.0.0-next.8+52.sha-25219ba.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5711,7 +5711,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.8+44.sha-5d5c94d.with-local-changes');
+var VERSION = new Version('9.0.0-next.8+52.sha-25219ba.with-local-changes');
 
 /**
  * @license
