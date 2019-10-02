b1f05c72cc7b12cf7bf3643d8893c7bba1368985
atscott <atscott01@google.com>
2019-10-02 16:58:40 +0000

    948b01ce5 feat(core): default to dynamic queries (#32720)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+52.sha-25219ba.with-local-changes
+ * @license Angular v9.0.0-next.8+51.sha-948b01c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
