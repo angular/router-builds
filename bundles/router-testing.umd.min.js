cb670bd8ef5d6f18a8dc02a6940a356f856fa43f
atscott <atscott01@gmail.com>
2019-10-03 17:19:25 +0000

    e24393c35 test(upgrade): add unit tests for AngularJSUrlCodec's parse method (#32976)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.9+10.sha-8237e95.with-local-changes
+ * @license Angular v8.2.9+11.sha-e24393c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
