0c7caf48fc31c0b9833827a836a8551197a1e53c
atscott <atscott@google.com>
2019-10-03 19:47:28 +0000

    7806596fb feat(core): default to dynamic queries (#32720)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+28.sha-deaac32.with-local-changes
+ * @license Angular v9.0.0-next.9+29.sha-7806596.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
