23072f036be0cb8cf5c4deff48cfd3249f4395e4
atscott <atscott@google.com>
2019-10-03 19:40:33 +0000

    dd2587d9e build: create script for setting up RBE in local dev environment (#31200)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.9+13.sha-2742649.with-local-changes
+ * @license Angular v8.2.9+14.sha-dd2587d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9909,7 +9909,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.9+13.sha-2742649.with-local-changes');
+const VERSION = new Version('8.2.9+14.sha-dd2587d.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.9+13.sha-2742649.with-local-changes
+ * @license Angular v8.2.9+14.sha-dd2587d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
