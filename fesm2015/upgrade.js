975ecf7c72129818330a5fb6009a59f5c80969b2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-04 19:08:30 +0000

    3a53e2c96 fix(core): ngNoopZone should have the same signature with ngZone (#32068)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+45.sha-01677b2.with-local-changes
+ * @license Angular v9.0.0-next.9+46.sha-3a53e2c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
