76891b15f0cf30f5800ba4b600cb187aec9540f3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 18:16:34 +0000

    393398e6f build(docs-infra): update in-memory-we-api and karma-jasmine-html-reporter version (#32892)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+55.sha-e007918.with-local-changes
+ * @license Angular v9.0.0-next.9+56.sha-393398e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5873,7 +5873,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.9+55.sha-e007918.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.9+56.sha-393398e.with-local-changes');
 
     /**
      * @license
