2b76336e1ebb4dd660326c6491e412fd49bffcfb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 17:47:23 +0000

    bad343433 fix(ivy): avoid exposing `ng` with Closure Compiler enhanced optimizations (#33010)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+52.sha-d18289f.with-local-changes
+ * @license Angular v9.0.0-next.9+51.sha-bad3434.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5686,7 +5686,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.9+52.sha-d18289f.with-local-changes');
+var VERSION = new Version('9.0.0-next.9+51.sha-bad3434.with-local-changes');
 
 /**
  * @license
