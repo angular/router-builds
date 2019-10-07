9f0d2aafa3c241e0f68105f334a17b3039b09565
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 18:45:03 +0000

    3541e590f docs: fix accessibility lint rules (#32661)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.9+19.sha-4263d9e.with-local-changes
+ * @license Angular v8.2.9+26.sha-3541e59.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
