e2bbceeafd3d20135a3ea8248c2f691e1b6d8777
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 18:19:28 +0000

    c61e4d784 fix(ivy): process nested animation metadata (#32818)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+56.sha-393398e.with-local-changes
+ * @license Angular v9.0.0-next.9+57.sha-c61e4d7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
