bfbaaf99af9c32c49f46bfa9c92bc179c0f87003
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 18:17:42 +0000

    c61e4d784 fix(ivy): process nested animation metadata (#32818)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+56.sha-393398e.with-local-changes
+ * @license Angular v9.0.0-next.9+57.sha-c61e4d7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5898,7 +5898,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.9+56.sha-393398e.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.9+57.sha-c61e4d7.with-local-changes');
 
     /**
      * @license
