531935f448b216ef05fc1141c5f2d2fe00f1da67
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 17:45:39 +0000

    bad343433 fix(ivy): avoid exposing `ng` with Closure Compiler enhanced optimizations (#33010)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+52.sha-d18289f.with-local-changes
+ * @license Angular v9.0.0-next.9+51.sha-bad3434.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
