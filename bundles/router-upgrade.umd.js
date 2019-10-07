14390b8875cf8e0002d64a2153cbc7c253ce21b9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 18:28:49 +0000

    c1bb88603 fix(common): expand type for "ngForOf" input to work with strict null checks (#31371)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+61.sha-2089727.with-local-changes
+ * @license Angular v9.0.0-next.9+60.sha-c1bb886.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
