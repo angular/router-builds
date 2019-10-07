cb6b2f915e8261ae960b7ec39b0c4360c6f44a74
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 18:21:46 +0000

    fee28e20b feat(forms): formGroupName and formArrayName also accepts a number (#32607)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+57.sha-c61e4d7.with-local-changes
+ * @license Angular v9.0.0-next.9+59.sha-fee28e2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5873,7 +5873,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.9+57.sha-c61e4d7.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.9+59.sha-fee28e2.with-local-changes');
 
     /**
      * @license
