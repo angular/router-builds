0f7706b88617f98b61fb686a92343fa5a5b22719
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 18:45:06 +0000

    40d87dd80 docs: fix accessibility lint rules (#32661)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+62.sha-0ce8621.with-local-changes
+ * @license Angular v9.0.0-next.9+63.sha-40d87dd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9908,7 +9908,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.9+62.sha-0ce8621.with-local-changes');
+const VERSION = new Version('9.0.0-next.9+63.sha-40d87dd.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+62.sha-0ce8621.with-local-changes
+ * @license Angular v9.0.0-next.9+63.sha-40d87dd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
