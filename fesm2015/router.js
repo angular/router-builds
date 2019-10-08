5c6c08383108ae68a218aa2ba212bd691e466b16
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-08 20:25:23 +0000

    9f0c549bc perf(ivy): avoid memory allocation in the isAnimationProp check (#32997)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+73.sha-ed71141.with-local-changes
+ * @license Angular v9.0.0-next.9+75.sha-9f0c549.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10103,7 +10103,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.9+73.sha-ed71141.with-local-changes');
+const VERSION = new Version('9.0.0-next.9+75.sha-9f0c549.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+73.sha-ed71141.with-local-changes
+ * @license Angular v9.0.0-next.9+75.sha-9f0c549.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5686,7 +5686,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.9+73.sha-ed71141.with-local-changes');
+var VERSION = new Version('9.0.0-next.9+75.sha-9f0c549.with-local-changes');
 
 /**
  * @license
