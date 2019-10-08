744f8ab23015ea69083c8341aaa96885a9dd73be
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-08 20:23:53 +0000

    9f0c549bc perf(ivy): avoid memory allocation in the isAnimationProp check (#32997)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+73.sha-ed71141.with-local-changes
+ * @license Angular v9.0.0-next.9+75.sha-9f0c549.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
