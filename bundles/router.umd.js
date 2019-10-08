414e91d1509f149b2330045b952a5ff3c52167ae
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-08 16:49:47 +0000

    2265cb593 refactor(core): remove deprecated Renderer (#33019)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+64.sha-c507dda.with-local-changes
+ * @license Angular v9.0.0-next.9+65.sha-2265cb5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5873,7 +5873,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.9+64.sha-c507dda.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.9+65.sha-2265cb5.with-local-changes');
 
     /**
      * @license
