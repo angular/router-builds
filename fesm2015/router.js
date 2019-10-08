bfa5db77a39c1d860fd48f5e07b7ebfd2fb4253d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-08 18:08:25 +0000

    ed711418f style(bazel): fix 2 unformatted .bzl files

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+72.sha-c653a16.with-local-changes
+ * @license Angular v9.0.0-next.9+73.sha-ed71141.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9908,7 +9908,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.9+72.sha-c653a16.with-local-changes');
+const VERSION = new Version('9.0.0-next.9+73.sha-ed71141.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
