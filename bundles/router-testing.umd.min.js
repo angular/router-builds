bf309cefa288f88b7e32071b7b2038053328ddaf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-09 20:37:57 +0000

    b2b917d2d feat(ngcc): expose `--create-ivy-entry-points` option on ivy-ngcc (#33049)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+75.sha-9f0c549.with-local-changes
+ * @license Angular v9.0.0-next.9+76.sha-b2b917d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
