4c99ec8d3a4a98c1648ea8760a4972a3c33781f5
Miško Hevery <misko@hevery.com>
2019-10-10 22:35:29 +0000

    1ae77da60 docs: update Stackblitz example for lazy-loading (#33070)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+22.sha-35a95a8.with-local-changes
+ * @license Angular v9.0.0-next.10+27.sha-1ae77da.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
