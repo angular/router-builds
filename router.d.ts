fb92b35a1c8259b0623175e59871c01fd2acc821
Miško Hevery <misko@hevery.com>
2019-10-10 21:17:40 +0000

    be54c580b docs(common): Fixes typo (#33003) PR Close #33003

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10.with-local-changes
+ * @license Angular v9.0.0-next.10+4.sha-be54c58.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
