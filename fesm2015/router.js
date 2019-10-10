fd0fb1f1703c35d9229513d711d5541283f6ac34
Miško Hevery <misko@hevery.com>
2019-10-10 21:36:29 +0000

    df78d7c0d build(docs-infra): upgrade cli command docs sources to a0ecddbf1 (#33081)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+19.sha-5ede5b7.with-local-changes
+ * @license Angular v9.0.0-next.10+25.sha-df78d7c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9908,7 +9908,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.10+19.sha-5ede5b7.with-local-changes');
+const VERSION = new Version('9.0.0-next.10+25.sha-df78d7c.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
