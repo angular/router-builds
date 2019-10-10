6bfe3ba9fda66072852dcfd7e644063450d3cae2
Miško Hevery <misko@hevery.com>
2019-10-10 22:42:26 +0000

    6ab5f3648 refactor: utilize type narrowing (#33075)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+27.sha-1ae77da.with-local-changes
+ * @license Angular v9.0.0-next.10+28.sha-6ab5f36.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5873,7 +5873,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.10+27.sha-1ae77da.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.10+28.sha-6ab5f36.with-local-changes');
 
     /**
      * @license
