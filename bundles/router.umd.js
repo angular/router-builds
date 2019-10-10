e6742cbb8bb63bf2d25ef0eb7974320700b20ac2
Miško Hevery <misko@hevery.com>
2019-10-10 21:19:13 +0000

    be54c580b docs(common): Fixes typo (#33003) PR Close #33003

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10.with-local-changes
+ * @license Angular v9.0.0-next.10+4.sha-be54c58.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5873,7 +5873,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.10.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.10+4.sha-be54c58.with-local-changes');
 
     /**
      * @license
