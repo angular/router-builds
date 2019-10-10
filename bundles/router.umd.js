aadb928346bb2ab06f7c3b36edbbb452ead41a50
Miško Hevery <misko@hevery.com>
2019-10-10 21:23:41 +0000

    a3ef3e11c docs: changed 'function' word in to 'steps' (#32211)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+16.sha-8d111da.with-local-changes
+ * @license Angular v9.0.0-next.10+2.sha-a3ef3e1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5898,7 +5898,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.10+16.sha-8d111da.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.10+2.sha-a3ef3e1.with-local-changes');
 
     /**
      * @license
