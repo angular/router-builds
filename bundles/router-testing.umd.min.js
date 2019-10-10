75827d68515e0b996f4be12edbfca2fe1dfcc8ab
Miško Hevery <misko@hevery.com>
2019-10-10 21:38:13 +0000

    90007e97c feat(ngcc): support version ranges in project/default configurations (#33008)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+25.sha-df78d7c.with-local-changes
+ * @license Angular v9.0.0-next.10+24.sha-90007e9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+25.sha-df78d7c.with-local-changes
+ * @license Angular v9.0.0-next.10+24.sha-90007e9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5873,7 +5873,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.10+25.sha-df78d7c.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.10+24.sha-90007e9.with-local-changes');
 
     /**
      * @license
