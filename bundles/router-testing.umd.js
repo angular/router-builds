04b4e10e5a2aaa1a58092d60bde1c6e65b57d5d9
Miško Hevery <misko@hevery.com>
2019-10-11 23:02:59 +0000

    d4d07233d perf(ivy): guard host binding execution with a TNode flag (#33102)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+37.sha-15e3b5f.with-local-changes
+ * @license Angular v9.0.0-next.10+41.sha-d4d0723.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
