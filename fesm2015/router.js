d65c2232e8d27ffceb8e9a71908b07b16200793e
Miško Hevery <misko@hevery.com>
2019-10-11 23:04:30 +0000

    d4d07233d perf(ivy): guard host binding execution with a TNode flag (#33102)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+37.sha-15e3b5f.with-local-changes
+ * @license Angular v9.0.0-next.10+41.sha-d4d0723.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10104,7 +10104,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.10+37.sha-15e3b5f.with-local-changes');
+const VERSION = new Version('9.0.0-next.10+41.sha-d4d0723.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
