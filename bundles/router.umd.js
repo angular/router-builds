acad96d6f65b92ddeb56d09acab99d08c2770f82
Miško Hevery <misko@hevery.com>
2019-10-14 20:37:30 +0000

    b04488d69 feat(compiler): record absolute span of template expressions in parser (#31897)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+57.sha-68f06c8.with-local-changes
+ * @license Angular v9.0.0-next.10+58.sha-b04488d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5874,7 +5874,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.10+57.sha-68f06c8.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.10+58.sha-b04488d.with-local-changes');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+57.sha-68f06c8.with-local-changes
+ * @license Angular v9.0.0-next.10+58.sha-b04488d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
