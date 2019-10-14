46615da007199aa98f4d75a9bb263e72a66f0dc9
Miško Hevery <misko@hevery.com>
2019-10-14 20:50:56 +0000

    39587ad12 fix(compiler-cli): resolve type of exported *ngIf variable. (#33016)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+61.sha-5557dec.with-local-changes
+ * @license Angular v9.0.0-next.10+62.sha-39587ad.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5874,7 +5874,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.10+61.sha-5557dec.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.10+62.sha-39587ad.with-local-changes');
 
     /**
      * @license
