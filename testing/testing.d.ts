0b63b34e6b0d3b026f3e7bf826566994f46963ed
Miško Hevery <misko@hevery.com>
2019-10-14 20:27:28 +0000

    68f06c8dd build: using the xlarge resource class for zonejs tests on CI (#33148)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+55.sha-868b3f9.with-local-changes
+ * @license Angular v9.0.0-next.10+57.sha-68f06c8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
