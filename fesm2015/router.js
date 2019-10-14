66a549dd629a31be7c701c2b09e90dca7c6650c5
Miško Hevery <misko@hevery.com>
2019-10-14 20:37:00 +0000

    9e76a3807 build: using the xlarge resource class for zonejs tests on CI (#33148)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.10+7.sha-2429819.with-local-changes
+ * @license Angular v8.2.10+8.sha-9e76a38.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9909,7 +9909,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.10+7.sha-2429819.with-local-changes');
+const VERSION = new Version('8.2.10+8.sha-9e76a38.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.10+7.sha-2429819.with-local-changes
+ * @license Angular v8.2.10+8.sha-9e76a38.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
