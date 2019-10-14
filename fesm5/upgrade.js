e95828b84e0186943d04048978ee485daae0b390
Miško Hevery <misko@hevery.com>
2019-10-14 20:41:19 +0000

    5557dec12 refactor(core): missing-injectable migration should respect providers of directives and components (#33011)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+58.sha-b04488d.with-local-changes
+ * @license Angular v9.0.0-next.10+61.sha-5557dec.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
