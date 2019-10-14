e6e8a5fdd45b28a70d8aa6e4074baa32c8a20de5
Miško Hevery <misko@hevery.com>
2019-10-14 16:45:02 +0000

    1a67d70bf refactor(core): rename ngDirectiveDef to ɵdir (#33110)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+46.sha-84ba1f0.with-local-changes
+ * @license Angular v9.0.0-next.10+45.sha-1a67d70.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5899,7 +5899,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.10+46.sha-84ba1f0.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.10+45.sha-1a67d70.with-local-changes');
 
     /**
      * @license
