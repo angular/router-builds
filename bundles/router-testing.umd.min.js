42faf8ef78cc2c01c7568f4d695b2bc0e8231554
Miško Hevery <misko@hevery.com>
2019-10-14 20:57:31 +0000

    cd7b19921 feat(ivy): check regular attributes that correspond with directive inputs (#33066)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+62.sha-39587ad.with-local-changes
+ * @license Angular v9.0.0-next.10+66.sha-cd7b199.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
