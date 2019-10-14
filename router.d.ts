67ad2071827facc5c21539f19e973c154dd57745
Miško Hevery <misko@hevery.com>
2019-10-14 16:50:06 +0000

    4b3010873 docs: add note to build Angular locally (#31053)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+48.sha-1a34fbc.with-local-changes
+ * @license Angular v9.0.0-next.10+50.sha-4b30108.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
