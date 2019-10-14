4539f69e570e811119530c0f91c2284624edf832
Miško Hevery <misko@hevery.com>
2019-10-14 16:54:22 +0000

    242981963 docs: add command for creating angular workspace (#32804)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.10+5.sha-4c7e7fb.with-local-changes
+ * @license Angular v8.2.10+7.sha-2429819.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
