014a24aa73f4802a64930c0dac393fe55401ecb3
Miško Hevery <misko@hevery.com>
2019-10-14 16:46:47 +0000

    25af147a8 refactor(ngcc): fix formatting of missing dependencies error (#33139)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+45.sha-1a67d70.with-local-changes
+ * @license Angular v9.0.0-next.10+49.sha-25af147.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9909,7 +9909,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.10+45.sha-1a67d70.with-local-changes');
+const VERSION = new Version('9.0.0-next.10+49.sha-25af147.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
