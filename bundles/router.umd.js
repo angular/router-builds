1225b60faa49d1c182429b694cc78ec098d84e0d
Miško Hevery <misko@hevery.com>
2019-10-14 23:59:09 +0000

    5265b7413 docs: fix typo in guide (#33155)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+76.sha-4c0726d.with-local-changes
+ * @license Angular v9.0.0-next.10+78.sha-5265b74.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5899,7 +5899,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.10+76.sha-4c0726d.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.10+78.sha-5265b74.with-local-changes');
 
     /**
      * @license
