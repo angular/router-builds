0552a14ed8bfa6d915f53d421693838288d04d0e
Miško Hevery <misko@hevery.com>
2019-10-14 16:47:41 +0000

    1a34fbce2 fix(ngcc): rename the executable from `ivy-ngcc` to `ngcc` (#33140)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+49.sha-25af147.with-local-changes
+ * @license Angular v9.0.0-next.10+48.sha-1a34fbc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+49.sha-25af147.with-local-changes
+ * @license Angular v9.0.0-next.10+48.sha-1a34fbc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5899,7 +5899,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.10+49.sha-25af147.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.10+48.sha-1a34fbc.with-local-changes');
 
     /**
      * @license
