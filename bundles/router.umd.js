dd60113831516b823fd012cef2ed58274ecd20bf
Miško Hevery <misko@hevery.com>
2019-10-14 16:51:37 +0000

    4b3010873 docs: add note to build Angular locally (#31053)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+48.sha-1a34fbc.with-local-changes
+ * @license Angular v9.0.0-next.10+50.sha-4b30108.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5874,7 +5874,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.10+48.sha-1a34fbc.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.10+50.sha-4b30108.with-local-changes');
 
     /**
      * @license
