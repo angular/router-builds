ea0e69d15af0a12070c74d09d7446c95ada58e61
Miško Hevery <misko@hevery.com>
2019-10-15 21:22:41 +0000

    11e04b189 fix(ivy): avoid DOM element assertions if procedural renderer is used (#33156)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+93.sha-ec6a9f2.with-local-changes
+ * @license Angular v9.0.0-next.10+94.sha-11e04b1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
