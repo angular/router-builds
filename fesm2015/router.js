5f769c1d8e285d094dbde0e7e426428b3c5a86c0
Miško Hevery <misko@hevery.com>
2019-10-15 21:30:55 +0000

    b71ccc295 docs: fix JDK url (#33168)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.10+13.sha-9409dce.with-local-changes
+ * @license Angular v8.2.10+18.sha-b71ccc2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9913,7 +9913,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.10+13.sha-9409dce.with-local-changes');
+const VERSION = new Version('8.2.10+18.sha-b71ccc2.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.10+13.sha-9409dce.with-local-changes
+ * @license Angular v8.2.10+18.sha-b71ccc2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
