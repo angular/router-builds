728be11dd0a58f08ede7eb4fe3fabfffd91ddd83
Miško Hevery <misko@hevery.com>
2019-10-15 23:20:22 +0000

    4198ea7ca release: cut the v9.0.0-next.11 release

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+96.sha-8c57a23.with-local-changes
+ * @license Angular v9.0.0-next.11.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
