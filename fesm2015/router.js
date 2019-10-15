fa9235ee673da9722a383b7b344a84559ba1d043
Miško Hevery <misko@hevery.com>
2019-10-15 00:07:07 +0000

    8321bd8a7 docs: clarify http header api doc (#33038)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+80.sha-72494c4.with-local-changes
+ * @license Angular v9.0.0-next.10+81.sha-8321bd8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9909,7 +9909,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.10+80.sha-72494c4.with-local-changes');
+const VERSION = new Version('9.0.0-next.10+81.sha-8321bd8.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
