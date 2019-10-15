19c1ffcbacf2cde70d6f349b98bb682abe76b143
Miško Hevery <misko@hevery.com>
2019-10-15 21:30:46 +0000

    ad72c9044 fix(ivy): i18n - add XLIFF aliases for legacy message id support (#33160)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+94.sha-11e04b1.with-local-changes
+ * @license Angular v9.0.0-next.10+95.sha-ad72c90.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5877,7 +5877,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.10+94.sha-11e04b1.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.10+95.sha-ad72c90.with-local-changes');
 
     /**
      * @license
