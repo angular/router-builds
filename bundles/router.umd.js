5bcfa3e055663bca336500348e6ec2d892cac4ad
Matias Niemelä <matias@yearofmoo.com>
2019-10-16 14:45:47 +0000

    67c914819 test(language-service): Move completions test to completions_spec.ts (#33159)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+2.sha-51b9ce4.with-local-changes
+ * @license Angular v9.0.0-next.11+3.sha-67c9148.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5877,7 +5877,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.11+2.sha-51b9ce4.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.11+3.sha-67c9148.with-local-changes');
 
     /**
      * @license
