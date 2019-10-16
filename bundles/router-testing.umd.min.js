7ab9ae93472d0efc6310a06e26078de3db861614
Matias Niemelä <matias@yearofmoo.com>
2019-10-16 21:10:18 +0000

    becd62d4a fix(upgrade): remove unused version export (#33180)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+10.sha-86104b8.with-local-changes
+ * @license Angular v9.0.0-next.11+11.sha-becd62d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
