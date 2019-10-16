08ab432bf4bdb099e74a771112d4aa9242b31b1b
Matias Niemelä <matias@yearofmoo.com>
2019-10-16 15:15:03 +0000

    4659cc26e style: emove unreachable 'return null' statement (#33174)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+3.sha-67c9148.with-local-changes
+ * @license Angular v9.0.0-next.11+4.sha-4659cc2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
