56fbbea025cf7549526f941700756fb46ac26c7d
Matias Niemelä <matias@yearofmoo.com>
2019-10-16 19:11:20 +0000

    3e14c2d02 perf(ivy): limit global state read / write in host bindings (#33195)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+5.sha-4c63e6b.with-local-changes
+ * @license Angular v9.0.0-next.11+8.sha-3e14c2d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
