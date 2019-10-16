f6caf44fcb6ec7a6e2703050ae6d4f1c844f0462
Matias Niemelä <matias@yearofmoo.com>
2019-10-16 21:06:54 +0000

    86104b82b refactor(core): rename ngInjectableDef to ɵprov (#33151)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+8.sha-3e14c2d.with-local-changes
+ * @license Angular v9.0.0-next.11+10.sha-86104b8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+8.sha-3e14c2d.with-local-changes
+ * @license Angular v9.0.0-next.11+10.sha-86104b8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5902,7 +5902,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.11+8.sha-3e14c2d.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.11+10.sha-86104b8.with-local-changes');
 
     /**
      * @license
