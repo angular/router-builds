10fdbd0e8dc0144c4a224d9223ee7f1ea4f14670
Matias Niemelä <matias@yearofmoo.com>
2019-10-16 23:26:17 +0000

    a67df6520 build: update webdriver-manager to support latest Chrome (#33206)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+13.sha-11bf767.with-local-changes
+ * @license Angular v9.0.0-next.11+17.sha-a67df65.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5877,7 +5877,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.11+13.sha-11bf767.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.11+17.sha-a67df65.with-local-changes');
 
     /**
      * @license
