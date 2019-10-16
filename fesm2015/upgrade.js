68a95778a406b87dcc02fb705d4e3948580bc303
Matias Niemelä <matias@yearofmoo.com>
2019-10-16 14:39:50 +0000

    51b9ce44e docs: fix typo in getting started guide (#33185)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11.with-local-changes
+ * @license Angular v9.0.0-next.11+2.sha-51b9ce4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
