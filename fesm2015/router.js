a2cec5ae1a729bd6b691465f1d39f38aa2415acd
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 04:52:13 +0000

    724707c6e feat(ivy): improve debugging experience for styles/classes (#33179)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+18.sha-e9ee685.with-local-changes
+ * @license Angular v9.0.0-next.11+19.sha-724707c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9913,7 +9913,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.11+18.sha-e9ee685.with-local-changes');
+const VERSION = new Version('9.0.0-next.11+19.sha-724707c.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
