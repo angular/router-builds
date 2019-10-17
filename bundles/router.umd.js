2a3853ed9a3270e0e8b48e248e43e94ee9e560f0
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 04:53:46 +0000

    724707c6e feat(ivy): improve debugging experience for styles/classes (#33179)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+18.sha-e9ee685.with-local-changes
+ * @license Angular v9.0.0-next.11+19.sha-724707c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5877,7 +5877,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.11+18.sha-e9ee685.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.11+19.sha-724707c.with-local-changes');
 
     /**
      * @license
