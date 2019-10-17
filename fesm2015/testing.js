48442471d7dd1a646aa9c66f6f0fc7517457536c
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 18:44:22 +0000

    43487f676 docs: correct vscode clang-format setup instructions (#33190)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+37.sha-6f203c9.with-local-changes
+ * @license Angular v9.0.0-next.11+38.sha-43487f6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
