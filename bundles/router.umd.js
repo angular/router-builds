eac1e19016325f0b1c854032ee78ac39925e0e09
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 18:43:41 +0000

    6f203c957 fix(ivy): handling className as an input properly (#33188)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+36.sha-08cb2fa.with-local-changes
+ * @license Angular v9.0.0-next.11+37.sha-6f203c9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5902,7 +5902,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.11+36.sha-08cb2fa.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.11+37.sha-6f203c9.with-local-changes');
 
     /**
      * @license
