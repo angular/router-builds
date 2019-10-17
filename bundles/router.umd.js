b92a6531f720636266cb99a58c3b15947557249c
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 20:26:51 +0000

    1a8bd22fa refactor(core): rename ngLocaleIdDef to ɵloc (#33212)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+39.sha-bb53b65.with-local-changes
+ * @license Angular v9.0.0-next.11+41.sha-1a8bd22.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5902,7 +5902,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.11+39.sha-bb53b65.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.11+41.sha-1a8bd22.with-local-changes');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+39.sha-bb53b65.with-local-changes
+ * @license Angular v9.0.0-next.11+41.sha-1a8bd22.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
