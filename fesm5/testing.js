61b79148ab31e3c28c56fa8815d67504ea047448
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 18:34:08 +0000

    d1323b5c8 test: improve missing-injectable migration test case in ng-update integration test (#33223)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+30.sha-cc7c2a8.with-local-changes
+ * @license Angular v9.0.0-next.11+33.sha-d1323b5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
