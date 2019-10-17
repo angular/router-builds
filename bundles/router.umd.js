c17f79f26ec0b6b0ea940ee2cf34ee68c71e8739
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 20:34:32 +0000

    7e64bbe5a fix(ivy): use container i18n meta if a message is a single ICU (#33191)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+41.sha-1a8bd22.with-local-changes
+ * @license Angular v9.0.0-next.11+42.sha-7e64bbe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5877,7 +5877,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.11+41.sha-1a8bd22.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.11+42.sha-7e64bbe.with-local-changes');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+41.sha-1a8bd22.with-local-changes
+ * @license Angular v9.0.0-next.11+42.sha-7e64bbe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
