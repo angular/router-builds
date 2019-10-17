fa713d88d9780ed3a6ba796bc4fcdee016498fed
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 04:43:52 +0000

    e9ee6859e revert: build: remove vendored Babel typings (#33176) (#33215)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+17.sha-a67df65.with-local-changes
+ * @license Angular v9.0.0-next.11+18.sha-e9ee685.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
