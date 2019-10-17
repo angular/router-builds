4cdc3f789e396d23cfbda1d2603f003294cdac16
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 21:13:04 +0000

    d5b59009d ci: fix dev-infra incorrectly matching all ".bzl" files as codeowner (#32956)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+44.sha-f45c431.with-local-changes
+ * @license Angular v9.0.0-next.11+45.sha-d5b5900.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
