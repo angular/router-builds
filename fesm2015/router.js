e53d4fd70f17d2ff43c65b2c122c211226edb3b9
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 18:42:47 +0000

    fab6ea010 docs: correct vscode clang-format setup instructions (#33190)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+2.sha-37cbcfa.with-local-changes
+ * @license Angular v8.2.11+5.sha-fab6ea0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9913,7 +9913,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.11+2.sha-37cbcfa.with-local-changes');
+const VERSION = new Version('8.2.11+5.sha-fab6ea0.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+2.sha-37cbcfa.with-local-changes
+ * @license Angular v8.2.11+5.sha-fab6ea0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
