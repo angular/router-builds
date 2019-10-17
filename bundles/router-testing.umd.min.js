567348e7f4596e671bd0533791f696bbd64ac5a8
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 20:22:27 +0000

    78214e72e fix(ngcc): avoid warning when reflecting on index signature member (#33198)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+38.sha-43487f6.with-local-changes
+ * @license Angular v9.0.0-next.11+40.sha-78214e7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
