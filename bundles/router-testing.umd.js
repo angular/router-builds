a0b1db1ce28633858339cbe5d737d29c9054e98d
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 06:59:37 +0000

    d192a7b47 revert: fix(docs-infra): consider all formats when compiling docs examples with `ngcc` (#33216)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+19.sha-724707c.with-local-changes
+ * @license Angular v9.0.0-next.11+23.sha-d192a7b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
