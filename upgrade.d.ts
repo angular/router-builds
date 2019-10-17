2e442852e77bb02b5b0195712a3b6cf1e54ea532
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 18:34:31 +0000

    0e260d2c3 style: enforce disallowance of String, Number and Boolean constructors (#33210)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+33.sha-d1323b5.with-local-changes
+ * @license Angular v9.0.0-next.11+26.sha-0e260d2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
