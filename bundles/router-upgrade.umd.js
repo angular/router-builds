5e22b362ea2e075aa5f0e6a7fb66c2d12ad41e28
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 19:17:34 +0000

    0e08ad628a fix(ivy): throw better error for missing generic type in ModuleWithProviders (#33187)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+74.sha-e0059c7.with-local-changes
+ * @license Angular v9.0.0-next.11+75.sha-0e08ad6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
