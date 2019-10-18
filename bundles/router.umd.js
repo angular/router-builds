4643854f86bdd7c4f0e668835aca7e4be8a25ce3
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 18:57:38 +0000

    bfd07b3c94 fix(ngcc): Esm5ReflectionHost.getDeclarationOfIdentifier should handle aliased inner declarations (#33252)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+62.sha-a0d16dc.with-local-changes
+ * @license Angular v9.0.0-next.11+64.sha-bfd07b3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.11+62.sha-a0d16dc.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.11+64.sha-bfd07b3.with-local-changes');
 
     /**
      * @license
