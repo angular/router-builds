12b7ba8f3d04845f720ba2c80da4c8ac837bd2f0
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 15:51:18 +0000

    eaa1984d4 docs: change "patchMatch" to "pathMatch" (#32935)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+58.sha-7fb55f2.with-local-changes
+ * @license Angular v9.0.0-next.11+57.sha-eaa1984.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
