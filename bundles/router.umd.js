30b2d20e7c411814f56976a66a44003097979ba4
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 19:05:33 +0000

    6958d11d95 feat(ivy): type checking of event bindings (#33125)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+67.sha-c44a1a7.with-local-changes
+ * @license Angular v9.0.0-next.11+65.sha-6958d11.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4206,24 +4206,9 @@
                         }
                         var navCancel = new NavigationCancel(t.id, _this.serializeUrl(t.extractedUrl), e.message);
                         eventsSubject.next(navCancel);
-                        // When redirecting, we need to delay resolving the navigation
-                        // promise and push it to the redirect navigation
-                        if (!redirecting) {
-                            t.resolve(false);
-                        }
-                        else {
-                            // setTimeout is required so this navigation finishes with
-                            // the return EMPTY below. If it isn't allowed to finish
-                            // processing, there can be multiple navigations to the same
-                            // URL.
-                            setTimeout(function () {
-                                var mergedTree = _this.urlHandlingStrategy.merge(e.url, _this.rawUrlTree);
-                                var extras = {
-                                    skipLocationChange: t.extras.skipLocationChange,
-                                    replaceUrl: _this.urlUpdateStrategy === 'eager'
-                                };
-                                return _this.scheduleNavigation(mergedTree, 'imperative', null, extras, { resolve: t.resolve, reject: t.reject, promise: t.promise });
-                            }, 0);
+                        t.resolve(false);
+                        if (redirecting) {
+                            _this.navigateByUrl(e.url);
                         }
                         /* All other errors should reset to the router's internal URL reference to the
                          * pre-error state. */
@@ -4512,7 +4497,7 @@
                 t.resolve(true);
             }, function (e) { _this.console.warn("Unhandled Navigation Error: "); });
         };
-        Router.prototype.scheduleNavigation = function (rawUrl, source, restoredState, extras, priorPromise) {
+        Router.prototype.scheduleNavigation = function (rawUrl, source, restoredState, extras) {
             var lastNavigation = this.getTransition();
             // If the user triggers a navigation imperatively (e.g., by using navigateByUrl),
             // and that navigation results in 'replaceState' that leads to the same URL,
@@ -4535,20 +4520,12 @@
                 lastNavigation.rawUrl.toString() === rawUrl.toString()) {
                 return Promise.resolve(true); // return value is not used
             }
-            var resolve;
-            var reject;
-            var promise;
-            if (priorPromise) {
-                resolve = priorPromise.resolve;
-                reject = priorPromise.reject;
-                promise = priorPromise.promise;
-            }
-            else {
-                promise = new Promise(function (res, rej) {
-                    resolve = res;
-                    reject = rej;
-                });
-            }
+            var resolve = null;
+            var reject = null;
+            var promise = new Promise(function (res, rej) {
+                resolve = res;
+                reject = rej;
+            });
             var id = ++this.navigationId;
             this.setTransition({
                 id: id,
@@ -5910,7 +5887,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.11+67.sha-c44a1a7.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.11+65.sha-6958d11.with-local-changes');
 
     /**
      * @license
