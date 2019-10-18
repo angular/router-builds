bda1de09863903928c08c0912118e61a184c45af
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 19:09:10 +0000

    19ada81a08 build: fix tslint issue with String instantiation

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+6.sha-f13e40d.with-local-changes
+ * @license Angular v8.2.11+8.sha-19ada81.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+6.sha-f13e40d.with-local-changes
+ * @license Angular v8.2.11+8.sha-19ada81.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9913,7 +9913,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.11+6.sha-f13e40d.with-local-changes');
+const VERSION = new Version('8.2.11+8.sha-19ada81.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
