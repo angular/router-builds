f2b9bc381b54926995f998f2b2c66b11c4747031
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 19:01:39 +0000

    f6667f8281 fix(router): adjust UrlTree redirect to replace URL if in eager update (#32988)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+63.sha-01d3599.with-local-changes
+ * @license Angular v9.0.0-next.11+66.sha-f6667f8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4201,9 +4201,24 @@
                         }
                         var navCancel = new NavigationCancel(t.id, _this.serializeUrl(t.extractedUrl), e.message);
                         eventsSubject.next(navCancel);
-                        t.resolve(false);
-                        if (redirecting) {
-                            _this.navigateByUrl(e.url);
+                        // When redirecting, we need to delay resolving the navigation
+                        // promise and push it to the redirect navigation
+                        if (!redirecting) {
+                            t.resolve(false);
+                        }
+                        else {
+                            // setTimeout is required so this navigation finishes with
+                            // the return EMPTY below. If it isn't allowed to finish
+                            // processing, there can be multiple navigations to the same
+                            // URL.
+                            setTimeout(function () {
+                                var mergedTree = _this.urlHandlingStrategy.merge(e.url, _this.rawUrlTree);
+                                var extras = {
+                                    skipLocationChange: t.extras.skipLocationChange,
+                                    replaceUrl: _this.urlUpdateStrategy === 'eager'
+                                };
+                                return _this.scheduleNavigation(mergedTree, 'imperative', null, extras, { resolve: t.resolve, reject: t.reject, promise: t.promise });
+                            }, 0);
                         }
                         /* All other errors should reset to the router's internal URL reference to the
                          * pre-error state. */
@@ -4492,7 +4507,7 @@
                 t.resolve(true);
             }, function (e) { _this.console.warn("Unhandled Navigation Error: "); });
         };
-        Router.prototype.scheduleNavigation = function (rawUrl, source, restoredState, extras) {
+        Router.prototype.scheduleNavigation = function (rawUrl, source, restoredState, extras, priorPromise) {
             var lastNavigation = this.getTransition();
             // If the user triggers a navigation imperatively (e.g., by using navigateByUrl),
             // and that navigation results in 'replaceState' that leads to the same URL,
@@ -4515,12 +4530,20 @@
                 lastNavigation.rawUrl.toString() === rawUrl.toString()) {
                 return Promise.resolve(true); // return value is not used
             }
-            var resolve = null;
-            var reject = null;
-            var promise = new Promise(function (res, rej) {
-                resolve = res;
-                reject = rej;
-            });
+            var resolve;
+            var reject;
+            var promise;
+            if (priorPromise) {
+                resolve = priorPromise.resolve;
+                reject = priorPromise.reject;
+                promise = priorPromise.promise;
+            }
+            else {
+                promise = new Promise(function (res, rej) {
+                    resolve = res;
+                    reject = rej;
+                });
+            }
             var id = ++this.navigationId;
             this.setTransition({
                 id: id,
@@ -5912,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.11+63.sha-01d3599.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.11+66.sha-f6667f8.with-local-changes');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+63.sha-01d3599.with-local-changes
+ * @license Angular v9.0.0-next.11+66.sha-f6667f8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
