a79f204dddb409fc69a1cb784641a93da1087fd9
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 01:02:30 +0000

    9e7668f16 fix(common): remove deprecated support for intl API (#29250)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+54.sha-5dfbcd5.with-local-changes
+ * @license Angular v9.0.0-next.11+55.sha-9e7668f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
