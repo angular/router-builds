8b380516a5533479803cefe029e9defbe0885d20
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 20:52:13 +0000

    1799f621b7 refactor(core): deprecate entryComponents (#33205)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12.with-local-changes
+ * @license Angular v9.0.0-next.12+1.sha-1799f62.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
