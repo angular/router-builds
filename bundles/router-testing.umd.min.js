e81b67a6a32a35c0248006f54a6ca764abebb405
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 01:34:45 +0000

    d2222541e refactor(language-service): use strict comparisons (#33114)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+55.sha-9e7668f.with-local-changes
+ * @license Angular v9.0.0-next.11+56.sha-d222254.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+55.sha-9e7668f.with-local-changes
+ * @license Angular v9.0.0-next.11+56.sha-d222254.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5902,7 +5902,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.11+55.sha-9e7668f.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.11+56.sha-d222254.with-local-changes');
 
     /**
      * @license
