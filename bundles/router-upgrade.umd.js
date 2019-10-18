6c471875cbe1d5bbde1d5ee25034bcb20a9dd6cf
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 00:01:09 +0000

    de445709d fix(ivy): use ReflectionHost to check exports when writing an import (#33192)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+48.sha-fd4fed1.with-local-changes
+ * @license Angular v9.0.0-next.11+53.sha-de44570.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
