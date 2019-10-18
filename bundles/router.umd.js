a72ee9b79e36c47513427f36e11fcc5f1e02190a
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 00:04:00 +0000

    5dfbcd563 fix(core): add CLI instructions when localize polyfill is missing (#33199)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+53.sha-de44570.with-local-changes
+ * @license Angular v9.0.0-next.11+54.sha-5dfbcd5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5877,7 +5877,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.11+53.sha-de44570.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.11+54.sha-5dfbcd5.with-local-changes');
 
     /**
      * @license
