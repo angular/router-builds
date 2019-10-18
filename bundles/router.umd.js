790c38ab129eef7d71f149d2d035cbf1ea23aa2a
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 22:34:45 +0000

    8d44b5edac docs: edit and organize language service doc (#33202)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+1.sha-1799f62.with-local-changes
+ * @license Angular v9.0.0-next.12+5.sha-8d44b5e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.12+1.sha-1799f62.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.12+5.sha-8d44b5e.with-local-changes');
 
     /**
      * @license
