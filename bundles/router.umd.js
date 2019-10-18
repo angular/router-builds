6c7cbbf6007295b608b35b1b44f2a73522c40052
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 19:03:45 +0000

    c44a1a78a9 docs: add ayaz hafiz as a contributor (#32975)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+66.sha-f6667f8.with-local-changes
+ * @license Angular v9.0.0-next.11+67.sha-c44a1a7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5910,7 +5910,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.11+66.sha-f6667f8.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.11+67.sha-c44a1a7.with-local-changes');
 
     /**
      * @license
