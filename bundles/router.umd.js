aea2aacd7f193574f3542ffdcb685f66282399b6
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 22:38:07 +0000

    f289411fa9 docs(core): add migration guide links to schematics (#33258)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+5.sha-8d44b5e.with-local-changes
+ * @license Angular v9.0.0-next.12+4.sha-f289411.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5910,7 +5910,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.12+5.sha-8d44b5e.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.12+4.sha-f289411.with-local-changes');
 
     /**
      * @license
