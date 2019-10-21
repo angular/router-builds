6da6308f3b754b487338344b9e109b30977db0cd
Matias Niemelä <matias@yearofmoo.com>
2019-10-21 15:50:16 +0000

    88869a2965 docs: update deployers table with ngx-deploy-npm (#33269)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+10.sha-958aba9.with-local-changes
+ * @license Angular v8.2.11+11.sha-88869a2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+10.sha-958aba9.with-local-changes
+ * @license Angular v8.2.11+11.sha-88869a2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9913,7 +9913,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.11+10.sha-958aba9.with-local-changes');
+const VERSION = new Version('8.2.11+11.sha-88869a2.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
