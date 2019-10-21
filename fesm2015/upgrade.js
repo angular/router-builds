1b1c6945d60b81182ca84b18c5d52ac44d645d96
Matias Niemelä <matias@yearofmoo.com>
2019-10-21 17:09:21 +0000

    e5081bcf25 build: update the integration/payloads limit

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+11.sha-d7dc6cb.with-local-changes
+ * @license Angular v9.0.0-next.12+17.sha-e5081bc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
