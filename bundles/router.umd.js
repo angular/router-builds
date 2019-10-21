a66d4aa5cd3a3eca55dc23dd2400fa94baf6ccd8
Matias Niemelä <matias@yearofmoo.com>
2019-10-21 20:10:56 +0000

    9de4b1c441 test: fix typo in cli e2e descriptions (#33175)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+17.sha-e5081bc.with-local-changes
+ * @license Angular v9.0.0-next.12+24.sha-9de4b1c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.12+17.sha-e5081bc.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.12+24.sha-9de4b1c.with-local-changes');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+17.sha-e5081bc.with-local-changes
+ * @license Angular v9.0.0-next.12+24.sha-9de4b1c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
