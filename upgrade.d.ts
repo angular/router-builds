f4174f8f4bf80646688f7571e4245e067467a19b
Matias Niemelä <matias@yearofmoo.com>
2019-10-21 15:43:14 +0000

    d4db746898 feat(ivy): give shim generation its own compiler options (#33256)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+6.sha-29bc3a7.with-local-changes
+ * @license Angular v9.0.0-next.12+7.sha-d4db746.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
