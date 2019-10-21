3dd66438e8299d07384ee8bef4b822116b76e58c
Matias Niemelä <matias@yearofmoo.com>
2019-10-21 21:14:57 +0000

    bb284066ca build: update bazel remote build config and documentation (#33304)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+13.sha-3f3e1bf.with-local-changes
+ * @license Angular v8.2.11+15.sha-bb28406.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5901,7 +5901,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.11+13.sha-3f3e1bf.with-local-changes');
+    var VERSION = new core.Version('8.2.11+15.sha-bb28406.with-local-changes');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+13.sha-3f3e1bf.with-local-changes
+ * @license Angular v8.2.11+15.sha-bb28406.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
