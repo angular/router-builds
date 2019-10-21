b03d8f20c522192f654d2ac7a1132581b9d5fd30
Matias Niemelä <matias@yearofmoo.com>
2019-10-21 21:11:49 +0000

    ec482dadb1 docs: clarrify use of sw behind redirect (#32915)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+33.sha-427a1cc.with-local-changes
+ * @license Angular v9.0.0-next.12+34.sha-ec482da.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5910,7 +5910,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.12+33.sha-427a1cc.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.12+34.sha-ec482da.with-local-changes');
 
     /**
      * @license
