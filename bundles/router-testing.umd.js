b906505e2bcd75e28cce4a59e16036ff1d934c7c
Matias Niemelä <matias@yearofmoo.com>
2019-10-21 15:46:15 +0000

    1b4eaea6d4 feat(ivy): input type coercion for template type-checking (#33243)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+9.sha-755a80c.with-local-changes
+ * @license Angular v9.0.0-next.12+8.sha-1b4eaea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+9.sha-755a80c.with-local-changes
+ * @license Angular v9.0.0-next.12+8.sha-1b4eaea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9943,7 +9943,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.12+9.sha-755a80c.with-local-changes');
+const VERSION = new Version('9.0.0-next.12+8.sha-1b4eaea.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
