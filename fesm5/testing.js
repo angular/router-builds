542bf98bc4000d6f8db35c76ed0881390f016f3b
Matias Niemelä <matias@yearofmoo.com>
2019-10-22 17:34:08 +0000

    e030375d9a feat(ngcc): enable private NgModule re-exports in ngcc on request (#33177)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+35.sha-a86a179.with-local-changes
+ * @license Angular v9.0.0-next.12+37.sha-e030375.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
