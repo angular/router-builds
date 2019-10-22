ed7eabeb653f0621d76e6d39a0c9e1349b5f964d
Matias Niemelä <matias@yearofmoo.com>
2019-10-22 17:54:47 +0000

    65a0d2b53d fix(language-service): Preserve CRLF in templates for language-service (#33241)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+37.sha-e030375.with-local-changes
+ * @license Angular v9.0.0-next.12+38.sha-65a0d2b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5910,7 +5910,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.12+37.sha-e030375.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.12+38.sha-65a0d2b.with-local-changes');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+37.sha-e030375.with-local-changes
+ * @license Angular v9.0.0-next.12+38.sha-65a0d2b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
