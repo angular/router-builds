fbff92006b49a0804c69b5f79e64186284ec0f64
Kara Erickson <karakara@google.com>
2019-10-22 19:23:34 +0000

    1b8b04cf26 docs: add ModuleWithProviders migration doc (#33085)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+46.sha-7f7dc7c.with-local-changes
+ * @license Angular v9.0.0-next.12+49.sha-1b8b04c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9943,7 +9943,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.12+46.sha-7f7dc7c.with-local-changes');
+const VERSION = new Version('9.0.0-next.12+49.sha-1b8b04c.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
