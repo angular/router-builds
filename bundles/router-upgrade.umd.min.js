be257e6d88c454643616128c9cd679c3ab4c0081
Kara Erickson <karakara@google.com>
2019-10-22 21:54:08 +0000

    398ff1e7e7 docs: add ModuleWithProviders deprecation (#33266)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+50.sha-b3d6d50.with-local-changes
+ * @license Angular v9.0.0-next.12+51.sha-398ff1e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+50.sha-b3d6d50.with-local-changes
+ * @license Angular v9.0.0-next.12+51.sha-398ff1e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10138,7 +10138,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.12+50.sha-b3d6d50.with-local-changes');
+const VERSION = new Version('9.0.0-next.12+51.sha-398ff1e.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
