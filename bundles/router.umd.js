6f7cb39bbd335daac25409472f7be7037a4c80e8
Andrew Kushnir <akushnir@google.com>
2019-10-23 23:59:49 +0000

    d40ee6a259 perf(ivy): improve styling performance (#33326)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+1.sha-c79d6ec.with-local-changes
+ * @license Angular v9.0.0-next.13+2.sha-d40ee6a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.13+1.sha-c79d6ec.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.13+2.sha-d40ee6a.with-local-changes');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+1.sha-c79d6ec.with-local-changes
+ * @license Angular v9.0.0-next.13+2.sha-d40ee6a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
