db1eab10e86e7e9f2edbfc5fff3d541014c047cb
Andrew Kushnir <akushnir@google.com>
2019-10-23 20:52:16 +0000

    e2211ed211 fix(ivy): handle method calls of local variables in template type checker (#33132)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+67.sha-77240e1.with-local-changes
+ * @license Angular v9.0.0-next.12+68.sha-e2211ed.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5738,7 +5738,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.12+67.sha-77240e1.with-local-changes');
+var VERSION = new Version('9.0.0-next.12+68.sha-e2211ed.with-local-changes');
 
 /**
  * @license
