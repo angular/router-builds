5e43737d90ee9d40f4df36a6a8b5f38af919c180
Andrew Kushnir <akushnir@google.com>
2019-10-23 16:41:41 +0000

    ef5c0dae7d build: fix gulp setup not working with node v12 (#33348)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+15.sha-bb28406.with-local-changes
+ * @license Angular v8.2.11+17.sha-ef5c0da.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
