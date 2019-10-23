9ed062ba26cc5f1777a51d5a1fc36b0778ca599b
Andrew Kushnir <akushnir@google.com>
2019-10-23 16:34:07 +0000

    c0b90c2010 docs: update localize migration doc (#33275)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+56.sha-43ac02e.with-local-changes
+ * @license Angular v9.0.0-next.12+52.sha-c0b90c2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5910,7 +5910,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.12+56.sha-43ac02e.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.12+52.sha-c0b90c2.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+56.sha-43ac02e.with-local-changes
+ * @license Angular v9.0.0-next.12+52.sha-c0b90c2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
