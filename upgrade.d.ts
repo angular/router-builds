8cea32bd6a8e8eb602af2ebf7d550d2da55a0aa1
Andrew Kushnir <akushnir@google.com>
2019-10-23 20:27:26 +0000

    77240e1b60 fix(ivy): align VE + Ivy #ref types in fullTemplateTypeCheck: false (#33261)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+66.sha-ba29e4d.with-local-changes
+ * @license Angular v9.0.0-next.12+67.sha-77240e1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
