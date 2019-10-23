460dbe14c4a6543719136f134978d8d8b0d3c946
Andrew Kushnir <akushnir@google.com>
2019-10-23 16:34:20 +0000

    49eec5d872 fix(language-service): Add directive selectors & banana-in-a-box to completions (#33311)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+52.sha-c0b90c2.with-local-changes
+ * @license Angular v9.0.0-next.12+53.sha-49eec5d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.12+52.sha-c0b90c2.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.12+53.sha-49eec5d.with-local-changes');
 
     /**
      * @license
