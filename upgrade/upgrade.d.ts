a634bea8fb18921f2f8ab950ba95bd3bf41be2b4
Andrew Kushnir <akushnir@google.com>
2019-10-23 22:48:34 +0000

    663aba5775 release: cut the v8.2.12 release

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+22.sha-2d465ee.with-local-changes
+ * @license Angular v8.2.11+23.sha-663aba5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
