167f70ef67d2a0b77e9eea6c106971622bd6e373
Andrew Kushnir <akushnir@google.com>
2019-10-23 19:05:01 +0000

    1b607529a6 docs: fix spelling error in angular component (#32971)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+63.sha-2ecc4c7.with-local-changes
+ * @license Angular v9.0.0-next.12+64.sha-1b60752.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
