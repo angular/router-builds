0b7a73ab3108356ff6f0a3bbbd24bd3e89012c48
Andrew Kushnir <akushnir@google.com>
2019-10-23 18:32:38 +0000

    704e3543dc docs: Fix appHighlightColor typo (#33331)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+19.sha-98e71d3.with-local-changes
+ * @license Angular v8.2.11+20.sha-704e354.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
