216050aa0ede807d4b7173730218c554c4e3b86f
Andrew Kushnir <akushnir@google.com>
2019-10-23 20:19:48 +0000

    ba29e4d953 feat(docs-infra): implement figure styles (#33259)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+65.sha-355e54a.with-local-changes
+ * @license Angular v9.0.0-next.12+66.sha-ba29e4d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
