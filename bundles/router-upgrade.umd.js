72e5c6ea82a467dbfb97beba7a8192e3df33878e
Andrew Kushnir <akushnir@google.com>
2019-10-23 16:32:27 +0000

    43ac02e566 fix(docs-infra): scroll to top when navigating to new page via address bar (#33344)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+57.sha-f013515.with-local-changes
+ * @license Angular v9.0.0-next.12+56.sha-43ac02e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
