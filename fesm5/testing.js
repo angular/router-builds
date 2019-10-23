91fee257a5e194ede620df8c482916ebcb56027c
Andrew Kushnir <akushnir@google.com>
2019-10-23 20:53:47 +0000

    e2211ed211 fix(ivy): handle method calls of local variables in template type checker (#33132)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+67.sha-77240e1.with-local-changes
+ * @license Angular v9.0.0-next.12+68.sha-e2211ed.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
