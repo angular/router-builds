362f919318675f94b7e9697f4feebbb4dc453805
Andrew Kushnir <akushnir@google.com>
2019-10-23 19:16:43 +0000

    355e54a410 fix(compiler): do not throw when using abstract directive from other compilation unit (#33347)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+64.sha-1b60752.with-local-changes
+ * @license Angular v9.0.0-next.12+65.sha-355e54a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
