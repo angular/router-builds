4aed1ce10fe0119a3b10f32dbc270741c51a3d22
Andrew Kushnir <akushnir@google.com>
2019-10-23 16:28:49 +0000

    383457f898 docs: clean up deprecation guide (#33338)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+51.sha-398ff1e.with-local-changes
+ * @license Angular v9.0.0-next.12+54.sha-383457f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5713,7 +5713,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.12+51.sha-398ff1e.with-local-changes');
+var VERSION = new Version('9.0.0-next.12+54.sha-383457f.with-local-changes');
 
 /**
  * @license
