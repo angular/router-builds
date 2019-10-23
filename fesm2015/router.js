caac89d449fb09d21d4ecb785730af034d6088d6
Andrew Kushnir <akushnir@google.com>
2019-10-23 20:22:36 +0000

    2d465ee37b feat(docs-infra): implement figure styles (#33259)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+21.sha-70238f7.with-local-changes
+ * @license Angular v8.2.11+22.sha-2d465ee.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9913,7 +9913,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.11+21.sha-70238f7.with-local-changes');
+const VERSION = new Version('8.2.11+22.sha-2d465ee.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+21.sha-70238f7.with-local-changes
+ * @license Angular v8.2.11+22.sha-2d465ee.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
