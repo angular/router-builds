97bcf8fc575f88f6f8b472bff7893f4d67a18af0
Andrew Kushnir <akushnir@google.com>
2019-10-23 23:00:28 +0000

    2f541a979e docs: release notes for the v8.2.12 release

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+68.sha-e2211ed.with-local-changes
+ * @license Angular v9.0.0-next.12+69.sha-2f541a9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+68.sha-e2211ed.with-local-changes
+ * @license Angular v9.0.0-next.12+69.sha-2f541a9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10138,7 +10138,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.12+68.sha-e2211ed.with-local-changes');
+const VERSION = new Version('9.0.0-next.12+69.sha-2f541a9.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
