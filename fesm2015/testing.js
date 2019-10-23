6bde829c47712ad7503b1bd8f18e5bf9c1df367b
Andrew Kushnir <akushnir@google.com>
2019-10-23 17:22:07 +0000

    8bc5fb2ab6 fix(bazel): remove deprecated ng_setup_workspace() function (#33330)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+60.sha-b04de13.with-local-changes
+ * @license Angular v9.0.0-next.12+61.sha-8bc5fb2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
