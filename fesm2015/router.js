73ebef99a2860fa76a54e65e48c8a81127deb75f
Andrew Kushnir <akushnir@google.com>
2019-10-24 00:00:03 +0000

    c79d6ec502 docs: deprecate esm5 and fesm5 code distribution in our npm packages (#33360)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13.with-local-changes
+ * @license Angular v9.0.0-next.13+1.sha-c79d6ec.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10138,7 +10138,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.13.with-local-changes');
+const VERSION = new Version('9.0.0-next.13+1.sha-c79d6ec.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13.with-local-changes
+ * @license Angular v9.0.0-next.13+1.sha-c79d6ec.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
