2d04b8138bbc22ff69aa07372dbd08d04d5b7266
Andrew Kushnir <akushnir@google.com>
2019-10-24 23:09:30 +0000

    4aa51b751b feat(ivy): verify whether TypeScript version is supported (#33377)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+27.sha-09a2bb8.with-local-changes
+ * @license Angular v9.0.0-next.13+28.sha-4aa51b7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5910,7 +5910,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.13+27.sha-09a2bb8.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.13+28.sha-4aa51b7.with-local-changes');
 
     /**
      * @license
