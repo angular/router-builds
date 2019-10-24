075db45876847dbb300041744bcb488e262eee8a
Andrew Kushnir <akushnir@google.com>
2019-10-24 21:33:36 +0000

    d883007cc6 build: update CLI and related dependencies (#33382)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+23.sha-4c4217c.with-local-changes
+ * @license Angular v9.0.0-next.13+24.sha-d883007.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+23.sha-4c4217c.with-local-changes
+ * @license Angular v9.0.0-next.13+24.sha-d883007.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.13+23.sha-4c4217c.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.13+24.sha-d883007.with-local-changes');
 
     /**
      * @license
