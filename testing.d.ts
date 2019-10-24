b854caa31a826f4b5e9139230afcdbc42b41775c
GitHub <noreply@github.com>
2019-10-24 23:27:34 +0000

    b28eb54ee7 test(ivy): improve microbenchmark reporting progress

---
+++ testing.d.ts
@@ -0,0 +1,7 @@
+/**
+ * @license Angular v9.0.0-next.13+29.sha-b28eb54.with-local-changes
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+export * from './testing/testing';
