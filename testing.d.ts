00cf809fdba502fb2b0a7b25bc3a67ff78c118da
Andrew Kushnir <akushnir@google.com>
2019-10-24 21:29:52 +0000

    93a0b1ba13 refactor(language-service): Remove AttrInfo interface (#33380)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+21.sha-73530a9.with-local-changes
+ * @license Angular v9.0.0-next.13+22.sha-93a0b1b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
