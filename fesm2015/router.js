f10e3b7108c02449fd9da0c668636199fe7d10c0
Andrew Kushnir <akushnir@google.com>
2019-10-24 17:20:37 +0000

    efbbae5a48 ci: publish tarballs for all Angular packages as build artifacts on PR builds (#33321)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+7.sha-3d11355.with-local-changes
+ * @license Angular v9.0.0-next.13+10.sha-efbbae5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10138,7 +10138,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.13+7.sha-3d11355.with-local-changes');
+const VERSION = new Version('9.0.0-next.13+10.sha-efbbae5.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
