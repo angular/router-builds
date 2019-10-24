30359bd8d3af70d8ac3593a6de1d02f618c0db31
Andrew Kushnir <akushnir@google.com>
2019-10-24 22:01:03 +0000

    09a2bb839f refactor(ivy): Intruduce LFrame to store global instruction information (#33178)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+24.sha-d883007.with-local-changes
+ * @license Angular v9.0.0-next.13+27.sha-09a2bb8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10138,7 +10138,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.13+24.sha-d883007.with-local-changes');
+const VERSION = new Version('9.0.0-next.13+27.sha-09a2bb8.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
