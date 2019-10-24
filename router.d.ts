24625d6c855521041c3fa2d356b19a128b92ce62
Andrew Kushnir <akushnir@google.com>
2019-10-24 20:03:01 +0000

    a42057d0f8 fix(ivy): support abstract directives in template type checking (#33131)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+17.sha-63f0ded.with-local-changes
+ * @license Angular v9.0.0-next.13+19.sha-a42057d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
