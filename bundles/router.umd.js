e54ab14283052645443551af34608bd749a6c2b8
Andrew Kushnir <akushnir@google.com>
2019-10-25 19:33:20 +0000

    8d15bfa6ee fix(ivy): allow abstract directives to have an invalid constructor (#32987)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+51.sha-e4e8dbd.with-local-changes
+ * @license Angular v9.0.0-next.13+52.sha-8d15bfa.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5910,7 +5910,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.13+51.sha-e4e8dbd.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.13+52.sha-8d15bfa.with-local-changes');
 
     /**
      * @license
