7d159070b38ee5d33b8c54c8173858b70bc2ac9b
Andrew Kushnir <akushnir@google.com>
2019-10-25 16:38:34 +0000

    b381497126 feat(ngcc): add a migration for undecorated child classes (#33362)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+44.sha-a78b701.with-local-changes
+ * @license Angular v9.0.0-next.13+42.sha-b381497.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
