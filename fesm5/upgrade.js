995931bbd0f3f4be1b5327a7311d3d79a3d6eb00
Martin Probst <martin@probst.io>
2019-10-25 22:56:52 +0000

    585ea89ab2 fix(zone.js): work around TS3.7 issue

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+7.sha-ed391ad.with-local-changes
+ * @license Angular v9.0.0-next.14+2.sha-585ea89.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
