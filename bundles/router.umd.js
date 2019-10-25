3332b763712e17e001caf0bc384971e0246577c3
Andrew Kushnir <akushnir@google.com>
2019-10-25 17:41:11 +0000

    e4e8dbdee0 revert: build: update to @bazel/bazel 1.0.0 (#33367) (#33407)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+49.sha-49bfc74.with-local-changes
+ * @license Angular v9.0.0-next.13+51.sha-e4e8dbd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.13+49.sha-49bfc74.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.13+51.sha-e4e8dbd.with-local-changes');
 
     /**
      * @license
