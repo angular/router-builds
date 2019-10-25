f3094897f7fe4e61993b4576f3f028d94117a93a
Andrew Kushnir <akushnir@google.com>
2019-10-25 17:16:50 +0000

    26a6db208c docs(docs-infra): document running docs examples in ivy mode (#33399)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.12+2.sha-e0428fe.with-local-changes
+ * @license Angular v8.2.12+3.sha-26a6db2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
