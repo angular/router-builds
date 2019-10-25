b8520c82e7559273ba213873e62b545cbf541343
Andrew Kushnir <akushnir@google.com>
2019-10-25 21:19:33 +0000

    5607ad8c62 perf(ivy): apply static styles/classes directly to an element's style/className properties (#33364)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+58.sha-335854f.with-local-changes
+ * @license Angular v9.0.0-next.13+59.sha-5607ad8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5900,7 +5900,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.13+58.sha-335854f.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.13+59.sha-5607ad8.with-local-changes');
 
     /**
      * @license
