9d93b99e451a251e9cede613f49fa18378b7b87f
Andrew Kushnir <akushnir@google.com>
2019-10-25 16:56:09 +0000

    49bfc7421a docs: add ivy compatibility guide (#33390)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+45.sha-e231073.with-local-changes
+ * @license Angular v9.0.0-next.13+49.sha-49bfc74.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5910,7 +5910,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.13+45.sha-e231073.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.13+49.sha-49bfc74.with-local-changes');
 
     /**
      * @license
