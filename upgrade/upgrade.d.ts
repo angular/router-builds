9dd15f10c1ebc2e1564b5c882dd50aa8667a20bd
Andrew Kushnir <akushnir@google.com>
2019-10-28 17:34:36 +0000

    4ee354da99 build: switch to @build_bazel_rules_nodejs//:index.bzl load point (#33433)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+7.sha-eb0d8c0.with-local-changes
+ * @license Angular v9.0.0-next.14+5.sha-4ee354d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
