2713e345e5c0ba92be81b1b025ed73ead55ff2ab
Andrew Kushnir <akushnir@google.com>
2019-10-28 23:40:54 +0000

    89704cc5f3 build: update integration/bazel to Ivy and new rollup_bundle (#33434)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+12.sha-2c623fd.with-local-changes
+ * @license Angular v9.0.0-next.14+13.sha-89704cc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
