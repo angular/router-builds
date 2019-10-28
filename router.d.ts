b530a36e2475c6a59106e47c2ca23e5109679533
Andrew Kushnir <akushnir@google.com>
2019-10-28 17:33:16 +0000

    eb0d8c09e3 build: temporarily set `build --define=enable_legacy_rollup_rule=1` in bazelrc (#33426)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+4.sha-41979d6.with-local-changes
+ * @license Angular v9.0.0-next.14+7.sha-eb0d8c0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
