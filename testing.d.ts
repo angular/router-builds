eb78fa3ce32481659b0e6a50505b3e0e31a18a5d
Andrew Kushnir <akushnir@google.com>
2019-10-29 19:01:34 +0000

    27745c5d8c docs: added missing whitespaces (#33424)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+15.sha-2f6c97e.with-local-changes
+ * @license Angular v9.0.0-next.14+16.sha-27745c5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
