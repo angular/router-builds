ed865dce7b4f73c886c04689a9268db4dcf37b70
Andrew Kushnir <akushnir@google.com>
2019-10-29 19:10:18 +0000

    f97c464a9e fix(compiler): i18n - ignore `alt-trans` tags in XLIFF 1.2 (#33464)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.12+3.sha-26a6db2.with-local-changes
+ * @license Angular v8.2.12+5.sha-f97c464.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
