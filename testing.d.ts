8fd91f4fe502ad4eb12536c8335e69204a3b376f
Andrew Kushnir <akushnir@google.com>
2019-10-29 19:07:29 +0000

    a3034ef92e build: bump maximum allowed yarn version (#33430)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+20.sha-bd40c89.with-local-changes
+ * @license Angular v9.0.0-next.14+21.sha-a3034ef.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
