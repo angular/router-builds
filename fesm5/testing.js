b5dfc39ea646b4397446110546c938851b7c2d03
Andrew Kushnir <akushnir@google.com>
2019-10-29 21:27:39 +0000

    bf913cc39b feat(bazel): update bazel-schematics to use Ivy and new rollup_bundle (#33435)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+28.sha-649093c.with-local-changes
+ * @license Angular v9.0.0-next.14+27.sha-bf913cc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
