e8a664b6425e4da2b4a83eefa83e49f02e5e6942
Andrew Kushnir <akushnir@google.com>
2019-10-29 19:27:28 +0000

    c3e93564d0 perf(ivy): avoid generating selectors array for directives without a selector (#33431)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+24.sha-d1246a1.with-local-changes
+ * @license Angular v9.0.0-next.14+25.sha-c3e9356.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
