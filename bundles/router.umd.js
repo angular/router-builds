3e4ce4df8cec8bc9718fc6a3a207abaa58e3819d
Andrew Kushnir <akushnir@google.com>
2019-10-29 19:09:26 +0000

    936700ad9f fix(compiler): i18n - ignore `alt-trans` tags in XLIFF 1.2 (#33450)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+17.sha-4e38cab.with-local-changes
+ * @license Angular v9.0.0-next.14+22.sha-936700a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5900,7 +5900,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.14+17.sha-4e38cab.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.14+22.sha-936700a.with-local-changes');
 
     /**
      * @license
