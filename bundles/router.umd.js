958fd08815b64d4c6f215590563662798088bf26
Andrew Kushnir <akushnir@google.com>
2019-10-29 23:43:10 +0000

    540d104b17 feat(bazel): update @bazel/schematics to Bazel 1.0.0 (#33476)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+30.sha-1de7579.with-local-changes
+ * @license Angular v9.0.0-next.14+32.sha-540d104.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5900,7 +5900,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.14+30.sha-1de7579.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.14+32.sha-540d104.with-local-changes');
 
     /**
      * @license
