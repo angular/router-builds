4af6a60cdf2c2756b4f0167213c649620a06437a
Andrew Kushnir <akushnir@google.com>
2019-10-30 18:00:44 +0000

    7e135f6d3a perf(ivy): class bindings benchmark (#33470)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+33.sha-5666d11.with-local-changes
+ * @license Angular v9.0.0-next.14+35.sha-7e135f6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
