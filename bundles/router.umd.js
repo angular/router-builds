1be1e31dd0929f0749a554323ebfd2e82c05b352
Andrew Kushnir <akushnir@google.com>
2019-10-30 18:51:40 +0000

    f197191a5f refactor(core): better error message for undecorated-classes-with-di migration (#33315)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+34.sha-fba72f0.with-local-changes
+ * @license Angular v9.0.0-next.14+36.sha-f197191.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5900,7 +5900,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.14+34.sha-fba72f0.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.14+36.sha-f197191.with-local-changes');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+34.sha-fba72f0.with-local-changes
+ * @license Angular v9.0.0-next.14+36.sha-f197191.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10156,7 +10156,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.14+34.sha-fba72f0.with-local-changes');
+const VERSION = new Version('9.0.0-next.14+36.sha-f197191.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
