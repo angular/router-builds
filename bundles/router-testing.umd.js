ea53ef52f5cc410598beb1aad16e3cb22249d3b1
Andrew Kushnir <akushnir@google.com>
2019-10-30 18:50:07 +0000

    f197191a5f refactor(core): better error message for undecorated-classes-with-di migration (#33315)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+34.sha-fba72f0.with-local-changes
+ * @license Angular v9.0.0-next.14+36.sha-f197191.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
