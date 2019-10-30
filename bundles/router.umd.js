e9f4fb5d34a197a3baa139a9df7821289214496a
Andrew Kushnir <akushnir@google.com>
2019-10-30 21:08:56 +0000

    c35587ba56 docs: add template type-checking guide (#33421)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+42.sha-31dccab.with-local-changes
+ * @license Angular v9.0.0-next.14+43.sha-c35587b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5900,7 +5900,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.14+42.sha-31dccab.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.14+43.sha-c35587b.with-local-changes');
 
     /**
      * @license
