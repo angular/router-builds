e2a1497ff92a1120bf5a29065c592f9ae1be9ab6
atscott <atscott01@gmail.com>
2019-10-31 23:13:48 +0000

    083d48e072 perf(ivy): avoid native node retrieval from LView (#33511)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+11.sha-4452d6d.with-local-changes
+ * @license Angular v9.0.0-rc.0+13.sha-083d48e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5703,7 +5703,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.0+11.sha-4452d6d.with-local-changes');
+var VERSION = new Version('9.0.0-rc.0+13.sha-083d48e.with-local-changes');
 
 /**
  * @license
