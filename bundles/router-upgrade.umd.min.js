c0411209eeb9510465fee3569de83bcaf3275a97
atscott <atscott01@gmail.com>
2019-10-31 23:09:53 +0000

    4452d6d848 perf(ivy): avoid repeated tNode.initialInputs reads (#33322)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+4.sha-66ab701.with-local-changes
+ * @license Angular v9.0.0-rc.0+11.sha-4452d6d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
