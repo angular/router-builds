0d3c8d236769347cd5ec99eda9cc98adc1ac9d3f
atscott <atscott01@gmail.com>
2019-10-31 22:42:08 +0000

    d863839ecc docs: fix selector in getting started guide (#33333) PR Close #33333

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+2.sha-4099822.with-local-changes
+ * @license Angular v9.0.0-rc.0+5.sha-d863839.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10156,7 +10156,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.0+2.sha-4099822.with-local-changes');
+const VERSION = new Version('9.0.0-rc.0+5.sha-d863839.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
