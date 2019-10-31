2f6c9d3b9887426899376b25cb641c43a853beb9
atscott <atscott01@gmail.com>
2019-10-31 22:40:29 +0000

    d863839ecc docs: fix selector in getting started guide (#33333) PR Close #33333

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+2.sha-4099822.with-local-changes
+ * @license Angular v9.0.0-rc.0+5.sha-d863839.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
