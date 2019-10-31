c5d481e8fb335da6455c89bc6a5df81e4700645a
Alan Agius <alan.agius4@gmail.com>
2019-10-31 23:58:03 +0000

    b43ae44d84 fix(bazel): exclude @angular/cli from metadata build

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.13+1.sha-2174827.with-local-changes
+ * @license Angular v8.2.13+2.sha-b43ae44.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
