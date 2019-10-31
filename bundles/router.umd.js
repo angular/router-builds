6ac888780d8aacb11e48f64a90f403587da8096a
atscott <atscott01@gmail.com>
2019-10-31 22:57:21 +0000

    66ab701606 docs: add example of a server in ruby for deployment (#33418) PR Close #33418

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+7.sha-1d141a8.with-local-changes
+ * @license Angular v9.0.0-rc.0+4.sha-66ab701.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.0+7.sha-1d141a8.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.0+4.sha-66ab701.with-local-changes');
 
     /**
      * @license
