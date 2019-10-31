b243bdc521c15d97d7ea5b04de89e979375dd0fb
atscott <atscott01@gmail.com>
2019-10-31 23:12:11 +0000

    083d48e072 perf(ivy): avoid native node retrieval from LView (#33511)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+11.sha-4452d6d.with-local-changes
+ * @license Angular v9.0.0-rc.0+13.sha-083d48e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
