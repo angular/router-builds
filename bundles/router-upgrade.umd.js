068920155b3004d3391bef831e8e6be88400dd09
atscott <atscott01@gmail.com>
2019-10-31 22:40:02 +0000

    4099822ba2 docs: fix and refine COMMITTER.md (#33276)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+3.sha-64ac106.with-local-changes
+ * @license Angular v9.0.0-rc.0+2.sha-4099822.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
