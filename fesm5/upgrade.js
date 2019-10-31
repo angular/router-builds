545632a7d2869856b328ee2e9478a91cc3433d24
atscott <atscott01@gmail.com>
2019-10-31 22:43:19 +0000

    d5ae854b5b refactor(upgrade): Drop unused imports (#33496)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+5.sha-d863839.with-local-changes
+ * @license Angular v9.0.0-rc.0+6.sha-d5ae854.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
