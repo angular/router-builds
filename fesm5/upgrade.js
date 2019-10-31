9ab714b081e17152eb5fedaa8c126f52e1726c86
Andrew Kushnir <akushnir@google.com>
2019-10-31 21:16:46 +0000

    7355906a95 docs: add detailed example to ContentChildren change (#33499)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0.with-local-changes
+ * @license Angular v9.0.0-rc.0+1.sha-7355906.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
