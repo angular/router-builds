725a3c79f847b7d8d3045bdef2eaacc70855948c
Andrew Kushnir <akushnir@google.com>
2019-10-31 17:51:07 +0000

    e4ebffab8d docs: fix format in ivy page (#33513)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.15+2.sha-c4695bb.with-local-changes
+ * @license Angular v9.0.0-next.15+3.sha-e4ebffa.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.15+2.sha-c4695bb.with-local-changes
+ * @license Angular v9.0.0-next.15+3.sha-e4ebffa.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10156,7 +10156,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.15+2.sha-c4695bb.with-local-changes');
+const VERSION = new Version('9.0.0-next.15+3.sha-e4ebffa.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
