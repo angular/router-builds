15ca0c2c1990823cc747b830cf4245481f2d1d70
atscott <atscott01@gmail.com>
2019-11-01 22:48:57 +0000

    9e83814e61 docs: remove erroneous brack  in animations guide (#33525)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+30.sha-f185fab.with-local-changes
+ * @license Angular v9.0.0-rc.0+31.sha-9e83814.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
