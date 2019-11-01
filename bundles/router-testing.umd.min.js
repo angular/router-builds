fdedfe4c1f775dad341cad078963ff4f03068014
atscott <atscott01@gmail.com>
2019-11-01 18:09:41 +0000

    f02213a661 build: restore functionality of the micro benchmarks `profile_all.js` script (#33494)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+24.sha-0953642.with-local-changes
+ * @license Angular v9.0.0-rc.0+25.sha-f02213a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+24.sha-0953642.with-local-changes
+ * @license Angular v9.0.0-rc.0+25.sha-f02213a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10156,7 +10156,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.0+24.sha-0953642.with-local-changes');
+const VERSION = new Version('9.0.0-rc.0+25.sha-f02213a.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
