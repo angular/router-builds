13929d5a9f6d0c8a013b0cc5953b7290610d6a10
atscott <atscott01@gmail.com>
2019-11-01 18:11:56 +0000

    fc8eecad3f fix(compiler-cli): remove unused CLI private exports (#33242)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+25.sha-f02213a.with-local-changes
+ * @license Angular v9.0.0-rc.0+23.sha-fc8eeca.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5900,7 +5900,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.0+25.sha-f02213a.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.0+23.sha-fc8eeca.with-local-changes');
 
     /**
      * @license
