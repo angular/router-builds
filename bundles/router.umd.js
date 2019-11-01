5e76f525e30ca7aad130168f195b644d3882d1a9
atscott <atscott01@gmail.com>
2019-11-01 22:43:10 +0000

    f185fabc11 docs: add GDE profile of David Muellerchen (#33455)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+29.sha-4857c53.with-local-changes
+ * @license Angular v9.0.0-rc.0+30.sha-f185fab.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.0+29.sha-4857c53.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.0+30.sha-f185fab.with-local-changes');
 
     /**
      * @license
