1677bbb9ace6bbe155085d0af5a82cfdc92d0e2c
atscott <atscott01@gmail.com>
2019-11-01 18:00:08 +0000

    9ebac71521 fix(language-service): Should not crash if expr ends unexpectedly (#33524)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+22.sha-feb5f27.with-local-changes
+ * @license Angular v9.0.0-rc.0+20.sha-9ebac71.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.0+22.sha-feb5f27.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.0+20.sha-9ebac71.with-local-changes');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+22.sha-feb5f27.with-local-changes
+ * @license Angular v9.0.0-rc.0+20.sha-9ebac71.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
