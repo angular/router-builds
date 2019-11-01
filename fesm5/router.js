834a036175053ed6957ce7c5a221f966eb08d7ad
atscott <atscott01@gmail.com>
2019-11-01 18:10:18 +0000

    fc8eecad3f fix(compiler-cli): remove unused CLI private exports (#33242)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+25.sha-f02213a.with-local-changes
+ * @license Angular v9.0.0-rc.0+23.sha-fc8eeca.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5738,7 +5738,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.0+25.sha-f02213a.with-local-changes');
+var VERSION = new Version('9.0.0-rc.0+23.sha-fc8eeca.with-local-changes');
 
 /**
  * @license
