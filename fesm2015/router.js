af29c32be9c1cb20a1d8811b86bea401db4f5100
atscott <atscott01@gmail.com>
2019-11-01 18:00:52 +0000

    feb5f27d09 docs: Add Mike Brocchi to the list of GDEs (#33466)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+21.sha-31116f0c.with-local-changes
+ * @license Angular v9.0.0-rc.0+22.sha-feb5f27.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10156,7 +10156,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.0+21.sha-31116f0c.with-local-changes');
+const VERSION = new Version('9.0.0-rc.0+22.sha-feb5f27.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+21.sha-31116f0c.with-local-changes
+ * @license Angular v9.0.0-rc.0+22.sha-feb5f27.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5703,7 +5703,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.0+21.sha-31116f0c.with-local-changes');
+var VERSION = new Version('9.0.0-rc.0+22.sha-feb5f27.with-local-changes');
 
 /**
  * @license
