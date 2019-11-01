134f4484c8a94c4abdeca67cb284a39aa37ae95a
atscott <atscott01@gmail.com>
2019-11-01 00:11:48 +0000

    4d4b527474 docs(common): Remove async pipe from ngFor example (#33378)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+17.sha-602eeee.with-local-changes
+ * @license Angular v9.0.0-rc.0+18.sha-4d4b527.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
