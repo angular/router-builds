548ae410e893b43e4db02f6219bb67593fb183a6
atscott <atscott01@gmail.com>
2019-11-01 17:59:17 +0000

    feb5f27d09 docs: Add Mike Brocchi to the list of GDEs (#33466)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+21.sha-31116f0c.with-local-changes
+ * @license Angular v9.0.0-rc.0+22.sha-feb5f27.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
