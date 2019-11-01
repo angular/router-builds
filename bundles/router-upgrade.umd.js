c441b2641f12cf28fbce3c2fe86d170307611099
atscott <atscott01@gmail.com>
2019-11-01 22:44:15 +0000

    67aedd8366 docs: remove erroneous brack  in animations guide (#33525)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+29.sha-d77453d.with-local-changes
+ * @license Angular v9.0.0-rc.0+31.sha-67aedd8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
