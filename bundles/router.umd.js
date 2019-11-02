8f934cba9921f5e850f2327ef427ef96b1dd4b07
atscott <atscott01@gmail.com>
2019-11-02 01:09:58 +0000

    a925c2d987 docs: add GDE profile of Fabian Gosebrink (#33438)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+31.sha-67aedd8.with-local-changes
+ * @license Angular v9.0.0-rc.0+32.sha-a925c2d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.0+31.sha-67aedd8.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.0+32.sha-a925c2d.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+31.sha-67aedd8.with-local-changes
+ * @license Angular v9.0.0-rc.0+32.sha-a925c2d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
