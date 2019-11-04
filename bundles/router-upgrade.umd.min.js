fff11b6554760a3d749e68d456dcc7aab505461a
atscott <atscott01@gmail.com>
2019-11-04 20:30:42 +0000

    3992e7f05b docs: Update link to angular + bazel example app (#33409)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+35.sha-316b30b.with-local-changes
+ * @license Angular v9.0.0-rc.0+37.sha-3992e7f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
