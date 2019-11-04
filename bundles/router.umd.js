dfabb9a4d092f3e88578424019646bae8b983805
atscott <atscott01@gmail.com>
2019-11-04 20:28:58 +0000

    e3189f97ff docs: Update link to angular + bazel example app (#33409)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+36.sha-4ff43e1.with-local-changes
+ * @license Angular v9.0.0-rc.0+37.sha-e3189f9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.0+36.sha-4ff43e1.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.0+37.sha-e3189f9.with-local-changes');
 
     /**
      * @license
