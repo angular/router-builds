2faab2493ea8ebe643d51da68af9d6bbf5fb2a33
atscott <atscott01@gmail.com>
2019-11-04 20:28:18 +0000

    4ff43e1324 refactor(ivy): remove the getHostNative utility function (#33554)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+35.sha-66725b7.with-local-changes
+ * @license Angular v9.0.0-rc.0+36.sha-4ff43e1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5900,7 +5900,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.0+35.sha-66725b7.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.0+36.sha-4ff43e1.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+35.sha-66725b7.with-local-changes
+ * @license Angular v9.0.0-rc.0+36.sha-4ff43e1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
