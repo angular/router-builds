fbc40d6f7ccbf30e9bf984800754add4eb5204b3
atscott <atscott01@gmail.com>
2019-11-04 16:54:04 +0000

    66725b7b37 perf(ivy): move local references into consts array (#33129)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+34.sha-5437e2d.with-local-changes
+ * @license Angular v9.0.0-rc.0+35.sha-66725b7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
