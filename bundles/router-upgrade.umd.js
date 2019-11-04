2ede97e54d3dbd0ae80c56be30078292f9304553
atscott <atscott01@gmail.com>
2019-11-04 16:35:35 +0000

    5437e2da29 refactor(core): typo in undecorated-classes-with-di AOT failure message (#33018)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+33.sha-c83f501.with-local-changes
+ * @license Angular v9.0.0-rc.0+34.sha-5437e2d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
