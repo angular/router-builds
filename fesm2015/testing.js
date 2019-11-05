948156dd03bfe3e3a3aa38fc643b0433b3029399
atscott <atscott01@gmail.com>
2019-11-05 23:08:52 +0000

    3de72e4124 revert: build: remove deps on legacy nodejs rules rollup_bundle internals (#33201) (#33604)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+62.sha-d751ca7.with-local-changes
+ * @license Angular v9.0.0-rc.0+63.sha-3de72e4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -273,5 +273,5 @@ RouterTestingModule.decorators = [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 
-export { RouterTestingModule, SpyNgModuleFactoryLoader, setupTestingRouter };
+export { setupTestingRouter, SpyNgModuleFactoryLoader, RouterTestingModule };
 //# sourceMappingURL=testing.js.map
