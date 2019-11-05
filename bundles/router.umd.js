21d259a4d6046206ea9d208f9d6e0b0aa41c7b01
atscott <atscott01@gmail.com>
2019-11-05 01:29:00 +0000

    25aaff2ee1 docs: Adding Doug Parker to contributors list. (#33516)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+41.sha-5bc1b6f.with-local-changes
+ * @license Angular v9.0.0-rc.0+42.sha-25aaff2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5900,7 +5900,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.0+41.sha-5bc1b6f.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.0+42.sha-25aaff2.with-local-changes');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+41.sha-5bc1b6f.with-local-changes
+ * @license Angular v9.0.0-rc.0+42.sha-25aaff2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
