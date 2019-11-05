406712ee54adcf86845c92c02468dac028b6646f
atscott <atscott01@gmail.com>
2019-11-05 00:36:45 +0000

    8de3fcfccd build: decrease payload size limit (#33587)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+37.sha-3992e7f.with-local-changes
+ * @license Angular v9.0.0-rc.0+40.sha-8de3fcf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
