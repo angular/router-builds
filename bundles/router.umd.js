e31c95af5e76422229fb23f2731059a98290996d
atscott <atscott01@gmail.com>
2019-11-05 01:14:11 +0000

    5bc1b6f9ac docs: remove redundant word from template syntax guide (#33534) PR Close #33534

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+40.sha-4abf15c.with-local-changes
+ * @license Angular v9.0.0-rc.0+41.sha-5bc1b6f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5900,7 +5900,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.0+40.sha-4abf15c.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.0+41.sha-5bc1b6f.with-local-changes');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+40.sha-4abf15c.with-local-changes
+ * @license Angular v9.0.0-rc.0+41.sha-5bc1b6f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
