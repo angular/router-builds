7e8034a724feff072f6d76d1ba21184b3b1a3eb1
atscott <atscott01@gmail.com>
2019-11-05 18:50:28 +0000

    12ddda3af8 feat(common): support loading locales from a global (#33523)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+43.sha-abdbd46.with-local-changes
+ * @license Angular v9.0.0-rc.0+53.sha-12ddda3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5738,7 +5738,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.0+43.sha-abdbd46.with-local-changes');
+var VERSION = new Version('9.0.0-rc.0+53.sha-12ddda3.with-local-changes');
 
 /**
  * @license
