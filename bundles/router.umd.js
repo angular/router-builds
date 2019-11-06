3bd475d7385afe7b2bee848d9ff6b74eb2b31aea
atscott <atscott01@gmail.com>
2019-11-06 20:15:59 +0000

    7b87392f47 docs: added value param wrt. method signature (#32968)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+71.sha-d749dd3.with-local-changes
+ * @license Angular v9.0.0-rc.0+73.sha-7b87392.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.0+71.sha-d749dd3.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.0+73.sha-7b87392.with-local-changes');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+71.sha-d749dd3.with-local-changes
+ * @license Angular v9.0.0-rc.0+73.sha-7b87392.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
