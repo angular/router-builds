37901779eecf4fd2b4c47f3fef10470a409b6f85
atscott <atscott01@gmail.com>
2019-11-06 20:12:39 +0000

    e2d7b25e0d fix(ivy): avoid implicit any errors in event handlers (#33550)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+70.sha-2a4061a.with-local-changes
+ * @license Angular v9.0.0-rc.0+72.sha-e2d7b25.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
