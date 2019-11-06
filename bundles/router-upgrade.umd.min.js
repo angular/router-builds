e951f9b1b09d8d3a7f4c972b185394094bf83075
atscott <atscott01@gmail.com>
2019-11-06 23:24:29 +0000

    55b1b7745b release: cut the v9.0.0-rc.1 release

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+78.sha-a20979d.with-local-changes
+ * @license Angular v9.0.0-rc.1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
