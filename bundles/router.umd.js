a51ff8e7b5018a8154aabfa3fed4a3b671796f39
atscott <atscott01@gmail.com>
2019-11-06 20:17:28 +0000

    fe12d0dc78 fix(ngcc): render adjacent statements after static properties (#33630)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+73.sha-7b87392.with-local-changes
+ * @license Angular v9.0.0-rc.0+74.sha-fe12d0d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.0+73.sha-7b87392.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.0+74.sha-fe12d0d.with-local-changes');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+73.sha-7b87392.with-local-changes
+ * @license Angular v9.0.0-rc.0+74.sha-fe12d0d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
