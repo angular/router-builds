9df9fd58abfc3968c33c615652e3c912fd6d8b19
atscott <atscott01@gmail.com>
2019-11-06 23:26:00 +0000

    55b1b7745b release: cut the v9.0.0-rc.1 release

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+78.sha-a20979d.with-local-changes
+ * @license Angular v9.0.0-rc.1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5703,7 +5703,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.0+78.sha-a20979d.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1.with-local-changes');
 
 /**
  * @license
