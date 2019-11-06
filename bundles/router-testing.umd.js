7e02907c5a49c3d99fa7abf7c3a07c122f0b0af8
atscott <atscott01@gmail.com>
2019-11-06 16:48:24 +0000

    3297a76195 refactor(ivy): remove TStylingContext locking in favor of firstUpdatePass flag (#33521)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+66.sha-5453c4c.with-local-changes
+ * @license Angular v9.0.0-rc.0+67.sha-3297a76.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+66.sha-5453c4c.with-local-changes
+ * @license Angular v9.0.0-rc.0+67.sha-3297a76.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5738,7 +5738,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.0+66.sha-5453c4c.with-local-changes');
+var VERSION = new Version('9.0.0-rc.0+67.sha-3297a76.with-local-changes');
 
 /**
  * @license
