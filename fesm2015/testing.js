843a5834c68d145f2b37aa8db746f860ddac6e6e
atscott <atscott01@gmail.com>
2019-11-06 20:54:23 +0000

    41560b47c4 refactor(ivy): move all styling configurations into `TNodeFlags` (#33540)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+77.sha-2ed04ff.with-local-changes
+ * @license Angular v9.0.0-rc.0+69.sha-41560b4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -273,5 +273,5 @@ RouterTestingModule.decorators = [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 
-export { RouterTestingModule, SpyNgModuleFactoryLoader, setupTestingRouter };
+export { setupTestingRouter, SpyNgModuleFactoryLoader, RouterTestingModule };
 //# sourceMappingURL=testing.js.map
