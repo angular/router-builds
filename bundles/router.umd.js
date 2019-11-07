43bf90ba4d91a77d0832b047ba7afdeaeca4df87
Andrew Scott <atscott01@gmail.com>
2019-11-07 22:38:24 +0000

    c77faf566e fix(common): update CLDR generated files to 36.0.0 (#33584)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+18.sha-71b400e.with-local-changes
+ * @license Angular v9.0.0-rc.1+20.sha-c77faf5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5900,7 +5900,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+18.sha-71b400e.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.1+20.sha-c77faf5.with-local-changes');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+18.sha-71b400e.with-local-changes
+ * @license Angular v9.0.0-rc.1+20.sha-c77faf5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5703,7 +5703,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+18.sha-71b400e.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+20.sha-c77faf5.with-local-changes');
 
 /**
  * @license
