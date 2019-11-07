0408c3b79408624e6ba4098c5d7311ccc3ddd3f1
Andrew Scott <atscott01@gmail.com>
2019-11-07 18:11:14 +0000

    becc820159 docs(changelog): Fixes typo in changelog (#33618)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+7.sha-bca4376.with-local-changes
+ * @license Angular v9.0.0-rc.1+10.sha-becc820.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
