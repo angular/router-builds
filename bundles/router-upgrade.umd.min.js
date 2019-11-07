3f58113a853e4bffa2660fe124e1e48abfe87010
Andrew Scott <atscott01@gmail.com>
2019-11-07 18:13:10 +0000

    8d72a37f3e test(ivy): introduce a benchmark for duplicate style/class bindings (#33600)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+10.sha-becc820.with-local-changes
+ * @license Angular v9.0.0-rc.1+11.sha-8d72a37.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+10.sha-becc820.with-local-changes
+ * @license Angular v9.0.0-rc.1+11.sha-8d72a37.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5703,7 +5703,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+10.sha-becc820.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+11.sha-8d72a37.with-local-changes');
 
 /**
  * @license
