0c6fa2a0eba23e40d1ca736e9d32b9693e9d6d00
atscott <atscott01@gmail.com>
2019-11-07 01:26:36 +0000

    c0f69f3245 fix(compiler): correctly parse attributes with a dot in the name (#32256)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+2.sha-e3895b8a.with-local-changes
+ * @license Angular v9.0.0-rc.1+1.sha-c0f69f3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5703,7 +5703,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+2.sha-e3895b8a.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+1.sha-c0f69f3.with-local-changes');
 
 /**
  * @license
