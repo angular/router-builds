0e985a6499ca285fbc49515dd2ceed8d41ee4446
Andrew Scott <atscott01@gmail.com>
2019-11-07 16:13:16 +0000

    99ead47bff fix(ivy): match directives on namespaced elements (#33555)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+4.sha-7bccef5.with-local-changes
+ * @license Angular v9.0.0-rc.1+7.sha-99ead47.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
