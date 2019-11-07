6accc7b29a498f2261e03dae4b54c1391e3a76bd
Andrew Scott <atscott01@gmail.com>
2019-11-07 20:53:09 +0000

    e8d43d8fde perf: report cold and minimal times when profiling in a browser (#33653)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+15.sha-81828ae.with-local-changes
+ * @license Angular v9.0.0-rc.1+16.sha-e8d43d8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
