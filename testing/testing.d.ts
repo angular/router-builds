2b6038ccee34246e1826f27cfef8e780499f39d0
atscott <atscott01@gmail.com>
2019-11-07 00:17:49 +0000

    55b1b7745b release: cut the v9.0.0-rc.1 release

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+76.sha-814ed31.with-local-changes
+ * @license Angular v9.0.0-rc.1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
