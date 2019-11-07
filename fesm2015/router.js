60efaf60187b85adbfa36fe310288570f5b5e4ea
Andrew Scott <atscott01@gmail.com>
2019-11-07 18:16:25 +0000

    8912b11f56 fix(ivy): recompile component when template changes in ngc watch mode (#33551)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+11.sha-8d72a37.with-local-changes
+ * @license Angular v9.0.0-rc.1+12.sha-8912b11.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9943,7 +9943,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+11.sha-8d72a37.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+12.sha-8912b11.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
