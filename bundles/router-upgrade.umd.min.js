60a7ead1306b926ab52721aa4d060b1baee14df1
atscott <atscott01@gmail.com>
2019-11-07 01:25:03 +0000

    c0f69f3245 fix(compiler): correctly parse attributes with a dot in the name (#32256)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+2.sha-e3895b8a.with-local-changes
+ * @license Angular v9.0.0-rc.1+1.sha-c0f69f3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
