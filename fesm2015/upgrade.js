b49849818a05fd2120615af0e8d763fd7a5f12cf
Andrew Scott <atscott01@gmail.com>
2019-11-07 02:15:41 +0000

    39550746f8 fix(ivy): better support for i18n attributes on <ng-container>s (#33599)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+1.sha-c0f69f3.with-local-changes
+ * @license Angular v9.0.0-rc.1+3.sha-3955074.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
