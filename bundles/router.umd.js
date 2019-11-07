c75883ff7c565529bd1aeb5e7bf09d9baaa700c0
Andrew Scott <atscott01@gmail.com>
2019-11-07 20:49:59 +0000

    81828ae7f4 fix(ngcc): add reexports only once (#33658)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+14.sha-1aa3053.with-local-changes
+ * @license Angular v9.0.0-rc.1+15.sha-81828ae.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5900,7 +5900,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+14.sha-1aa3053.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.1+15.sha-81828ae.with-local-changes');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+14.sha-1aa3053.with-local-changes
+ * @license Angular v9.0.0-rc.1+15.sha-81828ae.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
