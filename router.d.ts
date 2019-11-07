dfcf04e34b9ef816be83ff08064c51156fd89b27
Andrew Scott <atscott01@gmail.com>
2019-11-07 22:36:47 +0000

    c77faf566e fix(common): update CLDR generated files to 36.0.0 (#33584)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+18.sha-71b400e.with-local-changes
+ * @license Angular v9.0.0-rc.1+20.sha-c77faf5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
