38ed398eb7844b26fe3f807920f7b791dabb59ee
Andrew Scott <atscott01@gmail.com>
2019-11-07 20:51:38 +0000

    0ca2a390d5 docs: app-name not rendering on browser (#33613)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+16.sha-e8d43d8.with-local-changes
+ * @license Angular v9.0.0-rc.1+17.sha-0ca2a39.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
