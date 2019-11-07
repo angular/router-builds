18af5b054c8a56d9421ab4299e727e0955c6cf22
Andrew Scott <atscott01@gmail.com>
2019-11-07 20:51:33 +0000

    e8d43d8fde perf: report cold and minimal times when profiling in a browser (#33653)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+15.sha-81828ae.with-local-changes
+ * @license Angular v9.0.0-rc.1+16.sha-e8d43d8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9943,7 +9943,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+15.sha-81828ae.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+16.sha-e8d43d8.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
