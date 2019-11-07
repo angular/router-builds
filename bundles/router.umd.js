dc01ddb18dc9fa3157189361dfa072d7405faf85
Andrew Scott <atscott01@gmail.com>
2019-11-07 20:56:18 +0000

    4f69ff7e85 test(ivy): introduce a benchmark for duplicate map-based style/class bindings (#33608)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+13.sha-cd8333c.with-local-changes
+ * @license Angular v9.0.0-rc.1+19.sha-4f69ff7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+13.sha-cd8333c.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+19.sha-4f69ff7.with-local-changes');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+13.sha-cd8333c.with-local-changes
+ * @license Angular v9.0.0-rc.1+19.sha-4f69ff7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
