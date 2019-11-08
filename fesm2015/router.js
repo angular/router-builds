7db6a08c45103f3a23cdda5ecc81bbc40dc5da03
Kara Erickson <karakara@google.com>
2019-11-08 01:12:29 +0000

    f63e5d9319 fix(ivy): properly determine the first native node of a view (#33627)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+21.sha-a33162b.with-local-changes
+ * @license Angular v9.0.0-rc.1+23.sha-f63e5d9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10156,7 +10156,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+21.sha-a33162b.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+23.sha-f63e5d9.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
