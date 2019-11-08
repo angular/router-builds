01bfb7df8d9fb0f818510242b27d7741b5691b2d
Kara Erickson <karakara@google.com>
2019-11-08 19:14:05 +0000

    43ffe7257a docs: describe your change... (#32914)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+24.sha-c4fbd0b.with-local-changes
+ * @license Angular v9.0.0-rc.1+25.sha-43ffe72.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
