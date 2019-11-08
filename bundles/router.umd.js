f69b016a0b3527acdbe3c4157509464c6b798b65
Kara Erickson <karakara@google.com>
2019-11-08 01:10:57 +0000

    f63e5d9319 fix(ivy): properly determine the first native node of a view (#33627)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+21.sha-a33162b.with-local-changes
+ * @license Angular v9.0.0-rc.1+23.sha-f63e5d9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+21.sha-a33162b.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+23.sha-f63e5d9.with-local-changes');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+21.sha-a33162b.with-local-changes
+ * @license Angular v9.0.0-rc.1+23.sha-f63e5d9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
