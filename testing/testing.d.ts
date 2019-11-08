2ce1c2b26d1bce776f2e0da26b40a5bed4042e81
Kara Erickson <karakara@google.com>
2019-11-08 23:02:32 +0000

    a40103d849 docs: update jiali's info (#33698)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+29.sha-98766b1.with-local-changes
+ * @license Angular v9.0.0-rc.1+32.sha-a40103d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
