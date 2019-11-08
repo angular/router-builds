2ff8e41eb0b9506522e827663590484decfbcb62
Kara Erickson <karakara@google.com>
2019-11-08 19:12:44 +0000

    c4fbd0bef7 docs: update ivy compatibility guide with latest changes (#33675)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+23.sha-f63e5d9.with-local-changes
+ * @license Angular v9.0.0-rc.1+24.sha-c4fbd0b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
