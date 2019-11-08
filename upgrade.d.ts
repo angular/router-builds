456ea89bd2100eadef3cb0cf5b8f64510b27e4c8
Kara Erickson <karakara@google.com>
2019-11-08 19:14:27 +0000

    1db86500de style: increase timeout to give blaze sufficient time to build (#33678) PR Close #33678

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+28.sha-afbaaf9.with-local-changes
+ * @license Angular v9.0.0-rc.1+27.sha-1db8650.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
