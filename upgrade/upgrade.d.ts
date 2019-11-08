2520b370cc4ef979a568493184f4e00668de89b0
Kara Erickson <karakara@google.com>
2019-11-08 23:02:50 +0000

    b789c891bb docs: update jiali's info (#33698)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+30.sha-290ae43.with-local-changes
+ * @license Angular v9.0.0-rc.1+33.sha-b789c89.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
