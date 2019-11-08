0c0946c3eb65d19ef4076d9722c9daf7a0869478
Kara Erickson <karakara@google.com>
2019-11-08 21:03:37 +0000

    98766b1d07 docs: fix migration-renderer code snippets (#33553)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+26.sha-570936e.with-local-changes
+ * @license Angular v9.0.0-rc.1+29.sha-98766b1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
