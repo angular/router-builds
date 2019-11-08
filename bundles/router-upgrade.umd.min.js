7e26632f2901be58a091b5fee861871ec7987140
Kara Erickson <karakara@google.com>
2019-11-08 23:04:23 +0000

    b97577bd94 docs: grammar fix (#33170)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+32.sha-a40103d.with-local-changes
+ * @license Angular v9.0.0-rc.1+31.sha-b97577b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+32.sha-a40103d.with-local-changes
+ * @license Angular v9.0.0-rc.1+31.sha-b97577b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5738,7 +5738,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+32.sha-a40103d.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+31.sha-b97577b.with-local-changes');
 
 /**
  * @license
