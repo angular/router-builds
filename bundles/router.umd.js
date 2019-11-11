a42522d3763fb3be7d54675f604eb2e4bae48484
Kara Erickson <karakara@google.com>
2019-11-11 19:24:31 +0000

    ca1ff3ea11 docs: update update guide to use `ng update` instead of `npm i` (#33726)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+45.sha-a8f9ff5.with-local-changes
+ * @license Angular v9.0.0-rc.1+46.sha-ca1ff3e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+45.sha-a8f9ff5.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+46.sha-ca1ff3e.with-local-changes');
 
     /**
      * @license
