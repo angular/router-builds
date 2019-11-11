f0af1d64335880f07d0e612932bf5a82f7a401a9
Kara Erickson <karakara@google.com>
2019-11-11 21:24:31 +0000

    b3c3000004 fix(ngcc): ensure that adjacent statements go after helper calls (#33689)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+48.sha-c6ebcd1.with-local-changes
+ * @license Angular v9.0.0-rc.1+50.sha-b3c3000.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
