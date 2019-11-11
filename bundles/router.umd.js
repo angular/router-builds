cb1922458a4d4e703d214f1af4479cb3ebe92365
Kara Erickson <karakara@google.com>
2019-11-11 21:07:34 +0000

    c6ebcd1eb9 docs: fix numbering (#33736)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+47.sha-eece138.with-local-changes
+ * @license Angular v9.0.0-rc.1+48.sha-c6ebcd1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5900,7 +5900,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+47.sha-eece138.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.1+48.sha-c6ebcd1.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+47.sha-eece138.with-local-changes
+ * @license Angular v9.0.0-rc.1+48.sha-c6ebcd1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
