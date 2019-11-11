bcc173a5a69aa62017b31cbafccdd1128f38f6b4
Kara Erickson <karakara@google.com>
2019-11-11 18:13:35 +0000

    a1e9956290 ci: update material-unit-tests job commit (#33716)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+41.sha-b197e90.with-local-changes
+ * @license Angular v9.0.0-rc.1+44.sha-a1e9956.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5703,7 +5703,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+41.sha-b197e90.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+44.sha-a1e9956.with-local-changes');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+41.sha-b197e90.with-local-changes
+ * @license Angular v9.0.0-rc.1+44.sha-a1e9956.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
