bec8be6b6a16bc2198ebc3764cb482d13cd00cf9
Kara Erickson <karakara@google.com>
2019-11-12 18:27:20 +0000

    f1b0547f0a revert: fix(ivy): R3TestBed should clean up registered modules after each test (#32872) (#33663)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+58.sha-fcde671.with-local-changes
+ * @license Angular v9.0.0-rc.1+61.sha-f1b0547.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5703,7 +5703,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+58.sha-fcde671.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+61.sha-f1b0547.with-local-changes');
 
 /**
  * @license
