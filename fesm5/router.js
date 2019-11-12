1c5e33fbaa58d0040cdfbf58653fee358b153a8e
Kara Erickson <karakara@google.com>
2019-11-12 22:32:19 +0000

    4e027fe3e4 test(ivy): view insertion before ng-container with a ViewContainerRef (#33755)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+65.sha-c315881.with-local-changes
+ * @license Angular v9.0.0-rc.1+77.sha-4e027fe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5738,7 +5738,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+65.sha-c315881.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+77.sha-4e027fe.with-local-changes');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+65.sha-c315881.with-local-changes
+ * @license Angular v9.0.0-rc.1+77.sha-4e027fe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
