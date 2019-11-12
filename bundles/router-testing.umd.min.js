19c0eb5226d1ece7029a711b90d8ae272deeef7a
Kara Erickson <karakara@google.com>
2019-11-12 00:13:10 +0000

    942e2ebe44 fix: generate the new locale files (#33682)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+51.sha-d8be830.with-local-changes
+ * @license Angular v9.0.0-rc.1+56.sha-942e2eb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
