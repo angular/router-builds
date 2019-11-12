b4aa59f8ee4aafc77b04965792bba55a7bb4d6c1
Kara Erickson <karakara@google.com>
2019-11-12 20:14:32 +0000

    641c671bac fix(core): support `ngInjectableDef` on types with inherited `ɵprov` (#33732)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+67.sha-2f0b8bc.with-local-changes
+ * @license Angular v9.0.0-rc.1+68.sha-641c671.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+67.sha-2f0b8bc.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+68.sha-641c671.with-local-changes');
 
     /**
      * @license
