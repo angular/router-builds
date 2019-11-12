9e54a10d515e5be1859adf85daebdd97c9698dc7
Kara Erickson <karakara@google.com>
2019-11-12 18:24:04 +0000

    fcde671ae2 docs: updated amexio resource description (#33645)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+63.sha-2ce9cda.with-local-changes
+ * @license Angular v9.0.0-rc.1+58.sha-fcde671.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5738,7 +5738,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+63.sha-2ce9cda.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+58.sha-fcde671.with-local-changes');
 
 /**
  * @license
