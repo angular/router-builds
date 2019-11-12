ae7ec523e33ae1169aee81790365c6808118cde0
Kara Erickson <karakara@google.com>
2019-11-12 18:31:36 +0000

    c315881d04 perf(core): Avoid unnecessary creating provider factory (#33742)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+58.sha-6615743.with-local-changes
+ * @license Angular v9.0.0-rc.1+65.sha-c315881.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
