d9e3bbd8316cc6b284d6c5acfe4570d46b550e2e
Kara Erickson <karakara@google.com>
2019-11-12 20:06:24 +0000

    2f0b8bc541 build(common): generate correct "global" locale files (#33662)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+64.sha-ccee818.with-local-changes
+ * @license Angular v9.0.0-rc.1+67.sha-2f0b8bc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
