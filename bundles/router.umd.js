8eb49dbdd48149e8af18314eca21eb4f99390ed2
Kara Erickson <karakara@google.com>
2019-11-12 18:32:00 +0000

    ccee818034 perf(core): Avoid unnecessary creating provider factory (#33742)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+65.sha-e31f620.with-local-changes
+ * @license Angular v9.0.0-rc.1+64.sha-ccee818.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+65.sha-e31f620.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+64.sha-ccee818.with-local-changes');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+65.sha-e31f620.with-local-changes
+ * @license Angular v9.0.0-rc.1+64.sha-ccee818.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
