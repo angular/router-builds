6f4db111c3428f997c229c3549ae4e9d10b5707b
Kara Erickson <karakara@google.com>
2019-11-13 05:55:35 +0000

    cf10b336e7 fix(ivy): ComponentFactory.create should clear host element content (#33487)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+80.sha-2be0a1d.with-local-changes
+ * @license Angular v9.0.0-rc.1+79.sha-cf10b33.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10156,7 +10156,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+80.sha-2be0a1d.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+79.sha-cf10b33.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
