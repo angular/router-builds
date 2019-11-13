ea4d7e7b7528ea7445b0260cacec459712fc6b73
Kara Erickson <karakara@google.com>
2019-11-13 22:17:36 +0000

    8e5ed203bc fix(router): make routerLinkActive work with query params which contain arrays (#22666)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2.with-local-changes
+ * @license Angular v9.0.0-rc.2+11.sha-8e5ed20.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
