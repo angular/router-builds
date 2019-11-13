64da4cd2377fe668b821219a1e12a3fba2d59e43
Kara Erickson <karakara@google.com>
2019-11-13 21:34:25 +0000

    f1b32840d0 docs: release notes for the v9.0.0-rc.2 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+88.sha-18a8abb.with-local-changes
+ * @license Angular v9.0.0-rc.1+89.sha-f1b3284.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10156,7 +10156,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+88.sha-18a8abb.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+89.sha-f1b3284.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
