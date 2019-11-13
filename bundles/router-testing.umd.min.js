859cd0b6b47232804abd0c2b4935d473c3baa42e
Kara Erickson <karakara@google.com>
2019-11-13 21:32:47 +0000

    f1b32840d0 docs: release notes for the v9.0.0-rc.2 release

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+88.sha-18a8abb.with-local-changes
+ * @license Angular v9.0.0-rc.1+89.sha-f1b3284.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+88.sha-18a8abb.with-local-changes
+ * @license Angular v9.0.0-rc.1+89.sha-f1b3284.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+88.sha-18a8abb.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+89.sha-f1b3284.with-local-changes');
 
     /**
      * @license
