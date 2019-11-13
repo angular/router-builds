9c99ccff80350d03e4bf8262abcec16023ffe3a4
Kara Erickson <karakara@google.com>
2019-11-13 19:32:51 +0000

    1e1e242570 fix(ngcc): support minified ES5 scenarios (#33777)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+79.sha-cf10b33.with-local-changes
+ * @license Angular v9.0.0-rc.1+86.sha-1e1e242.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10156,7 +10156,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+79.sha-cf10b33.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+86.sha-1e1e242.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
