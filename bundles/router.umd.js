20ce160598c19101f608a2d2bc7f14d4eb5bf70e
Kara Erickson <karakara@google.com>
2019-11-13 21:04:20 +0000

    18a8abb2bf docs: release notes for the v8.2.14 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+87.sha-1d563a7.with-local-changes
+ * @license Angular v9.0.0-rc.1+88.sha-18a8abb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5900,7 +5900,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+87.sha-1d563a7.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.1+88.sha-18a8abb.with-local-changes');
 
     /**
      * @license
