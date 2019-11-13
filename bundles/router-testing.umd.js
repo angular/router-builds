73093640bc810e92538ff912660d22a709d7ae85
Kara Erickson <karakara@google.com>
2019-11-13 05:58:58 +0000

    8ed32e5e71 refactor(language-service): Colocate getAnalzyedModules and upToDate (#33779)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+78.sha-f750f18.with-local-changes
+ * @license Angular v9.0.0-rc.1+80.sha-8ed32e5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+78.sha-f750f18.with-local-changes
+ * @license Angular v9.0.0-rc.1+80.sha-8ed32e5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5738,7 +5738,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+78.sha-f750f18.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+80.sha-8ed32e5.with-local-changes');
 
 /**
  * @license
