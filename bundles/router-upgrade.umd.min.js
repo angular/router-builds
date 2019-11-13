84f15d6c4cc91db88d98af5e3bbdd4c6512f856a
Kara Erickson <karakara@google.com>
2019-11-13 19:31:10 +0000

    49e517d049 fix(ngcc): support minified ES5 scenarios (#33777)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+80.sha-8ed32e5.with-local-changes
+ * @license Angular v9.0.0-rc.1+86.sha-49e517d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
