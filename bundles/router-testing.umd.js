a4a13e85c676ab05a063ffab3a54df24f9941f66
Kara Erickson <karakara@google.com>
2019-11-13 05:53:59 +0000

    cf10b336e7 fix(ivy): ComponentFactory.create should clear host element content (#33487)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+80.sha-2be0a1d.with-local-changes
+ * @license Angular v9.0.0-rc.1+79.sha-cf10b33.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+80.sha-2be0a1d.with-local-changes
+ * @license Angular v9.0.0-rc.1+79.sha-cf10b33.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+80.sha-2be0a1d.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+79.sha-cf10b33.with-local-changes');
 
     /**
      * @license
