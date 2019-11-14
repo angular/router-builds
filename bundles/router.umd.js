c45c9a217fda8f7c1df0b908aeeb69f94397f27b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-14 20:32:45 +0000

    a61fb76d51 fix(ivy): avoid cyclical dependency in imports (#33831)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2+19.sha-b1d0a4f.with-local-changes
+ * @license Angular v9.0.0-rc.2+20.sha-a61fb76.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.2+19.sha-b1d0a4f.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.2+20.sha-a61fb76.with-local-changes');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2+19.sha-b1d0a4f.with-local-changes
+ * @license Angular v9.0.0-rc.2+20.sha-a61fb76.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
