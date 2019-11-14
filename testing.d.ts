1f4823f34f2cc1499311436f177874b87ef65b0e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-14 17:58:19 +0000

    aaa38cf3f3 build: minor improvements to build script output (#33823)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2+11.sha-8e5ed20.with-local-changes
+ * @license Angular v9.0.0-rc.2+17.sha-aaa38cf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
