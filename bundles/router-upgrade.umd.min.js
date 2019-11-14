88a8bd2071823a75f119256c6c85e5c04c5c7ec4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-14 17:56:55 +0000

    e25b256aa4 build: minor improvements to build script output (#33823)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+100.sha-b30bb8d.with-local-changes
+ * @license Angular v9.0.0-rc.1+106.sha-e25b256.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
