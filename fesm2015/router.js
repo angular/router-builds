b3f1079c9cbed15fd718d4da6f3af20b1b426aa3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-14 19:14:37 +0000

    eca85d5880 feat(bazel): update ng-add to use bazel v1.1.0 (#33813)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+106.sha-e25b256.with-local-changes
+ * @license Angular v9.0.0-rc.1+108.sha-eca85d5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9963,7 +9963,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+106.sha-e25b256.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+108.sha-eca85d5.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
