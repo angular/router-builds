8c147290a38d47a389f0d2413b50b65cc051528b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-15 19:13:12 +0000

    9d21065e25 build: fix zone.js version stamping after updating to new rollup_bundle (#33329)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+110.sha-e51ec67.with-local-changes
+ * @license Angular v9.0.0-rc.1+125.sha-9d21065.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10176,7 +10176,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+110.sha-e51ec67.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+125.sha-9d21065.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
