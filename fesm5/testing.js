8f560c60ea735591f149f757c867fb8c49339c10
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-15 21:34:05 +0000

    b90aac0d43 Revert "fix(router): make routerLinkActive work with query params which contain arrays (#22666)" (#33861)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+125.sha-9d21065.with-local-changes
+ * @license Angular v9.0.0-rc.1+126.sha-b90aac0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
