b1e9117390c8f735f5263a0be8e67b277271fc15
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-15 19:12:26 +0000

    c182dea146 build: fix zone.js version stamping after updating to new rollup_bundle (#33329)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2+21.sha-8452458.with-local-changes
+ * @license Angular v9.0.0-rc.2+36.sha-c182dea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
