feb03c9c7da693e152ea79662030f285e18c7ef6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-15 00:27:22 +0000

    e51ec671a5 fix(ivy): extend assertion in `directiveInject` function to support IcuContainers (#33832)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+109.sha-96c9ccc.with-local-changes
+ * @license Angular v9.0.0-rc.1+110.sha-e51ec67.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10176,7 +10176,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+109.sha-96c9ccc.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+110.sha-e51ec67.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
