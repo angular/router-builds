d61f7161e0e07704def2c18ee9980f05aa0377a5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-16 00:38:26 +0000

    7567f2b8c8 Revert "build: add ngJitMode to ng_rollup_bundle terser config (#33773)"

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2+37.sha-3074cdf.with-local-changes
+ * @license Angular v9.0.0-rc.2+45.sha-7567f2b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.2+37.sha-3074cdf.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.2+45.sha-7567f2b.with-local-changes');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2+37.sha-3074cdf.with-local-changes
+ * @license Angular v9.0.0-rc.2+45.sha-7567f2b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
