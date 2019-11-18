e3f9c84c3eb5a655153cc02b7db68caeb7e3e01d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-18 17:36:15 +0000

    1218ce4f5f docs: add xrefs to update app and deprecations policy (#33767)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2+45.sha-7567f2b.with-local-changes
+ * @license Angular v9.0.0-rc.2+61.sha-1218ce4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9943,7 +9943,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.2+45.sha-7567f2b.with-local-changes');
+const VERSION = new Version('9.0.0-rc.2+61.sha-1218ce4.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2+45.sha-7567f2b.with-local-changes
+ * @license Angular v9.0.0-rc.2+61.sha-1218ce4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
