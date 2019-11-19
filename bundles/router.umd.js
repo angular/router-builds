87f2e21b802bd112dce50762ccf6f38687e7861d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-19 17:56:21 +0000

    c68200cf42 build: remove rollup-rule temp settings (#33908)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+151.sha-341d584.with-local-changes
+ * @license Angular v9.0.0-rc.1+164.sha-c68200c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+151.sha-341d584.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+164.sha-c68200c.with-local-changes');
 
     /**
      * @license
