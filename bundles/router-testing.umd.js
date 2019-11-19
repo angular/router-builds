c8d14d65bec91b873dd4c22a96cb1bccf167c068
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-19 22:09:28 +0000

    bb290cefae fix(core): make QueryList implement Iterable in the type system (#33536)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+179.sha-850aee2.with-local-changes
+ * @license Angular v9.0.0-rc.1+180.sha-bb290ce.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+179.sha-850aee2.with-local-changes
+ * @license Angular v9.0.0-rc.1+180.sha-bb290ce.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+179.sha-850aee2.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+180.sha-bb290ce.with-local-changes');
 
     /**
      * @license
