55ee34721ff7196b26a5fd390f078bcebcab1d75
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-19 20:22:18 +0000

    6bf2531b19 fix(ivy): properly insert views before ng-container with injected ViewContainerRef (#33853)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+164.sha-c68200c.with-local-changes
+ * @license Angular v9.0.0-rc.1+177.sha-6bf2531.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
