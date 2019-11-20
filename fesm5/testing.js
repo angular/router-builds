11a849b7b491fb5da1e1b7521d076a755a10e7f2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 23:30:32 +0000

    fc2f6b8456 fix(ivy): wrap functions from "providers" in parentheses in Closure mode (#33609)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+201.sha-b07b6f1.with-local-changes
+ * @license Angular v9.0.0-rc.1+204.sha-fc2f6b8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
