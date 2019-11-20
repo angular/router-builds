db2edc21995567210e89d05d9e4f14739c119dd2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 22:18:32 +0000

    213e3c3e49 fix(ivy): move setClassMetadata calls into a pure iife (#33337)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2+96.sha-595375f.with-local-changes
+ * @license Angular v9.0.0-rc.2+98.sha-213e3c3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
