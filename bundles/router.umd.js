7b5263cff2c8b1004fcb9894c7564e3f14138d02
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 19:30:04 +0000

    9e45203679 test(docs-infra): more thoroughly clean up after `ScrollService` tests (#33937)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+181.sha-a959fae.with-local-changes
+ * @license Angular v9.0.0-rc.1+183.sha-9e45203.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5900,7 +5900,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+181.sha-a959fae.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.1+183.sha-9e45203.with-local-changes');
 
     /**
      * @license
