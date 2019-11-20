d4232ba7951d1be964920ec038c873ec497e0e8f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 23:28:50 +0000

    b07b6f1d40 fix(ivy): avoid infinite recursion when evaluation source files (#33772)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+199.sha-fd83d94.with-local-changes
+ * @license Angular v9.0.0-rc.1+201.sha-b07b6f1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+199.sha-fd83d94.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+201.sha-b07b6f1.with-local-changes');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+199.sha-fd83d94.with-local-changes
+ * @license Angular v9.0.0-rc.1+201.sha-b07b6f1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
