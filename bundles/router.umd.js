ce4baa5a6a29fb060b92ce0afca92905edf0b330
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 23:08:35 +0000

    55748dbc55 fix(core): allow css custom variables/properties in the style sanitizer (#33841)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+190.sha-d78d29f.with-local-changes
+ * @license Angular v9.0.0-rc.1+197.sha-55748db.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5935,7 +5935,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+190.sha-d78d29f.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+197.sha-55748db.with-local-changes');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+190.sha-d78d29f.with-local-changes
+ * @license Angular v9.0.0-rc.1+197.sha-55748db.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
