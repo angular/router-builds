586eee78793260ab8433fe0ca88bdd8f7b2268b8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 18:51:41 +0000

    a959fae66e build: add ngJitMode to ng_rollup_bundle terser config (#33865)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+180.sha-bb290ce.with-local-changes
+ * @license Angular v9.0.0-rc.1+181.sha-a959fae.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10156,7 +10156,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+180.sha-bb290ce.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+181.sha-a959fae.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
