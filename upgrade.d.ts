f46c8514787aa6a8460e3017665b33f812eb9715
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 23:31:14 +0000

    fc6ad19089 fix(ivy): wrap functions from "providers" in parentheses in Closure mode (#33609)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2+98.sha-213e3c3.with-local-changes
+ * @license Angular v9.0.0-rc.3+14.sha-fc6ad19.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
