766f59f85d08726fdd98be4243ad1c3a01e4634f
Matias Niemelä <matias@yearofmoo.com>
2019-11-21 22:19:20 +0000

    fd5f101bab docs: add CSRF to XSRF section of http guide (#32933)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+215.sha-2303dc4.with-local-changes
+ * @license Angular v9.0.0-rc.1+216.sha-fd5f101.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10156,7 +10156,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+215.sha-2303dc4.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+216.sha-fd5f101.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
