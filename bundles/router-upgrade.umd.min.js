0518d6fbe3bc2e0ef37c0677f401ffbf4ae51490
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 22:12:52 +0000

    de043a4bcb refactor(bazel): use getattr instead of hasattr in ng_module.bzl (#33765)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+232.sha-485ce64.with-local-changes
+ * @license Angular v9.0.0-rc.1+237.sha-de043a4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
