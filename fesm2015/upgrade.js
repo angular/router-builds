bb83dd1e87da7267e8dcdbed06a9d0ac65f74e63
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 23:06:35 +0000

    9248dba8fa docs: Add Joey Perrott to Angular team in contributors (#33962)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+47.sha-85a4a1a.with-local-changes
+ * @license Angular v9.0.0-rc.3+52.sha-9248dba.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+47.sha-85a4a1a.with-local-changes
+ * @license Angular v9.0.0-rc.3+52.sha-9248dba.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.3+47.sha-85a4a1a.with-local-changes');
+var VERSION = new Version('9.0.0-rc.3+52.sha-9248dba.with-local-changes');
 
 /**
  * @license
