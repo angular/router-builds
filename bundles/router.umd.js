c94a84f8e7e7d6d001bebb03c774b32a9d84533e
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 19:27:12 +0000

    485ce640fd ci: use more descriptive names for published build artifacts (#33957)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+231.sha-dca6ad8.with-local-changes
+ * @license Angular v9.0.0-rc.1+232.sha-485ce64.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+231.sha-dca6ad8.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+232.sha-485ce64.with-local-changes');
 
     /**
      * @license
