343f4ee7eee1f538424cb5281fbee37d24e055c7
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 17:56:56 +0000

    2dd572b15c docs(http): fix typo (#33978)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+220.sha-828e5c1.with-local-changes
+ * @license Angular v9.0.0-rc.1+221.sha-2dd572b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+220.sha-828e5c1.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.1+221.sha-2dd572b.with-local-changes');
 
     /**
      * @license
