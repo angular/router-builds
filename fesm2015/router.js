cff815ebad99e4b30cc1d543f91b1d8184995ab8
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 23:03:44 +0000

    a4c3ceeddb build: change visible compiler name during ng_module builds to be Ivy/ViewEngine (#33995)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+242.sha-e24453b.with-local-changes
+ * @license Angular v9.0.0-rc.1+240.sha-a4c3cee.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10209,7 +10209,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+242.sha-e24453b.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+240.sha-a4c3cee.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+242.sha-e24453b.with-local-changes
+ * @license Angular v9.0.0-rc.1+240.sha-a4c3cee.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5716,7 +5716,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+242.sha-e24453b.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+240.sha-a4c3cee.with-local-changes');
 
 /**
  * @license
