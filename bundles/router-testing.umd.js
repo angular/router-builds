2d223d0e06e18e31075516377dc219e735961efb
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 22:55:09 +0000

    588823b437 refactor(elements): ng-add schematics (#33723)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+237.sha-de043a4.with-local-changes
+ * @license Angular v9.0.0-rc.1+238.sha-588823b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+237.sha-de043a4.with-local-changes
+ * @license Angular v9.0.0-rc.1+238.sha-588823b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+237.sha-de043a4.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+238.sha-588823b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
