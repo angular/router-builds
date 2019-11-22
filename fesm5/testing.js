08b0c4447a22630fefef8a18952c7aeb6f82d39c
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 22:59:33 +0000

    17857a311f docs: fix typo in the aio/README.md (#33993)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+238.sha-588823b.with-local-changes
+ * @license Angular v9.0.0-rc.1+239.sha-17857a3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
