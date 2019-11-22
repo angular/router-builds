a77c9c40e6ec6dba908beaa4c1a6619b6022e6a1
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 22:57:38 +0000

    17857a311f docs: fix typo in the aio/README.md (#33993)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+238.sha-588823b.with-local-changes
+ * @license Angular v9.0.0-rc.1+239.sha-17857a3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+238.sha-588823b.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+239.sha-17857a3.with-local-changes');
 
     /**
      * @license
