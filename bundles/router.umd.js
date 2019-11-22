8a5c7a97f90a370273af51c60e139c8bd5accb71
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 23:02:12 +0000

    a4c3ceeddb build: change visible compiler name during ng_module builds to be Ivy/ViewEngine (#33995)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+242.sha-e24453b.with-local-changes
+ * @license Angular v9.0.0-rc.1+240.sha-a4c3cee.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+242.sha-e24453b.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+240.sha-a4c3cee.with-local-changes');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+242.sha-e24453b.with-local-changes
+ * @license Angular v9.0.0-rc.1+240.sha-a4c3cee.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
