75f6ee1f4d3be3f02c450c81a4da5df8fe4948d1
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 01:06:24 +0000

    7d579b913c build: create bazel define flag, `angular_ivy_enabled`, to build using ivy (#33975)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+217.sha-3d69693.with-local-changes
+ * @license Angular v9.0.0-rc.1+218.sha-7d579b9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
