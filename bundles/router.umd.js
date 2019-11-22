2d2ec6399e06579559bec81c3b1f2589b89d2146
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 22:56:40 +0000

    588823b437 refactor(elements): ng-add schematics (#33723)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+237.sha-de043a4.with-local-changes
+ * @license Angular v9.0.0-rc.1+238.sha-588823b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+237.sha-de043a4.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.1+238.sha-588823b.with-local-changes');
 
     /**
      * @license
