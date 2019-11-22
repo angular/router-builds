1716e2a6ba99c210f4519ba72734ccc9a2e3d2c6
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 23:01:16 +0000

    e24453b9f8 docs: Add Joey Perrott to Angular team in contributors (#33962)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+241.sha-4cf1979.with-local-changes
+ * @license Angular v9.0.0-rc.1+242.sha-e24453b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+241.sha-4cf1979.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+242.sha-e24453b.with-local-changes');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+241.sha-4cf1979.with-local-changes
+ * @license Angular v9.0.0-rc.1+242.sha-e24453b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
