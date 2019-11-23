d95dd51703d63e343129f57955a3ecd9b14ff14b
Matias Niemelä <matias@yearofmoo.com>
2019-11-23 00:01:11 +0000

    7b77ead88f ci: add JoostK to @angular/fw-compiler (#33994)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+52.sha-9248dba.with-local-changes
+ * @license Angular v9.0.0-rc.3+55.sha-7b77ead.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.3+52.sha-9248dba.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.3+55.sha-7b77ead.with-local-changes');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+52.sha-9248dba.with-local-changes
+ * @license Angular v9.0.0-rc.3+55.sha-7b77ead.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
