f6e87eabb5d761f06c348175b382022ccf57cb2d
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 18:07:11 +0000

    457ac3ac46 perf(ivy): fix creation time micro-benchmarks (#34031)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+63.sha-688b188.with-local-changes
+ * @license Angular v9.0.0-rc.3+64.sha-457ac3a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
