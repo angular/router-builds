fe24446cd3422ffa3b571ae4144bb769fcac58e0
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 16:55:49 +0000

    1217c6b7cc docs: replace class hero to interface wherever class properties not used (#34022)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+244.sha-5129bcc.with-local-changes
+ * @license Angular v9.0.0-rc.1+246.sha-1217c6b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
