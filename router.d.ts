123ced78012085ca4d650777caf5e1751f3de2fa
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 16:59:36 +0000

    44225e4010 fix(ngcc): render UMD global imports correctly (#34012)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+247.sha-51a56bc.with-local-changes
+ * @license Angular v9.0.0-rc.1+248.sha-44225e4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
