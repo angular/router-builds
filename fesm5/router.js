f2724acf06a1e0d16607c451d99930e53e629f92
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 17:05:33 +0000

    04b12fc0d6 refactor(ivy): remove isRootView check from getRenderParent (#33988)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+251.sha-478f882.with-local-changes
+ * @license Angular v9.0.0-rc.1+250.sha-04b12fc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+251.sha-478f882.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+250.sha-04b12fc.with-local-changes');
 
 /**
  * @license
