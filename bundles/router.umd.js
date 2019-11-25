fb597ebb10e6b39a27818cf9d39162a3008059db
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 21:54:21 +0000

    ec495c807f ci: do not check/upload `packages-dist/` UMD bundle sizes multiple times (#33987)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+255.sha-56f33bc.with-local-changes
+ * @license Angular v9.0.0-rc.1+258.sha-ec495c8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+255.sha-56f33bc.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+258.sha-ec495c8.with-local-changes');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+255.sha-56f33bc.with-local-changes
+ * @license Angular v9.0.0-rc.1+258.sha-ec495c8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+255.sha-56f33bc.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+258.sha-ec495c8.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
