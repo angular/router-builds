76af00372b96b2a21eeb8a32378810e9c1c04f7f
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 18:06:05 +0000

    31e2bdffa8 perf(ivy): fix creation time micro-benchmarks (#34031)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+253.sha-d80eeb6.with-local-changes
+ * @license Angular v9.0.0-rc.1+254.sha-31e2bdf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+253.sha-d80eeb6.with-local-changes
+ * @license Angular v9.0.0-rc.1+254.sha-31e2bdf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+253.sha-d80eeb6.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+254.sha-31e2bdf.with-local-changes');
 
     /**
      * @license
