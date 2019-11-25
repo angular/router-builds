a9a8fc268c176482f758c745b161da512bce0a3a
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 21:57:09 +0000

    25dcc7631f fix(ivy): add flag to skip non-exported classes (#33921)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+258.sha-ec495c8.with-local-changes
+ * @license Angular v9.0.0-rc.1+259.sha-25dcc76.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
