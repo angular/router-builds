bb07f03349ec33eb6cd4d5e1d34bc6c7510c2b1d
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 17:09:42 +0000

    688b188484 build: remove compile_strategy from ng_module.bzl (#33999)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+55.sha-7b77ead.with-local-changes
+ * @license Angular v9.0.0-rc.3+63.sha-688b188.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.3+55.sha-7b77ead.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.3+63.sha-688b188.with-local-changes');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+55.sha-7b77ead.with-local-changes
+ * @license Angular v9.0.0-rc.3+63.sha-688b188.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
