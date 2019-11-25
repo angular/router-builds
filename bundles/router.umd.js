e624a965a3268c6b060a831ca1edffefe4893fe5
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 16:56:18 +0000

    51a56bc4c6 docs: fix typos and highlight booleans to keep consistency (#34013)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+246.sha-1217c6b.with-local-changes
+ * @license Angular v9.0.0-rc.1+247.sha-51a56bc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+246.sha-1217c6b.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+247.sha-51a56bc.with-local-changes');
 
     /**
      * @license
