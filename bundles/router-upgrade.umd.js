13a3ae0332ebab8933e925bd107bf0e38d3f6370
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 17:07:13 +0000

    04b12fc0d6 refactor(ivy): remove isRootView check from getRenderParent (#33988)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+251.sha-478f882.with-local-changes
+ * @license Angular v9.0.0-rc.1+250.sha-04b12fc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
