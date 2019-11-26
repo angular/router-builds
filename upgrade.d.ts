d17946e1048956ce7e553b55ca9ef9480b7b1b41
Matias Niemelä <matias@yearofmoo.com>
2019-11-26 03:59:20 +0000

    d655093123 docs: add Animations import in Router doc (#33740)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+266.sha-953365d.with-local-changes
+ * @license Angular v9.0.0-rc.1+267.sha-d655093.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
