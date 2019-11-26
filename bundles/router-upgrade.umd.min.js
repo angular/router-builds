6d6dce4ca7bb05c2ec27de94de70b6df312d6e8d
Matias Niemelä <matias@yearofmoo.com>
2019-11-26 04:06:09 +0000

    b659aa32c9 fix(ivy): take styles extracted from template into account in JIT mode (#34017)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+73.sha-bbc8b0f.with-local-changes
+ * @license Angular v9.0.0-rc.3+76.sha-b659aa3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
