4875c6f1c09b02396f1deffcc104da00317630c3
Matias Niemelä <matias@yearofmoo.com>
2019-11-26 02:10:13 +0000

    d25de63ac8 build: update payload limits for `cli-hello-world-ivy-i18n` es2015 bundle (#34043)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+260.sha-9ba5344.with-local-changes
+ * @license Angular v9.0.0-rc.1+264.sha-d25de63.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
