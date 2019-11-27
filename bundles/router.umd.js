6f65142ae627da5654ebbeda16f6c82fa76f7ac3
Miško Hevery <misko@hevery.com>
2019-11-27 00:36:59 +0000

    49804fe017 fix(language-service): determine index types accessed using dot notation (#33884)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+286.sha-3f68377.with-local-changes
+ * @license Angular v9.0.0-rc.1+282.sha-49804fe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+286.sha-3f68377.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.1+282.sha-49804fe.with-local-changes');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+286.sha-3f68377.with-local-changes
+ * @license Angular v9.0.0-rc.1+282.sha-49804fe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
