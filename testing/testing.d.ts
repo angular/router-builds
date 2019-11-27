382e8df5b93aff8a527205901b0b64c305ddc33a
Miško Hevery <misko@hevery.com>
2019-11-27 00:40:51 +0000

    c291c8e2cb ci: do not run ng_benchmarks on CI (#34057)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+288.sha-005c4a1.with-local-changes
+ * @license Angular v9.0.0-rc.1+289.sha-c291c8e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
