d4706a807adb99d86c2fdea7b788e5e856b9f41b
Miško Hevery <misko@hevery.com>
2019-11-27 00:42:03 +0000

    005c4a1cc5 build: set default ts_devserver serving_path to /app_bundle.js to match g3 (#33996)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+282.sha-49804fe.with-local-changes
+ * @license Angular v9.0.0-rc.1+288.sha-005c4a1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+282.sha-49804fe.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.1+288.sha-005c4a1.with-local-changes');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+282.sha-49804fe.with-local-changes
+ * @license Angular v9.0.0-rc.1+288.sha-005c4a1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
