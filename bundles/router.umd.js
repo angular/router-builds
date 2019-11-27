1a270bc8c81aaf9c983130c7d3d50b5a0e089581
Miško Hevery <misko@hevery.com>
2019-11-27 21:27:19 +0000

    85b551a388 Revert "refactor: use isObservable provided by rxjs 6.1+ (#27668)"

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+289.sha-c291c8e.with-local-changes
+ * @license Angular v9.0.0-rc.1+301.sha-85b551a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+289.sha-c291c8e.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.1+301.sha-85b551a.with-local-changes');
 
     /**
      * @license
