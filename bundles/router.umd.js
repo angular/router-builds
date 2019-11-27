5c6fe2100fd8a6862adda69cce7628b4b580e0a2
Miško Hevery <misko@hevery.com>
2019-11-27 01:16:19 +0000

    9e5065a778 ci: do not run ng_benchmarks on CI (#34057)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+76.sha-b659aa3.with-local-changes
+ * @license Angular v9.0.0-rc.3+98.sha-9e5065a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.3+76.sha-b659aa3.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.3+98.sha-9e5065a.with-local-changes');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+76.sha-b659aa3.with-local-changes
+ * @license Angular v9.0.0-rc.3+98.sha-9e5065a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
