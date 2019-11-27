2f3b09d691c16bc5177e5f58632a6ae5a57ec471
Miško Hevery <misko@hevery.com>
2019-11-27 23:37:48 +0000

    168abc6d6f docs: release notes for the v9.0.0-rc.4 release

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+302.sha-6108962.with-local-changes
+ * @license Angular v9.0.0-rc.1+303.sha-168abc6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+302.sha-6108962.with-local-changes
+ * @license Angular v9.0.0-rc.1+303.sha-168abc6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+302.sha-6108962.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+303.sha-168abc6.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
