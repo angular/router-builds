2b93c320613705a9058b011a741e609122a850a1
Miško Hevery <misko@hevery.com>
2019-12-02 19:51:24 +0000

    39722df41e fix(language-service): Do not produce diagnostics if metadata for NgModule not found (#34113)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+308.sha-3408468.with-local-changes
+ * @license Angular v9.0.0-rc.1+315.sha-39722df.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
