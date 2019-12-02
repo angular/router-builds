9647e33b8e43d782dab83b4f54302e32072d48ff
Miško Hevery <misko@hevery.com>
2019-12-02 19:59:57 +0000

    c16a79df5c build: add failure message if compile build variable for angular/angular usage (#34109)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+316.sha-99320e1.with-local-changes
+ * @license Angular v9.0.0-rc.1+314.sha-c16a79d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
