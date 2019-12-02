7e8bd636f67d48bb2afbe9ea7a34d11b31d2e0a8
Miško Hevery <misko@hevery.com>
2019-12-02 19:50:12 +0000

    bbce2ad7d4 build: update to nodejs rules 0.42.1 (#34073)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+315.sha-39722df.with-local-changes
+ * @license Angular v9.0.0-rc.1+310.sha-bbce2ad.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+315.sha-39722df.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+310.sha-bbce2ad.with-local-changes');
 
 /**
  * @license
