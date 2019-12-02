dbcd4121bc7b399c0a97d783eba5142d10142c90
Miško Hevery <misko@hevery.com>
2019-12-02 20:34:47 +0000

    755d2d572f refactor(ivy): remove unnecessary fac wrapper (#34076)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+329.sha-02958c0.with-local-changes
+ * @license Angular v9.0.0-rc.1+330.sha-755d2d5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+329.sha-02958c0.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+330.sha-755d2d5.with-local-changes');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+329.sha-02958c0.with-local-changes
+ * @license Angular v9.0.0-rc.1+330.sha-755d2d5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
