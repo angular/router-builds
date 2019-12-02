8af2ca00e5a1132a459082c596f8915ff3aa01ef
Miško Hevery <misko@hevery.com>
2019-12-02 19:58:47 +0000

    5a7f33496b refactor(compiler-cli): remove bogus packages/compiler-cli/integrationtest/benchmarks/ (#34142)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+314.sha-c16a79d.with-local-changes
+ * @license Angular v9.0.0-rc.1+321.sha-5a7f334.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+314.sha-c16a79d.with-local-changes
+ * @license Angular v9.0.0-rc.1+321.sha-5a7f334.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+314.sha-c16a79d.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+321.sha-5a7f334.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
