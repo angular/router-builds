22470491f62f0e59849105595419e700d87d3064
Miško Hevery <misko@hevery.com>
2019-12-03 18:59:32 +0000

    671deadde1 fix(docs-infra): avoid version mismatch error when using local Angular packages (#34213)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+350.sha-f84f362.with-local-changes
+ * @license Angular v9.0.0-rc.1+351.sha-671dead.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+350.sha-f84f362.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.1+351.sha-671dead.with-local-changes');
 
     /**
      * @license
