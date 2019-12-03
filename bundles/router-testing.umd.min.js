1f300f544a6d521d99026020a3d974771f2c674c
Miško Hevery <misko@hevery.com>
2019-12-03 18:55:09 +0000

    6b1a47183d fix(ivy): i18n - trim whitespace when parsing metadata (#34154)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+348.sha-539d8f0.with-local-changes
+ * @license Angular v9.0.0-rc.1+347.sha-6b1a471.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+348.sha-539d8f0.with-local-changes
+ * @license Angular v9.0.0-rc.1+347.sha-6b1a471.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+348.sha-539d8f0.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+347.sha-6b1a471.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
