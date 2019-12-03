46f9c65e5e1ce94392787129a242244153cc7664
Miško Hevery <misko@hevery.com>
2019-12-03 19:13:46 +0000

    d2538cab5a fix(language-service): Make missing module suggestion instead of error (#34115) (#34193)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.4+28.sha-716fc84.with-local-changes
+ * @license Angular v9.0.0-rc.4+51.sha-d2538ca.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
