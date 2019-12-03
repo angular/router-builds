a8b46c978a8e060ab6ac9f3a33c70dcfadd5aafe
Miško Hevery <misko@hevery.com>
2019-12-03 18:36:18 +0000

    56f4e56094 docs: Ivy i18n guide updates for localize option (#34053)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+334.sha-dd944ef.with-local-changes
+ * @license Angular v9.0.0-rc.1+335.sha-56f4e56.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+334.sha-dd944ef.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.1+335.sha-56f4e56.with-local-changes');
 
     /**
      * @license
