472fdca02444b624dbff0a77067777428bc3f2d7
Miško Hevery <misko@hevery.com>
2019-12-03 18:55:27 +0000

    539d8f09e0 fix: implement Symbol.specics of Promise (#34162)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+344.sha-f16f6a2.with-local-changes
+ * @license Angular v9.0.0-rc.1+348.sha-539d8f0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
