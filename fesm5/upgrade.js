bccc190d3b36c2e7068a7c16d6983df3dab15fdf
Miško Hevery <misko@hevery.com>
2019-12-03 19:01:25 +0000

    86d5472d91 docs(service-worker): fix paths for `ngsw-config` command to match latest CLI (#34215)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+351.sha-671dead.with-local-changes
+ * @license Angular v9.0.0-rc.1+352.sha-86d5472.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
