117bd55a3645a13caa26c0658d1858654d8aa223
Miško Hevery <misko@hevery.com>
2019-12-03 18:57:05 +0000

    e6909bda89 fix(ivy): incorrectly validating html foreign objects inside svg (#34178)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+347.sha-6b1a471.with-local-changes
+ * @license Angular v9.0.0-rc.1+349.sha-e6909bd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
