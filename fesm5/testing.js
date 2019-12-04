72635196fd4985a1a16277fa5c9d2aa3631a3489
Miško Hevery <misko@hevery.com>
2019-12-04 00:24:53 +0000

    8f102fe674 docs: remove information about which version of Firefox started supporting custom elements (#34007) PR Close #34007

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+354.sha-15d3e74.with-local-changes
+ * @license Angular v9.0.0-rc.1+355.sha-8f102fe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
