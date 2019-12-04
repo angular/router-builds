a2628ced154bb887669256b9ef3878fc758742b6
Misko Hevery <misko@hevery.com>
2019-12-04 07:21:50 +0000

    634887c0e7 test(ivy): update `ngI18nClosureMode` flag usage in tests (#34224)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+365.sha-9b6a1b8.with-local-changes
+ * @license Angular v9.0.0-rc.1+370.sha-634887c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
