ece85d1490de8af423854dc3813e96624bd4ede5
Miško Hevery <misko@hevery.com>
2019-12-04 00:45:02 +0000

    fcbc38cb22 test(ivy): ViewContainerRef.move where the old and new indexes are the same (#34156)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+357.sha-bd820fd.with-local-changes
+ * @license Angular v9.0.0-rc.1+360.sha-fcbc38c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
