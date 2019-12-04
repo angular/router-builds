f32b0917eb7f478b25ac8b38cef1cf1247868f81
Misko Hevery <misko@hevery.com>
2019-12-04 07:21:36 +0000

    95557318bc test(ivy): update `ngI18nClosureMode` flag usage in tests (#34224)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.4+51.sha-d2538ca.with-local-changes
+ * @license Angular v9.0.0-rc.4+67.sha-9555731.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
