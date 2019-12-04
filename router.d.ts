78241416ccfab47f19f572fd64211b1c0cfc89e1
Miško Hevery <misko@hevery.com>
2019-12-04 00:37:42 +0000

    bd820fdf84 docs(core): update render factory description (#34137)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+356.sha-627814c.with-local-changes
+ * @license Angular v9.0.0-rc.1+357.sha-bd820fd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
