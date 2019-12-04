a3a1ce46fcaccc13249af681dfccb38892c3a04d
Miško Hevery <misko@hevery.com>
2019-12-04 00:55:21 +0000

    9b6a1b85b1 refactor(ivy): check metadata presence before compiling Type in R3TestBed (#34204)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+366.sha-cca2616.with-local-changes
+ * @license Angular v9.0.0-rc.1+365.sha-9b6a1b8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+366.sha-cca2616.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.1+365.sha-9b6a1b8.with-local-changes');
 
     /**
      * @license
