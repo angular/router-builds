7560ec70379c374a23c6fa7db49bb1190ae0dd56
Miško Hevery <misko@hevery.com>
2019-12-04 00:21:27 +0000

    15d3e741e9 feat: update the locale files (#33556)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+352.sha-86d5472.with-local-changes
+ * @license Angular v9.0.0-rc.1+354.sha-15d3e74.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+352.sha-86d5472.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+354.sha-15d3e74.with-local-changes');
 
     /**
      * @license
