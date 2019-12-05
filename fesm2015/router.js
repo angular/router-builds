3e7e48672e5d0a6a7a5f443197164b48148fc2db
Andrew Kushnir <akushnir@google.com>
2019-12-05 19:40:12 +0000

    02134172dd docs: add David Shevitz to about page (#34222)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+380.sha-ecb29de.with-local-changes
+ * @license Angular v9.0.0-rc.1+382.sha-0213417.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+380.sha-ecb29de.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+382.sha-0213417.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+380.sha-ecb29de.with-local-changes
+ * @license Angular v9.0.0-rc.1+382.sha-0213417.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+380.sha-ecb29de.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+382.sha-0213417.with-local-changes');
 
 /**
  * @license
