51ce3debe1c7e86cd4031309f94ad748ab3df9f3
Andrew Kushnir <akushnir@google.com>
2019-12-05 18:42:55 +0000

    37bb90140c refactor(platform-browser): avoid mutable exports. (#34207)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+377.sha-542f236.with-local-changes
+ * @license Angular v9.0.0-rc.1+378.sha-37bb901.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
