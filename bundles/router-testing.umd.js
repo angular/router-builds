da6e9e7b6b8d889d3a547e27871c4573c4fc5520
Andrew Kushnir <akushnir@google.com>
2019-12-05 21:36:37 +0000

    a91ca99b1f refactor(language-service): refactor the method TypeWrapper.name and PipeSymbol.selectSignature (#34177)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+387.sha-0d95c08.with-local-changes
+ * @license Angular v9.0.0-rc.1+391.sha-a91ca99.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+387.sha-0d95c08.with-local-changes
+ * @license Angular v9.0.0-rc.1+391.sha-a91ca99.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+387.sha-0d95c08.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+391.sha-a91ca99.with-local-changes');
 
     /**
      * @license
