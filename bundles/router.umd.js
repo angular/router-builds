907514d9bb6cb195a3cad7a7151198dbe7060ed3
Andrew Kushnir <akushnir@google.com>
2019-12-05 21:23:23 +0000

    421dba0184 fix(docs-infra): make header full-width and panels full-width on mobile screens (#34188)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+382.sha-0213417.with-local-changes
+ * @license Angular v9.0.0-rc.1+384.sha-421dba0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+382.sha-0213417.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+384.sha-421dba0.with-local-changes');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+382.sha-0213417.with-local-changes
+ * @license Angular v9.0.0-rc.1+384.sha-421dba0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
