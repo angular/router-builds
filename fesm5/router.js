860f063d2e9f379321f6a519325efcd9372c19fc
Andrew Kushnir <akushnir@google.com>
2019-12-05 18:33:03 +0000

    c0869ecfb3 fix(docs-infra): add deprecated-api-item class to remaining deprecated items (#34192)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+373.sha-28ef1af.with-local-changes
+ * @license Angular v9.0.0-rc.1+374.sha-c0869ec.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5716,7 +5716,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+373.sha-28ef1af.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+374.sha-c0869ec.with-local-changes');
 
 /**
  * @license
