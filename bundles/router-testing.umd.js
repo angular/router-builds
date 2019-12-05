c9c62f6fcce4d3c6bdf350cea99fa05c04028388
Andrew Kushnir <akushnir@google.com>
2019-12-05 18:47:17 +0000

    ecb29de82f build: update to rules_nodejs 0.42.2 (#34243)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+381.sha-023c9be.with-local-changes
+ * @license Angular v9.0.0-rc.1+380.sha-ecb29de.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
