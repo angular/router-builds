013d8decd29c4f12ebc1d20f0dd51220260e6e8a
Andrew Kushnir <akushnir@google.com>
2019-12-05 18:31:34 +0000

    c0869ecfb3 fix(docs-infra): add deprecated-api-item class to remaining deprecated items (#34192)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+373.sha-28ef1af.with-local-changes
+ * @license Angular v9.0.0-rc.1+374.sha-c0869ec.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+373.sha-28ef1af.with-local-changes
+ * @license Angular v9.0.0-rc.1+374.sha-c0869ec.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+373.sha-28ef1af.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+374.sha-c0869ec.with-local-changes');
 
     /**
      * @license
