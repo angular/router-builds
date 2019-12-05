fa03c5215c479561ec8d8f9d24882d5198c13efd
Andrew Kushnir <akushnir@google.com>
2019-12-05 21:31:32 +0000

    0d95c08cda perf: rename index to index_aot (#34258)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+386.sha-d6f2788.with-local-changes
+ * @license Angular v9.0.0-rc.1+387.sha-0d95c08.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
