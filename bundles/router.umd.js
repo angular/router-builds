379fc3e25ceed954011e8b107d52560ffd9059ae
Andrew Kushnir <akushnir@google.com>
2019-12-05 21:35:22 +0000

    0e911f87b0 fix(language-service): Remove getExternalFiles() (#34260)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+12.sha-81c8b83.with-local-changes
+ * @license Angular v9.0.0-rc.5+22.sha-0e911f8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.5+12.sha-81c8b83.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.5+22.sha-0e911f8.with-local-changes');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+12.sha-81c8b83.with-local-changes
+ * @license Angular v9.0.0-rc.5+22.sha-0e911f8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
