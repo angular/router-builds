41b1d90296ca435c0c4428abf492361005be416d
Andrew Kushnir <akushnir@google.com>
2019-12-05 19:41:50 +0000

    02134172dd docs: add David Shevitz to about page (#34222)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+380.sha-ecb29de.with-local-changes
+ * @license Angular v9.0.0-rc.1+382.sha-0213417.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
