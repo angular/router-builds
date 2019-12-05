fb1e1000638223f41c490e6fd81c28dfead348b7
Andrew Kushnir <akushnir@google.com>
2019-12-05 21:25:36 +0000

    de62d8ebdb docs: fix migrations formatting (#33267)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+385.sha-0341f32.with-local-changes
+ * @license Angular v9.0.0-rc.1+383.sha-de62d8e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+385.sha-0341f32.with-local-changes
+ * @license Angular v9.0.0-rc.1+383.sha-de62d8e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+385.sha-0341f32.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+383.sha-de62d8e.with-local-changes');
 
     /**
      * @license
