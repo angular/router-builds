8abffbd16b8e4b87ab252f9080a369bf87e5c5e9
Andrew Kushnir <akushnir@google.com>
2019-12-05 21:31:29 +0000

    d6f278867f build: Enable ivy in google3 if ng_module attr is True (#34238)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+383.sha-de62d8e.with-local-changes
+ * @license Angular v9.0.0-rc.1+386.sha-d6f2788.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
