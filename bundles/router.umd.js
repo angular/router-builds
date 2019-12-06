9b87d44bce837ad512f23c158e30ef33f15ed603
Andrew Kushnir <akushnir@google.com>
2019-12-06 19:08:50 +0000

    7a6e326ec6 docs: update CHANGELOG to add v9.0.0-rc.5 release notes (#34282)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+394.sha-613dc11.with-local-changes
+ * @license Angular v9.0.0-rc.1+395.sha-7a6e326.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+394.sha-613dc11.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+395.sha-7a6e326.with-local-changes');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+394.sha-613dc11.with-local-changes
+ * @license Angular v9.0.0-rc.1+395.sha-7a6e326.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
