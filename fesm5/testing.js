306c24a81d179dfbd358003102bda20184f9829c
Andrew Kushnir <akushnir@google.com>
2019-12-06 18:45:18 +0000

    7dfd327099 fix(language-service): Simplify resolution logic in banner (#34262)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+22.sha-0e911f8.with-local-changes
+ * @license Angular v9.0.0-rc.5+25.sha-7dfd327.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
