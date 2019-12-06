76a8092d3df47d1c536a7e51f0535a7a789e537b
Andrew Kushnir <akushnir@google.com>
2019-12-06 21:37:06 +0000

    10a33efc3e fix(ivy): align TestBed.overrideProvider with what happens with providers in TestBed providers array (#33769)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+28.sha-6e944ac.with-local-changes
+ * @license Angular v9.0.0-rc.5+32.sha-10a33ef.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.5+28.sha-6e944ac.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.5+32.sha-10a33ef.with-local-changes');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+28.sha-6e944ac.with-local-changes
+ * @license Angular v9.0.0-rc.5+32.sha-10a33ef.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
