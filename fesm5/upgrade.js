51df516ba5a2775526352f6452a21632ac4825d9
Andrew Kushnir <akushnir@google.com>
2019-12-06 18:44:50 +0000

    3805172f9c docs: edit copy of getting started step 3 (#32820)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+392.sha-f05e171.with-local-changes
+ * @license Angular v9.0.0-rc.1+393.sha-3805172.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
