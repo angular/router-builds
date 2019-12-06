dad44b14ccfc78170a30acafc68160dd9bee37e7
Andrew Kushnir <akushnir@google.com>
2019-12-06 21:29:46 +0000

    a1d0f1e5d2 fix(ivy): align TestBed.overrideProvider with what happens with providers in TestBed providers array (#33769)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+401.sha-a12b5f9.with-local-changes
+ * @license Angular v9.0.0-rc.1+402.sha-a1d0f1e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
