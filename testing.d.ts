7f52f1fb829057fd67ea156a3a7e19046ac530b3
Andrew Kushnir <akushnir@google.com>
2019-12-06 19:25:51 +0000

    f72de51a6f build: remove compile build variable (#34280)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+396.sha-e7cf1e0.with-local-changes
+ * @license Angular v9.0.0-rc.1+398.sha-f72de51.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
