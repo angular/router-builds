18d8d1bde59e0d559f6e3701e20f81e5327c4fdd
Andrew Kushnir <akushnir@google.com>
2019-12-06 21:25:46 +0000

    b342a69bbc fix(ivy): do not invoke change detection for destroyed views (#34241)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+399.sha-718d7fe.with-local-changes
+ * @license Angular v9.0.0-rc.1+400.sha-b342a69.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
