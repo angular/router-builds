38350d8b64e4a947056f1ade496bd41a60dc5972
Andrew Kushnir <akushnir@google.com>
2019-12-09 20:05:01 +0000

    9fa2c398e7 fix(compiler): switch to modern diagnostic formatting (#34234)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+405.sha-2f3d41f.with-local-changes
+ * @license Angular v9.0.0-rc.1+408.sha-9fa2c39.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+405.sha-2f3d41f.with-local-changes
+ * @license Angular v9.0.0-rc.1+408.sha-9fa2c39.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+405.sha-2f3d41f.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+408.sha-9fa2c39.with-local-changes');
 
     /**
      * @license
