4abbc1c4602efeb9f3b9aa5e6deb7f3e8d2b7b71
Andrew Kushnir <akushnir@google.com>
2019-12-09 18:56:55 +0000

    d3069dbec6 refactor(ivy): minor cleanup in the listener instruction (#34255)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+402.sha-a1d0f1e.with-local-changes
+ * @license Angular v9.0.0-rc.1+404.sha-d3069db.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+402.sha-a1d0f1e.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+404.sha-d3069db.with-local-changes');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+402.sha-a1d0f1e.with-local-changes
+ * @license Angular v9.0.0-rc.1+404.sha-d3069db.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
