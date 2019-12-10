2be3d0d09dbe6d89cf76850c98b89c498ad3b64f
Andrew Kushnir <akushnir@google.com>
2019-12-10 17:37:09 +0000

    f95e2c7e7e docs: fix broken links (#34311)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+411.sha-ead169a.with-local-changes
+ * @license Angular v9.0.0-rc.1+413.sha-f95e2c7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
