fe358d437f46b2f96cf9addb84e19c9b878c4dce
Andrew Kushnir <akushnir@google.com>
2019-12-10 00:33:57 +0000

    ead169a402 fix(ngcc): fix undecorated child migration when `exportAs` is present (#34014)

--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+407.sha-13c2fad.with-local-changes
+ * @license Angular v9.0.0-rc.1+411.sha-ead169a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
