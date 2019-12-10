d20d2ecf8041637c0cea6c2c0114088295a5c473
Andrew Kushnir <akushnir@google.com>
2019-12-10 00:35:40 +0000

    24d1f9e87c fix(ngcc): fix undecorated child migration when `exportAs` is present (#34014)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+37.sha-60051eb.with-local-changes
+ * @license Angular v9.0.0-rc.5+40.sha-24d1f9e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.5+37.sha-60051eb.with-local-changes');
+const VERSION = new Version('9.0.0-rc.5+40.sha-24d1f9e.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+37.sha-60051eb.with-local-changes
+ * @license Angular v9.0.0-rc.5+40.sha-24d1f9e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
