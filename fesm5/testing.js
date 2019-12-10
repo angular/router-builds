5ec297cf08e671f3d7ed03849b154ddaedd02faf
Andrew Kushnir <akushnir@google.com>
2019-12-10 17:44:17 +0000

    2ecf9f3f2a refactor(docs-infra): give more descriptive variable name (#34284)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+415.sha-8683c03.with-local-changes
+ * @license Angular v9.0.0-rc.1+416.sha-2ecf9f3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
