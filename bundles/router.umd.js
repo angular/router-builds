0a00635c6c973bd72f6775e0f4a7175e032d07ef
Andrew Kushnir <akushnir@google.com>
2019-12-10 17:38:20 +0000

    419b153b17 fix(docs-infra): fix styling of embedded Table-of-Contents (#34312)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+412.sha-659356a.with-local-changes
+ * @license Angular v9.0.0-rc.1+414.sha-419b153.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+412.sha-659356a.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+414.sha-419b153.with-local-changes');
 
     /**
      * @license
