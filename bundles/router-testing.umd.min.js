bdc359a9fa783afccbb8922e8f7cb9bcdd8d3f8e
Andrew Kushnir <akushnir@google.com>
2019-12-10 17:37:44 +0000

    7c2bb37e4e refactor(docs-infra): give more descriptive variable name (#34284)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+40.sha-24d1f9e.with-local-changes
+ * @license Angular v9.0.0-rc.5+44.sha-7c2bb37.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
