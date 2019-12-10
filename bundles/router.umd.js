1db049d565254d1d2b417e71a355cf5340c77f18
Andrew Kushnir <akushnir@google.com>
2019-12-10 18:54:22 +0000

    bb52fb798c fix(ivy): handle SafeStyles in [style.prop] correctly (#34286)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+416.sha-2ecf9f3.with-local-changes
+ * @license Angular v9.0.0-rc.1+417.sha-bb52fb7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+416.sha-2ecf9f3.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+417.sha-bb52fb7.with-local-changes');
 
     /**
      * @license
