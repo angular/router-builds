dd1b32ecedefcbd9f4549c357aaa67c25eb6b24e
Andrew Kushnir <akushnir@google.com>
2019-12-10 22:28:22 +0000

    74bbe3e413 refactor(ivy): avoid type coercion in `saveNameToExportMap` function (#34335)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+47.sha-46f9201.with-local-changes
+ * @license Angular v9.0.0-rc.5+48.sha-74bbe3e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.5+47.sha-46f9201.with-local-changes');
+const VERSION = new Version('9.0.0-rc.5+48.sha-74bbe3e.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+47.sha-46f9201.with-local-changes
+ * @license Angular v9.0.0-rc.5+48.sha-74bbe3e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
