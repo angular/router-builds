d926b8a869321b448b203e689da6f118bd6597dd
Andrew Kushnir <akushnir@google.com>
2019-12-10 00:35:30 +0000

    ead169a402 fix(ngcc): fix undecorated child migration when `exportAs` is present (#34014)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+407.sha-13c2fad.with-local-changes
+ * @license Angular v9.0.0-rc.1+411.sha-ead169a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+407.sha-13c2fad.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.1+411.sha-ead169a.with-local-changes');
 
     /**
      * @license
