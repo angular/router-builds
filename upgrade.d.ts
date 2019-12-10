b7eb53c7e6d492b5bdf8af10eb7f1956b7e9e56f
Andrew Kushnir <akushnir@google.com>
2019-12-10 18:58:39 +0000

    c8447d21bd refactor(ivy): pull property read into a constant (#34290)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+417.sha-bb52fb7.with-local-changes
+ * @license Angular v9.0.0-rc.1+419.sha-c8447d2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
