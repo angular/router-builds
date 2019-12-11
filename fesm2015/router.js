67a8b36237c050d499029c744fb128c922d36f16
Kara Erickson <karakara@google.com>
2019-12-11 21:43:55 +0000

    81c75cf560 fix(ngcc): handle imports in dts files when processing UMD (#34356)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.6.with-local-changes
+ * @license Angular v9.0.0-rc.6+8.sha-81c75cf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.6.with-local-changes');
+const VERSION = new Version('9.0.0-rc.6+8.sha-81c75cf.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
