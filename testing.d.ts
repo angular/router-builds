2c5948c42cc7ace7bd6a565da95579d46bd8bb42
Andrew Kushnir <akushnir@google.com>
2019-12-11 19:28:09 +0000

    db4f508737 docs: release notes for the v9.0.0-rc.6 release

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+426.sha-f503832.with-local-changes
+ * @license Angular v9.0.0-rc.1+429.sha-db4f508.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
