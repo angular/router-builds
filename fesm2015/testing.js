dc3bc2f488ea7000be259cec2f2c22cc3c9d77f8
Andrew Kushnir <akushnir@google.com>
2019-12-11 00:53:13 +0000

    0984fbc748 fix(compiler-cli): allow declaration-only template type check members (#34296)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+421.sha-c9df9af.with-local-changes
+ * @license Angular v9.0.0-rc.1+423.sha-0984fbc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
