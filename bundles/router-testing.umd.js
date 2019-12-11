099b6e8558dc0d58bfba1c7bbe4b77f8b32ff4f6
Andrew Kushnir <akushnir@google.com>
2019-12-11 18:14:22 +0000

    08ea38f197 fix(bazel): improve performance of tsHost.writeFile() (#34331)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+425.sha-b72c7a8.with-local-changes
+ * @license Angular v9.0.0-rc.1+428.sha-08ea38f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
