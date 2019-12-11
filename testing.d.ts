c323a5d4a020ad9f53f39622a5a557e334fdf979
Miško Hevery <misko@hevery.com>
2019-12-11 23:07:25 +0000

    7cd465aa9b refactor(ivy): Add style reconciliation algorithm

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+420.sha-ed486b5.with-local-changes
+ * @license Angular v9.0.0-rc.1+421.sha-7cd465a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
