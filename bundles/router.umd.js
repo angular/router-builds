309f073c169152e97a2533e54762883c819e5948
Andrew Kushnir <akushnir@google.com>
2019-12-11 18:14:52 +0000

    f503832abe docs: fix headers in "Getting Started" code-snippets (#34321)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+428.sha-08ea38f.with-local-changes
+ * @license Angular v9.0.0-rc.1+426.sha-f503832.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+428.sha-08ea38f.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.1+426.sha-f503832.with-local-changes');
 
     /**
      * @license
