52c5aa793939d32997f0464506952478a993b352
Andrew Kushnir <akushnir@google.com>
2019-12-11 01:00:56 +0000

    b72c7a89a9 refactor(ivy): include generic type for `ModuleWithProviders` in .d.ts files (#34235)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+423.sha-0984fbc.with-local-changes
+ * @license Angular v9.0.0-rc.1+425.sha-b72c7a8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+423.sha-0984fbc.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-rc.1+425.sha-b72c7a8.with-local-changes');
 
     /**
      * @license
