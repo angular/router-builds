7453a821feabec019e41a1b7494b7d82900dc29a
Andrew Kushnir <akushnir@google.com>
2019-12-11 19:27:32 +0000

    184b83911c release: cut the v9.0.0-rc.6 release

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+61.sha-d7c459a.with-local-changes
+ * @license Angular v9.0.0-rc.6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
