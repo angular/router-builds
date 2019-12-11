6d9c6977b73be5c0c28eea3a0052cd80614b2f20
Andrew Kushnir <akushnir@google.com>
2019-12-11 18:13:14 +0000

    f503832abe docs: fix headers in "Getting Started" code-snippets (#34321)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+428.sha-08ea38f.with-local-changes
+ * @license Angular v9.0.0-rc.1+426.sha-f503832.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
