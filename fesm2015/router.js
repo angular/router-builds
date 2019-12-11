64c7e39e1f76efe6192242033b5c9df17a436d59
Andrew Kushnir <akushnir@google.com>
2019-12-11 18:16:08 +0000

    d7c459a81a fix(bazel): improve performance of tsHost.writeFile() (#34331)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+58.sha-ad82ed8.with-local-changes
+ * @license Angular v9.0.0-rc.5+61.sha-d7c459a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.5+58.sha-ad82ed8.with-local-changes');
+const VERSION = new Version('9.0.0-rc.5+61.sha-d7c459a.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
