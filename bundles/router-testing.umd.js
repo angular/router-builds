5516c84bde6987f21ec0e6ffeb1a450a7387c614
Kara Erickson <karakara@google.com>
2019-12-12 22:37:35 +0000

    af95dddd7e perf(ivy): eagerly parse the template twice during analysis (#34334)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+457.sha-ff0a914.with-local-changes
+ * @license Angular v9.0.0-rc.1+458.sha-af95ddd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
