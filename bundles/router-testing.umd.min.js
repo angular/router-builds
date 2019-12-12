792f0858b37e7ead06c2ca2d78c8b9c92fdfb3df
Kara Erickson <karakara@google.com>
2019-12-12 19:25:37 +0000

    25b210a269 style(common): remove unnecessary jsdoc type (#34369)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.6+15.sha-c26738d.with-local-changes
+ * @license Angular v9.0.0-rc.6+16.sha-25b210a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
