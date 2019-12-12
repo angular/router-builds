fc3b6b89535640feb961e347213e814abf932e12
Kara Erickson <karakara@google.com>
2019-12-12 21:43:35 +0000

    45b73c2a7d test(core): fix schematics calls to run synchronously (#34364)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.6+16.sha-25b210a.with-local-changes
+ * @license Angular v9.0.0-rc.6+22.sha-45b73c2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
