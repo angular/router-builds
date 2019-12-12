3acf7f07d595d683b2032fdb741af9f7031975d1
Kara Erickson <karakara@google.com>
2019-12-12 21:45:08 +0000

    6e7ca05efc build: remove unused webpack npm dependency (#34366)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+451.sha-3255d2b.with-local-changes
+ * @license Angular v9.0.0-rc.1+450.sha-6e7ca05.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
