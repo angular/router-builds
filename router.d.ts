f6884495ef8efcdf622bfc01d1822364b00174da
Kara Erickson <karakara@google.com>
2019-12-12 00:18:08 +0000

    ad987021ce Revert "build: update integration/bazel to rules_nodejs 0.42.1" (#34360)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+435.sha-6ce9403.with-local-changes
+ * @license Angular v9.0.0-rc.1+442.sha-ad98702.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
