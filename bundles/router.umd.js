68b293c9dbf8edacaf340cba0c44d09e865f4d97
Kara Erickson <karakara@google.com>
2019-12-12 21:43:59 +0000

    6ba5fdc208 fix(ivy): generate a better error for template var writes (#34339)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+450.sha-6e7ca05.with-local-changes
+ * @license Angular v9.0.0-rc.1+449.sha-6ba5fdc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+450.sha-6e7ca05.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+449.sha-6ba5fdc.with-local-changes');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+450.sha-6e7ca05.with-local-changes
+ * @license Angular v9.0.0-rc.1+449.sha-6ba5fdc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+450.sha-6e7ca05.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+449.sha-6ba5fdc.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
