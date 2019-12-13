a7f50b6c423a65220110d1194df5ba4051e2df59
Kara Erickson <karakara@google.com>
2019-12-13 19:12:30 +0000

    ea57587b7c docs(ivy): document breaking changes for DebugElement classes and attributes (#34328)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+460.sha-0fba79c.with-local-changes
+ * @license Angular v9.0.0-rc.1+463.sha-ea57587
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+460.sha-0fba79c.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+463.sha-ea57587');
 
 /**
  * @license
