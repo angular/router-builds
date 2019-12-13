0571f1c0c527bc0d575d59fd656c8d83cae97d52
Kara Erickson <karakara@google.com>
2019-12-13 19:13:46 +0000

    b405942b0c build: update to yarn@1.21.1 (#34384)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+463.sha-ea57587
+ * @license Angular v9.0.0-rc.1+461.sha-b405942
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+463.sha-ea57587');
+    var VERSION = new core.Version('9.0.0-rc.1+461.sha-b405942');
 
     /**
      * @license
