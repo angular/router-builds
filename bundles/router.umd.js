3abc311d1050b2472dbf5a81348f4d1a3d55c277
Kara Erickson <karakara@google.com>
2019-12-13 00:19:16 +0000

    0fba79cda2 refactor(ivy): don't include removed classes in the styling debug (#34375)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+459.sha-a9e3cbd.with-local-changes
+ * @license Angular v9.0.0-rc.1+460.sha-0fba79c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+459.sha-a9e3cbd.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+460.sha-0fba79c.with-local-changes');
 
     /**
      * @license
