8140f2df2edf070ec86e447f3032d804f4ca3970
Kara Erickson <karakara@google.com>
2019-12-13 22:03:22 +0000

    4d99dfe6da Revert "build: update to yarn@1.21.1" (#34402)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.6+31.sha-5e075ae.with-local-changes
+ * @license Angular v9.0.0-rc.6+35.sha-4d99dfe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
