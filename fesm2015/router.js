2a8302bd2425dce811449b41f33f755ed39e10f8
Kara Erickson <karakara@google.com>
2019-12-16 16:06:57 +0000

    04ab03664d ci: ensure saucelabs test output is human readable (#34277)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+476.sha-3bbd12d
+ * @license Angular v9.0.0-rc.1+478.sha-04ab036
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+476.sha-3bbd12d');
+const VERSION = new Version('9.0.0-rc.1+478.sha-04ab036');
 
 /**
  * @fileoverview added by tsickle
