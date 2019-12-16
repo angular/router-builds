795e39846609aaa906446499d74cd7b73a6774c0
Kara Erickson <karakara@google.com>
2019-12-16 19:09:08 +0000

    23cf11a788 ci: fix remote name in rebase instructions (#34432)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+482.sha-d0a04bf
+ * @license Angular v9.0.0-rc.1+483.sha-23cf11a
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+482.sha-d0a04bf');
+    var VERSION = new core.Version('9.0.0-rc.1+483.sha-23cf11a');
 
     /**
      * @license
