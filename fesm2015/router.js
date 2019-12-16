76b1fb628082a99f9777a0350d049c5e55d9f8f4
Kara Erickson <karakara@google.com>
2019-12-16 16:35:18 +0000

    ddc229b69b fix(ivy): record correct absolute source span for ngForOf expressions (#31813)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+480.sha-12444a8
+ * @license Angular v9.0.0-rc.1+481.sha-ddc229b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10209,7 +10209,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+480.sha-12444a8');
+const VERSION = new Version('9.0.0-rc.1+481.sha-ddc229b');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+480.sha-12444a8
+ * @license Angular v9.0.0-rc.1+481.sha-ddc229b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
