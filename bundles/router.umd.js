d8d8470c4f220a9a612aa4ff710d349134a4e555
Kara Erickson <karakara@google.com>
2019-12-17 23:02:14 +0000

    7938ff34b1 refactor(compiler-cli): avoid unnecessarily calling `getSourceFile()` twice in `PartialEvaluator` (#34441)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+496.sha-b637b93
+ * @license Angular v9.0.0-rc.1+497.sha-7938ff3
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+496.sha-b637b93');
+    var VERSION = new i0.Version('9.0.0-rc.1+497.sha-7938ff3');
 
     /**
      * @license
