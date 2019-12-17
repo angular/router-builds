9e9449f8662b981ac0c03355273318741a990509
Kara Erickson <karakara@google.com>
2019-12-17 21:51:15 +0000

    deeb87dc24 ci(docs-infra): fix failure in `aio_monitoring_stable` due to yarn version mismatch (#34451)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.6+61.sha-b1d4c58
+ * @license Angular v9.0.0-rc.6+67.sha-deeb87d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.6+61.sha-b1d4c58
+ * @license Angular v9.0.0-rc.6+67.sha-deeb87d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.6+61.sha-b1d4c58');
+const VERSION = new Version('9.0.0-rc.6+67.sha-deeb87d');
 
 /**
  * @fileoverview added by tsickle
