9354c2c1268431a2639a0f141ed60b4762168d64
Kara Erickson <karakara@google.com>
2019-12-18 19:53:54 +0000

    9264f43511 refactor(ngcc): remove private declaration aliases (#34254)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+502.sha-dfecca2
+ * @license Angular v9.0.0-rc.1+509.sha-9264f43
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+502.sha-dfecca2');
+    var VERSION = new core.Version('9.0.0-rc.1+509.sha-9264f43');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+502.sha-dfecca2
+ * @license Angular v9.0.0-rc.1+509.sha-9264f43
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+502.sha-dfecca2');
+const VERSION = new Version('9.0.0-rc.1+509.sha-9264f43');
 
 /**
  * @fileoverview added by tsickle
