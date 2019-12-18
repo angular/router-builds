16fc0e39dd4c309bb51cd6ff2f35cf2dc0af5610
Kara Erickson <karakara@google.com>
2019-12-18 18:43:28 +0000

    dfecca29da refactor: TypeScript 3.7 fixes. (#34372)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+499.sha-9d1175e
+ * @license Angular v9.0.0-rc.1+502.sha-dfecca2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+499.sha-9d1175e');
+    var VERSION = new core.Version('9.0.0-rc.1+502.sha-dfecca2');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+499.sha-9d1175e
+ * @license Angular v9.0.0-rc.1+502.sha-dfecca2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
