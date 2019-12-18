6762cfc311ecea9f4df5c1e34c0475c68dd036f3
Kara Erickson <karakara@google.com>
2019-12-18 17:35:28 +0000

    1eae7c81e9 refactor(language-service): Append missing AttrAst to AstPath (#34459)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+498.sha-3e20118
+ * @license Angular v9.0.0-rc.1+501.sha-1eae7c8
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+498.sha-3e20118');
+    var VERSION = new i0.Version('9.0.0-rc.1+501.sha-1eae7c8');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+498.sha-3e20118
+ * @license Angular v9.0.0-rc.1+501.sha-1eae7c8
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10209,7 +10209,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+498.sha-3e20118');
+const VERSION = new Version('9.0.0-rc.1+501.sha-1eae7c8');
 
 /**
  * @fileoverview added by tsickle
