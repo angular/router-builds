96bf36d054ac8bc9d654d7a826445505afb88893
Kara Erickson <karakara@google.com>
2019-12-18 18:46:05 +0000

    5ec4fb2f2a refactor: TypeScript 3.7 fixes. (#34372)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.6+72.sha-1ebb405
+ * @license Angular v9.0.0-rc.6+73.sha-5ec4fb2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.6+72.sha-1ebb405
+ * @license Angular v9.0.0-rc.6+73.sha-5ec4fb2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.6+72.sha-1ebb405');
+var VERSION = new Version('9.0.0-rc.6+73.sha-5ec4fb2');
 
 /**
  * @license
