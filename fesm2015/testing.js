1c45238a208f4cd02bbde7659411fd596c343b66
Kara Erickson <karakara@google.com>
2019-12-18 23:26:52 +0000

    cf37c003ff feat(ivy): error in ivy when inheriting a ctor from an undecorated base (#34460)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+513.sha-3959511
+ * @license Angular v9.0.0-rc.1+518.sha-cf37c00
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+513.sha-3959511
+ * @license Angular v9.0.0-rc.1+518.sha-cf37c00
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+513.sha-3959511');
+var VERSION = new Version('9.0.0-rc.1+518.sha-cf37c00');
 
 /**
  * @license
