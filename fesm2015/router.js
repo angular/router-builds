cc6a3d698309144d77ead8a06da1040763c175da
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-12-19 19:56:23 +0000

    2dffe65cfd fix(language-service): completions after "let x of |" in ngFor (#34473)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+519.sha-260a061
+ * @license Angular v9.0.0-rc.1+520.sha-2dffe65
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+519.sha-260a061');
+const VERSION = new Version('9.0.0-rc.1+520.sha-2dffe65');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+519.sha-260a061
+ * @license Angular v9.0.0-rc.1+520.sha-2dffe65
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+519.sha-260a061');
+var VERSION = new Version('9.0.0-rc.1+520.sha-2dffe65');
 
 /**
  * @license
