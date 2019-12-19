e25439b89600907cba8293e811c80ccdccc357bb
Kara Erickson <karakara@google.com>
2019-12-19 00:10:34 +0000

    6c33b7040d release: cut the v9.0.0-rc.7 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.6+89.sha-f563c7c
+ * @license Angular v9.0.0-rc.7
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.6+89.sha-f563c7c');
+    var VERSION = new core.Version('9.0.0-rc.7');
 
     /**
      * @license
