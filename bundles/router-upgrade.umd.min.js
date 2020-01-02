46dfaae5a36822d0826dd5d972e67442acb961a9
Miško Hevery <misko@hevery.com>
2020-01-02 17:23:42 +0000

    f004195663 refactor(ivy): add `insertTStylingBinding` to keep track of bindings.

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+422.sha-a719656.with-local-changes
+ * @license Angular v9.0.0-rc.1+524.sha-f004195
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+422.sha-a719656.with-local-changes
+ * @license Angular v9.0.0-rc.1+524.sha-f004195
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+422.sha-a719656.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+524.sha-f004195');
 
     /**
      * @license
