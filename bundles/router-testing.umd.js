4c5e7435d465f2d38f577000e27ae4994430c293
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:46:24 +0000

    eab7f9f101 docs: add a comma (#34426) PR Close #34426

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+528.sha-e14e43b
+ * @license Angular v9.0.0-rc.1+529.sha-eab7f9f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+528.sha-e14e43b
+ * @license Angular v9.0.0-rc.1+529.sha-eab7f9f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+528.sha-e14e43b');
+const VERSION = new Version('9.0.0-rc.1+529.sha-eab7f9f');
 
 /**
  * @fileoverview added by tsickle
