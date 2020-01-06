0971bf3716f7a6a629ff0d2cf6c9fb05dff63f61
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:32:32 +0000

    19944c2424 feat(compiler): record end of expression Token (#33549)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+522.sha-8410278
+ * @license Angular v9.0.0-rc.1+523.sha-19944c2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5716,7 +5716,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+522.sha-8410278');
+var VERSION = new Version('9.0.0-rc.1+523.sha-19944c2');
 
 /**
  * @license
