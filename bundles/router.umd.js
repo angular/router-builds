0a3f42e9fd279bba87d9a39e5c21b20aeeaac240
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:22:09 +0000

    8410278b6d docs: copy edit getting started step 4 (#32839)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+521.sha-9b9116c
+ * @license Angular v9.0.0-rc.1+522.sha-8410278
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+521.sha-9b9116c');
+    var VERSION = new core.Version('9.0.0-rc.1+522.sha-8410278');
 
     /**
      * @license
