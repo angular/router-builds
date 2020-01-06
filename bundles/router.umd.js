5c1c7afd6af7690f12384d3043e9be7ce4ae553d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:38:59 +0000

    1de49ba369 refactor(ivy): consistently translate types to `ts.TypeNode` (#34021)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+523.sha-19944c2
+ * @license Angular v9.0.0-rc.1+525.sha-1de49ba
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+523.sha-19944c2');
+    var VERSION = new core.Version('9.0.0-rc.1+525.sha-1de49ba');
 
     /**
      * @license
