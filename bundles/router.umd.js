190e5b6dc716ce989413885ea4092b1dd630630e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-07 19:23:34 +0000

    3053e022d3 docs(ivy): minor changes to version 9 guide (#34498)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+537.sha-b9b8920
+ * @license Angular v9.0.0-rc.1+538.sha-3053e02
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+537.sha-b9b8920');
+    var VERSION = new i0.Version('9.0.0-rc.1+538.sha-3053e02');
 
     /**
      * @license
