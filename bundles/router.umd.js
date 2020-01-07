6391233bf3f5ca9c98d096ba9a9565519266e2f6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-07 18:51:19 +0000

    effb92dfae fix(ivy): skip field inheritance if InheritDefinitionFeature is present on parent def (#34244)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+533.sha-5b864ed
+ * @license Angular v9.0.0-rc.1+534.sha-effb92d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+533.sha-5b864ed');
+    var VERSION = new i0.Version('9.0.0-rc.1+534.sha-effb92d');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+533.sha-5b864ed
+ * @license Angular v9.0.0-rc.1+534.sha-effb92d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10209,7 +10209,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+533.sha-5b864ed');
+const VERSION = new Version('9.0.0-rc.1+534.sha-effb92d');
 
 /**
  * @fileoverview added by tsickle
