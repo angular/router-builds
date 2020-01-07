e79523c2617019a05fccc736e969d8e04dae21c5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-07 19:00:09 +0000

    0254cba4b3 docs: Fix double 'as an' in description at line 2069 (#34373)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+534.sha-effb92d
+ * @license Angular v9.0.0-rc.1+535.sha-0254cba
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+534.sha-effb92d');
+    var VERSION = new core.Version('9.0.0-rc.1+535.sha-0254cba');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+534.sha-effb92d
+ * @license Angular v9.0.0-rc.1+535.sha-0254cba
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
