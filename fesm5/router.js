9197e11113607f526e1a49816c7da3350f5b4c8d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-07 19:23:01 +0000

    b9b8920227 docs: typo in Deployment section of getting started (#34490)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+535.sha-0254cba
+ * @license Angular v9.0.0-rc.1+537.sha-b9b8920
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5716,7 +5716,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+535.sha-0254cba');
+var VERSION = new Version('9.0.0-rc.1+537.sha-b9b8920');
 
 /**
  * @license
