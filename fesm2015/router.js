7f31c63462142e8aa50b98ded21d72951e56c954
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-08 23:04:39 +0000

    02e4921ac9 docs: release notes for the v9.0.0-rc.8 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+556.sha-39bf104
+ * @license Angular v9.0.0-rc.1+557.sha-02e4921
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+556.sha-39bf104');
+const VERSION = new Version('9.0.0-rc.1+557.sha-02e4921');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+556.sha-39bf104
+ * @license Angular v9.0.0-rc.1+557.sha-02e4921
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
