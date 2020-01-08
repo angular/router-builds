6b405f3616dd3644a83ed96c3091a5e94f1effbe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-08 21:43:22 +0000

    39bf104a03 build: update `firebase-tools` to the latest version (#34682)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+555.sha-b3398ee
+ * @license Angular v9.0.0-rc.1+556.sha-39bf104
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+555.sha-b3398ee');
+    var VERSION = new core.Version('9.0.0-rc.1+556.sha-39bf104');
 
     /**
      * @license
