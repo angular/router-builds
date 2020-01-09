68d912ce56c3a4466ae1cfd97e6ec3062f1a37f0
atscott <atscott@google.com>
2020-01-09 23:33:28 +0000

    9c5510b28e docs(ivy): document breaking change with <select> and binding order (#34709)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+583.sha-c3f14bb
+ * @license Angular v9.0.0-rc.1+585.sha-9c5510b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+583.sha-c3f14bb');
+const VERSION = new Version('9.0.0-rc.1+585.sha-9c5510b');
 
 /**
  * @fileoverview added by tsickle
