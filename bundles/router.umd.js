eb2d31855989e72486fe347f40cc6d4ca05159bb
atscott <atscott@google.com>
2020-01-09 23:35:01 +0000

    9c5510b28e docs(ivy): document breaking change with <select> and binding order (#34709)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+583.sha-c3f14bb
+ * @license Angular v9.0.0-rc.1+585.sha-9c5510b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+583.sha-c3f14bb');
+    var VERSION = new i0.Version('9.0.0-rc.1+585.sha-9c5510b');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+583.sha-c3f14bb
+ * @license Angular v9.0.0-rc.1+585.sha-9c5510b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
