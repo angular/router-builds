bb021fae9c9f6b38944b6ebb9d82028d5ee749f2
atscott <atscott@google.com>
2020-01-09 22:37:33 +0000

    c3f14bb7a5 feat(ivy): improve `ExpressionChangedAfterChecked` error message for attributes (#34505)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+575.sha-0083443
+ * @license Angular v9.0.0-rc.1+583.sha-c3f14bb
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+575.sha-0083443');
+    var VERSION = new core.Version('9.0.0-rc.1+583.sha-c3f14bb');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+575.sha-0083443
+ * @license Angular v9.0.0-rc.1+583.sha-c3f14bb
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+575.sha-0083443');
+var VERSION = new Version('9.0.0-rc.1+583.sha-c3f14bb');
 
 /**
  * @license
