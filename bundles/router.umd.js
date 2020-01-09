5724b19c2306f89f2e33c0ace956e1859017a9d4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-09 00:07:43 +0000

    8815ace418 fix(ngcc): insert definitions after statement (#34677)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+557.sha-02e4921
+ * @license Angular v9.0.0-rc.1+574.sha-8815ace
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+557.sha-02e4921');
+    var VERSION = new i0.Version('9.0.0-rc.1+574.sha-8815ace');
 
     /**
      * @license
