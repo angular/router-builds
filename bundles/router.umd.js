dc509fbf17917b59c022c50038a08b7d21d082ad
atscott <atscott@google.com>
2020-01-09 21:52:14 +0000

    0083443050 docs: add note on importing AppRoutingModule (#34710)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+574.sha-8815ace
+ * @license Angular v9.0.0-rc.1+575.sha-0083443
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+574.sha-8815ace');
+    var VERSION = new i0.Version('9.0.0-rc.1+575.sha-0083443');
 
     /**
      * @license
