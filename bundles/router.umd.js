a771334a0404527dc2ac1639ae615f827cb5a123
atscott <atscott@google.com>
2020-01-10 22:32:42 +0000

    e88d652f2a Revert "build: upgrade to rules_nodejs 1.0.0 (first stable release) (#34589)" (#34730)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+611.sha-475ffd6
+ * @license Angular v9.0.0-rc.1+629.sha-e88d652
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('0.0.0');
+    var VERSION = new i0.Version('9.0.0-rc.1+629.sha-e88d652');
 
     /**
      * @license
