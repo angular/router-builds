8933e775814457dff2e13c25c7941ff31a394fa4
atscott <atscott@google.com>
2020-01-10 00:30:55 +0000

    f312848c2e refactor(core): remove duplicate InjectableType declaration (#34679)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+29.sha-f7a2ed7
+ * @license Angular v9.0.0-rc.8+30.sha-f312848
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.8+29.sha-f7a2ed7');
+const VERSION = new Version('9.0.0-rc.8+30.sha-f312848');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+29.sha-f7a2ed7
+ * @license Angular v9.0.0-rc.8+30.sha-f312848
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
