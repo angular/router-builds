8811c2df2b05b67db868ef184d96ea6152203b64
atscott <atscott@google.com>
2020-01-10 22:33:46 +0000

    c3490f7092 Revert "build: upgrade to rules_nodejs 1.0.0 (first stable release) (#34589)" (#34730)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+55.sha-a5cdad5
+ * @license Angular v9.0.0-rc.8+73.sha-c3490f7
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('0.0.0');
+const VERSION = new Version('9.0.0-rc.8+73.sha-c3490f7');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+55.sha-a5cdad5
+ * @license Angular v9.0.0-rc.8+73.sha-c3490f7
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
