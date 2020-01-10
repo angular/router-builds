9208e63fef2d9f84728424787462266e22464799
atscott <atscott@google.com>
2020-01-10 23:04:47 +0000

    6d28a209e4 test(language-service): Remove test cases from parsing-cases.ts (#34716)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+629.sha-e88d652
+ * @license Angular v9.0.0-rc.1+631.sha-6d28a20
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+629.sha-e88d652');
+    var VERSION = new i0.Version('9.0.0-rc.1+631.sha-6d28a20');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+629.sha-e88d652
+ * @license Angular v9.0.0-rc.1+631.sha-6d28a20
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10209,7 +10209,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+629.sha-e88d652');
+const VERSION = new Version('9.0.0-rc.1+631.sha-6d28a20');
 
 /**
  * @fileoverview added by tsickle
