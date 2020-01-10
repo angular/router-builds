49c5cf6ac63ad263814d7ebb2fa31f48d7dc9b44
atscott <atscott@google.com>
2020-01-10 16:57:49 +0000

    9e0583045c test: add coverage to  //tools/ts-api-guardian/... on WindowsCI (#34589)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+586.sha-25eaff4
+ * @license Angular v9.0.0-rc.1+609.sha-9e05830
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+586.sha-25eaff4');
+    var VERSION = new i0.Version('0.0.0');
 
     /**
      * @license
