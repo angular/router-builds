246180147d96c9e89279c427188a039e76a0fbee
atscott <atscott@google.com>
2020-01-13 18:20:47 +0000

    58f10026c4 docs: add deprecation info about default currency code (#32584)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+644.sha-cfbb1a1
+ * @license Angular v9.0.0-rc.1+649.sha-58f1002
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+644.sha-cfbb1a1');
+const VERSION = new Version('9.0.0-rc.1+649.sha-58f1002');
 
 /**
  * @fileoverview added by tsickle
