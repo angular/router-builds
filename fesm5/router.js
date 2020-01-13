04ff67d36c0904fbdb44a1cd4d136c101369f8a3
atscott <atscott@google.com>
2020-01-13 15:45:17 +0000

    ebcd59ae4f docs: replace from class hero to interface with accordance with # 344022 (#34294) Fixes #21186

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+640.sha-55037d1
+ * @license Angular v9.0.0-rc.1+641.sha-ebcd59a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+640.sha-55037d1');
+var VERSION = new Version('9.0.0-rc.1+641.sha-ebcd59a');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+640.sha-55037d1
+ * @license Angular v9.0.0-rc.1+641.sha-ebcd59a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
