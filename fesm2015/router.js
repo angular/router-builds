37522e5663fea77d114a9d717fbdf47f439b3b56
atscott <atscott@google.com>
2020-01-13 18:09:46 +0000

    cfbb1a1e77 fix(ngcc): correctly detect dependencies in CommonJS (#34528)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+642.sha-8de3c20
+ * @license Angular v9.0.0-rc.1+644.sha-cfbb1a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+642.sha-8de3c20');
+const VERSION = new Version('9.0.0-rc.1+644.sha-cfbb1a1');
 
 /**
  * @fileoverview added by tsickle
