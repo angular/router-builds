9ca8fa3e877e65e0134c2fa5b2e2d6c4bf8ae863
atscott <atscott@google.com>
2020-01-13 18:04:19 +0000

    8de3c20e50 fix(ivy): do not reset view dirty state in check no changes mode (#34495)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+641.sha-ebcd59a
+ * @license Angular v9.0.0-rc.1+642.sha-8de3c20
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5716,7 +5716,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+641.sha-ebcd59a');
+var VERSION = new Version('9.0.0-rc.1+642.sha-8de3c20');
 
 /**
  * @license
