9654ce45796873d43c2e1eb7008378e6a219de70
atscott <atscott@google.com>
2020-01-13 16:08:09 +0000

    ebcd59ae4f docs: replace from class hero to interface with accordance with # 344022 (#34294) Fixes #21186

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+640.sha-55037d1
+ * @license Angular v9.0.0-rc.1+641.sha-ebcd59a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+640.sha-55037d1');
+    var VERSION = new i0.Version('9.0.0-rc.1+641.sha-ebcd59a');
 
     /**
      * @license
