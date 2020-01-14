40f6359a74393d30ed58fc1cea23a7fc328bc28d
atscott <atscott@google.com>
2020-01-14 23:48:54 +0000

    cb142b6df9 refactor(compiler): refactor and export Bazel host creation. (#34686)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+673.sha-c3c72f6
+ * @license Angular v9.0.0-rc.1+674.sha-cb142b6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+673.sha-c3c72f6');
+const VERSION = new Version('9.0.0-rc.1+674.sha-cb142b6');
 
 /**
  * @fileoverview added by tsickle
