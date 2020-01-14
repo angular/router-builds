53edfc56acdb11063e833a32e35c75381cd40254
atscott <atscott@google.com>
2020-01-14 23:15:33 +0000

    3113bb7ad2 fix(forms): accept number length in length validators (#32057)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+115.sha-e100bbc
+ * @license Angular v9.0.0-rc.8+116.sha-3113bb7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+115.sha-e100bbc
+ * @license Angular v9.0.0-rc.8+116.sha-3113bb7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.8+115.sha-e100bbc');
+const VERSION = new Version('9.0.0-rc.8+116.sha-3113bb7');
 
 /**
  * @fileoverview added by tsickle
