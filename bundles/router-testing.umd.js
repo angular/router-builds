ac713aa3f8db2794f0f1ad3b064987c1749e92e0
atscott <atscott@google.com>
2020-01-14 23:50:30 +0000

    cb142b6df9 refactor(compiler): refactor and export Bazel host creation. (#34686)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+673.sha-c3c72f6
+ * @license Angular v9.0.0-rc.1+674.sha-cb142b6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+673.sha-c3c72f6
+ * @license Angular v9.0.0-rc.1+674.sha-cb142b6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5716,7 +5716,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+673.sha-c3c72f6');
+var VERSION = new Version('9.0.0-rc.1+674.sha-cb142b6');
 
 /**
  * @license
