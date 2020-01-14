8387aa8049f71543413b1bcd6bf0c77317f7677b
atscott <atscott@google.com>
2020-01-14 23:14:45 +0000

    9ceee07d83 fix(forms): accept number length in length validators (#32057)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+671.sha-d863526
+ * @license Angular v9.0.0-rc.1+672.sha-9ceee07
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+671.sha-d863526');
+var VERSION = new Version('9.0.0-rc.1+672.sha-9ceee07');
 
 /**
  * @license
