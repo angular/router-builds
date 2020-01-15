1c2ae14a4fbf511d21049a658ac508bfe9aec79c
atscott <atscott@google.com>
2020-01-15 18:47:55 +0000

    3d5bcd5883 test(ngcc): update dependency host test description (#34695)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+689.sha-b7385a7
+ * @license Angular v9.0.0-rc.1+692.sha-3d5bcd5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+689.sha-b7385a7');
+    var VERSION = new i0.Version('9.0.0-rc.1+692.sha-3d5bcd5');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+689.sha-b7385a7
+ * @license Angular v9.0.0-rc.1+692.sha-3d5bcd5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
