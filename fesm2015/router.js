3448ee3465ab945234b48a3ff3158cc614fd4b47
atscott <atscott@google.com>
2020-01-15 18:04:11 +0000

    b7385a77ad docs: release notes for the v9.0.0-rc.9 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+688.sha-bf8ba89
+ * @license Angular v9.0.0-rc.1+689.sha-b7385a7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10209,7 +10209,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+688.sha-bf8ba89');
+const VERSION = new Version('9.0.0-rc.1+689.sha-b7385a7');
 
 /**
  * @fileoverview added by tsickle
