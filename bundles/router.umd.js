9a73bcf33d2313b5eaeebf6c9be488baccb67205
atscott <atscott@google.com>
2020-01-15 00:16:45 +0000

    d7ea389c84 feat(language-service): provide completion for $event variable (#34566)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+674.sha-cb142b6
+ * @license Angular v9.0.0-rc.1+675.sha-d7ea389
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+674.sha-cb142b6');
+    var VERSION = new i0.Version('9.0.0-rc.1+675.sha-d7ea389');
 
     /**
      * @license
