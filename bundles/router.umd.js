b507feb37a374ba0d0ee443570df38ce9d3fc8ad
Matias Niemelä <matias@yearofmoo.com>
2020-01-15 20:55:28 +0000

    436e7388c3 build: update to rules_nodejs 1.1.0 (#34736)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+692.sha-3d5bcd5
+ * @license Angular v9.0.0-rc.1+710.sha-436e738
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+692.sha-3d5bcd5');
+    var VERSION = new core.Version('0.0.0');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+692.sha-3d5bcd5
+ * @license Angular v0.0.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
