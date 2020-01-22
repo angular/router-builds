06e978746576b93bb47f5c0ac003cb55fb427ae5
Matias Niemelä <matias@yearofmoo.com>
2020-01-22 20:10:17 +0000

    e7cf37d99f docs: clarify where the `async` utility function is coming from (#34876) inside the `beforeEach` for out testing guide.

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+740.sha-0d83095
+ * @license Angular v9.0.0-rc.1+746.sha-e7cf37d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+740.sha-0d83095');
+    var VERSION = new i0.Version('9.0.0-rc.1+746.sha-e7cf37d');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+740.sha-0d83095
+ * @license Angular v9.0.0-rc.1+746.sha-e7cf37d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5716,7 +5716,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+740.sha-0d83095');
+var VERSION = new Version('9.0.0-rc.1+746.sha-e7cf37d');
 
 /**
  * @license
