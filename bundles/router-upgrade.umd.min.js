81b5616aada6886a60138c01478aba51eb4fd6e7
Andrew Kushnir <akushnir@google.com>
2020-01-22 22:12:26 +0000

    e1fc44f5d6 build: remove @angular/http from modules (#34892)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+749.sha-13ce375
+ * @license Angular v9.0.0-rc.1+750.sha-e1fc44f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+749.sha-13ce375
+ * @license Angular v9.0.0-rc.1+750.sha-e1fc44f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+749.sha-13ce375');
+    var VERSION = new i0.Version('9.0.0-rc.1+750.sha-e1fc44f');
 
     /**
      * @license
