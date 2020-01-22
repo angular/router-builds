87d242770445df41f69add8a5df71ef7707638ed
Andrew Kushnir <akushnir@google.com>
2020-01-22 23:59:10 +0000

    d15cf60c49 feat(compiler-cli): require node 10 as runtime engine (#34722)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+755.sha-92c411f
+ * @license Angular v9.0.0-rc.1+760.sha-d15cf60
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+755.sha-92c411f');
+    var VERSION = new core.Version('9.0.0-rc.1+760.sha-d15cf60');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+755.sha-92c411f
+ * @license Angular v9.0.0-rc.1+760.sha-d15cf60
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+755.sha-92c411f');
+var VERSION = new Version('9.0.0-rc.1+760.sha-d15cf60');
 
 /**
  * @license
