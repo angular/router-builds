473403f0a843b9a948f191d2c4c92c3cb165c5fe
Matias Niemelä <matias@yearofmoo.com>
2020-01-22 21:20:19 +0000

    32489c7426 revert: refactor(ivy): remove styleSanitizer instruction in favor of an inline param (#34480) (#34910)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+747.sha-61ad50d
+ * @license Angular v9.0.0-rc.1+748.sha-32489c7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+747.sha-61ad50d
+ * @license Angular v9.0.0-rc.1+748.sha-32489c7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+747.sha-61ad50d');
+    var VERSION = new i0.Version('9.0.0-rc.1+748.sha-32489c7');
 
     /**
      * @license
