99913ce85c8f5414e517b5de8c309e2bdd8999ff
Andrew Kushnir <akushnir@google.com>
2020-01-22 23:19:19 +0000

    92c411f86d docs: update payload size limits for angular.io application (#34913)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+750.sha-e1fc44f
+ * @license Angular v9.0.0-rc.1+755.sha-92c411f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+750.sha-e1fc44f');
+    var VERSION = new core.Version('9.0.0-rc.1+755.sha-92c411f');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+750.sha-e1fc44f
+ * @license Angular v9.0.0-rc.1+755.sha-92c411f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+750.sha-e1fc44f');
+const VERSION = new Version('9.0.0-rc.1+755.sha-92c411f');
 
 /**
  * @fileoverview added by tsickle
