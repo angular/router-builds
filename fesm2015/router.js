2c3fbe6ebdcc1515d8a8c5b750bbe94f2431f8f4
Andrew Kushnir <akushnir@google.com>
2020-01-23 19:08:00 +0000

    0c8d085666 fix(ivy): use any for generic context checks when !strictTemplates (#34649)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+767.sha-6b468f9
+ * @license Angular v9.0.0-rc.1+773.sha-0c8d085
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+767.sha-6b468f9');
+const VERSION = new Version('9.0.0-rc.1+773.sha-0c8d085');
 
 /**
  * @fileoverview added by tsickle
