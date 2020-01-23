4b1a53e9ed4791ee36c60f7e53d06bf61aea04e6
Andrew Kushnir <akushnir@google.com>
2020-01-23 20:01:28 +0000

    eb4b7d2d8b build: vendor yarn using the .yarnrc yarn-path value (#34902)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+775.sha-970b22f
+ * @license Angular v9.0.0-rc.1+776.sha-eb4b7d2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+775.sha-970b22f');
+var VERSION = new Version('9.0.0-rc.1+776.sha-eb4b7d2');
 
 /**
  * @license
