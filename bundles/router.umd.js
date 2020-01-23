17c0f2f951848e663bed5e1a1ea3716e190ee293
Andrew Kushnir <akushnir@google.com>
2020-01-23 22:24:07 +0000

    fa8d324929 docs: value changes emits on additional events documented (#34497)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+782.sha-5aa0507
+ * @license Angular v9.0.0-rc.1+783.sha-fa8d324
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+782.sha-5aa0507');
+    var VERSION = new i0.Version('9.0.0-rc.1+783.sha-fa8d324');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+782.sha-5aa0507
+ * @license Angular v9.0.0-rc.1+783.sha-fa8d324
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
