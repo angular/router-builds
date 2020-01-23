1c4b316b30faa5bdf63f313da7759e05b0d1f75e
Andrew Kushnir <akushnir@google.com>
2020-01-23 18:35:46 +0000

    1ea04ffc05 feat(language-service): support multiple symbol definitions (#34782)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+762.sha-48f8ca5
+ * @license Angular v9.0.0-rc.1+763.sha-1ea04ff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+762.sha-48f8ca5');
+    var VERSION = new i0.Version('9.0.0-rc.1+763.sha-1ea04ff');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+762.sha-48f8ca5
+ * @license Angular v9.0.0-rc.1+763.sha-1ea04ff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
