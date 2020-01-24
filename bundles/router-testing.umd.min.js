0db91408366dda4d1a5c64d4d0b68c9ca99fd39d
Andrew Kushnir <akushnir@google.com>
2020-01-24 20:46:41 +0000

    ef95da6d3b fix(ivy): don't detect changes on detached child embedded views (#34846)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+792.sha-ed24100
+ * @license Angular v9.0.0-rc.1+793.sha-ef95da6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+792.sha-ed24100
+ * @license Angular v9.0.0-rc.1+793.sha-ef95da6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+792.sha-ed24100');
+    var VERSION = new i0.Version('9.0.0-rc.1+793.sha-ef95da6');
 
     /**
      * @license
