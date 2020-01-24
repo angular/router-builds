4ee2d47f750c5897f08f71c3c19413912f8698c5
George Kalpakas <kalpakas.g@gmail.com>
2020-01-24 20:06:38 +0000

    e35d9eaa7d build: vendor yarn using the .yarnrc yarn-path value

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.13+5.sha-03b3a38.with-local-changes
+ * @license Angular v8.2.14+1.sha-e35d9ea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
