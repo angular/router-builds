3f7f1950c6983b78a306bb123672d7c6f4d04ceb
Andrew Kushnir <akushnir@google.com>
2020-01-24 17:25:34 +0000

    31e9dda2c8 perf(ivy): add more styling use-cases to benchmarks (#34923)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+789.sha-0223aa6
+ * @license Angular v9.0.0-rc.1+790.sha-31e9dda
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+789.sha-0223aa6');
+    var VERSION = new i0.Version('9.0.0-rc.1+790.sha-31e9dda');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+789.sha-0223aa6
+ * @license Angular v9.0.0-rc.1+790.sha-31e9dda
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
