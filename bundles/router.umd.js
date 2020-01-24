7c10a3f9bd47124f7bf7835baf73cb5a33939eb6
Andrew Kushnir <akushnir@google.com>
2020-01-24 19:53:23 +0000

    ed24100976 ci(docs-infra): use the correct version of yarn in `aio_monitoring_stable` (#34954)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+791.sha-24b2f1d
+ * @license Angular v9.0.0-rc.1+792.sha-ed24100
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+791.sha-24b2f1d');
+    var VERSION = new core.Version('9.0.0-rc.1+792.sha-ed24100');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+791.sha-24b2f1d
+ * @license Angular v9.0.0-rc.1+792.sha-ed24100
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+791.sha-24b2f1d');
+var VERSION = new Version('9.0.0-rc.1+792.sha-ed24100');
 
 /**
  * @license
