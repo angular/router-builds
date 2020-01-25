617bf6629299f8b2a7078251d470ac1b23e7d707
Andrew Kushnir <akushnir@google.com>
2020-01-25 00:05:29 +0000

    8926f764a5 docs: release notes for the v9.0.0-rc.11 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+806.sha-9bd9590
+ * @license Angular v9.0.0-rc.1+807.sha-8926f76
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+806.sha-9bd9590');
+    var VERSION = new core.Version('9.0.0-rc.1+807.sha-8926f76');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+806.sha-9bd9590
+ * @license Angular v9.0.0-rc.1+807.sha-8926f76
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
