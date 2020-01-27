195aa79f3ec09f92bcbaac56621f999b98b5f4d4
Andrew Kushnir <akushnir@google.com>
2020-01-27 17:48:20 +0000

    724ce8c22b build(docs-infra): update to @angular/material 9.0.0-rc.8 (#34966)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+814.sha-3d4b143
+ * @license Angular v9.0.0-rc.1+817.sha-724ce8c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+814.sha-3d4b143');
+const VERSION = new Version('9.0.0-rc.1+817.sha-724ce8c');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+814.sha-3d4b143
+ * @license Angular v9.0.0-rc.1+817.sha-724ce8c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+814.sha-3d4b143');
+var VERSION = new Version('9.0.0-rc.1+817.sha-724ce8c');
 
 /**
  * @license
