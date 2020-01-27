7779e767e64a7af90a763741f09d21703de928a5
Andrew Kushnir <akushnir@google.com>
2020-01-27 19:09:48 +0000

    09869af90f fix(language-service): remove repeated symbol definitions for structural directive (#34847)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+819.sha-b3bd6ca
+ * @license Angular v9.0.0-rc.1+823.sha-09869af
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+819.sha-b3bd6ca');
+const VERSION = new Version('9.0.0-rc.1+823.sha-09869af');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+819.sha-b3bd6ca
+ * @license Angular v9.0.0-rc.1+823.sha-09869af
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+819.sha-b3bd6ca');
+var VERSION = new Version('9.0.0-rc.1+823.sha-09869af');
 
 /**
  * @license
