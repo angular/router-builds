5016472711eef903fd23ad07ff06261c9cf584ed
Andrew Kushnir <akushnir@google.com>
2020-01-27 20:59:31 +0000

    7069a83727 fix(localize): re-enable filename in code-frame error messages (#34994)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+827.sha-6e5cfd2
+ * @license Angular v9.0.0-rc.1+828.sha-7069a83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+827.sha-6e5cfd2');
+    var VERSION = new core.Version('9.0.0-rc.1+828.sha-7069a83');
 
     /**
      * @license
