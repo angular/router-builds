b383c1dcf3260821a204edb5ab429d17e25858ef
Andrew Kushnir <akushnir@google.com>
2020-01-27 18:02:53 +0000

    b3bd6ca925 build: update API Golden files after node 12 update (#34955)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+815.sha-c349cb2
+ * @license Angular v9.0.0-rc.1+819.sha-b3bd6ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+815.sha-c349cb2
+ * @license Angular v9.0.0-rc.1+819.sha-b3bd6ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+815.sha-c349cb2');
+    var VERSION = new i0.Version('9.0.0-rc.1+819.sha-b3bd6ca');
 
     /**
      * @license
