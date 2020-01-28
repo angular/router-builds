ab4c54b7e19677e1482c4270b060a4b172999906
Joey Perrott <josephperrott.github@gmail.com>
2020-01-28 17:56:38 +0000

    d15d6b7a63 ci: fix pullapprove_conditions (#35011)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+834.sha-296dc06
+ * @license Angular v9.0.0-rc.1+835.sha-d15d6b7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+834.sha-296dc06');
+    var VERSION = new core.Version('9.0.0-rc.1+835.sha-d15d6b7');
 
     /**
      * @license
