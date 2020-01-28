4c1b50531f39ecc8c6f183e66e9d969d868171df
Andrew Kushnir <akushnir@google.com>
2020-01-28 21:26:59 +0000

    a5a598104f docs: change p to pointer for better understanding (#34927)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+836.sha-1cee9b3
+ * @license Angular v9.0.0-rc.1+837.sha-a5a5981
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+836.sha-1cee9b3');
+var VERSION = new Version('9.0.0-rc.1+837.sha-a5a5981');
 
 /**
  * @license
