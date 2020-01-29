354abbcef0e8b55319c8eb7f58df30eb6237fd13
Andrew Kushnir <akushnir@google.com>
2020-01-29 00:10:43 +0000

    bd61cd0267 ci: filter out bad commit from the commit message check to green up master (#35017)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+838.sha-dff4e1e
+ * @license Angular v9.0.0-rc.1+840.sha-bd61cd0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+838.sha-dff4e1e');
+    var VERSION = new core.Version('9.0.0-rc.1+840.sha-bd61cd0');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+838.sha-dff4e1e
+ * @license Angular v9.0.0-rc.1+840.sha-bd61cd0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
