68010854dc87d21745c39dbcae7dd1accd151c58
Andrew Kushnir <akushnir@google.com>
2020-01-29 17:46:09 +0000

    fb852f7252 build(localize): sync up babel dependencies (#35008)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+847.sha-53c4c8d
+ * @license Angular v9.0.0-rc.1+848.sha-fb852f7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+847.sha-53c4c8d');
+var VERSION = new Version('9.0.0-rc.1+848.sha-fb852f7');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+847.sha-53c4c8d
+ * @license Angular v9.0.0-rc.1+848.sha-fb852f7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
