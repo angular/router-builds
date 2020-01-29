81c204b18b9accab2b5f4e5ad093b0ee61c14244
Andrew Kushnir <akushnir@google.com>
2020-01-29 22:37:29 +0000

    d5205a01cb build: update to @angular/cli@9.0.0-rc.11 (#35042)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+860.sha-7ca7437
+ * @license Angular v9.0.0-rc.1+858.sha-d5205a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+860.sha-7ca7437');
+    var VERSION = new i0.Version('9.0.0-rc.1+858.sha-d5205a0');
 
     /**
      * @license
