ae183c119418abe2af2b79f88b710f3d1d073ed4
Andrew Kushnir <akushnir@google.com>
2020-01-29 17:49:37 +0000

    d43187f7ef ci(docs-infra): ignore node version in `aio_monitoring_stable` CI job (for real) (#35033)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+848.sha-fb852f7
+ * @license Angular v9.0.0-rc.1+849.sha-d43187f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+848.sha-fb852f7');
+    var VERSION = new core.Version('9.0.0-rc.1+849.sha-d43187f');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+848.sha-fb852f7
+ * @license Angular v9.0.0-rc.1+849.sha-d43187f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+848.sha-fb852f7');
+var VERSION = new Version('9.0.0-rc.1+849.sha-d43187f');
 
 /**
  * @license
