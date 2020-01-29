f6bf0df028af202cff559d3b203192458167de49
Andrew Kushnir <akushnir@google.com>
2020-01-29 19:14:14 +0000

    8fd2a97c44 docs: ng-conf 2020 date change (#35019)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+849.sha-d43187f
+ * @license Angular v9.0.0-rc.1+850.sha-8fd2a97
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+849.sha-d43187f');
+    var VERSION = new i0.Version('9.0.0-rc.1+850.sha-8fd2a97');
 
     /**
      * @license
