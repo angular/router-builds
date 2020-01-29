29e2c5eea5d63ce1bcb74c7934189da3de2876aa
Andrew Kushnir <akushnir@google.com>
2020-01-29 22:34:48 +0000

    7ca74375cb ci: add missing codeowners for `aio/` files (#34988)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+857.sha-a892054
+ * @license Angular v9.0.0-rc.1+860.sha-7ca7437
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+857.sha-a892054');
+    var VERSION = new core.Version('9.0.0-rc.1+860.sha-7ca7437');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+857.sha-a892054
+ * @license Angular v9.0.0-rc.1+860.sha-7ca7437
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+857.sha-a892054');
+var VERSION = new Version('9.0.0-rc.1+860.sha-7ca7437');
 
 /**
  * @license
