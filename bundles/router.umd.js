69d0b877ccd1ecef732b3e11cf264b6a56fa4415
Andrew Kushnir <akushnir@google.com>
2020-01-29 00:18:48 +0000

    669df70da5 fix(ivy): ensure `DebugNode`/`DebugElement` are tree-shakeable in Ivy (#35003)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+840.sha-bd61cd0
+ * @license Angular v9.0.0-rc.1+844.sha-669df70
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+840.sha-bd61cd0');
+    var VERSION = new i0.Version('9.0.0-rc.1+844.sha-669df70');
 
     /**
      * @license
