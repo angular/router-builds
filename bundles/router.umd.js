06f08746c69492098c64ee660f30b9d1a5d37184
Andrew Kushnir <akushnir@google.com>
2020-01-29 17:43:27 +0000

    53c4c8de8f test: ensure global options for benchmark tests can be set in bazel (#34753)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+844.sha-669df70
+ * @license Angular v9.0.0-rc.1+847.sha-53c4c8d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+844.sha-669df70');
+    var VERSION = new i0.Version('9.0.0-rc.1+847.sha-53c4c8d');
 
     /**
      * @license
