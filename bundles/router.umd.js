2b8f33ff3aa89f7894ba00a482ef91c363949c64
Andrew Kushnir <akushnir@google.com>
2020-01-29 22:36:25 +0000

    95383ee6a9 ci: don't run benchmark_tests on remote executors (#34996)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+858.sha-d5205a0
+ * @license Angular v9.0.0-rc.1+861.sha-95383ee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+858.sha-d5205a0');
+    var VERSION = new core.Version('9.0.0-rc.1+861.sha-95383ee');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+858.sha-d5205a0
+ * @license Angular v9.0.0-rc.1+861.sha-95383ee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+858.sha-d5205a0');
+const VERSION = new Version('9.0.0-rc.1+861.sha-95383ee');
 
 /**
  * @fileoverview added by tsickle
