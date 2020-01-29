656d126c74e850829d217385ac5a2dee5d75cf85
Andrew Kushnir <akushnir@google.com>
2020-01-29 21:33:23 +0000

    a892054448 docs: remove named node version (#35014)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+855.sha-519e9e1
+ * @license Angular v9.0.0-rc.1+857.sha-a892054
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10208,7 +10208,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+855.sha-519e9e1');
+const VERSION = new Version('9.0.0-rc.1+857.sha-a892054');
 
 /**
  * @fileoverview added by tsickle
