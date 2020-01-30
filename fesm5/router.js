a5e8dc2c64275483a1b360dc64920d40bbe38920
Andrew Kushnir <akushnir@google.com>
2020-01-30 00:09:28 +0000

    17650523a8 build: remove unneeded style code for ngClass and ngStyle (#34998)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+864.sha-ee8b8f5
+ * @license Angular v9.0.0-rc.1+862.sha-1765052
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+864.sha-ee8b8f5');
+var VERSION = new Version('9.0.0-rc.1+862.sha-1765052');
 
 /**
  * @license
