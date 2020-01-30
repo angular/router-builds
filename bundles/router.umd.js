818ac1a00a694a0faaa44a8ee0265ed806000e87
Andrew Kushnir <akushnir@google.com>
2020-01-30 00:05:31 +0000

    ee8b8f52aa feat(ivy): Change static priority resolution to be same level as directive it belongs to (#34938)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+859.sha-a3de110
+ * @license Angular v9.0.0-rc.1+864.sha-ee8b8f5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+859.sha-a3de110');
+    var VERSION = new i0.Version('9.0.0-rc.1+864.sha-ee8b8f5');
 
     /**
      * @license
