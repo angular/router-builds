a7b86e15a5f85efeaea43adcaf6c703ebfbfd2da
Andrew Kushnir <akushnir@google.com>
2020-01-30 21:10:01 +0000

    9f5b490800 Revert "refactor(compiler): refactor and export Bazel host creation. (#34686)" (#35063)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+869.sha-cd9ae66
+ * @license Angular v9.0.0-rc.1+870.sha-9f5b490
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+869.sha-cd9ae66');
+    var VERSION = new i0.Version('9.0.0-rc.1+870.sha-9f5b490');
 
     /**
      * @license
