76847c0274ee69aafc12b9f9aea9ab5c8757b15a
Andrew Kushnir <akushnir@google.com>
2020-01-30 22:44:50 +0000

    a25cb2d1e0 docs: release notes for the v9.0.0-rc.12 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+872.sha-374a8ec
+ * @license Angular v9.0.0-rc.1+871.sha-a25cb2d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+872.sha-374a8ec');
+const VERSION = new Version('9.0.0-rc.1+871.sha-a25cb2d');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+872.sha-374a8ec
+ * @license Angular v9.0.0-rc.1+871.sha-a25cb2d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
