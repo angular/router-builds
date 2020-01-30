6ef35e88a07077d05fa37725d95ee85604b564ef
Andrew Kushnir <akushnir@google.com>
2020-01-30 01:37:33 +0000

    c1cf46c5c3 refactor(ivy): clean of  #34804 from previous merge (#35022)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+862.sha-1765052
+ * @license Angular v9.0.0-rc.1+865.sha-c1cf46c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+862.sha-1765052');
+const VERSION = new Version('9.0.0-rc.1+865.sha-c1cf46c');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+862.sha-1765052
+ * @license Angular v9.0.0-rc.1+865.sha-c1cf46c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+862.sha-1765052');
+var VERSION = new Version('9.0.0-rc.1+865.sha-c1cf46c');
 
 /**
  * @license
