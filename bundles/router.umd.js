42b78018ae2206dd43236fd79ee17a10f44d4cc0
Andrew Kushnir <akushnir@google.com>
2020-01-30 22:21:54 +0000

    3bdf1164ce ci: fix StephenFluin's casing in entries in PullApprove (#35062)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+67.sha-36dfca9
+ * @license Angular v9.0.0-rc.12+1.sha-3bdf116
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.11+67.sha-36dfca9');
+    var VERSION = new core.Version('9.0.0-rc.12+1.sha-3bdf116');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+67.sha-36dfca9
+ * @license Angular v9.0.0-rc.12+1.sha-3bdf116
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
