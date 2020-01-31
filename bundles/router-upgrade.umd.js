3a98e36f774ca3cefd3e96afb4a44bb74eb1a989
Misko Hevery <misko@hevery.com>
2020-01-31 22:55:14 +0000

    6d11a81994 fix(compiler-cli): add `sass` as a valid css preprocessor extension (#35052)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+887.sha-58b29f1
+ * @license Angular v9.0.0-rc.1+888.sha-6d11a81
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+887.sha-58b29f1
+ * @license Angular v9.0.0-rc.1+888.sha-6d11a81
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+887.sha-58b29f1');
+    var VERSION = new core.Version('9.0.0-rc.1+888.sha-6d11a81');
 
     /**
      * @license
