1ba70bd2a083c804bee43fdba51399310dc7d454
Misko Hevery <misko@hevery.com>
2020-01-31 19:36:44 +0000

    68f51e0adc ci: update pullapprove.yml to cover all files in the repo (#35072)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+871.sha-a25cb2d
+ * @license Angular v9.0.0-rc.1+873.sha-68f51e0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+871.sha-a25cb2d');
+    var VERSION = new i0.Version('9.0.0-rc.1+873.sha-68f51e0');
 
     /**
      * @license
