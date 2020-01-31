fea2fdf3d797664878ebc8157aea0c33aebcf63b
Misko Hevery <misko@hevery.com>
2020-01-31 19:38:44 +0000

    69b52903f0 build: update to @bazel/protractor@1.2.0 (#35068)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+873.sha-68f51e0
+ * @license Angular v9.0.0-rc.1+874.sha-69b5290
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+873.sha-68f51e0');
+    var VERSION = new core.Version('9.0.0-rc.1+874.sha-69b5290');
 
     /**
      * @license
