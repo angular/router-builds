93b08493df543b595fad6bb0ed1833f1fb202a0d
Misko Hevery <misko@hevery.com>
2020-01-31 21:11:38 +0000

    e672b1f2ac feat(docs-infra): change navigation in resources page (#34756)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+877.sha-6b710f7
+ * @license Angular v9.0.0-rc.1+880.sha-e672b1f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+877.sha-6b710f7');
+const VERSION = new Version('9.0.0-rc.1+880.sha-e672b1f');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+877.sha-6b710f7
+ * @license Angular v9.0.0-rc.1+880.sha-e672b1f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+877.sha-6b710f7');
+var VERSION = new Version('9.0.0-rc.1+880.sha-e672b1f');
 
 /**
  * @license
