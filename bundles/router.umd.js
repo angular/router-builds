a95ca0aa440b7a93cdb9d1822bd77daabaf4bee4
Misko Hevery <misko@hevery.com>
2020-01-31 19:47:36 +0000

    c070037357 refactor(compiler): rename diagnostics/src/code.ts to diagnostics/src/error_code.ts (#35067)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+874.sha-69b5290
+ * @license Angular v9.0.0-rc.1+876.sha-c070037
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+874.sha-69b5290');
+    var VERSION = new core.Version('9.0.0-rc.1+876.sha-c070037');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+874.sha-69b5290
+ * @license Angular v9.0.0-rc.1+876.sha-c070037
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
