c55a45d9fd96d7ab40cd4ca005b024d2952d9c97
Misko Hevery <misko@hevery.com>
2020-01-31 22:56:51 +0000

    6d11a81994 fix(compiler-cli): add `sass` as a valid css preprocessor extension (#35052)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+887.sha-58b29f1
+ * @license Angular v9.0.0-rc.1+888.sha-6d11a81
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+887.sha-58b29f1');
+var VERSION = new Version('9.0.0-rc.1+888.sha-6d11a81');
 
 /**
  * @license
