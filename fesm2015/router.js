881cf68f45f68e80b17bbf36f256fd546962275e
Miško Hevery <misko@hevery.com>
2020-02-03 17:01:10 +0000

    a3c7ab99b7 feat: performance improvement for eventListeners (#34613)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+896.sha-4d36b2f
+ * @license Angular v9.0.0-rc.1+897.sha-a3c7ab9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10208,7 +10208,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+896.sha-4d36b2f');
+const VERSION = new Version('9.0.0-rc.1+897.sha-a3c7ab9');
 
 /**
  * @fileoverview added by tsickle
