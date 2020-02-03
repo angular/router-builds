8410c1d76846747b1626a286a4b3e69c96d7b50a
Miško Hevery <misko@hevery.com>
2020-02-03 22:45:53 +0000

    523c785e8f fix(ngcc): correctly invalidate cache when moving/removing files/directories (#35106)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+905.sha-3c30474
+ * @license Angular v9.0.0-rc.1+906.sha-523c785
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+905.sha-3c30474');
+    var VERSION = new core.Version('9.0.0-rc.1+906.sha-523c785');
 
     /**
      * @license
