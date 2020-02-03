67c130fa6583d80a19eb15ddea3952868d5c5831
Miško Hevery <misko@hevery.com>
2020-02-03 17:24:47 +0000

    a8609ba0ad refactor(language-service): dedupe diagnostics using ts utility function (#35086)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+900.sha-01308e4
+ * @license Angular v9.0.0-rc.1+902.sha-a8609ba
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10208,7 +10208,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+900.sha-01308e4');
+const VERSION = new Version('9.0.0-rc.1+902.sha-a8609ba');
 
 /**
  * @fileoverview added by tsickle
