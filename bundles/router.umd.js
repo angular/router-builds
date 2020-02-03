341acbcfb17c3afa25742be3eb9eb87f4651ebcb
Miško Hevery <misko@hevery.com>
2020-02-03 22:28:14 +0000

    3c30474417 docs: update zone-bluebird patch document for angular (#34536)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+904.sha-9601b5d
+ * @license Angular v9.0.0-rc.1+905.sha-3c30474
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+904.sha-9601b5d');
+    var VERSION = new i0.Version('9.0.0-rc.1+905.sha-3c30474');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+904.sha-9601b5d
+ * @license Angular v9.0.0-rc.1+905.sha-3c30474
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
