9a6c4ef17823f5437d33de4fd625b244ebc4e79c
Miško Hevery <misko@hevery.com>
2020-02-03 17:19:21 +0000

    01308e4c7c refactor(ivy): Remove `TNode.directives` in favor of `TData` (#35050)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+899.sha-65dbd50
+ * @license Angular v9.0.0-rc.1+900.sha-01308e4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+899.sha-65dbd50');
+const VERSION = new Version('9.0.0-rc.1+900.sha-01308e4');
 
 /**
  * @fileoverview added by tsickle
