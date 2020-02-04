c26497b1c0138c138593bb8655b483978addcb9e
Miško Hevery <misko@hevery.com>
2020-02-04 19:08:32 +0000

    6ddf5508db fix(ivy): support emitting a reference to interface declarations (#34849)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+918.sha-991e665
+ * @license Angular v9.0.0-rc.1+916.sha-6ddf550
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+918.sha-991e665');
+    var VERSION = new core.Version('9.0.0-rc.1+916.sha-6ddf550');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+918.sha-991e665
+ * @license Angular v9.0.0-rc.1+916.sha-6ddf550
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
