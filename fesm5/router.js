fda732b0c9b2022881c2c78b9c2e2f2b09c89b77
Miško Hevery <misko@hevery.com>
2020-02-04 17:20:34 +0000

    7369dd6f24 docs: fix animations example/remove 1st person (#35046)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+907.sha-128d4ac
+ * @license Angular v9.0.0-rc.1+909.sha-7369dd6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+907.sha-128d4ac');
+var VERSION = new Version('9.0.0-rc.1+909.sha-7369dd6');
 
 /**
  * @license
