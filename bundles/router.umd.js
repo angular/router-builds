45ecb2f58bef03d9db7cf0f865a7e772561ec69c
Miško Hevery <misko@hevery.com>
2020-02-04 19:58:39 +0000

    7fb66eca25 docs: changes AoT to AOT for consistency (#35112)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+916.sha-6ddf550
+ * @license Angular v9.0.0-rc.1+919.sha-7fb66ec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+916.sha-6ddf550');
+    var VERSION = new core.Version('9.0.0-rc.1+919.sha-7fb66ec');
 
     /**
      * @license
