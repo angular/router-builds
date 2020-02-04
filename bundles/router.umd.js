88713d714da07a814efe083efb276cf5c4d86e5b
Miško Hevery <misko@hevery.com>
2020-02-04 04:39:52 +0000

    152eec5543 release: cut the v9.0.0-rc.14 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.13+10.sha-22357d4
+ * @license Angular v9.0.0-rc.14
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.13+10.sha-22357d4');
+    var VERSION = new core.Version('9.0.0-rc.14');
 
     /**
      * @license
