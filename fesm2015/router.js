2a72cb02b6fc468aa11d22ae5187e1b4cff9a087
Miško Hevery <misko@hevery.com>
2020-02-04 19:03:59 +0000

    5cada5cce1 fix(ivy): recompile on template change in ngc watch mode on Windows (#34015)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+914.sha-f7d4cc9
+ * @license Angular v9.0.0-rc.1+915.sha-5cada5c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+914.sha-f7d4cc9');
+const VERSION = new Version('9.0.0-rc.1+915.sha-5cada5c');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+914.sha-f7d4cc9
+ * @license Angular v9.0.0-rc.1+915.sha-5cada5c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
