b7e2c4e6e968d469bf94e3a27543ad49d75889f8
Miško Hevery <misko@hevery.com>
2020-02-04 19:09:19 +0000

    991e665fba fix(benchpress): formatted spec files (#35127)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+915.sha-5cada5c
+ * @license Angular v9.0.0-rc.1+918.sha-991e665
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10208,7 +10208,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+915.sha-5cada5c');
+const VERSION = new Version('9.0.0-rc.1+918.sha-991e665');
 
 /**
  * @fileoverview added by tsickle
