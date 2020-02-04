d6c85a0ad3286554db1b5d5c724be60745480745
Miško Hevery <misko@hevery.com>
2020-02-04 17:22:47 +0000

    0e3eeb4e71 docs: restructure nav for beginner concepts (#34681)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+909.sha-7369dd6
+ * @license Angular v9.0.0-rc.1+908.sha-0e3eeb4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+909.sha-7369dd6');
+    var VERSION = new i0.Version('9.0.0-rc.1+908.sha-0e3eeb4');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+909.sha-7369dd6
+ * @license Angular v9.0.0-rc.1+908.sha-0e3eeb4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10208,7 +10208,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+909.sha-7369dd6');
+const VERSION = new Version('9.0.0-rc.1+908.sha-0e3eeb4');
 
 /**
  * @fileoverview added by tsickle
