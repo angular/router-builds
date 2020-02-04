29936b3f7547341ad0cf1cf4e669badb02b6eda5
Miško Hevery <misko@hevery.com>
2020-02-04 19:05:37 +0000

    5cada5cce1 fix(ivy): recompile on template change in ngc watch mode on Windows (#34015)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+914.sha-f7d4cc9
+ * @license Angular v9.0.0-rc.1+915.sha-5cada5c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+914.sha-f7d4cc9');
+    var VERSION = new i0.Version('9.0.0-rc.1+915.sha-5cada5c');
 
     /**
      * @license
