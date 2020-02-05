ea6149138cfa8e30086c4d84553de8ab247b58eb
Miško Hevery <misko@hevery.com>
2020-02-05 00:19:29 +0000

    491654d882 fix(docs-infra): fix CSS issues on home page and search results (#35098)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+919.sha-7fb66ec
+ * @license Angular v9.0.0-rc.1+925.sha-491654d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+919.sha-7fb66ec
+ * @license Angular v9.0.0-rc.1+925.sha-491654d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+919.sha-7fb66ec');
+const VERSION = new Version('9.0.0-rc.1+925.sha-491654d');
 
 /**
  * @fileoverview added by tsickle
