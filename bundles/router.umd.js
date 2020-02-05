a346a00501d54a224b33ce36337fd5d2c5f22d4e
Miško Hevery <misko@hevery.com>
2020-02-05 00:24:12 +0000

    c35671c0a4 fix(ivy): template type-check errors from TS should not use NG error codes (#35146)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+926.sha-1f6da8c
+ * @license Angular v9.0.0-rc.1+927.sha-c35671c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+926.sha-1f6da8c');
+    var VERSION = new i0.Version('9.0.0-rc.1+927.sha-c35671c');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+926.sha-1f6da8c
+ * @license Angular v9.0.0-rc.1+927.sha-c35671c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+926.sha-1f6da8c');
+var VERSION = new Version('9.0.0-rc.1+927.sha-c35671c');
 
 /**
  * @license
