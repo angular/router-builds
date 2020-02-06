fd8de3054f1860558ab9b6ecc0d852f550536839
Misko Hevery <misko@hevery.com>
2020-02-06 22:15:59 +0000

    86db592b5c docs(ivy): add a not about compile-time constants to the ivy compatibility guide (#35194)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+938.sha-053dabb
+ * @license Angular v9.0.0-rc.1+939.sha-86db592
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+938.sha-053dabb');
+    var VERSION = new i0.Version('9.0.0-rc.1+939.sha-86db592');
 
     /**
      * @license
