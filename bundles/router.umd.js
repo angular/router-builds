94a62410b3e03e670a51b9c614f555ff4b9f1532
Misko Hevery <misko@hevery.com>
2020-02-06 23:55:37 +0000

    3c69442dbd feat(compiler-cli): implement NgTscPlugin on top of the NgCompiler API (#34792)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+942.sha-49dd7b6
+ * @license Angular v9.0.0-rc.1+944.sha-3c69442
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+942.sha-49dd7b6');
+    var VERSION = new core.Version('9.0.0-rc.1+944.sha-3c69442');
 
     /**
      * @license
