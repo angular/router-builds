ece304436fc541c425e9c4f431514e26ac3fa44b
Misko Hevery <misko@hevery.com>
2020-02-06 17:57:39 +0000

    c286fbb9dc docs(ivy): add size debugging section (#35178)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+933.sha-7526ef7
+ * @license Angular v9.0.0-rc.1+934.sha-c286fbb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10208,7 +10208,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+933.sha-7526ef7');
+const VERSION = new Version('9.0.0-rc.1+934.sha-c286fbb');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+933.sha-7526ef7
+ * @license Angular v9.0.0-rc.1+934.sha-c286fbb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
