2f4d81cce73e8993b86f9be8098a511bd7a76774
Misko Hevery <misko@hevery.com>
2020-02-06 17:57:11 +0000

    722251da40 build: update scripts/release/post-check with the latest versions (#35151)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+934.sha-c286fbb
+ * @license Angular v9.0.0-rc.1+935.sha-722251d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+934.sha-c286fbb');
+    var VERSION = new core.Version('9.0.0-rc.1+935.sha-722251d');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+934.sha-c286fbb
+ * @license Angular v9.0.0-rc.1+935.sha-722251d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+934.sha-c286fbb');
+const VERSION = new Version('9.0.0-rc.1+935.sha-722251d');
 
 /**
  * @fileoverview added by tsickle
