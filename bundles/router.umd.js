84e078664b8501869eb0583bd0a22893a422c09d
Misko Hevery <misko@hevery.com>
2020-02-07 18:23:19 +0000

    53193c9779 docs: fix typo in glossary (#35108)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+955.sha-16eb691
+ * @license Angular v9.0.0-rc.1+956.sha-53193c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+955.sha-16eb691');
+    var VERSION = new i0.Version('9.0.0-rc.1+956.sha-53193c9');
 
     /**
      * @license
