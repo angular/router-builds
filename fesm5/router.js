d2a026b5af1945cdec6a1f75268407a3853493ae
Kara Erickson <karakara@google.com>
2020-02-07 20:02:59 +0000

    daac33cdc8 feat: add basic jest support (#35080)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+965.sha-f7e1c21
+ * @license Angular v9.0.0-rc.1+966.sha-daac33c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+965.sha-f7e1c21');
+var VERSION = new Version('9.0.0-rc.1+966.sha-daac33c');
 
 /**
  * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+965.sha-f7e1c21
+ * @license Angular v9.0.0-rc.1+966.sha-daac33c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
