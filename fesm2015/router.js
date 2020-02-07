372ff3ce95bf7179b5eae7de1324feeb5b0c5995
Misko Hevery <misko@hevery.com>
2020-02-07 18:21:42 +0000

    53193c9779 docs: fix typo in glossary (#35108)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+955.sha-16eb691
+ * @license Angular v9.0.0-rc.1+956.sha-53193c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+955.sha-16eb691');
+const VERSION = new Version('9.0.0-rc.1+956.sha-53193c9');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+955.sha-16eb691
+ * @license Angular v9.0.0-rc.1+956.sha-53193c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
