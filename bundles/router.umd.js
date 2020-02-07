10bd01f4e119892d120c32f518493aeac399336c
Misko Hevery <misko@hevery.com>
2020-02-07 18:25:07 +0000

    79742a397f fix(docs-infra): fix parameters with @Optional() decorator do not match declared, optional type (#35150)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+956.sha-53193c9
+ * @license Angular v9.0.0-rc.1+958.sha-79742a3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+956.sha-53193c9');
+    var VERSION = new i0.Version('9.0.0-rc.1+958.sha-79742a3');
 
     /**
      * @license
