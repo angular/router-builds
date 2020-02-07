35b5dc8f399c1e4fb96ae98a0a48ae788c3aeadb
Misko Hevery <misko@hevery.com>
2020-02-07 00:01:37 +0000

    6fb538ed1d fix(bazel): update ibazel to 0.11.1 (#35158)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+944.sha-3c69442
+ * @license Angular v9.0.0-rc.1+948.sha-6fb538e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+944.sha-3c69442');
+    var VERSION = new i0.Version('9.0.0-rc.1+948.sha-6fb538e');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+944.sha-3c69442
+ * @license Angular v9.0.0-rc.1+948.sha-6fb538e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10208,7 +10208,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+944.sha-3c69442');
+const VERSION = new Version('9.0.0-rc.1+948.sha-6fb538e');
 
 /**
  * @fileoverview added by tsickle
