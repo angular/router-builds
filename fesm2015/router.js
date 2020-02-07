8f60bc91c4446b9f8d751e10ebcf1b3c8d200649
Kara Erickson <karakara@google.com>
2020-02-07 22:16:36 +0000

    5b5dc811c1 docs: add asset info to library guide (#34217)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+31.sha-dea1b96
+ * @license Angular v9.0.0+32.sha-5b5dc81
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0+31.sha-dea1b96');
+const VERSION = new Version('9.0.0+32.sha-5b5dc81');
 
 /**
  * @fileoverview added by tsickle
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+31.sha-dea1b96
+ * @license Angular v9.0.0+32.sha-5b5dc81
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
