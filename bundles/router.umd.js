df1f867ea7def7642c2ef4dbca9372ede46d20a9
Misko Hevery <misko@hevery.com>
2020-02-07 00:04:46 +0000

    d3650ce3d8 build(docs-infra): upgrade cli command docs sources to d452f0873 (#35185)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+946.sha-10ad984
+ * @license Angular v9.0.0-rc.1+954.sha-d3650ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+946.sha-10ad984');
+    var VERSION = new core.Version('9.0.0-rc.1+954.sha-d3650ce');
 
     /**
      * @license
