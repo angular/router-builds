60b4afa59e1fabc34dadc2bbbb8e9127b7a582e8
Misko Hevery <misko@hevery.com>
2020-02-07 18:23:45 +0000

    0cbdd54fd0 fix(ivy): ensure module imports are instantiated before the module being declared (#35172)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+958.sha-79742a3
+ * @license Angular v9.0.0-rc.1+959.sha-0cbdd54
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+958.sha-79742a3');
+var VERSION = new Version('9.0.0-rc.1+959.sha-0cbdd54');
 
 /**
  * @license
