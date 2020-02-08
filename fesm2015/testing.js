fe81f46c23f65e4aeb4be97477a80ff506680e01
Kara Erickson <karakara@google.com>
2020-02-08 00:36:08 +0000

    d5d9971c28 refactor(benchpress): delete outdated/unused folder (#35147)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+968.sha-26b17ae
+ * @license Angular v9.0.0-rc.1+972.sha-d5d9971
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+968.sha-26b17ae
+ * @license Angular v9.0.0-rc.1+972.sha-d5d9971
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+968.sha-26b17ae');
+var VERSION = new Version('9.0.0-rc.1+972.sha-d5d9971');
 
 /**
  * @license
