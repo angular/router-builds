3430af84591a7ea49dbdbba3eea193be7e99dc0c
Kara Erickson <karakara@google.com>
2020-02-08 01:46:07 +0000

    ae0253f34a fix(ivy): set namespace for host elements of dynamically created components (#35136)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+972.sha-d5d9971
+ * @license Angular v9.0.0-rc.1+973.sha-ae0253f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+972.sha-d5d9971');
+    var VERSION = new i0.Version('9.0.0-rc.1+973.sha-ae0253f');
 
     /**
      * @license
