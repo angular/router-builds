088bdc8ec0f3d7274491a2741cfc69fc19e9fafb
Kara Erickson <karakara@google.com>
2020-02-10 17:43:11 +0000

    bfcf6d09e1 fix(docs-infra): lighthouse reporting links to cross-origin destinations are unsafe (#35253)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+37.sha-480a4c3
+ * @license Angular v9.0.0+40.sha-bfcf6d0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+37.sha-480a4c3
+ * @license Angular v9.0.0+40.sha-bfcf6d0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0+37.sha-480a4c3');
+var VERSION = new Version('9.0.0+40.sha-bfcf6d0');
 
 /**
  * @license
