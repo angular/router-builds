e5e638e10fb2964dd88aea6e0b15f0cbcfa0ea6a
Kara Erickson <karakara@google.com>
2020-02-10 19:11:48 +0000

    76b77ec3c2 docs(forms): update the ngForm deprecation notice (#35263)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+976.sha-2d5bb26
+ * @license Angular v9.0.0-rc.1+977.sha-76b77ec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+976.sha-2d5bb26
+ * @license Angular v9.0.0-rc.1+977.sha-76b77ec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+976.sha-2d5bb26');
+    var VERSION = new core.Version('9.0.0-rc.1+977.sha-76b77ec');
 
     /**
      * @license
