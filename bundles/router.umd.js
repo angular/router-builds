e8e0b99ee00aa47625205f002701f69429af30db
Kara Erickson <karakara@google.com>
2020-02-11 01:01:43 +0000

    81241af7ac fix(language-service): Suggest ? and ! operator on nullable receiver (#35200)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+982.sha-a92d97c
+ * @license Angular v9.0.0-rc.1+983.sha-81241af
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+982.sha-a92d97c');
+    var VERSION = new core.Version('9.0.0-rc.1+983.sha-81241af');
 
     /**
      * @license
