cff63617cb056ecb114791bf0656e1de5b7ce443
Kara Erickson <karakara@google.com>
2020-02-11 20:12:28 +0000

    7c9735a995 fix(elements): schematics fails with schema.json not found error (#35211)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+984.sha-332937e
+ * @license Angular v9.0.0-rc.1+985.sha-7c9735a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-rc.1+984.sha-332937e');
+    var VERSION = new core.Version('9.0.0-rc.1+985.sha-7c9735a');
 
     /**
      * @license
