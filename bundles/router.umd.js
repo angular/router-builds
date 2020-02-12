ea76f42e7c2cae06ada56e20c6199f649c5e01c2
Kara Erickson <karakara@google.com>
2020-02-12 19:22:17 +0000

    4aeb6c4e78 docs: release notes for the v9.0.1 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+990.sha-eef047b
+ * @license Angular v9.0.0-rc.1+991.sha-4aeb6c4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+990.sha-eef047b');
+    var VERSION = new i0.Version('9.0.0-rc.1+991.sha-4aeb6c4');
 
     /**
      * @license
