6a7bf801a8d1a0d9dba3dfb85570b1e389a7f555
Kara Erickson <karakara@google.com>
2020-02-13 01:11:22 +0000

    e8ea3be843 docs: remove service from region where it was added before it was created (#35354)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+991.sha-4aeb6c4
+ * @license Angular v9.1.0-next.0+6.sha-e8ea3be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+991.sha-4aeb6c4');
+    var VERSION = new i0.Version('9.1.0-next.0+6.sha-e8ea3be');
 
     /**
      * @license
