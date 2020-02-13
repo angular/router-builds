c42940603733831c9bf0f3585970b959cc6ebe7e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-13 22:14:50 +0000

    9ca1faf865 docs: Fix minor typos and coding styles (#35325)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+16.sha-b2222e7
+ * @license Angular v9.1.0-next.0+17.sha-9ca1faf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.0+16.sha-b2222e7');
+    var VERSION = new core.Version('9.1.0-next.0+17.sha-9ca1faf');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+16.sha-b2222e7
+ * @license Angular v9.1.0-next.0+17.sha-9ca1faf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.0+16.sha-b2222e7');
+const VERSION = new Version('9.1.0-next.0+17.sha-9ca1faf');
 
 /**
  * @fileoverview added by tsickle
