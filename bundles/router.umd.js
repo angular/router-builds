6d15f649599549622bf20051df0cff657a4fd399
Kara Erickson <karakara@google.com>
2020-02-13 01:17:47 +0000

    a6aa35e598 ci: use pipeline values to define the CI_COMMIT_RANGE (#35348)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+6.sha-e8ea3be
+ * @license Angular v9.1.0-next.0+5.sha-a6aa35e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.0+6.sha-e8ea3be');
+    var VERSION = new i0.Version('9.1.0-next.0+5.sha-a6aa35e');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+6.sha-e8ea3be
+ * @license Angular v9.1.0-next.0+5.sha-a6aa35e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.0+6.sha-e8ea3be');
+var VERSION = new Version('9.1.0-next.0+5.sha-a6aa35e');
 
 /**
  * @license
