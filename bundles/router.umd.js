086e5d739161191b57de4da074e20ef89fec65c9
Kara Erickson <karakara@google.com>
2020-02-13 18:24:12 +0000

    dc63ceb5a3 docs: update release.md and rename 'beta' releases to 'next' (#35276)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+7.sha-d6bc63f
+ * @license Angular v9.1.0-next.0+8.sha-dc63ceb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.0+7.sha-d6bc63f');
+    var VERSION = new i0.Version('9.1.0-next.0+8.sha-dc63ceb');
 
     /**
      * @license
