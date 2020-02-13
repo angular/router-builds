58e037280515411e0135c4a0fd3f4250d7dc21cc
Kara Erickson <karakara@google.com>
2020-02-13 18:37:23 +0000

    b2222e7e02 docs: minor typo fix (#35423) PR Close #35423

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+8.sha-dc63ceb
+ * @license Angular v9.1.0-next.0+16.sha-b2222e7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.0+8.sha-dc63ceb');
+var VERSION = new Version('9.1.0-next.0+16.sha-b2222e7');
 
 /**
  * @license
