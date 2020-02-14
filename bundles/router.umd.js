9beebad662a611f1aea2aadecfc2950c408e9207
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-14 00:58:17 +0000

    27cd01f606 build: update to bazel_toolchains 2.1.0 (#35430)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+17.sha-9ca1faf
+ * @license Angular v9.1.0-next.0+20.sha-27cd01f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.0+17.sha-9ca1faf');
+    var VERSION = new core.Version('9.1.0-next.0+20.sha-27cd01f');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+17.sha-9ca1faf
+ * @license Angular v9.1.0-next.0+20.sha-27cd01f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.0+17.sha-9ca1faf');
+const VERSION = new Version('9.1.0-next.0+20.sha-27cd01f');
 
 /**
  * @fileoverview added by tsickle
