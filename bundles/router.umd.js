14cc1652992d4fe677e4b5dc76570b94204563eb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-14 19:46:18 +0000

    be5e75c804 docs: change `item` to `items` to match the code (#35433)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+23.sha-2ca7984
+ * @license Angular v9.1.0-next.0+24.sha-be5e75c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.0+23.sha-2ca7984');
+    var VERSION = new i0.Version('9.1.0-next.0+24.sha-be5e75c');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+23.sha-2ca7984
+ * @license Angular v9.1.0-next.0+24.sha-be5e75c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
