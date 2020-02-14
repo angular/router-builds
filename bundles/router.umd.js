5375365369ff8d272eddb9f6a3d952b61d5d03a5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-14 19:43:09 +0000

    2ca7984d55 fix(core): remove support for `Map`/`Set` in `[class]`/`[style]` bindings (#35392)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+21.sha-b9b512f
+ * @license Angular v9.1.0-next.0+23.sha-2ca7984
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.0+21.sha-b9b512f');
+    var VERSION = new core.Version('9.1.0-next.0+23.sha-2ca7984');
 
     /**
      * @license
