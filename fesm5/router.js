4c5c7db5f967fd9b73aaf02489b1ca21a28ee842
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-14 19:39:24 +0000

    b9b512f729 fix(ivy): `LFrame` needs to release memory on `leaveView()` (#35156)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+20.sha-27cd01f
+ * @license Angular v9.1.0-next.0+21.sha-b9b512f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.0+20.sha-27cd01f');
+var VERSION = new Version('9.1.0-next.0+21.sha-b9b512f');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+20.sha-27cd01f
+ * @license Angular v9.1.0-next.0+21.sha-b9b512f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
