4332f50ab3bceae086c7a5d315c6827349c63b24
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-14 19:44:36 +0000

    be5e75c804 docs: change `item` to `items` to match the code (#35433)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+23.sha-2ca7984
+ * @license Angular v9.1.0-next.0+24.sha-be5e75c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.0+23.sha-2ca7984');
+const VERSION = new Version('9.1.0-next.0+24.sha-be5e75c');
 
 /**
  * @fileoverview added by tsickle
