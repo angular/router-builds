11099e3df9031423f0b5cac423dfc84b1a0f974a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-15 00:05:20 +0000

    d05629be3b build: enable network for docker on remote executors (#35432)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+27.sha-939e2f7
+ * @license Angular v9.1.0-next.0+26.sha-d05629b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.0+27.sha-939e2f7');
+var VERSION = new Version('9.1.0-next.0+26.sha-d05629b');
 
 /**
  * @license
