faa1ebe99b529eaf8e05c330be6cbbb32d9ed093
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:21:42 +0000

    2f18e8565e style(docs-infra): properly format files (#35318)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+34.sha-c013dd4
+ * @license Angular v9.1.0-next.0+36.sha-2f18e85
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.0+34.sha-c013dd4');
+    var VERSION = new i0.Version('9.1.0-next.0+36.sha-2f18e85');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+34.sha-c013dd4
+ * @license Angular v9.1.0-next.0+36.sha-2f18e85
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
