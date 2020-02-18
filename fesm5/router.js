2c37f0d1802108e74509b148373d5d51f160a68b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:12:43 +0000

    d7c4f40171 docs(upgrade): separate AngularJS Material typings to its own block (#35514)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+26.sha-d05629b
+ * @license Angular v9.1.0-next.0+29.sha-d7c4f40
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.0+26.sha-d05629b');
+var VERSION = new Version('9.1.0-next.0+29.sha-d7c4f40');
 
 /**
  * @license
