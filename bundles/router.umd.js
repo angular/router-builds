5a82e89f0987ba52036abd58dbc8e22d1135c5f2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 20:34:29 +0000

    5edeee69dd release: cut the v9.0.2 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.1+44.sha-ce85cbf
+ * @license Angular v9.0.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.1+44.sha-ce85cbf');
+    var VERSION = new core.Version('9.0.2');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.1+44.sha-ce85cbf
+ * @license Angular v9.0.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.1+44.sha-ce85cbf');
+var VERSION = new Version('9.0.2');
 
 /**
  * @license
