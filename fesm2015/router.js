e570c4a9f93fe0201e5a384779e9ce1560c074e0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 21:27:47 +0000

    8e12707f88 build: remove dependency on `@types/chokidar` (#35371)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+3.sha-1997b86
+ * @license Angular v9.1.0-next.1+1.sha-8e12707
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10208,7 +10208,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+3.sha-1997b86');
+const VERSION = new Version('9.1.0-next.1+1.sha-8e12707');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+3.sha-1997b86
+ * @license Angular v9.1.0-next.1+1.sha-8e12707
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
