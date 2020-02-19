38a20b5e71048c13be55d4abfb84d298a8399d5c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 17:34:24 +0000

    d0d36a5ebf ci: increase AIO payload size limit (#35538)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+38.sha-5fbfe69
+ * @license Angular v9.1.0-next.0+43.sha-d0d36a5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+38.sha-5fbfe69
+ * @license Angular v9.1.0-next.0+43.sha-d0d36a5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.0+38.sha-5fbfe69');
+var VERSION = new Version('9.1.0-next.0+43.sha-d0d36a5');
 
 /**
  * @license
