367422fa7f6c4cf760ca8dd1df7beff20e14e459
Miško Hevery <misko@hevery.com>
2020-02-20 19:16:57 +0000

    1b72fc10eb docs: edit displaying-data page to introduce section (#35507)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+1.sha-8e12707
+ * @license Angular v9.1.0-next.1+5.sha-1b72fc1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10208,7 +10208,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+1.sha-8e12707');
+const VERSION = new Version('9.1.0-next.1+5.sha-1b72fc1');
 
 /**
  * @fileoverview added by tsickle
