dd27f2455142e10a815e1cf01d13a166ab0d9a94
Miško Hevery <misko@hevery.com>
2020-02-20 21:05:32 +0000

    72664cac19 fix(compiler): use FatalDiagnosticError to generate better error messages (#35244)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2+3.sha-3a97972
+ * @license Angular v9.0.2+10.sha-72664ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2+3.sha-3a97972
+ * @license Angular v9.0.2+10.sha-72664ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.2+3.sha-3a97972');
+const VERSION = new Version('9.0.2+10.sha-72664ca');
 
 /**
  * @fileoverview added by tsickle
