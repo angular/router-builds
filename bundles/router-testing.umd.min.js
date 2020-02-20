e7691e3e4e98cc9e8a7a543e5bc023e0352c8625
Miško Hevery <misko@hevery.com>
2020-02-20 21:05:55 +0000

    646655d09a fix(compiler): use FatalDiagnosticError to generate better error messages (#35244)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+4.sha-4c693ea
+ * @license Angular v9.1.0-next.1+11.sha-646655d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+4.sha-4c693ea
+ * @license Angular v9.1.0-next.1+11.sha-646655d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.1+4.sha-4c693ea');
+    var VERSION = new i0.Version('9.1.0-next.1+11.sha-646655d');
 
     /**
      * @license
