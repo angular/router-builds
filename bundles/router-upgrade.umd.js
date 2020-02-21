82c6bd3b67742b4f6ee6c6d8d17deef871778694
Miško Hevery <misko@hevery.com>
2020-02-21 22:48:28 +0000

    e084835fb1 Revert "feat: support passive event options by defining global variables in zone.js config file (#34503)"

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+31.sha-e17bde9
+ * @license Angular v9.1.0-next.1+32.sha-e084835
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+31.sha-e17bde9
+ * @license Angular v9.1.0-next.1+32.sha-e084835
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+31.sha-e17bde9');
+const VERSION = new Version('9.1.0-next.1+32.sha-e084835');
 
 /**
  * @fileoverview added by tsickle
