f841478f19d3e57bf0d15905a180da798510aec8
Miško Hevery <misko@hevery.com>
2020-02-21 21:01:33 +0000

    049f118c1b fix(language-service): provide hover for interpolation in attribute value (#35494)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+27.sha-54fd33f
+ * @license Angular v9.1.0-next.1+28.sha-049f118
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+27.sha-54fd33f
+ * @license Angular v9.1.0-next.1+28.sha-049f118
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.1+27.sha-54fd33f');
+var VERSION = new Version('9.1.0-next.1+28.sha-049f118');
 
 /**
  * @license
