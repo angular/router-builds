c8f28238ff093ea5d5e2d499a1adf23061bccf49
Miško Hevery <misko@hevery.com>
2020-02-21 21:09:00 +0000

    e17bde99f8 fix(ivy): error in AOT when pipe inherits constructor from injectable that uses DI (#35468)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+30.sha-4253662
+ * @license Angular v9.1.0-next.1+31.sha-e17bde9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.1+30.sha-4253662');
+    var VERSION = new core.Version('9.1.0-next.1+31.sha-e17bde9');
 
     /**
      * @license
