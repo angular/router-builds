00635ff04118530a7ff99479ab1869ed02380d76
Miško Hevery <misko@hevery.com>
2020-02-24 17:31:24 +0000

    71b5970450 fix(ngcc): capture path-mapped entry-points that start with same string (#35592)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+43.sha-a7d5c55
+ * @license Angular v9.1.0-next.1+45.sha-71b5970
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+43.sha-a7d5c55');
+const VERSION = new Version('9.1.0-next.1+45.sha-71b5970');
 
 /**
  * @fileoverview added by tsickle
