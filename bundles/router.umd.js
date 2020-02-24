42edf70d16ec00a43b6269bdba9f87519e5e1a3f
Miško Hevery <misko@hevery.com>
2020-02-24 17:21:44 +0000

    a7d5c55926 docs: fix routing code to use `navigate` (#35176)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+32.sha-e084835
+ * @license Angular v9.1.0-next.1+43.sha-a7d5c55
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.1+32.sha-e084835');
+    var VERSION = new i0.Version('9.1.0-next.1+43.sha-a7d5c55');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+32.sha-e084835
+ * @license Angular v9.1.0-next.1+43.sha-a7d5c55
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10208,7 +10208,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+32.sha-e084835');
+const VERSION = new Version('9.1.0-next.1+43.sha-a7d5c55');
 
 /**
  * @fileoverview added by tsickle
