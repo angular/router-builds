9bfd5970e8c8d5b04aa5267181d18c5d5f7a5394
Miško Hevery <misko@hevery.com>
2020-02-24 17:31:57 +0000

    53f059ee8f fix(localize): improve placeholder mismatch error message (#35593)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+45.sha-71b5970
+ * @license Angular v9.1.0-next.1+44.sha-53f059e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.1+45.sha-71b5970');
+    var VERSION = new core.Version('9.1.0-next.1+44.sha-53f059e');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+45.sha-71b5970
+ * @license Angular v9.1.0-next.1+44.sha-53f059e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9996,7 +9996,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+45.sha-71b5970');
+const VERSION = new Version('9.1.0-next.1+44.sha-53f059e');
 
 /**
  * @fileoverview added by tsickle
