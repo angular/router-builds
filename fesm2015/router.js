ef1ebdaeb35b408272b3553f5cfbe4022c28aa1c
Miško Hevery <misko@hevery.com>
2020-02-25 21:49:45 +0000

    835618c678 fix(ivy): error when accessing NgModuleRef.componentFactoryResolver in constructor (#35637)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+59.sha-8e354da
+ * @license Angular v9.1.0-next.1+62.sha-835618c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10207,7 +10207,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+59.sha-8e354da');
+const VERSION = new Version('9.1.0-next.1+62.sha-835618c');
 
 /**
  * @fileoverview added by tsickle
