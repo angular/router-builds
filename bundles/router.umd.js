35e1fe77412fa88f574287ccd176f1dd6fbb4bb6
Miško Hevery <misko@hevery.com>
2020-02-25 21:38:33 +0000

    294e56d529 refactor(platform-browser): Hoist functions to workaround optimization bug. (#32230)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+57.sha-c414f45
+ * @license Angular v9.1.0-next.1+60.sha-294e56d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.1+57.sha-c414f45');
+    var VERSION = new i0.Version('9.1.0-next.1+60.sha-294e56d');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+57.sha-c414f45
+ * @license Angular v9.1.0-next.1+60.sha-294e56d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10207,7 +10207,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+57.sha-c414f45');
+const VERSION = new Version('9.1.0-next.1+60.sha-294e56d');
 
 /**
  * @fileoverview added by tsickle
