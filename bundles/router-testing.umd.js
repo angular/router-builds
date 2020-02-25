f2363249b1335d4495dd87bcc97c68480e6c48d3
Miško Hevery <misko@hevery.com>
2020-02-25 01:52:51 +0000

    f1ffa7a367 ci: move saucelabs_ivy & saucelabs_view_engine to the daily monitoring CircleCI workflow (#35516)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+54.sha-f9d483e
+ * @license Angular v9.1.0-next.1+52.sha-f1ffa7a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+54.sha-f9d483e
+ * @license Angular v9.1.0-next.1+52.sha-f1ffa7a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+54.sha-f9d483e');
+const VERSION = new Version('9.1.0-next.1+52.sha-f1ffa7a');
 
 /**
  * @fileoverview added by tsickle
