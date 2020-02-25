73dc750bf9e59ff8f4b6708440b474bdf7454620
Miško Hevery <misko@hevery.com>
2020-02-25 21:32:10 +0000

    96cdf035d8 fix(service-worker): treat 503 as offline (#35595)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+52.sha-f1ffa7a
+ * @license Angular v9.1.0-next.1+56.sha-96cdf03
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+52.sha-f1ffa7a');
+const VERSION = new Version('9.1.0-next.1+56.sha-96cdf03');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+52.sha-f1ffa7a
+ * @license Angular v9.1.0-next.1+56.sha-96cdf03
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.1+52.sha-f1ffa7a');
+var VERSION = new Version('9.1.0-next.1+56.sha-96cdf03');
 
 /**
  * @license
