d5e205212ab636bd7adfa82844a2408d24285f00
Miško Hevery <misko@hevery.com>
2020-02-25 21:41:20 +0000

    8e354dae00 fix(language-service): get the right 'ElementAst' in the nested HTML tag (#35317)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+61.sha-1e20b2c
+ * @license Angular v9.1.0-next.1+59.sha-8e354da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+61.sha-1e20b2c
+ * @license Angular v9.1.0-next.1+59.sha-8e354da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10207,7 +10207,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+61.sha-1e20b2c');
+const VERSION = new Version('9.1.0-next.1+59.sha-8e354da');
 
 /**
  * @fileoverview added by tsickle
