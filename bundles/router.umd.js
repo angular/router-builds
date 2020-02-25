98e600f3c9682d6e81037038ba6a954596900515
Miško Hevery <misko@hevery.com>
2020-02-25 01:53:10 +0000

    f9d483e76e feat: add a temp solution to support passive event listeners. (#34503)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+53.sha-af76651
+ * @license Angular v9.1.0-next.1+54.sha-f9d483e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.1+53.sha-af76651');
+    var VERSION = new i0.Version('9.1.0-next.1+54.sha-f9d483e');
 
     /**
      * @license
