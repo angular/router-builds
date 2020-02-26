0000b2253349e6f754bdc4d7dc806e1222327b66
Miško Hevery <misko@hevery.com>
2020-02-26 21:02:34 +0000

    bde4cd7982 docs: add documentation of NgZone with zone.js (#34295)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+62.sha-835618c
+ * @license Angular v9.1.0-next.1+63.sha-bde4cd7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10207,7 +10207,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+62.sha-835618c');
+const VERSION = new Version('9.1.0-next.1+63.sha-bde4cd7');
 
 /**
  * @fileoverview added by tsickle
