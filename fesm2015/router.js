989275b6afe89e28bca2f06ea5ad763c174cc6e9
Miško Hevery <misko@hevery.com>
2020-02-26 21:32:50 +0000

    975a11b37f fix(docs-infra): do not break when cookies are disabled in the browser (#35557)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+69.sha-df816c9
+ * @license Angular v9.1.0-next.1+72.sha-975a11b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+69.sha-df816c9');
+const VERSION = new Version('9.1.0-next.1+72.sha-975a11b');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+69.sha-df816c9
+ * @license Angular v9.1.0-next.1+72.sha-975a11b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.1+69.sha-df816c9');
+var VERSION = new Version('9.1.0-next.1+72.sha-975a11b');
 
 /**
  * @license
