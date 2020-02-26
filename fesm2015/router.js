9e58f384bee4a1e874747ebb220791857f1f2803
Miško Hevery <misko@hevery.com>
2020-02-26 21:31:03 +0000

    df816c9c80 feat(ngcc): implement source-map flattening (#35132)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+66.sha-a7ed74f
+ * @license Angular v9.1.0-next.1+69.sha-df816c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10207,7 +10207,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+66.sha-a7ed74f');
+const VERSION = new Version('9.1.0-next.1+69.sha-df816c9');
 
 /**
  * @fileoverview added by tsickle
