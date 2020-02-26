eb8fc578a5b6e5b01b6bad6a1c0b4e1f99022d1d
Miško Hevery <misko@hevery.com>
2020-02-26 21:29:25 +0000

    df816c9c80 feat(ngcc): implement source-map flattening (#35132)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+66.sha-a7ed74f
+ * @license Angular v9.1.0-next.1+69.sha-df816c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.1+66.sha-a7ed74f');
+    var VERSION = new core.Version('9.1.0-next.1+69.sha-df816c9');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+66.sha-a7ed74f
+ * @license Angular v9.1.0-next.1+69.sha-df816c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
