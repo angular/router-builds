68f886ec076fe30a11fcce96fbc1373bbae874f7
atscott <atscott@google.com>
2020-02-27 22:31:08 +0000

    03d88c7965 feat: define all zone.js configurations to typescript interfaces (#35329)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+13.sha-b7519e5
+ * @license Angular v9.1.0-next.2+14.sha-03d88c7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+13.sha-b7519e5');
+var VERSION = new Version('9.1.0-next.2+14.sha-03d88c7');
 
 /**
  * @license
