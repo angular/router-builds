f93abc3b6039676d0d47e18a30bcf8b362905747
Matias Niemelä <matias@yearofmoo.com>
2020-02-27 21:31:35 +0000

    9b53054ea8 refactor(ngcc): guard against a crash if source-map flattening fails (#35718)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.3+6.sha-bf6fbf5
+ * @license Angular v9.0.3+9.sha-9b53054
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.3+6.sha-bf6fbf5');
+    var VERSION = new core.Version('9.0.3+9.sha-9b53054');
 
     /**
      * @license
