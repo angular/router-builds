2cac5996883e05d5c9fd860117dc281c79080892
Matias Niemelä <matias@yearofmoo.com>
2020-02-27 21:44:18 +0000

    5d8f7da3aa refactor(ngcc): guard against a crash if source-map flattening fails (#35718)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+6.sha-01ab168
+ * @license Angular v9.1.0-next.2+9.sha-5d8f7da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10207,7 +10207,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+6.sha-01ab168');
+const VERSION = new Version('9.1.0-next.2+9.sha-5d8f7da');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+6.sha-01ab168
+ * @license Angular v9.1.0-next.2+9.sha-5d8f7da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
