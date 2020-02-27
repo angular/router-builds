0a971398d4ebc5ad4357d0190b432c4bfee4e820
atscott <atscott@google.com>
2020-02-27 19:17:23 +0000

    20b0c80b0b fix(ngcc): allow deep-import warnings to be ignored (#35683)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+4.sha-30f29ee
+ * @license Angular v9.1.0-next.2+3.sha-20b0c80
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.2+4.sha-30f29ee');
+    var VERSION = new i0.Version('9.1.0-next.2+3.sha-20b0c80');
 
     /**
      * @license
