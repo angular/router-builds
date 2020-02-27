7dac790294f3aadc1b2b3d254887b4cde83b57be
atscott <atscott@google.com>
2020-02-27 22:30:41 +0000

    b7519e5cfa build: allow auto-discover all typings files in npm package by ts-api-guardian (#35691)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+12.sha-7b13977
+ * @license Angular v9.1.0-next.2+13.sha-b7519e5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10207,7 +10207,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+12.sha-7b13977');
+const VERSION = new Version('9.1.0-next.2+13.sha-b7519e5');
 
 /**
  * @fileoverview added by tsickle
