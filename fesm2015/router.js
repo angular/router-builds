0994e72ad93f80faa028e2c8d042946c8b4b1529
atscott <atscott@google.com>
2020-02-27 19:15:48 +0000

    20b0c80b0b fix(ngcc): allow deep-import warnings to be ignored (#35683)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+4.sha-30f29ee
+ * @license Angular v9.1.0-next.2+3.sha-20b0c80
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+4.sha-30f29ee');
+const VERSION = new Version('9.1.0-next.2+3.sha-20b0c80');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+4.sha-30f29ee
+ * @license Angular v9.1.0-next.2+3.sha-20b0c80
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
