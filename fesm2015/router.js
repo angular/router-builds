226fa9f2334584ac11d2b3dead899638a9720ea4
atscott <atscott@google.com>
2020-02-27 22:23:25 +0000

    8fed1fe792 perf(core): adding micro benchmark for host bindings (#35705)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+10.sha-bc7b589
+ * @license Angular v9.1.0-next.2+11.sha-8fed1fe
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+10.sha-bc7b589');
+const VERSION = new Version('9.1.0-next.2+11.sha-8fed1fe');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+10.sha-bc7b589
+ * @license Angular v9.1.0-next.2+11.sha-8fed1fe
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
