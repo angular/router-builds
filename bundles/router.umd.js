87130b62b9e097fb0191f9df0ddcb10babd2bb05
atscott <atscott@google.com>
2020-02-27 22:27:51 +0000

    7b13977c3a fix(core): use proper configuration to compile Injectable in JIT (#35706)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+11.sha-8fed1fe
+ * @license Angular v9.1.0-next.2+12.sha-7b13977
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.2+11.sha-8fed1fe');
+    var VERSION = new core.Version('9.1.0-next.2+12.sha-7b13977');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+11.sha-8fed1fe
+ * @license Angular v9.1.0-next.2+12.sha-7b13977
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
