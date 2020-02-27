edc1b397736055585a750d0965b37b1d2da31bab
atscott <atscott@google.com>
2020-02-27 19:14:48 +0000

    30f29ee8e7 docs: correct spelling of 'detection' (#35723)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+2.sha-0b97d07
+ * @license Angular v9.1.0-next.2+4.sha-30f29ee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.2+2.sha-0b97d07');
+    var VERSION = new i0.Version('9.1.0-next.2+4.sha-30f29ee');
 
     /**
      * @license
