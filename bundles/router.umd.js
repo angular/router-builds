b23842f21b2725335f82fd8e5aabe6e70fa05f99
atscott <atscott@google.com>
2020-02-27 19:31:39 +0000

    01ab168774 feat(docs-infra): add useful links if landed on 404 page and no search results found (#34978)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+3.sha-20b0c80
+ * @license Angular v9.1.0-next.2+6.sha-01ab168
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.2+3.sha-20b0c80');
+    var VERSION = new i0.Version('9.1.0-next.2+6.sha-01ab168');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+3.sha-20b0c80
+ * @license Angular v9.1.0-next.2+6.sha-01ab168
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10207,7 +10207,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+3.sha-20b0c80');
+const VERSION = new Version('9.1.0-next.2+6.sha-01ab168');
 
 /**
  * @fileoverview added by tsickle
