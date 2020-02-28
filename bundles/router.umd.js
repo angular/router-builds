e9b2953771a3ec9280db39c3031998f6ebbb5eb4
atscott <atscott@google.com>
2020-02-28 20:50:45 +0000

    ef75875ca3 fix(core): handle `<ng-template>` with local refs in i18n blocks (#35758)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+23.sha-4003538
+ * @license Angular v9.1.0-next.2+24.sha-ef75875
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.2+23.sha-4003538');
+    var VERSION = new i0.Version('9.1.0-next.2+24.sha-ef75875');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+23.sha-4003538
+ * @license Angular v9.1.0-next.2+24.sha-ef75875
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+23.sha-4003538');
+var VERSION = new Version('9.1.0-next.2+24.sha-ef75875');
 
 /**
  * @license
