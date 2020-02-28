603027d9f9eabec4806177732554b4961a37377f
atscott <atscott@google.com>
2020-02-28 15:35:30 +0000

    73ee57a6e3 test(language-service): differentiate feature and internal infra tests (#35688)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+17.sha-5e38056
+ * @license Angular v9.1.0-next.2+18.sha-73ee57a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+17.sha-5e38056');
+const VERSION = new Version('9.1.0-next.2+18.sha-73ee57a');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+17.sha-5e38056
+ * @license Angular v9.1.0-next.2+18.sha-73ee57a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+17.sha-5e38056');
+var VERSION = new Version('9.1.0-next.2+18.sha-73ee57a');
 
 /**
  * @license
