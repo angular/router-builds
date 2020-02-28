7a8a4e447a3421434cfc1117015a24ac9e231b1b
atscott <atscott@google.com>
2020-02-28 00:28:27 +0000

    3e3a1ef30d fix(ivy): support dynamic query tokens in AOT mode (#35307)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+14.sha-03d88c7
+ * @license Angular v9.1.0-next.2+15.sha-3e3a1ef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+14.sha-03d88c7');
+const VERSION = new Version('9.1.0-next.2+15.sha-3e3a1ef');
 
 /**
  * @fileoverview added by tsickle
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+14.sha-03d88c7
+ * @license Angular v9.1.0-next.2+15.sha-3e3a1ef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
