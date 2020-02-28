f26a8444a60b6df3cebf9c0f84ef28321f3eafbd
atscott <atscott@google.com>
2020-02-28 20:51:33 +0000

    e13fcbaa6f perf(core): use multiple directives in host bindings micro benchmark (#35736)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.4+10.sha-fcad075
+ * @license Angular v9.0.4+14.sha-e13fcba
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.4+10.sha-fcad075');
+    var VERSION = new core.Version('9.0.4+14.sha-e13fcba');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.4+10.sha-fcad075
+ * @license Angular v9.0.4+14.sha-e13fcba
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
