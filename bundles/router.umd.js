69a87de31b026ab29911ac5c5aeae7e324847763
atscott <atscott@google.com>
2020-02-28 20:46:18 +0000

    40035380e7 fix(core): Remove `debugger` statement when assert is thrown (#35763)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+21.sha-40039d8
+ * @license Angular v9.1.0-next.2+23.sha-4003538
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.2+21.sha-40039d8');
+    var VERSION = new core.Version('9.1.0-next.2+23.sha-4003538');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+21.sha-40039d8
+ * @license Angular v9.1.0-next.2+23.sha-4003538
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+21.sha-40039d8');
+const VERSION = new Version('9.1.0-next.2+23.sha-4003538');
 
 /**
  * @fileoverview added by tsickle
