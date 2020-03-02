50300ec9cb8b384bc693eb0b8ff3b10a407353da
atscott <atscott@google.com>
2020-03-02 16:44:01 +0000

    746e313a76 docs: Fix typo in template syntax (#35767)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+29.sha-0a5a841
+ * @license Angular v9.1.0-next.2+28.sha-746e313
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+29.sha-0a5a841
+ * @license Angular v9.1.0-next.2+28.sha-746e313
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+29.sha-0a5a841');
+var VERSION = new Version('9.1.0-next.2+28.sha-746e313');
 
 /**
  * @license
