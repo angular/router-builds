b7acd69af560c5cc175e4614756d8ec9682fec34
atscott <atscott@google.com>
2020-03-02 19:39:25 +0000

    5652fb1018 fix(core): allow null / undefined values in query results (#35796)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+36.sha-44e47da
+ * @license Angular v9.1.0-next.2+37.sha-5652fb1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.2+36.sha-44e47da');
+    var VERSION = new core.Version('9.1.0-next.2+37.sha-5652fb1');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+36.sha-44e47da
+ * @license Angular v9.1.0-next.2+37.sha-5652fb1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+36.sha-44e47da');
+const VERSION = new Version('9.1.0-next.2+37.sha-5652fb1');
 
 /**
  * @fileoverview added by tsickle
