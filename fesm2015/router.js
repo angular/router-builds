738af4d7f2820538424bdd8842c4b9c837a3f0f1
atscott <atscott@google.com>
2020-03-02 16:46:05 +0000

    40da51f641 fix(compiler): support i18n attributes on `<ng-template>` tags (#35681)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+32.sha-35c9f0d
+ * @license Angular v9.1.0-next.2+33.sha-40da51f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+32.sha-35c9f0d');
+const VERSION = new Version('9.1.0-next.2+33.sha-40da51f');
 
 /**
  * @fileoverview added by tsickle
