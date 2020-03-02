d6e7d881503530178b9a6021372d55a113cc5219
atscott <atscott@google.com>
2020-03-02 19:51:58 +0000

    4ba4d9076d style: Remove `ivy` scope, and add `ve` scope (#35799)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+37.sha-5652fb1
+ * @license Angular v9.1.0-next.2+38.sha-4ba4d90
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+37.sha-5652fb1');
+var VERSION = new Version('9.1.0-next.2+38.sha-4ba4d90');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+37.sha-5652fb1
+ * @license Angular v9.1.0-next.2+38.sha-4ba4d90
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
