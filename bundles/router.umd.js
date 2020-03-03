d51ecb49b6254e78d9e827539251f3a65efa7b02
atscott <atscott@google.com>
2020-03-03 17:31:51 +0000

    f13ee3e692 build: update to rules_nodejs 1.4.0 (#35813)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+51.sha-89b5b97
+ * @license Angular v9.1.0-next.2+49.sha-f13ee3e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.2+51.sha-89b5b97');
+    var VERSION = new i0.Version('9.1.0-next.2+49.sha-f13ee3e');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+51.sha-89b5b97
+ * @license Angular v9.1.0-next.2+49.sha-f13ee3e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10207,7 +10207,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+51.sha-89b5b97');
+const VERSION = new Version('9.1.0-next.2+49.sha-f13ee3e');
 
 /**
  * @fileoverview added by tsickle
