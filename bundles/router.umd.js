33e0ffa0cc9f68d5c3f72f47ed40c897de5254da
atscott <atscott@google.com>
2020-03-03 22:17:50 +0000

    ed44073a58 build(zone.js): sets rollup_bundle to use the --silent flag (#35835)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+58.sha-2c41bb8
+ * @license Angular v9.1.0-next.2+59.sha-ed44073
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.2+58.sha-2c41bb8');
+    var VERSION = new core.Version('9.1.0-next.2+59.sha-ed44073');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+58.sha-2c41bb8
+ * @license Angular v9.1.0-next.2+59.sha-ed44073
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+58.sha-2c41bb8');
+const VERSION = new Version('9.1.0-next.2+59.sha-ed44073');
 
 /**
  * @fileoverview added by tsickle
