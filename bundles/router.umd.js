1f3ef23ce1285ec6c0d0f41ff3eaa595f6cfc4d3
atscott <atscott@google.com>
2020-03-03 17:15:00 +0000

    47a1811e0b feat(language-service): modularize error messages (#35678)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+39.sha-ef2721b
+ * @license Angular v9.1.0-next.2+41.sha-47a1811
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.2+39.sha-ef2721b');
+    var VERSION = new i0.Version('9.1.0-next.2+41.sha-47a1811');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+39.sha-ef2721b
+ * @license Angular v9.1.0-next.2+41.sha-47a1811
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10207,7 +10207,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+39.sha-ef2721b');
+const VERSION = new Version('9.1.0-next.2+41.sha-47a1811');
 
 /**
  * @fileoverview added by tsickle
