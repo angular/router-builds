6cda0aa3735fd1c06951962b7ccb0cd505e60e82
atscott <atscott@google.com>
2020-03-03 22:03:18 +0000

    17cf04ebea fix(animations): allow computeStyle to work on elements created in Node (#35810)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+55.sha-c7d0567
+ * @license Angular v9.1.0-next.2+56.sha-17cf04e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+55.sha-c7d0567');
+const VERSION = new Version('9.1.0-next.2+56.sha-17cf04e');
 
 /**
  * @fileoverview added by tsickle
