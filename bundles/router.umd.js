bdafd9b9f444bc5d7e3f58ec769fc1e7a252bfe0
atscott <atscott@google.com>
2020-03-03 17:49:08 +0000

    5615928df9 build: no longer run tslint from within gulp task (#35800)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+52.sha-93fc392
+ * @license Angular v9.1.0-next.2+54.sha-5615928
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.2+52.sha-93fc392');
+    var VERSION = new i0.Version('9.1.0-next.2+54.sha-5615928');
 
     /**
      * @license
