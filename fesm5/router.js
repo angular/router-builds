edb8cee24cbd116ab0a932bed5b1b63b00a22a21
atscott <atscott@google.com>
2020-03-03 22:18:12 +0000

    2c41bb8490 fix(compiler): type-checking error for duplicate variables in templates (#35674)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+57.sha-1f8a243
+ * @license Angular v9.1.0-next.2+58.sha-2c41bb8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+57.sha-1f8a243');
+var VERSION = new Version('9.1.0-next.2+58.sha-2c41bb8');
 
 /**
  * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+57.sha-1f8a243
+ * @license Angular v9.1.0-next.2+58.sha-2c41bb8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
