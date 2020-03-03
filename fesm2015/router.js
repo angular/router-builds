77b0ae3463dd9328444a9dba29fc25b7aa57795f
atscott <atscott@google.com>
2020-03-03 17:24:22 +0000

    0653db14e7 perf(core): add micro benchmark for destroy hook invocation (#35784)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+47.sha-bf42807
+ * @license Angular v9.1.0-next.2+48.sha-0653db1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10207,7 +10207,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+47.sha-bf42807');
+const VERSION = new Version('9.1.0-next.2+48.sha-0653db1');
 
 /**
  * @fileoverview added by tsickle
