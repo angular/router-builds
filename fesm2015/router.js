977515ca109563c01d3905123c868b321a44604a
atscott <atscott@google.com>
2020-03-03 18:07:36 +0000

    c7d0567d37 Revert "docs: add comment markers for internal goog.define replacement (#32795)" (#35830)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+54.sha-5615928
+ * @license Angular v9.1.0-next.2+55.sha-c7d0567
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10207,7 +10207,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+54.sha-5615928');
+const VERSION = new Version('9.1.0-next.2+55.sha-c7d0567');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+54.sha-5615928
+ * @license Angular v9.1.0-next.2+55.sha-c7d0567
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
