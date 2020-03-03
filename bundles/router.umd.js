2f144dd7c04256b25ba11b563049a67cb73f4ef9
atscott <atscott@google.com>
2020-03-03 17:22:45 +0000

    bf42807aa6 docs(common): switch HTTP guide examples to `TestBed.inject()` (#35777)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+46.sha-c195d22
+ * @license Angular v9.1.0-next.2+47.sha-bf42807
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.2+46.sha-c195d22');
+    var VERSION = new i0.Version('9.1.0-next.2+47.sha-bf42807');
 
     /**
      * @license
