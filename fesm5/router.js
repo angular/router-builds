1db78ad30865bf65f9e7640a223096f2584c41ca
atscott <atscott@google.com>
2020-03-03 22:22:02 +0000

    00f3c58bb9 fix(core): log error instead of warning for unknown properties and elements (#35798)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+59.sha-ed44073
+ * @license Angular v9.1.0-next.2+60.sha-00f3c58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+59.sha-ed44073');
+var VERSION = new Version('9.1.0-next.2+60.sha-00f3c58');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+59.sha-ed44073
+ * @license Angular v9.1.0-next.2+60.sha-00f3c58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
