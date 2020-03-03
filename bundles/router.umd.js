0e2355cd9067f4409efa74511e7454e43e0e959c
atscott <atscott@google.com>
2020-03-03 17:33:24 +0000

    3b0b90527e fix(docs-infra): add top level folders into `topLevelFolders` (#35815)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+49.sha-f13ee3e
+ * @license Angular v9.1.0-next.2+50.sha-3b0b905
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.2+49.sha-f13ee3e');
+    var VERSION = new i0.Version('9.1.0-next.2+50.sha-3b0b905');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+49.sha-f13ee3e
+ * @license Angular v9.1.0-next.2+50.sha-3b0b905
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10207,7 +10207,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+49.sha-f13ee3e');
+const VERSION = new Version('9.1.0-next.2+50.sha-3b0b905');
 
 /**
  * @fileoverview added by tsickle
