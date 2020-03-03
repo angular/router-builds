2375ce4302e8ff5ed116ca70da9ab739938724e3
atscott <atscott@google.com>
2020-03-03 22:14:28 +0000

    1f8a243b67 fix(core): unable to NgModuleRef.injector in module constructor (#35731)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+56.sha-17cf04e
+ * @license Angular v9.1.0-next.2+57.sha-1f8a243
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+56.sha-17cf04e');
+const VERSION = new Version('9.1.0-next.2+57.sha-1f8a243');
 
 /**
  * @fileoverview added by tsickle
