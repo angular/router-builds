8aa60410ded068b0b71dc4ada51dc0122e49471e
atscott <atscott@google.com>
2020-03-04 00:23:11 +0000

    d543b13e5c Revert "fix(core): log error instead of warning for unknown properties and elements (#35798)" (#35845)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+60.sha-00f3c58
+ * @license Angular v9.1.0-next.2+61.sha-d543b13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.2+60.sha-00f3c58');
+    var VERSION = new core.Version('9.1.0-next.2+61.sha-d543b13');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+60.sha-00f3c58
+ * @license Angular v9.1.0-next.2+61.sha-d543b13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+60.sha-00f3c58');
+const VERSION = new Version('9.1.0-next.2+61.sha-d543b13');
 
 /**
  * @fileoverview added by tsickle
