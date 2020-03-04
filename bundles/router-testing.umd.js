38fc49b0004e1933d227fd8fcb60872e47b10b67
atscott <atscott@google.com>
2020-03-04 19:58:13 +0000

    dcf28bf320 docs: release notes for the v9.0.5 release

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+71.sha-1b8a668
+ * @license Angular v9.1.0-next.2+72.sha-dcf28bf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+71.sha-1b8a668
+ * @license Angular v9.1.0-next.2+72.sha-dcf28bf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.2+71.sha-1b8a668');
+    var VERSION = new core.Version('9.1.0-next.2+72.sha-dcf28bf');
 
     /**
      * @license
