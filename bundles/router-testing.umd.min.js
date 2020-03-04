90ee663349996b6f54bc2e12a1c1992aeffa9f15
atscott <atscott@google.com>
2020-03-04 17:04:30 +0000

    2e728f7fff docs: remove `ivy` and mention `ve` label in docs (#35809)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+61.sha-d543b13
+ * @license Angular v9.1.0-next.2+62.sha-2e728f7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+61.sha-d543b13
+ * @license Angular v9.1.0-next.2+62.sha-2e728f7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.2+61.sha-d543b13');
+    var VERSION = new i0.Version('9.1.0-next.2+62.sha-2e728f7');
 
     /**
      * @license
