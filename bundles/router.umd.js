721ed768dec0e3d8abe03e02dddf11fb08098888
Matias Niemelä <matias@yearofmoo.com>
2020-03-04 22:15:07 +0000

    de67978a7d fix(router): state data missing in routerLink (#33203)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+73.sha-b57be3b
+ * @license Angular v9.1.0-next.2+74.sha-de67978
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4741,6 +4741,7 @@
             var extras = {
                 skipLocationChange: attrBoolValue(this.skipLocationChange),
                 replaceUrl: attrBoolValue(this.replaceUrl),
+                state: this.state
             };
             this.router.navigateByUrl(this.urlTree, extras);
             return true;
@@ -5912,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.2+73.sha-b57be3b');
+    var VERSION = new i0.Version('9.1.0-next.2+74.sha-de67978');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+73.sha-b57be3b
+ * @license Angular v9.1.0-next.2+74.sha-de67978
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8075,6 +8075,7 @@ class RouterLink {
         const extras = {
             skipLocationChange: attrBoolValue(this.skipLocationChange),
             replaceUrl: attrBoolValue(this.replaceUrl),
+            state: this.state
         };
         this.router.navigateByUrl(this.urlTree, extras);
         return true;
@@ -10207,7 +10208,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+73.sha-b57be3b');
+const VERSION = new Version('9.1.0-next.2+74.sha-de67978');
 
 /**
  * @fileoverview added by tsickle
