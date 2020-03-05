71f422200c4555a618f6165d05d7906923d4a02e
Matias Niemelä <matias@yearofmoo.com>
2020-03-05 20:59:18 +0000

    a1d0396c55 build: fix integration tests flakes using local yarn cache for bazel-schematics & ng_elements_schematics demos (#35877)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+74.sha-de67978
+ * @license Angular v9.1.0-next.2+75.sha-a1d0396
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5913,7 +5913,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.2+74.sha-de67978');
+    var VERSION = new i0.Version('9.1.0-next.2+75.sha-a1d0396');
 
     /**
      * @license
