ffd6ea3a075b797b381e73248dda186a636fd5a5
Matias Niemelä <matias@yearofmoo.com>
2020-03-05 21:27:25 +0000

    8ef29b65ff fix: fix flaky test cases of passive events (#35679)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+77.sha-e0a35e1
+ * @license Angular v9.1.0-next.2+76.sha-8ef29b6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10208,7 +10208,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+77.sha-e0a35e1');
+const VERSION = new Version('9.1.0-next.2+76.sha-8ef29b6');
 
 /**
  * @fileoverview added by tsickle
