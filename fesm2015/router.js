8a710a3bd6d1397a699bcbd4063884b19d75ccca
Matias Niemelä <matias@yearofmoo.com>
2020-03-05 21:29:38 +0000

    716d50aa21 refactor(compiler): Break up parseTemplateBindings() for microsyntax (#35812)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+76.sha-8ef29b6
+ * @license Angular v9.1.0-next.2+79.sha-716d50a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10208,7 +10208,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+76.sha-8ef29b6');
+const VERSION = new Version('9.1.0-next.2+79.sha-716d50a');
 
 /**
  * @fileoverview added by tsickle
