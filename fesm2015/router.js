887a6d0226b070b443c1d148513ef333e450616f
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 18:06:45 +0000

    4e1d780554 fix(language-service): resolve the real path for symlink (#35895)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+89.sha-59078c4
+ * @license Angular v9.1.0-next.2+88.sha-4e1d780
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+89.sha-59078c4');
+const VERSION = new Version('9.1.0-next.2+88.sha-4e1d780');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+89.sha-59078c4
+ * @license Angular v9.1.0-next.2+88.sha-4e1d780
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
