c53be7da929a179f75820ebce239e3c12442bcc1
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:59:23 +0000

    55b3f97be0 fix(zone.js): `tickOptions`'s `processNewMacroTasksSynchronously` should default to true when flag omitted (#35814)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+95.sha-86a45ff
+ * @license Angular v9.1.0-next.2+99.sha-55b3f97
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10207,7 +10207,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+95.sha-86a45ff');
+const VERSION = new Version('9.1.0-next.2+99.sha-55b3f97');
 
 /**
  * @fileoverview added by tsickle
