bfcc3bf13b02d1dec3c12d271a7e83a38a7416a4
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 01:02:06 +0000

    b42e2e0dff build: create dev-infra-private npm package (#35862)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+84.sha-c55f900
+ * @license Angular v9.1.0-next.2+85.sha-b42e2e0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.2+84.sha-c55f900');
+    var VERSION = new i0.Version('9.1.0-next.2+85.sha-b42e2e0');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+84.sha-c55f900
+ * @license Angular v9.1.0-next.2+85.sha-b42e2e0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10207,7 +10207,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+84.sha-c55f900');
+const VERSION = new Version('9.1.0-next.2+85.sha-b42e2e0');
 
 /**
  * @fileoverview added by tsickle
