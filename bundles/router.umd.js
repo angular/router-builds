2ee4c8e3b93124ed95e2d37d55a71259e7be090e
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 18:07:28 +0000

    bef14cf424 refactor(compiler): rename _ParseAST.optionalCharacter TemplateBinding.expression (#35886)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+88.sha-4e1d780
+ * @license Angular v9.1.0-next.2+90.sha-bef14cf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.2+88.sha-4e1d780');
+    var VERSION = new core.Version('9.1.0-next.2+90.sha-bef14cf');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+88.sha-4e1d780
+ * @license Angular v9.1.0-next.2+90.sha-bef14cf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+88.sha-4e1d780');
+const VERSION = new Version('9.1.0-next.2+90.sha-bef14cf');
 
 /**
  * @fileoverview added by tsickle
