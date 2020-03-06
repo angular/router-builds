1c05c257ca0431bc918059c6491e9afb29aff633
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:14:29 +0000

    c06f9a1ce3 docs(zone.js): update comment and chapter format of ngzone doc. (#35738)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+92.sha-d912d4f
+ * @license Angular v9.1.0-next.2+93.sha-c06f9a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+92.sha-d912d4f');
+const VERSION = new Version('9.1.0-next.2+93.sha-c06f9a1');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+92.sha-d912d4f
+ * @license Angular v9.1.0-next.2+93.sha-c06f9a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
