85bef1a43ee23529dad3e3aca983a0e880d1d6aa
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 18:04:00 +0000

    59607dc495 fix(core): undecorated-classes-with-di migration should handle libraries generated with CLI versions past v6.2.0 (#35824)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+86.sha-a1b9061
+ * @license Angular v9.1.0-next.2+91.sha-59607dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+86.sha-a1b9061');
+const VERSION = new Version('9.1.0-next.2+91.sha-59607dc');
 
 /**
  * @fileoverview added by tsickle
