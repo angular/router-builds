e7c8810cb95d976ecf150b1c97b214d70e14a5ba
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:24:51 +0000

    86a45ff561 docs(zone.js): update coments of zone configuration API definition (#35849)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+94.sha-84c2458
+ * @license Angular v9.1.0-next.2+95.sha-86a45ff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+94.sha-84c2458');
+var VERSION = new Version('9.1.0-next.2+95.sha-86a45ff');
 
 /**
  * @license
