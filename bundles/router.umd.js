0ce001fd6dfd6037ce38af80da8449026d7a6e92
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:18:30 +0000

    84c2458ea7 refactor: cleanup zone.js test instruction (#34860)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+93.sha-c06f9a1
+ * @license Angular v9.1.0-next.2+94.sha-84c2458
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.2+93.sha-c06f9a1');
+    var VERSION = new i0.Version('9.1.0-next.2+94.sha-84c2458');
 
     /**
      * @license
