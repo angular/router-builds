018865837c31f0bf01949491047612724e33ff69
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:59:13 +0000

    958165888c fix(bazel): do not use manifest paths for generated imports within compilation unit (#35841)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+96.sha-82a6fc5
+ * @license Angular v9.1.0-next.2+98.sha-9581658
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+96.sha-82a6fc5
+ * @license Angular v9.1.0-next.2+98.sha-9581658
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.2+96.sha-82a6fc5');
+    var VERSION = new core.Version('9.1.0-next.2+98.sha-9581658');
 
     /**
      * @license
