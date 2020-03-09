0b5269c431825731e4e7b1641561fedab894aabd
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 18:48:23 +0000

    fc4c3c3eb5 fix(localize): merge translation from all  XLIFF `<file>` elements (#35936)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+111.sha-9ff9a07
+ * @license Angular v9.1.0-next.2+110.sha-fc4c3c3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.2+111.sha-9ff9a07');
+    var VERSION = new core.Version('9.1.0-next.2+110.sha-fc4c3c3');
 
     /**
      * @license
