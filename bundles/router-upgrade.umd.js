ba5c5b334cf2e14ff1ef01f0a77b3f350e0a75b5
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 21:55:53 +0000

    71309d223d Revert "feat: Monkey patches MessagePort onproperties (onmessage/onmessageerror) (#34610)" (#35973)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+112.sha-15482e7
+ * @license Angular v9.1.0-next.2+113.sha-71309d2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+112.sha-15482e7
+ * @license Angular v9.1.0-next.2+113.sha-71309d2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.2+112.sha-15482e7');
+    var VERSION = new i0.Version('9.1.0-next.2+113.sha-71309d2');
 
     /**
      * @license
