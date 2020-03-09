b3a16317d5ecfd62e06c52d3999da42f3de27683
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 16:45:25 +0000

    11f7e275e6 feat(compiler): type TemplateAst values as ASTWithSource (#35892)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+109.sha-1882451
+ * @license Angular v9.1.0-next.2+106.sha-11f7e27
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10207,7 +10207,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+109.sha-1882451');
+const VERSION = new Version('9.1.0-next.2+106.sha-11f7e27');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+109.sha-1882451
+ * @license Angular v9.1.0-next.2+106.sha-11f7e27
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
