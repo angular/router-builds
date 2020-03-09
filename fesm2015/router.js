3dc57b81ffe0ab22950c7baa341ed341315b6563
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 21:29:34 +0000

    15482e7367 Revert "feat(bazel): transform generated shims (in Ivy) with tsickle (#35848)" (#35970)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+110.sha-fc4c3c3
+ * @license Angular v9.1.0-next.2+112.sha-15482e7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10207,7 +10207,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+110.sha-fc4c3c3');
+const VERSION = new Version('9.1.0-next.2+112.sha-15482e7');
 
 /**
  * @fileoverview added by tsickle
