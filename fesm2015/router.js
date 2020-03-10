a3afa554ec802262a4ce4241306863bcdb18a476
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 19:02:43 +0000

    191e4d15b5 build: fix elements test failures on IE (#35940)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+125.sha-0bf6e58
+ * @license Angular v9.1.0-next.2+124.sha-191e4d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10207,7 +10207,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+125.sha-0bf6e58');
+const VERSION = new Version('9.1.0-next.2+124.sha-191e4d1');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+125.sha-0bf6e58
+ * @license Angular v9.1.0-next.2+124.sha-191e4d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+125.sha-0bf6e58');
+var VERSION = new Version('9.1.0-next.2+124.sha-191e4d1');
 
 /**
  * @license
