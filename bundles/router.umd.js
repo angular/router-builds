2a5c56ae9bdc82acb5cebd9a49e5ce7ba0d44ddf
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:13:53 +0000

    fc71032dc4 test(router): use pageYOffset in testing when scrollY is not available (#35976)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+117.sha-81cb54f
+ * @license Angular v9.1.0-next.2+118.sha-fc71032
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.2+117.sha-81cb54f');
+    var VERSION = new core.Version('9.1.0-next.2+118.sha-fc71032');
 
     /**
      * @license
