cbde3094f1cc3ea92d858f236cd5be29881ffbcf
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 19:01:00 +0000

    191e4d15b5 build: fix elements test failures on IE (#35940)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+125.sha-0bf6e58
+ * @license Angular v9.1.0-next.2+124.sha-191e4d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+125.sha-0bf6e58
+ * @license Angular v9.1.0-next.2+124.sha-191e4d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.2+125.sha-0bf6e58');
+    var VERSION = new core.Version('9.1.0-next.2+124.sha-191e4d1');
 
     /**
      * @license
