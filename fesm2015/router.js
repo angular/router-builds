0d97851d4ee163cc54c3ee7982d1594ef9d3c3fe
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:15:58 +0000

    64d6f13b87 fix(platform-browser): add missing peerDependency on `@angular/animations` (#35949)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+120.sha-287bfef
+ * @license Angular v9.1.0-next.2+119.sha-64d6f13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+120.sha-287bfef');
+const VERSION = new Version('9.1.0-next.2+119.sha-64d6f13');
 
 /**
  * @fileoverview added by tsickle
