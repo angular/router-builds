8f2786daeea2df8357cbd249882591d10a0fb925
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 19:01:18 +0000

    983f48136a test(compiler): add a public API guard for the public compiler options (#35885)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+119.sha-64d6f13
+ * @license Angular v9.1.0-next.2+122.sha-983f481
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+119.sha-64d6f13
+ * @license Angular v9.1.0-next.2+122.sha-983f481
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.2+119.sha-64d6f13');
+    var VERSION = new i0.Version('9.1.0-next.2+122.sha-983f481');
 
     /**
      * @license
