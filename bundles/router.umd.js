097f958a154aa9c1709eab0117817ea5c70d2fe5
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:14:37 +0000

    81cb54fc15 refactor(compiler): optionalOperator -> consumeOptionalOperator (#35980)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+114.sha-a73e125
+ * @license Angular v9.1.0-next.2+117.sha-81cb54f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.2+114.sha-a73e125');
+    var VERSION = new i0.Version('9.1.0-next.2+117.sha-81cb54f');
 
     /**
      * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+114.sha-a73e125
+ * @license Angular v9.1.0-next.2+117.sha-81cb54f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
