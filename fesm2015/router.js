822051a22c4cd36bfeb85f0ad0345ed5380ff462
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 19:01:25 +0000

    e4b1e6c622 build: Add pullapprove verification tool to dev-infra-private package (#35911)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+122.sha-983f481
+ * @license Angular v9.1.0-next.2+127.sha-e4b1e6c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10207,7 +10207,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+122.sha-983f481');
+const VERSION = new Version('9.1.0-next.2+127.sha-e4b1e6c');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+122.sha-983f481
+ * @license Angular v9.1.0-next.2+127.sha-e4b1e6c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
