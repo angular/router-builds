a6aa3df4e62fc7ed52e4564c53ed6dee5d4a1f34
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 22:06:56 +0000

    c98c6e80c8 build: adding a script to compare commits in master and stable branches (#35130)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+124.sha-191e4d1
+ * @license Angular v9.1.0-next.2+128.sha-c98c6e8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9995,7 +9995,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+124.sha-191e4d1');
+const VERSION = new Version('9.1.0-next.2+128.sha-c98c6e8');
 
 /**
  * @fileoverview added by tsickle
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+124.sha-191e4d1
+ * @license Angular v9.1.0-next.2+128.sha-c98c6e8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
