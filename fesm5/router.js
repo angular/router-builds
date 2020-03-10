e4c8cef96074e54fa182405866d75beac0539e86
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 22:15:51 +0000

    95c729f5d1 build: typescript 3.8 support (#35864)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+128.sha-c98c6e8
+ * @license Angular v9.1.0-next.2+129.sha-95c729f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+128.sha-c98c6e8');
+var VERSION = new Version('9.1.0-next.2+129.sha-95c729f');
 
 /**
  * @license
