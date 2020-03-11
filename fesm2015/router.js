025d24b0f5095ce8cd9f52dec43b2268aea28872
Andrew Kushnir <akushnir@google.com>
2020-03-11 22:30:21 +0000

    99ea5d7044 fix(zone.js): fix `zone-patch-rxjs` bundle to refer to `rxjs` (rather than include) it. (#35983)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+33.sha-8ea61a1
+ * @license Angular v9.1.0-next.4+34.sha-99ea5d7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+33.sha-8ea61a1');
+const VERSION = new Version('9.1.0-next.4+34.sha-99ea5d7');
 
 /**
  * @fileoverview added by tsickle
