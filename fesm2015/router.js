dd09a7005296e9b3254770f3f117e4a061541944
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:22:00 +0000

    32f099aa36 fix(bazel): update several packages for better windows support (#35991)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+23.sha-20f0116
+ * @license Angular v9.1.0-next.4+25.sha-32f099a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10206,7 +10206,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+23.sha-20f0116');
+const VERSION = new Version('9.1.0-next.4+25.sha-32f099a');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+23.sha-20f0116
+ * @license Angular v9.1.0-next.4+25.sha-32f099a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+23.sha-20f0116');
+var VERSION = new Version('9.1.0-next.4+25.sha-32f099a');
 
 /**
  * @license
