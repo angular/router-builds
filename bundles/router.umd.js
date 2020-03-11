a12e34c97ea0d5f2556cbffb4f03c4adc67a4647
Andrew Kushnir <akushnir@google.com>
2020-03-11 23:06:56 +0000

    2b6028b643 build: remove legacy integration test runner (#35985)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+34.sha-99ea5d7
+ * @license Angular v9.1.0-next.4+35.sha-2b6028b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.4+34.sha-99ea5d7');
+    var VERSION = new core.Version('9.1.0-next.4+35.sha-2b6028b');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+34.sha-99ea5d7
+ * @license Angular v9.1.0-next.4+35.sha-2b6028b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
