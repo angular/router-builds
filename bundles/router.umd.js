4f63d1c80204cc01a41ab2cfc3e5f475856ede6d
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:13:38 +0000

    10f1e94f18 build(bazel): remove hacky fake @angular/bazel npm package under tools/npm/@angular_bazel (#36009)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+15.sha-5f7d066
+ * @license Angular v9.1.0-next.4+12.sha-10f1e94
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.4+15.sha-5f7d066');
+    var VERSION = new core.Version('9.1.0-next.4+12.sha-10f1e94');
 
     /**
      * @license
