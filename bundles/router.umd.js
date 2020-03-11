f03b48cd4b1956b1b82bc6cab730773674147ba9
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 01:43:33 +0000

    15f8afa4bf ci: move public-api goldens to goldens directory (#35768)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+7.sha-13495c6
+ * @license Angular v9.1.0-next.4+5.sha-15f8afa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.4+7.sha-13495c6');
+    var VERSION = new i0.Version('9.1.0-next.4+5.sha-15f8afa');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+7.sha-13495c6
+ * @license Angular v9.1.0-next.4+5.sha-15f8afa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
