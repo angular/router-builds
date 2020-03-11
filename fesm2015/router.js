193bbe9664c6cb442adc5178d98f7596a9ee0c88
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 01:41:31 +0000

    15f8afa4bf ci: move public-api goldens to goldens directory (#35768)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+7.sha-13495c6
+ * @license Angular v9.1.0-next.4+5.sha-15f8afa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+7.sha-13495c6');
+const VERSION = new Version('9.1.0-next.4+5.sha-15f8afa');
 
 /**
  * @fileoverview added by tsickle
