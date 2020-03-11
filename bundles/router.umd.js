1d1c286a64c0b04604198c5d8eeafd81b6715619
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:11:24 +0000

    1f89c6130e fix(ngcc): show helpful error when providing an invalid option (#36010)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+8.sha-42375c4
+ * @license Angular v9.1.0-next.4+11.sha-1f89c61
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.4+8.sha-42375c4');
+    var VERSION = new core.Version('9.1.0-next.4+11.sha-1f89c61');
 
     /**
      * @license
