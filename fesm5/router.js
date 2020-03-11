4eaa06d32df57044a861de5df54e1cf4164ced40
Andrew Kushnir <akushnir@google.com>
2020-03-11 22:29:38 +0000

    8ea61a19cd feat(ngcc): support invalidating the entry-point manifest (#35931)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+28.sha-5af0e75
+ * @license Angular v9.1.0-next.4+33.sha-8ea61a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+28.sha-5af0e75');
+var VERSION = new Version('9.1.0-next.4+33.sha-8ea61a1');
 
 /**
  * @license
