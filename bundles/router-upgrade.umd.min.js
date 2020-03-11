65f501f1cdf1d8bb5f9ed7efffa0def6b7923293
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:12:34 +0000

    5e3a898f10 fix(bazel): update typescript peer dependency range (#36013)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+13.sha-49478f5
+ * @license Angular v9.1.0-next.4+14.sha-5e3a898
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+13.sha-49478f5
+ * @license Angular v9.1.0-next.4+14.sha-5e3a898
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+13.sha-49478f5');
+var VERSION = new Version('9.1.0-next.4+14.sha-5e3a898');
 
 /**
  * @license
