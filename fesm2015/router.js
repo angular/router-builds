13bf2ba26c2ccf29ddde3194480f01254c0cfeb1
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:20:05 +0000

    77da6887f7 build(dev-infra): handle async failures in `get-data.ts` (#36007)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+22.sha-d8f9ec5
+ * @license Angular v9.1.0-next.4+16.sha-77da688
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10206,7 +10206,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+22.sha-d8f9ec5');
+const VERSION = new Version('9.1.0-next.4+16.sha-77da688');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+22.sha-d8f9ec5
+ * @license Angular v9.1.0-next.4+16.sha-77da688
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+22.sha-d8f9ec5');
+var VERSION = new Version('9.1.0-next.4+16.sha-77da688');
 
 /**
  * @license
