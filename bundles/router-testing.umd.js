9a2ee2432b91953c15149116426ed990b6f0416b
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:12:26 +0000

    49478f5e19 docs: Added ngVikings 2020 on angular.io events (#35129)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+17.sha-b99b80c
+ * @license Angular v9.1.0-next.4+13.sha-49478f5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+17.sha-b99b80c
+ * @license Angular v9.1.0-next.4+13.sha-49478f5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.4+17.sha-b99b80c');
+    var VERSION = new core.Version('9.1.0-next.4+13.sha-49478f5');
 
     /**
      * @license
