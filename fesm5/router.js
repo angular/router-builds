df2cd30e7ea7338db560468aba605b8dc6b0646b
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:17:37 +0000

    d8f9ec5291 refactor(docs-infra): merge `.api-body` style blocks (#31574)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+18.sha-9a12403
+ * @license Angular v9.1.0-next.4+22.sha-d8f9ec5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+18.sha-9a12403');
+var VERSION = new Version('9.1.0-next.4+22.sha-d8f9ec5');
 
 /**
  * @license
