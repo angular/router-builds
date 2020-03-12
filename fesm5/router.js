1231e3cb16eed189223e3516941caf7ef90e0b71
Andrew Kushnir <akushnir@google.com>
2020-03-12 17:12:40 +0000

    37a48391f2 refactor(ngcc): remove unused `LockFileWithSignalHandlers` (#35938)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+36.sha-0d806c8
+ * @license Angular v9.1.0-next.4+37.sha-37a4839
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+36.sha-0d806c8');
+var VERSION = new Version('9.1.0-next.4+37.sha-37a4839');
 
 /**
  * @license
