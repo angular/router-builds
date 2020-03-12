7542eb29da2281eccc4a0a1770a94cae5a6a11f3
Andrew Kushnir <akushnir@google.com>
2020-03-12 17:10:59 +0000

    37a48391f2 refactor(ngcc): remove unused `LockFileWithSignalHandlers` (#35938)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+36.sha-0d806c8
+ * @license Angular v9.1.0-next.4+37.sha-37a4839
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.4+36.sha-0d806c8');
+    var VERSION = new core.Version('9.1.0-next.4+37.sha-37a4839');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+36.sha-0d806c8
+ * @license Angular v9.1.0-next.4+37.sha-37a4839
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
