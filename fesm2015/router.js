a8e64c05a470bf83505d918dfa8e8268f8d08760
Andrew Kushnir <akushnir@google.com>
2020-03-12 17:15:24 +0000

    372a56a0c9 revert: "ci: prevent api golden tests on windows CI (#36025)" (#36034)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+38.sha-43635a6
+ * @license Angular v9.1.0-next.4+40.sha-372a56a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+38.sha-43635a6');
+const VERSION = new Version('9.1.0-next.4+40.sha-372a56a');
 
 /**
  * @fileoverview added by tsickle
