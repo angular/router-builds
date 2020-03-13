b2fe68f7c8679bb7cadc4242bb48621fa063d9e7
Andrew Kushnir <akushnir@google.com>
2020-03-13 15:45:29 +0000

    6b4c5289b3 fix(docs-infra): remove `routerLink` from `top-bar` (#35951)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+50.sha-db74f20
+ * @license Angular v9.1.0-next.4+51.sha-6b4c528
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+50.sha-db74f20');
+const VERSION = new Version('9.1.0-next.4+51.sha-6b4c528');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+50.sha-db74f20
+ * @license Angular v9.1.0-next.4+51.sha-6b4c528
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+50.sha-db74f20');
+var VERSION = new Version('9.1.0-next.4+51.sha-6b4c528');
 
 /**
  * @license
