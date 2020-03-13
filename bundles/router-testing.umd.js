f720eae0eb51b8a2aca56df768c52ad3b422e284
Andrew Kushnir <akushnir@google.com>
2020-03-13 15:47:13 +0000

    6b4c5289b3 fix(docs-infra): remove `routerLink` from `top-bar` (#35951)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+50.sha-db74f20
+ * @license Angular v9.1.0-next.4+51.sha-6b4c528
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+50.sha-db74f20
+ * @license Angular v9.1.0-next.4+51.sha-6b4c528
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.4+50.sha-db74f20');
+    var VERSION = new i0.Version('9.1.0-next.4+51.sha-6b4c528');
 
     /**
      * @license
