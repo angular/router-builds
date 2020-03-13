1f6120639ef29ca5b60695a7886de294791f322c
Andrew Kushnir <akushnir@google.com>
2020-03-13 19:55:07 +0000

    05d0586223 test(core): Update transplanted views benchpress test to support VE (#36058)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+51.sha-6b4c528
+ * @license Angular v9.1.0-next.4+52.sha-05d0586
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+51.sha-6b4c528');
+const VERSION = new Version('9.1.0-next.4+52.sha-05d0586');
 
 /**
  * @fileoverview added by tsickle
