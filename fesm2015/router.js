c496c3f23366bb631ef4b2c5a35c44fdd8eea31b
Andrew Kushnir <akushnir@google.com>
2020-03-13 15:26:20 +0000

    db74f2074b build: enable forms tests on IE (#35971)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+49.sha-772bb5e
+ * @license Angular v9.1.0-next.4+50.sha-db74f20
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10206,7 +10206,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+49.sha-772bb5e');
+const VERSION = new Version('9.1.0-next.4+50.sha-db74f20');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+49.sha-772bb5e
+ * @license Angular v9.1.0-next.4+50.sha-db74f20
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
