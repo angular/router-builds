4043f0ae06d4c94fd316edc6115154838713d1c7
Andrew Kushnir <akushnir@google.com>
2020-03-16 16:22:27 +0000

    8456c5ec60 feat(zone.js): add a zone config to allow user disable wrapping uncaught promise rejection (#35873)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+52.sha-05d0586
+ * @license Angular v9.1.0-next.4+54.sha-8456c5e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10206,7 +10206,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+52.sha-05d0586');
+const VERSION = new Version('9.1.0-next.4+54.sha-8456c5e');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+52.sha-05d0586
+ * @license Angular v9.1.0-next.4+54.sha-8456c5e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
