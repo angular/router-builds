bd92b2bbd18d9dd5062a3ce865dec9ee9755c411
Andrew Kushnir <akushnir@google.com>
2020-03-16 23:11:20 +0000

    e552591768 release: cut the zone.js-0.10.3 release (#34508)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+60.sha-46fe8fb
+ * @license Angular v9.1.0-next.4+61.sha-e552591
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.4+60.sha-46fe8fb');
+    var VERSION = new i0.Version('9.1.0-next.4+61.sha-e552591');
 
     /**
      * @license
