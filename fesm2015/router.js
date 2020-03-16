9614c573efb1d45ed2b479d000a09801d06ce551
Andrew Kushnir <akushnir@google.com>
2020-03-16 16:22:43 +0000

    0f8e710c7c feat(zone.js): Monkey patch MessagePort.prototype onproperties (#34610)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+54.sha-8456c5e
+ * @license Angular v9.1.0-next.4+53.sha-0f8e710
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+54.sha-8456c5e');
+const VERSION = new Version('9.1.0-next.4+53.sha-0f8e710');
 
 /**
  * @fileoverview added by tsickle
