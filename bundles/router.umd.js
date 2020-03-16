dc771b9066b3736d54ec244a0bf443374a3e8dbf
Andrew Kushnir <akushnir@google.com>
2020-03-16 18:22:03 +0000

    46fe8fb8b4 build: depend on bazelisk rather than directly on Bazel (#36078)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+59.sha-31bec8c
+ * @license Angular v9.1.0-next.4+60.sha-46fe8fb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.4+59.sha-31bec8c');
+    var VERSION = new core.Version('9.1.0-next.4+60.sha-46fe8fb');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+59.sha-31bec8c
+ * @license Angular v9.1.0-next.4+60.sha-46fe8fb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+59.sha-31bec8c');
+const VERSION = new Version('9.1.0-next.4+60.sha-46fe8fb');
 
 /**
  * @fileoverview added by tsickle
