bb03027954db3f2ae3c03ca2b21a77d69f68e4c8
Andrew Kushnir <akushnir@google.com>
2020-03-17 16:53:10 +0000

    52d66ea5a9 feat(dev-infra): create common config file loading util (#36091)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+63.sha-acc483e
+ * @license Angular v9.1.0-next.4+65.sha-52d66ea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+63.sha-acc483e');
+const VERSION = new Version('9.1.0-next.4+65.sha-52d66ea');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+63.sha-acc483e
+ * @license Angular v9.1.0-next.4+65.sha-52d66ea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
