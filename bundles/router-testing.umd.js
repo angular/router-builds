ba7082b963cc37639e0c0ea592486ce7f55e2258
Andrew Kushnir <akushnir@google.com>
2020-03-17 19:46:33 +0000

    22be967104 ci: run ts-circular-deps check in lint job on ci (#35647)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+70.sha-34a17f3
+ * @license Angular v9.1.0-next.4+75.sha-22be967
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+70.sha-34a17f3
+ * @license Angular v9.1.0-next.4+75.sha-22be967
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+70.sha-34a17f3');
+const VERSION = new Version('9.1.0-next.4+75.sha-22be967');
 
 /**
  * @fileoverview added by tsickle
