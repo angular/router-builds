cecb7fb591f3ede0edd4f1146de119e467315250
Andrew Kushnir <akushnir@google.com>
2020-03-17 17:39:31 +0000

    e3ecdc6a63 feat(bazel): transform generated shims (in Ivy) with tsickle (#35975)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+66.sha-4af2a06
+ * @license Angular v9.1.0-next.4+67.sha-e3ecdc6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+66.sha-4af2a06');
+const VERSION = new Version('9.1.0-next.4+67.sha-e3ecdc6');
 
 /**
  * @fileoverview added by tsickle
