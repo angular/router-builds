4d1f541f58e8a18fd70e957567ce52591ef143be
Andrew Kushnir <akushnir@google.com>
2020-03-17 16:50:33 +0000

    61c37954c6 docs: remove browserstack badge from readme (#35684)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+62.sha-168a393
+ * @license Angular v9.1.0-next.4+64.sha-61c3795
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.4+62.sha-168a393');
+    var VERSION = new core.Version('9.1.0-next.4+64.sha-61c3795');
 
     /**
      * @license
