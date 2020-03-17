7393485a34a2a339501c3087cd3546c591a0d036
Andrew Kushnir <akushnir@google.com>
2020-03-17 20:58:04 +0000

    9e70bcb34f fix(ngcc): consistently delegate to TypeScript host for typing files (#36089)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+78.sha-a1e00f8
+ * @license Angular v9.1.0-next.4+80.sha-9e70bcb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+78.sha-a1e00f8
+ * @license Angular v9.1.0-next.4+80.sha-9e70bcb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.4+78.sha-a1e00f8');
+    var VERSION = new core.Version('9.1.0-next.4+80.sha-9e70bcb');
 
     /**
      * @license
