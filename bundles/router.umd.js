8dd7594180ce8b8869a09630e9232b91c50db062
Andrew Kushnir <akushnir@google.com>
2020-03-17 00:06:10 +0000

    168a393589 refactor(core): misc refactoring to support upcoming changes to improve synthetic host bindings (#35884)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+61.sha-e552591
+ * @license Angular v9.1.0-next.4+62.sha-168a393
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.4+61.sha-e552591');
+    var VERSION = new core.Version('9.1.0-next.4+62.sha-168a393');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+61.sha-e552591
+ * @license Angular v9.1.0-next.4+62.sha-168a393
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+61.sha-e552591');
+const VERSION = new Version('9.1.0-next.4+62.sha-168a393');
 
 /**
  * @fileoverview added by tsickle
