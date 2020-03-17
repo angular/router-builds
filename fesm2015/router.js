e53f388beedd6a8604aab37522532bbeae368823
Andrew Kushnir <akushnir@google.com>
2020-03-17 17:01:19 +0000

    4af2a068c5 build: update to latest `@bazel/ibazel` version (#36097)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+65.sha-52d66ea
+ * @license Angular v9.1.0-next.4+66.sha-4af2a06
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10206,7 +10206,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+65.sha-52d66ea');
+const VERSION = new Version('9.1.0-next.4+66.sha-4af2a06');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+65.sha-52d66ea
+ * @license Angular v9.1.0-next.4+66.sha-4af2a06
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
