108e1dc94d258b3d3420ccec88b2b28bef077ea9
Andrew Kushnir <akushnir@google.com>
2020-03-18 23:19:01 +0000

    d50881a86e docs: move asset management section (#35665)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.5+6.sha-c9f554c
+ * @license Angular v9.1.0-next.5+1.sha-d50881a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.5+6.sha-c9f554c');
+    var VERSION = new core.Version('9.1.0-next.5+1.sha-d50881a');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.5+6.sha-c9f554c
+ * @license Angular v9.1.0-next.5+1.sha-d50881a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
