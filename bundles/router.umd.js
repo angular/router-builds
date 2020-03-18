a58221890081f09f9baf38607a41f81591429a3f
Andrew Kushnir <akushnir@google.com>
2020-03-18 22:58:06 +0000

    c1a0f0617c release: cut the v9.1.0-next.5 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+104.sha-13f7631
+ * @license Angular v9.1.0-next.5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.4+104.sha-13f7631');
+    var VERSION = new core.Version('9.1.0-next.5');
 
     /**
      * @license
