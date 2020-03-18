b842df9661f0500707ab991dcdbdf0099215a26e
Andrew Kushnir <akushnir@google.com>
2020-03-18 17:20:49 +0000

    67b9adfd1a docs(upgrade): remove outdated/non-applicable AOT section from upgrade guide (#36015)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+80.sha-9e70bcb
+ * @license Angular v9.1.0-next.4+101.sha-67b9adf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+80.sha-9e70bcb');
+const VERSION = new Version('9.1.0-next.4+101.sha-67b9adf');
 
 /**
  * @fileoverview added by tsickle
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+80.sha-9e70bcb
+ * @license Angular v9.1.0-next.4+101.sha-67b9adf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
