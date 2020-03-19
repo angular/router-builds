5cbbd9478fdab0d1f622c0c60637f78af500ac41
Andrew Kushnir <akushnir@google.com>
2020-03-19 16:29:09 +0000

    e342ffd855 fix(core): adhere to bootstrap options for JIT compiled components (#35534)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.5+9.sha-fb92f5d
+ * @license Angular v9.1.0-next.5+8.sha-e342ffd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.5+9.sha-fb92f5d
+ * @license Angular v9.1.0-next.5+8.sha-e342ffd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-next.5+9.sha-fb92f5d');
+    var VERSION = new i0.Version('9.1.0-next.5+8.sha-e342ffd');
 
     /**
      * @license
