81a1a11f570a7b05526651ad8a48bbf052eb8840
Misko Hevery <misko@hevery.com>
2020-03-20 17:48:27 +0000

    df890d7629 fix(compiler): record correct end of expression (#34690)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+4.sha-989dea7
+ * @license Angular v9.1.0-rc.0+2.sha-df890d7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10206,7 +10206,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+4.sha-989dea7');
+const VERSION = new Version('9.1.0-rc.0+2.sha-df890d7');
 
 /**
  * @fileoverview added by tsickle
