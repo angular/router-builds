8457b524cd4f24342df615f74243bd3ff1cdf019
Misko Hevery <misko@hevery.com>
2020-03-20 21:22:38 +0000

    528e25a81a build(docs-infra): upgrade cli command docs sources to f5dd5b16a (#36159)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+11.sha-d25012e
+ * @license Angular v9.1.0-rc.0+12.sha-528e25a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+11.sha-d25012e
+ * @license Angular v9.1.0-rc.0+12.sha-528e25a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+11.sha-d25012e');
+const VERSION = new Version('9.1.0-rc.0+12.sha-528e25a');
 
 /**
  * @fileoverview added by tsickle
