e3e97f1d7944e139ec757c202d31d575119b9283
Misko Hevery <misko@hevery.com>
2020-03-20 17:47:09 +0000

    8968b206b4 feat(benchpress): create component_benchmark macro (#35692)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+1.sha-47bfec4
+ * @license Angular v9.1.0-rc.0+3.sha-8968b20
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+1.sha-47bfec4');
+var VERSION = new Version('9.1.0-rc.0+3.sha-8968b20');
 
 /**
  * @license
