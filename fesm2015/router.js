94b9d662c256a817ae730e81bcf90f513168b8f9
Misko Hevery <misko@hevery.com>
2020-03-20 17:36:48 +0000

    47bfec4e46 feat(core): add `isPromise` generic (#34168)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0
+ * @license Angular v9.1.0-rc.0+1.sha-47bfec4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0');
+const VERSION = new Version('9.1.0-rc.0+1.sha-47bfec4');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0
+ * @license Angular v9.1.0-rc.0+1.sha-47bfec4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0');
+var VERSION = new Version('9.1.0-rc.0+1.sha-47bfec4');
 
 /**
  * @license
