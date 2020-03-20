8b9f83179646317a8b9131b05862b1cbaa3dd0c3
Misko Hevery <misko@hevery.com>
2020-03-20 20:13:09 +0000

    05eeb7d279 test(compiler): remove whitespace in spans (#36169)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0
+ * @license Angular v9.1.0-rc.0+8.sha-05eeb7d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0');
+const VERSION = new Version('9.1.0-rc.0+8.sha-05eeb7d');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0
+ * @license Angular v9.1.0-rc.0+8.sha-05eeb7d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
