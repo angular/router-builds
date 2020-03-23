51be5a4159041055342ceedc7ec1b3d51c779fcb
Misko Hevery <misko@hevery.com>
2020-03-23 18:59:50 +0000

    fced8ee40e fix(localize): allow ICU expansion case to start with any character except `}` (#36123)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+16.sha-e81ad3a
+ * @license Angular v9.1.0-rc.0+19.sha-fced8ee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+16.sha-e81ad3a
+ * @license Angular v9.1.0-rc.0+19.sha-fced8ee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-rc.0+16.sha-e81ad3a');
+    var VERSION = new core.Version('9.1.0-rc.0+19.sha-fced8ee');
 
     /**
      * @license
