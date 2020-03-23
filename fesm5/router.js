9c8804b7e5ad14a81db63f34bc40d0ed85b29917
Misko Hevery <misko@hevery.com>
2020-03-23 19:44:04 +0000

    f71d132f7c fix(core): workaround Terser inlining bug (#36200)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+19.sha-ba3edda
+ * @license Angular v9.1.0-rc.0+20.sha-f71d132
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+19.sha-ba3edda');
+var VERSION = new Version('9.1.0-rc.0+20.sha-f71d132');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+19.sha-ba3edda
+ * @license Angular v9.1.0-rc.0+20.sha-f71d132
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
