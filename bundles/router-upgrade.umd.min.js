73578d4471f1a31bf7ea921475aed8962909ae7c
Misko Hevery <misko@hevery.com>
2020-03-24 23:42:56 +0000

    4f66250618 docs: release notes for the v9.1.0-rc.2 release

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+34.sha-d783519
+ * @license Angular v9.1.0-rc.0+35.sha-4f66250
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+34.sha-d783519
+ * @license Angular v9.1.0-rc.0+35.sha-4f66250
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+34.sha-d783519');
+const VERSION = new Version('9.1.0-rc.0+35.sha-4f66250');
 
 /**
  * @fileoverview added by tsickle
