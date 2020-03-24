7fe14260c2b104e4aea45b595974e9d4099ef03b
Misko Hevery <misko@hevery.com>
2020-03-24 17:39:51 +0000

    ae28d7c0b2 build(docs-infra): upgrade cli command docs sources to 56c648827 (#36224)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+22.sha-58f4254
+ * @license Angular v9.1.0-rc.0+27.sha-ae28d7c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+22.sha-58f4254');
+var VERSION = new Version('9.1.0-rc.0+27.sha-ae28d7c');
 
 /**
  * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+22.sha-58f4254
+ * @license Angular v9.1.0-rc.0+27.sha-ae28d7c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
