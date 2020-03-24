d9668fa15d7cfe342d80d21e017933864646a2c8
Misko Hevery <misko@hevery.com>
2020-03-24 17:37:40 +0000

    380de1e7b4 fix(ngcc): use path-mappings from tsconfig in dependency resolution (#36180)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+21.sha-4419907
+ * @license Angular v9.1.0-rc.0+26.sha-380de1e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+21.sha-4419907');
+const VERSION = new Version('9.1.0-rc.0+26.sha-380de1e');
 
 /**
  * @fileoverview added by tsickle
