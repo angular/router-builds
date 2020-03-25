8dcd8c6cc068b8c15ac314961002c89609b9a9ab
Misko Hevery <misko@hevery.com>
2020-03-25 17:02:42 +0000

    b6bd8d7572 release: cut the v9.1.0 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.2
+ * @license Angular v9.1.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.2');
+const VERSION = new Version('9.1.0');
 
 /**
  * @fileoverview added by tsickle
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.2
+ * @license Angular v9.1.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
