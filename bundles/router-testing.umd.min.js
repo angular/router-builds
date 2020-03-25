9cd1bd938da55e69647d2da59a78fb6243b5aeac
Misko Hevery <misko@hevery.com>
2020-03-25 21:44:13 +0000

    294c6297d7 docs: coalesce release notes for the v9.1.0 release (#36247)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+1.sha-58fc65d
+ * @license Angular v9.1.0+2.sha-294c629
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+1.sha-58fc65d
+ * @license Angular v9.1.0+2.sha-294c629
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0+1.sha-58fc65d');
+var VERSION = new Version('9.1.0+2.sha-294c629');
 
 /**
  * @license
