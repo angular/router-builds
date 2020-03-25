aac4aa6e3faf216a3229bf8d1383fd7ef2b71ae5
Misko Hevery <misko@hevery.com>
2020-03-25 19:15:55 +0000

    d37dad82f1 build: ensure that refs and shas for PRs only need to be requested once (#36207)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+36.sha-22710fc
+ * @license Angular v9.1.0-rc.0+37.sha-d37dad8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-rc.0+36.sha-22710fc');
+    var VERSION = new i0.Version('9.1.0-rc.0+37.sha-d37dad8');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+36.sha-22710fc
+ * @license Angular v9.1.0-rc.0+37.sha-d37dad8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10206,7 +10206,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+36.sha-22710fc');
+const VERSION = new Version('9.1.0-rc.0+37.sha-d37dad8');
 
 /**
  * @fileoverview added by tsickle
