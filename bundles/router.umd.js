655bc6c290e34760225ad3cb9504f7821c228321
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-27 18:25:24 +0000

    1c385a1c22 build(docs-infra): do not include CI-specific config in docs examples ZIP archives (#36018)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+46.sha-bfa7b1a
+ * @license Angular v9.1.0-rc.0+44.sha-1c385a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-rc.0+46.sha-bfa7b1a');
+    var VERSION = new i0.Version('9.1.0-rc.0+44.sha-1c385a1');
 
     /**
      * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+46.sha-bfa7b1a
+ * @license Angular v9.1.0-rc.0+44.sha-1c385a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
