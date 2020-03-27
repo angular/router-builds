511925bb665da8b6419dd66d985156a066f7eeb9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-27 18:23:26 +0000

    1c385a1c22 build(docs-infra): do not include CI-specific config in docs examples ZIP archives (#36018)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+46.sha-bfa7b1a
+ * @license Angular v9.1.0-rc.0+44.sha-1c385a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+46.sha-bfa7b1a');
+const VERSION = new Version('9.1.0-rc.0+44.sha-1c385a1');
 
 /**
  * @fileoverview added by tsickle
