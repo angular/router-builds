af3b0c51d88456ed4411260e40a5546d567b5774
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 18:56:03 +0000

    c6dd900f60 fix(ngcc): do not write entry-point manifest outside node_modules (#36299)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+55.sha-c94a33c
+ * @license Angular v9.1.0-rc.0+58.sha-c6dd900
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+55.sha-c94a33c');
+const VERSION = new Version('9.1.0-rc.0+58.sha-c6dd900');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+55.sha-c94a33c
+ * @license Angular v9.1.0-rc.0+58.sha-c6dd900
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+55.sha-c94a33c');
+var VERSION = new Version('9.1.0-rc.0+58.sha-c6dd900');
 
 /**
  * @license
