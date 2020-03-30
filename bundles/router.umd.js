420c919f1efe1bf4c50143b6393d38e408127fc2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 18:57:53 +0000

    c6dd900f60 fix(ngcc): do not write entry-point manifest outside node_modules (#36299)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+55.sha-c94a33c
+ * @license Angular v9.1.0-rc.0+58.sha-c6dd900
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-rc.0+55.sha-c94a33c');
+    var VERSION = new i0.Version('9.1.0-rc.0+58.sha-c6dd900');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+55.sha-c94a33c
+ * @license Angular v9.1.0-rc.0+58.sha-c6dd900
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
