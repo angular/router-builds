94f17600b9a89b2008b25cb33f4e2f11d71d1a4f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-31 18:22:54 +0000

    c5df9ce474 build(zone.js): update zone.js version to 0.10.3 (#36214)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+64.sha-b44f7b5
+ * @license Angular v9.1.0-rc.0+63.sha-c5df9ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-rc.0+64.sha-b44f7b5');
+    var VERSION = new core.Version('9.1.0-rc.0+63.sha-c5df9ce');
 
     /**
      * @license
