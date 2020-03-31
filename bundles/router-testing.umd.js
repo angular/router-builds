19e698295541cf3d0ab3785ee359d05a0ad9e971
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-31 18:24:37 +0000

    b44f7b5e16 fix(zone.js): fix 2 bluebird test cases for each/mapSeries (#36295)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+61.sha-80c6858
+ * @license Angular v9.1.0-rc.0+64.sha-b44f7b5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+61.sha-80c6858
+ * @license Angular v9.1.0-rc.0+64.sha-b44f7b5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-rc.0+61.sha-80c6858');
+    var VERSION = new i0.Version('9.1.0-rc.0+64.sha-b44f7b5');
 
     /**
      * @license
