268543127ae576f8d7adae554b56205a98f042d1
Kara Erickson <karakara@google.com>
2020-04-01 23:07:58 +0000

    75afd80ae8 refactor(compiler): add `@nocollapse` annotation using a synthetic comment (#35932)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+90.sha-e9de281
+ * @license Angular v9.1.0-rc.0+89.sha-75afd80
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5912,7 +5912,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-rc.0+90.sha-e9de281');
+    var VERSION = new i0.Version('9.1.0-rc.0+89.sha-75afd80');
 
     /**
      * @license
