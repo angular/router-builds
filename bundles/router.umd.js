808f675e8f54b87b2ea605a00ad47a07e0f3a65a
Kara Erickson <karakara@google.com>
2020-04-01 23:01:45 +0000

    e9de28111d build: enable service-worker tests on saucelabs (#36129)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+88.sha-a5eb0e5
+ * @license Angular v9.1.0-rc.0+90.sha-e9de281
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5948,7 +5948,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-rc.0+88.sha-a5eb0e5');
+    var VERSION = new core.Version('9.1.0-rc.0+90.sha-e9de281');
 
     /**
      * @license
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+88.sha-a5eb0e5
+ * @license Angular v9.1.0-rc.0+90.sha-e9de281
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+88.sha-a5eb0e5');
+var VERSION = new Version('9.1.0-rc.0+90.sha-e9de281');
 
 /**
  * @license
