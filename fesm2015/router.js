ba1c37329ea5fd2d43262ce5f5b8a99e717ec0bf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-04-01 20:57:01 +0000

    cb0a2a055d fix(ngcc): allow ngcc configuration to match pre-release versions of packages (#36370)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+27.sha-beb3f19
+ * @license Angular v9.1.0+43.sha-cb0a2a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0+27.sha-beb3f19');
+const VERSION = new Version('9.1.0+43.sha-cb0a2a0');
 
 /**
  * @fileoverview added by tsickle
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+27.sha-beb3f19
+ * @license Angular v9.1.0+43.sha-cb0a2a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
