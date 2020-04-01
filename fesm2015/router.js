bf32c2497d50f87bf2b92941f93eb77ab3749942
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-04-01 20:40:19 +0000

    c5c57f6737 build: update to clang 1.4.0 and only run clang format on changed files (#36203)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+68.sha-4d1d0fa
+ * @license Angular v9.1.0-rc.0+72.sha-c5c57f6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+68.sha-4d1d0fa');
+const VERSION = new Version('9.1.0-rc.0+72.sha-c5c57f6');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+68.sha-4d1d0fa
+ * @license Angular v9.1.0-rc.0+72.sha-c5c57f6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
