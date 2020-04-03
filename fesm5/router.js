092267f3c1ad814941e6fa3f5281c852745f6b42
Kara Erickson <karakara@google.com>
2020-04-03 19:17:40 +0000

    ca25c957bf fix(ngcc): correctly detect imported TypeScript helpers (#36284)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+106.sha-6402a9a
+ * @license Angular v9.1.0-rc.0+105.sha-ca25c95
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+106.sha-6402a9a');
+var VERSION = new Version('9.1.0-rc.0+105.sha-ca25c95');
 
 /**
  * @license
