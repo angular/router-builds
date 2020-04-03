1b737641ff36fe900d4350be829641b8901bd344
Kara Erickson <karakara@google.com>
2020-04-03 19:05:44 +0000

    6402a9ae2a build: rebuild yarn lock from scratch (#36377)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+108.sha-a555fdb
+ * @license Angular v9.1.0-rc.0+106.sha-6402a9a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+108.sha-a555fdb');
+var VERSION = new Version('9.1.0-rc.0+106.sha-6402a9a');
 
 /**
  * @license
