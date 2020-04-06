671dcf9ee17f707f491eaeff4cc7d675af0be048
Kara Erickson <karakara@google.com>
2020-04-06 21:22:17 +0000

    09c84169d5 docs: fix broken guide link (tutorial/index --> tutorial) (#36453)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+122.sha-d43c306
+ * @license Angular v9.1.0-rc.0+123.sha-09c8416
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5965,7 +5965,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-rc.0+122.sha-d43c306');
+    var VERSION = new core.Version('9.1.0-rc.0+123.sha-09c8416');
 
     /**
      * @license
