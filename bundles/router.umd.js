6186cfbb0e1c5eb6b55bf1e141a069bb7a1332f8
Kara Erickson <karakara@google.com>
2020-04-06 18:59:12 +0000

    f9fb8338f5 fix(ngcc): support ignoring deep-imports via package config (#36423)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+114.sha-1b4df64
+ * @license Angular v9.1.0-rc.0+118.sha-f9fb833
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5929,7 +5929,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-rc.0+114.sha-1b4df64');
+    var VERSION = new i0.Version('9.1.0-rc.0+118.sha-f9fb833');
 
     /**
      * @license
