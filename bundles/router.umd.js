d8e9745c452c7cc1ae51220987eac0e1d4542350
Kara Erickson <karakara@google.com>
2020-04-06 20:45:26 +0000

    76a8cd57ae fix(ngcc): add process title (#36448)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+117.sha-6b3aa60
+ * @license Angular v9.1.0-rc.0+120.sha-76a8cd5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5929,7 +5929,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-rc.0+117.sha-6b3aa60');
+    var VERSION = new i0.Version('9.1.0-rc.0+120.sha-76a8cd5');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+117.sha-6b3aa60
+ * @license Angular v9.1.0-rc.0+120.sha-76a8cd5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10206,7 +10206,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+117.sha-6b3aa60');
+const VERSION = new Version('9.1.0-rc.0+120.sha-76a8cd5');
 
 /**
  * @fileoverview added by tsickle
