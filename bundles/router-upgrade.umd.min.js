a50f38ef084023434174a6e73f62ff2b53e5bbb8
Kara Erickson <karakara@google.com>
2020-04-06 19:02:32 +0000

    6b3aa60446 fix(ngcc): support simple `browser` property in entry-points (#36396)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+118.sha-f9fb833
+ * @license Angular v9.1.0-rc.0+117.sha-6b3aa60
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+118.sha-f9fb833
+ * @license Angular v9.1.0-rc.0+117.sha-6b3aa60
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5929,7 +5929,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.1.0-rc.0+118.sha-f9fb833');
+    var VERSION = new i0.Version('9.1.0-rc.0+117.sha-6b3aa60');
 
     /**
      * @license
