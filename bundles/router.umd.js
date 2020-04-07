a8f5153eaf86d958205b920ff40822165aae9c84
Kara Erickson <karakara@google.com>
2020-04-07 22:32:39 +0000

    81195a238b fix(language-service): use the `HtmlAst` to get the span of HTML tag (#36371)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+141.sha-4a18428
+ * @license Angular v9.1.0-rc.0+144.sha-81195a2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5970,7 +5970,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-rc.0+141.sha-4a18428');
+    var VERSION = new core.Version('9.1.0-rc.0+144.sha-81195a2');
 
     /**
      * @license
