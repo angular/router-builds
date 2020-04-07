d0fa69c8666f6f49cf7789942ccc1d4574510b97
Kara Erickson <karakara@google.com>
2020-04-07 17:43:36 +0000

    e145fa13b1 test(language-service): delete expression-cases.ts (#36468)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+131.sha-7d0af17
+ * @license Angular v9.1.0-rc.0+136.sha-e145fa1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10206,7 +10206,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+131.sha-7d0af17');
+const VERSION = new Version('9.1.0-rc.0+136.sha-e145fa1');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+131.sha-7d0af17
+ * @license Angular v9.1.0-rc.0+136.sha-e145fa1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5715,7 +5715,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+131.sha-7d0af17');
+var VERSION = new Version('9.1.0-rc.0+136.sha-e145fa1');
 
 /**
  * @license
