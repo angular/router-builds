0c49d26f40fb4dde713ba459bb4794dcefa303f0
Kara Erickson <karakara@google.com>
2020-04-07 22:34:25 +0000

    96a3de6364 style(docs-infra): removed extra , from _resources.scss file (#35935)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+144.sha-81195a2
+ * @license Angular v9.1.0-rc.0+143.sha-96a3de6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5970,7 +5970,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-rc.0+144.sha-81195a2');
+    var VERSION = new core.Version('9.1.0-rc.0+143.sha-96a3de6');
 
     /**
      * @license
