67faeab72995ec3bed69e7bca6fd499120663b5f
Kara Erickson <karakara@google.com>
2020-04-07 17:10:28 +0000

    f48a065db0 style(forms): reformat of the `forms` package after clang update (#36466)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+88.sha-aeb6d0d
+ * @license Angular v9.1.0+89.sha-f48a065
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+88.sha-aeb6d0d
+ * @license Angular v9.1.0+89.sha-f48a065
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0+88.sha-aeb6d0d');
+var VERSION = new Version('9.1.0+89.sha-f48a065');
 
 /**
  * @license
