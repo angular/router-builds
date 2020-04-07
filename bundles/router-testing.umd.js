ad068dc63df9a5cdab3a1224e33de040b72d7c70
Kara Erickson <karakara@google.com>
2020-04-07 22:46:45 +0000

    fd2ef74a31 docs: update reactive forms page (#35969)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+99.sha-b02c950
+ * @license Angular v9.1.0+106.sha-fd2ef74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+99.sha-b02c950
+ * @license Angular v9.1.0+106.sha-fd2ef74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9999,7 +9999,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0+99.sha-b02c950');
+const VERSION = new Version('9.1.0+106.sha-fd2ef74');
 
 /**
  * @fileoverview added by tsickle
