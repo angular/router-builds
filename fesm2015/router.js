f1c93cba0c6a402f458587b2fa6c4bbfb407b11d
Kara Erickson <karakara@google.com>
2020-04-07 17:51:25 +0000

    95fc3d4c5c fix(core): ngOnDestroy on multi providers called with incorrect context (#35840)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+136.sha-e145fa1
+ * @license Angular v9.1.0-rc.0+137.sha-95fc3d4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9994,7 +9994,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+136.sha-e145fa1');
+const VERSION = new Version('9.1.0-rc.0+137.sha-95fc3d4');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+136.sha-e145fa1
+ * @license Angular v9.1.0-rc.0+137.sha-95fc3d4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5751,7 +5751,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+136.sha-e145fa1');
+var VERSION = new Version('9.1.0-rc.0+137.sha-95fc3d4');
 
 /**
  * @license
