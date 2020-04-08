3d108aad5be5fabfd4d00d4f250b757b8fe5dd82
Kara Erickson <karakara@google.com>
2020-04-08 18:26:30 +0000

    7549c65502 build: update matching regex for bazel stamping (#36523)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+150.sha-1beccc1
+ * @license Angular v9.1.0-rc.0+151.sha-7549c65
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10211,7 +10211,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+150.sha-1beccc1');
+const VERSION = new Version('9.1.0-rc.0+151.sha-7549c65');
 
 /**
  * @fileoverview added by tsickle
