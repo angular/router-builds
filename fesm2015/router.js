fd021fca661c3b25d79f3a5cd05b843fc590a050
atscott <atscott@google.com>
2020-04-08 23:25:26 +0000

    92c4f3d508 refactor(bazel): use runfiles helper in ts-api-guardian (#36471)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+4.sha-e92fce1
+ * @license Angular v10.0.0-next.1+5.sha-92c4f3d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9999,7 +9999,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+4.sha-e92fce1');
+const VERSION = new Version('10.0.0-next.1+5.sha-92c4f3d');
 
 /**
  * @fileoverview added by tsickle
