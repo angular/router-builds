1e612da51321968e4c62f4a2cdbec2c3d5270aa9
atscott <atscott@google.com>
2020-04-09 21:06:20 +0000

    2e4244d521 fix(docs-infra): fix `elements` example when used with ES5 (#36536)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+14.sha-4aa4e6f
+ * @license Angular v10.0.0-next.1+18.sha-2e4244d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10212,7 +10212,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+14.sha-4aa4e6f');
+const VERSION = new Version('10.0.0-next.1+18.sha-2e4244d');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+14.sha-4aa4e6f
+ * @license Angular v10.0.0-next.1+18.sha-2e4244d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
