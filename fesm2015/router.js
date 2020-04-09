1987143ad2b9a43e3d82143a72c9388ea68856c5
atscott <atscott@google.com>
2020-04-09 19:07:36 +0000

    4aa4e6fd03 fix(compiler): handle type references to namespaced symbols correctly (#36106)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+17.sha-3bedfda
+ * @license Angular v10.0.0-next.1+14.sha-4aa4e6f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10000,7 +10000,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+17.sha-3bedfda');
+const VERSION = new Version('10.0.0-next.1+14.sha-4aa4e6f');
 
 /**
  * @fileoverview added by tsickle
