111fdee683d19549fb0830ed89f8b503829e3846
atscott <atscott@google.com>
2020-04-09 17:07:46 +0000

    e526f74dfd refactor(compiler): create a new root `BindingScope` for each template (#36362)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+7.sha-9e78f55
+ * @license Angular v10.0.0-next.1+8.sha-e526f74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5970,7 +5970,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.1+7.sha-9e78f55');
+    var VERSION = new core.Version('10.0.0-next.1+8.sha-e526f74');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+7.sha-9e78f55
+ * @license Angular v10.0.0-next.1+8.sha-e526f74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9999,7 +9999,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+7.sha-9e78f55');
+const VERSION = new Version('10.0.0-next.1+8.sha-e526f74');
 
 /**
  * @fileoverview added by tsickle
