dba634adcccbd0f3e2a551ba2c52fd487de3e076
atscott <atscott@google.com>
2020-04-09 19:02:20 +0000

    3bedfdac9d perf(ngcc): only load  if it is needed (#36486)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+12.sha-13ae2d7
+ * @license Angular v10.0.0-next.1+17.sha-3bedfda
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5934,7 +5934,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.1+12.sha-13ae2d7');
+    var VERSION = new i0.Version('10.0.0-next.1+17.sha-3bedfda');
 
     /**
      * @license
