ce9d128dda889a44627b28880246cd45f82678da
atscott <atscott@google.com>
2020-04-09 19:04:44 +0000

    e06512b22f perf(ngcc): only load  if it is needed (#36486)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+19.sha-66724fd
+ * @license Angular v9.1.1+25.sha-e06512b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+19.sha-66724fd
+ * @license Angular v9.1.1+25.sha-e06512b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10000,7 +10000,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.1+19.sha-66724fd');
+const VERSION = new Version('9.1.1+25.sha-e06512b');
 
 /**
  * @fileoverview added by tsickle
