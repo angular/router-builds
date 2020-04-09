d13568006c2178df0c75a477e40d0f97e4157816
atscott <atscott@google.com>
2020-04-09 19:09:26 +0000

    4aa4e6fd03 fix(compiler): handle type references to namespaced symbols correctly (#36106)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+17.sha-3bedfda
+ * @license Angular v10.0.0-next.1+14.sha-4aa4e6f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+17.sha-3bedfda
+ * @license Angular v10.0.0-next.1+14.sha-4aa4e6f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5934,7 +5934,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.1+17.sha-3bedfda');
+    var VERSION = new i0.Version('10.0.0-next.1+14.sha-4aa4e6f');
 
     /**
      * @license
