d9d966190ad76aaef751677d3755d9578011e6ae
atscott <atscott@google.com>
2020-04-13 15:39:34 +0000

    9181b73e5a docs: fix typo in Tests guide (#36592)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+21.sha-ca67748
+ * @license Angular v10.0.0-next.1+23.sha-9181b73
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5756,7 +5756,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.1+21.sha-ca67748');
+var VERSION = new Version('10.0.0-next.1+23.sha-9181b73');
 
 /**
  * @license
