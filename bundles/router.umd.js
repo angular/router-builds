8a244c82c3880286ec3ae93fcb4b1aa89eb3870c
atscott <atscott@google.com>
2020-04-14 17:37:31 +0000

    29122c518b style: lint (#36580)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+27.sha-0cc53fb
+ * @license Angular v10.0.0-next.1+30.sha-29122c5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5934,7 +5934,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.1+27.sha-0cc53fb');
+    var VERSION = new i0.Version('10.0.0-next.1+30.sha-29122c5');
 
     /**
      * @license
