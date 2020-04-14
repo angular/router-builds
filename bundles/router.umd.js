a1b72c60aebe8d1b6735e94620738fc7622c5964
atscott <atscott@google.com>
2020-04-14 01:02:23 +0000

    0cc53fb398 fix(upgrade): update $locationShim to handle Location changes before initialization (#36498)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+28.sha-f88e635
+ * @license Angular v10.0.0-next.1+27.sha-0cc53fb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5934,7 +5934,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.1+28.sha-f88e635');
+    var VERSION = new i0.Version('10.0.0-next.1+27.sha-0cc53fb');
 
     /**
      * @license
