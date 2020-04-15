823dd16f6a5d22046b31e0dc5876a36929232200
atscott <atscott@google.com>
2020-04-15 16:48:50 +0000

    e041ac6f0d fix(ngcc): display output from the unlocker process on Windows (#36569)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+36.sha-663b768
+ * @license Angular v10.0.0-next.1+38.sha-e041ac6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5878,7 +5878,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.1+36.sha-663b768');
+var VERSION = new Version('10.0.0-next.1+38.sha-e041ac6');
 
 /**
  * @license
