af1f4e081e127218a2600a1c04de9b480d9732ae
atscott <atscott@google.com>
2020-04-16 18:30:58 +0000

    bfda7ca5bd docs(changelog): Add new line between changes (#36653)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+18.sha-305b059
+ * @license Angular v10.0.0-next.2+19.sha-bfda7ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6092,7 +6092,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.2+18.sha-305b059');
+    var VERSION = new core.Version('10.0.0-next.2+19.sha-bfda7ca');
 
     /**
      * @license
