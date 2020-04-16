abb9e64b4997fe7c2b75f159d0091ea3ea5af836
atscott <atscott@google.com>
2020-04-16 17:05:47 +0000

    c6e5fc4fbe fix(common): format day-periods that cross midnight (#36611)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2
+ * @license Angular v10.0.0-next.2+1.sha-c6e5fc4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2
+ * @license Angular v10.0.0-next.2+1.sha-c6e5fc4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5842,7 +5842,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.2');
+var VERSION = new Version('10.0.0-next.2+1.sha-c6e5fc4');
 
 /**
  * @license
