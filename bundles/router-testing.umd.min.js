36c18846de33fc29b8265f3e59b88ea75e0dff94
atscott <atscott@google.com>
2020-04-16 17:06:11 +0000

    1e3b8a1354 docs: remove unneeded code from `universal` example/guide (#36483)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+1.sha-c6e5fc4
+ * @license Angular v10.0.0-next.2+12.sha-1e3b8a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+1.sha-c6e5fc4
+ * @license Angular v10.0.0-next.2+12.sha-1e3b8a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10338,7 +10338,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+1.sha-c6e5fc4');
+const VERSION = new Version('10.0.0-next.2+12.sha-1e3b8a1');
 
 /**
  * @fileoverview added by tsickle
