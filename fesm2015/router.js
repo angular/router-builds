7a34590294f4e3bdfaf9913dbcb31b34aaa01a75
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 20:29:14 +0000

    78840e59a7 docs: remove rob (#36285)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+29.sha-8be0972
+ * @license Angular v10.0.0-next.2+28.sha-78840e5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10126,7 +10126,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+29.sha-8be0972');
+const VERSION = new Version('10.0.0-next.2+28.sha-78840e5');
 
 /**
  * @fileoverview added by tsickle
