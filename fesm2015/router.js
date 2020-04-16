cb079d149b87d6e1f2971cbe45e7bacf8feda552
atscott <atscott@google.com>
2020-04-16 17:08:32 +0000

    b1f1d3ffd2 fix(core): handle empty translations correctly (#36499)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+12.sha-1e3b8a1
+ * @license Angular v10.0.0-next.2+3.sha-b1f1d3f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10338,7 +10338,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+12.sha-1e3b8a1');
+const VERSION = new Version('10.0.0-next.2+3.sha-b1f1d3f');
 
 /**
  * @fileoverview added by tsickle
