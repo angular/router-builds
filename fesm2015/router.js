edbd9e67b2ba1628f09d2a33a086a621f81b4094
atscott <atscott@google.com>
2020-04-16 17:59:00 +0000

    305b059809 docs: refactor routing doc (#35566)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+17.sha-297b7da
+ * @license Angular v10.0.0-next.2+18.sha-305b059
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10338,7 +10338,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+17.sha-297b7da');
+const VERSION = new Version('10.0.0-next.2+18.sha-305b059');
 
 /**
  * @fileoverview added by tsickle
