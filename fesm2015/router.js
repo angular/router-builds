b8877ed4ffc474f944b687c84ac883f971c80c07
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 20:29:03 +0000

    c332d4d916 refactor(ngcc): moved shared setup into a single function (#36637)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+19.sha-bfda7ca
+ * @license Angular v10.0.0-next.2+27.sha-c332d4d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10338,7 +10338,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+19.sha-bfda7ca');
+const VERSION = new Version('10.0.0-next.2+27.sha-c332d4d');
 
 /**
  * @fileoverview added by tsickle
