40d8836d101bdd891838870b83d01e245fce82b9
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 21:00:20 +0000

    5e79799b89 chore: update saucelabs README with some more details on local testing (#36625)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+30.sha-1fa1dd5
+ * @license Angular v10.0.0-next.2+31.sha-5e79799
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6092,7 +6092,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.2+30.sha-1fa1dd5');
+    var VERSION = new core.Version('10.0.0-next.2+31.sha-5e79799');
 
     /**
      * @license
