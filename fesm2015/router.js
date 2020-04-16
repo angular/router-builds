fdb7e77738769399f2788c19829d5c19a8fda71c
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 20:32:12 +0000

    1fa1dd5aaa docs: getting started guide use pipe before introduction (#36584)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+28.sha-78840e5
+ * @license Angular v10.0.0-next.2+30.sha-1fa1dd5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10126,7 +10126,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+28.sha-78840e5');
+const VERSION = new Version('10.0.0-next.2+30.sha-1fa1dd5');
 
 /**
  * @fileoverview added by tsickle
