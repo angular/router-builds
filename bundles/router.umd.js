74a3ca845f6d80f347b1d9cbfcfc1f3bb770a1ac
atscott <atscott@google.com>
2020-04-16 17:03:41 +0000

    03ff380e13 docs: add " in architecture page (#36564)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+1.sha-c6e5fc4
+ * @license Angular v10.0.0-next.2+2.sha-03ff380
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6092,7 +6092,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.2+1.sha-c6e5fc4');
+    var VERSION = new core.Version('10.0.0-next.2+2.sha-03ff380');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+1.sha-c6e5fc4
+ * @license Angular v10.0.0-next.2+2.sha-03ff380
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
