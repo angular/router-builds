67632bc3d1848050a86f26c13c640dfda3aa644b
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 21:02:08 +0000

    5e79799b89 chore: update saucelabs README with some more details on local testing (#36625)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+30.sha-1fa1dd5
+ * @license Angular v10.0.0-next.2+31.sha-5e79799
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5842,7 +5842,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.2+30.sha-1fa1dd5');
+var VERSION = new Version('10.0.0-next.2+31.sha-5e79799');
 
 /**
  * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+30.sha-1fa1dd5
+ * @license Angular v10.0.0-next.2+31.sha-5e79799
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
