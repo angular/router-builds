0cb739ba3bd72f7da38009dcee9d04df1e5490c0
Matias Niemelä <matias@yearofmoo.com>
2020-04-17 16:56:30 +0000

    4da901f8c0 docs: add tony bove to about page (#36335)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+32.sha-4480ba3
+ * @license Angular v10.0.0-next.2+34.sha-4da901f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+32.sha-4480ba3
+ * @license Angular v10.0.0-next.2+34.sha-4da901f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6056,7 +6056,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.2+32.sha-4480ba3');
+    var VERSION = new i0.Version('10.0.0-next.2+34.sha-4da901f');
 
     /**
      * @license
