a57a71219f1178caf78903f9194586e888817588
Matias Niemelä <matias@yearofmoo.com>
2020-04-17 16:55:09 +0000

    bb150c2704 fixup!: build(docs-infra): Ensures that only member docs are linked (#36316)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+34.sha-4da901f
+ * @license Angular v10.0.0-next.2+40.sha-bb150c2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+34.sha-4da901f
+ * @license Angular v10.0.0-next.2+40.sha-bb150c2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5878,7 +5878,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.2+34.sha-4da901f');
+var VERSION = new Version('10.0.0-next.2+40.sha-bb150c2');
 
 /**
  * @license
