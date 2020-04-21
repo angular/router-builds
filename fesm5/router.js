0c3000b69168bfb99186676370457cb3996b0333
Matias Niemelä <matias@yearofmoo.com>
2020-04-21 17:15:10 +0000

    51b09244ff fix(dev-infra): extract commit headers before checking commit message validity (#36733)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+53.sha-f186c32
+ * @license Angular v10.0.0-next.2+51.sha-51b0924
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5842,7 +5842,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.2+53.sha-f186c32');
+var VERSION = new Version('10.0.0-next.2+51.sha-51b0924');
 
 /**
  * @license
