19be373c7cca7645f101960493d226d0c2871444
Matias Niemelä <matias@yearofmoo.com>
2020-04-21 17:51:44 +0000

    acf6075ca9 fix(core): do not use unbound attributes as inputs to structural directives (#36441)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+54.sha-28995db
+ * @license Angular v10.0.0-next.2+55.sha-acf6075
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6092,7 +6092,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.2+54.sha-28995db');
+    var VERSION = new core.Version('10.0.0-next.2+55.sha-acf6075');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+54.sha-28995db
+ * @license Angular v10.0.0-next.2+55.sha-acf6075
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
