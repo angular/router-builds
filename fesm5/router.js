aa929a7350ecab42a8254c46b8f0e9c57199c9c6
Matias Niemelä <matias@yearofmoo.com>
2020-04-21 17:17:18 +0000

    28995dba19 fix(core): missing-injectable migration should not migrate `@NgModule` classes (#36369)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+51.sha-51b0924
+ * @license Angular v10.0.0-next.2+54.sha-28995db
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5878,7 +5878,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.2+51.sha-51b0924');
+var VERSION = new Version('10.0.0-next.2+54.sha-28995db');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+51.sha-51b0924
+ * @license Angular v10.0.0-next.2+54.sha-28995db
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
