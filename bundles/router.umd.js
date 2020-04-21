9099ed1d6685cd2c7e8d586ad8924df78114a1fd
Matias Niemelä <matias@yearofmoo.com>
2020-04-21 17:13:21 +0000

    51b09244ff fix(dev-infra): extract commit headers before checking commit message validity (#36733)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+53.sha-f186c32
+ * @license Angular v10.0.0-next.2+51.sha-51b0924
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6092,7 +6092,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.2+53.sha-f186c32');
+    var VERSION = new core.Version('10.0.0-next.2+51.sha-51b0924');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+53.sha-f186c32
+ * @license Angular v10.0.0-next.2+51.sha-51b0924
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10125,7 +10125,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+53.sha-f186c32');
+const VERSION = new Version('10.0.0-next.2+51.sha-51b0924');
 
 /**
  * @fileoverview added by tsickle
