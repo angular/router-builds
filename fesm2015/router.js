1640b160f1d17d9b1fdacfd55e35a88b9142d2f8
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 21:32:27 +0000

    8306da5ce1 release(benchpress): bump version of benchpress to 0.2.0 (#36457)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+2.sha-47f9867
+ * @license Angular v10.0.0-next.3+5.sha-8306da5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10337,7 +10337,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+2.sha-47f9867');
+const VERSION = new Version('10.0.0-next.3+5.sha-8306da5');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+2.sha-47f9867
+ * @license Angular v10.0.0-next.3+5.sha-8306da5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5842,7 +5842,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.3+2.sha-47f9867');
+var VERSION = new Version('10.0.0-next.3+5.sha-8306da5');
 
 /**
  * @license
