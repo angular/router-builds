40237f5d967750f2f231049b23e620b3a342f888
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 20:57:03 +0000

    781f5611d1 build: update to @bazel/bazelisk@^1.4.0 (#36729)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+58.sha-5db8d93
+ * @license Angular v10.0.0-next.3+1.sha-781f561
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10337,7 +10337,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+58.sha-5db8d93');
+const VERSION = new Version('10.0.0-next.3+1.sha-781f561');
 
 /**
  * @fileoverview added by tsickle
