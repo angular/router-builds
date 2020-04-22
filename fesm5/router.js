1e6b2e7346b82a64b06b329d1ef8f677f1855e53
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 21:38:48 +0000

    4275b3412f build: add AndrewKushnir to the `fw-testing` group (#36744)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+6.sha-1b3ee17
+ * @license Angular v10.0.0-next.3+7.sha-4275b34
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5878,7 +5878,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.3+6.sha-1b3ee17');
+var VERSION = new Version('10.0.0-next.3+7.sha-4275b34');
 
 /**
  * @license
--- fesm5/upgrade.js
+++ fesm5/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+6.sha-1b3ee17
+ * @license Angular v10.0.0-next.3+7.sha-4275b34
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
