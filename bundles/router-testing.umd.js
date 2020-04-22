b3c69874e53cb7d0adac07d96fd62acdd9ca8b97
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 21:33:03 +0000

    fc1b557d98 build: run pre-check before publishing (#36527)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+5.sha-8306da5
+ * @license Angular v10.0.0-next.3+3.sha-fc1b557
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+5.sha-8306da5
+ * @license Angular v10.0.0-next.3+3.sha-fc1b557
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10337,7 +10337,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+5.sha-8306da5');
+const VERSION = new Version('10.0.0-next.3+3.sha-fc1b557');
 
 /**
  * @fileoverview added by tsickle
