fbf8873c160575eeb757da7e59470bcbbd5c2a39
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 20:55:03 +0000

    aa94cd505c fix(localize): include legacy ids when describing messages (#36761)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3
+ * @license Angular v9.1.3+2.sha-aa94cd5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10125,7 +10125,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.3');
+const VERSION = new Version('9.1.3+2.sha-aa94cd5');
 
 /**
  * @fileoverview added by tsickle
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3
+ * @license Angular v9.1.3+2.sha-aa94cd5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
