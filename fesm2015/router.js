a56b67dbc58407cdcf78a1646d3784e6e195b84a
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 02:24:53 +0000

    535c249f3a build: update `REQUIRED_BASE_SHA` in merge script to `commit-message` script update commit (#36750)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+56.sha-942b986
+ * @license Angular v10.0.0-next.2+57.sha-535c249
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10337,7 +10337,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+56.sha-942b986');
+const VERSION = new Version('10.0.0-next.2+57.sha-535c249');
 
 /**
  * @fileoverview added by tsickle
