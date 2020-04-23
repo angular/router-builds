8e809485e68ad96009885c74d8fbeafe1fa57977
Matias Niemelä <matias@yearofmoo.com>
2020-04-23 16:28:06 +0000

    113c75dd27 refactor(localize): remove unused code (#36745)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+7.sha-4275b34
+ * @license Angular v10.0.0-next.3+12.sha-113c75d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+7.sha-4275b34
+ * @license Angular v10.0.0-next.3+12.sha-113c75d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10125,7 +10125,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+7.sha-4275b34');
+const VERSION = new Version('10.0.0-next.3+12.sha-113c75d');
 
 /**
  * @fileoverview added by tsickle
