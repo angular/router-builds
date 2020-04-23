a469cf60a3ef1e81d5001aeeda5d1c7c125286d5
Matias Niemelä <matias@yearofmoo.com>
2020-04-23 16:37:23 +0000

    7743c43529 test(router): fix router test failing on IE (#36742)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+6.sha-d40bcce
+ * @license Angular v9.1.3+9.sha-7743c43
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5878,7 +5878,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.3+6.sha-d40bcce');
+var VERSION = new Version('9.1.3+9.sha-7743c43');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+6.sha-d40bcce
+ * @license Angular v9.1.3+9.sha-7743c43
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
