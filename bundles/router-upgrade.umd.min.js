d85d6cd25580de6d9eeaa69940a0f8e75f873cda
Andrew Kushnir <akushnir@google.com>
2020-04-23 19:39:18 +0000

    f0ec4ae941 ci: fix pullapprove incorrectly skipping fw-compiler as owner (#36661)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+16.sha-e5d2853
+ * @license Angular v10.0.0-next.3+19.sha-f0ec4ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+16.sha-e5d2853
+ * @license Angular v10.0.0-next.3+19.sha-f0ec4ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10337,7 +10337,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+16.sha-e5d2853');
+const VERSION = new Version('10.0.0-next.3+19.sha-f0ec4ae');
 
 /**
  * @fileoverview added by tsickle
