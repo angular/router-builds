40e8b14207d15eaaf8c706138c8c6619fc9fc525
Matias Niemelä <matias@yearofmoo.com>
2020-04-23 16:34:02 +0000

    dd806b0d99 docs: add Annie Wang to contributors.json (#36612)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+15.sha-152293d
+ * @license Angular v10.0.0-next.3+13.sha-dd806b0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6092,7 +6092,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.3+15.sha-152293d');
+    var VERSION = new core.Version('10.0.0-next.3+13.sha-dd806b0');
 
     /**
      * @license
