c82dd6d755438ad4a3bb27504c4eaab90b433122
Andrew Kushnir <akushnir@google.com>
2020-04-23 19:38:00 +0000

    e5d2853070 docs: correct ngSwitch definition (#35489)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+13.sha-dd806b0
+ * @license Angular v10.0.0-next.3+16.sha-e5d2853
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10337,7 +10337,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+13.sha-dd806b0');
+const VERSION = new Version('10.0.0-next.3+16.sha-e5d2853');
 
 /**
  * @fileoverview added by tsickle
