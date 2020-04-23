474ccd3c38cc4721f0298bf7e53a2cfb7b9529c8
Matias Niemelä <matias@yearofmoo.com>
2020-04-23 16:33:14 +0000

    152293d3cf test(router): fix router test failing on IE (#36742)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+14.sha-ceb61d1
+ * @license Angular v10.0.0-next.3+15.sha-152293d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10125,7 +10125,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+14.sha-ceb61d1');
+const VERSION = new Version('10.0.0-next.3+15.sha-152293d');
 
 /**
  * @fileoverview added by tsickle
