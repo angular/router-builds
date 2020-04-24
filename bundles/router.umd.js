8b3bff2a3bf237497d17e50289f0d4fab2ac5288
Andrew Kushnir <akushnir@google.com>
2020-04-24 19:57:45 +0000

    180a32894e build: migrate from gulp to ng-dev for running formatting (#36726)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+24.sha-cc71116
+ * @license Angular v9.1.3+26.sha-180a328
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6092,7 +6092,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.3+24.sha-cc71116');
+    var VERSION = new core.Version('9.1.3+26.sha-180a328');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+24.sha-cc71116
+ * @license Angular v9.1.3+26.sha-180a328
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
