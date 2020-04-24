b6fad4d211d7bd8392bab77dd26db6461e099b51
Andrew Kushnir <akushnir@google.com>
2020-04-24 16:26:59 +0000

    bcd31cb857 build(docs-infra): upgrade cli command docs sources to 730657c86 (#36790)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+26.sha-dd049ca
+ * @license Angular v10.0.0-next.3+25.sha-bcd31cb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10125,7 +10125,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+26.sha-dd049ca');
+const VERSION = new Version('10.0.0-next.3+25.sha-bcd31cb');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+26.sha-dd049ca
+ * @license Angular v10.0.0-next.3+25.sha-bcd31cb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
