2b23b5d6f2100de0368d14b15eb5d1abefe40fa4
Andrew Kushnir <akushnir@google.com>
2020-04-24 16:27:50 +0000

    dd049caf0a fix(language-service): disable update the `@angular/core` module (#36783)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+24.sha-e6161ca
+ * @license Angular v10.0.0-next.3+26.sha-dd049ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10337,7 +10337,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+24.sha-e6161ca');
+const VERSION = new Version('10.0.0-next.3+26.sha-dd049ca');
 
 /**
  * @fileoverview added by tsickle
