149a48089499d9f3626cb375cbdf8bde33d167dc
Andrew Kushnir <akushnir@google.com>
2020-04-28 22:32:41 +0000

    a6a7e1bb99 build: enable platform-browser tests on Saucelabs (#36797)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+43.sha-70dd27f
+ * @license Angular v10.0.0-next.3+44.sha-a6a7e1b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10137,7 +10137,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+43.sha-70dd27f');
+const VERSION = new Version('10.0.0-next.3+44.sha-a6a7e1b');
 
 /**
  * @fileoverview added by tsickle
