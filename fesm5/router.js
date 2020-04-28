a1af12a3f822dc22161ef31915c7dc6ed4cc9901
Andrew Kushnir <akushnir@google.com>
2020-04-28 16:35:06 +0000

    1d9375d115 ci: fix bad reference to `head` property in rebase-pr script (#36825)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+36.sha-f27deea
+ * @license Angular v10.0.0-next.3+37.sha-1d9375d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5885,7 +5885,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.3+36.sha-f27deea');
+var VERSION = new Version('10.0.0-next.3+37.sha-1d9375d');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+36.sha-f27deea
+ * @license Angular v10.0.0-next.3+37.sha-1d9375d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
