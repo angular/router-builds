47b2aeedebd30723a13186078fdbcef4ebaf33a1
Andrew Kushnir <akushnir@google.com>
2020-04-29 20:30:15 +0000

    cebd4fada7 build: use sh instead of exec for release scripts (#36862)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+46.sha-3d82aa7
+ * @license Angular v10.0.0-next.3+47.sha-cebd4fa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6063,7 +6063,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.3+46.sha-3d82aa7');
+    var VERSION = new i0.Version('10.0.0-next.3+47.sha-cebd4fa');
 
     /**
      * @license
