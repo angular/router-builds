ccb921723e32658ff0e10ac6b75da83b147ff07a
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:53:02 +0000

    45c09416ed refactor(ngcc): move `PathMappings` to separate file to avoid circular dependency (#36626)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+1.sha-62ba0ac
+ * @license Angular v10.0.0-next.4+13.sha-45c0941
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10137,7 +10137,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.4+1.sha-62ba0ac');
+const VERSION = new Version('10.0.0-next.4+13.sha-45c0941');
 
 /**
  * @fileoverview added by tsickle
