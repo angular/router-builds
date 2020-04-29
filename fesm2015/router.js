53b61e4c59e23d3206a1fd4740985e5d95466590
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:41:43 +0000

    52d2e46700 release: cut the v10.0.0-next.4 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+48.sha-c89421b
+ * @license Angular v10.0.0-next.4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10349,7 +10349,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+48.sha-c89421b');
+const VERSION = new Version('10.0.0-next.4');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+48.sha-c89421b
+ * @license Angular v10.0.0-next.4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
