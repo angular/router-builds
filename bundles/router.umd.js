d3ccf7b608be928c8342c875db1c19ed79c5491c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-04-30 19:08:19 +0000

    49be32c931 refactor(common): remove WrappedValue from AsyncPipe (#36633)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+14.sha-1786586
+ * @license Angular v10.0.0-next.4+15.sha-49be32c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6063,7 +6063,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.4+14.sha-1786586');
+    var VERSION = new i0.Version('10.0.0-next.4+15.sha-49be32c');
 
     /**
      * @license
