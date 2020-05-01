c80b3ed94744c1b1aec076cc92b3b92008bfef43
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-01 17:23:04 +0000

    a0342763b8 build(language-service): make test project a filegroup (#36865)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.4+14.sha-c8c2272
+ * @license Angular v9.1.4+17.sha-a034276
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5878,7 +5878,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.4+14.sha-c8c2272');
+var VERSION = new Version('9.1.4+17.sha-a034276');
 
 /**
  * @license
--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.4+14.sha-c8c2272
+ * @license Angular v9.1.4+17.sha-a034276
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
