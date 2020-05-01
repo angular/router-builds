3026eff7b14680667075a69472152eea3e44b909
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-01 17:28:03 +0000

    e9300c979e build: use @angular/dev-infra-private for bazel stamping (#36844)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+30.sha-95a407d
+ * @license Angular v10.0.0-next.4+29.sha-e9300c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5849,7 +5849,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.4+30.sha-95a407d');
+var VERSION = new Version('10.0.0-next.4+29.sha-e9300c9');
 
 /**
  * @license
