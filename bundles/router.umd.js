1a8fad3a5cd5cabc975112744e5e009b078d0671
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-01 17:27:04 +0000

    95a407d9dd build: Add entry point for Ivy language service (#36864)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+31.sha-12fcc7c
+ * @license Angular v10.0.0-next.4+30.sha-95a407d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6063,7 +6063,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.4+31.sha-12fcc7c');
+    var VERSION = new i0.Version('10.0.0-next.4+30.sha-95a407d');
 
     /**
      * @license
