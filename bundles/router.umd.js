ed1423fa46ba51dd9a865b0b9e1bfde5a158da79
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:10:19 +0000

    297dab845d build: migrate bazel related formatting/linting to ng-dev format (#36842)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+34.sha-0f3831b
+ * @license Angular v10.0.0-next.4+36.sha-297dab8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6070,7 +6070,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.4+34.sha-0f3831b');
+    var VERSION = new i0.Version('10.0.0-next.4+36.sha-297dab8');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+34.sha-0f3831b
+ * @license Angular v10.0.0-next.4+36.sha-297dab8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10362,7 +10362,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.4+34.sha-0f3831b');
+const VERSION = new Version('10.0.0-next.4+36.sha-297dab8');
 
 /**
  * @fileoverview added by tsickle
