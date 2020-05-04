f72989e94a5b1a396a8fc4f57cf17414cbe71a06
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:15:14 +0000

    6046e86506 build: update setup-rbe.sh script to accept non google.com/angular.io (#36846)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+43.sha-42c58d9
+ * @license Angular v10.0.0-next.4+37.sha-6046e86
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5892,7 +5892,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.4+43.sha-42c58d9');
+var VERSION = new Version('10.0.0-next.4+37.sha-6046e86');
 
 /**
  * @license
