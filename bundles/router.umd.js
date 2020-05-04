936f6dfe99ea31f3947858d161787b82fbdfd377
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:13:44 +0000

    e037840b88 perf(ngcc): speed up the `getBasePaths()` computation (#36881)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+36.sha-297dab8
+ * @license Angular v10.0.0-next.4+41.sha-e037840
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6070,7 +6070,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.4+36.sha-297dab8');
+    var VERSION = new i0.Version('10.0.0-next.4+41.sha-e037840');
 
     /**
      * @license
