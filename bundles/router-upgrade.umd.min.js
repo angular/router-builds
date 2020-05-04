e149d35cd0d1a49ad20ca9a785cc5a17b1a49f7b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:05:31 +0000

    9d9d46f52b fix(core): log error instead of warning for unknown properties and elements (#36399)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+32.sha-d9c4840
+ * @license Angular v10.0.0-next.4+33.sha-9d9d46f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+32.sha-d9c4840
+ * @license Angular v10.0.0-next.4+33.sha-9d9d46f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6070,7 +6070,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.4+32.sha-d9c4840');
+    var VERSION = new i0.Version('10.0.0-next.4+33.sha-9d9d46f');
 
     /**
      * @license
