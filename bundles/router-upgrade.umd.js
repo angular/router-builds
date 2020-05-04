02b8b688aa63bedd1065ff561dfa85c09860638a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:14:04 +0000

    58ea040570 test(language-service): add new mock host for testing ivy (#36879)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+41.sha-e037840
+ * @license Angular v10.0.0-next.4+38.sha-58ea040
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+41.sha-e037840
+ * @license Angular v10.0.0-next.4+38.sha-58ea040
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10362,7 +10362,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.4+41.sha-e037840');
+const VERSION = new Version('10.0.0-next.4+38.sha-58ea040');
 
 /**
  * @fileoverview added by tsickle
