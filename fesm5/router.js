43597b455c06abb7bd4a2a48da98437a798a042f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-05 19:27:10 +0000

    b95a336f12 build: fix and re-enable elements tests on saucelabs (#36929)

--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+7.sha-306f46c
+ * @license Angular v10.0.0-next.5+18.sha-b95a336
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5856,7 +5856,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.5+7.sha-306f46c');
+var VERSION = new Version('10.0.0-next.5+18.sha-b95a336');
 
 /**
  * @license
