b04e243e840cbb021bca81b2210cb55889f71b35
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 22:49:05 +0000

    f7815cf96d test(compiler-cli): ensure reflection tests are not brittle to case-sensitivity (#36859)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+61.sha-f930e75
+ * @license Angular v10.0.0-next.5+71.sha-f7815cf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6117,7 +6117,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.5+61.sha-f930e75');
+    var VERSION = new core.Version('10.0.0-next.5+71.sha-f7815cf');
 
     /**
      * @license
