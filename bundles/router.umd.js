bbc0e82518819805dbab7cdb07f25b4ee37a9021
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 02:03:15 +0000

    ecffc3557f perf(compiler-cli): perform template type-checking incrementally (#36211)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+25.sha-388dc93
+ * @license Angular v10.0.0-next.5+42.sha-ecffc35
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6070,7 +6070,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.5+25.sha-388dc93');
+    var VERSION = new i0.Version('10.0.0-next.5+42.sha-ecffc35');
 
     /**
      * @license
