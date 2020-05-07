1453457740c24f86bcd2204a715a2f39fd216b51
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-07 22:07:21 +0000

    d0280a0335 fix(compiler): remove outdated and invalid warning for unresolved DI parameters (#36985)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+72.sha-97d6d90
+ * @license Angular v10.0.0-next.5+73.sha-d0280a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10391,7 +10391,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+72.sha-97d6d90');
+const VERSION = new Version('10.0.0-next.5+73.sha-d0280a0');
 
 /**
  * @fileoverview added by tsickle
