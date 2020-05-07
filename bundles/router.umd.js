4caf6668e56de9164a11f9415d00c77102d8369d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-07 22:09:44 +0000

    42d1091d6a fix(compiler-cli): don't try to tag non-ts files as shims (#36987)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+73.sha-d0280a0
+ * @license Angular v10.0.0-next.5+74.sha-42d1091
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6083,7 +6083,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.5+73.sha-d0280a0');
+    var VERSION = new i0.Version('10.0.0-next.5+74.sha-42d1091');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+73.sha-d0280a0
+ * @license Angular v10.0.0-next.5+74.sha-42d1091
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10391,7 +10391,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+73.sha-d0280a0');
+const VERSION = new Version('10.0.0-next.5+74.sha-42d1091');
 
 /**
  * @fileoverview added by tsickle
