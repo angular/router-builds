4b265e3897bfd1abd81aa68b0fc7e757487b104a
Misko Hevery <misko@hevery.com>
2020-05-08 22:06:36 +0000

    df71c7c617 build(docs-infra): upgrade cli command docs sources to 15edb0231 (#37005)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+15.sha-1142c37
+ * @license Angular v10.0.0-next.6+11.sha-df71c7c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+15.sha-1142c37
+ * @license Angular v10.0.0-next.6+11.sha-df71c7c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6083,7 +6083,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.6+15.sha-1142c37');
+    var VERSION = new i0.Version('10.0.0-next.6+11.sha-df71c7c');
 
     /**
      * @license
