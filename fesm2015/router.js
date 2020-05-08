72b0fc05d616b6e05089fdc3eccfe0086d356dfd
Misko Hevery <misko@hevery.com>
2020-05-08 22:02:30 +0000

    82a3bd5e8b ci: disable sauce labs safari and ios tests (#36973)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+10.sha-c184de1
+ * @license Angular v10.0.0-next.6+14.sha-82a3bd5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10391,7 +10391,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+10.sha-c184de1');
+const VERSION = new Version('10.0.0-next.6+14.sha-82a3bd5');
 
 /**
  * @fileoverview added by tsickle
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+10.sha-c184de1
+ * @license Angular v10.0.0-next.6+14.sha-82a3bd5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
