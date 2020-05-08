f3382c67a0fe36aebf6e578786d171954e48b24b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 17:03:36 +0000

    fdd61152c5 docs: replace into with in (#36778)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+3.sha-7cc332ef
+ * @license Angular v10.0.0-next.6+4.sha-fdd6115
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+3.sha-7cc332ef
+ * @license Angular v10.0.0-next.6+4.sha-fdd6115
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10391,7 +10391,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+3.sha-7cc332ef');
+const VERSION = new Version('10.0.0-next.6+4.sha-fdd6115');
 
 /**
  * @fileoverview added by tsickle
