4c8265fc70ced6b2adfab5a5aa157f180370df1e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 17:06:31 +0000

    14be55c9fa docs(common): remove mention of the deprecated (#36953) web worker platform. Minor grammar/stylistic changes.

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+7.sha-39e066a
+ * @license Angular v10.0.0-next.6+5.sha-14be55c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+7.sha-39e066a
+ * @license Angular v10.0.0-next.6+5.sha-14be55c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6083,7 +6083,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.6+7.sha-39e066a');
+    var VERSION = new i0.Version('10.0.0-next.6+5.sha-14be55c');
 
     /**
      * @license
