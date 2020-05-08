9f773ef37107a51d9a258cb334cfe1a37ea39a3d
Misko Hevery <misko@hevery.com>
2020-05-08 22:05:07 +0000

    eb34aa551a feat(compiler): add name spans for property reads and method calls (#36826)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+11.sha-df71c7c
+ * @license Angular v10.0.0-next.6+16.sha-eb34aa5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6117,7 +6117,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.6+11.sha-df71c7c');
+    var VERSION = new core.Version('10.0.0-next.6+16.sha-eb34aa5');
 
     /**
      * @license
