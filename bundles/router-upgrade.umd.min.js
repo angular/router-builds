2a3c3f6c321bdf611f5a8ecb5b58d20ea881c297
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 17:02:33 +0000

    7cc332efe6 docs: update aot compiler docs (#35487)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6
+ * @license Angular v10.0.0-next.6+3.sha-7cc332ef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6
+ * @license Angular v10.0.0-next.6+3.sha-7cc332ef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10391,7 +10391,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6');
+const VERSION = new Version('10.0.0-next.6+3.sha-7cc332ef');
 
 /**
  * @fileoverview added by tsickle
