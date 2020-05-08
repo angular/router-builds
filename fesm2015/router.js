8848e2f206cba3a2b4a038e92aa48de94c0bf2e2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 17:02:09 +0000

    39e066add1 refactor(docs-infra): change class to interface where class properties not used (#36958)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+4.sha-fdd6115
+ * @license Angular v10.0.0-next.6+7.sha-39e066a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10173,7 +10173,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+4.sha-fdd6115');
+const VERSION = new Version('10.0.0-next.6+7.sha-39e066a');
 
 /**
  * @fileoverview added by tsickle
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+4.sha-fdd6115
+ * @license Angular v10.0.0-next.6+7.sha-39e066a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
