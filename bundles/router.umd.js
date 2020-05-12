a1ef008495aa31d571f013bb399e48b1bba08286
Misko Hevery <misko@hevery.com>
2020-05-12 18:15:22 +0000

    198fc6f108 docs(forms): remove rxjs of alias import in form async validator example (#36856)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+27.sha-8fe532e
+ * @license Angular v10.0.0-next.6+26.sha-198fc6f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6083,7 +6083,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.6+27.sha-8fe532e');
+    var VERSION = new i0.Version('10.0.0-next.6+26.sha-198fc6f');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+27.sha-8fe532e
+ * @license Angular v10.0.0-next.6+26.sha-198fc6f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10391,7 +10391,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+27.sha-8fe532e');
+const VERSION = new Version('10.0.0-next.6+26.sha-198fc6f');
 
 /**
  * @fileoverview added by tsickle
