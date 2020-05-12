2053bd8dfe6b78e576d9d7c14c486f3dc47d0022
Misko Hevery <misko@hevery.com>
2020-05-12 18:11:25 +0000

    8fe532e593 build(dev-infra): cleanup package dependencies for shared package (#36980)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+28.sha-681741c
+ * @license Angular v10.0.0-next.6+27.sha-8fe532e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+28.sha-681741c
+ * @license Angular v10.0.0-next.6+27.sha-8fe532e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6117,7 +6117,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.6+28.sha-681741c');
+    var VERSION = new core.Version('10.0.0-next.6+27.sha-8fe532e');
 
     /**
      * @license
