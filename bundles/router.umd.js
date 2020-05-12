c60eb240849214c2cbde4bd11a14e8ed2e1ac26e
Misko Hevery <misko@hevery.com>
2020-05-12 22:04:57 +0000

    d7c70caa4c docs(platform-webworker): remove mention of version 10 (#37052)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+31.sha-2cddeab
+ * @license Angular v10.0.0-next.6+32.sha-d7c70ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6117,7 +6117,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.6+31.sha-2cddeab');
+    var VERSION = new core.Version('10.0.0-next.6+32.sha-d7c70ca');
 
     /**
      * @license
