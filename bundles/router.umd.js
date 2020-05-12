d9a9eaf93672108188875322a7c1656cde823a08
Misko Hevery <misko@hevery.com>
2020-05-12 22:02:11 +0000

    4f3ac1d983 docs(common): fix CLDR website link in FormStyle doc (#37069)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+30.sha-7addc9f
+ * @license Angular v10.0.0-next.6+33.sha-4f3ac1d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6117,7 +6117,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.6+30.sha-7addc9f');
+    var VERSION = new core.Version('10.0.0-next.6+33.sha-4f3ac1d');
 
     /**
      * @license
