7d50e30d5dd5191a12189ee59a1522043cc51c54
Misko Hevery <misko@hevery.com>
2020-05-12 20:39:50 +0000

    cb6ddfc215 Revert "fix(common): `locales/global/*.js` are not ES5 compliant (#36342)" (#37074)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+26.sha-198fc6f
+ * @license Angular v10.0.0-next.6+29.sha-cb6ddfc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6083,7 +6083,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.6+26.sha-198fc6f');
+    var VERSION = new i0.Version('10.0.0-next.6+29.sha-cb6ddfc');
 
     /**
      * @license
