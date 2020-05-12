b50c0e01bd1f306d09b77239cedd7b0419053187
Misko Hevery <misko@hevery.com>
2020-05-12 18:11:13 +0000

    681741cd13 docs: minor typo fix in the bazel documentation (#37043)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+24.sha-1e208e8
+ * @license Angular v10.0.0-next.6+28.sha-681741c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6117,7 +6117,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.6+24.sha-1e208e8');
+    var VERSION = new core.Version('10.0.0-next.6+28.sha-681741c');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+24.sha-1e208e8
+ * @license Angular v10.0.0-next.6+28.sha-681741c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
