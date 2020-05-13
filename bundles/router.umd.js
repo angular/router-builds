c562cb370c5998188a65e451dd9511be63aa734a
Misko Hevery <misko@hevery.com>
2020-05-13 23:27:51 +0000

    9edea0bb75 feat(platform-server): use absolute URLs from Location for HTTP (#37071)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+10.sha-ce39755
+ * @license Angular v10.0.0-next.7+11.sha-9edea0bb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6083,7 +6083,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.7+10.sha-ce39755');
+    var VERSION = new i0.Version('10.0.0-next.7+11.sha-9edea0bb');
 
     /**
      * @license
