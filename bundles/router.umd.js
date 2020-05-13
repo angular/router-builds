f97ebc96114cfa292382a32a32ae35fe8f80a822
Misko Hevery <misko@hevery.com>
2020-05-13 23:10:54 +0000

    255c2b2ff0 docs: refactor lifecycle hooks doc topic for task orientation (#36353)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7
+ * @license Angular v10.0.0-next.7+1.sha-255c2b2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6083,7 +6083,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.7');
+    var VERSION = new i0.Version('10.0.0-next.7+1.sha-255c2b2');
 
     /**
      * @license
