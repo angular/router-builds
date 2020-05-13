d4320eb3804b90fbc2ca6c6aabec6d1920d222a6
Misko Hevery <misko@hevery.com>
2020-05-13 23:30:11 +0000

    90205079d7 docs(common): English grammar (#36908)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+11.sha-9edea0bb
+ * @license Angular v10.0.0-next.7+16.sha-9020507
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+11.sha-9edea0bb
+ * @license Angular v10.0.0-next.7+16.sha-9020507
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10391,7 +10391,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+11.sha-9edea0bb');
+const VERSION = new Version('10.0.0-next.7+16.sha-9020507');
 
 /**
  * @fileoverview added by tsickle
