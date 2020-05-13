a85adcb3c19c21f1fa89e4b4a99ea3b6783d8dff
Misko Hevery <misko@hevery.com>
2020-05-13 23:25:44 +0000

    ce39755937 docs: Remove duplicate `and` (#37067)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+14.sha-2cacab9
+ * @license Angular v10.0.0-next.7+10.sha-ce39755
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+14.sha-2cacab9
+ * @license Angular v10.0.0-next.7+10.sha-ce39755
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6117,7 +6117,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.7+14.sha-2cacab9');
+    var VERSION = new core.Version('10.0.0-next.7+10.sha-ce39755');
 
     /**
      * @license
