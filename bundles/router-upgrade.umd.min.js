dd8455f1e541167db6917deaf57d66787390820f
Misko Hevery <misko@hevery.com>
2020-05-13 23:13:26 +0000

    019835c721 docs: minor typos (#36772)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+1.sha-255c2b2
+ * @license Angular v10.0.0-next.7+2.sha-019835c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+1.sha-255c2b2
+ * @license Angular v10.0.0-next.7+2.sha-019835c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6117,7 +6117,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.7+1.sha-255c2b2');
+    var VERSION = new core.Version('10.0.0-next.7+2.sha-019835c');
 
     /**
      * @license
