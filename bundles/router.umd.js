bccf854dd09a9bb1780d0c2afd7429b88bfcb6d3
Misko Hevery <misko@hevery.com>
2020-05-13 23:15:56 +0000

    141fcb95a4 refactor(localize): use the `FileSystem` from ngtsc (#36843)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+2.sha-019835c
+ * @license Angular v10.0.0-next.7+6.sha-141fcb9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6083,7 +6083,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.7+2.sha-019835c');
+    var VERSION = new i0.Version('10.0.0-next.7+6.sha-141fcb9');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+2.sha-019835c
+ * @license Angular v10.0.0-next.7+6.sha-141fcb9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10391,7 +10391,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+2.sha-019835c');
+const VERSION = new Version('10.0.0-next.7+6.sha-141fcb9');
 
 /**
  * @fileoverview added by tsickle
