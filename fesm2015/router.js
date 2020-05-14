8fa0ccfd112c15979edc916aae5fb7c456ef3fec
Kara Erickson <karakara@google.com>
2020-05-14 23:11:23 +0000

    aaa89bb715 refactor(core): rename refreshDynamicEmbeddedViews to refreshEmbeddedViews (#37117)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+52.sha-ea971f7
+ * @license Angular v10.0.0-next.7+53.sha-aaa89bb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10486,7 +10486,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+52.sha-ea971f7');
+const VERSION = new Version('10.0.0-next.7+53.sha-aaa89bb');
 
 /**
  * @fileoverview added by tsickle
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+52.sha-ea971f7
+ * @license Angular v10.0.0-next.7+53.sha-aaa89bb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
