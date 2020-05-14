f3cae6185aee20e6a5c3eb99098001cbfa9b85e7
Kara Erickson <karakara@google.com>
2020-05-14 18:07:50 +0000

    2418c6aca1 refactor(docs-infra): refactors `addImageDimensionsImpl` (#37046)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+16.sha-9020507
+ * @license Angular v10.0.0-next.7+17.sha-2418c6a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10173,7 +10173,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+16.sha-9020507');
+const VERSION = new Version('10.0.0-next.7+17.sha-2418c6a');
 
 /**
  * @fileoverview added by tsickle
