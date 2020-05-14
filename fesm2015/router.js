9ca58e56ff1ad3f97777fd50ae2721933959c067
Kara Erickson <karakara@google.com>
2020-05-14 22:47:09 +0000

    7a30153aa1 test(core): verify that Ivy i18n works correctly with HTML namespaces (#36943)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+49.sha-7acd330
+ * @license Angular v10.0.0-next.7+50.sha-7a30153
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10486,7 +10486,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+49.sha-7acd330');
+const VERSION = new Version('10.0.0-next.7+50.sha-7a30153');
 
 /**
  * @fileoverview added by tsickle
