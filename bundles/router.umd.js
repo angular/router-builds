9025855875c04dc39993f8730806404b30d04d69
Kara Erickson <karakara@google.com>
2020-05-14 19:22:08 +0000

    5f64e52ed4 docs: clarifies the service limitation (#36349)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+44.sha-95f429c
+ * @license Angular v10.0.0-next.7+46.sha-5f64e52
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6083,7 +6083,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.7+44.sha-95f429c');
+    var VERSION = new i0.Version('10.0.0-next.7+46.sha-5f64e52');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+44.sha-95f429c
+ * @license Angular v10.0.0-next.7+46.sha-5f64e52
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10704,7 +10704,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+44.sha-95f429c');
+const VERSION = new Version('10.0.0-next.7+46.sha-5f64e52');
 
 /**
  * @fileoverview added by tsickle
