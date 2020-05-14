84eff43f4a445f0a4be2e8e0330aaf7a5ab6f93c
Kara Erickson <karakara@google.com>
2020-05-14 23:09:24 +0000

    ea971f7098 fix(core): inheritance delegate ctor regex updated to work on minified code (#36962)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+51.sha-ddaa124
+ * @license Angular v10.0.0-next.7+52.sha-ea971f7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10486,7 +10486,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+51.sha-ddaa124');
+const VERSION = new Version('10.0.0-next.7+52.sha-ea971f7');
 
 /**
  * @fileoverview added by tsickle
