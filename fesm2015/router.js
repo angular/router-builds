2c5f1e40635def02e62362400c0c3393b9bd6951
Kara Erickson <karakara@google.com>
2020-05-15 18:52:44 +0000

    3d7c85b2aa feat(dev-infra): create tool to determine conflicts created by a PR (#37051)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+62.sha-55c2433
+ * @license Angular v10.0.0-next.7+63.sha-3d7c85b2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10704,7 +10704,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+62.sha-55c2433');
+const VERSION = new Version('10.0.0-next.7+63.sha-3d7c85b2');
 
 /**
  * @fileoverview added by tsickle
