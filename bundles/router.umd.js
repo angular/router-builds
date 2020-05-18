17209cd52ffa08f75da56e2ba475dd1f928fe71a
Kara Erickson <karakara@google.com>
2020-05-18 17:48:26 +0000

    2ca2c8fe0f build: complete removal of gulp format (#37147)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8
+ * @license Angular v10.0.0-next.8+7.sha-2ca2c8f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6083,7 +6083,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.8');
+    var VERSION = new i0.Version('10.0.0-next.8+7.sha-2ca2c8f');
 
     /**
      * @license
