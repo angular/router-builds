e3c6d331ea326d54b494956a7b2b15a6ca042f86
Kara Erickson <karakara@google.com>
2020-05-18 16:37:15 +0000

    844208f463 ci: temporary disable components-repo-unit-tests (#37129)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+66.sha-3f9b3d7
+ * @license Angular v10.0.0-next.7+70.sha-844208f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6083,7 +6083,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.7+66.sha-3f9b3d7');
+    var VERSION = new i0.Version('10.0.0-next.7+70.sha-844208f');
 
     /**
      * @license
