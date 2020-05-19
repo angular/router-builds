29410a7d3bd7a2e94a44f6a6dfaaeb2c981c61e4
Kara Erickson <karakara@google.com>
2020-05-19 22:45:03 +0000

    383f04b96d feat(dev-infra): register ts-node when reading configuration (#37196)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+27.sha-772c5b8
+ * @license Angular v10.0.0-next.8+29.sha-383f04b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6116,7 +6116,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.8+27.sha-772c5b8');
+    var VERSION = new i0.Version('10.0.0-next.8+29.sha-383f04b');
 
     /**
      * @license
