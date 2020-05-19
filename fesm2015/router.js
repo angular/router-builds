371707758e497c7c4bee380f5727a9fef687bd4f
Kara Erickson <karakara@google.com>
2020-05-19 22:56:09 +0000

    211d33ad59 build: fix deprecation notice in scripts/github/merge-pr script (#37208)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+29.sha-383f04b
+ * @license Angular v10.0.0-next.8+30.sha-211d33a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10567,7 +10567,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.8+29.sha-383f04b');
+const VERSION = new Version('10.0.0-next.8+30.sha-211d33a');
 
 /**
  * @fileoverview added by tsickle
