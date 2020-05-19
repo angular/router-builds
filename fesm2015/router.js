30543915890c95028dab19d19b8263509778da56
Kara Erickson <karakara@google.com>
2020-05-19 22:28:13 +0000

    4e96cdc23f build: Add deprecation notice to merge-pr script to nudge to new tooling (#37204)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+25.sha-deac994
+ * @license Angular v10.0.0-next.8+28.sha-4e96cdc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10785,7 +10785,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.8+25.sha-deac994');
+const VERSION = new Version('10.0.0-next.8+28.sha-4e96cdc');
 
 /**
  * @fileoverview added by tsickle
