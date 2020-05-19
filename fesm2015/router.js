b80ed43f59b9ee9dda80aa9cf18716f9a04dfcfc
Kara Erickson <karakara@google.com>
2020-05-19 22:29:26 +0000

    772c5b8f64 refactor: update to tslib 2.0 and move to direct dependencies (#37198)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+28.sha-4e96cdc
+ * @license Angular v10.0.0-next.8+27.sha-772c5b8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10785,7 +10785,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.8+28.sha-4e96cdc');
+const VERSION = new Version('10.0.0-next.8+27.sha-772c5b8');
 
 /**
  * @fileoverview added by tsickle
