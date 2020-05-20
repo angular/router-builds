a2f8f54ba34c1232c336009ce00d23a758f282fe
Kara Erickson <karakara@google.com>
2020-05-20 17:02:25 +0000

    4c30aa8343 docs(dev-infra): update .circleci/README.md (#37212)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.9+1.sha-1faf9bb
+ * @license Angular v10.0.0-next.9+4.sha-4c30aa8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6116,7 +6116,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.9+1.sha-1faf9bb');
+    var VERSION = new i0.Version('10.0.0-next.9+4.sha-4c30aa8');
 
     /**
      * @license
