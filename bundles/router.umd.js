f27bb32609e29c0b5b91001a4ec0d3145addfd42
Kara Erickson <karakara@google.com>
2020-05-20 00:06:48 +0000

    325ad7eb53 release: cut the v10.0.0-next.9 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+31.sha-362fba4
+ * @license Angular v10.0.0-next.9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6151,7 +6151,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.8+31.sha-362fba4');
+    var VERSION = new core.Version('10.0.0-next.9');
 
     /**
      * @license
