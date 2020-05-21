90fa0f4df213f03077fe8585d6e258e627d8879f
Kara Erickson <karakara@google.com>
2020-05-21 17:58:44 +0000

    cdb5d07606 feat(dev-infra): expose script for determining merge branches (#37217)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0
+ * @license Angular v10.0.0-rc.0+2.sha-cdb5d07
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6116,7 +6116,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0');
+    var VERSION = new i0.Version('10.0.0-rc.0+2.sha-cdb5d07');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0
+ * @license Angular v10.0.0-rc.0+2.sha-cdb5d07
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5534,7 +5534,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0');
+const VERSION = new Version('10.0.0-rc.0+2.sha-cdb5d07');
 
 /**
  * @license
