6ed3faa83232c67c91e4751fa20a0ccb0b57e01c
Matias Niemelä <matias@yearofmoo.com>
2020-05-28 20:27:39 +0000

    98d820030c docs(platform-server): fix renderModule usage guidance with Ivy (#37296)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+20.sha-64fc8b8
+ * @license Angular v10.0.0-rc.0+22.sha-98d8200
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5555,7 +5555,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+20.sha-64fc8b8');
+const VERSION = new Version('10.0.0-rc.0+22.sha-98d8200');
 
 /**
  * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+20.sha-64fc8b8
+ * @license Angular v10.0.0-rc.0+22.sha-98d8200
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
