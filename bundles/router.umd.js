7fcb29005a6a6c55584e51793f32f93447de49cd
Matias Niemelä <matias@yearofmoo.com>
2020-06-01 18:45:10 +0000

    8bb741198b docs: release notes for the v10.0.0-rc.2 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+35.sha-4d0e175
+ * @license Angular v10.0.0-rc.0+37.sha-8bb7411
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6105,7 +6105,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+35.sha-4d0e175');
+    var VERSION = new core.Version('10.0.0-rc.0+37.sha-8bb7411');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+35.sha-4d0e175
+ * @license Angular v10.0.0-rc.0+37.sha-8bb7411
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
