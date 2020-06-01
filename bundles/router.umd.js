94992da74a1d77abd52494c4ff194d35424705fc
Matias Niemelä <matias@yearofmoo.com>
2020-06-01 21:45:08 +0000

    c3651cec0b docs(core): fix typo in decorators.ts relating to the use of Object.defineProperty. (#37369)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+38.sha-3b52f70
+ * @license Angular v10.0.0-rc.0+39.sha-c3651ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6105,7 +6105,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+38.sha-3b52f70');
+    var VERSION = new core.Version('10.0.0-rc.0+39.sha-c3651ce');
 
     /**
      * @license
