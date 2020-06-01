f28df2861e204b4a2532a453374be94d68183bf8
Matias Niemelä <matias@yearofmoo.com>
2020-06-01 18:44:27 +0000

    cf86f72eb7 release: cut the v10.0.0-rc.2 release

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+23.sha-d16a7f3
+ * @license Angular v10.0.0-rc.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+23.sha-d16a7f3
+ * @license Angular v10.0.0-rc.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6105,7 +6105,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+23.sha-d16a7f3');
+    var VERSION = new core.Version('10.0.0-rc.2');
 
     /**
      * @license
