57eaf2c627363ef4180a93a8a2aa3cb886589366
Matias Niemelä <matias@yearofmoo.com>
2020-06-02 19:41:54 +0000

    cb6996b5c3 build: fix integration payload sizes

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2
+ * @license Angular v10.0.0-rc.2+7.sha-cb6996b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -134,7 +134,7 @@ function setupTestingRouter(urlSerializer, contexts, location, loader, compiler,
  *
  * ```
  * beforeEach(() => {
- *   TestBed.configureTestModule({
+ *   TestBed.configureTestingModule({
  *     imports: [
  *       RouterTestingModule.withRoutes(
  *         [{path: '', component: BlankCmp}, {path: 'simple', component: SimpleCmp}]
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2
+ * @license Angular v10.0.0-rc.2+7.sha-cb6996b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
