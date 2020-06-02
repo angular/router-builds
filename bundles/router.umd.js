54832e048c314482c57355c674509ed954419198
Matias Niemelä <matias@yearofmoo.com>
2020-06-02 21:49:56 +0000

    561c0f81a0 perf(ngcc): allow immediately reporting a stale lock file (#37250)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+44.sha-1b55da1
+ * @license Angular v10.0.0-rc.0+46.sha-561c0f8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6105,7 +6105,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+44.sha-1b55da1');
+    var VERSION = new core.Version('10.0.0-rc.0+46.sha-561c0f8');
 
     /**
      * @license
