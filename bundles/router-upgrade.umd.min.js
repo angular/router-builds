b440c04664915dd32c4ade43d497378d0d4f60f7
Matias Niemelä <matias@yearofmoo.com>
2020-06-02 21:57:15 +0000

    044a19966d ci: temporarily disable Android 10 browser unit tests on Saucelabs (#37399)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+47.sha-17e9819
+ * @license Angular v10.0.0-rc.0+48.sha-044a199
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+47.sha-17e9819
+ * @license Angular v10.0.0-rc.0+48.sha-044a199
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6116,7 +6116,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+47.sha-17e9819');
+    var VERSION = new i0.Version('10.0.0-rc.0+48.sha-044a199');
 
     /**
      * @license
