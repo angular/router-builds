92b65fb0b25278a573e8cc3742ed3e06c98d3f41
Matias Niemelä <matias@yearofmoo.com>
2020-06-02 18:16:09 +0000

    1b55da10b1 build: fix integration payload sizes

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+39.sha-c3651ce
+ * @license Angular v10.0.0-rc.0+44.sha-1b55da1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6105,7 +6105,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+39.sha-c3651ce');
+    var VERSION = new core.Version('10.0.0-rc.0+44.sha-1b55da1');
 
     /**
      * @license
