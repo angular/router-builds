6e6a4d5a1557b15c3ea79e9145d7f5164659917c
atscott <atscott@google.com>
2020-06-03 18:42:54 +0000

    2d86dbb090 docs: update aio in support for #BlackLivesMatter (#37409)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+11.sha-91767ff
+ * @license Angular v10.0.0-rc.2+12.sha-2d86dbb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5509,7 +5509,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.2+11.sha-91767ff');
+const VERSION = new Version('10.0.0-rc.2+12.sha-2d86dbb');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+11.sha-91767ff
+ * @license Angular v10.0.0-rc.2+12.sha-2d86dbb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
