510055f4bf482c88636324f10cf5d962f6a048d9
atscott <atscott@google.com>
2020-06-03 22:53:11 +0000

    65c3888d01 docs: update file header to be correct (#37425)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+57.sha-e648a0c
+ * @license Angular v10.0.0-rc.0+58.sha-65c3888
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6116,7 +6116,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+57.sha-e648a0c');
+    var VERSION = new i0.Version('10.0.0-rc.0+58.sha-65c3888');
 
     /**
      * @license
