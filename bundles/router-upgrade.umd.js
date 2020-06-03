a6bbbe18f5b393682ed175f91e3b7d4b9c0ee4ce
atscott <atscott@google.com>
2020-06-03 20:41:38 +0000

    40f3bb5638 refactor(dev-infra): small changes and fixes (#36800)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+50.sha-06a8ad2
+ * @license Angular v10.0.0-rc.0+54.sha-40f3bb5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+50.sha-06a8ad2
+ * @license Angular v10.0.0-rc.0+54.sha-40f3bb5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6116,7 +6116,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+50.sha-06a8ad2');
+    var VERSION = new i0.Version('10.0.0-rc.0+54.sha-40f3bb5');
 
     /**
      * @license
