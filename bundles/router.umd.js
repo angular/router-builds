5877567ba4c0ba53aed2094ad6daa94f0f6725a3
atscott <atscott@google.com>
2020-06-03 19:55:53 +0000

    06a8ad28a7 refactor(forms): use a type guard to get rid of casts (#32541)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+49.sha-382aa50
+ * @license Angular v10.0.0-rc.0+50.sha-06a8ad2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6105,7 +6105,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+49.sha-382aa50');
+    var VERSION = new core.Version('10.0.0-rc.0+50.sha-06a8ad2');
 
     /**
      * @license
