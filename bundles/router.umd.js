36c79dc4b8f8748a1b2d56650807f0d8f46141e0
atscott <atscott@google.com>
2020-06-03 19:50:58 +0000

    382aa50c54 docs: refactor template-driven forms doc as a tutorial (#36732)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+48.sha-044a199
+ * @license Angular v10.0.0-rc.0+49.sha-382aa50
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6116,7 +6116,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+48.sha-044a199');
+    var VERSION = new i0.Version('10.0.0-rc.0+49.sha-382aa50');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+48.sha-044a199
+ * @license Angular v10.0.0-rc.0+49.sha-382aa50
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
