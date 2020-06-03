5809f46914c6c8c08871251580ca630a33a43886
atscott <atscott@google.com>
2020-06-03 23:21:42 +0000

    c4f4675ebf fix(language-service): Recover from error in analyzing Ng Modules (#37108)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+59.sha-3482755
+ * @license Angular v10.0.0-rc.0+60.sha-c4f4675
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+59.sha-3482755
+ * @license Angular v10.0.0-rc.0+60.sha-c4f4675
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5509,7 +5509,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+59.sha-3482755');
+const VERSION = new Version('10.0.0-rc.0+60.sha-c4f4675');
 
 /**
  * @license
