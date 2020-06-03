d63829e564d5583e4944043bd7dd034489fd8448
atscott <atscott@google.com>
2020-06-03 20:39:18 +0000

    40f3bb5638 refactor(dev-infra): small changes and fixes (#36800)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+50.sha-06a8ad2
+ * @license Angular v10.0.0-rc.0+54.sha-40f3bb5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5509,7 +5509,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+50.sha-06a8ad2');
+const VERSION = new Version('10.0.0-rc.0+54.sha-40f3bb5');
 
 /**
  * @license
