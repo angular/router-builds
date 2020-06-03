01a7c301128007dd83ed5fce4c2bed40e4b228f9
atscott <atscott@google.com>
2020-06-03 23:16:07 +0000

    34827559b9 docs: update the stackblitz in the GitHub Issue template (#37219)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+58.sha-65c3888
+ * @license Angular v10.0.0-rc.0+59.sha-3482755
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6116,7 +6116,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+58.sha-65c3888');
+    var VERSION = new i0.Version('10.0.0-rc.0+59.sha-3482755');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+58.sha-65c3888
+ * @license Angular v10.0.0-rc.0+59.sha-3482755
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5534,7 +5534,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+58.sha-65c3888');
+const VERSION = new Version('10.0.0-rc.0+59.sha-3482755');
 
 /**
  * @license
