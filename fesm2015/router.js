f28a901e76d89ea67def6c034022ff7f5d353fa0
atscott <atscott@google.com>
2020-06-04 18:08:58 +0000

    c8f7fc22c7 refactor(dev-infra): change required base commit sha (#37424)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+68.sha-c025357
+ * @license Angular v10.0.0-rc.0+70.sha-c8f7fc2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5534,7 +5534,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+68.sha-c025357');
+const VERSION = new Version('10.0.0-rc.0+70.sha-c8f7fc2');
 
 /**
  * @license
