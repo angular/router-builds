14b2ad476ceae38e8f487fc0d45928ee2255908e
atscott <atscott@google.com>
2020-06-04 16:38:22 +0000

    6e7bd939f6 perf(ngcc): cache parsed tsconfig between runs (#37417)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+60.sha-c4f4675
+ * @license Angular v10.0.0-rc.0+62.sha-6e7bd93
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6105,7 +6105,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+60.sha-c4f4675');
+    var VERSION = new core.Version('10.0.0-rc.0+62.sha-6e7bd93');
 
     /**
      * @license
