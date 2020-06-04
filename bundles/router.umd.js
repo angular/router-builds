9abbb193f9d4d3b566c72488efaeaf07196c7013
atscott <atscott@google.com>
2020-06-04 16:40:47 +0000

    2021ad12cf ci(docs-infra): skip deploying RC version when lexicographically smaller than stable (#37426)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+62.sha-6e7bd93
+ * @license Angular v10.0.0-rc.0+61.sha-2021ad1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6105,7 +6105,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+62.sha-6e7bd93');
+    var VERSION = new core.Version('10.0.0-rc.0+61.sha-2021ad1');
 
     /**
      * @license
