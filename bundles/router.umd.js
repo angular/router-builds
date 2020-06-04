20f781353b11d0b5e766847c3b1b2a0847014917
atscott <atscott@google.com>
2020-06-04 16:44:14 +0000

    c61decf7e5 docs: add blank line before header (#37391)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+61.sha-2021ad1
+ * @license Angular v10.0.0-rc.0+63.sha-c61decf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6105,7 +6105,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+61.sha-2021ad1');
+    var VERSION = new core.Version('10.0.0-rc.0+63.sha-c61decf');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+61.sha-2021ad1
+ * @license Angular v10.0.0-rc.0+63.sha-c61decf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
