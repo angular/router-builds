50a3d7a3b24711c1de8f181f1379ddb9a09868d2
atscott <atscott@google.com>
2020-06-04 16:45:58 +0000

    57411c85b9 feat(ngcc): implement a program-based entry-point finder (#37075)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+63.sha-c61decf
+ * @license Angular v10.0.0-rc.0+67.sha-57411c8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6105,7 +6105,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+63.sha-c61decf');
+    var VERSION = new core.Version('10.0.0-rc.0+67.sha-57411c8');
 
     /**
      * @license
