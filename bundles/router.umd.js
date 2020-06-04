00e74b2b6c20a63762c57e158a459304ca7ef193
atscott <atscott@google.com>
2020-06-04 20:08:13 +0000

    dffcca73e4 fix(dev-infra): clean up usages within pullapprove tooling (#37338)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+70.sha-c8f7fc2
+ * @license Angular v10.0.0-rc.0+71.sha-dffcca7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6116,7 +6116,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+70.sha-c8f7fc2');
+    var VERSION = new i0.Version('10.0.0-rc.0+71.sha-dffcca7');
 
     /**
      * @license
