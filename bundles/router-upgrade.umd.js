f8a8d78ff89706cc11618d05f6305d530a0de049
atscott <atscott@google.com>
2020-06-05 00:10:22 +0000

    fff424a35f fix(common): prevent duplicate URL change notifications (#37404)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+48.sha-d713e33
+ * @license Angular v10.0.0-rc.2+50.sha-fff424a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+48.sha-d713e33
+ * @license Angular v10.0.0-rc.2+50.sha-fff424a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6105,7 +6105,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.2+48.sha-d713e33');
+    var VERSION = new core.Version('10.0.0-rc.2+50.sha-fff424a');
 
     /**
      * @license
