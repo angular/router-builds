0fe9d7591c9cd4464071173106e831daa925f8f3
atscott <atscott@google.com>
2020-06-05 17:13:12 +0000

    85b6c94cc6 refactor(dev-infra): move GitClient to common util (#37318)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+77.sha-d4c0962
+ * @license Angular v10.0.0-rc.0+79.sha-85b6c94
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+77.sha-d4c0962
+ * @license Angular v10.0.0-rc.0+79.sha-85b6c94
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6116,7 +6116,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+77.sha-d4c0962');
+    var VERSION = new i0.Version('10.0.0-rc.0+79.sha-85b6c94');
 
     /**
      * @license
