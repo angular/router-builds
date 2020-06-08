e58bd7afe6d91fdea06539c8514efc4f5d65d105
atscott <atscott@google.com>
2020-06-08 16:55:31 +0000

    3a116179b1 build(docs-infra): upgrade cli command docs sources to b76099083 (#37471)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+88.sha-d63ecf4
+ * @license Angular v10.0.0-rc.0+90.sha-3a11617
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6105,7 +6105,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+88.sha-d63ecf4');
+    var VERSION = new core.Version('10.0.0-rc.0+90.sha-3a11617');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+88.sha-d63ecf4
+ * @license Angular v10.0.0-rc.0+90.sha-3a11617
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
