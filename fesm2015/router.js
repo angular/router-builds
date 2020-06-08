27536d7cebdab62caf0e83d779d15f6eb1003777
atscott <atscott@google.com>
2020-06-08 16:54:16 +0000

    5e64c2b1df style(ngcc): post-merge review tidy up (#37461)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+86.sha-d887ba8
+ * @license Angular v10.0.0-rc.0+89.sha-5e64c2b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5534,7 +5534,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+86.sha-d887ba8');
+const VERSION = new Version('10.0.0-rc.0+89.sha-5e64c2b');
 
 /**
  * @license
