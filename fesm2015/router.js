72c04c787b27cf86269d6872c15c03ecfca278d5
atscott <atscott@google.com>
2020-06-08 22:08:48 +0000

    713fb3b587 docs(docs-infra): fix small typo (#37258)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+94.sha-db11a0d
+ * @license Angular v10.0.0-rc.0+96.sha-713fb3b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5534,7 +5534,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+94.sha-db11a0d');
+const VERSION = new Version('10.0.0-rc.0+96.sha-713fb3b');
 
 /**
  * @license
