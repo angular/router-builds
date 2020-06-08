92e3ef8570408c554aa3415395950ff27b7cd3bf
atscott <atscott@google.com>
2020-06-08 16:38:29 +0000

    0c75a06b1e build: upgrade to bazel 3.2.0 and rules_nodejs 1.7.0 (#37358)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+84.sha-aaa2009
+ * @license Angular v10.0.0-rc.0+85.sha-0c75a06
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6105,7 +6105,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+84.sha-aaa2009');
+    var VERSION = new core.Version('10.0.0-rc.0+85.sha-0c75a06');
 
     /**
      * @license
