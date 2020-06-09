1f663c472ce36e77626fbadce4c50c1fe8bc5b42
atscott <atscott@google.com>
2020-06-09 15:48:15 +0000

    7301e70ddd fix(platform-server): correctly handle absolute relative URLs (#37341)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+101.sha-55979fe
+ * @license Angular v10.0.0-rc.0+104.sha-7301e70
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6110,7 +6110,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+101.sha-55979fe');
+    var VERSION = new core.Version('10.0.0-rc.0+104.sha-7301e70');
 
     /**
      * @license
