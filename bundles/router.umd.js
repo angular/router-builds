ac74807ba4d3ad06f805c8cc059358c67610b11c
atscott <atscott@google.com>
2020-06-09 15:48:25 +0000

    a3313709ea fix(dev-infra): local changes check not working (#37489)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+104.sha-7301e70
+ * @license Angular v10.0.0-rc.0+107.sha-a331370
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6110,7 +6110,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+104.sha-7301e70');
+    var VERSION = new core.Version('10.0.0-rc.0+107.sha-a331370');
 
     /**
      * @license
