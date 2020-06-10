b08fddcaac6faa92171a352575820f1050f223b9
atscott <atscott@google.com>
2020-06-10 18:50:04 +0000

    f7997256fc release: cut the v9.1.11 release

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.10+1.sha-dc9da17
+ * @license Angular v9.1.11
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.10+1.sha-dc9da17
+ * @license Angular v9.1.11
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -10206,7 +10206,7 @@ function provideRouterInitializer() {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.10+1.sha-dc9da17');
+const VERSION = new Version('9.1.11');
 
 /**
  * @fileoverview added by tsickle
