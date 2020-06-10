072821d91584ad27961f00e4c8dc3b6ccbb351f8
atscott <atscott01@gmail.com>
2020-06-10 18:23:55 +0000

    dc9da17f37 Revert "fix(elements): fire custom element output events during component initialization"

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.10
+ * @license Angular v9.1.10+1.sha-dc9da17
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6111,7 +6111,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.10');
+    var VERSION = new core.Version('9.1.10+1.sha-dc9da17');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.10
+ * @license Angular v9.1.10+1.sha-dc9da17
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
