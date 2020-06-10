7f3f3c82f9134a0025ea655b665106d2cef63f52
atscott <atscott@google.com>
2020-06-10 19:07:18 +0000

    021dd33ed8 docs: release notes for the v10.0.0-rc.4 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+116.sha-684726c
+ * @license Angular v10.0.0-rc.0+117.sha-021dd33
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5505,7 +5505,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+116.sha-684726c');
+const VERSION = new Version('10.0.0-rc.0+117.sha-021dd33');
 
 /**
  * @license
