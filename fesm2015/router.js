fb6c201df57c91d8796145ebc4f38e4b2bed5506
atscott <atscott@google.com>
2020-06-10 19:16:30 +0000

    a398d6af45 docs: remove redundant web-worker nav (#37289)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+119.sha-ae36486
+ * @license Angular v10.0.0-rc.0+121.sha-a398d6a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5539,7 +5539,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+119.sha-ae36486');
+const VERSION = new Version('10.0.0-rc.0+121.sha-a398d6a');
 
 /**
  * @license
