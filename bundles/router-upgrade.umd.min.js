f518d0e8836fcbccaa3824043604bee091f7158b
atscott <atscott01@gmail.com>
2020-06-10 17:50:14 +0000

    7b005bb9cf Revert "fix(elements): fire custom element output events during component initialization (#36161)" (#37524)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+114.sha-401ef71
+ * @license Angular v10.0.0-rc.0+115.sha-7b005bb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+114.sha-401ef71
+ * @license Angular v10.0.0-rc.0+115.sha-7b005bb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5505,7 +5505,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+114.sha-401ef71');
+const VERSION = new Version('10.0.0-rc.0+115.sha-7b005bb');
 
 /**
  * @license
