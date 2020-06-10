f943673bc5fbb01a73cf0c5f9f754b9fbee33f4f
atscott <atscott@google.com>
2020-06-10 19:15:29 +0000

    d76a872efb docs: add Mike Huang as a GDE (#36472)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+117.sha-021dd33
+ * @license Angular v10.0.0-rc.0+122.sha-d76a872
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6121,7 +6121,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+117.sha-021dd33');
+    var VERSION = new i0.Version('10.0.0-rc.0+122.sha-d76a872');
 
     /**
      * @license
