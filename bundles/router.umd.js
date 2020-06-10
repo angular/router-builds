9dd53ac7688c23ec5a264851e0e2a211c6a0ff4e
atscott <atscott@google.com>
2020-06-10 16:50:39 +0000

    401ef71ae5 fix(compiler-cli): downlevel angular decorators to static properties (#37382)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+113.sha-0a1d078
+ * @license Angular v10.0.0-rc.0+114.sha-401ef71
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6121,7 +6121,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+113.sha-0a1d078');
+    var VERSION = new i0.Version('10.0.0-rc.0+114.sha-401ef71');
 
     /**
      * @license
