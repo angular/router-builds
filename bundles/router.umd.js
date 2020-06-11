990ab4f8cd21e3b67a34bf9885b58f683b52811d
Misko Hevery <misko@hevery.com>
2020-06-11 18:38:03 +0000

    e5b09cc49a docs: add example links to 'DoCheck' lifeycle hook docs (#36574)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+128.sha-b199ef6
+ * @license Angular v10.0.0-rc.0+129.sha-e5b09cc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6121,7 +6121,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+128.sha-b199ef6');
+    var VERSION = new i0.Version('10.0.0-rc.0+129.sha-e5b09cc');
 
     /**
      * @license
