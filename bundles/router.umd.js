dda1171ae94c148949f8d9c3d547c8fe15c503fd
Misko Hevery <misko@hevery.com>
2020-06-15 17:04:30 +0000

    d330cd85ac refactor: move `hover` into a proper `@media` hover (#37320)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+172.sha-09d6dc5
+ * @license Angular v10.0.0-rc.0+173.sha-d330cd8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6129,7 +6129,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+172.sha-09d6dc5');
+    var VERSION = new i0.Version('10.0.0-rc.0+173.sha-d330cd8');
 
     /**
      * @license
