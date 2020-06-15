dbe5823ed5abc147fce0fae89a9c70f17f449efa
Misko Hevery <misko@hevery.com>
2020-06-15 20:17:26 +0000

    66e6b932d8 refactor(compiler-cli): skip class decorators in tooling constructor parameters transform (#37545)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+179.sha-cc49a91
+ * @license Angular v10.0.0-rc.0+180.sha-66e6b93
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6129,7 +6129,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+179.sha-cc49a91');
+    var VERSION = new i0.Version('10.0.0-rc.0+180.sha-66e6b93');
 
     /**
      * @license
