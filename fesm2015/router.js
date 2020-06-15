18c6c2fb1a95c49954c1e793af2c4f7fcbcf7142
Misko Hevery <misko@hevery.com>
2020-06-15 21:55:10 +0000

    87a679b210 build: import in-memory-web-api project (#37182)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+185.sha-2a145f2
+ * @license Angular v10.0.0-rc.0+182.sha-87a679b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5483,7 +5483,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+185.sha-2a145f2');
+const VERSION = new Version('10.0.0-rc.0+182.sha-87a679b');
 
 /**
  * @license
