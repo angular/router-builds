335331d8d3536175bc375b1b6e2ffb723d32008d
Misko Hevery <misko@hevery.com>
2020-06-15 16:59:59 +0000

    09d6dc50ef docs: Update documentation to reflect addition of `tsconfig.base.json`. (#37222)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+171.sha-7ac166f
+ * @license Angular v10.0.0-rc.0+172.sha-09d6dc5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+171.sha-7ac166f
+ * @license Angular v10.0.0-rc.0+172.sha-09d6dc5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6129,7 +6129,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+171.sha-7ac166f');
+    var VERSION = new i0.Version('10.0.0-rc.0+172.sha-09d6dc5');
 
     /**
      * @license
