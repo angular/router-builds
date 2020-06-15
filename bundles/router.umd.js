e8abad3d19899eeffe4973d8fa92d47fd538c179
Misko Hevery <misko@hevery.com>
2020-06-15 16:56:25 +0000

    7ac166ff08 feat(bazel): expose explicit mapping from closure to devmode files (#36262)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+170.sha-ae4f81c
+ * @license Angular v10.0.0-rc.0+171.sha-7ac166f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6109,7 +6109,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+170.sha-ae4f81c');
+    var VERSION = new core.Version('10.0.0-rc.0+171.sha-7ac166f');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+170.sha-ae4f81c
+ * @license Angular v10.0.0-rc.0+171.sha-7ac166f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5483,7 +5483,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+170.sha-ae4f81c');
+const VERSION = new Version('10.0.0-rc.0+171.sha-7ac166f');
 
 /**
  * @license
