091119531d9639ca4c4012b4cd1624b708439a61
Misko Hevery <misko@hevery.com>
2020-06-15 16:34:04 +0000

    ae4f81cbab docs: Change 'function' to 'method' for clarity that getHereos() is (#35998) intended as a class method

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+169.sha-c1fdfd4
+ * @license Angular v10.0.0-rc.0+170.sha-ae4f81c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6129,7 +6129,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+169.sha-c1fdfd4');
+    var VERSION = new i0.Version('10.0.0-rc.0+170.sha-ae4f81c');
 
     /**
      * @license
