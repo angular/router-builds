ca2cff552d379f0e86a1de78ccf7ae37d974a6c5
Misko Hevery <misko@hevery.com>
2020-06-16 19:26:20 +0000

    26b0f3dc96 docs: add side effect `package.json` in app structure (#37521)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+4.sha-97bb88f
+ * @license Angular v10.0.0-rc.6+7.sha-26b0f3d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5483,7 +5483,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.6+4.sha-97bb88f');
+const VERSION = new Version('10.0.0-rc.6+7.sha-26b0f3d');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+4.sha-97bb88f
+ * @license Angular v10.0.0-rc.6+7.sha-26b0f3d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
