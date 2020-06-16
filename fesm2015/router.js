fa50afba472b2bcc7736c83bd3294c5d6a249af0
Misko Hevery <misko@hevery.com>
2020-06-16 19:23:21 +0000

    7521834296 docs: add new section for CLI deprecations (#37332)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+191.sha-8154bbd
+ * @license Angular v10.0.0-rc.0+188.sha-7521834
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5483,7 +5483,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+191.sha-8154bbd');
+const VERSION = new Version('10.0.0-rc.0+188.sha-7521834');
 
 /**
  * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+191.sha-8154bbd
+ * @license Angular v10.0.0-rc.0+188.sha-7521834
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
