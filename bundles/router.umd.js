e0a7fa205f9da070d168bdfc67939b51c1080373
Misko Hevery <misko@hevery.com>
2020-06-16 19:26:02 +0000

    7521834296 docs: add new section for CLI deprecations (#37332)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+191.sha-8154bbd
+ * @license Angular v10.0.0-rc.0+188.sha-7521834
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6129,7 +6129,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+191.sha-8154bbd');
+    var VERSION = new i0.Version('10.0.0-rc.0+188.sha-7521834');
 
     /**
      * @license
