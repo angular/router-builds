22d4cb2c61ac3fc4d133aac75f8abc73403be3d4
Misko Hevery <misko@hevery.com>
2020-06-18 23:25:54 +0000

    bd7f440357 perf(ngcc): shortcircuit tokenizing in ESM dependency host (#37639)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+193.sha-71f008f
+ * @license Angular v10.0.0-rc.0+198.sha-bd7f440
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5487,7 +5487,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+193.sha-71f008f');
+const VERSION = new Version('10.0.0-rc.0+198.sha-bd7f440');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+193.sha-71f008f
+ * @license Angular v10.0.0-rc.0+198.sha-bd7f440
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
