75bd61b97dcb0f110aa56fab851d888a9661b0e6
Misko Hevery <misko@hevery.com>
2020-06-22 18:16:48 +0000

    7e0eccc4ef docs: Refactor-i18n (#36924)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+199.sha-2a970a0
+ * @license Angular v10.0.0-rc.0+200.sha-7e0eccc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6125,7 +6125,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+199.sha-2a970a0');
+    var VERSION = new i0.Version('10.0.0-rc.0+200.sha-7e0eccc');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+199.sha-2a970a0
+ * @license Angular v10.0.0-rc.0+200.sha-7e0eccc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
