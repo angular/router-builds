55184efa0490e676f6c38e4bd90e43077bc80f8f
Igor Minar <igor@angular.io>
2020-06-25 21:08:42 +0000

    0feb560136 fixup! feat(docs-infra): update deploy-to-firebase.sh script to support v9 multisite setup

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.11+1.sha-6c1ab47
+ * @license Angular v9.1.11+3.sha-0feb560
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.11+1.sha-6c1ab47
+ * @license Angular v9.1.11+3.sha-0feb560
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5897,7 +5897,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.11+1.sha-6c1ab47');
+var VERSION = new Version('9.1.11+3.sha-0feb560');
 
 /**
  * @license
