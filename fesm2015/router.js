f80ee9f14a316aa7c2875112a427157909145699
Misko Hevery <misko@hevery.com>
2020-06-25 03:16:09 +0000

    f5bbfd7b8c docs(changelog): fix v10 announcement url (#37722)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+226.sha-41ce524
+ * @license Angular v10.0.0-rc.0+227.sha-f5bbfd7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5479,7 +5479,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+226.sha-41ce524');
+const VERSION = new Version('10.0.0-rc.0+227.sha-f5bbfd7');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+226.sha-41ce524
+ * @license Angular v10.0.0-rc.0+227.sha-f5bbfd7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
