4ebdc7dff564c55ad234d9a149f5081d97b002e8
Andrew Kushnir <akushnir@google.com>
2020-06-25 23:06:24 +0000

    8572e885b4 test(compiler-cli): fix assertion of diagnostic filename on Windows (#37758)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+263.sha-0e3e9cd
+ * @license Angular v10.0.0-rc.0+264.sha-8572e88
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6118,7 +6118,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+263.sha-0e3e9cd');
+    var VERSION = new core.Version('10.0.0-rc.0+264.sha-8572e88');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+263.sha-0e3e9cd
+ * @license Angular v10.0.0-rc.0+264.sha-8572e88
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+263.sha-0e3e9cd');
+const VERSION = new Version('10.0.0-rc.0+264.sha-8572e88');
 
 /**
  * @license
