2a628d16491c35b10c3e355e5bff9543162c2666
Andrew Kushnir <akushnir@google.com>
2020-06-25 21:14:18 +0000

    2b2146bc58 docs: Uses correct component in the `MessageService` (#37666)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+243.sha-e36d5b2
+ * @license Angular v10.0.0-rc.0+247.sha-2b2146b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6168,7 +6168,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+243.sha-e36d5b2');
+    var VERSION = new i0.Version('10.0.0-rc.0+247.sha-2b2146b');
 
     /**
      * @license
