5e1cd2c3ecf84b54b0809f6ddc378c3836aaf9d6
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:01:51 +0000

    bedc0451a0 docs(ngcc): add additional next steps to an error (#37672)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+228.sha-4481beb
+ * @license Angular v10.0.0-rc.0+231.sha-bedc045
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6166,7 +6166,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+228.sha-4481beb');
+    var VERSION = new i0.Version('10.0.0-rc.0+231.sha-bedc045');
 
     /**
      * @license
