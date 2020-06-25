7738a40832da783a4b2e85889f286cf0dfbf53f6
Andrew Kushnir <akushnir@google.com>
2020-06-25 22:46:25 +0000

    a9a4edebe2 fix(docs-infra): fix typo in the deploy-to-firebase.sh script (#37754)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+20.sha-64f2ffa
+ * @license Angular v10.0.0+21.sha-a9a4ede
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5478,7 +5478,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0+20.sha-64f2ffa');
+const VERSION = new Version('10.0.0+21.sha-a9a4ede');
 
 /**
  * @license
