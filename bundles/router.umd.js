a86ac5a4df7d1472db38fe09bdab745b6740af69
Misko Hevery <misko@hevery.com>
2020-06-25 03:20:34 +0000

    4481bebb38 docs: add v9.angular.io to the angular.io version picker (#37719)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+227.sha-f5bbfd7
+ * @license Angular v10.0.0-rc.0+228.sha-4481beb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6166,7 +6166,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+227.sha-f5bbfd7');
+    var VERSION = new i0.Version('10.0.0-rc.0+228.sha-4481beb');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+227.sha-f5bbfd7
+ * @license Angular v10.0.0-rc.0+228.sha-4481beb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
