046693476bf9454ee02c5159ee4b1877ef7da401
Andrew Kushnir <akushnir@google.com>
2020-06-25 21:43:26 +0000

    ce879fc416 refactor(compiler-cli): more accurate reporting of complex function call (#37587)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+256.sha-5103d90
+ * @license Angular v10.0.0-rc.0+259.sha-ce879fc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6168,7 +6168,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+256.sha-5103d90');
+    var VERSION = new i0.Version('10.0.0-rc.0+259.sha-ce879fc');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+256.sha-5103d90
+ * @license Angular v10.0.0-rc.0+259.sha-ce879fc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
