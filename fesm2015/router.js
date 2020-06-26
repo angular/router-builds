110dda5399ef142438d0edc13cdc2f6a96669e00
Andrew Kushnir <akushnir@google.com>
2020-06-26 18:38:40 +0000

    acf3cff9ee ci: exclude "docs" commit type from minBodyLength commit message validation (#37764)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+276.sha-e0eeb4a
+ * @license Angular v10.0.0-rc.0+281.sha-acf3cff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+276.sha-e0eeb4a');
+const VERSION = new Version('10.0.0-rc.0+281.sha-acf3cff');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+276.sha-e0eeb4a
+ * @license Angular v10.0.0-rc.0+281.sha-acf3cff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
