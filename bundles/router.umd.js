d8d02b33cf2053a24ba778f5a701e72b36f874ad
Andrew Kushnir <akushnir@google.com>
2020-06-26 00:26:29 +0000

    b2816a1536 docs: typo fixes for schematics-for-libraries.md (#37753)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+264.sha-8572e88
+ * @license Angular v10.0.0-rc.0+267.sha-b2816a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6118,7 +6118,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+264.sha-8572e88');
+    var VERSION = new core.Version('10.0.0-rc.0+267.sha-b2816a1');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+264.sha-8572e88
+ * @license Angular v10.0.0-rc.0+267.sha-b2816a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+264.sha-8572e88');
+const VERSION = new Version('10.0.0-rc.0+267.sha-b2816a1');
 
 /**
  * @license
