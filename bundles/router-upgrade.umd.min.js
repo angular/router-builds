4857813b741e64d67d9d23c40eba61d0c2487131
Andrew Kushnir <akushnir@google.com>
2020-06-26 22:19:25 +0000

    ae5257cda6 fix(language-service): incorrect autocomplete results on unknown symbol (#37518)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+282.sha-8fd8143
+ * @license Angular v10.0.0-rc.0+283.sha-ae5257c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+282.sha-8fd8143
+ * @license Angular v10.0.0-rc.0+283.sha-ae5257c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6118,7 +6118,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+282.sha-8fd8143');
+    var VERSION = new core.Version('10.0.0-rc.0+283.sha-ae5257c');
 
     /**
      * @license
