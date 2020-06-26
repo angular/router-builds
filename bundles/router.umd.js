dffa7192044d51bdac27afd146f1eeb43494c636
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:25:47 +0000

    c942662d79 fix(language-service): reinstate getExternalFiles() (#37750)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+273.sha-f954ab6
+ * @license Angular v10.0.0-rc.0+275.sha-c942662
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6168,7 +6168,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+273.sha-f954ab6');
+    var VERSION = new i0.Version('10.0.0-rc.0+275.sha-c942662');
 
     /**
      * @license
