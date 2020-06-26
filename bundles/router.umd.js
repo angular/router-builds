4e505428a6497098e72070283dce4efae40fd69c
Andrew Kushnir <akushnir@google.com>
2020-06-26 18:42:34 +0000

    0879d2e85d refactor(core): throw more descriptive error message in case of invalid host element (#35916)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+281.sha-acf3cff
+ * @license Angular v10.0.0-rc.0+278.sha-0879d2e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6168,7 +6168,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+281.sha-acf3cff');
+    var VERSION = new i0.Version('10.0.0-rc.0+278.sha-0879d2e');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+281.sha-acf3cff
+ * @license Angular v10.0.0-rc.0+278.sha-0879d2e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
