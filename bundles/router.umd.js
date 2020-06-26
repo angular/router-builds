7f5a840a423d198639e5807dcb70f8b965af9153
Andrew Kushnir <akushnir@google.com>
2020-06-26 22:29:18 +0000

    c00f4ab2ae fix(core): don't consider inherited NG_ELEMENT_ID during DI (#37574)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+287.sha-2038568
+ * @license Angular v10.0.0-rc.0+288.sha-c00f4ab
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6118,7 +6118,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+287.sha-2038568');
+    var VERSION = new core.Version('10.0.0-rc.0+288.sha-c00f4ab');
 
     /**
      * @license
