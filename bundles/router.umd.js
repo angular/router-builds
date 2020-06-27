2e803d41d3e4d9aca352ccf659e628673c5d2e47
Andrew Kushnir <akushnir@google.com>
2020-06-27 00:04:16 +0000

    98c047b763 ci: decrease payload size limit for integration tests (#37784)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+288.sha-c00f4ab
+ * @license Angular v10.0.0-rc.0+290.sha-98c047b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6168,7 +6168,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+288.sha-c00f4ab');
+    var VERSION = new i0.Version('10.0.0-rc.0+290.sha-98c047b');
 
     /**
      * @license
