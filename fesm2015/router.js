52a8cbdd38491354152841483dd115e1b96432a8
Andrew Kushnir <akushnir@google.com>
2020-06-29 19:36:35 +0000

    ee1526029f docs: add Amadou Sall to GDE page (#36509)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+292.sha-eba9dd3
+ * @license Angular v10.0.0-rc.0+293.sha-ee15260
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+292.sha-eba9dd3');
+const VERSION = new Version('10.0.0-rc.0+293.sha-ee15260');
 
 /**
  * @license
