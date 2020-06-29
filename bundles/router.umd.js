a5d3d1b9b002c9559ca6bfcd550d3fd636e6815c
Andrew Kushnir <akushnir@google.com>
2020-06-29 18:05:21 +0000

    eba9dd364e fix(elements): fire custom element output events during component initialization (#37570)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+291.sha-78a4476
+ * @license Angular v10.0.0-rc.0+292.sha-eba9dd3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6168,7 +6168,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+291.sha-78a4476');
+    var VERSION = new i0.Version('10.0.0-rc.0+292.sha-eba9dd3');
 
     /**
      * @license
