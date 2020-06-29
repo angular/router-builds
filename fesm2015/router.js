c2b69f01686870f1a8082c3bc6c49fd10652ee2e
Andrew Kushnir <akushnir@google.com>
2020-06-29 19:40:53 +0000

    e07d940b9b docs: remove first person from 2 sentences (#37768)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+293.sha-ee15260
+ * @license Angular v10.0.0-rc.0+294.sha-e07d940
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5482,7 +5482,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+293.sha-ee15260');
+const VERSION = new Version('10.0.0-rc.0+294.sha-e07d940');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+293.sha-ee15260
+ * @license Angular v10.0.0-rc.0+294.sha-e07d940
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
