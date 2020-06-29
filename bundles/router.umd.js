53d52d31875755f7871a47429a4a6a8f7a4b1563
Andrew Kushnir <akushnir@google.com>
2020-06-29 20:13:22 +0000

    435a28e937 fix(core): fake_async_fallback should have the same logic with fake-async (#37680)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+298.sha-a87951a
+ * @license Angular v10.0.0-rc.0+300.sha-435a28e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6118,7 +6118,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+298.sha-a87951a');
+    var VERSION = new core.Version('10.0.0-rc.0+300.sha-435a28e');
 
     /**
      * @license
