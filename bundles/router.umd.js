b0e5ec665256e1cf4f5972802dfb804f0e396e58
Andrew Kushnir <akushnir@google.com>
2020-06-29 22:25:32 +0000

    873330b82c docs: correct the spelling mistake in observables error handling code (#36437)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+300.sha-435a28e
+ * @license Angular v10.0.0-rc.0+302.sha-873330b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6168,7 +6168,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+300.sha-435a28e');
+    var VERSION = new i0.Version('10.0.0-rc.0+302.sha-873330b');
 
     /**
      * @license
