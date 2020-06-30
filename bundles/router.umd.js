b18fd4d36890b0614edea2d06192818a1bc1fc2c
Andrew Kushnir <akushnir@google.com>
2020-06-30 19:36:29 +0000

    9206a26e1d docs: clean up api doc in core (#37053)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+308.sha-11dd2be
+ * @license Angular v10.0.0-rc.0+309.sha-9206a26
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6168,7 +6168,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+308.sha-11dd2be');
+    var VERSION = new i0.Version('10.0.0-rc.0+309.sha-9206a26');
 
     /**
      * @license
