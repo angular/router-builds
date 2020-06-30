da3751940c340ba6eda517113b09364ef6a7c9e5
Andrew Kushnir <akushnir@google.com>
2020-06-30 19:35:47 +0000

    11dd2bef2d docs: add missing single quote (#37854)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+307.sha-6fc8069
+ * @license Angular v10.0.0-rc.0+308.sha-11dd2be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6168,7 +6168,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+307.sha-6fc8069');
+    var VERSION = new i0.Version('10.0.0-rc.0+308.sha-11dd2be');
 
     /**
      * @license
