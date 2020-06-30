2d583d288775817268cee0c8660d0f20fa4b9392
Andrew Kushnir <akushnir@google.com>
2020-06-30 00:39:31 +0000

    71956250dd perf(compiler-cli): fix memory leak in retained incremental state (#37835)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+301.sha-a177b1b
+ * @license Angular v10.0.0-rc.0+304.sha-7195625
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6168,7 +6168,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+301.sha-a177b1b');
+    var VERSION = new i0.Version('10.0.0-rc.0+304.sha-7195625');
 
     /**
      * @license
