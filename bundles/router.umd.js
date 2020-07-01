4f345312476440323c9732748d0ff20f42529055
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-01 19:19:20 +0000

    196bfa8fae docs: restore link to lightweight token page in left nav (#37855)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+311.sha-1d5ca55
+ * @license Angular v10.0.0-rc.0+312.sha-196bfa8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6168,7 +6168,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+311.sha-1d5ca55');
+    var VERSION = new i0.Version('10.0.0-rc.0+312.sha-196bfa8');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+311.sha-1d5ca55
+ * @license Angular v10.0.0-rc.0+312.sha-196bfa8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5515,7 +5515,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+311.sha-1d5ca55');
+const VERSION = new Version('10.0.0-rc.0+312.sha-196bfa8');
 
 /**
  * @license
