e4d1b2ccf8e9a6bd4649d473c6c1bdc10fefd397
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-07 19:44:38 +0000

    289a96b078 docs: minor fixes to docs related to updating to v10 (#37897)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+324.sha-cb2ab26
+ * @license Angular v10.0.0-rc.0+325.sha-289a96b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6178,7 +6178,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+324.sha-cb2ab26');
+    var VERSION = new i0.Version('10.0.0-rc.0+325.sha-289a96b');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+324.sha-cb2ab26
+ * @license Angular v10.0.0-rc.0+325.sha-289a96b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5532,7 +5532,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+324.sha-cb2ab26');
+const VERSION = new Version('10.0.0-rc.0+325.sha-289a96b');
 
 /**
  * @license
