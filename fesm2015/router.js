3a40a027979b65ee24a304ddece687fee046a67f
atscott <atscott@google.com>
2020-07-08 23:32:19 +0000

    e50c7dcc4c docs(router): get rid of unnecessary line in wildcard route example and fixing wildcard docregions (#37127)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+3.sha-bdbbff6
+ * @license Angular v10.1.0-next.0+10.sha-e50c7dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5499,7 +5499,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+3.sha-bdbbff6');
+const VERSION = new Version('10.1.0-next.0+10.sha-e50c7dc');
 
 /**
  * @license
