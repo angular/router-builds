67192d0e70a5ea1901754482ff2ac64a2946c759
atscott <atscott@google.com>
2020-07-08 23:27:15 +0000

    f81d3f6199 docs(elements): fix typo (you custom element --> your custom element) (#37966)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+1.sha-d0c0f25
+ * @license Angular v10.1.0-next.0+2.sha-f81d3f6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6128,7 +6128,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.0+1.sha-d0c0f25');
+    var VERSION = new core.Version('10.1.0-next.0+2.sha-f81d3f6');
 
     /**
      * @license
