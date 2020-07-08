11fc2617625ac253b631945970208e6223fa3fff
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-08 19:36:17 +0000

    6eb868b63a build(compiler-cli): fix bazel deps rules for ngtsc testing packages (#37977)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+326.sha-82a2207
+ * @license Angular v10.0.0-rc.0+327.sha-6eb868b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+326.sha-82a2207
+ * @license Angular v10.0.0-rc.0+327.sha-6eb868b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6178,7 +6178,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+326.sha-82a2207');
+    var VERSION = new i0.Version('10.0.0-rc.0+327.sha-6eb868b');
 
     /**
      * @license
