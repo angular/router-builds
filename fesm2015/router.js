674a84b9e673adc80dce0bf2c5f472f4a61f29b7
atscott <atscott@google.com>
2020-07-08 23:29:50 +0000

    f81d3f6199 docs(elements): fix typo (you custom element --> your custom element) (#37966)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+1.sha-d0c0f25
+ * @license Angular v10.1.0-next.0+2.sha-f81d3f6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5532,7 +5532,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+1.sha-d0c0f25');
+const VERSION = new Version('10.1.0-next.0+2.sha-f81d3f6');
 
 /**
  * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+1.sha-d0c0f25
+ * @license Angular v10.1.0-next.0+2.sha-f81d3f6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
