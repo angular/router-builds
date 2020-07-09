239519644a2b4a56a353ac321e976eef9dd6806e
Doug Parker <dgp1130@users.noreply.github.com>
2020-07-09 19:41:18 +0000

    7b7e3d9bcb docs: correct flag default values in `--strict`

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+11.sha-44d0ba0
+ * @license Angular v10.1.0-next.0+11.sha-7b7e3d9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5499,7 +5499,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+11.sha-44d0ba0');
+const VERSION = new Version('10.1.0-next.0+11.sha-7b7e3d9');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+11.sha-44d0ba0
+ * @license Angular v10.1.0-next.0+11.sha-7b7e3d9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
