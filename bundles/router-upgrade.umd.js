7bf1b116a5b119eea17c1af310c8628da394d31d
atscott <atscott@google.com>
2020-07-09 16:44:39 +0000

    b98314e306 docs: add Formly library to the list of resources (#37257)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+9.sha-6b731a0
+ * @license Angular v10.1.0-next.0+11.sha-b98314e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+9.sha-6b731a0
+ * @license Angular v10.1.0-next.0+11.sha-b98314e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5532,7 +5532,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+9.sha-6b731a0');
+const VERSION = new Version('10.1.0-next.0+11.sha-b98314e');
 
 /**
  * @license
