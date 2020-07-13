0dcbe5413c135908b40e60c912fe7aa4f708d40a
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:42:30 +0000

    10aba15154 fix(language-service): remove completion for string (#37983)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+36.sha-0eaa084
+ * @license Angular v10.1.0-next.0+38.sha-10aba15
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+36.sha-0eaa084
+ * @license Angular v10.1.0-next.0+38.sha-10aba15
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6176,7 +6176,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.0+36.sha-0eaa084');
+    var VERSION = new i0.Version('10.1.0-next.0+38.sha-10aba15');
 
     /**
      * @license
