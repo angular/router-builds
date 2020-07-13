4680a5bb7cab6de40a1b1937ccb13fdd6b0d6806
Andrew Scott <atscott01@gmail.com>
2020-07-13 16:53:22 +0000

    d1aa4678be build(docs-infra): ensure the correct files are in the i18n example (#37947)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+31.sha-a259a84
+ * @license Angular v10.1.0-next.0+30.sha-d1aa467
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6176,7 +6176,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.0+31.sha-a259a84');
+    var VERSION = new i0.Version('10.1.0-next.0+30.sha-d1aa467');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+31.sha-a259a84
+ * @license Angular v10.1.0-next.0+30.sha-d1aa467
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5530,7 +5530,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+31.sha-a259a84');
+const VERSION = new Version('10.1.0-next.0+30.sha-d1aa467');
 
 /**
  * @license
