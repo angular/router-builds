9e7819a37a9f2fb0b7a75f1962c15eab9e512c02
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:38:52 +0000

    0eaa084ced docs(router): fix typo 'containa' to 'contains' (#36764)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+35.sha-62e19c8
+ * @license Angular v10.1.0-next.0+36.sha-0eaa084
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6176,7 +6176,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.0+35.sha-62e19c8');
+    var VERSION = new i0.Version('10.1.0-next.0+36.sha-0eaa084');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+35.sha-62e19c8
+ * @license Angular v10.1.0-next.0+36.sha-0eaa084
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
