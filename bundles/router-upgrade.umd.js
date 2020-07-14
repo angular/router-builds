f966d92dcbe566f118d2d994d4eb1e4a87e6ff2e
Andrew Scott <atscott01@gmail.com>
2020-07-14 16:37:48 +0000

    4ec3026a9a docs(router): fix typo in https://angular.io/guide/router#activated-route (#38034)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+42.sha-9021d3f
+ * @license Angular v10.1.0-next.0+43.sha-4ec3026
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+42.sha-9021d3f
+ * @license Angular v10.1.0-next.0+43.sha-4ec3026
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5557,7 +5557,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+42.sha-9021d3f');
+const VERSION = new Version('10.1.0-next.0+43.sha-4ec3026');
 
 /**
  * @license
