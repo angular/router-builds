fdec47b95a4dc12710eb0e21e64e2e9e2be1fd5f
Andrew Scott <atscott01@gmail.com>
2020-07-14 18:27:15 +0000

    396033da80 refactor(forms): remove unnecessary `!` operators from validators (#36805)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+55.sha-d690eec
+ * @license Angular v10.0.3+58.sha-396033d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+55.sha-d690eec
+ * @license Angular v10.0.3+58.sha-396033d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6112,7 +6112,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.3+55.sha-d690eec');
+    var VERSION = new core.Version('10.0.3+58.sha-396033d');
 
     /**
      * @license
