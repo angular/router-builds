c708474dd311165930168805f7888829939963b0
Andrew Scott <atscott01@gmail.com>
2020-07-14 20:42:51 +0000

    f4fac406b9 docs(core): fixes minor typo in initNgDevMode function docs (#38042)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+46.sha-421e807
+ * @license Angular v10.1.0-next.0+48.sha-f4fac40
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5557,7 +5557,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+46.sha-421e807');
+const VERSION = new Version('10.1.0-next.0+48.sha-f4fac40');
 
 /**
  * @license
