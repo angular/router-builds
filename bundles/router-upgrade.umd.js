ee2e4fbc9cb9d7d9c1526297808a705a36fbe864
Andrew Scott <atscott01@gmail.com>
2020-07-14 16:32:23 +0000

    9021d3f5e1 docs: fix live examples in testing guides (#38038)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+41.sha-81542b3
+ * @license Angular v10.1.0-next.0+42.sha-9021d3f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+41.sha-81542b3
+ * @license Angular v10.1.0-next.0+42.sha-9021d3f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6153,7 +6153,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.0+41.sha-81542b3');
+    var VERSION = new core.Version('10.1.0-next.0+42.sha-9021d3f');
 
     /**
      * @license
