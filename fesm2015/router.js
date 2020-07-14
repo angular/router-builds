de4be5d9b969246c00ada49b227725e2853f0411
Andrew Scott <atscott01@gmail.com>
2020-07-14 16:35:04 +0000

    9021d3f5e1 docs: fix live examples in testing guides (#38038)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+41.sha-81542b3
+ * @license Angular v10.1.0-next.0+42.sha-9021d3f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5557,7 +5557,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+41.sha-81542b3');
+const VERSION = new Version('10.1.0-next.0+42.sha-9021d3f');
 
 /**
  * @license
