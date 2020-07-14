e8dfff26a02dbffaab3704e58d612e9257ee49b5
Andrew Scott <atscott01@gmail.com>
2020-07-14 18:25:41 +0000

    421e807f80 docs(forms): Fix typos in template-driven forms tutorial (#37933)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+47.sha-5fd2c8f
+ * @license Angular v10.1.0-next.0+46.sha-421e807
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5524,7 +5524,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+47.sha-5fd2c8f');
+const VERSION = new Version('10.1.0-next.0+46.sha-421e807');
 
 /**
  * @license
