429c50aff8edb62ad8a42105cdf009678511303c
atscott <atscott01@gmail.com>
2020-07-15 18:52:37 +0000

    1d9e56eba0 docs: release notes for the v10.0.4 release

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1
+ * @license Angular v10.1.0-next.1+1.sha-1d9e56e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1
+ * @license Angular v10.1.0-next.1+1.sha-1d9e56e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6153,7 +6153,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.1');
+    var VERSION = new core.Version('10.1.0-next.1+1.sha-1d9e56e');
 
     /**
      * @license
