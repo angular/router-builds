9e12574ec2bb981104cfc107ba7fc21b7c11bfe2
Andrew Kushnir <akushnir@google.com>
2020-07-15 21:15:16 +0000

    469aba0140 docs: add Emma Twersky to DevRel Contributor page (#38084)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+12.sha-d8c47b9
+ * @license Angular v10.1.0-next.1+13.sha-469aba0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+12.sha-d8c47b9
+ * @license Angular v10.1.0-next.1+13.sha-469aba0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5524,7 +5524,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+12.sha-d8c47b9');
+const VERSION = new Version('10.1.0-next.1+13.sha-469aba0');
 
 /**
  * @license
