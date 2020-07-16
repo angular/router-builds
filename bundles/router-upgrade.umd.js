f0175a81830c79ae05d61fa0976d1c7b52615227
Andrew Kushnir <akushnir@google.com>
2020-07-16 18:32:00 +0000

    9ac3383d01 build(language-service): remove typescript from ivy bundle (#38088)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+12.sha-6e32972
+ * @license Angular v10.0.4+15.sha-9ac3383
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+12.sha-6e32972
+ * @license Angular v10.0.4+15.sha-9ac3383
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5476,7 +5476,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.4+12.sha-6e32972');
+const VERSION = new Version('10.0.4+15.sha-9ac3383');
 
 /**
  * @license
