7e920ad86a8f11d85e1949f04f2708b62d5a2692
Andrew Kushnir <akushnir@google.com>
2020-07-17 21:02:25 +0000

    095cfd961d build: Ignore .history for the xyz.local-history VSCode extension (#38121)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+27.sha-38a7021
+ * @license Angular v10.1.0-next.1+28.sha-095cfd9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5558,7 +5558,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+27.sha-38a7021');
+const VERSION = new Version('10.1.0-next.1+28.sha-095cfd9');
 
 /**
  * @license
