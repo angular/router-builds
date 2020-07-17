5174157c16efbe135f373cd559dfabb3d29082c9
Andrew Kushnir <akushnir@google.com>
2020-07-17 19:50:51 +0000

    b3b03c35b5 fix(docs-infra): correctly display SVG icons in IE11 (#38046)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+25.sha-fd45f74
+ * @license Angular v10.1.0-next.1+26.sha-b3b03c3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5591,7 +5591,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+25.sha-fd45f74');
+const VERSION = new Version('10.1.0-next.1+26.sha-b3b03c3');
 
 /**
  * @license
