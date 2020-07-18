bbdc0da64048d69705b76e21aa7a1664e1c18e08
Andrew Kushnir <akushnir@google.com>
2020-07-18 00:20:40 +0000

    8e305e7099 docs: correct flag default values in `--strict` (#37982)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+22.sha-481df83
+ * @license Angular v10.0.4+23.sha-8e305e7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5510,7 +5510,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.4+22.sha-481df83');
+const VERSION = new Version('10.0.4+23.sha-8e305e7');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+22.sha-481df83
+ * @license Angular v10.0.4+23.sha-8e305e7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
