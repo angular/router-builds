84ce0bcae1eedbec50402b9e795d4bf78ba5b355
Andrew Kushnir <akushnir@google.com>
2020-07-20 17:34:06 +0000

    72f1eec3ad docs(core): add note about not mutating multi provider arrays (#37645)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+32.sha-1a7a736
+ * @license Angular v10.1.0-next.1+30.sha-72f1eec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+32.sha-1a7a736
+ * @license Angular v10.1.0-next.1+30.sha-72f1eec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5591,7 +5591,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+32.sha-1a7a736');
+const VERSION = new Version('10.1.0-next.1+30.sha-72f1eec');
 
 /**
  * @license
