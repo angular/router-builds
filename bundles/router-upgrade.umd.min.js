11e0040813bfb2c25b891c06f8d279cf2aa36b68
Andrew Kushnir <akushnir@google.com>
2020-07-20 18:41:05 +0000

    5b31a0a294 docs: separate template syntax into multiple docs (#36954)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+35.sha-5d3d8b8
+ * @license Angular v10.1.0-next.1+36.sha-5b31a0a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+35.sha-5d3d8b8
+ * @license Angular v10.1.0-next.1+36.sha-5b31a0a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6187,7 +6187,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.1+35.sha-5d3d8b8');
+    var VERSION = new core.Version('10.1.0-next.1+36.sha-5b31a0a');
 
     /**
      * @license
