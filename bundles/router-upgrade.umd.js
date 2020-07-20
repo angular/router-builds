58a7fcfdd58b843018db7d06303aabec5d24643d
Andrew Kushnir <akushnir@google.com>
2020-07-20 18:38:10 +0000

    5d3d8b8330 docs: add known issue for bazel (#38074)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+34.sha-c8c7415
+ * @license Angular v10.1.0-next.1+35.sha-5d3d8b8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+34.sha-c8c7415
+ * @license Angular v10.1.0-next.1+35.sha-5d3d8b8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5558,7 +5558,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+34.sha-c8c7415');
+const VERSION = new Version('10.1.0-next.1+35.sha-5d3d8b8');
 
 /**
  * @license
