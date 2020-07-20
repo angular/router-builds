0e6b50314b7002a5721f8b0c5865c2fd446b5556
Andrew Kushnir <akushnir@google.com>
2020-07-20 21:38:42 +0000

    3373453736 feat(common): add ReadonlyMap in place of Map in keyValuePipe (#37311)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+38.sha-7c3c8d6
+ * @license Angular v10.1.0-next.1+40.sha-3373453
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6237,7 +6237,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.1+38.sha-7c3c8d6');
+    var VERSION = new i0.Version('10.1.0-next.1+40.sha-3373453');
 
     /**
      * @license
