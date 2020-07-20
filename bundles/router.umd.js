a0c5d258adf2511f319364038e4aec53329e0a0f
Andrew Kushnir <akushnir@google.com>
2020-07-20 17:27:40 +0000

    1a7a7360b0 fix(compiler): properly associate source spans for implicitly closed elements (#38126)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+29.sha-136acdf
+ * @license Angular v10.1.0-next.1+32.sha-1a7a736
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6237,7 +6237,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.1+29.sha-136acdf');
+    var VERSION = new i0.Version('10.1.0-next.1+32.sha-1a7a736');
 
     /**
      * @license
