c6c4b3fa46280155786c6d29d8d1b29e6c2002ef
Andrew Kushnir <akushnir@google.com>
2020-07-21 00:37:33 +0000

    eae13e42f3 docs: add routing terms to glossary (#38053)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+39.sha-4a6dcd0
+ * @license Angular v10.1.0-next.1+41.sha-eae13e4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5591,7 +5591,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+39.sha-4a6dcd0');
+const VERSION = new Version('10.1.0-next.1+41.sha-eae13e4');
 
 /**
  * @license
