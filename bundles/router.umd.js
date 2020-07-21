5a252ee9902d106d64b0dbafcf462daac78d6bc9
Andrew Kushnir <akushnir@google.com>
2020-07-21 18:03:18 +0000

    ad7046b934 feat(forms): AbstractControl to store raw validators in addition to combined validators function (#37881)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+42.sha-d72b1e4
+ * @license Angular v10.1.0-next.1+43.sha-ad7046b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6187,7 +6187,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.1+42.sha-d72b1e4');
+    var VERSION = new core.Version('10.1.0-next.1+43.sha-ad7046b');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+42.sha-d72b1e4
+ * @license Angular v10.1.0-next.1+43.sha-ad7046b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
