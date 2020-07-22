ee30417013f25eb2a27a8b6ba308ea17e0255977
Andrew Kushnir <akushnir@google.com>
2020-07-22 17:13:20 +0000

    8851ba9c9d docs: release notes for the v10.0.5 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+45.sha-7f82a2c
+ * @license Angular v10.1.0-next.1+46.sha-8851ba9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6187,7 +6187,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.1+45.sha-7f82a2c');
+    var VERSION = new core.Version('10.1.0-next.1+46.sha-8851ba9');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+45.sha-7f82a2c
+ * @license Angular v10.1.0-next.1+46.sha-8851ba9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
