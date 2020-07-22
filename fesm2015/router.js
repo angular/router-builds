e37aeca290b38ffc4827840a66cf25fd5f2ddeff
Andrew Kushnir <akushnir@google.com>
2020-07-22 17:41:15 +0000

    d88711c2fc docs: add Ivy and View Engine test scripts (#38149)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+8.sha-5a73362
+ * @license Angular v10.1.0-next.2+2.sha-d88711c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5558,7 +5558,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+8.sha-5a73362');
+const VERSION = new Version('10.1.0-next.2+2.sha-d88711c');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+8.sha-5a73362
+ * @license Angular v10.1.0-next.2+2.sha-d88711c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
