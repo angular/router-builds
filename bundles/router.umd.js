167f53527e5ad52154f0a31b2b62373c00731c56
Andrew Kushnir <akushnir@google.com>
2020-07-22 17:44:01 +0000

    d88711c2fc docs: add Ivy and View Engine test scripts (#38149)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+8.sha-5a73362
+ * @license Angular v10.1.0-next.2+2.sha-d88711c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6237,7 +6237,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.2+8.sha-5a73362');
+    var VERSION = new i0.Version('10.1.0-next.2+2.sha-d88711c');
 
     /**
      * @license
