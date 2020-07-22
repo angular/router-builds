fa64b0a2d3c93c3ec896a978bd93337c91178d6e
Andrew Kushnir <akushnir@google.com>
2020-07-22 17:32:57 +0000

    970d10c671 release: cut the v10.1.0-next.2 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+46.sha-8851ba9
+ * @license Angular v10.1.0-next.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6237,7 +6237,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.1+46.sha-8851ba9');
+    var VERSION = new i0.Version('10.1.0-next.2');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+46.sha-8851ba9
+ * @license Angular v10.1.0-next.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5591,7 +5591,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+46.sha-8851ba9');
+const VERSION = new Version('10.1.0-next.2');
 
 /**
  * @license
