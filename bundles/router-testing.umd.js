ebd1c50ee3747ae430a23cb8146f8b553ab60343
Andrew Kushnir <akushnir@google.com>
2020-07-22 17:30:20 +0000

    970d10c671 release: cut the v10.1.0-next.2 release

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+46.sha-8851ba9
+ * @license Angular v10.1.0-next.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+46.sha-8851ba9
+ * @license Angular v10.1.0-next.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6187,7 +6187,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.1+46.sha-8851ba9');
+    var VERSION = new core.Version('10.1.0-next.2');
 
     /**
      * @license
