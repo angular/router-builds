e8f1c8989d1dfcb2357555288bc6e34d391b71e9
Andrew Kushnir <akushnir@google.com>
2020-07-22 17:41:49 +0000

    5a733629b5 build(docs-infra): remove boilerplate file listings in `example-boilerplate.js` (#38173)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+1.sha-5e742d2
+ * @license Angular v10.1.0-next.2+8.sha-5a73362
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5591,7 +5591,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+1.sha-5e742d2');
+const VERSION = new Version('10.1.0-next.2+8.sha-5a73362');
 
 /**
  * @license
