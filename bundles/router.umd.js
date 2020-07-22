3d74ba47aa51bbd267d4c7fb672dd5f6d1100c81
Andrew Kushnir <akushnir@google.com>
2020-07-22 17:42:03 +0000

    6ec7297e43 build(docs-infra): remove boilerplate file listings in `example-boilerplate.js` (#38173)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5
+ * @license Angular v10.0.5+7.sha-6ec7297
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6146,7 +6146,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.5');
+    var VERSION = new core.Version('10.0.5+7.sha-6ec7297');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5
+ * @license Angular v10.0.5+7.sha-6ec7297
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
