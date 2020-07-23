f43416e7c706fe772ac293f6563f73f2a45c844c
Misko Hevery <misko@hevery.com>
2020-07-23 18:26:02 +0000

    51aa4aa1c3 docs: update dynamic-component loading guide (#36959)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+13.sha-8bb8f98
+ * @license Angular v10.1.0-next.2+14.sha-51aa4aa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+13.sha-8bb8f98
+ * @license Angular v10.1.0-next.2+14.sha-51aa4aa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5578,7 +5578,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+13.sha-8bb8f98');
+const VERSION = new Version('10.1.0-next.2+14.sha-51aa4aa');
 
 /**
  * @license
