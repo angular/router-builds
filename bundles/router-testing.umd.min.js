b444de9b8e07daac07f0fecb76b2a211bd1c1398
Misko Hevery <misko@hevery.com>
2020-07-23 03:54:12 +0000

    778ad3776a docs: create coding standards doc (#37700)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+2.sha-d88711c
+ * @license Angular v10.1.0-next.2+9.sha-778ad37
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+2.sha-d88711c
+ * @license Angular v10.1.0-next.2+9.sha-778ad37
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5591,7 +5591,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+2.sha-d88711c');
+const VERSION = new Version('10.1.0-next.2+9.sha-778ad37');
 
 /**
  * @license
