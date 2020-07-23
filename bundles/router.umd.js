26e6ce30b07fbc0b80168db6416cb196ee61650c
Misko Hevery <misko@hevery.com>
2020-07-23 18:23:57 +0000

    8bb8f98c28 docs: remove duplicate `https://` (#38199)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+12.sha-3c1866b
+ * @license Angular v10.1.0-next.2+13.sha-8bb8f98
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6257,7 +6257,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.2+12.sha-3c1866b');
+    var VERSION = new i0.Version('10.1.0-next.2+13.sha-8bb8f98');
 
     /**
      * @license
