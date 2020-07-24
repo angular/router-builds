be5b0ed2c9104e994cb66577cce052638a76b2bf
Misko Hevery <misko@hevery.com>
2020-07-24 17:14:39 +0000

    6e2db228af docs: Fix link by removing a space (#38214) PR Close #38214

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+22.sha-cce5583
+ * @license Angular v10.1.0-next.2+23.sha-6e2db22
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6207,7 +6207,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.2+22.sha-cce5583');
+    var VERSION = new core.Version('10.1.0-next.2+23.sha-6e2db22');
 
     /**
      * @license
