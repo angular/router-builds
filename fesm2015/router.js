ddf1682310f5e4ae45585652b7510da432f35df4
Misko Hevery <misko@hevery.com>
2020-07-24 22:37:51 +0000

    253337dc0a feat(zone.js): move MutationObserver/FileReader to different module (#31657)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+23.sha-6e2db22
+ * @license Angular v10.1.0-next.2+24.sha-253337d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5611,7 +5611,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+23.sha-6e2db22');
+const VERSION = new Version('10.1.0-next.2+24.sha-253337d');
 
 /**
  * @license
