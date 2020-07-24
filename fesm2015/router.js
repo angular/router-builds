35ce9a931f6e1f3df21d747f8a4641c3682e593a
Misko Hevery <misko@hevery.com>
2020-07-24 15:40:00 +0000

    b8af10902f docs: fixed that class attribute is not closed (#38219)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+15.sha-f411c9e
+ * @license Angular v10.0.5+16.sha-b8af109
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5530,7 +5530,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.5+15.sha-f411c9e');
+const VERSION = new Version('10.0.5+16.sha-b8af109');
 
 /**
  * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+15.sha-f411c9e
+ * @license Angular v10.0.5+16.sha-b8af109
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
