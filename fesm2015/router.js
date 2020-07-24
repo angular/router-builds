80e032c87ec6d155e13516a2a68fc0d25ab909e0
Misko Hevery <misko@hevery.com>
2020-07-24 23:09:43 +0000

    c4e6f585c5 fix(zone.js): patch nodejs EventEmtter.prototype.off (#37863)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+19.sha-7467fd3
+ * @license Angular v10.0.5+20.sha-c4e6f58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5530,7 +5530,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.5+19.sha-7467fd3');
+const VERSION = new Version('10.0.5+20.sha-c4e6f58');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+19.sha-7467fd3
+ * @license Angular v10.0.5+20.sha-c4e6f58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
