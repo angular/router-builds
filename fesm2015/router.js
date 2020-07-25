1d4d9bb64fd596d96e9035a44322c683a0fb885f
Misko Hevery <misko@hevery.com>
2020-07-25 01:24:05 +0000

    a123ef58b1 fix(dev-infra): Ensure conditions with groups do not fail verification (#37798)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+20.sha-c4e6f58
+ * @license Angular v10.0.5+23.sha-a123ef5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5530,7 +5530,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.5+20.sha-c4e6f58');
+const VERSION = new Version('10.0.5+23.sha-a123ef5');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+20.sha-c4e6f58
+ * @license Angular v10.0.5+23.sha-a123ef5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
