5b3b05baf587549b9a2349f77a93937d3576f00f
Misko Hevery <misko@hevery.com>
2020-07-27 17:29:29 +0000

    8e5969bb52 fix(compiler): share identical stylesheets between components in the same file (#38213)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+31.sha-4dfc3fe
+ * @license Angular v10.1.0-next.2+35.sha-8e5969b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+31.sha-4dfc3fe
+ * @license Angular v10.1.0-next.2+35.sha-8e5969b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6264,7 +6264,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.2+31.sha-4dfc3fe');
+    var VERSION = new i0.Version('10.1.0-next.2+35.sha-8e5969b');
 
     /**
      * @license
