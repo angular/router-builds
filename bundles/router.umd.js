898a566de330778aaad3aaf3b574077c6f999dd3
Misko Hevery <misko@hevery.com>
2020-07-28 22:29:22 +0000

    cdda60a430 release: cut the v10.0.6 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+51.sha-7570356
+ * @license Angular v10.0.6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6179,7 +6179,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.5+51.sha-7570356');
+    var VERSION = new core.Version('10.0.6');
 
     /**
      * @license
