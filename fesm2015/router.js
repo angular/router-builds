faf0984ca9f260ced6a32aa475d4a44ec7dae44f
Misko Hevery <misko@hevery.com>
2020-07-28 22:31:07 +0000

    28c3e2d15d docs: release notes for the v10.0.6 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+58.sha-2a45b93
+ * @license Angular v10.1.0-next.2+59.sha-28c3e2d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5591,7 +5591,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+58.sha-2a45b93');
+const VERSION = new Version('10.1.0-next.2+59.sha-28c3e2d');
 
 /**
  * @license
