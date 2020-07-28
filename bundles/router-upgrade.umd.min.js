4e4b566212435b79c18e4a3f811730adc85fe390
Misko Hevery <misko@hevery.com>
2020-07-28 22:33:36 +0000

    28c3e2d15d docs: release notes for the v10.0.6 release

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+58.sha-2a45b93
+ * @license Angular v10.1.0-next.2+59.sha-28c3e2d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+58.sha-2a45b93
+ * @license Angular v10.1.0-next.2+59.sha-28c3e2d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6269,7 +6269,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.2+58.sha-2a45b93');
+    var VERSION = new i0.Version('10.1.0-next.2+59.sha-28c3e2d');
 
     /**
      * @license
