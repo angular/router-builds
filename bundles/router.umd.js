125b2139345aea8b334f3fcef1233caae60ecb02
Misko Hevery <misko@hevery.com>
2020-07-28 23:04:20 +0000

    57575e379f release: cut the v10.1.0-next.3 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+59.sha-28c3e2d
+ * @license Angular v10.1.0-next.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6269,7 +6269,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.2+59.sha-28c3e2d');
+    var VERSION = new i0.Version('10.1.0-next.3');
 
     /**
      * @license
