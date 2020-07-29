bfae5444273b6627f4e95d2725a4fdbc209f0732
Misko Hevery <misko@hevery.com>
2020-07-29 16:58:56 +0000

    cf4da82ac3 Revert "refactor(platform-browser): specify return type of parseEventName (#38089)"

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3
+ * @license Angular v10.1.0-next.3+1.sha-cf4da82
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6269,7 +6269,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.3');
+    var VERSION = new i0.Version('10.1.0-next.3+1.sha-cf4da82');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3
+ * @license Angular v10.1.0-next.3+1.sha-cf4da82
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5623,7 +5623,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.3');
+const VERSION = new Version('10.1.0-next.3+1.sha-cf4da82');
 
 /**
  * @license
