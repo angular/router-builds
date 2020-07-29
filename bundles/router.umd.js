84d0dfa3f4b88a8d7acf2e4f84f5bdf079f94e1f
Misko Hevery <misko@hevery.com>
2020-07-29 17:58:24 +0000

    b280d54470 refactor(docs-infra): simplify/improve `CopierService` hidden textarea creation (#38244)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+13.sha-1b17722
+ * @license Angular v10.1.0-next.3+12.sha-b280d54
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6220,7 +6220,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.3+13.sha-1b17722');
+    var VERSION = new core.Version('10.1.0-next.3+12.sha-b280d54');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+13.sha-1b17722
+ * @license Angular v10.1.0-next.3+12.sha-b280d54
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
