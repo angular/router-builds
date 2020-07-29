0c869077a0b8613f192a5d8bc367853d6924f0b0
Misko Hevery <misko@hevery.com>
2020-07-29 17:54:06 +0000

    9c8bc4a239 fix(common): narrow `NgIf` context variables in template type checker (#36627)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+1.sha-cf4da82
+ * @license Angular v10.1.0-next.3+2.sha-9c8bc4a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6220,7 +6220,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.3+1.sha-cf4da82');
+    var VERSION = new core.Version('10.1.0-next.3+2.sha-9c8bc4a');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+1.sha-cf4da82
+ * @license Angular v10.1.0-next.3+2.sha-9c8bc4a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
