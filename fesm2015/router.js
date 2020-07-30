d10574d3428261e0632b14c81e2bad1adef6647c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 19:45:42 +0000

    3a525d196b Revert "fix(compiler): mark `NgModuleFactory` construction as not side effectful (#38147)" (#38303)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+18.sha-1eebb7f
+ * @license Angular v10.1.0-next.3+19.sha-3a525d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5591,7 +5591,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.3+18.sha-1eebb7f');
+const VERSION = new Version('10.1.0-next.3+19.sha-3a525d1');
 
 /**
  * @license
