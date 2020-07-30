5c8881b0689ae0b1b5251229def207cff2bc19c9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 19:48:18 +0000

    3a525d196b Revert "fix(compiler): mark `NgModuleFactory` construction as not side effectful (#38147)" (#38303)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+18.sha-1eebb7f
+ * @license Angular v10.1.0-next.3+19.sha-3a525d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6269,7 +6269,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.3+18.sha-1eebb7f');
+    var VERSION = new i0.Version('10.1.0-next.3+19.sha-3a525d1');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+18.sha-1eebb7f
+ * @license Angular v10.1.0-next.3+19.sha-3a525d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
