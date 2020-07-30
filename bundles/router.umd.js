9b7d6a826d1da6ab9b52fddf78d7e349b00c3858
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 22:43:13 +0000

    fd51e01335 fix(compiler): Metadata should not include methods on Object.prototype (#38292)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+19.sha-3a525d1
+ * @license Angular v10.1.0-next.3+20.sha-fd51e01
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6220,7 +6220,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.3+19.sha-3a525d1');
+    var VERSION = new core.Version('10.1.0-next.3+20.sha-fd51e01');
 
     /**
      * @license
