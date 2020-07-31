2466a5dccf55e3ec3f5f6b9e014c5b180ac77328
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 18:28:24 +0000

    9c5fc1693a refactor(docs-infra): update docs examples `tslint.json` to match CLI and fix failures (#38143)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.7+2.sha-7ff5ef2
+ * @license Angular v10.0.7+17.sha-9c5fc16
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6179,7 +6179,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.7+2.sha-7ff5ef2');
+    var VERSION = new core.Version('10.0.7+17.sha-9c5fc16');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.7+2.sha-7ff5ef2
+ * @license Angular v10.0.7+17.sha-9c5fc16
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
