cc83da8ec76f50723b76be6aa7d92e387c0ce7b5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 22:55:32 +0000

    ff9f4de4f1 fix(compiler): update unparsable character reference entity error messages (#38319)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+38.sha-3a46c2d
+ * @license Angular v10.1.0-next.3+39.sha-ff9f4de
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5591,7 +5591,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.3+38.sha-3a46c2d');
+const VERSION = new Version('10.1.0-next.3+39.sha-ff9f4de');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+38.sha-3a46c2d
+ * @license Angular v10.1.0-next.3+39.sha-ff9f4de
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
