25aed5112161331e3d7cb17a01c4914d7f5aafe1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 00:22:30 +0000

    e8896b9de3 fix(language-service): [ivy] do not retrieve ts.Program at startup (#38120)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+22.sha-6f6102d
+ * @license Angular v10.1.0-next.3+23.sha-e8896b9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6269,7 +6269,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.3+22.sha-6f6102d');
+    var VERSION = new i0.Version('10.1.0-next.3+23.sha-e8896b9');
 
     /**
      * @license
