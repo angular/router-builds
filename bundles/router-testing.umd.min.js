39ed2814a0444ed839b8348084714cd3fd8f9cb4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 23:00:55 +0000

    cea46786a2 fix(compiler): update unparsable character reference entity error messages (#38319)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.7+17.sha-9c5fc16
+ * @license Angular v10.0.7+18.sha-cea4678
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.7+17.sha-9c5fc16
+ * @license Angular v10.0.7+18.sha-cea4678
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6179,7 +6179,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.7+17.sha-9c5fc16');
+    var VERSION = new core.Version('10.0.7+18.sha-cea4678');
 
     /**
      * @license
