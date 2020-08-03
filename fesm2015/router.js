d5a93f62f944d28f20510efd8b9c83d5ef1fb972
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-03 20:17:24 +0000

    a91dd2e42a build: cleanup .bazelrc file to no longer set unused flags (#38124)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.7+18.sha-cea4678
+ * @license Angular v10.0.7+20.sha-a91dd2e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5543,7 +5543,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.7+18.sha-cea4678');
+const VERSION = new Version('10.0.7+20.sha-a91dd2e');
 
 /**
  * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.7+18.sha-cea4678
+ * @license Angular v10.0.7+20.sha-a91dd2e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
