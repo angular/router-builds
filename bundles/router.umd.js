1ef32cf26845121d3083797bbdb706cea9a8ade7
Andrew Kushnir <akushnir@google.com>
2020-08-05 23:39:38 +0000

    bb88c9fa3d fix(common): ensure scrollRestoration is writable (#30630)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+7.sha-1609815
+ * @license Angular v10.1.0-next.4+9.sha-bb88c9f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6271,7 +6271,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.4+7.sha-1609815');
+    var VERSION = new i0.Version('10.1.0-next.4+9.sha-bb88c9f');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+7.sha-1609815
+ * @license Angular v10.1.0-next.4+9.sha-bb88c9f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5625,7 +5625,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+7.sha-1609815');
+const VERSION = new Version('10.1.0-next.4+9.sha-bb88c9f');
 
 /**
  * @license
