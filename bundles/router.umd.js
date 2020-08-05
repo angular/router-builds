b14ec73bc602f2c48d23faa368060a1eb46e70aa
Andrew Kushnir <akushnir@google.com>
2020-08-05 18:19:14 +0000

    f0766a4474 feat(dev-infra): provide organization-wide merge-tool label configuration (#38223)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+1.sha-585ef2b
+ * @license Angular v10.1.0-next.4+4.sha-f0766a4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6220,7 +6220,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.4+1.sha-585ef2b');
+    var VERSION = new core.Version('10.1.0-next.4+4.sha-f0766a4');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+1.sha-585ef2b
+ * @license Angular v10.1.0-next.4+4.sha-f0766a4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
