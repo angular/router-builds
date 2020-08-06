cfba5c755e325a31d8f53ba661a6a4f20f1ab73a
Andrew Kushnir <akushnir@google.com>
2020-08-06 00:29:04 +0000

    0c2490368e refactor(platform-browser): specify return type of parseEventName (#38089)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+9.sha-bb88c9f
+ * @license Angular v10.1.0-next.4+10.sha-0c24903
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6222,7 +6222,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.4+9.sha-bb88c9f');
+    var VERSION = new core.Version('10.1.0-next.4+10.sha-0c24903');
 
     /**
      * @license
