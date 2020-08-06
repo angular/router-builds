90e92497ddee2e7540d7cd5661b5faa811ea9274
Andrew Kushnir <akushnir@google.com>
2020-08-06 01:55:38 +0000

    d7c043ba35 docs: add a page with the Angular roadmap (#38358)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+10.sha-0c24903
+ * @license Angular v10.1.0-next.4+11.sha-d7c043b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6222,7 +6222,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.4+10.sha-0c24903');
+    var VERSION = new core.Version('10.1.0-next.4+11.sha-d7c043b');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+10.sha-0c24903
+ * @license Angular v10.1.0-next.4+11.sha-d7c043b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
