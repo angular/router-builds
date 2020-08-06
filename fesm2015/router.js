3cf7ad8f2a5558bf557381cc6dc247bd65d548b3
Andrew Kushnir <akushnir@google.com>
2020-08-06 22:47:58 +0000

    26be5b4994 refactor(core): extract `icuSwitchCase`, `icuUpdateCase`, `removeNestedIcu` (#38154)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+17.sha-18cd1a9
+ * @license Angular v10.1.0-next.4+19.sha-26be5b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5625,7 +5625,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+17.sha-18cd1a9');
+const VERSION = new Version('10.1.0-next.4+19.sha-26be5b4');
 
 /**
  * @license
