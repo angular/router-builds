e9fa44c97a150c3955f12af34967204c7aae739d
Andrew Kushnir <akushnir@google.com>
2020-08-06 16:23:08 +0000

    dca4443a8e fix(compiler-cli): mark eager `NgModuleFactory` construction as not side effectful (#38320)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+11.sha-d7c043b
+ * @license Angular v10.1.0-next.4+15.sha-dca4443
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5593,7 +5593,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+11.sha-d7c043b');
+const VERSION = new Version('10.1.0-next.4+15.sha-dca4443');
 
 /**
  * @license
