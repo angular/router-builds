2c739be81caea70f8a657190b6b19d52d9b647c4
Andrew Kushnir <akushnir@google.com>
2020-08-07 22:25:56 +0000

    445ac15a78 docs: fix purpose description of "builders.json" (#36830) PR Close #36830

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+25.sha-856db56
+ * @license Angular v10.1.0-next.4+26.sha-445ac15
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5593,7 +5593,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+25.sha-856db56');
+const VERSION = new Version('10.1.0-next.4+26.sha-445ac15');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+25.sha-856db56
+ * @license Angular v10.1.0-next.4+26.sha-445ac15
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
