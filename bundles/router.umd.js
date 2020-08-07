5e0b70db8e27636b443414c9075c3d22ccbc4987
Andrew Kushnir <akushnir@google.com>
2020-08-07 19:06:54 +0000

    856db56cca refactor(forms): get rid of duplicate functions (#38371)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+24.sha-354e66e
+ * @license Angular v10.1.0-next.4+25.sha-856db56
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6271,7 +6271,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.4+24.sha-354e66e');
+    var VERSION = new i0.Version('10.1.0-next.4+25.sha-856db56');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+24.sha-354e66e
+ * @license Angular v10.1.0-next.4+25.sha-856db56
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5625,7 +5625,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+24.sha-354e66e');
+const VERSION = new Version('10.1.0-next.4+25.sha-856db56');
 
 /**
  * @license
