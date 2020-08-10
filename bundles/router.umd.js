517e4d7e5e6e7807fbcd6059fb62a1b6a5c31eb0
Andrew Kushnir <akushnir@google.com>
2020-08-10 22:55:58 +0000

    5dc8d287aa fix(core): queries not matching string injection tokens (#38321)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+33.sha-6da9e58
+ * @license Angular v10.1.0-next.4+34.sha-5dc8d28
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6271,7 +6271,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.4+33.sha-6da9e58');
+    var VERSION = new i0.Version('10.1.0-next.4+34.sha-5dc8d28');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+33.sha-6da9e58
+ * @license Angular v10.1.0-next.4+34.sha-5dc8d28
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5625,7 +5625,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+33.sha-6da9e58');
+const VERSION = new Version('10.1.0-next.4+34.sha-5dc8d28');
 
 /**
  * @license
