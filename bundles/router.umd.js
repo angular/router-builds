a8f1dafb3cc7e43bf1dd47ae4fa321f23722f7e3
Andrew Kushnir <akushnir@google.com>
2020-08-10 17:20:04 +0000

    6ff28ac944 docs: Remove redundant sentence from Router (#38398) PR Close #38398

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+26.sha-445ac15
+ * @license Angular v10.1.0-next.4+28.sha-6ff28ac
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6271,7 +6271,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.4+26.sha-445ac15');
+    var VERSION = new i0.Version('10.1.0-next.4+28.sha-6ff28ac');
 
     /**
      * @license
