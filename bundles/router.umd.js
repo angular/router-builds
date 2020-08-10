a0d94fb34572cdb2dd412454ebcf15a03cf0a8e6
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:07:03 +0000

    6d8c73a4d6 fix(core): Store the currently selected ICU in `LView` (#38345)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+27.sha-0de93fd
+ * @license Angular v10.1.0-next.4+29.sha-6d8c73a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6222,7 +6222,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.4+27.sha-0de93fd');
+    var VERSION = new core.Version('10.1.0-next.4+29.sha-6d8c73a');
 
     /**
      * @license
