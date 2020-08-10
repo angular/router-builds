ec21efefb7a14bcc6045ae7cfc2e5a24f240cf24
Andrew Kushnir <akushnir@google.com>
2020-08-10 17:26:44 +0000

    0de93fd402 docs: update team contributors page (#38384)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+28.sha-6ff28ac
+ * @license Angular v10.1.0-next.4+27.sha-0de93fd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6271,7 +6271,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.4+28.sha-6ff28ac');
+    var VERSION = new i0.Version('10.1.0-next.4+27.sha-0de93fd');
 
     /**
      * @license
