d064638ec55a875d3a63b382ecec3b81413d8aad
Andrew Kushnir <akushnir@google.com>
2020-08-11 20:28:03 +0000

    ca2b4bcd4b fix(compiler-cli): avoid creating value expressions for symbols from type-only imports (#38415)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+36.sha-f6cfc92
+ * @license Angular v10.0.8+37.sha-ca2b4bc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+36.sha-f6cfc92
+ * @license Angular v10.0.8+37.sha-ca2b4bc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6178,7 +6178,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.8+36.sha-f6cfc92');
+    var VERSION = new core.Version('10.0.8+37.sha-ca2b4bc');
 
     /**
      * @license
