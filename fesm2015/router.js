8479d135ae6d4467548c9c310c00a87db6911a96
Andrew Kushnir <akushnir@google.com>
2020-08-11 18:24:43 +0000

    e2e5f83869 ci: update payload size limits for Closure tests (#38411)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+36.sha-df76a20
+ * @license Angular v10.1.0-next.4+43.sha-e2e5f83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+36.sha-df76a20');
+const VERSION = new Version('10.1.0-next.4+43.sha-e2e5f83');
 
 /**
  * @license
