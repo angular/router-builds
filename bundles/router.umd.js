f204de70a10504fd2d4633130fa89e626ec13bd5
Andrew Kushnir <akushnir@google.com>
2020-08-11 23:57:41 +0000

    a2e069fdda build: run formatting automatically on pre-commit hook (#38402)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+43.sha-e2e5f83
+ * @license Angular v10.1.0-next.4+46.sha-a2e069f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6279,7 +6279,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.4+43.sha-e2e5f83');
+    var VERSION = new i0.Version('10.1.0-next.4+46.sha-a2e069f');
 
     /**
      * @license
