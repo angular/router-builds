1f20250e5c22ae2e3bd06f905c8af3831f6798c1
Andrew Kushnir <akushnir@google.com>
2020-08-11 00:52:22 +0000

    3d156162af fix(dev-infra): update i18n-related file locations in PullApprove config (#38403)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+34.sha-5dc8d28
+ * @license Angular v10.1.0-next.4+35.sha-3d15616
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6271,7 +6271,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.4+34.sha-5dc8d28');
+    var VERSION = new i0.Version('10.1.0-next.4+35.sha-3d15616');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+34.sha-5dc8d28
+ * @license Angular v10.1.0-next.4+35.sha-3d15616
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5625,7 +5625,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+34.sha-5dc8d28');
+const VERSION = new Version('10.1.0-next.4+35.sha-3d15616');
 
 /**
  * @license
