9ede7457233fb3deb9c19e7d78f25691c3677d4c
Andrew Scott <atscott01@gmail.com>
2020-08-12 18:59:49 +0000

    5f2e475abf docs: remove unused Input decorator (#38306)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+2.sha-aa3520e
+ * @license Angular v10.1.0-next.5+3.sha-5f2e475
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6230,7 +6230,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.5+2.sha-aa3520e');
+    var VERSION = new core.Version('10.1.0-next.5+3.sha-5f2e475');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+2.sha-aa3520e
+ * @license Angular v10.1.0-next.5+3.sha-5f2e475
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
