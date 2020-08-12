34ab4efb2264756f2f2a3811b6c2ffe215fdc691
Andrew Kushnir <akushnir@google.com>
2020-08-12 17:34:23 +0000

    d6d7caa2a8 release: cut the v10.1.0-next.5 release

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+48.sha-dcf7baf
+ * @license Angular v10.1.0-next.5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+48.sha-dcf7baf
+ * @license Angular v10.1.0-next.5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+48.sha-dcf7baf');
+const VERSION = new Version('10.1.0-next.5');
 
 /**
  * @license
