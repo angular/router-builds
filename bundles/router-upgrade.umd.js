754363a5399979d297f6491860b922085bc21ff1
Andrew Scott <atscott01@gmail.com>
2020-08-12 23:33:48 +0000

    afd4417a7b refactor(dev-infra): extract the commit message parsing function into its own file (#38429)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+3.sha-2f53bbb
+ * @license Angular v10.0.9+4.sha-afd4417
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+3.sha-2f53bbb
+ * @license Angular v10.0.9+4.sha-afd4417
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5542,7 +5542,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.9+3.sha-2f53bbb');
+const VERSION = new Version('10.0.9+4.sha-afd4417');
 
 /**
  * @license
