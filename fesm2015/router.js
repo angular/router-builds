822e507cebd46569ced40b5f0380facaa371b84f
Andrew Kushnir <akushnir@google.com>
2020-08-12 15:49:41 +0000

    4d17418569 docs: delete one superfluous sentence (#38339)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+44.sha-a6292fa
+ * @license Angular v10.1.0-next.4+47.sha-4d17418
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5633,7 +5633,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+44.sha-a6292fa');
+const VERSION = new Version('10.1.0-next.4+47.sha-4d17418');
 
 /**
  * @license
