dcd55482b7b24f28042f30114e6509ef0db83a57
Andrew Scott <atscott01@gmail.com>
2020-08-13 20:19:40 +0000

    175c79d1d8 test(docs-infra): remove deprecated `ReflectiveInjector` (#38408)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+12.sha-945751e
+ * @license Angular v10.1.0-next.5+13.sha-175c79d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6290,7 +6290,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.5+12.sha-945751e');
+    var VERSION = new i0.Version('10.1.0-next.5+13.sha-175c79d');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+12.sha-945751e
+ * @license Angular v10.1.0-next.5+13.sha-175c79d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5644,7 +5644,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+12.sha-945751e');
+const VERSION = new Version('10.1.0-next.5+13.sha-175c79d');
 
 /**
  * @license
