81060b857bae76c7acd83223e3376a19a4557eb6
Andrew Scott <atscott01@gmail.com>
2020-08-13 20:58:20 +0000

    1388c1761f perf(compiler-cli): don't emit template guards when child scope is empty (#38418)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+13.sha-175c79d
+ * @license Angular v10.1.0-next.5+16.sha-1388c17
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6290,7 +6290,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.5+13.sha-175c79d');
+    var VERSION = new i0.Version('10.1.0-next.5+16.sha-1388c17');
 
     /**
      * @license
