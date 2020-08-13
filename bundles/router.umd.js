16f2648eeff7f037c0e9f78d1166740174c48c86
Andrew Scott <atscott01@gmail.com>
2020-08-13 16:11:27 +0000

    f4ced74e3a feat(dev-infra): save invalid commit message attempts to be restored on next commit attempt (#38304)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+4.sha-8366eff
+ * @license Angular v10.1.0-next.5+5.sha-f4ced74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6279,7 +6279,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.5+4.sha-8366eff');
+    var VERSION = new i0.Version('10.1.0-next.5+5.sha-f4ced74');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+4.sha-8366eff
+ * @license Angular v10.1.0-next.5+5.sha-f4ced74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
