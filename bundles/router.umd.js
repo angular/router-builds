474c7abd359bbf45fbbb6abb6eb40c7d6e1f6bc6
Andrew Scott <atscott01@gmail.com>
2020-08-13 18:01:39 +0000

    a80f654af9 fix(core): error if CSS custom property in host binding has number in name (#38432)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+9.sha-aa847cb
+ * @license Angular v10.1.0-next.5+10.sha-a80f654
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6279,7 +6279,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.5+9.sha-aa847cb');
+    var VERSION = new i0.Version('10.1.0-next.5+10.sha-a80f654');
 
     /**
      * @license
