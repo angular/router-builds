e3f9ab4d4b9717b7310e0c4e0db901fe9ce3429d
Andrew Scott <atscott01@gmail.com>
2020-08-13 21:01:44 +0000

    b071495f92 fix(core): fix multiple nested views removal from ViewContainerRef (#38317)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+17.sha-d5f819e
+ * @license Angular v10.1.0-next.5+18.sha-b071495
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6241,7 +6241,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.5+17.sha-d5f819e');
+    var VERSION = new core.Version('10.1.0-next.5+18.sha-b071495');
 
     /**
      * @license
