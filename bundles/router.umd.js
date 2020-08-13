316667ce4cef9f5247fb1c18dc21ea3da60122c2
Andrew Scott <atscott01@gmail.com>
2020-08-13 21:02:49 +0000

    d5f819ebc1 style(compiler-cli): remove unused constant (#38441)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+16.sha-1388c17
+ * @license Angular v10.1.0-next.5+17.sha-d5f819e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6290,7 +6290,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.5+16.sha-1388c17');
+    var VERSION = new i0.Version('10.1.0-next.5+17.sha-d5f819e');
 
     /**
      * @license
