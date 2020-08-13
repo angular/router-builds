993c78ce351ae15464cf447207bb34076e9105ed
Andrew Scott <atscott01@gmail.com>
2020-08-13 19:10:16 +0000

    945751e2e8 ci: disable closure size tracking test (#38449)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+11.sha-b769771
+ * @license Angular v10.1.0-next.5+12.sha-945751e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6241,7 +6241,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.5+11.sha-b769771');
+    var VERSION = new core.Version('10.1.0-next.5+12.sha-945751e');
 
     /**
      * @license
