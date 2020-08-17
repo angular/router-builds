20e99c3bdbce59d54e5f96335a97bdce26324169
atscott <atscott01@gmail.com>
2020-08-17 22:59:49 +0000

    e472f5f688 refactor(ngcc): update yargs and typings for yargs (#38470)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6
+ * @license Angular v10.1.0-next.6+3.sha-e472f5f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6344,7 +6344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.6');
+    var VERSION = new i0.Version('10.1.0-next.6+3.sha-e472f5f');
 
     /**
      * @license
