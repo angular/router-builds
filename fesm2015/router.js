229d03896a337091215449c8e09347dba1728d53
Andrew Scott <atscott@google.com>
2020-08-17 18:12:04 +0000

    81c3e809aa fix(localize): render ICU placeholders in extracted translation files (#38484)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+22.sha-cb05c01
+ * @license Angular v10.1.0-next.5+24.sha-81c3e80
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5674,7 +5674,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+22.sha-cb05c01');
+const VERSION = new Version('10.1.0-next.5+24.sha-81c3e80');
 
 /**
  * @license
