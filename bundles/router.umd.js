4682c327a19cdae41d5f2cccc540015a75851f73
atscott <atscott01@gmail.com>
2020-08-17 20:42:17 +0000

    3e80f0e526 release: cut the v10.0.10 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+21.sha-f3dd6c2
+ * @license Angular v10.0.10
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6208,7 +6208,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.9+21.sha-f3dd6c2');
+    var VERSION = new core.Version('10.0.10');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+21.sha-f3dd6c2
+ * @license Angular v10.0.10
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
