a91f27275e056d4d748b467e231ac8f20068297e
atscott <atscott01@gmail.com>
2020-08-17 20:57:15 +0000

    64cf087ae5 release: cut the v10.1.0-next.6 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+29.sha-fec9dcb
+ * @license Angular v10.1.0-next.6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6344,7 +6344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.5+29.sha-fec9dcb');
+    var VERSION = new i0.Version('10.1.0-next.6');
 
     /**
      * @license
