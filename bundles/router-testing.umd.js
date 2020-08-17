7f1b1ace4c32c350fe350b0c581575baf86988ac
Andrew Scott <atscott@google.com>
2020-08-17 17:39:13 +0000

    cb05c0102f fix(core): move generated i18n statements to the `consts` field of ComponentDef (#38404)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+20.sha-71079ce
+ * @license Angular v10.1.0-next.5+22.sha-cb05c01
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+20.sha-71079ce
+ * @license Angular v10.1.0-next.5+22.sha-cb05c01
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6276,7 +6276,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.5+20.sha-71079ce');
+    var VERSION = new core.Version('10.1.0-next.5+22.sha-cb05c01');
 
     /**
      * @license
