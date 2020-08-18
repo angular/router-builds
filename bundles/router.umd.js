e5ef16559b5d7a9a6c9d3a8bb65a8f5a1563809e
Andrew Scott <atscott@google.com>
2020-08-18 19:19:46 +0000

    aaa1d8e2fe release: cut the zone.js-0.11.0 release (#38473)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+6.sha-dbfb50e
+ * @license Angular v10.1.0-next.6+7.sha-aaa1d8e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6347,7 +6347,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.6+6.sha-dbfb50e');
+    var VERSION = new i0.Version('10.1.0-next.6+7.sha-aaa1d8e');
 
     /**
      * @license
