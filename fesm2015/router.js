c1bca928f36e7b76996e8f0d0eac6f5bc85cbb0f
Andrew Scott <atscott@google.com>
2020-08-18 17:33:21 +0000

    723a9ff095 docs(common): Wrong parameter description on TrackBy (#38495)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+3.sha-e472f5f
+ * @license Angular v10.1.0-next.6+4.sha-723a9ff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5668,7 +5668,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.6+3.sha-e472f5f');
+const VERSION = new Version('10.1.0-next.6+4.sha-723a9ff');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+3.sha-e472f5f
+ * @license Angular v10.1.0-next.6+4.sha-723a9ff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
