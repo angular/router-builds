037bdd98aef7b672da0e8bb640ab9e56d456eede
Misko Hevery <misko@hevery.com>
2020-08-19 21:43:51 +0000

    552853648c fix(core): remove closing body tag from inert DOM builder (#38454)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11
+ * @license Angular v10.0.11+5.sha-5528536
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5582,7 +5582,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.11');
+const VERSION = new Version('10.0.11+5.sha-5528536');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11
+ * @license Angular v10.0.11+5.sha-5528536
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
