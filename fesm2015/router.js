29ff0497ab78c803f3d3d7d58024fc88ff17a456
Andrew Scott <atscott@google.com>
2020-08-19 15:50:38 +0000

    0570c240e4 docs: Fix typo in the inputs and outputs guide (#38524)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.10+8.sha-398118f
+ * @license Angular v10.0.10+9.sha-0570c24
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5582,7 +5582,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.10+8.sha-398118f');
+const VERSION = new Version('10.0.10+9.sha-0570c24');
 
 /**
  * @license
