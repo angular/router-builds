39cc7b6f703a9e6e635cf3c25c33153bc4e07bde
Andrew Scott <atscott@google.com>
2020-08-19 16:30:23 +0000

    6b662d10c1 fix(zone.js): zone.js package.json should not include files/directories field (#38528)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+10.sha-55fd725
+ * @license Angular v10.1.0-next.6+11.sha-6b662d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5671,7 +5671,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.6+10.sha-55fd725');
+const VERSION = new Version('10.1.0-next.6+11.sha-6b662d1');
 
 /**
  * @license
