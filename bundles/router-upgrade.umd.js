a1d02a014063983e7981d281d99091e63c7298f3
Andrew Scott <atscott@google.com>
2020-08-19 15:51:25 +0000

    55fd725e74 docs: Fix typo in the inputs and outputs guide (#38524)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+9.sha-f77fd5e
+ * @license Angular v10.1.0-next.6+10.sha-55fd725
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+9.sha-f77fd5e
+ * @license Angular v10.1.0-next.6+10.sha-55fd725
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6298,7 +6298,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.6+9.sha-f77fd5e');
+    var VERSION = new core.Version('10.1.0-next.6+10.sha-55fd725');
 
     /**
      * @license
