747b997d359554922434766b840a1abb12b20775
Misko Hevery <misko@hevery.com>
2020-08-19 21:43:08 +0000

    68a9a01a64 fix(localize): parse all parts of a translation with nested HTML (#38452)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+4.sha-0b54c0c
+ * @license Angular v10.1.0-next.7+6.sha-68a9a01
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6362,7 +6362,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.7+4.sha-0b54c0c');
+    var VERSION = new i0.Version('10.1.0-next.7+6.sha-68a9a01');
 
     /**
      * @license
