770ae403511ac298ff9a4afd6c43912e68dcb4a0
Misko Hevery <misko@hevery.com>
2020-08-20 17:05:15 +0000

    8017ca4db3 fix(docs-infra): fix vertical alignment of external link icons (#38410)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+10.sha-6442875
+ * @license Angular v10.1.0-next.7+13.sha-8017ca4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6347,7 +6347,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.7+10.sha-6442875');
+    var VERSION = new i0.Version('10.1.0-next.7+13.sha-8017ca4');
 
     /**
      * @license
