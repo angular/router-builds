091dbb9cd82f9920f070d50b6bae3514249d953a
Misko Hevery <misko@hevery.com>
2020-08-20 17:03:27 +0000

    22f1ac3e37 docs: udpate the details (#37967)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+13.sha-8017ca4
+ * @license Angular v10.1.0-next.7+12.sha-22f1ac3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+13.sha-8017ca4
+ * @license Angular v10.1.0-next.7+12.sha-22f1ac3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6298,7 +6298,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.7+13.sha-8017ca4');
+    var VERSION = new core.Version('10.1.0-next.7+12.sha-22f1ac3');
 
     /**
      * @license
