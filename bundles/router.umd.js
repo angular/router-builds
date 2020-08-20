c0e7e39031248541b2136beef88fd77c6e743c09
Misko Hevery <misko@hevery.com>
2020-08-20 16:58:52 +0000

    6442875c99 docs(core): Fix typo in JSDoc for AbstractType<T> (#38541)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+11.sha-4ee5e73
+ * @license Angular v10.1.0-next.7+10.sha-6442875
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6347,7 +6347,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.7+11.sha-4ee5e73');
+    var VERSION = new i0.Version('10.1.0-next.7+10.sha-6442875');
 
     /**
      * @license
