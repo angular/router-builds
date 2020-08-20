8e60ad7205645cdbb106778cf9be789fff8fe737
Misko Hevery <misko@hevery.com>
2020-08-20 16:56:14 +0000

    6442875c99 docs(core): Fix typo in JSDoc for AbstractType<T> (#38541)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+11.sha-4ee5e73
+ * @license Angular v10.1.0-next.7+10.sha-6442875
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5671,7 +5671,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.7+11.sha-4ee5e73');
+const VERSION = new Version('10.1.0-next.7+10.sha-6442875');
 
 /**
  * @license
