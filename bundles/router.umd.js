c84a0f01b27b8b0d61af2de40be84dcc98b4c91b
Misko Hevery <misko@hevery.com>
2020-08-20 20:24:17 +0000

    956b25a100 docs: apply code styling in template reference variables guide (#38522)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+12.sha-22f1ac3
+ * @license Angular v10.1.0-next.7+14.sha-956b25a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6298,7 +6298,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.7+12.sha-22f1ac3');
+    var VERSION = new core.Version('10.1.0-next.7+14.sha-956b25a');
 
     /**
      * @license
