3d87a5bfb80748615ca393161352b02034301b58
Misko Hevery <misko@hevery.com>
2020-08-21 19:49:12 +0000

    814b43647d fix(compiler-cli): adding references to const enums in runtime code (#38542)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11+11.sha-7f2a3e4
+ * @license Angular v10.0.11+13.sha-814b436
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11+11.sha-7f2a3e4
+ * @license Angular v10.0.11+13.sha-814b436
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6211,7 +6211,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.11+11.sha-7f2a3e4');
+    var VERSION = new core.Version('10.0.11+13.sha-814b436');
 
     /**
      * @license
