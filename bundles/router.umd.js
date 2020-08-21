7fcce3e19c6db0cff482650c77d16abd6c728893
Misko Hevery <misko@hevery.com>
2020-08-21 19:54:31 +0000

    874792dc43 feat(compiler): support unary operators for more accurate type checking (#37918)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+17.sha-e7da404
+ * @license Angular v10.1.0-next.7+18.sha-874792d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6347,7 +6347,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.7+17.sha-e7da404');
+    var VERSION = new i0.Version('10.1.0-next.7+18.sha-874792d');
 
     /**
      * @license
