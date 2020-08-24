59239555a0746a12dcb49b5b7446f3d4f8b5bd4c
Misko Hevery <misko@hevery.com>
2020-08-24 16:56:07 +0000

    b48cc6ead5 feat(language-service): introduce hybrid visitor to locate AST node (#38540)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+22.sha-201a546
+ * @license Angular v10.1.0-next.7+19.sha-b48cc6e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6347,7 +6347,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.7+22.sha-201a546');
+    var VERSION = new i0.Version('10.1.0-next.7+19.sha-b48cc6e');
 
     /**
      * @license
