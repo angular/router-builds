6bd44f7be865014b5cce8dd1277448ac3ece003c
Misko Hevery <misko@hevery.com>
2020-08-25 17:18:15 +0000

    d06636848e docs: add correct path for karma.conf.js file (#38571)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.12
+ * @license Angular v10.0.12+3.sha-d066368
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5582,7 +5582,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.12');
+const VERSION = new Version('10.0.12+3.sha-d066368');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.12
+ * @license Angular v10.0.12+3.sha-d066368
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
