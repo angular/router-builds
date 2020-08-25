1b8c6772ae7e46efe62dec88ad484c4651d90aa5
Misko Hevery <misko@hevery.com>
2020-08-25 22:38:57 +0000

    841dfa68f9 docs: typo fixes in the component testing scenarios guide (#38574)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.8+10.sha-14e90be
+ * @license Angular v10.1.0-next.8+12.sha-841dfa6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5671,7 +5671,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.8+10.sha-14e90be');
+const VERSION = new Version('10.1.0-next.8+12.sha-841dfa6');
 
 /**
  * @license
