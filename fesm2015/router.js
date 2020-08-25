9fb73dee2e87f94f49448e9dc8b1793a7c74b0a4
Misko Hevery <misko@hevery.com>
2020-08-25 22:40:17 +0000

    f0af387f6c fix(localize): ensure required XLIFF parameters are serialized (#38575)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.8+12.sha-841dfa6
+ * @license Angular v10.1.0-next.8+11.sha-f0af387
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5671,7 +5671,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.8+12.sha-841dfa6');
+const VERSION = new Version('10.1.0-next.8+11.sha-f0af387');
 
 /**
  * @license
