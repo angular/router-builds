68b1136fe469fb36cbe44752ffda59889e6145ed
Misko Hevery <misko@hevery.com>
2020-08-25 17:21:13 +0000

    9cbde86534 docs: fix typo in the testing component basics guide (#38573)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+29.sha-18e474f
+ * @license Angular v10.1.0-next.7+30.sha-9cbde86
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6347,7 +6347,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.7+29.sha-18e474f');
+    var VERSION = new i0.Version('10.1.0-next.7+30.sha-9cbde86');
 
     /**
      * @license
