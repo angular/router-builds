dc2723590bcecc1784cac9070de72e66fd1724cc
Misko Hevery <misko@hevery.com>
2020-08-25 22:37:32 +0000

    14e90bef58 fix(localize): render text of extracted placeholders (#38536)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+31.sha-94a3e0e
+ * @license Angular v10.1.0-next.8+10.sha-14e90be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6347,7 +6347,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.7+31.sha-94a3e0e');
+    var VERSION = new i0.Version('10.1.0-next.8+10.sha-14e90be');
 
     /**
      * @license
