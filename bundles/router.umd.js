e111aee89a972717dbf8c362eb0d11950ac6eb06
Joey Perrott <josephperrott@gmail.com>
2020-08-26 20:20:18 +0000

    618cb32407 docs: add Sam Vloeberghs to GDE list (#37970)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+1.sha-0681a20
+ * @license Angular v10.1.0-rc.0+3.sha-618cb32
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6347,7 +6347,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-rc.0+1.sha-0681a20');
+    var VERSION = new i0.Version('10.1.0-rc.0+3.sha-618cb32');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+1.sha-0681a20
+ * @license Angular v10.1.0-rc.0+3.sha-618cb32
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
