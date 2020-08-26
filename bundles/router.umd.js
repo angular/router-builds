f07e1fd602da19f95213cad2923799c32290e020
Joey Perrott <josephperrott@gmail.com>
2020-08-26 20:21:20 +0000

    4aee0087ea docs: remove outdated CircleCI link from `DEVELOPER.md` (#38554)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+3.sha-618cb32
+ * @license Angular v10.1.0-rc.0+2.sha-4aee008
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6347,7 +6347,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-rc.0+3.sha-618cb32');
+    var VERSION = new i0.Version('10.1.0-rc.0+2.sha-4aee008');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+3.sha-618cb32
+ * @license Angular v10.1.0-rc.0+2.sha-4aee008
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
