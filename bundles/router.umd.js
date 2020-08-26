651baa56e4062f671ad82b9606ce6e3470e44fdb
Misko Hevery <misko@hevery.com>
2020-08-26 19:16:04 +0000

    7d8dce11c0 release: cut the v10.0.14 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.12+4.sha-9168919
+ * @license Angular v10.0.14
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6211,7 +6211,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.12+4.sha-9168919');
+    var VERSION = new core.Version('10.0.14');
 
     /**
      * @license
