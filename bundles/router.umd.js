e0dc372efdd738520b3e84155918be8b5d0c7747
Joey Perrott <josephperrott@gmail.com>
2020-08-31 19:06:26 +0000

    0fc2bef0cd docs(service-worker): add links to service worker communication guide (#36847)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+24.sha-0a7a5e3
+ * @license Angular v10.1.0-rc.0+29.sha-0fc2bef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6298,7 +6298,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-rc.0+24.sha-0a7a5e3');
+    var VERSION = new core.Version('10.1.0-rc.0+29.sha-0fc2bef');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+24.sha-0a7a5e3
+ * @license Angular v10.1.0-rc.0+29.sha-0fc2bef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
