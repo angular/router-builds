3a8185fba5cc2a8cb3d350f48f8183b847e2f663
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:10:28 +0000

    c1bc070b40 refactor(dev-infra): remove `style` type from commit style guide (#38639)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+20.sha-52c7a4b
+ * @license Angular v10.1.0-rc.0+16.sha-c1bc070
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6298,7 +6298,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-rc.0+20.sha-52c7a4b');
+    var VERSION = new core.Version('10.1.0-rc.0+16.sha-c1bc070');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+20.sha-52c7a4b
+ * @license Angular v10.1.0-rc.0+16.sha-c1bc070
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
