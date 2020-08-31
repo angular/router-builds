2b796d0ae79ae36016bc8050f1c8d29790e244ed
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:08:58 +0000

    5d5caf21b8 docs: fix broken markdown in start/start-data (#38644) PR Close #38644

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+15.sha-930eeaf
+ * @license Angular v10.1.0-rc.0+17.sha-5d5caf2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6347,7 +6347,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-rc.0+15.sha-930eeaf');
+    var VERSION = new i0.Version('10.1.0-rc.0+17.sha-5d5caf2');
 
     /**
      * @license
