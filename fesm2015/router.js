49f8962a7a51574e8b778249e1bcf24b0f45acee
Joey Perrott <josephperrott@gmail.com>
2020-08-31 17:53:56 +0000

    0a7a5e3aff docs: Remove confusion between do/avoid templates (#38647) PR Close #38647

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+22.sha-ebc0e46
+ * @license Angular v10.1.0-rc.0+24.sha-0a7a5e3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5703,7 +5703,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-rc.0+22.sha-ebc0e46');
+const VERSION = new Version('10.1.0-rc.0+24.sha-0a7a5e3');
 
 /**
  * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+22.sha-ebc0e46
+ * @license Angular v10.1.0-rc.0+24.sha-0a7a5e3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
