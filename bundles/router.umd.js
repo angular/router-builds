a7ed7edaad6bdd33612824bd88fb69cff251393d
Joey Perrott <josephperrott@gmail.com>
2020-08-31 19:58:02 +0000

    5e4aeaa348 refactor(dev-infra): use a mixin to require a `github-token` for an ng-dev command (#38630)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+15.sha-cbbf8b5
+ * @license Angular v10.1.0-rc.0+16.sha-5e4aeaa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6298,7 +6298,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-rc.0+15.sha-cbbf8b5');
+    var VERSION = new core.Version('10.1.0-rc.0+16.sha-5e4aeaa');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+15.sha-cbbf8b5
+ * @license Angular v10.1.0-rc.0+16.sha-5e4aeaa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
