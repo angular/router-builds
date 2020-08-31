b7f951045acf46df58d4dc111abfb69cd92b355b
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:10:44 +0000

    6b1a505566 feat(dev-infra): write outputs of command runs to ng-dev log file (#38599)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+6.sha-dd82f2f
+ * @license Angular v10.1.0-rc.0+12.sha-6b1a505
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6298,7 +6298,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-rc.0+6.sha-dd82f2f');
+    var VERSION = new core.Version('10.1.0-rc.0+12.sha-6b1a505');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+6.sha-dd82f2f
+ * @license Angular v10.1.0-rc.0+12.sha-6b1a505
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
