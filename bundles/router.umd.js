927fa84dbf91aaa13d931bba8c0b1d4cab9d0327
GitHub <noreply@github.com>
2020-09-01 18:01:07 +0000

    03dbcc7a56 build: update the package.json to 11.0.0-next.0 (#38667)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+33.sha-c142b07
+ * @license Angular v10.1.0-rc.0+34.sha-03dbcc7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6347,7 +6347,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-rc.0+33.sha-c142b07');
+    var VERSION = new i0.Version('10.1.0-rc.0+34.sha-03dbcc7');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+33.sha-c142b07
+ * @license Angular v10.1.0-rc.0+34.sha-03dbcc7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
