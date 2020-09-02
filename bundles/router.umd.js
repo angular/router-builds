ae314a9962e774fa5837f467b3c88ef4524c7a07
Andrew Scott <atscott@google.com>
2020-09-02 22:11:33 +0000

    86e7cd8117 docs: correct link to chrome status in component style guide (#38682)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0
+ * @license Angular v11.0.0-next.0+1.sha-86e7cd8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6298,7 +6298,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.0');
+    var VERSION = new core.Version('11.0.0-next.0+1.sha-86e7cd8');
 
     /**
      * @license
