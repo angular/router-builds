a2d3a5bc594c8fb91dcada187c0364985ef381e3
Joey Perrott <josephperrott@gmail.com>
2020-09-02 20:22:06 +0000

    c01bd0fe8e release: cut the v10.1.0 release

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+19.sha-5588324
+ * @license Angular v10.1.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+19.sha-5588324
+ * @license Angular v10.1.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5671,7 +5671,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-rc.0+19.sha-5588324');
+const VERSION = new Version('10.1.0');
 
 /**
  * @license
