f44b0d25fee0f184b76a03c1d857a554aeb280c2
Andrew Scott <atscott@google.com>
2020-09-02 22:14:38 +0000

    1d8c5d88cd refactor(compiler): `element.sourceSpan` should span the `outerHTML` (#38581)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+1.sha-86e7cd8
+ * @license Angular v11.0.0-next.0+5.sha-1d8c5d8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6347,7 +6347,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.0+1.sha-86e7cd8');
+    var VERSION = new i0.Version('11.0.0-next.0+5.sha-1d8c5d8');
 
     /**
      * @license
