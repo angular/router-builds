bc24f09322e2ab71c7159308fb4cf5a4ba3c9323
atscott <atscott01@gmail.com>
2020-09-03 17:06:48 +0000

    29c89c9297 docs(dev-infra): fix typo in comment (arguements --> arguments) (#38653)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+3.sha-6aac499
+ * @license Angular v10.1.0+6.sha-29c89c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+3.sha-6aac499
+ * @license Angular v10.1.0+6.sha-29c89c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6298,7 +6298,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0+3.sha-6aac499');
+    var VERSION = new core.Version('10.1.0+6.sha-29c89c9');
 
     /**
      * @license
