4036437697a12dc18852b5855e0ccea3d2094cbd
atscott <atscott01@gmail.com>
2020-09-03 19:23:54 +0000

    25afbcc459 docs: add dayjs date adapter to resources page (#38031)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+6.sha-29c89c9
+ * @license Angular v10.1.0+7.sha-25afbcc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6298,7 +6298,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0+6.sha-29c89c9');
+    var VERSION = new core.Version('10.1.0+7.sha-25afbcc');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+6.sha-29c89c9
+ * @license Angular v10.1.0+7.sha-25afbcc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
