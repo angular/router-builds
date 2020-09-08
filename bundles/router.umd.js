03f5de32585ab0ef98d40aee803be843b7d6da11
atscott <atscott01@gmail.com>
2020-09-08 17:30:01 +0000

    9f28f82598 docs: add space between icon and text in issue template (#38712)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+16.sha-c2d017d
+ * @license Angular v11.0.0-next.0+22.sha-9f28f82
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6298,7 +6298,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.0+16.sha-c2d017d');
+    var VERSION = new core.Version('11.0.0-next.0+22.sha-9f28f82');
 
     /**
      * @license
