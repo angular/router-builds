0319bc90ee4f85c10dda4e02395629b7f73e6d9d
atscott <atscott01@gmail.com>
2020-09-08 17:30:50 +0000

    5863537575 build: upgrade all preview-server JS dependencies to latest versions (#38736)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+10.sha-0a277c6
+ * @license Angular v10.1.0+20.sha-5863537
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+10.sha-0a277c6
+ * @license Angular v10.1.0+20.sha-5863537
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5671,7 +5671,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0+10.sha-0a277c6');
+const VERSION = new Version('10.1.0+20.sha-5863537');
 
 /**
  * @license
