0e54a7aac97616584bc72f49fd73c4ae29568e70
atscott <atscott01@gmail.com>
2020-09-08 17:30:11 +0000

    3e57ca1d98 docs: fix typos in deployment guide (#38727)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+18.sha-79d8795
+ * @license Angular v11.0.0-next.0+17.sha-3e57ca1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6347,7 +6347,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.0+18.sha-79d8795');
+    var VERSION = new i0.Version('11.0.0-next.0+17.sha-3e57ca1');
 
     /**
      * @license
