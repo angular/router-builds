5519aaf44da89f72fa2daa1207c3c3d3365e606b
atscott <atscott01@gmail.com>
2020-09-08 23:01:39 +0000

    e156e29edd perf(compiler-cli): optimize computation of type-check scope information (#38749)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+34.sha-190dca0
+ * @license Angular v10.1.0+36.sha-e156e29
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+34.sha-190dca0
+ * @license Angular v10.1.0+36.sha-e156e29
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6299,7 +6299,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0+34.sha-190dca0');
+    var VERSION = new core.Version('10.1.0+36.sha-e156e29');
 
     /**
      * @license
