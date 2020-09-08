eae1095d238deeac261fa2170496c7f3266c86f7
atscott <atscott01@gmail.com>
2020-09-08 17:34:49 +0000

    261f689e9b docs: remove duplicate trans-unit element closing tag (#38715)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+19.sha-d9da7e5
+ * @license Angular v11.0.0-next.0+21.sha-261f689
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+19.sha-d9da7e5
+ * @license Angular v11.0.0-next.0+21.sha-261f689
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6347,7 +6347,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.0+19.sha-d9da7e5');
+    var VERSION = new i0.Version('11.0.0-next.0+21.sha-261f689');
 
     /**
      * @license
