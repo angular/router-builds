a1c9cf5ca4c3f829973aa9a4457cd6292b47d2d7
atscott <atscott01@gmail.com>
2020-09-09 16:29:46 +0000

    3cb2a79399 docs: update v9 support status to LTS (#38744)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+45.sha-ba95b79
+ * @license Angular v11.0.0-next.0+49.sha-3cb2a79
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5705,7 +5705,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.0+45.sha-ba95b79');
+const VERSION = new Version('11.0.0-next.0+49.sha-3cb2a79');
 
 /**
  * @license
