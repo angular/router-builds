ca365e12b360e8e9bc79515eae011348d01d76b2
atscott <atscott01@gmail.com>
2020-09-09 23:51:03 +0000

    3c4b8b97c1 docs: put docs style guide back in left nav (#38683)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+1.sha-c6ebb77
+ * @license Angular v11.0.0-next.1+3.sha-3c4b8b9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+1.sha-c6ebb77
+ * @license Angular v11.0.0-next.1+3.sha-3c4b8b9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5737,7 +5737,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.1+1.sha-c6ebb77');
+const VERSION = new Version('11.0.0-next.1+3.sha-3c4b8b9');
 
 /**
  * @license
