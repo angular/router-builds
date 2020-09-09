c727326876317aa8e3e1e9a7e7873ae42738c69c
atscott <atscott01@gmail.com>
2020-09-09 23:45:58 +0000

    f9421184ef refactor(dev-infra): update commit message validation to return validation result (#38703)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1
+ * @license Angular v11.0.0-next.1+2.sha-f942118
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6382,7 +6382,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.1');
+    var VERSION = new i0.Version('11.0.0-next.1+2.sha-f942118');
 
     /**
      * @license
