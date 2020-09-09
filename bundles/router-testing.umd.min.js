1005a692b3f7047abb6542c427b03c7ffd95b330
atscott <atscott01@gmail.com>
2020-09-09 23:46:19 +0000

    c6ebb77cec test(language-service): [ivy] remove all markers from test (#38777)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+2.sha-f942118
+ * @license Angular v11.0.0-next.1+1.sha-c6ebb77
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+2.sha-f942118
+ * @license Angular v11.0.0-next.1+1.sha-c6ebb77
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.1+2.sha-f942118');
+    var VERSION = new core.Version('11.0.0-next.1+1.sha-c6ebb77');
 
     /**
      * @license
