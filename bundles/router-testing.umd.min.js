1d81aa017a22c4d5989538254c6925aee1f086c8
Andrew Kushnir <akushnir@google.com>
2020-09-10 18:22:24 +0000

    a47383d1e8 fix(localize): ensure that `formatOptions` is optional (#38787)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+11.sha-9078187
+ * @license Angular v10.1.1+12.sha-a47383d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+11.sha-9078187
+ * @license Angular v10.1.1+12.sha-a47383d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6299,7 +6299,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.1+11.sha-9078187');
+    var VERSION = new core.Version('10.1.1+12.sha-a47383d');
 
     /**
      * @license
