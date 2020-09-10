d1e9cd613979dbf7b1f08c27a0bdf771d80c1263
Andrew Kushnir <akushnir@google.com>
2020-09-10 21:56:19 +0000

    31d0ee4cbf docs: Move Displaying data topic and ToH tutorial (#38774)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+12.sha-a47383d
+ * @license Angular v10.1.1+13.sha-31d0ee4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+12.sha-a47383d
+ * @license Angular v10.1.1+13.sha-31d0ee4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5672,7 +5672,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.1+12.sha-a47383d');
+const VERSION = new Version('10.1.1+13.sha-31d0ee4');
 
 /**
  * @license
