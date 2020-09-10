f12f29d2bc534e9d5875e173fa2a5bb4daf5f5d5
Andrew Kushnir <akushnir@google.com>
2020-09-10 18:20:39 +0000

    26f28200bf fix(common): do not round up fractions of a millisecond in `DatePipe` (#38009)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+12.sha-ce1efc1
+ * @license Angular v11.0.0-next.1+13.sha-26f2820
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6337,7 +6337,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.1+12.sha-ce1efc1');
+    var VERSION = new core.Version('11.0.0-next.1+13.sha-26f2820');
 
     /**
      * @license
