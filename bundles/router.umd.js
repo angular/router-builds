c8555ec3377fc861594b66f657914b8b730c8380
Andrew Kushnir <akushnir@google.com>
2020-09-11 16:32:40 +0000

    66129f8ea6 docs: clarify what fixes are merged to LTS versions (#38788)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+23.sha-6acea54
+ * @license Angular v11.0.0-next.1+26.sha-66129f8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6386,7 +6386,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.1+23.sha-6acea54');
+    var VERSION = new i0.Version('11.0.0-next.1+26.sha-66129f8');
 
     /**
      * @license
