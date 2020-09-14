e6c83c3030f7d8a4964cc9b4b676364c4989697a
Andrew Kushnir <akushnir@google.com>
2020-09-14 21:53:42 +0000

    3934f0a833 docs(elements): convert the `ng add` command to code-example (#38834)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+34.sha-297c060
+ * @license Angular v11.0.0-next.1+35.sha-3934f0a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6337,7 +6337,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.1+34.sha-297c060');
+    var VERSION = new core.Version('11.0.0-next.1+35.sha-3934f0a');
 
     /**
      * @license
