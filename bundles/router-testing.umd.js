883aa06d385342df1ba846f3c129619382010acd
Andrew Kushnir <akushnir@google.com>
2020-09-15 23:31:49 +0000

    3d94919800 refactor(dev-infra): refactor commit-message files (#38845)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+32.sha-af3b401
+ * @license Angular v10.1.1+33.sha-3d94919
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+32.sha-af3b401
+ * @license Angular v10.1.1+33.sha-3d94919
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6299,7 +6299,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.1+32.sha-af3b401');
+    var VERSION = new core.Version('10.1.1+33.sha-3d94919');
 
     /**
      * @license
