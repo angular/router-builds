3b8daa9a8ab8554e088f661784948760979827a6
Andrew Kushnir <akushnir@google.com>
2020-09-15 23:27:58 +0000

    d192c87f6a refactor(dev-infra): refactor commit-message files (#38845)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+45.sha-3817e5f
+ * @license Angular v11.0.0-next.1+46.sha-d192c87
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6337,7 +6337,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.1+45.sha-3817e5f');
+    var VERSION = new core.Version('11.0.0-next.1+46.sha-d192c87');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+45.sha-3817e5f
+ * @license Angular v11.0.0-next.1+46.sha-d192c87
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5709,7 +5709,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.1+45.sha-3817e5f');
+const VERSION = new Version('11.0.0-next.1+46.sha-d192c87');
 
 /**
  * @license
