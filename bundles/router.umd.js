23c66e53e98911b4962760f842e0f78e51617fa9
Andrew Kushnir <akushnir@google.com>
2020-09-15 18:55:37 +0000

    171a0d0696 docs: add ngc to glossary (#36781)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+43.sha-a1c1c45
+ * @license Angular v11.0.0-next.1+44.sha-171a0d0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6386,7 +6386,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.1+43.sha-a1c1c45');
+    var VERSION = new i0.Version('11.0.0-next.1+44.sha-171a0d0');
 
     /**
      * @license
