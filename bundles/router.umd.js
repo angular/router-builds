34ec878e5f364f325ebc78a80be5b76675c0ac15
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:39:59 +0000

    7849fdde09 feat(router): add migration to update calls to navigateByUrl and createUrlTree with invalid parameters (#38825)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+6.sha-97adc27
+ * @license Angular v11.0.0-next.2+7.sha-7849fdd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6337,7 +6337,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.2+6.sha-97adc27');
+    var VERSION = new core.Version('11.0.0-next.2+7.sha-7849fdd');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+6.sha-97adc27
+ * @license Angular v11.0.0-next.2+7.sha-7849fdd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
