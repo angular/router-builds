f3739ff9c11787920ecdc94096498b27193c8f20
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:41:53 +0000

    97adc27207 docs(bazel): fix typo in BUILD.bazel comments (#38054)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+2.sha-4407449
+ * @license Angular v11.0.0-next.2+6.sha-97adc27
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+2.sha-4407449
+ * @license Angular v11.0.0-next.2+6.sha-97adc27
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6386,7 +6386,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.2+2.sha-4407449');
+    var VERSION = new i0.Version('11.0.0-next.2+6.sha-97adc27');
 
     /**
      * @license
