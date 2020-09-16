0640fb0135dbf1e60ec01dd1324577a3ceeaa220
Joey Perrott <josephperrott@gmail.com>
2020-09-16 22:24:59 +0000

    a85109fd72 release: cut the v11.0.0-next.2 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+47.sha-85a2626
+ * @license Angular v11.0.0-next.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6337,7 +6337,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.1+47.sha-85a2626');
+    var VERSION = new core.Version('11.0.0-next.2');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+47.sha-85a2626
+ * @license Angular v11.0.0-next.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
