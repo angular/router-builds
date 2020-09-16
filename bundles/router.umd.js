7211d4f91b346819c1759fd3b4848ee749b6e8eb
mgechev <mgechev@gmail.com>
2020-09-16 23:04:57 +0000

    4a21eb84a7 docs: add upcoming angular conferences

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+47.sha-956e4f2
+ * @license Angular v11.0.0-next.2+12.sha-4a21eb8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6337,7 +6337,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.1+47.sha-956e4f2');
+    var VERSION = new core.Version('11.0.0-next.2+12.sha-4a21eb8');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+47.sha-956e4f2
+ * @license Angular v11.0.0-next.2+12.sha-4a21eb8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
