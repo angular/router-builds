8e3366f79160f1858e40ef43f65c937e4a4183d0
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:47:57 +0000

    49ee90b1b5 docs: fix typo in architecture.md guide (#38853)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+9.sha-e62a918
+ * @license Angular v11.0.0-next.2+8.sha-49ee90b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6386,7 +6386,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.2+9.sha-e62a918');
+    var VERSION = new i0.Version('11.0.0-next.2+8.sha-49ee90b');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+9.sha-e62a918
+ * @license Angular v11.0.0-next.2+8.sha-49ee90b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
