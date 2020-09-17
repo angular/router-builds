5469ef74c608337bfc382a021053b81b2d87b19f
Misko Hevery <misko@hevery.com>
2020-09-17 16:58:32 +0000

    c4b8964424 docs: drop `newEvent()` compatibility function (#37251)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+15.sha-a33d630
+ * @license Angular v11.0.0-next.2+14.sha-c4b8964
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6337,7 +6337,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.2+15.sha-a33d630');
+    var VERSION = new core.Version('11.0.0-next.2+14.sha-c4b8964');
 
     /**
      * @license
