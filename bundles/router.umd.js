15c86070d2cf68b458992866006efc8622b67f35
Misko Hevery <misko@hevery.com>
2020-09-17 16:59:13 +0000

    a33d630a21 fix(zone.js): should have better backward compatibilities (#38797)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+12.sha-1291071
+ * @license Angular v11.0.0-next.2+15.sha-a33d630
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6386,7 +6386,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.2+12.sha-1291071');
+    var VERSION = new i0.Version('11.0.0-next.2+15.sha-a33d630');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+12.sha-1291071
+ * @license Angular v11.0.0-next.2+15.sha-a33d630
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
