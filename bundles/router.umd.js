a85681779e96bbd0efa8c56ca0f1d27354cb80d6
Misko Hevery <misko@hevery.com>
2020-09-18 15:32:04 +0000

    d3169c533e refactor(core): remove unused imports (#38818)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+17.sha-02c6bff
+ * @license Angular v11.0.0-next.2+21.sha-d3169c5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6386,7 +6386,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.2+17.sha-02c6bff');
+    var VERSION = new i0.Version('11.0.0-next.2+21.sha-d3169c5');
 
     /**
      * @license
