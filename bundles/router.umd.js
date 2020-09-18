21b8fa7b1201bd480def5941c0337951e5cbeab4
Misko Hevery <misko@hevery.com>
2020-09-18 18:44:23 +0000

    88d7bb8386 fix(http): Fix error message when we call jsonp without importing HttpClientJsonpModule (#38756)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+25.sha-2d6105a
+ * @license Angular v11.0.0-next.2+26.sha-88d7bb8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6337,7 +6337,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.2+25.sha-2d6105a');
+    var VERSION = new core.Version('11.0.0-next.2+26.sha-88d7bb8');
 
     /**
      * @license
