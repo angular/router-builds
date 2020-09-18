47e5d477a0858d3b65f6543aed58cb85cadb4d28
Misko Hevery <misko@hevery.com>
2020-09-18 18:47:32 +0000

    d92a0dd72f fix(zone.js): should invoke xhr send task when no response error occurs (#38836)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+26.sha-88d7bb8
+ * @license Angular v11.0.0-next.2+23.sha-d92a0dd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6337,7 +6337,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.2+26.sha-88d7bb8');
+    var VERSION = new core.Version('11.0.0-next.2+23.sha-d92a0dd');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+26.sha-88d7bb8
+ * @license Angular v11.0.0-next.2+23.sha-d92a0dd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5709,7 +5709,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+26.sha-88d7bb8');
+const VERSION = new Version('11.0.0-next.2+23.sha-d92a0dd');
 
 /**
  * @license
