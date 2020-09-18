1222302d5a4f1211a058767b033158d78e13f3cf
Misko Hevery <misko@hevery.com>
2020-09-18 18:41:13 +0000

    55485713a3 fix(dev-infra): skip husky git commit hooks while merging pull requests (#38888)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+18.sha-7fb388f
+ * @license Angular v11.0.0-next.2+22.sha-5548571
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5709,7 +5709,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+18.sha-7fb388f');
+const VERSION = new Version('11.0.0-next.2+22.sha-5548571');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+18.sha-7fb388f
+ * @license Angular v11.0.0-next.2+22.sha-5548571
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
