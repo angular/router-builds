3195e670cc61d99216720db7c19fcb4a02b995a4
Misko Hevery <misko@hevery.com>
2020-09-18 15:32:36 +0000

    7fb388f929 docs: updating the text of user input page to reflect real location of the forms page (#38802)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+21.sha-d3169c5
+ * @license Angular v11.0.0-next.2+18.sha-7fb388f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6386,7 +6386,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.2+21.sha-d3169c5');
+    var VERSION = new i0.Version('11.0.0-next.2+18.sha-7fb388f');
 
     /**
      * @license
