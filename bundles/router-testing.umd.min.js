ee9a1738cab3f2b86904e5004b9cd78351f973e6
Misko Hevery <misko@hevery.com>
2020-09-18 18:43:45 +0000

    2d6105a784 feat(dev-infra): output the number of new and fixed cycles (#38805)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+22.sha-5548571
+ * @license Angular v11.0.0-next.2+25.sha-2d6105a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+22.sha-5548571
+ * @license Angular v11.0.0-next.2+25.sha-2d6105a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6337,7 +6337,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.2+22.sha-5548571');
+    var VERSION = new core.Version('11.0.0-next.2+25.sha-2d6105a');
 
     /**
      * @license
