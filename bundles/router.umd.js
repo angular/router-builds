1b971dc834128668722131dcc8e2b3ec0db0123d
Misko Hevery <misko@hevery.com>
2020-09-19 00:56:27 +0000

    d5ddb9f340 docs: Grammar fixes (#38900)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+30.sha-a91f0f6
+ * @license Angular v11.0.0-next.2+29.sha-d5ddb9f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6386,7 +6386,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.2+30.sha-a91f0f6');
+    var VERSION = new i0.Version('11.0.0-next.2+29.sha-d5ddb9f');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+30.sha-a91f0f6
+ * @license Angular v11.0.0-next.2+29.sha-d5ddb9f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5741,7 +5741,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+30.sha-a91f0f6');
+const VERSION = new Version('11.0.0-next.2+29.sha-d5ddb9f');
 
 /**
  * @license
