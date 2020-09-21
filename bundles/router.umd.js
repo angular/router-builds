b5b859073e9a27312f6824713e246f4bc47ce27b
Misko Hevery <misko@hevery.com>
2020-09-21 19:54:06 +0000

    984ed39195 feat(common): Add ISO week-numbering year formats support to formatDate (#38828)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+37.sha-2b1b718
+ * @license Angular v11.0.0-next.2+35.sha-984ed39
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6383,7 +6383,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.2+37.sha-2b1b718');
+    var VERSION = new i0.Version('11.0.0-next.2+35.sha-984ed39');
 
     /**
      * @license
