ba42f55ba5ebd76cad9d1f42483866af09073d2f
Misko Hevery <misko@hevery.com>
2020-09-21 19:56:12 +0000

    0c0c54d615 refactor(compiler): simplify visitor logic for attributes (#38899)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+33.sha-dfbfabc
+ * @license Angular v11.0.0-next.2+34.sha-0c0c54d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5738,7 +5738,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+33.sha-dfbfabc');
+const VERSION = new Version('11.0.0-next.2+34.sha-0c0c54d');
 
 /**
  * @license
