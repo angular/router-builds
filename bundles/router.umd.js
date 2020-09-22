3526fa03d51319f900770f78af7e7863fc098dbf
Misko Hevery <misko@hevery.com>
2020-09-22 16:57:12 +0000

    a2068523fd feat(service-worker): add the option to prefer network for navigation requests (#38565)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+49.sha-145ab3d
+ * @license Angular v11.0.0-next.2+50.sha-a206852
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6383,7 +6383,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.2+49.sha-145ab3d');
+    var VERSION = new i0.Version('11.0.0-next.2+50.sha-a206852');
 
     /**
      * @license
