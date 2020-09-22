8989476e39edb5c0eaad0374ba7ee9aaa9def4ab
Misko Hevery <misko@hevery.com>
2020-09-22 22:39:42 +0000

    c8f056beb6 fix(core): ensure TestBed is not instantiated before override provider (#38717)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+53.sha-7561050
+ * @license Angular v11.0.0-next.2+51.sha-c8f056b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6334,7 +6334,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.2+53.sha-7561050');
+    var VERSION = new core.Version('11.0.0-next.2+51.sha-c8f056b');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+53.sha-7561050
+ * @license Angular v11.0.0-next.2+51.sha-c8f056b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5706,7 +5706,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+53.sha-7561050');
+const VERSION = new Version('11.0.0-next.2+51.sha-c8f056b');
 
 /**
  * @license
