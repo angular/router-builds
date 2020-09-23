35105bfa7534e0c6d54fef8a7b3026ac3fefba7a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-23 20:14:47 +0000

    15ea811f05 feat(router): Add `relativeLinkResolution` migration to update default value (#38698)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+4.sha-f979914
+ * @license Angular v11.0.0-next.3+1.sha-15ea811
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6383,7 +6383,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.3+4.sha-f979914');
+    var VERSION = new i0.Version('11.0.0-next.3+1.sha-15ea811');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+4.sha-f979914
+ * @license Angular v11.0.0-next.3+1.sha-15ea811
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5738,7 +5738,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+4.sha-f979914');
+const VERSION = new Version('11.0.0-next.3+1.sha-15ea811');
 
 /**
  * @license
