793789543f39059dfa9e98bad2779423ca25c3c7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-23 20:18:02 +0000

    8236904933 test(platform-browser): remove usage of blacklist in test naming (#38928)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.3
+ * @license Angular v10.1.3+3.sha-8236904
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.3
+ * @license Angular v10.1.3+3.sha-8236904
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6296,7 +6296,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.3');
+    var VERSION = new core.Version('10.1.3+3.sha-8236904');
 
     /**
      * @license
