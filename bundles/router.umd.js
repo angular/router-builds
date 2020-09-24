785106ce76d79f49bd6fa7c6a74feaaeab1bcd23
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-24 16:07:05 +0000

    4c8766573d refactor(language-service): Update hybrid visitor to use keySpan for bound attributes (#38955)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+7.sha-8422633
+ * @license Angular v11.0.0-next.3+8.sha-4c87665
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6383,7 +6383,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.3+7.sha-8422633');
+    var VERSION = new i0.Version('11.0.0-next.3+8.sha-4c87665');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+7.sha-8422633
+ * @license Angular v11.0.0-next.3+8.sha-4c87665
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
