dde64050a40158a5f8dded837e632246e3534732
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:57:44 +0000

    4ca1c736bb fix(packaging): remove polyfills needed to run tests on IE9 and IE 10 (#38931)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+19.sha-cf9176e
+ * @license Angular v11.0.0-next.3+17.sha-4ca1c73
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6382,7 +6382,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.3+19.sha-cf9176e');
+    var VERSION = new i0.Version('11.0.0-next.3+17.sha-4ca1c73');
 
     /**
      * @license
