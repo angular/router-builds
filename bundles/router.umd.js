ede47f3e14c422766d5ad26bfdab0e91c94a0b59
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 19:02:26 +0000

    239968d2f1 feat(compiler): Add keySpan to Variable Node (#38965)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+14.sha-e790c85
+ * @license Angular v11.0.0-next.3+20.sha-239968d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6382,7 +6382,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.3+14.sha-e790c85');
+    var VERSION = new i0.Version('11.0.0-next.3+20.sha-239968d');
 
     /**
      * @license
