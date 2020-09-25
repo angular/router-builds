6e8ad0c397ce7a2d5ce4c4a1b7abbd5850766901
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 19:03:16 +0000

    323be39297 fix(language-service): hybrid visitor returns parent node of BoundAttribute (#38995)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+20.sha-239968d
+ * @license Angular v11.0.0-next.3+22.sha-323be39
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6382,7 +6382,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.3+20.sha-239968d');
+    var VERSION = new i0.Version('11.0.0-next.3+22.sha-323be39');
 
     /**
      * @license
