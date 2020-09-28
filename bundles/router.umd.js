d3ca3a516c3594e8e64fd51424100b21fe5c8916
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:53:32 +0000

    c5e178997c build(docs-infra): update docs examples to Angular v10 (#38993)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+69.sha-4fd635a
+ * @license Angular v11.0.0-next.3+65.sha-c5e1789
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.3+69.sha-4fd635a');
+    var VERSION = new core.Version('11.0.0-next.3+65.sha-c5e1789');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+69.sha-4fd635a
+ * @license Angular v11.0.0-next.3+65.sha-c5e1789
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
