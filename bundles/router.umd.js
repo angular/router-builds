c179b66ba7d83d2df4bb0aafe14effe1030a3a93
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:57:47 +0000

    3ba97ab391 ci: note Paul and Pawel as OOO in pullapprove (#39028)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+65.sha-c5e1789
+ * @license Angular v11.0.0-next.3+77.sha-3ba97ab
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6382,7 +6382,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.3+65.sha-c5e1789');
+    var VERSION = new i0.Version('11.0.0-next.3+77.sha-3ba97ab');
 
     /**
      * @license
