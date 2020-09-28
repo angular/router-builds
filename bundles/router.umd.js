71028489c6fe233abdd0cd6361364d460975e78a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 16:47:41 +0000

    4744c229db fix(common): correct typing and implementation of `SlicePipe` (#37447)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+22.sha-323be39
+ * @license Angular v11.0.0-next.3+29.sha-4744c22
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.3+22.sha-323be39');
+    var VERSION = new core.Version('11.0.0-next.3+29.sha-4744c22');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+22.sha-323be39
+ * @license Angular v11.0.0-next.3+29.sha-4744c22
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
