277eec1f69129eb9e4e5f79150184b37d9db5067
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:55:58 +0000

    4fd635a03a build(docs-infra): enable AOT in development mode for all docs examples (#39001)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+63.sha-4a9f452
+ * @license Angular v11.0.0-next.3+69.sha-4fd635a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+63.sha-4a9f452
+ * @license Angular v11.0.0-next.3+69.sha-4fd635a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6382,7 +6382,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.3+63.sha-4a9f452');
+    var VERSION = new i0.Version('11.0.0-next.3+69.sha-4fd635a');
 
     /**
      * @license
