bc1068e06223d0957e589c67a68f363f2ea9aad2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:49:50 +0000

    4a9f4520ce docs: remove usage of whitelist in cli analytics docs (#38963)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+62.sha-914d709
+ * @license Angular v11.0.0-next.3+63.sha-4a9f452
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.3+62.sha-914d709');
+    var VERSION = new core.Version('11.0.0-next.3+63.sha-4a9f452');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+62.sha-914d709
+ * @license Angular v11.0.0-next.3+63.sha-4a9f452
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
