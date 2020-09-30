fab35b78c4f415a86fc39444cffb90ddf2fcbaa0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 14:01:32 +0000

    31e42f0947 refactor(language-service): create getTypeDefinitionAtPosition stubs (#39050)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+95.sha-ddc9e8e
+ * @license Angular v11.0.0-next.3+96.sha-31e42f0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.3+95.sha-ddc9e8e');
+    var VERSION = new core.Version('11.0.0-next.3+96.sha-31e42f0');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+95.sha-ddc9e8e
+ * @license Angular v11.0.0-next.3+96.sha-31e42f0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
