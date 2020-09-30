33b095cd71eca3fe45f51bc53e4095e3576615f5
Joey Perrott <josephperrott@gmail.com>
2020-09-30 20:20:33 +0000

    79620f5139 refactor(localize): avoid free-standing `FileSystem` functions (#39006)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+9.sha-bd5e425
+ * @license Angular v11.0.0-next.4+8.sha-79620f5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6382,7 +6382,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.4+9.sha-bd5e425');
+    var VERSION = new i0.Version('11.0.0-next.4+8.sha-79620f5');
 
     /**
      * @license
