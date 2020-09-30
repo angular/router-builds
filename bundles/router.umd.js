9c9acc6f582a2fe390df07e4ed7688794f1f9970
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 14:02:54 +0000

    ddc9e8e47a refactor(compiler): refactor template symbol builder (#39047)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+92.sha-827245f
+ * @license Angular v11.0.0-next.3+95.sha-ddc9e8e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6382,7 +6382,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.3+92.sha-827245f');
+    var VERSION = new i0.Version('11.0.0-next.3+95.sha-ddc9e8e');
 
     /**
      * @license
