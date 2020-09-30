1a18901ebe43bb05f22372015a061a16219c552a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:56:00 +0000

    1e3f810f8f fix(bazel): clean up outstanding failure message for usages of --define=compile (#39046)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+89.sha-2aeaedc
+ * @license Angular v11.0.0-next.3+94.sha-1e3f810
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.3+89.sha-2aeaedc');
+    var VERSION = new core.Version('11.0.0-next.3+94.sha-1e3f810');
 
     /**
      * @license
