89bbff553000b44c6c655cf8e09e351f62952f94
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:57:25 +0000

    2aeaedc519 docs(docs-infra): replace a comma with a period at the end of a sentence (#39034)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+87.sha-8f11b51
+ * @license Angular v11.0.0-next.3+89.sha-2aeaedc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6382,7 +6382,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.3+87.sha-8f11b51');
+    var VERSION = new i0.Version('11.0.0-next.3+89.sha-2aeaedc');
 
     /**
      * @license
