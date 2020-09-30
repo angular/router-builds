2749a4302b7be64818e6cce748846cb1e1ed39fe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:51:46 +0000

    8f66540152 test(language-service): Update AppComponent test project to include more fields (#39033)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+82.sha-ded9aeb
+ * @license Angular v11.0.0-next.3+88.sha-8f66540
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6382,7 +6382,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.3+82.sha-ded9aeb');
+    var VERSION = new i0.Version('11.0.0-next.3+88.sha-8f66540');
 
     /**
      * @license
