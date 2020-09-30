01287406ce2b3b0697b64f0cef81112e9cd0c5e4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 14:16:42 +0000

    a1bf0de711 release: cut the zone.js-0.11.2 release (#39032)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.3+60.sha-69302ad
+ * @license Angular v10.1.3+72.sha-a1bf0de
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5669,7 +5669,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.3+60.sha-69302ad');
+const VERSION = new Version('10.1.3+72.sha-a1bf0de');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.3+60.sha-69302ad
+ * @license Angular v10.1.3+72.sha-a1bf0de
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
