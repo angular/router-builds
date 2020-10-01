a5d6e6274cbbda1b8c3bfc7d185d5ba97564b7fc
Joey Perrott <josephperrott@gmail.com>
2020-10-01 16:58:16 +0000

    50e83e2566 refactor(docs-infra): leave TODO comments for aligning tslint with CLI (#39018)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+7.sha-bb11fd9
+ * @license Angular v10.1.4+14.sha-50e83e2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+7.sha-bb11fd9
+ * @license Angular v10.1.4+14.sha-50e83e2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5668,7 +5668,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.4+7.sha-bb11fd9');
+const VERSION = new Version('10.1.4+14.sha-50e83e2');
 
 /**
  * @license
