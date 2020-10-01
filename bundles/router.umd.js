9ba7b374613606452c647cd0c32960c91f18d1ed
Joey Perrott <josephperrott@gmail.com>
2020-10-01 16:58:33 +0000

    7dd0db6d4f refactor(compiler-cli): implement `BabelAstFactory` and `AstHost`s (#38866)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+13.sha-21213e1
+ * @license Angular v11.0.0-next.4+16.sha-7dd0db6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6381,7 +6381,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.4+13.sha-21213e1');
+    var VERSION = new i0.Version('11.0.0-next.4+16.sha-7dd0db6');
 
     /**
      * @license
