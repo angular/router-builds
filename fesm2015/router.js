45d2e258aa926176dbf6506ad4ccbece99efae1a
Joey Perrott <josephperrott@gmail.com>
2020-10-02 21:55:17 +0000

    5dbf357224 feat(compiler-cli): support getting resource dependencies for a source file (#38048)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+34.sha-3bbbf39
+ * @license Angular v11.0.0-next.4+35.sha-5dbf357
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5736,7 +5736,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+34.sha-3bbbf39');
+const VERSION = new Version('11.0.0-next.4+35.sha-5dbf357');
 
 /**
  * @license
