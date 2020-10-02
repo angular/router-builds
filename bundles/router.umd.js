60ed9076e4efaeb438bfd730335ed74cd6e36ad1
Joey Perrott <josephperrott@gmail.com>
2020-10-02 18:25:23 +0000

    904adb72d2 feat(language-service): add quick info for inline templates in ivy (#39060)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+31.sha-d3bd00b
+ * @license Angular v11.0.0-next.4+33.sha-904adb7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6381,7 +6381,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.4+31.sha-d3bd00b');
+    var VERSION = new i0.Version('11.0.0-next.4+33.sha-904adb7');
 
     /**
      * @license
