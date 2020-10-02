9af9482828be4514cdc8095b75ab7809607fa9c3
Joey Perrott <josephperrott@gmail.com>
2020-10-02 21:52:50 +0000

    5dbf357224 feat(compiler-cli): support getting resource dependencies for a source file (#38048)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+34.sha-3bbbf39
+ * @license Angular v11.0.0-next.4+35.sha-5dbf357
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6332,7 +6332,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.4+34.sha-3bbbf39');
+    var VERSION = new core.Version('11.0.0-next.4+35.sha-5dbf357');
 
     /**
      * @license
