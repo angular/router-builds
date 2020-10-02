32ec646451016791dd3000390254db62a2fadae0
Joey Perrott <josephperrott@gmail.com>
2020-10-02 22:43:50 +0000

    89c5255b8c refactor(compiler): iteratively parse interpolations (#38977)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+35.sha-5dbf357
+ * @license Angular v11.0.0-next.4+36.sha-89c5255
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5736,7 +5736,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+35.sha-5dbf357');
+const VERSION = new Version('11.0.0-next.4+36.sha-89c5255');
 
 /**
  * @license
