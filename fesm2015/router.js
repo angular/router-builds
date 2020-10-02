e245c7e404cd71bbc7d07413c84b16d827e39d3d
Joey Perrott <josephperrott@gmail.com>
2020-10-02 18:23:16 +0000

    30433a0710 fix(docs-infra): better distinguish wrapped headings from other entries in TOC (#39092)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+18.sha-42f9679
+ * @license Angular v10.1.4+20.sha-30433a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5668,7 +5668,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.4+18.sha-42f9679');
+const VERSION = new Version('10.1.4+20.sha-30433a0');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+18.sha-42f9679
+ * @license Angular v10.1.4+20.sha-30433a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
