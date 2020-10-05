85815d413c159a7d2333545b7f44eb63310bae86
Joey Perrott <josephperrott@gmail.com>
2020-10-05 17:46:45 +0000

    878e2f0deb feat(dev-infra): show CI status of all active release trains (#39067)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+23.sha-4c30f51
+ * @license Angular v10.1.4+24.sha-878e2f0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6295,7 +6295,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.4+23.sha-4c30f51');
+    var VERSION = new core.Version('10.1.4+24.sha-878e2f0');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+23.sha-4c30f51
+ * @license Angular v10.1.4+24.sha-878e2f0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
