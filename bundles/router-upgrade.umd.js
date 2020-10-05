0ff888d5440c42839e748565d404992573ce3f6e
Joey Perrott <josephperrott@gmail.com>
2020-10-05 23:37:51 +0000

    faa81dcdd1 refactor(language-service): Extract common methods from VE and Ivy to new package (#39098)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+46.sha-e10b3e2
+ * @license Angular v11.0.0-next.4+47.sha-faa81dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+46.sha-e10b3e2
+ * @license Angular v11.0.0-next.4+47.sha-faa81dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6332,7 +6332,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.4+46.sha-e10b3e2');
+    var VERSION = new core.Version('11.0.0-next.4+47.sha-faa81dc');
 
     /**
      * @license
