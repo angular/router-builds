6330d5437a48c1dda7e7020151bb1d248e7cc0a4
Joey Perrott <josephperrott@gmail.com>
2020-10-05 20:53:55 +0000

    bc69182bdd docs: Migrate section, view encapsulation, from Component Styles topic into its own topic. (#38986)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+43.sha-82d54fe
+ * @license Angular v11.0.0-next.4+44.sha-bc69182
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6381,7 +6381,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.4+43.sha-82d54fe');
+    var VERSION = new i0.Version('11.0.0-next.4+44.sha-bc69182');
 
     /**
      * @license
