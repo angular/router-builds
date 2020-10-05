3ec3784a713da1e0704977fbbb8d05d1284f576c
Joey Perrott <josephperrott@gmail.com>
2020-10-05 15:29:42 +0000

    4beaddcb83 docs(upgrade): expand upon change detection implications for downgraded components (#39100)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+36.sha-89c5255
+ * @license Angular v11.0.0-next.4+37.sha-4beaddc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+36.sha-89c5255
+ * @license Angular v11.0.0-next.4+37.sha-4beaddc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6332,7 +6332,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.4+36.sha-89c5255');
+    var VERSION = new core.Version('11.0.0-next.4+37.sha-4beaddc');
 
     /**
      * @license
