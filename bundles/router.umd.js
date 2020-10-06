9d9c892097aa6dbc216030170fe25742fc48754c
Joey Perrott <josephperrott@gmail.com>
2020-10-06 21:19:33 +0000

    aeec223db1 feat(forms): add migration for AbstractControl.parent accesses (#39009)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+57.sha-3975dd9
+ * @license Angular v11.0.0-next.4+60.sha-aeec223
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6384,7 +6384,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.4+57.sha-3975dd9');
+    var VERSION = new i0.Version('11.0.0-next.4+60.sha-aeec223');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+57.sha-3975dd9
+ * @license Angular v11.0.0-next.4+60.sha-aeec223
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5739,7 +5739,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+57.sha-3975dd9');
+const VERSION = new Version('11.0.0-next.4+60.sha-aeec223');
 
 /**
  * @license
