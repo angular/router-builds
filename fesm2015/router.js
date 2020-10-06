2bfcacc843373dd8a0ee5938bc1729079723f4ac
Joey Perrott <josephperrott@gmail.com>
2020-10-06 00:33:29 +0000

    3c474ecf56 fix(common): add boolean to valid json for testing (#37893)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+49.sha-ee03408
+ * @license Angular v11.0.0-next.4+50.sha-3c474ec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5707,7 +5707,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+49.sha-ee03408');
+const VERSION = new Version('11.0.0-next.4+50.sha-3c474ec');
 
 /**
  * @license
