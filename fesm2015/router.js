e94c956b6ef1518bac05e1a51a2f3985c8c09d14
Joey Perrott <josephperrott@gmail.com>
2020-10-06 18:58:57 +0000

    05672ab136 refactor(compiler-cli): `attachComments()` now expects a defined `leadingComments` array (#39076)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+55.sha-70e13dc
+ * @license Angular v11.0.0-next.4+56.sha-05672ab
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5707,7 +5707,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+55.sha-70e13dc');
+const VERSION = new Version('11.0.0-next.4+56.sha-05672ab');
 
 /**
  * @license
