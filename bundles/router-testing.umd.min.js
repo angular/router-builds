f1497f09755acb29cb419b874b0d97a1d15bf531
Joey Perrott <josephperrott@gmail.com>
2020-10-06 00:37:45 +0000

    21dfb33b27 build: update bazelversion (#39123)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+50.sha-3c474ec
+ * @license Angular v11.0.0-next.4+51.sha-21dfb33
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+50.sha-3c474ec
+ * @license Angular v11.0.0-next.4+51.sha-21dfb33
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6384,7 +6384,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.4+50.sha-3c474ec');
+    var VERSION = new i0.Version('11.0.0-next.4+51.sha-21dfb33');
 
     /**
      * @license
