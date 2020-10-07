3536e22546e94827d3c7e6ec6a11eb1847466796
Joey Perrott <josephperrott@gmail.com>
2020-10-07 20:59:02 +0000

    cc13b37446 build(docs-infra): render optional decorator options with a ? (#39167)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+33.sha-9f132d0
+ * @license Angular v10.1.4+36.sha-cc13b37
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6295,7 +6295,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.4+33.sha-9f132d0');
+    var VERSION = new core.Version('10.1.4+36.sha-cc13b37');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+33.sha-9f132d0
+ * @license Angular v10.1.4+36.sha-cc13b37
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
