c6261e80a0b6af128ce2d293991ea4c4f8b3a881
Joey Perrott <josephperrott@gmail.com>
2020-10-07 20:57:33 +0000

    5fa012efb8 docs: change heading to a sub-heading (#39131)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+59.sha-f2fca6d
+ * @license Angular v11.0.0-next.4+61.sha-5fa012e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+59.sha-f2fca6d
+ * @license Angular v11.0.0-next.4+61.sha-5fa012e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6384,7 +6384,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.4+59.sha-f2fca6d');
+    var VERSION = new i0.Version('11.0.0-next.4+61.sha-5fa012e');
 
     /**
      * @license
