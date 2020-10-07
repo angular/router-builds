873c5379d0396c0c1d6b9d94b3a31a29b3216066
atscott <atscott01@gmail.com>
2020-10-07 23:33:44 +0000

    eb34037cd7 Revert "refactor(compiler-cli): wrap RHS of property write in parens (#39143)" (#39171)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+36.sha-cc13b37
+ * @license Angular v10.1.5+2.sha-eb34037
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+36.sha-cc13b37
+ * @license Angular v10.1.5+2.sha-eb34037
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6295,7 +6295,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.4+36.sha-cc13b37');
+    var VERSION = new core.Version('10.1.5+2.sha-eb34037');
 
     /**
      * @license
