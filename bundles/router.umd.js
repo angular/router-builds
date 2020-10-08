76ac8d9cd675c6299e6d8c3e814cebd993b705e9
atscott <atscott01@gmail.com>
2020-10-08 16:11:19 +0000

    4604fe9ed2 feat(language-service): Add module name to directive quick info (#39121)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+2.sha-de66818
+ * @license Angular v11.0.0-next.5+4.sha-4604fe9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6384,7 +6384,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.5+2.sha-de66818');
+    var VERSION = new i0.Version('11.0.0-next.5+4.sha-4604fe9');
 
     /**
      * @license
