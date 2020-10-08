b08a0d4d64cf1b880c2959a592c466bbd06ea655
atscott <atscott01@gmail.com>
2020-10-08 19:19:25 +0000

    42a164f522 build: upgrade angular build, integration/bazel and @angular/bazel package to rule_nodejs 2.2.0 (#39182)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+7.sha-049b453
+ * @license Angular v11.0.0-next.5+8.sha-42a164f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5707,7 +5707,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.5+7.sha-049b453');
+const VERSION = new Version('11.0.0-next.5+8.sha-42a164f');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+7.sha-049b453
+ * @license Angular v11.0.0-next.5+8.sha-42a164f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
