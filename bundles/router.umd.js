2e218ddc6e7ab26615441b9ee8d48ab12768ebb9
atscott <atscott01@gmail.com>
2020-10-08 16:10:59 +0000

    de66818cf6 test(compiler-cli): temporarily disable integrationtest (#39168)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+3.sha-01ec0f0
+ * @license Angular v11.0.0-next.5+2.sha-de66818
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6384,7 +6384,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.5+3.sha-01ec0f0');
+    var VERSION = new i0.Version('11.0.0-next.5+2.sha-de66818');
 
     /**
      * @license
