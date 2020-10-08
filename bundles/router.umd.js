21e4ea58a8675e2cad7fb18c2eba60fe2adb21a4
atscott <atscott01@gmail.com>
2020-10-08 16:10:50 +0000

    8cf16b4815 feat(docs-infra): add short url for strict mode guide (#39129)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+2.sha-eb34037
+ * @license Angular v10.1.5+4.sha-8cf16b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6295,7 +6295,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.5+2.sha-eb34037');
+    var VERSION = new core.Version('10.1.5+4.sha-8cf16b4');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+2.sha-eb34037
+ * @license Angular v10.1.5+4.sha-8cf16b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
