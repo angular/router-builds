f3a5e1720aa628764b974317f318330c29a62ddb
atscott <atscott01@gmail.com>
2020-10-08 16:14:24 +0000

    63624a2d46 feat(language-service): [Ivy] getSemanticDiagnostics for external templates (#39065)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+4.sha-4604fe9
+ * @license Angular v11.0.0-next.5+5.sha-63624a2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6384,7 +6384,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.5+4.sha-4604fe9');
+    var VERSION = new i0.Version('11.0.0-next.5+5.sha-63624a2');
 
     /**
      * @license
