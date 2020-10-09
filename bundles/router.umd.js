7cfdcda00c52e1127ef7e4f0db2643f7eac87426
atscott <atscott01@gmail.com>
2020-10-09 16:04:15 +0000

    7f689a291a fix(compiler): incorrectly encapsulating @import containing colons and semicolons (#38716)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+14.sha-ca4ef61
+ * @license Angular v11.0.0-next.5+15.sha-7f689a2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6384,7 +6384,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.5+14.sha-ca4ef61');
+    var VERSION = new i0.Version('11.0.0-next.5+15.sha-7f689a2');
 
     /**
      * @license
