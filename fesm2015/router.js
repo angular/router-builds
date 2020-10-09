de17e6bdba5373a49290c912ca00d26e9630a7b9
atscott <atscott01@gmail.com>
2020-10-09 16:00:23 +0000

    52a0c6b36e fix(compiler): incorrectly encapsulating @import containing colons and semicolons (#38716)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+7.sha-8913aee
+ * @license Angular v10.1.5+8.sha-52a0c6b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5668,7 +5668,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+7.sha-8913aee');
+const VERSION = new Version('10.1.5+8.sha-52a0c6b');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+7.sha-8913aee
+ * @license Angular v10.1.5+8.sha-52a0c6b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
