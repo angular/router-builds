3481958013cf733eb85e926501cfffbc1f04dc0d
atscott <atscott01@gmail.com>
2020-10-12 20:04:57 +0000

    72f4497f11 docs: move Inputs and Outputs topic to Components section (#38984)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+45.sha-96dd78e
+ * @license Angular v11.0.0-next.5+46.sha-72f4497
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+45.sha-96dd78e
+ * @license Angular v11.0.0-next.5+46.sha-72f4497
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6384,7 +6384,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.5+45.sha-96dd78e');
+    var VERSION = new i0.Version('11.0.0-next.5+46.sha-72f4497');
 
     /**
      * @license
