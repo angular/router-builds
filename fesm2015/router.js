ff02c688c1fd29e1ec658a52f14e991e75b6ca49
atscott <atscott01@gmail.com>
2020-10-12 18:10:50 +0000

    c76dda6aeb fix(dev-infra): correctly check for commit that closes PR (#39135)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+41.sha-5471789
+ * @license Angular v11.0.0-next.5+43.sha-c76dda6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5707,7 +5707,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.5+41.sha-5471789');
+const VERSION = new Version('11.0.0-next.5+43.sha-c76dda6');
 
 /**
  * @license
