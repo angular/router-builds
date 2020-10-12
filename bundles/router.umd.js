66683fc6738512c18756dd63ef152d3570a98021
atscott <atscott01@gmail.com>
2020-10-12 15:35:21 +0000

    a84976fdfc feat(language-service): Add getTypeDefinitionAtPosition (go to type definition) (#39145)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+17.sha-9f3388e
+ * @license Angular v11.0.0-next.5+19.sha-a84976f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6384,7 +6384,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.5+17.sha-9f3388e');
+    var VERSION = new i0.Version('11.0.0-next.5+19.sha-a84976f');
 
     /**
      * @license
