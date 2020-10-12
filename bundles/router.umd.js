a59851853fde02a557b3227504b353c58b266784
atscott <atscott01@gmail.com>
2020-10-12 20:02:38 +0000

    72f4497f11 docs: move Inputs and Outputs topic to Components section (#38984)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+45.sha-96dd78e
+ * @license Angular v11.0.0-next.5+46.sha-72f4497
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6335,7 +6335,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.5+45.sha-96dd78e');
+    var VERSION = new core.Version('11.0.0-next.5+46.sha-72f4497');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+45.sha-96dd78e
+ * @license Angular v11.0.0-next.5+46.sha-72f4497
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5707,7 +5707,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.5+45.sha-96dd78e');
+const VERSION = new Version('11.0.0-next.5+46.sha-72f4497');
 
 /**
  * @license
