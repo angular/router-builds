f020cde4f5bf5ce7c5158f7664d0524d96210530
atscott <atscott01@gmail.com>
2020-10-12 18:08:21 +0000

    5471789664 refactor(animations): do not assign to innerHTML (#37397)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+40.sha-952710b
+ * @license Angular v11.0.0-next.5+41.sha-5471789
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6335,7 +6335,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.5+40.sha-952710b');
+    var VERSION = new core.Version('11.0.0-next.5+41.sha-5471789');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+40.sha-952710b
+ * @license Angular v11.0.0-next.5+41.sha-5471789
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
