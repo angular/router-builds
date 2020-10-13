592fd6f3abaf06e8562d169ede87a9a7007ffaea
atscott <atscott01@gmail.com>
2020-10-13 16:17:17 +0000

    437e563507 refactor(language-service): Allow "go to definition" for directives in Ivy (#39228)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+47.sha-b0b4953
+ * @license Angular v11.0.0-next.5+48.sha-437e563
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6335,7 +6335,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.5+47.sha-b0b4953');
+    var VERSION = new core.Version('11.0.0-next.5+48.sha-437e563');
 
     /**
      * @license
