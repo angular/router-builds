5ab28689a1091245365acc28b94478f003fb9c6b
atscott <atscott01@gmail.com>
2020-10-13 19:22:56 +0000

    0875fd2360 feat(core): create internal Trusted Types module (#39207)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+49.sha-5ce71e0
+ * @license Angular v11.0.0-next.5+51.sha-0875fd2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6384,7 +6384,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.5+49.sha-5ce71e0');
+    var VERSION = new i0.Version('11.0.0-next.5+51.sha-0875fd2');
 
     /**
      * @license
