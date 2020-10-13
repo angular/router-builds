20a99c254b9a25548e668897496e3dce2af56f85
atscott <atscott01@gmail.com>
2020-10-13 17:21:51 +0000

    5ce71e0fbc feat(core): add automated migration to replace async with waitForAsync (#39212)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+48.sha-437e563
+ * @license Angular v11.0.0-next.5+49.sha-5ce71e0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6335,7 +6335,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.5+48.sha-437e563');
+    var VERSION = new core.Version('11.0.0-next.5+49.sha-5ce71e0');
 
     /**
      * @license
