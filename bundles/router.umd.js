e89df136a40e395073497eea0cfc5f1ed3ca2b85
atscott <atscott01@gmail.com>
2020-10-13 23:19:12 +0000

    8fd25d9614 docs: remove IE10 references from comments in the code (#39090)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+55.sha-584f37c
+ * @license Angular v11.0.0-next.5+62.sha-8fd25d9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6384,7 +6384,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.5+55.sha-584f37c');
+    var VERSION = new i0.Version('11.0.0-next.5+62.sha-8fd25d9');
 
     /**
      * @license
