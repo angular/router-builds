0fb5d1e9d107de7835cc4d401fae1da04561b988
atscott <atscott01@gmail.com>
2020-10-14 21:40:06 +0000

    a83693ddd9 refactor(core): use relative import paths in micro benchmarks (#39142)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+1.sha-cdf642b
+ * @license Angular v11.0.0-next.6+5.sha-a83693d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+1.sha-cdf642b');
+    var VERSION = new i0.Version('11.0.0-next.6+5.sha-a83693d');
 
     /**
      * @license
