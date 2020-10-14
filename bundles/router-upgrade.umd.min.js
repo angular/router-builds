00bf5f63ef94e35418698dead3b6ffe344529077
atscott <atscott01@gmail.com>
2020-10-14 21:25:09 +0000

    a5d8d03edb release: cut the v11.0.0-next.6 release

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+74.sha-0ec7043
+ * @license Angular v11.0.0-next.6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+74.sha-0ec7043
+ * @license Angular v11.0.0-next.6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6286,7 +6286,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.5+74.sha-0ec7043');
+    var VERSION = new core.Version('11.0.0-next.6');
 
     /**
      * @license
