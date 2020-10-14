c7b5c04809d19b8e7e3d53c51ef6c293f6994abb
atscott <atscott01@gmail.com>
2020-10-14 16:59:20 +0000

    5221df8e92 perf(ngcc): do not rescan program source files when referenced from multiple root files (#39254)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+20.sha-23fc2b4
+ * @license Angular v10.1.5+22.sha-5221df8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+20.sha-23fc2b4
+ * @license Angular v10.1.5+22.sha-5221df8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6295,7 +6295,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.5+20.sha-23fc2b4');
+    var VERSION = new core.Version('10.1.5+22.sha-5221df8');
 
     /**
      * @license
