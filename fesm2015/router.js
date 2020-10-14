899ea09e2d3c09398b0200701ea24f2ddd2f939f
atscott <atscott01@gmail.com>
2020-10-14 17:03:08 +0000

    f6d5cdfbd7 fix(core): use Trusted Types policy in named_array_type (#39209)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+68.sha-898be92
+ * @license Angular v11.0.0-next.5+67.sha-f6d5cdf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5739,7 +5739,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.5+68.sha-898be92');
+const VERSION = new Version('11.0.0-next.5+67.sha-f6d5cdf');
 
 /**
  * @license
