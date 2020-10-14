6c05a43017876207d30fb64c777ef902235336e1
atscott <atscott01@gmail.com>
2020-10-14 18:14:19 +0000

    c6350116b5 docs: add Discord as an official communication channel (#39149)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+67.sha-f6d5cdf
+ * @license Angular v11.0.0-next.5+69.sha-c635011
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6384,7 +6384,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.5+67.sha-f6d5cdf');
+    var VERSION = new i0.Version('11.0.0-next.5+69.sha-c635011');
 
     /**
      * @license
