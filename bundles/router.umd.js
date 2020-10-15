461b43c19c22b816ce44a8397254974eb6ae65fe
atscott <atscott01@gmail.com>
2020-10-15 16:35:07 +0000

    497af77d7a build(zone.js): zone.js should output esm format for fesm2015 bundles (#39203)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+13.sha-6e18d2d
+ * @license Angular v11.0.0-next.6+11.sha-497af77
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+13.sha-6e18d2d');
+    var VERSION = new i0.Version('11.0.0-next.6+11.sha-497af77');
 
     /**
      * @license
