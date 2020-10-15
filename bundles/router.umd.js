5d4554dbe807e6176a76dc58dc251595e6e0d8c1
Andrew Kushnir <akushnir@google.com>
2020-10-15 23:29:19 +0000

    5bda62c51d refactor(language-service): Return directive defs when input name is part of selector (#39243)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+22.sha-7669827
+ * @license Angular v11.0.0-next.6+24.sha-5bda62c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6286,7 +6286,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+22.sha-7669827');
+    var VERSION = new core.Version('11.0.0-next.6+24.sha-5bda62c');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+22.sha-7669827
+ * @license Angular v11.0.0-next.6+24.sha-5bda62c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
