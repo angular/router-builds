cd9b0292085dc3943c9710d277d743fff31b5359
Andrew Kushnir <akushnir@google.com>
2020-10-15 18:53:22 +0000

    85be258845 ci: add jessicajaniuk to require-minimum-review reviewers (#39268)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+11.sha-497af77
+ * @license Angular v11.0.0-next.6+15.sha-85be258
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+11.sha-497af77');
+    var VERSION = new i0.Version('11.0.0-next.6+15.sha-85be258');
 
     /**
      * @license
