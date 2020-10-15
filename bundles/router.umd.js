d34d6e87f3dec235d3974e389b6b8143cdfd7910
atscott <atscott01@gmail.com>
2020-10-15 16:32:53 +0000

    6e18d2dacc fix(compiler): promote constants in templates to Trusted Types (#39211)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+10.sha-822b838
+ * @license Angular v11.0.0-next.6+13.sha-6e18d2d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+10.sha-822b838');
+    var VERSION = new i0.Version('11.0.0-next.6+13.sha-6e18d2d');
 
     /**
      * @license
