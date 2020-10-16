fd3b8061edba45980157ab09fbb48e0705db1ae6
Andrew Kushnir <akushnir@google.com>
2020-10-16 17:31:36 +0000

    4dc8d83d84 docs: edit attribute-binding doc and move colSpan note to property binding (#38860)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+34.sha-34b74ce
+ * @license Angular v11.0.0-next.6+36.sha-4dc8d83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5696,7 +5696,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+34.sha-34b74ce');
+const VERSION = new Version('11.0.0-next.6+36.sha-4dc8d83');
 
 /**
  * @license
