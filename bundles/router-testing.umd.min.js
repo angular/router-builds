56719cf93e8227d82ac76d3406b7b9d052531f95
Andrew Kushnir <akushnir@google.com>
2020-10-16 00:26:32 +0000

    96f59f64a0 docs: Very minor spelling mistake (#39299)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+24.sha-5bda62c
+ * @license Angular v11.0.0-next.6+25.sha-96f59f6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+24.sha-5bda62c
+ * @license Angular v11.0.0-next.6+25.sha-96f59f6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+24.sha-5bda62c');
+    var VERSION = new i0.Version('11.0.0-next.6+25.sha-96f59f6');
 
     /**
      * @license
