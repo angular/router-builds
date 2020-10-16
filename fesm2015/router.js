3880382a2ac0e39d5415dd73aac44e9edd494529
Andrew Kushnir <akushnir@google.com>
2020-10-16 23:32:00 +0000

    10db99588f refactor(docs-infra): fix `strictTemplates` failures in `accessibility` docs example (#39248)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+21.sha-8fd59da
+ * @license Angular v10.1.6+22.sha-10db995
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5668,7 +5668,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.6+21.sha-8fd59da');
+const VERSION = new Version('10.1.6+22.sha-10db995');
 
 /**
  * @license
