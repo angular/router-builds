6e6e8c7395ebf2b91331d12ab8cc8d9177c99040
Andrew Kushnir <akushnir@google.com>
2020-10-16 19:59:22 +0000

    d2ea9b05d8 test(language-service): Make project service a singleton (#39308)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+30.sha-a0fde29
+ * @license Angular v11.0.0-next.6+32.sha-d2ea9b0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6286,7 +6286,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+30.sha-a0fde29');
+    var VERSION = new core.Version('11.0.0-next.6+32.sha-d2ea9b0');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+30.sha-a0fde29
+ * @license Angular v11.0.0-next.6+32.sha-d2ea9b0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
