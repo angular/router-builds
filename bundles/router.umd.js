5d3618008128f3a0351a77a4b88b8252cd27d790
Andrew Kushnir <akushnir@google.com>
2020-10-16 19:59:09 +0000

    0c01c4a898 test(language-service): wrap setup() in beforeAll to speed up fit() test (#39305)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+37.sha-a38293d
+ * @license Angular v11.0.0-next.6+40.sha-0c01c4a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+37.sha-a38293d');
+    var VERSION = new i0.Version('11.0.0-next.6+40.sha-0c01c4a');
 
     /**
      * @license
