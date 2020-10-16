f9bc7232f0a0e65e0dabf9dfe2e4cf8beea1f259
Andrew Kushnir <akushnir@google.com>
2020-10-16 17:44:31 +0000

    a0fde29f80 release: cut the zone.js-0.11.2 release to fix several new issues (#39298)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+26.sha-fdbc611
+ * @license Angular v11.0.0-next.6+30.sha-a0fde29
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6286,7 +6286,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+26.sha-fdbc611');
+    var VERSION = new core.Version('11.0.0-next.6+30.sha-a0fde29');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+26.sha-fdbc611
+ * @license Angular v11.0.0-next.6+30.sha-a0fde29
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
