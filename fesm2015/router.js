90bd6f659c25a4178b67540230eb953c0a3cdf5b
Andrew Kushnir <akushnir@google.com>
2020-10-16 19:58:27 +0000

    563fb6cdbe feat(language-service): Implement go to definition for style and template urls (#39202)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+41.sha-1b21350
+ * @license Angular v11.0.0-next.6+39.sha-563fb6c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5666,7 +5666,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+41.sha-1b21350');
+const VERSION = new Version('11.0.0-next.6+39.sha-563fb6c');
 
 /**
  * @license
