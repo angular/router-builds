fd9c92b764775e0cff10529ad3335a050541b481
Andrew Kushnir <akushnir@google.com>
2020-10-16 17:29:15 +0000

    4dc8d83d84 docs: edit attribute-binding doc and move colSpan note to property binding (#38860)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+34.sha-34b74ce
+ * @license Angular v11.0.0-next.6+36.sha-4dc8d83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+34.sha-34b74ce
+ * @license Angular v11.0.0-next.6+36.sha-4dc8d83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6286,7 +6286,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+34.sha-34b74ce');
+    var VERSION = new core.Version('11.0.0-next.6+36.sha-4dc8d83');
 
     /**
      * @license
