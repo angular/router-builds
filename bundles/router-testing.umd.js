8b713acc0911ecc18f332df04e858081c10157b6
Andrew Kushnir <akushnir@google.com>
2020-10-16 20:00:53 +0000

    563fb6cdbe feat(language-service): Implement go to definition for style and template urls (#39202)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+41.sha-1b21350
+ * @license Angular v11.0.0-next.6+39.sha-563fb6c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+41.sha-1b21350
+ * @license Angular v11.0.0-next.6+39.sha-563fb6c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+41.sha-1b21350');
+    var VERSION = new i0.Version('11.0.0-next.6+39.sha-563fb6c');
 
     /**
      * @license
