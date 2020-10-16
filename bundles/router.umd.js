8a2e804cff91225b2564e747cb21709bf918e168
Andrew Kushnir <akushnir@google.com>
2020-10-16 15:42:13 +0000

    34b74cecb6 build(docs-infra): add a tool to create new examples (#39283)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+33.sha-81aa119
+ * @license Angular v11.0.0-next.6+34.sha-34b74ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6286,7 +6286,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+33.sha-81aa119');
+    var VERSION = new core.Version('11.0.0-next.6+34.sha-34b74ce');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+33.sha-81aa119
+ * @license Angular v11.0.0-next.6+34.sha-34b74ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
