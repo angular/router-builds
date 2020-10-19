dcbcbe5cb700ec179600de071e153e15e977d749
Andrew Kushnir <akushnir@google.com>
2020-10-19 19:29:05 +0000

    767fdccf59 feat(dev-infra): prompt caretaker to confirm the merge branches on merge (#39333)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+50.sha-7e742ae
+ * @license Angular v11.0.0-next.6+51.sha-767fdcc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6286,7 +6286,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+50.sha-7e742ae');
+    var VERSION = new core.Version('11.0.0-next.6+51.sha-767fdcc');
 
     /**
      * @license
