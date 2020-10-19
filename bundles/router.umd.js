f22410857a22bcd9922a8939f45d10a5e8a8134c
Andrew Kushnir <akushnir@google.com>
2020-10-19 16:18:59 +0000

    38db976617 docs: add Johannes Hoppe to GDE resources (#34694)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+22.sha-10db995
+ * @license Angular v10.1.6+24.sha-38db976
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6295,7 +6295,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.6+22.sha-10db995');
+    var VERSION = new core.Version('10.1.6+24.sha-38db976');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+22.sha-10db995
+ * @license Angular v10.1.6+24.sha-38db976
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
