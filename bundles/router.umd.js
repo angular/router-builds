fed3361b179019db9ffb16e0ed4d6e450179d984
Andrew Kushnir <akushnir@google.com>
2020-10-20 00:03:12 +0000

    5167eead45 docs: add Jessica Janiuk to contributors.json (#39253)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+42.sha-736e064
+ * @license Angular v11.0.0-next.6+43.sha-5167eea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6286,7 +6286,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+42.sha-736e064');
+    var VERSION = new core.Version('11.0.0-next.6+43.sha-5167eea');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+42.sha-736e064
+ * @license Angular v11.0.0-next.6+43.sha-5167eea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
