4deb53b6d2f0b729537bf790a2377e279b5af64f
Andrew Kushnir <akushnir@google.com>
2020-10-20 23:55:54 +0000

    cfac8e0764 build: upgrade karma to version 4.4.0 (#39180)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+60.sha-9fb7bde
+ * @license Angular v11.0.0-next.6+61.sha-cfac8e0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+60.sha-9fb7bde');
+    var VERSION = new i0.Version('11.0.0-next.6+61.sha-cfac8e0');
 
     /**
      * @license
