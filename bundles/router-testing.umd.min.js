2c3405ae997c265f1675be6b1887667ec7f26dae
Andrew Kushnir <akushnir@google.com>
2020-10-20 18:45:05 +0000

    a67895cee3 docs: Add Component Overview topic to angular.io (#39186)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+54.sha-7040f17
+ * @license Angular v11.0.0-next.6+57.sha-a67895c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+54.sha-7040f17
+ * @license Angular v11.0.0-next.6+57.sha-a67895c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6286,7 +6286,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+54.sha-7040f17');
+    var VERSION = new core.Version('11.0.0-next.6+57.sha-a67895c');
 
     /**
      * @license
