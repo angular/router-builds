bf694fa4a36ad90486627771abf3c4dd457777fe
Andrew Kushnir <akushnir@google.com>
2020-10-21 18:46:18 +0000

    3525b2fa28 docs: release notes for the v10.2.0 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+66.sha-7768aeb
+ * @license Angular v11.0.0-next.6+67.sha-3525b2f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5696,7 +5696,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+66.sha-7768aeb');
+const VERSION = new Version('11.0.0-next.6+67.sha-3525b2f');
 
 /**
  * @license
