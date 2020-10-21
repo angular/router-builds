a0026c6d4e996ec3a47945aebb92cca8d7dca419
Andrew Kushnir <akushnir@google.com>
2020-10-21 17:02:26 +0000

    d33eaa64a2 build(docs-infra): upgrade cli command docs sources to 9d53eac1b (#39359)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+64.sha-ef2d07f
+ * @license Angular v11.0.0-next.6+65.sha-d33eaa6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+64.sha-ef2d07f
+ * @license Angular v11.0.0-next.6+65.sha-d33eaa6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6286,7 +6286,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+64.sha-ef2d07f');
+    var VERSION = new core.Version('11.0.0-next.6+65.sha-d33eaa6');
 
     /**
      * @license
