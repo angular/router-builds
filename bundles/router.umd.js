1f88a243d2e418d65fb2d1747f17442699951372
Andrew Kushnir <akushnir@google.com>
2020-10-21 15:55:04 +0000

    ef2d07fdd1 docs: remove `style` commit type from CONTRIBUTING.md (#39357)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+61.sha-cfac8e0
+ * @license Angular v11.0.0-next.6+64.sha-ef2d07f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+61.sha-cfac8e0');
+    var VERSION = new i0.Version('11.0.0-next.6+64.sha-ef2d07f');
 
     /**
      * @license
