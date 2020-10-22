936272a2162c70e104a130013cb0f5448e7f995b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-22 01:56:20 +0000

    08f3d62391 refactor(core): clean up circular dependencies (#39233)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+69.sha-eb4c05d
+ * @license Angular v11.0.0-next.6+81.sha-08f3d62
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5666,7 +5666,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+69.sha-eb4c05d');
+const VERSION = new Version('11.0.0-next.6+81.sha-08f3d62');
 
 /**
  * @license
