b6b45fbd0afefb6a30b94c2f0aca6b75b30573b3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 18:16:29 +0000

    35fdc6d92f build(docs-infra): ignore `doc.basePath` when generating keywords for each document (#39409)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+100.sha-cf60e0e
+ * @license Angular v11.0.0-next.6+103.sha-35fdc6d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+100.sha-cf60e0e');
+    var VERSION = new i0.Version('11.0.0-next.6+103.sha-35fdc6d');
 
     /**
      * @license
