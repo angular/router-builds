3510db740d368c06103b415ed7dd6adf539d8f0d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 18:15:53 +0000

    308b93058d docs: fix typo in component overview (#39425)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.0+1.sha-513ed49
+ * @license Angular v10.2.0+17.sha-308b930
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6295,7 +6295,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.2.0+1.sha-513ed49');
+    var VERSION = new core.Version('10.2.0+17.sha-308b930');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.0+1.sha-513ed49
+ * @license Angular v10.2.0+17.sha-308b930
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
