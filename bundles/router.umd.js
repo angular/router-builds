9c1b9791c81b069649bc080ef12ecf090764a0fd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 18:16:05 +0000

    3236ae0ee1 docs: fix typo in component overview (#39425)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+103.sha-35fdc6d
+ * @license Angular v11.0.0-next.6+105.sha-3236ae0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6286,7 +6286,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+103.sha-35fdc6d');
+    var VERSION = new core.Version('11.0.0-next.6+105.sha-3236ae0');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+103.sha-35fdc6d
+ * @license Angular v11.0.0-next.6+105.sha-3236ae0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
