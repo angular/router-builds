0135a09d07dd6b91a36bdb01eb1acf6a76eb1b9b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 17:41:44 +0000

    90c0772743 fix(zone.js): remove global declaration (#37861)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+95.sha-b989ba2
+ * @license Angular v11.0.0-next.6+99.sha-90c0772
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5696,7 +5696,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+95.sha-b989ba2');
+const VERSION = new Version('11.0.0-next.6+99.sha-90c0772');
 
 /**
  * @license
