c9eb756031d632f7b76d29d0c3fff9e699c79c84
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 18:18:32 +0000

    3236ae0ee1 docs: fix typo in component overview (#39425)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+103.sha-35fdc6d
+ * @license Angular v11.0.0-next.6+105.sha-3236ae0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5696,7 +5696,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+103.sha-35fdc6d');
+const VERSION = new Version('11.0.0-next.6+105.sha-3236ae0');
 
 /**
  * @license
