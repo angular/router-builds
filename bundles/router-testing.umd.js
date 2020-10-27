44688bea0fa94eebc78cbd3d426a0fd824548bb7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-27 22:19:37 +0000

    e649f1dda6 ci: bust cache of stored node_modules on CircleCI (#39461)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+102.sha-2b09f5b
+ * @license Angular v11.0.0-next.6+130.sha-e649f1d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+102.sha-2b09f5b
+ * @license Angular v11.0.0-next.6+130.sha-e649f1d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6286,7 +6286,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+102.sha-2b09f5b');
+    var VERSION = new core.Version('11.0.0-next.6+130.sha-e649f1d');
 
     /**
      * @license
