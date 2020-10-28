003e91d8bbf7808387e37e29f996e1242d73ddc6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 21:44:29 +0000

    bdec818b3a docs: release notes for the v11.0.0-rc.1 release

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+146.sha-e42d3ec
+ * @license Angular v11.0.0-next.6+147.sha-bdec818
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+146.sha-e42d3ec
+ * @license Angular v11.0.0-next.6+147.sha-bdec818
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6286,7 +6286,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+146.sha-e42d3ec');
+    var VERSION = new core.Version('11.0.0-next.6+147.sha-bdec818');
 
     /**
      * @license
