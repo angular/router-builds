9a25d6d7e47c8eaec03de1660d262cc59048f9a4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:29:07 +0000

    246d50e934 build(docs-infra): upgrade cli command docs sources to bf9a92299 (#39465)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+142.sha-fdb6b98
+ * @license Angular v11.0.0-next.6+143.sha-246d50e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+142.sha-fdb6b98');
+    var VERSION = new i0.Version('11.0.0-next.6+143.sha-246d50e');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+142.sha-fdb6b98
+ * @license Angular v11.0.0-next.6+143.sha-246d50e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5696,7 +5696,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+142.sha-fdb6b98');
+const VERSION = new Version('11.0.0-next.6+143.sha-246d50e');
 
 /**
  * @license
