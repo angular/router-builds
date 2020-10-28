a9e49d88b608c57c1ba32ddf32f7d0dec1565133
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:21:21 +0000

    7c45cbc523 docs: fix links to Material examples (#39093)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+133.sha-b9358c2
+ * @license Angular v11.0.0-next.6+134.sha-7c45cbc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+133.sha-b9358c2');
+    var VERSION = new i0.Version('11.0.0-next.6+134.sha-7c45cbc');
 
     /**
      * @license
