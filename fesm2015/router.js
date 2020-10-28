a31034cef70fed00dd44ea8dc8e1510cf98c1d0f
Joey Perrott <josephperrott@gmail.com>
2020-10-28 22:08:29 +0000

    dd0ba3f4ab docs: rename `ng xi18n` to `ng extract-i18n` (#39337)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+149.sha-e3d2f46
+ * @license Angular v11.0.0-next.6+150.sha-dd0ba3f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5666,7 +5666,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+149.sha-e3d2f46');
+const VERSION = new Version('11.0.0-next.6+150.sha-dd0ba3f');
 
 /**
  * @license
