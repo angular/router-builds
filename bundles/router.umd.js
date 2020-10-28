bd57b7e1c2e354c66c4ed0e89c2c1d1403a71fd7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:24:02 +0000

    20c26cd49b build: add PullApprove: disable as a caretaker note label (#39430)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+137.sha-1508fd2
+ * @license Angular v11.0.0-next.6+140.sha-20c26cd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6286,7 +6286,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+137.sha-1508fd2');
+    var VERSION = new core.Version('11.0.0-next.6+140.sha-20c26cd');
 
     /**
      * @license
