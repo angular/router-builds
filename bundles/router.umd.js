15d840f8893c88a535a6866a7ec301d204691903
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 21:19:22 +0000

    e42d3ecfcc docs: release notes for the v10.2.1 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+145.sha-6d51ae2
+ * @license Angular v11.0.0-next.6+146.sha-e42d3ec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6286,7 +6286,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+145.sha-6d51ae2');
+    var VERSION = new core.Version('11.0.0-next.6+146.sha-e42d3ec');
 
     /**
      * @license
