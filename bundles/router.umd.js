e574ab9de86a14b32180f5a664a5ea5c4b61bddb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:27:26 +0000

    371fb9a955 refactor(compiler-cli): Track external component resources in ResourceRegistry (#39373)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+140.sha-20c26cd
+ * @license Angular v11.0.0-next.6+136.sha-371fb9a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+140.sha-20c26cd');
+    var VERSION = new i0.Version('11.0.0-next.6+136.sha-371fb9a');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+140.sha-20c26cd
+ * @license Angular v11.0.0-next.6+136.sha-371fb9a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5696,7 +5696,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+140.sha-20c26cd');
+const VERSION = new Version('11.0.0-next.6+136.sha-371fb9a');
 
 /**
  * @license
