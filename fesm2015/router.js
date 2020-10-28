9612042e509f33c4b42e54617af5cd877c1798f0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:19:13 +0000

    b9358c2ad5 docs: add carlos caballero to GDE resources (#37976)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+132.sha-e6ca3d3
+ * @license Angular v11.0.0-next.6+133.sha-b9358c2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5696,7 +5696,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+132.sha-e6ca3d3');
+const VERSION = new Version('11.0.0-next.6+133.sha-b9358c2');
 
 /**
  * @license
