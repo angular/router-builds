048255ab6cca21e476689884285db7ef01bd0ef6
Joey Perrott <josephperrott@gmail.com>
2020-10-29 17:21:55 +0000

    d6a765909b fix(dev-infra): update generated ng-dev compiled output (#39474)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+151.sha-75bfc40
+ * @license Angular v11.0.0-next.6+155.sha-d6a7659
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+151.sha-75bfc40');
+    var VERSION = new i0.Version('11.0.0-next.6+155.sha-d6a7659');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+151.sha-75bfc40
+ * @license Angular v11.0.0-next.6+155.sha-d6a7659
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5696,7 +5696,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+151.sha-75bfc40');
+const VERSION = new Version('11.0.0-next.6+155.sha-d6a7659');
 
 /**
  * @license
