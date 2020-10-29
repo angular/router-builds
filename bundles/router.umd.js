591b601f23f13edf842b3bafd9c531ca1cc66b46
Joey Perrott <josephperrott@gmail.com>
2020-10-29 23:53:55 +0000

    c83b2ad87f docs: tView.preOrderHooks and tView.preOrderCheckHooks docs update (#39497)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+163.sha-4a8d5ae
+ * @license Angular v11.0.0-next.6+165.sha-c83b2ad
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+163.sha-4a8d5ae');
+    var VERSION = new i0.Version('11.0.0-next.6+165.sha-c83b2ad');
 
     /**
      * @license
