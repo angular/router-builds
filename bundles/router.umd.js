049d30c36c064eed46c6ed44e5553b36d645a929
Joey Perrott <josephperrott@gmail.com>
2020-10-29 21:19:00 +0000

    e45da28ed5 build: update to CLI 11.0.0-rc.1 (#39432)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+10.sha-ec416d0
+ * @license Angular v11.0.0-rc.1+12.sha-e45da28
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6286,7 +6286,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-rc.1+10.sha-ec416d0');
+    var VERSION = new core.Version('11.0.0-rc.1+12.sha-e45da28');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+10.sha-ec416d0
+ * @license Angular v11.0.0-rc.1+12.sha-e45da28
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
