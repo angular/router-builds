84dca708e94ed9a42b4cf6a62ec777042a4e9530
Joey Perrott <josephperrott@gmail.com>
2020-10-29 20:09:14 +0000

    e3365724f2 feat(compiler): recover expression parsing in more malformed pipe cases (#39437)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+158.sha-8d324ec
+ * @license Angular v11.0.0-next.6+159.sha-e336572
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+158.sha-8d324ec
+ * @license Angular v11.0.0-next.6+159.sha-e336572
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+158.sha-8d324ec');
+    var VERSION = new i0.Version('11.0.0-next.6+159.sha-e336572');
 
     /**
      * @license
