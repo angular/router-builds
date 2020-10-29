36f5d20d1f2a08ba5c4ce1df948069c3d19b8ab9
Joey Perrott <josephperrott@gmail.com>
2020-10-29 16:05:05 +0000

    75bfc400cd docs: Match browser name with custom launcher name (#39480)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+150.sha-dd0ba3f
+ * @license Angular v11.0.0-next.6+151.sha-75bfc40
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6333,7 +6333,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+150.sha-dd0ba3f');
+    var VERSION = new i0.Version('11.0.0-next.6+151.sha-75bfc40');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+150.sha-dd0ba3f
+ * @license Angular v11.0.0-next.6+151.sha-75bfc40
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5696,7 +5696,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+150.sha-dd0ba3f');
+const VERSION = new Version('11.0.0-next.6+151.sha-75bfc40');
 
 /**
  * @license
