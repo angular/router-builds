1e4a26c5e446b67228345588a035a7789fe628d7
Joey Perrott <josephperrott@gmail.com>
2020-10-29 17:18:29 +0000

    cff61b2abe fix(dev-infra): update generated ng-dev compiled output (#39474)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+4.sha-9205fce
+ * @license Angular v10.2.1+8.sha-cff61b2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6295,7 +6295,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.2.1+4.sha-9205fce');
+    var VERSION = new core.Version('10.2.1+8.sha-cff61b2');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+4.sha-9205fce
+ * @license Angular v10.2.1+8.sha-cff61b2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
