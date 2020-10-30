c40a1cd46e2242ef9bf61475a1fbbacf926d9d70
Joey Perrott <josephperrott@gmail.com>
2020-10-30 18:14:59 +0000

    a8e0db73ba fix(compiler): preserve this.$event and this.$any accesses in expressions (#39323)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+16.sha-3091534
+ * @license Angular v11.0.0-rc.1+18.sha-a8e0db7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5666,7 +5666,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-rc.1+16.sha-3091534');
+const VERSION = new Version('11.0.0-rc.1+18.sha-a8e0db7');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+16.sha-3091534
+ * @license Angular v11.0.0-rc.1+18.sha-a8e0db7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
