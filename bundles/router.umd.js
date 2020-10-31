234d0d904197e24a951876e619670c058105aa3d
Joey Perrott <josephperrott@gmail.com>
2020-10-31 01:28:00 +0000

    3b0b7d2210 fix(compiler-cli): report missing pipes when `fullTemplateTypeCheck` is disabled (#39320)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+172.sha-3af585c
+ * @license Angular v11.0.0-next.6+173.sha-3b0b7d2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6341,7 +6341,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+172.sha-3af585c');
+    var VERSION = new i0.Version('11.0.0-next.6+173.sha-3b0b7d2');
 
     /**
      * @license
