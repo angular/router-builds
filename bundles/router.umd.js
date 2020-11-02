0af1d4507196f2a4b0ef4585a808e6a0358c396e
Joey Perrott <josephperrott@gmail.com>
2020-11-02 18:57:05 +0000

    eaace44d57 refactor(language-service): refactor `HybridVisitor` and expand its capabilities (#39505)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+184.sha-cf48d50
+ * @license Angular v11.0.0-next.6+186.sha-eaace44
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6341,7 +6341,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+184.sha-cf48d50');
+    var VERSION = new i0.Version('11.0.0-next.6+186.sha-eaace44');
 
     /**
      * @license
