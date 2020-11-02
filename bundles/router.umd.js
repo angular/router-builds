6dcfcc8dd3b1279e44a8f62cd3a6785d52a0456a
Joey Perrott <josephperrott@gmail.com>
2020-11-02 20:31:35 +0000

    ad62edac9c docs: update forwardRef example (#39519)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+186.sha-eaace44
+ * @license Angular v11.0.0-next.6+187.sha-ad62eda
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6341,7 +6341,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+186.sha-eaace44');
+    var VERSION = new i0.Version('11.0.0-next.6+187.sha-ad62eda');
 
     /**
      * @license
