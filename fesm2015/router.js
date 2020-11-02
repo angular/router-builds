46ab2812dd27a5b8a1b10ff579aacf6a2aa39992
Joey Perrott <josephperrott@gmail.com>
2020-11-02 22:02:41 +0000

    3cab823222 docs(bazel): Mention Alex Eagle's article on migration doc (#39507)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+188.sha-5f1e975
+ * @license Angular v11.0.0-next.6+189.sha-3cab823
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5674,7 +5674,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+188.sha-5f1e975');
+const VERSION = new Version('11.0.0-next.6+189.sha-3cab823');
 
 /**
  * @license
