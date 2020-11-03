ff1b6ebf40e5de35b1f826a62e250cc30607d98d
Joey Perrott <josephperrott@gmail.com>
2020-11-03 01:08:07 +0000

    be64f98076 fix(dev-infra): run fetches in the git client in quiet mode (#39503)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+189.sha-3cab823
+ * @license Angular v11.0.0-next.6+191.sha-be64f98
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6341,7 +6341,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+189.sha-3cab823');
+    var VERSION = new i0.Version('11.0.0-next.6+191.sha-be64f98');
 
     /**
      * @license
