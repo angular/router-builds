a5eb9a3b66f17b1b0da2c55172d0a77a59705cb0
Joey Perrott <josephperrott@gmail.com>
2020-11-03 22:13:02 +0000

    aa3bd27cbf build: update dev dependency on @angular/cli to 10.2.0 (#39553)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+15.sha-71d0063
+ * @license Angular v10.2.1+29.sha-aa3bd27
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+15.sha-71d0063
+ * @license Angular v10.2.1+29.sha-aa3bd27
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6303,7 +6303,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.2.1+15.sha-71d0063');
+    var VERSION = new core.Version('10.2.1+29.sha-aa3bd27');
 
     /**
      * @license
