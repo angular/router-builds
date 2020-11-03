b5102667f89dc319e7dde3a9a5220f103faee39d
Joey Perrott <josephperrott@gmail.com>
2020-11-03 01:06:06 +0000

    db1c48ff36 ci: only check testing directories within packages/ for fw-testing (#39536)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+191.sha-be64f98
+ * @license Angular v11.0.0-next.6+190.sha-db1c48f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6294,7 +6294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+191.sha-be64f98');
+    var VERSION = new core.Version('11.0.0-next.6+190.sha-db1c48f');
 
     /**
      * @license
