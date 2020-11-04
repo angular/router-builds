9b890783a49defb3fb31969ec27d2d9682c9b25b
Joey Perrott <josephperrott@gmail.com>
2020-11-04 21:12:35 +0000

    0aa53cdd60 docs: fix typo in the "Property Binding" guide (#39569) PR Close #39569

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+206.sha-3701376
+ * @license Angular v11.0.0-next.6+208.sha-0aa53cd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6294,7 +6294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+206.sha-3701376');
+    var VERSION = new core.Version('11.0.0-next.6+208.sha-0aa53cd');
 
     /**
      * @license
