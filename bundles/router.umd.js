ffb576af0f14d3cb8a64aedc93580d8bd8df0997
Joey Perrott <josephperrott@gmail.com>
2020-11-04 19:21:55 +0000

    43b4940c9d fix(forms): remove validators while cleaning up a control (#39234)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+42.sha-450830c
+ * @license Angular v11.0.0-rc.1+44.sha-43b4940
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6294,7 +6294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-rc.1+42.sha-450830c');
+    var VERSION = new core.Version('11.0.0-rc.1+44.sha-43b4940');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+42.sha-450830c
+ * @license Angular v11.0.0-rc.1+44.sha-43b4940
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5674,7 +5674,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-rc.1+42.sha-450830c');
+const VERSION = new Version('11.0.0-rc.1+44.sha-43b4940');
 
 /**
  * @license
