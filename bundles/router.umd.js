1ae8b384bedaad9308f716f55d76081bc3fea102
Misko Hevery <misko@hevery.com>
2020-11-05 16:23:48 +0000

    6138bc2774 build(docs-infra): upgrade cli command docs sources to 0defb1136 (#39575)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+207.sha-8c40578
+ * @license Angular v11.0.0-next.6+209.sha-6138bc2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6294,7 +6294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+207.sha-8c40578');
+    var VERSION = new core.Version('11.0.0-next.6+209.sha-6138bc2');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+207.sha-8c40578
+ * @license Angular v11.0.0-next.6+209.sha-6138bc2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5674,7 +5674,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+207.sha-8c40578');
+const VERSION = new Version('11.0.0-next.6+209.sha-6138bc2');
 
 /**
  * @license
