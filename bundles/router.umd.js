2dbe116d34a0c1ba76e511e18edf07202b079c77
Misko Hevery <misko@hevery.com>
2020-11-06 23:55:46 +0000

    0929099e41 refactor(compiler-cli): remove TypeScript 3.9 workarounds (#39586)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+223.sha-bdce769
+ * @license Angular v11.0.0-next.6+225.sha-0929099
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6341,7 +6341,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+223.sha-bdce769');
+    var VERSION = new i0.Version('11.0.0-next.6+225.sha-0929099');
 
     /**
      * @license
