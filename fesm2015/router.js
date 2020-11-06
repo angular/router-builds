cb031d623e0d626dbb7d86b46101fc8feabcd254
Misko Hevery <misko@hevery.com>
2020-11-06 17:29:25 +0000

    1f956184c4 fix(compiler): skipping leading whitespace should not break placeholder source-spans (#39486)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+210.sha-27358eb
+ * @license Angular v11.0.0-next.6+214.sha-1f95618
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5674,7 +5674,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+210.sha-27358eb');
+const VERSION = new Version('11.0.0-next.6+214.sha-1f95618');
 
 /**
  * @license
