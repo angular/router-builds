2153e0c55ce7335bf7ebbfc6d22dae338f64cf28
Misko Hevery <misko@hevery.com>
2020-11-06 17:42:03 +0000

    8e384d9f0d docs: typo fix for 'Intall' (#39585)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+214.sha-1f95618
+ * @license Angular v11.0.0-next.6+215.sha-8e384d9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6341,7 +6341,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+214.sha-1f95618');
+    var VERSION = new i0.Version('11.0.0-next.6+215.sha-8e384d9');
 
     /**
      * @license
