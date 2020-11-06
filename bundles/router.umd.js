18a4e79a64b0d077c2784bb399f56093b113e796
Misko Hevery <misko@hevery.com>
2020-11-06 17:57:20 +0000

    290ea57a93 fix(core): Access injected parent values using SelfSkip (#39464)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+217.sha-b9b9178
+ * @license Angular v11.0.0-next.6+218.sha-290ea57
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6294,7 +6294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+217.sha-b9b9178');
+    var VERSION = new core.Version('11.0.0-next.6+218.sha-290ea57');
 
     /**
      * @license
