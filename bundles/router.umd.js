a605427323927f3621d55f1e54bf5742e31875e0
Misko Hevery <misko@hevery.com>
2020-11-09 16:09:32 +0000

    48d15fc760 docs: update cli deprecations (#39547)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+229.sha-0edf6fc
+ * @license Angular v11.0.0-next.6+227.sha-48d15fc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6294,7 +6294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+229.sha-0edf6fc');
+    var VERSION = new core.Version('11.0.0-next.6+227.sha-48d15fc');
 
     /**
      * @license
