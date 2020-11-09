f2f2502f5eaba2901329e731362d3614af7aabaa
Misko Hevery <misko@hevery.com>
2020-11-09 21:34:04 +0000

    a1b319e25d docs: release notes for the v10.2.3 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+230.sha-f405aa5
+ * @license Angular v11.0.0-next.6+231.sha-a1b319e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6341,7 +6341,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+230.sha-f405aa5');
+    var VERSION = new i0.Version('11.0.0-next.6+231.sha-a1b319e');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+230.sha-f405aa5
+ * @license Angular v11.0.0-next.6+231.sha-a1b319e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
