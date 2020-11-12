65bbc7ca138ac6ff255075d2b8f617f3083230d8
atscott <atscott01@gmail.com>
2020-11-12 17:31:30 +0000

    d1355caa74 docs: added missing slash to close tag (#39626) PR Close #39626

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+235.sha-3be344c
+ * @license Angular v11.0.0-next.6+237.sha-d1355ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6341,7 +6341,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+235.sha-3be344c');
+    var VERSION = new i0.Version('11.0.0-next.6+237.sha-d1355ca');
 
     /**
      * @license
