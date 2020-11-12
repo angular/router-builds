989b3bc20237286e5120f257b8769173dfb33d09
atscott <atscott01@gmail.com>
2020-11-12 22:22:30 +0000

    8e17dc0278 fix(core): remove deprecated wtfZoneSpec from NgZone (#37864)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+243.sha-7ce8250
+ * @license Angular v11.0.0-next.6+245.sha-8e17dc0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6294,7 +6294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+243.sha-7ce8250');
+    var VERSION = new core.Version('11.0.0-next.6+245.sha-8e17dc0');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+243.sha-7ce8250
+ * @license Angular v11.0.0-next.6+245.sha-8e17dc0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
