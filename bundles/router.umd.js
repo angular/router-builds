b1c12d3d30b4793bdfd76060be69371584a1e3cf
atscott <atscott01@gmail.com>
2020-11-12 22:28:36 +0000

    ade6da95e4 perf(compiler-cli): reduce filesystem hits during resource resolution (#39604)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+245.sha-8e17dc0
+ * @license Angular v11.0.0-next.6+247.sha-ade6da9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6341,7 +6341,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+245.sha-8e17dc0');
+    var VERSION = new i0.Version('11.0.0-next.6+247.sha-ade6da9');
 
     /**
      * @license
