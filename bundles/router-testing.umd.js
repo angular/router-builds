f720edad4d21162c67baca99f769b617e0e7cb81
atscott <atscott01@gmail.com>
2020-11-12 22:20:15 +0000

    f8956adb73 build: update zone.js version to 0.11.3 (#39317)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+241.sha-a872362
+ * @license Angular v11.0.0-next.6+242.sha-f8956ad
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+241.sha-a872362
+ * @license Angular v11.0.0-next.6+242.sha-f8956ad
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6341,7 +6341,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+241.sha-a872362');
+    var VERSION = new i0.Version('11.0.0-next.6+242.sha-f8956ad');
 
     /**
      * @license
