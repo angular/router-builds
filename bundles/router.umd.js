b31e5d03c5982fef11d15d9f3fd4afea3eefa802
atscott <atscott01@gmail.com>
2020-11-12 22:17:19 +0000

    a87236292e docs: edit two-way binding doc (#38952)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+240.sha-b015d3e
+ * @license Angular v11.0.0-next.6+241.sha-a872362
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6341,7 +6341,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+240.sha-b015d3e');
+    var VERSION = new i0.Version('11.0.0-next.6+241.sha-a872362');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+240.sha-b015d3e
+ * @license Angular v11.0.0-next.6+241.sha-a872362
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5704,7 +5704,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+240.sha-b015d3e');
+const VERSION = new Version('11.0.0-next.6+241.sha-a872362');
 
 /**
  * @license
