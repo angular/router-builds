ed35c1c59aa2995fee0260bc195cd396b6c58246
atscott <atscott01@gmail.com>
2020-11-12 20:03:12 +0000

    b015d3e950 fix(core): not inserting ViewContainerRef nodes when inside root of a component (#39599)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+239.sha-1bc53eb
+ * @license Angular v11.0.0-next.6+240.sha-b015d3e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+239.sha-1bc53eb
+ * @license Angular v11.0.0-next.6+240.sha-b015d3e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6294,7 +6294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+239.sha-1bc53eb');
+    var VERSION = new core.Version('11.0.0-next.6+240.sha-b015d3e');
 
     /**
      * @license
