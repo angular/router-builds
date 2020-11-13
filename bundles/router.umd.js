587e2fa332453abe4de386dc6a228540d0f98b49
atscott <atscott01@gmail.com>
2020-11-13 19:50:07 +0000

    eea5d6b38f test(compiler-cli): convert element related compliance tests (#39617)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+258.sha-3f66a84
+ * @license Angular v11.0.0-next.6+257.sha-eea5d6b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6294,7 +6294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+258.sha-3f66a84');
+    var VERSION = new core.Version('11.0.0-next.6+257.sha-eea5d6b');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+258.sha-3f66a84
+ * @license Angular v11.0.0-next.6+257.sha-eea5d6b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5674,7 +5674,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+258.sha-3f66a84');
+const VERSION = new Version('11.0.0-next.6+257.sha-eea5d6b');
 
 /**
  * @license
