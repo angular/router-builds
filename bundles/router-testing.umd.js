c8bfe5b87703b6b76b8e645dd99b0907b26eb522
atscott <atscott01@gmail.com>
2020-11-13 19:57:12 +0000

    25d6fcacc4 docs: add doc with list of example apps currently in aio (#39295)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+251.sha-9e53c6e
+ * @license Angular v11.0.0-next.6+259.sha-25d6fca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+251.sha-9e53c6e
+ * @license Angular v11.0.0-next.6+259.sha-25d6fca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6294,7 +6294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+251.sha-9e53c6e');
+    var VERSION = new core.Version('11.0.0-next.6+259.sha-25d6fca');
 
     /**
      * @license
