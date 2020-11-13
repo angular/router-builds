76abc21f6aa6b6df390468e2dc20aefb91ac655f
atscott <atscott01@gmail.com>
2020-11-13 19:49:41 +0000

    3f66a84e01 docs: edit inputs-outputs copy and headers (#39119)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+250.sha-8a1c98c
+ * @license Angular v11.0.0-next.6+258.sha-3f66a84
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5674,7 +5674,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+250.sha-8a1c98c');
+const VERSION = new Version('11.0.0-next.6+258.sha-3f66a84');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+250.sha-8a1c98c
+ * @license Angular v11.0.0-next.6+258.sha-3f66a84
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
