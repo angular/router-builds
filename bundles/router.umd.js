7a8e82d2bcfa0d4542f49bda228c753ad998f77d
atscott <atscott01@gmail.com>
2020-11-16 17:26:18 +0000

    5e92d649f2 feat(core): add shouldCoalesceRunChangeDetection option to coalesce change detections in the same event loop. (#39422)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+262.sha-d68cac6
+ * @license Angular v11.0.0-next.6+263.sha-5e92d64
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6294,7 +6294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+262.sha-d68cac6');
+    var VERSION = new core.Version('11.0.0-next.6+263.sha-5e92d64');
 
     /**
      * @license
