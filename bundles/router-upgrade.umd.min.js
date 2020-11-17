a623a1d58de84b1561e618c0f37fee0ffc61590d
atscott <atscott01@gmail.com>
2020-11-17 21:38:21 +0000

    39266654e6 fix(http): queue jsonp <script> tag onLoad event handler in microtask (#39512)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+290.sha-49410f8
+ * @license Angular v11.0.0-next.6+289.sha-3926665
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+290.sha-49410f8
+ * @license Angular v11.0.0-next.6+289.sha-3926665
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6341,7 +6341,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+290.sha-49410f8');
+    var VERSION = new i0.Version('11.0.0-next.6+289.sha-3926665');
 
     /**
      * @license
