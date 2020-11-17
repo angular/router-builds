eceb55e9a5c550498ebef12d3a263f48278ea711
atscott <atscott01@gmail.com>
2020-11-17 21:33:27 +0000

    49410f8e93 docs: add spanish lang (#39502)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+288.sha-b6893d2
+ * @license Angular v11.0.0-next.6+290.sha-49410f8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6294,7 +6294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+288.sha-b6893d2');
+    var VERSION = new core.Version('11.0.0-next.6+290.sha-49410f8');
 
     /**
      * @license
