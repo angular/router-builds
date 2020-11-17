df9a3fd181dbb1942f7646658ead4dfd29a848fa
atscott <atscott01@gmail.com>
2020-11-17 18:38:48 +0000

    d281ea820b perf(compiler): use raw bytes to represent utf-8 encoded strings (#39694)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+280.sha-9f20942
+ * @license Angular v11.0.0-next.6+282.sha-d281ea8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6294,7 +6294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+280.sha-9f20942');
+    var VERSION = new core.Version('11.0.0-next.6+282.sha-d281ea8');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+280.sha-9f20942
+ * @license Angular v11.0.0-next.6+282.sha-d281ea8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
