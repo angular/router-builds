9c553069e55ae553e61ba64465c259e158088c72
atscott <atscott01@gmail.com>
2020-11-18 00:42:55 +0000

    00291ae054 docs: fix dead CSRF link (#35929)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+292.sha-d39c4bb
+ * @license Angular v11.0.0-next.6+293.sha-00291ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6341,7 +6341,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+292.sha-d39c4bb');
+    var VERSION = new i0.Version('11.0.0-next.6+293.sha-00291ae');
 
     /**
      * @license
