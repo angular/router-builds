c744eb549a4879a5642464e3b0cc76cc4c8a5d7f
atscott <atscott01@gmail.com>
2020-11-18 19:12:08 +0000

    79bd1bd959 docs: release notes for the v11.0.1 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+300.sha-7261e39
+ * @license Angular v11.0.0-next.6+307.sha-79bd1bd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6341,7 +6341,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+300.sha-7261e39');
+    var VERSION = new i0.Version('11.0.0-next.6+307.sha-79bd1bd');
 
     /**
      * @license
