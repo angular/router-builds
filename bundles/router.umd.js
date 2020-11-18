d6aa91a9f6287b9c6b606480c58c55193bcf5415
atscott <atscott01@gmail.com>
2020-11-18 19:32:40 +0000

    ea2312ea85 release: cut the v11.1.0-next.0 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+307.sha-79bd1bd
+ * @license Angular v11.1.0-next.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6341,7 +6341,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+307.sha-79bd1bd');
+    var VERSION = new i0.Version('11.1.0-next.0');
 
     /**
      * @license
