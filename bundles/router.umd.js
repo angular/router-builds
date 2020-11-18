5226b1132b0c18a9f358e4d87f6497bdd7cfd663
atscott <atscott01@gmail.com>
2020-11-18 17:38:00 +0000

    e4028ae5c4 build(docs-infra): compute previous version list (#39689)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+295.sha-b33b89d
+ * @license Angular v11.0.0-next.6+297.sha-e4028ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6294,7 +6294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+295.sha-b33b89d');
+    var VERSION = new core.Version('11.0.0-next.6+297.sha-e4028ae');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+295.sha-b33b89d
+ * @license Angular v11.0.0-next.6+297.sha-e4028ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5674,7 +5674,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+295.sha-b33b89d');
+const VERSION = new Version('11.0.0-next.6+297.sha-e4028ae');
 
 /**
  * @license
