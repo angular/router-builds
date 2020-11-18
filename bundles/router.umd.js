45add5b0aa1f5a09afde5e66c1b10cd4bd328d53
atscott <atscott01@gmail.com>
2020-11-18 18:46:39 +0000

    d20cab1448 docs: add links to corresponding commits in v11.0.0 breaking changes in `CHANGELOG.md` (#39704)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+298.sha-6fbe219
+ * @license Angular v11.0.0-next.6+306.sha-d20cab1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6294,7 +6294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.6+298.sha-6fbe219');
+    var VERSION = new core.Version('11.0.0-next.6+306.sha-d20cab1');
 
     /**
      * @license
