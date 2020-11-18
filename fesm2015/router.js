a53fb56da7849adb71a5acdfd68320a5e6483274
atscott <atscott01@gmail.com>
2020-11-18 17:38:18 +0000

    7d68f92315 docs(core): fix directive.ts preserving whitespace example comment (#37377)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+297.sha-e4028ae
+ * @license Angular v11.0.0-next.6+296.sha-7d68f92
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5674,7 +5674,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+297.sha-e4028ae');
+const VERSION = new Version('11.0.0-next.6+296.sha-7d68f92');
 
 /**
  * @license
