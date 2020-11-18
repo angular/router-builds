61919d0056f8b348ea95484431ebe89dbc818264
atscott <atscott01@gmail.com>
2020-11-18 17:49:46 +0000

    3b2e5be6cb refactor(core): clean up circular dependencies (#39722)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+296.sha-7d68f92
+ * @license Angular v11.0.0-next.6+299.sha-3b2e5be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+296.sha-7d68f92
+ * @license Angular v11.0.0-next.6+299.sha-3b2e5be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6341,7 +6341,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+296.sha-7d68f92');
+    var VERSION = new i0.Version('11.0.0-next.6+299.sha-3b2e5be');
 
     /**
      * @license
