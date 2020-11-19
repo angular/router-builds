e0df7b646fc907d5dae7bd18810b3820a4d17ca2
Andrew Kushnir <akushnir@google.com>
2020-11-19 19:19:32 +0000

    2fe2104738 ci: decrease payload size limit for AIO tests (#39766)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.1+3.sha-26533a0
+ * @license Angular v11.0.1+17.sha-2fe2104
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5674,7 +5674,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.1+3.sha-26533a0');
+const VERSION = new Version('11.0.1+17.sha-2fe2104');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.1+3.sha-26533a0
+ * @license Angular v11.0.1+17.sha-2fe2104
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
