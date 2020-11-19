47af71ca169421ff120a15ef11357f2fcce94bfa
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:47:21 +0000

    be998e830b refactor(core): move `injectAttributeImpl` to avoid cycles (#37085)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+31.sha-a965589
+ * @license Angular v11.1.0-next.0+33.sha-be998e8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6294,7 +6294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.0+31.sha-a965589');
+    var VERSION = new core.Version('11.1.0-next.0+33.sha-be998e8');
 
     /**
      * @license
