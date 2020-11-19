064d775c2b9f0a5532ad8c70852d62bfc04c1b68
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:45:38 +0000

    a965589eb8 feat(core): adds get method to QueryList (#36907)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+24.sha-1eb4066
+ * @license Angular v11.1.0-next.0+31.sha-a965589
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6341,7 +6341,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.0+24.sha-1eb4066');
+    var VERSION = new i0.Version('11.1.0-next.0+31.sha-a965589');
 
     /**
      * @license
