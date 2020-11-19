f5646698bef8a2a32a8184f60cad517b436e6973
Andrew Kushnir <akushnir@google.com>
2020-11-19 00:15:15 +0000

    b521b5eb39 build(docs-infra): update @angular/material to 11.0.0 (#39600)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+5.sha-dc697d3
+ * @license Angular v11.1.0-next.0+11.sha-b521b5e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+5.sha-dc697d3
+ * @license Angular v11.1.0-next.0+11.sha-b521b5e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6341,7 +6341,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.0+5.sha-dc697d3');
+    var VERSION = new i0.Version('11.1.0-next.0+11.sha-b521b5e');
 
     /**
      * @license
