2d09febddbc03ed115f32a02b66af7d456475f39
Andrew Kushnir <akushnir@google.com>
2020-11-19 00:47:11 +0000

    1951342124 docs(zone.js): update zone.js readme for the bundle format (#39508)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+14.sha-5b45fab
+ * @license Angular v11.1.0-next.0+15.sha-1951342
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6294,7 +6294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.0+14.sha-5b45fab');
+    var VERSION = new core.Version('11.1.0-next.0+15.sha-1951342');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+14.sha-5b45fab
+ * @license Angular v11.1.0-next.0+15.sha-1951342
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5674,7 +5674,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+14.sha-5b45fab');
+const VERSION = new Version('11.1.0-next.0+15.sha-1951342');
 
 /**
  * @license
