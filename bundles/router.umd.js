bfe87f2f0b59df81de38655d2d7743e80a0bedfa
Andrew Kushnir <akushnir@google.com>
2020-11-20 17:51:43 +0000

    ff0a90e4a8 fix(core): meta addTag() adds incorrect attribute for httpEquiv (#32531)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+44.sha-9aaa869
+ * @license Angular v11.1.0-next.0+46.sha-ff0a90e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.0+44.sha-9aaa869');
+    var VERSION = new core.Version('11.1.0-next.0+46.sha-ff0a90e');
 
     /**
      * @license
