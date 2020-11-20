cc3e0d58e935210322031cac6ce90bff6f6590c8
Andrew Kushnir <akushnir@google.com>
2020-11-20 17:47:42 +0000

    9aaa86967d build(language-service): include sources for Ivy LS (#39748)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+45.sha-156f9f3
+ * @license Angular v11.1.0-next.0+44.sha-9aaa869
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5677,7 +5677,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+45.sha-156f9f3');
+const VERSION = new Version('11.1.0-next.0+44.sha-9aaa869');
 
 /**
  * @license
