dd68d323d300bc056c79a6192825b4971651ac56
Andrew Kushnir <akushnir@google.com>
2020-11-20 17:45:38 +0000

    156f9f30ae docs: edit di providers doc (#39403)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+43.sha-c33a823
+ * @license Angular v11.1.0-next.0+45.sha-156f9f3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.0+43.sha-c33a823');
+    var VERSION = new core.Version('11.1.0-next.0+45.sha-156f9f3');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+43.sha-c33a823
+ * @license Angular v11.1.0-next.0+45.sha-156f9f3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
