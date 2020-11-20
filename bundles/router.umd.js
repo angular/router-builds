705323ebda8e0e8e039467f1ac1c9f8612387c1c
Andrew Kushnir <akushnir@google.com>
2020-11-20 16:53:52 +0000

    fe1ada86e8 docs: add missing return to example storage.service.ts (#31434)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+35.sha-17d5266
+ * @license Angular v11.1.0-next.0+36.sha-fe1ada8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6294,7 +6294,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.0+35.sha-17d5266');
+    var VERSION = new core.Version('11.1.0-next.0+36.sha-fe1ada8');
 
     /**
      * @license
