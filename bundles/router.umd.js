472a2d2b1c07efe9b32f09db074284b1278ff66a
Andrew Kushnir <akushnir@google.com>
2020-11-23 20:37:03 +0000

    726d7c123c ci: migrate rebase-pr CI script to the circleci directory (#39592)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+63.sha-5a49465
+ * @license Angular v11.1.0-next.0+61.sha-726d7c1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6344,7 +6344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.0+63.sha-5a49465');
+    var VERSION = new i0.Version('11.1.0-next.0+61.sha-726d7c1');
 
     /**
      * @license
