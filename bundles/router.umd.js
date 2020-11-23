9289a4d849e733f25a1c1f184a6ace6f6d2dbb0e
Andrew Kushnir <akushnir@google.com>
2020-11-23 17:43:30 +0000

    7c9b84f528 docs(http): Add mention to default value 'OK' for statusText. #23334 (#23494)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+36.sha-e32fe1a
+ * @license Angular v11.0.2+39.sha-7c9b84f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.2+36.sha-e32fe1a');
+    var VERSION = new core.Version('11.0.2+39.sha-7c9b84f');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+36.sha-e32fe1a
+ * @license Angular v11.0.2+39.sha-7c9b84f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
