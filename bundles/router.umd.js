b9a557c40e519c5e8b15bda889ecc8949c095b05
Andrew Kushnir <akushnir@google.com>
2020-11-24 23:20:38 +0000

    23c36a24ed test(docs-infra): disable the Selenium Promise Manager in docs examples e2e tests (#39818)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+84.sha-8758006
+ * @license Angular v11.1.0-next.0+89.sha-23c36a2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.0+84.sha-8758006');
+    var VERSION = new core.Version('11.1.0-next.0+89.sha-23c36a2');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+84.sha-8758006
+ * @license Angular v11.1.0-next.0+89.sha-23c36a2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
