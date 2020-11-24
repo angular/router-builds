ff63bd988a1127e699fb7277dbc1315bbacf7df6
Andrew Kushnir <akushnir@google.com>
2020-11-24 19:09:44 +0000

    a1b6ad07a8 fix(core): Allow passing AbstractType to the inject function (#37958)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+70.sha-453b32f
+ * @license Angular v11.1.0-next.0+71.sha-a1b6ad0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6344,7 +6344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.0+70.sha-453b32f');
+    var VERSION = new i0.Version('11.1.0-next.0+71.sha-a1b6ad0');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+70.sha-453b32f
+ * @license Angular v11.1.0-next.0+71.sha-a1b6ad0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5707,7 +5707,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+70.sha-453b32f');
+const VERSION = new Version('11.1.0-next.0+71.sha-a1b6ad0');
 
 /**
  * @license
