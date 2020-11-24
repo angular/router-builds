982fe292701749be063333579a063a84ebf0da5b
Andrew Kushnir <akushnir@google.com>
2020-11-24 22:46:41 +0000

    8758006acc refactor(language-service): create getReferencesAtPosition stubs (#39829)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+83.sha-3a6e7b5
+ * @license Angular v11.1.0-next.0+84.sha-8758006
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.0+83.sha-3a6e7b5');
+    var VERSION = new core.Version('11.1.0-next.0+84.sha-8758006');
 
     /**
      * @license
