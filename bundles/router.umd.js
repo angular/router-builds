fee91bb7863bebeff5f4ae27b1dba06281aaf78d
Andrew Kushnir <akushnir@google.com>
2020-11-24 21:33:46 +0000

    e75244ec00 feat(compiler-cli): support for partial compilation of components (#39707)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+72.sha-39a47c2
+ * @license Angular v11.1.0-next.0+82.sha-e75244e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6344,7 +6344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.0+72.sha-39a47c2');
+    var VERSION = new i0.Version('11.1.0-next.0+82.sha-e75244e');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+72.sha-39a47c2
+ * @license Angular v11.1.0-next.0+82.sha-e75244e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
