ea1417f4a08fbeff2eb91866e6f36fad7c1b2aeb
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 19:28:10 +0000

    2bfa8eec7a docs: make MDN link locale-indepenent (#39842)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+56.sha-9dc0a4e
+ * @license Angular v11.0.2+59.sha-2bfa8ee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.2+56.sha-9dc0a4e');
+    var VERSION = new core.Version('11.0.2+59.sha-2bfa8ee');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+56.sha-9dc0a4e
+ * @license Angular v11.0.2+59.sha-2bfa8ee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
