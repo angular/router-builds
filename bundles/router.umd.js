83439680f2ebbbbb2c4baf50d5fe118c94ce1c6d
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 22:49:01 +0000

    c43267b912 build: update peerDependencies of zone.js to 0.10~0.11 (#39809)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+101.sha-ba6945b
+ * @license Angular v11.1.0-next.0+102.sha-c43267b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6344,7 +6344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.0+101.sha-ba6945b');
+    var VERSION = new i0.Version('11.1.0-next.0+102.sha-c43267b');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+101.sha-ba6945b
+ * @license Angular v11.1.0-next.0+102.sha-c43267b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
