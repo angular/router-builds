c14bf964731d912e453c7252afaebe39593e02a1
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 19:31:53 +0000

    bf12f51b49 build(docs-infra): upgrade cli command docs sources to 70150e3f0 (#39839)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+92.sha-bfa197f
+ * @license Angular v11.1.0-next.0+93.sha-bf12f51
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5707,7 +5707,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+92.sha-bfa197f');
+const VERSION = new Version('11.1.0-next.0+93.sha-bf12f51');
 
 /**
  * @license
