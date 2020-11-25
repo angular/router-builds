54492cbd9a5aeddce8ae357a953d61def9a314a7
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 19:22:14 +0000

    5684ac5e34 feat(localize): support Application Resource Bundle (ARB) translation file format (#36795)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+89.sha-23c36a2
+ * @license Angular v11.1.0-next.0+91.sha-5684ac5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6344,7 +6344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.0+89.sha-23c36a2');
+    var VERSION = new i0.Version('11.1.0-next.0+91.sha-5684ac5');
 
     /**
      * @license
