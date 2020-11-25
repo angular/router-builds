4c5d0be56778fc5026507fd53d30157f764031d3
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 22:54:48 +0000

    e148382bd0 docs(forms): Deprecate legacy options for FormBuilder.group (#39769)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+102.sha-c43267b
+ * @license Angular v11.1.0-next.0+103.sha-e148382
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+102.sha-c43267b
+ * @license Angular v11.1.0-next.0+103.sha-e148382
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6344,7 +6344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.0+102.sha-c43267b');
+    var VERSION = new i0.Version('11.1.0-next.0+103.sha-e148382');
 
     /**
      * @license
