a031c79c9566246b06e93de91f76b08e518099c5
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 23:28:20 +0000

    1de59d2818 docs: edit step 2 of getting started start-routing.md (#39593)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+114.sha-dd3ed3c
+ * @license Angular v11.1.0-next.0+115.sha-1de59d2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5677,7 +5677,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+114.sha-dd3ed3c');
+const VERSION = new Version('11.1.0-next.0+115.sha-1de59d2');
 
 /**
  * @license
