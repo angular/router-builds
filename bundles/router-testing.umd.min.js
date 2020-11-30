05ece48330a5246d034e34afa71cbbea965e9faa
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 19:30:26 +0000

    93d689ffa8 docs: edit dependency-injection.md (#39255)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+104.sha-68d4a74
+ * @license Angular v11.1.0-next.0+105.sha-93d689f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+104.sha-68d4a74
+ * @license Angular v11.1.0-next.0+105.sha-93d689f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.0+104.sha-68d4a74');
+    var VERSION = new core.Version('11.1.0-next.0+105.sha-93d689f');
 
     /**
      * @license
