3a0bae68ff8e2023442fa45146b020eab359516c
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 19:41:04 +0000

    e2040af412 docs: fix typo in two-way-binding guide (#39859)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+107.sha-df97fbc
+ * @license Angular v11.1.0-next.0+108.sha-e2040af
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.0+107.sha-df97fbc');
+    var VERSION = new core.Version('11.1.0-next.0+108.sha-e2040af');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+107.sha-df97fbc
+ * @license Angular v11.1.0-next.0+108.sha-e2040af
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
