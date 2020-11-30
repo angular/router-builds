4d3bbb923dc9b6c99eca2b3d5f82873291307599
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 19:43:30 +0000

    e2040af412 docs: fix typo in two-way-binding guide (#39859)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+107.sha-df97fbc
+ * @license Angular v11.1.0-next.0+108.sha-e2040af
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5707,7 +5707,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+107.sha-df97fbc');
+const VERSION = new Version('11.1.0-next.0+108.sha-e2040af');
 
 /**
  * @license
