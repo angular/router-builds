bc0364408c5b0d1b620d59d94f5a21ff1ac4ca74
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 19:32:55 +0000

    93d689ffa8 docs: edit dependency-injection.md (#39255)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+104.sha-68d4a74
+ * @license Angular v11.1.0-next.0+105.sha-93d689f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5707,7 +5707,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+104.sha-68d4a74');
+const VERSION = new Version('11.1.0-next.0+105.sha-93d689f');
 
 /**
  * @license
