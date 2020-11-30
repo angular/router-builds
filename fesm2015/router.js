213b6b59f2340048f92684a0dbc1ff986d84bff1
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 20:34:07 +0000

    dd3ed3cb2b docs: fix builder section of generated angular.json file for libs (#39888)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+113.sha-71a5314
+ * @license Angular v11.1.0-next.0+114.sha-dd3ed3c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5677,7 +5677,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+113.sha-71a5314');
+const VERSION = new Version('11.1.0-next.0+114.sha-dd3ed3c');
 
 /**
  * @license
