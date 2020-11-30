81001b3f56a499d002ffe6c9106d4b1dd2d43ea0
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 20:36:33 +0000

    dd3ed3cb2b docs: fix builder section of generated angular.json file for libs (#39888)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+113.sha-71a5314
+ * @license Angular v11.1.0-next.0+114.sha-dd3ed3c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+113.sha-71a5314
+ * @license Angular v11.1.0-next.0+114.sha-dd3ed3c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6344,7 +6344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.0+113.sha-71a5314');
+    var VERSION = new i0.Version('11.1.0-next.0+114.sha-dd3ed3c');
 
     /**
      * @license
