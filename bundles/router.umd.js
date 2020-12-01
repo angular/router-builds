07ab8c5c07b9c4528b557cacc159a66c4fe1e9c4
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 20:29:30 +0000

    b40aa0a5d3 docs: edit binding-syntax doc (#38773)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+119.sha-00d56e4
+ * @license Angular v11.1.0-next.0+120.sha-b40aa0a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.0+119.sha-00d56e4');
+    var VERSION = new core.Version('11.1.0-next.0+120.sha-b40aa0a');
 
     /**
      * @license
