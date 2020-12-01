db87780a723ce7564a9db15d642344cee8b0912d
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 20:32:01 +0000

    b40aa0a5d3 docs: edit binding-syntax doc (#38773)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+119.sha-00d56e4
+ * @license Angular v11.1.0-next.0+120.sha-b40aa0a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+119.sha-00d56e4
+ * @license Angular v11.1.0-next.0+120.sha-b40aa0a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5707,7 +5707,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+119.sha-00d56e4');
+const VERSION = new Version('11.1.0-next.0+120.sha-b40aa0a');
 
 /**
  * @license
