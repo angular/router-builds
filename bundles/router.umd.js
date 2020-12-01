b0bf3838afd082cf4c2607edc3f2e78759234c14
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 23:44:52 +0000

    eac85b4e23 test(compiler-cli): migrate view compliance tests (#39914)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+125.sha-7695671
+ * @license Angular v11.1.0-next.0+126.sha-eac85b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6344,7 +6344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.0+125.sha-7695671');
+    var VERSION = new i0.Version('11.1.0-next.0+126.sha-eac85b4');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+125.sha-7695671
+ * @license Angular v11.1.0-next.0+126.sha-eac85b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
