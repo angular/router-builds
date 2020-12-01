b16526492b3c7fb253cf63f0b705e3a56875a429
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 00:48:16 +0000

    11cd37fae3 fix(core): not invoking object's toString when rendering to the DOM (#39843)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+115.sha-1de59d2
+ * @license Angular v11.1.0-next.0+116.sha-11cd37f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5707,7 +5707,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+115.sha-1de59d2');
+const VERSION = new Version('11.1.0-next.0+116.sha-11cd37f');
 
 /**
  * @license
