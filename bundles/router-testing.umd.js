51884b4ceaffbf77c5636ffb096ac681519e1b07
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 00:45:54 +0000

    11cd37fae3 fix(core): not invoking object's toString when rendering to the DOM (#39843)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+115.sha-1de59d2
+ * @license Angular v11.1.0-next.0+116.sha-11cd37f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+115.sha-1de59d2
+ * @license Angular v11.1.0-next.0+116.sha-11cd37f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.0+115.sha-1de59d2');
+    var VERSION = new core.Version('11.1.0-next.0+116.sha-11cd37f');
 
     /**
      * @license
