3e170445342e8b8dc37dddd905a09208dae0a5a4
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-02 19:39:05 +0000

    7851a254d8 docs(common): fix typo in HttpClient docs (#39904)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+127.sha-784667c
+ * @license Angular v11.1.0-next.1+3.sha-7851a25
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5677,7 +5677,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+127.sha-784667c');
+const VERSION = new Version('11.1.0-next.1+3.sha-7851a25');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+127.sha-784667c
+ * @license Angular v11.1.0-next.1+3.sha-7851a25
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
