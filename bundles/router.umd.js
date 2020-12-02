c91167ab85301180b6d9c7e58a565c6b93e400c9
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-02 19:59:45 +0000

    943ae37f99 docs: archive svg-in-templates.md (#39896)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+5.sha-b596366
+ * @license Angular v11.1.0-next.1+2.sha-943ae37
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6344,7 +6344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.1+5.sha-b596366');
+    var VERSION = new i0.Version('11.1.0-next.1+2.sha-943ae37');
 
     /**
      * @license
