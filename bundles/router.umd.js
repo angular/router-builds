a217bf6cf928bf20d8c5a50f4b239a1b121e67f6
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-02 21:05:43 +0000

    c923aaf64b docs: add wes grimes to GDE resources (#34741)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+2.sha-943ae37
+ * @license Angular v11.1.0-next.1+6.sha-c923aaf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6344,7 +6344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.1+2.sha-943ae37');
+    var VERSION = new i0.Version('11.1.0-next.1+6.sha-c923aaf');
 
     /**
      * @license
