8fed5b324a43edfaf27744378e8b415e2c62d3be
Misko Hevery <misko@hevery.com>
2020-12-02 21:20:02 +0000

    eba185edc8 test(docs-infra): reduce ambiguity in `deploy-to-firebase.js` test descriptions (#39853)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+18.sha-75fc893
+ * @license Angular v11.1.0-next.1+12.sha-eba185e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.1+18.sha-75fc893');
+    var VERSION = new core.Version('11.1.0-next.1+12.sha-eba185e');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+18.sha-75fc893
+ * @license Angular v11.1.0-next.1+12.sha-eba185e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5677,7 +5677,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+18.sha-75fc893');
+const VERSION = new Version('11.1.0-next.1+12.sha-eba185e');
 
 /**
  * @license
