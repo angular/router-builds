5d55d307f56c1b638be24c6c839a3d0dfae3206c
Misko Hevery <misko@hevery.com>
2020-12-03 15:43:50 +0000

    2b84882ab0 fix(language-service): do not return external template that does not exist (#39898)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+24.sha-efd0d33
+ * @license Angular v11.1.0-next.1+26.sha-2b84882
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.1+24.sha-efd0d33');
+    var VERSION = new core.Version('11.1.0-next.1+26.sha-2b84882');
 
     /**
      * @license
