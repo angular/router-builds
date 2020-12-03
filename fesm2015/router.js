ecb75a8ab020445a04a9594c3d89f1b2977adc93
Misko Hevery <misko@hevery.com>
2020-12-03 22:12:08 +0000

    f01c713ee2 ci: fix AIO deployment to multiple Firebase sites (#39948)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+37.sha-3b70098
+ * @license Angular v11.1.0-next.1+34.sha-f01c713
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5707,7 +5707,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+37.sha-3b70098');
+const VERSION = new Version('11.1.0-next.1+34.sha-f01c713');
 
 /**
  * @license
