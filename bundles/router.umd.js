27a9d1aae249a31543296e823b854ff348e58846
Misko Hevery <misko@hevery.com>
2020-12-07 18:14:37 +0000

    6d19c16d32 build: remove check-env script (#39980)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+51.sha-d9356f2
+ * @license Angular v11.1.0-next.1+52.sha-6d19c16
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.1+51.sha-d9356f2');
+    var VERSION = new core.Version('11.1.0-next.1+52.sha-6d19c16');
 
     /**
      * @license
