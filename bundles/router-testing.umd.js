f6547b137550d566ce6ffb709ca92a354e054bc8
Misko Hevery <misko@hevery.com>
2020-12-07 21:46:08 +0000

    6bb6dfd616 test(compiler-cli): Add test for checkTypeOfDomReferences = false (#39539)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+39.sha-6d62971
+ * @license Angular v11.0.3+41.sha-6bb6dfd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+39.sha-6d62971
+ * @license Angular v11.0.3+41.sha-6bb6dfd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.3+39.sha-6d62971');
+    var VERSION = new core.Version('11.0.3+41.sha-6bb6dfd');
 
     /**
      * @license
