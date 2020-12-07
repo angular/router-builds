b1bd66944528b58ad5ee86fb56e34ce715bf99c2
Misko Hevery <misko@hevery.com>
2020-12-07 18:17:18 +0000

    5fc45082ca fix(core): Support extending differs from root `NgModule` (#39981)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+53.sha-775394c
+ * @license Angular v11.1.0-next.1+54.sha-5fc4508
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.1+53.sha-775394c');
+    var VERSION = new core.Version('11.1.0-next.1+54.sha-5fc4508');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+53.sha-775394c
+ * @license Angular v11.1.0-next.1+54.sha-5fc4508
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5677,7 +5677,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+53.sha-775394c');
+const VERSION = new Version('11.1.0-next.1+54.sha-5fc4508');
 
 /**
  * @license
