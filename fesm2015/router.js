2124dcf04cbf3fe0da7f8b9e0dd79035bffc6cf2
Misko Hevery <misko@hevery.com>
2020-12-08 00:51:31 +0000

    b8714c384f test(compiler-cli): add source-map compliance tests for inline-templates (#39939)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+55.sha-7901eba
+ * @license Angular v11.1.0-next.1+62.sha-b8714c3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5707,7 +5707,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+55.sha-7901eba');
+const VERSION = new Version('11.1.0-next.1+62.sha-b8714c3');
 
 /**
  * @license
