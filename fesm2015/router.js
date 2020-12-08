c12927fee30f3e2c86eb354adfd8efa16e42d1fb
Misko Hevery <misko@hevery.com>
2020-12-08 17:50:47 +0000

    fbfc7dff48 build: update to use npm_package.pack rule (#39636)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+72.sha-86fdc77
+ * @license Angular v11.1.0-next.1+75.sha-fbfc7df
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5707,7 +5707,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+72.sha-86fdc77');
+const VERSION = new Version('11.1.0-next.1+75.sha-fbfc7df');
 
 /**
  * @license
