c8e069ba16354ae9fd22e66a66178b951cf2105a
Misko Hevery <misko@hevery.com>
2020-12-08 19:14:48 +0000

    a694838c41 refactor(compiler-cli): TemplateTypeChecker with checkTypeOfAttributes=false should still work (#39537)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+76.sha-5a2a907
+ * @license Angular v11.1.0-next.1+73.sha-a694838
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5677,7 +5677,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+76.sha-5a2a907');
+const VERSION = new Version('11.1.0-next.1+73.sha-a694838');
 
 /**
  * @license
