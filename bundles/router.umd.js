fac24e32330f4d1abc4f62444028a530011b927d
Misko Hevery <misko@hevery.com>
2020-12-08 17:39:06 +0000

    86fdc77ddf docs: replace image with one containing correct text (#40022)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+71.sha-2ffd739
+ * @license Angular v11.1.0-next.1+72.sha-86fdc77
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.1+71.sha-2ffd739');
+    var VERSION = new core.Version('11.1.0-next.1+72.sha-86fdc77');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+71.sha-2ffd739
+ * @license Angular v11.1.0-next.1+72.sha-86fdc77
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5677,7 +5677,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+71.sha-2ffd739');
+const VERSION = new Version('11.1.0-next.1+72.sha-86fdc77');
 
 /**
  * @license
