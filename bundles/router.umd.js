5d3caac99f4ee69e83df40cf0486a3b1e4e6c053
Misko Hevery <misko@hevery.com>
2020-12-08 20:32:23 +0000

    e69288418c refactor(compiler-cli): reformat directive/pipe metadata extraction (#39961)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+79.sha-5848439
+ * @license Angular v11.1.0-next.1+83.sha-e692884
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6344,7 +6344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.1+79.sha-5848439');
+    var VERSION = new i0.Version('11.1.0-next.1+83.sha-e692884');
 
     /**
      * @license
