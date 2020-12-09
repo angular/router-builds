3fe0aa03d6dc5c9288c64ffa17ae13c8d3f95ac9
Misko Hevery <misko@hevery.com>
2020-12-09 00:37:34 +0000

    61b9adb1b1 docs: add Pato Vargas to GDE resources (#40029)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+89.sha-55c21fa
+ * @license Angular v11.1.0-next.1+90.sha-61b9adb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6344,7 +6344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.1+89.sha-55c21fa');
+    var VERSION = new i0.Version('11.1.0-next.1+90.sha-61b9adb');
 
     /**
      * @license
