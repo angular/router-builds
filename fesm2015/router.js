0e516394dd194a5bc622a24d7cb808815045c4d0
Misko Hevery <misko@hevery.com>
2020-12-09 17:31:41 +0000

    13126d213a build(docs-infra): upgrade cli command docs sources to 416dff296 (#40034)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+99.sha-13dfef1
+ * @license Angular v11.1.0-next.1+101.sha-13126d2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5677,7 +5677,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+99.sha-13dfef1');
+const VERSION = new Version('11.1.0-next.1+101.sha-13126d2');
 
 /**
  * @license
