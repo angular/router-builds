975de483f7ea4cebeddc4c2c38337db97429f91c
Misko Hevery <misko@hevery.com>
2020-12-09 00:43:00 +0000

    634c393d35 fix(dev-infra): correct the environment variable to skip husky during merge (#40025)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+90.sha-61b9adb
+ * @license Angular v11.1.0-next.1+91.sha-634c393
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+90.sha-61b9adb
+ * @license Angular v11.1.0-next.1+91.sha-634c393
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.1+90.sha-61b9adb');
+    var VERSION = new core.Version('11.1.0-next.1+91.sha-634c393');
 
     /**
      * @license
