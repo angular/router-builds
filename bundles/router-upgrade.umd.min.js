410a5916cf28280bcd8a9b378855054ba91643e0
Misko Hevery <misko@hevery.com>
2020-12-09 17:24:56 +0000

    bb3f207f44 docs: add reason to use Angular in README (#36927)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+95.sha-cfb094d
+ * @license Angular v11.1.0-next.1+96.sha-bb3f207
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+95.sha-cfb094d
+ * @license Angular v11.1.0-next.1+96.sha-bb3f207
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5707,7 +5707,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+95.sha-cfb094d');
+const VERSION = new Version('11.1.0-next.1+96.sha-bb3f207');
 
 /**
  * @license
