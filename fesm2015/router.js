4815ea7f8481a21245a9a65867dcac2983fa1d50
Misko Hevery <misko@hevery.com>
2020-12-09 17:23:13 +0000

    1c193af65f docs: add angular-prerender to resources.json (#39387)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+96.sha-bb3f207
+ * @license Angular v11.1.0-next.1+97.sha-1c193af
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5677,7 +5677,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+96.sha-bb3f207');
+const VERSION = new Version('11.1.0-next.1+97.sha-1c193af');
 
 /**
  * @license
