cda51925df311f3e5d5e3fdf6e44c50023355997
Misko Hevery <misko@hevery.com>
2020-12-09 17:30:44 +0000

    13dfef14dc docs: remove cursor: hand (#39918)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+100.sha-e39f923
+ * @license Angular v11.1.0-next.1+99.sha-13dfef1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5677,7 +5677,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+100.sha-e39f923');
+const VERSION = new Version('11.1.0-next.1+99.sha-13dfef1');
 
 /**
  * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+100.sha-e39f923
+ * @license Angular v11.1.0-next.1+99.sha-13dfef1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
