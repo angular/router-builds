70d0518a857cf2f43b724276a22891466996825a
Misko Hevery <misko@hevery.com>
2020-12-09 17:35:41 +0000

    42d478b16a docs(common): use table layout for Pre-defined format options (#40036)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+101.sha-13126d2
+ * @license Angular v11.1.0-next.1+102.sha-42d478b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6297,7 +6297,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.1+101.sha-13126d2');
+    var VERSION = new core.Version('11.1.0-next.1+102.sha-42d478b');
 
     /**
      * @license
