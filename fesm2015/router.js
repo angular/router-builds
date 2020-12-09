b7c178786cebbc0c166cff3c9ca78374df0b0071
Misko Hevery <misko@hevery.com>
2020-12-09 00:45:10 +0000

    2f8a42036a refactor(compiler-cli): Return addEventListener symbol for native output bindings (#39312)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+91.sha-634c393
+ * @license Angular v11.1.0-next.1+92.sha-2f8a420
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5677,7 +5677,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+91.sha-634c393');
+const VERSION = new Version('11.1.0-next.1+92.sha-2f8a420');
 
 /**
  * @license
