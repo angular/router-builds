1800c80e5701349fdcae035fd70868a95145a772
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:54:58 +0000

    91fb830c86 docs: remove prefixed border-radius related properties (#40066)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+21.sha-79a42d9
+ * @license Angular v11.1.0-next.2+23.sha-91fb830
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6306,7 +6306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.2+21.sha-79a42d9');
+    var VERSION = new core.Version('11.1.0-next.2+23.sha-91fb830');
 
     /**
      * @license
