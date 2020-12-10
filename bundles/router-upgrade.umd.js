10444eb0af909d5d521ed99614aa419b0fe7313e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:13:23 +0000

    2a386415ce build(language-service): install @angular/language-service in root node_modules (#40058)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+17.sha-0817758
+ * @license Angular v11.1.0-next.2+18.sha-2a38641
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+17.sha-0817758
+ * @license Angular v11.1.0-next.2+18.sha-2a38641
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6306,7 +6306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.2+17.sha-0817758');
+    var VERSION = new core.Version('11.1.0-next.2+18.sha-2a38641');
 
     /**
      * @license
