fe45c2924635a08ac981392c01b1735cb271c7b4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 19:43:07 +0000

    dc6d40e5bc fix(compiler): handle strings inside bindings that contain binding characters (#39826)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+5.sha-93a8326
+ * @license Angular v11.1.0-next.2+6.sha-dc6d40e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5677,7 +5677,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.2+5.sha-93a8326');
+const VERSION = new Version('11.1.0-next.2+6.sha-dc6d40e');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+5.sha-93a8326
+ * @license Angular v11.1.0-next.2+6.sha-dc6d40e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
