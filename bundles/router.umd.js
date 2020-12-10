2311879339fccb66f9f0d3e3a35ed67fd9fff98f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:04:44 +0000

    76e3de253c docs: remove -webkit-box-sizing and -moz-box-sizing (#40039)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+9.sha-1f73af7
+ * @license Angular v11.1.0-next.2+10.sha-76e3de2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6355,7 +6355,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.2+9.sha-1f73af7');
+    var VERSION = new i0.Version('11.1.0-next.2+10.sha-76e3de2');
 
     /**
      * @license
