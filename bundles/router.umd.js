3ec652dfa75b0e77af14d9cff789bf6e41b86e59
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:11:43 +0000

    973f797ad5 feat(language-service): enable get references for directive and component from template (#40054)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+13.sha-b4b21bd
+ * @license Angular v11.1.0-next.2+16.sha-973f797
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6355,7 +6355,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.2+13.sha-b4b21bd');
+    var VERSION = new i0.Version('11.1.0-next.2+16.sha-973f797');
 
     /**
      * @license
