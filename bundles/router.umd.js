f26448c43e8c6e9c718a8a8dc6d036d3eace2e2c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 19:29:51 +0000

    29298f4f3d docs: fix the code sample displayed in the "reusable animations" guide (#37262)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+1.sha-d39d64c
+ * @license Angular v11.1.0-next.2+2.sha-29298f4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6344,7 +6344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.2+1.sha-d39d64c');
+    var VERSION = new i0.Version('11.1.0-next.2+2.sha-29298f4');
 
     /**
      * @license
