e04c836e2734242265cda333753989c27c74f4c5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 21:46:17 +0000

    85b07adc67 docs: add Teri Glover to contributor file (#39830)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+7.sha-112324a
+ * @license Angular v11.1.0-next.2+8.sha-85b07ad
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6355,7 +6355,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.2+7.sha-112324a');
+    var VERSION = new i0.Version('11.1.0-next.2+8.sha-85b07ad');
 
     /**
      * @license
