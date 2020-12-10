51bc0a2ef8ff703d7691e1a78e81291f837340f6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 19:36:20 +0000

    93a83266f9 feat(language-service): autocompletion within expression contexts (#39727)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+4.sha-269a775
+ * @license Angular v11.1.0-next.2+5.sha-93a8326
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6344,7 +6344,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.2+4.sha-269a775');
+    var VERSION = new i0.Version('11.1.0-next.2+5.sha-93a8326');
 
     /**
      * @license
