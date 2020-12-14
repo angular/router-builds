a6288a2e817711f1443b4fbf6a63fd215152b0e3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:17:30 +0000

    524d581dfd docs: fix typo (#40106)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+36.sha-c18a9d5
+ * @license Angular v11.1.0-next.2+38.sha-524d581
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6355,7 +6355,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.2+36.sha-c18a9d5');
+    var VERSION = new i0.Version('11.1.0-next.2+38.sha-524d581');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+36.sha-c18a9d5
+ * @license Angular v11.1.0-next.2+38.sha-524d581
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5718,7 +5718,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.2+36.sha-c18a9d5');
+const VERSION = new Version('11.1.0-next.2+38.sha-524d581');
 
 /**
  * @license
