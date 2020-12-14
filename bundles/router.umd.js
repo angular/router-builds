88bc5bd4dec6f82c485f954c01c8ec5037f95e0d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:01:07 +0000

    35a1975697 build: update to use npm_package.pack rule (#39636)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+34.sha-1e05109
+ * @license Angular v11.1.0-next.2+30.sha-35a1975
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6306,7 +6306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.2+34.sha-1e05109');
+    var VERSION = new core.Version('11.1.0-next.2+30.sha-35a1975');
 
     /**
      * @license
