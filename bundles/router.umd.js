7c7c1e56e339c59ecbaeb455b74a353ff821a468
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:01:41 +0000

    4f4f318d28 docs: remove prefixed animation related CSS properties (#40084)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+30.sha-35a1975
+ * @license Angular v11.1.0-next.2+35.sha-4f4f318
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6306,7 +6306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.2+30.sha-35a1975');
+    var VERSION = new core.Version('11.1.0-next.2+35.sha-4f4f318');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+30.sha-35a1975
+ * @license Angular v11.1.0-next.2+35.sha-4f4f318
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5686,7 +5686,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.2+30.sha-35a1975');
+const VERSION = new Version('11.1.0-next.2+35.sha-4f4f318');
 
 /**
  * @license
