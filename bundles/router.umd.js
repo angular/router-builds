0a43ee2171bed24f9910b7ba5cd4fc318cdfe725
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:36:26 +0000

    2a7443117b feat(language-service): completions for structural directives (#40032)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+38.sha-524d581
+ * @license Angular v11.1.0-next.2+46.sha-2a74431
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6306,7 +6306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.2+38.sha-524d581');
+    var VERSION = new core.Version('11.1.0-next.2+46.sha-2a74431');
 
     /**
      * @license
