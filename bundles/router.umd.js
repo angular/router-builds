036cf9fca6abbf300a42b21746b6825083a558aa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:01:01 +0000

    3b02e4e26a docs: Rename Main concepts to Understanding Angular, Built-in Features to Developer Guides (#39930)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+28.sha-028e4f7
+ * @license Angular v11.1.0-next.2+31.sha-3b02e4e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6355,7 +6355,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.2+28.sha-028e4f7');
+    var VERSION = new i0.Version('11.1.0-next.2+31.sha-3b02e4e');
 
     /**
      * @license
