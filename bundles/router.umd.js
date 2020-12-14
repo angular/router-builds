1923fad3139c3a11e9e6dcee2ea3f435db8cf09d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 19:46:18 +0000

    028e4f7f6d fix(language-service): force compileNonExportedClasses: false in LS (#40092)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+27.sha-2b74a05
+ * @license Angular v11.1.0-next.2+28.sha-028e4f7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6355,7 +6355,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.2+27.sha-2b74a05');
+    var VERSION = new i0.Version('11.1.0-next.2+28.sha-028e4f7');
 
     /**
      * @license
