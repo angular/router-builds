7facef82e53a7eebc9b58bf06b53bea11636c641
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-15 00:38:15 +0000

    973bb403a5 fix(compiler-cli): remove classes in .d.ts files from provider checks (#40118)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+46.sha-2a74431
+ * @license Angular v11.1.0-next.2+47.sha-973bb40
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6306,7 +6306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.2+46.sha-2a74431');
+    var VERSION = new core.Version('11.1.0-next.2+47.sha-973bb40');
 
     /**
      * @license
