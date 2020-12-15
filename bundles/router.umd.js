05f70246d40853ca9227516f8d0dee85564e0fe4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-15 22:09:24 +0000

    caa4666335 fix(compiler): avoid duplicate i18n blocks for i18n attrs on elements with structural directives (#40077)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+52.sha-245dccc
+ * @license Angular v11.1.0-next.2+53.sha-caa4666
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6355,7 +6355,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.2+52.sha-245dccc');
+    var VERSION = new i0.Version('11.1.0-next.2+53.sha-caa4666');
 
     /**
      * @license
