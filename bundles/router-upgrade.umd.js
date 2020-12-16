b5a78010ea49ad698567e9635cd92c0702aeca57
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-16 18:03:09 +0000

    47d9b6d72d fix(core): fix possible XSS attack in development through SSR. (#40136)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+54.sha-2e7727c
+ * @license Angular v11.1.0-next.2+55.sha-47d9b6d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+54.sha-2e7727c
+ * @license Angular v11.1.0-next.2+55.sha-47d9b6d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6355,7 +6355,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.2+54.sha-2e7727c');
+    var VERSION = new i0.Version('11.1.0-next.2+55.sha-47d9b6d');
 
     /**
      * @license
