a7cfbe6692a0695b599db628b26c19a83f7bd57c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-16 18:00:47 +0000

    47d9b6d72d fix(core): fix possible XSS attack in development through SSR. (#40136)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+54.sha-2e7727c
+ * @license Angular v11.1.0-next.2+55.sha-47d9b6d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5686,7 +5686,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.2+54.sha-2e7727c');
+const VERSION = new Version('11.1.0-next.2+55.sha-47d9b6d');
 
 /**
  * @license
