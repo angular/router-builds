336fccd4bc69ef4bb1e1ee83ea47604bf6b86711
Joey Perrott <josephperrott@gmail.com>
2020-12-17 20:17:08 +0000

    475468cc8f refactor(core): remove custom globalThis (#40123)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+4.sha-effc079
+ * @license Angular v11.1.0-next.3+5.sha-475468c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6355,7 +6355,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.3+4.sha-effc079');
+    var VERSION = new i0.Version('11.1.0-next.3+5.sha-475468c');
 
     /**
      * @license
