51d7c84d2f78f3129abd93479756413dd263cc56
Joey Perrott <josephperrott@gmail.com>
2020-12-17 18:27:11 +0000

    356a256909 refactor(docs-infra): enable tslint rules for the angular.io app (#39307)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+2.sha-c53bae8
+ * @license Angular v11.1.0-next.3+3.sha-356a256
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+2.sha-c53bae8
+ * @license Angular v11.1.0-next.3+3.sha-356a256
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6306,7 +6306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.3+2.sha-c53bae8');
+    var VERSION = new core.Version('11.1.0-next.3+3.sha-356a256');
 
     /**
      * @license
