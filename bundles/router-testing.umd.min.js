74d13749f5617c1bb57c2a27206e59fadb76fa6b
Joey Perrott <josephperrott@gmail.com>
2020-12-17 20:30:19 +0000

    362f45c4bf docs: use an explicit product id in the getting started example (#34934)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+5.sha-475468c
+ * @license Angular v11.1.0-next.3+6.sha-362f45c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+5.sha-475468c
+ * @license Angular v11.1.0-next.3+6.sha-362f45c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6355,7 +6355,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.3+5.sha-475468c');
+    var VERSION = new i0.Version('11.1.0-next.3+6.sha-362f45c');
 
     /**
      * @license
