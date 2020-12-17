1f2cbd22ca1507fc05bf8993358e9853746d5306
Joey Perrott <josephperrott@gmail.com>
2020-12-17 20:27:54 +0000

    362f45c4bf docs: use an explicit product id in the getting started example (#34934)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+5.sha-475468c
+ * @license Angular v11.1.0-next.3+6.sha-362f45c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6306,7 +6306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.3+5.sha-475468c');
+    var VERSION = new core.Version('11.1.0-next.3+6.sha-362f45c');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+5.sha-475468c
+ * @license Angular v11.1.0-next.3+6.sha-362f45c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5686,7 +5686,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.3+5.sha-475468c');
+const VERSION = new Version('11.1.0-next.3+6.sha-362f45c');
 
 /**
  * @license
