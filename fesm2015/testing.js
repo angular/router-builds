578beb92f41b50315b41bc8a1bdb76344cdc7172
Joey Perrott <josephperrott@gmail.com>
2020-12-17 00:51:10 +0000

    31448197e4 release: cut the v9.1.13 release

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.12+13.sha-9ae216c
+ * @license Angular v9.1.13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm5/router.js
+++ fesm5/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.12+13.sha-9ae216c
+ * @license Angular v9.1.13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5897,7 +5897,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.12+13.sha-9ae216c');
+var VERSION = new Version('9.1.13');
 
 /**
  * @license
