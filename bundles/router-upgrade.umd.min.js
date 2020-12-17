de495fba6af40e707ad95648b9ae01a326e213ca
Joey Perrott <josephperrott@gmail.com>
2020-12-17 18:06:35 +0000

    c53bae839d ci: fail CI jobs when rebasing on master fails (#40161)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+1.sha-ee6b8a7
+ * @license Angular v11.1.0-next.3+2.sha-c53bae8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+1.sha-ee6b8a7
+ * @license Angular v11.1.0-next.3+2.sha-c53bae8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6306,7 +6306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.3+1.sha-ee6b8a7');
+    var VERSION = new core.Version('11.1.0-next.3+2.sha-c53bae8');
 
     /**
      * @license
