4eeb28f8bee59b23f84da1d817199b139b398bcc
Joey Perrott <josephperrott@gmail.com>
2020-12-22 23:18:06 +0000

    e4fbab9ec8 fix(core): error if detectChanges is called at the wrong time under specific circumstances (#40206)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+25.sha-12cb39c
+ * @license Angular v11.1.0-next.3+27.sha-e4fbab9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6355,7 +6355,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.3+25.sha-12cb39c');
+    var VERSION = new i0.Version('11.1.0-next.3+27.sha-e4fbab9');
 
     /**
      * @license
