f4f263ab2ca757afbbde6e46df5bcc9a90ab524e
Joey Perrott <josephperrott@gmail.com>
2020-12-23 17:01:58 +0000

    53b64fb659 docs: use kebab-case for CLI commands (#40240)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+27.sha-e4fbab9
+ * @license Angular v11.1.0-next.3+29.sha-53b64fb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+27.sha-e4fbab9
+ * @license Angular v11.1.0-next.3+29.sha-53b64fb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6355,7 +6355,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.3+27.sha-e4fbab9');
+    var VERSION = new i0.Version('11.1.0-next.3+29.sha-53b64fb');
 
     /**
      * @license
