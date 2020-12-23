fa887e14f119cceae613161e7fdb7b78eb9c4499
Joey Perrott <josephperrott@gmail.com>
2020-12-23 21:32:33 +0000

    d7665fdc96 build(docs-infra): upgrade cli command docs sources to 4cefc7dc1 (#40250)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+32.sha-c22d005
+ * @license Angular v11.1.0-next.3+33.sha-d7665fd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6355,7 +6355,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.3+32.sha-c22d005');
+    var VERSION = new i0.Version('11.1.0-next.3+33.sha-d7665fd');
 
     /**
      * @license
