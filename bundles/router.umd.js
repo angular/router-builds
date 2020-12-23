1724d0768f74e8e117faebca38c2902dcffefe2c
Joey Perrott <josephperrott@gmail.com>
2020-12-23 18:14:35 +0000

    e54261b8d8 docs: update boolean and enumerated CLI section (#40224)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+29.sha-53b64fb
+ * @license Angular v11.1.0-next.3+30.sha-e54261b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6306,7 +6306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.3+29.sha-53b64fb');
+    var VERSION = new core.Version('11.1.0-next.3+30.sha-e54261b');
 
     /**
      * @license
