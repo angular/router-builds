158424c15117c350189071b8c2c4d6acb4d5bb75
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:29:30 +0000

    6abc13330b fix(compiler): don't report parse error for interpolation inside string in property binding (#40267)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+51.sha-6057753
+ * @license Angular v11.1.0-next.3+55.sha-6abc133
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5780,7 +5780,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.3+51.sha-6057753');
+const VERSION = new Version('11.1.0-next.3+55.sha-6abc133');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+51.sha-6057753
+ * @license Angular v11.1.0-next.3+55.sha-6abc133
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
