d5cb5e8691733255bace8aef494675077a007c5a
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:22:45 +0000

    8cdfd7706d docs(core): fix typo in component overview (#40269)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+52.sha-c1d7805
+ * @license Angular v11.1.0-next.3+53.sha-8cdfd77
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+52.sha-c1d7805
+ * @license Angular v11.1.0-next.3+53.sha-8cdfd77
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6411,7 +6411,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.3+52.sha-c1d7805');
+    var VERSION = new core.Version('11.1.0-next.3+53.sha-8cdfd77');
 
     /**
      * @license
