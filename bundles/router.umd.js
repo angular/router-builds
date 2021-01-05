4dcfc2cde8f47545a271028c6a08e1c872cdc078
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:24:07 +0000

    6a9d7e5969 refactor(core): express trusted constants with tagged template literals (#40082)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+53.sha-8cdfd77
+ * @license Angular v11.1.0-next.3+54.sha-6a9d7e5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6411,7 +6411,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.3+53.sha-8cdfd77');
+    var VERSION = new core.Version('11.1.0-next.3+54.sha-6a9d7e5');
 
     /**
      * @license
