fd5346c3156ce16775074667e49644784afbe953
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:25:07 +0000

    8cdfd7706d docs(core): fix typo in component overview (#40269)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+52.sha-c1d7805
+ * @license Angular v11.1.0-next.3+53.sha-8cdfd77
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6460,7 +6460,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.3+52.sha-c1d7805');
+    var VERSION = new i0.Version('11.1.0-next.3+53.sha-8cdfd77');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+52.sha-c1d7805
+ * @license Angular v11.1.0-next.3+53.sha-8cdfd77
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5812,7 +5812,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.3+52.sha-c1d7805');
+const VERSION = new Version('11.1.0-next.3+53.sha-8cdfd77');
 
 /**
  * @license
