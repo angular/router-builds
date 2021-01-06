e0b9b6578e854763f324d8f9bafac3ff1434cd07
GitHub <noreply@github.com>
2021-01-06 21:47:02 +0000

    c81d5bfb9a docs: release notes for the v11.0.6 release (#40329)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+71.sha-183fb7e
+ * @license Angular v11.1.0-next.3+72.sha-c81d5bf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6460,7 +6460,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.3+71.sha-183fb7e');
+    var VERSION = new i0.Version('11.1.0-next.3+72.sha-c81d5bf');
 
     /**
      * @license
