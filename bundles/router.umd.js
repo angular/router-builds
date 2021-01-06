6bc315a39c417e482e74d3bb963695be5b1197a7
GitHub <noreply@github.com>
2021-01-06 21:49:46 +0000

    bff48a0885 release: cut the v11.1.0-next.4 release (#40330)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+72.sha-c81d5bf
+ * @license Angular v11.1.0-next.3+73.sha-bff48a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6460,7 +6460,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.3+72.sha-c81d5bf');
+    var VERSION = new i0.Version('11.1.0-next.3+73.sha-bff48a0');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+72.sha-c81d5bf
+ * @license Angular v11.1.0-next.3+73.sha-bff48a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
