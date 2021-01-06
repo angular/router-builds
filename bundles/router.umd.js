cfcdce3234efc735c093c7698b88afa58eca5c8e
Joey Perrott <josephperrott@gmail.com>
2021-01-06 15:32:49 +0000

    d5a7a268cf ci: add jessicajaniuk to pullapprove groups (#40176)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+58.sha-0a317f3
+ * @license Angular v11.1.0-next.3+57.sha-d5a7a26
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6411,7 +6411,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.3+58.sha-0a317f3');
+    var VERSION = new core.Version('11.1.0-next.3+57.sha-d5a7a26');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+58.sha-0a317f3
+ * @license Angular v11.1.0-next.3+57.sha-d5a7a26
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
