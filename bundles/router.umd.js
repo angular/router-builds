9889f9174aef2ec69b25ad1342b2da96d58a6afd
Joey Perrott <josephperrott@gmail.com>
2021-01-06 16:56:23 +0000

    d4327d51d1 feat(compiler-cli): JIT compilation of component declarations (#40127)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+61.sha-c130812
+ * @license Angular v11.1.0-next.3+64.sha-d4327d5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6411,7 +6411,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.3+61.sha-c130812');
+    var VERSION = new core.Version('11.1.0-next.3+64.sha-d4327d5');
 
     /**
      * @license
