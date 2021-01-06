00d4d79c5e33ec4be6603d93d7a2970c26ab37bb
GitHub <noreply@github.com>
2021-01-06 21:41:00 +0000

    5b9c981b6d release: cut the v11.0.6 release (#40328)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+55.sha-b6a9717
+ * @license Angular v11.0.5+56.sha-5b9c981
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+55.sha-b6a9717
+ * @license Angular v11.0.5+56.sha-5b9c981
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6398,7 +6398,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.5+55.sha-b6a9717');
+    var VERSION = new core.Version('11.0.5+56.sha-5b9c981');
 
     /**
      * @license
