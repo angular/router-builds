855c30afbcf7e698a74140f79bb3302245fe4ba8
GitHub <noreply@github.com>
2021-01-06 21:44:26 +0000

    c81d5bfb9a docs: release notes for the v11.0.6 release (#40329)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+71.sha-183fb7e
+ * @license Angular v11.1.0-next.3+72.sha-c81d5bf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+71.sha-183fb7e
+ * @license Angular v11.1.0-next.3+72.sha-c81d5bf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5780,7 +5780,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.3+71.sha-183fb7e');
+const VERSION = new Version('11.1.0-next.3+72.sha-c81d5bf');
 
 /**
  * @license
