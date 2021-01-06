d7c8ec4f187bf61c78f5bff5be0d56f57280a351
Joey Perrott <josephperrott@gmail.com>
2021-01-06 15:57:16 +0000

    7eadf2e6da docs: correct some spellings (#40322)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+60.sha-7c16762
+ * @license Angular v11.1.0-next.3+62.sha-7eadf2e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5812,7 +5812,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.3+60.sha-7c16762');
+const VERSION = new Version('11.1.0-next.3+62.sha-7eadf2e');
 
 /**
  * @license
