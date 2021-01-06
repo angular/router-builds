79ac3b7f6fb390c5b49bf1aa337028eac239a7d1
atscott <atscott01@gmail.com>
2021-01-06 22:17:14 +0000

    989b4a94d4 refactor(compiler-cli): Return symbols for all matching outputs (#40144)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+1.sha-13020f9
+ * @license Angular v11.1.0-next.4+3.sha-989b4a9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6416,7 +6416,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+1.sha-13020f9');
+    var VERSION = new core.Version('11.1.0-next.4+3.sha-989b4a9');
 
     /**
      * @license
