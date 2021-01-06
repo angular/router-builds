a3cde39546819ae96dbc13d50ab01ae45cdf8785
Joey Perrott <josephperrott@gmail.com>
2021-01-06 17:11:33 +0000

    fdec650ef2 docs(forms): typo word "property" was missing (#40323)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+64.sha-d4327d5
+ * @license Angular v11.1.0-next.3+65.sha-fdec650
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6460,7 +6460,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.3+64.sha-d4327d5');
+    var VERSION = new i0.Version('11.1.0-next.3+65.sha-fdec650');
 
     /**
      * @license
