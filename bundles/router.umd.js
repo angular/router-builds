08fa60857ffb9ec979524303ad34fbcd9d552352
Joey Perrott <josephperrott@gmail.com>
2021-01-06 15:57:47 +0000

    c130812afe docs: fix `fullTemplateTypeCheck` compiler option description (#40253)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+62.sha-7eadf2e
+ * @license Angular v11.1.0-next.3+61.sha-c130812
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6411,7 +6411,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.3+62.sha-7eadf2e');
+    var VERSION = new core.Version('11.1.0-next.3+61.sha-c130812');
 
     /**
      * @license
