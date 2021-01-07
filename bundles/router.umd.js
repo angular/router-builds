cca7a31f12b08dea68f4dcd140e73133b2c9fdb8
atscott <atscott01@gmail.com>
2021-01-07 21:41:38 +0000

    266cc9b162 refactor(compiler-cli): support external template source-mapping when linking (#40237)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+11.sha-335c1ab
+ * @license Angular v11.1.0-next.4+21.sha-266cc9b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6417,7 +6417,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+11.sha-335c1ab');
+    var VERSION = new core.Version('11.1.0-next.4+21.sha-266cc9b');
 
     /**
      * @license
