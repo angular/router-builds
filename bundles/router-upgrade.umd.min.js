556989097eea5b7d716084fe2275ea3b3158504f
atscott <atscott01@gmail.com>
2021-01-07 21:55:49 +0000

    da6c739bb6 test(core): update test expectation to account for IE11 anonymous function name (#40342)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+30.sha-15b15be
+ * @license Angular v11.1.0-next.4+31.sha-da6c739
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+30.sha-15b15be
+ * @license Angular v11.1.0-next.4+31.sha-da6c739
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6466,7 +6466,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+30.sha-15b15be');
+    var VERSION = new i0.Version('11.1.0-next.4+31.sha-da6c739');
 
     /**
      * @license
