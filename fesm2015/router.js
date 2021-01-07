d808a14fe7cb245e48e0f488eccaf45d7b73d924
GitHub <noreply@github.com>
2021-01-07 20:52:55 +0000

    64d94e8a68 release: cut the v11.0.7 release (#40343)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.6+6.sha-6429be1
+ * @license Angular v11.0.6+7.sha-64d94e8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5770,7 +5770,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.6+6.sha-6429be1');
+const VERSION = new Version('11.0.6+7.sha-64d94e8');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.6+6.sha-6429be1
+ * @license Angular v11.0.6+7.sha-64d94e8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
