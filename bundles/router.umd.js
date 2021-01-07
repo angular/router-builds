b513fc1e033e399f026d9edf880f9994bab887e0
atscott <atscott01@gmail.com>
2021-01-07 21:34:05 +0000

    9be9e466b1 docs: remove duplicated 'the' (#40333)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+13.sha-aea265b
+ * @license Angular v11.1.0-next.4+12.sha-9be9e46
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6466,7 +6466,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+13.sha-aea265b');
+    var VERSION = new i0.Version('11.1.0-next.4+12.sha-9be9e46');
 
     /**
      * @license
