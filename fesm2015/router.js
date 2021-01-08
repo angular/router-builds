338c4cf77ffd1cd4b9bfca1117d42425908fb34d
Andrew Scott <atscott@google.com>
2021-01-08 18:15:42 +0000

    14389755e6 docs: import in-memory-web-api readme (#40203)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+38.sha-fdbd3ca
+ * @license Angular v11.1.0-next.4+41.sha-1438975
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5783,7 +5783,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+38.sha-fdbd3ca');
+const VERSION = new Version('11.1.0-next.4+41.sha-1438975');
 
 /**
  * @license
