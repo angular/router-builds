c9f56b2b6078337d2e730ff9a3cab12503c2df3b
Andrew Scott <atscott@google.com>
2021-01-08 18:12:46 +0000

    fdbd3cae8a fix(forms): handle standalone `<form>` tag correctly in `NgControlStatusGroup` directive (#40344)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+37.sha-7a819ca
+ * @license Angular v11.1.0-next.4+38.sha-fdbd3ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6466,7 +6466,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+37.sha-7a819ca');
+    var VERSION = new i0.Version('11.1.0-next.4+38.sha-fdbd3ca');
 
     /**
      * @license
