f5048795f81960b13973d37b40886e98a966224d
Andrew Scott <atscott@google.com>
2021-01-08 18:11:58 +0000

    7a819caef6 fix(dev-infra): delay checking if a closed pr has been merged by 30 seconds (#40181)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+35.sha-e27b920
+ * @license Angular v11.1.0-next.4+37.sha-7a819ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6466,7 +6466,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+35.sha-e27b920');
+    var VERSION = new i0.Version('11.1.0-next.4+37.sha-7a819ca');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+35.sha-e27b920
+ * @license Angular v11.1.0-next.4+37.sha-7a819ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
