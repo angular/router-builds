c1d62e00c92db96c7a9fa6b0fc0ac4a6283b69db
atscott <atscott01@gmail.com>
2021-01-11 18:12:58 +0000

    94ec027865 build(docs-infra): upgrade cli command docs sources to e1bcfba66 (#40373)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+48.sha-9a024d8
+ * @license Angular v11.1.0-next.4+49.sha-94ec027
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6484,7 +6484,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+48.sha-9a024d8');
+    var VERSION = new i0.Version('11.1.0-next.4+49.sha-94ec027');
 
     /**
      * @license
