2163592bee69612e9b5a70777552d2d6ecf0a03c
atscott <atscott01@gmail.com>
2021-01-11 18:25:51 +0000

    34d2988d14 docs: Fix compiler option name reference (#40362)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+53.sha-3d7e207
+ * @license Angular v11.1.0-next.4+51.sha-34d2988
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6484,7 +6484,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+53.sha-3d7e207');
+    var VERSION = new i0.Version('11.1.0-next.4+51.sha-34d2988');
 
     /**
      * @license
