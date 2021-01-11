0dcac2544087ae5bf88002889177964962b6f58a
atscott <atscott01@gmail.com>
2021-01-11 19:18:46 +0000

    f8aad11866 refactor(service-worker): remove work-around for Chrome code highlighting bug (#40234)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+57.sha-27d0e54
+ * @license Angular v11.1.0-next.4+64.sha-f8aad11
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6435,7 +6435,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+57.sha-27d0e54');
+    var VERSION = new core.Version('11.1.0-next.4+64.sha-f8aad11');
 
     /**
      * @license
