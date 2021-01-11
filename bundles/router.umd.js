dc4a04878e2ef12e99f3d8e9cca9055854be3d90
atscott <atscott01@gmail.com>
2021-01-11 18:24:16 +0000

    3d7e207a25 fix(dev-infra): detect multiple target labels as invalid (#40156)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+50.sha-1e46b73
+ * @license Angular v11.1.0-next.4+53.sha-3d7e207
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6484,7 +6484,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+50.sha-1e46b73');
+    var VERSION = new i0.Version('11.1.0-next.4+53.sha-3d7e207');
 
     /**
      * @license
