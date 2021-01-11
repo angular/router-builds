f5cba90856a8b350ff77bbb34e3c75e47fab1721
atscott <atscott01@gmail.com>
2021-01-11 23:59:33 +0000

    957332e59f docs: edit gh-pages instructions in deployment guide (#40139)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+74.sha-85af573
+ * @license Angular v11.1.0-next.4+75.sha-957332e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6419,7 +6419,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+74.sha-85af573');
+    var VERSION = new core.Version('11.1.0-next.4+75.sha-957332e');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+74.sha-85af573
+ * @license Angular v11.1.0-next.4+75.sha-957332e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
