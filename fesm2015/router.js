72bbc6a8e7b2b33c7bb53b32eff7d2b69f9b1929
atscott <atscott01@gmail.com>
2021-01-12 00:03:15 +0000

    184d0e568f docs: edit getting started start-forms.md (#39681)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+75.sha-957332e
+ * @license Angular v11.1.0-next.4+77.sha-184d0e5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5809,7 +5809,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+75.sha-957332e');
+const VERSION = new Version('11.1.0-next.4+77.sha-184d0e5');
 
 /**
  * @license
