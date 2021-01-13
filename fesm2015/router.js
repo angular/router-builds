49cf440f85013b4a20c444c7d44ab455f86ae5fd
GitHub <noreply@github.com>
2021-01-13 21:46:06 +0000

    47b3b08118 Update next branch to reflect new release-train "v11.2.0-next.0". (#40418)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+83.sha-391e0eb
+ * @license Angular v11.1.0-next.4+84.sha-47b3b08
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5777,7 +5777,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+83.sha-391e0eb');
+const VERSION = new Version('11.1.0-next.4+84.sha-47b3b08');
 
 /**
  * @license
