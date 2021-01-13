fb9f37dbc2d571b3a81c4afc44550cab2c8953b3
atscott <atscott01@gmail.com>
2021-01-13 19:23:43 +0000

    4db89f4576 fix(compiler-cli): report non-template diagnostics (#40331)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+81.sha-625d2c2
+ * @license Angular v11.1.0-next.4+82.sha-4db89f4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6468,7 +6468,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+81.sha-625d2c2');
+    var VERSION = new i0.Version('11.1.0-next.4+82.sha-4db89f4');
 
     /**
      * @license
