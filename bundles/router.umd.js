16303119f7eece82391837f29a7802de7dd01d75
Andrew Kushnir <akushnir@google.com>
2021-01-14 19:58:15 +0000

    10994ae68c docs(core): change URL to shadow dom spec (#39797)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+97.sha-b3b8509
+ * @license Angular v11.1.0-next.4+98.sha-10994ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6468,7 +6468,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+97.sha-b3b8509');
+    var VERSION = new i0.Version('11.1.0-next.4+98.sha-10994ae');
 
     /**
      * @license
