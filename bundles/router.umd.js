8e9235fa1ddd9d82ec0dd3b84b4389a3992d66ad
Andrew Kushnir <akushnir@google.com>
2021-01-16 01:10:31 +0000

    4e8198d60f feat(language-service): Implement `getRenameInfo` (#40439)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+114.sha-40e0bfd
+ * @license Angular v11.1.0-next.4+115.sha-4e8198d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6423,7 +6423,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+114.sha-40e0bfd');
+    var VERSION = new core.Version('11.1.0-next.4+115.sha-4e8198d');
 
     /**
      * @license
