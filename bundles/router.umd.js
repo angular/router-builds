c68e57f37389b04833b8dbff8ee59e65fc05190c
Andrew Kushnir <akushnir@google.com>
2021-01-19 22:42:28 +0000

    5e95153d44 docs: Return SVG topic to documentation; light edits for clarity (#40487)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+125.sha-3e97a1e
+ * @license Angular v11.1.0-next.4+126.sha-5e95153
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6423,7 +6423,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+125.sha-3e97a1e');
+    var VERSION = new core.Version('11.1.0-next.4+126.sha-5e95153');
 
     /**
      * @license
