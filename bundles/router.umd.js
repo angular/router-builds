4875d93757bdf911bed48bad03ee729616505ff6
Andrew Kushnir <akushnir@google.com>
2021-01-19 17:50:12 +0000

    2c95f57266 feat(docs-infra): use Record type instead of key-value (#40463)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+119.sha-b636406
+ * @license Angular v11.1.0-next.4+120.sha-2c95f57
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6472,7 +6472,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+119.sha-b636406');
+    var VERSION = new i0.Version('11.1.0-next.4+120.sha-2c95f57');
 
     /**
      * @license
