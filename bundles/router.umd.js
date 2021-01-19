0fa4d683c34040e3021b8048c34bb84578185c4e
Andrew Kushnir <akushnir@google.com>
2021-01-19 18:42:25 +0000

    afabb83696 refactor(core): add links to top compiler errors (#40326)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+121.sha-0568c73
+ * @license Angular v11.1.0-next.4+123.sha-afabb83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6423,7 +6423,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+121.sha-0568c73');
+    var VERSION = new core.Version('11.1.0-next.4+123.sha-afabb83');
 
     /**
      * @license
