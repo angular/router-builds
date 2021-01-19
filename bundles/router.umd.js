fa8dfd267573f33ba3ed2b7ba7eb03c8300c5c9e
Andrew Kushnir <akushnir@google.com>
2021-01-19 20:05:27 +0000

    2c5ad5cbe8 fix(forms): error if control is removed as a result of another one being reset (#40462)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-rc.0+34.sha-106df61
+ * @license Angular v11.1.0-rc.0+35.sha-2c5ad5c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6423,7 +6423,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-rc.0+34.sha-106df61');
+    var VERSION = new core.Version('11.1.0-rc.0+35.sha-2c5ad5c');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-rc.0+34.sha-106df61
+ * @license Angular v11.1.0-rc.0+35.sha-2c5ad5c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
