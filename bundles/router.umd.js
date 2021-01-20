c5c7b4d86f75f5ef9a0b9dfe51f3b69350c80a69
Andrew Kushnir <akushnir@google.com>
2021-01-20 23:37:17 +0000

    0b46f814d0 docs: release notes for the v11.1.0 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+129.sha-3cfb4b4
+ * @license Angular v11.1.0-next.4+130.sha-0b46f81
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6472,7 +6472,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+129.sha-3cfb4b4');
+    var VERSION = new i0.Version('11.1.0-next.4+130.sha-0b46f81');
 
     /**
      * @license
