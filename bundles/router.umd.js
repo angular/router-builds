4b0eea1ffad6cd5d39a81f6ae780683e91c8d989
Andrew Kushnir <akushnir@google.com>
2021-01-20 23:26:38 +0000

    59680c2644 release: cut the v11.1.0 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-rc.0+39.sha-34092a5
+ * @license Angular v11.1.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6423,7 +6423,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-rc.0+39.sha-34092a5');
+    var VERSION = new core.Version('11.1.0');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-rc.0+39.sha-34092a5
+ * @license Angular v11.1.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
