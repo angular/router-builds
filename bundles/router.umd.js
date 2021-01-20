1fbf3ec1806b907ac42abaf5dd9c2b24562c40e9
Andrew Kushnir <akushnir@google.com>
2021-01-20 17:33:57 +0000

    34092a5052 docs: add embedded videos to error guides (#40453)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-rc.0+37.sha-2731a4b
+ * @license Angular v11.1.0-rc.0+39.sha-34092a5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6423,7 +6423,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-rc.0+37.sha-2731a4b');
+    var VERSION = new core.Version('11.1.0-rc.0+39.sha-34092a5');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-rc.0+37.sha-2731a4b
+ * @license Angular v11.1.0-rc.0+39.sha-34092a5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
