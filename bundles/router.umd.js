48ee091c540a8b45bec6debae7a203a31dbd58df
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 22:32:03 +0000

    2b2a847ad7 refactor(migrations): remove rxjs usage within static queries migration (#38657)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+153.sha-dc06873
+ * @license Angular v11.1.0-next.4+151.sha-2b2a847
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6470,7 +6470,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+153.sha-dc06873');
+    var VERSION = new i0.Version('11.1.0-next.4+151.sha-2b2a847');
 
     /**
      * @license
