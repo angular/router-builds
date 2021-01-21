8ada3f266004cfdfc6a8fdb64f4a96ef2f918745
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 22:25:12 +0000

    575a2d162c feat(common): implement `appendAll()` method on `HttpParams` (#20930)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+149.sha-c9033d6
+ * @license Angular v11.1.0-next.4+150.sha-575a2d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+149.sha-c9033d6
+ * @license Angular v11.1.0-next.4+150.sha-575a2d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6470,7 +6470,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+149.sha-c9033d6');
+    var VERSION = new i0.Version('11.1.0-next.4+150.sha-575a2d1');
 
     /**
      * @license
