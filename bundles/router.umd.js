095780abef924cf4083245bdc2a8742b6ba5d637
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 01:25:10 +0000

    5bbb5ea955 refactor(language-service): do not mutate the original template node span (#40484)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+139.sha-3cf4e3c
+ * @license Angular v11.1.0-next.4+140.sha-5bbb5ea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6472,7 +6472,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+139.sha-3cf4e3c');
+    var VERSION = new i0.Version('11.1.0-next.4+140.sha-5bbb5ea');
 
     /**
      * @license
