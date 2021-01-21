c6cf10094e969465257b60c241765cd3adb22309
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 00:53:59 +0000

    315b272f9f docs: add missing type parameter to testing example (#40479)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+130.sha-0b46f81
+ * @license Angular v11.1.0-next.4+137.sha-315b272
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6472,7 +6472,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+130.sha-0b46f81');
+    var VERSION = new i0.Version('11.1.0-next.4+137.sha-315b272');
 
     /**
      * @license
