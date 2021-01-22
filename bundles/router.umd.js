62a371b9ea20916da0e6b51f394429f9cac07222
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 22:42:23 +0000

    bb3b315eee fix(core): fix possible XSS attack in development through SSR (#40525)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+157.sha-c5599cc
+ * @license Angular v11.1.0-next.4+158.sha-bb3b315
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6421,7 +6421,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+157.sha-c5599cc');
+    var VERSION = new core.Version('11.1.0-next.4+158.sha-bb3b315');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+157.sha-c5599cc
+ * @license Angular v11.1.0-next.4+158.sha-bb3b315
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
