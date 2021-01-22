e4be9c018006f08146d269f6e65791eb56bf0ba5
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 18:45:26 +0000

    1e4b51e9f7 fix(core): improve injector debug information in `ngDevMode` (#40476)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+154.sha-07b7af3
+ * @license Angular v11.1.0-next.4+155.sha-1e4b51e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6421,7 +6421,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+154.sha-07b7af3');
+    var VERSION = new core.Version('11.1.0-next.4+155.sha-1e4b51e');
 
     /**
      * @license
