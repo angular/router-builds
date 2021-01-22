778145da961be973974d5723711f7a5c6fcef244
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 18:54:21 +0000

    e25413c6da build: update to @bazel/bazelisk@1.7.3 (#40519)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+155.sha-1e4b51e
+ * @license Angular v11.1.0-next.4+156.sha-e25413c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+155.sha-1e4b51e');
+const VERSION = new Version('11.1.0-next.4+156.sha-e25413c');
 
 /**
  * @license
