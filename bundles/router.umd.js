6d472dead28ae66ba1e4f4099e0c4aa4e9c68937
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 21:56:26 +0000

    c5599ccd3e docs: fix NG0201 error description (#40500)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+156.sha-e25413c
+ * @license Angular v11.1.0-next.4+157.sha-c5599cc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6421,7 +6421,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+156.sha-e25413c');
+    var VERSION = new core.Version('11.1.0-next.4+157.sha-c5599cc');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+156.sha-e25413c
+ * @license Angular v11.1.0-next.4+157.sha-c5599cc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
