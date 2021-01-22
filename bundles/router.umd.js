4bc1dd7623372515a617d1449030eba9131e9a30
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 18:44:05 +0000

    07b7af332f fix(compiler): support multiple `:host-context()` selectors (#40494)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+151.sha-2b2a847
+ * @license Angular v11.1.0-next.4+154.sha-07b7af3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6421,7 +6421,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+151.sha-2b2a847');
+    var VERSION = new core.Version('11.1.0-next.4+154.sha-07b7af3');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+151.sha-2b2a847
+ * @license Angular v11.1.0-next.4+154.sha-07b7af3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
