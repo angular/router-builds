cdd26011b4dc10a739e21ed825ab12b7a326797b
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-23 01:02:29 +0000

    c64a56fbcc Revert "fix(core): fix possible XSS attack in development through SSR (#40525)" (#40533)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+158.sha-bb3b315
+ * @license Angular v11.1.0-next.4+159.sha-c64a56f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+158.sha-bb3b315
+ * @license Angular v11.1.0-next.4+159.sha-c64a56f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6421,7 +6421,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+158.sha-bb3b315');
+    var VERSION = new core.Version('11.1.0-next.4+159.sha-c64a56f');
 
     /**
      * @license
