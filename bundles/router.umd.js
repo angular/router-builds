9cd5b51d994af041616c9f80148e7026efb1f356
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 23:25:46 +0000

    33e0f2bd56 fix(compiler-cli): use `Map` rather than `object` for map of partial linkers (#40563)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+39.sha-2fab148
+ * @license Angular v11.1.0+41.sha-33e0f2b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6421,7 +6421,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0+39.sha-2fab148');
+    var VERSION = new core.Version('11.1.0+41.sha-33e0f2b');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+39.sha-2fab148
+ * @license Angular v11.1.0+41.sha-33e0f2b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
