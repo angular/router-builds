ed66e8fdc65d32697f3b2ebd38137b665013e9b7
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 19:06:13 +0000

    1f53301fd3 refactor(zone.js): refactor zone rollup config (#40481)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+164.sha-21bc16d
+ * @license Angular v11.1.0-next.4+165.sha-1f53301
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6421,7 +6421,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+164.sha-21bc16d');
+    var VERSION = new core.Version('11.1.0-next.4+165.sha-1f53301');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+164.sha-21bc16d
+ * @license Angular v11.1.0-next.4+165.sha-1f53301
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
