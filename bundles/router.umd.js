decac2b5c4a080f75eeef49e1c8302d4f2d59357
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 20:12:20 +0000

    c9fe455fa2 fix(forms): allow `patchValue()` method of `FormGroup` and `FormArray` classes to skip `null` values (#40534)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+171.sha-8b9f6b5
+ * @license Angular v11.1.0-next.4+173.sha-c9fe455
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6470,7 +6470,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+171.sha-8b9f6b5');
+    var VERSION = new i0.Version('11.1.0-next.4+173.sha-c9fe455');
 
     /**
      * @license
