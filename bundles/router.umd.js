4ec375f7fe56fa640acc3d4a67a66c4fe51f5674
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 18:37:47 +0000

    37710b9be3 fix(service-worker): handle error with console.error (#40236)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+26.sha-dfe4200
+ * @license Angular v11.1.0+30.sha-37710b9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6421,7 +6421,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0+26.sha-dfe4200');
+    var VERSION = new core.Version('11.1.0+30.sha-37710b9');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+26.sha-dfe4200
+ * @license Angular v11.1.0+30.sha-37710b9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
