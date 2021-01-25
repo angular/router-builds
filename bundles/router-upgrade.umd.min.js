3a7f9d3d9b53792d2a58a2c4aa2ad5260dc2b679
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 19:26:35 +0000

    56612982c7 fix(localize): include meaning in generated ARB files (#40546)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+30.sha-37710b9
+ * @license Angular v11.1.0+36.sha-5661298
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+30.sha-37710b9
+ * @license Angular v11.1.0+36.sha-5661298
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6421,7 +6421,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0+30.sha-37710b9');
+    var VERSION = new core.Version('11.1.0+36.sha-5661298');
 
     /**
      * @license
