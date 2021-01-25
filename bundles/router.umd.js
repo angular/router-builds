d373766fc5b646f4c76cb7e432c34af2f90a2bd4
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 22:46:22 +0000

    ecae75f477 feat(language-service): Add diagnostics to suggest turning on strict mode (#40423)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+173.sha-c9fe455
+ * @license Angular v11.1.0-next.4+174.sha-ecae75f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6470,7 +6470,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+173.sha-c9fe455');
+    var VERSION = new i0.Version('11.1.0-next.4+174.sha-ecae75f');
 
     /**
      * @license
