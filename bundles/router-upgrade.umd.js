16bd253f816e69ac20a9450bc3ddd4fd724beaf5
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 20:08:24 +0000

    39b3cbbdf5 build: update .bazelversion (#40579)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+181.sha-ffad040
+ * @license Angular v11.1.0-next.4+182.sha-39b3cbb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+181.sha-ffad040
+ * @license Angular v11.1.0-next.4+182.sha-39b3cbb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6470,7 +6470,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+181.sha-ffad040');
+    var VERSION = new i0.Version('11.1.0-next.4+182.sha-39b3cbb');
 
     /**
      * @license
