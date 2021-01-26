5f8fb927dd4ac816efad5df2128697e4ef0f893b
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 19:26:34 +0000

    ffad040a96 build: update to yarn v1.22.10 (#40562)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+179.sha-6bf99e0
+ * @license Angular v11.1.0-next.4+181.sha-ffad040
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+179.sha-6bf99e0
+ * @license Angular v11.1.0-next.4+181.sha-ffad040
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6421,7 +6421,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+179.sha-6bf99e0');
+    var VERSION = new core.Version('11.1.0-next.4+181.sha-ffad040');
 
     /**
      * @license
