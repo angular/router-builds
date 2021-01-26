b606e423a9d12e96a4920bec71b2379b46ca0238
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 23:35:14 +0000

    cffb00ec11 build: update bazel rules_nodejs to 2.3.3 (#40581)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+182.sha-39b3cbb
+ * @license Angular v11.1.0-next.4+183.sha-cffb00e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5811,7 +5811,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+182.sha-39b3cbb');
+const VERSION = new Version('11.1.0-next.4+183.sha-cffb00e');
 
 /**
  * @license
