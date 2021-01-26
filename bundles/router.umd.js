d0a6f5576a64b9c56461c789afe90ea11ad0da80
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 23:34:02 +0000

    95e68c4f27 build: update bazel rules_nodejs to 2.3.3 (#40581)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+48.sha-67c4ab6
+ * @license Angular v11.1.0+49.sha-95e68c4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6421,7 +6421,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0+48.sha-67c4ab6');
+    var VERSION = new core.Version('11.1.0+49.sha-95e68c4');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+48.sha-67c4ab6
+ * @license Angular v11.1.0+49.sha-95e68c4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
