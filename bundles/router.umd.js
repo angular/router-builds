2571b4c80fa7a3776a894e610e5519dbcc126326
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 19:28:55 +0000

    ffad040a96 build: update to yarn v1.22.10 (#40562)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+179.sha-6bf99e0
+ * @license Angular v11.1.0-next.4+181.sha-ffad040
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6470,7 +6470,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+179.sha-6bf99e0');
+    var VERSION = new i0.Version('11.1.0-next.4+181.sha-ffad040');
 
     /**
      * @license
