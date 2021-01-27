e204178e71a63b296fda4a0cebf9d9773999b646
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 19:10:45 +0000

    d703d21656 fix(dev-infra): fix substitution string for component_benchmark (#40591)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+186.sha-f1cf875
+ * @license Angular v11.1.0-next.4+190.sha-d703d21
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6470,7 +6470,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+186.sha-f1cf875');
+    var VERSION = new i0.Version('11.1.0-next.4+190.sha-d703d21');
 
     /**
      * @license
