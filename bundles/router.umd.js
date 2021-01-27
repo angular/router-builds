0d3add83c23c0f6908f78902f8bae8c2db7b70d3
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 00:08:51 +0000

    a8269264bf fix(core): make DefaultIterableDiffer keep the order of duplicates (#23941)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+183.sha-cffb00e
+ * @license Angular v11.1.0-next.4+184.sha-a826926
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6421,7 +6421,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+183.sha-cffb00e');
+    var VERSION = new core.Version('11.1.0-next.4+184.sha-a826926');
 
     /**
      * @license
