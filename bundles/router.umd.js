1b3555610957eb24a8f6c1e2672b2d8e92f4b179
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 21:29:39 +0000

    d4bb4a0cf1 docs: update release notes for the 11.1.1 release (#40602)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+196.sha-8772fbd
+ * @license Angular v11.1.0-next.4+197.sha-d4bb4a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6421,7 +6421,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+196.sha-8772fbd');
+    var VERSION = new core.Version('11.1.0-next.4+197.sha-d4bb4a0');
 
     /**
      * @license
