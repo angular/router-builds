301efc075e8c8cddaf7c9a676eebd3f13cdea1c7
GitHub <noreply@github.com>
2021-01-27 21:23:29 +0000

    8772fbd4bb release: bump the version to 12.0.0-next.0 (#40604)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+195.sha-e75a4f2
+ * @license Angular v11.1.0-next.4+196.sha-8772fbd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6421,7 +6421,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+195.sha-e75a4f2');
+    var VERSION = new core.Version('11.1.0-next.4+196.sha-8772fbd');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+195.sha-e75a4f2
+ * @license Angular v11.1.0-next.4+196.sha-8772fbd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
