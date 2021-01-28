4d650389666887e6fc6abd66c68d6cf809373160
Misko Hevery <misko@hevery.com>
2021-01-28 23:01:44 +0000

    e706f46a16 Revert "fix(core): remove duplicated EMPTY_ARRAY constant (#40587)"

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.1+1.sha-de12485
+ * @license Angular v11.1.1+10.sha-e706f46
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.1+1.sha-de12485
+ * @license Angular v11.1.1+10.sha-e706f46
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6421,7 +6421,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.1+1.sha-de12485');
+    var VERSION = new core.Version('11.1.1+10.sha-e706f46');
 
     /**
      * @license
