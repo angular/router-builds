27132369d848d461a011f4bd230ece85055d0c7a
Misko Hevery <misko@hevery.com>
2021-01-29 00:18:13 +0000

    22f9e454a4 fix(core): NgZone coaleascing options should trigger onStable correctly (#40540)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+208.sha-88f8ddd
+ * @license Angular v11.1.0-next.4+209.sha-22f9e45
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6470,7 +6470,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+208.sha-88f8ddd');
+    var VERSION = new i0.Version('11.1.0-next.4+209.sha-22f9e45');
 
     /**
      * @license
