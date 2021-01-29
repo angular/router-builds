ffdf5feb03454013dae8c32194900d2f3eb15306
Misko Hevery <misko@hevery.com>
2021-01-29 19:40:41 +0000

    00eeebf43f fix(compiler-cli): preserve user line endings in diagnostic template parse (#40597)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.1+14.sha-cafd4f5
+ * @license Angular v11.1.1+17.sha-00eeebf4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.1+14.sha-cafd4f5
+ * @license Angular v11.1.1+17.sha-00eeebf4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6421,7 +6421,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.1+14.sha-cafd4f5');
+    var VERSION = new core.Version('11.1.1+17.sha-00eeebf4');
 
     /**
      * @license
