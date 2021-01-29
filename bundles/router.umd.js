0cb29fbe48dad190b5bfb7840b4f535925f7ed99
Misko Hevery <misko@hevery.com>
2021-01-29 00:20:43 +0000

    028b38352a docs: add guide for NG0302 (#40580)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+211.sha-2ba62e8
+ * @license Angular v11.1.0-next.4+212.sha-028b383
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6470,7 +6470,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+211.sha-2ba62e8');
+    var VERSION = new i0.Version('11.1.0-next.4+212.sha-028b383');
 
     /**
      * @license
