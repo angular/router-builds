1e47b9a7be4c421c72a29971652e5c00d70cf979
Misko Hevery <misko@hevery.com>
2021-02-02 20:25:53 +0000

    21994ee985 refactor(docs-infra): remove unused styles for `.heading` CSS class (#40427)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0-next.0+18.sha-caafac2
+ * @license Angular v11.2.0-next.0+30.sha-21994ee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5779,7 +5779,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.0-next.0+18.sha-caafac2');
+const VERSION = new Version('11.2.0-next.0+30.sha-21994ee');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0-next.0+18.sha-caafac2
+ * @license Angular v11.2.0-next.0+30.sha-21994ee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
