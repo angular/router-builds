1d321993156f54632bea1348d12de2cb602e549a
Misko Hevery <misko@hevery.com>
2021-02-03 00:48:27 +0000

    a3b0864428 refactor(compiler-cli): remove the overrideComponentTemplate API (#40585)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+229.sha-11ca2f0
+ * @license Angular v11.1.0-next.4+231.sha-a3b0864
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6421,7 +6421,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+229.sha-11ca2f0');
+    var VERSION = new core.Version('11.1.0-next.4+231.sha-a3b0864');
 
     /**
      * @license
