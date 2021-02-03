69894653bcfede27ebf710a8d758a8d39565a72b
Misko Hevery <misko@hevery.com>
2021-02-03 19:38:42 +0000

    0ee9ea8148 refactor(language-service): pull out interfaces on package toplevel (#40621) (#40695)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.1+29.sha-20654c9
+ * @license Angular v11.1.1+37.sha-0ee9ea8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6421,7 +6421,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.1+29.sha-20654c9');
+    var VERSION = new core.Version('11.1.1+37.sha-0ee9ea8');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.1+29.sha-20654c9
+ * @license Angular v11.1.1+37.sha-0ee9ea8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
