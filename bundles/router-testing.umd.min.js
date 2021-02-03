47a86a108eb37f95bc6118b749b315c3a5111d5b
Misko Hevery <misko@hevery.com>
2021-02-03 23:28:22 +0000

    9517ae3f23 release: cut the v11.1.2 release

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.1+37.sha-0ee9ea8
+ * @license Angular v11.1.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.1+37.sha-0ee9ea8
+ * @license Angular v11.1.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6421,7 +6421,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.1+37.sha-0ee9ea8');
+    var VERSION = new core.Version('11.1.2');
 
     /**
      * @license
