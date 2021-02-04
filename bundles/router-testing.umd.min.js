ec9a670d2a947f040b27de65db7fc9d9574d0acf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-04 19:25:53 +0000

    0fc5a1690a docs: add Angular Global Summit to event pages (#40697)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.2
+ * @license Angular v11.1.2+4.sha-0fc5a16
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.2
+ * @license Angular v11.1.2+4.sha-0fc5a16
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1974,6 +1974,7 @@ export declare class Router {
     private navigations;
     private lastSuccessfulNavigation;
     private currentNavigation;
+    private disposed;
     private locationSubscription?;
     /**
      * Tracks the previously seen location change from the location subscription so we can compare
