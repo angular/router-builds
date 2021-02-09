a1a9972dff0992f4f6a68fb21634fb94d7b2dccf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:03:01 +0000

    d9e4d751f0 docs: add Taiga UI to "EXPLORE ANGULAR RESOURCES" page (#40572) PR Close #40572

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+247.sha-348d365
+ * @license Angular v11.1.0-next.4+248.sha-d9e4d75
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6427,7 +6427,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+247.sha-348d365');
+    var VERSION = new core.Version('11.1.0-next.4+248.sha-d9e4d75');
 
     /**
      * @license
