50375748976446c8d3f0d609af51b3865dcca844
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 18:57:26 +0000

    348d365666 docs(service-worker): copyedit getting started guide (#40543)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+246.sha-8fb83ea
+ * @license Angular v11.1.0-next.4+247.sha-348d365
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6476,7 +6476,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+246.sha-8fb83ea');
+    var VERSION = new i0.Version('11.1.0-next.4+247.sha-348d365');
 
     /**
      * @license
