9c55248bfafcc558941a3dbcd469665c5c8eace7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 22:14:53 +0000

    46ab85996c build(docs-infra): upgrade cli command docs sources to c61df5d30 (#40758)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+305.sha-4e4fcac
+ * @license Angular v11.1.0-next.4+307.sha-46ab859
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+305.sha-4e4fcac
+ * @license Angular v11.1.0-next.4+307.sha-46ab859
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6476,7 +6476,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+305.sha-4e4fcac');
+    var VERSION = new i0.Version('11.1.0-next.4+307.sha-46ab859');
 
     /**
      * @license
