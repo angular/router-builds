22bff8dcefb6ea330230ec8a8f84f2309fa208ed
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 23:07:20 +0000

    820d95ba40 docs: add redirect from updating-v10 to v11 (#40108)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+308.sha-f76f2eb
+ * @license Angular v11.1.0-next.4+310.sha-820d95b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6476,7 +6476,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+308.sha-f76f2eb');
+    var VERSION = new i0.Version('11.1.0-next.4+310.sha-820d95b');
 
     /**
      * @license
