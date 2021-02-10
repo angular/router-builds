fa6998bfc483e7f64d960672e23d81f7df301563
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:29:01 +0000

    378da71f27 fix(compiler-cli): don't crash when we can't resolve a resource (#40660)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+312.sha-bc5a694
+ * @license Angular v11.1.0-next.4+313.sha-378da71
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6476,7 +6476,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+312.sha-bc5a694');
+    var VERSION = new i0.Version('11.1.0-next.4+313.sha-378da71');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+312.sha-bc5a694
+ * @license Angular v11.1.0-next.4+313.sha-378da71
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5817,7 +5817,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+312.sha-bc5a694');
+const VERSION = new Version('11.1.0-next.4+313.sha-378da71');
 
 /**
  * @license
