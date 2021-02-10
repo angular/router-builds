fcddab214c5f0e6719759d647ee08d2b63516005
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:12:52 +0000

    bc5a694b82 release: update 11.2.0-rc.0 release notes (#40706)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+309.sha-61a0b6d
+ * @license Angular v11.1.0-next.4+312.sha-bc5a694
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5817,7 +5817,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+309.sha-61a0b6d');
+const VERSION = new Version('11.1.0-next.4+312.sha-bc5a694');
 
 /**
  * @license
