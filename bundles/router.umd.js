4c6dfc34a88db1268a5ed7435c67fbcb9728fc84
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:36:59 +0000

    03f0b157c1 fix(common): date is not correctly formatted when year is between 0 and 99 (#40448)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+313.sha-378da71
+ * @license Angular v11.1.0-next.4+316.sha-03f0b15
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6427,7 +6427,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+313.sha-378da71');
+    var VERSION = new core.Version('11.1.0-next.4+316.sha-03f0b15');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+313.sha-378da71
+ * @license Angular v11.1.0-next.4+316.sha-03f0b15
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
