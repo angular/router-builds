b063dfa4aaa2daa0c17765b054726244fb3ed4c9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:39:32 +0000

    bbf61fc2be fix(compiler): include parenthesis in expression source spans (#40740)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+316.sha-03f0b15
+ * @license Angular v11.1.0-next.4+320.sha-bbf61fc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5817,7 +5817,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+316.sha-03f0b15');
+const VERSION = new Version('11.1.0-next.4+320.sha-bbf61fc');
 
 /**
  * @license
