c4ac8ee8efca43c7adff1bb3d96a5613ac7a751c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:10:40 +0000

    bc5a694b82 release: update 11.2.0-rc.0 release notes (#40706)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+309.sha-61a0b6d
+ * @license Angular v11.1.0-next.4+312.sha-bc5a694
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6427,7 +6427,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+309.sha-61a0b6d');
+    var VERSION = new core.Version('11.1.0-next.4+312.sha-bc5a694');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+309.sha-61a0b6d
+ * @license Angular v11.1.0-next.4+312.sha-bc5a694
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
