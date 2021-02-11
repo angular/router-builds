202c9ec256f32b9f8505d049e8c07220a53165f6
Joey Perrott <josephperrott@gmail.com>
2021-02-11 21:03:11 +0000

    e27787a583 build: remove jpm dependency (#40807)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+12.sha-fcf00de
+ * @license Angular v12.0.0-next.0+13.sha-e27787a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6476,7 +6476,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.0+12.sha-fcf00de');
+    var VERSION = new i0.Version('12.0.0-next.0+13.sha-e27787a');
 
     /**
      * @license
