14066f01642987500d3a38e5f6e29c4b2f6f8c60
Joey Perrott <josephperrott@gmail.com>
2021-02-11 21:51:00 +0000

    5eb195416b fix(compiler-cli): extend `angularCompilerOptions` in tsconfig from node (#40694)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+16.sha-dfc9f36
+ * @license Angular v12.0.0-next.0+19.sha-5eb1954
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5785,7 +5785,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.0+16.sha-dfc9f36');
+const VERSION = new Version('12.0.0-next.0+19.sha-5eb1954');
 
 /**
  * @license
