369b442c6e8942c0fd220081e52a303e8f994d2f
Joey Perrott <josephperrott@gmail.com>
2021-02-11 21:52:46 +0000

    5eb195416b fix(compiler-cli): extend `angularCompilerOptions` in tsconfig from node (#40694)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+16.sha-dfc9f36
+ * @license Angular v12.0.0-next.0+19.sha-5eb1954
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6476,7 +6476,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.0+16.sha-dfc9f36');
+    var VERSION = new i0.Version('12.0.0-next.0+19.sha-5eb1954');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+16.sha-dfc9f36
+ * @license Angular v12.0.0-next.0+19.sha-5eb1954
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
