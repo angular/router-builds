a342f724d03f91c3b79ee7fbe8412f0501807034
Joey Perrott <josephperrott@gmail.com>
2021-02-11 17:21:01 +0000

    7ca244a38b docs(core): add missing dot at the end of the sentence (#40800)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+4.sha-68da05d
+ * @license Angular v12.0.0-next.0+6.sha-7ca244a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+4.sha-68da05d
+ * @license Angular v12.0.0-next.0+6.sha-7ca244a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6427,7 +6427,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.0+4.sha-68da05d');
+    var VERSION = new core.Version('12.0.0-next.0+6.sha-7ca244a');
 
     /**
      * @license
