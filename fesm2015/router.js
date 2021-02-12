9d168809f1d4ef1ceff9a4eaa4e6954087c14684
Joey Perrott <josephperrott@gmail.com>
2021-02-12 17:13:14 +0000

    43ecf8a77b feat(platform-server): allow shimming the global env sooner (#40559)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+20.sha-80f4ff3
+ * @license Angular v12.0.0-next.0+24.sha-43ecf8a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5785,7 +5785,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.0+20.sha-80f4ff3');
+const VERSION = new Version('12.0.0-next.0+24.sha-43ecf8a');
 
 /**
  * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+20.sha-80f4ff3
+ * @license Angular v12.0.0-next.0+24.sha-43ecf8a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
