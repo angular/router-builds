cf4f7b7ed7f8f03a4b95e788ce13746637c4eb8a
Joey Perrott <josephperrott@gmail.com>
2021-02-12 17:20:54 +0000

    8225bb5b57 refactor(compiler-cli): implement `ɵɵngDeclarePipe()` (#40803)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0+18.sha-9482a4d
+ * @license Angular v11.2.0+20.sha-8225bb5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5785,7 +5785,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.0+18.sha-9482a4d');
+const VERSION = new Version('11.2.0+20.sha-8225bb5');
 
 /**
  * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0+18.sha-9482a4d
+ * @license Angular v11.2.0+20.sha-8225bb5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
