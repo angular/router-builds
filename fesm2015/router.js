8b1d85219275f572f807f4bf4c29e08c08bb95f5
Joey Perrott <josephperrott@gmail.com>
2021-02-12 17:20:40 +0000

    6425a6d543 docs: typo fix (#40804) PR Close #40804

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+25.sha-9cdf654
+ * @license Angular v12.0.0-next.0+26.sha-6425a6d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5817,7 +5817,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.0+25.sha-9cdf654');
+const VERSION = new Version('12.0.0-next.0+26.sha-6425a6d');
 
 /**
  * @license
