3ade7b6dcbccf272826e97d09081343d74f626a0
Joey Perrott <josephperrott@gmail.com>
2021-02-13 01:07:36 +0000

    9661873df1 docs: add ng-conf 2021 (#40783)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+32.sha-bbbfa3e
+ * @license Angular v12.0.0-next.0+34.sha-9661873
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5785,7 +5785,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.0+32.sha-bbbfa3e');
+const VERSION = new Version('12.0.0-next.0+34.sha-9661873');
 
 /**
  * @license
