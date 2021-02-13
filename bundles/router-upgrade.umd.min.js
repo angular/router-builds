c7cd14f425b32ac4eddc5a2b3be8afecedd7264e
Joey Perrott <josephperrott@gmail.com>
2021-02-13 01:08:54 +0000

    9661873df1 docs: add ng-conf 2021 (#40783)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+32.sha-bbbfa3e
+ * @license Angular v12.0.0-next.0+34.sha-9661873
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+32.sha-bbbfa3e
+ * @license Angular v12.0.0-next.0+34.sha-9661873
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6476,7 +6476,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.0+32.sha-bbbfa3e');
+    var VERSION = new i0.Version('12.0.0-next.0+34.sha-9661873');
 
     /**
      * @license
