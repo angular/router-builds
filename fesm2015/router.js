5d4a3a99bf1df3f624eed10e0e75db0ef456d77a
Joey Perrott <josephperrott@gmail.com>
2021-02-16 16:03:08 +0000

    67d85b3680 docs: capitalize Angular word in upgrade-setup.md file (#40862) PR Close #40862

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+38.sha-5f0c219
+ * @license Angular v12.0.0-next.0+37.sha-67d85b3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5817,7 +5817,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.0+38.sha-5f0c219');
+const VERSION = new Version('12.0.0-next.0+37.sha-67d85b3');
 
 /**
  * @license
