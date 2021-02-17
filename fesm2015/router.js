992d1fcd1725ef2647cc06a24156eb6cc92b7e14
GitHub <noreply@github.com>
2021-02-17 19:18:58 +0000

    b31098d5e9 release: cut the v12.0.0-next.1 release (#40889)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+77.sha-a9936c2
+ * @license Angular v12.0.0-next.1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5798,7 +5798,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.0+77.sha-a9936c2');
+const VERSION = new Version('12.0.0-next.1');
 
 /**
  * @license
