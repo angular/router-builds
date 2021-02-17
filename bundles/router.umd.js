82d4807acff2b3cc055ed5619c9b3875398f2c95
Joey Perrott <josephperrott@gmail.com>
2021-02-17 19:52:17 +0000

    8658cd59b2 docs: add what is angular topic (#40811)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1
+ * @license Angular v12.0.0-next.1+1.sha-8658cd5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6489,7 +6489,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.1');
+    var VERSION = new i0.Version('12.0.0-next.1+1.sha-8658cd5');
 
     /**
      * @license
