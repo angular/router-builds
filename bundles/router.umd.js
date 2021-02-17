6e237c6f57b3e30863c90cb2fbc597569cc5fd73
Joey Perrott <josephperrott@gmail.com>
2021-02-17 15:21:46 +0000

    0807d13201 docs: remove the extra backtick character (#40873)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+76.sha-9b26f46
+ * @license Angular v12.0.0-next.0+75.sha-0807d13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6440,7 +6440,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.0+76.sha-9b26f46');
+    var VERSION = new core.Version('12.0.0-next.0+75.sha-0807d13');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+76.sha-9b26f46
+ * @license Angular v12.0.0-next.0+75.sha-0807d13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
