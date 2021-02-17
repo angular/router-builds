d3b9e662f32714261dc8d0774a71ecc6a358ccc6
atscott <atscott01@gmail.com>
2021-02-17 20:27:58 +0000

    b7a2d0da20 perf(core): use `ngDevMode` to tree-shake warning (#40876)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+3.sha-cdf1ea1
+ * @license Angular v12.0.0-next.1+2.sha-b7a2d0d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6489,7 +6489,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.1+3.sha-cdf1ea1');
+    var VERSION = new i0.Version('12.0.0-next.1+2.sha-b7a2d0d');
 
     /**
      * @license
