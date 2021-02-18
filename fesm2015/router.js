bb6d4738869766151e339f91f9dbd024e156976c
Zach Arend <zachzach@google.com>
2021-02-18 01:26:53 +0000

    65baac6055 docs: update attribute-binding.md to correctly describe [attr.binding] rules (#40878)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+8.sha-2375b85
+ * @license Angular v12.0.0-next.1+9.sha-65baac6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5830,7 +5830,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+8.sha-2375b85');
+const VERSION = new Version('12.0.0-next.1+9.sha-65baac6');
 
 /**
  * @license
