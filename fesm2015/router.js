3a8239c9bb0d77a6c7321e5f645796bca258ab98
atscott <atscott01@gmail.com>
2021-02-19 17:26:25 +0000

    aed2782c4a docs: the token should be associated with LibHeaderComponent (#40922)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+11.sha-30d5e25
+ * @license Angular v12.0.0-next.1+12.sha-aed2782
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5798,7 +5798,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+11.sha-30d5e25');
+const VERSION = new Version('12.0.0-next.1+12.sha-aed2782');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+11.sha-30d5e25
+ * @license Angular v12.0.0-next.1+12.sha-aed2782
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
