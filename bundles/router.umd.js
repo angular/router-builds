3e05ec1a9c92c738748c35a43fb3824ecb8ec97a
atscott <atscott01@gmail.com>
2021-02-19 22:57:30 +0000

    d1d1dadb41 refactor(core): use RuntimeError to throw provider not found error (#40901)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+34.sha-f340a5b
+ * @license Angular v12.0.0-next.1+35.sha-d1d1dad
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6493,7 +6493,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.1+34.sha-f340a5b');
+    var VERSION = new i0.Version('12.0.0-next.1+35.sha-d1d1dad');
 
     /**
      * @license
