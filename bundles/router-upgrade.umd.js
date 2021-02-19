65f39caa5fce33508f7e3a68ba82be354d3f782e
atscott <atscott01@gmail.com>
2021-02-19 17:40:11 +0000

    a82fddf1ce feat(router): Allow for custom router outlet implementations (#40827)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+32.sha-09e1e19
+ * @license Angular v12.0.0-next.1+16.sha-a82fddf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+32.sha-09e1e19
+ * @license Angular v12.0.0-next.1+16.sha-a82fddf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6493,7 +6493,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.1+32.sha-09e1e19');
+    var VERSION = new i0.Version('12.0.0-next.1+16.sha-a82fddf');
 
     /**
      * @license
