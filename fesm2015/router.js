214e4d350272f553f34ceb30cfb117fb097756a7
atscott <atscott01@gmail.com>
2021-02-19 17:27:36 +0000

    d0b6270990 docs: edit Component lifecycle title (#40894)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+12.sha-aed2782
+ * @license Angular v12.0.0-next.1+15.sha-d0b6270
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5798,7 +5798,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+12.sha-aed2782');
+const VERSION = new Version('12.0.0-next.1+15.sha-d0b6270');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+12.sha-aed2782
+ * @license Angular v12.0.0-next.1+15.sha-d0b6270
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
