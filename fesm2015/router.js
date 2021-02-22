4d51a0a1c79567e08f083f3426335ce641329817
atscott <atscott01@gmail.com>
2021-02-22 23:39:07 +0000

    09b92d755a refactor(compiler-cli): separate out constant target of g3 patch (#40950)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.1+40.sha-263bbd4
+ * @license Angular v11.2.2+1.sha-09b92d7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5798,7 +5798,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.1+40.sha-263bbd4');
+const VERSION = new Version('11.2.2+1.sha-09b92d7');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.1+40.sha-263bbd4
+ * @license Angular v11.2.2+1.sha-09b92d7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
