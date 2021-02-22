329a171f0722363cd1429fdc390e2ddad9bc23c9
Andrew Scott <atscott@google.com>
2021-02-22 20:55:46 +0000

    b84f719747 docs: update year (#40925)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,6 +1,6 @@
 /**
- * @license Angular v12.0.0-next.1+41.sha-dc9fd1a
- * (c) 2010-2020 Google LLC. https://angular.io/
+ * @license Angular v12.0.0-next.1+42.sha-b84f719
+ * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
 
