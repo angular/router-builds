d7f7726bf4ae1aac3a3d1de5fda750edf85585a5
atscott <atscott01@gmail.com>
2021-02-22 23:36:55 +0000

    ddf7970b78 refactor(compiler-cli): separate out constant target of g3 patch (#40950)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+44.sha-a58f066
+ * @license Angular v12.0.0-next.1+45.sha-ddf7970
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6444,7 +6444,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.1+44.sha-a58f066');
+    var VERSION = new core.Version('12.0.0-next.1+45.sha-ddf7970');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+44.sha-a58f066
+ * @license Angular v12.0.0-next.1+45.sha-ddf7970
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
