031243b8f65abac5fd8dfc7380cae4d5c505a4b3
atscott <atscott01@gmail.com>
2021-02-22 16:59:32 +0000

    53c65f468f test(language-service): update compiler_spec to use the new testing env (#40679)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+35.sha-d1d1dad
+ * @license Angular v12.0.0-next.1+37.sha-53c65f4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6444,7 +6444,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.1+35.sha-d1d1dad');
+    var VERSION = new core.Version('12.0.0-next.1+37.sha-53c65f4');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+35.sha-d1d1dad
+ * @license Angular v12.0.0-next.1+37.sha-53c65f4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5802,7 +5802,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+35.sha-d1d1dad');
+const VERSION = new Version('12.0.0-next.1+37.sha-53c65f4');
 
 /**
  * @license
