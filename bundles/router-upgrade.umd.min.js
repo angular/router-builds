f69b7e1599d7f038da67c680a413d302f29b49a8
GitHub <noreply@github.com>
2021-02-22 22:07:03 +0000

    263bbd43c1 release: cut the v11.2.2 release (#40951)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.1+39.sha-73a3ff1
+ * @license Angular v11.2.1+40.sha-263bbd4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.1+39.sha-73a3ff1
+ * @license Angular v11.2.1+40.sha-263bbd4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6440,7 +6440,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.1+39.sha-73a3ff1');
+    var VERSION = new core.Version('11.2.1+40.sha-263bbd4');
 
     /**
      * @license
