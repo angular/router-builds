7c2727c9e686e5365d680b3f75c5bde6711ea6c8
atscott <atscott01@gmail.com>
2021-02-22 17:00:19 +0000

    995adb2297 test(core): refactor ApplicationInitStatus tests to avoid TestBed side-effects (#33222)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+37.sha-53c65f4
+ * @license Angular v12.0.0-next.1+39.sha-995adb2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5802,7 +5802,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+37.sha-53c65f4');
+const VERSION = new Version('12.0.0-next.1+39.sha-995adb2');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+37.sha-53c65f4
+ * @license Angular v12.0.0-next.1+39.sha-995adb2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
