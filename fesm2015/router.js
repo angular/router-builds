2eab5fee0ed6db3bbdac94b2ce6334e66cc1e0c3
atscott <atscott01@gmail.com>
2021-02-22 23:36:44 +0000

    a58f066647 docs: copy release notes from 11.2.2 (#40953)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+43.sha-f31a601
+ * @license Angular v12.0.0-next.1+44.sha-a58f066
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5802,7 +5802,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+43.sha-f31a601');
+const VERSION = new Version('12.0.0-next.1+44.sha-a58f066');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+43.sha-f31a601
+ * @license Angular v12.0.0-next.1+44.sha-a58f066
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
