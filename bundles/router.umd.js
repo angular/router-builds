73d5236577878e45e433ad6b7c1393e5c1a663f3
atscott <atscott01@gmail.com>
2021-02-23 19:09:35 +0000

    4d1299bd82 docs: improve accessibility of toh-pt5 example (#40632)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+48.sha-49e02ca
+ * @license Angular v12.0.0-next.1+52.sha-4d1299b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6493,7 +6493,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.1+48.sha-49e02ca');
+    var VERSION = new i0.Version('12.0.0-next.1+52.sha-4d1299b');
 
     /**
      * @license
