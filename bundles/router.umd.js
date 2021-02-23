95135f6e29d7230fae10c590bc14386399883024
atscott <atscott01@gmail.com>
2021-02-23 16:36:03 +0000

    d3705b3284 fix(common): avoid mutating context object in NgTemplateOutlet (#40360)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+45.sha-ddf7970
+ * @license Angular v12.0.0-next.1+47.sha-d3705b3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6493,7 +6493,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.1+45.sha-ddf7970');
+    var VERSION = new i0.Version('12.0.0-next.1+47.sha-d3705b3');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+45.sha-ddf7970
+ * @license Angular v12.0.0-next.1+47.sha-d3705b3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
