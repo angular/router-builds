10517ae9e1fe24d85383b78f672734de89af0ddb
atscott <atscott01@gmail.com>
2021-02-23 19:09:38 +0000

    1f91672d50 docs: improve accessibility of toh2 example (#40575)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+51.sha-edd26a2
+ * @license Angular v12.0.0-next.1+55.sha-1f91672
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6444,7 +6444,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.1+51.sha-edd26a2');
+    var VERSION = new core.Version('12.0.0-next.1+55.sha-1f91672');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+51.sha-edd26a2
+ * @license Angular v12.0.0-next.1+55.sha-1f91672
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
