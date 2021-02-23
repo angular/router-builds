f6e848b12430a7347beca34514bec55ec77a72de
atscott <atscott01@gmail.com>
2021-02-23 21:36:46 +0000

    011a527497 docs: improve examples, description of PipeTransform (#40863)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+58.sha-7101267
+ * @license Angular v12.0.0-next.1+59.sha-011a527
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6493,7 +6493,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.1+58.sha-7101267');
+    var VERSION = new i0.Version('12.0.0-next.1+59.sha-011a527');
 
     /**
      * @license
