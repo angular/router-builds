e7d587ac8cf6376ad80d966df5b8aba8f8202adc
atscott <atscott01@gmail.com>
2021-02-23 19:12:35 +0000

    1f7921be33 docs: improve accessibility of tohpt3 (#40617)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+55.sha-1f91672
+ * @license Angular v12.0.0-next.1+54.sha-1f7921b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+55.sha-1f91672
+ * @license Angular v12.0.0-next.1+54.sha-1f7921b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6493,7 +6493,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.1+55.sha-1f91672');
+    var VERSION = new i0.Version('12.0.0-next.1+54.sha-1f7921b');
 
     /**
      * @license
