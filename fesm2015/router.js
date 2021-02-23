0ae398d1048d3ac5fbdee5d454c02cb73f40fce8
atscott <atscott01@gmail.com>
2021-02-23 19:08:56 +0000

    49e02ca7d6 docs: move JsonParser class above CustomJsonInterceptor (#40964)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+49.sha-ad38cbb
+ * @license Angular v12.0.0-next.1+48.sha-49e02ca
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5834,7 +5834,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+49.sha-ad38cbb');
+const VERSION = new Version('12.0.0-next.1+48.sha-49e02ca');
 
 /**
  * @license
