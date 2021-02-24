c872234bada2a0ff5bc01f6aefc58c8629a3efd3
Zach Arend <zachzach@google.com>
2021-02-24 22:43:18 +0000

    8d13f631d9 refactor(ngcc): support processing only the typings files of packages (#40976)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+76.sha-140ff8a
+ * @license Angular v12.0.0-next.2+1.sha-8d13f63
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6444,7 +6444,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.1+76.sha-140ff8a');
+    var VERSION = new core.Version('12.0.0-next.2+1.sha-8d13f63');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+76.sha-140ff8a
+ * @license Angular v12.0.0-next.2+1.sha-8d13f63
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
