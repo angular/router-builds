384493df8c8e724e4c0b7dab39c1ba56a399ec0a
Andrew Scott <atscott@google.com>
2021-02-24 16:24:31 +0000

    35514347df build: update to rules_nodejs@3.2.1 (#40972)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.2+13.sha-db28ed2
+ * @license Angular v11.2.2+15.sha-3551434
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.2+13.sha-db28ed2
+ * @license Angular v11.2.2+15.sha-3551434
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5798,7 +5798,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.2+13.sha-db28ed2');
+const VERSION = new Version('11.2.2+15.sha-3551434');
 
 /**
  * @license
