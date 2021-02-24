7232efd93ad96b6ff6c4b30587575de3884c57aa
Zach Arend <zachzach@google.com>
2021-02-24 23:29:57 +0000

    3c24136b98 fix(compiler): ensure JIT compilation of ɵɵngDeclarePipe() works (#40929)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+4.sha-e986a97
+ * @license Angular v12.0.0-next.2+5.sha-3c24136
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5834,7 +5834,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+4.sha-e986a97');
+const VERSION = new Version('12.0.0-next.2+5.sha-3c24136');
 
 /**
  * @license
