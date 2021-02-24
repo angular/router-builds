eade3a829c8260e7d784616d8e4626d7b8e8b91f
atscott <atscott01@gmail.com>
2021-02-24 17:13:59 +0000

    3fe759eb6c test(language-service): remove circular dependency (#40966)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.2+15.sha-3551434
+ * @license Angular v11.2.2+25.sha-3fe759e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5798,7 +5798,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.2+15.sha-3551434');
+const VERSION = new Version('11.2.2+25.sha-3fe759e');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.2+15.sha-3551434
+ * @license Angular v11.2.2+25.sha-3fe759e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
