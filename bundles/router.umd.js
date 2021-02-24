b3345fe4fa7f6443de12a50f7d60e9806d08810c
Zach Arend <zachzach@google.com>
2021-02-24 23:27:25 +0000

    e986a9787b fix(language-service): use single entry point for Ivy and View Engine (#40967)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+3.sha-51a7977
+ * @license Angular v12.0.0-next.2+4.sha-e986a97
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6493,7 +6493,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.2+3.sha-51a7977');
+    var VERSION = new i0.Version('12.0.0-next.2+4.sha-e986a97');
 
     /**
      * @license
