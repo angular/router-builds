dad3d9dbf4b5b863ab917593dab9a0121870324b
Andrew Scott <atscott@google.com>
2021-02-24 16:17:05 +0000

    03de2f2b78 docs: fix ch 4 so it compiles in strict mode (#40970)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+59.sha-011a527
+ * @license Angular v12.0.0-next.1+60.sha-03de2f2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6444,7 +6444,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.1+59.sha-011a527');
+    var VERSION = new core.Version('12.0.0-next.1+60.sha-03de2f2');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+59.sha-011a527
+ * @license Angular v12.0.0-next.1+60.sha-03de2f2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5802,7 +5802,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+59.sha-011a527');
+const VERSION = new Version('12.0.0-next.1+60.sha-03de2f2');
 
 /**
  * @license
