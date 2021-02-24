3893aaf408e7efa733f85a4c96dc12c0cecc35b2
Andrew Scott <atscott@google.com>
2021-02-24 16:20:39 +0000

    aaf9b31fb4 feat(core): drop support for zone.js 0.10.x (#40823)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+60.sha-03de2f2
+ * @license Angular v12.0.0-next.1+63.sha-aaf9b31
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6444,7 +6444,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.1+60.sha-03de2f2');
+    var VERSION = new core.Version('12.0.0-next.1+63.sha-aaf9b31');
 
     /**
      * @license
