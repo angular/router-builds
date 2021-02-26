9e63a6ce22a5b606e29940a11fbb9910a7827e36
Zach Arend <zachzach@google.com>
2021-02-26 16:49:11 +0000

    284af7308b fix(ngcc): do not fail hard when a format-path points to a non-existing or empty file (#40985)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+8.sha-6206d99
+ * @license Angular v12.0.0-next.2+10.sha-284af73
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5863,7 +5863,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+8.sha-6206d99');
+const VERSION = new Version('12.0.0-next.2+10.sha-284af73');
 
 /**
  * @license
