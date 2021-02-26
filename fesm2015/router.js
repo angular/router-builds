ec6533a6ddc4071320914e8e6e6fc42d96a40216
Zach Arend <zachzach@google.com>
2021-02-26 23:32:56 +0000

    fb82558d78 docs: Change lexical in security guide (#41016)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+14.sha-91cdc11
+ * @license Angular v12.0.0-next.2+15.sha-fb82558
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5863,7 +5863,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+14.sha-91cdc11');
+const VERSION = new Version('12.0.0-next.2+15.sha-fb82558');
 
 /**
  * @license
