b0fb214665ef25d5ef111eb9e086a10d8f852ca8
JoostK <joost.koehoorn@gmail.com>
2021-02-26 23:23:49 +0000

    8d159b0942 fixup! perf(compiler-cli): detect semantic changes and their effect on an incremental rebuild

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+16.sha-3df1582
+ * @license Angular v12.0.0-next.2+16.sha-8d159b0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5863,7 +5863,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+16.sha-3df1582');
+const VERSION = new Version('12.0.0-next.2+16.sha-8d159b0');
 
 /**
  * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+16.sha-3df1582
+ * @license Angular v12.0.0-next.2+16.sha-8d159b0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
