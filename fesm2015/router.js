ca5647ac587f07de981280947edc494da98e287f
Zach Arend <zachzach@google.com>
2021-02-26 23:57:39 +0000

    272b5645c4 fix(docs-infra): improve the layout of the "Resources" page (#40944)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+15.sha-fb82558
+ * @license Angular v12.0.0-next.2+29.sha-272b564
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5895,7 +5895,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+15.sha-fb82558');
+const VERSION = new Version('12.0.0-next.2+29.sha-272b564');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+15.sha-fb82558
+ * @license Angular v12.0.0-next.2+29.sha-272b564
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
