23fbb347f0979abf2381de694fee1c7c3db5583a
Zach Arend <zachzach@google.com>
2021-02-26 20:57:55 +0000

    91cdc11aa0 fix(common): allow number or boolean as http params (#40663)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+13.sha-383d226
+ * @license Angular v12.0.0-next.2+14.sha-91cdc11
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5863,7 +5863,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+13.sha-383d226');
+const VERSION = new Version('12.0.0-next.2+14.sha-91cdc11');
 
 /**
  * @license
