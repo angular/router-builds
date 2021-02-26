e90132aec56f9649ecf6baf28e290e72756a79b2
Zach Arend <zachzach@google.com>
2021-02-26 16:43:50 +0000

    953e98e211 docs: Change grammatically in security guide (#40995)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+7.sha-6c05c80
+ * @license Angular v12.0.0-next.2+9.sha-953e98e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6504,7 +6504,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.2+7.sha-6c05c80');
+    var VERSION = new core.Version('12.0.0-next.2+9.sha-953e98e');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+7.sha-6c05c80
+ * @license Angular v12.0.0-next.2+9.sha-953e98e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
