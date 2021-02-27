ad6baed8df2927ffba870187046b15e01b55d6a6
Zach Arend <zachzach@google.com>
2021-02-27 00:11:48 +0000

    be8893fd1d docs: add youtube to social icons (#40987)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+33.sha-bebdeba
+ * @license Angular v12.0.0-next.2+34.sha-be8893f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6504,7 +6504,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.2+33.sha-bebdeba');
+    var VERSION = new core.Version('12.0.0-next.2+34.sha-be8893f');
 
     /**
      * @license
