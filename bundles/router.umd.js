0ea2b72ec35ebafe683ec8be969600dd9e69f1f0
Zach Arend <zachzach@google.com>
2021-03-01 23:46:43 +0000

    a31da48507 fix(animations): cleanup DOM elements when the root view is removed (#41001)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+37.sha-8110cf0
+ * @license Angular v12.0.0-next.2+38.sha-a31da48
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6504,7 +6504,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.2+37.sha-8110cf0');
+    var VERSION = new core.Version('12.0.0-next.2+38.sha-a31da48');
 
     /**
      * @license
