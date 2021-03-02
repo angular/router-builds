affb9ede5c8da78ac196a6adac3361fa3447572c
Zach Arend <zachzach@google.com>
2021-03-02 00:06:02 +0000

    3370ade9a4 refactor(platform-browser): cast response of JSON.parse (#41020)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+41.sha-6c783c7
+ * @license Angular v12.0.0-next.2+43.sha-3370ade
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6504,7 +6504,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.2+41.sha-6c783c7');
+    var VERSION = new core.Version('12.0.0-next.2+43.sha-3370ade');
 
     /**
      * @license
