b249001d6f3cfcb284607e94ff730bd0b33cc6a0
Zach Arend <zachzach@google.com>
2021-03-02 00:20:00 +0000

    198e841564 docs(http): add example for interceptor order (#39938)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+43.sha-3370ade
+ * @license Angular v12.0.0-next.2+42.sha-198e841
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5895,7 +5895,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+43.sha-3370ade');
+const VERSION = new Version('12.0.0-next.2+42.sha-198e841');
 
 /**
  * @license
