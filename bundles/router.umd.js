da6952ceba6467b66985dd3b78565dce0d39ec06
JoostK <joost.koehoorn@gmail.com>
2021-03-02 18:28:11 +0000

    cba03bd126 fixup! perf(compiler-cli): detect semantic changes and their effect on an incremental rebuild

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+16.sha-8d159b0
+ * @license Angular v12.0.0-next.2+52.sha-cba03bd
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6504,7 +6504,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.2+16.sha-8d159b0');
+    var VERSION = new core.Version('12.0.0-next.2+52.sha-cba03bd');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+16.sha-8d159b0
+ * @license Angular v12.0.0-next.2+52.sha-cba03bd
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
