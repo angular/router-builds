38b74cf177fa5937e7c60852b8457440d31fcdee
Zach Arend <zachzach@google.com>
2021-03-03 18:09:28 +0000

    e9e7c33f3c fix(language-service): Add plugin option to force strictTemplates (#41062)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+49.sha-21f0dee
+ * @license Angular v12.0.0-next.2+50.sha-e9e7c33
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5895,7 +5895,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+49.sha-21f0dee');
+const VERSION = new Version('12.0.0-next.2+50.sha-e9e7c33');
 
 /**
  * @license
