20c0de369c94496eb6e83f151fc568e19b4f99bf
Zach Arend <zachzach@google.com>
2021-03-03 17:54:54 +0000

    5eab6e14f1 docs: fix typo in router.md file (#41064) PR Close #41064

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+44.sha-96f15f0
+ * @license Angular v12.0.0-next.2+45.sha-5eab6e1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5863,7 +5863,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+44.sha-96f15f0');
+const VERSION = new Version('12.0.0-next.2+45.sha-5eab6e1');
 
 /**
  * @license
