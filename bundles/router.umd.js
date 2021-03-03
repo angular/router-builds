108ddfdc4ae5e9ea4355360fb8de6eef44f45e27
Zach Arend <zachzach@google.com>
2021-03-03 21:58:40 +0000

    bb6e5e2dd0 fix(language-service): don't show external template diagnostics in ts files (#41070)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+57.sha-0847a03
+ * @license Angular v12.0.0-next.2+58.sha-bb6e5e2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6504,7 +6504,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.2+57.sha-0847a03');
+    var VERSION = new core.Version('12.0.0-next.2+58.sha-bb6e5e2');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+57.sha-0847a03
+ * @license Angular v12.0.0-next.2+58.sha-bb6e5e2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
