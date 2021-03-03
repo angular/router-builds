1a562838580d6269e5a04f9333b21e34fdf10a59
Zach Arend <zachzach@google.com>
2021-03-03 18:19:25 +0000

    736b1f9fd4 fix(compiler): recover from an incomplete open tag at the end of a file (#41054)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+51.sha-ffbacbb
+ * @license Angular v12.0.0-next.2+52.sha-736b1f9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6553,7 +6553,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.2+51.sha-ffbacbb');
+    var VERSION = new i0.Version('12.0.0-next.2+52.sha-736b1f9');
 
     /**
      * @license
