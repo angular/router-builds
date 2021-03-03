f33ce244404975ce2753f7012126b4617122fbed
Zach Arend <zachzach@google.com>
2021-03-03 18:25:57 +0000

    7765b648e7 docs: update Angular Global Summit on event pages (#41050)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+54.sha-d105994
+ * @license Angular v12.0.0-next.2+55.sha-7765b64
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+54.sha-d105994
+ * @license Angular v12.0.0-next.2+55.sha-7765b64
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6553,7 +6553,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.2+54.sha-d105994');
+    var VERSION = new i0.Version('12.0.0-next.2+55.sha-7765b64');
 
     /**
      * @license
