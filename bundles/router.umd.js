a52935e864829e1336a3e53a7bb6156b87c76b80
Zach Arend <zachzach@google.com>
2021-03-03 18:23:33 +0000

    97b88f3631 fix(compiler): allow binding to autocomplete property on select and textarea elements (#40928)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+52.sha-736b1f9
+ * @license Angular v12.0.0-next.2+53.sha-97b88f3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6553,7 +6553,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.2+52.sha-736b1f9');
+    var VERSION = new i0.Version('12.0.0-next.2+53.sha-97b88f3');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+52.sha-736b1f9
+ * @license Angular v12.0.0-next.2+53.sha-97b88f3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
