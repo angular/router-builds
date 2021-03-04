3f474a7b9a33eb540286007c41f1d8e36bb4a339
Andrew Kushnir <akushnir@google.com>
2021-03-04 19:20:04 +0000

    d9acaa8547 docs: remove Codelyzer recommendation from style guide (#40992)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+3.sha-ad40fca
+ * @license Angular v12.0.0-next.3+4.sha-d9acaa8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+3.sha-ad40fca
+ * @license Angular v12.0.0-next.3+4.sha-d9acaa8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6504,7 +6504,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.3+3.sha-ad40fca');
+    var VERSION = new core.Version('12.0.0-next.3+4.sha-d9acaa8');
 
     /**
      * @license
