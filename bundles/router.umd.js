f595fdf7655c67b03f2d064f2d1dc573f9862747
Andrew Kushnir <akushnir@google.com>
2021-03-05 18:04:12 +0000

    4637df5551 test(forms): add integration test app to keep track of payload size (#41045)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+15.sha-fbc9df1
+ * @license Angular v12.0.0-next.3+16.sha-4637df5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6504,7 +6504,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.3+15.sha-fbc9df1');
+    var VERSION = new core.Version('12.0.0-next.3+16.sha-4637df5');
 
     /**
      * @license
