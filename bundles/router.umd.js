07853db6f821dd856efdc7a10887833df95d651c
Andrew Kushnir <akushnir@google.com>
2021-03-08 18:19:13 +0000

    c1a93fcf32 docs: remove augury resource (#41107)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+29.sha-fed6a7c
+ * @license Angular v12.0.0-next.3+31.sha-c1a93fc
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6504,7 +6504,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.3+29.sha-fed6a7c');
+    var VERSION = new core.Version('12.0.0-next.3+31.sha-c1a93fc');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+29.sha-fed6a7c
+ * @license Angular v12.0.0-next.3+31.sha-c1a93fc
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5863,7 +5863,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.3+29.sha-fed6a7c');
+const VERSION = new Version('12.0.0-next.3+31.sha-c1a93fc');
 
 /**
  * @license
