a8a11b4dacc84365c6abbe36631aa08819728c49
Andrew Kushnir <akushnir@google.com>
2021-03-08 17:16:28 +0000

    2d69f0c9ca docs: extend :host selector documentation (#41055)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+23.sha-7854c4d
+ * @license Angular v12.0.0-next.3+26.sha-2d69f0c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6553,7 +6553,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.3+23.sha-7854c4d');
+    var VERSION = new i0.Version('12.0.0-next.3+26.sha-2d69f0c');
 
     /**
      * @license
