678199eac1a7c2d244449f19541d858476f3a5bc
Andrew Kushnir <akushnir@google.com>
2021-03-08 17:13:57 +0000

    7854c4ddbe test(docs-infra): run a11y tests against `/tutorial` and update a11y scores (#41103)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+25.sha-681c341
+ * @license Angular v12.0.0-next.3+23.sha-7854c4d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+25.sha-681c341
+ * @license Angular v12.0.0-next.3+23.sha-7854c4d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6553,7 +6553,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.3+25.sha-681c341');
+    var VERSION = new i0.Version('12.0.0-next.3+23.sha-7854c4d');
 
     /**
      * @license
