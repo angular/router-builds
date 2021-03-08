e10a7871d496fe4707cbab17d034e681371bb792
Andrew Kushnir <akushnir@google.com>
2021-03-08 17:11:09 +0000

    681c3417fe docs: add Discord, YouTube, StackOverflow to the "Community" section in `README.md` (#41086)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+19.sha-13681d9
+ * @license Angular v12.0.0-next.3+25.sha-681c341
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6504,7 +6504,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.3+19.sha-13681d9');
+    var VERSION = new core.Version('12.0.0-next.3+25.sha-681c341');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+19.sha-13681d9
+ * @license Angular v12.0.0-next.3+25.sha-681c341
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
