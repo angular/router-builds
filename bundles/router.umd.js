5c50a30cdff2affd63d9dc9067a620f8a8ef1b9f
Andrew Kushnir <akushnir@google.com>
2021-03-09 17:23:19 +0000

    a730315fb2 docs: use double quotes in Tour of Heroes (pt1) (#41134)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+60.sha-e29a29c
+ * @license Angular v12.0.0-next.3+59.sha-a730315
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6504,7 +6504,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.3+60.sha-e29a29c');
+    var VERSION = new core.Version('12.0.0-next.3+59.sha-a730315');
 
     /**
      * @license
