8f158e3930eedf34847d4003a9cc4ea2add2a5c6
Andrew Kushnir <akushnir@google.com>
2021-03-09 17:18:09 +0000

    f281310d39 fix(docs-infra): print info to help debugging SW cache issue (#41106)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+57.sha-7a27cd2
+ * @license Angular v12.0.0-next.3+58.sha-f281310
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6504,7 +6504,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.3+57.sha-7a27cd2');
+    var VERSION = new core.Version('12.0.0-next.3+58.sha-f281310');
 
     /**
      * @license
