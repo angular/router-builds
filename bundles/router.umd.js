4111e66cbbd38746e5683653c9566841738e49f2
Andrew Kushnir <akushnir@google.com>
2021-03-09 00:23:57 +0000

    fa3689f432 test(forms): split Forms example app into Reactive and Template-driven ones (#41108)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+42.sha-2ebe2bc
+ * @license Angular v12.0.0-next.3+43.sha-fa3689f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6504,7 +6504,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.3+42.sha-2ebe2bc');
+    var VERSION = new core.Version('12.0.0-next.3+43.sha-fa3689f');
 
     /**
      * @license
