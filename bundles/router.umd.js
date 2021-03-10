87f968e18f66d7fa901bdb18f28007027a8e2fd2
Andrew Kushnir <akushnir@google.com>
2021-03-10 19:26:04 +0000

    ba81e8cc94 build: remove commit message max line length limit (#41157)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+64.sha-1fb0dbb
+ * @license Angular v12.0.0-next.4+2.sha-ba81e8c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6504,7 +6504,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.3+64.sha-1fb0dbb');
+    var VERSION = new core.Version('12.0.0-next.4+2.sha-ba81e8c');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+64.sha-1fb0dbb
+ * @license Angular v12.0.0-next.4+2.sha-ba81e8c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5863,7 +5863,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.3+64.sha-1fb0dbb');
+const VERSION = new Version('12.0.0-next.4+2.sha-ba81e8c');
 
 /**
  * @license
