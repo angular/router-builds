baa4925f07003c1ae17f1363fea460a4f53d6755
Andrew Kushnir <akushnir@google.com>
2021-03-11 17:44:56 +0000

    79e21b966e fix(docs-infra): fix angular.io on IE 11 (#41162)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+4.sha-3c66b10
+ * @license Angular v12.0.0-next.4+6.sha-79e21b9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6506,7 +6506,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.4+4.sha-3c66b10');
+    var VERSION = new core.Version('12.0.0-next.4+6.sha-79e21b9');
 
     /**
      * @license
