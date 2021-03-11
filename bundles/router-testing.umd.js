99e406030c70f26bbdedc7d7be711e81f61cc1f6
Andrew Kushnir <akushnir@google.com>
2021-03-11 17:50:03 +0000

    e1e766a880 fix(docs-infra): generate the `404.html` page correctly (#41163)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+6.sha-79e21b9
+ * @license Angular v12.0.0-next.4+7.sha-e1e766a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+6.sha-79e21b9
+ * @license Angular v12.0.0-next.4+7.sha-e1e766a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6556,7 +6556,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.4+6.sha-79e21b9');
+    var VERSION = new i0.Version('12.0.0-next.4+7.sha-e1e766a');
 
     /**
      * @license
