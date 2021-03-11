1639e37d3b5ca893cc386bb3704268c5ffc5c4f3
Andrew Kushnir <akushnir@google.com>
2021-03-11 20:50:51 +0000

    74aba139ed fix(docs-infra): fix angular.io on IE 11 (#41162) (#41184)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+7.sha-ed6c09a
+ * @license Angular v11.2.5+9.sha-74aba13
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5798,7 +5798,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.5+7.sha-ed6c09a');
+const VERSION = new Version('11.2.5+9.sha-74aba13');
 
 /**
  * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+7.sha-ed6c09a
+ * @license Angular v11.2.5+9.sha-74aba13
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
