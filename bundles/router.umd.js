5901feb3f8e3a837bcbf09c6f861710b5dfa2104
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-12 22:41:33 +0000

    8a9fe49a2a fix(forms): properly handle the change to the FormGroup shape (#40829)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+20.sha-e3ea863
+ * @license Angular v12.0.0-next.4+21.sha-8a9fe49
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6506,7 +6506,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.4+20.sha-e3ea863');
+    var VERSION = new core.Version('12.0.0-next.4+21.sha-8a9fe49');
 
     /**
      * @license
