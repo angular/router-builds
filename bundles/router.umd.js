72e7afb3ae904cea3def89ad61ff03fa6b64a1cf
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-12 21:48:23 +0000

    b37296ae64 ci: remove `CHANGELOG.md` from the `public-api` group (#41135)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+9.sha-74aba13
+ * @license Angular v11.2.5+16.sha-b37296a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6440,7 +6440,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.5+9.sha-74aba13');
+    var VERSION = new core.Version('11.2.5+16.sha-b37296a');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+9.sha-74aba13
+ * @license Angular v11.2.5+16.sha-b37296a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
