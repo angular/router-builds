0d88ad96ff6ae0445b50370045a8427fcca16b80
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-13 00:15:23 +0000

    937e90cd16 perf(forms): make built-in ControlValueAccessors more tree-shakable (#41146)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+21.sha-8a9fe49
+ * @license Angular v12.0.0-next.4+22.sha-937e90c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6556,7 +6556,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.4+21.sha-8a9fe49');
+    var VERSION = new i0.Version('12.0.0-next.4+22.sha-937e90c');
 
     /**
      * @license
