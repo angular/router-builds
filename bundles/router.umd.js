91e31966676c0e307e3b04f78e38f7842aec7982
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 18:38:58 +0000

    3bd1992218 perf(forms): avoid direct references to the `Validators` class (#41189)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+24.sha-1644d64
+ * @license Angular v12.0.0-next.4+25.sha-3bd1992
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6556,7 +6556,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.4+24.sha-1644d64');
+    var VERSION = new i0.Version('12.0.0-next.4+25.sha-3bd1992');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+24.sha-1644d64
+ * @license Angular v12.0.0-next.4+25.sha-3bd1992
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5898,7 +5898,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.4+24.sha-1644d64');
+const VERSION = new Version('12.0.0-next.4+25.sha-3bd1992');
 
 /**
  * @license
