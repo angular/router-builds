1f3c6dcd885584b57fd56628ec49345d5cf6d87e
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 19:40:50 +0000

    bec919ace9 docs: add contributors guide to aio (#41061)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+20.sha-da5f5a4
+ * @license Angular v11.2.5+22.sha-bec919a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6440,7 +6440,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.5+20.sha-da5f5a4');
+    var VERSION = new core.Version('11.2.5+22.sha-bec919a');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+20.sha-da5f5a4
+ * @license Angular v11.2.5+22.sha-bec919a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
