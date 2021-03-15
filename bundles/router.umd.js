6822cc078b4acf399c4fec2ed3dde8dfab8e728b
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 18:33:48 +0000

    da5f5a4910 docs: add youtube to social icons (#40987) (#41202)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+19.sha-b448412
+ * @license Angular v11.2.5+20.sha-da5f5a4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6440,7 +6440,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.5+19.sha-b448412');
+    var VERSION = new core.Version('11.2.5+20.sha-da5f5a4');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+19.sha-b448412
+ * @license Angular v11.2.5+20.sha-da5f5a4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
