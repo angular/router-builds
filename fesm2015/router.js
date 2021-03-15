3389d487bbcaab21333f2bf1b9039dbe01501242
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 17:53:03 +0000

    b4484125d6 docs: document `help wanted` and `good first issue` labels (#41195)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+18.sha-5908eda
+ * @license Angular v11.2.5+19.sha-b448412
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5798,7 +5798,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.5+18.sha-5908eda');
+const VERSION = new Version('11.2.5+19.sha-b448412');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+18.sha-5908eda
+ * @license Angular v11.2.5+19.sha-b448412
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
