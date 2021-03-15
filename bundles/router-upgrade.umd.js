5aef258a6cdcf28ff389138eddb424b53297f4a3
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 20:46:12 +0000

    1a9f526315 docs(core): fix typo in developer docs (#41080)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+26.sha-cc75e1d
+ * @license Angular v12.0.0-next.4+34.sha-1a9f526
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+26.sha-cc75e1d
+ * @license Angular v12.0.0-next.4+34.sha-1a9f526
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6506,7 +6506,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.4+26.sha-cc75e1d');
+    var VERSION = new core.Version('12.0.0-next.4+34.sha-1a9f526');
 
     /**
      * @license
