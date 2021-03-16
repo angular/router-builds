7cd2ad3035b442b14fe1086b808d11aeb74a6893
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-16 16:53:24 +0000

    7d6db009e9 docs: update docs to reflect production mode by default (#41173)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+34.sha-1a9f526
+ * @license Angular v12.0.0-next.4+35.sha-7d6db00
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6556,7 +6556,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.4+34.sha-1a9f526');
+    var VERSION = new i0.Version('12.0.0-next.4+35.sha-7d6db00');
 
     /**
      * @license
