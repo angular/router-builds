352aca509fe03c8abe0c3a79c7048ab0f52e8504
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-16 22:08:06 +0000

    18cd7bbd9b docs: remove unused attribute (#41211)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+27.sha-ca9af40
+ * @license Angular v11.2.5+28.sha-18cd7bb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+27.sha-ca9af40
+ * @license Angular v11.2.5+28.sha-18cd7bb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6440,7 +6440,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.5+27.sha-ca9af40');
+    var VERSION = new core.Version('11.2.5+28.sha-18cd7bb');
 
     /**
      * @license
