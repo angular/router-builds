f978f13098996135ba18cd351e2775c87dad56e9
GitHub <noreply@github.com>
2021-03-17 19:04:08 +0000

    02e8901d9e release: cut the v12.0.0-next.5 release (#41249)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+50.sha-fa04894
+ * @license Angular v12.0.0-next.4+52.sha-02e8901
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+50.sha-fa04894
+ * @license Angular v12.0.0-next.4+52.sha-02e8901
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6514,7 +6514,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.4+50.sha-fa04894');
+    var VERSION = new core.Version('12.0.0-next.4+52.sha-02e8901');
 
     /**
      * @license
