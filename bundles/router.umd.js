722acdb002e6cfc18aa09ff2e7bd6d5e23d5c498
Misko Hevery <misko@hevery.com>
2021-03-18 17:35:24 +0000

    ac9c4289e0 docs: replace deprecated karma-coverage-istanbul-reporter (#41246)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+3.sha-81d2aab
+ * @license Angular v12.0.0-next.5+6.sha-ac9c428
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6564,7 +6564,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.5+3.sha-81d2aab');
+    var VERSION = new i0.Version('12.0.0-next.5+6.sha-ac9c428');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+3.sha-81d2aab
+ * @license Angular v12.0.0-next.5+6.sha-ac9c428
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5898,7 +5898,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+3.sha-81d2aab');
+const VERSION = new Version('12.0.0-next.5+6.sha-ac9c428');
 
 /**
  * @license
