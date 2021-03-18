7dc6d960e9000f3f0808541753a7d8a07dc9b829
Misko Hevery <misko@hevery.com>
2021-03-18 17:39:39 +0000

    e23994191c docs: make `HeroService#gerHero()` in `toh-pt5` compatible with TS strict mode (#41234)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+7.sha-ac9a250
+ * @license Angular v12.0.0-next.5+5.sha-e239941
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6564,7 +6564,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.5+7.sha-ac9a250');
+    var VERSION = new i0.Version('12.0.0-next.5+5.sha-e239941');
 
     /**
      * @license
