446da52d5d5ea62d1522d3f7bb9fb9596aa3d29c
Misko Hevery <misko@hevery.com>
2021-03-19 20:13:38 +0000

    fbdb19c263 docs(localize): expose documentation for the `@angular/localize` package (#40317)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+8.sha-6555070
+ * @license Angular v12.0.0-next.5+9.sha-fbdb19c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+8.sha-6555070
+ * @license Angular v12.0.0-next.5+9.sha-fbdb19c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6564,7 +6564,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.5+8.sha-6555070');
+    var VERSION = new i0.Version('12.0.0-next.5+9.sha-fbdb19c');
 
     /**
      * @license
