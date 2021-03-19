9188afdc63f1479aa64da49594aa5c92100aca72
Misko Hevery <misko@hevery.com>
2021-03-19 20:15:25 +0000

    3a55698402 fix(core): remove obsolete check for [class] and [className] presence (#41254)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+10.sha-2ccb579
+ * @license Angular v12.0.0-next.5+11.sha-3a55698
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6514,7 +6514,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.5+10.sha-2ccb579');
+    var VERSION = new core.Version('12.0.0-next.5+11.sha-3a55698');
 
     /**
      * @license
