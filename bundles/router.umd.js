0daf4bdb1842b9d6f8cd0693436f2cea6fd73bf6
Misko Hevery <misko@hevery.com>
2021-03-19 20:23:22 +0000

    5eb7f3491f fix(dev-infra): remove commit message wizard and builder (#41280)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+12.sha-aa039a1
+ * @license Angular v12.0.0-next.5+15.sha-5eb7f34
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6564,7 +6564,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.5+12.sha-aa039a1');
+    var VERSION = new i0.Version('12.0.0-next.5+15.sha-5eb7f34');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+12.sha-aa039a1
+ * @license Angular v12.0.0-next.5+15.sha-5eb7f34
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5898,7 +5898,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+12.sha-aa039a1');
+const VERSION = new Version('12.0.0-next.5+15.sha-5eb7f34');
 
 /**
  * @license
