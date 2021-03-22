23a57ebb52c194b93609adf4a18a11cf34174ab8
Misko Hevery <misko@hevery.com>
2021-03-22 16:17:41 +0000

    efe02d8b04 docs: update contributors.json (#41284)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+16.sha-ed5bfe8
+ * @license Angular v12.0.0-next.5+19.sha-efe02d8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5898,7 +5898,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+16.sha-ed5bfe8');
+const VERSION = new Version('12.0.0-next.5+19.sha-efe02d8');
 
 /**
  * @license
