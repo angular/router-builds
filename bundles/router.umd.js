fdf8e29ec7104d170e5dbd6185764c2ce4d4ad35
Misko Hevery <misko@hevery.com>
2021-03-22 16:14:47 +0000

    5bc0a472bd refactor(docs-infra): remove image size exclusion list (#41292)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+15.sha-5eb7f34
+ * @license Angular v12.0.0-next.5+18.sha-5bc0a47
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6514,7 +6514,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.5+15.sha-5eb7f34');
+    var VERSION = new core.Version('12.0.0-next.5+18.sha-5bc0a47');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+15.sha-5eb7f34
+ * @license Angular v12.0.0-next.5+18.sha-5bc0a47
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
