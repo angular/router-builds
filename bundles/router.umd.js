94b347b88996dd038bf4505187b510921a214bd5
Misko Hevery <misko@hevery.com>
2021-03-22 20:19:38 +0000

    c49b28013a fix(animations): cleanup DOM elements when the root view is removed (#41059)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+24.sha-b555160
+ * @license Angular v12.0.0-next.5+25.sha-c49b280
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6564,7 +6564,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.5+24.sha-b555160');
+    var VERSION = new i0.Version('12.0.0-next.5+25.sha-c49b280');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+24.sha-b555160
+ * @license Angular v12.0.0-next.5+25.sha-c49b280
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5898,7 +5898,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+24.sha-b555160');
+const VERSION = new Version('12.0.0-next.5+25.sha-c49b280');
 
 /**
  * @license
