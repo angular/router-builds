d48ac9dfa871351a10c64d473278892b571d316b
Misko Hevery <misko@hevery.com>
2021-03-22 16:15:56 +0000

    ed5bfe8173 docs: simplify deleteHero method in tutorial (#41303)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+18.sha-5bc0a47
+ * @license Angular v12.0.0-next.5+16.sha-ed5bfe8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+18.sha-5bc0a47
+ * @license Angular v12.0.0-next.5+16.sha-ed5bfe8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6514,7 +6514,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.5+18.sha-5bc0a47');
+    var VERSION = new core.Version('12.0.0-next.5+16.sha-ed5bfe8');
 
     /**
      * @license
