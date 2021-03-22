cab358a445906b74819115e0175f5bfc13eb1be8
Misko Hevery <misko@hevery.com>
2021-03-22 16:16:10 +0000

    5bc0a472bd refactor(docs-infra): remove image size exclusion list (#41292)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+15.sha-5eb7f34
+ * @license Angular v12.0.0-next.5+18.sha-5bc0a47
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5898,7 +5898,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+15.sha-5eb7f34');
+const VERSION = new Version('12.0.0-next.5+18.sha-5bc0a47');
 
 /**
  * @license
