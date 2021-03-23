e87e6a09a43f2e7bec203062d8a5bcb5c55810ae
Joey Perrott <josephperrott@gmail.com>
2021-03-23 17:11:26 +0000

    1eba57eb00 fix(language-service): show suggestion when type inference is suboptimal (#41072)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+29.sha-80f11a9
+ * @license Angular v12.0.0-next.5+30.sha-1eba57e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+29.sha-80f11a9
+ * @license Angular v12.0.0-next.5+30.sha-1eba57e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6514,7 +6514,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.5+29.sha-80f11a9');
+    var VERSION = new core.Version('12.0.0-next.5+30.sha-1eba57e');
 
     /**
      * @license
