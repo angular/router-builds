6be5d8b97297c8690fa2ffdf31a7ffb19871cceb
Joey Perrott <josephperrott@gmail.com>
2021-03-23 17:11:57 +0000

    e340d190bd docs: add some boilerplate button, h3, and hr styles (#41052)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+26.sha-31dc483
+ * @license Angular v12.0.0-next.5+27.sha-e340d19
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6564,7 +6564,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.5+26.sha-31dc483');
+    var VERSION = new i0.Version('12.0.0-next.5+27.sha-e340d19');
 
     /**
      * @license
