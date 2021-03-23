50d43f96729894386d250d23ff40ab85c2cfa897
Joey Perrott <josephperrott@gmail.com>
2021-03-23 18:39:11 +0000

    190fa07b9a feat(router): add migration for ActivatedRouteSnapshot.fragment (#41092)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+32.sha-6a6d332
+ * @license Angular v12.0.0-next.5+31.sha-190fa07
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6564,7 +6564,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.5+32.sha-6a6d332');
+    var VERSION = new i0.Version('12.0.0-next.5+31.sha-190fa07');
 
     /**
      * @license
