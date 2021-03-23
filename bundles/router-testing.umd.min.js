71738acd91f2aa1b39f19d0ba9d7373515d781b6
Joey Perrott <josephperrott@gmail.com>
2021-03-23 20:33:45 +0000

    0516fbb180 refactor(dev-infra): use conventional-commits-parser for commit parsing (#41286)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+36.sha-eba1289
+ * @license Angular v12.0.0-next.5+37.sha-0516fbb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+36.sha-eba1289
+ * @license Angular v12.0.0-next.5+37.sha-0516fbb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6514,7 +6514,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.5+36.sha-eba1289');
+    var VERSION = new core.Version('12.0.0-next.5+37.sha-0516fbb');
 
     /**
      * @license
