e3e9df93f3e1f5b60855e76e172e09460c6f717c
Joey Perrott <josephperrott@gmail.com>
2021-03-23 18:39:00 +0000

    6a6d332c06 docs: add Alexander Inkin to GDE list (#41310)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+30.sha-1eba57e
+ * @license Angular v12.0.0-next.5+32.sha-6a6d332
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6564,7 +6564,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.5+30.sha-1eba57e');
+    var VERSION = new i0.Version('12.0.0-next.5+32.sha-6a6d332');
 
     /**
      * @license
