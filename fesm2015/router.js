b59cbe03d0a1c2683b02c639f42a1839b3e45b8c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-24 20:56:09 +0000

    dd82c8e9f4 docs(docs-infra): add Zach Arend to contributors.json (#41325)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+41.sha-9da7ea9
+ * @license Angular v12.0.0-next.6+1.sha-dd82c8e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5865,7 +5865,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+41.sha-9da7ea9');
+const VERSION = new Version('12.0.0-next.6+1.sha-dd82c8e');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+41.sha-9da7ea9
+ * @license Angular v12.0.0-next.6+1.sha-dd82c8e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
