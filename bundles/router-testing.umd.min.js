78fd0dca042c2cc9ab03c820860b65d3d7ffc529
Joey Perrott <josephperrott@gmail.com>
2021-03-24 16:46:59 +0000

    3177d6375e docs: update changelog for the v11.2.7 release

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+39.sha-81a0e71
+ * @license Angular v12.0.0-next.5+40.sha-3177d63
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+39.sha-81a0e71
+ * @license Angular v12.0.0-next.5+40.sha-3177d63
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6564,7 +6564,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.5+39.sha-81a0e71');
+    var VERSION = new i0.Version('12.0.0-next.5+40.sha-3177d63');
 
     /**
      * @license
