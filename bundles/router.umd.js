84e4c5da394a94cd23cdccfdc35520bcf42bc2de
Joey Perrott <josephperrott@gmail.com>
2021-03-24 16:45:40 +0000

    3177d6375e docs: update changelog for the v11.2.7 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+39.sha-81a0e71
+ * @license Angular v12.0.0-next.5+40.sha-3177d63
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6514,7 +6514,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.5+39.sha-81a0e71');
+    var VERSION = new core.Version('12.0.0-next.5+40.sha-3177d63');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+39.sha-81a0e71
+ * @license Angular v12.0.0-next.5+40.sha-3177d63
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5865,7 +5865,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+39.sha-81a0e71');
+const VERSION = new Version('12.0.0-next.5+40.sha-3177d63');
 
 /**
  * @license
