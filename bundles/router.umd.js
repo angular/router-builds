09c23e34400b18aa5e43bb1c09099147ed3c2b65
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 19:14:29 +0000

    c6d9a193d6 fix(dev-infra): Correctly place comments in dockerfile (#41374)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+42.sha-ff9470b
+ * @license Angular v12.0.0-next.6+43.sha-c6d9a19
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6514,7 +6514,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.6+42.sha-ff9470b');
+    var VERSION = new core.Version('12.0.0-next.6+43.sha-c6d9a19');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+42.sha-ff9470b
+ * @license Angular v12.0.0-next.6+43.sha-c6d9a19
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
