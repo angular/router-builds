bb385cc2b46da9a8b02ea53a733d073428e77222
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 18:52:17 +0000

    db55f8e671 ci: update commit-message verification range parameters (#41341)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+36.sha-18bc9ff
+ * @license Angular v12.0.0-next.6+38.sha-db55f8e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6514,7 +6514,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.6+36.sha-18bc9ff');
+    var VERSION = new core.Version('12.0.0-next.6+38.sha-db55f8e');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+36.sha-18bc9ff
+ * @license Angular v12.0.0-next.6+38.sha-db55f8e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
