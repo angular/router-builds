7d8e20f6e0cb1f9de3d41c92618cb3031a420b7e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 18:57:03 +0000

    3470ea17d5 fix(docs-infra): increase the max width of the `file-not-found` page (#41275)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+38.sha-db55f8e
+ * @license Angular v12.0.0-next.6+40.sha-3470ea1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6514,7 +6514,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.6+38.sha-db55f8e');
+    var VERSION = new core.Version('12.0.0-next.6+40.sha-3470ea1');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+38.sha-db55f8e
+ * @license Angular v12.0.0-next.6+40.sha-3470ea1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
