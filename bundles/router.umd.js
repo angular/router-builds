86a039d368ae658734fd24770063ddbe43c3f3de
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 19:02:51 +0000

    ff9470b0a0 fix(compiler): include used components during JIT compilation of partial component declaration (#41353)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+41.sha-0226a11
+ * @license Angular v12.0.0-next.6+42.sha-ff9470b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6564,7 +6564,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.6+41.sha-0226a11');
+    var VERSION = new i0.Version('12.0.0-next.6+42.sha-ff9470b');
 
     /**
      * @license
