e5c4106ad807de027fff3e80168e2af2fee9fe22
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 18:58:19 +0000

    3470ea17d5 fix(docs-infra): increase the max width of the `file-not-found` page (#41275)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+38.sha-db55f8e
+ * @license Angular v12.0.0-next.6+40.sha-3470ea1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5898,7 +5898,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.6+38.sha-db55f8e');
+const VERSION = new Version('12.0.0-next.6+40.sha-3470ea1');
 
 /**
  * @license
