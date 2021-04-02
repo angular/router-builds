68b72335d3debc1887b5227f66c2b05d32fbb790
atscott <atscott01@gmail.com>
2021-04-02 18:12:12 +0000

    03a46afc91 docs: Including Visual Studio (#41414)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+3.sha-a43f36b
+ * @license Angular v12.0.0-next.7+5.sha-03a46af
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5865,7 +5865,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.7+3.sha-a43f36b');
+const VERSION = new Version('12.0.0-next.7+5.sha-03a46af');
 
 /**
  * @license
