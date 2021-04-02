e9d95313552532402f888df2a1d80e1e9c357ca4
atscott <atscott01@gmail.com>
2021-04-02 17:51:49 +0000

    a43f36babd fix(dev-infra): require npm login for all publishes via wombat proxy (#41422)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+2.sha-46a97b9
+ * @license Angular v12.0.0-next.7+3.sha-a43f36b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5865,7 +5865,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.7+2.sha-46a97b9');
+const VERSION = new Version('12.0.0-next.7+3.sha-a43f36b');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+2.sha-46a97b9
+ * @license Angular v12.0.0-next.7+3.sha-a43f36b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
