40f7bcfe72a95c1410801385e6ecac8c430a4bf2
atscott <atscott01@gmail.com>
2021-04-02 23:09:21 +0000

    64b979e12b fix(dev-infra): exclude node_modules from circular dependency checks (#41407)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+5.sha-03a46af
+ * @license Angular v12.0.0-next.7+7.sha-64b979e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+5.sha-03a46af
+ * @license Angular v12.0.0-next.7+7.sha-64b979e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6564,7 +6564,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.7+5.sha-03a46af');
+    var VERSION = new i0.Version('12.0.0-next.7+7.sha-64b979e');
 
     /**
      * @license
