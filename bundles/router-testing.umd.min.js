e12d591a0d42672aab9b433a048b81554cc98601
atscott <atscott01@gmail.com>
2021-04-02 18:13:29 +0000

    03a46afc91 docs: Including Visual Studio (#41414)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+3.sha-a43f36b
+ * @license Angular v12.0.0-next.7+5.sha-03a46af
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+3.sha-a43f36b
+ * @license Angular v12.0.0-next.7+5.sha-03a46af
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6564,7 +6564,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.7+3.sha-a43f36b');
+    var VERSION = new i0.Version('12.0.0-next.7+5.sha-03a46af');
 
     /**
      * @license
