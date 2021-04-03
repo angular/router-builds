f0f28cb511403c3691693f819865a725a08fbc91
atscott <atscott01@gmail.com>
2021-04-03 20:13:32 +0000

    1de04b124e feat(compiler-cli): support transforming component style resources (#41307)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+8.sha-dc65526
+ * @license Angular v12.0.0-next.7+9.sha-1de04b1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6564,7 +6564,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.7+8.sha-dc65526');
+    var VERSION = new i0.Version('12.0.0-next.7+9.sha-1de04b1');
 
     /**
      * @license
