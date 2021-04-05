042d92e26c8b7591023897a429eea779c0604df3
atscott <atscott01@gmail.com>
2021-04-05 15:38:11 +0000

    fcd190aa9c docs: fix grammar mistake in `glossary.md` ("can exported" --> "can be exported") (#41440)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+9.sha-1de04b1
+ * @license Angular v12.0.0-next.7+10.sha-fcd190a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5865,7 +5865,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.7+9.sha-1de04b1');
+const VERSION = new Version('12.0.0-next.7+10.sha-fcd190a');
 
 /**
  * @license
