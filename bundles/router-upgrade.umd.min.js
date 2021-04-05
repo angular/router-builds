9dc03dcb223a829de2372ca6c1e018226e8350e3
atscott <atscott01@gmail.com>
2021-04-05 16:19:44 +0000

    ab47f417d6 test(core): fix `Function#name` shim used in IE11 (#41439)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+12.sha-180c1b5
+ * @license Angular v12.0.0-next.7+11.sha-ab47f41
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+12.sha-180c1b5
+ * @license Angular v12.0.0-next.7+11.sha-ab47f41
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5898,7 +5898,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.7+12.sha-180c1b5');
+const VERSION = new Version('12.0.0-next.7+11.sha-ab47f41');
 
 /**
  * @license
