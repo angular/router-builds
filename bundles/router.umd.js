b0b6af45233db57e14bfa7d4cfd799fd3bf82081
atscott <atscott01@gmail.com>
2021-04-05 20:02:19 +0000

    ee25034a77 build: update the recommended `Dockerfile` for VSCode remote development (#41396)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+14.sha-2cef385
+ * @license Angular v12.0.0-next.7+15.sha-ee25034
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6514,7 +6514,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.7+14.sha-2cef385');
+    var VERSION = new core.Version('12.0.0-next.7+15.sha-ee25034');
 
     /**
      * @license
