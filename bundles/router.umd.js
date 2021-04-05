78c45cff1c17b4f0af27ca583834a9c9b8ce8e39
atscott <atscott01@gmail.com>
2021-04-05 16:18:21 +0000

    180c1b555f fix(docs-infra): avoid auto-linking generic word `state` (#41438)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+10.sha-fcd190a
+ * @license Angular v12.0.0-next.7+12.sha-180c1b5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6564,7 +6564,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.7+10.sha-fcd190a');
+    var VERSION = new i0.Version('12.0.0-next.7+12.sha-180c1b5');
 
     /**
      * @license
