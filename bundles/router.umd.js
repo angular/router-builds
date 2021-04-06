1768602db80cb23a53c923de774907f94762e8ec
atscott <atscott01@gmail.com>
2021-04-06 16:47:46 +0000

    e05a6f3bb3 feat(common): add `historyGo` method to `Location` service (#38890)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+18.sha-3a823ab
+ * @license Angular v12.0.0-next.7+19.sha-e05a6f3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6564,7 +6564,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.7+18.sha-3a823ab');
+    var VERSION = new i0.Version('12.0.0-next.7+19.sha-e05a6f3');
 
     /**
      * @license
