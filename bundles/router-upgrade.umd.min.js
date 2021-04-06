748630e04e345b482142b7c33ea22902549fa715
atscott <atscott01@gmail.com>
2021-04-06 20:06:58 +0000

    4a68e13d1b build: update package.json engines range for nodes in preparation for upgrade (#41472)

--- bundles/router-upgrade.umd.min.js
+++ bundles/router-upgrade.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+32.sha-ff9253b
+ * @license Angular v12.0.0-next.7+33.sha-4a68e13
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+32.sha-ff9253b
+ * @license Angular v12.0.0-next.7+33.sha-4a68e13
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6517,7 +6517,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.7+32.sha-ff9253b');
+    var VERSION = new core.Version('12.0.0-next.7+33.sha-4a68e13');
 
     /**
      * @license
