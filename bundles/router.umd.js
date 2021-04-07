4abdacf7ffc1eb2b6469032655c0857cf49302bc
GitHub <noreply@github.com>
2021-04-07 19:02:31 +0000

    eda27c9a87 release: cut the v12.0.0-next.8 release (#41493)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+39.sha-ae6c195
+ * @license Angular v12.0.0-next.7+41.sha-eda27c9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6567,7 +6567,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.7+39.sha-ae6c195');
+    var VERSION = new i0.Version('12.0.0-next.7+41.sha-eda27c9');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+39.sha-ae6c195
+ * @license Angular v12.0.0-next.7+41.sha-eda27c9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5901,7 +5901,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.7+39.sha-ae6c195');
+const VERSION = new Version('12.0.0-next.7+41.sha-eda27c9');
 
 /**
  * @license
