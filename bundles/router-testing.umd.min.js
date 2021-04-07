25a45eb91dadde11a2bd3a941f607ea460a647a9
atscott <atscott01@gmail.com>
2021-04-07 17:10:09 +0000

    8f12f47492 fix(compiler-cli): Allow analysis to continue with invalid style url (#41403)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+37.sha-f98f379
+ * @license Angular v12.0.0-next.7+38.sha-8f12f47
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+37.sha-f98f379
+ * @license Angular v12.0.0-next.7+38.sha-8f12f47
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6517,7 +6517,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.7+37.sha-f98f379');
+    var VERSION = new core.Version('12.0.0-next.7+38.sha-8f12f47');
 
     /**
      * @license
