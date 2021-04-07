0dbfb09fbd4d75062f7d6cff2071fd5c47b9f24b
atscott <atscott01@gmail.com>
2021-04-07 19:28:08 +0000

    c385e74454 build: rely on engines to prevent using npm for dependency install (#41477)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+7.sha-d641542
+ * @license Angular v12.0.0-next.8+9.sha-c385e74
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5901,7 +5901,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+7.sha-d641542');
+const VERSION = new Version('12.0.0-next.8+9.sha-c385e74');
 
 /**
  * @license
