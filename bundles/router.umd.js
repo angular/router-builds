b48a70a9a7dbc0bcbbbd84f31225d5a851bd299d
atscott <atscott01@gmail.com>
2021-04-07 19:16:13 +0000

    d641542587 refactor(core): remove deprecated rxjs signature usage in event emitter and make more minifier-friendly (#41450)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+6.sha-7a49aa5
+ * @license Angular v12.0.0-next.8+7.sha-d641542
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6567,7 +6567,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+6.sha-7a49aa5');
+    var VERSION = new i0.Version('12.0.0-next.8+7.sha-d641542');
 
     /**
      * @license
