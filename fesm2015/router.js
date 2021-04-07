3bfd0bf399f4ca2e73ac603b6f9cc3f945ce2fa8
atscott <atscott01@gmail.com>
2021-04-07 19:10:47 +0000

    5a81490554 docs: use the right tag in the HeroesComponent's template snippet (#41473)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+40.sha-4b8bbe2
+ * @license Angular v12.0.0-next.8+1.sha-5a81490
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5868,7 +5868,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.7+40.sha-4b8bbe2');
+const VERSION = new Version('12.0.0-next.8+1.sha-5a81490');
 
 /**
  * @license
