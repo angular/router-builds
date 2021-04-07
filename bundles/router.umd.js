e9ca365e6244aca8a16b47ad0c90a7f4fff9362c
atscott <atscott01@gmail.com>
2021-04-07 17:02:53 +0000

    f98f379dd8 fix(dev-infra): make scopes for `test` and `refactor` commit types optional (#41486)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+34.sha-dd66da1
+ * @license Angular v12.0.0-next.7+37.sha-f98f379
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6517,7 +6517,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.7+34.sha-dd66da1');
+    var VERSION = new core.Version('12.0.0-next.7+37.sha-f98f379');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+34.sha-dd66da1
+ * @license Angular v12.0.0-next.7+37.sha-f98f379
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5868,7 +5868,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.7+34.sha-dd66da1');
+const VERSION = new Version('12.0.0-next.7+37.sha-f98f379');
 
 /**
  * @license
