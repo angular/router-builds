98a2441c5528b7f0ff7c0f0b0a3c8aca6f396b0c
Zach Arend <zachzach@google.com>
2021-04-07 21:13:26 +0000

    0a4308f756 docs: fix typo in migration guide (#41447)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+9.sha-c385e74
+ * @license Angular v12.0.0-next.8+12.sha-0a4308f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+9.sha-c385e74
+ * @license Angular v12.0.0-next.8+12.sha-0a4308f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6567,7 +6567,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+9.sha-c385e74');
+    var VERSION = new i0.Version('12.0.0-next.8+12.sha-0a4308f');
 
     /**
      * @license
