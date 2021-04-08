ad1836598e0aa8e0b281ed9988e73483f959238e
Zach Arend <zachzach@google.com>
2021-04-08 22:40:41 +0000

    7a40d8cefd fix(dev-infra): use base ref name rather than sha for the githubTargetBranch (#41523)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+19.sha-51bb922
+ * @license Angular v12.0.0-next.8+20.sha-7a40d8c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5905,7 +5905,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+19.sha-51bb922');
+const VERSION = new Version('12.0.0-next.8+20.sha-7a40d8c');
 
 /**
  * @license
