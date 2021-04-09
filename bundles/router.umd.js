adde1e4fa52ea9ebbe902b652c7af8f40a7bad77
Zach Arend <zachzach@google.com>
2021-04-09 15:09:17 +0000

    8b9d025b13 test(dev-infra): remove chai from ts-api-guardian tests (#41503)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+20.sha-7a40d8c
+ * @license Angular v12.0.0-next.8+23.sha-8b9d025
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6521,7 +6521,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+20.sha-7a40d8c');
+    var VERSION = new core.Version('12.0.0-next.8+23.sha-8b9d025');
 
     /**
      * @license
