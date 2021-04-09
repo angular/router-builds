7685c6b28e157b6d71cf2fc7e0fed30045dfc7b2
Zach Arend <zachzach@google.com>
2021-04-09 19:43:39 +0000

    61bfa3d9df test(language-service): Add test to expose bug caused by source file change (#41500)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+25.sha-5e0d5a9
+ * @license Angular v12.0.0-next.8+26.sha-61bfa3d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5905,7 +5905,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+25.sha-5e0d5a9');
+const VERSION = new Version('12.0.0-next.8+26.sha-61bfa3d');
 
 /**
  * @license
