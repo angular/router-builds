553cf2fac383b7985eeb23ae1c57b3a7a047a3a1
Zach Arend <zachzach@google.com>
2021-04-12 17:56:56 +0000

    42e3a5241d test(compiler-cli): add integration test for relative rootDir (#41359)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+32.sha-aa0e54f
+ * @license Angular v12.0.0-next.8+35.sha-42e3a52
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6571,7 +6571,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+32.sha-aa0e54f');
+    var VERSION = new i0.Version('12.0.0-next.8+35.sha-42e3a52');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+32.sha-aa0e54f
+ * @license Angular v12.0.0-next.8+35.sha-42e3a52
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5905,7 +5905,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+32.sha-aa0e54f');
+const VERSION = new Version('12.0.0-next.8+35.sha-42e3a52');
 
 /**
  * @license
