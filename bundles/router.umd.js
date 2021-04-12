fbc09b095a4059506f7aee3f159a0ae76552b73a
Zach Arend <zachzach@google.com>
2021-04-12 17:30:04 +0000

    05cc3c8ee2 docs: remove 'ui-jar' from Resources (#41552)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+26.sha-61bfa3d
+ * @license Angular v12.0.0-next.8+27.sha-05cc3c8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6521,7 +6521,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+26.sha-61bfa3d');
+    var VERSION = new core.Version('12.0.0-next.8+27.sha-05cc3c8');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+26.sha-61bfa3d
+ * @license Angular v12.0.0-next.8+27.sha-05cc3c8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
