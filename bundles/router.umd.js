640b9a9e0eb31cd594be15b93017faefecf7571b
Zach Arend <zachzach@google.com>
2021-04-13 20:05:56 +0000

    c7f9516ab9 feat(language-service): implement signature help (#41581)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+60.sha-60d0234
+ * @license Angular v12.0.0-next.8+64.sha-c7f9516
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6524,7 +6524,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+60.sha-60d0234');
+    var VERSION = new core.Version('12.0.0-next.8+64.sha-c7f9516');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+60.sha-60d0234
+ * @license Angular v12.0.0-next.8+64.sha-c7f9516
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5872,7 +5872,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+60.sha-60d0234');
+const VERSION = new Version('12.0.0-next.8+64.sha-c7f9516');
 
 /**
  * @license
