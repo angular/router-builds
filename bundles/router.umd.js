7be56ec49072a6e87feb8cf1303ad1e822fbd33f
Zach Arend <zachzach@google.com>
2021-04-13 19:11:44 +0000

    15c307b200 fix(dev-infra): only create authenticated github instance once in yargs (#41603)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+57.sha-c9aa87c
+ * @license Angular v12.0.0-next.8+58.sha-15c307b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6524,7 +6524,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+57.sha-c9aa87c');
+    var VERSION = new core.Version('12.0.0-next.8+58.sha-15c307b');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+57.sha-c9aa87c
+ * @license Angular v12.0.0-next.8+58.sha-15c307b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5872,7 +5872,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+57.sha-c9aa87c');
+const VERSION = new Version('12.0.0-next.8+58.sha-15c307b');
 
 /**
  * @license
