664d780082a44a4c8029c140d4178045c2c1ddeb
Zach Arend <zachzach@google.com>
2021-04-13 04:27:30 +0000

    319da894be docs: change ActivatedRouteStub code sample to work with strictNullChecks on (#41559)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+48.sha-1381301
+ * @license Angular v12.0.0-next.8+49.sha-319da89
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6574,7 +6574,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+48.sha-1381301');
+    var VERSION = new i0.Version('12.0.0-next.8+49.sha-319da89');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+48.sha-1381301
+ * @license Angular v12.0.0-next.8+49.sha-319da89
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5905,7 +5905,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+48.sha-1381301');
+const VERSION = new Version('12.0.0-next.8+49.sha-319da89');
 
 /**
  * @license
