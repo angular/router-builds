0ee0dd32502ca023488341a1fe80553a300b5d38
Zach Arend <zachzach@google.com>
2021-04-13 21:13:31 +0000

    ba84fa6f42 refactor(compiler-cli): remove i18n options from `LinkerOptions` (#41554)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+69.sha-78236bf
+ * @license Angular v12.0.0-next.8+71.sha-ba84fa6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6524,7 +6524,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+69.sha-78236bf');
+    var VERSION = new core.Version('12.0.0-next.8+71.sha-ba84fa6');
 
     /**
      * @license
