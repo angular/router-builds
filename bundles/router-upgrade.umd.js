36874c642cf425b31c60960d01fb999925122e1a
Zach Arend <zachzach@google.com>
2021-04-13 20:29:13 +0000

    04235fa079 docs: replace Codelyzer recommendation with Angular-ESLint in accessibility guide (#41294)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+36.sha-4847c14
+ * @license Angular v11.2.9+37.sha-04235fa
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+36.sha-4847c14
+ * @license Angular v11.2.9+37.sha-04235fa
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5805,7 +5805,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.9+36.sha-4847c14');
+const VERSION = new Version('11.2.9+37.sha-04235fa');
 
 /**
  * @license
