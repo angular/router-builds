f38febe4c36d9be9f1abafb9dd54342c225f49d1
Zach Arend <zachzach@google.com>
2021-04-13 20:30:49 +0000

    78236bfdca fix(language-service): use script versions for incremental compilations (#41475)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+65.sha-71b8c9a
+ * @license Angular v12.0.0-next.8+69.sha-78236bf
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6574,7 +6574,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+65.sha-71b8c9a');
+    var VERSION = new i0.Version('12.0.0-next.8+69.sha-78236bf');
 
     /**
      * @license
