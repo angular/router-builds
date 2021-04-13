b26d627f3ea4b159a46e5d778a852fd5a0905ba5
Zach Arend <zachzach@google.com>
2021-04-13 02:30:41 +0000

    1381301afe refactor(compiler-cli): track a dependency on a default import on `WrappedNodeExpr` (#41557)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+45.sha-528e7d7
+ * @license Angular v12.0.0-next.8+48.sha-1381301
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6524,7 +6524,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+45.sha-528e7d7');
+    var VERSION = new core.Version('12.0.0-next.8+48.sha-1381301');
 
     /**
      * @license
