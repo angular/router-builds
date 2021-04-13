8b3ec548a70ec215e4c183a3f4aeb4c34c9d536e
Zach Arend <zachzach@google.com>
2021-04-13 04:41:35 +0000

    ffea31f433 perf(compiler-cli): allow incremental compilation in the presence of redirected source files (#41448)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+56.sha-fe60029
+ * @license Angular v12.0.0-next.8+51.sha-ffea31f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+56.sha-fe60029
+ * @license Angular v12.0.0-next.8+51.sha-ffea31f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6574,7 +6574,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+56.sha-fe60029');
+    var VERSION = new i0.Version('12.0.0-next.8+51.sha-ffea31f');
 
     /**
      * @license
