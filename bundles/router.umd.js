56e3782cf7124cb2e01951a33c1adf985156811e
Zach Arend <zachzach@google.com>
2021-04-14 23:12:49 +0000

    1043aa42ad build: update typescript to version 4.2.4 (#41618)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+101.sha-2af894d
+ * @license Angular v12.0.0-next.8+102.sha-1043aa4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6574,7 +6574,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+101.sha-2af894d');
+    var VERSION = new i0.Version('12.0.0-next.8+102.sha-1043aa4');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+101.sha-2af894d
+ * @license Angular v12.0.0-next.8+102.sha-1043aa4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
