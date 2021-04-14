c284bd72f43122abbd66187cf08c2623e9458de4
GitHub <noreply@github.com>
2021-04-14 20:50:36 +0000

    eaeb7ef54b release: bump the next branch to v12.1.0-next.0 (#41629)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+97.sha-3923201
+ * @license Angular v12.0.0-next.8+99.sha-eaeb7ef
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6574,7 +6574,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+97.sha-3923201');
+    var VERSION = new i0.Version('12.0.0-next.8+99.sha-eaeb7ef');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+97.sha-3923201
+ * @license Angular v12.0.0-next.8+99.sha-eaeb7ef
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
