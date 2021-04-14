2cbd26b9ce6b966f92b7dcb7c14c8dae1b1c279b
Zach Arend <zachzach@google.com>
2021-04-14 23:10:50 +0000

    2af894d164 refactor: format renovate configuration with 2 spaces (#41619)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+99.sha-eaeb7ef
+ * @license Angular v12.0.0-next.8+101.sha-2af894d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6524,7 +6524,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+99.sha-eaeb7ef');
+    var VERSION = new core.Version('12.0.0-next.8+101.sha-2af894d');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+99.sha-eaeb7ef
+ * @license Angular v12.0.0-next.8+101.sha-2af894d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
