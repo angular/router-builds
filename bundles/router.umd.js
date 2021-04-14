9d8a73360527bd1d22350ad99660c39cc7452590
Zach Arend <zachzach@google.com>
2021-04-14 18:04:32 +0000

    5a8bc1bfe1 build: `ng_package` no longer generate minified UMDs (#41425)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+95.sha-beafa22
+ * @license Angular v12.0.0-next.8+96.sha-5a8bc1b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6524,7 +6524,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+95.sha-beafa22');
+    var VERSION = new core.Version('12.0.0-next.8+96.sha-5a8bc1b');
 
     /**
      * @license
