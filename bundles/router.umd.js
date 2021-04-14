fcaf70356bef79047d062631a08f95c1208e7be3
Zach Arend <zachzach@google.com>
2021-04-14 23:20:17 +0000

    dde81ba0cd perf(compiler): reduce amount of generated code for safe accesses and nullish coalescing (#41563)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+107.sha-b649273
+ * @license Angular v12.0.0-next.8+106.sha-dde81ba
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+107.sha-b649273');
+    var VERSION = new i0.Version('12.0.0-next.8+106.sha-dde81ba');
 
     /**
      * @license
