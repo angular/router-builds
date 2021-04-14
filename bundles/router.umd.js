8125dea67efa250a2a0c1092f05371322e9ed6eb
Zach Arend <zachzach@google.com>
2021-04-14 16:38:20 +0000

    de93a7a4bb fix(language-service): resolve to the pre-compiled style when compiled css url is provided (#41538)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+76.sha-bd34bc9
+ * @license Angular v12.0.0-next.8+77.sha-de93a7a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6524,7 +6524,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+76.sha-bd34bc9');
+    var VERSION = new core.Version('12.0.0-next.8+77.sha-de93a7a');
 
     /**
      * @license
