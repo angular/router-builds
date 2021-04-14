b079211a0c0522f3368b7c0e097fc6f9f21b8a75
Zach Arend <zachzach@google.com>
2021-04-14 22:28:20 +0000

    3a2b5906ae release: cut the v11.2.10 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+41.sha-b8c4da9
+ * @license Angular v11.2.10
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5805,7 +5805,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.9+41.sha-b8c4da9');
+const VERSION = new Version('11.2.10');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+41.sha-b8c4da9
+ * @license Angular v11.2.10
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
