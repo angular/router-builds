e39c9755015edd2ff5e6fe20638ba715b13ed9f8
Zach Arend <zachzach@google.com>
2021-04-14 23:21:51 +0000

    4329ccae2c docs: add rx-angular to resources.json (#39291)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+106.sha-dde81ba
+ * @license Angular v12.0.0-next.8+108.sha-4329cca
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+106.sha-dde81ba');
+    var VERSION = new core.Version('12.0.0-next.8+108.sha-4329cca');
 
     /**
      * @license
