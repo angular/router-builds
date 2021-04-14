976a1e179a7984f404f7a811995189da6d587c5a
Zach Arend <zachzach@google.com>
2021-04-14 00:56:51 +0000

    fd9a7ca8c9 build: update to latest version of `rules_nodejs` v3.3.0 (#41599)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+74.sha-f7e391a
+ * @license Angular v12.0.0-next.8+75.sha-fd9a7ca
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6524,7 +6524,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+74.sha-f7e391a');
+    var VERSION = new core.Version('12.0.0-next.8+75.sha-fd9a7ca');
 
     /**
      * @license
