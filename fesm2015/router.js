da4a04f5dc01d4a849e38a65d4eecdbc630b29af
GitHub <noreply@github.com>
2021-04-14 20:49:20 +0000

    eaeb7ef54b release: bump the next branch to v12.1.0-next.0 (#41629)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+97.sha-3923201
+ * @license Angular v12.0.0-next.8+99.sha-eaeb7ef
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5872,7 +5872,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+97.sha-3923201');
+const VERSION = new Version('12.0.0-next.8+99.sha-eaeb7ef');
 
 /**
  * @license
