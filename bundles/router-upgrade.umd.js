35895755e5e20162832747b2f1c803b075d8e736
Zach Arend <zachzach@google.com>
2021-04-15 02:14:36 +0000

    5332a4a919 refactor: update `$schema` and `id` keywords (#41574)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+108.sha-4329cca
+ * @license Angular v12.0.0-next.8+110.sha-5332a4a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+108.sha-4329cca
+ * @license Angular v12.0.0-next.8+110.sha-5332a4a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+108.sha-4329cca');
+    var VERSION = new i0.Version('12.0.0-next.8+110.sha-5332a4a');
 
     /**
      * @license
