d4dfb1281aa9b24375183dc06b462712ed288b80
Andrew Kushnir <akushnir@google.com>
2021-04-15 21:58:04 +0000

    9d8f345c0c docs: update copy in schematics.md (#41521)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+117.sha-8d0511d
+ * @license Angular v12.0.0-next.8+120.sha-9d8f345
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+117.sha-8d0511d');
+    var VERSION = new i0.Version('12.0.0-next.8+120.sha-9d8f345');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+117.sha-8d0511d
+ * @license Angular v12.0.0-next.8+120.sha-9d8f345
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
