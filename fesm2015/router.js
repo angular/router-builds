22c3c924c5bfa86c5c6a5dd4dc7968d207a9f7e0
Andrew Kushnir <akushnir@google.com>
2021-04-16 16:18:58 +0000

    fe5bf7f53f fix(compiler-cli): autocomplete literal types in templates. (#41456) (#41645)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+123.sha-5d73b33
+ * @license Angular v12.0.0-next.8+126.sha-fe5bf7f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+123.sha-5d73b33');
+const VERSION = new Version('12.0.0-next.8+126.sha-fe5bf7f');
 
 /**
  * @license
