a5ed605e9e903bdee72aedb40bfa874cbe6d67f6
Andrew Kushnir <akushnir@google.com>
2021-04-16 16:23:47 +0000

    d3edc5c0f5 test(docs-infra): run all example tests in IVY runtime (#41638)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+124.sha-b23850e
+ * @license Angular v12.0.0-next.8+130.sha-d3edc5c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+124.sha-b23850e');
+    var VERSION = new core.Version('12.0.0-next.8+130.sha-d3edc5c');
 
     /**
      * @license
