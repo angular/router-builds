3d0e8a07213dbeb2c8187ae4c8946fe877ab2248
Andrew Kushnir <akushnir@google.com>
2021-04-16 21:32:12 +0000

    83d6a96163 feat(dev-infra): prevent pr merges of features or breaking changes on rc (#41660)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+132.sha-62e3f32
+ * @license Angular v12.0.0-next.8+133.sha-83d6a96
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+132.sha-62e3f32');
+const VERSION = new Version('12.0.0-next.8+133.sha-83d6a96');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+132.sha-62e3f32
+ * @license Angular v12.0.0-next.8+133.sha-83d6a96
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
