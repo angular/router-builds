86db19902c24afce4c7a69a433be882c28baa7f4
Andrew Kushnir <akushnir@google.com>
2021-04-17 00:47:19 +0000

    de25cdac54 docs: move lazy loading section to the best practices section (#41378)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+136.sha-96175fd
+ * @license Angular v12.0.0-next.8+138.sha-de25cda
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+136.sha-96175fd');
+const VERSION = new Version('12.0.0-next.8+138.sha-de25cda');
 
 /**
  * @license
