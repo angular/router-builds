f7b860032b0ca55e13dcce221f6e13f9843b67e8
Andrew Kushnir <akushnir@google.com>
2021-04-17 00:44:28 +0000

    d5a8302dfd docs: separate steps for github actions (#41594) PR Close #41594

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+133.sha-83d6a96
+ * @license Angular v12.0.0-next.8+135.sha-d5a8302
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+133.sha-83d6a96');
+const VERSION = new Version('12.0.0-next.8+135.sha-d5a8302');
 
 /**
  * @license
