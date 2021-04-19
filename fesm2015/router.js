7148c212a1702a20dbb82e11ebee8493f3167209
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:35:39 +0000

    ef4cf878b1 build: update google-closure-compiler to version 20210406.0.0 (#41687)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+169.sha-5e34317
+ * @license Angular v12.0.0-next.8+170.sha-ef4cf87
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+169.sha-5e34317');
+const VERSION = new Version('12.0.0-next.8+170.sha-ef4cf87');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+169.sha-5e34317
+ * @license Angular v12.0.0-next.8+170.sha-ef4cf87
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
