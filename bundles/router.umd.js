ff1cec59e012fc1b7d7ee6cc77cd49704051b21c
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:20:49 +0000

    13913231f1 test(docs-infra): change URL separator from `\t` to `-->` in `URLS_TO_REDIRECT.txt` (#41680)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+144.sha-75168a2
+ * @license Angular v12.0.0-next.8+147.sha-1391323
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+144.sha-75168a2');
+    var VERSION = new core.Version('12.0.0-next.8+147.sha-1391323');
 
     /**
      * @license
