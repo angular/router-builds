daee9bcc5b21f37a81a63527514f36a6afdb4f30
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:26:24 +0000

    185090b15d docs: add missing backticks in `schematics-for-libraries.md` (#41681)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+149.sha-6a153a2
+ * @license Angular v12.0.0-next.8+148.sha-185090b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+149.sha-6a153a2');
+const VERSION = new Version('12.0.0-next.8+148.sha-185090b');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+149.sha-6a153a2
+ * @license Angular v12.0.0-next.8+148.sha-185090b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
