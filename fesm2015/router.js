788e5ce93155901309752b0ef6cfabc5587ac7e6
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:42:19 +0000

    70341bcfed build(docs-infra): update docs examples to Angular v11.2.10 (#41689)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+172.sha-6cdb8fb
+ * @license Angular v12.0.0-next.8+175.sha-70341bc
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+172.sha-6cdb8fb');
+const VERSION = new Version('12.0.0-next.8+175.sha-70341bc');
 
 /**
  * @license
