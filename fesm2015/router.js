a5d8fbed5c91800da47feda3bec966649f8e75ac
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:30:18 +0000

    322db1755a docs: add missing preposition in the animations guide (#41697)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+160.sha-b64b6b3
+ * @license Angular v12.0.0-next.8+159.sha-322db17
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+160.sha-b64b6b3');
+const VERSION = new Version('12.0.0-next.8+159.sha-322db17');
 
 /**
  * @license
