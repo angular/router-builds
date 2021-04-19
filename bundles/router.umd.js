c372604792d6aa83433c88548c911d56bc3841a6
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:40:39 +0000

    c617f1f768 build: update jsdom to version 16.5.3 (#41695)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+170.sha-ef4cf87
+ * @license Angular v12.0.0-next.8+171.sha-c617f1f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+170.sha-ef4cf87');
+    var VERSION = new i0.Version('12.0.0-next.8+171.sha-c617f1f');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+170.sha-ef4cf87
+ * @license Angular v12.0.0-next.8+171.sha-c617f1f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+170.sha-ef4cf87');
+const VERSION = new Version('12.0.0-next.8+171.sha-c617f1f');
 
 /**
  * @license
