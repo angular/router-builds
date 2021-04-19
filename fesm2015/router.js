b6f9034467abb3f8f8d06cdfacb030ac9ace2e6e
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:06:42 +0000

    d58747de8a Revert "fix(zone.js): should continue to executue listeners when throw error (#41562)" (#41707)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+165.sha-e256d92
+ * @license Angular v12.0.0-next.8+167.sha-d58747d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+165.sha-e256d92');
+const VERSION = new Version('12.0.0-next.8+167.sha-d58747d');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+165.sha-e256d92
+ * @license Angular v12.0.0-next.8+167.sha-d58747d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
