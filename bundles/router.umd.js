afbdcf2d65334fea1b43951eacbf354235c803d3
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:28:31 +0000

    0c23c8b0ac build: update image-size to version 1.0.0 (#41691)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+148.sha-185090b
+ * @license Angular v12.0.0-next.8+154.sha-0c23c8b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+148.sha-185090b');
+    var VERSION = new i0.Version('12.0.0-next.8+154.sha-0c23c8b');
 
     /**
      * @license
