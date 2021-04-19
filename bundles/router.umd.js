eb632cb8dccc814765468dca7743c7b2424240f7
Andrew Kushnir <akushnir@google.com>
2021-04-19 22:30:29 +0000

    853016dca2 build: update firebase-tools to version 9.10.0 (#41686)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+175.sha-70341bc
+ * @license Angular v12.0.0-next.8+177.sha-853016d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+175.sha-70341bc');
+    var VERSION = new i0.Version('12.0.0-next.8+177.sha-853016d');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+175.sha-70341bc
+ * @license Angular v12.0.0-next.8+177.sha-853016d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+175.sha-70341bc');
+const VERSION = new Version('12.0.0-next.8+177.sha-853016d');
 
 /**
  * @license
