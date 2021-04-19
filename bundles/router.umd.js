ad206013c94dda4b9c745e9d77e935d98420569f
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:20:46 +0000

    3738dbba05 ci: disable automatic updates of rollup (#41672)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+142.sha-cf6c5c1
+ * @license Angular v12.0.0-next.8+145.sha-3738dbb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+142.sha-cf6c5c1');
+    var VERSION = new i0.Version('12.0.0-next.8+145.sha-3738dbb');
 
     /**
      * @license
