0c34214e1f788f140ce3c6a1e369e75d7df557b8
Andrew Kushnir <akushnir@google.com>
2021-04-19 23:18:50 +0000

    a76f07bd13 docs: documentation of descendants property of @ContentChildren (#35927)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+31.sha-3631934
+ * @license Angular v11.2.10+35.sha-a76f07b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6468,7 +6468,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.10+31.sha-3631934');
+    var VERSION = new core.Version('11.2.10+35.sha-a76f07b');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+31.sha-3631934
+ * @license Angular v11.2.10+35.sha-a76f07b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
