e6ecaa550135be69aad397ad7a0608f0b4483b61
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:15:21 +0000

    d0b1f99598 docs: fix broken links to CLI wiki in the "Component styles" guide (#41516)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+138.sha-de25cda
+ * @license Angular v12.0.0-next.8+140.sha-d0b1f99
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+138.sha-de25cda');
+    var VERSION = new i0.Version('12.0.0-next.8+140.sha-d0b1f99');
 
     /**
      * @license
