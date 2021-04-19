842b2d986754f6b4d8a27fe59db244bef2c7a79f
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:34:19 +0000

    5e343173e8 docs: fix styling issues in example CSS (#41222)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+167.sha-d58747d
+ * @license Angular v12.0.0-next.8+169.sha-5e34317
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+167.sha-d58747d');
+    var VERSION = new core.Version('12.0.0-next.8+169.sha-5e34317');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+167.sha-d58747d
+ * @license Angular v12.0.0-next.8+169.sha-5e34317
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+167.sha-d58747d');
+const VERSION = new Version('12.0.0-next.8+169.sha-5e34317');
 
 /**
  * @license
