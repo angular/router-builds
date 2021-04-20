0f14cfd0a9bd7784076dbf558ade46bf24e029a3
Andrew Kushnir <akushnir@google.com>
2021-04-20 16:57:18 +0000

    03f3dcb700 docs: add API CMS capability to Angular App resources (#40924)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+178.sha-251bec1
+ * @license Angular v12.0.0-next.8+179.sha-03f3dcb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+178.sha-251bec1');
+    var VERSION = new core.Version('12.0.0-next.8+179.sha-03f3dcb');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+178.sha-251bec1
+ * @license Angular v12.0.0-next.8+179.sha-03f3dcb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
