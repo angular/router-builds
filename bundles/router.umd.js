e16f5071ddcf30dcb9b24d16e225027c2b83e3ed
Andrew Kushnir <akushnir@google.com>
2021-04-20 17:01:33 +0000

    e238f2c33a docs: rewrites animation query () method use (#41710)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+179.sha-03f3dcb
+ * @license Angular v12.0.0-next.8+182.sha-e238f2c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+179.sha-03f3dcb');
+    var VERSION = new i0.Version('12.0.0-next.8+182.sha-e238f2c');
 
     /**
      * @license
