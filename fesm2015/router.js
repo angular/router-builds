1c00033735a9c9c8c3c9105bf8d9afd9700b691b
Andrew Kushnir <akushnir@google.com>
2021-04-20 19:36:18 +0000

    a2b671e61b docs: add margin to button boilerplate CSS for examples (#41413)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+186.sha-73824d5
+ * @license Angular v12.0.0-next.8+187.sha-a2b671e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+186.sha-73824d5');
+const VERSION = new Version('12.0.0-next.8+187.sha-a2b671e');
 
 /**
  * @license
