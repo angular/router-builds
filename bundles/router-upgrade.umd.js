c1d2c1f5e9366e8b542f5f7d1a6d2119d2d0e837
Andrew Kushnir <akushnir@google.com>
2021-04-20 23:20:01 +0000

    7bd8741109 build: update io_bazel_rules_sass to version 1.32.11 (#41713)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.9+87.sha-87b5026
+ * @license Angular v12.0.0-next.9+88.sha-7bd8741
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.9+87.sha-87b5026
+ * @license Angular v12.0.0-next.9+88.sha-7bd8741
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.9+87.sha-87b5026');
+const VERSION = new Version('12.0.0-next.9+88.sha-7bd8741');
 
 /**
  * @license
