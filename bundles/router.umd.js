ca603298cc629dd3f753579f0e816152858fbc1d
Andrew Kushnir <akushnir@google.com>
2021-04-21 17:53:28 +0000

    e7ab84051e docs: release notes for the v12.0.0-rc.0 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+189.sha-ff26e85
+ * @license Angular v12.0.0-next.8+190.sha-e7ab840
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+189.sha-ff26e85');
+    var VERSION = new i0.Version('12.0.0-next.8+190.sha-e7ab840');
 
     /**
      * @license
