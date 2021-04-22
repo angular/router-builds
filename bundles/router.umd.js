78e0fd65844e0d0058b205960db76d0e72c15377
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 18:04:19 +0000

    e0fb18a133 build: update @babel/cli to version 7.13.16 (#41734)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+202.sha-c3a512a
+ * @license Angular v12.0.0-next.8+203.sha-e0fb18a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+202.sha-c3a512a');
+    var VERSION = new i0.Version('12.0.0-next.8+203.sha-e0fb18a');
 
     /**
      * @license
