f21d52a38caa977905776ba93356855d64956617
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 15:59:25 +0000

    dfdd4a2d74 docs: ident the library shematics guide properly (#41755)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+198.sha-122c8ba
+ * @license Angular v12.0.0-next.8+199.sha-dfdd4a2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+198.sha-122c8ba');
+const VERSION = new Version('12.0.0-next.8+199.sha-dfdd4a2');
 
 /**
  * @license
