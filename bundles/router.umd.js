fe70459802065a2b172caeea3e98efc8fed3633f
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 18:10:56 +0000

    de4fafb818 refactor(core): add single type for injector token (#41580)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+204.sha-64567d3
+ * @license Angular v12.0.0-next.8+206.sha-de4fafb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+204.sha-64567d3');
+    var VERSION = new i0.Version('12.0.0-next.8+206.sha-de4fafb');
 
     /**
      * @license
