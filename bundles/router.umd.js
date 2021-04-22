63cf6e9eb0e593e0bfefff34e18c1424c7532883
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 00:51:21 +0000

    a4a55f0687 fix(dev-infra): verify python3 is available before release (#41753)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+195.sha-8bfd9e8
+ * @license Angular v12.0.0-next.8+196.sha-a4a55f0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+195.sha-8bfd9e8');
+    var VERSION = new core.Version('12.0.0-next.8+196.sha-a4a55f0');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+195.sha-8bfd9e8
+ * @license Angular v12.0.0-next.8+196.sha-a4a55f0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+195.sha-8bfd9e8');
+const VERSION = new Version('12.0.0-next.8+196.sha-a4a55f0');
 
 /**
  * @license
