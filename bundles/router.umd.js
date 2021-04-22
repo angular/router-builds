84e6eb0ad91581b8defedd7f65c74b4bacf51d17
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 15:58:45 +0000

    122c8ba0e3 build(docs-infra): fix `StackblitzBuilder` after `jsdom` update (#41725)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+196.sha-a4a55f0
+ * @license Angular v12.0.0-next.8+198.sha-122c8ba
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+196.sha-a4a55f0');
+    var VERSION = new core.Version('12.0.0-next.8+198.sha-122c8ba');
 
     /**
      * @license
