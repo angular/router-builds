bd6aa35509522465abc6cb2ba6bbc533a75a7179
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-23 18:58:35 +0000

    9f88da220c docs: Indicate `HttpClientModule` being imported (#41749)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+212.sha-5071331
+ * @license Angular v12.0.0-next.8+213.sha-9f88da2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+212.sha-5071331');
+    var VERSION = new core.Version('12.0.0-next.8+213.sha-9f88da2');
 
     /**
      * @license
