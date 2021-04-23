2fe41584be6e88acab554af6cc7cdb12d72c5f11
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-23 19:12:18 +0000

    73105aa7d6 ci: remove trailing comma from renovate config (#41790)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+213.sha-9f88da2
+ * @license Angular v12.0.0-next.8+214.sha-73105aa
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+213.sha-9f88da2');
+    var VERSION = new core.Version('12.0.0-next.8+214.sha-73105aa');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+213.sha-9f88da2
+ * @license Angular v12.0.0-next.8+214.sha-73105aa
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
