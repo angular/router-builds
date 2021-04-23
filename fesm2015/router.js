37075910e739d2637fa7e8fd94b7ab9dbf4f6cb5
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-23 19:13:36 +0000

    73105aa7d6 ci: remove trailing comma from renovate config (#41790)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+213.sha-9f88da2
+ * @license Angular v12.0.0-next.8+214.sha-73105aa
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+213.sha-9f88da2');
+const VERSION = new Version('12.0.0-next.8+214.sha-73105aa');
 
 /**
  * @license
