54d215acdd64d3b131a4f29bca5d7c037a2102e3
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-23 18:21:49 +0000

    50713310bf build: use platform from shared dev-infra package for remote execution (#41767)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+208.sha-98fc4f4
+ * @license Angular v12.0.0-next.8+212.sha-5071331
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+208.sha-98fc4f4');
+    var VERSION = new core.Version('12.0.0-next.8+212.sha-5071331');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+208.sha-98fc4f4
+ * @license Angular v12.0.0-next.8+212.sha-5071331
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
