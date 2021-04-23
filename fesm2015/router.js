a36da224f6f34283cfae629f17f0c451c97991b9
GitHub <noreply@github.com>
2021-04-23 18:51:34 +0000

    45ffab59f1 ci: remove trailing comma from renovate config

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+139.sha-aa35a1a
+ * @license Angular v12.0.0-next.8+213.sha-45ffab5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+139.sha-aa35a1a');
+const VERSION = new Version('12.0.0-next.8+213.sha-45ffab5');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+139.sha-aa35a1a
+ * @license Angular v12.0.0-next.8+213.sha-45ffab5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
