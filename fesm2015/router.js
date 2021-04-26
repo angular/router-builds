3b51e1ff47f4136f351aef4fe2c8354744b26dd1
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 22:42:36 +0000

    af12d8deaa fix(core): update supported range of node versions to only include LTS versions (#41822)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+233.sha-e47ea29
+ * @license Angular v12.0.0-next.8+251.sha-af12d8d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+233.sha-e47ea29');
+const VERSION = new Version('12.0.0-next.8+251.sha-af12d8d');
 
 /**
  * @license
