d5b379996ea39069d87588b66b3e6afb52e5007f
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 22:35:36 +0000

    e47ea2959c docs: add chau tran to GDE resources (#41817)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+232.sha-4b4f565
+ * @license Angular v12.0.0-next.8+233.sha-e47ea29
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+232.sha-4b4f565');
+    var VERSION = new core.Version('12.0.0-next.8+233.sha-e47ea29');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+232.sha-4b4f565
+ * @license Angular v12.0.0-next.8+233.sha-e47ea29
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
