f6faf00e50fa24b8d02894b7dbb513a7b60a1449
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:47:41 +0000

    bd986cb460 build: update bazel packages (#41795)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+218.sha-27ad762
+ * @license Angular v12.0.0-next.8+220.sha-bd986cb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+218.sha-27ad762');
+    var VERSION = new i0.Version('12.0.0-next.8+220.sha-bd986cb');
 
     /**
      * @license
