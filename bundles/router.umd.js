7306450b1c7d05ba6422e9e1588867b561ed1278
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:31:44 +0000

    70fb3e958b docs: improve accessibility of http example (#41057)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+214.sha-73105aa
+ * @license Angular v12.0.0-next.8+215.sha-70fb3e9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+214.sha-73105aa');
+    var VERSION = new i0.Version('12.0.0-next.8+215.sha-70fb3e9');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+214.sha-73105aa
+ * @license Angular v12.0.0-next.8+215.sha-70fb3e9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
