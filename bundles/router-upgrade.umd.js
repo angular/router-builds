8ffd2ac45f616c9a97ffbc2721e22e6cc9a27025
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 19:31:45 +0000

    4b4f565df1 docs: fix links to docs with ambiguous paths (#41788)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+228.sha-295889e
+ * @license Angular v12.0.0-next.8+232.sha-4b4f565
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+228.sha-295889e
+ * @license Angular v12.0.0-next.8+232.sha-4b4f565
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+228.sha-295889e');
+const VERSION = new Version('12.0.0-next.8+232.sha-4b4f565');
 
 /**
  * @license
