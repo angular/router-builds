aa381ae92ae0432708b0abba696e933d1c42bf7f
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 18:20:59 +0000

    a938849148 fix(docs-infra): apply custom `autoLinkCode` filters to single-word `<code>` elements (#41709)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+227.sha-99f2ffc
+ * @license Angular v12.0.0-next.8+226.sha-a938849
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+227.sha-99f2ffc');
+    var VERSION = new core.Version('12.0.0-next.8+226.sha-a938849');
 
     /**
      * @license
