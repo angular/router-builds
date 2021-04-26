f7d03e6bbc9ff797420dafab71903ac6496dd633
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 18:22:20 +0000

    a938849148 fix(docs-infra): apply custom `autoLinkCode` filters to single-word `<code>` elements (#41709)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+227.sha-99f2ffc
+ * @license Angular v12.0.0-next.8+226.sha-a938849
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+227.sha-99f2ffc');
+const VERSION = new Version('12.0.0-next.8+226.sha-a938849');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+227.sha-99f2ffc
+ * @license Angular v12.0.0-next.8+226.sha-a938849
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
