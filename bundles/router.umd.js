100b8bd3ad3f5b44605d9625748dffeec9d46538
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:30:41 +0000

    326884736e docs: make inputs-outputs example more accessible (#41288)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+215.sha-70fb3e9
+ * @license Angular v12.0.0-next.8+216.sha-3268847
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+215.sha-70fb3e9');
+    var VERSION = new core.Version('12.0.0-next.8+216.sha-3268847');
 
     /**
      * @license
