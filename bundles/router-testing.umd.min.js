7ccd6b137b30cfe03386c50107e2c62578fac563
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 23:20:33 +0000

    7756ead0bf docs: add metadata documentation to httpClient (#41706)

--- bundles/router-testing.umd.min.js
+++ bundles/router-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+18.sha-aa578f5
+ * @license Angular v11.2.11+20.sha-7756ead
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+18.sha-aa578f5
+ * @license Angular v11.2.11+20.sha-7756ead
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5816,7 +5816,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.11+18.sha-aa578f5');
+const VERSION = new Version('11.2.11+20.sha-7756ead');
 
 /**
  * @license
