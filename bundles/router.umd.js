b0ee00f29e4245e9a39ab8d437b6fa5fca7a7160
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 17:04:38 +0000

    21f1466d84 build(docs-infra): fix e2e testing SystemJS-based docs examples (#41796)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+10.sha-163c076
+ * @license Angular v11.2.11+15.sha-21f1466
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6468,7 +6468,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.11+10.sha-163c076');
+    var VERSION = new core.Version('11.2.11+15.sha-21f1466');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+10.sha-163c076
+ * @license Angular v11.2.11+15.sha-21f1466
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
