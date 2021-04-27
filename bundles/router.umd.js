dfac8f534f141908f7b70331188ddc7061628ae7
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 17:38:05 +0000

    3783976543 docs: remove the section "Using components vs services from other modules" (#41835)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+253.sha-f978976
+ * @license Angular v12.0.0-next.8+255.sha-3783976
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+253.sha-f978976');
+    var VERSION = new i0.Version('12.0.0-next.8+255.sha-3783976');
 
     /**
      * @license
