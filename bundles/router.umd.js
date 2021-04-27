003c8d101a4840b43815a8be99aff1a3d7b7e14c
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 16:40:09 +0000

    da6ed1562e fix(compiler): strip scoped selectors from `@font-face` rules (#41815)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+251.sha-af12d8d
+ * @license Angular v12.0.0-next.8+252.sha-da6ed15
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+251.sha-af12d8d');
+    var VERSION = new core.Version('12.0.0-next.8+252.sha-da6ed15');
 
     /**
      * @license
