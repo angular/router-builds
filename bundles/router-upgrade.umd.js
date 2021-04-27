653dbbfa1345db77ff1cbff33cfe2a84254cf018
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 20:46:37 +0000

    d580b27195 ci: several updates to renovate configuration (#41834)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+258.sha-0140d04
+ * @license Angular v12.0.0-next.8+259.sha-d580b27
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+258.sha-0140d04
+ * @license Angular v12.0.0-next.8+259.sha-d580b27
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+258.sha-0140d04');
+    var VERSION = new i0.Version('12.0.0-next.8+259.sha-d580b27');
 
     /**
      * @license
