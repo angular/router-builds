0176b6ee46cd9d9648169cd8c1a4ecf92b6d7658
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 18:57:08 +0000

    e7586581af ci: add Zach Arend to pullapprove groups (#41839)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+23.sha-84a44a9
+ * @license Angular v11.2.11+24.sha-e758658
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6468,7 +6468,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.11+23.sha-84a44a9');
+    var VERSION = new core.Version('11.2.11+24.sha-e758658');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+23.sha-84a44a9
+ * @license Angular v11.2.11+24.sha-e758658
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
