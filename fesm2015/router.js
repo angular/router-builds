f8473db5bff116e2ced21e460e9f117d9d10a90d
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 17:36:41 +0000

    3783976543 docs: remove the section "Using components vs services from other modules" (#41835)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+253.sha-f978976
+ * @license Angular v12.0.0-next.8+255.sha-3783976
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+253.sha-f978976');
+const VERSION = new Version('12.0.0-next.8+255.sha-3783976');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+253.sha-f978976
+ * @license Angular v12.0.0-next.8+255.sha-3783976
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
