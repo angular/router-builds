1bc26e193bc7359199b24c45613cc6ea9341150f
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 19:45:12 +0000

    0140d04ced feat(dev-infra): create the prettier formatter for ng-dev format tooling (#41824)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+257.sha-cd004d4
+ * @license Angular v12.0.0-next.8+258.sha-0140d04
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+257.sha-cd004d4');
+const VERSION = new Version('12.0.0-next.8+258.sha-0140d04');
 
 /**
  * @license
