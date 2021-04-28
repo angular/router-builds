98d8349b15d6d079836c7809f0f67a6aec89f445
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 21:53:58 +0000

    45792f145c docs: remove tslint, codelyzer & protractor from dev deps (#41826)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.1+6.sha-c691b66
+ * @license Angular v12.0.0-rc.1+9.sha-45792f1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.1+6.sha-c691b66
+ * @license Angular v12.0.0-rc.1+9.sha-45792f1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-rc.1+6.sha-c691b66');
+    var VERSION = new core.Version('12.0.0-rc.1+9.sha-45792f1');
 
     /**
      * @license
