f7428984dfa1e4f303e1616c6a4662c00ba35006
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 22:46:14 +0000

    55d9713664 docs: Edits to remove jargon (#41536)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+281.sha-c31d6ea
+ * @license Angular v12.0.0-next.8+282.sha-55d9713
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+281.sha-c31d6ea');
+    var VERSION = new core.Version('12.0.0-next.8+282.sha-55d9713');
 
     /**
      * @license
