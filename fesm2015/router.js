9115ca70c379cbe2a422a174f8ff62be124bf907
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 17:19:30 +0000

    641fa6944f ci: re-enable the `@angular/components` unit tests (#41816)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+269.sha-044b380
+ * @license Angular v12.0.0-next.8+270.sha-641fa69
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+269.sha-044b380');
+const VERSION = new Version('12.0.0-next.8+270.sha-641fa69');
 
 /**
  * @license
