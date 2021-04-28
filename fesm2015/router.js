7402bbfe42fba8d2e117b80828ae22e273bb0430
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 16:47:02 +0000

    044b380e5a build: update remark packages (#41721)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+268.sha-1aed2f3
+ * @license Angular v12.0.0-next.8+269.sha-044b380
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+268.sha-1aed2f3');
+const VERSION = new Version('12.0.0-next.8+269.sha-044b380');
 
 /**
  * @license
