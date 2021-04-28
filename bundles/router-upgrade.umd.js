4a3d1d58d118ad2eaa207f9ae973e7bff18f2ab3
GitHub <noreply@github.com>
2021-04-28 19:18:37 +0000

    98de217640 release: cut the v12.0.0-rc.1 release (#41864)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.0+80.sha-a66421e
+ * @license Angular v12.0.0-rc.0+81.sha-98de217
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.0+80.sha-a66421e
+ * @license Angular v12.0.0-rc.0+81.sha-98de217
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-rc.0+80.sha-a66421e');
+    var VERSION = new core.Version('12.0.0-rc.0+81.sha-98de217');
 
     /**
      * @license
