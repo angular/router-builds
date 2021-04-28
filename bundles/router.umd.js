90518fb2f2fbf0325f632bd628a5ed335424e25e
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 19:28:59 +0000

    bbda73e55a docs: release notes for the v12.0.0-rc.1 release

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+271.sha-444775c
+ * @license Angular v12.0.0-next.8+272.sha-bbda73e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+271.sha-444775c');
+    var VERSION = new core.Version('12.0.0-next.8+272.sha-bbda73e');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+271.sha-444775c
+ * @license Angular v12.0.0-next.8+272.sha-bbda73e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+271.sha-444775c');
+const VERSION = new Version('12.0.0-next.8+272.sha-bbda73e');
 
 /**
  * @license
