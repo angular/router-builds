1d1573ab8200d60f69710bec420eca8a77789d1f
Misko Hevery <misko@hevery.com>
2021-04-29 21:14:24 +0000

    0255a2c631 docs(docs-infra): fix handling of client-side errors in networking (#36608)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.1+16.sha-77b77ff
+ * @license Angular v12.0.0-rc.1+20.sha-0255a2c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.1+16.sha-77b77ff
+ * @license Angular v12.0.0-rc.1+20.sha-0255a2c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-rc.1+16.sha-77b77ff');
+const VERSION = new Version('12.0.0-rc.1+20.sha-0255a2c');
 
 /**
  * @license
