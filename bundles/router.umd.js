7ea6ad5451576e0ca660be67921b280ac6e83528
Misko Hevery <misko@hevery.com>
2021-04-29 17:29:55 +0000

    d80ad951c6 docs: correct toh router tutorial (#41859)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+283.sha-595a75c
+ * @license Angular v12.0.0-next.8+284.sha-d80ad95
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+283.sha-595a75c');
+    var VERSION = new core.Version('12.0.0-next.8+284.sha-d80ad95');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+283.sha-595a75c
+ * @license Angular v12.0.0-next.8+284.sha-d80ad95
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
