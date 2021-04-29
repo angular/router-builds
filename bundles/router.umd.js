02b06670f660ce5b3fece83e61fb28b39f6904c2
Misko Hevery <misko@hevery.com>
2021-04-29 17:45:09 +0000

    05209fa397 ci: exclude packages which don't have a major from being batched (#41865)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+288.sha-f3331a8
+ * @license Angular v12.0.0-next.8+286.sha-05209fa
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+288.sha-f3331a8');
+    var VERSION = new core.Version('12.0.0-next.8+286.sha-05209fa');
 
     /**
      * @license
