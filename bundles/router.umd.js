b970376710e432f355ecc505c55610a1ee1d2420
Misko Hevery <misko@hevery.com>
2021-04-30 20:50:00 +0000

    0dfd940a18 Revert "build: update dependency karma to v6 (#41696)" (#41904)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+293.sha-ea89617
+ * @license Angular v12.0.0-next.8+294.sha-0dfd940
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+293.sha-ea89617');
+    var VERSION = new i0.Version('12.0.0-next.8+294.sha-0dfd940');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+293.sha-ea89617
+ * @license Angular v12.0.0-next.8+294.sha-0dfd940
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+293.sha-ea89617');
+const VERSION = new Version('12.0.0-next.8+294.sha-0dfd940');
 
 /**
  * @license
