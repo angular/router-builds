d6a4c7f70b727ff00b628b9d3aa4d9ffcf1a3f8e
Misko Hevery <misko@hevery.com>
2021-04-30 21:34:17 +0000

    2dd96e08ae Revert "perf(core): avoid storing LView in __ngContext__ (#41358)" (#41901)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+294.sha-0dfd940
+ * @license Angular v12.0.0-next.8+295.sha-2dd96e0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+294.sha-0dfd940');
+    var VERSION = new core.Version('12.0.0-next.8+295.sha-2dd96e0');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+294.sha-0dfd940
+ * @license Angular v12.0.0-next.8+295.sha-2dd96e0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
