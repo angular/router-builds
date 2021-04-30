f5a78ba091f3d208c1a2c2c8293e55b41e59d461
Misko Hevery <misko@hevery.com>
2021-04-30 21:38:14 +0000

    72384a3357 fix(docs-infra): switch over to img[loading=lazy] for front page images (#34900)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+296.sha-6581a1b
+ * @license Angular v12.0.0-next.8+300.sha-72384a3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+296.sha-6581a1b');
+const VERSION = new Version('12.0.0-next.8+300.sha-72384a3');
 
 /**
  * @license
