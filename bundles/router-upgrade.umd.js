d87f661d49d64b60d336ebefc2ec9026c10d773d
Misko Hevery <misko@hevery.com>
2021-04-30 21:42:32 +0000

    35450c78f7 fix(compiler-cli): prefer non-aliased exports in reference emitters (#41866)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+300.sha-72384a3
+ * @license Angular v12.0.0-next.8+298.sha-35450c7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+300.sha-72384a3
+ * @license Angular v12.0.0-next.8+298.sha-35450c7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+300.sha-72384a3');
+const VERSION = new Version('12.0.0-next.8+298.sha-35450c7');
 
 /**
  * @license
