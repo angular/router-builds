48542deafd825c548a8c11663ed7f13fea8085b2
Misko Hevery <misko@hevery.com>
2021-05-03 17:26:26 +0000

    38f47be7af docs: update tagline (#41922)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+309.sha-f12d067
+ * @license Angular v12.0.0-next.8+310.sha-38f47be
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+309.sha-f12d067');
+const VERSION = new Version('12.0.0-next.8+310.sha-38f47be');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+309.sha-f12d067
+ * @license Angular v12.0.0-next.8+310.sha-38f47be
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
