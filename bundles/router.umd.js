da1a4edd440300270ffe0a0d5e6839c31529ddbf
Misko Hevery <misko@hevery.com>
2021-05-03 17:31:15 +0000

    6967f3ca28 build(docs-infra): revert `watchr` to v3.0.1 to restore `docs-watch` performance (#41903)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+310.sha-38f47be
+ * @license Angular v12.0.0-next.8+311.sha-6967f3c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+310.sha-38f47be');
+    var VERSION = new i0.Version('12.0.0-next.8+311.sha-6967f3c');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+310.sha-38f47be
+ * @license Angular v12.0.0-next.8+311.sha-6967f3c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
