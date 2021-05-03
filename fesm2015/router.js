e3224c6655e84a6d4162aeef3f3e9f2a00258938
Misko Hevery <misko@hevery.com>
2021-05-03 17:29:54 +0000

    6967f3ca28 build(docs-infra): revert `watchr` to v3.0.1 to restore `docs-watch` performance (#41903)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+310.sha-38f47be
+ * @license Angular v12.0.0-next.8+311.sha-6967f3c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+310.sha-38f47be');
+const VERSION = new Version('12.0.0-next.8+311.sha-6967f3c');
 
 /**
  * @license
