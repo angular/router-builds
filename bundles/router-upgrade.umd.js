5a747b5f5311694e7cc3ad06f213abf65a9371ed
Misko Hevery <misko@hevery.com>
2021-05-03 21:48:48 +0000

    f42796a8f5 build: lock file maintenance (#41924)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+316.sha-3777cf5
+ * @license Angular v12.0.0-next.8+315.sha-f42796a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+316.sha-3777cf5
+ * @license Angular v12.0.0-next.8+315.sha-f42796a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+316.sha-3777cf5');
+const VERSION = new Version('12.0.0-next.8+315.sha-f42796a');
 
 /**
  * @license
