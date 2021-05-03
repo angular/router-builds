d7f913294662189e575db29fa536c45740489c42
Misko Hevery <misko@hevery.com>
2021-05-03 21:48:17 +0000

    3b589030a8 docs: Remove references to protractor from documentation (#41731)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+312.sha-253ed5d
+ * @license Angular v12.0.0-next.8+313.sha-3b58903
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+312.sha-253ed5d
+ * @license Angular v12.0.0-next.8+313.sha-3b58903
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+312.sha-253ed5d');
+const VERSION = new Version('12.0.0-next.8+313.sha-3b58903');
 
 /**
  * @license
