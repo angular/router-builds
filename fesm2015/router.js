89b770d55bf75a0005774aaac4b2a48deba1876d
Misko Hevery <misko@hevery.com>
2021-05-03 21:50:21 +0000

    741c295129 docs: update file structure document (#41923)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+315.sha-f42796a
+ * @license Angular v12.0.0-next.8+314.sha-741c295
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+315.sha-f42796a');
+const VERSION = new Version('12.0.0-next.8+314.sha-741c295');
 
 /**
  * @license
