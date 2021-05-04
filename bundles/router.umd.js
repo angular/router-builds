b87266352fa824ee6d9eddf86c7694d244fe51f3
Misko Hevery <misko@hevery.com>
2021-05-04 15:38:45 +0000

    6d7d659919 docs: add new migration guide to v12 documentation that describes how to migrate older localization ids to new ones (#41828)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+318.sha-b7db98f
+ * @license Angular v12.0.0-next.8+319.sha-6d7d659
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+318.sha-b7db98f');
+    var VERSION = new i0.Version('12.0.0-next.8+319.sha-6d7d659');
 
     /**
      * @license
