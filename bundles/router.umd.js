19b1bfca5f213a645f2a9d5a1758bfc24450237b
Misko Hevery <misko@hevery.com>
2021-05-04 15:56:26 +0000

    e387d22f83 fix(core): AsyncPipe now compatible with RxJS 7 (#41590)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+321.sha-dac78d1
+ * @license Angular v12.0.0-next.8+323.sha-e387d22
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+321.sha-dac78d1');
+    var VERSION = new core.Version('12.0.0-next.8+323.sha-e387d22');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+321.sha-dac78d1
+ * @license Angular v12.0.0-next.8+323.sha-e387d22
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
