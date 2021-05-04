a8c2a12fc131ff885a8c74fe9a0a313462b3d926
Misko Hevery <misko@hevery.com>
2021-05-04 15:40:25 +0000

    4dae97de43 docs: update Node.js supported version statement (#41820)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+319.sha-6d7d659
+ * @license Angular v12.0.0-next.8+320.sha-4dae97d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+319.sha-6d7d659');
+const VERSION = new Version('12.0.0-next.8+320.sha-4dae97d');
 
 /**
  * @license
