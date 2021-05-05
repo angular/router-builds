3194177c7d11145a34e30df0b60f4ff9d87f3f98
Misko Hevery <misko@hevery.com>
2021-05-05 23:19:12 +0000

    d7454a1685 docs: release notes for the v12.0.0-rc.2 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+339.sha-c472c5b
+ * @license Angular v12.0.0-next.8+342.sha-d7454a1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+339.sha-c472c5b');
+const VERSION = new Version('12.0.0-next.8+342.sha-d7454a1');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+339.sha-c472c5b
+ * @license Angular v12.0.0-next.8+342.sha-d7454a1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
