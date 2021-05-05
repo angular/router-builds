2ef5692190ba447e185f5ecdb96634b48497f07b
Misko Hevery <misko@hevery.com>
2021-05-05 04:25:33 +0000

    fdf2e02bff build: fix crash when validating commit messages (#41949)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+323.sha-e387d22
+ * @license Angular v12.0.0-next.8+325.sha-fdf2e02
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+323.sha-e387d22');
+const VERSION = new Version('12.0.0-next.8+325.sha-fdf2e02');
 
 /**
  * @license
