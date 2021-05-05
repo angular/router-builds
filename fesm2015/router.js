3a3df621159e348815dfc8497e8bc005cbb302c4
Misko Hevery <misko@hevery.com>
2021-05-05 21:09:28 +0000

    530e45f093 fix(dev-infra): publish script python check not working on windows (#41943)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+338.sha-931d40b
+ * @license Angular v12.0.0-next.8+336.sha-530e45f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+338.sha-931d40b');
+const VERSION = new Version('12.0.0-next.8+336.sha-530e45f');
 
 /**
  * @license
