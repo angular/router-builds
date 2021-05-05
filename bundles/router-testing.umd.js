e985d2c268c8c7aeb31b4daceb8ff02ff7114f2f
Misko Hevery <misko@hevery.com>
2021-05-05 04:26:52 +0000

    fdf2e02bff build: fix crash when validating commit messages (#41949)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+323.sha-e387d22
+ * @license Angular v12.0.0-next.8+325.sha-fdf2e02
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+323.sha-e387d22
+ * @license Angular v12.0.0-next.8+325.sha-fdf2e02
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+323.sha-e387d22');
+    var VERSION = new i0.Version('12.0.0-next.8+325.sha-fdf2e02');
 
     /**
      * @license
