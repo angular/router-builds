fbd69605a32b30109347d03e5ffacd1130b7d2af
Misko Hevery <misko@hevery.com>
2021-05-05 21:10:52 +0000

    530e45f093 fix(dev-infra): publish script python check not working on windows (#41943)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+338.sha-931d40b
+ * @license Angular v12.0.0-next.8+336.sha-530e45f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+338.sha-931d40b
+ * @license Angular v12.0.0-next.8+336.sha-530e45f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+338.sha-931d40b');
+    var VERSION = new i0.Version('12.0.0-next.8+336.sha-530e45f');
 
     /**
      * @license
