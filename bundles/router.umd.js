1af0459c2ccd5353f97490cead180804eb17d64a
Misko Hevery <misko@hevery.com>
2021-05-05 21:10:10 +0000

    931d40b18a docs: add documentation for Angular linker (#41485)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+334.sha-fb38175
+ * @license Angular v12.0.0-next.8+338.sha-931d40b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+334.sha-fb38175');
+    var VERSION = new i0.Version('12.0.0-next.8+338.sha-931d40b');
 
     /**
      * @license
