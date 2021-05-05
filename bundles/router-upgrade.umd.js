7b380501fe865821f7c296faab26ddd6bd95de8b
Misko Hevery <misko@hevery.com>
2021-05-05 04:34:54 +0000

    5947239f89 fix(dev-infra): do not set lts dist tag on packages from release-candidate train (#41946)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+330.sha-378bb04
+ * @license Angular v12.0.0-next.8+329.sha-5947239
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+330.sha-378bb04
+ * @license Angular v12.0.0-next.8+329.sha-5947239
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+330.sha-378bb04');
+    var VERSION = new core.Version('12.0.0-next.8+329.sha-5947239');
 
     /**
      * @license
