ac407e821bf04617ff85574a1018a683bed69e90
Misko Hevery <misko@hevery.com>
2021-05-05 21:05:52 +0000

    753fb06cef docs: remove reference to strict mode in local setup (#41952)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+335.sha-21c2a06
+ * @license Angular v12.0.0-next.8+333.sha-753fb06
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+335.sha-21c2a06');
+const VERSION = new Version('12.0.0-next.8+333.sha-753fb06');
 
 /**
  * @license
