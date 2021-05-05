0a965649f8646e33192714e2bf5f2b3e9e908ffe
Misko Hevery <misko@hevery.com>
2021-05-05 04:33:46 +0000

    378bb04d7b docs(common): Corrected the wrong formatting (#41945)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+327.sha-43cc5a1
+ * @license Angular v12.0.0-next.8+330.sha-378bb04
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+327.sha-43cc5a1');
+    var VERSION = new core.Version('12.0.0-next.8+330.sha-378bb04');
 
     /**
      * @license
