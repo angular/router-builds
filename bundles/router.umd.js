30eb851650e0a326c44df50033d463d8591bbd98
Misko Hevery <misko@hevery.com>
2021-05-05 04:27:25 +0000

    43cc5a126b docs: add dylhunn to contributors (#41936)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+325.sha-fdf2e02
+ * @license Angular v12.0.0-next.8+327.sha-43cc5a1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+325.sha-fdf2e02');
+    var VERSION = new core.Version('12.0.0-next.8+327.sha-43cc5a1');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+325.sha-fdf2e02
+ * @license Angular v12.0.0-next.8+327.sha-43cc5a1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
