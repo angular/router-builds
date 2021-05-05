7bf35e205c87b228bb78d03e27e52c1d291b7a79
Misko Hevery <misko@hevery.com>
2021-05-05 04:39:51 +0000

    6b8baad940 fix(common): add body as an optional property on the options of HttpClient.delete (#19438) (#41723)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+331.sha-19b12d7
+ * @license Angular v12.0.0-next.8+332.sha-6b8baad
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+331.sha-19b12d7');
+    var VERSION = new core.Version('12.0.0-next.8+332.sha-6b8baad');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+331.sha-19b12d7
+ * @license Angular v12.0.0-next.8+332.sha-6b8baad
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
