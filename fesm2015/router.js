b1fac91e4c8ad347cf7ab0f237d7782a5b491045
Misko Hevery <misko@hevery.com>
2021-05-05 04:34:59 +0000

    378bb04d7b docs(common): Corrected the wrong formatting (#41945)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+327.sha-43cc5a1
+ * @license Angular v12.0.0-next.8+330.sha-378bb04
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+327.sha-43cc5a1');
+const VERSION = new Version('12.0.0-next.8+330.sha-378bb04');
 
 /**
  * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+327.sha-43cc5a1
+ * @license Angular v12.0.0-next.8+330.sha-378bb04
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
