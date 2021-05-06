8f4534d4e6d37a5cdf006addeae028f9eb61793e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 20:07:31 +0000

    640ec7828f refactor(common): change missing NgSwitch provider error message (#41704)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+352.sha-3a5f006
+ * @license Angular v12.0.0-next.8+354.sha-640ec78
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+352.sha-3a5f006');
+    var VERSION = new i0.Version('12.0.0-next.8+354.sha-640ec78');
 
     /**
      * @license
