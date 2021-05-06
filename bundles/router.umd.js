e40da2295b3510d1bfc4c12e1372f69356377841
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 13:54:47 +0000

    abcd4bbfaa fix(compiler): preserve @page rules in encapsulated styles (#41915)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+343.sha-efe8566
+ * @license Angular v12.0.0-next.8+344.sha-abcd4bb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+343.sha-efe8566');
+    var VERSION = new i0.Version('12.0.0-next.8+344.sha-abcd4bb');
 
     /**
      * @license
