9e61253bf6597f4b8bbc5ddf2cc88e5a09a1533d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 14:00:01 +0000

    35fbd4f411 build: update dependency fs-extra to v10 (#41937)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+344.sha-abcd4bb
+ * @license Angular v12.0.0-next.8+345.sha-35fbd4f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+344.sha-abcd4bb');
+const VERSION = new Version('12.0.0-next.8+345.sha-35fbd4f');
 
 /**
  * @license
