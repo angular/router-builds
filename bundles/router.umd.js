f78053d7948af2f25a09e0289627e773d2ee248e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 19:23:41 +0000

    a4e3ea0311 feat(docs-infra): add support for unformatted code-snippets (#41335)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+348.sha-90d4b22
+ * @license Angular v12.0.0-next.8+351.sha-a4e3ea0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+348.sha-90d4b22');
+    var VERSION = new core.Version('12.0.0-next.8+351.sha-a4e3ea0');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+348.sha-90d4b22
+ * @license Angular v12.0.0-next.8+351.sha-a4e3ea0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
