b59b2cccab836693ddbeb5c5a9156c14df5d470a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 20:10:22 +0000

    4d7294d9f7 feat(dev-infra): include release notes in Github release entries when publishing (#41964)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+354.sha-640ec78
+ * @license Angular v12.0.0-next.8+355.sha-4d7294d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+354.sha-640ec78');
+const VERSION = new Version('12.0.0-next.8+355.sha-4d7294d');
 
 /**
  * @license
