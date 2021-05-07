73128a2bbab95749b3eabcc1d2cc428b96b45e1c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 15:06:37 +0000

    4bc3cf216d feat(common): add URLSearchParams to request body (#37852)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+367.sha-cb418bd
+ * @license Angular v12.0.0-next.8+370.sha-4bc3cf2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+367.sha-cb418bd');
+    var VERSION = new core.Version('12.0.0-next.8+370.sha-4bc3cf2');
 
     /**
      * @license
