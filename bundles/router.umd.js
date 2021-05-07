2a42c3cd0f218bf9afcd69e527938ad664269866
Andrew Scott <atscott@google.com>
2021-05-07 01:31:08 +0000

    e29eb8abfb docs: reword a few phrases in the GITHUB_PROCESS.md doc (#41977)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+355.sha-4d7294d
+ * @license Angular v12.0.0-next.8+364.sha-e29eb8a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+355.sha-4d7294d');
+    var VERSION = new i0.Version('12.0.0-next.8+364.sha-e29eb8a');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+355.sha-4d7294d
+ * @license Angular v12.0.0-next.8+364.sha-e29eb8a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
