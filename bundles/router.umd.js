584eca2639f866a8bbdd4f56d3685f5ad2ed149c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 14:59:24 +0000

    089daea98b refactor(compiler-cli): remove redundant `potentialSourceMapUrl` property (#42000)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+387.sha-ddf2a6c
+ * @license Angular v12.0.0-next.8+385.sha-089daea
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+387.sha-ddf2a6c');
+    var VERSION = new i0.Version('12.0.0-next.8+385.sha-089daea');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+387.sha-ddf2a6c
+ * @license Angular v12.0.0-next.8+385.sha-089daea
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
