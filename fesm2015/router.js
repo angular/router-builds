5fdc339a3eb3c8ce31d3449fce3f17f89565feef
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 14:58:53 +0000

    ddf2a6c975 build: update node version to stop aio from breaking while generating docs (#42009)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+386.sha-a529d4f
+ * @license Angular v12.0.0-next.8+387.sha-ddf2a6c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+386.sha-a529d4f');
+const VERSION = new Version('12.0.0-next.8+387.sha-ddf2a6c');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+386.sha-a529d4f
+ * @license Angular v12.0.0-next.8+387.sha-ddf2a6c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
