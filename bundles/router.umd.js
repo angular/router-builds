29b2b828cc5abd1a1f9844136d15b0a836d7e836
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 22:18:08 +0000

    eac42e9889 docs: release notes for the v12.0.0-rc.3 release (#42032)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+394.sha-96624b7
+ * @license Angular v12.0.0-next.8+395.sha-eac42e9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+394.sha-96624b7');
+    var VERSION = new core.Version('12.0.0-next.8+395.sha-eac42e9');
 
     /**
      * @license
