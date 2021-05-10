0c384d82094e82c4d97a310bb7d75890bb0776c4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 14:57:58 +0000

    089daea98b refactor(compiler-cli): remove redundant `potentialSourceMapUrl` property (#42000)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+387.sha-ddf2a6c
+ * @license Angular v12.0.0-next.8+385.sha-089daea
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+387.sha-ddf2a6c');
+const VERSION = new Version('12.0.0-next.8+385.sha-089daea');
 
 /**
  * @license
