a2a80137ecb4c919be2163767949f9f740802787
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 20:50:06 +0000

    96624b71a7 fix(platform-browser): prevent memory leak of style nodes if shadow DOM encapsulation is used (#42005)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+391.sha-6ebb86f
+ * @license Angular v12.0.0-next.8+394.sha-96624b7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+391.sha-6ebb86f');
+const VERSION = new Version('12.0.0-next.8+394.sha-96624b7');
 
 /**
  * @license
