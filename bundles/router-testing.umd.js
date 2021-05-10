8d98213f8916cb236407a50eca30e24f833b717f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 14:48:38 +0000

    23f6b76d1a fix(docs-infra): update glossary to include the term template-input-variable (#41632)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+380.sha-d59f2b0
+ * @license Angular v12.0.0-next.8+376.sha-23f6b76
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+380.sha-d59f2b0
+ * @license Angular v12.0.0-next.8+376.sha-23f6b76
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+380.sha-d59f2b0');
+    var VERSION = new core.Version('12.0.0-next.8+376.sha-23f6b76');
 
     /**
      * @license
