7016e76a35c4eda219ebf9ddf5002ef8705dd85f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 17:05:34 +0000

    6ebb86f9a2 docs: clarify the IE11 deprecation timeline, and link to the RFC (#42023)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+389.sha-d2296bc
+ * @license Angular v12.0.0-next.8+391.sha-6ebb86f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+389.sha-d2296bc');
+const VERSION = new Version('12.0.0-next.8+391.sha-6ebb86f');
 
 /**
  * @license
