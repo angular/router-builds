aefe97d2f2746441dc6a3ec6625ff64b52cbff04
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-12 21:31:39 +0000

    dad205e502 docs: add release notes for v12.0.0 to CHANGELOG

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+396.sha-a74eb52
+ * @license Angular v12.0.0-next.8+397.sha-dad205e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+396.sha-a74eb52');
+    var VERSION = new i0.Version('12.0.0-next.8+397.sha-dad205e');
 
     /**
      * @license
