c8cb295e8d6c86e7454fac48cf7966907f25bad1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-12 21:51:07 +0000

    65ee1c3f50 docs: release notes for the v11.2.14 release CHANGELOG.md

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+397.sha-dad205e
+ * @license Angular v12.0.0-next.8+398.sha-65ee1c3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+397.sha-dad205e
+ * @license Angular v12.0.0-next.8+398.sha-65ee1c3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+397.sha-dad205e');
+    var VERSION = new i0.Version('12.0.0-next.8+398.sha-65ee1c3');
 
     /**
      * @license
