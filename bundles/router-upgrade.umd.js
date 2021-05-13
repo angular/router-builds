18715e51fcbdee0a762591e618150eed8c98ce19
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-13 01:05:27 +0000

    53cd7e5eb5 docs: update the angular roadmap (#42050)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+399.sha-d34ce6b
+ * @license Angular v12.0.0-next.8+401.sha-53cd7e5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+399.sha-d34ce6b
+ * @license Angular v12.0.0-next.8+401.sha-53cd7e5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+399.sha-d34ce6b');
+const VERSION = new Version('12.0.0-next.8+401.sha-53cd7e5');
 
 /**
  * @license
