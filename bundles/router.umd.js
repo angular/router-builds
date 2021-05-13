244e92804633024a3b0a50db3a531dc5d3d1b677
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-13 17:52:45 +0000

    45083bc302 build: lock file maintenance (#42015)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+401.sha-53cd7e5
+ * @license Angular v12.0.0-next.8+403.sha-45083bc
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+401.sha-53cd7e5');
+    var VERSION = new i0.Version('12.0.0-next.8+403.sha-45083bc');
 
     /**
      * @license
