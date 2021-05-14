d9d4e0e32efb2f695e9f947852fc9b087ae9fbb1
Zach Arend <zachzach@google.com>
2021-05-14 21:32:38 +0000

    5960d4c513 docs(benchpress): reword a comment (#42096)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+415.sha-6d87895
+ * @license Angular v12.0.0-next.8+416.sha-5960d4c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+415.sha-6d87895');
+const VERSION = new Version('12.0.0-next.8+416.sha-5960d4c');
 
 /**
  * @license
