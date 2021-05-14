211a3de2f9a0ba9af3a2eba9e8699334117ec4ec
Zach Arend <zachzach@google.com>
2021-05-14 19:34:58 +0000

    3aec55b626 ci: update to latest feature request triage bot commit (#42084)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+411.sha-4bc5b4d
+ * @license Angular v12.0.0-next.8+412.sha-3aec55b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+411.sha-4bc5b4d
+ * @license Angular v12.0.0-next.8+412.sha-3aec55b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+411.sha-4bc5b4d');
+const VERSION = new Version('12.0.0-next.8+412.sha-3aec55b');
 
 /**
  * @license
