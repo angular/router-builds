d2c380a5eadedccddd16d117aa70f85da7167c92
atscott <atscott01@gmail.com>
2021-05-17 18:16:20 +0000

    c32bea6ccf docs: add link to the blog post in the changelog (#42109) PR Close #42109

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+418.sha-fc1bc0e
+ * @license Angular v12.0.0-next.8+420.sha-c32bea6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+418.sha-fc1bc0e');
+const VERSION = new Version('12.0.0-next.8+420.sha-c32bea6');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+418.sha-fc1bc0e
+ * @license Angular v12.0.0-next.8+420.sha-c32bea6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
