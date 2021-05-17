d61cf86ed48d5c747897a47c99079e83c78a64ba
atscott <atscott01@gmail.com>
2021-05-17 18:10:15 +0000

    e86a1d3441 docs: make all examples compatible with strict mode (#41999)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+416.sha-5960d4c
+ * @license Angular v12.0.0-next.8+417.sha-e86a1d3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.0-next.8+416.sha-5960d4c');
+    var VERSION = new core.Version('12.0.0-next.8+417.sha-e86a1d3');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+416.sha-5960d4c
+ * @license Angular v12.0.0-next.8+417.sha-e86a1d3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
