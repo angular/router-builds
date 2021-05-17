ab8f3581329a4f52d82c3120d40af963d731bacd
atscott <atscott01@gmail.com>
2021-05-17 18:17:36 +0000

    fc1bc0e0e8 docs: update examples to use correct `Injector.create()` overload (#42006)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+417.sha-e86a1d3
+ * @license Angular v12.0.0-next.8+418.sha-fc1bc0e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+417.sha-e86a1d3');
+    var VERSION = new i0.Version('12.0.0-next.8+418.sha-fc1bc0e');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+417.sha-e86a1d3
+ * @license Angular v12.0.0-next.8+418.sha-fc1bc0e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+417.sha-e86a1d3');
+const VERSION = new Version('12.0.0-next.8+418.sha-fc1bc0e');
 
 /**
  * @license
