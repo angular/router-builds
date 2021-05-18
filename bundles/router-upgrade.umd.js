30c2fef9f11efc73d4d3dfa2016a08ccf4a2509c
atscott <atscott01@gmail.com>
2021-05-18 22:57:06 +0000

    b7d161487f build: update dependency esbuild to ^0.12.0 (#42143)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+432.sha-6d14776
+ * @license Angular v12.0.0-next.8+435.sha-b7d1614
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+432.sha-6d14776
+ * @license Angular v12.0.0-next.8+435.sha-b7d1614
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+432.sha-6d14776');
+    var VERSION = new i0.Version('12.0.0-next.8+435.sha-b7d1614');
 
     /**
      * @license
