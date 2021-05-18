5b219df0767338d5c6acabad9a9e9bdb21f1fd24
Andrew Scott <atscott@google.com>
2021-05-18 18:44:03 +0000

    6d1477664b ci: add pullapprove entries for devtools (#42152)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+426.sha-4579200
+ * @license Angular v12.0.0-next.8+432.sha-6d14776
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+426.sha-4579200');
+    var VERSION = new i0.Version('12.0.0-next.8+432.sha-6d14776');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+426.sha-4579200
+ * @license Angular v12.0.0-next.8+432.sha-6d14776
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+426.sha-4579200');
+const VERSION = new Version('12.0.0-next.8+432.sha-6d14776');
 
 /**
  * @license
