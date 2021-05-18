27f08f4e87e5acebb89f89f00a7f5eb3ad25fe4b
atscott <atscott01@gmail.com>
2021-05-18 15:57:41 +0000

    2dfa010763 docs: update bazel migration doc link (#42142) PR Close #42142

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+422.sha-5885cf5
+ * @license Angular v12.0.0-next.8+425.sha-2dfa010
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+422.sha-5885cf5');
+const VERSION = new Version('12.0.0-next.8+425.sha-2dfa010');
 
 /**
  * @license
