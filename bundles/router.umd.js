7ee1052ee303d1fa3dc8629636d6497faf1fefb9
atscott <atscott01@gmail.com>
2021-05-18 16:02:28 +0000

    fd4dbcb0c7 docs: decorate input properties with @Input (#42128) PR Close #42128

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+425.sha-2dfa010
+ * @license Angular v12.0.0-next.8+423.sha-fd4dbcb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+425.sha-2dfa010');
+    var VERSION = new i0.Version('12.0.0-next.8+423.sha-fd4dbcb');
 
     /**
      * @license
