4fd94cafa3844249d155e1f21a8348609c15a40b
Zach Arend <zachzach@google.com>
2021-05-20 17:45:05 +0000

    d6bc61ab5f build: update angular (#42090)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+6.sha-a01fe8d
+ * @license Angular v12.1.0-next.2+8.sha-d6bc61a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.2+6.sha-a01fe8d');
+    var VERSION = new core.Version('12.1.0-next.2+8.sha-d6bc61a');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+6.sha-a01fe8d
+ * @license Angular v12.1.0-next.2+8.sha-d6bc61a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
