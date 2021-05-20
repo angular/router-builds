00af8e6e26016e55ac30c1f7174613f08780d5eb
Zach Arend <zachzach@google.com>
2021-05-20 17:17:37 +0000

    a01fe8d9b4 ci: add Teri to global approvers for docs changes list (#42156)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+3.sha-04ab5ed
+ * @license Angular v12.1.0-next.2+6.sha-a01fe8d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+3.sha-04ab5ed');
+const VERSION = new Version('12.1.0-next.2+6.sha-a01fe8d');
 
 /**
  * @license
