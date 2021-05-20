fc7e700325325b46dbcc9267b333435dbc72379b
Zach Arend <zachzach@google.com>
2021-05-20 16:31:18 +0000

    04ab5edf65 test(docs-infra): correctly test URL redirects when the destination URL is also redirected (#42018)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+2.sha-ef676bf
+ * @license Angular v12.1.0-next.2+3.sha-04ab5ed
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.2+2.sha-ef676bf');
+    var VERSION = new i0.Version('12.1.0-next.2+3.sha-04ab5ed');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+2.sha-ef676bf
+ * @license Angular v12.1.0-next.2+3.sha-04ab5ed
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+2.sha-ef676bf');
+const VERSION = new Version('12.1.0-next.2+3.sha-04ab5ed');
 
 /**
  * @license
