b98caf2d1a0395d29871c3712b2eda9cdba6c3fa
Zach Arend <zachzach@google.com>
2021-05-20 23:23:41 +0000

    d2a89dbbd9 docs: edits to remove jargon (#41282)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+8.sha-d6bc61a
+ * @license Angular v12.1.0-next.2+11.sha-d2a89db
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+8.sha-d6bc61a
+ * @license Angular v12.1.0-next.2+11.sha-d2a89db
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+8.sha-d6bc61a');
+const VERSION = new Version('12.1.0-next.2+11.sha-d2a89db');
 
 /**
  * @license
