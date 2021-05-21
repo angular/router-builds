a1e59ed463498a1969381f506e8745912f69612a
Joey Perrott <josephperrott@gmail.com>
2021-05-21 21:01:07 +0000

    30c461d862 ci: update to latest feature request triage bot commit (#42229)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+17.sha-03825cf
+ * @license Angular v12.1.0-next.2+18.sha-30c461d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.2+17.sha-03825cf');
+    var VERSION = new core.Version('12.1.0-next.2+18.sha-30c461d');
 
     /**
      * @license
