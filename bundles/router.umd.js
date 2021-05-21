1b8a9fbfee8760f68676171b430a88f1d09fea12
Zach Arend <zachzach@google.com>
2021-05-21 18:25:27 +0000

    62becc88f9 fix(docs-infra): fix example compilation issues (#42199)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+14.sha-8c158ba
+ * @license Angular v12.1.0-next.2+15.sha-62becc8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.2+14.sha-8c158ba');
+    var VERSION = new core.Version('12.1.0-next.2+15.sha-62becc8');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+14.sha-8c158ba
+ * @license Angular v12.1.0-next.2+15.sha-62becc8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+14.sha-8c158ba');
+const VERSION = new Version('12.1.0-next.2+15.sha-62becc8');
 
 /**
  * @license
