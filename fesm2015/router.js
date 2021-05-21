aa4b953ee733de414234d12210b643e7d18bf1f7
Zach Arend <zachzach@google.com>
2021-05-21 18:24:49 +0000

    8c158babdc fix(dev-infra): formatting errors not reported with failure message (#42202)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+11.sha-d2a89db
+ * @license Angular v12.1.0-next.2+14.sha-8c158ba
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+11.sha-d2a89db');
+const VERSION = new Version('12.1.0-next.2+14.sha-8c158ba');
 
 /**
  * @license
