ec7adb36cc0b2a098e03ed4cfa8573cd67b1b763
Zach Arend <zachzach@google.com>
2021-05-24 16:19:51 +0000

    4ac55ca676 fix(dev-infra): use template strings for release note templates (#42224)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+22.sha-d7a1da9
+ * @license Angular v12.1.0-next.2+24.sha-4ac55ca
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.2+22.sha-d7a1da9');
+    var VERSION = new i0.Version('12.1.0-next.2+24.sha-4ac55ca');
 
     /**
      * @license
