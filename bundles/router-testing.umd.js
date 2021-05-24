7c728fcf1317443c9f2448511aa1d5443d9f8efb
Zach Arend <zachzach@google.com>
2021-05-24 22:36:30 +0000

    47bb841ed6 build(docs-infra): temporarily serve the DevTools guide at `/devtools` too (#42283)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+32.sha-cb48bde
+ * @license Angular v12.1.0-next.2+34.sha-47bb841
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+32.sha-cb48bde
+ * @license Angular v12.1.0-next.2+34.sha-47bb841
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+32.sha-cb48bde');
+const VERSION = new Version('12.1.0-next.2+34.sha-47bb841');
 
 /**
  * @license
