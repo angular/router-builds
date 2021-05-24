cad10afbd301f6958a35d3f9c391794dc218c942
Zach Arend <zachzach@google.com>
2021-05-24 15:06:47 +0000

    d7a1da9d4f docs: fix incorrect point in universal-in-action (#42262)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+23.sha-c03280f
+ * @license Angular v12.1.0-next.2+22.sha-d7a1da9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+23.sha-c03280f');
+const VERSION = new Version('12.1.0-next.2+22.sha-d7a1da9');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+23.sha-c03280f
+ * @license Angular v12.1.0-next.2+22.sha-d7a1da9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
