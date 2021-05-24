382f8d39ffad9080ac23f9c353e2432cfeac3ff7
Zach Arend <zachzach@google.com>
2021-05-24 15:06:24 +0000

    c03280fd54 docs: update the intro image in the devtools guide (#42162)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+21.sha-3293dd3
+ * @license Angular v12.1.0-next.2+23.sha-c03280f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.2+21.sha-3293dd3');
+    var VERSION = new i0.Version('12.1.0-next.2+23.sha-c03280f');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+21.sha-3293dd3
+ * @license Angular v12.1.0-next.2+23.sha-c03280f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5916,7 +5916,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+21.sha-3293dd3');
+const VERSION = new Version('12.1.0-next.2+23.sha-c03280f');
 
 /**
  * @license
