4cd36c5e881e893728d76a2bd2df8403e0a2cb27
Zach Arend <zachzach@google.com>
2021-05-24 23:00:42 +0000

    1040697787 docs: remove sentence about how the not reloading the page. (#42290)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+33.sha-ad12b31
+ * @license Angular v12.1.0-next.2+37.sha-1040697
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+33.sha-ad12b31
+ * @license Angular v12.1.0-next.2+37.sha-1040697
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.2+33.sha-ad12b31');
+    var VERSION = new core.Version('12.1.0-next.2+37.sha-1040697');
 
     /**
      * @license
