07417a2160ef5f68c24b74d3bde378ded3b2dc27
Zach Arend <zachzach@google.com>
2021-05-25 18:14:26 +0000

    d15a6a3a71 docs: update web-worker syntax (#42306)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+59.sha-1955dbb
+ * @license Angular v12.1.0-next.2+58.sha-d15a6a3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.2+59.sha-1955dbb');
+    var VERSION = new core.Version('12.1.0-next.2+58.sha-d15a6a3');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+59.sha-1955dbb
+ * @license Angular v12.1.0-next.2+58.sha-d15a6a3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
