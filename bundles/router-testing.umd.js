8e2bff54aeab140e519c7d47203a1a4edef4b62b
Zach Arend <zachzach@google.com>
2021-05-25 03:03:17 +0000

    1c3fe69f92 docs: change Zeit -> Vercel (#42285)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+38.sha-a391698
+ * @license Angular v12.1.0-next.2+40.sha-1c3fe69
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+38.sha-a391698
+ * @license Angular v12.1.0-next.2+40.sha-1c3fe69
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.2+38.sha-a391698');
+    var VERSION = new core.Version('12.1.0-next.2+40.sha-1c3fe69');
 
     /**
      * @license
