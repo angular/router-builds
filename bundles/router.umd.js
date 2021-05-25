ee2be099221703afa97f2c665eec6c813dccc19b
Zach Arend <zachzach@google.com>
2021-05-25 22:31:15 +0000

    bdd3cca9ab docs: update wording for angular libraries mapping peer dependencies (#42322)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+70.sha-3d9062d
+ * @license Angular v12.1.0-next.2+72.sha-bdd3cca
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.2+70.sha-3d9062d');
+    var VERSION = new core.Version('12.1.0-next.2+72.sha-bdd3cca');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+70.sha-3d9062d
+ * @license Angular v12.1.0-next.2+72.sha-bdd3cca
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
