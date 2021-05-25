0b63d175ab9502d193eaad90b3665b57be1f0427
Zach Arend <zachzach@google.com>
2021-05-25 22:32:59 +0000

    4a2cc3e451 docs: add explanation of how to use locally created build artifacts in a local project (#42319)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+71.sha-063d178
+ * @license Angular v12.1.0-next.2+77.sha-4a2cc3e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.2+71.sha-063d178');
+    var VERSION = new core.Version('12.1.0-next.2+77.sha-4a2cc3e');
 
     /**
      * @license
