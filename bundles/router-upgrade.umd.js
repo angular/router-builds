7a30632bc60358833ad1c2e1eaa8aa6a43f24528
Zach Arend <zachzach@google.com>
2021-05-25 20:50:04 +0000

    3a6af8e629 fix(platform-browser): update started state on reset (#41608)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+68.sha-d7768c6
+ * @license Angular v12.1.0-next.2+69.sha-3a6af8e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+68.sha-d7768c6
+ * @license Angular v12.1.0-next.2+69.sha-3a6af8e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.2+68.sha-d7768c6');
+    var VERSION = new core.Version('12.1.0-next.2+69.sha-3a6af8e');
 
     /**
      * @license
