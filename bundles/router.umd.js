35977871a2fe1227fb5b341e454b7a1a4c19d67d
Zach Arend <zachzach@google.com>
2021-05-25 18:13:48 +0000

    29e5d509aa docs: clarify expectations for `ActivatedRoute` observables (#42316)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+56.sha-9f09c3b
+ * @license Angular v12.1.0-next.2+57.sha-29e5d50
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.2+56.sha-9f09c3b');
+    var VERSION = new core.Version('12.1.0-next.2+57.sha-29e5d50');
 
     /**
      * @license
