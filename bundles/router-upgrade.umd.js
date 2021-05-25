18866dc389304301dbdd4706e469fac92cce1683
Zach Arend <zachzach@google.com>
2021-05-25 18:20:05 +0000

    c3cbfc22e0 fix(dev-infra): format command incorrectly prints outdated files (#42252)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+65.sha-f74fd64
+ * @license Angular v12.1.0-next.2+63.sha-c3cbfc2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+65.sha-f74fd64
+ * @license Angular v12.1.0-next.2+63.sha-c3cbfc2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6545,7 +6545,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.2+65.sha-f74fd64');
+    var VERSION = new core.Version('12.1.0-next.2+63.sha-c3cbfc2');
 
     /**
      * @license
