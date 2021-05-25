261cdf6837117328f559e19561acea2368b719b7
Zach Arend <zachzach@google.com>
2021-05-25 18:10:43 +0000

    9f09c3b9b1 build(core): update supported range of node versions to be less restrictive (#42205)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+40.sha-1c3fe69
+ * @license Angular v12.1.0-next.2+56.sha-9f09c3b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6595,7 +6595,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.2+40.sha-1c3fe69');
+    var VERSION = new i0.Version('12.1.0-next.2+56.sha-9f09c3b');
 
     /**
      * @license
