cdef707e7f1c27ff5aa1ad0d72c4eb02af31865f
Zach Arend <zachzach@google.com>
2021-05-26 20:29:09 +0000

    de8a6ae9ed docs: add example that exports a part of an animation for reuse (#42321)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+6.sha-38b0e71
+ * @license Angular v12.1.0-next.3+9.sha-de8a6ae
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6550,7 +6550,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.3+6.sha-38b0e71');
+    var VERSION = new core.Version('12.1.0-next.3+9.sha-de8a6ae');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+6.sha-38b0e71
+ * @license Angular v12.1.0-next.3+9.sha-de8a6ae
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
