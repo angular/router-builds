487ff57df4b511d9515504a857ed6003cba01a5c
Andrew Kushnir <akushnir@google.com>
2021-05-26 22:45:45 +0000

    44027c4e0e build: allow node v15 and above for aio (#42367)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+18.sha-516923e
+ * @license Angular v12.1.0-next.3+19.sha-44027c4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6600,7 +6600,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.3+18.sha-516923e');
+    var VERSION = new i0.Version('12.1.0-next.3+19.sha-44027c4');
 
     /**
      * @license
