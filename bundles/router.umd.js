05d82b1adda3cdab324afdae261df871705ad1b2
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:41:57 +0000

    bcfe4afda9 docs: clarify that `forRoot` can be called in other modules (#42361)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+15.sha-4cb11cc
+ * @license Angular v12.1.0-next.3+17.sha-bcfe4af
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6550,7 +6550,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.3+15.sha-4cb11cc');
+    var VERSION = new core.Version('12.1.0-next.3+17.sha-bcfe4af');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+15.sha-4cb11cc
+ * @license Angular v12.1.0-next.3+17.sha-bcfe4af
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
