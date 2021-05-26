7a6601a80a3eb5db19056f05aedf6de8067e3b44
Zach Arend <zachzach@google.com>
2021-05-26 20:36:47 +0000

    9193ea20e4 docs: clarify providedIn options (#42355)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+11.sha-a787f78
+ * @license Angular v12.1.0-next.3+13.sha-9193ea2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6550,7 +6550,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.3+11.sha-a787f78');
+    var VERSION = new core.Version('12.1.0-next.3+13.sha-9193ea2');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+11.sha-a787f78
+ * @license Angular v12.1.0-next.3+13.sha-9193ea2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
