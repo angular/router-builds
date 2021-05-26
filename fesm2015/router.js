df5bbba1b721c3481282b3a0ee03a0713e45f165
Zach Arend <zachzach@google.com>
2021-05-26 20:34:38 +0000

    a787f78074 test: clean up internal testing utilities (#42177)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+10.sha-bd51762
+ * @license Angular v12.1.0-next.3+11.sha-a787f78
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5921,7 +5921,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+10.sha-bd51762');
+const VERSION = new Version('12.1.0-next.3+11.sha-a787f78');
 
 /**
  * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+10.sha-bd51762
+ * @license Angular v12.1.0-next.3+11.sha-a787f78
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
