2e8334648e008177b587b2c4f43f273da6969ffa
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:42:29 +0000

    15726594a9 ci: update the github feature request process action and run on cron (#42337)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+17.sha-bcfe4af
+ * @license Angular v12.1.0-next.3+16.sha-1572659
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6550,7 +6550,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.3+17.sha-bcfe4af');
+    var VERSION = new core.Version('12.1.0-next.3+16.sha-1572659');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+17.sha-bcfe4af
+ * @license Angular v12.1.0-next.3+16.sha-1572659
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
