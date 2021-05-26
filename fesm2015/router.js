84b319737b1a8f1a04b46b7990594428889bbdfa
Zach Arend <zachzach@google.com>
2021-05-26 20:26:26 +0000

    0166391402 docs: edit project structure description (#42346)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+1.sha-74ebdf6
+ * @license Angular v12.1.0-next.3+2.sha-0166391
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5921,7 +5921,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+1.sha-74ebdf6');
+const VERSION = new Version('12.1.0-next.3+2.sha-0166391');
 
 /**
  * @license
