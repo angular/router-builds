b7909621ecf8247c17541f661d8d544fcf59b405
Zach Arend <zachzach@google.com>
2021-05-26 20:33:01 +0000

    bd51762d6e docs: add section on binding to passive events (#42292)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+9.sha-de8a6ae
+ * @license Angular v12.1.0-next.3+10.sha-bd51762
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6600,7 +6600,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.3+9.sha-de8a6ae');
+    var VERSION = new i0.Version('12.1.0-next.3+10.sha-bd51762');
 
     /**
      * @license
