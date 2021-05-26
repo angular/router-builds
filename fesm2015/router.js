bdcf56d333cbddaacfc86f9ce37a7c3576bf3963
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:40:53 +0000

    4cb11cc4e7 docs: add note about why module import order is important (#42323)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+14.sha-4e9b61a
+ * @license Angular v12.1.0-next.3+15.sha-4cb11cc
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5921,7 +5921,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+14.sha-4e9b61a');
+const VERSION = new Version('12.1.0-next.3+15.sha-4cb11cc');
 
 /**
  * @license
