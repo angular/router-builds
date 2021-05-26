cd69c5afd0c8d338375dd90801a47073a0fe3baa
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:40:20 +0000

    4e9b61aaf9 docs: remove/update broken links in resources appearing on aio (#42232)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+13.sha-9193ea2
+ * @license Angular v12.1.0-next.3+14.sha-4e9b61a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6600,7 +6600,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.3+13.sha-9193ea2');
+    var VERSION = new i0.Version('12.1.0-next.3+14.sha-4e9b61a');
 
     /**
      * @license
