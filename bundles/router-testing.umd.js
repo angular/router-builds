d054f7120adcce890b710a9d6d5540799a8c7057
Andrew Kushnir <akushnir@google.com>
2021-05-27 18:31:10 +0000

    e73fef4d0b docs: add a note about <select> and nested <option>s in Ivy compat guide (#42338)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+30.sha-306f7ac
+ * @license Angular v12.1.0-next.3+31.sha-e73fef4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+30.sha-306f7ac
+ * @license Angular v12.1.0-next.3+31.sha-e73fef4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6550,7 +6550,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.3+30.sha-306f7ac');
+    var VERSION = new core.Version('12.1.0-next.3+31.sha-e73fef4');
 
     /**
      * @license
