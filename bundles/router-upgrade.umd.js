d6fb65d9ed8654d974554258592b949a757ed9e7
Andrew Kushnir <akushnir@google.com>
2021-05-27 22:54:47 +0000

    3a959152f9 docs: Edited to remove jargon, fixed issue (#42026)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+41.sha-d33fdea
+ * @license Angular v12.1.0-next.3+44.sha-3a95915
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+41.sha-d33fdea
+ * @license Angular v12.1.0-next.3+44.sha-3a95915
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5888,7 +5888,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+41.sha-d33fdea');
+const VERSION = new Version('12.1.0-next.3+44.sha-3a95915');
 
 /**
  * @license
