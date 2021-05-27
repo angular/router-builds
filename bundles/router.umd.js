bcac095804a93480221918199157e6518c9b715e
Andrew Kushnir <akushnir@google.com>
2021-05-27 16:55:13 +0000

    306f7ac543 docs: cherry pick release notes for 12.0.2 to main (#42390)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+29.sha-deb8930
+ * @license Angular v12.1.0-next.3+30.sha-306f7ac
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6600,7 +6600,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.3+29.sha-deb8930');
+    var VERSION = new i0.Version('12.1.0-next.3+30.sha-306f7ac');
 
     /**
      * @license
