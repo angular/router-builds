8f32e4ba5a4e3d64fba5d78e268971286c68f807
Andrew Kushnir <akushnir@google.com>
2021-05-27 21:22:34 +0000

    fa0144fde6 docs: clarify the shortcomings of local variable technique for parent-child component interactions (#42331)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+38.sha-cdb8f55
+ * @license Angular v12.1.0-next.3+39.sha-fa0144f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6550,7 +6550,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.3+38.sha-cdb8f55');
+    var VERSION = new core.Version('12.1.0-next.3+39.sha-fa0144f');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+38.sha-cdb8f55
+ * @license Angular v12.1.0-next.3+39.sha-fa0144f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
