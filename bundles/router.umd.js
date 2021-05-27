ab93129d0f2a46939b94659fc9c9fa9cd1ba4af2
Andrew Kushnir <akushnir@google.com>
2021-05-27 18:44:31 +0000

    a86a6b95c5 feat(docs-infra): implement dark mode (#41129)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.2+25.sha-2513a53
+ * @license Angular v12.0.2+39.sha-a86a6b9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6550,7 +6550,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.2+25.sha-2513a53');
+    var VERSION = new core.Version('12.0.2+39.sha-a86a6b9');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.2+25.sha-2513a53
+ * @license Angular v12.0.2+39.sha-a86a6b9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
