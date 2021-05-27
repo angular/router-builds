ff4c327bcf9ddae1c51c58ebc53851933da434e2
Andrew Kushnir <akushnir@google.com>
2021-05-27 16:19:04 +0000

    deb893095f build(docs-infra): remove unused `spy-directive.gif` image (#42388)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+26.sha-e87dbaf
+ * @license Angular v12.1.0-next.3+29.sha-deb8930
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6550,7 +6550,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.3+26.sha-e87dbaf');
+    var VERSION = new core.Version('12.1.0-next.3+29.sha-deb8930');
 
     /**
      * @license
