4ad7c47119c1139bbfa105b293bee84453773abe
Andrew Kushnir <akushnir@google.com>
2021-05-27 21:54:12 +0000

    d33fdea1e3 docs: clarify behavior of strict templates flags (#42392)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+39.sha-fa0144f
+ * @license Angular v12.1.0-next.3+41.sha-d33fdea
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6600,7 +6600,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.3+39.sha-fa0144f');
+    var VERSION = new i0.Version('12.1.0-next.3+41.sha-d33fdea');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+39.sha-fa0144f
+ * @license Angular v12.1.0-next.3+41.sha-d33fdea
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5921,7 +5921,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+39.sha-fa0144f');
+const VERSION = new Version('12.1.0-next.3+41.sha-d33fdea');
 
 /**
  * @license
