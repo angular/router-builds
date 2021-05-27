e6491d45a8b0e34810a2b989cd00be46504b800d
Andrew Kushnir <akushnir@google.com>
2021-05-27 16:53:48 +0000

    306f7ac543 docs: cherry pick release notes for 12.0.2 to main (#42390)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+29.sha-deb8930
+ * @license Angular v12.1.0-next.3+30.sha-306f7ac
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5888,7 +5888,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+29.sha-deb8930');
+const VERSION = new Version('12.1.0-next.3+30.sha-306f7ac');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+29.sha-deb8930
+ * @license Angular v12.1.0-next.3+30.sha-306f7ac
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
