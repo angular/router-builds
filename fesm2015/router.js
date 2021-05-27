3638763342280db9532bb34ceb45dcffa13e7a5e
Andrew Kushnir <akushnir@google.com>
2021-05-27 00:37:27 +0000

    141791e070 docs: add a note about the ngc command and command line options (#42373)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+23.sha-7a6358b
+ * @license Angular v12.1.0-next.3+22.sha-141791e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5921,7 +5921,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+23.sha-7a6358b');
+const VERSION = new Version('12.1.0-next.3+22.sha-141791e');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+23.sha-7a6358b
+ * @license Angular v12.1.0-next.3+22.sha-141791e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
