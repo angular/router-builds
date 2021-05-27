a56a1f89c3d89922dac7354ade20df2a73d1c888
Andrew Kushnir <akushnir@google.com>
2021-05-27 16:14:14 +0000

    38378ce50a docs: explain what a deploy url is and how it differs from a base href (#42363)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+24.sha-d74d130
+ * @license Angular v12.1.0-next.3+25.sha-38378ce
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6600,7 +6600,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.3+24.sha-d74d130');
+    var VERSION = new i0.Version('12.1.0-next.3+25.sha-38378ce');
 
     /**
      * @license
