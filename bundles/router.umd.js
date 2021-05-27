2c0a979bac75ab4daf67f579672177a53a4f971a
Andrew Kushnir <akushnir@google.com>
2021-05-27 00:36:23 +0000

    7a6358b4a8 docs: add note to clarify that ngOnChanges is only called if inputs are bound (#42374)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+21.sha-74186c3
+ * @license Angular v12.1.0-next.3+23.sha-7a6358b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6600,7 +6600,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.3+21.sha-74186c3');
+    var VERSION = new i0.Version('12.1.0-next.3+23.sha-7a6358b');
 
     /**
      * @license
