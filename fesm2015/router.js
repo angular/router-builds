3cdc4914a4780907d626ccb1567ac8e6cf98058a
Andrew Kushnir <akushnir@google.com>
2021-05-27 16:14:51 +0000

    e87dbafba1 docs: fix a typo (#42384)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+25.sha-38378ce
+ * @license Angular v12.1.0-next.3+26.sha-e87dbaf
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5921,7 +5921,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+25.sha-38378ce');
+const VERSION = new Version('12.1.0-next.3+26.sha-e87dbaf');
 
 /**
  * @license
