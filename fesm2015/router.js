ffb2d7c9114786aa9daecb964bde00533b3751b0
Andrew Kushnir <akushnir@google.com>
2021-05-28 00:06:14 +0000

    4cd2cc439d docs: Edits to remove jargon in Reference section (#42033)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+45.sha-ee0e3fb
+ * @license Angular v12.1.0-next.3+46.sha-4cd2cc4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5888,7 +5888,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+45.sha-ee0e3fb');
+const VERSION = new Version('12.1.0-next.3+46.sha-4cd2cc4');
 
 /**
  * @license
