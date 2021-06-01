80a7762b98820275fd2af92509ed87aecab67709
Andrew Kushnir <akushnir@google.com>
2021-06-01 20:28:41 +0000

    5babdb2284 build: update angular (#42379)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+62.sha-e039075
+ * @license Angular v12.1.0-next.3+63.sha-5babdb2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6605,7 +6605,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.3+62.sha-e039075');
+    var VERSION = new i0.Version('12.1.0-next.3+63.sha-5babdb2');
 
     /**
      * @license
