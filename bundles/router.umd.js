bdf2dcd1d123a4f9264251904787cfc824b209da
Andrew Kushnir <akushnir@google.com>
2021-06-01 17:33:23 +0000

    1b8bbddff5 docs: list supported values for `read` in decorator metadata (#42370)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+53.sha-c54643f
+ * @license Angular v12.1.0-next.3+50.sha-1b8bbdd
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6605,7 +6605,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.3+53.sha-c54643f');
+    var VERSION = new i0.Version('12.1.0-next.3+50.sha-1b8bbdd');
 
     /**
      * @license
