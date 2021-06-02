261c75ba6a9e9c15f2207798fa081851d787463b
Andrew Kushnir <akushnir@google.com>
2021-06-02 20:35:43 +0000

    4c39869566 docs: add a note about javascript mime-types to the deployment guide (#42339)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+4.sha-f3f1014
+ * @license Angular v12.1.0-next.4+5.sha-4c39869
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5893,7 +5893,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+4.sha-f3f1014');
+const VERSION = new Version('12.1.0-next.4+5.sha-4c39869');
 
 /**
  * @license
