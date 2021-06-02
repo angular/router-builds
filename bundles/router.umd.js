6b8d479c0ce70b37d197eddd6148eb0dd25575da
GitHub <noreply@github.com>
2021-06-02 20:33:40 +0000

    f8814d3558 fix(dev-infra): update outdated build-worker.js file (#42458)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4
+ * @license Angular v12.1.0-next.4+2.sha-f8814d3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6605,7 +6605,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.4');
+    var VERSION = new i0.Version('12.1.0-next.4+2.sha-f8814d3');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4
+ * @license Angular v12.1.0-next.4+2.sha-f8814d3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5926,7 +5926,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4');
+const VERSION = new Version('12.1.0-next.4+2.sha-f8814d3');
 
 /**
  * @license
