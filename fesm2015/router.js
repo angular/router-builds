c432bc5b1e5ecd41845e7c22338318185a57b6eb
Andrew Kushnir <akushnir@google.com>
2021-06-02 20:37:30 +0000

    1fe1dd1f19 docs: clarify the wording around live examples for all testing guides (#42406)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+5.sha-4c39869
+ * @license Angular v12.1.0-next.4+6.sha-1fe1dd1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5893,7 +5893,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+5.sha-4c39869');
+const VERSION = new Version('12.1.0-next.4+6.sha-1fe1dd1');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+5.sha-4c39869
+ * @license Angular v12.1.0-next.4+6.sha-1fe1dd1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
