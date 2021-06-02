5b0a4d3bb206b4a58edbd5e55da1aad6db675f52
Andrew Kushnir <akushnir@google.com>
2021-06-02 20:38:46 +0000

    1fe1dd1f19 docs: clarify the wording around live examples for all testing guides (#42406)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+5.sha-4c39869
+ * @license Angular v12.1.0-next.4+6.sha-1fe1dd1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6605,7 +6605,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.4+5.sha-4c39869');
+    var VERSION = new i0.Version('12.1.0-next.4+6.sha-1fe1dd1');
 
     /**
      * @license
