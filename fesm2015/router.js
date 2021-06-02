447502cdabf1d5c5e82e45ff7f7280f4e4080d3f
Andrew Kushnir <akushnir@google.com>
2021-06-02 18:30:57 +0000

    7bd5ea4a7e release: cut the v12.1.0-next.4 release

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+66.sha-574f640
+ * @license Angular v12.1.0-next.4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5926,7 +5926,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+66.sha-574f640');
+const VERSION = new Version('12.1.0-next.4');
 
 /**
  * @license
