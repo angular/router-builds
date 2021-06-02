625e9d26576b4bc2c7223778dfcd79f983e447a6
Andrew Kushnir <akushnir@google.com>
2021-06-02 20:42:03 +0000

    c0b2eeb54c docs: remove unused imports from unit tests (#42441)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+6.sha-1fe1dd1
+ * @license Angular v12.1.0-next.4+8.sha-c0b2eeb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6605,7 +6605,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.4+6.sha-1fe1dd1');
+    var VERSION = new i0.Version('12.1.0-next.4+8.sha-c0b2eeb');
 
     /**
      * @license
