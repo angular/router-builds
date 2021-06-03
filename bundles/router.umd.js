ed31724efb881adba48b296714647211415da528
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 20:36:39 +0000

    fa84d19e97 refactor(dev-infra): always use en-US locale for printing date in merge tool (#42470)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+30.sha-69e5782
+ * @license Angular v12.1.0-next.4+32.sha-fa84d19
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6555,7 +6555,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.4+30.sha-69e5782');
+    var VERSION = new core.Version('12.1.0-next.4+32.sha-fa84d19');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+30.sha-69e5782
+ * @license Angular v12.1.0-next.4+32.sha-fa84d19
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5893,7 +5893,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+30.sha-69e5782');
+const VERSION = new Version('12.1.0-next.4+32.sha-fa84d19');
 
 /**
  * @license
