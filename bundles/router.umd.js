950d4d598edfd528556cda73d2eb7784d0fe5f64
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 18:59:31 +0000

    69e57827e2 refactor(compiler-cli): remove unused return type transform (#41996)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+24.sha-bd1836b
+ * @license Angular v12.1.0-next.4+30.sha-69e5782
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6555,7 +6555,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.4+24.sha-bd1836b');
+    var VERSION = new core.Version('12.1.0-next.4+30.sha-69e5782');
 
     /**
      * @license
