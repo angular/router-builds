81ed4e9a6afd7a4e87210832570f423b2b52fe80
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 18:52:04 +0000

    bd1836b999 fix(compiler-cli): exclude type-only imports from cycle analysis (#42453)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+23.sha-374fa2c
+ * @license Angular v12.1.0-next.4+24.sha-bd1836b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6555,7 +6555,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.4+23.sha-374fa2c');
+    var VERSION = new core.Version('12.1.0-next.4+24.sha-bd1836b');
 
     /**
      * @license
