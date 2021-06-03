40abca049add41eeb52a9fad026c705c7e8b0e9f
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 23:24:08 +0000

    ae858c0504 ci: extend default timeout of karma tests for legacy saucelab tests (#42473)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+34.sha-ba08485
+ * @license Angular v12.1.0-next.4+39.sha-ae858c0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5893,7 +5893,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+34.sha-ba08485');
+const VERSION = new Version('12.1.0-next.4+39.sha-ae858c0');
 
 /**
  * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+34.sha-ba08485
+ * @license Angular v12.1.0-next.4+39.sha-ae858c0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
