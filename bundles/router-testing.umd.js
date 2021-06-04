ac9c49e71e909ab7dd53bac6cb95067a163d53a0
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-04 18:38:18 +0000

    71e14a71f5 test: update compiler-cli compliance goldens due to TS 4.3 emit format regression (#42022)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+41.sha-6929625
+ * @license Angular v12.1.0-next.4+43.sha-71e14a7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+41.sha-6929625
+ * @license Angular v12.1.0-next.4+43.sha-71e14a7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5893,7 +5893,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+41.sha-6929625');
+const VERSION = new Version('12.1.0-next.4+43.sha-71e14a7');
 
 /**
  * @license
