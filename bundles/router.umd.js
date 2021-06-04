c792883ff59e3f456ffe1a81cc2eb507f115ee14
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-04 17:56:25 +0000

    69296254da build: update dependency google-closure-compiler to v20210601 (#42475)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+40.sha-00b1444
+ * @license Angular v12.1.0-next.4+41.sha-6929625
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6555,7 +6555,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.4+40.sha-00b1444');
+    var VERSION = new core.Version('12.1.0-next.4+41.sha-6929625');
 
     /**
      * @license
