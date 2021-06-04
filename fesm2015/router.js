d49cc4ea46d2f83df79bd7a64a9ff5fcd557bbe5
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-04 01:04:18 +0000

    00b1444d12 Revert "feat(forms): add `ng-submitted` class to forms that have been submitted." (#42474)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+39.sha-ae858c0
+ * @license Angular v12.1.0-next.4+40.sha-00b1444
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5926,7 +5926,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+39.sha-ae858c0');
+const VERSION = new Version('12.1.0-next.4+40.sha-00b1444');
 
 /**
  * @license
