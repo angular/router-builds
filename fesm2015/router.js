bbe3eda1a8cfe0b48a4c26e7d3ee0048d6bc5a38
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-04 20:53:18 +0000

    4fc24addcb build: suppress errors in git hooks (#42484)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+43.sha-71e14a7
+ * @license Angular v12.1.0-next.4+46.sha-4fc24ad
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5893,7 +5893,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+43.sha-71e14a7');
+const VERSION = new Version('12.1.0-next.4+46.sha-4fc24ad');
 
 /**
  * @license
