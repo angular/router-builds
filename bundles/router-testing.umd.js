198ae4a5f7fe9ee804fa59ee0ebaec15b952f5c3
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 16:53:17 +0000

    cd22c7a1f5 docs: Updated the code snippet path (#42494) PR Close #42494

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+47.sha-3005917
+ * @license Angular v12.1.0-next.4+48.sha-cd22c7a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+47.sha-3005917
+ * @license Angular v12.1.0-next.4+48.sha-cd22c7a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5926,7 +5926,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+47.sha-3005917');
+const VERSION = new Version('12.1.0-next.4+48.sha-cd22c7a');
 
 /**
  * @license
