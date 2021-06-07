89b34623b92e93011c29b68412c6300f82868688
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 18:11:22 +0000

    f4c55e464a docs: provide more info on the `NO_ERRORS_SCHEMA` schema (#42327)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+51.sha-cc904b5
+ * @license Angular v12.1.0-next.4+53.sha-f4c55e4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6555,7 +6555,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.4+51.sha-cc904b5');
+    var VERSION = new core.Version('12.1.0-next.4+53.sha-f4c55e4');
 
     /**
      * @license
