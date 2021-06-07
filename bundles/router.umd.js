22c6e414806fdf0f62865073e5a6e04f03c55b56
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 19:16:43 +0000

    8bb6fb7704 docs(router): fix code-example headers (#42507)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+41.sha-65d5930
+ * @license Angular v12.0.3+42.sha-8bb6fb7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6555,7 +6555,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.3+41.sha-65d5930');
+    var VERSION = new core.Version('12.0.3+42.sha-8bb6fb7');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+41.sha-65d5930
+ * @license Angular v12.0.3+42.sha-8bb6fb7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
