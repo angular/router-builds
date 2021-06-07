7929e1572252e2e4cf550f922ce939ad8fff7bda
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 16:56:44 +0000

    5d70f09032 docs: Corrected spelling (#42493)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+48.sha-cd22c7a
+ * @license Angular v12.1.0-next.4+49.sha-5d70f09
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6605,7 +6605,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.4+48.sha-cd22c7a');
+    var VERSION = new i0.Version('12.1.0-next.4+49.sha-5d70f09');
 
     /**
      * @license
