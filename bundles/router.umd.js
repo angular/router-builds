440a2344604e2a20a3ed40f4198cdb6f0218493e
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 19:43:38 +0000

    a493ea9bcb fix(language-service): fix autocomplete info display for some cases (#42472)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+56.sha-f85a120
+ * @license Angular v12.1.0-next.4+57.sha-a493ea9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6555,7 +6555,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.4+56.sha-f85a120');
+    var VERSION = new core.Version('12.1.0-next.4+57.sha-a493ea9');
 
     /**
      * @license
