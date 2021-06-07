0421addc942202b2e2895b6436760e79eab93d44
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 22:53:45 +0000

    b762820485 build: simplify generation of closure locale file (#42230)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+57.sha-a493ea9
+ * @license Angular v12.1.0-next.4+63.sha-b762820
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6555,7 +6555,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.4+57.sha-a493ea9');
+    var VERSION = new core.Version('12.1.0-next.4+63.sha-b762820');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+57.sha-a493ea9
+ * @license Angular v12.1.0-next.4+63.sha-b762820
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
