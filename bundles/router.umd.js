4d78236f6468d5fd80508a7ce4231c830df705d8
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 18:03:43 +0000

    c44ab4f6da fix(router): fix `serializeQueryParams` logic (#42481)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+72.sha-d10c38a
+ * @license Angular v12.1.0-next.4+76.sha-c44ab4f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1339,12 +1339,14 @@
             .join('');
     }
     function serializeQueryParams(params) {
-        var strParams = Object.keys(params).map(function (name) {
+        var strParams = Object.keys(params)
+            .map(function (name) {
             var value = params[name];
             return Array.isArray(value) ?
                 value.map(function (v) { return encodeUriQuery(name) + "=" + encodeUriQuery(v); }).join('&') :
                 encodeUriQuery(name) + "=" + encodeUriQuery(value);
-        });
+        })
+            .filter(function (s) { return !!s; });
         return strParams.length ? "?" + strParams.join('&') : '';
     }
     var SEGMENT_RE = /^[^\/()?;=#]+/;
@@ -6555,7 +6557,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.4+72.sha-d10c38a');
+    var VERSION = new core.Version('12.1.0-next.4+76.sha-c44ab4f');
 
     /**
      * @license
