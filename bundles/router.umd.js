c4c08cd81387add6e975a6e858f2802ea04b1cea
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:04:22 +0000

    5c0541f351 docs: fix issue 24571 in ngOnChanges (#42517)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+82.sha-386550c
+ * @license Angular v12.1.0-next.4+84.sha-5c0541f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6607,7 +6607,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.4+82.sha-386550c');
+    var VERSION = new i0.Version('12.1.0-next.4+84.sha-5c0541f');
 
     /**
      * @license
