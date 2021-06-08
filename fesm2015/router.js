8ad6e4c95c2e00bf6c94f8a2b826513d1f467377
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 19:35:33 +0000

    0031c8cf41 docs: improve accessibility of attribute-binding example (#41432)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+79.sha-8fe4002
+ * @license Angular v12.1.0-next.4+81.sha-0031c8c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5895,7 +5895,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+79.sha-8fe4002');
+const VERSION = new Version('12.1.0-next.4+81.sha-0031c8c');
 
 /**
  * @license
