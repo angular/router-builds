b1d49a14ed4d81c05d6315aebd4d3cac34febff4
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:10:13 +0000

    0777faccfb docs: Edited to remove jargon in Reference, Glossary, and Style sections (#42187)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+87.sha-4975a10
+ * @license Angular v12.1.0-next.4+89.sha-0777fac
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+87.sha-4975a10
+ * @license Angular v12.1.0-next.4+89.sha-0777fac
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6607,7 +6607,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.4+87.sha-4975a10');
+    var VERSION = new i0.Version('12.1.0-next.4+89.sha-0777fac');
 
     /**
      * @license
