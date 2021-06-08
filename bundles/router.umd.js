3f046800f41f4db4bb0f21408369e0224dbab64c
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 17:58:38 +0000

    645cad5614 docs(core): describe interactions between view-encapsulated components (#42397)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+71.sha-751f68d
+ * @license Angular v12.1.0-next.4+73.sha-645cad5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6555,7 +6555,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.4+71.sha-751f68d');
+    var VERSION = new core.Version('12.1.0-next.4+73.sha-645cad5');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+71.sha-751f68d
+ * @license Angular v12.1.0-next.4+73.sha-645cad5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
