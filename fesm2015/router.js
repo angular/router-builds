65cf29cefa113a7deb8df3b2c9bb31a63fee2a3c
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 17:26:52 +0000

    66f49c2497 Revert "refactor: remove checked-in locale files" (#42521)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+64.sha-f1d58ef
+ * @license Angular v12.1.0-next.4+70.sha-66f49c2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5893,7 +5893,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+64.sha-f1d58ef');
+const VERSION = new Version('12.1.0-next.4+70.sha-66f49c2');
 
 /**
  * @license
