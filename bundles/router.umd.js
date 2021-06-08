731afb84191c1ab4e1895792f7348d96c77c83c3
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:09:56 +0000

    4975a106dc docs(platform-server): added commands for universal and add prerendering info (#36005)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+85.sha-43b4a8c
+ * @license Angular v12.1.0-next.4+87.sha-4975a10
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6607,7 +6607,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.4+85.sha-43b4a8c');
+    var VERSION = new i0.Version('12.1.0-next.4+87.sha-4975a10');
 
     /**
      * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+85.sha-43b4a8c
+ * @license Angular v12.1.0-next.4+87.sha-4975a10
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
