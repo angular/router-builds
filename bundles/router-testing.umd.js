ced81f9fa17db7c4d97dcc804539164fc47b812b
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:11:03 +0000

    5f6492e332 docs: Edited to remove jargon in Reference, Glossary, and Style sections (#42187)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+55.sha-5746afe
+ * @license Angular v12.0.3+63.sha-5f6492e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+55.sha-5746afe
+ * @license Angular v12.0.3+63.sha-5f6492e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5895,7 +5895,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.3+55.sha-5746afe');
+const VERSION = new Version('12.0.3+63.sha-5f6492e');
 
 /**
  * @license
