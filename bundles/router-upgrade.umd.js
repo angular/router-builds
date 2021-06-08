32822378208dd1b68e51fe4639a1c531f310f389
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:02:55 +0000

    5c0541f351 docs: fix issue 24571 in ngOnChanges (#42517)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+82.sha-386550c
+ * @license Angular v12.1.0-next.4+84.sha-5c0541f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+82.sha-386550c
+ * @license Angular v12.1.0-next.4+84.sha-5c0541f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5895,7 +5895,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+82.sha-386550c');
+const VERSION = new Version('12.1.0-next.4+84.sha-5c0541f');
 
 /**
  * @license
