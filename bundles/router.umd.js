02c42aa8f4c3f63ced1e9264d2b0bda76075287c
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 18:04:48 +0000

    171428ec98 docs: add selector documentation for `ViewChildren`, `ContentChildren`, and `ContentChild` (#42366)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+74.sha-4cf442c
+ * @license Angular v12.1.0-next.4+75.sha-171428e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6555,7 +6555,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.4+74.sha-4cf442c');
+    var VERSION = new core.Version('12.1.0-next.4+75.sha-171428e');
 
     /**
      * @license
