0d953054277429910c11bf66ad4b0f252c45ca44
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 22:45:03 +0000

    230f0c41b7 build: lock file maintenance (#42499)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+90.sha-34ce635
+ * @license Angular v12.1.0-next.4+91.sha-230f0c4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6557,7 +6557,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.4+90.sha-34ce635');
+    var VERSION = new core.Version('12.1.0-next.4+91.sha-230f0c4');
 
     /**
      * @license
