b15646dc604ecf95986a481ac81a0f1ee713a443
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-09 01:13:51 +0000

    3de774e778 docs: add guidance on adding filtering and sorting logic to componetns instead of pipes (#42368)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+92.sha-e1a80d4
+ * @license Angular v12.1.0-next.4+93.sha-3de774e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6557,7 +6557,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.4+92.sha-e1a80d4');
+    var VERSION = new core.Version('12.1.0-next.4+93.sha-3de774e');
 
     /**
      * @license
