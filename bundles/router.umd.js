a76c3614c0779323d80fc7886ddce21aaca23341
GitHub <noreply@github.com>
2021-06-09 18:22:46 +0000

    dfb072a93c release: cut the v12.0.4 release (#42531)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+65.sha-6203f5e
+ * @license Angular v12.0.3+66.sha-dfb072a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6565,7 +6565,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.3+65.sha-6203f5e');
+    var VERSION = new core.Version('12.0.3+66.sha-dfb072a');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+65.sha-6203f5e
+ * @license Angular v12.0.3+66.sha-dfb072a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
