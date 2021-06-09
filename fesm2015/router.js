5de27078e39e4db456079481d156890e0472db7c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-09 23:37:36 +0000

    665b986896 docs: CLI users polyfills (#42263)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+3.sha-22bda22
+ * @license Angular v12.1.0-next.5+4.sha-665b986
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5936,7 +5936,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+3.sha-22bda22');
+const VERSION = new Version('12.1.0-next.5+4.sha-665b986');
 
 /**
  * @license
