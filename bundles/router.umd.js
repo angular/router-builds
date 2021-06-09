698ebaaf2ce8ce29812948f0d03f78ef66707629
GitHub <noreply@github.com>
2021-06-09 18:27:59 +0000

    7b5e2fef98 docs: release notes for the v12.0.4 release (#42532)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+95.sha-536c373
+ * @license Angular v12.1.0-next.4+96.sha-7b5e2fe
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6565,7 +6565,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.4+95.sha-536c373');
+    var VERSION = new core.Version('12.1.0-next.4+96.sha-7b5e2fe');
 
     /**
      * @license
