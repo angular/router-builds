30c5859447264c4f3e232d7ca667d57247d585b2
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-09 16:36:44 +0000

    3c25242ada build(docs-infra): upgrade cli command docs sources to 18bbd044d (#42525)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+93.sha-3de774e
+ * @license Angular v12.1.0-next.4+94.sha-3c25242
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6607,7 +6607,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.4+93.sha-3de774e');
+    var VERSION = new i0.Version('12.1.0-next.4+94.sha-3c25242');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+93.sha-3de774e
+ * @license Angular v12.1.0-next.4+94.sha-3c25242
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
