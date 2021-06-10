979ad5ef53d4fa17c9ee2d091a2e922b039b4c22
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 17:47:05 +0000

    e299683692 docs: improve accessibility of router example (#40914)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+5.sha-299f92c
+ * @license Angular v12.1.0-next.5+6.sha-e299683
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5903,7 +5903,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+5.sha-299f92c');
+const VERSION = new Version('12.1.0-next.5+6.sha-e299683');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+5.sha-299f92c
+ * @license Angular v12.1.0-next.5+6.sha-e299683
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
