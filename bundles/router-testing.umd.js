ac9ad131509d566c9d39f751c1eef5e13e8f21cb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 16:45:08 +0000

    299f92c3b6 fix(zone.js): only one listener should also re-throw an error correctly (#41868)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+4.sha-665b986
+ * @license Angular v12.1.0-next.5+5.sha-299f92c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+4.sha-665b986
+ * @license Angular v12.1.0-next.5+5.sha-299f92c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5903,7 +5903,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+4.sha-665b986');
+const VERSION = new Version('12.1.0-next.5+5.sha-299f92c');
 
 /**
  * @license
