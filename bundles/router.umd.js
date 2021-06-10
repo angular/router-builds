a9dc8aef75ec914c7f060c9be6040fd9001cc8b7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 21:23:03 +0000

    cddb3a88d3 docs: fix typo in "deploy multiple locales" section (#42541)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+7.sha-aef489f
+ * @license Angular v12.1.0-next.5+9.sha-cddb3a8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6615,7 +6615,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.5+7.sha-aef489f');
+    var VERSION = new i0.Version('12.1.0-next.5+9.sha-cddb3a8');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+7.sha-aef489f
+ * @license Angular v12.1.0-next.5+9.sha-cddb3a8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5936,7 +5936,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+7.sha-aef489f');
+const VERSION = new Version('12.1.0-next.5+9.sha-cddb3a8');
 
 /**
  * @license
