cb1c906c690395155bd04cb725040e4201fc3877
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 21:33:42 +0000

    4001e9d808 fix(language-service): 'go to defininition' for objects defined in template (#42559)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+30.sha-228beea
+ * @license Angular v12.1.0-next.5+31.sha-4001e9d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6012,7 +6012,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+30.sha-228beea');
+const VERSION = new Version('12.1.0-next.5+31.sha-4001e9d');
 
 /**
  * @license
