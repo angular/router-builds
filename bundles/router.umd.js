1ba099a573ee6da30c2146204a5f0a44032dd6b3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 17:15:03 +0000

    828fde6e0d feat(docs-infra): implement popup to inform about the use of cookies (#42259)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+14.sha-5161084
+ * @license Angular v12.1.0-next.5+18.sha-828fde6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6691,7 +6691,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.5+14.sha-5161084');
+    var VERSION = new i0.Version('12.1.0-next.5+18.sha-828fde6');
 
     /**
      * @license
