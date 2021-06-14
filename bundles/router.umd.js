f1203a3e0833783ac708418b93b3c0990b87de8f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 21:30:54 +0000

    228beeabd1 fix(language-service): Use last child end span for parent without close tag (#42554)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+28.sha-044e022
+ * @license Angular v12.1.0-next.5+30.sha-228beea
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6641,7 +6641,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.5+28.sha-044e022');
+    var VERSION = new core.Version('12.1.0-next.5+30.sha-228beea');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+28.sha-044e022
+ * @license Angular v12.1.0-next.5+30.sha-228beea
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
