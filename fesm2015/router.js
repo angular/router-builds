13ddcc70e3309266304ec2d5aa89f4d10fbe0f8d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 17:19:27 +0000

    56a0582d79 fix(docs-infra): correctly serve `index.html` with a query string (#42547)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+21.sha-e7c69b8
+ * @license Angular v12.1.0-next.5+19.sha-56a0582
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5979,7 +5979,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+21.sha-e7c69b8');
+const VERSION = new Version('12.1.0-next.5+19.sha-56a0582');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+21.sha-e7c69b8
+ * @license Angular v12.1.0-next.5+19.sha-56a0582
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
