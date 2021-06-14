12ce98dbdce18f30bd4ccf71418012df10c036cb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 17:19:37 +0000

    e7c69b8551 docs: Grammatical error (#42571)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+20.sha-7180ec9
+ * @license Angular v12.1.0-next.5+21.sha-e7c69b8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6012,7 +6012,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+20.sha-7180ec9');
+const VERSION = new Version('12.1.0-next.5+21.sha-e7c69b8');
 
 /**
  * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+20.sha-7180ec9
+ * @license Angular v12.1.0-next.5+21.sha-e7c69b8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
