a9ab15bcf911c5811ed91bf340e76cb034e2b936
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 21:32:20 +0000

    4001e9d808 fix(language-service): 'go to defininition' for objects defined in template (#42559)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+30.sha-228beea
+ * @license Angular v12.1.0-next.5+31.sha-4001e9d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+30.sha-228beea
+ * @license Angular v12.1.0-next.5+31.sha-4001e9d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6641,7 +6641,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.5+30.sha-228beea');
+    var VERSION = new core.Version('12.1.0-next.5+31.sha-4001e9d');
 
     /**
      * @license
