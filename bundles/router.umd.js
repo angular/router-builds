c94dc4e45f0edec49f4e72919bbb45e96d254ec0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-15 21:22:21 +0000

    6bf4cb6d44 refactor(language-service): add missing helper method (#42579)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.4+21.sha-11e0f53
+ * @license Angular v12.0.4+25.sha-6bf4cb6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6641,7 +6641,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.4+21.sha-11e0f53');
+    var VERSION = new core.Version('12.0.4+25.sha-6bf4cb6');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.4+21.sha-11e0f53
+ * @license Angular v12.0.4+25.sha-6bf4cb6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
