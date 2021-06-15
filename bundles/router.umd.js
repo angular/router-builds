5cd05129f121cb0ac981096944ff7593ae64c500
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-15 20:13:50 +0000

    b52e93543f docs: add Daniel Trevino to contributors file (#42573)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+32.sha-a66dd88
+ * @license Angular v12.1.0-next.5+33.sha-b52e935
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6691,7 +6691,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.5+32.sha-a66dd88');
+    var VERSION = new i0.Version('12.1.0-next.5+33.sha-b52e935');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+32.sha-a66dd88
+ * @license Angular v12.1.0-next.5+33.sha-b52e935
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
