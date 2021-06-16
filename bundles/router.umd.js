126f3c15386d15c5963dbdadd67dd934e97152a7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 17:10:44 +0000

    2d1347b2ce Revert "refactor: remove checked-in locale files (#42230)" (#42583)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+33.sha-b52e935
+ * @license Angular v12.1.0-next.5+40.sha-2d1347b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6641,7 +6641,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.5+33.sha-b52e935');
+    var VERSION = new core.Version('12.1.0-next.5+40.sha-2d1347b');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+33.sha-b52e935
+ * @license Angular v12.1.0-next.5+40.sha-2d1347b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
