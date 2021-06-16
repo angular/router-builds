57d0587fa6d1099050f1909a9686f30d24e61afd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:21:38 +0000

    62aca30286 feat(docs-infra): add support for "special elements" (#41299)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+40.sha-2d1347b
+ * @license Angular v12.1.0-next.5+41.sha-62aca30
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6691,7 +6691,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.5+40.sha-2d1347b');
+    var VERSION = new i0.Version('12.1.0-next.5+41.sha-62aca30');
 
     /**
      * @license
