e148eefe608cafcd413c488f39c89bb3b41fd6f3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 23:03:59 +0000

    7db8e1f82e release: cut the v12.0.5 release (#42585)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.4+32.sha-f9a5d25
+ * @license Angular v12.0.4+33.sha-7db8e1f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.4+32.sha-f9a5d25
+ * @license Angular v12.0.4+33.sha-7db8e1f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6641,7 +6641,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.0.4+32.sha-f9a5d25');
+    var VERSION = new core.Version('12.0.4+33.sha-7db8e1f');
 
     /**
      * @license
