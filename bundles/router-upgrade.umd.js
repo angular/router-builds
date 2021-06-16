51ba82fb5dfd886264047fdc551ccbc6a3a8356b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 23:32:06 +0000

    7961e2fc52 release: bump the next branch to v12.2.0-next.0

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+48.sha-5c25595
+ * @license Angular v12.1.0-next.5+49.sha-7961e2f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+48.sha-5c25595
+ * @license Angular v12.1.0-next.5+49.sha-7961e2f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6691,7 +6691,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.5+48.sha-5c25595');
+    var VERSION = new i0.Version('12.1.0-next.5+49.sha-7961e2f');
 
     /**
      * @license
