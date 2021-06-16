3bebbebfff989ab8c4470070057e864acae86234
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:25:45 +0000

    eb04684ed3 fix(docs-infra): make anchor in list item inherit line-height (#42572)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+46.sha-7b85a4d
+ * @license Angular v12.1.0-next.5+47.sha-eb04684
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6691,7 +6691,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.5+46.sha-7b85a4d');
+    var VERSION = new i0.Version('12.1.0-next.5+47.sha-eb04684');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+46.sha-7b85a4d
+ * @license Angular v12.1.0-next.5+47.sha-eb04684
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
