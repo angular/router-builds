295e16bce8ea92e6126fd4846a7b847d1a5d7ce2
Dylan Hunn <dylhunn@gmail.com>
2021-06-17 18:30:00 +0000

    166e98a594 build: update all non-major dependencies (#42544)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.6+2.sha-0af9b89
+ * @license Angular v12.1.0-next.6+3.sha-166e98a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6691,7 +6691,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.6+2.sha-0af9b89');
+    var VERSION = new i0.Version('12.1.0-next.6+3.sha-166e98a');
 
     /**
      * @license
