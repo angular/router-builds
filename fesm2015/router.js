3805fc6e2dbee085c36369f584a122eaa263f31d
Dylan Hunn <dylhunn@gmail.com>
2021-06-17 18:33:46 +0000

    b037df28fd Revert "fix(docs-infra): do not redirect disambiguated URLs" (#42414)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.6+4.sha-07c1ddc
+ * @license Angular v12.1.0-next.6+7.sha-b037df2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5987,7 +5987,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.6+4.sha-07c1ddc');
+const VERSION = new Version('12.1.0-next.6+7.sha-b037df2');
 
 /**
  * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.6+4.sha-07c1ddc
+ * @license Angular v12.1.0-next.6+7.sha-b037df2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
