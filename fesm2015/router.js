69b58cdabf91e3367e2c86797529b09ebc6d221f
Dylan Hunn <dylhunn@gmail.com>
2021-06-18 17:53:23 +0000

    44291889ee docs(docs-infra): document how to configure redirects (#42452)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.6+13.sha-0c105c3
+ * @license Angular v12.1.0-next.6+23.sha-4429188
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5987,7 +5987,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.6+13.sha-0c105c3');
+const VERSION = new Version('12.1.0-next.6+23.sha-4429188');
 
 /**
  * @license
