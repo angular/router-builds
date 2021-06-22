15464d0f450eca77454508851add95f1765fb810
Dylan Hunn <dylhunn@gmail.com>
2021-06-22 16:55:02 +0000

    8a67770687 docs(core): improve applicationref.bootstrap docs (#42407)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.6+38.sha-9498da1
+ * @license Angular v12.1.0-next.6+39.sha-8a67770
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6020,7 +6020,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.6+38.sha-9498da1');
+const VERSION = new Version('12.1.0-next.6+39.sha-8a67770');
 
 /**
  * @license
