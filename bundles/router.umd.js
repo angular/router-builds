d71ddff5335283f56f1322ef5c06981686a9ad05
Dylan Hunn <dylhunn@gmail.com>
2021-06-22 16:46:36 +0000

    81a19e4e65 feat(dev-infra): update pullapprove verification to ensure all groups have reviewers (#42614)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.6+36.sha-75bbcf7
+ * @license Angular v12.1.0-next.6+37.sha-81a19e4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6649,7 +6649,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.0-next.6+36.sha-75bbcf7');
+    var VERSION = new core.Version('12.1.0-next.6+37.sha-81a19e4');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.6+36.sha-75bbcf7
+ * @license Angular v12.1.0-next.6+37.sha-81a19e4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
