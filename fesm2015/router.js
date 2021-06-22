26c4717f83cce5657fee945216e0cbb9b7239aa5
Dylan Hunn <dylhunn@gmail.com>
2021-06-22 16:48:03 +0000

    81a19e4e65 feat(dev-infra): update pullapprove verification to ensure all groups have reviewers (#42614)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.6+36.sha-75bbcf7
+ * @license Angular v12.1.0-next.6+37.sha-81a19e4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6020,7 +6020,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.6+36.sha-75bbcf7');
+const VERSION = new Version('12.1.0-next.6+37.sha-81a19e4');
 
 /**
  * @license
