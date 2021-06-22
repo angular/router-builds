ad56096bf745de87355384369f6d0f064d2aaa51
Dylan Hunn <dylhunn@gmail.com>
2021-06-22 17:03:40 +0000

    9de65dbdce fix(compiler): should not break a text token on a non-valid start tag (#42605)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.6+39.sha-8a67770
+ * @license Angular v12.1.0-next.6+41.sha-9de65db
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6699,7 +6699,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.6+39.sha-8a67770');
+    var VERSION = new i0.Version('12.1.0-next.6+41.sha-9de65db');
 
     /**
      * @license
