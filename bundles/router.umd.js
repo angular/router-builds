284867ce080ead915f9b556d23bb6e82839e44fb
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-25 00:45:45 +0000

    261b060fa1 fix(dev-infra): fetch 100 branches from Github instead of 30 (#42658)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0
+ * @license Angular v12.2.0-next.0+1.sha-261b060
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6705,7 +6705,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.0');
+    var VERSION = new i0.Version('12.2.0-next.0+1.sha-261b060');
 
     /**
      * @license
