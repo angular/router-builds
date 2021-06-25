c0504d25232df7c888c7961b4af7c025020a6a61
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-25 00:44:19 +0000

    261b060fa1 fix(dev-infra): fetch 100 branches from Github instead of 30 (#42658)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0
+ * @license Angular v12.2.0-next.0+1.sha-261b060
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5987,7 +5987,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.0');
+const VERSION = new Version('12.2.0-next.0+1.sha-261b060');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0
+ * @license Angular v12.2.0-next.0+1.sha-261b060
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
