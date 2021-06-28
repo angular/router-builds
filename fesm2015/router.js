577dec1bbcdbf0cd1ac2aea62d73c628302975f0
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 18:46:51 +0000

    b54e8aee37 docs: update fonts optimization options description (#42642)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+12.sha-c93df7d
+ * @license Angular v12.2.0-next.0+13.sha-b54e8ae
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5987,7 +5987,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.0+12.sha-c93df7d');
+const VERSION = new Version('12.2.0-next.0+13.sha-b54e8ae');
 
 /**
  * @license
