604a78a7a1ace5cd12c2b25a1a68faaaac4389dc
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 16:53:41 +0000

    56fb4eb08d docs(compiler): interally typo fixed (#42676)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+3.sha-37a740c
+ * @license Angular v12.2.0-next.0+4.sha-56fb4eb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6705,7 +6705,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.0+3.sha-37a740c');
+    var VERSION = new i0.Version('12.2.0-next.0+4.sha-56fb4eb');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+3.sha-37a740c
+ * @license Angular v12.2.0-next.0+4.sha-56fb4eb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6020,7 +6020,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.0+3.sha-37a740c');
+const VERSION = new Version('12.2.0-next.0+4.sha-56fb4eb');
 
 /**
  * @license
