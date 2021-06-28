66d7af19f49aa4dd5babfdd6ef76eafcc56c99ce
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 17:12:25 +0000

    1142b51462 fix(docs-infra): styling fix on error pages (#42627)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+10.sha-4bea630
+ * @license Angular v12.2.0-next.0+11.sha-1142b51
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6705,7 +6705,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.0+10.sha-4bea630');
+    var VERSION = new i0.Version('12.2.0-next.0+11.sha-1142b51');
 
     /**
      * @license
