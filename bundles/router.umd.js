7426c398529d088fe4ac61384651858bcf86991b
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 16:54:42 +0000

    2a260dc249 docs: update Roadmap section header from "Done" to "Completed" (#42620)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+4.sha-56fb4eb
+ * @license Angular v12.2.0-next.0+9.sha-2a260dc
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6705,7 +6705,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.0+4.sha-56fb4eb');
+    var VERSION = new i0.Version('12.2.0-next.0+9.sha-2a260dc');
 
     /**
      * @license
