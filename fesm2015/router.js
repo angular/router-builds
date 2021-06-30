7e2dd49246e7e4d676a84cd3e7fa37a1125b2997
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 21:52:50 +0000

    0f23f7343e fix(core): error in TestBed if module is reset mid-compilation in ViewEngine (#42669)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+29.sha-1445dba
+ * @license Angular v12.2.0-next.1+1.sha-0f23f73
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -5987,7 +5987,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.0+29.sha-1445dba');
+const VERSION = new Version('12.2.0-next.1+1.sha-0f23f73');
 
 /**
  * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+29.sha-1445dba
+ * @license Angular v12.2.0-next.1+1.sha-0f23f73
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
