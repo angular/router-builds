f8ea64486789f6379ae0aa99a03018ce808edc74
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 17:55:00 +0000

    234b5edcc7 fix(platform-browser): in `Meta.addTag()` do not add duplicate meta tags (#42703)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+21.sha-f3965ff
+ * @license Angular v12.2.0-next.0+22.sha-234b5ed
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6655,7 +6655,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.2.0-next.0+21.sha-f3965ff');
+    var VERSION = new core.Version('12.2.0-next.0+22.sha-234b5ed');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+21.sha-f3965ff
+ * @license Angular v12.2.0-next.0+22.sha-234b5ed
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
