3cca208c6a71624b693a02a7118115a43a232777
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 18:00:48 +0000

    9f5cc7c808 feat(compiler): support number separators in templates (#42672)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+22.sha-234b5ed
+ * @license Angular v12.2.0-next.0+23.sha-9f5cc7c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6705,7 +6705,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.0+22.sha-234b5ed');
+    var VERSION = new i0.Version('12.2.0-next.0+23.sha-9f5cc7c');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+22.sha-234b5ed
+ * @license Angular v12.2.0-next.0+23.sha-9f5cc7c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6020,7 +6020,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.0+22.sha-234b5ed');
+const VERSION = new Version('12.2.0-next.0+23.sha-9f5cc7c');
 
 /**
  * @license
