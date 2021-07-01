7847fa40889c8bc1d132bf8bf3d2b22cefdba2ee
atscott <atscott01@gmail.com>
2021-07-01 22:43:56 +0000

    ffeea63f43 fix(language-service): Do not override TS LS methods not supported by VE NgLS (#42727)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+4.sha-74350a5
+ * @license Angular v12.2.0-next.1+5.sha-ffeea63
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6655,7 +6655,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.2.0-next.1+4.sha-74350a5');
+    var VERSION = new core.Version('12.2.0-next.1+5.sha-ffeea63');
 
     /**
      * @license
