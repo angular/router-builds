fbb5ef796867f8900f3bd49666d975118ce2d7b2
Jessica Janiuk <jessicajaniuk@google.com>
2021-07-01 16:54:00 +0000

    d997303cf4 build: update dependency husky to v7 (#42730)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+1.sha-0f23f73
+ * @license Angular v12.2.0-next.1+2.sha-d997303
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6705,7 +6705,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.1+1.sha-0f23f73');
+    var VERSION = new i0.Version('12.2.0-next.1+2.sha-d997303');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+1.sha-0f23f73
+ * @license Angular v12.2.0-next.1+2.sha-d997303
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
