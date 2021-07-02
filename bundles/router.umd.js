c96d85efb7b6003b948f938aa01d324a44f6fb8e
atscott <atscott01@gmail.com>
2021-07-02 17:11:43 +0000

    e81efe6667 docs: convert commit SHAs and PR numbers in changelog to links (#42732)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+7.sha-19bcdfb
+ * @license Angular v12.2.0-next.1+9.sha-e81efe6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6705,7 +6705,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.1+7.sha-19bcdfb');
+    var VERSION = new i0.Version('12.2.0-next.1+9.sha-e81efe6');
 
     /**
      * @license
