e4cdeecf67ea988556eb16bf84afcd6e30dfc013
atscott <atscott01@gmail.com>
2021-07-02 17:10:34 +0000

    19bcdfb028 fix(docs-infra): correctly configure top-menu nav-item in the sidenav (#42747)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+5.sha-ffeea63
+ * @license Angular v12.2.0-next.1+7.sha-19bcdfb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+5.sha-ffeea63
+ * @license Angular v12.2.0-next.1+7.sha-19bcdfb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6020,7 +6020,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+5.sha-ffeea63');
+const VERSION = new Version('12.2.0-next.1+7.sha-19bcdfb');
 
 /**
  * @license
