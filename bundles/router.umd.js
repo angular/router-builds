12ff9d86eb544e1cb15e161ebb9830a3c7e78c0a
Andrew Scott <atscott@google.com>
2021-07-07 20:57:46 +0000

    24ac37891c fix(docs-infra): slightly improve top-menu responsiveness (#42753)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+18.sha-e064f17
+ * @license Angular v12.2.0-next.1+19.sha-24ac378
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6705,7 +6705,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.1+18.sha-e064f17');
+    var VERSION = new i0.Version('12.2.0-next.1+19.sha-24ac378');
 
     /**
      * @license
