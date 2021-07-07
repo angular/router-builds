6ded38c14dc4173f1e64b8cfebad7a971b70a463
Andrew Scott <atscott@google.com>
2021-07-07 20:59:06 +0000

    8b95816fc4 fix(docs-infra): log more SW debug info (#42776)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+19.sha-24ac378
+ * @license Angular v12.2.0-next.1+24.sha-8b95816
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6705,7 +6705,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.1+19.sha-24ac378');
+    var VERSION = new i0.Version('12.2.0-next.1+24.sha-8b95816');
 
     /**
      * @license
