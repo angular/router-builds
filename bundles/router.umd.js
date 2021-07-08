8346e93f7ca8797d0055059c20d66f712140c59f
atscott <atscott01@gmail.com>
2021-07-08 17:52:17 +0000

    cb2ca9a66e fix(service-worker): correctly handle unrecoverable state when a client no longer exists (#42736)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+29.sha-ad08b17
+ * @license Angular v12.2.0-next.1+39.sha-cb2ca9a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6705,7 +6705,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.1+29.sha-ad08b17');
+    var VERSION = new i0.Version('12.2.0-next.1+39.sha-cb2ca9a');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+29.sha-ad08b17
+ * @license Angular v12.2.0-next.1+39.sha-cb2ca9a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
