be61a0683e6d826a7681f83bd5a13e4027f36283
atscott <atscott01@gmail.com>
2021-07-08 21:25:51 +0000

    e26bfc1131 refactor(core): remove no longer needed types workaround (#42798)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+28.sha-f592a12
+ * @license Angular v12.2.0-next.1+40.sha-e26bfc1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+28.sha-f592a12
+ * @license Angular v12.2.0-next.1+40.sha-e26bfc1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6655,7 +6655,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.2.0-next.1+28.sha-f592a12');
+    var VERSION = new core.Version('12.2.0-next.1+40.sha-e26bfc1');
 
     /**
      * @license
