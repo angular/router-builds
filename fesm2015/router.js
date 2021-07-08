0b0c1e5a24da872787130f7380fe718184497430
atscott <atscott01@gmail.com>
2021-07-08 21:29:14 +0000

    c30502ee1b ci: use issue forms for issue creation (#42099)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+40.sha-e26bfc1
+ * @license Angular v12.2.0-next.1+42.sha-c30502e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6020,7 +6020,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+40.sha-e26bfc1');
+const VERSION = new Version('12.2.0-next.1+42.sha-c30502e');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+40.sha-e26bfc1
+ * @license Angular v12.2.0-next.1+42.sha-c30502e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
