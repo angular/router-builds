08c2411d134b9034bf04598c54162e3a4505fdcc
atscott <atscott01@gmail.com>
2021-07-08 22:12:40 +0000

    e7832a6b2f docs(core): fix spelling error (#42783)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+42.sha-c30502e
+ * @license Angular v12.2.0-next.1+43.sha-e7832a6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6705,7 +6705,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.1+42.sha-c30502e');
+    var VERSION = new i0.Version('12.2.0-next.1+43.sha-e7832a6');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+42.sha-c30502e
+ * @license Angular v12.2.0-next.1+43.sha-e7832a6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
