1781f76486dd12fd5b08de10d747881affcae351
atscott <atscott01@gmail.com>
2021-07-08 17:53:03 +0000

    f592a12005 fix(service-worker): avoid storing redundant metadata for hashed assets (#42606)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+39.sha-cb2ca9a
+ * @license Angular v12.2.0-next.1+28.sha-f592a12
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6705,7 +6705,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.1+39.sha-cb2ca9a');
+    var VERSION = new i0.Version('12.2.0-next.1+28.sha-f592a12');
 
     /**
      * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+39.sha-cb2ca9a
+ * @license Angular v12.2.0-next.1+28.sha-f592a12
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
