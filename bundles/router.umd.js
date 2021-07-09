7e7be322df405b503f70de77f1dffbbfaaaaab6a
Andrew Kushnir <akushnir@google.com>
2021-07-09 17:35:19 +0000

    9ec241926e build: update all non-major dependencies (#42739)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+46.sha-dfe9e1d
+ * @license Angular v12.2.0-next.1+47.sha-9ec2419
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6705,7 +6705,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.1+46.sha-dfe9e1d');
+    var VERSION = new i0.Version('12.2.0-next.1+47.sha-9ec2419');
 
     /**
      * @license
