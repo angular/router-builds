21114f2d8dd68143e74513ab6cf27e49e1a41ce2
Andrew Kushnir <akushnir@google.com>
2021-07-09 22:09:12 +0000

    762e057e85 build: update to rules_nodejs v4.0.0-beta.0 (#42760)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+53.sha-9d58ebf
+ * @license Angular v12.2.0-next.1+58.sha-762e057
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6695,7 +6695,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.2.0-next.1+53.sha-9d58ebf');
+    var VERSION = new core.Version('12.2.0-next.1+58.sha-762e057');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+53.sha-9d58ebf
+ * @license Angular v12.2.0-next.1+58.sha-762e057
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6023,7 +6023,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+53.sha-9d58ebf');
+const VERSION = new Version('12.2.0-next.1+58.sha-762e057');
 
 /**
  * @license
