0ac3e31ffdf105e309811be3773fb25a321ec1eb
Andrew Kushnir <akushnir@google.com>
2021-07-12 22:44:34 +0000

    c1c1cda866 Revert "refactor(dev-infra): add spawnSync to child process utils, normalize naming of child-process utils (#42394)" (#42829)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+91.sha-9456eca
+ * @license Angular v12.2.0-next.1+98.sha-c1c1cda
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6695,7 +6695,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.2.0-next.1+91.sha-9456eca');
+    var VERSION = new core.Version('12.2.0-next.1+98.sha-c1c1cda');
 
     /**
      * @license
