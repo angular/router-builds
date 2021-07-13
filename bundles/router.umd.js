4bf9cc9a269ccf933aa8c2278922c1985674fc7e
Andrew Kushnir <akushnir@google.com>
2021-07-13 21:28:50 +0000

    970d7f7c6f docs: update routing-overview topic with link to custom route matches tutorial (#42837)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+101.sha-e42aa6c
+ * @license Angular v12.2.0-next.1+102.sha-970d7f7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6695,7 +6695,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.2.0-next.1+101.sha-e42aa6c');
+    var VERSION = new core.Version('12.2.0-next.1+102.sha-970d7f7');
 
     /**
      * @license
