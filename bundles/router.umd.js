df65292ee80fe6ceec164c5997c7db8f5be8d647
Andrew Kushnir <akushnir@google.com>
2021-07-13 16:11:44 +0000

    81dce5c664 fix(compiler-cli): check split two way binding (#42601)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+99.sha-88b15d5
+ * @license Angular v12.2.0-next.1+100.sha-81dce5c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6745,7 +6745,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.1+99.sha-88b15d5');
+    var VERSION = new i0.Version('12.2.0-next.1+100.sha-81dce5c');
 
     /**
      * @license
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+99.sha-88b15d5
+ * @license Angular v12.2.0-next.1+100.sha-81dce5c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6056,7 +6056,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+99.sha-88b15d5');
+const VERSION = new Version('12.2.0-next.1+100.sha-81dce5c');
 
 /**
  * @license
