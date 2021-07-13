d200c95d436fb9ce6354c5bb89f594b0155b96d9
Andrew Kushnir <akushnir@google.com>
2021-07-13 18:58:19 +0000

    e42aa6c13b fix(common): re-sort output of `KeyValuePipe` when `compareFn` changes (#42821)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+100.sha-81dce5c
+ * @license Angular v12.2.0-next.1+101.sha-e42aa6c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6695,7 +6695,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.2.0-next.1+100.sha-81dce5c');
+    var VERSION = new core.Version('12.2.0-next.1+101.sha-e42aa6c');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+100.sha-81dce5c
+ * @license Angular v12.2.0-next.1+101.sha-e42aa6c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
