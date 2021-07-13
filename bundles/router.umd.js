a895119a4b0f9733fdf2afd8b163de2b53401519
Andrew Kushnir <akushnir@google.com>
2021-07-13 23:20:57 +0000

    cd2d82a91a fix(core): associate the NgModule scope for an overridden component (#42817)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+103.sha-51156f3
+ * @license Angular v12.2.0-next.1+104.sha-cd2d82a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6745,7 +6745,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.1+103.sha-51156f3');
+    var VERSION = new i0.Version('12.2.0-next.1+104.sha-cd2d82a');
 
     /**
      * @license
