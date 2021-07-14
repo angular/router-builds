ec787234fad25446a0a244aab701e7fb4b0dfc34
Andrew Kushnir <akushnir@google.com>
2021-07-14 18:32:45 +0000

    64bb7cd704 docs: update incorrect bullet text (#42839)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+106.sha-0b429d3
+ * @license Angular v12.2.0-next.2+1.sha-64bb7cd
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6695,7 +6695,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.2.0-next.1+106.sha-0b429d3');
+    var VERSION = new core.Version('12.2.0-next.2+1.sha-64bb7cd');
 
     /**
      * @license
