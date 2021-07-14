f9aae31d615273639c3937ff9a85ea7621989ecd
GitHub <noreply@github.com>
2021-07-14 18:26:35 +0000

    0b429d314f release: cut the v12.2.0-next.2 release (#42858)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+105.sha-4b8f7c8
+ * @license Angular v12.2.0-next.1+106.sha-0b429d3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6695,7 +6695,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.2.0-next.1+105.sha-4b8f7c8');
+    var VERSION = new core.Version('12.2.0-next.1+106.sha-0b429d3');
 
     /**
      * @license
