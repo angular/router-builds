3d216128205096d890182818aef25c7eeba6d400
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:55:06 +0000

    97fa1cf544 build: add configuration for caretaker handoff (#42859)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.2+3.sha-e6f01d7
+ * @license Angular v12.1.2+13.sha-97fa1cf
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.2+3.sha-e6f01d7
+ * @license Angular v12.1.2+13.sha-97fa1cf
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6023,7 +6023,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.2+3.sha-e6f01d7');
+const VERSION = new Version('12.1.2+13.sha-97fa1cf');
 
 /**
  * @license
