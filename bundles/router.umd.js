d811ae6e40e33e150a2223d5fd8dda5df6abf4f9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 19:17:04 +0000

    29f50976db fix(docs-infra): fix keyword color in CLI pages code blocks (#42889)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.2+29.sha-e6b7472
+ * @license Angular v12.1.2+33.sha-29f5097
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6695,7 +6695,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.1.2+29.sha-e6b7472');
+    var VERSION = new core.Version('12.1.2+33.sha-29f5097');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.2+29.sha-e6b7472
+ * @license Angular v12.1.2+33.sha-29f5097
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
