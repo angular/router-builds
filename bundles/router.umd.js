d8919f679a3f117ed9386c313723511dc3aaf6f3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:55:31 +0000

    722eb5dd45 fix(docs-infra): prevent main content from overlapping with ToC (#42884)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+35.sha-7271ad1
+ * @license Angular v12.2.0-next.2+38.sha-722eb5d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6695,7 +6695,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.2.0-next.2+35.sha-7271ad1');
+    var VERSION = new core.Version('12.2.0-next.2+38.sha-722eb5d');
 
     /**
      * @license
