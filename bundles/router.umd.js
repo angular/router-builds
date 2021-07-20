df3915070085d69ab00406845c00cb57de9f4a3c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:57:03 +0000

    307dac67bc fix(core): use correct injector when resolving DI tokens from within a directive provider factory (#42886)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+38.sha-722eb5d
+ * @license Angular v12.2.0-next.2+39.sha-307dac6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6695,7 +6695,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.2.0-next.2+38.sha-722eb5d');
+    var VERSION = new core.Version('12.2.0-next.2+39.sha-307dac6');
 
     /**
      * @license
