811e6e86b6edb13cde019257fbefd2f0aab39049
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:53:03 +0000

    4286b08b5d ci: exempt bazel scope from merge tool target label checks (#42869)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+32.sha-96d31d8
+ * @license Angular v12.2.0-next.2+33.sha-4286b08
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6745,7 +6745,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.2+32.sha-96d31d8');
+    var VERSION = new i0.Version('12.2.0-next.2+33.sha-4286b08');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+32.sha-96d31d8
+ * @license Angular v12.2.0-next.2+33.sha-4286b08
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
