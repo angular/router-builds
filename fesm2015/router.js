f485eb3adb3ff8c59056d22ee29a224ebc75ec18
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:56:49 +0000

    722eb5dd45 fix(docs-infra): prevent main content from overlapping with ToC (#42884)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+35.sha-7271ad1
+ * @license Angular v12.2.0-next.2+38.sha-722eb5d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6056,7 +6056,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.2+35.sha-7271ad1');
+const VERSION = new Version('12.2.0-next.2+38.sha-722eb5d');
 
 /**
  * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+35.sha-7271ad1
+ * @license Angular v12.2.0-next.2+38.sha-722eb5d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
