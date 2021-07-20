3fab3a3bb6691b232c69436bb78d5910e3cd56d0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:54:09 +0000

    7eddd12bf0 feat(dev-infra): add option to setup global types in API golden test (#42876)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+34.sha-30165f9
+ * @license Angular v12.2.0-next.2+36.sha-7eddd12
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6745,7 +6745,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.2+34.sha-30165f9');
+    var VERSION = new i0.Version('12.2.0-next.2+36.sha-7eddd12');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+34.sha-30165f9
+ * @license Angular v12.2.0-next.2+36.sha-7eddd12
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
