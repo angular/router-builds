2ac52931826915cb93cada1a652474099809e97c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:36:11 +0000

    96d31d8c9f fix(dev-infra): prevent verbose messaging of git checkout and commit for release (#42871)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+28.sha-0ce8f6e
+ * @license Angular v12.2.0-next.2+32.sha-96d31d8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6695,7 +6695,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.2.0-next.2+28.sha-0ce8f6e');
+    var VERSION = new core.Version('12.2.0-next.2+32.sha-96d31d8');
 
     /**
      * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+28.sha-0ce8f6e
+ * @license Angular v12.2.0-next.2+32.sha-96d31d8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
