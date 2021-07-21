ac97d791f362a5b1f4a91f5266edc0885b1a56a3
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 22:59:19 +0000

    a502279592 feat(forms): allow minLength/maxLength validator to be bound to `null` (#42565)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.3+4.sha-eefe168.with-local-changes
+ * @license Angular v12.2.0-next.3+5.sha-a502279.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
