14fdb403645b03e64a8ec4c43db77e9bb0ae27d8
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 22:57:25 +0000

    eefe1682e8 fix(core): correctly handle `null` or `undefined` in `ErrorHandler#handleError()` (#42881)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.3+2.sha-992dc93.with-local-changes
+ * @license Angular v12.2.0-next.3+4.sha-eefe168.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
