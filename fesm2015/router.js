ede3b3f9c821b033d2f3986d339184fefd648658
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 22:56:02 +0000

    992dc93ea3 refactor(dev-infra): remove usages and dependency on shelljs (#42911)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.3+3.sha-4d86ea6.with-local-changes
+ * @license Angular v12.2.0-next.3+2.sha-992dc93.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6056,7 +6056,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.3+3.sha-4d86ea6.with-local-changes');
+const VERSION = new Version('12.2.0-next.3+2.sha-992dc93.with-local-changes');
 
 /**
  * @license
