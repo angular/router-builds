2af80e0c5fa87e3c89c5af291db47adafeca7698
Dylan Hunn <dylhunn@gmail.com>
2021-07-22 16:49:13 +0000

    b5f177939f docs: Edits to remove jargon (#42907)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.3+9.sha-7e9673d.with-local-changes
+ * @license Angular v12.2.0-next.3+7.sha-b5f1779.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6745,7 +6745,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.3+9.sha-7e9673d.with-local-changes');
+    var VERSION = new i0.Version('12.2.0-next.3+7.sha-b5f1779.with-local-changes');
 
     /**
      * @license
