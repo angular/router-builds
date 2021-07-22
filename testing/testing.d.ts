5c113a638640d0f49a1b1ae584cc94bd2e9b1360
Dylan Hunn <dylhunn@gmail.com>
2021-07-22 16:47:38 +0000

    b5f177939f docs: Edits to remove jargon (#42907)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.3+9.sha-7e9673d.with-local-changes
+ * @license Angular v12.2.0-next.3+7.sha-b5f1779.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
