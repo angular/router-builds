75806ea92d980fa10835f63612599af83305f7ac
Dylan Hunn <dylhunn@gmail.com>
2021-07-22 16:47:42 +0000

    7e9673d14a ci: fix typo in docs-bug issue template (#42908)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.3+6.sha-5fb23ec.with-local-changes
+ * @license Angular v12.2.0-next.3+9.sha-7e9673d.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6745,7 +6745,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.3+6.sha-5fb23ec.with-local-changes');
+    var VERSION = new i0.Version('12.2.0-next.3+9.sha-7e9673d.with-local-changes');
 
     /**
      * @license
