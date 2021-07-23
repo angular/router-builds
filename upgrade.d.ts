c7d3144585b68321fcdabe7b584f806eb1a1c440
Dylan Hunn <dylhunn@gmail.com>
2021-07-23 19:31:11 +0000

    8fc08c5ab1 ci: update pullapprove config to properly handle global approvals (#42929)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+71.sha-9073633.with-local-changes
+ * @license Angular v12.2.0-next.1+73.sha-c74927d.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
