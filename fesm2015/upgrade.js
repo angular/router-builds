1daecc821f78388f1812360c97ba3bf7f1dcfafd
Dylan Hunn <dylhunn@gmail.com>
2021-07-26 19:08:39 +0000

    271700cbb6 docs: fix the selector in component interaction guide (#42891)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+77.sha-abc77a6.with-local-changes
+ * @license Angular v12.2.0-next.1+78.sha-634ba9c.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
