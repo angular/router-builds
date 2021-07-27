439f4a58308935b9464b32a325e3f15bc2785889
Dylan Hunn <dylhunn@gmail.com>
2021-07-27 01:26:07 +0000

    057e577403 refactor(compiler-cli): add makeTemplateDiagnostic wrapper (#42937)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+81.sha-c13ccc3.with-local-changes
+ * @license Angular v12.2.0-next.1+82.sha-01e869a.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6777,7 +6777,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.1+81.sha-c13ccc3.with-local-changes');
+    var VERSION = new i0.Version('12.2.0-next.1+82.sha-01e869a.with-local-changes');
 
     /**
      * @license
