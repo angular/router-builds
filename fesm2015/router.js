143738036ff7229573ec2f55806316227bc6e79f
Dylan Hunn <dylhunn@gmail.com>
2021-07-27 16:46:41 +0000

    85e93c3833 fix(docs-infra): notify `ErrorHandler` of `UnrecoverableState` errors (#42941)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+83.sha-388496c.with-local-changes
+ * @license Angular v12.2.0-next.1+85.sha-bfc4c3c.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6087,7 +6087,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+83.sha-388496c.with-local-changes');
+const VERSION = new Version('12.2.0-next.1+85.sha-bfc4c3c.with-local-changes');
 
 /**
  * @license
