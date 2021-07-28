abcf09373b5ca70e440f13a78278d57f7cd27ac6
Dylan Hunn <dylhunn@gmail.com>
2021-07-28 18:18:43 +0000

    a2c893309d build: update bazel to v4.0.0-beta.1 (#42967)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+87.sha-1dffa51.with-local-changes
+ * @license Angular v12.2.0-next.1+90.sha-a524af1.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
