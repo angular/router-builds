a6314c6d76014fae05208ca121c5a19128cb31c6
Dylan Hunn <dylhunn@gmail.com>
2021-07-28 23:15:39 +0000

    8ce1ac603a docs: Add josmar-crwdstffng to contributors list. (#42972)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+94.sha-08444c6.with-local-changes
+ * @license Angular v12.2.0-next.1+95.sha-f0d857e.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
