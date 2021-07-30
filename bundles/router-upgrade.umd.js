ff101b5e7855c24500da1d097924d4443bb4d5cf
Andrew Kushnir <akushnir@google.com>
2021-07-30 16:52:54 +0000

    0af354ce05 refactor(bazel): extract function to patch fileNameToModuleName on host (#42974)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+98.sha-c1c1cda.with-local-changes
+ * @license Angular v12.2.0-next.1+100.sha-81dce5c.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
