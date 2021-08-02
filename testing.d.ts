4e95ca3d0bcc3389d91cf5fbacfe352c56ceeee3
atscott <atscott01@gmail.com>
2021-08-02 17:13:36 +0000

    fe12651580 test(compiler): add a test for parsing multiline expressions in attributes (#42062)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+97.sha-3d66816.with-local-changes
+ * @license Angular v12.2.0-next.2+8.sha-c556128.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
