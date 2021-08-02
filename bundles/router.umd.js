429be0075cb1c25e809a58235b54a4f2d87752aa
atscott <atscott01@gmail.com>
2021-08-02 17:59:24 +0000

    8861d73e7f build: rename shims_for_IE.js -> shims_for_internal_tests.js (#43002)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+8.sha-c556128.with-local-changes
+ * @license Angular v12.2.0-next.2+10.sha-670300e.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6777,7 +6777,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.2+8.sha-c556128.with-local-changes');
+    var VERSION = new i0.Version('12.2.0-next.2+10.sha-670300e.with-local-changes');
 
     /**
      * @license
