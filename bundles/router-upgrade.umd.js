94fb06f26f250ff54089f796086089a000baca29
Andrew Scott <atscott@google.com>
2021-08-02 21:16:19 +0000

    0e47334db8 refactor(core): update peerDependencies to allow rxjs7 (#42991)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+6.sha-ca1fde9.with-local-changes
+ * @license Angular v12.2.0-next.2+7.sha-c11bcb7.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
