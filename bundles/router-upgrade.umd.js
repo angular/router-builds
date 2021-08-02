019c51521dcf8cee3e5c74bef82fdc32465bc79c
atscott <atscott01@gmail.com>
2021-08-02 17:16:21 +0000

    3574e564ad test(compiler): add a test for parsing multiline expressions in attributes (#42062)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+68.sha-a926402.with-local-changes
+ * @license Angular v12.1.1+82.sha-04472d2.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
