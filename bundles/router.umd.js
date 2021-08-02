3d8981892f5c2e9683a7e26d072465179fb61008
atscott <atscott01@gmail.com>
2021-08-02 22:58:27 +0000

    f62366fd16 docs: amend links in content-projection guide (#43009)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+12.sha-f5baa55.with-local-changes
+ * @license Angular v12.2.0-next.2+13.sha-7422821.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6777,7 +6777,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.2+12.sha-f5baa55.with-local-changes');
+    var VERSION = new i0.Version('12.2.0-next.2+13.sha-7422821.with-local-changes');
 
     /**
      * @license
