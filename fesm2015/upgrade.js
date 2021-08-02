fc0bef69b3d34f4768bd1c4e16b55d66e332551b
Andrew Scott <atscott@google.com>
2021-08-02 21:16:10 +0000

    9a3cf661a2 refactor(core): update peerDependencies to allow rxjs7 (#42991)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+11.sha-4e97514.with-local-changes
+ * @license Angular v12.2.0-next.2+12.sha-f5baa55.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
