abe522d089655336213aa5af3b02d3735184ead6
atscott <atscott01@gmail.com>
2021-08-03 22:58:58 +0000

    6875d9463f Revert "refactor(ngcc): remove unused import (#42062)" (#43033)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+83.sha-4f8a728.with-local-changes
+ * @license Angular v12.1.2.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
