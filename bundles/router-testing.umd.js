39ac3bc48bbe12bc0fcea77b6151ac68628cfca3
GitHub <noreply@github.com>
2021-08-04 17:43:51 +0000

    cdc8c7cad0 release: cut the v12.2.0 release (#43047)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+24.sha-3b2f607.with-local-changes
+ * @license Angular v12.2.0-next.2+25.sha-5d7359e.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
