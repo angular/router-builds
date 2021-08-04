d527265c67d060c089b7dfa08ea0997a89636a04
GitHub <noreply@github.com>
2021-08-04 19:04:56 +0000

    a99892e62f release: cut the v13.0.0-next.0 release (#43049)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+31.sha-fd92a3e.with-local-changes
+ * @license Angular v12.2.0-next.2+32.sha-96d31d8.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6777,7 +6777,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.2+31.sha-fd92a3e.with-local-changes');
+    var VERSION = new i0.Version('12.2.0-next.2+32.sha-96d31d8.with-local-changes');
 
     /**
      * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+31.sha-fd92a3e.with-local-changes
+ * @license Angular v12.2.0-next.2+32.sha-96d31d8.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
