1d1c5146066bf5755771074fb9644cc49abf056c
Andrew Kushnir <akushnir@google.com>
2021-08-04 23:48:50 +0000

    e49fc96ed3 feat(forms): Make Form Statuses use stricter types. (#42952)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+33.sha-4286b08.with-local-changes
+ * @license Angular v12.2.0-next.2+34.sha-30165f9.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6777,7 +6777,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.2+33.sha-4286b08.with-local-changes');
+    var VERSION = new i0.Version('12.2.0-next.2+34.sha-30165f9.with-local-changes');
 
     /**
      * @license
