5a7e120271dd6c8b46b02d4a1ebc9c2f450d19d8
GitHub <noreply@github.com>
2021-08-04 17:56:31 +0000

    4f93984e41 docs: release notes for the v12.2.0 release (#43048)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+30.sha-89509df.with-local-changes
+ * @license Angular v12.2.0-next.2+31.sha-fd92a3e.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
