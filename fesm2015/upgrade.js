a9b8e07adfdfc3f9d97721f54a94bd21ae83def3
GitHub <noreply@github.com>
2021-08-04 17:27:38 +0000

    82c8393d37 docs: release notes for the v12.1.5 release (#43046)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+29.sha-5327926.with-local-changes
+ * @license Angular v12.2.0-next.2+30.sha-89509df.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
