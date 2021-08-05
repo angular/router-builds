995063681b91fc4a43315e50c1061d22701d15b7
Andrew Kushnir <akushnir@google.com>
2021-08-05 23:25:21 +0000

    a26b0dcc72 ci: stop publishing snapshots to angular/dev-infra-private-builds (#43059)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+27.sha-b41a2b3.with-local-changes
+ * @license Angular v12.2.0-next.2+28.sha-0ce8f6e.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
