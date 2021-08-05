e0aa6eaf2173fefc6c9e5ec6923f2e72d71b8230
Andrew Kushnir <akushnir@google.com>
2021-08-05 22:52:55 +0000

    30c5b9c144 docs: fix interval type in the example (#43042)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+26.sha-f12c533.with-local-changes
+ * @license Angular v12.2.0-next.2+27.sha-b41a2b3.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
