4e66d566e17c3a036cd0e39d7237cb442af28e47
Andrew Kushnir <akushnir@google.com>
2021-08-05 22:53:37 +0000

    644f1b3924 docs: fix interval type in the example (#43042)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+34.sha-30165f9.with-local-changes
+ * @license Angular v12.2.0-next.2+35.sha-7271ad1.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
