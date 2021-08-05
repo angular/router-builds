655409f870a24617ec755d5a38620afbd9666222
Andrew Kushnir <akushnir@google.com>
2021-08-05 23:27:19 +0000

    4db80d69d7 ci: stop publishing snapshots to angular/dev-infra-private-builds (#43059)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+35.sha-7271ad1.with-local-changes
+ * @license Angular v12.2.0-next.2+36.sha-7eddd12.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6777,7 +6777,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.2+35.sha-7271ad1.with-local-changes');
+    var VERSION = new i0.Version('12.2.0-next.2+36.sha-7eddd12.with-local-changes');
 
     /**
      * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+35.sha-7271ad1.with-local-changes
+ * @license Angular v12.2.0-next.2+36.sha-7eddd12.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
