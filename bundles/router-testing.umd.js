5788451db9766f36ae78c75cef7a453b7672b158
Andrew Kushnir <akushnir@google.com>
2021-08-06 16:15:05 +0000

    d8690d867c fix(dev-infra): search since HEAD on the local branch for version tags when creating stamps (#43065)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+36.sha-7eddd12.with-local-changes
+ * @license Angular v13.0.0-next.0+5.sha-d8690d8.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
