47ff3365a875255690e6f5490185557a5b7d53fa
Andrew Kushnir <akushnir@google.com>
2021-08-06 18:08:46 +0000

    784671597e fix(router): Allow question marks in query param values (#31187)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.0+5.sha-d8690d8.with-local-changes
+ * @license Angular v13.0.0-next.0+6.sha-7846715.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
