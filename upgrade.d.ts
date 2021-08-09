653ebc925b815f341a6fea002852731502173ca4
Andrew Kushnir <akushnir@google.com>
2021-08-09 19:46:12 +0000

    8a3d2aad30 fix(docs-infra): correctly detect chunk load errors (#43088)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0+8.sha-dd3abdb.with-local-changes
+ * @license Angular v12.2.0+9.sha-8a3d2aa.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
