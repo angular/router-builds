378e85bb2b1b9283cd10b90f0d9b401a552461e4
Andrew Kushnir <akushnir@google.com>
2021-08-09 17:02:23 +0000

    04879a1007 docs: fix cli link (#43085) PR Close #43085

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.0+6.sha-7846715.with-local-changes
+ * @license Angular v13.0.0-next.0+9.sha-04879a1.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6087,7 +6087,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.0+6.sha-7846715.with-local-changes');
+const VERSION = new Version('13.0.0-next.0+9.sha-04879a1.with-local-changes');
 
 /**
  * @license
