dd374f88b45c81f0f4aac90dc8b2570bcac388d7
Andrew Kushnir <akushnir@google.com>
2021-08-09 17:03:14 +0000

    93590895b5 build(docs-infra): update docs examples to Angular v12.1.3 (#42949)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.0+9.sha-04879a1.with-local-changes
+ * @license Angular v13.0.0-next.0+8.sha-9359089.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
