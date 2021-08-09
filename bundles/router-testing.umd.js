b045cbc53d9e8a1fd7644d9a19371ecb46e34e25
Andrew Kushnir <akushnir@google.com>
2021-08-09 19:46:33 +0000

    6c3ad1f1a4 fix(docs-infra): correctly detect chunk load errors (#43088)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.0+10.sha-bbad423.with-local-changes
+ * @license Angular v13.0.0-next.0+11.sha-6c3ad1f.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.0+10.sha-bbad423.with-local-changes
+ * @license Angular v13.0.0-next.0+11.sha-6c3ad1f.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6733,7 +6733,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('13.0.0-next.0+10.sha-bbad423.with-local-changes');
+    var VERSION = new core.Version('13.0.0-next.0+11.sha-6c3ad1f.with-local-changes');
 
     /**
      * @license
