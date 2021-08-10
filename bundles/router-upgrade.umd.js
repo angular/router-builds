5c1d47e0c5c5b01e10f031b49e5b1bdcd8e91de3
Andrew Kushnir <akushnir@google.com>
2021-08-10 23:15:54 +0000

    e25d35f4cb refactor(compiler-cli): add test for `BananaInBoxCheck` (#42984)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.0+18.sha-e3415e5.with-local-changes
+ * @license Angular v13.0.0-next.0+26.sha-e25d35f.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
