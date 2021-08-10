527b48984ca54ac81dd952d71c85ee89b06cb5bb
Andrew Kushnir <akushnir@google.com>
2021-08-10 19:16:03 +0000

    596d533f97 docs: add angular community meetup and ngxp to community resources (#43103)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.0+16.sha-002d959.with-local-changes
+ * @license Angular v13.0.0-next.0+17.sha-596d533.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
