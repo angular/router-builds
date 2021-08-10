b972fcb1098befab726b852faa84ff8d81b657d0
Andrew Kushnir <akushnir@google.com>
2021-08-10 23:14:56 +0000

    e25d35f4cb refactor(compiler-cli): add test for `BananaInBoxCheck` (#42984)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.0+18.sha-e3415e5.with-local-changes
+ * @license Angular v13.0.0-next.0+26.sha-e25d35f.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6733,7 +6733,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('13.0.0-next.0+18.sha-e3415e5.with-local-changes');
+    var VERSION = new core.Version('13.0.0-next.0+26.sha-e25d35f.with-local-changes');
 
     /**
      * @license
