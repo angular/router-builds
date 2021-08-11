1718ae5fa2c3d7e67b272c05437d19a9259d01aa
GitHub <noreply@github.com>
2021-08-11 17:33:42 +0000

    9136dbd65c release: cut the v12.2.1 release (#43117)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0+24.sha-4a1d9a7.with-local-changes
+ * @license Angular v12.2.0+25.sha-9136dbd.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
