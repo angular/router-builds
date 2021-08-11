70c9a35dfdbf6ecb932e72a66655da88e99f204b
GitHub <noreply@github.com>
2021-08-11 17:46:31 +0000

    1f1822216e release: cut the v13.0.0-next.1 release (#43119)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.0+27.sha-817c386.with-local-changes
+ * @license Angular v13.0.0-next.0+28.sha-1f18222.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6060,7 +6060,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.0+27.sha-817c386.with-local-changes');
+const VERSION = new Version('13.0.0-next.0+28.sha-1f18222.with-local-changes');
 
 /**
  * @license
--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.0+27.sha-817c386.with-local-changes
+ * @license Angular v13.0.0-next.0+28.sha-1f18222.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
