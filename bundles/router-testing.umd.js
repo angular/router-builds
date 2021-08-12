fa9bd41b3b11577736bdd9187cea8f40c31d6fb5
Dylan Hunn <dylhunn@gmail.com>
2021-08-12 22:59:10 +0000

    7a67597d01 docs: fix description: "property decorator" (not parameter) (#43068) (#43128)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0+25.sha-9136dbd.with-local-changes
+ * @license Angular v12.2.1+3.sha-7a67597.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
