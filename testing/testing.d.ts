39cc7f8b8b54f5155141067f9710e718eca92340
Dylan Hunn <dylhunn@gmail.com>
2021-08-12 22:51:50 +0000

    a2a9b917b5 docs: adjust description to code example (#43127)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.1+2.sha-d5f9890.with-local-changes
+ * @license Angular v13.0.0-next.1+3.sha-a2a9b91.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
