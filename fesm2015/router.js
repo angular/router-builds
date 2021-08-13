74b410c86d1170e127943d3c9c933745848262d7
Dylan Hunn <dylhunn@gmail.com>
2021-08-13 22:20:38 +0000

    b0b7ef2985 build: update dependency build_bazel_rules_nodejs to v3.8.0 (#43136)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.1+7.sha-2a1aa58.with-local-changes
+ * @license Angular v13.0.0-next.1+8.sha-b0b7ef2.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6093,7 +6093,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.1+7.sha-2a1aa58.with-local-changes');
+const VERSION = new Version('13.0.0-next.1+8.sha-b0b7ef2.with-local-changes');
 
 /**
  * @license
--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.1+7.sha-2a1aa58.with-local-changes
+ * @license Angular v13.0.0-next.1+8.sha-b0b7ef2.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
