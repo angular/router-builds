87ff9aa2b0bacdf0807443c4cf0449411a6ffa59
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 18:05:36 +0000

    83fa3ee581 docs: add tmdavis to contributors (#43135)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.1+9.sha-e0a8ca7.with-local-changes
+ * @license Angular v12.2.1+12.sha-83fa3ee.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
