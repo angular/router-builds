37e0fd58ab3564079b07c3d4a4fafb70adf64c4e
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 20:31:13 +0000

    cd10a4cc13 test(compiler): add a test for parsing multiline expressions in attributes (#43129)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.1+12.sha-83fa3ee.with-local-changes
+ * @license Angular v12.2.1+19.sha-cd10a4c.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
