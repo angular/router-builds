fa96c96680bb7b67160a14780d3c1a0975e43020
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 17:13:02 +0000

    30bf472597 docs: update Aristeidis bio in GDE list (#43164)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.1+12.sha-286b280.with-local-changes
+ * @license Angular v13.0.0-next.1+13.sha-30bf472.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6797,7 +6797,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('13.0.0-next.1+12.sha-286b280.with-local-changes');
+    var VERSION = new i0.Version('13.0.0-next.1+13.sha-30bf472.with-local-changes');
 
     /**
      * @license
