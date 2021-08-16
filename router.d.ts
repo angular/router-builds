6302fc0d1ad3dea8634e6f573f5b54deab66c0f7
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 20:29:13 +0000

    564ad0622a build: uipdate to latest @angular/dev-infra-private-builds (#43179)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.1+20.sha-c91cecd.with-local-changes
+ * @license Angular v13.0.0-next.1+22.sha-564ad06.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
