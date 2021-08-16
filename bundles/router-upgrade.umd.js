f618d3f2a52c6dba3b22395a082065b6bcdae8b8
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 17:01:01 +0000

    0c84f6fd52 refactor(compiler): fix lateron typo in i18n ExtractorHost (#43160)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.1+8.sha-b0b7ef2.with-local-changes
+ * @license Angular v13.0.0-next.1+9.sha-0c84f6f.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
