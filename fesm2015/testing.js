ed70758b2a69c8fa7d94615b590733adc4194af5
Dylan Hunn <dylhunn@gmail.com>
2021-08-17 16:44:12 +0000

    7e71370fe6 perf(platform-browser): avoid intermediate arrays in server transition (#43145)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.1+31.sha-cf33849.with-local-changes
+ * @license Angular v13.0.0-next.1+34.sha-7e71370.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
