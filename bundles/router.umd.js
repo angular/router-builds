d845ac0831753a51bb91fad3dd89f5dd0384ac7d
Dylan Hunn <dylhunn@gmail.com>
2021-08-17 00:43:22 +0000

    bb6555979d fix(animations): add pure annotations to static property initializers (#43064)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.1+19.sha-cd10a4c.with-local-changes
+ * @license Angular v12.2.1+26.sha-bb65559.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6747,7 +6747,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.2.1+19.sha-cd10a4c.with-local-changes');
+    var VERSION = new core.Version('12.2.1+26.sha-bb65559.with-local-changes');
 
     /**
      * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.1+19.sha-cd10a4c.with-local-changes
+ * @license Angular v12.2.1+26.sha-bb65559.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
