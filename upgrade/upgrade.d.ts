73232691f4bf14e72ff65793a4b690831740b782
Dylan Hunn <dylhunn@gmail.com>
2021-08-18 18:11:59 +0000

    513b17043e docs: fix typo (#43190)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.1+32.sha-c501e86.with-local-changes
+ * @license Angular v12.2.2+1.sha-513b170.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
