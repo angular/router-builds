13fea88c6ef8704da2d82ac1b5379807bf2da78b
GitHub <noreply@github.com>
2021-08-18 21:57:47 +0000

    e3b6e26565 release: cut the v13.0.0-next.2 release (#43200)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.1+43.sha-3e756f0.with-local-changes
+ * @license Angular v13.0.0-next.1+44.sha-e3b6e26.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
