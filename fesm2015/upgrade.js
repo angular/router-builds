9a11aac11314b928fa0fd855e9c7437d411e6801
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-19 18:43:21 +0000

    19fa033f79 refactor(docs-infra): use tslint in aio's tools-lint script (#43010)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.2+1.sha-513b170.with-local-changes
+ * @license Angular v12.2.2+2.sha-19fa033.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
