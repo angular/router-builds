36915e3759e8d058ab3757fe48c95ad377cde69c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-19 19:17:59 +0000

    c389052bab test(language-service): add extended diagnostics test (#43107)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.2+3.sha-bed121c.with-local-changes
+ * @license Angular v13.0.0-next.2+8.sha-c389052.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6107,7 +6107,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.2+3.sha-bed121c.with-local-changes');
+const VERSION = new Version('13.0.0-next.2+8.sha-c389052.with-local-changes');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.2+3.sha-bed121c.with-local-changes
+ * @license Angular v13.0.0-next.2+8.sha-c389052.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
