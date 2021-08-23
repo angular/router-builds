069b5b23f772c15f7a1afae8eaae983099d4c139
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-23 20:50:51 +0000

    6e924313c3 fix(service-worker): NPE if onActionClick is undefined (#43210)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.2+9.sha-ccb09b4.with-local-changes
+ * @license Angular v13.0.0-next.2+11.sha-6e92431.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
