9904a2134e05cdbcdb99aadd7b2176f8b606c055
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-25 18:02:46 +0000

    00aad59047 test(language-service): add test for extended diagnostics (#43134)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.2+12.sha-27fdd52.with-local-changes
+ * @license Angular v13.0.0-next.2+18.sha-00aad59.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6832,7 +6832,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('13.0.0-next.2+12.sha-27fdd52.with-local-changes');
+    var VERSION = new i0.Version('13.0.0-next.2+18.sha-00aad59.with-local-changes');
 
     /**
      * @license
