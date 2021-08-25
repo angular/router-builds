c4015852b2cdba0517a3921a132da310068f3685
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-25 22:32:44 +0000

    e7e78e6709 refactor(zone.js): Rename class ZoneDelegate to _ZoneDelegate to differentiate it from the ZoneDelgate interface (#43019)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.2+17.sha-b818361.with-local-changes
+ * @license Angular v12.2.3+2.sha-e7e78e6.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6074,7 +6074,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.2+17.sha-b818361.with-local-changes');
+const VERSION = new Version('12.2.3+2.sha-e7e78e6.with-local-changes');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.2+17.sha-b818361.with-local-changes
+ * @license Angular v12.2.3+2.sha-e7e78e6.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
