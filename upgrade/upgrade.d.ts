4f92eeda24b822858c8e3d17d89ca654b71066e4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-25 18:01:45 +0000

    00aad59047 test(language-service): add test for extended diagnostics (#43134)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.2+12.sha-27fdd52.with-local-changes
+ * @license Angular v13.0.0-next.2+18.sha-00aad59.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
