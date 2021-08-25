f767733db51e6fd73ab2a613dfb4f55cd3b8e91f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-25 18:04:00 +0000

    073b20b540 build: update all non-major dependencies (#43201)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.2+18.sha-00aad59.with-local-changes
+ * @license Angular v13.0.0-next.2+19.sha-073b20b.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
