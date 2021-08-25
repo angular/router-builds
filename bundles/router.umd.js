7819460fe3e4ed9184e371c056eb1c58e0669ec3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-25 21:40:54 +0000

    b8183614f5 release: cut the v12.2.3 release (#43257)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.2+16.sha-b46ce7d.with-local-changes
+ * @license Angular v12.2.2+17.sha-b818361.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6747,7 +6747,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.2.2+16.sha-b46ce7d.with-local-changes');
+    var VERSION = new core.Version('12.2.2+17.sha-b818361.with-local-changes');
 
     /**
      * @license
