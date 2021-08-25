c877aa291f0a4cff2ea44b1cfe9c2904e2f2c917
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-25 22:14:20 +0000

    49766d2d0a release: cut the v13.0.0-next.3 release (#43259)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.2+21.sha-86feec6.with-local-changes
+ * @license Angular v13.0.0-next.2+22.sha-49766d2.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
