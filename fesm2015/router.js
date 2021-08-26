47d9e4459e65025bd79ed521e5fb4b419f66ecc5
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 17:28:40 +0000

    78690ee354 docs(router): Fix incorrect name for url match options (#43268)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.3+7.sha-c396804.with-local-changes
+ * @license Angular v13.0.0-next.3+8.sha-78690ee.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4022,7 +4022,7 @@ function defaultRouterHook(snapshot, runExtras) {
     return of(null);
 }
 /**
- * The equivalent `IsActiveUrlTreeOptions` options for `Router.isActive` is called with `true`
+ * The equivalent `IsActiveMatchOptions` options for `Router.isActive` is called with `true`
  * (exact = true).
  */
 const exactMatchOptions = {
@@ -4032,7 +4032,7 @@ const exactMatchOptions = {
     queryParams: 'exact'
 };
 /**
- * The equivalent `IsActiveUrlTreeOptions` options for `Router.isActive` is called with `false`
+ * The equivalent `IsActiveMatchOptions` options for `Router.isActive` is called with `false`
  * (exact = false).
  */
 const subsetMatchOptions = {
@@ -6142,7 +6142,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.3+7.sha-c396804.with-local-changes');
+const VERSION = new Version('13.0.0-next.3+8.sha-78690ee.with-local-changes');
 
 /**
  * @license
--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.3+7.sha-c396804.with-local-changes
+ * @license Angular v13.0.0-next.3+8.sha-78690ee.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2322,9 +2322,9 @@ export declare class Router {
      * Returns whether the url is activated.
      *
      * @deprecated
-     * Use `IsActiveUrlTreeOptions` instead.
+     * Use `IsActiveMatchOptions` instead.
      *
-     * - The equivalent `IsActiveUrlTreeOptions` for `true` is
+     * - The equivalent `IsActiveMatchOptions` for `true` is
      * `{paths: 'exact', queryParams: 'exact', fragment: 'ignored', matrixParams: 'ignored'}`.
      * - The equivalent for `false` is
      * `{paths: 'subset', queryParams: 'subset', fragment: 'ignored', matrixParams: 'ignored'}`.
