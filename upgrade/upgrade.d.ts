8b8fe31c59aa4afb82956968a40cb32f74a479a5
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-30 20:58:17 +0000

    d8f637ab43 docs: add step to declare `ProductAlertsComponent` in tutorial (#43229)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.3+15.sha-46bd8f2.with-local-changes
+ * @license Angular v12.2.3+16.sha-d8f637a.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
