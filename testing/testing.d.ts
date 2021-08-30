0a995705ad911ae254f58df3b7439392c5aa4e63
Joey Perrott <josephperrott@gmail.com>
2021-08-30 15:46:11 +0000

    46bd8f2495 ci: update pullapprove config to reflect kyliau's departure (#43230)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.3+13.sha-692e8ab.with-local-changes
+ * @license Angular v12.2.3+15.sha-46bd8f2.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
