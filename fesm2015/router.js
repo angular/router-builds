0da6bed3648cf17b061af83e717515db3f85ba5e
Andrew Scott <atscott01@gmail.com>
2021-09-02 17:13:34 +0000

    fc3b50e427 fix(language-service): exclude the `SafePropertyRead` when applying the optional chaining (#43321)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.4+2.sha-3c863c5.with-local-changes
+ * @license Angular v13.0.0-next.4+11.sha-fc3b50e.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6163,7 +6163,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.4+2.sha-3c863c5.with-local-changes');
+const VERSION = new Version('13.0.0-next.4+11.sha-fc3b50e.with-local-changes');
 
 /**
  * @license
--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.4+2.sha-3c863c5.with-local-changes
+ * @license Angular v13.0.0-next.4+11.sha-fc3b50e.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
