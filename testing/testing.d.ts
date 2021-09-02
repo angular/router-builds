44ac17976ec268a8dd7678018053d8f75086681d
Andrew Scott <atscott01@gmail.com>
2021-09-02 17:06:20 +0000

    c7a93e632b docs: enhance spacings in dynamic component loader section examples (#43241)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.4+1.sha-95f3aec.with-local-changes
+ * @license Angular v13.0.0-next.4+10.sha-c7a93e6.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
