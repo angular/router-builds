18366287e9c9473407e7f72263f789a0c96ee847
Andrew Scott <atscott01@gmail.com>
2021-09-02 17:13:26 +0000

    c6a93001eb fix(common): synchronise location mock behavior with the navigators (#41730)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.4+11.sha-fc3b50e.with-local-changes
+ * @license Angular v13.0.0-next.4+12.sha-c6a9300.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
