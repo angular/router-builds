444135eae97f6b81d23ab3e549cabfd8da3d6b25
Andrew Scott <atscott01@gmail.com>
2021-09-07 19:03:14 +0000

    652a6d36d4 ci: avoid failures when yarn unlinks bazelisk in windows jobs (#43373)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.4+15.sha-5472c28.with-local-changes
+ * @license Angular v13.0.0-next.4+16.sha-652a6d3.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6803,7 +6803,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('13.0.0-next.4+15.sha-5472c28.with-local-changes');
+    var VERSION = new core.Version('13.0.0-next.4+16.sha-652a6d3.with-local-changes');
 
     /**
      * @license
