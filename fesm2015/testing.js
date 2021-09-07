898f8303b7b2cf91bb73af898a1bdc1df9015179
Andrew Scott <atscott01@gmail.com>
2021-09-07 23:25:12 +0000

    c02e5e15b6 refactor(common): removed TODO no longer considered necessary (#43378)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.4+18.sha-01d2f03.with-local-changes
+ * @license Angular v12.2.4+19.sha-c02e5e1.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
