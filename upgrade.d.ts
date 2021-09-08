3af014fa47c375db503854188127dd9a8992a22b
Andrew Scott <atscott01@gmail.com>
2021-09-08 16:43:51 +0000

    ed24c4a78b docs: fix misspelled word in routerLink migration (#43380)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.4+26.sha-2efc18e.with-local-changes
+ * @license Angular v13.0.0-next.4+27.sha-ed24c4a.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
