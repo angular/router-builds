348e7dd93a1d80ef51a8672b103003624ab5f098
Andrew Kushnir <akushnir@google.com>
2021-09-09 16:39:56 +0000

    e064509613 docs(bazel): fix outdated redirect URL for `/guide/bazel` (#43376)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.5+3.sha-5fea50c.with-local-changes
+ * @license Angular v13.0.0-next.5+4.sha-e064509.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
