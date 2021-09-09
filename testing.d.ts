e55710f8fdab3b9b7859856dd46729ca8f9bfa8c
Andrew Kushnir <akushnir@google.com>
2021-09-09 18:54:10 +0000

    8cc93b723c docs: add Angular Projects book in resources (#43130)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.5+4.sha-2658219.with-local-changes
+ * @license Angular v12.2.5+8.sha-8cc93b7.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
