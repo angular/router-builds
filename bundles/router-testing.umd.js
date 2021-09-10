a3cc2855ece0be5e1e10c83ef4e24de14b9f8d74
Andrew Kushnir <akushnir@google.com>
2021-09-10 18:18:08 +0000

    75a3c778b1 build: update dependency karma to v6 (#43413)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.5+28.sha-578aba1.with-local-changes
+ * @license Angular v13.0.0-next.5+29.sha-75a3c77.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
