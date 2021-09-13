d3b7fc4457e259c565689448bea95b3423c5d23c
Andrew Kushnir <akushnir@google.com>
2021-09-13 21:56:08 +0000

    59353c6305 fix(service-worker): clear service worker cache in safety worker (#43324)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.5+31.sha-de682eb.with-local-changes
+ * @license Angular v13.0.0-next.5+32.sha-59353c6.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6842,7 +6842,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('13.0.0-next.5+31.sha-de682eb.with-local-changes');
+    var VERSION = new i0.Version('13.0.0-next.5+32.sha-59353c6.with-local-changes');
 
     /**
      * @license
