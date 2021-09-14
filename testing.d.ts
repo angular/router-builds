558a14eecff38f9f703cf84a6dcc0c0fa17f44d8
Andrew Kushnir <akushnir@google.com>
2021-09-14 18:30:53 +0000

    3ec9ee65b9 refactor(router): Remove TODO and slightly adjust eager browserUrlTree update (#43426)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.5+45.sha-71e4323.with-local-changes
+ * @license Angular v13.0.0-next.5+47.sha-3ec9ee6.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
