20426e6500ace0e37340d2aee0ce9de0d9b2904e
Andrew Kushnir <akushnir@google.com>
2021-09-14 18:30:13 +0000

    71e432305b build: lock file maintenance (#43285)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.5+44.sha-796da64.with-local-changes
+ * @license Angular v13.0.0-next.5+45.sha-71e4323.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.5+44.sha-796da64.with-local-changes
+ * @license Angular v13.0.0-next.5+45.sha-71e4323.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6155,7 +6155,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.5+44.sha-796da64.with-local-changes');
+const VERSION = new Version('13.0.0-next.5+45.sha-71e4323.with-local-changes');
 
 /**
  * @license
