4fe9110341592c8870d5ddb160ff41693456e47e
Andrew Kushnir <akushnir@google.com>
2021-09-14 18:32:32 +0000

    5163999fb9 test(router): Add tests to document expected behavior (#43449)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.5+47.sha-3ec9ee6.with-local-changes
+ * @license Angular v13.0.0-next.5+46.sha-5163999.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4870,12 +4870,15 @@
                         browserUrlTree !== _this.currentUrlTree.toString();
                     var processCurrentUrl = (_this.onSameUrlNavigation === 'reload' ? true : urlTransition) &&
                         _this.urlHandlingStrategy.shouldProcessUrl(t.rawUrl);
+                    // If the source of the navigation is from a browser event, the URL is
+                    // already updated. We already need to sync the internal state.
+                    if (isBrowserTriggeredNavigation(t.source)) {
+                        // TODO(atscott): this should be `t.extractedUrl`. The `browserUrlTree`
+                        // should only be the part of the URL that is handled by the router. In
+                        // addition, this should only be done if we process the current url.
+                        _this.browserUrlTree = t.rawUrl;
+                    }
                     if (processCurrentUrl) {
-                        // If the source of the navigation is from a browser event, the URL is
-                        // already updated. We already need to sync the internal state.
-                        if (isBrowserTriggeredNavigation(t.source)) {
-                            _this.browserUrlTree = t.extractedUrl;
-                        }
                         return rxjs.of(t).pipe(
                         // Fire NavigationStart event
                         operators.switchMap(function (t) {
@@ -6842,7 +6845,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('13.0.0-next.5+47.sha-3ec9ee6.with-local-changes');
+    var VERSION = new i0.Version('13.0.0-next.5+46.sha-5163999.with-local-changes');
 
     /**
      * @license
