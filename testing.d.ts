76e7c9e4aa2eb2dd207810832608adfeb678f293
Andrew Kushnir <akushnir@google.com>
2021-09-15 18:09:44 +0000

    e5d08c1902 docs: update reviewed tag (#43452)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.6+13.sha-66c6dbd.with-local-changes
+ * @license Angular v13.0.0-next.6+14.sha-e5d08c1.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
