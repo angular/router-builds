d40e48486e328e55da4bbde201f51304a84af8e4
Andrew Kushnir <akushnir@google.com>
2021-09-15 20:43:06 +0000

    def6315a76 docs: update reviewed tag (#43452)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.5+40.sha-2050b08.with-local-changes
+ * @license Angular v12.2.6+15.sha-def6315.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
