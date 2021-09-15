c7fd612059a58ad54cf9e39b1628d28b9ea0ad0f
Andrew Kushnir <akushnir@google.com>
2021-09-15 18:08:35 +0000

    1986407ab5 docs: move i18n angular.json snippets to external file (#43216)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.5+56.sha-5e9eb7a.with-local-changes
+ * @license Angular v13.0.0-next.6+11.sha-1986407.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
