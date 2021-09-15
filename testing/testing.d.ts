4720244e3fc7c5de3856145cb7d65f5592bfe480
Andrew Kushnir <akushnir@google.com>
2021-09-15 03:56:41 +0000

    2050b0898b docs: update grammar (#43451)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.5+33.sha-24cf4ea.with-local-changes
+ * @license Angular v12.2.5+40.sha-2050b08.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
