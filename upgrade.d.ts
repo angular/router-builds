43517d3d1c91a94606fb2313f611c65a8d6632c1
GitHub <noreply@github.com>
2021-09-15 17:56:52 +0000

    bab963fd7f docs: release notes for the v12.2.6 release (#43463)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.5+54.sha-02ccceb.with-local-changes
+ * @license Angular v13.0.0-next.5+55.sha-bab963f.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
