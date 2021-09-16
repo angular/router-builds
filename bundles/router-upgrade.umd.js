c98b7802350da8dbf4b40a6c2f835e078f310627
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-16 18:38:28 +0000

    05e1adba48 test(compiler): add a test for parsing multiline expressions in attributes (#43132)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.6+21.sha-b43f21b.with-local-changes
+ * @license Angular v12.2.6+28.sha-05e1adb.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
