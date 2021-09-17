7c1cfb07460698e9485405fec6fe95202fde87c0
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-17 16:29:14 +0000

    ed235bdce9 docs: Edits to remove jargon (#42999)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.6+28.sha-05e1adb.with-local-changes
+ * @license Angular v12.2.6+30.sha-ed235bd.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6093,7 +6093,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.6+28.sha-05e1adb.with-local-changes');
+const VERSION = new Version('12.2.6+30.sha-ed235bd.with-local-changes');
 
 /**
  * @license
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.6+28.sha-05e1adb.with-local-changes
+ * @license Angular v12.2.6+30.sha-ed235bd.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
