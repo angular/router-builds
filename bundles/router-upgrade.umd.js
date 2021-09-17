ece709f4949883516db40186737db6f4bd755cdc
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-17 17:01:16 +0000

    312e421ff3 docs: Edits to remove jargon (#43045)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.6+30.sha-313ea92.with-local-changes
+ * @license Angular v13.0.0-next.6+31.sha-312e421.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
