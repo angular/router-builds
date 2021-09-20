a53fbe72fe36fc35fdb92c98a62f44e357aab10d
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-20 17:29:20 +0000

    e41a583c40 docs: reviewed tag added (#43493)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.6+39.sha-80f5a4e.with-local-changes
+ * @license Angular v13.0.0-next.6+42.sha-e41a583.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6775,7 +6775,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('13.0.0-next.6+39.sha-80f5a4e.with-local-changes');
+    var VERSION = new core.Version('13.0.0-next.6+42.sha-e41a583.with-local-changes');
 
     /**
      * @license
