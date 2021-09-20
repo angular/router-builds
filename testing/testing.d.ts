f6fd41f14f86ed85dfc8e02ed0528d75c90e2548
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-20 16:22:52 +0000

    020f0d3858 fix(docs-infra): improve aio contributors page responsiveness (#43355)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.6+37.sha-909c706.with-local-changes
+ * @license Angular v12.2.6+38.sha-020f0d3.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
