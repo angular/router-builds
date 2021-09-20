b6a64f8ad4353c52fbc25f74a86fcdf02520892a
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-20 20:38:27 +0000

    9470f56ad3 fix(compiler-cli): handle directives that refer to a namespaced class in a type parameter bound (#43511)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.6+41.sha-5f86650.with-local-changes
+ * @license Angular v13.0.0-next.6+43.sha-9470f56.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
