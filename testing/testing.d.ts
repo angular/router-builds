ffb63283a48deb0aabda40ba0e2064dc486b5507
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-20 23:10:09 +0000

    7d6ba72b90 docs: Edits to remove jargon (#42928)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.6+47.sha-bba683b.with-local-changes
+ * @license Angular v13.0.0-next.6+48.sha-7d6ba72.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
