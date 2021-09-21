dd56492a527a8824ebd8b42f82c20fe052cf64d0
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-21 16:34:13 +0000

    d1baf7cdfb fix(docs-infra): add a "skip to content" link for better accessibility (#43460)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.6+48.sha-7d6ba72.with-local-changes
+ * @license Angular v13.0.0-next.6+53.sha-d1baf7c.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
