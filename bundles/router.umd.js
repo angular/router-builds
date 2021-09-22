d48c33468c33c03d8e63f11efd3378a659773d3f
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-22 18:10:33 +0000

    a1633c1c6e docs(service-worker): improve service worker guides (#43508)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.6+62.sha-a2eae02.with-local-changes
+ * @license Angular v13.0.0-next.6+63.sha-a1633c1.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6805,7 +6805,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('13.0.0-next.6+62.sha-a2eae02.with-local-changes');
+    var VERSION = new i0.Version('13.0.0-next.6+63.sha-a1633c1.with-local-changes');
 
     /**
      * @license
--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.6+62.sha-a2eae02.with-local-changes
+ * @license Angular v13.0.0-next.6+63.sha-a1633c1.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
