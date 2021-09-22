ea92e8a6b4c6eedcb1173494806cb11f1248d271
GitHub <noreply@github.com>
2021-09-22 19:23:38 +0000

    dad213e8c7 release: cut the v12.2.7 release (#43537)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.6+55.sha-9b7cdae.with-local-changes
+ * @license Angular v12.2.6+58.sha-dad213e.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
