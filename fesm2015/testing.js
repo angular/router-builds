ce7971677c6edb567f67b22a4d4ba7e46640b83e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-23 21:55:13 +0000

    b78b13ffa9 docs: Edits to remove jargon (#42904)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.6+65.sha-49d29e1.with-local-changes
+ * @license Angular v13.0.0-next.7+4.sha-b78b13f.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
