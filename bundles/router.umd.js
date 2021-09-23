86f58dc57b783e02e96410aefce99517f758d17e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-23 21:55:31 +0000

    69aa690bb8 docs: Edits to remove jargon (#42950)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.7+4.sha-b78b13f.with-local-changes
+ * @license Angular v13.0.0-next.7+5.sha-69aa690.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6805,7 +6805,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('13.0.0-next.7+4.sha-b78b13f.with-local-changes');
+    var VERSION = new i0.Version('13.0.0-next.7+5.sha-69aa690.with-local-changes');
 
     /**
      * @license
