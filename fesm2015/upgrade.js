ab181d029a1435cc07bcf49ca7cfe769d9656ba3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-24 18:14:45 +0000

    e131540f71 fix(service-worker): do not unassign clients from a broken version (#43518)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.7+36.sha-67ff93d.with-local-changes
+ * @license Angular v13.0.0-next.7+38.sha-e131540.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
