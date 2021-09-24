beb730e99f5aec5519036840d4ffffecf432ff55
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-24 18:09:39 +0000

    c231849f72 test: update localize-extract source paths (#43417)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.7+29.sha-d9d8f95.with-local-changes
+ * @license Angular v13.0.0-next.7+34.sha-c231849.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
