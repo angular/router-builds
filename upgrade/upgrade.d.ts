ce937d660f9f4529e4d26ad1702b7a277ee7b36c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-24 18:19:29 +0000

    d02c8da7bd fix(docs-infra): display deprecation notes for properties (#43566)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.7+40.sha-9d290bd.with-local-changes
+ * @license Angular v13.0.0-next.7+39.sha-d02c8da.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
