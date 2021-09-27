9f30e49aec457c7bc4e1b82740486028272975bc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-27 18:13:52 +0000

    49e0b17e56 docs: links added for each builder schema (#43564)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.8+3.sha-81d09d1.with-local-changes
+ * @license Angular v13.0.0-next.8+5.sha-49e0b17.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
