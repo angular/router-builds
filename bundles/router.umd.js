8f743a22a9df843bafae94691d08c1c31f390f59
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-27 18:17:59 +0000

    f055a886f3 build: remove api-extractor workaround (#43563)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.8+5.sha-49e0b17.with-local-changes
+ * @license Angular v13.0.0-next.8+7.sha-f055a88.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6805,7 +6805,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('13.0.0-next.8+5.sha-49e0b17.with-local-changes');
+    var VERSION = new i0.Version('13.0.0-next.8+7.sha-f055a88.with-local-changes');
 
     /**
      * @license
