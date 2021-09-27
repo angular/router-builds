430ad4cced39cbe030565445c02432f06e37340e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-27 18:04:17 +0000

    353cad283d docs: Edits to remove jargon (#43000)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.7+41.sha-22eebba.with-local-changes
+ * @license Angular v13.0.0-next.8+1.sha-353cad2.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
