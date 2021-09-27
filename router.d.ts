89b808de92c7bf9622826a829dc4ad9242428f79
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-27 21:40:36 +0000

    66fb311d20 fix(core): incorrect signature for initTestEnvironment (#43615)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.8+7.sha-f055a88.with-local-changes
+ * @license Angular v13.0.0-next.8+9.sha-66fb311.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
