78ef7550ff5e6963b40ac7fb44eb24eb28fedcdd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-28 21:39:35 +0000

    8d34f86edb docs: deprecate `PlatformRef.bootstrapModuleFactory` and factory-based signature of `ApplicationRef.bootstrap` (#43560)

--- fesm2015/upgrade.js
+++ fesm2015/upgrade.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.8+14.sha-4f3beff.with-local-changes
+ * @license Angular v13.0.0-next.8+15.sha-8d34f86.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
