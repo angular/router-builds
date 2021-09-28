cfa1c6a6db5037ecf8a78faa8a798e45eac20460
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-28 21:37:20 +0000

    8d2b6affcd fix(compiler-cli): correctly interpret token arrays in @Injectable `deps` (#43226)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.8+11.sha-de662da.with-local-changes
+ * @license Angular v13.0.0-next.8+13.sha-8d2b6af.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
