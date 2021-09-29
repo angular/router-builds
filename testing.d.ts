895657beabb6f03bafe70dc5a6439dcee0148130
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 17:15:57 +0000

    51149ab530 docs: deprecate `aotSummaries` usage in TestBed (#43395)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.8+21.sha-c008e0f.with-local-changes
+ * @license Angular v13.0.0-next.8+22.sha-51149ab.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
