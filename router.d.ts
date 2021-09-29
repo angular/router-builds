00fd7c530575812bf1dae7489ed7b2fc9971377f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 18:58:51 +0000

    ab3de40ba3 Revert "refactor(migrations): support use of an ESM `@angular/compiler` package (#43627)"

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.8+29.sha-09d325a.with-local-changes
+ * @license Angular v13.0.0-next.8+30.sha-ab3de40.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
