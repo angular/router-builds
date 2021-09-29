e8f8beea4ee62fed987d0a53278a395e653bac68
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 16:10:22 +0000

    c008e0fa90 refactor(migrations): support use of an ESM `@angular/compiler` package (#43627)

--- bundles/router-upgrade.umd.js
+++ bundles/router-upgrade.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.8+18.sha-4555d8a.with-local-changes
+ * @license Angular v13.0.0-next.8+21.sha-c008e0f.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
