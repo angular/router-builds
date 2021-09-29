e91b1deedd4b73bff96ade510240f5261a82a954
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 17:19:30 +0000

    d1589604a8 build(docs-infra): ensure all overloads are shown in interfaces (#43614)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.8+25.sha-e0a7285.with-local-changes
+ * @license Angular v13.0.0-next.8+26.sha-d158960.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6773,7 +6773,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('13.0.0-next.8+25.sha-e0a7285.with-local-changes');
+    var VERSION = new core.Version('13.0.0-next.8+26.sha-d158960.with-local-changes');
 
     /**
      * @license
