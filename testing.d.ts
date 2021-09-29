7c3a6f3c41836e226e29a1bfb627f3d489b8c9a7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 21:11:48 +0000

    7dccbdd27b feat(core): add support for Types in ViewContainerRef.createComponent (#43022)

--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.8+30.sha-ab3de40.with-local-changes
+ * @license Angular v13.0.0-next.8+31.sha-7dccbdd.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
