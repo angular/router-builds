12783e45bebd3d801ae106f7009f01bf0fc8f541
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 18:36:02 +0000

    09d325a9e6 test(router): refactor tests to not use deprecated loadChildren (#43578)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.8+27.sha-6a3492b.with-local-changes
+ * @license Angular v13.0.0-next.8+29.sha-09d325a.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
