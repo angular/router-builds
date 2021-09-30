2d1ed7cab261b8b276155b3caf5b32293f004684
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-30 00:01:53 +0000

    cadef4caa8 ci: correctly handle commit message with carriage returns in `payload-size.sh` (#43569)

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.7+9.sha-2306a32.with-local-changes
+ * @license Angular v12.2.7+32.sha-cadef4c.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
