19de13f8ddc4018273be4b6a19663099a171de5f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-30 20:45:12 +0000

    9d9d05911d release: cut the v13.0.0-next.9 release (#43656)

--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.8+36.sha-e5b0c39.with-local-changes
+ * @license Angular v13.0.0-next.8+37.sha-9d9d059.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6822,7 +6822,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('13.0.0-next.8+36.sha-e5b0c39.with-local-changes');
+    var VERSION = new i0.Version('13.0.0-next.8+37.sha-9d9d059.with-local-changes');
 
     /**
      * @license
