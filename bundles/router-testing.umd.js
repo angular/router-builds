68321cb4b15a6bf036997c75d81f303a3f7c014f
Dylan Hunn <dylhunn@gmail.com>
2021-10-01 16:43:57 +0000

    74ca3c5c7a docs: fix typo in AOT compiler (#43640) PR Close #43640

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.9+4.sha-7aea525.with-local-changes
+ * @license Angular v13.0.0-next.9+6.sha-74ca3c5.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
