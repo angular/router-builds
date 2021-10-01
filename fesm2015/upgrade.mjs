1d6f5e1f85c502db8fddbafebbe92544d62aca0c
Dylan Hunn <dylhunn@gmail.com>
2021-10-01 20:40:04 +0000

    c721135e37 release: cut the v13.0.0-next.10 release (#43672)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.9+84.sha-c15b8c7.with-local-changes
+ * @license Angular v13.0.0-next.9+85.sha-c721135.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
