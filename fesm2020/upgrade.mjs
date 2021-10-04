c6e6023f0f88f1da0f5030d52c2b6a156fd97209
Dylan Hunn <dylhunn@gmail.com>
2021-10-04 23:43:03 +0000

    6d1c2f79da refactor(migrations): ensure CommonJS migrations can be accessed (#43657)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.10+9.sha-a268c44.with-local-changes
+ * @license Angular v13.0.0-next.10+11.sha-6d1c2f7.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
