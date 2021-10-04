1ccc3e19363f2c01f2c6abd07f30c3d968511c66
Dylan Hunn <dylhunn@gmail.com>
2021-10-04 23:22:48 +0000

    a268c4471f refactor(router): restore history in a consistent way on canceled navigations (#43651)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.10+8.sha-e5c3017.with-local-changes
+ * @license Angular v13.0.0-next.10+9.sha-a268c44.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
