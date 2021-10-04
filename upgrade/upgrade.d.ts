6355d1342c16b8429ec47ec78b1f6047b2776227
Dylan Hunn <dylhunn@gmail.com>
2021-10-04 18:14:12 +0000

    dbfb2d20b3 docs: remove web-animations from required polyfills (since IE11 is deprecated) (#43605)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.10+5.sha-2cc5cc9.with-local-changes
+ * @license Angular v13.0.0-next.10+7.sha-dbfb2d2.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
