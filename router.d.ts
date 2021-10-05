f0461dcc2739c754241336ba4fc7c9a65787bf79
Dylan Hunn <dylhunn@gmail.com>
2021-10-05 20:58:03 +0000

    d585fbc3d8 ci: change ownership of `goldens/public-api/manage.js` (#43664)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.11+9.sha-0dc4544.with-local-changes
+ * @license Angular v13.0.0-next.11+11.sha-d585fbc.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
