c73fdb56f608d0d6234332abdfc66e651e73157d
Dylan Hunn <dylhunn@gmail.com>
2021-10-05 00:52:50 +0000

    bdc6ff465a refactor(compiler-cli): remove `source-map` from dependencies (#43644)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.10+22.sha-81c7eb8.with-local-changes
+ * @license Angular v13.0.0-next.11+3.sha-bdc6ff4.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
