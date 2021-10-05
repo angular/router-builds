180b400baabd113d3971c402de66c84bf95f4cb5
Dylan Hunn <dylhunn@gmail.com>
2021-10-05 17:07:37 +0000

    318cf9144e test(core): fix failing unit test (#43714)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.11+3.sha-bdc6ff4.with-local-changes
+ * @license Angular v13.0.0-next.11+6.sha-318cf91.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
