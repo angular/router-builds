a14405202b4ae13ff12903b5a517c5abea0535a9
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 16:44:19 +0000

    52587e5b43 test: remove `progress` option from `dev-server` builder (#43730)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.12+4.sha-6ac35d0.with-local-changes
+ * @license Angular v13.0.0-next.12+5.sha-52587e5.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
