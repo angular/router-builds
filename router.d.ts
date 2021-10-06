cbf5d126f3f790c9474eae8021b50053f1e474f2
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 18:14:54 +0000

    e0a0d05d45 feat(core): update node version support range to support v16 (#43740)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.12+14.sha-841c5ae.with-local-changes
+ * @license Angular v13.0.0-next.12+16.sha-e0a0d05.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
