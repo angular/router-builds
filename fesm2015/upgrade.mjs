0a8e1de104c605a7cbf70fa61a35fa5e5a26c8aa
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 16:42:36 +0000

    6ac35d0660 fix(docs-infra): convert button-like elements to actual buttons (#43601)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.12+2.sha-fa6aa1b.with-local-changes
+ * @license Angular v13.0.0-next.12+4.sha-6ac35d0.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
