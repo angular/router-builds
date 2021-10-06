3673c4d2dec229c369d30e9d95745a229cf6e39f
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 02:11:23 +0000

    e3d6e97050 docs: deprecate ViewEngine-based JIT Compiler symbols (#43710)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.11+16.sha-7acf3be.with-local-changes
+ * @license Angular v13.0.0-next.12+1.sha-e3d6e97.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
