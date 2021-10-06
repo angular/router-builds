97684b90496e4018a3c94eab1dd1a1498f11dc8a
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 00:48:48 +0000

    c14085e434 feat(core): drop support for TypeScript 4.2 and 4.3 (#43642)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.11+10.sha-fddb50b.with-local-changes
+ * @license Angular v13.0.0-next.11+14.sha-c14085e.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
