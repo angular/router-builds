122ac6f7541fd108fea421b33ecf65663bdb44d7
Andrew Scott <atscott01@gmail.com>
2021-10-07 18:45:44 +0000

    e0667efa6e feat(bazel): allow for custom conditions to be set in `ng_package` targets (#43764)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+1.sha-e1c2fbd.with-local-changes
+ * @license Angular v13.0.0-next.14+2.sha-e0667ef.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
