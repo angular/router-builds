83004fffb6fdbceac7a634fac8f26292ce03f5a8
Joey Perrott <josephperrott@gmail.com>
2021-10-08 00:08:55 +0000

    4eedf77fd0 docs: deprecate ViewEngine-based `renderModuleFactory` (#43757)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+5.sha-5b1b2fd.with-local-changes
+ * @license Angular v13.0.0-next.14+7.sha-4eedf77.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
