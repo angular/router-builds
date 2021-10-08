e75772108addfe0e1fe00976a0236af80ed608cf
Andrew Scott <atscott01@gmail.com>
2021-10-08 20:59:47 +0000

    1869474386 refactor(docs-infra): remove linting from i18n example angular.json (#43747)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.15+8.sha-a64adab.with-local-changes
+ * @license Angular v13.0.0-next.15+12.sha-1869474.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
