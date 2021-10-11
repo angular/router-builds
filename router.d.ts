cc2e5489b61d30e437be537efada191a02e8c871
Andrew Scott <atscott01@gmail.com>
2021-10-11 17:01:10 +0000

    33d6b5888c docs: Formalize language describing HttpClient (#43778) PR Close #43778

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+19.sha-8b29714.with-local-changes
+ * @license Angular v13.0.0-next.14+20.sha-33d6b58.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
