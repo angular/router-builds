3a0854be0b05f197658e129416d20cb507dc7fe0
Andrew Scott <atscott01@gmail.com>
2021-10-12 18:01:26 +0000

    17f5299967 ci: use Node.js browsers image (#43810)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+23.sha-08caead.with-local-changes
+ * @license Angular v13.0.0-next.14+30.sha-17f5299.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
