f407ea2f37c82563f5db9889e83023e749ee413b
Andrew Scott <atscott01@gmail.com>
2021-10-12 18:08:03 +0000

    eec3839e83 fix(docs-infra): improve cancel icon in top-menu search (#43600)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+36.sha-16e5a31.with-local-changes
+ * @license Angular v13.0.0-next.14+38.sha-eec3839.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
