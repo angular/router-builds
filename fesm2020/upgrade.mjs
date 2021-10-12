c96c75419311c6fae8de6ca24236191af54ac65a
Andrew Scott <atscott01@gmail.com>
2021-10-12 18:13:33 +0000

    657ce1d911 docs: remove mention of router API that 'may be deprecated' (#43765)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+38.sha-eec3839.with-local-changes
+ * @license Angular v13.0.0-next.14+39.sha-657ce1d.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
