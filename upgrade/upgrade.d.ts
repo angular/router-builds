0bbc4aef0111bacc7d622d8ced4c6358a04f700a
Andrew Scott <atscott01@gmail.com>
2021-10-13 17:11:09 +0000

    b5da715d9f build: update to cldr@39 (#43820)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+44.sha-512f643.with-local-changes
+ * @license Angular v13.0.0-next.14+43.sha-b5da715.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
