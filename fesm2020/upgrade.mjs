b695c009feda520ba28ea89e5758a7794b5e121e
Andrew Kushnir <akushnir@google.com>
2021-10-14 17:21:41 +0000

    41cf9e9768 docs: add missing value types to optimization configuration (#43839)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+52.sha-c5dc79d.with-local-changes
+ * @license Angular v13.0.0-next.14+53.sha-41cf9e9.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
