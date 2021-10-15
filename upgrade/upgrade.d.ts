5c44227916cdf4dfbc3016e3303205ac2a66c60e
Andrew Kushnir <akushnir@google.com>
2021-10-15 21:16:14 +0000

    ee2678ca42 build: update bazel integration test to `rules_nodejs` v4.4.1 (#43853)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.0+14.sha-70c14ed.with-local-changes
+ * @license Angular v13.0.0-rc.0+16.sha-ee2678c.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
