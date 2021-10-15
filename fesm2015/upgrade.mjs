755a8bc851e59e8471ad11a9e87be1a6b6534f0c
Andrew Kushnir <akushnir@google.com>
2021-10-15 17:35:18 +0000

    426a3ecae7 fix(compiler-cli): updates `ngc` to pass the build when only warnings are emitted (#43673)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.0+8.sha-91e73c4.with-local-changes
+ * @license Angular v13.0.0-rc.0+11.sha-426a3ec.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
