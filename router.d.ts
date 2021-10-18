771209a02935015d79c7365bbed31fd98ed0cd49
Andrew Kushnir <akushnir@google.com>
2021-10-18 17:59:44 +0000

    83f857c4fb docs: fix a couple of random typos (#43878)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+72.sha-a3ee474.with-local-changes
+ * @license Angular v13.0.0-next.14+74.sha-83f857c.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
