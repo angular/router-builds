04bbc3dcf45ad63ee124b7df4edfa9caaa648de3
Andrew Kushnir <akushnir@google.com>
2021-10-18 22:40:56 +0000

    b489ae5ecd ci: remove browserstack configuration (#43881)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.0+25.sha-fc6519c.with-local-changes
+ * @license Angular v13.0.0-rc.0+27.sha-b489ae5.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
