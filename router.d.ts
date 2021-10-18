06d469ee6a3f3b802d23b29adec638b3a41c31e7
Andrew Kushnir <akushnir@google.com>
2021-10-18 22:36:46 +0000

    9f40d2a0ea ci: remove browserstack configuration (#43881)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+76.sha-60f3b33.with-local-changes
+ * @license Angular v13.0.0-next.14+78.sha-9f40d2a.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
