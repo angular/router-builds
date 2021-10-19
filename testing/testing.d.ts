4be1bede4a46d1be7a3877caca3851bacef45895
Andrew Kushnir <akushnir@google.com>
2021-10-19 23:44:33 +0000

    007b96eb2e test(compiler-cli): run i18n extraction tests using Ivy compiler (#43893)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+91.sha-222c5a8.with-local-changes
+ * @license Angular v13.0.0-next.14+93.sha-007b96e.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
