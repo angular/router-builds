eade1460f087a132e1e7fbb8d0d413dd04b14d34
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-20 20:42:39 +0000

    665466def3 docs: move deprecations example snippets to external file (#43700)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.0+43.sha-b31a9a7.with-local-changes
+ * @license Angular v13.0.0-rc.1+2.sha-665466d.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
