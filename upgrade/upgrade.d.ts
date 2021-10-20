6333737e4e909c9540587f182663ddb260835e33
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-20 20:31:50 +0000

    b8f1f82fe2 docs: clarify meaning of ellipsis in bazel command (#43890)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+96.sha-4c9f35b.with-local-changes
+ * @license Angular v13.0.0-next.14+97.sha-b8f1f82.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
