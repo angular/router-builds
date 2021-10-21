5a8e7dd1d972f82fae6cc243c44f9d74d4036eb1
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 19:12:15 +0000

    97fc9de2c4 refactor(core): made comment structure consistent in index.ts (#43684)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.1+8.sha-72f6877.with-local-changes
+ * @license Angular v13.0.0-rc.1+11.sha-97fc9de.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
