9ec689a47c4c54ad7317c4ddbb1d2ad9a3940152
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 20:29:01 +0000

    7817892684 refactor(core): made comment structure consistent in index.ts (#43684)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.11+3.sha-cef01b8.with-local-changes
+ * @license Angular v12.2.11+6.sha-7817892.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6096,7 +6096,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.11+3.sha-cef01b8.with-local-changes');
+const VERSION = new Version('12.2.11+6.sha-7817892.with-local-changes');
 
 /**
  * @license
--- testing.d.ts
+++ testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.11+3.sha-cef01b8.with-local-changes
+ * @license Angular v12.2.11+6.sha-7817892.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
