d05b5f1ca432f1a836fea077a9ac316c8d1eacf8
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 16:46:25 +0000

    cef01b804a ci: update `angular/dev-infra/github-actions` sha (#43904)

--- bundles/router-testing.umd.js
+++ bundles/router-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.11+1.sha-e09556f.with-local-changes
+ * @license Angular v12.2.11+3.sha-cef01b8.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
--- bundles/router.umd.js
+++ bundles/router.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.11+1.sha-e09556f.with-local-changes
+ * @license Angular v12.2.11+3.sha-cef01b8.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6769,7 +6769,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('12.2.11+1.sha-e09556f.with-local-changes');
+    var VERSION = new core.Version('12.2.11+3.sha-cef01b8.with-local-changes');
 
     /**
      * @license
