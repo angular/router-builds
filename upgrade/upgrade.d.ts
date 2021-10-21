752e8af6be2b4fefee7d58a76f7788c2b9b4be39
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 18:32:24 +0000

    72f6877cb0 docs: remove Ivy-specific guides (#43860)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.1+7.sha-adc68b1.with-local-changes
+ * @license Angular v13.0.0-rc.1+8.sha-72f6877.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
