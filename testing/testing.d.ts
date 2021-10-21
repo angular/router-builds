a19e85ed8e67bc3d538828f545578dcb4a57b63f
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 14:39:54 +0000

    c5d0b76e29 refactor(service-worker): avoid unnecessary writes to cache when version fails (#43680)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+98.sha-c78f1cd.with-local-changes
+ * @license Angular v13.0.0-next.14+99.sha-c5d0b76.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
