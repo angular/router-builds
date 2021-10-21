873f58dbb7f233e0d6052242cc5818db3da2dd27
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 15:01:51 +0000

    974767d428 docs: fix typos in the workspace config guide (#43918)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+99.sha-c5d0b76.with-local-changes
+ * @license Angular v13.0.0-next.14+100.sha-974767d.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
