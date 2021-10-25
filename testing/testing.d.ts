0f45e434385a6955f59a43642afb609c37bea9d5
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 22:24:46 +0000

    940eafa351 docs(localize): fix spelling of documentation link (#43941)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.1+18.sha-087fae5.with-local-changes
+ * @license Angular v13.0.0-rc.1+20.sha-940eafa.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
