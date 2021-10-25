8709a472d8af56965705212ad988202ded9301ce
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 18:14:44 +0000

    786f02b0c9 build: update dependency eslint-plugin-jsdoc to v37 (#43930)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+111.sha-be564e6.with-local-changes
+ * @license Angular v13.0.0-next.14+112.sha-786f02b.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
