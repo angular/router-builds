f2a9abdc2959602c59895fc934846b1432773b45
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 18:19:59 +0000

    c831c67c00 test(platform-browser): complete component bootstrap before switching to the next test (#43933)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.11+6.sha-7817892.with-local-changes
+ * @license Angular v12.2.11+8.sha-c831c67.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
