19ac03fd40b88965cd9fde36cd31b2a32910ff51
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 18:15:31 +0000

    6e37c938e2 fix(ngcc): support alternate UMD layout when adding new imports (#43931)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+112.sha-786f02b.with-local-changes
+ * @license Angular v13.0.0-next.14+113.sha-6e37c93.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
