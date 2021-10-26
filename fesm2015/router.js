5655b6a4dccca37411b5e3bc71c7437376e7a599
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 22:34:33 +0000

    8fcfd87d99 ci: update commit-message-based-labels job to be called labels (#43944)

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.11+12.sha-bf59401.with-local-changes
+ * @license Angular v12.2.11+14.sha-8fcfd87.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6096,7 +6096,7 @@ function provideRouterInitializer() {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.11+12.sha-bf59401.with-local-changes');
+const VERSION = new Version('12.2.11+14.sha-8fcfd87.with-local-changes');
 
 /**
  * @license
