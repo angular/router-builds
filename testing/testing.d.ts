3a725bca1c4820a5156738f6d234450170fc70e5
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 20:45:09 +0000

    c44d1cbd58 refactor(forms): update minlength/maxlength to inherit abstractValidator (#43835)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.1+23.sha-fa7693f.with-local-changes
+ * @license Angular v13.0.0-rc.1+27.sha-c44d1cb.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
