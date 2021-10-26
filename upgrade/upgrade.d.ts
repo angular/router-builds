8470eecb69937537cd942f239f13d5d2db07d054
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 23:59:49 +0000

    284f58546a docs: reviewed tag added (#43488)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+125.sha-6c47139.with-local-changes
+ * @license Angular v13.0.0-next.14+126.sha-284f585.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
