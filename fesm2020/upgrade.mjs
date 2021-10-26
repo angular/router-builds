94a5471dc3903fdba12586c13d48c5dc8b0c395e
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 22:24:02 +0000

    6c471396af build: update all non-major dependencies (#43917)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+124.sha-5c22dfe.with-local-changes
+ * @license Angular v13.0.0-next.14+125.sha-6c47139.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
