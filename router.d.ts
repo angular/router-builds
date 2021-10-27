f909c37dd630ab1acf1db6a67b7dbfa820b70b90
GitHub <noreply@github.com>
2021-10-27 18:53:36 +0000

    a84983efea docs: release notes for the v13.0.0-rc.2 release (#43969)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+134.sha-96944eb.with-local-changes
+ * @license Angular v13.0.0-next.14+135.sha-a84983e.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
