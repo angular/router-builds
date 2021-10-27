708e4352bfd17a748d65360e9e90ab164bdf668d
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-27 00:15:51 +0000

    82b668aada docs: reviewed tag added (#43487)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+130.sha-e2f9dcf.with-local-changes
+ * @license Angular v13.0.0-next.14+127.sha-82b668a.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
