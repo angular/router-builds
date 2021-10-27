f002e19a7e36f0fa3d0c3207b13d3212f3303c31
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-27 16:30:51 +0000

    7de7dc89bf fix(docs-infra): fix heritage links (#43956)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.1+31.sha-cfc509c.with-local-changes
+ * @license Angular v13.0.0-rc.1+33.sha-7de7dc8.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
