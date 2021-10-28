04f646619a1f1ff585ba26c1dbc4ad05b94f33d3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-28 18:39:22 +0000

    65cb2c5565 fix(core): support `InjectFlags` argument in `NodeInjector.get()` (#41592)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+135.sha-a84983e.with-local-changes
+ * @license Angular v13.0.0-next.14+139.sha-65cb2c5.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
