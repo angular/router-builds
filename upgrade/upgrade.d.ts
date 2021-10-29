c44fdc5e4e7f6021f296630f4f70f0d3e4e5a960
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-29 23:03:23 +0000

    a01e02d4e2 refactor(docs-infra): remove obsolete way of disabling `DocViewer` animations (#43977)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+164.sha-967283e.with-local-changes
+ * @license Angular v13.0.0-next.14+171.sha-a01e02d.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
