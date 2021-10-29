1a32e905dcd6ec0cb96286a814e591507e155e8d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-29 20:30:46 +0000

    7b35a3426f refactor(docs-infra): align docs examples with default CLI TS config (#43880)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+139.sha-65cb2c5.with-local-changes
+ * @license Angular v13.0.0-next.14+144.sha-7b35a34.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
