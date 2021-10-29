875c4384b326f6962636df36e492538da5532874
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-29 23:06:10 +0000

    408e4dafbd fix(docs-infra): search box input line-height (#43983)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+171.sha-a01e02d.with-local-changes
+ * @license Angular v13.0.0-next.14+175.sha-408e4da.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
