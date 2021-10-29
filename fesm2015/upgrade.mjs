369e9a906fa53e05396fb8db2726af5b86bbb4d5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-29 22:56:49 +0000

    967283e8a1 docs: update dynamic component guide to use factory-less ViewContainerRef.createComponent (#43895)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+163.sha-8d8cdc1.with-local-changes
+ * @license Angular v13.0.0-next.14+164.sha-967283e.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
