605f808713afe5dac935162666b603a576c40a63
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-01 16:46:03 +0000

    d015ac214f Revert "refactor(router): restore history in a consistent way on canceled navigations (#43651)" (#44006)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+185.sha-3d1ac6a.with-local-changes
+ * @license Angular v13.0.0-next.14+186.sha-d015ac2.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
