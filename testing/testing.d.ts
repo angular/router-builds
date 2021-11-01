373ce81d231e4c366d0247d30ad82a5598f7ae26
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-01 21:21:52 +0000

    32fa5335c6 docs: update the roadmap in alignment with v13 progress (#43986)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+186.sha-d015ac2.with-local-changes
+ * @license Angular v13.0.0-next.14+187.sha-32fa533.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
