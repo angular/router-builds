5c99e09982aee2f24c83fc3d09b0b55dd0e4e164
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-02 01:43:40 +0000

    509b14536a docs: initial commit of Angular Package Format documentation (#43987)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+187.sha-32fa533.with-local-changes
+ * @license Angular v13.0.0-next.14+189.sha-509b145.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
