6fc441fa7c144f961841ecbb27429c8ef8bbe892
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-02 01:42:46 +0000

    4fd388b084 docs: initial commit of Angular Package Format documentation (#43987)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.2+53.sha-74c4972.with-local-changes
+ * @license Angular v13.0.0-rc.2+54.sha-4fd388b.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
