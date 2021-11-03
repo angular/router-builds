6cfeffcffb5220cdd1d7131139770cf4b6542d9d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-03 20:57:29 +0000

    1cbe864d12 docs: release notes for the v12.2.13 release (#44039)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+198.sha-1a1dfab.with-local-changes
+ * @license Angular v13.0.0-next.14+199.sha-1cbe864.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
