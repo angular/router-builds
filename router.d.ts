fa2acbb4d297401a2716a522b1cacf6fab012283
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-03 18:24:03 +0000

    1a1dfaba6e docs: release notes for the v13.0.0 release (#44034)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+197.sha-8d308dc.with-local-changes
+ * @license Angular v13.0.0-next.14+198.sha-1a1dfab.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
