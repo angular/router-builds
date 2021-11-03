7a68bf2226c6a4292b725f4e28b86584a6490e52
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-03 23:39:59 +0000

    b361242a13 build(docs-infra): target webcontainers for stackblitz "live-example" links (#44041)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-rc.3+3.sha-b6f2a55.with-local-changes
+ * @license Angular v13.0.0+1.sha-b361242.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
