2eb2f003a2bdb3426afa81feba06fd4cf83fb728
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-03 22:24:29 +0000

    595a7e3dcd build: update angular (#44024)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.14+200.sha-cbf96c1.with-local-changes
+ * @license Angular v13.1.0-next.0+1.sha-595a7e3.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
