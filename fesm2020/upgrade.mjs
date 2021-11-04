f5122a222cc34e12a87f61a176c16450324f80bb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-04 00:41:57 +0000

    a0c2e0780e docs: add blog post link to CHANGELOG (#44049)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.0+3.sha-96e84cc.with-local-changes
+ * @license Angular v13.1.0-next.0+4.sha-a0c2e07.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
