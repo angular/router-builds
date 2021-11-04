820ea543d7d93377d21e22f05eaf8e1babb5e945
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-04 00:20:19 +0000

    96e84ccdcd docs: update CHANGELOG with deprecations (#44048)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.0+2.sha-8020457.with-local-changes
+ * @license Angular v13.1.0-next.0+3.sha-96e84cc.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
