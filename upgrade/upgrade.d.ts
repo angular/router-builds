eb8f76f198f03bc452150b7ad48896529a342789
Andrew Scott <atscott01@gmail.com>
2021-11-04 19:12:42 +0000

    36388b3ab2 build(docs-infra): configure region parser to allow `.mjs` (#44027)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.0+7.sha-b6798f3.with-local-changes
+ * @license Angular v13.1.0-next.0+17.sha-36388b3.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
