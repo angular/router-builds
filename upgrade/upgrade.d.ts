4862365fdc7493628e65aafafe4a1d4b208fbb35
Andrew Scott <atscott01@gmail.com>
2021-11-04 19:09:28 +0000

    b6798f30b9 build: update dependency http-server to v14 (#43900)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.0+4.sha-a0c2e07.with-local-changes
+ * @license Angular v13.1.0-next.0+7.sha-b6798f3.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
