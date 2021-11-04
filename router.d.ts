a13a06c82ae20f0a938e5bf782263bdea7029413
Andrew Scott <atscott01@gmail.com>
2021-11-04 19:17:03 +0000

    523e8fd8a3 docs: update Definition of Terms in APF guide (#44060)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.0+19.sha-ec7dd5d.with-local-changes
+ * @license Angular v13.1.0-next.0+22.sha-523e8fd.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
