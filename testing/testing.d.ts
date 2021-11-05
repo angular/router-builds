7a4f99064e0931bf0a761849dda0d4d1f29b2617
Andrew Scott <atscott01@gmail.com>
2021-11-05 16:49:56 +0000

    64af4b9c1b docs: fix typo in the CHANGELOG (#44058)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.0+28.sha-3507279.with-local-changes
+ * @license Angular v13.1.0-next.0+29.sha-64af4b9.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
