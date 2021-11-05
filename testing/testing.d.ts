d90a3c10ecd77935a0b85643b0146afbf6afd424
Andrew Scott <atscott01@gmail.com>
2021-11-05 16:41:12 +0000

    35072796c2 docs: improve documentation (#44031) PR Close #44031

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.0+23.sha-22edf02.with-local-changes
+ * @license Angular v13.1.0-next.0+28.sha-3507279.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
