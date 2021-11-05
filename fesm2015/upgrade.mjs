f75c86ceca80b8415f824e4101fd1334b21889bc
Andrew Scott <atscott01@gmail.com>
2021-11-05 20:27:36 +0000

    1543aaedd6 refactor(forms): update minlength/maxlength to inherit abstractValidator (#43998)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.0+31.sha-544c26d.with-local-changes
+ * @license Angular v13.1.0-next.0+36.sha-1543aae.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
