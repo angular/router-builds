3566d834139ea833b5bdc97abff13fdb57a19bd8
Andrew Scott <atscott01@gmail.com>
2021-11-05 18:26:56 +0000

    544c26d779 docs: fix typescript errors for the schematics-for-libraries example (#30966)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.0+29.sha-64af4b9.with-local-changes
+ * @license Angular v13.1.0-next.0+31.sha-544c26d.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
