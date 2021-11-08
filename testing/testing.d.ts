5485647d9f68a01041285dfa0b92d841d7fa8c0f
Andrew Scott <atscott01@gmail.com>
2021-11-08 17:41:46 +0000

    49964c89e6 docs: add Angular 13 support information (#44098)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.0+46.sha-baab456.with-local-changes
+ * @license Angular v13.1.0-next.0+47.sha-49964c8.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
