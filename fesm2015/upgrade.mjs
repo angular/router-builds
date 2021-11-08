649f55a5f1ef36168362d9b234c41278cc3db099
Andrew Scott <atscott01@gmail.com>
2021-11-08 21:54:45 +0000

    5eac4343c1 refactor(compiler): reverse testing of ternary operator to ensure typescript properly narrows types (#44089)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0+53.sha-644e1e2.with-local-changes
+ * @license Angular v13.0.0+54.sha-5eac434.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
