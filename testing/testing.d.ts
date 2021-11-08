76f7d27be0dda11ff070865ea38882afa4775941
Andrew Scott <atscott01@gmail.com>
2021-11-08 17:52:20 +0000

    e5a960b159 fix(compiler): generate correct code for safe method calls (#44088)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0+29.sha-58f453d.with-local-changes
+ * @license Angular v13.0.0+49.sha-e5a960b.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
