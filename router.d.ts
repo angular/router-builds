60b2d4d300051fd488b5dc32475963bdeae27125
Andrew Scott <atscott01@gmail.com>
2021-11-08 17:31:38 +0000

    acddb7f1fa build: update dependency vlq to v2 (#43901)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.0+40.sha-9f37914.with-local-changes
+ * @license Angular v13.1.0-next.0+42.sha-acddb7f.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
