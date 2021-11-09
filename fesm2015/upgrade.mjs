27f23085820efab392cfb76686885bea1e7704f1
Andrew Scott <atscott01@gmail.com>
2021-11-09 21:14:29 +0000

    247c18dee0 fix(compiler-cli): handle pre-release versions when checking version (#44109)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.0+62.sha-28ef5af.with-local-changes
+ * @license Angular v13.1.0-next.0+64.sha-247c18d.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
