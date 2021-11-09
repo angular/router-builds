ee5156008aa6f43a896542a575caca904d66ac43
Andrew Scott <atscott01@gmail.com>
2021-11-09 18:37:40 +0000

    28ef5af03f refactor(ngcc): improve logging of progress (#43996)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.0+57.sha-4f8eaac.with-local-changes
+ * @license Angular v13.1.0-next.0+62.sha-28ef5af.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
