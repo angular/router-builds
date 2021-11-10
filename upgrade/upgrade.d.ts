439a3e9d333bbedd68c0f425cf98e630d4b1b3a8
Andrew Scott <atscott01@gmail.com>
2021-11-10 18:46:35 +0000

    393efa54e6 fix(compiler): ensure that partially compiled queries can handle forward references (#44113)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.0+70.sha-ac7d5fa.with-local-changes
+ * @license Angular v13.1.0-next.0+74.sha-393efa5.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
