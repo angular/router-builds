d638d531d7b811d6ea63ff018a23df67922ba5e9
Andrew Scott <atscott01@gmail.com>
2021-11-10 01:38:46 +0000

    236bdff831 fix(platform-browser): use correct parent in animation removeChild callback (#44033)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.0+64.sha-247c18d.with-local-changes
+ * @license Angular v13.1.0-next.0+68.sha-236bdff.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
