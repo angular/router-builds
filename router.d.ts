375a764067a56c4e9f7715344b749da7cf73eee8
Andrew Scott <atscott01@gmail.com>
2021-11-10 01:39:15 +0000

    30a27adf9a fix(platform-browser): use correct parent in animation removeChild callback (#44033)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0+54.sha-5eac434.with-local-changes
+ * @license Angular v13.0.0+68.sha-30a27ad.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
