6885121ab21ad8b1edbf0867ba0b2f0d3ffc84db
Andrew Kushnir <akushnir@google.com>
2021-11-15 19:36:35 +0000

    44890437ae build: update to yarn v1.22.17 (#44150)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.0+81.sha-f817e30.with-local-changes
+ * @license Angular v13.1.0-next.1+1.sha-4489043.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
