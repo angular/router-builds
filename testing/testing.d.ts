4f0bc4a2bc05696db39adff26d5c0f6bcf37f6ce
Andrew Kushnir <akushnir@google.com>
2021-11-16 20:24:48 +0000

    5a24f96792 refactor(elements): remove non-existent schematic collection breaking with `ng add` (#44187)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.1+6.sha-00ae55c.with-local-changes
+ * @license Angular v13.1.0-next.1+8.sha-5a24f96.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
