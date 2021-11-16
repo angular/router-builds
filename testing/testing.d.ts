ae286192b00790be47e4d2917d56d7b5ef12de60
Andrew Kushnir <akushnir@google.com>
2021-11-16 22:27:33 +0000

    158ad09945 docs: Adding type annotation to ngOnInit() to make it consistent with the class structure produced by the CLI (#43579)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.1+6.sha-aa98cf1.with-local-changes
+ * @license Angular v13.0.1+12.sha-158ad09.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
