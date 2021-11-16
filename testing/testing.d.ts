fd42d4fcf1de7ec6c946a2a59cad0d15e4f0dcad
Andrew Kushnir <akushnir@google.com>
2021-11-16 18:34:38 +0000

    00ae55c3fb docs(animations): improve wording for animation example (#44168)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.1+3.sha-b7fd9eb.with-local-changes
+ * @license Angular v13.1.0-next.1+6.sha-00ae55c.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
