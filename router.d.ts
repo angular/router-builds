20ad38755e2502ffea8e99c0fe065c50f5bf992b
GitHub <noreply@github.com>
2021-11-17 18:07:03 +0000

    90a4ae1dd7 release: cut the v13.1.0-next.2 release (#44201)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.1+12.sha-6c618de.with-local-changes
+ * @license Angular v13.1.0-next.1+14.sha-90a4ae1.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
