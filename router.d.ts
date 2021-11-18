0fdd80943864fedf6b8923adbe279014d3507246
Andrew Kushnir <akushnir@google.com>
2021-11-18 18:36:47 +0000

    8330520a0d build: update dependency lighthouse to v9 (#44184)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.2+7.sha-38c02e1.with-local-changes
+ * @license Angular v13.1.0-next.2+10.sha-8330520.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
