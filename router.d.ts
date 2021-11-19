e61ba227dc1a72d6e35376c28d9fd85fba51d329
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-19 20:26:22 +0000

    53bdbc6d2c build: update angular (#44221)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.2+21.sha-48ca7dc.with-local-changes
+ * @license Angular v13.1.0-next.2+24.sha-53bdbc6.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
