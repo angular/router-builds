f56170f7bdee17c6482de45c40ebe444db99ae9c
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-19 19:52:45 +0000

    48ca7dce4f fix(compiler-cli): interpret string concat calls (#44167)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.2+16.sha-7e121e4.with-local-changes
+ * @license Angular v13.1.0-next.2+21.sha-48ca7dc.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
