bf503afe73f420d1590694c74f6e1c21ee87aff2
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-19 22:45:48 +0000

    3289aae620 refactor(bazel): always add `strictTemplates` option to `tsconfig.json` (#43674)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.2+19.sha-6e351ed.with-local-changes
+ * @license Angular v13.0.2+21.sha-3289aae.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
