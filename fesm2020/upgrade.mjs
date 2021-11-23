65e75df3de9995c49a431f41eefde92dcdf4baba
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-23 17:30:03 +0000

    40eb2f57f4 build: update dependency source-map-support to v0.5.21 (#44225)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.2+33.sha-a0177c6.with-local-changes
+ * @license Angular v13.1.0-next.2+34.sha-40eb2f5.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
