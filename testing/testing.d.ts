6b011fa7c18320c43fea499379919faaf0c8066c
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-23 17:31:45 +0000

    d265d0d241 fix(router): prevent componentless routes from being detached (#44240)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.2+27.sha-d9c99f7.with-local-changes
+ * @license Angular v13.0.2+32.sha-d265d0d.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
