7e502a811371525aa2811716c477cd1e685b57c6
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-23 21:28:06 +0000

    8c71b9fc42 refactor: delete the View Engine runtime (#43884)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.2+36.sha-f44cb57.with-local-changes
+ * @license Angular v13.1.0-next.2+39.sha-8c71b9f.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
