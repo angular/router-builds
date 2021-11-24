e2fb34c15398975311e7e615968279418f4ae48e
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-24 19:43:41 +0000

    d8e65776cb refactor(ngcc): remove Ivy switch marker transform (#43891)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.2+49.sha-83bbbd3.with-local-changes
+ * @license Angular v13.1.0-next.2+52.sha-d8e6577.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
