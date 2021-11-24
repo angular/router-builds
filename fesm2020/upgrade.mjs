5ae7895c1d407cd17cc055c65b2330ec3633e838
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-24 19:14:51 +0000

    b06e3981bc fix(core): add missing info about a component in the "pipe could not be found" error message (#44081)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.2+44.sha-6ae3858.with-local-changes
+ * @license Angular v13.1.0-next.2+50.sha-b06e398.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
