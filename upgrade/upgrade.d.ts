5dfc7a1385eb5eb4d132006fbff1be731e1c0b69
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-24 19:59:12 +0000

    b31973c176 test: remove Ivy/ViewEngine switch helpers and obsolete tests (#44120)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.2+52.sha-d8e6577.with-local-changes
+ * @license Angular v13.1.0-next.2+53.sha-b31973c.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
