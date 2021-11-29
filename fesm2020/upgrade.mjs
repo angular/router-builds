d85f012ee3b9790cb50dbfaa2f8c051fef2a9c79
Dylan Hunn <dylhunn@gmail.com>
2021-11-29 22:38:51 +0000

    cabc1786de fix(language-service): Correctly parse inputs and selectors with dollar signs (#44268)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.2+48.sha-dcea04b.with-local-changes
+ * @license Angular v13.0.2+62.sha-cabc178.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
