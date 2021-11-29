bbe03f8d71da1db385fcb72d3465d0e3a994e992
Dylan Hunn <dylhunn@gmail.com>
2021-11-29 17:54:09 +0000

    47f1c0fa33 fix(docs-infra): ensure deprecated styling is not overridden (#44275)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.2+54.sha-2c954bb.with-local-changes
+ * @license Angular v13.1.0-next.2+56.sha-47f1c0f.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
