3ece68af63028dbb5ebb71a1f456c50ef5b04a6c
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 19:36:00 +0000

    96dfc7af7c refactor(core): make the error messages tree shakable (#44219)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.2+80.sha-f058109.with-local-changes
+ * @license Angular v13.1.0-next.2+82.sha-96dfc7a.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
