f15b6138eb3684766a968da650ce7bcea6483bda
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 19:07:08 +0000

    c92095c201 refactor(docs-infra): clean up entryComponents usages (#44257)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.2+70.sha-f702c92.with-local-changes
+ * @license Angular v13.0.2+72.sha-c92095c.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
