dd1de202d35186424a47358b78672d0f1b8975ed
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 17:13:34 +0000

    0fcf18d5f4 ci: increase jasmine default timeout for saucelabs legacy tests (#44281)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.2+67.sha-3aafa76.with-local-changes
+ * @license Angular v13.1.0-next.2+74.sha-0fcf18d.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
