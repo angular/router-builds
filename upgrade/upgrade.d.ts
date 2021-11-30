5c0159b63e9a8af5bbb07198728b6a17e0cc0458
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 21:49:17 +0000

    a532040d97 Revert "feat(core): add migration to remove entryComponents (#44308)" (#44318)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.2+83.sha-24baa5e.with-local-changes
+ * @license Angular v13.1.0-next.2+84.sha-a532040.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
