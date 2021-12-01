23d3938d63db149bddb8084e5fc6aad90993fa8e
Dylan Hunn <dylhunn@gmail.com>
2021-12-01 23:38:44 +0000

    078940150e fix(docs-infra): prevent automatic linking of 'json' (#44294)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.2+82.sha-257a30d.with-local-changes
+ * @license Angular v13.0.3+1.sha-0789401.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
