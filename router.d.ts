b537d90bcc39f4e9e05524cb4568dcf333be1e85
Dylan Hunn <dylhunn@gmail.com>
2021-12-01 20:23:06 +0000

    e65a245a0b feat(core): add migration to remove entryComponents (#44322)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.2+92.sha-f7ea524.with-local-changes
+ * @license Angular v13.1.0-next.2+93.sha-e65a245.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
