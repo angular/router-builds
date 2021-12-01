8720470256ed56b5dd19261b0b195258d60201c5
GitHub <noreply@github.com>
2021-12-01 23:30:13 +0000

    4cf1a81265 release: cut the v12.2.14 release (#44341)

--- upgrade.d.ts
+++ upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.13+9.sha-a1bd9d7.with-local-changes
+ * @license Angular v12.2.13+10.sha-4cf1a81.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
