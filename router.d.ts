39b1001d575100df4f9788db8ca7a538f742deb9
Andrew Kushnir <akushnir@google.com>
2021-12-02 20:11:42 +0000

    a8f96e8aaa build(docs-infra): update `.browserslistrc` to align with new CLI apps (#44352)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.3+4.sha-7fb4309.with-local-changes
+ * @license Angular v13.0.3+5.sha-a8f96e8.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
