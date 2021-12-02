03ec81bb80e38e77e0c249504edfea2ba8deec86
Andrew Kushnir <akushnir@google.com>
2021-12-02 17:43:49 +0000

    13362972bb perf(common): code size reduction of `ngFor` directive (#44315)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.3+3.sha-99d85cb.with-local-changes
+ * @license Angular v13.1.0-next.3+4.sha-1336297.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
