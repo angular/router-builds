c1327e5c8ed22849afd081990b885235966fc4d1
Andrew Kushnir <akushnir@google.com>
2021-12-02 17:05:26 +0000

    360867b654 build: update dependency tsec to ^0.2.0 (#44274)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.3+1.sha-f3898f8.with-local-changes
+ * @license Angular v13.1.0-next.3+2.sha-360867b.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
