1d2132ee903e42acc9b1a3b1becdafe8d7495b30
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-03 17:06:30 +0000

    e8e21ac9d0 build: update dependency jsdom to v19 (#44362)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.3+5.sha-a8f96e8.with-local-changes
+ * @license Angular v13.0.3+7.sha-e8e21ac.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
