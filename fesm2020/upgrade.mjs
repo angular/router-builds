deae420667ab70af1ad14e7d7fc868813b33c323
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-03 17:05:20 +0000

    927bfb9e7b build: update dependency eslint to v8 (#43789)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.3+5.sha-5109369.with-local-changes
+ * @license Angular v13.1.0-next.3+6.sha-927bfb9.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
