da9b84d2f3a80c820f8a5c5fe02bcd61d1984612
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-06 18:29:32 +0000

    43db24302c refactor(compiler): delete View Engine components of @angular/compiler (#44368)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.3+8.sha-433a956.with-local-changes
+ * @license Angular v13.1.0-next.3+9.sha-43db243.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
