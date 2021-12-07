ceb0dd121264eebf7c03f5fb3a4746a85f514923
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-07 13:52:48 +0000

    41265919aa fix(ngcc): correctly resolve UMD dependencies (#44381)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.3+13.sha-52a73c6.with-local-changes
+ * @license Angular v13.1.0-next.3+10.sha-4126591.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
