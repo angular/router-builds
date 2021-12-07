8b8c840d724d8626d442c582eb6b1e3ec6fbf065
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-07 13:52:30 +0000

    52a73c6b14 refactor(animations): reuse existing Version class (#44375)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.3+9.sha-43db243.with-local-changes
+ * @license Angular v13.1.0-next.3+13.sha-52a73c6.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
