356e4a071edd7df3a6c51846ef2521b5e0bd626e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-07 13:52:30 +0000

    763212d5c6 refactor(animations): reuse existing Version class (#44375)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.3+11.sha-d06e546.with-local-changes
+ * @license Angular v13.0.3+15.sha-763212d.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
