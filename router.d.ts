dd81e86b1c6c8bba5b4462bac39b1e2b880eaf86
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-08 18:39:26 +0000

    80ab604fe0 docs(localize): fix typo in i18n-overview.md (#44389)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.3+10.sha-4126591.with-local-changes
+ * @license Angular v13.1.0-next.3+14.sha-80ab604.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
