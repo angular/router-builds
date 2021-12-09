f92c1b3cfda2bfad9526b0def43e935106f5c540
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-09 18:30:28 +0000

    79baf7a87a Revert "release: cut the v13.2.0-next.0 release (#44421)"

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.3+26.sha-e3f4360.with-local-changes
+ * @license Angular v13.1.0-next.3+27.sha-79baf7a.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
