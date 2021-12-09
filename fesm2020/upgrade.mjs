1b9702d9da5f5cc26fc552e66358ad465844bdcf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-09 17:18:53 +0000

    b11d3c3759 release: cut the v13.1.0 release (#44418)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.3+24.sha-08694e3.with-local-changes
+ * @license Angular v13.1.0-next.3+31.sha-b11d3c3.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
