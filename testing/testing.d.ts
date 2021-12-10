bc081d59310f2bdf8b65b1de7f67b50da3c74937
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-10 22:29:52 +0000

    7df9127088 refactor(forms): Fix weak helper types in the Forms package, mostly `Function` types that can be strengthened to specific callbacks. This was originally proposed as part of Typed Forms, but it may be possible to submit at least some of these changes separately and unconditionally (i.e. without relying on `TypedOrUntyped`.) (#44370)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.0+4.sha-7b6ca4c.with-local-changes
+ * @license Angular v13.2.0-next.0+6.sha-7df9127.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
