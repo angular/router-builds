ee11f3a95a3870fa8e8f9bd5b7428fb32593a095
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-10 21:47:17 +0000

    944b6705f8 build: remove unused starlark file that is no longer used (#44430)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0-next.3+28.sha-a7c117a.with-local-changes
+ * @license Angular v13.2.0-next.0+2.sha-944b670.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
