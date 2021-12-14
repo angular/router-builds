99c573133e2130c3f6c76c072b518f3b85d87e20
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:24:55 +0000

    ab2248e773 docs(docs-infra): change variable names in route guards examples (#44444)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.0+14.sha-e483993.with-local-changes
+ * @license Angular v13.2.0-next.0+13.sha-ab2248e.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
