0d9da60df114a27a2b9506792987e94ae10ba51b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:27:12 +0000

    815d4ceeef fix(forms): I indroduced a minor error in a previous PR: pendingValue is a value not a boolean flag. (#44450)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.0+16.sha-6571799.with-local-changes
+ * @license Angular v13.2.0-next.0+15.sha-815d4ce.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
