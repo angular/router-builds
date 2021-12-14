33a725166fa61c85586abb47faf22b5660b8ef6f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:22:41 +0000

    030747a688 docs: update Aristeidis Bampakos bio (#44410) PR Close #44410

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.0+8.sha-bab7ed3.with-local-changes
+ * @license Angular v13.2.0-next.0+10.sha-030747a.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
