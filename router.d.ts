faed3d09ed28a7d99eef38f4cb1f4b973b27a809
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:36:12 +0000

    4aa6965d41 test(docs-infra): Correct an invalid assumption in aio tests. (#44479)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.0+19.sha-f394831.with-local-changes
+ * @license Angular v13.2.0-next.0+22.sha-4aa6965.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
