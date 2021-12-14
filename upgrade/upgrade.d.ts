ba3a4e696e39199b8ad37e835179fd9af40ef546
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:20:48 +0000

    6c05c3fd2d build: update all non-major dependencies (#44152)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.0+6.sha-7df9127.with-local-changes
+ * @license Angular v13.2.0-next.0+7.sha-6c05c3f.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
