5161b28479f7be76c766caf2df2417d6d6644e04
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:22:07 +0000

    bab7ed3541 fix(animations): should not invoke disabled child animations (#37724)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.0+7.sha-6c05c3f.with-local-changes
+ * @license Angular v13.2.0-next.0+8.sha-bab7ed3.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
