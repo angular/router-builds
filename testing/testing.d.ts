821938f90c67b7f0f564d6d7853acc02e3061ed9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:36:59 +0000

    093fd39f93 refactor(compiler): clean up workarounds for TypeScript 4.5 (#44477)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.0+22.sha-4aa6965.with-local-changes
+ * @license Angular v13.2.0-next.0+21.sha-093fd39.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
