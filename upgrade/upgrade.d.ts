54f102f2c85f852529e4289704d0ee35d8014f9a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:29:24 +0000

    f56ac0fe83 refactor(animations): improve _finalKeyframe non-null assertion (#44458)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.0+17.sha-5da31d6.with-local-changes
+ * @license Angular v13.2.0-next.0+18.sha-f56ac0f.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
