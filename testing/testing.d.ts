56cc36a92d38a9161558846c2fa4da63a23a6574
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:23:01 +0000

    c68385a967 docs: add jaybell to gde list (#44423)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.0+10.sha-030747a.with-local-changes
+ * @license Angular v13.2.0-next.0+11.sha-c68385a.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
