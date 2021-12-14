19e09af1e6ffd0d3e6c9ba2dd314e386f93eb51b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:36:49 +0000

    240edcbc4e test(docs-infra): Correct an invalid assumption in aio tests. (#44479)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0+7.sha-a0316ab.with-local-changes
+ * @license Angular v13.1.0+24.sha-240edcb.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
