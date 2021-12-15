1957fff163ef8c9eef34bb3ed37e085e487c1986
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-15 18:38:32 +0000

    c2ed1ae945 fix(docs-infra): update examples to Angular 13.1 (#44487)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0+25.sha-bce108a.with-local-changes
+ * @license Angular v13.1.0+26.sha-c2ed1ae.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
