a9aa6b5ac4bbb2365d0876e7ca055bcf732124db
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-15 18:38:22 +0000

    7c246d513b fix(docs-infra): update examples to Angular 13.1 (#44487)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.0+26.sha-9921b2c.with-local-changes
+ * @license Angular v13.2.0-next.0+27.sha-7c246d5.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
