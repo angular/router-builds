3e360b50d6c5688b08c334e872569d6caa8d32e1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-15 19:51:43 +0000

    0115e2b664 release: cut the v13.1.1 release (#44495)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0+26.sha-c2ed1ae.with-local-changes
+ * @license Angular v13.1.0+27.sha-0115e2b.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
