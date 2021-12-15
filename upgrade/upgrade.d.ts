1292bf214dadc5d01a5e9710fe29982dd1057d7b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-15 17:43:50 +0000

    bce108ab49 fix(forms): `_reduceValue` arrow function now has correct types. (#44483)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0+24.sha-240edcb.with-local-changes
+ * @license Angular v13.1.0+25.sha-bce108a.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
