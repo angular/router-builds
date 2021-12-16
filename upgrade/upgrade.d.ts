b1a9b532fe0b8b4bd31e8d63c8c820b4878e88df
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-16 19:47:22 +0000

    beb18286aa docs: remove recent departures from contributors page (#44514)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.0+27.sha-0115e2b.with-local-changes
+ * @license Angular v13.1.1+1.sha-beb1828.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
