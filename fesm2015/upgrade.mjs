bbf6e2d0d97a339aadbfab056e46d988eb7807ec
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:38:20 +0000

    a864d1c926 fix(docs-infra): adjust code link styling height (#44376)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+27.sha-5ae1030.with-local-changes
+ * @license Angular v13.2.0-next.1+29.sha-a864d1c.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
