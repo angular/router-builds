44bb062fdeb78a013c82609d1ea64b558d4115bf
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:30:07 +0000

    67c81ccef0 refactor(docs-infra): improve aio-footer's layout (#44532)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+14.sha-f7aa937.with-local-changes
+ * @license Angular v13.2.0-next.1+15.sha-67c81cc.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
