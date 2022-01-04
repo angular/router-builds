2bf359b473c7ef682ae453b926ccd62927c59c1d
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:30:56 +0000

    9d2401d99a docs: fix several typos (#44508)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+15.sha-67c81cc.with-local-changes
+ * @license Angular v13.2.0-next.1+16.sha-9d2401d.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
