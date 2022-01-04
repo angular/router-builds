381c82adee543295e2f90eead71ea281535f0a37
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:20:45 +0000

    a01c077a67 docs: remove Angular 10 from support table (#44611)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+4.sha-af512de.with-local-changes
+ * @license Angular v13.2.0-next.1+5.sha-a01c077.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
