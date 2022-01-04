f61c71b8da76551584f3c4aeeba2b695d91c151f
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 19:17:36 +0000

    2469b88af2 docs: fix typo (#44592) PR Close #44592

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.1+2.sha-e6beeab.with-local-changes
+ * @license Angular v13.1.1+3.sha-2469b88.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
