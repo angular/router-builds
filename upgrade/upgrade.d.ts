93727050b5b3f123cbbd38d8065eb7dfd7c98348
Dylan Hunn <dylhunn@gmail.com>
2022-01-05 00:14:56 +0000

    c7ac2dfcf8 refactor(compiler): remove output AST functions (#44411)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+30.sha-d4015f7.with-local-changes
+ * @license Angular v13.2.0-next.1+49.sha-c7ac2df.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
