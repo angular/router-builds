26a094da96226cd6d093d54105fcb0a3639b6fc9
Dylan Hunn <dylhunn@gmail.com>
2022-01-05 00:09:48 +0000

    d4015f762f test: remove newlines to make a test more stable (#44616)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+31.sha-f2b858d.with-local-changes
+ * @license Angular v13.2.0-next.1+30.sha-d4015f7.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
