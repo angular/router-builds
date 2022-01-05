602a6148ee3ed6f941733835cd2a7541c3d93315
Dylan Hunn <dylhunn@gmail.com>
2022-01-05 00:50:51 +0000

    d5ced77d31 refactor(docs-infra): fix eslint issues for aio ugrade-* examples (#44534)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+49.sha-c7ac2df.with-local-changes
+ * @license Angular v13.2.0-next.1+50.sha-d5ced77.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
