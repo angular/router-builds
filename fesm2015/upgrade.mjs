973c8a0bda4e820b3fb259acdb83ccd671e0ba27
Andrew Scott <atscott01@gmail.com>
2022-01-05 23:38:36 +0000

    e39f804f1e test(upgrade): fix flaky tests (#44597)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+61.sha-2d14724.with-local-changes
+ * @license Angular v13.2.0-next.1+60.sha-e39f804.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
