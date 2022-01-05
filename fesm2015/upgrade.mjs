4c0dc3e788824821357693f0c1a16b8d0f7d70c7
Andrew Scott <atscott01@gmail.com>
2022-01-05 18:03:32 +0000

    7278a9c584 docs(animations): fix filter-animations docregion (#44544)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+50.sha-d5ced77.with-local-changes
+ * @license Angular v13.2.0-next.1+51.sha-7278a9c.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
