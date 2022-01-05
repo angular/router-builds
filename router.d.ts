d91ed1e64f1743ec3020a1e31bdb2646e92cb454
Andrew Scott <atscott01@gmail.com>
2022-01-05 23:36:46 +0000

    98592b03bd docs(common): url link format updated (#44638)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.1+49.sha-0daa9bf.with-local-changes
+ * @license Angular v13.1.1+57.sha-98592b0.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
