201d0978875bde643bd3db4653aa97ec6177b332
Andrew Scott <atscott01@gmail.com>
2022-01-06 00:40:33 +0000

    9b9042c8d1 docs(animations): fix links to the AnimationBuilder.build method (#44594)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.1+57.sha-98592b0.with-local-changes
+ * @license Angular v13.1.1+58.sha-9b9042c.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
