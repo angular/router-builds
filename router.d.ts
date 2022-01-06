34408d32ffba04280612a8d18d313f57b11100d3
Andrew Scott <atscott01@gmail.com>
2022-01-06 18:52:45 +0000

    7f51f1dcae docs(docs-infra): improve the content of attribute directives (#44628)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+62.sha-b69a514.with-local-changes
+ * @license Angular v13.2.0-next.1+65.sha-7f51f1d.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
