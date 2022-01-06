b73006a701ed569d473fe07b4ab4df8c9c33cdd8
Andrew Scott <atscott01@gmail.com>
2022-01-06 18:53:06 +0000

    359103274b test: clean up more entryComponents usages (#44561)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+65.sha-7f51f1d.with-local-changes
+ * @license Angular v13.2.0-next.1+64.sha-3591032.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
