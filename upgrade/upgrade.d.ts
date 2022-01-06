20d1dd84af46fc951f14c01bd43d71293b5a8570
Andrew Scott <atscott01@gmail.com>
2022-01-06 00:40:32 +0000

    b69a5148aa docs(animations): fix links to the AnimationBuilder.build method (#44594)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+63.sha-ea91edb.with-local-changes
+ * @license Angular v13.2.0-next.1+62.sha-b69a514.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
