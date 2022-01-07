bce0b0a1d2cb7f0f76c95245ac22185002582979
Andrew Kushnir <akushnir@google.com>
2022-01-07 00:58:42 +0000

    57d5f9529d build: use correct target name (#44651)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.1+62.sha-5017bf9.with-local-changes
+ * @license Angular v13.1.1+70.sha-57d5f95.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
