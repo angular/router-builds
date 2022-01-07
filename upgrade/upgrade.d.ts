0f19a50ad8afc489149f6355a40551e6a4204119
Andrew Scott <atscott01@gmail.com>
2022-01-07 18:26:05 +0000

    21f8d12c3c release: cherry-pick the v12.2.15 release notes (#44619)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+77.sha-0cf5501.with-local-changes
+ * @license Angular v13.2.0-next.1+79.sha-21f8d12.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
