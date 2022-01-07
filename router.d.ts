20adf65e006be52e31a28c39f9fe95f448949395
Andrew Scott <atscott01@gmail.com>
2022-01-07 18:28:15 +0000

    c57e22a21b refactor(docs-infra): remove IE11 code (#44570)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+81.sha-277d2af.with-local-changes
+ * @license Angular v13.2.0-next.1+83.sha-c57e22a.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
