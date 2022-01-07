5fc5ce25a9a0be62a4a1fab584584731f8b3041e
Andrew Scott <atscott01@gmail.com>
2022-01-07 19:33:32 +0000

    919f8ae4e8 build: update dependency google-closure-compiler to v20220104 (#44655)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.1+78.sha-63c8e56.with-local-changes
+ * @license Angular v13.1.1+79.sha-919f8ae.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
