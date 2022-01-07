cb500bc3e04748ceba30c6bc5dc09bca53fee9f0
Andrew Scott <atscott01@gmail.com>
2022-01-07 18:29:44 +0000

    63c8e56a3a fix(compiler-cli): incorrectly interpreting $any calls with a property read (#44657)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.1+73.sha-d252cff.with-local-changes
+ * @license Angular v13.1.1+78.sha-63c8e56.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
