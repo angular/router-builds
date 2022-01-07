3b2ff7d0ae72835f60a13fb2f2182d123b2bf3ba
Andrew Kushnir <akushnir@google.com>
2022-01-07 00:59:38 +0000

    0cf5501d5c build: use correct target name (#44651)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+69.sha-67df935.with-local-changes
+ * @license Angular v13.2.0-next.1+77.sha-0cf5501.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
