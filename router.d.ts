45ff75825996bf65981ab7bac41d256e47be3669
Andrew Scott <atscott01@gmail.com>
2022-01-07 22:32:54 +0000

    8ba43a1a1e fix(animations): retain triggers values for moved tracked list items (#44578)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+85.sha-13d5f64.with-local-changes
+ * @license Angular v13.2.0-next.1+86.sha-8ba43a1.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
