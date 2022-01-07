61060f146b3426c1d269c572e6b36ae6679b98f4
Andrew Scott <atscott01@gmail.com>
2022-01-07 18:28:31 +0000

    de93b6e770 refactor(compiler-cli): update template typechecking link to latest angular.io version (#44649)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+83.sha-c57e22a.with-local-changes
+ * @license Angular v13.2.0-next.1+80.sha-de93b6e.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
