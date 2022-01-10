ae252c95bebe929c43f562058e34b508400fe297
Andrew Scott <atscott01@gmail.com>
2022-01-10 20:10:47 +0000

    a55e8422fb docs: missing whitespace in HostListener example (#44624)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+89.sha-c82e541.with-local-changes
+ * @license Angular v13.2.0-next.1+88.sha-a55e842.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
