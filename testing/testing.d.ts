fd9ae74fb3d3d6121b586b06dcc9b98b39ad5b27
Andrew Scott <atscott01@gmail.com>
2022-01-10 21:38:23 +0000

    1a9121826e fix(compiler-cli): enable narrowing of using type guard methods (#44447)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+91.sha-ebf8446.with-local-changes
+ * @license Angular v13.2.0-next.1+92.sha-1a91218.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
