1c771e4aeb5a803a203b7ec5b8c3af82896364bb
Andrew Scott <atscott01@gmail.com>
2022-01-10 21:42:12 +0000

    af2a1317cb feat(language-service): support completions for animation (#44630)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+92.sha-1a91218.with-local-changes
+ * @license Angular v13.2.0-next.1+94.sha-af2a131.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
