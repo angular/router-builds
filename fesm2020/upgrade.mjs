59f00924376b24a84471170310c72ffbd4fb580a
Andrew Scott <atscott01@gmail.com>
2022-01-11 22:20:52 +0000

    e98b8f5879 docs: add miniprogram cross-platform development resource with resources.json (#44646)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+106.sha-de0ae1b.with-local-changes
+ * @license Angular v13.2.0-next.1+107.sha-e98b8f5.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
