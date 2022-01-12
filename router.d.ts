e49b0a68d6e24cb6abe8127c144c18b64e4b6269
Andrew Scott <atscott01@gmail.com>
2022-01-12 20:57:46 +0000

    57d0ca18e7 ci: add golden files for runtime error codes (#44677)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+1.sha-c888dfd.with-local-changes
+ * @license Angular v13.2.0-next.2+3.sha-57d0ca1.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
