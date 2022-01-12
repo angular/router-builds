2047c5a2173a7d8ec78f0f432e4ba672cb5cdb25
Andrew Scott <atscott01@gmail.com>
2022-01-12 20:58:07 +0000

    c7b210d484 refactor(forms): update email validator to inherit abstractValidator (#44545)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+3.sha-57d0ca1.with-local-changes
+ * @license Angular v13.2.0-next.2+2.sha-c7b210d.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
