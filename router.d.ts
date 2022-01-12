396f0dae87eedec93b1fdf3b87ae7cf03ba2cb83
Andrew Scott <atscott01@gmail.com>
2022-01-12 20:59:21 +0000

    ec95631ceb refactor(animations): Remove unnecessary IE specific code (#44686)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.1+97.sha-d1762c7.with-local-changes
+ * @license Angular v13.1.2+4.sha-ec95631.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
