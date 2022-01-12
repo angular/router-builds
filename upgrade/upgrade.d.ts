065316c5f9b718b066c33cf4c54de2e53488a94e
GitHub <noreply@github.com>
2022-01-12 19:03:38 +0000

    d1762c78af release: cut the v13.1.2 release (#44698)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.1+96.sha-92784ca.with-local-changes
+ * @license Angular v13.1.1+97.sha-d1762c7.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
