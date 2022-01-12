3cc4e91fd803ac91b89f2db2cd704db5442b8e34
GitHub <noreply@github.com>
2022-01-12 19:12:19 +0000

    555a57d505 docs: release notes for the v13.1.2 release (#44699)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.1+107.sha-e98b8f5.with-local-changes
+ * @license Angular v13.2.0-next.1+108.sha-555a57d.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
