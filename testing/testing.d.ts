f6cf082221a543dfbf7beafa49bbb1748dc0fbf9
Dylan Hunn <dylhunn@gmail.com>
2022-01-13 20:18:44 +0000

    d460e02d9e build: update all non-major dependencies (#44705)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+13.sha-c2da290.with-local-changes
+ * @license Angular v13.2.0-next.2+11.sha-d460e02.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
