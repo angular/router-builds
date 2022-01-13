a8ef8358002195573dc9db321091e5e3e7873f21
Dylan Hunn <dylhunn@gmail.com>
2022-01-13 20:19:35 +0000

    adc487fb75 fix(animations): apply setStyles to only rootTimelines (#44515)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+11.sha-d460e02.with-local-changes
+ * @license Angular v13.2.0-next.2+12.sha-adc487f.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
