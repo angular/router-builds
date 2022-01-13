f8de9e4fa692238fce6c371ea787fb4c728c381d
Andrew Scott <atscott01@gmail.com>
2022-01-13 18:43:45 +0000

    2189588ff0 docs: remove unnecessary parenthesis in View Encapsulation file (#44702) PR Close #44702

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+2.sha-c7b210d.with-local-changes
+ * @license Angular v13.2.0-next.2+6.sha-2189588.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
