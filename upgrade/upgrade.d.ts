e982096c8a0e74b3123b06702c8cebaf25239511
Dylan Hunn <dylhunn@gmail.com>
2022-01-13 21:05:08 +0000

    6e0481572f docs(docs-infra): change the testing guide due to deprecation (#44674) The jasmine used in the docs is

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+12.sha-adc487f.with-local-changes
+ * @license Angular v13.2.0-next.2+14.sha-6e04815.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
