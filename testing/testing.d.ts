d9150d134fb05e9286ed507fc076645ef789f494
Andrew Scott <atscott01@gmail.com>
2022-01-13 18:44:05 +0000

    432e518931 refactor(elements): remove the createCustomEvent function (#44703)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+6.sha-2189588.with-local-changes
+ * @license Angular v13.2.0-next.2+5.sha-432e518.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
