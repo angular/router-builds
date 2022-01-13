ae0642a00e1f334033cf060da2703446993d29dc
Dylan Hunn <dylhunn@gmail.com>
2022-01-13 20:18:39 +0000

    c2da290659 feat(docs-infra): Add a new processor for `@overriddenImplementation`. (#44689)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+10.sha-f06b0d9.with-local-changes
+ * @license Angular v13.2.0-next.2+13.sha-c2da290.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
