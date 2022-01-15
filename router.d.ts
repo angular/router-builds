1bea9d64033cf6d7db0c5c876e2d437725e4ebfe
Dylan Hunn <dylhunn@gmail.com>
2022-01-15 02:04:09 +0000

    b66e479cdb fix(ngcc): support element accesses for export declarations (#44669)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+14.sha-6e04815.with-local-changes
+ * @license Angular v13.2.0-next.2+15.sha-b66e479.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
