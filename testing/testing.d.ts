6136badbafe7d28b9b5711c62f72e9f27676a2c2
Dylan Hunn <dylhunn@gmail.com>
2022-01-18 17:41:24 +0000

    617610e3b0 docs: add Ankit to GDE resources (#44659)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.2+13.sha-f9ca4d8.with-local-changes
+ * @license Angular v13.1.2+15.sha-617610e.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
