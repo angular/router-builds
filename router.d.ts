094259f20f84439954e641fee5e955edaa087ec4
Dylan Hunn <dylhunn@gmail.com>
2022-01-18 23:10:55 +0000

    e9bd16adbe refactor(compiler): remove directive matching from template compiler (#44731)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.2+16.sha-cab81ba.with-local-changes
+ * @license Angular v13.1.2+19.sha-e9bd16a.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
