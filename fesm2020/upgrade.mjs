eda7bb354a233c90b0af3fbc7013f2a49496ac37
Dylan Hunn <dylhunn@gmail.com>
2022-01-18 17:41:15 +0000

    ccb73c0ac8 docs: add Ankit to GDE resources (#44659)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+15.sha-b66e479.with-local-changes
+ * @license Angular v13.2.0-next.2+17.sha-ccb73c0.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
