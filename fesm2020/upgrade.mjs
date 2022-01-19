c0935c7bb303bb651d366a0dee9714f436ebaa9d
Dylan Hunn <dylhunn@gmail.com>
2022-01-19 01:55:11 +0000

    3797d1022d refactor(core): make the error messages tree shakable (#44359) Long error messages can be tree-shaken in the production build and replaced with error codes.

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.2+20.sha-929788d.with-local-changes
+ * @license Angular v13.1.2+21.sha-3797d10.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
