a7d852031da136f690e6a6239de6b057c40e2e68
Dylan Hunn <dylhunn@gmail.com>
2022-01-19 00:36:04 +0000

    41b8ecc9b8 refactor(forms): Make a couple small cleanups encountered while rebasing typed forms. (#44748)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+23.sha-ece530f.with-local-changes
+ * @license Angular v13.2.0-next.2+24.sha-41b8ecc.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
