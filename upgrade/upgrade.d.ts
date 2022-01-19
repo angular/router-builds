d476b214c86f4b1b4b2a38f4fa7e8909359a07ab
Dylan Hunn <dylhunn@gmail.com>
2022-01-19 17:41:50 +0000

    34d2292323 docs: update deprecated form of the tap operator in http example (#44738)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+27.sha-b49ffcd.with-local-changes
+ * @license Angular v13.2.0-next.2+28.sha-34d2292.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
