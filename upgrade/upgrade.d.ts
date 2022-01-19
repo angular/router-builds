4a751e66f55981deacbced3bf98aef132a61c271
Dylan Hunn <dylhunn@gmail.com>
2022-01-19 00:09:11 +0000

    ece530faf5 refactor(animations): change errors type from any to string (#44726)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+21.sha-1f6249d.with-local-changes
+ * @license Angular v13.2.0-next.2+23.sha-ece530f.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
