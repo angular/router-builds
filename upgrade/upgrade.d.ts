295b4bbc07603564716641d9c5b9af1a0918334e
GitHub <noreply@github.com>
2022-01-20 01:53:07 +0000

    caeb162513 release: cut the v13.2.0-rc.0 release (#44765)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+35.sha-ed21f5c.with-local-changes
+ * @license Angular v13.2.0-next.2+36.sha-caeb162.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
