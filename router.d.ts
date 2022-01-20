57170ca5e4e7ced03e09f70cd19bb633d82ede2a
Dylan Hunn <dylhunn@gmail.com>
2022-01-20 01:04:15 +0000

    ec1057014b fix(docs-infra): fix date parsing in a flaky test (#44763)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+33.sha-fa835b5.with-local-changes
+ * @license Angular v13.2.0-next.2+34.sha-ec10570.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
