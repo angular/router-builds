f561a1a510da6d64fb08caf3a466fe193c5ef148
Andrew Kushnir <akushnir@google.com>
2022-01-20 22:21:03 +0000

    197f3f4a26 fix(compiler-cli): remove leftover `_extendedTemplateDiagnostics` requirements (#44777)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+39.sha-4e180cb.with-local-changes
+ * @license Angular v13.2.0-next.2+40.sha-197f3f4.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
