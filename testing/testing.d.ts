f41d5cfcc4b05acf576d3a07bb75123a267f7fc7
Andrew Kushnir <akushnir@google.com>
2022-01-20 19:34:38 +0000

    4e180cb43b refactor(compiler): pass rootDir to tsickle (#44768)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+38.sha-011d0f2.with-local-changes
+ * @license Angular v13.2.0-next.2+39.sha-4e180cb.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
