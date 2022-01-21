4e3d3b1e6bcaf6a5d5eb229332f554b4754dc808
Andrew Kushnir <akushnir@google.com>
2022-01-21 19:35:38 +0000

    fb9163ae50 fix(core): consistently use namespace short name rather than URI (#44766)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+40.sha-197f3f4.with-local-changes
+ * @license Angular v13.2.0-next.2+43.sha-fb9163a.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
