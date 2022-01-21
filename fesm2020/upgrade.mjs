63b45fff89c81b9d98e50234ec85b7270a2bda30
Andrew Kushnir <akushnir@google.com>
2022-01-21 19:39:14 +0000

    ef5486fab5 build: update all non-major dependencies (#44713)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.3+4.sha-a9f5c87.with-local-changes
+ * @license Angular v13.1.3+7.sha-ef5486f.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
