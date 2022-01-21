c87096d6e63cf92a44531539542d4b585d3bf8ad
Andrew Kushnir <akushnir@google.com>
2022-01-21 19:36:10 +0000

    9d4af65e34 feat(language-service): Provide plugin to delegate rename requests to Angular (#44696)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+43.sha-fb9163a.with-local-changes
+ * @license Angular v13.2.0-next.2+41.sha-9d4af65.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
