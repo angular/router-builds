51000e06f54604b01a7be7204f594169f661d4e3
Andrew Kushnir <akushnir@google.com>
2022-01-21 21:28:22 +0000

    80467c3aa5 refactor(common): unused return value from attemptFocus (#44457)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+46.sha-627e807.with-local-changes
+ * @license Angular v13.2.0-next.2+47.sha-80467c3.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
