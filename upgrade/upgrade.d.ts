6671ab22d9ca9b10099ea9802e440e58537afd02
Andrew Kushnir <akushnir@google.com>
2022-01-21 21:30:30 +0000

    45503b43e7 refactor(common): unused return value from attemptFocus (#44457)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.3+7.sha-ef5486f.with-local-changes
+ * @license Angular v13.1.3+9.sha-45503b4.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
