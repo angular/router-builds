415d34c4283bbf130bde15747f6d076c3216c901
Andrew Kushnir <akushnir@google.com>
2022-01-21 21:28:13 +0000

    627e807eb8 fix(compiler): properly compile DI factories when coverage reporting is enabled (#44732)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+45.sha-e2920d9.with-local-changes
+ * @license Angular v13.2.0-next.2+46.sha-627e807.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
