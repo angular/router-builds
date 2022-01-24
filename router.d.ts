52e971a1c91a64a14af450fa7394af569e21cee0
Andrew Kushnir <akushnir@google.com>
2022-01-24 18:59:06 +0000

    ec2e6f6a3f fix(compiler): correct spans when parsing bindings with comments (#44785)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+49.sha-25a83eb.with-local-changes
+ * @license Angular v13.2.0-next.2+54.sha-ec2e6f6.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
