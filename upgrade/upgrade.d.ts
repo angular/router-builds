bbf6e12cc01e76a8a3b5937c7b1cd29d66191819
Andrew Kushnir <akushnir@google.com>
2022-01-24 19:04:30 +0000

    a52685bdaf fix(core): error if NgZone.isInAngularZone is called with a noop zone (#44800)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.3+9.sha-45503b4.with-local-changes
+ * @license Angular v13.1.3+13.sha-a52685b.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
