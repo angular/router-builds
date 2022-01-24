5ca2b5b047971d2f91200d1319393ba74878d5c4
GitHub <noreply@github.com>
2022-01-24 18:06:40 +0000

    955a677aab docs: release notes for the v13.2.0-rc.1 release (#44805)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+47.sha-80467c3.with-local-changes
+ * @license Angular v13.2.0-next.2+48.sha-955a677.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
