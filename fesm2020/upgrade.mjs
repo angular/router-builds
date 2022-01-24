1d5e97cb5dd89b91e4b12e7296b1c5b53435b9fd
Andrew Kushnir <akushnir@google.com>
2022-01-24 23:10:50 +0000

    6a68bab58e docs: add link to AbstractControlOptions reference (#44764) PR Close #44764

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+63.sha-c9fcc4f.with-local-changes
+ * @license Angular v13.2.0-next.2+74.sha-6a68bab.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
