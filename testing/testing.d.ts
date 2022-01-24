a68f6ab6099ca09889fee1aa1c3bdd0af18d274a
Andrew Kushnir <akushnir@google.com>
2022-01-24 23:07:54 +0000

    ec0a0e0669 feat(service-worker): add `cacheOpaqueResponses` option for data-groups (#44723)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+58.sha-a534a78.with-local-changes
+ * @license Angular v13.2.0-next.2+62.sha-ec0a0e0.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
