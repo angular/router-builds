eb0c7e56878f2e2565be47dcaffea21ab41b4ee5
GitHub <noreply@github.com>
2022-01-24 17:58:32 +0000

    fce521e4d2 release: cut the v13.2.0-rc.1 release (#44804)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-rc.0+9.sha-ade3057.with-local-changes
+ * @license Angular v13.2.0-rc.0+10.sha-fce521e.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
