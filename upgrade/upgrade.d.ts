0f656570b02b43076cdeb96d076489bb3ac53f2c
Andrew Kushnir <akushnir@google.com>
2022-01-25 17:41:40 +0000

    333cbfeb48 build(docs-infra): update `dgeni-packages` to v0.29.3 (#44810)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+75.sha-05b8de7.with-local-changes
+ * @license Angular v13.2.0-next.2+76.sha-333cbfe.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
