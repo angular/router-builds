1aac8249372575ae4f5bc31e6a3b4ce19aac4e61
Andrew Kushnir <akushnir@google.com>
2022-01-25 17:41:01 +0000

    05b8de7523 build: update angular (#44771)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+74.sha-6a68bab.with-local-changes
+ * @license Angular v13.2.0-next.2+75.sha-05b8de7.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
