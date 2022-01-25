fdb6da7864b7ca53e4aa6a81f9c5f76ed448d992
Andrew Kushnir <akushnir@google.com>
2022-01-25 01:26:57 +0000

    dd7364fc16 ci: update payload size limit for the Forms test app (#44813)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.3+13.sha-a52685b.with-local-changes
+ * @license Angular v13.1.3+27.sha-dd7364f.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
