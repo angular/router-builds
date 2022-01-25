70cccb173d0971bbb9f30e212660d57d6ed9dd75
Andrew Kushnir <akushnir@google.com>
2022-01-25 22:31:24 +0000

    fe3e4d6865 fix(compiler-cli): Handle `ng-template` with structural directive in indexer (#44788)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-rc.1+24.sha-6cb7c3e.with-local-changes
+ * @license Angular v13.2.0-rc.1+25.sha-fe3e4d6.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
