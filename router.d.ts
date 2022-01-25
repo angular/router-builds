99ff53fe76222f5b5538b7f4204af1d64de22472
Andrew Kushnir <akushnir@google.com>
2022-01-25 22:32:12 +0000

    1bce51c0ed fix(compiler-cli): Handle `ng-template` with structural directive in indexer (#44788)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+76.sha-333cbfe.with-local-changes
+ * @license Angular v13.2.0-next.2+77.sha-1bce51c.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
