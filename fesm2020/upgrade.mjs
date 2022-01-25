c492eac8748441975be6a709c5ad413339198197
Andrew Kushnir <akushnir@google.com>
2022-01-25 22:35:03 +0000

    9cd3583920 fix(compiler-cli): Handle `ng-template` with structural directive in indexer (#44788)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.1.3+29.sha-02d3d65.with-local-changes
+ * @license Angular v13.1.3+30.sha-9cd3583.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
