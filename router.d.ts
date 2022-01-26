2c41a36a70ab77dd03815fefff8cdb41241c2717
Andrew Kushnir <akushnir@google.com>
2022-01-26 21:10:43 +0000

    fcc6528159 test(animations): Add bundling size regression test for animations package (#44826)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0+1.sha-dc0cac2.with-local-changes
+ * @license Angular v13.2.0+2.sha-fcc6528.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
