883a0d600d3eea90cd69a3341aab67a33cdb1739
Andrew Kushnir <akushnir@google.com>
2022-01-26 01:20:35 +0000

    a38ae0aefa refactor(forms): Temporarily disable the typed forms migration. (#44828)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-rc.1+25.sha-fe3e4d6.with-local-changes
+ * @license Angular v13.2.0-rc.1+26.sha-a38ae0a.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
