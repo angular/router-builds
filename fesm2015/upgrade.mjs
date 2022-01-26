e936e020063a73b25b803b2e8c8e6aa2da37a31f
GitHub <noreply@github.com>
2022-01-26 20:02:19 +0000

    de1e35b944 release: cut the v13.2.0 release (#44838)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-rc.1+26.sha-a38ae0a.with-local-changes
+ * @license Angular v13.2.0-rc.1+27.sha-de1e35b.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
