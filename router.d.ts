978e28285abbced7a6258625a7cf1240bd6d13c3
Andrew Kushnir <akushnir@google.com>
2022-01-26 20:52:15 +0000

    e46ac81f54 docs: cleanup CHANGELOG.md file (#44841)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0-next.2+81.sha-04eec90.with-local-changes
+ * @license Angular v14.0.0-next.0+1.sha-e46ac81.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
