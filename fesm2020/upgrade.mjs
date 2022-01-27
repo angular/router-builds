207441ef4c0397cb92d917dc126550f4e5a1f9fc
Andrew Kushnir <akushnir@google.com>
2022-01-27 17:41:04 +0000

    df50af2bc5 ci: only build test targets for components repo unit test job (#44832)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1020.sha-e9124b4.with-local-changes
+ * @license Angular v14.0.0-next.0+1022.sha-df50af2.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
