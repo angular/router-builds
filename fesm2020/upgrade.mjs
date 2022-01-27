7c8c6beef81afea8206b0c932795e9f2c8c84b96
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-27 22:11:32 +0000

    7b7d644286 refactor: fix type checking error with devtools state-serializer spec (#44735)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1028.sha-c12110f.with-local-changes
+ * @license Angular v14.0.0-next.0+1027.sha-7b7d644.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
