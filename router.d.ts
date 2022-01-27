679413f660f47f4972ec1d14d8290f459c524422
Andrew Kushnir <akushnir@google.com>
2022-01-27 17:37:00 +0000

    e9124b42d5 refactor(compiler-cli): Tolerate source span errors in indexer (#44825)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1019.sha-eb11bcb.with-local-changes
+ * @license Angular v14.0.0-next.0+1020.sha-e9124b4.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
