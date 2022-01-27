e16153066116b6162339f2209719a420aaae169f
Andrew Kushnir <akushnir@google.com>
2022-01-27 20:36:24 +0000

    522b845bec docs: enable the survey banner (#44861)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0+3.sha-4a91d9b.with-local-changes
+ * @license Angular v13.2.0+8.sha-522b845.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
