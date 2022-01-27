42233523f87fa4c9d11297a0b71494901af80261
Andrew Kushnir <akushnir@google.com>
2022-01-27 01:54:16 +0000

    a03f80a409 build: update dependency rules_pkg to v0.6.0 (#44818)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1016.sha-c020f60.with-local-changes
+ * @license Angular v14.0.0-next.0+1018.sha-a03f80a.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
