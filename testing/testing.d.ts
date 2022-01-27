09f8c1dd14a48c5ac14e359ed82e3048d2a5b6ae
Andrew Kushnir <akushnir@google.com>
2022-01-27 01:53:58 +0000

    4a91d9b0a0 build: update dependency rules_pkg to v0.6.0 (#44818)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0+2.sha-fcc6528.with-local-changes
+ * @license Angular v13.2.0+3.sha-4a91d9b.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
