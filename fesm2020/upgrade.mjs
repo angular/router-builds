e87f09a355ccfd223948dfb6441f6e0ca6dd6e57
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 16:13:16 +0000

    07f827eec3 docs(core): edit and add docs for ng-container (#44762)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1029.sha-910de8b.with-local-changes
+ * @license Angular v14.0.0-next.0+1030.sha-07f827e.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
