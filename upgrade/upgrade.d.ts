3013299b122e63c4824459ffecc61b3ea975dc33
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 17:54:36 +0000

    a8fca09371 docs: fix typo in update guide (#44853)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1034.sha-f490c2d.with-local-changes
+ * @license Angular v14.0.0-next.0+1035.sha-a8fca09.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
