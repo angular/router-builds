b647843a772428d0d268b380ee4e19bf9ce31ec9
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 22:15:54 +0000

    6b7fffcbeb fix(forms): Update the typed forms migration schematic to find all files. (#44881)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0+18.sha-641f27a.with-local-changes
+ * @license Angular v13.2.0+19.sha-6b7fffc.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
