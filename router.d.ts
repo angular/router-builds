637dc84ce0f26addb3e2e557681094a20990ee5b
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 16:25:47 +0000

    c577a1dc93 fix(docs-infra): fix links to CLI commands source code (#44835)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1031.sha-f1a5145.with-local-changes
+ * @license Angular v14.0.0-next.0+1032.sha-c577a1d.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
