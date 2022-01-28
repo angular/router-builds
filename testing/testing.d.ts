7d250c84d7a2c35bf4c2e7b02bd5c63a8161b54e
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 18:06:53 +0000

    ab63ed3448 test(docs-infra): fix type checking errors with new jasmine types (#44876)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0+14.sha-f398717.with-local-changes
+ * @license Angular v13.2.0+17.sha-ab63ed3.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
