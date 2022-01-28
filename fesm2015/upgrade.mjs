7c341e6f606a9c9a79875af6339892b5f5ac9068
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 18:19:46 +0000

    9229511b1d build: bump version for `@angular/benchpress` to v0.2.2 (#44879)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1036.sha-c2e09e0.with-local-changes
+ * @license Angular v14.0.0-next.0+1037.sha-9229511.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
