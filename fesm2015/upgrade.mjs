607bb89862e633872f0b61f6b223c48adefaeea7
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 17:31:31 +0000

    94c9e47dbe Revert "fix(docs-infra): content overflow in mobile (#44851)" (#44899)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0+19.sha-6b7fffc.with-local-changes
+ * @license Angular v13.2.0+20.sha-94c9e47.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
