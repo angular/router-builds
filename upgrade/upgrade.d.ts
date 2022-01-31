4d02d4ff0bd7551531c578a4a273eb65cf0c9d77
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:27:00 +0000

    b51d111d72 docs: update Nx headline (#44910) PR Close #44910

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0+21.sha-47c02ca.with-local-changes
+ * @license Angular v13.2.0+25.sha-b51d111.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
