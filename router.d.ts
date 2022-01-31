4bed6a24aea97ad5ccd254d61588ca752e6c27eb
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 23:48:32 +0000

    db05ae13a6 refactor(compiler): remove parsing support for quote expressions (#44915)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1069.sha-f0cfa00.with-local-changes
+ * @license Angular v14.0.0-next.0+1070.sha-db05ae1.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
