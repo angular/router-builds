734a795077565c20a26c43173844dcf1c5d12d60
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:23:26 +0000

    390a653b7b build: update dependency d3 to v7 (#44892)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1048.sha-dea30d4.with-local-changes
+ * @license Angular v14.0.0-next.0+1049.sha-390a653.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
