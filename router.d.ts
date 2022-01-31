097a68acaac4b9913b026dbe29f3a2191b80f048
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 17:54:14 +0000

    10092f24fc build: fix formatting in zone.js BUILD file (#44912)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1042.sha-0650066.with-local-changes
+ * @license Angular v14.0.0-next.0+1043.sha-10092f2.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
