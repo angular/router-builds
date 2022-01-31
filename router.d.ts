27abc0a3242b55da4b126307785198a83cfa4818
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:50:18 +0000

    07185f4ed1 fix(compiler-cli): enable nullish coalescing check only with `strictNullChecks` (#44862)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0+25.sha-b51d111.with-local-changes
+ * @license Angular v13.2.0+28.sha-07185f4.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
