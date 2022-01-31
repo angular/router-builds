188e746abbdd621c4aaab639e02269006a117cf8
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:54:22 +0000

    6642e3c8fd perf(animations): remove no longer needed CssKeyframes classes (#44903)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1062.sha-5e099b5.with-local-changes
+ * @license Angular v14.0.0-next.0+1063.sha-6642e3c.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
