3605bcd7b2b94dd80bf0b1422d742799fa085d85
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:24:06 +0000

    3d07d1f772 fix(docs-infra): make it possible to see whole notification text on smaller screens (#44900)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1049.sha-390a653.with-local-changes
+ * @license Angular v14.0.0-next.0+1051.sha-3d07d1f.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
