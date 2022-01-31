4989b4682d7783ce7f5eb21af917017cbff01dd2
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:49:22 +0000

    5e099b5e6c docs: Update deployment docs for Vercel (#44904) PR Close #44904

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1061.sha-7155e9e.with-local-changes
+ * @license Angular v14.0.0-next.0+1062.sha-5e099b5.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
