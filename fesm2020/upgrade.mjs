df65c4de69f1ff6cb2dd48d55dae8f1fd45acad7
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 17:17:36 +0000

    d614a97af9 build: update legacy saucelabs bundle generation to account for esbuild update (#44830)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1038.sha-deb9531.with-local-changes
+ * @license Angular v14.0.0-next.0+1041.sha-d614a97.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
