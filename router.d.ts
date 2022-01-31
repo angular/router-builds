355f93a290732dd4cb014d69a6aa484db46f84ac
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:18:12 +0000

    a8686c64b4 build: update dependency @types/chrome to ^0.0.177 (#44889)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1046.sha-e62f010.with-local-changes
+ * @license Angular v14.0.0-next.0+1047.sha-a8686c6.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
