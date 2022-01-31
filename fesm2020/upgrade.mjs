61677ae1085fc4a777fd800b04a94d1c15d8dcdf
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:45:24 +0000

    7a81481fb2 perf(animations): Remove generic objects in favor of Maps (#44482)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1052.sha-8d9eb99.with-local-changes
+ * @license Angular v14.0.0-next.0+1054.sha-7a81481.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
