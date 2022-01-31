68eea4c2c1da842d61351bd05755ef7d80023b3f
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:55:53 +0000

    9eff8908af refactor(core): strict templates type-checking compatibility for perf tests (#44905)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1065.sha-889de82.with-local-changes
+ * @license Angular v14.0.0-next.0+1066.sha-9eff890.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
