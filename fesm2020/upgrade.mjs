dbf002d3863ce9498f2313af766f72b72764126f
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 21:45:09 +0000

    d41dedb85b fix(devtools): highlighter should only show the hovered component's name (#44863)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1079.sha-c89cf63.with-local-changes
+ * @license Angular v14.0.0-next.0+1081.sha-d41dedb.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
