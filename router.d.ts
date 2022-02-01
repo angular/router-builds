cebe697d7a474fa6ab7bd7288d91846ea89ff79f
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 18:38:24 +0000

    6829fc4a4c docs: add colon before introducing the list of items (#44751)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1073.sha-fdfcef5.with-local-changes
+ * @license Angular v14.0.0-next.0+1074.sha-6829fc4.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
