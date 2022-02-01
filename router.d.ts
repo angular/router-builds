b2621b777b4597b5432414dd6b9c409b24764b9e
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 18:44:30 +0000

    9f5edce0e9 docs: explain how `ngsw.json` is generated (#44823)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1074.sha-6829fc4.with-local-changes
+ * @license Angular v14.0.0-next.0+1076.sha-9f5edce.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
