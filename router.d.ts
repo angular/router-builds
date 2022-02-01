3e213153631a1ab5dd1df0080665dc95f8452b46
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 21:47:45 +0000

    812c1ba971 build: update all non-major dependencies (#44930)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0+48.sha-1cc1588.with-local-changes
+ * @license Angular v13.2.0+49.sha-812c1ba.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
