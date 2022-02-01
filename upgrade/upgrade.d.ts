27dbfd46881f41ddddcbc4f862e68ed1665f7320
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 00:33:13 +0000

    ed1732c268 refactor(core): the `RuntimeError` class should support more compact syntax (#44783)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1070.sha-db05ae1.with-local-changes
+ * @license Angular v14.0.0-next.0+1071.sha-ed1732c.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
