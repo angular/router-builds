b145a77d7e2fce67719bf50804a60cf53575a8c9
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 19:33:39 +0000

    5d7ffd5c0c docs: correct the demo code of NgZone.run (#44733)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1075.sha-e0a340e.with-local-changes
+ * @license Angular v14.0.0-next.0+1077.sha-5d7ffd5.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
