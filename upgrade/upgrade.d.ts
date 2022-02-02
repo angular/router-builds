897d253704e45bbc875b846fd8297e754f0e8d16
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-02 02:15:23 +0000

    cae1e44608 fix(forms): Update the typed forms migration to use FormArray<T> instead of FormArray<T[]>. (#44933)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1089.sha-1b56437.with-local-changes
+ * @license Angular v14.0.0-next.0+1090.sha-cae1e44.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
