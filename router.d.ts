2afcc0915fb2dc48c8147e7d0d18eb7e5d421edc
Dylan Hunn <dylhunn@gmail.com>
2022-02-02 23:53:33 +0000

    9cce12cbf5 refactor(compiler): add `@suppress {msgDescriptions}` if no description is present on an i18n message (#44787)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.1+3.sha-b9aab0c.with-local-changes
+ * @license Angular v13.2.1+4.sha-9cce12c.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
