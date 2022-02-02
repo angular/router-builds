b27f367a0d0236151c50a9f1c7d2d9620364f0b7
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-02 20:21:38 +0000

    a01bcb8e7e fix(platform-browser): do not run change detection when loading Hammer (#44921)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+1.sha-39b90b4.with-local-changes
+ * @license Angular v14.0.0-next.1+2.sha-a01bcb8.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
