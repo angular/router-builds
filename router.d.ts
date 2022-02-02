03d1327ddeef0ae3a03fd8ace2b9245a4ba20723
GitHub <noreply@github.com>
2022-02-02 20:08:54 +0000

    0c36f82134 release: cut the v14.0.0-next.1 release (#44951)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.0+1092.sha-38bb2be.with-local-changes
+ * @license Angular v14.0.0-next.0+1093.sha-0c36f82.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
