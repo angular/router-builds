306d941ff607480e016c5601994489c5c4f3f375
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-02 00:24:30 +0000

    4a5ad1793f fix(compiler-cli): ensure casing of logical paths is preserved (#44798)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.0+52.sha-5a75721.with-local-changes
+ * @license Angular v13.2.0+55.sha-4a5ad17.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
