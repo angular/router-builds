e17359402251d07703fd003c3ae89edeb7285b3e
Dylan Hunn <dylhunn@gmail.com>
2022-02-02 23:19:55 +0000

    eb3f06ae5f build: update pullapprove (#44954)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+4.sha-202a1a5.with-local-changes
+ * @license Angular v14.0.0-next.1+5.sha-eb3f06a.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
