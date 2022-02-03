ef0cc5201968eebcdf30d45e7922112a447dc92e
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 17:16:07 +0000

    0072eb48ba feat(compiler-cli): initial implementation of standalone components (#44812)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+9.sha-1aae414.with-local-changes
+ * @license Angular v14.0.0-next.1+12.sha-0072eb4.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
