39b2eb1ce543f430407c2187a4accecfbe62b5eb
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 20:58:42 +0000

    226fac67bd docs: describe your change... (#42899)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+18.sha-f7ea275.with-local-changes
+ * @license Angular v14.0.0-next.1+19.sha-226fac6.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
