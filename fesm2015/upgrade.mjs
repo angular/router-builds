286b006a64885b823700e784312d8ee9e744db0e
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 23:12:15 +0000

    1b216297f4 docs: update input-output-diagram.svg (#39900)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.1+13.sha-f092bad.with-local-changes
+ * @license Angular v13.2.1+15.sha-1b21629.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
