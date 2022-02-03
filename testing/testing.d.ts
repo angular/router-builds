b6204f9d18c9ef831fb56ac779a12e396a08d676
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 21:00:55 +0000

    0d19be1996 refactor(docs-infra): add types to all the aio examples buttons (#44557)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+19.sha-226fac6.with-local-changes
+ * @license Angular v14.0.0-next.1+22.sha-0d19be1.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
