c311e275b0476776874b1b17850c4e3bfa53dec3
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 17:10:32 +0000

    1aae414b36 docs: remove duplicate closing tags in View Encapsulation file (#44958)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+8.sha-077593f.with-local-changes
+ * @license Angular v14.0.0-next.1+9.sha-1aae414.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
