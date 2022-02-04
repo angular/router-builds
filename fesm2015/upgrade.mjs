a65924a806da4ef89394e4037e44460a6806010b
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 18:59:42 +0000

    38c03a2035 feat(common): support years greater than 9999 (#43622)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+32.sha-d183e7d.with-local-changes
+ * @license Angular v14.0.0-next.1+33.sha-38c03a2.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
