ff22dbe8f8cddfe8d393cad31a1344961cd87b97
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 18:58:58 +0000

    459cbed33b refactor(dev-infra): convert nullish coalescing operator to plain javascript (#44968)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+30.sha-8dd3f82.with-local-changes
+ * @license Angular v14.0.0-next.1+31.sha-459cbed.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
