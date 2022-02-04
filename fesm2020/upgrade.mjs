a0173ef6bd0cf91b96a3690865da6f447033979c
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 07:05:36 +0000

    b32647dc68 fix(platform-browser): Make transfer state key typesafe. (#23020)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+28.sha-64f837d.with-local-changes
+ * @license Angular v14.0.0-next.1+29.sha-b32647d.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
