0330bc05c8bfe1eef9c41a8a71642e035be07eca
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 18:59:13 +0000

    d183e7d736 docs: correct typo (#44793)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+31.sha-459cbed.with-local-changes
+ * @license Angular v14.0.0-next.1+32.sha-d183e7d.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
