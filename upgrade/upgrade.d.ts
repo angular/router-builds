77c770c46fb36300bd677a13912c8835928460e6
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 19:01:49 +0000

    30d1c34384 refactor(core): cleanup `DebugNode` and `DebugElement` implementations (#44270)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.1+16.sha-59a6ec4.with-local-changes
+ * @license Angular v13.2.1+19.sha-30d1c34.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
