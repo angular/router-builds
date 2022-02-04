df6633ddf7c555dae2d79774ea2571c796b53147
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 20:12:24 +0000

    cd97d8793f build: update all non-major dependencies (#44974)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.1+20.sha-3fae663.with-local-changes
+ * @license Angular v13.2.1+21.sha-cd97d87.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
