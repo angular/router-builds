6a6b86302e44422afd2e10f6819f25f73189c949
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 20:01:54 +0000

    3fae6637e7 perf(http): remove IE special status handling (#44354)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.1+19.sha-30d1c34.with-local-changes
+ * @license Angular v13.2.1+20.sha-3fae663.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
