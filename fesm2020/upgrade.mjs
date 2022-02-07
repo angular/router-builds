cfcd9b84f34aa55826b2ae17a2ea2de215d1c612
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 23:49:11 +0000

    d40ac2056e refactor(core): drop all `.ngfactory` and `.ngsummary` imports (#44957)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.1+27.sha-1368f58.with-local-changes
+ * @license Angular v13.2.1+28.sha-d40ac20.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
