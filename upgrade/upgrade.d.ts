0081cafb62b8401dd1536ec4ad557909e55ab615
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 18:03:13 +0000

    638a2d56dc ci: group all scorecard action dependencies (#44998)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+38.sha-4708cd0.with-local-changes
+ * @license Angular v14.0.0-next.1+39.sha-638a2d5.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
