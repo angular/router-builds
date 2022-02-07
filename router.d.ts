a8f4362a71f95c56d279dec4f29ab3115fb9cecf
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 23:48:51 +0000

    64acbc4242 refactor(core): drop all `.ngfactory` and `.ngsummary` imports (#44957)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+45.sha-d4da832.with-local-changes
+ * @license Angular v14.0.0-next.1+46.sha-64acbc4.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
