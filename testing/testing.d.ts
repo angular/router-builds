10216be83d6ee062e47c005f9004eeb94bfc3f3a
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 19:04:59 +0000

    db6cf7e7c1 fix(compiler): allow banana-in-a-box bindings to end with non-null assertion (#37809)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+39.sha-638a2d5.with-local-changes
+ * @license Angular v14.0.0-next.1+40.sha-db6cf7e.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
