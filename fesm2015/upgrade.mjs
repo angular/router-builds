3ea97d10ed3bea6dd49910c20838493f95371cf1
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 17:38:17 +0000

    2cda6cf700 build: update angular (#44981)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+35.sha-2839303.with-local-changes
+ * @license Angular v14.0.0-next.1+36.sha-2cda6cf.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
