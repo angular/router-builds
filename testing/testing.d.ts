3682040dab878517ed1a161343e9378660c71369
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 18:02:00 +0000

    4708cd0000 docs(docs-infra): fix cosidered typo (#44987)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.1+37.sha-33df840.with-local-changes
+ * @license Angular v14.0.0-next.1+38.sha-4708cd0.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
