c3536aac7d40400a8382cba12c9e25c07e5b2143
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 01:56:58 +0000

    28e943634a refactor: one more CHANGELOG fixup. (#45010)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.2+1.sha-4d63b80.with-local-changes
+ * @license Angular v14.0.0-next.2+2.sha-28e9436.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
