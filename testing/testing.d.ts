8c443d9c35fd57ce76f43b866fde7bc83d06632d
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 18:17:28 +0000

    835b326759 docs(forms): improve email validator directive docs (#45014)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.1+29.sha-7bc4dbd.with-local-changes
+ * @license Angular v13.2.2+5.sha-835b326.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
