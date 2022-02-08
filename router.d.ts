b48e4b1879d3e58d5624b225957f2872eb87b22e
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 18:14:21 +0000

    1b91e1049e perf(compiler): chain element start/end instructions (#44994)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.2+5.sha-635683a.with-local-changes
+ * @license Angular v14.0.0-next.2+7.sha-1b91e10.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
