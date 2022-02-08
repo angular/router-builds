d4978af396f9265fd560a97172b974b6f3aa51da
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 05:34:24 +0000

    7bc4dbd7ff docs: fix command to clear cache on Windows (#44988) PR Close #44988

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.1+28.sha-d40ac20.with-local-changes
+ * @license Angular v13.2.1+29.sha-7bc4dbd.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
