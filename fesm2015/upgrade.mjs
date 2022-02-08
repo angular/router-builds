897b4cf9ba404d8818893946c181932b30871bfa
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 19:32:57 +0000

    a599fa8d7f build: update dependency google-closure-compiler to v20220202 (#45013)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.2+10.sha-226cb08.with-local-changes
+ * @license Angular v14.0.0-next.2+11.sha-a599fa8.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
