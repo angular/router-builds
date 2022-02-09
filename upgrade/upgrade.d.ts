e1bf015641ba71bf176324bb92a939d70c78ed9e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-09 19:44:50 +0000

    5ac5967b2a build: update .pullapprove.yml (#45007)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.2+18.sha-7ea70c4.with-local-changes
+ * @license Angular v14.0.0-next.2+19.sha-5ac5967.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
