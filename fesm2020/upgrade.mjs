529b080b24919a8be6a3c16eac98ff3c087dde7f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-09 18:42:13 +0000

    7ea70c4292 docs: improve try-it-now card focus area (#45002)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.2+15.sha-b8f8e8c.with-local-changes
+ * @license Angular v14.0.0-next.2+18.sha-7ea70c4.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
