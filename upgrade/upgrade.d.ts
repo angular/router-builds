6c6118f721e3fca3b133fa5560ab58fdfac074fc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-09 20:03:38 +0000

    dd177e2c18 refactor(http): deprecated symbol used (#44985)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.2+11.sha-0b82086.with-local-changes
+ * @license Angular v13.2.2+12.sha-dd177e2.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
