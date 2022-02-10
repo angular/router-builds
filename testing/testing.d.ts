d748d581bb03e605f75ec363a3258c0e39522e21
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-10 17:33:34 +0000

    8793dbd8a2 build: update angular (#45011)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.2+21.sha-bbababe.with-local-changes
+ * @license Angular v14.0.0-next.2+22.sha-8793dbd.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
