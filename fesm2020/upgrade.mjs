aa19db423f73f1c6295d8532c850db40e6787bf5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-11 22:51:21 +0000

    a782be62e9 refactor(docs-infra): replace deprecated toPromise usages (#45045)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.2+13.sha-d8a1871.with-local-changes
+ * @license Angular v13.2.2+15.sha-a782be6.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
