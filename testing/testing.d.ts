20c63b4ee0a312e2e451dfe85998fc5a07ccb4ff
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-11 22:49:44 +0000

    b4157449c4 refactor(docs-infra): replace deprecated subscribe usages (#45046)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.2+22.sha-8793dbd.with-local-changes
+ * @license Angular v14.0.0-next.2+24.sha-b415744.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
