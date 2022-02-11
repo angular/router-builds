094eaf5740f207b50076aad248af05abc927ec99
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-11 22:50:37 +0000

    723032a79b build: update angular (#45049)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.2+25.sha-4443548.with-local-changes
+ * @license Angular v14.0.0-next.2+23.sha-723032a.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
