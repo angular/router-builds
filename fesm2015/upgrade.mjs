1dadd4e4eeae6fa1f226e085741f5cf07dd55371
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-15 20:48:28 +0000

    aeeb1b55d3 docs(localize): fix angular.json syntax error about i18n (#45048)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.2+22.sha-0050b01.with-local-changes
+ * @license Angular v13.2.2+24.sha-aeeb1b5.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
