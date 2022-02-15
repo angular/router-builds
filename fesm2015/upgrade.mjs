fd7dd177bd17967f6f8e0538aecedc9eace92d6b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-15 00:01:55 +0000

    e632fd1f0f test(animations): Fix e2e intermittent failures (#45083)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.2+15.sha-a782be6.with-local-changes
+ * @license Angular v13.2.2+21.sha-e632fd1.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
