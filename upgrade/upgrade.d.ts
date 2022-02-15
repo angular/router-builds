ab7cd76484b11f77835c080e2a737f41f585e4e4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-15 00:29:55 +0000

    0050b01b62 perf(animations): made errors in the animations package tree shakeable (#45079)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.2+21.sha-e632fd1.with-local-changes
+ * @license Angular v13.2.2+22.sha-0050b01.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
