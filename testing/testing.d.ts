fd17af326916e15b41dad458b1aab39ad0cf3b49
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-15 20:47:52 +0000

    f1d10baf08 docs(localize): fix angular.json syntax error about i18n (#45048)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.2+35.sha-1317242.with-local-changes
+ * @license Angular v14.0.0-next.2+36.sha-f1d10ba.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
