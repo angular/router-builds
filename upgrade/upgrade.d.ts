7aaec7e5ea978407f8d13387df8b6d610c139676
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-15 20:46:21 +0000

    13172421f9 docs: fix grammatical error (#45092)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.2+34.sha-787eaa7.with-local-changes
+ * @license Angular v14.0.0-next.2+35.sha-1317242.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
