1467d1871f3b220f6f31fb5dbfe2d344c77c43d2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-16 21:59:52 +0000

    80585f427b docs: fix typos (#45103)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.2+39.sha-df3a075.with-local-changes
+ * @license Angular v14.0.0-next.2+40.sha-80585f4.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
