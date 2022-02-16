af959745a670c8e68988cd218f43596d6ed7cf29
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-16 22:09:16 +0000

    782f678236 build: update scorecard action dependencies to c4e058a (#45036)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.2+24.sha-aeeb1b5.with-local-changes
+ * @license Angular v13.2.2+31.sha-782f678.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
