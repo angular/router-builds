7e90a5c4a7cdb9e763cce2cf483b91eb9a3ae550
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-16 22:06:01 +0000

    5c7c56bc85 perf(animations): improve algorithm to balance animation namespaces (#45057)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.2+43.sha-1eae65e.with-local-changes
+ * @license Angular v14.0.0-next.2+44.sha-5c7c56b.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
