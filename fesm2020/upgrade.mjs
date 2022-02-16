1bd36d6202ce139a43789913b59c7e10aa8cc8ff
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-16 21:54:27 +0000

    df3a075b02 docs: fix case types guide (#45095)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.2+36.sha-f1d10ba.with-local-changes
+ * @license Angular v14.0.0-next.2+39.sha-df3a075.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
