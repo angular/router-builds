c488beca651dd532dc9483b99cdb6a525a99d173
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-17 00:07:22 +0000

    54d09a6bc0 release: cut the v13.2.3 release (#45116)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.2+31.sha-782f678.with-local-changes
+ * @license Angular v13.2.2+32.sha-54d09a6.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
