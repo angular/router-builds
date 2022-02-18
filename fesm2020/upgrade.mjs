1229385ec039590cae1ead556db13dfc54e6f3d7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-18 21:49:29 +0000

    071c8af8ba perf(core): avoid storing LView in __ngContext__ (#45051)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.2+49.sha-4333662.with-local-changes
+ * @license Angular v14.0.0-next.3+1.sha-071c8af.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
