462bb1761df153a1e90744fc1c900bbc04ad673b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-18 21:59:08 +0000

    f28276c952 docs: replace deprecated `SwUpdate#available/activated` with new API (#44858)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.3+7.sha-702e196.with-local-changes
+ * @license Angular v14.0.0-next.3+11.sha-f28276c.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
