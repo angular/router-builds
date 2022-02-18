cdd99841d2124404c1c18260921d70e278caec6d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-18 21:51:07 +0000

    4ba95ea93a docs(forms): remove `type` duplicate from `button` in reactive-forms example (#45124)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.3+3.sha-32f83ab.with-local-changes
+ * @license Angular v14.0.0-next.3+5.sha-4ba95ea.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
