0f43d0730b1abfcab23dae35b01100855e3e345f
Andrew Scott <atscott01@gmail.com>
2022-02-22 23:52:11 +0000

    032276253f fix(docs-infra): removed duplicated style in aio examples (#45157)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.3+11.sha-d607d0b.with-local-changes
+ * @license Angular v13.2.3+12.sha-0322762.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
