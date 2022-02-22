ab7cc564acc8498dd560a5aa39ffe3e14010ad3c
Andrew Scott <atscott01@gmail.com>
2022-02-22 21:04:31 +0000

    d607d0b93c build: update ossf/scorecard-action commit hash to 3662744 (#45162)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.3+10.sha-c02e4d7.with-local-changes
+ * @license Angular v13.2.3+11.sha-d607d0b.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
