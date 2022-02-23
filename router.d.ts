a6f9bf2ea703add269ef9fcc97722613db387d7b
Andrew Scott <atscott01@gmail.com>
2022-02-23 17:15:26 +0000

    d65706a3b2 feat(zone.js): update electron patch to support electron/remote 14 (#45073)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.3+20.sha-f8dc660.with-local-changes
+ * @license Angular v14.0.0-next.3+19.sha-d65706a.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
