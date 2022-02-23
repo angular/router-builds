29f95119317831bbd8796d01068cffd24a2ce59d
GitHub <noreply@github.com>
2022-02-23 21:07:16 +0000

    ded96ca6cb release: cut the v14.0.0-next.4 release (#45184)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.3+22.sha-f58ad88.with-local-changes
+ * @license Angular v14.0.0-next.3+24.sha-ded96ca.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
