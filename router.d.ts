2395e665ad3d597b6a079883072adfe380086177
Andrew Scott <atscott01@gmail.com>
2022-02-23 00:07:31 +0000

    4fa530757b fix(docs-infra): removed duplicated style in aio examples (#45157)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.3+15.sha-ff6be32.with-local-changes
+ * @license Angular v14.0.0-next.3+18.sha-4fa5307.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
