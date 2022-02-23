38a2822c2fd081029bdec8d0e883860298f188d2
Andrew Scott <atscott01@gmail.com>
2022-02-23 17:59:26 +0000

    f58ad88504 test(common): remove initial navigation since it may redirect to debug.html (#45171)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.3+21.sha-75f20d8.with-local-changes
+ * @license Angular v14.0.0-next.3+22.sha-f58ad88.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
