3e0cdf0bf3688cbcbbacaa5975c3c6301790f3d1
Andrew Scott <atscott01@gmail.com>
2022-02-23 21:19:31 +0000

    2e7e46f1b6 ci: use renovate Github Action tag version pinning (#45178)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.3+23.sha-0c26c93.with-local-changes
+ * @license Angular v14.0.0-next.4+1.sha-2e7e46f.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
