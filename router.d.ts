d1207eaed12576fec904fd0ea3dd23364259ef48
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 19:01:38 +0000

    7fd416d060 fix(router): Fix type of Route.pathMatch to be more accurate (#45176)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.4+6.sha-909b21a.with-local-changes
+ * @license Angular v14.0.0-next.4+8.sha-7fd416d.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1953,7 +1953,7 @@ export declare interface Route {
      * to the redirect destination, creating an endless loop.
      *
      */
-    pathMatch?: string;
+    pathMatch?: 'prefix' | 'full';
     /**
      * A custom URL-matching function. Cannot be used together with `path`.
      */
