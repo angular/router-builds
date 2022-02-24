0c527a9217958fd9dd695f14bc3f8b9ad5994ca8
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 19:32:28 +0000

    b39db80f91 test(animations): add route animation delay to pages tests (#45144)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.4+6.sha-3b86dda.with-local-changes
+ * @license Angular v13.2.4+7.sha-b39db80.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
