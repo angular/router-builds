273bdd2d1dc91115ccbf6e87a659007192a2c8f4
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 21:33:34 +0000

    21a51fb6a4 test(animations): Fix flaky animations e2e test (#45196)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.4+7.sha-b39db80.with-local-changes
+ * @license Angular v13.2.4+8.sha-21a51fb.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
