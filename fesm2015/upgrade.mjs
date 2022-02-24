2265c0b55837a76c0f968664f52e3ae2b7fdb3c0
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 23:14:42 +0000

    c6e64aa8ad docs: fix style guide example app included as routing (#45148)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.4+9.sha-7379d75.with-local-changes
+ * @license Angular v13.2.4+10.sha-c6e64aa.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
