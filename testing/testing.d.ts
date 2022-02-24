eb471861555001e7f8ab048280521ed749e8e816
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 01:12:28 +0000

    bad509688b docs: remove activatedRouteData backward accesses in aio guides (#45140)

--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.4+1.sha-2e7e46f.with-local-changes
+ * @license Angular v14.0.0-next.4+2.sha-bad5096.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
