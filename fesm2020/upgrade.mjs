895cb291eadf4355d2746aa00b55b244fa8ed083
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 01:12:22 +0000

    4f1870362e docs: remove activatedRouteData backward accesses in aio guides (#45140)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.3+14.sha-e914da1.with-local-changes
+ * @license Angular v13.2.4+3.sha-4f18703.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
