d3dcb794a108574b5236343939f2a462fad127e9
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 21:59:39 +0000

    0debf84045 ci: allow Babel to be updated by Renovate (#44931)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.4+10.sha-8d634ac.with-local-changes
+ * @license Angular v14.0.0-next.4+12.sha-0debf84.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
