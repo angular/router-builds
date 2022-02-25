793df2dc5d2c1090f6fe11ee904eb1f870fcbcaa
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-25 00:09:47 +0000

    88f1168506 perf(core): only track LViews that are referenced in __ngContext__ (#45172)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.4+16.sha-0920104.with-local-changes
+ * @license Angular v14.0.0-next.4+17.sha-88f1168.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
