e60b92d97d07133306f00ebf2441e5d80caa4351
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-25 19:30:39 +0000

    e56472d3c2 ci: fix stamping for builds performed in CI (#45147)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.4+22.sha-e01820c.with-local-changes
+ * @license Angular v14.0.0-next.4+23.sha-e56472d.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
