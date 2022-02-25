c807b0ca1ab0a007c171d604231219934b170893
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-25 19:19:42 +0000

    63e9cc7cba ci: update aio payload size golden to reflect Angular update (#45117)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.4+18.sha-aae07c3.with-local-changes
+ * @license Angular v14.0.0-next.4+21.sha-63e9cc7.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
