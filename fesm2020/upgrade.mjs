e6fcd0dcbe331c05810aa4fd26696f49937fde31
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-28 17:56:07 +0000

    f4fd488d4d build: update tooling to use new entry-point for dev-infra exports (#45206)

--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.0.0-next.4+26.sha-4c80ca3.with-local-changes
+ * @license Angular v14.0.0-next.4+28.sha-f4fd488
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
