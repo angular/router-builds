9914a9f5fea1565d67c4865b16a8fc1784a0ef18
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-28 17:42:35 +0000

    84b4ad812b docs(docs-infra): fix the stackblitz on page i18n-example (#45097)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.4+16.sha-76c6dca.with-local-changes
+ * @license Angular v13.2.4+17.sha-84b4ad8.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
