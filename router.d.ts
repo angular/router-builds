0cce302bc40232119278b8a9215501597b31bad9
Jessica Janiuk <jessicajaniuk@google.com>
2022-03-01 18:56:22 +0000

    71ff12c1cc perf(core): make `LOCALE_ID` and other tokens from `ApplicationModule` tree-shakable (#45102) (#45222)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.4+18.sha-8907fbb.with-local-changes
+ * @license Angular v13.2.4+21.sha-71ff12c.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
