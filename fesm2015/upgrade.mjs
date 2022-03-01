42400855cb851c3ae793bae3f6f49c43d81b91f5
Jessica Janiuk <jessicajaniuk@google.com>
2022-03-01 19:33:12 +0000

    e8fd452bd2 fix(core): remove individual commands for updating gold files (#45198)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.4+21.sha-71ff12c.with-local-changes
+ * @license Angular v13.2.4+22.sha-e8fd452.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
