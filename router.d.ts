71d264e5649c1bb6e3467fac634f78508da7eb97
GitHub <noreply@github.com>
2022-03-02 18:35:08 +0000

    8bf3b53a1d release: cut the v13.2.5 release (#45241)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.4+23.sha-56ca7d3.with-local-changes
+ * @license Angular v13.2.4+24.sha-8bf3b53.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
