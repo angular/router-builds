f64f087406581456da4107cd1c3cd22cc8360f84
Jessica Janiuk <jessicajaniuk@google.com>
2022-03-02 01:19:52 +0000

    56ca7d385b perf(common): make `NgLocalization` token tree-shakable (#45118) (#45226)

--- upgrade/upgrade.d.ts
+++ upgrade/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.2.4+22.sha-e8fd452.with-local-changes
+ * @license Angular v13.2.4+23.sha-56ca7d3.with-local-changes
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
