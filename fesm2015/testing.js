a303ee8c19fd5ee95d0d22487182043802522145
Joey Perrott <josephperrott.github@gmail.com>
2022-03-23 22:29:55 +0000

    68901bf0f3 fix(docs-infra): prevent framing of AIO with X-Frame-Options

--- fesm2015/testing.js
+++ fesm2015/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.15+3.sha-b2a081a.with-local-changes
+ * @license Angular v12.2.16+1.sha-68901bf.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
