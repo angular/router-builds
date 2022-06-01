52e20b21f61b5870d91baefee96c43d62d12fb4e
Andrew Scott <atscott01@gmail.com>
2022-06-01 18:26:05 +0000

    94c2cfbe25 docs(router): Adjust example for scrollPositionRestoration (#46201)

--- router.d.ts
+++ router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.3.10+10.sha-ce2f149
+ * @license Angular v13.3.11+2.sha-94c2cfb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -801,21 +801,22 @@ export declare interface ExtraOptions {
      * in the following example.
      *
      * ```typescript
-     * class AppModule {
-     *   constructor(router: Router, viewportScroller: ViewportScroller) {
-     *     router.events.pipe(
-     *       filter((e: Event): e is Scroll => e instanceof Scroll)
+     * class AppComponent {
+     *   movieData: any;
+     *
+     *   constructor(private router: Router, private viewportScroller: ViewportScroller,
+     * changeDetectorRef: ChangeDetectorRef) {
+     *   router.events.pipe(filter((event: Event): event is Scroll => event instanceof Scroll)
      *     ).subscribe(e => {
-     *       if (e.position) {
-     *         // backward navigation
-     *         viewportScroller.scrollToPosition(e.position);
-     *       } else if (e.anchor) {
-     *         // anchor navigation
-     *         viewportScroller.scrollToAnchor(e.anchor);
-     *       } else {
-     *         // forward navigation
-     *         viewportScroller.scrollToPosition([0, 0]);
-     *       }
+     *       fetch('http://example.com/movies.json').then(response => {
+     *         this.movieData = response.json();
+     *         // update the template with the data before restoring scroll
+     *         changeDetectorRef.detectChanges();
+     *
+     *         if (e.position) {
+     *           viewportScroller.scrollToPosition(e.position);
+     *         }
+     *       });
      *     });
      *   }
      * }
--- testing/testing.d.ts
+++ testing/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.3.10+10.sha-ce2f149
+ * @license Angular v13.3.11+2.sha-94c2cfb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
