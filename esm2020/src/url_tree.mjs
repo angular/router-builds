494462afa2c20643e9ff4eb2c8665ce0d1e9f98a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-22 18:09:52 +0000

    876ba77bcd refactor(docs-infra): fix TypeScript warnings due to property types (#47201)

--- esm2020/src/url_tree.mjs
+++ esm2020/src/url_tree.mjs
@@ -268,9 +268,9 @@ export function mapChildrenIntoArray(segment, fn) {
  */
 export class UrlSerializer {
 }
-UrlSerializer.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.3.0-next.0+sha-b3879db", ngImport: i0, type: UrlSerializer, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
-UrlSerializer.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "14.3.0-next.0+sha-b3879db", ngImport: i0, type: UrlSerializer, providedIn: 'root', useFactory: () => new DefaultUrlSerializer() });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.3.0-next.0+sha-b3879db", ngImport: i0, type: UrlSerializer, decorators: [{
+UrlSerializer.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.3.0-next.0+sha-876ba77", ngImport: i0, type: UrlSerializer, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
+UrlSerializer.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "14.3.0-next.0+sha-876ba77", ngImport: i0, type: UrlSerializer, providedIn: 'root', useFactory: () => new DefaultUrlSerializer() });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.3.0-next.0+sha-876ba77", ngImport: i0, type: UrlSerializer, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root', useFactory: () => new DefaultUrlSerializer() }]
         }] });
--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.3.0-next.0+sha-b3879db
+ * @license Angular v14.3.0-next.0+sha-876ba77
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
