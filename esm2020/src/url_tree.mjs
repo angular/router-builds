8f1fdeac7bfd48952c8da0b5eab0152edaf41c16
Dylan Hunn <dylhunn@gmail.com>
2022-09-09 21:47:13 +0000

    4a13210ecd fix(forms): don't prevent default behavior for forms with method="dialog" (#47308)

--- esm2020/src/url_tree.mjs
+++ esm2020/src/url_tree.mjs
@@ -268,9 +268,9 @@ export function mapChildrenIntoArray(segment, fn) {
  */
 export class UrlSerializer {
 }
-UrlSerializer.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "15.0.0-next.1+sha-ca7bf65", ngImport: i0, type: UrlSerializer, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
-UrlSerializer.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "15.0.0-next.1+sha-ca7bf65", ngImport: i0, type: UrlSerializer, providedIn: 'root', useFactory: () => new DefaultUrlSerializer() });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "15.0.0-next.1+sha-ca7bf65", ngImport: i0, type: UrlSerializer, decorators: [{
+UrlSerializer.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "15.0.0-next.1+sha-4a13210", ngImport: i0, type: UrlSerializer, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
+UrlSerializer.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "15.0.0-next.1+sha-4a13210", ngImport: i0, type: UrlSerializer, providedIn: 'root', useFactory: () => new DefaultUrlSerializer() });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "15.0.0-next.1+sha-4a13210", ngImport: i0, type: UrlSerializer, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root', useFactory: () => new DefaultUrlSerializer() }]
         }] });
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.1+sha-ca7bf65
+ * @license Angular v15.0.0-next.1+sha-4a13210
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
