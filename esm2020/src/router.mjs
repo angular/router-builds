cbf97eca3331dff1c06671a6ea19d2016a86ecdd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-26 21:20:50 +0000

    2c3e2d6df0 build: fix rxjs 7 build failure (#47548)

--- esm2020/src/router.mjs
+++ esm2020/src/router.mjs
@@ -1020,9 +1020,9 @@ export class Router {
         return { navigationId };
     }
 }
-Router.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.2.3+sha-7e7769f", ngImport: i0, type: Router, deps: "invalid", target: i0.ɵɵFactoryTarget.Injectable });
-Router.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "14.2.3+sha-7e7769f", ngImport: i0, type: Router, providedIn: 'root', useFactory: setupRouter });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.2.3+sha-7e7769f", ngImport: i0, type: Router, decorators: [{
+Router.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.2.3+sha-2c3e2d6", ngImport: i0, type: Router, deps: "invalid", target: i0.ɵɵFactoryTarget.Injectable });
+Router.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "14.2.3+sha-2c3e2d6", ngImport: i0, type: Router, providedIn: 'root', useFactory: setupRouter });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.2.3+sha-2c3e2d6", ngImport: i0, type: Router, decorators: [{
             type: Injectable,
             args: [{
                     providedIn: 'root',
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.2.3+sha-7e7769f
+ * @license Angular v14.2.3+sha-2c3e2d6
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
