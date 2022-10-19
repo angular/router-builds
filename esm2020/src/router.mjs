066767fef3a5415fdc8f0468ac70bdf48bfd00c7
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 21:31:20 +0000

    79e9e8ab77 fix(router): Delay router scroll event until navigated components have rendered (#47563)

--- esm2020/src/router.mjs
+++ esm2020/src/router.mjs
@@ -1016,9 +1016,9 @@ export class Router {
         return { navigationId };
     }
 }
-Router.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "15.0.0-next.6+sha-716352c", ngImport: i0, type: Router, deps: "invalid", target: i0.ɵɵFactoryTarget.Injectable });
-Router.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "15.0.0-next.6+sha-716352c", ngImport: i0, type: Router, providedIn: 'root', useFactory: setupRouter });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "15.0.0-next.6+sha-716352c", ngImport: i0, type: Router, decorators: [{
+Router.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "15.0.0-next.6+sha-79e9e8a", ngImport: i0, type: Router, deps: "invalid", target: i0.ɵɵFactoryTarget.Injectable });
+Router.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "15.0.0-next.6+sha-79e9e8a", ngImport: i0, type: Router, providedIn: 'root', useFactory: setupRouter });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "15.0.0-next.6+sha-79e9e8a", ngImport: i0, type: Router, decorators: [{
             type: Injectable,
             args: [{
                     providedIn: 'root',
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0-next.6+sha-716352c
+ * @license Angular v15.0.0-next.6+sha-79e9e8a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
