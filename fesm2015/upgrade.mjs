e4af68aaabb136cb24e0f1de962a521f669c5144
Andrew Scott <atscott01@gmail.com>
2022-11-28 19:11:41 +0000

    afc668acd5 refactor(router): Move navigationId handling to the transition manager (#48202)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-fcf8572
+ * @license Angular v15.1.0-next.0+sha-afc668a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-fcf8572
+ * @license Angular v15.1.0-next.0+sha-afc668a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2602,8 +2602,10 @@ export declare class RouteConfigLoadStart {
  * @publicApi
  */
 export declare class Router {
+    private readonly urlSerializer;
+    private readonly rootContexts;
+    private readonly location;
     config: Routes;
-    private navigations;
     private disposed;
     private locationSubscription?;
     /**
@@ -2624,9 +2626,9 @@ export declare class Router {
     private console;
     private isNgZoneEnabled;
     /**
-     * An event stream for routing events in this NgModule.
+     * An event stream for routing events.
      */
-    readonly events: Observable<Event_2>;
+    get events(): Observable<Event_2>;
     /**
      * The current state of routing in this NgModule.
      */
@@ -2747,14 +2749,7 @@ export declare class Router {
      */
     constructor(
     /** @internal */
-    rootComponentType: Type<any> | null, 
-    /** @internal */
-    urlSerializer: UrlSerializer, 
-    /** @internal */
-    rootContexts: ChildrenOutletContexts, 
-    /** @internal */
-    location: Location_2, injector: Injector, compiler: Compiler, config: Routes);
-    private setTransition;
+    rootComponentType: Type<any> | null, urlSerializer: UrlSerializer, rootContexts: ChildrenOutletContexts, location: Location_2, injector: Injector, compiler: Compiler, config: Routes);
     /**
      * Sets up the location change listener and performs the initial navigation.
      */
@@ -2919,7 +2914,6 @@ export declare class Router {
      */
     isActive(url: string | UrlTree, matchOptions: IsActiveMatchOptions): boolean;
     private removeEmptyProps;
-    private processNavigations;
     private resetState;
     private resetUrlToCurrentUrlTree;
     private generateNgRouterState;
@@ -4003,7 +3997,7 @@ export declare interface UrlCreationOptions {
      *    constructor(private router: Router, private route: ActivatedRoute) {}
      *
      *    go() {
-     *      this.router.navigate(['../list'], { relativeTo: this.route });
+     *      router.navigate(['../list'], { relativeTo: this.route });
      *    }
      *  }
      * ```
@@ -4017,7 +4011,7 @@ export declare interface UrlCreationOptions {
      *
      * ```
      * // Navigate to /results?page=1
-     * this.router.navigate(['/results'], { queryParams: { page: 1 } });
+     * router.navigate(['/results'], { queryParams: { page: 1 } });
      * ```
      */
     queryParams?: Params | null;
@@ -4026,7 +4020,7 @@ export declare interface UrlCreationOptions {
      *
      * ```
      * // Navigate to /results#top
-     * this.router.navigate(['/results'], { fragment: 'top' });
+     * router.navigate(['/results'], { fragment: 'top' });
      * ```
      */
     fragment?: string;
@@ -4039,13 +4033,13 @@ export declare interface UrlCreationOptions {
      * The "preserve" option discards any new query params:
      * ```
      * // from /view1?page=1 to/view2?page=1
-     * this.router.navigate(['/view2'], { queryParams: { page: 2 },  queryParamsHandling: "preserve"
+     * router.navigate(['/view2'], { queryParams: { page: 2 },  queryParamsHandling: "preserve"
      * });
      * ```
      * The "merge" option appends new query params to the params from the current URL:
      * ```
      * // from /view1?page=1 to/view2?page=1&otherKey=2
-     * this.router.navigate(['/view2'], { queryParams: { otherKey: 2 },  queryParamsHandling: "merge"
+     * router.navigate(['/view2'], { queryParams: { otherKey: 2 },  queryParamsHandling: "merge"
      * });
      * ```
      * In case of a key collision between current parameters and those in the `queryParams` object,
@@ -4058,7 +4052,7 @@ export declare interface UrlCreationOptions {
      *
      * ```
      * // Preserve fragment from /results#top to /view#top
-     * this.router.navigate(['/view'], { preserveFragment: true });
+     * router.navigate(['/view'], { preserveFragment: true });
      * ```
      */
     preserveFragment?: boolean;
