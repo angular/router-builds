05746b0498f6d2532d1678779912dffd53b0b36d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-06 00:45:49 +0000

    8aa8b4b77c fix(forms): Form provider FormsModule.withConfig return a FormsModule (#48526)

--- esm2020/src/router.mjs
+++ esm2020/src/router.mjs
@@ -669,9 +669,9 @@ export class Router {
         return { navigationId };
     }
 }
-Router.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "15.1.0-rc.0+sha-61fc474", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
-Router.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "15.1.0-rc.0+sha-61fc474", ngImport: i0, type: Router, providedIn: 'root' });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "15.1.0-rc.0+sha-61fc474", ngImport: i0, type: Router, decorators: [{
+Router.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "15.1.0-rc.0+sha-8aa8b4b", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
+Router.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "15.1.0-rc.0+sha-8aa8b4b", ngImport: i0, type: Router, providedIn: 'root' });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "15.1.0-rc.0+sha-8aa8b4b", ngImport: i0, type: Router, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }], ctorParameters: function () { return []; } });
--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-rc.0+sha-61fc474
+ * @license Angular v15.1.0-rc.0+sha-8aa8b4b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
