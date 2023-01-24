e01385e776a2b0f339509ace377331d21633b907
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 21:26:53 +0000

    552cd3fb5c refactor: remove unnecessary test support check utilities (#47543)

--- esm2020/testing/src/router_testing_module.mjs
+++ esm2020/testing/src/router_testing_module.mjs
@@ -109,15 +109,15 @@ export class RouterTestingModule {
         };
     }
 }
-RouterTestingModule.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "15.1.1+sha-067c4ce", ngImport: i0, type: RouterTestingModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
-RouterTestingModule.ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "15.1.1+sha-067c4ce", ngImport: i0, type: RouterTestingModule, exports: [RouterModule] });
-RouterTestingModule.ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "15.1.1+sha-067c4ce", ngImport: i0, type: RouterTestingModule, providers: [
+RouterTestingModule.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "15.1.1+sha-552cd3f", ngImport: i0, type: RouterTestingModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
+RouterTestingModule.ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "15.1.1+sha-552cd3f", ngImport: i0, type: RouterTestingModule, exports: [RouterModule] });
+RouterTestingModule.ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "15.1.1+sha-552cd3f", ngImport: i0, type: RouterTestingModule, providers: [
         ROUTER_PROVIDERS,
         provideLocationMocks(),
         withPreloading(NoPreloading).ɵproviders,
         { provide: ROUTES, multi: true, useValue: [] },
     ], imports: [RouterModule] });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "15.1.1+sha-067c4ce", ngImport: i0, type: RouterTestingModule, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "15.1.1+sha-552cd3f", ngImport: i0, type: RouterTestingModule, decorators: [{
             type: NgModule,
             args: [{
                     exports: [RouterModule],
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.1+sha-067c4ce
+ * @license Angular v15.1.1+sha-552cd3f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
