98efc58b300fd51a2b85e2a9f4f795f60f5e48fd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-21 21:57:30 +0000

    7b4d3c036d build: update actions/cache digest to 69d9d44 (#49148)

--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-rc.0+sha-0950ef6
+ * @license Angular v15.2.0-rc.0+sha-7b4d3c0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.0-rc.0+sha-0950ef6
+ * @license Angular v15.2.0-rc.0+sha-7b4d3c0
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -576,7 +576,7 @@ export declare type CanLoadFn = (route: Route, segments: UrlSegment[]) => Observ
  * ```
  * class UserToken {}
  * class Permissions {
- *   canAccess(user: UserToken, id: string, segments: UrlSegment[]): boolean {
+ *   canAccess(user: UserToken, route: Route, segments: UrlSegment[]): boolean {
  *     return true;
  *   }
  * }
