5e039413d62e9fbf734faa1f28d6551210214ece
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-06 18:55:14 +0000

    10ffd03e35 ci: update integration test size (#49339)

--- esm2020/src/router_config_loader.mjs
+++ esm2020/src/router_config_loader.mjs
@@ -116,10 +116,10 @@ class RouterConfigLoader {
         }));
     }
 }
-RouterConfigLoader.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.0.0-next.1+sha-27379d0", ngImport: i0, type: RouterConfigLoader, deps: [{ token: i0.Injector }, { token: i0.Compiler }], target: i0.ɵɵFactoryTarget.Injectable });
-RouterConfigLoader.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.0.0-next.1+sha-27379d0", ngImport: i0, type: RouterConfigLoader, providedIn: 'root' });
+RouterConfigLoader.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.0.0-next.1+sha-10ffd03", ngImport: i0, type: RouterConfigLoader, deps: [{ token: i0.Injector }, { token: i0.Compiler }], target: i0.ɵɵFactoryTarget.Injectable });
+RouterConfigLoader.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.0.0-next.1+sha-10ffd03", ngImport: i0, type: RouterConfigLoader, providedIn: 'root' });
 export { RouterConfigLoader };
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.0.0-next.1+sha-27379d0", ngImport: i0, type: RouterConfigLoader, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.0.0-next.1+sha-10ffd03", ngImport: i0, type: RouterConfigLoader, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }], ctorParameters: function () { return [{ type: i0.Injector }, { type: i0.Compiler }]; } });
--- fesm2015/upgrade.mjs
+++ fesm2015/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.1+sha-27379d0
+ * @license Angular v16.0.0-next.1+sha-10ffd03
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
