e5dc6abbceb129c09137cbcf7706711588e2e7d1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-14 17:03:42 +0000

    e4516c75d2 docs(upgrade): mark lifecycle methods as nodoc (#49416) PR Close #49416

--- esm2020/src/router.mjs
+++ esm2020/src/router.mjs
@@ -665,9 +665,9 @@ export class Router {
         return { navigationId };
     }
 }
-Router.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "15.2.2+sha-6c189ed", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
-Router.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "15.2.2+sha-6c189ed", ngImport: i0, type: Router, providedIn: 'root' });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "15.2.2+sha-6c189ed", ngImport: i0, type: Router, decorators: [{
+Router.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "15.2.2+sha-e4516c7", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
+Router.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "15.2.2+sha-e4516c7", ngImport: i0, type: Router, providedIn: 'root' });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "15.2.2+sha-e4516c7", ngImport: i0, type: Router, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }], ctorParameters: function () { return []; } });
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.2+sha-6c189ed
+ * @license Angular v15.2.2+sha-e4516c7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
