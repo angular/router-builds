e2fc8d7c6b58673742f67592884a4e8b5ac18aa7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-17 07:51:12 +0000

    66fe827635 docs: Track and show request progress - insert updated link. (#49439)

--- esm2020/src/navigation_transition.mjs
+++ esm2020/src/navigation_transition.mjs
@@ -386,10 +386,10 @@ class NavigationTransitions {
         t.resolve(false);
     }
 }
-NavigationTransitions.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.0.0-next.3+sha-82102c1", ngImport: i0, type: NavigationTransitions, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
-NavigationTransitions.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.0.0-next.3+sha-82102c1", ngImport: i0, type: NavigationTransitions, providedIn: 'root' });
+NavigationTransitions.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.0.0-next.3+sha-66fe827", ngImport: i0, type: NavigationTransitions, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
+NavigationTransitions.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.0.0-next.3+sha-66fe827", ngImport: i0, type: NavigationTransitions, providedIn: 'root' });
 export { NavigationTransitions };
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.0.0-next.3+sha-82102c1", ngImport: i0, type: NavigationTransitions, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.0.0-next.3+sha-66fe827", ngImport: i0, type: NavigationTransitions, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }], ctorParameters: function () { return []; } });
--- fesm2020/upgrade.mjs
+++ fesm2020/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.3+sha-82102c1
+ * @license Angular v16.0.0-next.3+sha-66fe827
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
