674b4106de216c8c2653e33650347c3be5617ac0
Dylan Hunn <dylhunn@gmail.com>
2023-04-05 00:00:26 +0000

    3b863ddc1e refactor(core): Remove `ReflectiveInjector` symbol (#48103)

--- esm2022/src/router_config_loader.mjs
+++ esm2022/src/router_config_loader.mjs
@@ -110,11 +110,11 @@ class RouterConfigLoader {
             }
         }));
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.0.0-next.6+sha-34b2d34", ngImport: i0, type: RouterConfigLoader, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.0.0-next.6+sha-34b2d34", ngImport: i0, type: RouterConfigLoader, providedIn: 'root' }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.0.0-next.6+sha-3b863dd", ngImport: i0, type: RouterConfigLoader, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.0.0-next.6+sha-3b863dd", ngImport: i0, type: RouterConfigLoader, providedIn: 'root' }); }
 }
 export { RouterConfigLoader };
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.0.0-next.6+sha-34b2d34", ngImport: i0, type: RouterConfigLoader, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.0.0-next.6+sha-3b863dd", ngImport: i0, type: RouterConfigLoader, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }] });
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.6+sha-34b2d34
+ * @license Angular v16.0.0-next.6+sha-3b863dd
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
