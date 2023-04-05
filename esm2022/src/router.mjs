a745b493c7b91f35078c41bbe470c8ee9ec9e27c
Dylan Hunn <dylhunn@gmail.com>
2023-04-05 16:21:49 +0000

    136ffbca8e refactor(forms): Add a test that uses ControlConfig with a non-static validator. (#49693)

--- esm2022/src/router.mjs
+++ esm2022/src/router.mjs
@@ -704,11 +704,11 @@ class Router {
         }
         return { navigationId };
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.0.0-next.6+sha-3b863dd", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.0.0-next.6+sha-3b863dd", ngImport: i0, type: Router, providedIn: 'root' }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.0.0-next.6+sha-136ffbc", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.0.0-next.6+sha-136ffbc", ngImport: i0, type: Router, providedIn: 'root' }); }
 }
 export { Router };
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.0.0-next.6+sha-3b863dd", ngImport: i0, type: Router, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.0.0-next.6+sha-136ffbc", ngImport: i0, type: Router, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }], ctorParameters: function () { return []; } });
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-next.6+sha-3b863dd
+ * @license Angular v16.0.0-next.6+sha-136ffbc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
