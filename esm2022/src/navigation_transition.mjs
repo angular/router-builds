e4bf79d2a201c10c8b7c06d5c70af9e4f39981b4
Dylan Hunn <dylhunn@gmail.com>
2023-04-25 16:43:24 +0000

    02a539cb14 refactor(core): Synchronously emit the current signal value in toObservable (#49894)

--- esm2022/src/navigation_transition.mjs
+++ esm2022/src/navigation_transition.mjs
@@ -382,11 +382,11 @@ class NavigationTransitions {
         this.events.next(navCancel);
         t.resolve(false);
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.1.0-next.0+sha-5607e0f", ngImport: i0, type: NavigationTransitions, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.1.0-next.0+sha-5607e0f", ngImport: i0, type: NavigationTransitions, providedIn: 'root' }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.1.0-next.0+sha-02a539c", ngImport: i0, type: NavigationTransitions, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.1.0-next.0+sha-02a539c", ngImport: i0, type: NavigationTransitions, providedIn: 'root' }); }
 }
 export { NavigationTransitions };
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.1.0-next.0+sha-5607e0f", ngImport: i0, type: NavigationTransitions, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.1.0-next.0+sha-02a539c", ngImport: i0, type: NavigationTransitions, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }], ctorParameters: function () { return []; } });
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-5607e0f
+ * @license Angular v16.1.0-next.0+sha-02a539c
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
