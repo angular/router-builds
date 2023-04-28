c83910da0d81ce46f8e14e00b889b27f7beecfb4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-04-28 09:52:58 +0000

    ea82d3b0e9 ci: update link to start google presubmit (#50060)

--- esm2020/src/url_tree.mjs
+++ esm2020/src/url_tree.mjs
@@ -268,9 +268,9 @@ export function mapChildrenIntoArray(segment, fn) {
  */
 export class UrlSerializer {
 }
-UrlSerializer.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "15.2.8+sha-9107e93", ngImport: i0, type: UrlSerializer, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
-UrlSerializer.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "15.2.8+sha-9107e93", ngImport: i0, type: UrlSerializer, providedIn: 'root', useFactory: () => new DefaultUrlSerializer() });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "15.2.8+sha-9107e93", ngImport: i0, type: UrlSerializer, decorators: [{
+UrlSerializer.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "15.2.8+sha-ea82d3b", ngImport: i0, type: UrlSerializer, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
+UrlSerializer.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "15.2.8+sha-ea82d3b", ngImport: i0, type: UrlSerializer, providedIn: 'root', useFactory: () => new DefaultUrlSerializer() });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "15.2.8+sha-ea82d3b", ngImport: i0, type: UrlSerializer, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root', useFactory: () => new DefaultUrlSerializer() }]
         }] });
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.2.8+sha-9107e93
+ * @license Angular v15.2.8+sha-ea82d3b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
