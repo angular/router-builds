2a96632c4f9e91f1dcba6d6273cb25c73bb29efa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 17:05:04 +0000

    da7ff37dc9 Revert "docs(docs-infra): Remove unused annotation template (#50114)" (#50206)

--- esm2022/src/router_config_loader.mjs
+++ esm2022/src/router_config_loader.mjs
@@ -110,11 +110,11 @@ class RouterConfigLoader {
             }
         }));
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.0.0+sha-2f03eef", ngImport: i0, type: RouterConfigLoader, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.0.0+sha-2f03eef", ngImport: i0, type: RouterConfigLoader, providedIn: 'root' }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.0.0+sha-da7ff37", ngImport: i0, type: RouterConfigLoader, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.0.0+sha-da7ff37", ngImport: i0, type: RouterConfigLoader, providedIn: 'root' }); }
 }
 export { RouterConfigLoader };
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.0.0+sha-2f03eef", ngImport: i0, type: RouterConfigLoader, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.0.0+sha-da7ff37", ngImport: i0, type: RouterConfigLoader, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }] });
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0+sha-2f03eef
+ * @license Angular v16.0.0+sha-da7ff37
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
