9355d6d066bef541f650b9a238c2a00abf951846
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-09 22:12:16 +0000

    4e99439e3f refactor(router): remove private export of `withPreloading` (#50194)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-570114e
+ * @license Angular v16.1.0-next.0+sha-4e99439
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-570114e
+ * @license Angular v16.1.0-next.0+sha-4e99439
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4624,9 +4624,7 @@ export declare function withNavigationErrorHandler(fn: (error: NavigationError) 
  *
  * @publicApi
  */
-declare function withPreloading(preloadingStrategy: Type<PreloadingStrategy>): PreloadingFeature;
-export { withPreloading }
-export { withPreloading as ɵwithPreloading }
+export declare function withPreloading(preloadingStrategy: Type<PreloadingStrategy>): PreloadingFeature;
 
 /**
  * Allows to provide extra parameters to configure Router.
