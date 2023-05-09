61c2d7966b09e97f45f6947658835a5e092d455b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-09 22:07:51 +0000

    55dfe80700 docs: standalone component preloading config (#50193)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-822b307
+ * @license Angular v16.1.0-next.0+sha-55dfe80
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4624,7 +4624,9 @@ export declare function withNavigationErrorHandler(fn: (error: NavigationError) 
  *
  * @publicApi
  */
-export declare function withPreloading(preloadingStrategy: Type<PreloadingStrategy>): PreloadingFeature;
+declare function withPreloading(preloadingStrategy: Type<PreloadingStrategy>): PreloadingFeature;
+export { withPreloading }
+export { withPreloading as ɵwithPreloading }
 
 /**
  * Allows to provide extra parameters to configure Router.
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.0+sha-822b307
+ * @license Angular v16.1.0-next.0+sha-55dfe80
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
