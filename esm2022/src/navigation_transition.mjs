15ac76805a4514d7bbbc77b3ff4390908527b3e8
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-19 14:35:43 +0000

    b68306e616 build: update dependency google-closure-compiler to v20230502 (#50153)

--- esm2022/src/navigation_transition.mjs
+++ esm2022/src/navigation_transition.mjs
@@ -382,11 +382,11 @@ class NavigationTransitions {
         this.events.next(navCancel);
         t.resolve(false);
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.1.0-next.1+sha-87b8aba", ngImport: i0, type: NavigationTransitions, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.1.0-next.1+sha-87b8aba", ngImport: i0, type: NavigationTransitions, providedIn: 'root' }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.1.0-next.1+sha-b68306e", ngImport: i0, type: NavigationTransitions, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.1.0-next.1+sha-b68306e", ngImport: i0, type: NavigationTransitions, providedIn: 'root' }); }
 }
 export { NavigationTransitions };
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.1.0-next.1+sha-87b8aba", ngImport: i0, type: NavigationTransitions, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.1.0-next.1+sha-b68306e", ngImport: i0, type: NavigationTransitions, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }], ctorParameters: function () { return []; } });
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.0-next.1+sha-87b8aba
+ * @license Angular v16.1.0-next.1+sha-b68306e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
