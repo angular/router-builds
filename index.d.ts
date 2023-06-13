a21b99e720a1ec315b742a513ac5d4a9dbc9414b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 11:32:22 +0000

    05ac0868c9 fix(core): avoid duplicated content during hydration while processing a component with i18n (#50644)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.5+sha-50e1858
+ * @license Angular v16.0.5+sha-05ac086
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3171,7 +3171,7 @@ export declare abstract class RouteReuseStrategy {
  * class MyService {
  *   constructor(public router: Router) {
  *     router.events.pipe(
- *        filter((e: Event): e is RouterEvent => e instanceof RouterEvent)
+ *        filter((e: Event | RouterEvent): e is RouterEvent => e instanceof RouterEvent)
  *     ).subscribe((e: RouterEvent) => {
  *       // Do something
  *     });
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.5+sha-50e1858
+ * @license Angular v16.0.5+sha-05ac086
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
