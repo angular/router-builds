2dca083f761541f8b29a430176756dfbd4fc1df3
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-17 21:18:35 +0000

    3da03cacae docs: add mention of paramsInheritanceStrategy in the router doc (#51051)

--- esm2022/src/router.mjs
+++ esm2022/src/router.mjs
@@ -683,10 +683,10 @@ export class Router {
         }
         return { navigationId };
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.1.5+sha-dea8dc0", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.1.5+sha-dea8dc0", ngImport: i0, type: Router, providedIn: 'root' }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.1.5+sha-3da03ca", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.1.5+sha-3da03ca", ngImport: i0, type: Router, providedIn: 'root' }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.1.5+sha-dea8dc0", ngImport: i0, type: Router, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.1.5+sha-3da03ca", ngImport: i0, type: Router, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }], ctorParameters: function () { return []; } });
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.1.5+sha-dea8dc0
+ * @license Angular v16.1.5+sha-3da03ca
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
