87cdbe4f877730e457a4848934b0e958a659ffa5
Dylan Hunn <dylhunn@gmail.com>
2023-08-08 17:58:52 +0000

    899e51ad8e docs: Instead of CanActivateFn, CanActivateChildFn appears in CanActivateFn docs (#51283)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-rc.0+sha-0ba2b5e
+ * @license Angular v16.2.0-rc.0+sha-899e51a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -397,7 +397,7 @@ export declare type CanActivateChildFn = (childRoute: ActivatedRouteSnapshot, st
  * navigation is cancelled. If any guard returns a `UrlTree`, the current navigation
  * is cancelled and a new navigation begins to the `UrlTree` returned from the guard.
  *
- * The following example implements and uses a `CanActivateChildFn` that checks whether the
+ * The following example implements and uses a `CanActivateFn` that checks whether the
  * current user has permission to activate the requested route.
  *
  * {@example router/route_functional_guards.ts region="CanActivateFn"}
