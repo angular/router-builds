35c62395dad8a259d4dc971bbb0249379d4c6299
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-14 21:51:24 +0000

    3b1c1b91a2 docs: Add Missing SSR integrity marker error doc page (#51340)

--- esm2022/src/router.mjs
+++ esm2022/src/router.mjs
@@ -683,10 +683,10 @@ export class Router {
         }
         return { navigationId };
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "17.0.0-next.0+sha-5a2b768", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "17.0.0-next.0+sha-5a2b768", ngImport: i0, type: Router, providedIn: 'root' }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "17.0.0-next.0+sha-3b1c1b9", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "17.0.0-next.0+sha-3b1c1b9", ngImport: i0, type: Router, providedIn: 'root' }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "17.0.0-next.0+sha-5a2b768", ngImport: i0, type: Router, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "17.0.0-next.0+sha-3b1c1b9", ngImport: i0, type: Router, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }], ctorParameters: function () { return []; } });
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.0+sha-5a2b768
+ * @license Angular v17.0.0-next.0+sha-3b1c1b9
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
