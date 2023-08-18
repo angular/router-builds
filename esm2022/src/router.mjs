b69226e6e42f3e77a996e249ed6e0173861caba2
Andrew Kushnir <akushnir@google.com>
2023-08-18 15:12:19 +0000

    39ace8664b fix(compiler-cli): enforce a minimum version to be used when a library uses input transform (#51413)

--- esm2022/src/router.mjs
+++ esm2022/src/router.mjs
@@ -683,10 +683,10 @@ export class Router {
         }
         return { navigationId };
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.2.1+sha-cf463a6", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.2.1+sha-cf463a6", ngImport: i0, type: Router, providedIn: 'root' }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.2.1+sha-39ace86", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.2.1+sha-39ace86", ngImport: i0, type: Router, providedIn: 'root' }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.1+sha-cf463a6", ngImport: i0, type: Router, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.1+sha-39ace86", ngImport: i0, type: Router, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }], ctorParameters: function () { return []; } });
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.1+sha-cf463a6
+ * @license Angular v16.2.1+sha-39ace86
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
