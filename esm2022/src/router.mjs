c1a6405d36d30fe0d7c2afd17248dd2990824612
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-01 16:29:00 +0000

    8ce22ebae3 docs: fix missing information in createComponent (#51493)

--- esm2022/src/router.mjs
+++ esm2022/src/router.mjs
@@ -784,10 +784,10 @@ export class Router {
         }
         return { navigationId };
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.2.3+sha-af9cd84", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.2.3+sha-af9cd84", ngImport: i0, type: Router, providedIn: 'root' }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.2.3+sha-8ce22eb", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.2.3+sha-8ce22eb", ngImport: i0, type: Router, providedIn: 'root' }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.3+sha-af9cd84", ngImport: i0, type: Router, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.3+sha-8ce22eb", ngImport: i0, type: Router, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }], ctorParameters: function () { return []; } });
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.3+sha-af9cd84
+ * @license Angular v16.2.3+sha-8ce22eb
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
