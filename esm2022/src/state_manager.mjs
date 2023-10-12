b755b62a429966f04cb9c08ab92b4eed004ad38e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-12 13:52:02 +0000

    65b4604 fix(common): missing space in ngSwitch equality warning (#52180)

--- esm2022/src/state_manager.mjs
+++ esm2022/src/state_manager.mjs
@@ -213,10 +213,10 @@ export class StateManager {
         }
         return { navigationId };
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "17.0.0-next.8+sha-2e5ee1c", ngImport: i0, type: StateManager, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "17.0.0-next.8+sha-2e5ee1c", ngImport: i0, type: StateManager, providedIn: 'root' }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "17.0.0-next.8+sha-65b4604", ngImport: i0, type: StateManager, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "17.0.0-next.8+sha-65b4604", ngImport: i0, type: StateManager, providedIn: 'root' }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "17.0.0-next.8+sha-2e5ee1c", ngImport: i0, type: StateManager, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "17.0.0-next.8+sha-65b4604", ngImport: i0, type: StateManager, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }] });
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.0-next.8+sha-2e5ee1c
+ * @license Angular v17.0.0-next.8+sha-65b4604
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
