0fa3fc69defe8e225b611c73a28de9e3bf4852cc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 19:40:33 +0000

    5ff99f2f9e docs: add warning for localhost proxy (#51948) PR Close #51948

--- esm2022/src/router.mjs
+++ esm2022/src/router.mjs
@@ -784,10 +784,10 @@ export class Router {
         }
         return { navigationId };
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.2.11+sha-38b4602", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.2.11+sha-38b4602", ngImport: i0, type: Router, providedIn: 'root' }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.2.11+sha-5ff99f2", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "16.2.11+sha-5ff99f2", ngImport: i0, type: Router, providedIn: 'root' }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.11+sha-38b4602", ngImport: i0, type: Router, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.11+sha-5ff99f2", ngImport: i0, type: Router, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }], ctorParameters: function () { return []; } });
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.11+sha-38b4602
+ * @license Angular v16.2.11+sha-5ff99f2
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
