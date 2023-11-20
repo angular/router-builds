b4fc47e0b564f1c33fed99d7f32bd17cdb939cd8
Andrew Kushnir <akushnir@google.com>
2023-11-20 20:13:35 +0000

    91486aa fix(router): Resolvers in different parts of the route tree should be able to execute together (#52934)

--- esm2022/src/router_config_loader.mjs
+++ esm2022/src/router_config_loader.mjs
@@ -74,10 +74,10 @@ export class RouterConfigLoader {
         this.childrenLoaders.set(route, loader);
         return loader;
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "17.0.4+sha-710a24a", ngImport: i0, type: RouterConfigLoader, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "17.0.4+sha-710a24a", ngImport: i0, type: RouterConfigLoader, providedIn: 'root' }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "17.0.4+sha-91486aa", ngImport: i0, type: RouterConfigLoader, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "17.0.4+sha-91486aa", ngImport: i0, type: RouterConfigLoader, providedIn: 'root' }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "17.0.4+sha-710a24a", ngImport: i0, type: RouterConfigLoader, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "17.0.4+sha-91486aa", ngImport: i0, type: RouterConfigLoader, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }] });
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.0.4+sha-710a24a
+ * @license Angular v17.0.4+sha-91486aa
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
