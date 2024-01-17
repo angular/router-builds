5c57f39510832c5a02eb0d5d351d1ef0ca2ea262
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 19:42:28 +0000

    18fa6d7 refactor(compiler-cli): interpolatedSignalNotInvoked diagnostic for input signals (#53883)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-rc.0+sha-9c00506
+ * @license Angular v17.1.0-rc.0+sha-18fa6d7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -115,9 +115,7 @@ function setUpLocationSync(ngUpgrade, urlType = 'path') {
  */
 let anchor;
 function resolveUrl(url) {
-    if (!anchor) {
-        anchor = document.createElement('a');
-    }
+    anchor ??= document.createElement('a');
     anchor.setAttribute('href', url);
     anchor.setAttribute('href', anchor.href);
     return {
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-rc.0+sha-9c00506
+ * @license Angular v17.1.0-rc.0+sha-18fa6d7
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
