97eea0b17c3f676b8dd0f398b87569bcac61607a
Andrew Scott <atscott@google.com>
2024-04-08 18:41:22 +0000

    4d2a800 docs(docs-infra): move CLI docs to ADEV (#55242)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,6 +1,6 @@
 /**
- * @license Angular v17.3.3+sha-80d1094
- * (c) 2010-2022 Google LLC. https://angular.io/
+ * @license Angular v17.3.3+sha-4d2a800
+ * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
 
