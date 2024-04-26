d2c2754d5eec0cbe12e843e12021b161419269eb
Andrew Kushnir <akushnir@google.com>
2024-04-26 16:39:21 +0000

    51ac883 fix(compiler-cli): don't type check the bodies of control flow nodes in basic mode (#55558)

--- esm2022/src/router.mjs
+++ esm2022/src/router.mjs
@@ -557,10 +557,10 @@ export class Router {
             return Promise.reject(e);
         });
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "17.3.6+sha-507d83d", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "17.3.6+sha-507d83d", ngImport: i0, type: Router, providedIn: 'root' }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "17.3.6+sha-51ac883", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "17.3.6+sha-51ac883", ngImport: i0, type: Router, providedIn: 'root' }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "17.3.6+sha-507d83d", ngImport: i0, type: Router, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "17.3.6+sha-51ac883", ngImport: i0, type: Router, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }], ctorParameters: () => [] });
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.3.6+sha-507d83d
+ * @license Angular v17.3.6+sha-51ac883
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
