5236cda05a9d51e5c3e50d135e1310844a975d3e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-06-12 20:18:31 +0000

    995b7fc build: update babel dependencies to v7.24.7 (#56373)

--- esm2022/src/router.mjs
+++ esm2022/src/router.mjs
@@ -554,10 +554,10 @@ export class Router {
             return Promise.reject(e);
         });
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "18.0.2+sha-e49a81b", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "18.0.2+sha-e49a81b", ngImport: i0, type: Router, providedIn: 'root' }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "18.0.2+sha-995b7fc", ngImport: i0, type: Router, deps: [], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "18.0.2+sha-995b7fc", ngImport: i0, type: Router, providedIn: 'root' }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "18.0.2+sha-e49a81b", ngImport: i0, type: Router, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "18.0.2+sha-995b7fc", ngImport: i0, type: Router, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }], ctorParameters: () => [] });
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.2+sha-e49a81b
+ * @license Angular v18.0.2+sha-995b7fc
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
