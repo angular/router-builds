53c9030638bc5dd58a5674c1d1ab89ad799350aa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-23 17:21:31 +0000

    b80af11 refactor(core): restructure `AfterRenderManager` to connect related phases (#57453)

--- esm2022/src/router_outlet_context.mjs
+++ esm2022/src/router_outlet_context.mjs
@@ -80,10 +80,10 @@ export class ChildrenOutletContexts {
     getContext(childName) {
         return this.contexts.get(childName) || null;
     }
-    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "19.0.0-next.1+sha-4e803ae", ngImport: i0, type: ChildrenOutletContexts, deps: [{ token: i0.EnvironmentInjector }], target: i0.ɵɵFactoryTarget.Injectable }); }
-    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "19.0.0-next.1+sha-4e803ae", ngImport: i0, type: ChildrenOutletContexts, providedIn: 'root' }); }
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "19.0.0-next.1+sha-b80af11", ngImport: i0, type: ChildrenOutletContexts, deps: [{ token: i0.EnvironmentInjector }], target: i0.ɵɵFactoryTarget.Injectable }); }
+    static { this.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "19.0.0-next.1+sha-b80af11", ngImport: i0, type: ChildrenOutletContexts, providedIn: 'root' }); }
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "19.0.0-next.1+sha-4e803ae", ngImport: i0, type: ChildrenOutletContexts, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "19.0.0-next.1+sha-b80af11", ngImport: i0, type: ChildrenOutletContexts, decorators: [{
             type: Injectable,
             args: [{ providedIn: 'root' }]
         }], ctorParameters: () => [{ type: i0.EnvironmentInjector }] });
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.1+sha-4e803ae
+ * @license Angular v19.0.0-next.1+sha-b80af11
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
