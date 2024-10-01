1f887ef0138ce55d1acbc94debd7e98cf5c75ea6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 08:17:15 +0000

    2ece9df docs(router): Document missing input for route binding (#57697)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.7+sha-7bfe62a
+ * @license Angular v19.0.0-next.7+sha-2ece9df
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.7+sha-7bfe62a
+ * @license Angular v19.0.0-next.7+sha-2ece9df
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4679,6 +4679,24 @@ export declare interface ViewTransitionsFeatureOptions {
  * );
  * ```
  *
+ * The router bindings information from any of the following sources:
+ *
+ *  - query parameters
+ *  - path and matrix parameters
+ *  - static route data
+ *  - data from resolvers
+ *
+ * Duplicate keys are resolved in the same order from above, from least to greatest,
+ * meaning that resolvers have the highest precedence and override any of the other information
+ * from the route.
+ *
+ * Importantly, when an input does not have an item in the route data with a matching key, this
+ * input is set to `undefined`. This prevents previous information from being
+ * retained if the data got removed from the route (i.e. if a query parameter is removed).
+ * Default values can be provided with a resolver on the route to ensure the value is always present
+ * or an input and use an input transform in the component.
+ *
+ * @see {@link guide/components/inputs#input-transforms input transforms}
  * @returns A set of providers for use with `provideRouter`.
  */
 export declare function withComponentInputBinding(): ComponentInputBindingFeature;
