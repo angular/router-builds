0bf6d8aa988acfb82a4f20ca2e7235c27f018014
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-01 18:03:40 +0000

    b927ce8 refactor(common): update AsyncPipe to explicitly accept PromiseLike only (#60551)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-eb97303
+ * @license Angular v20.0.0-next.4+sha-b927ce8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-eb97303');
+const VERSION = new Version('20.0.0-next.4+sha-b927ce8');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-eb97303
+ * @license Angular v20.0.0-next.4+sha-b927ce8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
