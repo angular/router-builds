5cb04c9bfd60ab1132b396fbf6bca453db8ac886
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-01 18:22:43 +0000

    b55f4de ci: update to latest dev-infra action versions (#60675)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-b927ce8
+ * @license Angular v20.0.0-next.4+sha-b55f4de
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-b927ce8');
+const VERSION = new Version('20.0.0-next.4+sha-b55f4de');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-b927ce8
+ * @license Angular v20.0.0-next.4+sha-b55f4de
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
