09524847cd562fb3ba942fe78ce32262049a6da5
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 15:22:11 +0000

    6acce7c fix(core): fixes #592882 ng generate @angular/core:signal-queries-migration (#60688)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-59d40f2
+ * @license Angular v20.0.0-next.4+sha-6acce7c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-59d40f2');
+const VERSION = new Version('20.0.0-next.4+sha-6acce7c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
