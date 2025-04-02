eb12f8efd11a6fec9ecc8b9761456f2fffc1734e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 14:06:36 +0000

    0c5b697 docs: add a note about styles collision resolution (#59266) PR Close #59266

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.4+sha-f90dc69
+ * @license Angular v19.2.4+sha-0c5b697
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.4+sha-f90dc69');
+const VERSION = new Version('19.2.4+sha-0c5b697');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.4+sha-f90dc69
+ * @license Angular v19.2.4+sha-0c5b697
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
