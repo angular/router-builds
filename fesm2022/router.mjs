fdb9489d09fafc3a18e45dc497edd4cc1d6b68db
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 18:33:43 +0000

    1c7b356 fix(core): release `hasPendingTasks` observers (#59723)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-0bb4bd6
+ * @license Angular v20.0.0-next.4+sha-1c7b356
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-0bb4bd6');
+const VERSION = new Version('20.0.0-next.4+sha-1c7b356');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
