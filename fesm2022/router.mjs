30cac4e94547f77cd191b00a4dd31d459b919b3d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 16:26:46 +0000

    4f86f3f docs(docs-infra): fix algolia icon in the search dialog on safari (#60702)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-5f7f046
+ * @license Angular v20.0.0-next.4+sha-4f86f3f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-5f7f046');
+const VERSION = new Version('20.0.0-next.4+sha-4f86f3f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-5f7f046
+ * @license Angular v20.0.0-next.4+sha-4f86f3f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
