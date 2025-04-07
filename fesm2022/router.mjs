4a646637b21263e6232db511fd06781a0e63d934
Andrew Scott <atscott@google.com>
2025-04-07 20:40:43 +0000

    6fa8d44 build: update io_bazel_rules_sass digest to b4800f3 (#60737)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-3d85d93
+ * @license Angular v20.0.0-next.5+sha-6fa8d44
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-3d85d93');
+const VERSION = new Version('20.0.0-next.5+sha-6fa8d44');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
