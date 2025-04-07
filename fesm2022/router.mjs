d96c36ea48291bba747d86837f95595696c00ab1
Andrew Scott <atscott@google.com>
2025-04-07 16:33:24 +0000

    00bbd9b fix(core): fix docs for output migration (#60764)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.5+sha-e9a719e
+ * @license Angular v19.2.5+sha-00bbd9b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.5+sha-e9a719e');
+const VERSION = new Version('19.2.5+sha-00bbd9b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.5+sha-e9a719e
+ * @license Angular v19.2.5+sha-00bbd9b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
