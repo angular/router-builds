0e11b5bb2e0ce8da700b65fd287afcd245bd992a
Andrew Scott <atscott@google.com>
2025-04-08 16:27:18 +0000

    80a3258 docs: Update guide link in http package md file (#59955) PR Close #59955

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-ba79f23
+ * @license Angular v20.0.0-next.5+sha-80a3258
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-ba79f23');
+const VERSION = new Version('20.0.0-next.5+sha-80a3258');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-ba79f23
+ * @license Angular v20.0.0-next.5+sha-80a3258
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
