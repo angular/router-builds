e1ac014128de5b76e906d756685c252f97fa08e8
Andrew Scott <atscott@google.com>
2025-04-08 23:33:58 +0000

    ae8a4c4 build: update github/codeql-action action to v3.28.15 (#60780)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.5+sha-9241615
+ * @license Angular v19.2.5+sha-ae8a4c4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.5+sha-9241615');
+const VERSION = new Version('19.2.5+sha-ae8a4c4');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
