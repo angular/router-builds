52622364a5893d862dc3f39e720482372e35d0fd
Andrew Scott <atscott@google.com>
2025-04-09 16:39:45 +0000

    e50f8a2 Revert "refactor(core): use stream in rxResource instead of loader (#59910)" (#60803)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-2b4fd9b
+ * @license Angular v20.0.0-next.5+sha-e50f8a2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-2b4fd9b');
+const VERSION = new Version('20.0.0-next.5+sha-e50f8a2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-2b4fd9b
+ * @license Angular v20.0.0-next.5+sha-e50f8a2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
