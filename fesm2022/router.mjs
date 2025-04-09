69b6fc482d75fffa8fe048e498673bd1eaeeccd5
Andrew Scott <atscott01@gmail.com>
2025-04-09 20:08:56 +0000

    ea58402 release: cut the v20.0.0-next.6 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-c3113b9
+ * @license Angular v20.0.0-next.6+sha-ea58402
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-c3113b9');
+const VERSION = new Version('20.0.0-next.6+sha-ea58402');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-BZ-UA-h1.d.ts
+++ router_module.d-BZ-UA-h1.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-c3113b9
+ * @license Angular v20.0.0-next.6+sha-ea58402
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
