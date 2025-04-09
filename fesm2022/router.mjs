abe616c4920a05c231f857d8d8089ad92c0cbfe8
Andrew Scott <atscott01@gmail.com>
2025-04-09 19:36:59 +0000

    c3113b9 docs: release notes for the v19.2.6 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-8744c9a
+ * @license Angular v20.0.0-next.5+sha-c3113b9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-8744c9a');
+const VERSION = new Version('20.0.0-next.5+sha-c3113b9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
