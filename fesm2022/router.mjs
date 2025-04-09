1378560be7e194adf73f84fed1d755a0cdb09635
Andrew Scott <atscott@google.com>
2025-04-09 21:33:25 +0000

    d39e09d fix(http): Include HTTP status code and headers when HTTP requests errored in `httpResource` (#60802)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-54f9269
+ * @license Angular v19.2.6+sha-d39e09d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.6+sha-54f9269');
+const VERSION = new Version('19.2.6+sha-d39e09d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
