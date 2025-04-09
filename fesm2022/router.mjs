c9f7456d90773e1106cd3b1a3a0af53156ca2ffe
Andrew Scott <atscott@google.com>
2025-04-09 16:23:54 +0000

    2b4fd9b refactor(core): allow multiple DI profilers (#60562) (#60562)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-df42976
+ * @license Angular v20.0.0-next.5+sha-2b4fd9b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-df42976');
+const VERSION = new Version('20.0.0-next.5+sha-2b4fd9b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-df42976
+ * @license Angular v20.0.0-next.5+sha-2b4fd9b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
