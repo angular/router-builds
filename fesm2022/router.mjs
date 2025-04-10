22e89a680442a4652f074b0b66233efaa0a77ed4
kirjs <kirjs@users.noreply.github.com>
2025-04-10 17:39:55 +0000

    b144126 fix(core): inject migration: replace param with this. (#60713)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-77c6041
+ * @license Angular v19.2.6+sha-b144126
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.6+sha-77c6041');
+const VERSION = new Version('19.2.6+sha-b144126');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-77c6041
+ * @license Angular v19.2.6+sha-b144126
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
