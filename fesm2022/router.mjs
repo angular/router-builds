f5069d971b0ebd4cc5bd0165f4a9e0b526230047
kirjs <kirjs@users.noreply.github.com>
2025-04-10 16:00:50 +0000

    5c575a8 build: ensure material dependencies are properly linked for devtools (#60822)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-6aa0bbd
+ * @license Angular v20.0.0-next.6+sha-5c575a8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-6aa0bbd');
+const VERSION = new Version('20.0.0-next.6+sha-5c575a8');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-BZ-UA-h1.d.ts
+++ router_module.d-BZ-UA-h1.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-6aa0bbd
+ * @license Angular v20.0.0-next.6+sha-5c575a8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
