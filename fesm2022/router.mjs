e4e9115019ae04de49750a11f7c2ec0f90f143cd
kirjs <kirjs@users.noreply.github.com>
2025-04-11 14:26:12 +0000

    1522e6c build: update cross-repo angular dependencies (#60835)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-7b8c2fa
+ * @license Angular v20.0.0-next.6+sha-1522e6c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-7b8c2fa');
+const VERSION = new Version('20.0.0-next.6+sha-1522e6c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-7b8c2fa
+ * @license Angular v20.0.0-next.6+sha-1522e6c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
