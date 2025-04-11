1bff4f831a9cda65bc15d2b0eaa7ce14d6e038bc
kirjs <kirjs@users.noreply.github.com>
2025-04-11 21:26:21 +0000

    c0ef923 docs: Restructure example build rules (#60778)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-72dd133
+ * @license Angular v20.0.0-next.6+sha-c0ef923
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-72dd133');
+const VERSION = new Version('20.0.0-next.6+sha-c0ef923');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
