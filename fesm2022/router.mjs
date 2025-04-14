7e489f8655c73cf680dae021157568efce861425
kirjs <kirjs@users.noreply.github.com>
2025-04-14 15:08:24 +0000

    c80f24d docs: fix missing card-title tag in example of fallback-content section (#60855)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-c0e4ff1
+ * @license Angular v19.2.6+sha-c80f24d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.6+sha-c0e4ff1');
+const VERSION = new Version('19.2.6+sha-c80f24d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-c0e4ff1
+ * @license Angular v19.2.6+sha-c80f24d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
