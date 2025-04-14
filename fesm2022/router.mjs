6c6b9028aa833312c8645d1222e871c88fff65a8
kirjs <kirjs@users.noreply.github.com>
2025-04-14 15:19:32 +0000

    c1bcae9 feat(core): expose performance data in Chrome DevTools (#60789)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-84d359c
+ * @license Angular v20.0.0-next.6+sha-c1bcae9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-84d359c');
+const VERSION = new Version('20.0.0-next.6+sha-c1bcae9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
