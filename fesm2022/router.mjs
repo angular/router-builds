b6804d05fde7154868e742f126802a86b5ddd43a
kirjs <kirjs@users.noreply.github.com>
2025-04-15 21:43:06 +0000

    aaf07d8 docs: Add error handling documentation (#60848)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-9171401
+ * @license Angular v19.2.6+sha-aaf07d8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.6+sha-9171401');
+const VERSION = new Version('19.2.6+sha-aaf07d8');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-9171401
+ * @license Angular v19.2.6+sha-aaf07d8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
