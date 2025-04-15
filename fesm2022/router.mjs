abebe55dbc7cccf271481a6f65efdc700f29e5b8
kirjs <kirjs@users.noreply.github.com>
2025-04-15 13:26:26 +0000

    b2c8fb3 refactor(devtools): update release process to commit before publishing (#60646)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-939ee08
+ * @license Angular v20.0.0-next.6+sha-b2c8fb3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-939ee08');
+const VERSION = new Version('20.0.0-next.6+sha-b2c8fb3');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-939ee08
+ * @license Angular v20.0.0-next.6+sha-b2c8fb3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
