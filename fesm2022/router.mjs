332a546fb9c0dfe594d0f7dc06cb37ee865f2a06
kirjs <kirjs@users.noreply.github.com>
2025-04-15 22:47:38 +0000

    e90daf3 refactor(core): Update Transition finished promise to not throw unhandled rejection (#60816)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-388c749
+ * @license Angular v20.0.0-next.6+sha-e90daf3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-388c749');
+const VERSION = new Version('20.0.0-next.6+sha-e90daf3');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-388c749
+ * @license Angular v20.0.0-next.6+sha-e90daf3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
