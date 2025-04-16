1bed27082baffbbf679bbcbb7a840af22129af5c
kirjs <kirjs@users.noreply.github.com>
2025-04-16 21:15:10 +0000

    104ad0a build: update cross-repo angular dependencies to v20.0.0-next.7 (#60891)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-58728c7
+ * @license Angular v20.0.0-next.7+sha-104ad0a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-58728c7');
+const VERSION = new Version('20.0.0-next.7+sha-104ad0a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
