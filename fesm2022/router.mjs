c0d11a8722b538ca15d963bfc2f288f84b3f2e72
kirjs <kirjs@users.noreply.github.com>
2025-04-16 16:09:13 +0000

    cbbea70 fix(common): issue a warning instead of an error when `NgOptimizedImage` exceeds the preload limit (#60879)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-f22c72b
+ * @license Angular v20.0.0-next.6+sha-cbbea70
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.6+sha-f22c72b');
+const VERSION = new Version('20.0.0-next.6+sha-cbbea70');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-ktU8Dnrg.d.ts
+++ router_module.d-ktU8Dnrg.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.6+sha-f22c72b
+ * @license Angular v20.0.0-next.6+sha-cbbea70
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
