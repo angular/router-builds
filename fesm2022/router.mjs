21535cee33013eb2761bdb1c7d77c32138d692b3
kirjs <kirjs@users.noreply.github.com>
2025-04-16 14:32:21 +0000

    2a65a12 docs(docs-infra): first-app tutorial - step-14-http src-final correction (#60738)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-5cd8052
+ * @license Angular v19.2.6+sha-2a65a12
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.6+sha-5cd8052');
+const VERSION = new Version('19.2.6+sha-2a65a12');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-5cd8052
+ * @license Angular v19.2.6+sha-2a65a12
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
