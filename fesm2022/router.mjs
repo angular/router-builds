e9c0520221eee04152ea634a503c01e32cd9cdcc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-18 10:40:22 +0000

    2f3f8d6 refactor(core): promote `pendingUntilEvent` to developerPreview (#60904)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-f838726
+ * @license Angular v20.0.0-next.7+sha-2f3f8d6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.7+sha-f838726');
+const VERSION = new Version('20.0.0-next.7+sha-2f3f8d6');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-f838726
+ * @license Angular v20.0.0-next.7+sha-2f3f8d6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
