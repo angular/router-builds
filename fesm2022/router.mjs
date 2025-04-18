913ecbd023eeade1c3271c71b729073d89ff8971
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-18 19:20:42 +0000

    037dede refactor(devtools): improve components tab side pane UI (#60901)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-3a6121f
+ * @license Angular v20.0.0-next.7+sha-037dede
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.7+sha-3a6121f');
+const VERSION = new Version('20.0.0-next.7+sha-037dede');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-BFPyiLx0.d.ts
+++ router_module.d-BFPyiLx0.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-3a6121f
+ * @license Angular v20.0.0-next.7+sha-037dede
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
