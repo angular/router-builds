6407eb4c9600d74936cb6b608d208b6775533696
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-18 13:42:55 +0000

    3a6121f docs: add new routing in-depth guide (#60006)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-1694739
+ * @license Angular v20.0.0-next.7+sha-3a6121f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.7+sha-1694739');
+const VERSION = new Version('20.0.0-next.7+sha-3a6121f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-BFPyiLx0.d.ts
+++ router_module.d-BFPyiLx0.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-1694739
+ * @license Angular v20.0.0-next.7+sha-3a6121f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
