88ece438151a1d5b8bfa069bfac34cf84c4803f3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 09:17:30 +0000

    5555e9f docs: update docs centered on NgModules (#60933)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.7+sha-763e3ca
+ * @license Angular v19.2.7+sha-5555e9f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.7+sha-763e3ca');
+const VERSION = new Version('19.2.7+sha-5555e9f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.7+sha-763e3ca
+ * @license Angular v19.2.7+sha-5555e9f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
