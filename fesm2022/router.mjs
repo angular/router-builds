3d9a09a17747a43ca52ee42d128dbbca20cee933
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 11:45:28 +0000

    2c463a4 build: lock file maintenance (#60942)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-be99562
+ * @license Angular v20.0.0-next.7+sha-2c463a4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.7+sha-be99562');
+const VERSION = new Version('20.0.0-next.7+sha-2c463a4');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-be99562
+ * @license Angular v20.0.0-next.7+sha-2c463a4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
