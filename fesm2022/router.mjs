90b673f51cf8cc1c40c50df67c610d68ff832c5f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 14:42:55 +0000

    6c2fbad refactor(core): Improve NG0600 error message. (#60418)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-00dff8b
+ * @license Angular v20.0.0-next.7+sha-6c2fbad
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.7+sha-00dff8b');
+const VERSION = new Version('20.0.0-next.7+sha-6c2fbad');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-BFPyiLx0.d.ts
+++ router_module.d-BFPyiLx0.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-00dff8b
+ * @license Angular v20.0.0-next.7+sha-6c2fbad
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
