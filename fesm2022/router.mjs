e60e5d91fc4b9161de6bd646b8f9916f65a358aa
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 19:52:48 +0000

    3824519 docs: replace EventEmitter with output and self-closing tags in templates (#60910)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.7+sha-adac95c
+ * @license Angular v19.2.7+sha-3824519
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.7+sha-adac95c');
+const VERSION = new Version('19.2.7+sha-3824519');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.7+sha-adac95c
+ * @license Angular v19.2.7+sha-3824519
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
