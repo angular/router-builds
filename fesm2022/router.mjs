3e90e08b9ff03e9c109d354b1b134c6aa3d6eb5d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 11:12:58 +0000

    ea4a211 fix(forms): make NgForm emit FormSubmittedEvent and FormResetEvent (#60887)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.7+sha-9d08525
+ * @license Angular v19.2.7+sha-ea4a211
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.7+sha-9d08525');
+const VERSION = new Version('19.2.7+sha-ea4a211');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.7+sha-9d08525
+ * @license Angular v19.2.7+sha-ea4a211
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
