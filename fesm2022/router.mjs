c68e49f9295499624c1f257d61a1a322a763d406
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-23 14:37:13 +0000

    94b2a8c release: cut the v19.2.8 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.7+sha-b557618
+ * @license Angular v19.2.8+sha-94b2a8c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.7+sha-b557618');
+const VERSION = new Version('19.2.8+sha-94b2a8c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
