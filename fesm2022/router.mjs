9be7fb4ef0ca36614adf400bcbabb1f86dabe3a6
Miles Malerba <mmalerba@google.com>
2025-04-24 16:51:41 +0000

    e6c14f9 docs: remove old pipes docs. (#60996)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.8+sha-c6b41e1
+ * @license Angular v20.0.0-next.8+sha-e6c14f9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.8+sha-c6b41e1');
+const VERSION = new Version('20.0.0-next.8+sha-e6c14f9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.8+sha-c6b41e1
+ * @license Angular v20.0.0-next.8+sha-e6c14f9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
