e1c25011a1ccfa880831637dfe454cc1d22628eb
Miles Malerba <mmalerba@google.com>
2025-04-28 15:15:35 +0000

    aca1f45 docs: fix non-working link (#61011)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.8+sha-7d133c0
+ * @license Angular v19.2.8+sha-aca1f45
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.8+sha-7d133c0');
+const VERSION = new Version('19.2.8+sha-aca1f45');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.8+sha-7d133c0
+ * @license Angular v19.2.8+sha-aca1f45
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
