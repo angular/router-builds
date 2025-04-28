b3d5cdb6dcb5e1f76fa378a2add6bb3a43d4b22b
Miles Malerba <mmalerba@google.com>
2025-04-28 17:09:51 +0000

    578041b docs(docs-infra): fix function api ref header width (#61022)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.8+sha-0203e0b
+ * @license Angular v19.2.8+sha-578041b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.8+sha-0203e0b');
+const VERSION = new Version('19.2.8+sha-578041b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-BivBj8FC.d.ts
+++ router_module.d-BivBj8FC.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.8+sha-0203e0b
+ * @license Angular v19.2.8+sha-578041b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
