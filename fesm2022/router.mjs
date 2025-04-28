3e5e5079c2a69447147d1d98999e40c9deaf9b52
Miles Malerba <mmalerba@google.com>
2025-04-28 15:25:43 +0000

    46eba63 fix(docs-infra): use proper API for style overrides (#61008)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.8+sha-11e3101
+ * @license Angular v20.0.0-next.8+sha-46eba63
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.8+sha-11e3101');
+const VERSION = new Version('20.0.0-next.8+sha-46eba63');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
