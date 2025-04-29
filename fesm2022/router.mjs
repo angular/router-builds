cbe8b58030dcbb5a318734efe73bc4361c3c7cf8
Miles Malerba <mmalerba@google.com>
2025-04-29 19:29:54 +0000

    b7d3f3d feat(common): Allow passing ScrollOptions to ViewportScroller (#61002)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.8+sha-b250bb0
+ * @license Angular v20.0.0-next.8+sha-b7d3f3d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.8+sha-b250bb0');
+const VERSION = new Version('20.0.0-next.8+sha-b7d3f3d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
