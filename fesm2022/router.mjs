88087cf0666bed99f6ac7e8195ea9bac23e1b1f5
Miles Malerba <mmalerba@google.com>
2025-04-30 16:01:46 +0000

    dbb8702 fix(core): call DestroyRef on destroy callback if view is destroyed [patch] (#61061)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.8+sha-a554208
+ * @license Angular v19.2.8+sha-dbb8702
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.8+sha-a554208');
+const VERSION = new Version('19.2.8+sha-dbb8702');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
