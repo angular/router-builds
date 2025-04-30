d9b40d8c818678e8c46f79076654dbcdb09dae6f
Andrew Kushnir <akushnir@google.com>
2025-04-30 22:20:19 +0000

    05feabd build: update cross-repo angular dependencies (#61069)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.9+sha-b8f80ab
+ * @license Angular v20.0.0-next.9+sha-05feabd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.9+sha-b8f80ab');
+const VERSION = new Version('20.0.0-next.9+sha-05feabd');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
