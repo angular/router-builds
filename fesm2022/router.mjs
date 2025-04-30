98eb2073742d764284f2df655d081e2dddb3c47d
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-04-30 10:29:11 +0000

    6b76bca Revert "build: Run browsers tests without `platform-browser-dynamic` (#60937)" (#61059)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.8+sha-946b844
+ * @license Angular v19.2.8+sha-6b76bca
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.8+sha-946b844');
+const VERSION = new Version('19.2.8+sha-6b76bca');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.8+sha-946b844
+ * @license Angular v19.2.8+sha-6b76bca
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
