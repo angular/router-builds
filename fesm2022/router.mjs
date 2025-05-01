aa4996685334e4ea0a91eabfa0f283c8382f2d20
Andrew Kushnir <akushnir@google.com>
2025-05-01 15:53:40 +0000

    d2cb0b9 refactor(core): tree-shake `getDirectiveDef` error (#61075)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-33a336f
+ * @license Angular v20.1.0-next.0+sha-d2cb0b9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-33a336f');
+const VERSION = new Version('20.1.0-next.0+sha-d2cb0b9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-33a336f
+ * @license Angular v20.1.0-next.0+sha-d2cb0b9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
