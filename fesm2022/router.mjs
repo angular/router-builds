0683877c7ebf580eb3e4c8761c5968547a237e39
Andrew Kushnir <akushnir@google.com>
2025-05-02 16:25:03 +0000

    238441a build: enable rules_js interop mode in ng-dev release (#61088)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.9+sha-9685469
+ * @license Angular v19.2.9+sha-238441a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.9+sha-9685469');
+const VERSION = new Version('19.2.9+sha-238441a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-BivBj8FC.d.ts
+++ router_module.d-BivBj8FC.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.9+sha-9685469
+ * @license Angular v19.2.9+sha-238441a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
