d71378097c64dd6a9142e01d2644cf03a30ff7cc
Andrew Kushnir <akushnir@google.com>
2025-05-05 21:45:32 +0000

    2c17145 refactor(compiler): element references not resolved when selectorless matcher is passed in (#61100)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-310e5ff
+ * @license Angular v20.1.0-next.0+sha-2c17145
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-310e5ff');
+const VERSION = new Version('20.1.0-next.0+sha-2c17145');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
