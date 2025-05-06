da1963d04bc3a62a30dbac393837a0cd93184416
Andrew Kushnir <akushnir@google.com>
2025-05-06 17:33:46 +0000

    121e0e6 docs: fix whitespace after a link (#61136)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.9+sha-2b7aa91
+ * @license Angular v20.0.0-next.9+sha-121e0e6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.9+sha-2b7aa91');
+const VERSION = new Version('20.0.0-next.9+sha-121e0e6');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.9+sha-2b7aa91
+ * @license Angular v20.0.0-next.9+sha-121e0e6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
