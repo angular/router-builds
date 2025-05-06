06eb50c797501aa5006f4657e754cdb780fd8996
Andrew Kushnir <akushnir@google.com>
2025-05-06 00:10:58 +0000

    ff943e3 docs: update ssr docs to include prerendering wildcards (#61116)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.9+sha-3b3466a
+ * @license Angular v20.0.0-next.9+sha-ff943e3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.9+sha-3b3466a');
+const VERSION = new Version('20.0.0-next.9+sha-ff943e3');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-CcYEiCKn.d.ts
+++ router_module.d-CcYEiCKn.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.9+sha-3b3466a
+ * @license Angular v20.0.0-next.9+sha-ff943e3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
