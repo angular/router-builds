a5dc07a9d94e2113df4c5a70bbea0993ffa43a50
Andrew Kushnir <akushnir@google.com>
2025-05-06 16:19:35 +0000

    2b7aa91 ci: add interop pnpm and aspect files to fileFilters (#61138)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.9+sha-ff943e3
+ * @license Angular v20.0.0-next.9+sha-2b7aa91
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.9+sha-ff943e3');
+const VERSION = new Version('20.0.0-next.9+sha-2b7aa91');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.9+sha-ff943e3
+ * @license Angular v20.0.0-next.9+sha-2b7aa91
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
