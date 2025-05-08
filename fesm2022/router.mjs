cea8c97759e5a391e19f7037cb304947881ed2ce
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 19:04:52 +0000

    db06708 ci: update workflow to clone repo with history (#61223)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-adffb2e-with-local-changes
+ * @license Angular v20.1.0-next.0+sha-db06708-with-local-changes
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-adffb2e-with-local-changes');
+const VERSION = new Version('20.1.0-next.0+sha-db06708-with-local-changes');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
