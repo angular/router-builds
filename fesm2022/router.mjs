b230ee732a07b94545a3c308c904b0d675dc6811
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 12:48:57 +0000

    6f4307f ci: remove redundant dash in workflow file (#61206)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-50993be
+ * @license Angular v20.1.0-next.0+sha-6f4307f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-50993be');
+const VERSION = new Version('20.1.0-next.0+sha-6f4307f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-50993be
+ * @license Angular v20.1.0-next.0+sha-6f4307f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
