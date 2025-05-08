4e47ac97cb8d39621f18e82e2c8ba642c0337a41
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 19:04:42 +0000

    5cecf0d ci: update workflow to clone repo with history (#61223)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-3076117
+ * @license Angular v20.0.0-rc.0+sha-5cecf0d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-3076117');
+const VERSION = new Version('20.0.0-rc.0+sha-5cecf0d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
