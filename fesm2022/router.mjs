a73179a68c8cc3e13d6707fb942e3a81789560da
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 17:05:50 +0000

    5b60442 docs:  update the description of example (#61205) PR Close #61205

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-684af21
+ * @license Angular v19.2.10+sha-5b60442
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-684af21');
+const VERSION = new Version('19.2.10+sha-5b60442');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-684af21
+ * @license Angular v19.2.10+sha-5b60442
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
