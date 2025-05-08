64f686275adf9508efc35b493b0fc967cf8811dc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 17:05:17 +0000

    11727dd docs:  update the description of example (#61205) PR Close #61205

--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-b3bb2a1
+ * @license Angular v20.1.0-next.0+sha-11727dd-with-local-changes
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
