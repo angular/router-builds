a2647ea8dca4e822fd92105c3ca7f9f670f01f31
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 18:41:36 +0000

    adffb2e ci: use ` --force-with-lease`  when pushing to remote branch (#61222)

--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-11727dd-with-local-changes
+ * @license Angular v20.1.0-next.0+sha-adffb2e-with-local-changes
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
