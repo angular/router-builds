c3410621383e8bbbbb84c94465dd23c558e319f5
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 14:39:58 +0000

    66753dc ci: use bash syntax for `CURRENT_BRANCH` in GitHub Actions (#61212)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-6f4307f
+ * @license Angular v20.1.0-next.0+sha-66753dc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-6f4307f');
+const VERSION = new Version('20.1.0-next.0+sha-66753dc');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-6f4307f
+ * @license Angular v20.1.0-next.0+sha-66753dc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
