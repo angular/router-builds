6530c617a6016d48d8d3dcaa8375c3f0d02e688f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 16:53:51 +0000

    684af21 docs: swap the anchor to the correct one (#61200)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-655954a
+ * @license Angular v19.2.10+sha-684af21
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-655954a');
+const VERSION = new Version('19.2.10+sha-684af21');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
