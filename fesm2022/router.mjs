c216aff9ad1f1e07501b5f9fde3b44cce895ed8b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 16:36:12 +0000

    655954a ci: fix step name (#61213)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-3b9440b
+ * @license Angular v19.2.10+sha-655954a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-3b9440b');
+const VERSION = new Version('19.2.10+sha-655954a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-3b9440b
+ * @license Angular v19.2.10+sha-655954a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
