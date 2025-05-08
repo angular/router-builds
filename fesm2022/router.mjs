d7fd44de6e7a09ff7977f95b8123f7970dd4ff62
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 11:38:38 +0000

    e9c9cfd ci: add workflow to sync non-default branches (#61201)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-4292d8a
+ * @license Angular v19.2.10+sha-e9c9cfd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-4292d8a');
+const VERSION = new Version('19.2.10+sha-e9c9cfd');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-4292d8a
+ * @license Angular v19.2.10+sha-e9c9cfd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
