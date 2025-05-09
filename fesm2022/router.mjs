0caecd17efeda443efadaca69237b5e737251843
Joey Perrott <josephperrott@gmail.com>
2025-05-09 16:50:19 +0000

    12a5b7b build: migrate upgrade package to use ts_project (#61245)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-5f1c08d
+ * @license Angular v20.1.0-next.0+sha-12a5b7b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-5f1c08d');
+const VERSION = new Version('20.1.0-next.0+sha-12a5b7b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-mqMs4KSV.d.ts
+++ router_module.d-mqMs4KSV.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-5f1c08d
+ * @license Angular v20.1.0-next.0+sha-12a5b7b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
