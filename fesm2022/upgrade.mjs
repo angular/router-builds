0e10de601f48652c37d8bc35d97131b9a3cc221f
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-09 10:01:34 +0000

    c8dfe6d ci: ensure pnpm-lock file and aspect lock files are up-to-date (#61224)

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-db06708-with-local-changes
+ * @license Angular v20.1.0-next.0+sha-c8dfe6d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
