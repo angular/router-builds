9adcc2e0d9326132d5a4d2bd17e5e670c0613b2a
Joey Perrott <josephperrott@gmail.com>
2025-05-09 16:13:15 +0000

    69bfece build: migrate adev devtools package to use ts_project (#61219)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-83c98ad
+ * @license Angular v20.0.0-rc.0+sha-69bfece
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-83c98ad');
+const VERSION = new Version('20.0.0-rc.0+sha-69bfece');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-83c98ad
+ * @license Angular v20.0.0-rc.0+sha-69bfece
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
