e62909880026c70d1e7f52ed8bd6b796919e3b34
Joey Perrott <josephperrott@gmail.com>
2025-05-09 16:37:21 +0000

    5f1c08d build: migrate adev shared-docs package to use ts_project (#61193)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-83c22ec
+ * @license Angular v20.1.0-next.0+sha-5f1c08d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-83c22ec');
+const VERSION = new Version('20.1.0-next.0+sha-5f1c08d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-83c22ec
+ * @license Angular v20.1.0-next.0+sha-5f1c08d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
