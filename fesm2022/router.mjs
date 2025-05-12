7fd1be41ef13b8f190ebd6ba082abc5789d2099f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-12 22:43:47 +0000

    7be6e5d refactor(migrations): remove unused code (#61260)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-223ca9f
+ * @license Angular v19.2.10+sha-7be6e5d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-223ca9f');
+const VERSION = new Version('19.2.10+sha-7be6e5d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-223ca9f
+ * @license Angular v19.2.10+sha-7be6e5d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
