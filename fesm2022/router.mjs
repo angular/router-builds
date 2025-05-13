d81308187bfe51ef885014595f76b63874a84c04
Joey Perrott <josephperrott@gmail.com>
2025-05-13 22:56:13 +0000

    1b4b44e refactor: add explicit types for exports relying on inferred call return type (#61312)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-c1b9d3e
+ * @license Angular v20.0.0-rc.0+sha-1b4b44e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-c1b9d3e');
+const VERSION = new Version('20.0.0-rc.0+sha-1b4b44e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
