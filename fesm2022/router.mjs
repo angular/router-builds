780efcb585b3dcacca190457624e1cb189849767
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-14 11:25:20 +0000

    a7b58e3 build: update dependency @rollup/plugin-node-resolve to v16 (#61326)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-0fc0d67
+ * @license Angular v20.1.0-next.0+sha-a7b58e3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-0fc0d67');
+const VERSION = new Version('20.1.0-next.0+sha-a7b58e3');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-0fc0d67
+ * @license Angular v20.1.0-next.0+sha-a7b58e3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
