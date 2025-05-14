e196b4ca3844a3e31538a42032aa73c2fc10087a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-14 13:43:10 +0000

    4108efa docs: document the use of `this` in template expression syntax (#61250)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-8e78b4e
+ * @license Angular v20.1.0-next.0+sha-4108efa
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-8e78b4e');
+const VERSION = new Version('20.1.0-next.0+sha-4108efa');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-BF5ftZkT.d.ts
+++ router_module.d-BF5ftZkT.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-8e78b4e
+ * @license Angular v20.1.0-next.0+sha-4108efa
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
