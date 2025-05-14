5f9a270aa81c44917222c7fab5205a43498557c3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-14 15:42:26 +0000

    d7dbabe build: add script to diff packages and ensure no unexpected changes (#61335)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-c028918
+ * @license Angular v20.0.0-rc.0+sha-d7dbabe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-c028918');
+const VERSION = new Version('20.0.0-rc.0+sha-d7dbabe');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-c028918
+ * @license Angular v20.0.0-rc.0+sha-d7dbabe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
