d0757c232a4e4812fcbfd70a11d7bd9ddb6b8d0f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-15 17:25:34 +0000

    ca6295e fix(core): handle different DI token types in Chrome DevTools integration (#61333)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-bc2cab7
+ * @license Angular v20.0.0-rc.1+sha-ca6295e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-bc2cab7');
+const VERSION = new Version('20.0.0-rc.1+sha-ca6295e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
