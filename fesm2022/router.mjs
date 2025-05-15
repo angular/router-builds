3fd279b276056ca98a2e5c36f99725fa2a83af66
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-15 12:54:00 +0000

    ff62ff1 refactor: explicitly ensure `ngDevMode` types are available (#61365)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-31abc9d
+ * @license Angular v20.1.0-next.0+sha-ff62ff1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-31abc9d');
+const VERSION = new Version('20.1.0-next.0+sha-ff62ff1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-31abc9d
+ * @license Angular v20.1.0-next.0+sha-ff62ff1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
