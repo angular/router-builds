9c00ec13cbd7c5b953796f259a5046577e20147b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 10:28:40 +0000

    3dd2f82 docs: fix typos (#61257)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-ac190f4
+ * @license Angular v20.1.0-next.0+sha-3dd2f82
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-ac190f4');
+const VERSION = new Version('20.1.0-next.0+sha-3dd2f82');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
