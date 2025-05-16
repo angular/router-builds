ed7b664d966dc3c1f3d6abbac3c6ef3c34146ab7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 14:07:11 +0000

    35a40cb build: fix golden approval script (#61407)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-dce4978
+ * @license Angular v20.1.0-next.0+sha-35a40cb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-dce4978');
+const VERSION = new Version('20.1.0-next.0+sha-35a40cb');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
