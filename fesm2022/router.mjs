9239e32778317cf5edb6890674df5b9bad72ef71
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 13:56:55 +0000

    e13d6a3 build: update dev-infra for latest RBE changes (#61412)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-a90c720
+ * @license Angular v20.0.0-rc.1+sha-e13d6a3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-a90c720');
+const VERSION = new Version('20.0.0-rc.1+sha-e13d6a3');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-a90c720
+ * @license Angular v20.0.0-rc.1+sha-e13d6a3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
