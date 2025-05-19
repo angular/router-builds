5fafac740aa7730358c1972b0bb229597ccd8b39
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 12:45:12 +0000

    0a25aba build: `--config=debug` should not disable sandbox for all actions (#61468)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-bd2b454
+ * @license Angular v19.2.11+sha-0a25aba
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-bd2b454');
+const VERSION = new Version('19.2.11+sha-0a25aba');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-bd2b454
+ * @license Angular v19.2.11+sha-0a25aba
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
