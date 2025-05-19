48369863361722ba55329d18e84227d98fe41ae2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 11:12:45 +0000

    5d35288 refactor(migrations): gracefully proceed if reference cannot be resolved (#61426)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-6290000
+ * @license Angular v20.0.0-rc.1+sha-5d35288
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-6290000');
+const VERSION = new Version('20.0.0-rc.1+sha-5d35288');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
