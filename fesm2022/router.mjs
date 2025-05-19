484fe8029fe4bd296d9c76d38f2a253502f2b178
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 13:34:52 +0000

    2b3c89d refactor(core): re-introduce TestBed.flushEffects()" (#61462)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-1e86aa7
+ * @license Angular v20.1.0-next.0+sha-2b3c89d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-1e86aa7');
+const VERSION = new Version('20.1.0-next.0+sha-2b3c89d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-1e86aa7
+ * @license Angular v20.1.0-next.0+sha-2b3c89d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
