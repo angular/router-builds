e6140d47ec3e96216b1bb475818bb6794cb0aaf1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 17:05:02 +0000

    bb31f6f docs(docs-infra): display CLI enum values for enums (#61521)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-efda872
+ * @license Angular v19.2.11+sha-bb31f6f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-efda872');
+const VERSION = new Version('19.2.11+sha-bb31f6f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-efda872
+ * @license Angular v19.2.11+sha-bb31f6f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
