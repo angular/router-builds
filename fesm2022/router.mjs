928ccb1fb66eed26783423eb55863d245d34a1f4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 09:02:42 +0000

    dbd66ec refactor(devtools): switch default font to material symbols (#61281)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-1360e4b
+ * @license Angular v19.2.11+sha-dbd66ec
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-1360e4b');
+const VERSION = new Version('19.2.11+sha-dbd66ec');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-1360e4b
+ * @license Angular v19.2.11+sha-dbd66ec
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
