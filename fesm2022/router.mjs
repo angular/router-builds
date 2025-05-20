12eb88b2f4789ef8e69b28555643649a1876e62e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 12:15:10 +0000

    2796f18 test: set the `noCheck` TS compiler option in declaration-only emission compliance tests (#61470)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-2e7032e
+ * @license Angular v20.1.0-next.0+sha-2796f18
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-2e7032e');
+const VERSION = new Version('20.1.0-next.0+sha-2796f18');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
