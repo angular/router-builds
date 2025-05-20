626176a78e70cf9d222b7253ed9822d2007bd0ef
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 10:30:45 +0000

    f702f0e build: update dependency conventional-changelog to v7 (#61491)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-b55fa4e
+ * @license Angular v20.0.0-rc.1+sha-f702f0e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-b55fa4e');
+const VERSION = new Version('20.0.0-rc.1+sha-f702f0e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
