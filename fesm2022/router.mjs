14ee77dea00625a45bcfe43fd57d385b2377b4ad
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 11:35:29 +0000

    6e6d952 build: replace `platform-browser-dynamic` with  `platform-browser` (#61497)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-f0ce891
+ * @license Angular v20.0.0-rc.1+sha-6e6d952
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-f0ce891');
+const VERSION = new Version('20.0.0-rc.1+sha-6e6d952');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
