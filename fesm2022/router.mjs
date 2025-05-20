039971cdd2903461eef7e315a5db0186d8b9ee90
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 15:17:35 +0000

    8768894 fix(common): prevent reading chunks if app is destroyed (#61354)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-1148b7e
+ * @license Angular v20.1.0-next.0+sha-8768894
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-1148b7e');
+const VERSION = new Version('20.1.0-next.0+sha-8768894');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
