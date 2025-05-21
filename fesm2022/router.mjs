232624efcd437841e215371018d6fe2f168c7411
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 10:17:27 +0000

    4e5d6de refactor(core): Merge R3TemplateRef implementation and TemplateRef interface (#61455)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-3cc0910
+ * @license Angular v20.0.0-rc.1+sha-4e5d6de
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-3cc0910');
+const VERSION = new Version('20.0.0-rc.1+sha-4e5d6de');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
