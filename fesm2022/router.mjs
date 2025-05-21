b0cf5ba6973abdd2b8dfb1c14146ca7856fa623b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 17:15:11 +0000

    3a8bbe9 build: migrate language service to use rules_js (#61578)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-fc2483e
+ * @license Angular v19.2.11+sha-3a8bbe9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-fc2483e');
+const VERSION = new Version('19.2.11+sha-3a8bbe9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
