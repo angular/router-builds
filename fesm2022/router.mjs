ac5931405336eec113f0204f6666e3ae72109139
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 16:23:32 +0000

    f68001d docs: fix descendants default explanation for contentChild (#61580)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-373480c
+ * @license Angular v20.1.0-next.0+sha-f68001d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-373480c');
+const VERSION = new Version('20.1.0-next.0+sha-f68001d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-373480c
+ * @license Angular v20.1.0-next.0+sha-f68001d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
