9f782e1c0279265c66060f458ceab91875be61c0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 17:46:56 +0000

    b6d7b4b build: remove the "restamping" substitutions from adev shared-docs package (#61585)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-6f6e91d
+ * @license Angular v19.2.11+sha-b6d7b4b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-6f6e91d');
+const VERSION = new Version('19.2.11+sha-b6d7b4b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-6f6e91d
+ * @license Angular v19.2.11+sha-b6d7b4b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
