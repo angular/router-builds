4135c4c3dc96ddfe9ad374d30e1a6c68385c5b39
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 17:35:21 +0000

    6f6e91d build: add comment explaining path maps (#61584)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-3a8bbe9
+ * @license Angular v19.2.11+sha-6f6e91d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-3a8bbe9');
+const VERSION = new Version('19.2.11+sha-6f6e91d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-3a8bbe9
+ * @license Angular v19.2.11+sha-6f6e91d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
