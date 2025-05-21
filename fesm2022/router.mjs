52a3ddd30f1ca6efd0ffc1f014abf8d03515ab7b
Andrew Scott <atscott01@gmail.com>
2025-05-21 19:58:43 +0000

    ac76549 release: cut the v19.2.12 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-b6d7b4b
+ * @license Angular v19.2.12+sha-ac76549
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-b6d7b4b');
+const VERSION = new Version('19.2.12+sha-ac76549');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-Bx9ArA6K.d.ts
+++ router_module.d-Bx9ArA6K.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-b6d7b4b
+ * @license Angular v19.2.12+sha-ac76549
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
