8a5478b90ecf912ba947b949373e4a29128da058
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 14:26:44 +0000

    abdade9 refactor(common): drop `httpResource` error message (#61570)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-88c70eb
+ * @license Angular v20.0.0-rc.1+sha-abdade9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-88c70eb');
+const VERSION = new Version('20.0.0-rc.1+sha-abdade9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-88c70eb
+ * @license Angular v20.0.0-rc.1+sha-abdade9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
