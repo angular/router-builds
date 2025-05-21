51fc728a420b800a649f7672a6b0aca324593a95
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 12:52:50 +0000

    e7656b8 refactor(core): drop `rxResource` error messages (#61565)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-46af023
+ * @license Angular v20.1.0-next.0+sha-e7656b8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-46af023');
+const VERSION = new Version('20.1.0-next.0+sha-e7656b8');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
