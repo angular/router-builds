6a7a2a1b07556d96d61ece0bf75c91254a53e6c3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 09:14:31 +0000

    0a82e58 docs: fix info about `descendants`  for content queries. (#61534)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-3b564e7
+ * @license Angular v20.0.0-rc.1+sha-0a82e58
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-3b564e7');
+const VERSION = new Version('20.0.0-rc.1+sha-0a82e58');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
