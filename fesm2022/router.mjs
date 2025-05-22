5fff02c12dd1ef8fb780feb609c3c13c80a16068
Andrew Scott <atscott@google.com>
2025-05-22 18:43:35 +0000

    687e018 build: migrate platform-server to rules_js (#61589)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-a28cadc
+ * @license Angular v20.1.0-next.0+sha-687e018
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-a28cadc');
+const VERSION = new Version('20.1.0-next.0+sha-687e018');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
