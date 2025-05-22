2efb54410012f8d98f8bfba9a3f943d37eeff2e1
Andrew Scott <atscott@google.com>
2025-05-22 17:11:24 +0000

    ce916fe fix(service-worker): do not register service worker if app is destroyed before it is ready to register (#61101)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-ad8931c
+ * @license Angular v20.1.0-next.0+sha-ce916fe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-ad8931c');
+const VERSION = new Version('20.1.0-next.0+sha-ce916fe');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-ad8931c
+ * @license Angular v20.1.0-next.0+sha-ce916fe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
