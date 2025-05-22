f218a668b3cf0d655c9a9538905acc04f1298d0a
Andrew Scott <atscott@google.com>
2025-05-22 18:00:53 +0000

    114253e release: bump Angular DevTools to version 1.0.32 (#61620)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.2+sha-959258d
+ * @license Angular v20.0.0-rc.2+sha-114253e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.2+sha-959258d');
+const VERSION = new Version('20.0.0-rc.2+sha-114253e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
