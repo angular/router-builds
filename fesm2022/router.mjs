d68d05951731b1868329eb9cfd0315ef43894815
Andrew Scott <atscott@google.com>
2025-05-22 03:51:32 +0000

    5f1bd07 fix(zone.js): classes that extend Error should retain cause property (#61599)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.12+sha-2f5912e
+ * @license Angular v19.2.12+sha-5f1bd07
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.12+sha-2f5912e');
+const VERSION = new Version('19.2.12+sha-5f1bd07');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-Bx9ArA6K.d.ts
+++ router_module.d-Bx9ArA6K.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.12+sha-2f5912e
+ * @license Angular v19.2.12+sha-5f1bd07
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
