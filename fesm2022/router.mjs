37ee84351541619327704a4430215b1268493a88
Andrew Scott <atscott@google.com>
2025-05-22 20:47:59 +0000

    1ea39e0 refactor(zone.js): Add a `withProxyZone` helper that might be used for unpatched test frameworks (#61626)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-768e715
+ * @license Angular v20.1.0-next.0+sha-1ea39e0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-768e715');
+const VERSION = new Version('20.1.0-next.0+sha-1ea39e0');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-768e715
+ * @license Angular v20.1.0-next.0+sha-1ea39e0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
