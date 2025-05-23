49d8a5f024e4f71ff455661093bc23fc2b5bdddb
Andrew Scott <atscott@google.com>
2025-05-23 17:11:33 +0000

    abcb865 fix(common): avoid injecting ApplicationRef in FetchBackend (#61649)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-4356e85
+ * @license Angular v20.1.0-next.0+sha-abcb865
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-4356e85');
+const VERSION = new Version('20.1.0-next.0+sha-abcb865');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-4356e85
+ * @license Angular v20.1.0-next.0+sha-abcb865
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
