58c126a5b58349c1add288f316dc6cdfd5d7988e
Andrew Scott <atscott@google.com>
2025-05-23 22:26:23 +0000

    968bbe9 build: migrate platform-browser and platform-browser-dynamic package to use rules_js (#61623)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.2+sha-aa7190d
+ * @license Angular v20.0.0-rc.2+sha-968bbe9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.2+sha-aa7190d');
+const VERSION = new Version('20.0.0-rc.2+sha-968bbe9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-BX1wlLux.d.ts
+++ router_module.d-BX1wlLux.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.2+sha-aa7190d
+ * @license Angular v20.0.0-rc.2+sha-968bbe9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
