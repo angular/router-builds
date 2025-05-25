ab78e568e2c9e1b940e91e2259ecf67345114cdb
Andrew Scott <atscott@google.com>
2025-05-25 21:35:00 +0000

    2e5362a feat(common): accept undefined inputs in NgTemplateOutlet (#61404)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.2+sha-f1568ef
+ * @license Angular v20.0.0-rc.2+sha-2e5362a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.2+sha-f1568ef');
+const VERSION = new Version('20.0.0-rc.2+sha-2e5362a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
