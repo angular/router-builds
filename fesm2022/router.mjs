398af14fa3e19795d07bebe48cd6ccd628ff75a5
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-26 09:32:31 +0000

    556413d refactor(compiler-cli): remove hardcoded config for unused standalone imports rule enablement (#61622)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.2+sha-2e5362a
+ * @license Angular v20.0.0-rc.2+sha-556413d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.2+sha-2e5362a');
+const VERSION = new Version('20.0.0-rc.2+sha-556413d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
