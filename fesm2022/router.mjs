eb3b7f67e870a0281991ce305f3af3849b6b6ae0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-26 11:22:05 +0000

    9a9511a refactor(platform-browser): replace `isPlatformServer` with `ngServerMode` (#59496)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-47660b7
+ * @license Angular v20.1.0-next.0+sha-9a9511a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-47660b7');
+const VERSION = new Version('20.1.0-next.0+sha-9a9511a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-47660b7
+ * @license Angular v20.1.0-next.0+sha-9a9511a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
