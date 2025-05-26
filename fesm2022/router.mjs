257c2db211a19f08ce693c3799941f9ab5433b42
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-26 12:05:57 +0000

    73d3e00 build: fix failing test (#61683)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.13+sha-6fea9c8
+ * @license Angular v19.2.13+sha-73d3e00
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.13+sha-6fea9c8');
+const VERSION = new Version('19.2.13+sha-73d3e00');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
