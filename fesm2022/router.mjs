5af7b1019bb41e6a283bed44412c3d071be4833d
Andrew Scott <atscott01@gmail.com>
2025-05-27 23:09:22 +0000

    d19669c docs: release notes for the v20.0.0-rc.3 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-fe03aa0
+ * @license Angular v20.1.0-next.0+sha-d19669c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-fe03aa0');
+const VERSION = new Version('20.1.0-next.0+sha-d19669c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
