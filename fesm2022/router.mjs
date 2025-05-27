0ffb9b6f596c9ef5962f20036aa624f311ef5129
Andrew Scott <atscott@google.com>
2025-05-27 22:30:47 +0000

    fe03aa0 Revert "fix(core): call `DestroyRef` on destroy callback if view is destroyed (#58008)" (#61625)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-c77e198
+ * @license Angular v20.1.0-next.0+sha-fe03aa0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-c77e198');
+const VERSION = new Version('20.1.0-next.0+sha-fe03aa0');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-c77e198
+ * @license Angular v20.1.0-next.0+sha-fe03aa0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
