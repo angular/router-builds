d5c2683430d0c3ef94bd361a7371a2fce01b117e
Andrew Scott <atscott@google.com>
2025-05-28 18:00:55 +0000

    1035240 release: bump version of in memory web api (#61731)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-bf7816e
+ * @license Angular v20.0.0+sha-1035240
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-bf7816e');
+const VERSION = new Version('20.0.0+sha-1035240');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-bf7816e
+ * @license Angular v20.0.0+sha-1035240
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
