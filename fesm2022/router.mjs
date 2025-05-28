3258be696d21c84751dff6286f08aec9dfcc783e
Andrew Scott <atscott@google.com>
2025-05-28 18:19:42 +0000

    aed8a77 docs: update versions.json with v20 (#61732)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-1035240
+ * @license Angular v20.0.0+sha-aed8a77
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-1035240');
+const VERSION = new Version('20.0.0+sha-aed8a77');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
