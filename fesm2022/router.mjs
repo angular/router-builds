7edef551d659dddd24a07d48aae36b503258d8f0
Andrew Scott <atscott@google.com>
2025-05-28 17:11:29 +0000

    55306bd docs: update tutorials and playground to v20 (#61728)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-40e2b5b
+ * @license Angular v20.0.0+sha-55306bd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-40e2b5b');
+const VERSION = new Version('20.0.0+sha-55306bd');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-40e2b5b
+ * @license Angular v20.0.0+sha-55306bd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
