a37e7d65769655d7db6690c0fa13379e452dbc64
Andrew Scott <atscott01@gmail.com>
2025-05-28 14:00:43 +0000

    40e2b5b release: cut the v20.0.0 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.3+sha-0502cf2
+ * @license Angular v20.0.0+sha-40e2b5b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.3+sha-0502cf2');
+const VERSION = new Version('20.0.0+sha-40e2b5b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.3+sha-0502cf2
+ * @license Angular v20.0.0+sha-40e2b5b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
