2cbf353bd98be6ac451c7b6b7700ca95827ef534
Andrew Scott <atscott@google.com>
2025-05-28 17:36:59 +0000

    d5ac380 build: bump peer deps for angular-in-memory-web-api (#61729)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-8cf7e23
+ * @license Angular v20.1.0-next.0+sha-d5ac380
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-8cf7e23');
+const VERSION = new Version('20.1.0-next.0+sha-d5ac380');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
