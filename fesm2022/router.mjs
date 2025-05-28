4e397013cb5e763ae38a35bd5e2750620a69236d
Andrew Scott <atscott@google.com>
2025-05-28 19:35:44 +0000

    a0d599e docs: link to v20 blog post in changelog (#61734)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-aed8a77
+ * @license Angular v20.0.0+sha-a0d599e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-aed8a77');
+const VERSION = new Version('20.0.0+sha-a0d599e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-aed8a77
+ * @license Angular v20.0.0+sha-a0d599e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
