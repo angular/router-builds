b70876166af5155c5619b0d4a2beb35ed38c69cc
kirjs <kirts@google.com>
2025-05-29 22:29:06 +0000

    6113fae docs: fix copy-paste error on defer page in interaction section (#61751) PR Close #61751

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-1802d36
+ * @license Angular v20.0.0+sha-6113fae
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-1802d36');
+const VERSION = new Version('20.0.0+sha-6113fae');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
