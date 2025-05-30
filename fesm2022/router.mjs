1e56c727f52686d815b0a199f6cc42d808551c7f
kirjs <kirts@google.com>
2025-05-30 15:24:31 +0000

    0f13d90 build: migrate all `modules/...` code to `ts_project` (#61779)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-b561a50
+ * @license Angular v20.0.0+sha-0f13d90
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-b561a50');
+const VERSION = new Version('20.0.0+sha-0f13d90');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-b561a50
+ * @license Angular v20.0.0+sha-0f13d90
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
