03e615543d9004a3fbb63ec45e53d8615c56a81c
kirjs <kirts@google.com>
2025-05-30 14:04:34 +0000

    2f0ecfc refactor: expand DevTools reviewer list (#61709)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-5f187b2
+ * @license Angular v20.1.0-next.0+sha-2f0ecfc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-5f187b2');
+const VERSION = new Version('20.1.0-next.0+sha-2f0ecfc');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-5f187b2
+ * @license Angular v20.1.0-next.0+sha-2f0ecfc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
