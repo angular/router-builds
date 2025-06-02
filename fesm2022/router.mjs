0ce5797ad1f5fe962165f45a8f0cc9205c250339
kirjs <kirts@google.com>
2025-06-02 18:08:59 +0000

    1e84061 build: remove ng_update_migration integration test. (#61822)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.14+sha-cb1b167
+ * @license Angular v19.2.14+sha-1e84061
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.14+sha-cb1b167');
+const VERSION = new Version('19.2.14+sha-1e84061');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.14+sha-cb1b167
+ * @license Angular v19.2.14+sha-1e84061
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
