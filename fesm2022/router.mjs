72f89567ec6fdc74f12df7ced619939f87ad59c2
kirjs <kirts@google.com>
2025-06-02 20:51:32 +0000

    9317b1c docs: fix deprecated docs navigation background color. (#61776)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.14+sha-1e84061
+ * @license Angular v19.2.14+sha-9317b1c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.14+sha-1e84061');
+const VERSION = new Version('19.2.14+sha-9317b1c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.14+sha-1e84061
+ * @license Angular v19.2.14+sha-9317b1c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
