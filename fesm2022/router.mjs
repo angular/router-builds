27d2788e7557dc0df6de87d451c3752dac360db8
kirjs <kirts@google.com>
2025-06-03 18:07:35 +0000

    939ca07 docs(docs-infra): fix tutorial tooltip (#61782)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-9a24a2b
+ * @license Angular v20.0.0+sha-939ca07
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-9a24a2b');
+const VERSION = new Version('20.0.0+sha-939ca07');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-9a24a2b
+ * @license Angular v20.0.0+sha-939ca07
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
