29aceb4723e0ec24a9b0b89c812e1810dbf7d1eb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-03 09:49:35 +0000

    3af65af build: remove `npm_package` rule for `packages/bazel` (#61826)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-42f07ce
+ * @license Angular v20.1.0-next.0+sha-3af65af
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-42f07ce');
+const VERSION = new Version('20.1.0-next.0+sha-3af65af');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-BX1wlLux.d.ts
+++ router_module.d-BX1wlLux.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-42f07ce
+ * @license Angular v20.1.0-next.0+sha-3af65af
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
