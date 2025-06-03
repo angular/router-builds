61818155ab7b10bfdeead2dd1c507cdbd25d7581
kirjs <kirts@google.com>
2025-06-03 12:29:37 +0000

    b5d21b0 build: ensure optimization rule can use `compiler-cli` from HEAD (#61810) (#61830)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-30ea478
+ * @license Angular v20.0.0+sha-b5d21b0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-30ea478');
+const VERSION = new Version('20.0.0+sha-b5d21b0');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
