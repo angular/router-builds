cada2d4062dc291b229e69b27901fdc4d83c5a48
kirjs <kirts@google.com>
2025-06-03 15:05:43 +0000

    c663277 fix(service-worker): update service worker to handle seeking better for videos (#60029)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-4fa1dca
+ * @license Angular v20.1.0-next.0+sha-c663277
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-4fa1dca');
+const VERSION = new Version('20.1.0-next.0+sha-c663277');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-4fa1dca
+ * @license Angular v20.1.0-next.0+sha-c663277
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
