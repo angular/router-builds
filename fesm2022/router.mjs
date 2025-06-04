878cae40506bd4ae75f76cc7b2d8448a3dd124fd
kirjs <kirts@google.com>
2025-06-04 15:54:40 +0000

    3c64468 refactor(core): use RefactorStreamItem type in rxResource (#59887)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-0bf8642
+ * @license Angular v20.0.0+sha-3c64468
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-0bf8642');
+const VERSION = new Version('20.0.0+sha-3c64468');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
