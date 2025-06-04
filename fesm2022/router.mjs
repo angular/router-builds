b6cf48c5107ded050bc8b7ad6edf00479db41528
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-06-04 06:39:53 +0000

    eb62a58 docs: open llms files in new tab (#61851)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-2bf7249
+ * @license Angular v20.0.0+sha-eb62a58
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-2bf7249');
+const VERSION = new Version('20.0.0+sha-eb62a58');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-BX1wlLux.d.ts
+++ router_module.d-BX1wlLux.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-2bf7249
+ * @license Angular v20.0.0+sha-eb62a58
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
