1e3aa76947b962b9ead855f480f1d7e09cd4be90
kirjs <kirts@google.com>
2025-06-04 15:37:09 +0000

    0bf8642 docs(migrations): update the docs for the migration to control flow syntax (#61816)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-9e850c8
+ * @license Angular v20.0.0+sha-0bf8642
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-9e850c8');
+const VERSION = new Version('20.0.0+sha-0bf8642');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
