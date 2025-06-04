e6ec805ec422cd7780a5ab911c0de74dd30b1c27
kirjs <kirts@google.com>
2025-06-04 13:46:43 +0000

    5c31e7e fix(core): unregister `onDestroy` when observable errors in `toSignal` (#61596)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-eb62a58
+ * @license Angular v20.0.0+sha-5c31e7e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-eb62a58');
+const VERSION = new Version('20.0.0+sha-5c31e7e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
