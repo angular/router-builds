6a3008760153e38546528e1d257c41358b960ef5
kirjs <kirts@google.com>
2025-06-04 00:28:42 +0000

    e5d9ea2 docs: update resource docs (#61846)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-6adf022
+ * @license Angular v20.1.0-next.0+sha-e5d9ea2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-6adf022');
+const VERSION = new Version('20.1.0-next.0+sha-e5d9ea2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
