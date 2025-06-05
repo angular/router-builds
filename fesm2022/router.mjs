8850b9f8b66150714c731a42fb1c69715f4592b1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-05 09:24:48 +0000

    e81ea0c fix(core): unregister `onDestroy` in `outputToObservable` (#61882)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-967c8fe
+ * @license Angular v20.1.0-next.0+sha-e81ea0c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-967c8fe');
+const VERSION = new Version('20.1.0-next.0+sha-e81ea0c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-mlGavL8F.d.ts
+++ router_module.d-mlGavL8F.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-967c8fe
+ * @license Angular v20.1.0-next.0+sha-e81ea0c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
