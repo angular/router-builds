1a9b77e7c643c262418271768fcca9788c569a6f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-05 09:00:18 +0000

    59eb720 docs(docs-infra): use application builder for adev karma tests (#58291)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.1+sha-f6841ea
+ * @license Angular v20.0.1+sha-59eb720
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.1+sha-f6841ea');
+const VERSION = new Version('20.0.1+sha-59eb720');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.1+sha-f6841ea
+ * @license Angular v20.0.1+sha-59eb720
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
