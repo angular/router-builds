3b522720740e6a31fa7992c623aa084302df5949
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-05 09:29:12 +0000

    32ce149 build: update scorecard action dependencies (#61891)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.1+sha-59eb720
+ * @license Angular v20.0.1+sha-32ce149
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.1+sha-59eb720');
+const VERSION = new Version('20.0.1+sha-32ce149');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
