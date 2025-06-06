e635c3e2e57c034c6aa4023c628f1b2b16f5b6c8
Andrew Kushnir <akushnir@google.com>
2025-06-06 00:54:27 +0000

    cfb0dc2 fix(docs-infra): Fix import name for house location (#61914)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-9354efc
+ * @license Angular v20.1.0-next.0+sha-cfb0dc2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-9354efc');
+const VERSION = new Version('20.1.0-next.0+sha-cfb0dc2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
