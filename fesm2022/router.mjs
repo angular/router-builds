b79cf15fa5e7343aa667190a468c7a7153e37256
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-06 08:39:35 +0000

    78c417a build: update cross-repo angular dependencies (#61910)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-8f9d13e
+ * @license Angular v20.1.0-next.0+sha-78c417a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-8f9d13e');
+const VERSION = new Version('20.1.0-next.0+sha-78c417a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
