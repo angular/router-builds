1fc873452e4f08fa83197e800efac00c458d0553
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-06 09:34:57 +0000

    a6a193f docs: fix broken reveal answer button (#61925)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.1+sha-9ae8027
+ * @license Angular v20.0.1+sha-a6a193f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.1+sha-9ae8027');
+const VERSION = new Version('20.0.1+sha-a6a193f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
