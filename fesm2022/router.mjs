80b877e044a4c5a94990af5f28e7fe92641addab
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-06 11:28:45 +0000

    0d198f2 docs: update first-app tutorial to signal inputs (#61876)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.1+sha-a6a193f
+ * @license Angular v20.0.1+sha-0d198f2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.1+sha-a6a193f');
+const VERSION = new Version('20.0.1+sha-0d198f2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-mlGavL8F.d.ts
+++ router_module.d-mlGavL8F.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.1+sha-a6a193f
+ * @license Angular v20.0.1+sha-0d198f2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
