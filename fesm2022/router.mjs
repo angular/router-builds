9f4df88a5c7fc797553fb2961bc07abba1a33375
Andrew Kushnir <akushnir@google.com>
2025-06-06 01:57:42 +0000

    8f9d13e fix(docs-infra): fix wrong `visibleLines` numbers in `docs-code` blocks (#61909)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-cfb0dc2
+ * @license Angular v20.1.0-next.0+sha-8f9d13e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-cfb0dc2');
+const VERSION = new Version('20.1.0-next.0+sha-8f9d13e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-mlGavL8F.d.ts
+++ router_module.d-mlGavL8F.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-cfb0dc2
+ * @license Angular v20.1.0-next.0+sha-8f9d13e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
