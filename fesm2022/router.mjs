082dcab86b7c7c182e75d15e5fd3d6d78b838d09
Andrew Kushnir <akushnir@google.com>
2025-06-06 19:51:40 +0000

    a76f64b docs: Add error handling doc to the menu (#61945)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-321c8a3
+ * @license Angular v20.0.2+sha-a76f64b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.2+sha-321c8a3');
+const VERSION = new Version('20.0.2+sha-a76f64b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-321c8a3
+ * @license Angular v20.0.2+sha-a76f64b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
