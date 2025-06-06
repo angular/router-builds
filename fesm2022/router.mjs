21ece7650f625d7bcae78cb47cdf37de5eaf5b98
Andrew Kushnir <akushnir@google.com>
2025-06-06 14:56:45 +0000

    bb6ec10 Revert "refactor(service-worker): make `SwPush` and `SwUpdate` tree-shakable (#61670)" (#61923)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.1+sha-6ae07ad
+ * @license Angular v20.0.1+sha-bb6ec10
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.1+sha-6ae07ad');
+const VERSION = new Version('20.0.1+sha-bb6ec10');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
