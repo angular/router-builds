a1e5dc4a4ef2452f689ccebf0eeb7d2da62b915c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-06 09:38:07 +0000

    e01b4c2 docs(docs-infra): fix deps for first-app tutorial (#61926)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-0576724
+ * @license Angular v20.1.0-next.0+sha-e01b4c2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-0576724');
+const VERSION = new Version('20.1.0-next.0+sha-e01b4c2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
