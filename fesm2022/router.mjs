f9b66fd22dc69dcac204b470ef3bea6add81de79
Andrew Kushnir <akushnir@google.com>
2025-06-09 19:05:43 +0000

    ee4acc2 build: update dependency google-closure-compiler to v20250603 (#61960)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-85e9505
+ * @license Angular v20.0.2+sha-ee4acc2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.2+sha-85e9505');
+const VERSION = new Version('20.0.2+sha-ee4acc2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-85e9505
+ * @license Angular v20.0.2+sha-ee4acc2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
