a7335df1802bff17994a2d1056d8b016f6524773
Andrew Kushnir <akushnir@google.com>
2025-06-10 22:58:55 +0000

    6f6db99 ci: update to latest version of dev-infra actions (#61991)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.14+sha-9317b1c
+ * @license Angular v19.2.14+sha-6f6db99
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.14+sha-9317b1c');
+const VERSION = new Version('19.2.14+sha-6f6db99');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.14+sha-9317b1c
+ * @license Angular v19.2.14+sha-6f6db99
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
