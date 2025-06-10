c000440998340a08a247f488154144ed737679c0
Andrew Kushnir <akushnir@google.com>
2025-06-10 19:13:49 +0000

    135c594 refactor: use zone.js from npm instead of packages/zone.js throughout repo (#61978)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-ee4acc2
+ * @license Angular v20.0.2+sha-135c594
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.2+sha-ee4acc2');
+const VERSION = new Version('20.0.2+sha-135c594');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-ee4acc2
+ * @license Angular v20.0.2+sha-135c594
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
