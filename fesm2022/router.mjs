6b609c3418454df40460209fea3fa840219f2eaa
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-11 10:17:25 +0000

    ff27f21 build: update all non-major dependencies (#61954)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-06979c6
+ * @license Angular v20.0.2+sha-ff27f21
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.2+sha-06979c6');
+const VERSION = new Version('20.0.2+sha-ff27f21');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-mlGavL8F.d.ts
+++ router_module.d-mlGavL8F.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-06979c6
+ * @license Angular v20.0.2+sha-ff27f21
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
