eba470c51c6d1a2527d29efc1b04588693de5a6f
Andrew Kushnir <akushnir@google.com>
2025-06-11 00:39:43 +0000

    229c6b4 build: update dependency typescript to v5.8.3 (#61958)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-eef6faf
+ * @license Angular v20.0.2+sha-229c6b4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.2+sha-eef6faf');
+const VERSION = new Version('20.0.2+sha-229c6b4');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-eef6faf
+ * @license Angular v20.0.2+sha-229c6b4
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
