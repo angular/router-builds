f0ab1c6be442465225b47cdf978601af1982cd59
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-11 11:47:04 +0000

    554b81e docs(docs-infra): retain the results in the search dialog while typing (#61935)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-ff27f21
+ * @license Angular v20.0.2+sha-554b81e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.2+sha-ff27f21');
+const VERSION = new Version('20.0.2+sha-554b81e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-ff27f21
+ * @license Angular v20.0.2+sha-554b81e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
