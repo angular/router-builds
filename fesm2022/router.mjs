b25313a93fd87f86ae69f36bb8eb3bff5659f1f8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-12 10:29:09 +0000

    61fb6e7 build: fix hermetic execution of `packages/core/test/...` (#62027)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.1+sha-6f779b2
+ * @license Angular v20.1.0-next.1+sha-61fb6e7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.1+sha-6f779b2');
+const VERSION = new Version('20.1.0-next.1+sha-61fb6e7');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
