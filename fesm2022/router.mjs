077541a4a7ce2f80efd159ecdbb5a76440d4b428
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-13 10:46:43 +0000

    be2747e fix(devtools): errors caused by the injector tree (#62029)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.1+sha-31da435
+ * @license Angular v20.1.0-next.1+sha-be2747e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.1+sha-31da435');
+const VERSION = new Version('20.1.0-next.1+sha-be2747e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.1+sha-31da435
+ * @license Angular v20.1.0-next.1+sha-be2747e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
