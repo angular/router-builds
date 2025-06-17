8d1e504935c42c155ea02bd5c38c4ef1afdc4ce5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-17 09:31:32 +0000

    fb56d51 docs(router): improve discoverability of route guard section (#62060)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-734c13a
+ * @license Angular v20.0.3+sha-fb56d51
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.3+sha-734c13a');
+const VERSION = new Version('20.0.3+sha-fb56d51');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-734c13a
+ * @license Angular v20.0.3+sha-fb56d51
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
