521ea6e6cdbb718163725f9a9c254f33930f6047
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-17 09:32:08 +0000

    89efd27 refactor(devtools): create devtools signals view (#61919)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.1+sha-132fd3c
+ * @license Angular v20.1.0-next.1+sha-89efd27
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.1+sha-132fd3c');
+const VERSION = new Version('20.1.0-next.1+sha-89efd27');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-mlGavL8F.d.ts
+++ router_module.d-mlGavL8F.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.1+sha-132fd3c
+ * @license Angular v20.1.0-next.1+sha-89efd27
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
