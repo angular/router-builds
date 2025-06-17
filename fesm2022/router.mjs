53ed8a5db03dbd7b47a88a56034ef527f3ae83a0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-17 17:03:22 +0000

    18a7ce8 build: migrate back to using the @npm// sourced benchmark_test macro (#62101)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.1+sha-2e0c98b
+ * @license Angular v20.1.0-next.1+sha-18a7ce8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.1+sha-2e0c98b');
+const VERSION = new Version('20.1.0-next.1+sha-18a7ce8');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
