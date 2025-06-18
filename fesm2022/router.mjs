4c8490795491dc22d8f62df941776dad41d5d42e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-18 07:54:12 +0000

    2e1e9aa docs: remove duplicate entry for tagged template literals (#62082)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-24b4484
+ * @license Angular v20.0.3+sha-2e1e9aa
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.3+sha-24b4484');
+const VERSION = new Version('20.0.3+sha-2e1e9aa');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-24b4484
+ * @license Angular v20.0.3+sha-2e1e9aa
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
