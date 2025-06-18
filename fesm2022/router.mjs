290818956fc9dc5397ea5b34feae85ff93c1e327
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-18 16:12:03 +0000

    f201df9 build: migrate to using new jasmine_test (#62106)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-b40613f
+ * @license Angular v20.0.3+sha-f201df9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.3+sha-b40613f');
+const VERSION = new Version('20.0.3+sha-f201df9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-b40613f
+ * @license Angular v20.0.3+sha-f201df9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
