1733d5ba50ea0db7e084515a94e7a3ff378e86b7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-18 12:09:25 +0000

    ae212b5 fix(core): Wrap ErrorEvent with no error property (#62081)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-4aa262b
+ * @license Angular v20.0.3+sha-ae212b5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.3+sha-4aa262b');
+const VERSION = new Version('20.0.3+sha-ae212b5');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-4aa262b
+ * @license Angular v20.0.3+sha-ae212b5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
