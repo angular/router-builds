ffc45aa48bc4f61913159176b3b6e5a4ffe59ae6
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-06-19 13:22:41 +0000

    fa5ae92 refactor(core): update FakeNavigation to the latest spec (#62017)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.2+sha-900cd37
+ * @license Angular v20.1.0-next.2+sha-fa5ae92
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.2+sha-900cd37');
+const VERSION = new Version('20.1.0-next.2+sha-fa5ae92');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.2+sha-900cd37
+ * @license Angular v20.1.0-next.2+sha-fa5ae92
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
