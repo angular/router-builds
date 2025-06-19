27a38efdac66a07404ceeca6fa2e1035c912c7a6
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-06-19 10:49:33 +0000

    f042cb3 docs(docs-infra): fix code-editor flakiness (#62143)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-288700c
+ * @license Angular v20.0.4+sha-f042cb3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-288700c');
+const VERSION = new Version('20.0.4+sha-f042cb3');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-288700c
+ * @license Angular v20.0.4+sha-f042cb3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
