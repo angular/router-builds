7dac45f9a6b41df36ec77fdf8bd3263d291d6fc5
Andrew Kushnir <akushnir@google.com>
2025-06-20 20:16:11 +0000

    2020005 docs: update Angular CLI help (#62167)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-dff1417
+ * @license Angular v20.0.4+sha-2020005
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-dff1417');
+const VERSION = new Version('20.0.4+sha-2020005');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-dff1417
+ * @license Angular v20.0.4+sha-2020005
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
