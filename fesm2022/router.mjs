662482beecee153e6f1af3baa02c6fd8ee5bec3f
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-06-23 12:37:51 +0000

    d25a6a0 fix(compiler-cli): handle initializer APIs wrapped in type casts (#62203)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.2+sha-f364d50
+ * @license Angular v20.1.0-next.2+sha-d25a6a0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.2+sha-f364d50');
+const VERSION = new Version('20.1.0-next.2+sha-d25a6a0');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d-dBTUdUNJ.d.ts
+++ router_module.d-dBTUdUNJ.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.2+sha-f364d50
+ * @license Angular v20.1.0-next.2+sha-d25a6a0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
