6c23548b62866e046f745fb526f026f0268544f5
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-06-23 12:09:00 +0000

    e909287 build: remove unused dependency (#62206) (#62210)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-f8a6537
+ * @license Angular v20.0.4+sha-e909287
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-f8a6537');
+const VERSION = new Version('20.0.4+sha-e909287');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
