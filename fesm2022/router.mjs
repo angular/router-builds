3d74029893daa4520710942854fbdf7c358507e0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-24 15:15:54 +0000

    9d20d6f Revert "test(devtools): revive cypress tests (#61972)" (#62243)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-a42169a
+ * @license Angular v20.0.4+sha-9d20d6f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-a42169a');
+const VERSION = new Version('20.0.4+sha-9d20d6f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
