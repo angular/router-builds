e50ab4d69e261d2ce79357d087a2c8329b34e8aa
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-25 09:59:08 +0000

    b489f9f refactor(language-service): add methods for decoding classifications to public api (#62251)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.2+sha-73269cf
+ * @license Angular v20.1.0-next.2+sha-b489f9f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.2+sha-73269cf');
+const VERSION = new Version('20.1.0-next.2+sha-b489f9f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.2+sha-73269cf
+ * @license Angular v20.1.0-next.2+sha-b489f9f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
