2a5e433ab97293f244ab1f0f4553bbae64058447
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-25 17:15:15 +0000

    a7a6a5b refactor(devtools): inject number and symbol values with injection tokens (#56898)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-c6f0f54
+ * @license Angular v20.0.5+sha-a7a6a5b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-c6f0f54');
+const VERSION = new Version('20.0.5+sha-a7a6a5b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-c6f0f54
+ * @license Angular v20.0.5+sha-a7a6a5b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
