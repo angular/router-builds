2c07845ea6024a4e3b33441a9d4347e3b55b7292
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-25 13:33:08 +0000

    a2e6f31 fix(core): allow to set a resource in an error state (#62253)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-17a8ca3
+ * @license Angular v20.0.4+sha-a2e6f31
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-17a8ca3');
+const VERSION = new Version('20.0.4+sha-a2e6f31');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
