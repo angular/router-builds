11a5c3abf240e8c2a4464d8020dff340ae316930
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 12:15:29 +0000

    a3c246a refactor(common): remove redundant `ɵloadImpl` check in client bundles (#62191)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-8b861e2
+ * @license Angular v20.0.5+sha-a3c246a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-8b861e2');
+const VERSION = new Version('20.0.5+sha-a3c246a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-8b861e2
+ * @license Angular v20.0.5+sha-a3c246a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
