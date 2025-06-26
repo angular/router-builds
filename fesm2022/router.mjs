7a7a6e28b6e199424f8e4a3834ae4163886ff90f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 14:46:29 +0000

    402eaa1 docs: fix deferrable views doc links (#62293)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-13875d0
+ * @license Angular v20.1.0-next.3+sha-402eaa1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-13875d0');
+const VERSION = new Version('20.1.0-next.3+sha-402eaa1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
