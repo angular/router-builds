cd3089bcf99604c3756aabfb25aa50bb66e7eb9c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 10:36:52 +0000

    92ecd69 build: update dependency rollup to v4.44.1 (#62286)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-029043c
+ * @license Angular v20.0.5+sha-92ecd69
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-029043c');
+const VERSION = new Version('20.0.5+sha-92ecd69');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-029043c
+ * @license Angular v20.0.5+sha-92ecd69
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
