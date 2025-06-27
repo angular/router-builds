dc2772844afede31a5eabc4d53b68c5be4c52c35
Joey Perrott <josephperrott@gmail.com>
2025-06-27 22:35:54 +0000

    8d58150 build: migrate modules to use `@rules_browsers` protractor rules (#62345)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-0866077
+ * @license Angular v20.0.5+sha-8d58150
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-0866077');
+const VERSION = new Version('20.0.5+sha-8d58150');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-0866077
+ * @license Angular v20.0.5+sha-8d58150
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
