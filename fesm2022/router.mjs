a5443c4bd1d79b017a6c5e0e5c7ab63738506a51
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 18:45:06 +0000

    5357539 ci: add jkrems to pullapprove (public-api) (#62340)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-27b7ec0
+ * @license Angular v20.1.0-next.3+sha-5357539
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-27b7ec0');
+const VERSION = new Version('20.1.0-next.3+sha-5357539');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
