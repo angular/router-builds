7d8829b12651add78defccc9e826eab99b486b5c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 09:07:57 +0000

    41230e6 build: update dependency marked to v16 (#62322)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-308c82d
+ * @license Angular v20.0.5+sha-41230e6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-308c82d');
+const VERSION = new Version('20.0.5+sha-41230e6');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-308c82d
+ * @license Angular v20.0.5+sha-41230e6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
