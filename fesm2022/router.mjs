18330ca6cede24aaf0fab84ec8b404d5edd96e17
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 09:35:14 +0000

    e8addbf refactor(devtools): improve flamegraph UI by employing the new flamegraph API (#62299)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-5ccb81e
+ * @license Angular v20.1.0-next.3+sha-e8addbf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-5ccb81e');
+const VERSION = new Version('20.1.0-next.3+sha-e8addbf');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-5ccb81e
+ * @license Angular v20.1.0-next.3+sha-e8addbf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
