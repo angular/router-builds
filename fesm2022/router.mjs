08882688833cbbb4e24a262d6a02b8212f5a37b1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 12:03:15 +0000

    c0532f0 docs: add page NG8115 (uninvokedTrackFunction) (#61962)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-82cdd5c
+ * @license Angular v20.0.5+sha-c0532f0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-82cdd5c');
+const VERSION = new Version('20.0.5+sha-c0532f0');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-82cdd5c
+ * @license Angular v20.0.5+sha-c0532f0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
