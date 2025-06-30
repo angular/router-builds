4a2c992d87af2cff4b6612d3ef5f6dc4952728bc
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 09:39:21 +0000

    d283595 fix(devtools): sanitize route data. (#62339)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-a4300a7
+ * @license Angular v20.1.0-next.3+sha-d283595
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-a4300a7');
+const VERSION = new Version('20.1.0-next.3+sha-d283595');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-a4300a7
+ * @license Angular v20.1.0-next.3+sha-d283595
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
