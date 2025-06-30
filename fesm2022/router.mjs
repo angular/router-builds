e1c3662e269ea4624180311035e48babd8675059
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 08:24:33 +0000

    85d6474 refactor(core): update NG0303 error message (#62363)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-5edbb54
+ * @license Angular v20.0.5+sha-85d6474
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-5edbb54');
+const VERSION = new Version('20.0.5+sha-85d6474');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-5edbb54
+ * @license Angular v20.0.5+sha-85d6474
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
