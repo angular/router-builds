ad3f4301c7bbfb739285d6aba64cf788f13bfb4d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 09:34:05 +0000

    7c82f26 build: lock file maintenance (#62369)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-4bc8187
+ * @license Angular v20.0.5+sha-7c82f26
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-4bc8187');
+const VERSION = new Version('20.0.5+sha-7c82f26');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
