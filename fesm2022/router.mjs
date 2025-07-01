451fdd56774b972e798dd0998020c0a090d41e64
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-01 09:42:27 +0000

    9cd1c42 docs: update breaking change for nullish coalesching + parenthesis (#62382)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-bf881cc
+ * @license Angular v20.0.5+sha-9cd1c42
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-bf881cc');
+const VERSION = new Version('20.0.5+sha-9cd1c42');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-bf881cc
+ * @license Angular v20.0.5+sha-9cd1c42
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
