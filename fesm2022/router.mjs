6f97520e854b38ca1c7d0c8764b398bc60c4d999
Andrew Kushnir <akushnir@google.com>
2025-07-01 19:29:00 +0000

    fb1bc99 ci: fix incorrect base branch in renovate config (#62406)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-4ac0147
+ * @license Angular v20.2.0-next.0+sha-fb1bc99
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-4ac0147');
+const VERSION = new Version('20.2.0-next.0+sha-fb1bc99');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-4ac0147
+ * @license Angular v20.2.0-next.0+sha-fb1bc99
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
