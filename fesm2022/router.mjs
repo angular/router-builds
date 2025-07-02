1ec0c3c29ae7cbfdd7645229ef7474cb03f8718f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 17:15:34 +0000

    b49f2db refactor(compiler-cli): do not rely on `url` when loading file system logic (#62431)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-567597a
+ * @license Angular v20.2.0-next.0+sha-b49f2db
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-567597a');
+const VERSION = new Version('20.2.0-next.0+sha-b49f2db');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-567597a
+ * @license Angular v20.2.0-next.0+sha-b49f2db
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
