cbf1458e2d61889db827873ab33548626ffc7b43
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 16:19:22 +0000

    d1d131a build: migrate devtools to use packaged version of @angular/* packages (#62413)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-42a3dbb
+ * @license Angular v20.1.0-rc.0+sha-d1d131a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-42a3dbb');
+const VERSION = new Version('20.1.0-rc.0+sha-d1d131a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-42a3dbb
+ * @license Angular v20.1.0-rc.0+sha-d1d131a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
