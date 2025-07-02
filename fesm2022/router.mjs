cc71ffc5f53b8e9ef1a40bdd90be60c6a1a00119
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 14:56:36 +0000

    42a3dbb docs: Update documentation httpResource with credentials option (#62415)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-56769de
+ * @license Angular v20.1.0-rc.0+sha-42a3dbb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-56769de');
+const VERSION = new Version('20.1.0-rc.0+sha-42a3dbb');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-56769de
+ * @license Angular v20.1.0-rc.0+sha-42a3dbb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
