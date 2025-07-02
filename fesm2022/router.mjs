dceb50ec43e18366992a1b04e0789d25b5d05b6c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 17:06:09 +0000

    4c332c3 docs: fix formatting (#62400)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.6+sha-e0e78fe
+ * @license Angular v20.0.6+sha-4c332c3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.6+sha-e0e78fe');
+const VERSION = new Version('20.0.6+sha-4c332c3');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.6+sha-e0e78fe
+ * @license Angular v20.0.6+sha-4c332c3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
