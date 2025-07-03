369c6d163c193fc10d309cd8313df6c2429c9679
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 09:45:13 +0000

    902121e refactor(migrations): ensure tsurge can properly emit references in g3 (#62447)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-aad1e74
+ * @license Angular v20.2.0-next.0+sha-902121e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-aad1e74');
+const VERSION = new Version('20.2.0-next.0+sha-902121e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
