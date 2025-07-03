0183e38c49c1949a254159780534351c46e17d7f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 12:00:16 +0000

    2943591 docs: improve support for decorator APIs (#60411)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-fca058a
+ * @license Angular v20.1.0-rc.0+sha-2943591
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-fca058a');
+const VERSION = new Version('20.1.0-rc.0+sha-2943591');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-fca058a
+ * @license Angular v20.1.0-rc.0+sha-2943591
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
