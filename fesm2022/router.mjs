0c108867b3ba48f8cc785ae24a2e976aa3c21fe5
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 09:24:00 +0000

    f5f80d8 ci: update pullapprove reviewers (#62448)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-f5a1020
+ * @license Angular v20.1.0-rc.0+sha-f5f80d8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-f5a1020');
+const VERSION = new Version('20.1.0-rc.0+sha-f5f80d8');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-f5a1020
+ * @license Angular v20.1.0-rc.0+sha-f5f80d8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
