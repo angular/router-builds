2e9aede0fd21deedc646bc52e1a5dd6a91ecd68e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-04 11:09:20 +0000

    434508f docs: fix typo on guidelines file (#62468)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.6+sha-aa04a4a
+ * @license Angular v20.0.6+sha-434508f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.6+sha-aa04a4a');
+const VERSION = new Version('20.0.6+sha-434508f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.6+sha-aa04a4a
+ * @license Angular v20.0.6+sha-434508f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
