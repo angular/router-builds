0103f1588c95e34d2b0b48819eeaca3b530826a8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 18:38:21 +0000

    40e52b9 build: disable firefox tests in devtools (#62504)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-136c051
+ * @license Angular v20.2.0-next.0+sha-40e52b9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-136c051');
+const VERSION = new Version('20.2.0-next.0+sha-40e52b9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
