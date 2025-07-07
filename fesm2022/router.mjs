2474cb6a7fff03ec7c4ba22159e66207e08c9e6f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 21:03:41 +0000

    efc6405 build: update devinfra digest to 6f54d14 (#62508)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-aee83d1
+ * @license Angular v20.1.0-rc.0+sha-efc6405
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-aee83d1');
+const VERSION = new Version('20.1.0-rc.0+sha-efc6405');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-aee83d1
+ * @license Angular v20.1.0-rc.0+sha-efc6405
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
