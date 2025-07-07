b07d5605abf3bd7d5b023ae89016050d040696f9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-07-07 11:10:01 +0000

    5fca07c refactor(compiler-cli): ensure mock file system can be loaded in the browser (#62493)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.6+sha-434508f
+ * @license Angular v20.0.6+sha-5fca07c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.6+sha-434508f');
+const VERSION = new Version('20.0.6+sha-5fca07c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.6+sha-434508f
+ * @license Angular v20.0.6+sha-5fca07c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
