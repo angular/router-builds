d85178666e4120ae7d3fe4dd122a7ec0dbc39cde
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 15:44:16 +0000

    30218af build: update dependency aspect_rules_ts to v3.6.2 (#62477)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-d774314
+ * @license Angular v20.1.0-rc.0+sha-30218af
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-d774314');
+const VERSION = new Version('20.1.0-rc.0+sha-30218af');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-d774314
+ * @license Angular v20.1.0-rc.0+sha-30218af
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
