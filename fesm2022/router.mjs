1ae4f965dedd428a0bf67f92bbfd66b01423954e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 16:23:51 +0000

    633d01b refactor(compiler): ensure compiler passes closure conformance checks (#62500)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-c6a720b
+ * @license Angular v20.2.0-next.0+sha-633d01b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-c6a720b');
+const VERSION = new Version('20.2.0-next.0+sha-633d01b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-c6a720b
+ * @license Angular v20.2.0-next.0+sha-633d01b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
