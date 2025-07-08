312a706ee4f8ede52a0a1d9c0a810a4197689b75
Andrew Kushnir <akushnir@google.com>
2025-07-08 18:38:29 +0000

    c4de0c0 fix(docs-infra): fix tutorial formatting issues (for v20) (#62457)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-1a8297b
+ * @license Angular v20.1.0-rc.0+sha-c4de0c0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-1a8297b');
+const VERSION = new Version('20.1.0-rc.0+sha-c4de0c0');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
