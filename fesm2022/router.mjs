c4c8833debb4a8f9840dc2ec7d777cd6aa11a9d4
Andrew Kushnir <akushnir@google.com>
2025-07-08 23:11:45 +0000

    fed402f build: update dependency aspect_rules_ts to v3.6.3 (#62514)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-dda655f
+ * @license Angular v20.1.0-rc.0+sha-fed402f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-rc.0+sha-dda655f');
+const VERSION = new Version('20.1.0-rc.0+sha-fed402f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-rc.0+sha-dda655f
+ * @license Angular v20.1.0-rc.0+sha-fed402f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
