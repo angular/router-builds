862246d7ee44718e41805dd0e97e0a0d25117597
Andrew Kushnir <akushnir@google.com>
2025-07-09 19:54:05 +0000

    276836e fix(http): do not display warnings `Angular detected that a `HttpClient` request with the `keepalive` option was sent using XHR` when option is not true (#62536)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0+sha-bf1b51c
+ * @license Angular v20.1.0+sha-276836e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0+sha-bf1b51c');
+const VERSION = new Version('20.1.0+sha-276836e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0+sha-bf1b51c
+ * @license Angular v20.1.0+sha-276836e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
