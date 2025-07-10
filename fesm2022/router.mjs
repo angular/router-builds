cb099a31e070d0d124b5259f66bd8b504134febd
Andrew Kushnir <akushnir@google.com>
2025-07-10 20:00:41 +0000

    aea0a70 ci: adjust renovate teams (#62578)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0+sha-6549418
+ * @license Angular v20.1.0+sha-aea0a70
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0+sha-6549418');
+const VERSION = new Version('20.1.0+sha-aea0a70');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
