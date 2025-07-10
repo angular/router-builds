ba6c5e6911afd7fb1b396fa41e02e6290c412703
Andrew Kushnir <akushnir@google.com>
2025-07-10 20:53:02 +0000

    4f1e88e build: use esbuild from aspect rules (#62568)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0+sha-aea0a70
+ * @license Angular v20.1.0+sha-4f1e88e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0+sha-aea0a70');
+const VERSION = new Version('20.1.0+sha-4f1e88e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0+sha-aea0a70
+ * @license Angular v20.1.0+sha-4f1e88e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
