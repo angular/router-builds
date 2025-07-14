702ebf1d9832f8587c5b3e6cf1d3bc02a5639baf
Andrew Kushnir <akushnir@google.com>
2025-07-14 21:58:29 +0000

    74beeec docs: correctly look for examples in the bin_dir instead of execPath (#62632)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0+sha-f202160
+ * @license Angular v20.1.0+sha-74beeec
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0+sha-f202160');
+const VERSION = new Version('20.1.0+sha-74beeec');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
