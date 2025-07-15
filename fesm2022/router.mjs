cd967355583a68d2a15f4a852113ca692f012529
Andrew Kushnir <akushnir@google.com>
2025-07-15 16:47:48 +0000

    4574095 build: update all non-major dependencies (#62642)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-80da073
+ * @license Angular v20.2.0-next.0+sha-4574095
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-80da073');
+const VERSION = new Version('20.2.0-next.0+sha-4574095');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-80da073
+ * @license Angular v20.2.0-next.0+sha-4574095
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
