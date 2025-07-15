021580c8b254aab3fcfb654ab6ba94f0bb4f7f8d
Andrew Kushnir <akushnir@google.com>
2025-07-15 20:35:21 +0000

    70c8780 fix(compiler): more permissive parsing of @ characters (#62644)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0+sha-acf8b74
+ * @license Angular v20.1.0+sha-70c8780
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0+sha-acf8b74');
+const VERSION = new Version('20.1.0+sha-70c8780');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
