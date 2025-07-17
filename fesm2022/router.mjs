42394bda1b7af04d8902db13866d8a17e94ba8c4
Kirill Cherkashin <kirjs@google.com>
2025-07-17 18:14:00 +0000

    dcab8c9 refactor(devtools): rearrange main toolbar menu buttons (#62684)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.1+sha-1930e99
+ * @license Angular v20.1.1+sha-dcab8c9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.1+sha-1930e99');
+const VERSION = new Version('20.1.1+sha-dcab8c9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.1+sha-1930e99
+ * @license Angular v20.1.1+sha-dcab8c9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
