64475f37740cdf401a7c26f10c3c9b31152d0dce
Kirill Cherkashin <kirjs@google.com>
2025-07-17 18:30:17 +0000

    8ad10fd fix(compiler): fix detection of directive deps in JIT (#62666)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.1+sha-dcab8c9
+ * @license Angular v20.1.1+sha-8ad10fd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.1+sha-dcab8c9');
+const VERSION = new Version('20.1.1+sha-8ad10fd');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
