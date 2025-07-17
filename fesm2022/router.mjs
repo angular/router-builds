034fd178acf5f8bc70756b033244a6ac65db8802
Kirill Cherkashin <kirjs@google.com>
2025-07-17 17:35:35 +0000

    1930e99 docs: Update versions.md to use canonical Baseline site (#62680)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.1+sha-0db3613
+ * @license Angular v20.1.1+sha-1930e99
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.1+sha-0db3613');
+const VERSION = new Version('20.1.1+sha-1930e99');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.1+sha-0db3613
+ * @license Angular v20.1.1+sha-1930e99
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
