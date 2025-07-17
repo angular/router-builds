0fa46ceb90e34e7da353bd93b1c035de671c4816
Kirill Cherkashin <kirjs@google.com>
2025-07-17 17:36:58 +0000

    2e6c2ff refactor(devtools): disable view source button if source function is not available (#62678)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-fc8247d
+ * @license Angular v20.2.0-next.1+sha-2e6c2ff
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-fc8247d');
+const VERSION = new Version('20.2.0-next.1+sha-2e6c2ff');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-fc8247d
+ * @license Angular v20.2.0-next.1+sha-2e6c2ff
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
