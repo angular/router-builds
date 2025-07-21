82d8dbef7b2506b1f9edf5a20b2bfbb55a828a59
Kirill Cherkashin <kirjs@google.com>
2025-07-21 20:43:35 +0000

    45d3eef test(devtools): bump cypress timeout to 10 seconds (#62726)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-f041042
+ * @license Angular v20.1.2+sha-45d3eef
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-f041042');
+const VERSION = new Version('20.1.2+sha-45d3eef');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-f041042
+ * @license Angular v20.1.2+sha-45d3eef
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
