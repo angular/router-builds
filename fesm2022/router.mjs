f180b0a3811fb43796aad71f38c3c88c2a04885d
Kirill Cherkashin <kirjs@google.com>
2025-07-21 14:30:34 +0000

    a1a78b2 build: update all non-major dependencies (#62716)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-cf48b13
+ * @license Angular v20.1.2+sha-a1a78b2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-cf48b13');
+const VERSION = new Version('20.1.2+sha-a1a78b2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-cf48b13
+ * @license Angular v20.1.2+sha-a1a78b2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
