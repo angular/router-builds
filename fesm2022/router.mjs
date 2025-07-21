32e33864ef7d3a1bbf8fb44ce968bb3a0e14c591
Kirill Cherkashin <kirjs@google.com>
2025-07-21 21:10:23 +0000

    38b7ab2 refactor(core): add a `getTransferState` for the devtools (#62722)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-45d3eef
+ * @license Angular v20.1.2+sha-38b7ab2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-45d3eef');
+const VERSION = new Version('20.1.2+sha-38b7ab2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-45d3eef
+ * @license Angular v20.1.2+sha-38b7ab2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
