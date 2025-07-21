fa875540d8d2e424d56646bc10ba87d865eff95e
Kirill Cherkashin <kirjs@google.com>
2025-07-21 14:16:33 +0000

    a6b2d7d fix(devtools): add event tagging to prevent DDOS (#62645)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-97b1bd1
+ * @license Angular v20.2.0-next.1+sha-a6b2d7d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-97b1bd1');
+const VERSION = new Version('20.2.0-next.1+sha-a6b2d7d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-97b1bd1
+ * @license Angular v20.2.0-next.1+sha-a6b2d7d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
