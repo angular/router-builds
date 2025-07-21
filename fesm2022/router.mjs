1576f71fb94f7e2ad6349b7b4b0c70ddd89d0b46
Kirill Cherkashin <kirjs@google.com>
2025-07-21 16:14:51 +0000

    70d0c10 docs: update Angular CLI help (#62730)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-bca8a3c
+ * @license Angular v20.1.2+sha-70d0c10
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-bca8a3c');
+const VERSION = new Version('20.1.2+sha-70d0c10');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-bca8a3c
+ * @license Angular v20.1.2+sha-70d0c10
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
