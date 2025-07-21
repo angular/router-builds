e899b0a2e36332b808de9429354b384e13630cad
Kirill Cherkashin <kirjs@google.com>
2025-07-21 16:26:06 +0000

    aee5260 fix(devtools): stop reseting currentlyMatchedIndex when a node is selected in the component explorer (#62727)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-70d0c10
+ * @license Angular v20.1.2+sha-aee5260
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-70d0c10');
+const VERSION = new Version('20.1.2+sha-aee5260');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
