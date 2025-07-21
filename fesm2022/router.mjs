d9e918ce865fad2c23858bbf087ffae1e2c2dbc3
Kirill Cherkashin <kirjs@google.com>
2025-07-21 20:20:20 +0000

    f041042 build: update cross-repo angular dependencies (#62731)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-74e4778
+ * @license Angular v20.1.2+sha-f041042
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-74e4778');
+const VERSION = new Version('20.1.2+sha-f041042');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-74e4778
+ * @license Angular v20.1.2+sha-f041042
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
