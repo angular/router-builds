dde63c4d7f95354efcd8516c188034dfa48a27f0
Kirill Cherkashin <kirjs@google.com>
2025-07-22 14:22:39 +0000

    db8f7ce build: move generating doc files for @angular/ssr to using an intermediate typescript file (#62734)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-38b7ab2
+ * @license Angular v20.1.2+sha-db8f7ce
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-38b7ab2');
+const VERSION = new Version('20.1.2+sha-db8f7ce');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-38b7ab2
+ * @license Angular v20.1.2+sha-db8f7ce
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
