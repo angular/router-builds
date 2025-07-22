4e9f5b7b0988ab91ee3636469d2d04de5c899322
Kirill Cherkashin <kirjs@google.com>
2025-07-22 18:27:25 +0000

    0a9f55d build: update dependency angular-split to v20 (#62740)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-92e03e1
+ * @license Angular v20.2.0-next.1+sha-0a9f55d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-92e03e1');
+const VERSION = new Version('20.2.0-next.1+sha-0a9f55d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
