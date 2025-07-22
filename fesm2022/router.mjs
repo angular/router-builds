4129318be6c51e5c9d7988a27816b3819fc0b00f
Kirill Cherkashin <kirjs@google.com>
2025-07-22 16:13:57 +0000

    92e03e1 build: update @angular/ng-dev digest to 75a2aee (#62744)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-4ac6171
+ * @license Angular v20.2.0-next.1+sha-92e03e1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-4ac6171');
+const VERSION = new Version('20.2.0-next.1+sha-92e03e1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-4ac6171
+ * @license Angular v20.2.0-next.1+sha-92e03e1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
