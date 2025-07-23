80beff284d444c7407b7b586c9c6bee6867ee47d
Kirill Cherkashin <kirjs@google.com>
2025-07-23 19:09:55 +0000

    063b5e1 fix(core): switch check to documentElement with chaining (#62773)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-9562b63
+ * @license Angular v20.2.0-next.1+sha-063b5e1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-9562b63');
+const VERSION = new Version('20.2.0-next.1+sha-063b5e1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
