b328c76a2f3699e4890846d96f1b5a5c8bfe4764
Kirill Cherkashin <kirjs@google.com>
2025-07-23 15:16:05 +0000

    2fd1f7b fix(platform-browser): resolve component resources before bootstrapping in JIT mode (#62758)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-b7c745b
+ * @license Angular v20.1.2+sha-2fd1f7b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-b7c745b');
+const VERSION = new Version('20.1.2+sha-2fd1f7b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-b7c745b
+ * @license Angular v20.1.2+sha-2fd1f7b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
