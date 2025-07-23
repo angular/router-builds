dcc71a4092f2569016e66349524ce32e217d0ba4
Kirill Cherkashin <kirjs@google.com>
2025-07-23 17:03:44 +0000

    593cc8a fix(core): checks if body exists before continuing (#62768)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-a4a4f7f
+ * @license Angular v20.2.0-next.1+sha-593cc8a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.1+sha-a4a4f7f');
+const VERSION = new Version('20.2.0-next.1+sha-593cc8a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-a4a4f7f
+ * @license Angular v20.2.0-next.1+sha-593cc8a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
