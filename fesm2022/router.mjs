7da3fcb2175c7377d114812e5233072c7514d61e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 15:59:13 +0000

    61eeb9b refactor(devtools): simular transfer cache data in the devtools demo app. (#62665)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-24e6f4d
+ * @license Angular v20.2.0-next.2+sha-61eeb9b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-24e6f4d');
+const VERSION = new Version('20.2.0-next.2+sha-61eeb9b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-24e6f4d
+ * @license Angular v20.2.0-next.2+sha-61eeb9b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
