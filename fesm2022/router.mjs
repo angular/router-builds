ad955fc98d4fbfdc5ca9bc0cbbb32a5dbff00391
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 11:11:06 +0000

    94b0ef1 build: update github/codeql-action action to v3.29.4 (#62789)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-92b0802
+ * @license Angular v20.2.0-next.2+sha-94b0ef1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-92b0802');
+const VERSION = new Version('20.2.0-next.2+sha-94b0ef1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
