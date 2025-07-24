66b63624a47aaada04d3bec0e9ea346b1226b0c2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 14:09:19 +0000

    da0fb33 docs: add initial documentation for MCP server tools (#62777)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-206d153
+ * @license Angular v20.1.3+sha-da0fb33
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.3+sha-206d153');
+const VERSION = new Version('20.1.3+sha-da0fb33');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
