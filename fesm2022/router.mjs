4044bb5cfd6af948bdf7e789d2b10b6506d1d0ae
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 12:03:45 +0000

    5aaf726 build: update dependency source-map to v0.7.6 (#62791)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-187cb9c
+ * @license Angular v20.1.3+sha-5aaf726
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.3+sha-187cb9c');
+const VERSION = new Version('20.1.3+sha-5aaf726');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-187cb9c
+ * @license Angular v20.1.3+sha-5aaf726
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
