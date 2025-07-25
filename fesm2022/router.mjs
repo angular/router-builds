3fa8b72a1373c098235b34f5470f7303ec9e49ce
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-25 11:46:25 +0000

    632eba5 build: update to the TypeScript 5.9 RC (#62815)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-c19d0ae
+ * @license Angular v20.2.0-next.2+sha-632eba5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-c19d0ae');
+const VERSION = new Version('20.2.0-next.2+sha-632eba5');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-c19d0ae
+ * @license Angular v20.2.0-next.2+sha-632eba5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
