bd64238142d8acda0e4beb7c1909694cef6bc228
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-25 15:04:29 +0000

    c66e9fe docs: fix link for NG8117 uninvokedFunctionInTextInterpolation (#62820)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-63a6b5a
+ * @license Angular v20.2.0-next.2+sha-c66e9fe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-63a6b5a');
+const VERSION = new Version('20.2.0-next.2+sha-c66e9fe');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-63a6b5a
+ * @license Angular v20.2.0-next.2+sha-c66e9fe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
