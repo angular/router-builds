00ee304c419178dc1c06aa5a318ed9e66b06f27d
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-28 12:30:40 +0000

    aa46ad2 build: update dependency rollup to v4.46.1 (#62833)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-9a8b5c7
+ * @license Angular v20.1.3+sha-aa46ad2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.3+sha-9a8b5c7');
+const VERSION = new Version('20.1.3+sha-aa46ad2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-9a8b5c7
+ * @license Angular v20.1.3+sha-aa46ad2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
