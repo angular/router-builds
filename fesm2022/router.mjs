d6204f2b17576cac542c4d6d8b0bc2539fb4646a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-29 17:21:10 +0000

    c9fb4e3 build: use ts_project based extract_types (#62875)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-ea1afec
+ * @license Angular v20.1.3+sha-c9fb4e3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.3+sha-ea1afec');
+const VERSION = new Version('20.1.3+sha-c9fb4e3');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-ea1afec
+ * @license Angular v20.1.3+sha-c9fb4e3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
