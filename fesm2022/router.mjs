7725e0ce0b0507d2c9d9e399bfa59c0d72a7494e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 17:43:03 +0000

    e7d748e build: remove @bazel/esbuild (#62906)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-335cdc7
+ * @license Angular v20.2.0-next.2+sha-e7d748e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-335cdc7');
+const VERSION = new Version('20.2.0-next.2+sha-e7d748e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
