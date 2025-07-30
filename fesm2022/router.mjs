6377eb67ab379e3269281fdba74807a0be84942f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 16:25:27 +0000

    335cdc7 build: update to latest devinfra in workspace (#62901)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-ecb50ad
+ * @license Angular v20.2.0-next.2+sha-335cdc7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-ecb50ad');
+const VERSION = new Version('20.2.0-next.2+sha-335cdc7');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
