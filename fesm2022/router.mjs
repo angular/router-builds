b787b79802d8d3493a443deb0c3ccf2f31b08188
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 10:07:40 +0000

    55e575a docs(docs-infra): increase until timeout (#62894)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-a2e3bfc
+ * @license Angular v20.1.3+sha-55e575a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.3+sha-a2e3bfc');
+const VERSION = new Version('20.1.3+sha-55e575a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-a2e3bfc
+ * @license Angular v20.1.3+sha-55e575a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
