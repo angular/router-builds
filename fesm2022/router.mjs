5a47b062abbb3a372b2eb34d06d100b4250d3a53
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-31 09:22:54 +0000

    8255e0c feat(service-worker): add messageerror event handling and logging (#62834)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.3+sha-78a6b68
+ * @license Angular v20.2.0-next.3+sha-8255e0c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.3+sha-78a6b68');
+const VERSION = new Version('20.2.0-next.3+sha-8255e0c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
