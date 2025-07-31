f1b2df23c1675e54c2decb0d65efb5b53b247d85
Joey Perrott <josephperrott@gmail.com>
2025-07-31 22:12:21 +0000

    82cc576 build: use pnpm as the package manager instead of yarn (#62924)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.3+sha-892c2d1
+ * @license Angular v20.2.0-next.3+sha-82cc576
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.3+sha-892c2d1');
+const VERSION = new Version('20.2.0-next.3+sha-82cc576');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
