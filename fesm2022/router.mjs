bc69a99aa699fcc94cdc9c56166eb07e89693f65
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 13:31:05 +0000

    f700bd9 build: update pnpm to v10.14.0 (#62946)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.4+sha-3a8fa72
+ * @license Angular v20.1.4+sha-f700bd9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.4+sha-3a8fa72');
+const VERSION = new Version('20.1.4+sha-f700bd9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.4+sha-3a8fa72
+ * @license Angular v20.1.4+sha-f700bd9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
