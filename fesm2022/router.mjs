dae5270a6ec0be2b2f41052dabdb674e83c42ec3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 08:18:09 +0000

    e5d6fb5 fix(http): Reset headers, progress, and statusCode when using `set()` in `HttpResource` (#62873)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.3+sha-d05138a
+ * @license Angular v20.2.0-next.3+sha-e5d6fb5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.3+sha-d05138a');
+const VERSION = new Version('20.2.0-next.3+sha-e5d6fb5');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.3+sha-d05138a
+ * @license Angular v20.2.0-next.3+sha-e5d6fb5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
