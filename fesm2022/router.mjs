01c888bd64d53a82ae8b4e87f477506ffd79c6d8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 14:28:51 +0000

    3e6e1c1 docs: Add `MAX_ANIMATION_TIMEOUT` to the animate docs (#62949)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.3+sha-857675f
+ * @license Angular v20.2.0-next.3+sha-3e6e1c1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.3+sha-857675f');
+const VERSION = new Version('20.2.0-next.3+sha-3e6e1c1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
