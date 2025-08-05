6f8369fc20e585f82be342404628bf8f46cedd22
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-05 16:21:29 +0000

    c966746 docs: add context7.json config (#63000)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.4+sha-16a4d80
+ * @license Angular v20.1.4+sha-c966746
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.4+sha-16a4d80');
+const VERSION = new Version('20.1.4+sha-c966746');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.4+sha-16a4d80
+ * @license Angular v20.1.4+sha-c966746
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
