227a12bc4948e488d67ff659b079c1ff9dd1372c
Andrew Scott <atscott@google.com>
2025-08-07 16:36:36 +0000

    bba7c6f build: remove unused old toolchain dependencies (#63039)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-8f234ff
+ * @license Angular v20.1.6+sha-bba7c6f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.6+sha-8f234ff');
+const VERSION = new Version('20.1.6+sha-bba7c6f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-8f234ff
+ * @license Angular v20.1.6+sha-bba7c6f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
