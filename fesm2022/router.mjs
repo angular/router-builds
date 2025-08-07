059728e815b5c32bf580e19810dd69aedc8bde30
Andrew Scott <atscott@google.com>
2025-08-07 16:16:30 +0000

    8f234ff docs(devtools): update `pnpm` install command (#63040)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-0430a56
+ * @license Angular v20.1.6+sha-8f234ff
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.6+sha-0430a56');
+const VERSION = new Version('20.1.6+sha-8f234ff');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-0430a56
+ * @license Angular v20.1.6+sha-8f234ff
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
