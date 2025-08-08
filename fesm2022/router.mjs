c486ddf92c5a40d75501b079418a9600ce225f92
Andrew Scott <atscott@google.com>
2025-08-08 18:11:29 +0000

    63aa602 docs(docs-infra): add next page reference (#62566)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-322042c
+ * @license Angular v20.1.6+sha-63aa602
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.6+sha-322042c');
+const VERSION = new Version('20.1.6+sha-63aa602');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-322042c
+ * @license Angular v20.1.6+sha-63aa602
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
