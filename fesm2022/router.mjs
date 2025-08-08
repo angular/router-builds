f6ecd2a06bfb2b7565b30e2ff9728f3a82f2bcf0
Andrew Scott <atscott@google.com>
2025-08-08 17:09:59 +0000

    6a11846 fix(core): prevents duplicate nodes when `@if` toggles with leave animations (#63048)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.6+sha-c0bfe23
+ * @license Angular v20.2.0-next.6+sha-6a11846
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.6+sha-c0bfe23');
+const VERSION = new Version('20.2.0-next.6+sha-6a11846');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.6+sha-c0bfe23
+ * @license Angular v20.2.0-next.6+sha-6a11846
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
