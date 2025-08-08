5da120dd6f90073594c620a0363f70df5311808e
Andrew Scott <atscott@google.com>
2025-08-08 18:12:14 +0000

    3b214d2 feat(service-worker): Logs unhandled promise rejections in service worker (#63059)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.6+sha-6a11846
+ * @license Angular v20.2.0-next.6+sha-3b214d2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.6+sha-6a11846');
+const VERSION = new Version('20.2.0-next.6+sha-3b214d2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
