c288bb620780fc160053c59c61278aea2796c2f4
Andrew Scott <atscott@google.com>
2025-08-11 22:48:26 +0000

    af247ca refactor(core): remove temporary resource setter function. (#63097)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.0+sha-499fa8f
+ * @license Angular v20.2.0-rc.0+sha-af247ca
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.0+sha-499fa8f');
+const VERSION = new Version('20.2.0-rc.0+sha-af247ca');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.0+sha-499fa8f
+ * @license Angular v20.2.0-rc.0+sha-af247ca
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
