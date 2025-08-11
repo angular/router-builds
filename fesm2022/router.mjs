9787a4239343eb9b6b44978288839447f774d37d
Andrew Scott <atscott@google.com>
2025-08-11 18:09:34 +0000

    06f5879 ci: several fixes for renovate (#63083)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0-next.0+sha-c4dcfdf
+ * @license Angular v20.3.0-next.0+sha-06f5879
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0-next.0+sha-c4dcfdf');
+const VERSION = new Version('20.3.0-next.0+sha-06f5879');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
