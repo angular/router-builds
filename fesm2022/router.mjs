61b9b78675fce31025b1ad5a91b21db0ffcf0aa7
Andrew Scott <atscott@google.com>
2025-08-11 19:21:07 +0000

    499fa8f docs: remove router testing from the testing scenarios guide (#63078)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.0+sha-79b9159
+ * @license Angular v20.2.0-rc.0+sha-499fa8f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.0+sha-79b9159');
+const VERSION = new Version('20.2.0-rc.0+sha-499fa8f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.0+sha-79b9159
+ * @license Angular v20.2.0-rc.0+sha-499fa8f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
