b6edc4a776ef10e193261fba05c6d2f30546ba5b
Andrew Scott <atscott@google.com>
2025-08-11 17:39:55 +0000

    c4dcfdf build: update to the latest version of devinfra and rename npm2 workspace (#63093)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0-next.0+sha-57184d0
+ * @license Angular v20.3.0-next.0+sha-c4dcfdf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0-next.0+sha-57184d0');
+const VERSION = new Version('20.3.0-next.0+sha-c4dcfdf');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0-next.0+sha-57184d0
+ * @license Angular v20.3.0-next.0+sha-c4dcfdf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
