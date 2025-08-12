35c72f78a1b6f5e7d56132fd0187adda0d567f34
Andrew Scott <atscott01@gmail.com>
2025-08-12 20:35:25 +0000

    ada3c37 refactor(compiler-cli): ensure JIT transforms with queries work with closure property renaming (#63123)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.0+sha-4635095
+ * @license Angular v20.2.0-rc.0+sha-ada3c37
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.0+sha-4635095');
+const VERSION = new Version('20.2.0-rc.0+sha-ada3c37');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.0+sha-4635095
+ * @license Angular v20.2.0-rc.0+sha-ada3c37
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
