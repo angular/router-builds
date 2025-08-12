28d8420bcfe7000759fc01ba70a6d57158657f31
Andrew Scott <atscott01@gmail.com>
2025-08-12 23:58:04 +0000

    d7a5ead build: update pnpm to v10 (#63087)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0-next.0+sha-ea78278
+ * @license Angular v20.3.0-next.0+sha-d7a5ead
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0-next.0+sha-ea78278');
+const VERSION = new Version('20.3.0-next.0+sha-d7a5ead');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0-next.0+sha-ea78278
+ * @license Angular v20.3.0-next.0+sha-d7a5ead
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
