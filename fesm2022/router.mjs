827ae4d25e2c56f581a88cebf835aba36a16643b
Andrew Scott <atscott@google.com>
2025-08-12 18:09:52 +0000

    bb5a69c fix(docs-infra): put content in `<main>` element (#63121)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-f251c7d
+ * @license Angular v20.1.6+sha-bb5a69c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.6+sha-f251c7d');
+const VERSION = new Version('20.1.6+sha-bb5a69c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-f251c7d
+ * @license Angular v20.1.6+sha-bb5a69c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
