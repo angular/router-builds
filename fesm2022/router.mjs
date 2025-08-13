ca51da2e2a634ee65587cf9b64856b76e4433e0f
Andrew Scott <atscott01@gmail.com>
2025-08-13 01:51:04 +0000

    657061e build: patch `@angular/build` to allow fw/cli mismatch (#63098)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0-next.0+sha-4d9385b
+ * @license Angular v21.0.0-next.0+sha-657061e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0-next.0+sha-4d9385b');
+const VERSION = new Version('21.0.0-next.0+sha-657061e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
