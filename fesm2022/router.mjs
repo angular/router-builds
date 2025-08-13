91605fab126b0c2a798d229f5c93274253ec8fd0
Andrew Scott <atscott01@gmail.com>
2025-08-13 16:20:21 +0000

    32ba8b5 docs: add link to ai-config command (#63139)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-91b3466
+ * @license Angular v21.0.0-next.0+sha-32ba8b5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-91b3466');
+const VERSION = new Version('21.0.0-next.0+sha-32ba8b5');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-91b3466
+ * @license Angular v21.0.0-next.0+sha-32ba8b5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
