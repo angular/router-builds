6efaf91be7955a2e6515c1331feb3c8e8e8fdeb6
Andrew Scott <atscott01@gmail.com>
2025-08-13 02:13:05 +0000

    91b3466 build: update actions/checkout action to v5 (#63114)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-657061e
+ * @license Angular v21.0.0-next.0+sha-91b3466
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-657061e');
+const VERSION = new Version('21.0.0-next.0+sha-91b3466');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-657061e
+ * @license Angular v21.0.0-next.0+sha-91b3466
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
