e6a474445a6ccade03965ebecacbd7ed95aeebc0
Andrew Scott <atscott01@gmail.com>
2025-08-13 16:34:55 +0000

    55cd8c2 docs: add rxjs-interop section for `rxResource` (#63125)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-32ba8b5
+ * @license Angular v21.0.0-next.0+sha-55cd8c2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-32ba8b5');
+const VERSION = new Version('21.0.0-next.0+sha-55cd8c2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
