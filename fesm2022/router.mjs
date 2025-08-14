03176a2fb56d75f7992537fbc5552070f85e817a
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-14 11:15:42 +0000

    8747f9a docs: fix edit links for block/element api entries (#62766)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-1cf4d34
+ * @license Angular v20.2.0-rc.1+sha-8747f9a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-1cf4d34');
+const VERSION = new Version('20.2.0-rc.1+sha-8747f9a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-1cf4d34
+ * @license Angular v20.2.0-rc.1+sha-8747f9a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
