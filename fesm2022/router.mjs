b76134095cd2f1c17d459b2fd55eb9ed79215023
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-15 11:47:45 +0000

    f43f8bb docs(docs-infra): replace Input by signal one (#63169)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-691f5ed
+ * @license Angular v20.1.7+sha-f43f8bb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.7+sha-691f5ed');
+const VERSION = new Version('20.1.7+sha-f43f8bb');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-691f5ed
+ * @license Angular v20.1.7+sha-f43f8bb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
