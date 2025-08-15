12b70d185391a34a609f88b8e2a84fcab659c92a
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-15 07:59:37 +0000

    5b3933f fix(compiler): error when ng-content fallback has translated children (#63156)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-5129dae
+ * @license Angular v21.0.0-next.0+sha-5b3933f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-5129dae');
+const VERSION = new Version('21.0.0-next.0+sha-5b3933f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
