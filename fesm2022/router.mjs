870af61a958408843911e38031abaa82604f9ea6
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-18 11:03:28 +0000

    726d602 ci: mark myself as unavailable during OOO (#63214)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-9e828d5
+ * @license Angular v20.2.0-rc.1+sha-726d602
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-9e828d5');
+const VERSION = new Version('20.2.0-rc.1+sha-726d602');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-9e828d5
+ * @license Angular v20.2.0-rc.1+sha-726d602
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
