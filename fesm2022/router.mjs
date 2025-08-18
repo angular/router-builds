03074728bd47b732e79347eb601d71cfff635d9d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-18 15:13:50 +0000

    b413b62 build: update the failure message with the correct command (#63219)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-bc3798d
+ * @license Angular v20.1.7+sha-b413b62
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.7+sha-bc3798d');
+const VERSION = new Version('20.1.7+sha-b413b62');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-bc3798d
+ * @license Angular v20.1.7+sha-b413b62
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
