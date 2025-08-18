99321d43a5fb9eae9d4885127431a383ae2c55b4
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-18 08:11:43 +0000

    5d31e20 docs(docs-infra): remove @adev/constant path (#63213)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-a9bab9e
+ * @license Angular v20.1.7+sha-5d31e20
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.7+sha-a9bab9e');
+const VERSION = new Version('20.1.7+sha-5d31e20');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-a9bab9e
+ * @license Angular v20.1.7+sha-5d31e20
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
