a04a2be62dd55a7d78b0ccccb3fc707505e5d357
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-18 12:46:33 +0000

    a7cfb42 fix(core): ensure animate events do not have duplicate elements (#63216)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-6c8d000
+ * @license Angular v21.0.0-next.0+sha-a7cfb42
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-6c8d000');
+const VERSION = new Version('21.0.0-next.0+sha-a7cfb42');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-6c8d000
+ * @license Angular v21.0.0-next.0+sha-a7cfb42
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
