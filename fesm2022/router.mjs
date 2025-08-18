c3d0a49b447ff5af26033e56594e42d1faf09b05
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-18 07:18:46 +0000

    ff01e33 docs: revert remove of `build-and-link` (#63187)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-3f88fbd
+ * @license Angular v21.0.0-next.0+sha-ff01e33
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-3f88fbd');
+const VERSION = new Version('21.0.0-next.0+sha-ff01e33');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-3f88fbd
+ * @license Angular v21.0.0-next.0+sha-ff01e33
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
