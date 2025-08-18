ad43d0124e6fec38661d45b36c4c789f14527a22
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-18 15:47:15 +0000

    5115050 fix(devtools): DOM traversal bug (#62719)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-d0cf624
+ * @license Angular v21.0.0-next.0+sha-5115050
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-d0cf624');
+const VERSION = new Version('21.0.0-next.0+sha-5115050');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-d0cf624
+ * @license Angular v21.0.0-next.0+sha-5115050
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
