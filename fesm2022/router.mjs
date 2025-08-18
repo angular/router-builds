fcef34b86716d022546f8f1fd893d7706499fb89
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-18 16:01:03 +0000

    cab3adf fix(compiler): fixes animation event host bindings not firing (#63217)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-5115050
+ * @license Angular v21.0.0-next.0+sha-cab3adf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-5115050');
+const VERSION = new Version('21.0.0-next.0+sha-cab3adf');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
