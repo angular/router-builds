06b7020ea48fd5ab8781286a64e07303df62750a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-19 08:21:45 +0000

    966d95d docs: update Angular CLI help (#63226)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-fe27810
+ * @license Angular v20.2.0-rc.1+sha-966d95d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-fe27810');
+const VERSION = new Version('20.2.0-rc.1+sha-966d95d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-fe27810
+ * @license Angular v20.2.0-rc.1+sha-966d95d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
