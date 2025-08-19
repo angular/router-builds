0735a22e585646ec72d5cdbf946c5b968c807de6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-19 08:34:18 +0000

    9e26158 refactor(core): update NG0303 error message (#63222) Ensure consistency in error message wording by aligning the NG0303 error with other Angular error strings. This improves clarity and maintains a uniform developer experience

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-d81baf6
+ * @license Angular v20.1.7+sha-9e26158
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.7+sha-d81baf6');
+const VERSION = new Version('20.1.7+sha-9e26158');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-d81baf6
+ * @license Angular v20.1.7+sha-9e26158
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
