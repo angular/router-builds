1cf092fc2ec4aa3d97ce3f82ab110bf09d68e7d0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-19 11:20:28 +0000

    9425179 build: update dependency shiki to v3.10.0 (#63235)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-6712456
+ * @license Angular v21.0.0-next.0+sha-9425179
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-6712456');
+const VERSION = new Version('21.0.0-next.0+sha-9425179');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
