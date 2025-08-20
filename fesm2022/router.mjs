2ff57e1ba35538740639e0b7f9271ea0d4b41762
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 11:20:01 +0000

    9655ea7 build: update dependency playwright-core to v1.55.0 (#63273)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-d76a911
+ * @license Angular v20.2.0-rc.1+sha-9655ea7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-d76a911');
+const VERSION = new Version('20.2.0-rc.1+sha-9655ea7');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-d76a911
+ * @license Angular v20.2.0-rc.1+sha-9655ea7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
