df4dc489906ab62ff0295780985425a8014a30c6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 11:35:51 +0000

    7bf4906 fix(compiler-cli): account for expression with type arguments during HMR extraction (#63261)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-3d7b31c
+ * @license Angular v21.0.0-next.0+sha-7bf4906
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-3d7b31c');
+const VERSION = new Version('21.0.0-next.0+sha-7bf4906');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-3d7b31c
+ * @license Angular v21.0.0-next.0+sha-7bf4906
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
