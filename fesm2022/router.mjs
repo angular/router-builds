2323bd4499041b046cd7ac01eb0c35cd25c74576
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 09:32:15 +0000

    373670c docs: update `NG_VALIDATORS` examples to use `forwardRef` (#63247)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-39febe1
+ * @license Angular v20.1.7+sha-373670c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.7+sha-39febe1');
+const VERSION = new Version('20.1.7+sha-373670c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-39febe1
+ * @license Angular v20.1.7+sha-373670c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
