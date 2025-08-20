9f069e2d09f0786564b52a4927cf27d0793b7f5a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 08:34:47 +0000

    c935d42 docs: remove redundant Angular docs link in tailwind guide (#63205)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.7+sha-683ae03
+ * @license Angular v20.1.7+sha-c935d42
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.7+sha-683ae03');
+const VERSION = new Version('20.1.7+sha-c935d42');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
