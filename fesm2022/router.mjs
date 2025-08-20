3b72e823d92203cf7a274653b46bbd1242474050
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 09:48:02 +0000

    591c7e2 fix(language-service): Support to resolve the re-export component. (#62585)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-81dc006
+ * @license Angular v20.2.0-rc.1+sha-591c7e2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-81dc006');
+const VERSION = new Version('20.2.0-rc.1+sha-591c7e2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
