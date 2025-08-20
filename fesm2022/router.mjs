89114b64c7d6e47338ba08c973574a5f78da618b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 16:04:48 +0000

    1c5fb1e build: enforce frozen lockfile mode (#63260)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-d8bdf56
+ * @license Angular v21.0.0-next.0+sha-1c5fb1e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-d8bdf56');
+const VERSION = new Version('21.0.0-next.0+sha-1c5fb1e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-d8bdf56
+ * @license Angular v21.0.0-next.0+sha-1c5fb1e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
