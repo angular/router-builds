22716f1bf5722465174b64ab2cf820b99c61d78e
Andrew Kushnir <akushnir@google.com>
2025-08-21 21:17:55 +0000

    6f541b8 build: update dependency rollup to v4.47.1 (#63304)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-69df2c0
+ * @license Angular v21.0.0-next.0+sha-6f541b8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-69df2c0');
+const VERSION = new Version('21.0.0-next.0+sha-6f541b8');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-69df2c0
+ * @license Angular v21.0.0-next.0+sha-6f541b8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
