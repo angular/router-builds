585e82d866f59cbcd8b93261d191f650c4ae5908
Andrew Kushnir <akushnir@google.com>
2025-08-21 17:20:55 +0000

    7d6ae95 docs(docs-infra): fix mermaid rendering (#63311)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-0dd95c5
+ * @license Angular v21.0.0-next.0+sha-7d6ae95
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-0dd95c5');
+const VERSION = new Version('21.0.0-next.0+sha-7d6ae95');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/router_module.mjs
+++ fesm2022/router_module.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-0dd95c5
+ * @license Angular v21.0.0-next.0+sha-7d6ae95
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
