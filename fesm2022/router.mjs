d3c9d6d5a13a568eedaa99b357f96e9251f19489
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-08-21 10:57:35 +0000

    1c27fa9 release: cut the v20.2.1 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0+sha-a28672f
+ * @license Angular v20.2.1+sha-1c27fa9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0+sha-a28672f');
+const VERSION = new Version('20.2.1+sha-1c27fa9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0+sha-a28672f
+ * @license Angular v20.2.1+sha-1c27fa9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
