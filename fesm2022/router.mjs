201c9c04924c8d573a4a2ab814c35e3392df6c20
Andrew Kushnir <akushnir@google.com>
2025-08-22 19:05:57 +0000

    a842272 docs(docs-infra): fix spelling (#63334)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-56a8408
+ * @license Angular v21.0.0-next.0+sha-a842272
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-56a8408');
+const VERSION = new Version('21.0.0-next.0+sha-a842272');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-56a8408
+ * @license Angular v21.0.0-next.0+sha-a842272
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
