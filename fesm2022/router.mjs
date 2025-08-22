8e3fd598ceaf94b54ec20ab1cd6e142698c146cf
Andrew Kushnir <akushnir@google.com>
2025-08-22 18:34:59 +0000

    56a8408 build: update github/codeql-action action to v3.29.11 (#63325)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-6f541b8
+ * @license Angular v21.0.0-next.0+sha-56a8408
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-6f541b8');
+const VERSION = new Version('21.0.0-next.0+sha-56a8408');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-6f541b8
+ * @license Angular v21.0.0-next.0+sha-56a8408
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
