1fc91261b51669e985f5ad6b7b6fd200f948ee69
Andrew Kushnir <akushnir@google.com>
2025-08-25 22:44:02 +0000

    1febf52 build: update dependency @bazel/ibazel to v0.26.10 (#63373)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-5a11106
+ * @license Angular v21.0.0-next.0+sha-1febf52
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-5a11106');
+const VERSION = new Version('21.0.0-next.0+sha-1febf52');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
