58edb9247ac95df723e6f6673134eda87bf4d1a4
Miles Malerba <mmalerba@google.com>
2025-08-27 23:22:38 +0000

    059e0d3 docs: add customizing route behavior guide for router (#63398)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-1bb30c7
+ * @license Angular v21.0.0-next.1+sha-059e0d3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-1bb30c7');
+const VERSION = new Version('21.0.0-next.1+sha-059e0d3');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-1bb30c7
+ * @license Angular v21.0.0-next.1+sha-059e0d3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
