c6eb3495d0df47bc52cb17c6057680357e85daaf
Andrew Kushnir <akushnir@google.com>
2025-08-27 14:40:52 +0000

    f4f5a8a ci: pin create-pull-request version to sha (#63409)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-a43057c
+ * @license Angular v21.0.0-next.0+sha-f4f5a8a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-a43057c');
+const VERSION = new Version('21.0.0-next.0+sha-f4f5a8a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
