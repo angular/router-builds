1c941a33a5e82fcfc60708ba5ca6bd92c05e98cd
Miles Malerba <mmalerba@google.com>
2025-08-28 16:46:54 +0000

    2f5f975 refactor(forms): fix dependencies (#63451)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-1f4c5f7
+ * @license Angular v21.0.0-next.1+sha-2f5f975
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-1f4c5f7');
+const VERSION = new Version('21.0.0-next.1+sha-2f5f975');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/router2.mjs
+++ fesm2022/router2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-1f4c5f7
+ * @license Angular v21.0.0-next.1+sha-2f5f975
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
