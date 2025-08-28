d43478e12fde82df9ef502a88c8ff789b4766631
Miles Malerba <mmalerba@google.com>
2025-08-28 16:20:48 +0000

    1f4c5f7 refactor(bazel): reduce build deps (#63348)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-cac0852
+ * @license Angular v21.0.0-next.1+sha-1f4c5f7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-cac0852');
+const VERSION = new Version('21.0.0-next.1+sha-1f4c5f7');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/router2.mjs
+++ fesm2022/router2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-cac0852
+ * @license Angular v21.0.0-next.1+sha-1f4c5f7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
