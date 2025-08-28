4286bd67b1c1ae556db8b87113df49f42411b09f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 09:54:53 +0000

    7ccf368 build: define typescript version via string in module.bazel file (#63431)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-b2ed21b
+ * @license Angular v21.0.0-next.1+sha-7ccf368
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-b2ed21b');
+const VERSION = new Version('21.0.0-next.1+sha-7ccf368');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-b2ed21b
+ * @license Angular v21.0.0-next.1+sha-7ccf368
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
