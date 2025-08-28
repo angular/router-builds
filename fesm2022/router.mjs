1b9acf0494bf6b45ab93292029416f0b41a8614e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 14:15:18 +0000

    ed3d1f2 fix(core): Fix cancellation of animation enter classes (#63442)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-99cf028
+ * @license Angular v21.0.0-next.1+sha-ed3d1f2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-99cf028');
+const VERSION = new Version('21.0.0-next.1+sha-ed3d1f2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/router2.mjs
+++ fesm2022/router2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-99cf028
+ * @license Angular v21.0.0-next.1+sha-ed3d1f2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
