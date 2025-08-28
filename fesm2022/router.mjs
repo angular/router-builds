400f8bbab20d16a8156234108f97cb8b99beb287
Miles Malerba <mmalerba@google.com>
2025-08-28 16:06:52 +0000

    b8314bd feat(forms): add experimental signal-based forms (#63408)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-a1d1cdf
+ * @license Angular v21.0.0-next.1+sha-b8314bd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-a1d1cdf');
+const VERSION = new Version('21.0.0-next.1+sha-b8314bd');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/router2.mjs
+++ fesm2022/router2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-a1d1cdf
+ * @license Angular v21.0.0-next.1+sha-b8314bd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
