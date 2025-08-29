821fb32c55f6d356797e8f7ccca05e1837981cbd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-29 13:06:54 +0000

    c357a03 docs(docs-infra): makes signal readonly (#63449)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-64ddde2
+ * @license Angular v20.2.3+sha-c357a03
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-64ddde2');
+const VERSION = new Version('20.2.3+sha-c357a03');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-64ddde2
+ * @license Angular v20.2.3+sha-c357a03
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
