3b8dadb4d78a02ed34803b055a75acea14880e70
Miles Malerba <mmalerba@google.com>
2025-08-29 22:15:29 +0000

    16fa983 ci: add more team members as approvers (#63495)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-db95a5c
+ * @license Angular v21.0.0-next.1+sha-16fa983
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-db95a5c');
+const VERSION = new Version('21.0.0-next.1+sha-16fa983');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-db95a5c
+ * @license Angular v21.0.0-next.1+sha-16fa983
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
