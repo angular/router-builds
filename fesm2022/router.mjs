b5562a0519630b120e3173a660afb2a9cf228213
Miles Malerba <mmalerba@google.com>
2025-08-29 21:11:00 +0000

    e9096b9 docs(docs-infra): properties should be readonly (#63492)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-5bb4b75
+ * @license Angular v20.2.3+sha-e9096b9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-5bb4b75');
+const VERSION = new Version('20.2.3+sha-e9096b9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-5bb4b75
+ * @license Angular v20.2.3+sha-e9096b9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
