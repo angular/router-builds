6e6a08885ca6c9e6707828e7b75a007aa8403eb0
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-08-29 12:38:45 +0000

    1875d5c docs: release notes for the v20.2.3 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-0a82138
+ * @license Angular v21.0.0-next.1+sha-1875d5c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-0a82138');
+const VERSION = new Version('21.0.0-next.1+sha-1875d5c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-0a82138
+ * @license Angular v21.0.0-next.1+sha-1875d5c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
