43373b151f7d86c63bb7142a79562a4c535d067b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-29 13:23:56 +0000

    3fb02ed build: update google-github-actions/auth action to v3 (#63461)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-a3938d5
+ * @license Angular v21.0.0-next.1+sha-3fb02ed
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-a3938d5');
+const VERSION = new Version('21.0.0-next.1+sha-3fb02ed');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
