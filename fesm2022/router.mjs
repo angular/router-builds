57955ec3f848a76bd683aae7140bcd2c08d9d9ca
Miles Malerba <mmalerba@google.com>
2025-09-02 17:12:25 +0000

    1e6d311 build: update cross-repo angular dependencies (#63548)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-7aa9350
+ * @license Angular v21.0.0-next.1+sha-1e6d311
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-7aa9350');
+const VERSION = new Version('21.0.0-next.1+sha-1e6d311');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-7aa9350
+ * @license Angular v21.0.0-next.1+sha-1e6d311
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
