ead8d4cb31fc8ce963b8d4da37904a891ce40bd6
Miles Malerba <mmalerba@google.com>
2025-09-02 18:07:47 +0000

    92e09ad fix(core): Remove ignoreChangesOutsideZone option (#62700)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-1e6d311
+ * @license Angular v21.0.0-next.1+sha-92e09ad
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-1e6d311');
+const VERSION = new Version('21.0.0-next.1+sha-92e09ad');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
