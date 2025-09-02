ced71e03c462211cfecc025ea62d81bcfba98a09
Miles Malerba <mmalerba@google.com>
2025-09-02 18:04:34 +0000

    dc64f3e fix(core): Fixed inject migration schematics for migrate destructured properties (#62832)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-957b367
+ * @license Angular v20.2.3+sha-dc64f3e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-957b367');
+const VERSION = new Version('20.2.3+sha-dc64f3e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-957b367
+ * @license Angular v20.2.3+sha-dc64f3e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
