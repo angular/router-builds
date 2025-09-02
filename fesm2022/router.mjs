7a6e8b00a81c9bb9763dddc90f487ff2d9c8ccc2
Miles Malerba <mmalerba@google.com>
2025-09-02 16:41:43 +0000

    3b42314 docs(docs-infra): export class (#63506)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-dddce83
+ * @license Angular v20.2.3+sha-3b42314
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-dddce83');
+const VERSION = new Version('20.2.3+sha-3b42314');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/router2.mjs
+++ fesm2022/router2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-dddce83
+ * @license Angular v20.2.3+sha-3b42314
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
