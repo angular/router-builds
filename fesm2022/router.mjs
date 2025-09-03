444cea29ed7383de7ce099c60a08180622c8d540
Miles Malerba <mmalerba@google.com>
2025-09-03 16:37:43 +0000

    8738b81 build: use common jasmine rule from dev-infra (#63568)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-49d5de4
+ * @license Angular v21.0.0-next.1+sha-8738b81
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-49d5de4');
+const VERSION = new Version('21.0.0-next.1+sha-8738b81');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/router2.mjs
+++ fesm2022/router2.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-49d5de4
+ * @license Angular v21.0.0-next.1+sha-8738b81
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
