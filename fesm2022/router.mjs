bcc781ff098f8bf2869957a1f1fb77597329a767
Miles Malerba <mmalerba@google.com>
2025-09-03 16:20:38 +0000

    0c2255b refactor(platform-server): simplify transfer state serialization tracking (#63525)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-f4a2d0a
+ * @license Angular v20.2.3+sha-0c2255b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-f4a2d0a');
+const VERSION = new Version('20.2.3+sha-0c2255b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-f4a2d0a
+ * @license Angular v20.2.3+sha-0c2255b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
