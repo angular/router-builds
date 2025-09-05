78ee9ba5d5ec415b32d68d6b262df99501d1e833
Andrew Scott <atscott@google.com>
2025-09-05 19:58:05 +0000

    c3303a5 test: increase mermaid spec timeout to reduce flakes (#63610)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-f697995
+ * @license Angular v20.2.4+sha-c3303a5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-f697995');
+const VERSION = new Version('20.2.4+sha-c3303a5');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
