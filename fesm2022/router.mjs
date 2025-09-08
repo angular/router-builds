a843b4c3a539393c8cb9862f5d683d813e40ba6a
Andrew Scott <atscott@google.com>
2025-09-08 18:19:52 +0000

    a6640e3 build: add additional comments explaining deep imports in adev (#63644)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-f2ec6fb
+ * @license Angular v20.2.4+sha-a6640e3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-f2ec6fb');
+const VERSION = new Version('20.2.4+sha-a6640e3');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-f2ec6fb
+ * @license Angular v20.2.4+sha-a6640e3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
