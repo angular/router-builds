3d7ebb4aba417cdf7668c05ff9f0251dc8ebf199
Andrew Scott <atscott@google.com>
2025-09-08 23:36:09 +0000

    c7b84f5 docs: Update Language Service IDE list (#63647)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-a857f72
+ * @license Angular v20.2.4+sha-c7b84f5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-a857f72');
+const VERSION = new Version('20.2.4+sha-c7b84f5');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-a857f72
+ * @license Angular v20.2.4+sha-c7b84f5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
