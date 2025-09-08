012b2d590a463fa0c939ef5e53268bf137a8d567
Andrew Scott <atscott@google.com>
2025-09-08 19:50:35 +0000

    a6ed784 build: properly override the repo for rules_angular (#63584)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-d124fba
+ * @license Angular v20.2.4+sha-a6ed784
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-d124fba');
+const VERSION = new Version('20.2.4+sha-a6ed784');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-d124fba
+ * @license Angular v20.2.4+sha-a6ed784
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
