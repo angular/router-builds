0144896ef9a936d5aab62f0c1b5d7a655de0a3cd
Andrew Scott <atscott@google.com>
2025-09-08 19:14:52 +0000

    d124fba docs: improve headings of docs (#63604)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-a6640e3
+ * @license Angular v20.2.4+sha-d124fba
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-a6640e3');
+const VERSION = new Version('20.2.4+sha-d124fba');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-a6640e3
+ * @license Angular v20.2.4+sha-d124fba
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
