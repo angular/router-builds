c555dcc06ee3579c8281035dc6482077bd106f81
Andrew Scott <atscott@google.com>
2025-09-08 17:19:01 +0000

    e7fbee0 docs(docs-infra): quote title attributes in links (#63578)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-bacda4f
+ * @license Angular v21.0.0-next.2+sha-e7fbee0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-bacda4f');
+const VERSION = new Version('21.0.0-next.2+sha-e7fbee0');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-bacda4f
+ * @license Angular v21.0.0-next.2+sha-e7fbee0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
