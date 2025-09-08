7e67833b978811fd8844dcc7ab24f55b28e40114
Andrew Scott <atscott@google.com>
2025-09-08 21:27:11 +0000

    9943230 docs: remove installation (#63508)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-35a1d5d
+ * @license Angular v20.2.4+sha-9943230
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-35a1d5d');
+const VERSION = new Version('20.2.4+sha-9943230');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-35a1d5d
+ * @license Angular v20.2.4+sha-9943230
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
