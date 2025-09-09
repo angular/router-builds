2b6c5e6b3d96ee3051cba92bcb237820f5b90e1c
Andrew Scott <atscott@google.com>
2025-09-09 18:03:11 +0000

    28926ba feat(core): introduce `BootstrapContext` for improved server bootstrapping (#63562)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-3dc5056
+ * @license Angular v21.0.0-next.2+sha-28926ba
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-3dc5056');
+const VERSION = new Version('21.0.0-next.2+sha-28926ba');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-3dc5056
+ * @license Angular v21.0.0-next.2+sha-28926ba
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
