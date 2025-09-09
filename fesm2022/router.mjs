fe51cfeb592bd33dc7358e5521d7b323a615a26b
Andrew Scott <atscott@google.com>
2025-09-09 20:01:39 +0000

    9ffc83e docs: add signals tutorial (#62750)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-6d12f10
+ * @license Angular v21.0.0-next.2+sha-9ffc83e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-6d12f10');
+const VERSION = new Version('21.0.0-next.2+sha-9ffc83e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
