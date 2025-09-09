0674482aeb7e751c56b1b92bde8c1ccd90a9832d
Andrew Scott <atscott@google.com>
2025-09-09 19:15:02 +0000

    0c8c855 docs: add missing drag & drop docs (#63619)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-e097269
+ * @license Angular v20.2.4+sha-0c8c855
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-e097269');
+const VERSION = new Version('20.2.4+sha-0c8c855');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-e097269
+ * @license Angular v20.2.4+sha-0c8c855
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
