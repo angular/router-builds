985588859907993a06aa0a66f1baf5656dcc6c00
Andrew Scott <atscott@google.com>
2025-09-09 22:49:44 +0000

    97d3615 build: create a script and ci setup for managing and uploading known synonyms for algolia (#63663)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-c357650
+ * @license Angular v21.0.0-next.2+sha-97d3615
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-c357650');
+const VERSION = new Version('21.0.0-next.2+sha-97d3615');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-c357650
+ * @license Angular v21.0.0-next.2+sha-97d3615
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
