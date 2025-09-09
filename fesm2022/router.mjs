a131d4026f57539c12317ca70c26813dbe9a0ce9
Andrew Scott <atscott@google.com>
2025-09-09 19:39:08 +0000

    f50b83f ci: update runs-on to ubuntu-latest (#63656)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-0c8c855
+ * @license Angular v20.2.4+sha-f50b83f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-0c8c855');
+const VERSION = new Version('20.2.4+sha-f50b83f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
