565693a93594d2059fb9a232f197eb24e4b208e0
Andrew Scott <atscott@google.com>
2025-09-10 19:12:19 +0000

    8266790 build: update cross-repo angular dependencies (#63707)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-802dbcc
+ * @license Angular v20.3.0+sha-8266790
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-802dbcc');
+const VERSION = new Version('20.3.0+sha-8266790');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-802dbcc
+ * @license Angular v20.3.0+sha-8266790
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
