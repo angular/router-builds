4a5b8bf01dca8e8e93541bc96b4fa6f4c900268f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-10 23:12:46 +0000

    97b0796 docs: update mcp guide (#63664)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-d9483fa
+ * @license Angular v21.0.0-next.3+sha-97b0796
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-d9483fa');
+const VERSION = new Version('21.0.0-next.3+sha-97b0796');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-d9483fa
+ * @license Angular v21.0.0-next.3+sha-97b0796
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
