7b9dd54bb130df82ca5c10eb4e0e0eaa34fb84c5
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 17:44:48 +0000

    50b3e9f docs: Adds guide for extending Angular service worker (#63629)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-b62cadb
+ * @license Angular v20.3.0+sha-50b3e9f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-b62cadb');
+const VERSION = new Version('20.3.0+sha-50b3e9f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
