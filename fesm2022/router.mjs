02a3bcafdc8a4318f0870b5992022140458cf792
Jan Martin <jan.krems@gmail.com>
2025-09-11 14:17:36 +0000

    cdf5d67 release: bump Angular DevTools version to 1.2.0 (#63730)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-97b0796
+ * @license Angular v21.0.0-next.3+sha-cdf5d67
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-97b0796');
+const VERSION = new Version('21.0.0-next.3+sha-cdf5d67');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
