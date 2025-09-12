beea5fea5a33d0013b56a600dd21ed5d95739e06
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 15:28:01 +0000

    3ec8a5c fix(core): Prevent leave animations on a move operation (#63745)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-4ee3993
+ * @license Angular v20.3.0+sha-3ec8a5c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-4ee3993');
+const VERSION = new Version('20.3.0+sha-3ec8a5c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
