7df25fd812fd146e74388f045a64870e5348f293
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-09-12 07:55:26 +0000

    185ac24 docs: fix links and remove second banner (#63755)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-e5de410
+ * @license Angular v20.3.0+sha-185ac24
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-e5de410');
+const VERSION = new Version('20.3.0+sha-185ac24');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
