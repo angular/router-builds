2903a5842d7bcbcc644ca77570589616a1a5bd1a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 16:53:45 +0000

    f8cea65 refactor(devtools): property-tab folder structure (#63759)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-5fd6022
+ * @license Angular v21.0.0-next.3+sha-f8cea65
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-5fd6022');
+const VERSION = new Version('21.0.0-next.3+sha-f8cea65');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
