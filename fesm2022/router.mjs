557a9e4783074eb9faf31927b9f28cd5fd26e54d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 16:09:01 +0000

    9de50b1 docs(docs-infra): move the navigation related files (#63729)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-895f0fb
+ * @license Angular v20.3.0+sha-9de50b1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-895f0fb');
+const VERSION = new Version('20.3.0+sha-9de50b1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-895f0fb
+ * @license Angular v20.3.0+sha-9de50b1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
