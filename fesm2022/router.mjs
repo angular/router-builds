9252bad7dd6f801d0d6821ce1bf5b7ddd2ca14e0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 17:49:29 +0000

    9def492 build: update pnpm to v10.16.0 (#63770)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-15b6e1a
+ * @license Angular v20.3.0+sha-9def492
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-15b6e1a');
+const VERSION = new Version('20.3.0+sha-9def492');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
