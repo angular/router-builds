e7f397cd467c4d099557dc956d385a99af5b55f2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 17:26:31 +0000

    15b6e1a build: update cross-repo angular dependencies (#63768)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-8449001
+ * @license Angular v20.3.0+sha-15b6e1a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-8449001');
+const VERSION = new Version('20.3.0+sha-15b6e1a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-8449001
+ * @license Angular v20.3.0+sha-15b6e1a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
