382f542c65773c1a4a2ad86480c1aa2185fd4a8e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 17:08:16 +0000

    de8be97 ci: update pullapprove public-api rules (#63737)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-d201cd2
+ * @license Angular v21.0.0-next.3+sha-de8be97
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-d201cd2');
+const VERSION = new Version('21.0.0-next.3+sha-de8be97');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-d201cd2
+ * @license Angular v21.0.0-next.3+sha-de8be97
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
