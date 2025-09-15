2e465e96f61ebcc69e10c9d8d19311cdceef329c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 18:52:03 +0000

    7fb5a80 fix(compiler): Add support for `aria-invalid` (#63748)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-a2ea2ec
+ * @license Angular v20.3.0+sha-7fb5a80
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-a2ea2ec');
+const VERSION = new Version('20.3.0+sha-7fb5a80');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-a2ea2ec
+ * @license Angular v20.3.0+sha-7fb5a80
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
