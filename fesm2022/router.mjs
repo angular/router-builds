f7efab5d4b21cc072a7a71a1c0ef928fc1732914
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 15:56:35 +0000

    0a60e35 fix(compiler-cli): signal not invoked diagnostic not raised when input has same name in template (#63754)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-cfe0f03
+ * @license Angular v21.0.0-next.3+sha-0a60e35
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-cfe0f03');
+const VERSION = new Version('21.0.0-next.3+sha-0a60e35');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
