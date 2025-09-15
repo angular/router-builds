b8561e0aebfb9c411eff65609cfc26fbab6b23c3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 18:30:18 +0000

    a5e5dbb refactor(core): remove `moduleId` from Component metadata (#63482)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-2473931
+ * @license Angular v21.0.0-next.3+sha-a5e5dbb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-2473931');
+const VERSION = new Version('21.0.0-next.3+sha-a5e5dbb');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-2473931
+ * @license Angular v21.0.0-next.3+sha-a5e5dbb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
