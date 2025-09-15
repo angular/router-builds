295fa1a000c249d96f83806da47f3abcbbd73b77
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 17:13:14 +0000

    a2ea2ec docs: Update outputs.md to demonstrate how to use event data (#63732) PR Close #63732

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-cd36189
+ * @license Angular v20.3.0+sha-a2ea2ec
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-cd36189');
+const VERSION = new Version('20.3.0+sha-a2ea2ec');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-cd36189
+ * @license Angular v20.3.0+sha-a2ea2ec
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
