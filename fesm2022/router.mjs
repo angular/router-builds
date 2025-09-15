c4ecdbd0e262776225576d33a013aa31219deeb6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 16:56:20 +0000

    d201cd2 feat(forms): Prevents marking fields as touched/dirty when state is hidden/readonly/disabled (#63633)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-0a60e35
+ * @license Angular v21.0.0-next.3+sha-d201cd2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-0a60e35');
+const VERSION = new Version('21.0.0-next.3+sha-d201cd2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
