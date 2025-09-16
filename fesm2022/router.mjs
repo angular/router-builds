76ba01083913dad0b983570f73c480b8fc41d2c1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 15:14:19 +0000

    27b2a4a refactor(core): Update async-test.ts to use globalThis polyfill (#57505)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-c80d9ba
+ * @license Angular v20.3.0+sha-27b2a4a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-c80d9ba');
+const VERSION = new Version('20.3.0+sha-27b2a4a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-c80d9ba
+ * @license Angular v20.3.0+sha-27b2a4a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
