b31863244c30d5de078dec651887ad7fb0a68351
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 20:52:58 +0000

    1fec2a2 build: remove bazel flag usage for adev (#63845)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-1352fbd
+ * @license Angular v21.0.0-next.3+sha-1fec2a2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-1352fbd');
+const VERSION = new Version('21.0.0-next.3+sha-1fec2a2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
