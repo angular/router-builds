04b659cf666e2d5bda774da71885acc7c92e5a17
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 15:27:54 +0000

    af33647 refactor(core): remove usages of Promise.withResolvers (#63856)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-e523384
+ * @license Angular v21.0.0-next.3+sha-af33647
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-e523384');
+const VERSION = new Version('21.0.0-next.3+sha-af33647');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
