f8029579937a48032aa8cb3616ff37fb144b154f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 16:17:11 +0000

    8843707 fix(compiler-cli): only bind inputs that are part of microsyntax to a structural directive (#52453)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-dcdca98
+ * @license Angular v20.3.0+sha-8843707
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-dcdca98');
+const VERSION = new Version('20.3.0+sha-8843707');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
