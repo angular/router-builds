47bc50aa1f90bb3d020e7a928cfcb1de59a7a8f7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 15:01:20 +0000

    e523384 refactor(common): trim undefined from locale data. (#63520)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-5fa2c05
+ * @license Angular v21.0.0-next.3+sha-e523384
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-5fa2c05');
+const VERSION = new Version('21.0.0-next.3+sha-e523384');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
