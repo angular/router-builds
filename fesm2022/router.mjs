5e277180e37024b9a6f05d71c167f939339ba080
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-18 20:22:50 +0000

    307e4ea refactor(migrations): remove common module if there are no references (#63892)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-e426302
+ * @license Angular v21.0.0-next.4+sha-307e4ea
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-e426302');
+const VERSION = new Version('21.0.0-next.4+sha-307e4ea');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
