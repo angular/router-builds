3b4975a595bf9bcb04f5cdbc5269e35802a88504
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-18 21:39:37 +0000

    ba40153 fix(compiler-cli): capture metadata for undecorated fields (#63904)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-d40867c
+ * @license Angular v20.3.1+sha-ba40153
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-d40867c');
+const VERSION = new Version('20.3.1+sha-ba40153');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-d40867c
+ * @license Angular v20.3.1+sha-ba40153
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
