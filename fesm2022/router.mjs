d79d1287d2cc383fb833d4340a3efb6ab2227b61
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 14:55:31 +0000

    d0c7ea8 refactor(core): fix MockRenderer in unit tests. (#63936)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-ba40153
+ * @license Angular v20.3.1+sha-d0c7ea8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-ba40153');
+const VERSION = new Version('20.3.1+sha-d0c7ea8');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-ba40153
+ * @license Angular v20.3.1+sha-d0c7ea8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
