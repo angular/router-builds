39418e9c0176bde9265e0be845c2cf54775658d4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 19:03:57 +0000

    2d232b3 Revert "refactor(compiler-cli): track member metadata using output AST (#63904)" (#63952)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-b60d5e4
+ * @license Angular v21.0.0-next.4+sha-2d232b3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-b60d5e4');
+const VERSION = new Version('21.0.0-next.4+sha-2d232b3');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
