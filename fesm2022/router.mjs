d41534742274934e67f87ea96d14e3505053e7fb
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 17:11:25 +0000

    150ed2b docs: add documentation for NG0401 error (#63947)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-d0c7ea8
+ * @license Angular v20.3.1+sha-150ed2b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-d0c7ea8');
+const VERSION = new Version('20.3.1+sha-150ed2b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-d0c7ea8
+ * @license Angular v20.3.1+sha-150ed2b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
