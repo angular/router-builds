ab9f4a1395ff814130be4994cba7f118f6ad9d4e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 22:17:43 +0000

    6782f71 docs: remove api grouping toggle (#63961)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-40ae5af
+ * @license Angular v21.0.0-next.4+sha-6782f71
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-40ae5af');
+const VERSION = new Version('21.0.0-next.4+sha-6782f71');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-40ae5af
+ * @license Angular v21.0.0-next.4+sha-6782f71
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
