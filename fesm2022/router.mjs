576c570931254b15d27cca6142c7589b85f5a2b6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-22 22:51:57 +0000

    3d1e8fa docs: add types category for signal forms API entries (#63967)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-304d91f
+ * @license Angular v21.0.0-next.4+sha-3d1e8fa
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-304d91f');
+const VERSION = new Version('21.0.0-next.4+sha-3d1e8fa');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-304d91f
+ * @license Angular v21.0.0-next.4+sha-3d1e8fa
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
