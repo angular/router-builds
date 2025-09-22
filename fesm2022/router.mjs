d243c98db8147284793020d892433fd44e2dadd9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-22 16:59:28 +0000

    5406e1a refactor(core): use version>0 instead of hasRun (#62467)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-3bd91de
+ * @license Angular v21.0.0-next.4+sha-5406e1a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-3bd91de');
+const VERSION = new Version('21.0.0-next.4+sha-5406e1a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-3bd91de
+ * @license Angular v21.0.0-next.4+sha-5406e1a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
