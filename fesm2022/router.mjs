b192e8b306683a51d32d95dd5ef2214357820fee
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-23 14:27:01 +0000

    9515a70 fix(core): fix narrowing of `Resource.hasValue()` (#63994)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-2d07610
+ * @license Angular v20.3.1+sha-9515a70
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-2d07610');
+const VERSION = new Version('20.3.1+sha-9515a70');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-2d07610
+ * @license Angular v20.3.1+sha-9515a70
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
