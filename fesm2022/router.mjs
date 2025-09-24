f449d6600e58aa8f69a7760fbd91b0574bfa77e9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 21:31:58 +0000

    090c0bb build: update dependency vsce to v2 (#64054)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-e8beaa4
+ * @license Angular v21.0.0-next.4+sha-090c0bb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-e8beaa4');
+const VERSION = new Version('21.0.0-next.4+sha-090c0bb');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
