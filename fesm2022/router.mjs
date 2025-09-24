7ac96f13845691c0d92a31c0d3c19424e23c3f06
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 14:30:05 +0000

    521c2a7 feat(devtools): Change lazy/eager visualization by using dashed lines and correctly positioned edge arrows (#63980)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-f36a504
+ * @license Angular v21.0.0-next.4+sha-521c2a7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-f36a504');
+const VERSION = new Version('21.0.0-next.4+sha-521c2a7');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
