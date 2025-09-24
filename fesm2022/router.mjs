38630419444a33707e31034e93cc9d140d694932
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 16:06:27 +0000

    3ebaecc fix(migrations): handle reused templates in control flow migration (#63996)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-3c14d05
+ * @license Angular v20.3.1+sha-3ebaecc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-3c14d05');
+const VERSION = new Version('20.3.1+sha-3ebaecc');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-3c14d05
+ * @license Angular v20.3.1+sha-3ebaecc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
