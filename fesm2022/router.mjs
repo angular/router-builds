68fb9904ad6a7e3d96fc755c3c20dd5c34ee267b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-09-24 23:26:57 +0000

    31f2030 release: cut the v20.3.2 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-c525a5a
+ * @license Angular v20.3.2+sha-31f2030
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-c525a5a');
+const VERSION = new Version('20.3.2+sha-31f2030');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-c525a5a
+ * @license Angular v20.3.2+sha-31f2030
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
