4e56965eab206df0269287f77bb4db860e02f69e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 18:36:03 +0000

    ce6c7d9 docs(docs-infra): align docs-step number to the heading (#64035)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-1fd8d5d
+ * @license Angular v20.3.1+sha-ce6c7d9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-1fd8d5d');
+const VERSION = new Version('20.3.1+sha-ce6c7d9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-1fd8d5d
+ * @license Angular v20.3.1+sha-ce6c7d9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
