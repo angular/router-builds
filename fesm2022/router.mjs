6852a127a21f64867f90d59b7b3d59658fce60db
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 15:08:16 +0000

    a9145f3 Revert "feat(common): update to cldr 47 (#63923)" (#64031)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-7a7bc27
+ * @license Angular v21.0.0-next.4+sha-a9145f3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-7a7bc27');
+const VERSION = new Version('21.0.0-next.4+sha-a9145f3');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
