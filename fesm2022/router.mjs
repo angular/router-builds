1ecbb28c6b3746e882adb11249258cc8f1d90ed1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 21:54:16 +0000

    057eff6 build: update dependency prettier to v3 (#64053)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-090c0bb
+ * @license Angular v21.0.0-next.4+sha-057eff6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-090c0bb');
+const VERSION = new Version('21.0.0-next.4+sha-057eff6');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-090c0bb
+ * @license Angular v21.0.0-next.4+sha-057eff6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
