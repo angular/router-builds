7ee42c726ea895a0ade310dacda28f74c8407fa8
kirjs <kirts@google.com>
2025-09-25 19:40:44 +0000

    3d2e1a6 docs: fix XLIFF 1.2 specification URL in i18n translation files guide (#64075)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-566077f
+ * @license Angular v21.0.0-next.5+sha-3d2e1a6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-566077f');
+const VERSION = new Version('21.0.0-next.5+sha-3d2e1a6');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
