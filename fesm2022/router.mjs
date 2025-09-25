dd44c0b9b70de3543c6ce42fdce039479b4a4c3d
kirjs <kirts@google.com>
2025-09-25 15:02:55 +0000

    fe0e5ab build: bump `vscode-tmgrammar-test` to `0.1.3` (#64078)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-9fd8d40
+ * @license Angular v21.0.0-next.5+sha-fe0e5ab
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-9fd8d40');
+const VERSION = new Version('21.0.0-next.5+sha-fe0e5ab');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-9fd8d40
+ * @license Angular v21.0.0-next.5+sha-fe0e5ab
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
