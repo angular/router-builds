405a3e636a11ef917ab9e1b6bd6e1428c19f3ecf
kirjs <kirts@google.com>
2025-09-25 14:30:42 +0000

    95fa943 refactor: update dependencies and build configurations (#64073)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-0e8e7ba
+ * @license Angular v21.0.0-next.5+sha-95fa943
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-0e8e7ba');
+const VERSION = new Version('21.0.0-next.5+sha-95fa943');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-0e8e7ba
+ * @license Angular v21.0.0-next.5+sha-95fa943
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
