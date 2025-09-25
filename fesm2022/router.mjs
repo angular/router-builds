8b778831a4ba8a51639742aec184562cd357326f
kirjs <kirts@google.com>
2025-09-25 20:27:58 +0000

    6fee508 refactor: break circular dependency in integration tests (#64091)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-3d2e1a6
+ * @license Angular v21.0.0-next.5+sha-6fee508
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-3d2e1a6');
+const VERSION = new Version('21.0.0-next.5+sha-6fee508');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
