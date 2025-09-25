fdb8e5c4be7925670b07f70e5de28150b6fc90e3
kirjs <kirts@google.com>
2025-09-25 18:32:53 +0000

    d7dead3 docs: fix homepage width (#64087)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-cd23761
+ * @license Angular v21.0.0-next.5+sha-d7dead3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-cd23761');
+const VERSION = new Version('21.0.0-next.5+sha-d7dead3');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-cd23761
+ * @license Angular v21.0.0-next.5+sha-d7dead3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
