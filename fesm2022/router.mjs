b94d6e5b97922896ff6cd239b30772efb8ae34e2
kirjs <kirts@google.com>
2025-09-26 12:35:17 +0000

    14d5b0e refactor: make language service integration tests hermetic (#64098)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-e2fd79b
+ * @license Angular v21.0.0-next.5+sha-14d5b0e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-e2fd79b');
+const VERSION = new Version('21.0.0-next.5+sha-14d5b0e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-e2fd79b
+ * @license Angular v21.0.0-next.5+sha-14d5b0e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
