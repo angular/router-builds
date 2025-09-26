9e8e88f0f50d7f014a97b7d03cc6a9c718dfa562
kirjs <kirts@google.com>
2025-09-26 13:37:39 +0000

    8ff536b docs: update text (#64095)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-14d5b0e
+ * @license Angular v21.0.0-next.5+sha-8ff536b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-14d5b0e');
+const VERSION = new Version('21.0.0-next.5+sha-8ff536b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-14d5b0e
+ * @license Angular v21.0.0-next.5+sha-8ff536b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
