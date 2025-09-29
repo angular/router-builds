58a05944b9bc4316f23d4a055fdd026ebe5d8d60
kirjs <kirts@google.com>
2025-09-29 17:36:24 +0000

    aa28aa2 docs: Adds guide for custom HTTP parameter encoding (#64119)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-4b3f7a0
+ * @license Angular v20.3.2+sha-aa28aa2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.2+sha-4b3f7a0');
+const VERSION = new Version('20.3.2+sha-aa28aa2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-4b3f7a0
+ * @license Angular v20.3.2+sha-aa28aa2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
