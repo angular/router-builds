71678063ae3ac3dc94a6cec70ded8ad0f96b236d
kirjs <kirts@google.com>
2025-09-30 14:36:02 +0000

    a10eb38 feat(devtools): Add possibility to log directive instances and their prop values (#64143)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-9a913dc
+ * @license Angular v21.0.0-next.5+sha-a10eb38
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-9a913dc');
+const VERSION = new Version('21.0.0-next.5+sha-a10eb38');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-9a913dc
+ * @license Angular v21.0.0-next.5+sha-a10eb38
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
