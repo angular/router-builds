1164a22258a136fc084ca543584f1410ce637535
kirjs <kirts@google.com>
2025-09-30 19:01:27 +0000

    c3a949a refactor(compiler-cli): include linkedSignal in the signal debugName transformer (#64106)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-2aca6da
+ * @license Angular v21.0.0-next.5+sha-c3a949a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-2aca6da');
+const VERSION = new Version('21.0.0-next.5+sha-c3a949a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
