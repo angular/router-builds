affcbe25a6fcb94473039e2e5037ec87c3608f7e
kirjs <kirts@google.com>
2025-09-30 20:12:23 +0000

    f51ab32 fix(compiler): recover template literals with broken expressions (#64150)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-77b6305
+ * @license Angular v20.3.2+sha-f51ab32
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.2+sha-77b6305');
+const VERSION = new Version('20.3.2+sha-f51ab32');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-77b6305
+ * @license Angular v20.3.2+sha-f51ab32
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
