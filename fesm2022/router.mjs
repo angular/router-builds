a8a1c945c067f1e357d7f91390db7fa0af211827
kirjs <kirts@google.com>
2025-09-30 17:40:53 +0000

    899c25f docs: add mention of `host-context()` being spec deprecated but still supported (#64153)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-20bb459
+ * @license Angular v21.0.0-next.5+sha-899c25f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-20bb459');
+const VERSION = new Version('21.0.0-next.5+sha-899c25f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-20bb459
+ * @license Angular v21.0.0-next.5+sha-899c25f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
