3f60202cdba5813aea11921110898d352eca4b15
kirjs <kirts@google.com>
2025-10-01 13:07:38 +0000

    24c979d docs: add allowed-host mention (#64145)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-f51ab32
+ * @license Angular v20.3.2+sha-24c979d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.2+sha-f51ab32');
+const VERSION = new Version('20.3.2+sha-24c979d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-f51ab32
+ * @license Angular v20.3.2+sha-24c979d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
