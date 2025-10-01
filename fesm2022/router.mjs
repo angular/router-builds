ed37477b542683fec808497bb1b53960b99a6364
kirjs <kirts@google.com>
2025-10-01 17:18:08 +0000

    ee7d49a docs: update Angular CLI help (#64174)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-0537083
+ * @license Angular v21.0.0-next.5+sha-ee7d49a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-0537083');
+const VERSION = new Version('21.0.0-next.5+sha-ee7d49a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-0537083
+ * @license Angular v21.0.0-next.5+sha-ee7d49a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
