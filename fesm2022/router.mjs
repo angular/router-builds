02200a1627f5e130ccb927c5f25282d289ccf3ac
kirjs <kirts@google.com>
2025-10-01 13:15:04 +0000

    1cb16fd fix(core): Fixes animations in conjunction with content projection (#63776)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-f866132
+ * @license Angular v21.0.0-next.5+sha-1cb16fd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-f866132');
+const VERSION = new Version('21.0.0-next.5+sha-1cb16fd');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
