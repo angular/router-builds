a2c8fa289c16d21f8a15d02602966aec066ad098
Miles Malerba <mmalerba@google.com>
2025-10-01 22:47:05 +0000

    d892aab Revert "fix(core): Fixes animations in conjunction with content projection (#63776)" (#64189)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-be0455a
+ * @license Angular v21.0.0-next.5+sha-d892aab
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-be0455a');
+const VERSION = new Version('21.0.0-next.5+sha-d892aab');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-be0455a
+ * @license Angular v21.0.0-next.5+sha-d892aab
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
