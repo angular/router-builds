beaf75f0b9da8bcd64ec93af1cfa772efdf8b7ab
kirjs <kirts@google.com>
2025-10-01 14:51:14 +0000

    cc8f5e1 build: update ossf/scorecard-action action to v2.4.3 (#64166)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-0e928fb
+ * @license Angular v20.3.2+sha-cc8f5e1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.2+sha-0e928fb');
+const VERSION = new Version('20.3.2+sha-cc8f5e1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-0e928fb
+ * @license Angular v20.3.2+sha-cc8f5e1
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
