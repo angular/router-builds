9c392ee1cc525b5b0558c02acdcedec40f069a02
Kirill Cherkashin <kirill@kcherkashin.org>
2025-10-02 12:28:53 +0000

    732635b release: cut the v21.0.0-next.6 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-4699ccf
+ * @license Angular v21.0.0-next.6+sha-732635b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-4699ccf');
+const VERSION = new Version('21.0.0-next.6+sha-732635b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-4699ccf
+ * @license Angular v21.0.0-next.6+sha-732635b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
