53c03b59f2e83064cab7d9627e8922925745086a
Miles Malerba <mmalerba@google.com>
2025-10-02 17:35:27 +0000

    e2820d1 docs(docs-infra): keep file name visible when code block is hidden (#64204)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.3+sha-6fed986
+ * @license Angular v20.3.3+sha-e2820d1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.3+sha-6fed986');
+const VERSION = new Version('20.3.3+sha-e2820d1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.3+sha-6fed986
+ * @license Angular v20.3.3+sha-e2820d1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
