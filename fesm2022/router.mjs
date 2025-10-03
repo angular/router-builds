bae2b345fed37e18f5f6471b843d06849d566cff
Miles Malerba <mmalerba@google.com>
2025-10-03 20:35:13 +0000

    853ed16 fix(core): ensure missing leave animations don't queue leave animations (#64226)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.3+sha-28668a5
+ * @license Angular v20.3.3+sha-853ed16
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.3+sha-28668a5');
+const VERSION = new Version('20.3.3+sha-853ed16');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
