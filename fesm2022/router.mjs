d6e2ae8707bb2a22aaa0389a9012dacfb909fdda
Miles Malerba <mmalerba@google.com>
2025-10-03 17:23:30 +0000

    ab780a7 docs: add info about disabling request caching (#64216)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-e403078
+ * @license Angular v21.0.0-next.6+sha-ab780a7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-e403078');
+const VERSION = new Version('21.0.0-next.6+sha-ab780a7');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-e403078
+ * @license Angular v21.0.0-next.6+sha-ab780a7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
