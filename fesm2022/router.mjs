b1bc848a83054524aa7f45253df1d1d6ee09e16e
Miles Malerba <mmalerba@google.com>
2025-10-06 19:16:51 +0000

    8528bb5 docs(docs-infra): fix @example in class members (#64249)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.3+sha-6e5be38
+ * @license Angular v20.3.3+sha-8528bb5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.3+sha-6e5be38');
+const VERSION = new Version('20.3.3+sha-8528bb5');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.3+sha-6e5be38
+ * @license Angular v20.3.3+sha-8528bb5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
