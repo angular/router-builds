487c7acb1f15bcf1d85c1f0bf5796378c6338a44
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-10-08 10:16:25 +0000

    76c8d04 build: update `rules_sass` to `1184a80751a21af8348f308abc5b38a41f26850e` (#64288)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-47b66b1
+ * @license Angular v21.0.0-next.6+sha-76c8d04
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-47b66b1');
+const VERSION = new Version('21.0.0-next.6+sha-76c8d04');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-47b66b1
+ * @license Angular v21.0.0-next.6+sha-76c8d04
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
