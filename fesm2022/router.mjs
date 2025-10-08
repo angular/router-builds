9056af24d1b4135669af7a2736dab6897ac3dc21
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-10-08 08:23:50 +0000

    47b66b1 release: bump Angular DevTools version to 1.4.0 (#64287)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-5cbdefc
+ * @license Angular v21.0.0-next.6+sha-47b66b1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-5cbdefc');
+const VERSION = new Version('21.0.0-next.6+sha-47b66b1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-5cbdefc
+ * @license Angular v21.0.0-next.6+sha-47b66b1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
