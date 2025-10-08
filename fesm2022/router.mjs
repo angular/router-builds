8ce26ba6ca890354f54cc9154bfa3bacb0a74519
Miles Malerba <mmalerba@google.com>
2025-10-08 00:34:27 +0000

    b49db16 docs(docs-infra): refactor navigation code based on angular style guide (#64177)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-ac0eb23
+ * @license Angular v21.0.0-next.6+sha-b49db16
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-ac0eb23');
+const VERSION = new Version('21.0.0-next.6+sha-b49db16');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-ac0eb23
+ * @license Angular v21.0.0-next.6+sha-b49db16
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
