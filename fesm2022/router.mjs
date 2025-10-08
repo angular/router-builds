c434009da8157841a54a7f2f75ba0cb9d36c2e4b
Miles Malerba <mmalerba@google.com>
2025-10-08 00:52:26 +0000

    5cbdefc feat(http): add support for fetch referrerPolicy option in HttpClient (#64116)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.6+sha-b49db16
+ * @license Angular v21.0.0-next.6+sha-5cbdefc
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.6+sha-b49db16');
+const VERSION = new Version('21.0.0-next.6+sha-5cbdefc');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
