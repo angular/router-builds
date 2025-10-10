42cba6d98184757930707b0afcabb66a859c01f1
Andrew Kushnir <akushnir@google.com>
2025-10-10 13:52:00 +0000

    c4812fe build: update dependency bazel to v7.6.2 (#64331)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.4+sha-8dec92f
+ * @license Angular v20.3.4+sha-c4812fe
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.4+sha-8dec92f');
+const VERSION = new Version('20.3.4+sha-c4812fe');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
