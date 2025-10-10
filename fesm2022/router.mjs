2223a19a776f17fb56e754bf55e599750de747c2
Andrew Kushnir <akushnir@google.com>
2025-10-10 13:58:13 +0000

    da38cd9 docs: add example for assertInInjectionContext (#64325)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.4+sha-c4812fe
+ * @license Angular v20.3.4+sha-da38cd9
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.4+sha-c4812fe');
+const VERSION = new Version('20.3.4+sha-da38cd9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.4+sha-c4812fe
+ * @license Angular v20.3.4+sha-da38cd9
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
