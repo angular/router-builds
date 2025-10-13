2b2cb81709725d730a0c04333d245968c90ec122
Andrew Kushnir <akushnir@google.com>
2025-10-13 23:02:26 +0000

    c0510cd docs: Removes standalone flag from drag-drop examples (#64395)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-2a7a5de
+ * @license Angular v21.0.0-next.7+sha-c0510cd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.7+sha-2a7a5de');
+const VERSION = new Version('21.0.0-next.7+sha-c0510cd');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-2a7a5de
+ * @license Angular v21.0.0-next.7+sha-c0510cd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
