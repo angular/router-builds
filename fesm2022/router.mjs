f8a1e4ae99da800da195dc55945344fa797ab3a4
Andrew Kushnir <akushnir@google.com>
2025-10-13 22:15:38 +0000

    2a7a5de feat(core): Allow passing application providers in `bootstrapModule` options (#64354)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-32f86d3
+ * @license Angular v21.0.0-next.7+sha-2a7a5de
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.7+sha-32f86d3');
+const VERSION = new Version('21.0.0-next.7+sha-2a7a5de');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-32f86d3
+ * @license Angular v21.0.0-next.7+sha-2a7a5de
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
