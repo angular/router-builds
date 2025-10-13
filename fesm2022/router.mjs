c299aef2be128f3df80e4cd74daef107f601452e
Andrew Kushnir <akushnir@google.com>
2025-10-13 16:32:56 +0000

    09a927a ci: remove `cache-node-modules` from `checkout-and-setup-node` (#64387)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-62cda78
+ * @license Angular v21.0.0-next.7+sha-09a927a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.7+sha-62cda78');
+const VERSION = new Version('21.0.0-next.7+sha-09a927a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-62cda78
+ * @license Angular v21.0.0-next.7+sha-09a927a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
