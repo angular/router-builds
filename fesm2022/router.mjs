cefa4eecce1fc13676991f6a476c3ea2b8db0c80
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-15 21:18:06 +0000

    29c397f fix(platform-browser): `DomEventsPlugin` should always be the last plugin to be called for `supports()`. (#50394)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-9b9ae42
+ * @license Angular v21.0.0-next.8+sha-29c397f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-9b9ae42');
+const VERSION = new Version('21.0.0-next.8+sha-29c397f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-9b9ae42
+ * @license Angular v21.0.0-next.8+sha-29c397f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
