cebc53a5b92a210d46d1775424e9b83858055e02
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-10-15 17:20:33 +0000

    013effd release: cut the v21.0.0-next.8 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-e3fa24d
+ * @license Angular v21.0.0-next.8+sha-013effd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.7+sha-e3fa24d');
+const VERSION = new Version('21.0.0-next.8+sha-013effd');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-e3fa24d
+ * @license Angular v21.0.0-next.8+sha-013effd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
