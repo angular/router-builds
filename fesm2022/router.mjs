6a3c8ba67d814cc0ab4d27d5ec7746453785461c
Andrew Kushnir <akushnir@google.com>
2025-10-15 18:52:33 +0000

    e6c0223 docs: update Angular CLI help (#64434)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.5+sha-43fe755
+ * @license Angular v20.3.5+sha-e6c0223
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.5+sha-43fe755');
+const VERSION = new Version('20.3.5+sha-e6c0223');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.5+sha-43fe755
+ * @license Angular v20.3.5+sha-e6c0223
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
