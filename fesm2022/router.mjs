6174a3fdb25c4560d675c77656dd287aa58ae38f
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-10-15 17:12:52 +0000

    e3fa24d docs: release notes for the v20.3.5 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-3d1777b
+ * @license Angular v21.0.0-next.7+sha-e3fa24d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.7+sha-3d1777b');
+const VERSION = new Version('21.0.0-next.7+sha-e3fa24d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-3d1777b
+ * @license Angular v21.0.0-next.7+sha-e3fa24d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
