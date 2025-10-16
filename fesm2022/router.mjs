5379821c22e31bbab68c304f7c0832cab24d7792
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-16 17:48:58 +0000

    911d682 fix(core): update animation scheduling (#64441)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.5+sha-2d159af
+ * @license Angular v20.3.5+sha-911d682
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.5+sha-2d159af');
+const VERSION = new Version('20.3.5+sha-911d682');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
