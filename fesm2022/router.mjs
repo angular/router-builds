897dff47be831603f146d862e90e9a1af2070485
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-16 14:58:58 +0000

    df780fa docs: Add docs for EVENT_MANAGER_PLUGINS (#64453)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-6af231e
+ * @license Angular v21.0.0-next.8+sha-df780fa
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-6af231e');
+const VERSION = new Version('21.0.0-next.8+sha-df780fa');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-6af231e
+ * @license Angular v21.0.0-next.8+sha-df780fa
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
