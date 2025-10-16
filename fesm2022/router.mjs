41efdb2c2740cd52e8115839e0ecbdfb59d071f9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-16 21:36:32 +0000

    c241038 fix(core): update symbols (#64481)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-f7776fa
+ * @license Angular v21.0.0-next.8+sha-c241038
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-f7776fa');
+const VERSION = new Version('21.0.0-next.8+sha-c241038');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-f7776fa
+ * @license Angular v21.0.0-next.8+sha-c241038
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
