1903a6db20a49fcf297124959ebe1f97b95f9d7b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-17 18:21:17 +0000

    062a696 refactor(platform-server): use URL constructor for robust parsing (#64494)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-cc5d698
+ * @license Angular v21.0.0-next.8+sha-062a696
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-cc5d698');
+const VERSION = new Version('21.0.0-next.8+sha-062a696');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-cc5d698
+ * @license Angular v21.0.0-next.8+sha-062a696
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
