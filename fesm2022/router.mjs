696d5c47e33d7be58d1a8d05964b5f26d4d2942d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-17 14:34:45 +0000

    e169900 docs: Documents sourcesContent option for source maps (#64482)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.6+sha-e1a6375
+ * @license Angular v20.3.6+sha-e169900
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.6+sha-e1a6375');
+const VERSION = new Version('20.3.6+sha-e169900');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
