9a36fdef9b4cfa5d0821f5fe9cfe33bf8ec5963a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-17 14:58:01 +0000

    56afa3d docs(docs-infra): Corrects repo name typo in entry parsing logic (#64487)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.6+sha-ccaa9e5
+ * @license Angular v20.3.6+sha-56afa3d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.6+sha-ccaa9e5');
+const VERSION = new Version('20.3.6+sha-56afa3d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.6+sha-ccaa9e5
+ * @license Angular v20.3.6+sha-56afa3d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
