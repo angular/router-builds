894cc0b7890456becb65a3a208594e41237430f0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 16:20:34 +0000

    fad6e13 build: update all non-major dependencies (#64514)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-aca2f75
+ * @license Angular v21.0.0-next.8+sha-fad6e13
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-aca2f75');
+const VERSION = new Version('21.0.0-next.8+sha-fad6e13');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-aca2f75
+ * @license Angular v21.0.0-next.8+sha-fad6e13
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
