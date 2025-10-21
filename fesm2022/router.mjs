4bf9535d5b7b76fd039ea3565d054b3fdea29c9a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-21 16:48:22 +0000

    50e2c54 build: update rules_browsers digest to 6a699bf (#64559)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.6+sha-e568ddb
+ * @license Angular v20.3.6+sha-50e2c54
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.6+sha-e568ddb');
+const VERSION = new Version('20.3.6+sha-50e2c54');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.6+sha-e568ddb
+ * @license Angular v20.3.6+sha-50e2c54
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
