807deca65f81d97a6093d14d040729469b726fdf
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-21 16:55:21 +0000

    bd38cd4 fix(animations): account for `Element.animate` exceptions (#64506)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.6+sha-50e2c54
+ * @license Angular v20.3.6+sha-bd38cd4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.6+sha-50e2c54');
+const VERSION = new Version('20.3.6+sha-bd38cd4');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
