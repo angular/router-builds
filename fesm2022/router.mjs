c939f1341152094e72a260abfd15a97bb15dd28e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-22 22:01:57 +0000

    de2ee6e docs: update Angular CDK apis (#64599)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.9+sha-d183153
+ * @license Angular v21.0.0-next.9+sha-de2ee6e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.9+sha-d183153');
+const VERSION = new Version('21.0.0-next.9+sha-de2ee6e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.9+sha-d183153
+ * @license Angular v21.0.0-next.9+sha-de2ee6e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
