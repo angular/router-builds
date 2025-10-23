79e3bebb33bee7b1b1b760368fc2b5f77dc1773e
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-23 16:17:38 +0000

    02f7263 refactor(forms): rename files related to metadata (#64603)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.9+sha-0385c81
+ * @license Angular v21.0.0-next.9+sha-02f7263
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.9+sha-0385c81');
+const VERSION = new Version('21.0.0-next.9+sha-02f7263');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
