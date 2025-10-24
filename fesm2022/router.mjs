04077a6e139aa727fd56a30a2a67878858e8b22f
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-24 13:29:59 +0000

    b97a677 docs: update workspace configuration link in security documentation

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.7+sha-50d2670
+ * @license Angular v20.3.7+sha-b97a677
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.7+sha-50d2670');
+const VERSION = new Version('20.3.7+sha-b97a677');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.7+sha-50d2670
+ * @license Angular v20.3.7+sha-b97a677
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
