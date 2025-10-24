deedbafb27e7949fb2e755bbefb02acf338c0da7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-24 14:23:20 +0000

    029ed83 docs: update Angular CLI help

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-1a0d2da
+ * @license Angular v21.1.0-next.0+sha-029ed83
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.1.0-next.0+sha-1a0d2da');
+const VERSION = new Version('21.1.0-next.0+sha-029ed83');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
