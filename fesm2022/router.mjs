e34d81b67e0cbf62d1b6b020e76c47b1e098699f
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-24 17:08:19 +0000

    e9f55a8 Revert "build: refactor location of best-practices.md"

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.9+sha-5099ed4
+ * @license Angular v21.0.0-next.9+sha-e9f55a8
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.9+sha-5099ed4');
+const VERSION = new Version('21.0.0-next.9+sha-e9f55a8');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
