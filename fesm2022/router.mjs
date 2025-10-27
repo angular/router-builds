3a7f97cc5512888cd2412a1b88dc847d2f3a9759
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-27 16:27:15 +0000

    8511759 build: update rules_angular digest to 1f1aa3e

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.9+sha-04dd75b
+ * @license Angular v21.0.0-next.9+sha-8511759
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -12,76 +12,23 @@ import 'rxjs';
 import 'rxjs/operators';
 import '@angular/platform-browser';
 
-/**
- * Maps an array of injectable classes with canMatch functions to an array of equivalent
- * `CanMatchFn` for use in a `Route` definition.
- *
- * Usage {@example router/utils/functional_guards.ts region='CanActivate'}
- *
- * @publicApi
- * @see {@link Route}
- */
 function mapToCanMatch(providers) {
-    return providers.map((provider) => (...params) => inject(provider).canMatch(...params));
+  return providers.map(provider => (...params) => inject(provider).canMatch(...params));
 }
-/**
- * Maps an array of injectable classes with canActivate functions to an array of equivalent
- * `CanActivateFn` for use in a `Route` definition.
- *
- * Usage {@example router/utils/functional_guards.ts region='CanActivate'}
- *
- * @publicApi
- * @see {@link Route}
- */
 function mapToCanActivate(providers) {
-    return providers.map((provider) => (...params) => inject(provider).canActivate(...params));
+  return providers.map(provider => (...params) => inject(provider).canActivate(...params));
 }
-/**
- * Maps an array of injectable classes with canActivateChild functions to an array of equivalent
- * `CanActivateChildFn` for use in a `Route` definition.
- *
- * Usage {@example router/utils/functional_guards.ts region='CanActivate'}
- *
- * @publicApi
- * @see {@link Route}
- */
 function mapToCanActivateChild(providers) {
-    return providers.map((provider) => (...params) => inject(provider).canActivateChild(...params));
+  return providers.map(provider => (...params) => inject(provider).canActivateChild(...params));
 }
-/**
- * Maps an array of injectable classes with canDeactivate functions to an array of equivalent
- * `CanDeactivateFn` for use in a `Route` definition.
- *
- * Usage {@example router/utils/functional_guards.ts region='CanActivate'}
- *
- * @publicApi
- * @see {@link Route}
- */
 function mapToCanDeactivate(providers) {
-    return providers.map((provider) => (...params) => inject(provider).canDeactivate(...params));
+  return providers.map(provider => (...params) => inject(provider).canDeactivate(...params));
 }
-/**
- * Maps an injectable class with a resolve function to an equivalent `ResolveFn`
- * for use in a `Route` definition.
- *
- * Usage {@example router/utils/functional_guards.ts region='Resolve'}
- *
- * @publicApi
- * @see {@link Route}
- */
 function mapToResolve(provider) {
-    return (...params) => inject(provider).resolve(...params);
+  return (...params) => inject(provider).resolve(...params);
 }
 
-/**
- * @module
- * @description
- * Entry point for all public APIs of the router package.
- */
-/**
- * @publicApi
- */
-const VERSION = new Version('21.0.0-next.9+sha-04dd75b');
+const VERSION = new Version('21.0.0-next.9+sha-8511759');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.9+sha-04dd75b
+ * @license Angular v21.0.0-next.9+sha-8511759
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
