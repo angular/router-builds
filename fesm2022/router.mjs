5876b44c934dd36d261856e231a5bc4d006f4f14
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-27 18:50:31 +0000

    ca3fd78 fix(core): Clear lView from IcuIteratorState when stack is empty to prevent memory leak

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.7+sha-14b5590
+ * @license Angular v20.3.7+sha-ca3fd78
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.7+sha-14b5590');
+const VERSION = new Version('20.3.7+sha-ca3fd78');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.7+sha-14b5590
+ * @license Angular v20.3.7+sha-ca3fd78
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
