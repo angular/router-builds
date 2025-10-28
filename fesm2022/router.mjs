c5c73f71d345fefee23d42e202e9b544b54c39c5
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-28 14:12:36 +0000

    92d1897 docs: align adev best practices with llm-eval

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.7+sha-f96df92
+ * @license Angular v20.3.7+sha-92d1897
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.7+sha-f96df92');
+const VERSION = new Version('20.3.7+sha-92d1897');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
