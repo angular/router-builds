7331f1f52304235f810adc6ce34727855debb7fa
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-28 15:02:41 +0000

    766559e fix(docs-infra): update guide navigation to remain active with query params

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.7+sha-92d1897
+ * @license Angular v20.3.7+sha-766559e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.7+sha-92d1897');
+const VERSION = new Version('20.3.7+sha-766559e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.7+sha-92d1897
+ * @license Angular v20.3.7+sha-766559e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
