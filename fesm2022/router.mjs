1aff3e42974e42bca6cbbb856aae174e06c4e0a7
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-28 15:08:34 +0000

    8e50cdb refactor(compiler-cli): Remove deep imports of `compiler-cli` in angular/core

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-07b8e95
+ * @license Angular v21.1.0-next.0+sha-8e50cdb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-07b8e95');
+const VERSION = new Version('21.1.0-next.0+sha-8e50cdb');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
