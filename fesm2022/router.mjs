f6271e3cd7a4486c2086341736c652e3c5d656e9
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-29 13:16:14 +0000

    9123e3d fix(docs-infra): fix broken adev tests (#64761)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.7+sha-2edc82a
+ * @license Angular v20.3.7+sha-9123e3d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.7+sha-2edc82a');
+const VERSION = new Version('20.3.7+sha-9123e3d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
