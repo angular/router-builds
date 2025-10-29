fa9a75d26362b0536131cdd5e470c9e768e74f34
Andrew Scott <atscott@google.com>
2025-10-29 20:30:27 +0000

    981fabc feat(docs-infra): add support for namespaces

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-2a06e2b
+ * @license Angular v21.0.0-rc.0+sha-981fabc
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.0.0-rc.0+sha-2a06e2b');
+const VERSION = new Version('21.0.0-rc.0+sha-981fabc');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-2a06e2b
+ * @license Angular v21.0.0-rc.0+sha-981fabc
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
