6fe28f99edf119db57545044f816e56d20d20dc6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-29 17:41:20 +0000

    a639f7a docs: release notes for the v20.3.9 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-aaa3020
+ * @license Angular v21.1.0-next.0+sha-a639f7a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-aaa3020');
+const VERSION = new Version('21.1.0-next.0+sha-a639f7a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
