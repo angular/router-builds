18f532246faf1c733676def5d238227ea96f2cdd
Andrew Scott <atscott@google.com>
2025-10-29 20:53:39 +0000

    366392b refactor(compiler): desambiguate and export `RegularExpressionLiteralExpr`

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-81b18fc
+ * @license Angular v21.0.0-rc.0+sha-366392b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.0.0-rc.0+sha-81b18fc');
+const VERSION = new Version('21.0.0-rc.0+sha-366392b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-81b18fc
+ * @license Angular v21.0.0-rc.0+sha-366392b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
