3db92904eec03228cf2ae602b367a79bcd217e31
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-29 11:42:59 +0000

    aaa3020 refactor(devtools): Improves error messaging for profiler file import

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-64cb085
+ * @license Angular v21.1.0-next.0+sha-aaa3020
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-64cb085');
+const VERSION = new Version('21.1.0-next.0+sha-aaa3020');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-64cb085
+ * @license Angular v21.1.0-next.0+sha-aaa3020
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
