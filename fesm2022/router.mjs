4694e7ace732ecaf2d622e48338d900b5e73234d
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-29 17:54:00 +0000

    54ad3f6 docs: Rename TodoListItem to UserProfile and fix usage of `isValidUserId`

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-b517c9c
+ * @license Angular v20.3.9+sha-54ad3f6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.9+sha-b517c9c');
+const VERSION = new Version('20.3.9+sha-54ad3f6');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
