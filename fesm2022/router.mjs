62f6352f1ac18969a7f7ee7beb1f2f3048dc74cf
Andrew Scott <atscott01@gmail.com>
2025-10-30 17:29:49 +0000

    040585d ci: make the github action fail when a firebase command fails (#64801)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-54ad3f6
+ * @license Angular v20.3.9+sha-040585d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.9+sha-54ad3f6');
+const VERSION = new Version('20.3.9+sha-040585d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
