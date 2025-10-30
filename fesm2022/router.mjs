afc689eb6d0d3979c158aecdc2b5947ef058ceb2
Andrew Scott <atscott01@gmail.com>
2025-10-30 19:27:11 +0000

    f321e22 docs: update dependency injection guides (part 1) (#63652)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-29d6eba
+ * @license Angular v21.1.0-next.0+sha-f321e22
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-29d6eba');
+const VERSION = new Version('21.1.0-next.0+sha-f321e22');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-29d6eba
+ * @license Angular v21.1.0-next.0+sha-f321e22
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
