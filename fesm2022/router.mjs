c8eea650098de220ab9d02240cbe023ef67c53b9
Andrew Scott <atscott01@gmail.com>
2025-10-30 17:29:43 +0000

    58f921d docs: add essential signal forms guide (#64665)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-90452f5
+ * @license Angular v21.0.0-rc.0+sha-58f921d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.0.0-rc.0+sha-90452f5');
+const VERSION = new Version('21.0.0-rc.0+sha-58f921d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
