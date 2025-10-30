a02dcaed417f8044d36bd0701137dbca5823e72d
Andrew Scott <atscott01@gmail.com>
2025-10-30 19:27:08 +0000

    fee4baf docs: update dependency injection guides (part 1) (#63652)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-d23bb3c
+ * @license Angular v21.0.0-rc.0+sha-fee4baf
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.0.0-rc.0+sha-d23bb3c');
+const VERSION = new Version('21.0.0-rc.0+sha-fee4baf');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-d23bb3c
+ * @license Angular v21.0.0-rc.0+sha-fee4baf
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
