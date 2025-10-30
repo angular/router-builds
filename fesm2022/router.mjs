9580f9444d8c03a480169b7d446cc8c9bd62d518
Andrew Scott <atscott01@gmail.com>
2025-10-30 19:38:31 +0000

    a4fe078 refactor(common): remove unused import (#64699)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-fee4baf
+ * @license Angular v21.0.0-rc.0+sha-a4fe078
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.0.0-rc.0+sha-fee4baf');
+const VERSION = new Version('21.0.0-rc.0+sha-a4fe078');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-fee4baf
+ * @license Angular v21.0.0-rc.0+sha-a4fe078
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
