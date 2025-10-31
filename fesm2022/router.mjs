d06f6fdbc823a243b31dddcf189fc9bce3c801e0
Andrew Scott <atscott01@gmail.com>
2025-10-31 22:00:12 +0000

    334f856 build: update dependency bazel to v7.7.0

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-a0fe177
+ * @license Angular v21.1.0-next.0+sha-334f856
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-a0fe177');
+const VERSION = new Version('21.1.0-next.0+sha-334f856');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-a0fe177
+ * @license Angular v21.1.0-next.0+sha-334f856
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
