30b4061edcd4958fb58a4ee9e796e1f9d32cffce
Andrew Scott <atscott01@gmail.com>
2025-11-03 21:40:31 +0000

    429672d docs(docs-infra): Updates page title strategy to sync meta tags

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-434ddbc
+ * @license Angular v21.1.0-next.0+sha-429672d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-434ddbc');
+const VERSION = new Version('21.1.0-next.0+sha-429672d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
