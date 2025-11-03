693c7647b2acfdc07593f56065a7e354c0b284af
Andrew Scott <atscott@google.com>
2025-11-03 21:06:31 +0000

    3409fe8 docs(docs-infra): ensure all redirections are absolute

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-1572baf
+ * @license Angular v20.3.9+sha-3409fe8
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.9+sha-1572baf');
+const VERSION = new Version('20.3.9+sha-3409fe8');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-1572baf
+ * @license Angular v20.3.9+sha-3409fe8
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
