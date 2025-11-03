a028d076eaece376c3e24db3bb6f2ed198c423e2
Andrew Scott <atscott01@gmail.com>
2025-11-03 20:10:01 +0000

    79b7ac7 refactor(core): Replace usages of the `Function` type for animations

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-bfdb652
+ * @license Angular v21.1.0-next.0+sha-79b7ac7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-bfdb652');
+const VERSION = new Version('21.1.0-next.0+sha-79b7ac7');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-bfdb652
+ * @license Angular v21.1.0-next.0+sha-79b7ac7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
