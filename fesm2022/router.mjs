5df690aaf4b098618f2b587bc92917dd27f06c63
Andrew Scott <atscott@google.com>
2025-11-04 00:03:45 +0000

    65c7805 docs: add v21 landing page

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-a57c03d
+ * @license Angular v20.3.9+sha-65c7805
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.9+sha-a57c03d');
+const VERSION = new Version('20.3.9+sha-65c7805');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-a57c03d
+ * @license Angular v20.3.9+sha-65c7805
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
