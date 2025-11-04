12cd6bfc02617c6ddbfbe1d3fbbefe426ba7e2b6
Andrew Scott <atscott01@gmail.com>
2025-11-04 18:45:24 +0000

    0a0fe44 feat(docs-infra): generate the sitemap for angular.dev

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-dd7cb3d
+ * @license Angular v20.3.9+sha-0a0fe44
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.9+sha-dd7cb3d');
+const VERSION = new Version('20.3.9+sha-0a0fe44');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-dd7cb3d
+ * @license Angular v20.3.9+sha-0a0fe44
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
