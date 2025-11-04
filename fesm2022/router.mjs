a6007c5cb3d9f9014333eb64d894a093546e8c88
Andrew Scott <atscott01@gmail.com>
2025-11-04 19:22:22 +0000

    9596020 docs: update cross-repo adev docs

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-0b1339e
+ * @license Angular v21.0.0-rc.0+sha-9596020
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.0.0-rc.0+sha-0b1339e');
+const VERSION = new Version('21.0.0-rc.0+sha-9596020');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
