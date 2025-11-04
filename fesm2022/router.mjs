c374f671e1d0fc8af39a23d1b5ef7db1cf974bb9
Andrew Scott <atscott@google.com>
2025-11-04 00:31:17 +0000

    3d6a206 docs: remove standalone: true since it's now the default

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-192eb35
+ * @license Angular v20.3.9+sha-3d6a206
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.9+sha-192eb35');
+const VERSION = new Version('20.3.9+sha-3d6a206');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-192eb35
+ * @license Angular v20.3.9+sha-3d6a206
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
