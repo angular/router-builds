98567686fe0fe9c9fe2121a62288d0a4374827c5
Andrew Scott <atscott01@gmail.com>
2025-11-04 00:31:59 +0000

    c4c7fb8 docs: remove standalone: true since it's now the default

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-7a422c2
+ * @license Angular v21.1.0-next.0+sha-c4c7fb8
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-7a422c2');
+const VERSION = new Version('21.1.0-next.0+sha-c4c7fb8');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-7a422c2
+ * @license Angular v21.1.0-next.0+sha-c4c7fb8
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
