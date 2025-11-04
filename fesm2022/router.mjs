2f21d32bc3c3603c00a1eec00aca712561590f20
Andrew Scott <atscott@google.com>
2025-11-04 23:30:21 +0000

    a45e6b2 fix(migrations): Prevent removal of templates referenced with preceding whitespace characters

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-c374e80
+ * @license Angular v20.3.9+sha-a45e6b2
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.9+sha-c374e80');
+const VERSION = new Version('20.3.9+sha-a45e6b2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-c374e80
+ * @license Angular v20.3.9+sha-a45e6b2
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
