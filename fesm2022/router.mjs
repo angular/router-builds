3e08ced5498ec2e2a38fd8c529f7f532468e1084
Andrew Scott <atscott@google.com>
2025-11-04 00:38:56 +0000

    4f7b97c docs: simplify tutorial signals with self-closing component tags

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-3d6a206
+ * @license Angular v20.3.9+sha-4f7b97c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.9+sha-3d6a206');
+const VERSION = new Version('20.3.9+sha-4f7b97c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
