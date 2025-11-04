6c98e5531f98b2854eab1c9f0a94240ece537942
Andrew Scott <atscott01@gmail.com>
2025-11-04 17:44:06 +0000

    8f49af9 docs(router): uniformize lazy loading syntax

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-2ad1b59
+ * @license Angular v21.1.0-next.0+sha-8f49af9
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-2ad1b59');
+const VERSION = new Version('21.1.0-next.0+sha-8f49af9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
