c145165807881ef4bed022f80261c7fa717c055c
Andrew Scott <atscott01@gmail.com>
2025-11-04 23:30:52 +0000

    5b210e9 fix(migrations): Prevent removal of templates referenced with preceding whitespace characters

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-c277214
+ * @license Angular v21.1.0-next.0+sha-5b210e9
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-c277214');
+const VERSION = new Version('21.1.0-next.0+sha-5b210e9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
