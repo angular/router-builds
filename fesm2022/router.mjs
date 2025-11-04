cd1ebe63fcc834874ef64f5ce7daf39d2e5c6d2c
Andrew Scott <atscott@google.com>
2025-11-04 04:26:58 +0000

    36f4c9b refactor(core): Removes unused flag for onDestroy

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-3140b87
+ * @license Angular v21.0.0-rc.0+sha-36f4c9b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.0.0-rc.0+sha-3140b87');
+const VERSION = new Version('21.0.0-rc.0+sha-36f4c9b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
