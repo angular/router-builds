13f82d20bd62bfafede640bbacb7003e694f69db
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-05 23:29:26 +0000

    de1f22c build: update diff release package script

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-9ae1f44
+ * @license Angular v21.1.0-next.0+sha-de1f22c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-9ae1f44');
+const VERSION = new Version('21.1.0-next.0+sha-de1f22c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-9ae1f44
+ * @license Angular v21.1.0-next.0+sha-de1f22c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
