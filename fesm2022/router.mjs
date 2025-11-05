1c286ec025eefa85d3dbc58d98904c2180f02c9b
GitHub <noreply@github.com>
2025-11-05 20:00:22 +0000

    e1dfe32 build: add VSCode extension release script

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-e18aac8
+ * @license Angular v21.1.0-next.0+sha-e1dfe32
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-e18aac8');
+const VERSION = new Version('21.1.0-next.0+sha-e1dfe32');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
