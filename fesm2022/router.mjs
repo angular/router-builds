07918f9359536e1e94cab4c2ac6d8890c3361729
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-06 20:05:43 +0000

    1dcecdf refactor(platform-browser): remove unused Platform ID dependency from DomRendererFactory2

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.10+sha-036c5d2
+ * @license Angular v20.3.10+sha-1dcecdf
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.10+sha-036c5d2');
+const VERSION = new Version('20.3.10+sha-1dcecdf');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.10+sha-036c5d2
+ * @license Angular v20.3.10+sha-1dcecdf
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
