d8812fec9421f351005aea4f4ff4621d58b41e9a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-06 22:30:00 +0000

    e69ada4 refactor(core): move profile_types.ts to primtives

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-6d6fda8
+ * @license Angular v21.0.0-rc.1+sha-e69ada4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.0.0-rc.1+sha-6d6fda8');
+const VERSION = new Version('21.0.0-rc.1+sha-e69ada4');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-6d6fda8
+ * @license Angular v21.0.0-rc.1+sha-e69ada4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
