6891ec50c13f5f29547f07880fa7f51b73a7a84c
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-06 21:14:28 +0000

    dcfd7fe Revert "build: restrain visibility to compiler-cli subpackages (#64732)"

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-acaa1e6
+ * @license Angular v21.0.0-rc.1+sha-dcfd7fe
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.0.0-rc.1+sha-acaa1e6');
+const VERSION = new Version('21.0.0-rc.1+sha-dcfd7fe');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-acaa1e6
+ * @license Angular v21.0.0-rc.1+sha-dcfd7fe
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
