c085322e5c4f4696558e501031f5a59479cb9f78
Andrew Kushnir <akushnir@google.com>
2025-11-07 19:41:58 +0000

    1e53f4e refactor: exclude tsconfig.json from VSIX package contents (#64991)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-f54cc4f
+ * @license Angular v21.0.0-rc.1+sha-1e53f4e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.0.0-rc.1+sha-f54cc4f');
+const VERSION = new Version('21.0.0-rc.1+sha-1e53f4e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-f54cc4f
+ * @license Angular v21.0.0-rc.1+sha-1e53f4e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
