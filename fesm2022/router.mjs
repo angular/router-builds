3108b7c291411639f47f0a9bfcb15bda2a2760a9
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-07 18:18:43 +0000

    dff8035 docs(docs-infra): refactor update to use host property and refactor isWindows

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-0efb3f6
+ * @license Angular v21.1.0-next.0+sha-dff8035
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-0efb3f6');
+const VERSION = new Version('21.1.0-next.0+sha-dff8035');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-0efb3f6
+ * @license Angular v21.1.0-next.0+sha-dff8035
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
