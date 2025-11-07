df5a45f2160202da9bf460e66e295ffadbe06082
Andrew Kushnir <akushnir@google.com>
2025-11-07 18:51:44 +0000

    f54cc4f fix(compiler): support commas in :host() argument

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-0552e72
+ * @license Angular v21.0.0-rc.1+sha-f54cc4f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.0.0-rc.1+sha-0552e72');
+const VERSION = new Version('21.0.0-rc.1+sha-f54cc4f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-0552e72
+ * @license Angular v21.0.0-rc.1+sha-f54cc4f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
