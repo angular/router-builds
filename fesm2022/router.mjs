91982a208499735f721acebafec52a10be003d1a
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-07 20:08:05 +0000

    850f0d6 perf(forms): only update interop controls when bound field changes

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-033ea75
+ * @license Angular v21.1.0-next.0+sha-850f0d6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-033ea75');
+const VERSION = new Version('21.1.0-next.0+sha-850f0d6');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
