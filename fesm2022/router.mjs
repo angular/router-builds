0fe2e6b1e84b20deaa3aa682d1538d63e00b5862
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-08 00:07:39 +0000

    b47054c Revert "fix(compiler): support commas in :host() argument"

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.10+sha-143883a
+ * @license Angular v20.3.10+sha-b47054c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.10+sha-143883a');
+const VERSION = new Version('20.3.10+sha-b47054c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.10+sha-143883a
+ * @license Angular v20.3.10+sha-b47054c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
