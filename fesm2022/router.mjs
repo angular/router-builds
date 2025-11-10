5a55143cb9af5867ca8397bd279aef8aa4be449b
Andrew Kushnir <akushnir@google.com>
2025-11-10 22:21:13 +0000

    e0407c0 docs: add links to SDUI example and livestream

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.10+sha-8922cae
+ * @license Angular v20.3.10+sha-e0407c0
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.10+sha-8922cae');
+const VERSION = /* @__PURE__ */ new Version('20.3.10+sha-e0407c0');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.10+sha-8922cae
+ * @license Angular v20.3.10+sha-e0407c0
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
