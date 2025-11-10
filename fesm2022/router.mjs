c6c618dd99973f22e421a3aa644013e492d4853b
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-10 16:07:35 +0000

    2db8f2e refactor(router): Add handling of abort event on signal of NavigateEvent

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-d2b854b
+ * @license Angular v21.1.0-next.0+sha-2db8f2e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-d2b854b');
+const VERSION = new Version('21.1.0-next.0+sha-2db8f2e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-d2b854b
+ * @license Angular v21.1.0-next.0+sha-2db8f2e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
