216d5cd824b199a43a9c7bdca30a036334660512
Andrew Kushnir <akushnir@google.com>
2025-11-10 16:05:56 +0000

    b20bf5d docs: add shell language to CLI installation examples

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.10+sha-b47054c
+ * @license Angular v20.3.10+sha-b20bf5d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.10+sha-b47054c');
+const VERSION = new Version('20.3.10+sha-b20bf5d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.10+sha-b47054c
+ * @license Angular v20.3.10+sha-b20bf5d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
