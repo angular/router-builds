916bfaaa51745372c50032e4febff7634a242f34
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-10 18:39:04 +0000

    4ff6348 docs: update cross-repo adev docs

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-cfb26f5
+ * @license Angular v21.1.0-next.0+sha-4ff6348
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-cfb26f5');
+const VERSION = new Version('21.1.0-next.0+sha-4ff6348');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
