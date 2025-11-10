2cf4400b3d9fbf5f339e52114e875c8d185c320a
Andrew Kushnir <akushnir@google.com>
2025-11-10 16:07:30 +0000

    5092a89 docs: add shell language to CLI installation examples

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-590244a
+ * @license Angular v21.0.0-rc.1+sha-5092a89
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = new Version('21.0.0-rc.1+sha-590244a');
+const VERSION = new Version('21.0.0-rc.1+sha-5092a89');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
