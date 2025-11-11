157a6081cab2b2626a38fa763f3cb027aadd510e
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-11 20:04:53 +0000

    d337cfb feat(forms): add `debounce()` rule for signal forms

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-631cf97
+ * @license Angular v21.1.0-next.0+sha-d337cfb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-631cf97');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-d337cfb');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-631cf97
+ * @license Angular v21.1.0-next.0+sha-d337cfb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
