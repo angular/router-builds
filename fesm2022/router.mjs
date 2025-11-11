76069b18574128b8d24b3d310e0d0f5b21f96720
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-11 16:58:04 +0000

    91dbb4b build: lock file maintenance

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-557213e
+ * @license Angular v21.0.0-rc.1+sha-91dbb4b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-557213e');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-91dbb4b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
