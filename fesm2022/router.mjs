e784363bc3275dea8e670d0729d6eb17361d5d95
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-12 17:49:25 +0000

    8e81d0a build: update pnpm to v10.21.0

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-4609991
+ * @license Angular v21.1.0-next.0+sha-8e81d0a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-4609991');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-8e81d0a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-4609991
+ * @license Angular v21.1.0-next.0+sha-8e81d0a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
