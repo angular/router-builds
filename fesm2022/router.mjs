a3f14a22930a40139a2f9261eed2b159164451ec
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-12 19:39:14 +0000

    3737724 docs: correct code block language in error handling and signals interop

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.11+sha-3bafe23
+ * @license Angular v20.3.11+sha-3737724
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.11+sha-3bafe23');
+const VERSION = /* @__PURE__ */ new Version('20.3.11+sha-3737724');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
