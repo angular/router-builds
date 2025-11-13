b0eacebddfb66dc11115cefb1c35b1d1445a2a76
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-13 23:18:34 +0000

    4b0f208 ci: use hardcoded global approvers group

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-482e137
+ * @license Angular v21.0.0-rc.2+sha-4b0f208
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-482e137');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-4b0f208');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-482e137
+ * @license Angular v21.0.0-rc.2+sha-4b0f208
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
