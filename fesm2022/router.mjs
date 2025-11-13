5f67de26870c86a3d2ef8fc760c16ceed3cb2504
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-13 17:23:56 +0000

    3f80ae7 fix(core): SSR error in signal forms

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-b6a91d4
+ * @license Angular v21.1.0-next.0+sha-3f80ae7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-b6a91d4');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-3f80ae7');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-b6a91d4
+ * @license Angular v21.1.0-next.0+sha-3f80ae7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
