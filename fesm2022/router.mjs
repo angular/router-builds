992360d5df3d1560761e9b57878cfec9a5032840
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-13 22:10:05 +0000

    70da36f docs(docs-infra): improve theme picker accessibility and add animations

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.11+sha-06bc146
+ * @license Angular v20.3.11+sha-70da36f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.11+sha-06bc146');
+const VERSION = /* @__PURE__ */ new Version('20.3.11+sha-70da36f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
