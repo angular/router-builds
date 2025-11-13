1a1c96be7b2890364f14024b8af451c4e18e777c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-13 17:24:25 +0000

    67fbd5f fix(core): SSR error in signal forms

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-31ca56a
+ * @license Angular v21.0.0-rc.2+sha-67fbd5f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-31ca56a');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-67fbd5f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-31ca56a
+ * @license Angular v21.0.0-rc.2+sha-67fbd5f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
