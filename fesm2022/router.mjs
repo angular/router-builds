60dcc11175716d807ef8f51d604228b098914d69
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 16:47:01 +0000

    c137f1f docs: ExperimentalIsolatedShadowDom mentions

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-8989536
+ * @license Angular v21.1.0-next.0+sha-c137f1f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-8989536');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-c137f1f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-8989536
+ * @license Angular v21.1.0-next.0+sha-c137f1f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
