9abcb748b61f2450c2e6053267418557f8751b33
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 16:51:14 +0000

    4126ef1 fix(devtools): use isLazy flag to determine disabled state for action button

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-c137f1f
+ * @license Angular v21.1.0-next.0+sha-4126ef1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-c137f1f');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-4126ef1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
