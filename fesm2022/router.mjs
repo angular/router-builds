7489f38481521ffcfcdd07172c675bc88dbd7bd2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 16:48:44 +0000

    f550f4a docs: ExperimentalIsolatedShadowDom mentions

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-c2a52fd
+ * @license Angular v21.0.0-rc.2+sha-f550f4a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-c2a52fd');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-f550f4a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-c2a52fd
+ * @license Angular v21.0.0-rc.2+sha-f550f4a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
