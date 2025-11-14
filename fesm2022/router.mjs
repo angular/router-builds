16b0f45381fdc60737365630f07b719d80637bf0
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 17:27:37 +0000

    fc1ef79 build(forms): expose signal forms compat package

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-5552707
+ * @license Angular v21.1.0-next.0+sha-fc1ef79
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-5552707');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-fc1ef79');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-5552707
+ * @license Angular v21.1.0-next.0+sha-fc1ef79
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
