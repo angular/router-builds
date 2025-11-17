df7055e714bca8eb76810e19ba597997567f7906
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 18:44:05 +0000

    cb481a5 docs: aria listbox examples (#65273)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-88dfd96
+ * @license Angular v21.1.0-next.0+sha-cb481a5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-88dfd96');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-cb481a5');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-88dfd96
+ * @license Angular v21.1.0-next.0+sha-cb481a5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
