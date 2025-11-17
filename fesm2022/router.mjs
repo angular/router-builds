bd1244e1610647d3a19b795cf5993acbd7de24c2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 22:44:37 +0000

    81b6e45 docs(docs-infra): add advanced recommendations

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.12+sha-967e111
+ * @license Angular v20.3.12+sha-81b6e45
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-967e111');
+const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-81b6e45');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
