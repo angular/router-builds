15ea4fba8c988d07253534bc8333684af67c36f6
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-17 21:43:32 +0000

    f279aab release: cut the v21.0.0-rc.3 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-e8f238a
+ * @license Angular v21.0.0-rc.3+sha-f279aab
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-e8f238a');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-f279aab');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
