08b02971ad990ff5983728b3ceae09daeabb470c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 19:22:21 +0000

    6551b65 docs: update examples to latest Angular

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-b7e9183
+ * @license Angular v21.0.0-rc.2+sha-6551b65
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-b7e9183');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-6551b65');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-b7e9183
+ * @license Angular v21.0.0-rc.2+sha-6551b65
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
