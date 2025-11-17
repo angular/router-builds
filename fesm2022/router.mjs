80d45f9d39821c49a180e7503c47d0d0e2a5c973
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-17 23:23:26 +0000

    8187833 build: update cross-repo angular dependencies to v21.0.0-rc.3

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-c93d021
+ * @license Angular v21.1.0-next.0+sha-8187833
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-c93d021');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-8187833');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
