e0b69db4cbfae7c195ef4ef13ee99fd3b1f96ddf
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-17 22:37:57 +0000

    f13159f docs: fix markdown table in custom elements guide

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-cbd77b9
+ * @license Angular v21.1.0-next.0+sha-f13159f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-cbd77b9');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-f13159f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
