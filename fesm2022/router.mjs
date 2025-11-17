4ecc863775d843d0398d7ab8a5eabe8b08421bbd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 16:16:33 +0000

    59543d6 docs: add bun package manager and standardize order in testing guides

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-74b95de
+ * @license Angular v21.0.0-rc.2+sha-59543d6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-74b95de');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.2+sha-59543d6');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
