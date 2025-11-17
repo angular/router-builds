ccd04bfe233794f04a5bdcf100ff5171719e02e0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 16:05:21 +0000

    f7fcef1 docs(docs-infra): simplify file headers in <docs-code> blocks

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.12+sha-e0a6dad
+ * @license Angular v20.3.12+sha-f7fcef1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-e0a6dad');
+const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-f7fcef1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.12+sha-e0a6dad
+ * @license Angular v20.3.12+sha-f7fcef1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
