198ec7f990b72f928b8a6cee059ece63516ffd2d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 17:34:11 +0000

    4a1a703 docs(docs-infra): set max-height to code example

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.12+sha-daae263
+ * @license Angular v20.3.12+sha-4a1a703
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-daae263');
+const VERSION = /* @__PURE__ */ new Version('20.3.12+sha-4a1a703');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.12+sha-daae263
+ * @license Angular v20.3.12+sha-4a1a703
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
