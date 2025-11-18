0030e1c9499db16a9bc3f1c504682efee6d82e8c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-18 22:17:54 +0000

    73e9700 docs: format examples with replacing <docs-code> typescript examples with fenced ts code blocks

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-e43b4da
+ * @license Angular v21.0.0-rc.3+sha-73e9700
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-e43b4da');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-73e9700');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
