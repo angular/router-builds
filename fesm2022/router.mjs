fd4031e1571748f40a1e66ce1cfe724b907eb280
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 21:12:53 +0000

    3d9c82c build: provide a specific version of zone.js for the vscode pre standalone test

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-28810e0
+ * @license Angular v21.0.0+sha-3d9c82c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-28810e0');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-3d9c82c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
