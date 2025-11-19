cbefdbde8c54ce35fee1515ca836b502644907a5
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-19 01:05:47 +0000

    7a910e6 docs: add signal forms comparison guide

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-47eb77f
+ * @license Angular v21.1.0-next.0+sha-7a910e6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-47eb77f');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-7a910e6');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
