15a1c969304dd6080b1de51687899ed8bb658ea4
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-19 22:57:50 +0000

    38f4ea1 docs: add signal forms validation guide

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-e9ba63a
+ * @license Angular v21.1.0-next.0+sha-38f4ea1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-e9ba63a');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-38f4ea1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-e9ba63a
+ * @license Angular v21.1.0-next.0+sha-38f4ea1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
