29a9651bb5773df84328b3e402120b0acab44487
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-19 16:31:24 +0000

    040af1a fix(docs-infra): add moduleResolution to TypeScript compiler options for playground

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-1289371
+ * @license Angular v21.1.0-next.0+sha-040af1a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-1289371');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-040af1a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
