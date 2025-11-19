263bd81e29aa88341911846203dda3587e05f4bd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 16:24:35 +0000

    e69855a docs: aria grid replace overview example with a playful one

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-28c1a2d
+ * @license Angular v21.0.0-rc.3+sha-e69855a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-28c1a2d');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-e69855a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-28c1a2d
+ * @license Angular v21.0.0-rc.3+sha-e69855a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
