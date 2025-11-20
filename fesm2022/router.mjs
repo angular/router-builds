10b184c02856d22fddfc5b86fca75af1c138b47e
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-20 16:51:02 +0000

    183d194 docs: add 20 to 21 update guide

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-29185a8
+ * @license Angular v21.1.0-next.0+sha-183d194
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-29185a8');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-183d194');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-29185a8
+ * @license Angular v21.1.0-next.0+sha-183d194
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
