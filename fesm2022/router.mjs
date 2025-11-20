89355e40483f72601001827934e2c42fdce25d33
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-20 19:30:26 +0000

    82c085d docs: update cross-repo adev docs

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-7219759
+ * @license Angular v21.0.0+sha-82c085d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-7219759');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-82c085d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-7219759
+ * @license Angular v21.0.0+sha-82c085d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
