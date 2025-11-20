ca0ae675bd8eecdd221c063bc30707c8f7e48c88
kirjs <kirts@google.com>
2025-11-20 17:46:02 +0000

    cd86de1 docs: consolidate patterns and iterate on conclusion for angular aria overview

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-eadc8de
+ * @license Angular v21.0.0+sha-cd86de1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-eadc8de');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-cd86de1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-eadc8de
+ * @license Angular v21.0.0+sha-cd86de1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
