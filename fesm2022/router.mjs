2cc990b0383730fd776e0f9b04032bfe7024d9bc
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-21 20:53:03 +0000

    68aa83e docs: format JSON examples and replace <docs-code> with fenced code blocks

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-67d3c25
+ * @license Angular v21.1.0-next.0+sha-68aa83e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-67d3c25');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-68aa83e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
