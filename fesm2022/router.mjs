e79b6e953ec95da4189adc5e9cf09ad95995700a
kirjs <kirts@google.com>
2025-11-21 16:01:33 +0000

    1cdcd5a docs: fix incorrect Signal Forms link

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-053bf98
+ * @license Angular v21.0.0+sha-1cdcd5a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-053bf98');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-1cdcd5a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-053bf98
+ * @license Angular v21.0.0+sha-1cdcd5a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
