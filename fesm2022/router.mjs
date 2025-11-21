2e1a520eaca5d4bed1b89609221c4a8abcdd6fda
kirjs <kirts@google.com>
2025-11-21 20:58:15 +0000

    b8da1d4 docs: format JSON examples and replace <docs-code> with fenced code blocks

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-16babdd
+ * @license Angular v21.0.0+sha-b8da1d4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-16babdd');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-b8da1d4');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
