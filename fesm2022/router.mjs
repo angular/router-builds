df3f9538a9b8444f5f88105ed34374934e5237e7
kirjs <kirts@google.com>
2025-11-22 14:15:36 +0000

    696aaa1 docs: fix router event subscription in error handler example

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-a70a191
+ * @license Angular v21.0.0+sha-696aaa1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-a70a191');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-696aaa1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-a70a191
+ * @license Angular v21.0.0+sha-696aaa1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
