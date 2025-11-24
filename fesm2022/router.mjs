c44adfe007a24e66b35c48d9fa47b8fcfba74d44
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 14:39:46 +0000

    5961e96 build: update all github actions

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-48a05dd
+ * @license Angular v21.1.0-next.0+sha-5961e96
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-48a05dd');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-5961e96');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-48a05dd
+ * @license Angular v21.1.0-next.0+sha-5961e96
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
