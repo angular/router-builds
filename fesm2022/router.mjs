22c607b7e3cedddc68178fa9a9fbfbfecd08ea32
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 17:23:43 +0000

    9dd7cc1 ci: adding myself as optional to the compiler scope

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-0f4b11c
+ * @license Angular v21.1.0-next.0+sha-9dd7cc1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-0f4b11c');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-9dd7cc1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-0f4b11c
+ * @license Angular v21.1.0-next.0+sha-9dd7cc1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
