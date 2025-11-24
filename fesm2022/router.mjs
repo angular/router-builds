031f4d73aed3ebdadff38da3cb73e97e9d725be2
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 14:12:40 +0000

    48a05dd docs: fix v21 event header image

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-72b1f7f
+ * @license Angular v21.1.0-next.0+sha-48a05dd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-72b1f7f');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-48a05dd');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
