1c2a26406b363b11155ed18ae40ff925ba4fe49b
kirjs <kirts@google.com>
2025-11-24 17:28:22 +0000

    70bc311 docs(docs-infra): keep 'Show code' button aligned right and prevent metadata wrapping

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-9852033
+ * @license Angular v21.0.0+sha-70bc311
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-9852033');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-70bc311');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-9852033
+ * @license Angular v21.0.0+sha-70bc311
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
