9cc05079a1a1152c1ef9da6ad2e8dd122b83450e
kirjs <kirts@google.com>
2025-11-24 15:43:40 +0000

    4303f51 docs: html examples and replace <docs-code> with fenced code blocks

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-1f20b54
+ * @license Angular v21.0.0+sha-4303f51
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-1f20b54');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-4303f51');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-1f20b54
+ * @license Angular v21.0.0+sha-4303f51
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
