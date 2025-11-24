a35325f1d675ef930bfc8b9be0954d39338e9cf3
kirjs <kirts@google.com>
2025-11-24 22:04:26 +0000

    0e458c7 refactor(core): Remove toggles used for zoneless by default migration

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-70507b8
+ * @license Angular v21.0.0+sha-0e458c7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-70507b8');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-0e458c7');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-70507b8
+ * @license Angular v21.0.0+sha-0e458c7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
