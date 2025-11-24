b36c32f76c999efb796dc59b6d9c29b8e7e45e93
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 17:45:04 +0000

    a4f50bd fix(migrations): detect structural ngTemplateOutlet and ngComponentOutlet

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-b26fe73
+ * @license Angular v21.1.0-next.0+sha-a4f50bd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-b26fe73');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-a4f50bd');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-b26fe73
+ * @license Angular v21.1.0-next.0+sha-a4f50bd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
