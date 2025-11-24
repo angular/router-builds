4fa4fa18eb3a8e87b6fcb749cd23a122a1e30c67
kirjs <kirts@google.com>
2025-11-24 15:30:16 +0000

    1f20b54 docs(docs-infra): simplify tab selection

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-2d658b3
+ * @license Angular v21.0.0+sha-1f20b54
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-2d658b3');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-1f20b54');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
