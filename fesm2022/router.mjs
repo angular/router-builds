b77987367b000b212088908867b5dbeb8a77bb84
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 15:29:36 +0000

    119c6dd docs(docs-infra): simplify tab selection

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-f7f9707
+ * @license Angular v21.1.0-next.0+sha-119c6dd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-f7f9707');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-119c6dd');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-f7f9707
+ * @license Angular v21.1.0-next.0+sha-119c6dd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
