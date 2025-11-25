cc544d3b0e8e2d683879bf4d9989736a59a5182d
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 20:10:52 +0000

    6b1fb02 docs: release notes for the v21.0.1 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-40790ef
+ * @license Angular v21.1.0-next.0+sha-6b1fb02
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-40790ef');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-6b1fb02');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-40790ef
+ * @license Angular v21.1.0-next.0+sha-6b1fb02
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
