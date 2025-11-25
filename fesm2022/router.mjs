10c8f5b8c2c27927406a5a1f0e55be61405d9c9e
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 18:53:41 +0000

    40790ef fix(http): prevent XSRF token leakage to protocol-relative URLs

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-400fc82
+ * @license Angular v21.1.0-next.0+sha-40790ef
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-400fc82');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-40790ef');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-400fc82
+ * @license Angular v21.1.0-next.0+sha-40790ef
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
