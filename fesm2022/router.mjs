22443ba71382637be86517703e9c406739a7b383
kirjs <kirts@google.com>
2025-11-25 15:26:10 +0000

    477df38 docs: improve core package API documentation with additional reference links

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-8acf5d2
+ * @license Angular v21.0.0+sha-477df38
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-8acf5d2');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-477df38');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-8acf5d2
+ * @license Angular v21.0.0+sha-477df38
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
