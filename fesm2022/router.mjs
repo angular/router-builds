0f8371f667fc71a2a2a8586d7dc76c5187393e98
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 23:34:38 +0000

    ef034c1 docs(docs-infra): renders inline code snippets in docs pills

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-4c0ba06
+ * @license Angular v21.1.0-next.0+sha-ef034c1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-4c0ba06');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-ef034c1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
