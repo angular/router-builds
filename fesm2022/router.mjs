4205a173b11a14d6efbad1404e7610aebcbc74ca
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 20:10:02 +0000

    47a7396 release: cut the v21.0.1 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-3240d85
+ * @license Angular v21.0.1+sha-47a7396
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-3240d85');
+const VERSION = /* @__PURE__ */new Version('21.0.1+sha-47a7396');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
