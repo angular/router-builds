6fe6ff631a33c2a46b75f63f5c70c57577cf80ca
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 18:59:34 +0000

    0276479 fix(http): prevent XSRF token leakage to protocol-relative URLs

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.13+sha-edcbe22
+ * @license Angular v20.3.13+sha-0276479
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.13+sha-edcbe22');
+const VERSION = /* @__PURE__ */ new Version('20.3.13+sha-0276479');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.13+sha-edcbe22
+ * @license Angular v20.3.13+sha-0276479
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
