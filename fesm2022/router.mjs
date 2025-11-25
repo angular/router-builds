ca64863a2344f085d3c2906f10bbf879c6d8cb3b
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 18:10:29 +0000

    400fc82 refactor: replace `getDocument()` with `inject(DOCUMENT)`

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-3b1fa82
+ * @license Angular v21.1.0-next.0+sha-400fc82
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-3b1fa82');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-400fc82');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
