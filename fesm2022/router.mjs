9a3c8886aa57c980571d9be196c5db677c1108d8
kirjs <kirts@google.com>
2025-11-26 21:07:18 +0000

    fec1f1a docs: update code snippets in reference doocumentation

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.1+sha-99b6341
+ * @license Angular v21.0.1+sha-fec1f1a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.1+sha-99b6341');
+const VERSION = /* @__PURE__ */new Version('21.0.1+sha-fec1f1a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.1+sha-99b6341
+ * @license Angular v21.0.1+sha-fec1f1a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
