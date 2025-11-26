031146e0cfbae5145e07229989ce69686eb279e3
GitHub <noreply@github.com>
2025-11-26 17:02:05 +0000

    add8c41 test(core): test bundling of dynamic component creation and bindings

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-04101e6
+ * @license Angular v21.1.0-next.0+sha-add8c41
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-04101e6');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-add8c41');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-04101e6
+ * @license Angular v21.1.0-next.0+sha-add8c41
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
