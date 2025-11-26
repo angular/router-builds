be3724986a873b9ed8cd5ea34cc7306a3dcbb8cb
kirjs <kirts@google.com>
2025-11-26 17:02:41 +0000

    0ae14cb test(core): test bundling of dynamic component creation and bindings

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.1+sha-10201b6
+ * @license Angular v21.0.1+sha-0ae14cb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.1+sha-10201b6');
+const VERSION = /* @__PURE__ */new Version('21.0.1+sha-0ae14cb');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.1+sha-10201b6
+ * @license Angular v21.0.1+sha-0ae14cb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
