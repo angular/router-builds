079b4ad5eee716779d565ca5554049a304116e54
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-26 20:55:12 +0000

    42e73ff build: update cross-repo angular dependencies

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-1c38681
+ * @license Angular v21.1.0-next.0+sha-42e73ff
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-1c38681');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-42e73ff');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-1c38681
+ * @license Angular v21.1.0-next.0+sha-42e73ff
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
