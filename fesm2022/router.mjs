0303dc8895ab6a72b6a5c7e4e5d7fcdcec9b12db
Alan Agius <alan.agius4@gmail.com>
2025-11-26 08:13:20 +0000

    04101e6 docs: release notes for the v19.2.16 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-ef034c1
+ * @license Angular v21.1.0-next.0+sha-04101e6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-ef034c1');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-04101e6');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-ef034c1
+ * @license Angular v21.1.0-next.0+sha-04101e6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
