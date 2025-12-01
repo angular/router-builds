928a9b9ae440b99e5888af79edbb0260d7e16a8a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-01 11:46:18 +0000

    ee53e6f docs: release notes for the v19.2.17 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-1c6b070
+ * @license Angular v21.1.0-next.0+sha-ee53e6f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-1c6b070');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-ee53e6f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-1c6b070
+ * @license Angular v21.1.0-next.0+sha-ee53e6f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
