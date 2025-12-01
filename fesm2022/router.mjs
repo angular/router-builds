d4111166de7cdac86440120e0d8c8f1735212162
GitHub <noreply@github.com>
2025-12-01 09:31:37 +0000

    1c6b070 fix(compiler): prevent XSS via SVG animation `attributeName` and MathML/SVG URLs

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-8f3fdc3
+ * @license Angular v21.1.0-next.0+sha-1c6b070
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-8f3fdc3');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-1c6b070');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-8f3fdc3
+ * @license Angular v21.1.0-next.0+sha-1c6b070
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
