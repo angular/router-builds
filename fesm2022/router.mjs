66cc0c2d30eb7c52f8c63d04d92ec1e688d55003
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 13:12:23 +0000

    d608784 build: update all github actions

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-dfd7d3b
+ * @license Angular v21.1.0-next.0+sha-d608784
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-dfd7d3b');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-d608784');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-dfd7d3b
+ * @license Angular v21.1.0-next.0+sha-d608784
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
