3e12e0306affdfca9f335115ca4d7d5afbb1d026
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-03 19:30:30 +0000

    eebcaf4 ci: use defined ng-dev config for deploying docs site

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-7f8ffa3
+ * @license Angular v21.1.0-next.1+sha-eebcaf4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-7f8ffa3');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-eebcaf4');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-7f8ffa3
+ * @license Angular v21.1.0-next.1+sha-eebcaf4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
