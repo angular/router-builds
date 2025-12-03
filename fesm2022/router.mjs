9772b3f5c70b5491c68fdcebcb50fbd25376b577
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 12:43:16 +0000

    dfd7d3b build: lock file maintenance

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-3e89577
+ * @license Angular v21.1.0-next.0+sha-dfd7d3b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-3e89577');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-dfd7d3b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-3e89577
+ * @license Angular v21.1.0-next.0+sha-dfd7d3b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
