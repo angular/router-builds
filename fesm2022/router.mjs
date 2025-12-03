bdb70635b6ec1d3bcdd48763ea7f547bbfc86f41
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-03 20:56:46 +0000

    b96f65a fix(forms): memoize reads of child fields in signal forms (#65802)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-fc23fcd
+ * @license Angular v21.1.0-next.1+sha-b96f65a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-fc23fcd');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-b96f65a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
