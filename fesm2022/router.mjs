b6680716121d72085a1b4a23ea515678cf3bf176
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 10:58:51 +0000

    819b640 docs: prevent showing typescript diagnostic errors until the node runtime reaches a ready state

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-13353bf
+ * @license Angular v21.1.0-next.0+sha-819b640
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-13353bf');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-819b640');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-13353bf
+ * @license Angular v21.1.0-next.0+sha-819b640
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
