3526cd59c7db401d2119a6ca21bfdf0db7a92ae0
GitHub <noreply@github.com>
2025-12-04 19:31:48 +0000

    c3bd22d build: update cross-repo angular dependencies

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-bf1c12c
+ * @license Angular v21.0.3+sha-c3bd22d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-bf1c12c');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-c3bd22d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-bf1c12c
+ * @license Angular v21.0.3+sha-c3bd22d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
