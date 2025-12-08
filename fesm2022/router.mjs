b33beebe439e31b54639823a36cab6eff770ff53
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-08 17:20:01 +0000

    f253f12 build: update bazel dependencies

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-5b911d6
+ * @license Angular v21.0.3+sha-f253f12
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-5b911d6');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-f253f12');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-5b911d6
+ * @license Angular v21.0.3+sha-f253f12
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
