9bd538fa253d2f8d5f16cf349eaf5ad7171f372d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-08 17:30:10 +0000

    7ab88c1 docs: add attribute binding reference in accessibility guide

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-f253f12
+ * @license Angular v21.0.3+sha-7ab88c1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-f253f12');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-7ab88c1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-f253f12
+ * @license Angular v21.0.3+sha-7ab88c1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
