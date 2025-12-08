63c5588cdbc01a3c4a7e128e21579df7a2bbe7c9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-08 17:41:53 +0000

    b2ea55c build: update cross-repo angular dependencies

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-7ab88c1
+ * @license Angular v21.0.3+sha-b2ea55c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-7ab88c1');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-b2ea55c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-7ab88c1
+ * @license Angular v21.0.3+sha-b2ea55c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
