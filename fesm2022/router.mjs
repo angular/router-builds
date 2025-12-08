9cffa8039d2ae37a17f23de397cf2682f40917ef
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-08 16:57:15 +0000

    28017c1 docs: add browser-mode option description to Vitest migration guide

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-789f91b
+ * @license Angular v21.1.0-next.1+sha-28017c1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-789f91b');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-28017c1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-789f91b
+ * @license Angular v21.1.0-next.1+sha-28017c1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
