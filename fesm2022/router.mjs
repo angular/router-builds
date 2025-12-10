7dbfbe8424793124995d8c6a2970590770b9a014
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 16:29:03 +0000

    d355205 refactor(core): match spec updates for FakeNavigation

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-9ace7d7
+ * @license Angular v21.1.0-next.1+sha-d355205
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-9ace7d7');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-d355205');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
