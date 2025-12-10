818878bd74eb9eac96922d95d780c2919b174d0f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 20:46:56 +0000

    67a5a65 docs: fix typos and clarify root effect

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-497c0e3
+ * @license Angular v21.0.3+sha-67a5a65
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.3+sha-497c0e3');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-67a5a65');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
