633c450250e64c6bcc0f638c20fe790d87f8b8a9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 19:26:57 +0000

    4f6014a fix(core): avoid false-positive deprecation when using `InjectionToken` with factory only

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.2+sha-1636451
+ * @license Angular v21.1.0-next.2+sha-4f6014a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.2+sha-1636451');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.2+sha-4f6014a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.2+sha-1636451
+ * @license Angular v21.1.0-next.2+sha-4f6014a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
