16d02a8147f81d71d3cfdb98ae69395465344d9b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 23:54:11 +0000

    7c23448 release: cut the v21.1.0-next.3 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.2+sha-bf86f53
+ * @license Angular v21.1.0-next.3+sha-7c23448
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.2+sha-bf86f53');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-7c23448');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.2+sha-bf86f53
+ * @license Angular v21.1.0-next.3+sha-7c23448
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
