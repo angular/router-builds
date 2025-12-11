d7d1c99b52f2c6518b5f503fa60a2e0593788778
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 00:55:37 +0000

    d343ff3 docs: release notes for the v21.0.4 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-a52c834
+ * @license Angular v21.1.0-next.1+sha-d343ff3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-a52c834');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-d343ff3');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-a52c834
+ * @license Angular v21.1.0-next.1+sha-d343ff3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
