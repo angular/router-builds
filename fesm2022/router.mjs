a4792ed04d3bf27b97c0766f4f9dc4c5fb77e841
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-12 16:17:48 +0000

    348f149 feat(forms): pass field directive to class config

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-23547f0
+ * @license Angular v21.1.0-next.3+sha-348f149
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-23547f0');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-348f149');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
