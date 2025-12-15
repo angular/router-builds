9226eb8374f67697fbf012878b82e9ca1781fbca
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-15 17:50:10 +0000

    b5205c7 docs(docs-infra): throw when rendering absolute links to adev in our guides

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-8243bb3
+ * @license Angular v21.1.0-next.3+sha-b5205c7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-8243bb3');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-b5205c7');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
