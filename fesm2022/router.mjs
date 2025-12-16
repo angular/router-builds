d122d6758bcf94609d8e715e403c2e832ce4242a
Andrew Kushnir <akushnir@google.com>
2025-12-16 17:47:18 +0000

    0a5fd6b refactor(core): conditionally include debug names based on `ngDevMode`

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-c250bd0
+ * @license Angular v21.0.5+sha-0a5fd6b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-c250bd0');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-0a5fd6b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-c250bd0
+ * @license Angular v21.0.5+sha-0a5fd6b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
