1f6845e6adcb4cd00fcba2e8f88abb0d29b1c765
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 21:24:01 +0000

    664fa39 release: cut the v21.1.0-next.4 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-868cb7c
+ * @license Angular v21.1.0-next.4+sha-664fa39
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-868cb7c');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-664fa39');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
