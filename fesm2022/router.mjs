18a9668750333a512ecb01b2f2c9741a302c6d5f
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 22:44:05 +0000

    e2a9938 fix(core): explicitly cast signal node value to String

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-ad88c72
+ * @license Angular v21.1.0-next.4+sha-e2a9938
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-ad88c72');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-e2a9938');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-ad88c72
+ * @license Angular v21.1.0-next.4+sha-e2a9938
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
