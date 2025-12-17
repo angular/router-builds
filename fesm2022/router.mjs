06bdf211c4bf0370c13d14f039108183b5519570
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 18:06:17 +0000

    a27bdd3 build: update bazel dependencies

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-3b3b982
+ * @license Angular v21.0.5+sha-a27bdd3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-3b3b982');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-a27bdd3');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
