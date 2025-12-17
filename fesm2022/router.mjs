86dbd0790ae798ac20b9873f86e284f58e848e86
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 01:42:26 +0000

    9fa77af Revert "fix(core): explicitly cast signal node value to String"

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-c501b25
+ * @license Angular v21.1.0-next.3+sha-9fa77af
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-c501b25');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-9fa77af');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
