aef621110d01296f3ee8a72c0dd1febd4aaa8acc
Andrew Kushnir <akushnir@google.com>
2025-12-17 17:58:26 +0000

    3b3b982 docs: add documentation for schema validation with Signal Forms

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-14b529e
+ * @license Angular v21.0.5+sha-3b3b982
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-14b529e');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-3b3b982');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-14b529e
+ * @license Angular v21.0.5+sha-3b3b982
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
