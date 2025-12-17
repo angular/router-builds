30311327a7f26b58eb4f46197cecea371d13badc
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 18:19:46 +0000

    868cb7c build: migrate Bazel ignore rules from .bazelignore to REPO.bazel

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-2e58b66
+ * @license Angular v21.1.0-next.3+sha-868cb7c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-2e58b66');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-868cb7c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
