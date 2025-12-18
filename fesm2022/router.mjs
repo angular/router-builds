82bf4a01ee6689af4dcbfdc6d2ff96cd33796557
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-18 01:02:24 +0000

    069974a build: update dependency @angular/aria to v21.0.4

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-59b3790
+ * @license Angular v21.0.6+sha-069974a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-59b3790');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-069974a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-59b3790
+ * @license Angular v21.0.6+sha-069974a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
