2b845997465029230fc88b07cea252e32c76d2dc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-02 13:29:35 +0000

    71a4373 build: update cross-repo angular dependencies

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-f8b6a3f
+ * @license Angular v21.0.6+sha-71a4373
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-f8b6a3f');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-71a4373');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-f8b6a3f
+ * @license Angular v21.0.6+sha-71a4373
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
