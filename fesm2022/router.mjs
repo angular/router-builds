04c99396cc6843cf8439c1cd36b58029461c0867
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-05 21:03:40 +0000

    46a276a docs: update MCP devserver tool names to use dot notation

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-a6b8cb6
+ * @license Angular v21.1.0-next.4+sha-46a276a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-a6b8cb6');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-46a276a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-a6b8cb6
+ * @license Angular v21.1.0-next.4+sha-46a276a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
