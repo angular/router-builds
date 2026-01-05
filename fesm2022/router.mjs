29e2506c738918633994abc16b4fff414f8efcfc
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-05 21:58:41 +0000

    1532be9 refactor(core): introduce tree-shakeable runtime error codes for `NgModule` handling and `ViewContainerRef` errors

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-46a276a
+ * @license Angular v21.1.0-next.4+sha-1532be9
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-46a276a');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-1532be9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
