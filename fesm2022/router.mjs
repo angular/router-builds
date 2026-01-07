d890e1ee12e17049834d78b55d94915b62122bbc
GitHub <noreply@github.com>
2026-01-07 22:39:38 +0000

    5c75d29 refactor(core): Improve tree-shakable `RuntimeError` error for `INVALID_FIELD_DIRECTIVE_HOST`

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-af4e521
+ * @license Angular v21.1.0-next.4+sha-5c75d29
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-af4e521');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-5c75d29');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-af4e521
+ * @license Angular v21.1.0-next.4+sha-5c75d29
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
