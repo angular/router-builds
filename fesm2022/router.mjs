a2af3ad1ca6e550d8e7c563a176db80385555613
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-08 17:12:50 +0000

    71149ef docs(docs-infra): Fix toggle button in dark mode Fix visibility toggle button in dark mode

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-adabdcd
+ * @license Angular v21.1.0-next.4+sha-71149ef
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-adabdcd');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-71149ef');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
