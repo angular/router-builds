ee9bd69ea6bf9a1b53fdef310ecc6dab74dbf546
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-08 19:34:26 +0000

    86dc128 fix(core): handle cancelled traversals in fake navigation

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-ad49d48
+ * @license Angular v21.1.0-next.4+sha-86dc128
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-ad49d48');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-86dc128');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
