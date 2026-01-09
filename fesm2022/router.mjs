ca8d052c342d1c7efc4542b076b5047e0e12a31b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-09 18:04:45 +0000

    4fb24ca refactor(compiler): switch `Binary.isAssignmentOperation` to type guard function

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-94b7079
+ * @license Angular v21.2.0-next.0+sha-4fb24ca
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-94b7079');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-4fb24ca');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-94b7079
+ * @license Angular v21.2.0-next.0+sha-4fb24ca
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
