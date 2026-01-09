7ec6d778e4b91d588125697854dbc18ee94a2757
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 18:47:21 +0000

    5bc072e ci: add pnpm and typescript exclusions to a Renovate package rule.

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-018edd5
+ * @license Angular v21.0.8+sha-5bc072e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-018edd5');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-5bc072e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-018edd5
+ * @license Angular v21.0.8+sha-5bc072e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
