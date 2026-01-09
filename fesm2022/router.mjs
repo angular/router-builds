0a47e06bbb953eaf5cc7516b2be59be96c1c0589
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 17:31:42 +0000

    24b0920 docs: replace UnambiguousRoleValidator in asyc-valdotors part form-validation.md

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-1c055aa
+ * @license Angular v21.1.0-rc.0+sha-24b0920
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-1c055aa');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-24b0920');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-1c055aa
+ * @license Angular v21.1.0-rc.0+sha-24b0920
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
