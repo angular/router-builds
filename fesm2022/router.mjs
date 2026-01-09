7eb40d3cd829a724f656ba2bffb6dcb61eb66608
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-09 16:03:48 +0000

    3a65814 docs(docs-infra): improve label fallback logic in search results

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-3ccd5a4
+ * @license Angular v21.2.0-next.0+sha-3a65814
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-3ccd5a4');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-3a65814');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-3ccd5a4
+ * @license Angular v21.2.0-next.0+sha-3a65814
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
