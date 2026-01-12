1cdc84ecd58113d2361bf4ec993cadba6dadbbab
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 18:21:18 +0000

    1f47e88 build(docs-infra): Update dependency @xterm/xterm to v6

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-d1f6bcd
+ * @license Angular v21.1.0-rc.0+sha-1f47e88
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-d1f6bcd');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-1f47e88');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-d1f6bcd
+ * @license Angular v21.1.0-rc.0+sha-1f47e88
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
