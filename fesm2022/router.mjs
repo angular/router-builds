e63a61d2afa37b1aabcf3fceb72469d5eebf199d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-12 17:47:10 +0000

    a056413 docs(docs-infra): Show API decorators docs

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-ad699ce
+ * @license Angular v21.2.0-next.0+sha-a056413
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-ad699ce');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-a056413');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-ad699ce
+ * @license Angular v21.2.0-next.0+sha-a056413
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
