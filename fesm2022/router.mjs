67005730c1b07ca4dc49bdd898d3d973b6e4ebf1
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 17:47:29 +0000

    13a7ee8 docs(docs-infra): Show API decorators docs

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-addccea
+ * @license Angular v21.1.0-rc.0+sha-13a7ee8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-addccea');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-13a7ee8');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-addccea
+ * @license Angular v21.1.0-rc.0+sha-13a7ee8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
