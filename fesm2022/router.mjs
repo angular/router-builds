645d2c65f16f4c43e772a51f8261eaa69295def1
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-13 19:15:57 +0000

    b95753e fix(vscode-extension): add syntax highlighting for arrow functions

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-871fa1b
+ * @license Angular v21.2.0-next.0+sha-b95753e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-871fa1b');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-b95753e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-871fa1b
+ * @license Angular v21.2.0-next.0+sha-b95753e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
