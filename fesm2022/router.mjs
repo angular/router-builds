de8bbd82878c3a5f6813eaa2fafb0d504da34890
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 00:11:56 +0000

    6e8f142 build: improve changelog generation and tag fetching in release script

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-d5caac6
+ * @license Angular v21.0.8+sha-6e8f142
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-d5caac6');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-6e8f142');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-d5caac6
+ * @license Angular v21.0.8+sha-6e8f142
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
