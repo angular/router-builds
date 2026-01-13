c3a525016d83edf5ec15be1e876de20e3bc23414
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 16:38:00 +0000

    33de839 docs: update hierarchical-dependency-injection.md

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-6e8f142
+ * @license Angular v21.0.8+sha-33de839
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-6e8f142');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-33de839');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-6e8f142
+ * @license Angular v21.0.8+sha-33de839
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
