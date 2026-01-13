8f9a1d8027fcd26398135ff4ae880542fcf6dbb0
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-13 00:11:19 +0000

    69da639 build: improve changelog generation and tag fetching in release script

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-7875577
+ * @license Angular v21.2.0-next.0+sha-69da639
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-7875577');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-69da639');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-7875577
+ * @license Angular v21.2.0-next.0+sha-69da639
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
