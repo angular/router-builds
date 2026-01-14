0db601a2a45c5b133c9017a35fb600bc68a38c89
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-14 16:16:37 +0000

    654ed65 docs: add IMPORTANT prefix to pipe usage warning and

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-5594a53
+ * @license Angular v21.1.0-rc.0+sha-654ed65
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-5594a53');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-654ed65');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-5594a53
+ * @license Angular v21.1.0-rc.0+sha-654ed65
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
