218b9eaedc0e77d2b0900a3f1dbcc81915b5ea5b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-14 17:26:00 +0000

    79273c1 Revert "docs(docs-infra): use Signals Forms focusBoundControl"

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-d710ebe
+ * @license Angular v21.0.8+sha-79273c1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-d710ebe');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-79273c1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-d710ebe
+ * @license Angular v21.0.8+sha-79273c1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
