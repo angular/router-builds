78f91ed19b59cf40ef078e2b2e2f0acf544d6913
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-14 17:32:16 +0000

    b108ece ci: update algolia synonym updater to use node directly rather than tsx

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-3cc549c
+ * @license Angular v21.1.0-rc.0+sha-b108ece
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-3cc549c');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-b108ece');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-3cc549c
+ * @license Angular v21.1.0-rc.0+sha-b108ece
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
