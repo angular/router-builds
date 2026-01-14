24bdb538585b1c3fa4beac176229ef6e1c483b8f
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-14 16:16:47 +0000

    c9f584b docs: add IMPORTANT prefix to pipe usage warning and

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-074666b
+ * @license Angular v21.2.0-next.0+sha-c9f584b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-074666b');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-c9f584b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
