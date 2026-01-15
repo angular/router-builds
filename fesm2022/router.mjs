e1efd1ac6e3d7ecdf65cf85c1439048c60612c49
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-15 21:48:34 +0000

    d9c980a build: initial test of TypeScript 6

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-417c385
+ * @license Angular v21.2.0-next.0+sha-d9c980a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-417c385');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-d9c980a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
