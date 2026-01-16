2da2f9b6588fb1a4fdbe70f47b04dce40910ca25
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-16 21:33:39 +0000

    5d46554 docs: Add section on `animate.leave` for clarity

--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-e55260f-with-local-changes
+ * @license Angular v21.2.0-next.0+sha-5d46554-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
