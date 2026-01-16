2b3768c0a58966d11630f76487b42b893f0a19b0
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-16 18:32:52 +0000

    ac50dbc build: update cross-repo angular dependencies

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-32ad1e0
+ * @license Angular v21.1.0+sha-ac50dbc-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-32ad1e0');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-ac50dbc-with-local-changes');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
