1039b67931fd9cb1ddf1ca450c27080341b1e540
Alon Mishne <amishne@users.noreply.github.com>
2026-01-20 17:42:14 +0000

    82593de fix(docs-infra): support providing links to multiple symbols within a single line of a code block

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-6b34e5f-with-local-changes
+ * @license Angular v21.1.0+sha-82593de
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-6b34e5f-with-local-changes');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-82593de');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-6b34e5f-with-local-changes
+ * @license Angular v21.1.0+sha-82593de
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
