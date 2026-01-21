e1a9a6172e94f1e4ff60009808122d4e4a27ceee
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-21 00:20:09 +0000

    94a8f79 fix(docs-infra): don't use private symbol

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-ed78fa0
+ * @license Angular v21.2.0-next.0+sha-94a8f79
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-ed78fa0');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-94a8f79');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-ed78fa0
+ * @license Angular v21.2.0-next.0+sha-94a8f79
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
