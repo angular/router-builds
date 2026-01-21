f58e9f56ef8072354cad62ad5115622f4c8c6b48
Alon Mishne <amishne@users.noreply.github.com>
2026-01-21 01:22:19 +0000

    6a56204 fix(docs-infra): use focus-visible for focus styling

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-62d8473
+ * @license Angular v21.1.0+sha-6a56204
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-62d8473');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-6a56204');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-62d8473
+ * @license Angular v21.1.0+sha-6a56204
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
