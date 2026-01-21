69bb29f75d40e08fcbedf14bf9cf01c14ffb9760
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-21 00:36:37 +0000

    6284126 refactor(common): cleanup unused code in doc regions

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-94a8f79
+ * @license Angular v21.2.0-next.0+sha-6284126
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-94a8f79');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-6284126');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-94a8f79
+ * @license Angular v21.2.0-next.0+sha-6284126
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
