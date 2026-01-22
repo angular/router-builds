44d4821efb7a4bc1087a2bd03b3c2e0ca86730c9
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-22 00:48:51 +0000

    bfc2afd release: cut the v21.2.0-next.0 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-e35eea6
+ * @license Angular v21.2.0-next.0+sha-bfc2afd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-e35eea6');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-bfc2afd');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
