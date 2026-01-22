104763a518b7f6b01e0ffb0c1bc91e20a921b4bb
Andrew Scott <atscott01@gmail.com>
2026-01-22 19:51:37 +0000

    61dbdee build: update cross-repo angular dependencies

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-27eeff9
+ * @license Angular v21.1.1+sha-61dbdee
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-27eeff9');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-61dbdee');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-27eeff9
+ * @license Angular v21.1.1+sha-61dbdee
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
