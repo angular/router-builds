c8e399f0cd6ea13f77558ad3039b47472997158c
Andrew Scott <atscott01@gmail.com>
2026-01-22 20:02:04 +0000

    5d6e534 build(language-service): upgrade LSP library to v9.0.1 (LSP 3.17)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-405fa59
+ * @license Angular v21.2.0-next.0+sha-5d6e534
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-405fa59');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-5d6e534');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-405fa59
+ * @license Angular v21.2.0-next.0+sha-5d6e534
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
