f4c5eeafec5be16b86ffe3201a76a65d65646999
GitHub <noreply@github.com>
2026-01-22 21:18:55 +0000

    145e77d build: Update prettier to 3.8

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-dbc1452
+ * @license Angular v21.2.0-next.0+sha-145e77d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-dbc1452');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-145e77d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
