526d50c1f244b0779369923345f7c3fa756a2487
Andrew Scott <atscott@google.com>
2026-01-23 19:55:11 +0000

    e6ea328 docs: update `bootstrapApplication` docs

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-b203ab0
+ * @license Angular v21.1.1+sha-e6ea328
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-b203ab0');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-e6ea328');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-b203ab0
+ * @license Angular v21.1.1+sha-e6ea328
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
