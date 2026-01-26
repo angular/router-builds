692ce7817b569110bc07cd4c3fbe3b5ae2b45869
Andrew Scott <atscott01@gmail.com>
2026-01-26 21:04:41 +0000

    21ecdc0 fix(router): Do not intercept reload events with Navigation integration

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-2499238
+ * @license Angular v21.1.1+sha-21ecdc0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-2499238');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-21ecdc0');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
