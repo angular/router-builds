12e73aead66dd94d10065a9a9d6450605472f96d
Andrew Scott <atscott01@gmail.com>
2026-01-26 23:25:20 +0000

    06952b4 docs(docs-infra): remove unused examples

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-c133a2c
+ * @license Angular v21.1.1+sha-06952b4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.1+sha-c133a2c');
+const VERSION = /* @__PURE__ */new Version('21.1.1+sha-06952b4');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-c133a2c
+ * @license Angular v21.1.1+sha-06952b4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
