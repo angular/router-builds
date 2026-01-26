fb438a751570e732bf69637ba4c367e1c6983e42
GitHub <noreply@github.com>
2026-01-26 17:11:28 +0000

    d306737 docs: add resource composition with snapshots section

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-8bbe6dc
+ * @license Angular v21.2.0-next.0+sha-d306737
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-8bbe6dc');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-d306737');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
