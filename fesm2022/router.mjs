6a35085d1030b54d0bbd967ce566f3f25ed5481c
Andrew Scott <atscott01@gmail.com>
2026-01-28 21:01:04 +0000

    d072791 refactor(core): remove unused restriction parameter

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-5a6fefb
+ * @license Angular v21.2.0-next.1+sha-d072791
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-5a6fefb');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-d072791');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-5a6fefb
+ * @license Angular v21.2.0-next.1+sha-d072791
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
