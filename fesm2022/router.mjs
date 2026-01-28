c2bd35ed6ab86b15e47c1517da2a15ae4d09dd10
Andrew Scott <atscott01@gmail.com>
2026-01-28 20:44:27 +0000

    571747d docs: release notes for the v21.1.2 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-7bb241f
+ * @license Angular v21.2.0-next.0+sha-571747d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-7bb241f');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-571747d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
