05d35917532008233463f24d98dad0bde715acf4
Andrew Scott <atscott01@gmail.com>
2026-01-28 19:30:35 +0000

    7bb241f refactor(language-service): introduce withFallback helper in ts_plugin

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-e682e53
+ * @license Angular v21.2.0-next.0+sha-7bb241f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-e682e53');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-7bb241f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-e682e53
+ * @license Angular v21.2.0-next.0+sha-7bb241f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
