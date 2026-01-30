0bcb773497f553085abe02043d3a8b65965b2e70
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-30 17:29:25 +0000

    6f75b6e fix(forms): Resolves debounce promise on abort in debounceForDuration

--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.2+sha-efb64bf-with-local-changes
+ * @license Angular v21.1.2+sha-6f75b6e-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
