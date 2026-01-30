678c36a1aca4c0127b028db03d3f670ae9827c5f
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-30 17:27:53 +0000

    b1bf535 fix(forms): Resolves debounce promise on abort in debounceForDuration

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-26d1215
+ * @license Angular v21.2.0-next.1+sha-b1bf535
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-26d1215');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-b1bf535');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-26d1215
+ * @license Angular v21.2.0-next.1+sha-b1bf535
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
