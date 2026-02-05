3dc42ebdb103a8ebdecc120f4fa4b92d263438d8
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-05 00:20:17 +0000

    c3229b3 release: cut the v21.2.0-next.2 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-8fe34e1
+ * @license Angular v21.2.0-next.2+sha-c3229b3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-8fe34e1');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-c3229b3');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-8fe34e1
+ * @license Angular v21.2.0-next.2+sha-c3229b3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
