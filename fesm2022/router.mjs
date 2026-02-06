bbf516ce5482a97b1ff3a0be0da010db09d0f877
Andrew Kushnir <akushnir@google.com>
2026-02-06 01:03:25 +0000

    0729181 test(compiler): remove zone-based testing utilities

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.3+sha-70668be
+ * @license Angular v21.1.3+sha-0729181
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.3+sha-70668be');
+const VERSION = /* @__PURE__ */new Version('21.1.3+sha-0729181');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.3+sha-70668be
+ * @license Angular v21.1.3+sha-0729181
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
