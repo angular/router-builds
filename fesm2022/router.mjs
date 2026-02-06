e75712266ff9ce5090447be43a21511cf5614ea9
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 19:19:47 +0000

    c633697 Revert "fix(compiler-cli): update diagnostic to flag no-op arrow functions in listeners"

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.3+sha-be4ffb6
+ * @license Angular v21.1.3+sha-c633697
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.3+sha-be4ffb6');
+const VERSION = /* @__PURE__ */new Version('21.1.3+sha-c633697');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
