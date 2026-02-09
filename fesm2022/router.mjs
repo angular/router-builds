3f8faa21c7663854c8c520188c5eac281a333ae1
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-09 16:03:37 +0000

    4b3b149 refactor(benchpress): remove zone-based testing utilities

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.2+sha-63d828a
+ * @license Angular v21.2.0-next.2+sha-4b3b149
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-63d828a');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-4b3b149');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
