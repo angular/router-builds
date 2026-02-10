fcfcfd0e2a98497c428ed898d9d3c906848c3feb
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-10 15:53:39 +0000

    0f47fda test(router): move timeout and autoTick helpers to shared testing utilities

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.2+sha-760910e
+ * @license Angular v21.2.0-next.2+sha-0f47fda
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-760910e');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-0f47fda');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.2+sha-760910e
+ * @license Angular v21.2.0-next.2+sha-0f47fda
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
