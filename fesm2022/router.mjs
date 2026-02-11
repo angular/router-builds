6abe65e6e00c70693e285fcbf7d0ad4b87d053be
Matthew Beck (Berry) <me@mattrb.com>
2026-02-11 23:44:39 +0000

    937e98f build: update pnpm to v10.29.2

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-4ef0f1f
+ * @license Angular v21.1.4+sha-937e98f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-4ef0f1f');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-937e98f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
