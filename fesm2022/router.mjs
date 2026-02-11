2276965b76d909436e5b390f9d1d985066926129
Matthew Beck (Berry) <me@mattrb.com>
2026-02-11 23:29:24 +0000

    6245333 build: add workspace rule for antigravity

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-d75046b
+ * @license Angular v21.2.0-next.3+sha-6245333
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-d75046b');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-6245333');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-d75046b
+ * @license Angular v21.2.0-next.3+sha-6245333
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
