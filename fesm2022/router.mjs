a679416b5aca49255b42691f06ef4a35df6883dd
Matthew Beck (Berry) <me@mattrb.com>
2026-02-12 17:45:54 +0000

    65a8df1 build: update bazel dependencies

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-937e98f
+ * @license Angular v21.1.4+sha-65a8df1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-937e98f');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-65a8df1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-937e98f
+ * @license Angular v21.1.4+sha-65a8df1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
