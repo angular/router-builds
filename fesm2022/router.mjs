5bb3acd1197b079b2b0b2f07525324d2766ba5cf
Matt Beck <mattrb@google.com>
2026-02-13 20:19:49 +0000

    a6a0347 refactor(forms): extract common parser logic (#66917)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-2d99878
+ * @license Angular v21.2.0-next.3+sha-a6a0347
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-2d99878');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-a6a0347');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-2d99878
+ * @license Angular v21.2.0-next.3+sha-a6a0347
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
