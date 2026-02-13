ea49e1614b1540b06b3ea6ab499cbd800d594634
Matthew Beck <me@mattrb.com>
2026-02-13 17:51:26 +0000

    23c0858 refactor(compiler-cli): use phaseModifier for type-only import detection

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-50c5353
+ * @license Angular v21.1.4+sha-23c0858
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-50c5353');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-23c0858');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
