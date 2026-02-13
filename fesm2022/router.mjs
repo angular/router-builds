3f2f533747138b86574b2eb6af5cee0536815667
Matthew Beck <me@mattrb.com>
2026-02-13 01:00:06 +0000

    55b501a refactor(common): improve image directive typings

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-5cf357b
+ * @license Angular v21.1.4+sha-55b501a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-5cf357b');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-55b501a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
