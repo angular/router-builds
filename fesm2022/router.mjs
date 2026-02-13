02357f7594e2885684bb3cfc08fca1336cb080c4
Matthew Beck (Berry) <me@mattrb.com>
2026-02-13 17:50:03 +0000

    e10a634 refactor(compiler-cli): use phaseModifier for type-only import detection

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-dfd5e3a
+ * @license Angular v21.2.0-next.3+sha-e10a634
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-dfd5e3a');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-e10a634');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
