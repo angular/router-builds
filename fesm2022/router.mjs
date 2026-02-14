e79e0bc1276621782343621490c2f89a83c16f4f
GitHub <noreply@github.com>
2026-02-14 00:55:47 +0000

    815e1a0 refactor(compiler-cli): Add skeleton tests around source->source compiler transform mode

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-a37b6c5
+ * @license Angular v21.2.0-next.3+sha-815e1a0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-a37b6c5');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-815e1a0');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-a37b6c5
+ * @license Angular v21.2.0-next.3+sha-815e1a0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
