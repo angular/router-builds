2fe5431a3dc8889d1d3c98571c8ca8dbc405f171
Matthew Beck (Berry) <me@mattrb.com>
2026-02-17 18:33:09 +0000

    95b3f37 feat(compiler): Exhaustive checks for switch blocks

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-a8aab64
+ * @license Angular v21.2.0-next.3+sha-95b3f37
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-a8aab64');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-95b3f37');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-a8aab64
+ * @license Angular v21.2.0-next.3+sha-95b3f37
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
