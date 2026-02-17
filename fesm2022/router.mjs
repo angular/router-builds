e81c6d8413878e83240d27b0a74c0c0ef7d6e764
Matt Beck <mattrb@google.com>
2026-02-17 16:30:48 +0000

    24d00e9 docs: typo in documentation

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-a3588a0
+ * @license Angular v21.1.4+sha-24d00e9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-a3588a0');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-24d00e9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-a3588a0
+ * @license Angular v21.1.4+sha-24d00e9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
