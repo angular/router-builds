ab97513bc0b7f1e1f235b9e5f0085e9bec67446a
Matt Beck <mattrb@google.com>
2026-02-17 19:39:38 +0000

    9c1ec6c fix(zone.js): support passthrough of Promise.try API

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-fc873cd
+ * @license Angular v21.1.4+sha-9c1ec6c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-fc873cd');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-9c1ec6c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-fc873cd
+ * @license Angular v21.1.4+sha-9c1ec6c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
