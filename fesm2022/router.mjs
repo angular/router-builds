0313a10fe8a260db9350c0b91661277104013bd5
GitHub <noreply@github.com>
2026-02-18 19:45:06 +0000

    9a9f4e7 docs: update best practice performances nav structure to be more comprehensive

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-b8811b4
+ * @license Angular v21.2.0-next.3+sha-9a9f4e7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-b8811b4');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-9a9f4e7');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-b8811b4
+ * @license Angular v21.2.0-next.3+sha-9a9f4e7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
