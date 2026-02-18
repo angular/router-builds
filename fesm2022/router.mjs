34cf21d06cbd19bf959c17c4b1f9ffd591d5a06c
GitHub <noreply@github.com>
2026-02-18 23:55:57 +0000

    42c3962 docs: release notes for the vscode extension 21.2.0 release (#67135)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.3.0-next.0+sha-2eb6b0d
+ * @license Angular v21.3.0-next.0+sha-42c3962
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-2eb6b0d');
+const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-42c3962');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.3.0-next.0+sha-2eb6b0d
+ * @license Angular v21.3.0-next.0+sha-42c3962
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
