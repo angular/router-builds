423483df2f6db946bf2bb5aeb6f4bf27d9a69e3f
Matthew Beck (Berry) <me@mattrb.com>
2026-02-18 22:30:03 +0000

    f9d6d98 release: cut the v21.1.5 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-b90a0fc
+ * @license Angular v21.1.5+sha-f9d6d98
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-b90a0fc');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-f9d6d98');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-b90a0fc
+ * @license Angular v21.1.5+sha-f9d6d98
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
