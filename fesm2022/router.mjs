f8f9dbb622cbd92dfc9cebcfe2fa8bce5730dbdd
GitHub <noreply@github.com>
2026-02-18 19:11:12 +0000

    b8811b4 docs: add signal forms async operations guide

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-47486f8
+ * @license Angular v21.2.0-next.3+sha-b8811b4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-47486f8');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-b8811b4');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-47486f8
+ * @license Angular v21.2.0-next.3+sha-b8811b4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
