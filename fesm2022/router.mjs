6f0de0d2087dab254c0a4301eb5609cded58736d
Matthew Beck <me@mattrb.com>
2026-02-18 19:46:16 +0000

    b90a0fc docs: update best practice performances nav structure to be more comprehensive

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-a4eb347
+ * @license Angular v21.1.4+sha-b90a0fc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-a4eb347');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-b90a0fc');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-a4eb347
+ * @license Angular v21.1.4+sha-b90a0fc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
