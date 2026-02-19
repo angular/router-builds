3df1c668e34f42ffc0a62225a8a1bfbab08f1ad5
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-19 23:56:13 +0000

    cccaed0 docs(docs-infra): preserve content before docs-card-container in adev

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-9943cb7
+ * @license Angular v21.1.5+sha-cccaed0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-9943cb7');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-cccaed0');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-9943cb7
+ * @license Angular v21.1.5+sha-cccaed0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
