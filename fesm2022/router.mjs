16fc5cdfa690e5791038ae4755d1658068ca0dd9
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-19 19:30:01 +0000

    77fc7b9 docs: update changelog to note no changes in 21.1.5 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-210ddf9
+ * @license Angular v21.1.5+sha-77fc7b9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-210ddf9');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-77fc7b9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-210ddf9
+ * @license Angular v21.1.5+sha-77fc7b9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
