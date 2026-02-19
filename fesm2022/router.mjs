b381d369adc830a4ae1212c2e5d8fa7bc5dd2326
GitHub <noreply@github.com>
2026-02-19 17:06:46 +0000

    a3731a1 refactor(devtools): rename directive-explorer component names and folder structure

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.3.0-next.0+sha-1771d54
+ * @license Angular v21.3.0-next.0+sha-a3731a1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-1771d54');
+const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-a3731a1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.3.0-next.0+sha-1771d54
+ * @license Angular v21.3.0-next.0+sha-a3731a1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
