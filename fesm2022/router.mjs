67be83d89578d7abcefb71857caaed50f97fe02d
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 18:09:37 +0000

    866da08 docs: add new debugging and troubleshooting di guide

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-c143de2
+ * @license Angular v21.1.5+sha-866da08
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-c143de2');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-866da08');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-c143de2
+ * @license Angular v21.1.5+sha-866da08
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
