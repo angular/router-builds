d165cfe759d9a240664afae7a905e8ffab4bca83
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 21:35:58 +0000

    35f0663 refactor(core): guards stringify calls with ngDevMode

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-8737289
+ * @license Angular v21.2.0-rc.0+sha-35f0663
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-8737289');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-35f0663');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
