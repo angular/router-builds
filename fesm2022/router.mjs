72be91fc713b020c76cdc4abf17851778d45338f
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-20 00:05:37 +0000

    f68078b docs: add section on reactive context and async operations

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.3.0-next.0+sha-bc473cf
+ * @license Angular v21.3.0-next.0+sha-f68078b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-bc473cf');
+const VERSION = /* @__PURE__ */new Version('21.3.0-next.0+sha-f68078b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
