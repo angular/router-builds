02eac5be5e3d3e4846d40a8e1203f4e6bc4b1b8e
GitHub <noreply@github.com>
2026-02-20 21:35:53 +0000

    c89d94b refactor(core): guards stringify calls with ngDevMode

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-d7ddebc
+ * @license Angular v22.0.0-next.0+sha-c89d94b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-d7ddebc');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-c89d94b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
