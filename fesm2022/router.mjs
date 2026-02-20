3cdc4e3ab9afc215cb86e82fd7ca226bf7931151
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-20 19:07:56 +0000

    d7ddebc ci: update ng-dev config and pullapprove

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-fe25c57
+ * @license Angular v22.0.0-next.0+sha-d7ddebc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-fe25c57');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-d7ddebc');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
