e790ea5bb3420771e82c938cad4b066a79123599
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 18:34:32 +0000

    631f60d fix(forms): preserve parse errors when parse returns value

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-390efd5
+ * @license Angular v21.2.0-rc.0+sha-631f60d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-390efd5');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-631f60d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-390efd5
+ * @license Angular v21.2.0-rc.0+sha-631f60d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
