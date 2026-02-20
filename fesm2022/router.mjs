56a20dc0c28147d2d602d01a5a80ae799e8ad535
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 17:50:18 +0000

    c2cedd1 refactor(compiler-cli): improve diagnostic with help link

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-3c4deaa
+ * @license Angular v21.2.0-rc.0+sha-c2cedd1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-3c4deaa');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-c2cedd1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
