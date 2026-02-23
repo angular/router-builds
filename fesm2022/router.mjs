c94f911d04fa03741248059d3f28db5e55173cac
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-23 23:22:17 +0000

    30e486a fix(docs-infra): use shared heading ID generation logic

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-a73d543
+ * @license Angular v21.2.0-rc.0+sha-30e486a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-a73d543');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-30e486a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-a73d543
+ * @license Angular v21.2.0-rc.0+sha-30e486a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
