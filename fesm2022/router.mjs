417cf9262bf643f013db1f1b49bbeffca7c6d4b4
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 22:30:22 +0000

    24b39ab docs: update README to clarify app setup confirmation

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-0101922
+ * @license Angular v21.1.5+sha-24b39ab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-0101922');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-24b39ab');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-0101922
+ * @license Angular v21.1.5+sha-24b39ab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
