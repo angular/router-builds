cb2020f703674c8d0094f7b4e7ba746c194925de
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 19:30:21 +0000

    787cd87 refactor(vscode-extension): Add support for exhaustive type check in the syntax

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-1a151f2
+ * @license Angular v21.2.0-rc.0+sha-787cd87
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-1a151f2');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-787cd87');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
