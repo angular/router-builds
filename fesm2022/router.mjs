678d1cc3e18f829425e1c71d3c58ca4e4201ee5f
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-25 23:44:59 +0000

    a57ebc9 docs: release notes for the vscode extension 21.2.1 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-de0cc81
+ * @license Angular v22.0.0-next.0+sha-a57ebc9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-de0cc81');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-a57ebc9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
