dc75ddd70567ce08786a72d9a6efac4a0e1fd1c3
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-26 22:39:24 +0000

    4e9648d build: update cross-repo angular dependencies

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-54a0541
+ * @license Angular v21.2.0+sha-4e9648d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-54a0541');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-4e9648d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-54a0541
+ * @license Angular v21.2.0+sha-4e9648d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
