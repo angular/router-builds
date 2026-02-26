b5e3f84b451232016254686ebedbf18a337ba526
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-26 20:28:42 +0000

    385b948 build: update to latest sha of dev-infra repo

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-6585e4f
+ * @license Angular v22.0.0-next.0+sha-385b948
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-6585e4f');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-385b948');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
