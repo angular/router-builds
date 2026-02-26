809563a7489142606723c8c812a5ea80f4c836b6
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-26 18:04:13 +0000

    3d3e6cb build: update repository locations for bazel rules

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-2158bf8
+ * @license Angular v21.2.0+sha-3d3e6cb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-2158bf8');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-3d3e6cb');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-2158bf8
+ * @license Angular v21.2.0+sha-3d3e6cb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
