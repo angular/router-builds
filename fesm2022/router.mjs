3c54e5760c427f9dfae8fa2ba5cbb98189795ce4
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-02 19:49:15 +0000

    9758ea9 refactor(core): ignore infinite animations in animate api

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-a2d5048
+ * @license Angular v22.0.0-next.0+sha-9758ea9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-a2d5048');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-9758ea9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-a2d5048
+ * @license Angular v22.0.0-next.0+sha-9758ea9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
