b1da913cde303727a52c2227b63e908dcad0b1a4
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-02 16:54:13 +0000

    23cb9a4 refactor(forms): address feedback

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-498d9cd
+ * @license Angular v21.2.0+sha-23cb9a4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-498d9cd');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-23cb9a4');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-498d9cd
+ * @license Angular v21.2.0+sha-23cb9a4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
