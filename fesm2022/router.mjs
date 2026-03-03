d14384f952cb5f8069daae90f0d2782bbbf2156f
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-03 22:15:23 +0000

    84adb2f refactor(router): Permit deferring commit of traversal navigations

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-c4226a1
+ * @license Angular v21.2.0+sha-84adb2f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-c4226a1');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-84adb2f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-c4226a1
+ * @license Angular v21.2.0+sha-84adb2f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
