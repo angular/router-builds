f200917cfd244c838e06e91aaff69bb280111559
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-04 17:27:17 +0000

    5338b59 Revert "refactor(http): Improves base64 encoding/decoding with feature detection (#67002)"

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-9e64147
+ * @license Angular v22.0.0-next.0+sha-5338b59
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-9e64147');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-5338b59');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-9e64147
+ * @license Angular v22.0.0-next.0+sha-5338b59
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
