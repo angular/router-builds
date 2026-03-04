90fdec26896015e9b26d71f0136ccd504bc6ccf6
GitHub <noreply@github.com>
2026-03-04 20:46:14 +0000

    05476ea release: bump VSCode extension version to 21.2.2 (#67449)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.1+sha-f38169d
+ * @license Angular v21.2.1+sha-05476ea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.1+sha-f38169d');
+const VERSION = /* @__PURE__ */new Version('21.2.1+sha-05476ea');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.1+sha-f38169d
+ * @license Angular v21.2.1+sha-05476ea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
