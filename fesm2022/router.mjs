e503535b9c3464584e390863c39a3dd79451b2c0
Andrew Scott <atscott01@gmail.com>
2026-03-06 23:41:50 +0000

    276c12a build: update dependency node to v22.22.1

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.1+sha-104ac11
+ * @license Angular v21.2.1+sha-276c12a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.1+sha-104ac11');
+const VERSION = /* @__PURE__ */new Version('21.2.1+sha-276c12a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.1+sha-104ac11
+ * @license Angular v21.2.1+sha-276c12a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
