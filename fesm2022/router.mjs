fbcd6584824b12bd6680389e25ecf83862e2dffd
Andrew Scott <atscott01@gmail.com>
2026-03-09 18:30:20 +0000

    0588133 refactor(core): remove private export of `getDebugNode`

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.1+sha-bd72571
+ * @license Angular v21.2.2+sha-0588133
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.1+sha-bd72571');
+const VERSION = /* @__PURE__ */new Version('21.2.2+sha-0588133');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.1+sha-bd72571
+ * @license Angular v21.2.2+sha-0588133
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
