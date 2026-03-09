cb76637511511c7f98df38ca34c544c598815fec
Andrew Scott <atscott01@gmail.com>
2026-03-09 23:34:56 +0000

    bf93ff8 ci: fix exclusion of manual_api_docs

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.1+sha-b918bed
+ * @license Angular v22.0.0-next.1+sha-bf93ff8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-b918bed');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-bf93ff8');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.1+sha-b918bed
+ * @license Angular v22.0.0-next.1+sha-bf93ff8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
