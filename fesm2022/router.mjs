0a4ef1efcd9df68cbd7d6be176a4f97ea0d12ed6
Andrew Scott <atscott@google.com>
2026-03-09 17:29:05 +0000

    79ac570 refactor(devtools): fix browser-specific styles infra

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.1+sha-276c12a
+ * @license Angular v21.2.1+sha-79ac570
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.1+sha-276c12a');
+const VERSION = /* @__PURE__ */new Version('21.2.1+sha-79ac570');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.1+sha-276c12a
+ * @license Angular v21.2.1+sha-79ac570
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
