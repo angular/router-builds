b41446cc9bb494a55cd9b3d3856bd179ea0c5633
Andrew Scott <atscott01@gmail.com>
2026-03-11 18:37:08 +0000

    b401c18 fix(core): include signal debug names in their `toString()` representation

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.1+sha-0837d25
+ * @license Angular v22.0.0-next.1+sha-b401c18
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-0837d25');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-b401c18');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.1+sha-0837d25
+ * @license Angular v22.0.0-next.1+sha-b401c18
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
