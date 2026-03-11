fdae1a90321aaa9bc2b52691f47ef928f7beff29
Andrew Scott <atscott01@gmail.com>
2026-03-11 21:26:09 +0000

    ec76985 build: update bazel dependencies

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.2+sha-88235a0
+ * @license Angular v22.0.0-next.2+sha-ec76985
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.2+sha-88235a0');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.2+sha-ec76985');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.2+sha-88235a0
+ * @license Angular v22.0.0-next.2+sha-ec76985
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
