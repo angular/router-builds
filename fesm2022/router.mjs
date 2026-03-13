56483f52b6f9ff0a9166a37cb0275e6612aed6b7
Matthew Beck (Berry) <me@mattrb.com>
2026-03-13 22:34:07 +0000

    ada150c build: update cross-repo angular dependencies

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.3+sha-e127927
+ * @license Angular v22.0.0-next.3+sha-ada150c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-e127927');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-ada150c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
