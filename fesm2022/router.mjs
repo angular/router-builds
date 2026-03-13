306a3d236d86e7edc2f0f1a671f485ba44d771ca
Matthew Beck (Berry) <me@mattrb.com>
2026-03-13 22:32:00 +0000

    f39896d build: update cross-repo angular dependencies

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-21cab20
+ * @license Angular v21.2.4+sha-f39896d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-21cab20');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-f39896d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-21cab20
+ * @license Angular v21.2.4+sha-f39896d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
