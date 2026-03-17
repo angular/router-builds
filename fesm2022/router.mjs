52fa653531cc2c7e8f00faa89dd53da8302334c3
Matthew Beck <me@mattrb.com>
2026-03-17 19:33:28 +0000

    67e0ba7 fix(compiler-cli): generic types not filled out correctly in type check block

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-ca328ea
+ * @license Angular v21.2.4+sha-67e0ba7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-ca328ea');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-67e0ba7');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-ca328ea
+ * @license Angular v21.2.4+sha-67e0ba7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
