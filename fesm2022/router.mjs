9f6213a657b53a94f8a5bd7eda2928fc03424bf5
Matthew Beck <me@mattrb.com>
2026-03-18 00:09:02 +0000

    076d41c fix(migrations): prevent trailing comma syntax errors after removing NgStyle

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-a8f80c1
+ * @license Angular v21.2.4+sha-076d41c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-a8f80c1');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-076d41c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-a8f80c1
+ * @license Angular v21.2.4+sha-076d41c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
