eb40e3216afb8266751a0f6a34576cfbe0ae2ed1
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-23 22:48:09 +0000

    0eeb1b5 fix(forms): allow `FormRoot` to be used without submission options (#67727)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.4+sha-36edf48
+ * @license Angular v22.0.0-next.4+sha-0eeb1b5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-36edf48');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-0eeb1b5');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
