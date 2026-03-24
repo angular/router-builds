aa3d58eb65dd39f1f0ff30bc895d1064f194a564
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-24 22:43:59 +0000

    050b14b docs: fix incorrect getHarnesses() method to getAllHarnesses and broken link

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.4+sha-d9712e6
+ * @license Angular v22.0.0-next.4+sha-050b14b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-d9712e6');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-050b14b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
