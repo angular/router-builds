1add72303732bd45b3b79511211c2bf062406b84
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-24 22:26:50 +0000

    a0ca5a5 docs(docs-infra): redefine /guide/pipes redirect

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.5+sha-e731bbd
+ * @license Angular v21.2.5+sha-a0ca5a5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.5+sha-e731bbd');
+const VERSION = /* @__PURE__ */new Version('21.2.5+sha-a0ca5a5');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.5+sha-e731bbd
+ * @license Angular v21.2.5+sha-a0ca5a5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
