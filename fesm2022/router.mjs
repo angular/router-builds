55404c52ebb27df87b387278b1c6c40719d6676c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-03-25 09:59:13 +0000

    2c7d3b5 build: re-sync file

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.5+sha-6bd2121
+ * @license Angular v21.2.5+sha-2c7d3b5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.5+sha-6bd2121');
+const VERSION = /* @__PURE__ */new Version('21.2.5+sha-2c7d3b5');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.5+sha-6bd2121
+ * @license Angular v21.2.5+sha-2c7d3b5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
