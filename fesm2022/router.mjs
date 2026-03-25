e81405111e4598294f1ce69f31e04c3c3624c4cd
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-25 21:27:15 +0000

    24e52d4 feat(forms): add debounce option to validateAsync and validateHttp

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.4+sha-b5af15a
+ * @license Angular v22.0.0-next.4+sha-24e52d4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-b5af15a');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-24e52d4');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
