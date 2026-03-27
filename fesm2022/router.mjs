ffd02ab53188a0d6506f5e531d532c9d5fe49502
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-27 16:25:41 +0000

    9b48cb2 docs(docs-infra): introduce a custom UrlSerializer

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-bba5ed8
+ * @license Angular v21.2.6+sha-9b48cb2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-bba5ed8');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-9b48cb2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
