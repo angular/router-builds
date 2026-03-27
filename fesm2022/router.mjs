86569ff9d8f0d01365889b7afd1db34666d5a0e4
Andrew Scott <atscott@google.com>
2026-03-27 18:49:24 +0000

    0c9c270 docs: add agent skills documentation and new readme

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-9b48cb2
+ * @license Angular v21.2.6+sha-0c9c270
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-9b48cb2');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-0c9c270');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
