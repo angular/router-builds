fc803b8a1eb34f035e93dcd51d48f623dd9e5f05
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-30 09:19:14 +0000

    efbba94 docs: align list formatting and improve emphasis

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-94b1b53
+ * @license Angular v21.2.6+sha-efbba94
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-94b1b53');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-efbba94');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
