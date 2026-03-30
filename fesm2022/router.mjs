bb4123ae6d2b3b381aa0a7f07d6fa9ec9b81967b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-30 09:22:12 +0000

    60930f8 build: update all non-major dependencies

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.5+sha-713eca7
+ * @license Angular v22.0.0-next.5+sha-60930f8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-713eca7');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-60930f8');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
