a110fd833feea8f01a9d298f7eb16a155aa0c072
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-31 11:57:59 +0000

    ef7679b refactor(forms): use strict equality for pending status getter

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.5+sha-c950299
+ * @license Angular v22.0.0-next.5+sha-ef7679b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-c950299');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.5+sha-ef7679b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.5+sha-c950299
+ * @license Angular v22.0.0-next.5+sha-ef7679b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
