53917f4ca44a58b44aff7d23758d6439fef33887
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-04-01 09:55:03 +0000

    0960592 fix(router): pass outlet context to split to fix empty path named outlets

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-f916531
+ * @license Angular v21.2.6+sha-0960592
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-f916531');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-0960592');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-f916531
+ * @license Angular v21.2.6+sha-0960592
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
