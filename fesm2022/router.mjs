508f65d7dbf03142023e1ce15d15e8382920a193
Andrew Scott <atscott01@gmail.com>
2026-04-03 16:51:46 +0000

    9c55fcb feat(core): de-duplicate host directives

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.6+sha-9d76ac8
+ * @license Angular v22.0.0-next.6+sha-9c55fcb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-9d76ac8');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-9c55fcb');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.6+sha-9d76ac8
+ * @license Angular v22.0.0-next.6+sha-9c55fcb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
