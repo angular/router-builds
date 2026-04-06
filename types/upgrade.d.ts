9b084d4b7d5b9bde82da9785a858f74cca14bde3
Andrew Scott <atscott01@gmail.com>
2026-04-06 18:56:26 +0000

    c91bd07 ci: use immutable sha for checkout

--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.7+sha-2ec9ff0-with-local-changes
+ * @license Angular v21.2.7+sha-c91bd07-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
