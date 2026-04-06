8f179964589bba09b60ca590fb3f6ab2e1cc119b
Andrew Scott <atscott01@gmail.com>
2026-04-06 18:42:09 +0000

    33b001d fix(devtools): clean up removed directive entries in IdentityTracker to prevent memory leak

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.6+sha-2cb67c0
+ * @license Angular v22.0.0-next.6+sha-33b001d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-2cb67c0');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-33b001d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.6+sha-2cb67c0
+ * @license Angular v22.0.0-next.6+sha-33b001d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
