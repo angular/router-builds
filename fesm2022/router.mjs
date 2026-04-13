7183274cd499348c1219aef60aeb134b3a470b81
kirjs <kirts@google.com>
2026-04-13 18:29:00 +0000

    17ffa19 docs(devtools): create router tree documentation

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.8+sha-aa5d237
+ * @license Angular v21.2.8+sha-17ffa19
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-aa5d237');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-17ffa19');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.8+sha-aa5d237
+ * @license Angular v21.2.8+sha-17ffa19
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
