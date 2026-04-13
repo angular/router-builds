23b607554cbe85e14419018b91f6e96926dc6e7a
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 11:03:35 +0000

    70368ea refactor(http): Make `BrowserXhr`/`XhrFactory` tree-shakable

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-f25c7ce
+ * @license Angular v22.0.0-next.7+sha-70368ea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-f25c7ce');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-70368ea');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
