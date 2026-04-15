03c983e590539daf18662775a776c16fb8386e00
kirjs <kirts@google.com>
2026-04-15 16:54:47 +0000

    32a8302 docs: add new signal forms - form submission guide

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.8+sha-4f65bb3
+ * @license Angular v21.2.8+sha-32a8302
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-4f65bb3');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-32a8302');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.8+sha-4f65bb3
+ * @license Angular v21.2.8+sha-32a8302
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
