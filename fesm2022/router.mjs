022a8346e8596e86d5a7a119163b9095583169d1
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-22 18:14:42 +0000

    750af5b build: update cross-repo angular dependencies to v21.2.8

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-5533ab4
+ * @license Angular v21.2.9+sha-750af5b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-5533ab4');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-750af5b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
