40f59ce3e8d548d5a7c35821d74cf7e3fc97a8e8
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-23 21:24:13 +0000

    f3308dc build: update cross-repo angular dependencies

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.10+sha-f9b70a0
+ * @license Angular v21.2.10+sha-f3308dc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.10+sha-f9b70a0');
+const VERSION = /* @__PURE__ */new Version('21.2.10+sha-f3308dc');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.10+sha-f9b70a0
+ * @license Angular v21.2.10+sha-f3308dc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
