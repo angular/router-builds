135a0267a1f84733eb13f6ca7cb204a2622bbcfd
Alon Mishne <amishne@users.noreply.github.com>
2026-04-29 21:08:32 +0000

    6c6db5b docs(docs-infra): open update guide external links in new tabs

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.10+sha-631e303
+ * @license Angular v21.2.10+sha-6c6db5b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.10+sha-631e303');
+const VERSION = /* @__PURE__ */new Version('21.2.10+sha-6c6db5b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.10+sha-631e303
+ * @license Angular v21.2.10+sha-6c6db5b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
