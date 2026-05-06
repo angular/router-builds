53355d420dc8c9827990b7252d7f20dacfe3d749
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-06 23:51:22 +0000

    e3dc87c release: cut the zone.js-0.16.2 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-853a91e
+ * @license Angular v22.1.0-next.0+sha-e3dc87c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-853a91e');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-e3dc87c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-853a91e
+ * @license Angular v22.1.0-next.0+sha-e3dc87c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
