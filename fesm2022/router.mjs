3ef84edf72c84dd4eb5011d7aec5421d70c4e7e4
Matthew Beck (Berry) <me@mattrb.com>
2026-05-07 23:03:37 +0000

    7b4791f docs: add `FormField/FormRoot` imports + move comment w/backticks

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-e3dc87c
+ * @license Angular v22.1.0-next.0+sha-7b4791f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-e3dc87c');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-7b4791f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-e3dc87c
+ * @license Angular v22.1.0-next.0+sha-7b4791f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
