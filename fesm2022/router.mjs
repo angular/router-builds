ee549e5f8c0d608af2820c78af56a9df0b7d2929
Matthew Beck (Berry) <me@mattrb.com>
2026-05-11 19:17:31 +0000

    ef134ac docs: add new signal forms field metadata guide

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-538d0a8
+ * @license Angular v22.1.0-next.0+sha-ef134ac
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-538d0a8');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-ef134ac');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-538d0a8
+ * @license Angular v22.1.0-next.0+sha-ef134ac
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
