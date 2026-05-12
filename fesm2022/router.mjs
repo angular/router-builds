48f8f87dd1742d01d68ec8136652ceffda3eedad
Matthew Beck (Berry) <me@mattrb.com>
2026-05-12 21:45:22 +0000

    6a07fdd build: update @angular/ng-dev

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-e0593ec
+ * @license Angular v22.1.0-next.0+sha-6a07fdd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-e0593ec');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-6a07fdd');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-e0593ec
+ * @license Angular v22.1.0-next.0+sha-6a07fdd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
