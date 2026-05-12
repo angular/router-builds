b0fcb3baedcfd96aa7e1f6e2c95ca209247aad5e
Matthew Beck <me@mattrb.com>
2026-05-12 21:44:46 +0000

    7dc1017 fix(compiler): simplify handling of colon host with a selector list

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.12+sha-a4ec2b4
+ * @license Angular v22.0.0-next.12+sha-7dc1017
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-a4ec2b4');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-7dc1017');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
