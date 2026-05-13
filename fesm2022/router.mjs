5d3a1dd47c4a30fd63ce6332f3a722c3f77aad89
Matthew Beck <me@mattrb.com>
2026-05-13 19:31:02 +0000

    1f0c1ab refactor(core): patch special provider classes with __NG_ELEMENT_ID__

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.12+sha-ccb7d42
+ * @license Angular v22.0.0-next.12+sha-1f0c1ab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-ccb7d42');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-1f0c1ab');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.12+sha-ccb7d42
+ * @license Angular v22.0.0-next.12+sha-1f0c1ab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
