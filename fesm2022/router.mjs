b9e9483cec908a8e0550ee0010d3a95a82340806
Matthew Beck <me@mattrb.com>
2026-05-13 22:45:42 +0000

    639a785 docs: add version 22 to the browser support reference table

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.12+sha-1f0c1ab
+ * @license Angular v22.0.0-next.12+sha-639a785
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-1f0c1ab');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-639a785');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.12+sha-1f0c1ab
+ * @license Angular v22.0.0-next.12+sha-639a785
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
