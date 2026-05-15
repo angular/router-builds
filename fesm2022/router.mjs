78ec71a6ca285ccb887c2f3c5d3c01b1a2834e8f
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-15 18:55:22 +0000

    defb172 build: update dependency node to v22.22.3

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.0+sha-4dd9839
+ * @license Angular v22.0.0-rc.0+sha-defb172
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-4dd9839');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-defb172');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.0+sha-4dd9839
+ * @license Angular v22.0.0-rc.0+sha-defb172
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
