e7f6857d03b258906b1ebfc8cbb9af2bd8e1486a
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-15 17:50:15 +0000

    358d2e6 docs: move Angular 19 to unsupported versions

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-71f8d48
+ * @license Angular v22.1.0-next.0+sha-358d2e6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-71f8d48');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-358d2e6');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-71f8d48
+ * @license Angular v22.1.0-next.0+sha-358d2e6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
