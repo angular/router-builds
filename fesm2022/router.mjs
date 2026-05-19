ccb1c160702112059b9f53d6bc1b23e3b00fe5f3
leonsenft <leonsenft@users.noreply.github.com>
2026-05-19 20:50:11 +0000

    027c7c0 docs: update HTTP testing setup guidance

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.13+sha-7f444e1
+ * @license Angular v21.2.13+sha-027c7c0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.13+sha-7f444e1');
+const VERSION = /* @__PURE__ */new Version('21.2.13+sha-027c7c0');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.13+sha-7f444e1
+ * @license Angular v21.2.13+sha-027c7c0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
