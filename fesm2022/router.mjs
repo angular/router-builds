fad03c266d472950f22d087fe2d49a778ec1eb5d
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-20 22:42:54 +0000

    3fe8562 release: cut the v21.2.14 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.13+sha-9627e02
+ * @license Angular v21.2.14+sha-3fe8562
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.13+sha-9627e02');
+const VERSION = /* @__PURE__ */new Version('21.2.14+sha-3fe8562');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.13+sha-9627e02
+ * @license Angular v21.2.14+sha-3fe8562
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
