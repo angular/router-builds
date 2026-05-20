4b939deb397b1e64576c36342a7c95fc3001d7f9
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-20 01:41:00 +0000

    5993c47 test(core): remove obsolete SVG script sanitization translation test

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.0+sha-1f71ebd
+ * @license Angular v22.0.0-rc.0+sha-5993c47
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-1f71ebd');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-5993c47');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.0+sha-1f71ebd
+ * @license Angular v22.0.0-rc.0+sha-5993c47
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
