f9923666c3dcae2fcea7c0ae40e104db965263c9
leonsenft <leonsenft@users.noreply.github.com>
2026-05-20 17:42:30 +0000

    83026cf fix(docs-infra): make absolute angular.dev hrefs relative in CLI option descriptions

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.0+sha-3ff5536
+ * @license Angular v22.0.0-rc.0+sha-83026cf
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-3ff5536');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.0+sha-83026cf');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.0+sha-3ff5536
+ * @license Angular v22.0.0-rc.0+sha-83026cf
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
