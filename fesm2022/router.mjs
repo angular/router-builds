76e2e354d59f5e935fef60fbafa1d7fe137f5e56
leonsenft <leonsenft@users.noreply.github.com>
2026-05-20 20:42:57 +0000

    232b21d docs: document content projection limitations

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.13+sha-1d6e71d
+ * @license Angular v21.2.13+sha-232b21d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.13+sha-1d6e71d');
+const VERSION = /* @__PURE__ */new Version('21.2.13+sha-232b21d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.13+sha-1d6e71d
+ * @license Angular v21.2.13+sha-232b21d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
