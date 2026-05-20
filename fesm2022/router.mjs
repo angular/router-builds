06ed3c0eb7c83acb6f1498b704e10bb69c4f8516
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-20 22:44:03 +0000

    16195d0 docs: release notes for the v21.2.14 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-04f31cc
+ * @license Angular v22.1.0-next.0+sha-16195d0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-04f31cc');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-16195d0');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-04f31cc
+ * @license Angular v22.1.0-next.0+sha-16195d0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
