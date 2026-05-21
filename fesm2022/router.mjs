5ee97f8ae1670128073886b5e925a48788d7daf5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-21 18:10:41 +0000

    1563aae perf(forms): avoid redundant invalidations in parser errors signal

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-fd6ef34
+ * @license Angular v22.1.0-next.0+sha-1563aae
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-fd6ef34');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-1563aae');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
