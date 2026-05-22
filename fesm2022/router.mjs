8e44d8a0513a442d426639ede5884ef2b176f8b2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-22 17:07:30 +0000

    c08f22a refactor(vscode-extension): Remove effectively dead code

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.1+sha-3b4ef1e
+ * @license Angular v22.0.0-rc.1+sha-c08f22a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-3b4ef1e');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-c08f22a');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
