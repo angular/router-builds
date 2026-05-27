de4fd4756a27f40fcf1f85db8a1de5d62e6a428d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 18:37:15 +0000

    d9c38e5 docs: fix typos in source code comments

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.1+sha-2200b4a
+ * @license Angular v22.0.0-rc.1+sha-d9c38e5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-2200b4a');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-d9c38e5');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
