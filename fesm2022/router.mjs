016f8f91578563ca76fac17f4ebec1d1fb1f7a29
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 22:09:31 +0000

    af04936 fix(core): sanitize meta selectors

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.21+sha-e617fa0
+ * @license Angular v20.3.21+sha-af04936
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.21+sha-e617fa0');
+const VERSION = /* @__PURE__ */ new Version('20.3.21+sha-af04936');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.21+sha-e617fa0
+ * @license Angular v20.3.21+sha-af04936
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
