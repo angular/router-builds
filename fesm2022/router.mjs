4e29ab6927d7cecc01cf095ce20528833bfbb126
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 18:14:10 +0000

    e617fa0 fix(service-worker): Preserves HTTP cache mode in asset group requests

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.21+sha-3d135ce
+ * @license Angular v20.3.21+sha-e617fa0
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.21+sha-3d135ce');
+const VERSION = /* @__PURE__ */ new Version('20.3.21+sha-e617fa0');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.21+sha-3d135ce
+ * @license Angular v20.3.21+sha-e617fa0
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
