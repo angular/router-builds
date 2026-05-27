e1cd2c8d86ef7ebc02993e1e138a51bd2ff4ef64
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 17:43:24 +0000

    3d135ce fix(common): add upper bounds for digitsInfo

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.21+sha-591fa53
+ * @license Angular v20.3.21+sha-3d135ce
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.21+sha-591fa53');
+const VERSION = /* @__PURE__ */ new Version('20.3.21+sha-3d135ce');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
