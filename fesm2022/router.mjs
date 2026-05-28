53c070fdb82fa2142e5210adc08320252759950e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-28 17:00:49 +0000

    cea6588 release: cut the v21.2.15 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.14+sha-6b8202e
+ * @license Angular v21.2.15+sha-cea6588
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.14+sha-6b8202e');
+const VERSION = /* @__PURE__ */new Version('21.2.15+sha-cea6588');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.14+sha-6b8202e
+ * @license Angular v21.2.15+sha-cea6588
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
