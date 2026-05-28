b4cba90edccd4323b731d9415fa757248de31814
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-28 17:00:13 +0000

    99c1eeb docs: release notes for the v21.2.15 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-e2ccea9
+ * @license Angular v22.1.0-next.0+sha-99c1eeb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-e2ccea9');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-99c1eeb');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
