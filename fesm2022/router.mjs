2f6eabfcde6f2eb5af0a400530859e4190fb6e08
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 18:16:23 +0000

    7b91309 fix(platform-server): throw on suspicious URLs and restrict protocol-relative URLs

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.2+sha-ae12a09
+ * @license Angular v22.0.0-rc.2+sha-7b91309
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.2+sha-ae12a09');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.2+sha-7b91309');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.2+sha-ae12a09
+ * @license Angular v22.0.0-rc.2+sha-7b91309
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
