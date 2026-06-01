feeb6918336689fa53af6631433efb75203d2905
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 09:40:51 +0000

    69c0d48 fix(docs-infra): round up media queries

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.15+sha-7e38336
+ * @license Angular v21.2.15+sha-69c0d48
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.15+sha-7e38336');
+const VERSION = /* @__PURE__ */new Version('21.2.15+sha-69c0d48');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
