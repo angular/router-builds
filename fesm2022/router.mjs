7a74084834e746800b5d330f052018100e573815
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-02 11:16:16 +0000

    c56f1ec release: cut the v20.3.24 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.23+sha-6ca433e
+ * @license Angular v20.3.24+sha-c56f1ec
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.23+sha-6ca433e');
+const VERSION = /* @__PURE__ */ new Version('20.3.24+sha-c56f1ec');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.23+sha-6ca433e
+ * @license Angular v20.3.24+sha-c56f1ec
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
