af9e943f9b5da8a70bb6c4212c648c76c6954742
Andrew Scott <atscott01@gmail.com>
2026-06-03 19:38:53 +0000

    d0dd9b6 build: deprecate @angular/animations and @angular/platform-browser-dynamic

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-e843003
+ * @license Angular v22.1.0-next.0+sha-d0dd9b6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-e843003');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-d0dd9b6');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
