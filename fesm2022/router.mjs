22012b52dc988ba8facec0673388487c90571e79
Andrew Scott <atscott01@gmail.com>
2026-06-03 22:54:13 +0000

    592b121 refactor(devtools): Prevent cross-tab privilege escalation

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-8e4fd59
+ * @license Angular v22.0.0+sha-592b121
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-8e4fd59');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-592b121');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-8e4fd59
+ * @license Angular v22.0.0+sha-592b121
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
