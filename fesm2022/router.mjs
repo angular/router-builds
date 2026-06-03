8a910053f53f531d3551f8bca243d7cd888b0084
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 09:29:44 +0000

    3401189 docs: add Signal Forms integration example to Angular Aria Autocomplete guide

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.3+sha-e10647b
+ * @license Angular v22.0.0-rc.3+sha-3401189
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.3+sha-e10647b');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.3+sha-3401189');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.3+sha-e10647b
+ * @license Angular v22.0.0-rc.3+sha-3401189
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
