e8b4983a0927028a6bbb3cf501bd31525aedef3b
Andrew Scott <atscott01@gmail.com>
2026-06-05 17:49:58 +0000

    1a62130 fix(common): use cryptographically secure SHA-256 for transfer cache key generation

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.24+sha-49368c1
+ * @license Angular v20.3.24+sha-1a62130
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.24+sha-49368c1');
+const VERSION = /* @__PURE__ */ new Version('20.3.24+sha-1a62130');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.24+sha-49368c1
+ * @license Angular v20.3.24+sha-1a62130
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
