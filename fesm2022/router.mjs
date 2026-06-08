89c879173be57b09bd8066cdbba4f1ca4fad9862
Andrew Scott <atscott01@gmail.com>
2026-06-08 20:21:57 +0000

    d65a5f4 fix(service-worker): Strips sensitive headers on cross-origin redirects

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.24+sha-1a62130
+ * @license Angular v20.3.24+sha-d65a5f4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.24+sha-1a62130');
+const VERSION = /* @__PURE__ */ new Version('20.3.24+sha-d65a5f4');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.24+sha-1a62130
+ * @license Angular v20.3.24+sha-d65a5f4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
