10a9abd3a4092eb5f5a4ce038dc96638f4c6c087
Andrew Scott <atscott01@gmail.com>
2026-06-08 17:24:42 +0000

    11b206b fix(core): introduce disposal mechanism for Angular views in foreign `@content`

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-255151a
+ * @license Angular v22.1.0-next.0+sha-11b206b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-255151a');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-11b206b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-255151a
+ * @license Angular v22.1.0-next.0+sha-11b206b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
