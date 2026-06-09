3ee48dbe24ec65d0ba4adec2351da2b122b774c3
Andrew Scott <atscott01@gmail.com>
2026-06-09 23:27:38 +0000

    d1539a8 refactor(core): Fix DirectiveDefinition interface to allow abstract classes

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-7541f30
+ * @license Angular v22.1.0-next.0+sha-d1539a8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-7541f30');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-d1539a8');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
