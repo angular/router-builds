a2a4982ed799fc16df9c9a86c48518ca1f3ab4f6
Andrew Scott <atscott01@gmail.com>
2026-06-09 17:51:09 +0000

    3dd35c2 fix(common): escape CSS string-terminating characters in escapeCssUrl

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-c121407
+ * @license Angular v22.1.0-next.0+sha-3dd35c2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-c121407');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-3dd35c2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
