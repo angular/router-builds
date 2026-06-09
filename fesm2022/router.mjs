731221ca3aab5cfeb38b0ec477cc0d7f1667dec3
Andrew Scott <atscott01@gmail.com>
2026-06-09 20:22:51 +0000

    4289c4c fix(core): update comment for Default change detection

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-562a566
+ * @license Angular v22.0.0+sha-4289c4c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-562a566');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-4289c4c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-562a566
+ * @license Angular v22.0.0+sha-4289c4c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
