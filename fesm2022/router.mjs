af9b510c2f999fc98e1446439e3d44d9f1d1bff1
GitHub <noreply@github.com>
2026-06-09 17:14:11 +0000

    13fb0af refactor(platform-server): deprecate ServerXhr (#69255)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.16+sha-86a56dc
+ * @license Angular v21.2.16+sha-13fb0af
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.16+sha-86a56dc');
+const VERSION = /* @__PURE__ */new Version('21.2.16+sha-13fb0af');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.16+sha-86a56dc
+ * @license Angular v21.2.16+sha-13fb0af
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
