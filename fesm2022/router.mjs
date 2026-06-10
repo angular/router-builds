a25a3f23f6ca3123d579a0076557e0856e1fc3ec
Andrew Scott <atscott01@gmail.com>
2026-06-10 20:25:10 +0000

    9f4b3dc docs: release notes for the v22.0.1 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-7e94ab9
+ * @license Angular v22.1.0-next.0+sha-9f4b3dc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-7e94ab9');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-9f4b3dc');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
