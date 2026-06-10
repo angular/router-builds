cdaa22923b86e89c25bc4d3046c9be76f66ebf96
Andrew Scott <atscott01@gmail.com>
2026-06-10 18:19:00 +0000

    f6695b0 fix(vscode-extension): resolve relative workspace tsdk paths to absolute

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-4645850
+ * @license Angular v22.0.0+sha-f6695b0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-4645850');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-f6695b0');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-4645850
+ * @license Angular v22.0.0+sha-f6695b0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
