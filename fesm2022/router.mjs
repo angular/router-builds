e6c5e8c7e4d3823796396afa7d644de7093078cb
Andrew Scott <atscott01@gmail.com>
2026-06-10 00:18:02 +0000

    6e3d51d refactor(migrations): Improve `safeNavigationMigration` heuristic

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-01ea640
+ * @license Angular v22.0.0+sha-6e3d51d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-01ea640');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-6e3d51d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
