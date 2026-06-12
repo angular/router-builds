a0d6f5e6ddfd377f18f4bc0696c48fb1f8eed77a
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-12 16:17:05 +0000

    b7aa8dc fix(docs-infra): add explicit font styles to docs-primary-btn

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-d45e318
+ * @license Angular v22.0.1+sha-b7aa8dc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-d45e318');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-b7aa8dc');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-d45e318
+ * @license Angular v22.0.1+sha-b7aa8dc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
