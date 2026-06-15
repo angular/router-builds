33aabaf51a5d5b0c2211c0696e26dfc7dc3bcec2
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-15 16:37:32 +0000

    6f11719 fix(compiler): restrict possible event handler check to property names longer than 2 characters

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-fe7f9ed
+ * @license Angular v22.0.1+sha-6f11719
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-fe7f9ed');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-6f11719');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-fe7f9ed
+ * @license Angular v22.0.1+sha-6f11719
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
