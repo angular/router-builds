a6ac72e3c2275e571b9785c05bc10367d54aab5d
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-16 15:55:57 +0000

    f2c5045 docs: use the @Service decorator in the learn-angular DI tutorial

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-93d0a5f
+ * @license Angular v22.0.1+sha-f2c5045
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-93d0a5f');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-f2c5045');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-93d0a5f
+ * @license Angular v22.0.1+sha-f2c5045
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
