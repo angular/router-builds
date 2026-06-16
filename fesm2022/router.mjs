90f86630a461ec865f5b989a33ff787b56f11d6a
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-16 15:39:47 +0000

    93d0a5f fix(core): prevent unsubscribe during emit from throwing off other listeners

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-fef8bcf
+ * @license Angular v22.0.1+sha-93d0a5f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-fef8bcf');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-93d0a5f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-fef8bcf
+ * @license Angular v22.0.1+sha-93d0a5f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
