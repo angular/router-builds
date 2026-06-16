6d05e93de09be07b7b787e60b4957d9bfa2c1fea
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-16 16:17:19 +0000

    b32ee7c fix(core): treat iframe credentialless as security-sensitive

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-f2c5045
+ * @license Angular v22.0.1+sha-b32ee7c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-f2c5045');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-b32ee7c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-f2c5045
+ * @license Angular v22.0.1+sha-b32ee7c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
