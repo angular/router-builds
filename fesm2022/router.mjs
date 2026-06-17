e8889baf06dfd28a04fa90d01eff294eb8a8f39a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-17 16:20:21 +0000

    2d2d5fc build: lock file maintenance

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-a5ee50b
+ * @license Angular v22.1.0-next.0+sha-2d2d5fc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-a5ee50b');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-2d2d5fc');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
