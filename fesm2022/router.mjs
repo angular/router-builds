c1940f851c356ddef99151d1174923f0ac5152c1
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-17 17:40:35 +0000

    e3d392f ci: add id-token: write permission to release workflow

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-74f2b91
+ * @license Angular v22.0.1+sha-e3d392f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-74f2b91');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-e3d392f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-74f2b91
+ * @license Angular v22.0.1+sha-e3d392f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
