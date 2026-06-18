91fae4b9bfdfb0bcdea58b12381d59df44c33324
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-18 14:58:28 +0000

    ffcc8ee fix(docs-infra): correct select dropdown scrolling, sizing, and selection

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.1+sha-c23ddd6
+ * @license Angular v22.1.0-next.1+sha-ffcc8ee
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-c23ddd6');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-ffcc8ee');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
