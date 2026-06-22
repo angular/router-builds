7b42822c752ff3db19875f827ea7de8d3e776b77
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-22 20:42:58 +0000

    792edab refactor(migrations): account for inheritance in service migration

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.1+sha-a286a32
+ * @license Angular v22.1.0-next.1+sha-792edab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-a286a32');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-792edab');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.1+sha-a286a32
+ * @license Angular v22.1.0-next.1+sha-792edab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
