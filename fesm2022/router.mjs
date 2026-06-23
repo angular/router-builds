7bc12c8d266ca2a7c10d16e8783b5803053b9aab
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-23 15:36:59 +0000

    dead64f docs: add error guide for NG05101

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.1+sha-e5d7b3a
+ * @license Angular v22.1.0-next.1+sha-dead64f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-e5d7b3a');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-dead64f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.1+sha-e5d7b3a
+ * @license Angular v22.1.0-next.1+sha-dead64f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
