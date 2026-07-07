1a7a9e44686a0457a61196a8948694b91d8bcae3
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-07 17:21:56 +0000

    91df739 fix(http): prevent caching of responses with Set-Cookie headers

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.17+sha-8d22cc9
+ * @license Angular v21.2.17+sha-91df739
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.17+sha-8d22cc9');
+const VERSION = /* @__PURE__ */new Version('21.2.17+sha-91df739');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.17+sha-8d22cc9
+ * @license Angular v21.2.17+sha-91df739
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
