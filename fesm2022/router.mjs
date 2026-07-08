8a6b26a42196d84f57402e7100b0e97e078cb9d4
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 21:57:22 +0000

    248d4e5 release: cut the v22.1.0-next.5 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.4+sha-099c5c6
+ * @license Angular v22.1.0-next.5+sha-248d4e5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-099c5c6');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-248d4e5');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.4+sha-099c5c6
+ * @license Angular v22.1.0-next.5+sha-248d4e5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
