43777192cfb1c0a3a10a9728f302a6756dce445f
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 16:00:11 +0000

    42862f7 docs: update formatting

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.4+sha-95b7385
+ * @license Angular v22.1.0-next.4+sha-42862f7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-95b7385');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.4+sha-42862f7');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
