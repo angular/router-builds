012ef1d3f47add36af9fbae3c91798e33a096067
Matthew Beck <me@mattrb.com>
2026-07-09 23:17:51 +0000

    97c830b ci: fix workflows broken by renamed command

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.6+sha-8ce1fcf
+ * @license Angular v22.0.6+sha-97c830b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.6+sha-8ce1fcf');
+const VERSION = /* @__PURE__ */new Version('22.0.6+sha-97c830b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
