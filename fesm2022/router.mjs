97129bb385e6463bdea80755112aa3df83ec24e5
Matthew Beck <me@mattrb.com>
2026-07-09 17:08:29 +0000

    aa03cb9 test(language-service): empty template literal interpolation

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.6+sha-89e2c77
+ * @license Angular v22.0.6+sha-aa03cb9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.6+sha-89e2c77');
+const VERSION = /* @__PURE__ */new Version('22.0.6+sha-aa03cb9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.6+sha-89e2c77
+ * @license Angular v22.0.6+sha-aa03cb9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
