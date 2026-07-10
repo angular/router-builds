618cd84d1befccf6a392076323890686788b1694
Matthew Beck <me@mattrb.com>
2026-07-10 21:32:22 +0000

    cfb77d6 docs: update cross-repo adev docs

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.6+sha-e6272f2
+ * @license Angular v22.0.6+sha-cfb77d6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -33,7 +33,7 @@ class RouterTestingModule {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.6+sha-e6272f2",
+    version: "22.0.6+sha-cfb77d6",
     ngImport: i0,
     type: RouterTestingModule,
     deps: [],
@@ -41,14 +41,14 @@ class RouterTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "22.0.6+sha-e6272f2",
+    version: "22.0.6+sha-cfb77d6",
     ngImport: i0,
     type: RouterTestingModule,
     exports: [RouterModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "22.0.6+sha-e6272f2",
+    version: "22.0.6+sha-cfb77d6",
     ngImport: i0,
     type: RouterTestingModule,
     providers: [ROUTER_PROVIDERS, provideLocationMocks(), withPreloading(NoPreloading).ɵproviders, {
@@ -61,7 +61,7 @@ class RouterTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.6+sha-e6272f2",
+  version: "22.0.6+sha-cfb77d6",
   ngImport: i0,
   type: RouterTestingModule,
   decorators: [{
@@ -97,7 +97,7 @@ class RootFixtureService {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.6+sha-e6272f2",
+    version: "22.0.6+sha-cfb77d6",
     ngImport: i0,
     type: RootFixtureService,
     deps: [],
@@ -105,14 +105,14 @@ class RootFixtureService {
   });
   static ɵprov = i0.ɵɵngDeclareService({
     minVersion: "22.0.0",
-    version: "22.0.6+sha-e6272f2",
+    version: "22.0.6+sha-cfb77d6",
     ngImport: i0,
     type: RootFixtureService
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.6+sha-e6272f2",
+  version: "22.0.6+sha-cfb77d6",
   ngImport: i0,
   type: RootFixtureService,
   decorators: [{
@@ -126,7 +126,7 @@ class RootCmp {
   }] : []));
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.6+sha-e6272f2",
+    version: "22.0.6+sha-cfb77d6",
     ngImport: i0,
     type: RootCmp,
     deps: [],
@@ -134,7 +134,7 @@ class RootCmp {
   });
   static ɵcmp = i0.ɵɵngDeclareComponent({
     minVersion: "14.0.0",
-    version: "22.0.6+sha-e6272f2",
+    version: "22.0.6+sha-cfb77d6",
     type: RootCmp,
     isStandalone: true,
     selector: "ng-component",
@@ -160,7 +160,7 @@ class RootCmp {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.6+sha-e6272f2",
+  version: "22.0.6+sha-cfb77d6",
   ngImport: i0,
   type: RootCmp,
   decorators: [{
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.6+sha-e6272f2
+ * @license Angular v22.0.6+sha-cfb77d6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
