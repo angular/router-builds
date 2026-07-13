0b5b46cc3ccb41dceab2ae2314aad9de709d1139
Matthew Beck <me@mattrb.com>
2026-07-13 21:53:52 +0000

    d5e0b13 feat(docs-infra): show Angie in the error snack bar

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.5+sha-d55f5c4
+ * @license Angular v22.1.0-next.5+sha-d5e0b13
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-d55f5c4');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-d5e0b13');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.5+sha-d55f5c4
+ * @license Angular v22.1.0-next.5+sha-d5e0b13
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
