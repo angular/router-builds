9a22a91d35609d992d459612e06bd501d96c9113
Matthew Beck <me@mattrb.com>
2026-07-15 19:16:27 +0000

    70500e4 fix(core): preserve explicit input transform write type

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.6+sha-e881b96
+ * @license Angular v22.0.6+sha-70500e4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.6+sha-e881b96');
+const VERSION = /* @__PURE__ */new Version('22.0.6+sha-70500e4');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
