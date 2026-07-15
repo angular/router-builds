c41995c560e8911d0132d03f4b24da1f4bcd8674
Matthew Beck <me@mattrb.com>
2026-07-15 21:57:06 +0000

    829d98e build: update cross-repo angular dependencies to v22.0.7

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.6+sha-70500e4
+ * @license Angular v22.0.6+sha-829d98e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.6+sha-70500e4');
+const VERSION = /* @__PURE__ */new Version('22.0.6+sha-829d98e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.6+sha-70500e4
+ * @license Angular v22.0.6+sha-829d98e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
