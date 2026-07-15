8731ead101f4418bc496f9f6739738c65231cd35
Douglas Parker <dgp1130@users.noreply.github.com>
2026-07-15 20:31:47 +0000

    d7a13b9 release: bump Angular DevTools version to 1.17.0

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.5+sha-0ae6d81
+ * @license Angular v22.1.0-next.5+sha-d7a13b9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-0ae6d81');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-d7a13b9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
