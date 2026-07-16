2adda750fb29e20ab4f3048a4e898cb5f4fd9e39
Matthew Beck <me@mattrb.com>
2026-07-16 04:35:08 +0000

    f8c9e9d release: cut the v22.1.0-next.6 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.5+sha-d43acaf
+ * @license Angular v22.1.0-next.6+sha-f8c9e9d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.5+sha-d43acaf');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-f8c9e9d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.5+sha-d43acaf
+ * @license Angular v22.1.0-next.6+sha-f8c9e9d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
