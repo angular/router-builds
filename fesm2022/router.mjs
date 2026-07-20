597126dca596ef01b1c93533cbc27039503e37af
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 12:30:48 +0000

    00699f3 refactor(devtools): add alt text to the empty-state screen illustrations

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.6+sha-d6cad90
+ * @license Angular v22.1.0-next.6+sha-00699f3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-d6cad90');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-00699f3');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
