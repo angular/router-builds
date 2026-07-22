672e40eb3bb38e2111bea6419dfe1cf002151467
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 14:24:16 +0000

    e428df2 build: update pnpm-lock.yaml

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.6+sha-ab52df4
+ * @license Angular v22.1.0-next.6+sha-e428df2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-ab52df4');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-e428df2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
