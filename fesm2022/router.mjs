2763d6bc3899db6f8e6e0932b6e15790e137fa58
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 11:30:12 +0000

    bc3ff3b build: update cross-repo angular dependencies

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.7+sha-21c45a8
+ * @license Angular v22.0.7+sha-bc3ff3b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.7+sha-21c45a8');
+const VERSION = /* @__PURE__ */new Version('22.0.7+sha-bc3ff3b');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.7+sha-21c45a8
+ * @license Angular v22.0.7+sha-bc3ff3b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
