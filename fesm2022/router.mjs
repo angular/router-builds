56953f47e9fb222a235b7058b6cd63eb30aa2328
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 10:07:12 +0000

    6557df5 docs: update stale Twitter reference to X in the URL matcher guide

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.6+sha-6371f0b
+ * @license Angular v22.1.0-next.6+sha-6557df5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-6371f0b');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-6557df5');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.6+sha-6371f0b
+ * @license Angular v22.1.0-next.6+sha-6557df5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
