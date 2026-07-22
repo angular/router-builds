c8ee410300ac841eff60370bd98d8d54219a9510
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 14:51:40 +0000

    3bf2430 docs: release notes for the v22.1.0-rc.0 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.6+sha-509cad6
+ * @license Angular v22.2.0-next.0+sha-3bf2430
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-509cad6');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-3bf2430');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.6+sha-509cad6
+ * @license Angular v22.2.0-next.0+sha-3bf2430
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
