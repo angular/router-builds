6a7f75385e68dc88caba90e8ad389496afb4ba78
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 17:43:48 +0000

    0650966 docs: modernize directives guides (#69822)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.8+sha-433276b
+ * @license Angular v22.0.8+sha-0650966
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.8+sha-433276b');
+const VERSION = /* @__PURE__ */new Version('22.0.8+sha-0650966');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.8+sha-433276b
+ * @license Angular v22.0.8+sha-0650966
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
