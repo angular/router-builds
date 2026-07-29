02fe926d32f824561c09ab532b75661d84fffa72
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-29 16:01:06 +0000

    7b884f5 fix(compiler): restrict possible event handler check to property names longer than 2 characters

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.18+sha-9949dcc
+ * @license Angular v21.2.18+sha-7b884f5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.18+sha-9949dcc');
+const VERSION = /* @__PURE__ */new Version('21.2.18+sha-7b884f5');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.18+sha-9949dcc
+ * @license Angular v21.2.18+sha-7b884f5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
