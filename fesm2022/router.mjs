844ea459a1435a3ef79e4f50bd1dd36f6e82f6c8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-29 16:56:00 +0000

    f90690f docs: fix self-referential link in defer block description

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.8+sha-45d7de4
+ * @license Angular v22.0.8+sha-f90690f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.8+sha-45d7de4');
+const VERSION = /* @__PURE__ */new Version('22.0.8+sha-f90690f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.8+sha-45d7de4
+ * @license Angular v22.0.8+sha-f90690f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
