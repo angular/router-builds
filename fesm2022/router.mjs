76939760c05b154d8b508b4ff6eb875bd14cbc83
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-29 19:58:03 +0000

    72a8512 release: cut the v20.3.27 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.26+sha-db0d4a1
+ * @license Angular v20.3.27+sha-72a8512
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.26+sha-db0d4a1');
+const VERSION = /* @__PURE__ */ new Version('20.3.27+sha-72a8512');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- upgrade/index.d.ts
+++ upgrade/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.26+sha-db0d4a1
+ * @license Angular v20.3.27+sha-72a8512
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
