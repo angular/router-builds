6e51c93702a9830b856f69378262fba9767322aa
GitHub <noreply@github.com>
2026-07-30 15:36:48 +0000

    6afe6fa fix(core): sanitize host bindings on concrete hosts

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.19+sha-07cb840
+ * @license Angular v21.2.19+sha-6afe6fa
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.19+sha-07cb840');
+const VERSION = /* @__PURE__ */new Version('21.2.19+sha-6afe6fa');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.19+sha-07cb840
+ * @license Angular v21.2.19+sha-6afe6fa
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
