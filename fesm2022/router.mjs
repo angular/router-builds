055fc5e75a756d58bf6b514bf45578a55cbb6da0
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-30 16:04:34 +0000

    eb10e93 refactor(common): modernize pipes & non bindable tests to rely on `whenStable`

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0+sha-644455b
+ * @license Angular v22.1.0+sha-eb10e93
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-644455b');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-eb10e93');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0+sha-644455b
+ * @license Angular v22.1.0+sha-eb10e93
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
