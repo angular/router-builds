59b0079adeee949c875dfba6ffd321df57dfe6fb
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-07-31 22:23:03 +0000

    caf6166 fix(http): run root interceptors in the terminal request chain

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.19+sha-6afe6fa
+ * @license Angular v21.2.19+sha-caf6166
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.19+sha-6afe6fa');
+const VERSION = /* @__PURE__ */new Version('21.2.19+sha-caf6166');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.19+sha-6afe6fa
+ * @license Angular v21.2.19+sha-caf6166
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
