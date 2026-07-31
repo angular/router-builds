29e094820b029aac04c04b4c8d373d164317e157
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-07-31 22:21:15 +0000

    e4c416c fix(http): run root interceptors in the terminal request chain

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.27+sha-2f96c80
+ * @license Angular v20.3.27+sha-e4c416c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.27+sha-2f96c80');
+const VERSION = /* @__PURE__ */ new Version('20.3.27+sha-e4c416c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.27+sha-2f96c80
+ * @license Angular v20.3.27+sha-e4c416c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
