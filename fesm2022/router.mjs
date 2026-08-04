612986797325f9e606bc417534daf891e2a30558
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-04 16:45:41 +0000

    ef4dfea fix(http): avoid aborting completed requests in FetchBackend

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.0+sha-2f0be5b
+ * @license Angular v22.2.0-next.0+sha-ef4dfea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-2f0be5b');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-ef4dfea');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.0+sha-2f0be5b
+ * @license Angular v22.2.0-next.0+sha-ef4dfea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
