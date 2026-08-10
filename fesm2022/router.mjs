7ba14d58dc80d600f2a3224c9e6d3b07a18811a1
Alon Mishne <amishne@users.noreply.github.com>
2026-08-10 22:44:19 +0000

    0df9d5e fix(core): ensure i18n_util hasOwnProperty checks are safe for property renaming

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.1+sha-596356e
+ * @license Angular v22.1.1+sha-0df9d5e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.1+sha-596356e');
+const VERSION = /* @__PURE__ */new Version('22.1.1+sha-0df9d5e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.1+sha-596356e
+ * @license Angular v22.1.1+sha-0df9d5e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
