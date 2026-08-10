ce682cebf945b3501e18ea2a7a9e61f9108b2af9
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-10 16:49:19 +0000

    596356e docs: update cross-repo adev docs

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.1+sha-4d985a1
+ * @license Angular v22.1.1+sha-596356e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.1+sha-4d985a1');
+const VERSION = /* @__PURE__ */new Version('22.1.1+sha-596356e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.1+sha-4d985a1
+ * @license Angular v22.1.1+sha-596356e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
