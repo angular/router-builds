7d67a823e7b1373995cc8bec7d60c656c45b6d18
Alon Mishne <amishne@users.noreply.github.com>
2026-08-11 17:09:12 +0000

    65e2aed fix(docs-infra): contain scroll within adev dropdown menus

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.1+sha-0df9d5e
+ * @license Angular v22.1.1+sha-65e2aed
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.1+sha-0df9d5e');
+const VERSION = /* @__PURE__ */new Version('22.1.1+sha-65e2aed');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.1+sha-0df9d5e
+ * @license Angular v22.1.1+sha-65e2aed
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
