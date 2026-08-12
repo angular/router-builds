04e5ba493bb4be29ec101db5339fe241921280dd
Alon Mishne <amishne@users.noreply.github.com>
2026-08-12 18:19:24 +0000

    299607d docs: clarify animate.leave child animation behavior

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.1+sha-2f82601
+ * @license Angular v22.1.1+sha-299607d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.1+sha-2f82601');
+const VERSION = /* @__PURE__ */new Version('22.1.1+sha-299607d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.1+sha-2f82601
+ * @license Angular v22.1.1+sha-299607d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
