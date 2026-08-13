e75fa8b13c9489c9d08d8aaa8d9e989133a3db33
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-13 19:14:36 +0000

    5ed071d docs: release notes for the v22.1.2 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.2+sha-40d23fa
+ * @license Angular v22.2.0-next.2+sha-5ed071d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-40d23fa');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.2+sha-5ed071d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
