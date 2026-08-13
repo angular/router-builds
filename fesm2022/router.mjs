abba3dacf29f966d69cb3822ed130b86997e91f6
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-13 16:16:58 +0000

    16e42c3 build: update all non-major dependencies

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.1+sha-647bf8e
+ * @license Angular v22.2.0-next.1+sha-16e42c3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-647bf8e');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-16e42c3');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
