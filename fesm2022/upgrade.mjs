d1d06e817e0424166e7a6db6f4d684aabd554990
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-13 15:48:29 +0000

    6f848db build(vscode-extension): dynamically resolve upstream remote and use token for https pushes

--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.1+sha-c9990c4
+ * @license Angular v22.2.0-next.1+sha-6f848db
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,12 +1,12 @@
 /**
- * @license Angular v22.2.0-next.1+sha-c9990c4
+ * @license Angular v22.2.0-next.1+sha-6f848db
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import * as i0 from '@angular/core';
 import { ModuleWithProviders, WritableSignal, DebugElement, Type } from '@angular/core';
-import { Routes, ExtraOptions, RouterModule } from './_router_module-chunk.js';
+import { Routes, ExtraOptions, RouterModule } from './router.js';
 export { ɵEmptyOutletComponent as ɵɵEmptyOutletComponent, RouterLink as ɵɵRouterLink, RouterLinkActive as ɵɵRouterLinkActive, RouterOutlet as ɵɵRouterOutlet } from './_router_module-chunk.js';
 import { ComponentFixture } from '@angular/core/testing';
 import 'rxjs';
