036629b2f16f88b541f55af5dac8f4c236a8c495
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-13 16:05:15 +0000

    647bf8e docs: note SafeResourceUrl audio src change

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.1+sha-6f848db
+ * @license Angular v22.2.0-next.1+sha-647bf8e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-6f848db');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.1+sha-647bf8e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.1+sha-6f848db
+ * @license Angular v22.2.0-next.1+sha-647bf8e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
