e060fbfd53d2554cbe055de426da14d211cca479
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-13 16:05:11 +0000

    0a708e9 docs: note SafeResourceUrl audio src change

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.1+sha-910f391
+ * @license Angular v22.1.1+sha-0a708e9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.1+sha-910f391');
+const VERSION = /* @__PURE__ */new Version('22.1.1+sha-0a708e9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.1+sha-910f391
+ * @license Angular v22.1.1+sha-0a708e9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
