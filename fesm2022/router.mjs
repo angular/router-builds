5dc83ac9353374f1f07c7ed2a366eed6cbd0f4ea
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-14 15:40:16 +0000

    c04931c fix(core): throw a descriptive error instead of crashing when a hydration node is missing

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.2+sha-0cd243d
+ * @license Angular v22.1.2+sha-c04931c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.2+sha-0cd243d');
+const VERSION = /* @__PURE__ */new Version('22.1.2+sha-c04931c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.2+sha-0cd243d
+ * @license Angular v22.1.2+sha-c04931c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
