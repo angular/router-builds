9a656c513b4ee56dbbd0d7ce8aff9d61df40d312
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-17 20:51:55 +0000

    7538744 fix(platform-browser): disallow event handler attributes in Meta

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.28+sha-1d576ee
+ * @license Angular v20.3.28+sha-7538744
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -81,7 +81,7 @@ function mapToResolve(provider) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.28+sha-1d576ee');
+const VERSION = /* @__PURE__ */ new Version('20.3.28+sha-7538744');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- router_module.d.d.ts
+++ router_module.d.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.28+sha-1d576ee
+ * @license Angular v20.3.28+sha-7538744
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
