c2e8179d1bd599fe2a23821e897769c441c5e125
leonsenft <leonsenft@users.noreply.github.com>
2026-08-25 23:37:45 +0000

    b199bdf fix(core): explicitly reject foreign components in JIT mode

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.3+sha-bf69e85
+ * @license Angular v22.1.3+sha-b199bdf
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.3+sha-bf69e85');
+const VERSION = /* @__PURE__ */new Version('22.1.3+sha-b199bdf');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
