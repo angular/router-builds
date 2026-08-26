2e037336f1a089d08f898a77a5ad50bc9abd58c4
leonsenft <leonsenft@users.noreply.github.com>
2026-08-26 20:58:19 +0000

    151a8f2 refactor(core): Decouple ɵɵFactoryDeclaration and ɵɵInjectableDeclaration from type parameter T.

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.3+sha-b199bdf
+ * @license Angular v22.1.3+sha-151a8f2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.3+sha-b199bdf');
+const VERSION = /* @__PURE__ */new Version('22.1.3+sha-151a8f2');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.3+sha-b199bdf
+ * @license Angular v22.1.3+sha-151a8f2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
