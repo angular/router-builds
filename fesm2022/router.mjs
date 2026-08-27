df08bd338ca80c47c1fce4e0d037e239ea036de5
Matthew Beck <me@mattrb.com>
2026-08-27 15:12:37 +0000

    6f51be6 build: pin `vscode-languageserver-textdocument` version

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.4+sha-8983809
+ * @license Angular v22.1.4+sha-6f51be6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.4+sha-8983809');
+const VERSION = /* @__PURE__ */new Version('22.1.4+sha-6f51be6');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.4+sha-8983809
+ * @license Angular v22.1.4+sha-6f51be6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
