d4ab2776934307ebfc80484311fe56c5c29b586d
GitHub <noreply@github.com>
2026-08-27 19:54:07 +0000

    6f1dc26 docs(router): Add router resource docs

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.4+sha-4fc45a9
+ * @license Angular v22.2.0-next.4+sha-6f1dc26
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-4fc45a9');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-6f1dc26');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.4+sha-4fc45a9
+ * @license Angular v22.2.0-next.4+sha-6f1dc26
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
