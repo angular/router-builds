268997d95970c04c44eda956df3c7f5cbbc202c5
Matthew Beck <me@mattrb.com>
2026-08-27 17:29:54 +0000

    bee058e fix(docs-infra): point the tutorial .gitignore at live GitHub docs

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.4+sha-6f51be6
+ * @license Angular v22.1.4+sha-bee058e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.4+sha-6f51be6');
+const VERSION = /* @__PURE__ */new Version('22.1.4+sha-bee058e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.4+sha-6f51be6
+ * @license Angular v22.1.4+sha-bee058e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
