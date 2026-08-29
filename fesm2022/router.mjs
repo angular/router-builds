3f06448a2c74b1a65803ea0b135b3cf64f1d7af1
Matthew Beck <me@mattrb.com>
2026-08-29 01:13:00 +0000

    563850e docs: update cross-repo adev docs

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.4+sha-34817da
+ * @license Angular v22.2.0-next.4+sha-563850e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-34817da');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-563850e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.4+sha-34817da
+ * @license Angular v22.2.0-next.4+sha-563850e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
