012c7c3a218d0178dabac06ce1971af5afd2b992
Matthew Beck <me@mattrb.com>
2026-09-01 21:53:06 +0000

    b2903de ci: move `packages/private` under `fw-general`

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.4+sha-d90698d
+ * @license Angular v22.1.4+sha-b2903de
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.4+sha-d90698d');
+const VERSION = /* @__PURE__ */new Version('22.1.4+sha-b2903de');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.4+sha-d90698d
+ * @license Angular v22.1.4+sha-b2903de
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
