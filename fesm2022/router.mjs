6f0fdb583161e77d018b8dbe63044f740c348f63
Matthew Beck <me@mattrb.com>
2026-09-02 22:23:15 +0000

    d7af4b5 docs: decouple selection from aria toolbar examples and guide

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.4+sha-45148da
+ * @license Angular v22.2.0-next.4+sha-d7af4b5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-45148da');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.4+sha-d7af4b5');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.4+sha-45148da
+ * @license Angular v22.2.0-next.4+sha-d7af4b5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
