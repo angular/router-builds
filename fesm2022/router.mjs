fc4483f09f8a2a2e4951939fda07f92e28a3c1a6
Matthew Beck <me@mattrb.com>
2026-09-02 21:29:09 +0000

    0a1bbbe release: cut the zone.js-0.16.3 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.4+sha-b2903de
+ * @license Angular v22.1.4+sha-0a1bbbe
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.4+sha-b2903de');
+const VERSION = /* @__PURE__ */new Version('22.1.4+sha-0a1bbbe');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.4+sha-b2903de
+ * @license Angular v22.1.4+sha-0a1bbbe
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
