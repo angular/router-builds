485d292eb7cdbcf1c98b7999d1973c524b0eaaed
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-09-04 14:33:37 +0000

    bae170d build: update bazel dependencies

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.5+sha-74c1716
+ * @license Angular v22.2.0-next.5+sha-bae170d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.5+sha-74c1716');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.5+sha-bae170d');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.5+sha-74c1716
+ * @license Angular v22.2.0-next.5+sha-bae170d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
