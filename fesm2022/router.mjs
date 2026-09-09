5fc7f8627ee7fd0da37ed01ae2ec6d8637a135b7
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-09-09 20:42:51 +0000

    da8dac6 fix(docs-infra): highlight home page code for the resolved theme

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.6+sha-356adf7
+ * @license Angular v22.1.6+sha-da8dac6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.6+sha-356adf7');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-da8dac6');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.6+sha-356adf7
+ * @license Angular v22.1.6+sha-da8dac6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
