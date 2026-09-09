62a07b2ebe36833dbbdcd90a9ad3003dcf236aa9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 20:02:51 +0000

    81385ea release: cut the v21.2.23 release

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.22+sha-fc2e8fb
+ * @license Angular v21.2.23+sha-81385ea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.22+sha-fc2e8fb');
+const VERSION = /* @__PURE__ */new Version('21.2.23+sha-81385ea');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.22+sha-fc2e8fb
+ * @license Angular v21.2.23+sha-81385ea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
