da40b30f985d7c4893805e07ca5e46dbad1c9582
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 14:46:14 +0000

    2c71e88 docs: update cross-repo adev docs

--- types/testing.d.ts
+++ types/testing.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.5+sha-5afdd98
+ * @license Angular v22.2.0-next.5+sha-2c71e88-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
