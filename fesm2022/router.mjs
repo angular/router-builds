cc440a531a7e3ca2c3a70d2b7a14345deba1c825
Andrew Scott <atscott01@gmail.com>
2026-09-10 22:43:30 +0000

    e205075 docs: add error boundaries entries

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.7+sha-29f9dd5
+ * @license Angular v22.2.0-next.7+sha-e205075-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-29f9dd5');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-e205075-with-local-changes');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
