e30fa304d4a7cf0c5b660ea3302e0480ded74091
Andrew Scott <atscott01@gmail.com>
2026-09-10 20:33:25 +0000

    14dbbf9 fix(compiler): wrap `@for` collection expression before appending non-null assertion

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.7+sha-8ca879e
+ * @license Angular v22.2.0-next.7+sha-14dbbf9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-8ca879e');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-14dbbf9');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.7+sha-8ca879e
+ * @license Angular v22.2.0-next.7+sha-14dbbf9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
