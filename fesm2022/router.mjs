6a73ffce8dadcc1a2fea516f5fa31ef0bc0e2d45
Andrew Scott <atscott01@gmail.com>
2026-09-11 22:25:00 +0000

    376b71e docs(forms): document what required() considers empty

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.6+sha-4b5571c
+ * @license Angular v22.1.6+sha-376b71e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.6+sha-4b5571c');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-376b71e');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.6+sha-4b5571c
+ * @license Angular v22.1.6+sha-376b71e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
