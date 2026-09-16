4c55fb88f3c64c22623537c5fe8d904e00b51eb4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-09-16 14:52:43 +0000

    f73f059 docs: fix API links that point at missing member anchors

--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.6+sha-24ca9d4
+ * @license Angular v22.1.6+sha-f73f059
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1222,7 +1222,7 @@ interface UrlCreationOptions {
  *
  * @see {@link Router#navigate}
  * @see {@link Router#navigateByUrl}
- * @see {@link Router#createurltree}
+ * @see {@link Router#createUrlTree}
  * @see [Routing and Navigation guide](guide/routing/common-router-tasks)
  * @see {@link UrlCreationOptions}
  * @see {@link NavigationBehaviorOptions}
