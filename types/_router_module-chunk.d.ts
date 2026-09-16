5b4c62fa9e4048463f5dcb9feaceaf6b9b3ddfc9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-09-16 14:54:53 +0000

    312e1d8 feat(compiler-cli): add strictUnclaimedEventNames option to catch misspelled output bindings

--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.7+sha-5cdae0a
+ * @license Angular v22.2.0-next.7+sha-312e1d8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1222,7 +1222,7 @@ interface UrlCreationOptions {
  *
  * @see {@link Router#navigate}
  * @see {@link Router#navigateByUrl}
- * @see {@link Router#createurltree}
+ * @see {@link Router#createUrlTree}
  * @see [Routing and Navigation guide](guide/routing/common-router-tasks)
  * @see {@link UrlCreationOptions}
  * @see {@link NavigationBehaviorOptions}
