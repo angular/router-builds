00b80b1da5e294730c731759abd9a8265c109dcb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-09-16 13:39:21 +0000

    b4138e6 docs: show the toggle members in the i18n conditional example

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.6+sha-47afc90
+ * @license Angular v22.1.6+sha-b4138e6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.6+sha-47afc90');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-b4138e6');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.6+sha-47afc90
+ * @license Angular v22.1.6+sha-b4138e6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
