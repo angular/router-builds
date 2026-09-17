90a1a393baa7a1cb645e0c5846cb6e2cd2553861
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-17 21:25:57 +0000

    0b632de refactor(platform-browser): type shadow DOM renderer fields

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.7+sha-03754b3
+ * @license Angular v22.1.7+sha-0b632de
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.7+sha-03754b3');
+const VERSION = /* @__PURE__ */new Version('22.1.7+sha-0b632de');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.7+sha-03754b3
+ * @license Angular v22.1.7+sha-0b632de
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
