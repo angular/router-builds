4885863fed42125d54ff6c072e1c3f2019961067
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-17 16:18:32 +0000

    2cf2473 refactor(compiler): enable checkUnclaimedEventNames by default for g3.

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-rc.0+sha-80f9e3b
+ * @license Angular v22.2.0-rc.0+sha-2cf2473
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-80f9e3b');
+const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-2cf2473');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
