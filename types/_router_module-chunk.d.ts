f0fb756f7f04a898e6bf464caff8d65b0aaf89bb
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-21 17:34:31 +0000

    6688a3d fix(forms): create controls before listeners

--- types/_router_module-chunk.d.ts
+++ types/_router_module-chunk.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-e048e75-with-local-changes
+ * @license Angular v22.3.0-next.0+sha-6688a3d-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
