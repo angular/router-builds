7e77094a0173e363908c6fc31c6557764c811cdc
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 17:18:01 +0000

    aa2cabb refactor(compiler): add method to emitter context

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.7+sha-f39db77
+ * @license Angular v22.1.7+sha-aa2cabb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.7+sha-f39db77');
+const VERSION = /* @__PURE__ */new Version('22.1.7+sha-aa2cabb');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.7+sha-f39db77
+ * @license Angular v22.1.7+sha-aa2cabb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
