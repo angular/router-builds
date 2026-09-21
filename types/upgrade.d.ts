0622cdea367ab8a6647fcda9912ba3fc409efed2
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-21 18:39:22 +0000

    b0d4010 test(router): Fix broken test

--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-6688a3d-with-local-changes
+ * @license Angular v22.3.0-next.0+sha-b0d4010-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
