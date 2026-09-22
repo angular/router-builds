e1b1ce922ce28817c45e3e8dd262152bd85276bc
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-22 21:25:25 +0000

    249f55f docs: correct the adev writing guide skill

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-6cae42c
+ * @license Angular v22.3.0-next.0+sha-249f55f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-6cae42c');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-249f55f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-6cae42c
+ * @license Angular v22.3.0-next.0+sha-249f55f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
