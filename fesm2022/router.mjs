85352dc7d845d4e6b1fb50e13d000b9d7b723a0f
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 21:56:03 +0000

    59e2c2f docs(service-worker): render the SwPush and SwRegistrationOptions examples

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.7+sha-726368b
+ * @license Angular v22.1.7+sha-59e2c2f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.7+sha-726368b');
+const VERSION = /* @__PURE__ */new Version('22.1.7+sha-59e2c2f');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.7+sha-726368b
+ * @license Angular v22.1.7+sha-59e2c2f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
