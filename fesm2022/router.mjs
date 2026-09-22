43939c29dd6e27a9d4724a72213b5e15655c6d06
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 16:49:11 +0000

    5c927fb fix(vscode-extension): provide folding ranges for templates with interpolated strings

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-rc.0+sha-d0f7229
+ * @license Angular v22.2.0-rc.0+sha-5c927fb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-d0f7229');
+const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-5c927fb');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
