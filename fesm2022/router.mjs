312296cda3fd6fad63202ec8bc99b55d77d90290
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-23 15:50:32 +0000

    a2fedc1 docs(upgrade): add maintenance mode note

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.7+sha-282f9fe
+ * @license Angular v22.1.7+sha-a2fedc1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.7+sha-282f9fe');
+const VERSION = /* @__PURE__ */new Version('22.1.7+sha-a2fedc1');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/upgrade.d.ts
+++ types/upgrade.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.7+sha-282f9fe
+ * @license Angular v22.1.7+sha-a2fedc1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
