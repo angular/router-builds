7460f8311b1c4aaec7df445c732d8ebefae57cb0
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-23 17:28:46 +0000

    573293c build: update dependency mermaid to v12

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-344b10f
+ * @license Angular v22.3.0-next.0+sha-573293c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-344b10f');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-573293c');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- fesm2022/upgrade.mjs
+++ fesm2022/upgrade.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-344b10f
+ * @license Angular v22.3.0-next.0+sha-573293c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
