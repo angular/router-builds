ae50855b63d00e02cccaaf8e6d4835d0238589e3
Andrew Scott <atscott01@gmail.com>
2026-09-24 19:45:27 +0000

    21851fc fix(core): add debugName on afterRenderEffect

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0+sha-7f9c14d
+ * @license Angular v22.2.0+sha-21851fc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0+sha-7f9c14d');
+const VERSION = /* @__PURE__ */new Version('22.2.0+sha-21851fc');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0+sha-7f9c14d
+ * @license Angular v22.2.0+sha-21851fc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
