ebd80e193b1d4029c8713fc6b0643710d65cc9d3
Andrew Scott <atscott01@gmail.com>
2026-09-25 23:11:48 +0000

    deade10 docs(router): fix UrlHandlingStrategy guide link

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-3c5ed06
+ * @license Angular v22.3.0-next.0+sha-deade10
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-3c5ed06');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-deade10');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
