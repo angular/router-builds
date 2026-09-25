a21b40c9f17c32e7d313d8af183083763457d92e
GitHub <noreply@github.com>
2026-09-25 17:53:43 +0000

    5db6fc4 release: bump VSCode extension version to 22.2.0 (#70950)

--- fesm2022/router.mjs
+++ fesm2022/router.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0+sha-21851fc
+ * @license Angular v22.2.0+sha-5db6fc4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -28,7 +28,7 @@ function mapToResolve(provider) {
   return (...params) => inject(provider).resolve(...params);
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0+sha-21851fc');
+const VERSION = /* @__PURE__ */new Version('22.2.0+sha-5db6fc4');
 
 export { VERSION, mapToCanActivate, mapToCanActivateChild, mapToCanDeactivate, mapToCanMatch, mapToResolve };
 //# sourceMappingURL=router.mjs.map
